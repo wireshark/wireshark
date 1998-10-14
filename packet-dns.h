ca377bf6cd7464e2f1324ce0cc282804310dc55f
Gilbert Ramirez <gram@alumni.rice.edu>
1998-10-14 20:03:03 +0000

    Needed for common routines between DNS and NBNS.
    svn path=/trunk/; revision=56

---
+++ packet-dns.h
@@ -0,0 +1,44 @@
+/* packet-dns.h
+ * Definitions for packet disassembly structures and routines used both by
+ * DNS and NBNS.
+ *
+ * $Id: packet-dns.h,v 1.1 1998/10/14 20:03:03 gram Exp $
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@zing.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+#ifndef __PACKET_DNS_H__
+#define __PACKET_DNS_H__
+
+char *dns_class_name(int class);
+
+int get_dns_name(const u_char *dns_data_ptr, const u_char *pd, int offset,
+  char *nameptr, int maxname);
+
+#define MAXDNAME        1025            /* maximum domain name length */
+
+GtkWidget *
+add_rr_to_tree(GtkWidget *trr, int rr_type, int offset, const char *name,
+  int namelen, const char *type_name, const char *class_name, u_int ttl,
+  u_short data_len);
+
+#endif /* packet-dns.h */
+
