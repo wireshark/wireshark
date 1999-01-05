bea932a887fea4397443a9de17273ca62385a41c
hannes <hannes@localhost>
1999-01-05 00:08:49 +0000

    Finished the first version of CDP support. -> this works for all CDP packets I've got
    some minor additions will follow
    
    svn path=/trunk/; revision=156

--- packet-cdp.c
+++ packet-cdp.c
@@ -2,7 +2,7 @@
  * Routines for the disassembly of the "Cisco Discovery Protocoll"
  * (c) Copyright Hannes R. Boehm <hannes@boehm.org>
  *
- * $Id: packet-cdp.c,v 1.3 1999/01/04 21:08:45 hannes Exp $
+ * $Id: packet-cdp.c,v 1.4 1999/01/05 00:08:49 hannes Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -59,6 +59,8 @@ dissect_cdp(const u_char *pd, int offset, frame_data *fd, GtkTree *tree) {
 
     e_tlv_struct *tlv;
 	e_cdp_hdr *cdp_hdr;
+    char *stringmem;
+    gint32	mgmt_ip;
 
 
     if (check_col(fd, COL_PROTOCOL))
@@ -68,7 +70,7 @@ dissect_cdp(const u_char *pd, int offset, frame_data *fd, GtkTree *tree) {
 
     if(tree){
         ti = add_item_to_tree(GTK_WIDGET(tree), offset, (fd->cap_len - offset), 
-                                                          "Cisco Discovery Protocoll");
+                                                          "Cisco Discovery Protocol");
 	cdp_tree = gtk_tree_new(); 
 	add_subtree(ti, cdp_tree, ETT_CDP);
 	
@@ -88,28 +90,57 @@ dissect_cdp(const u_char *pd, int offset, frame_data *fd, GtkTree *tree) {
 	while( offset <= fd->cap_len ){
 		tlv = (e_tlv_struct *)  &pd[offset];
 		switch( ntohs(tlv->type) ){
+			case 0: /* ??? Mgmt Addr */
+				offset+=ntohs(tlv->length) + 4;
+				break;
 			case 1: /* ??? Chasis ID */
 				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, "Chassis ID: %s", &pd[offset+4] );
 				offset+=ntohs(tlv->length);
 				break;
+			case 2:  
+				/* this is quite strange: this tlv contains no data itself but two tlvs which
+                 * calculate the length without the 2 byte type and 2 byte length field
+                 */
+				offset+=4; 
+				break;
 			case 3: /* ??? Port  */    
-				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, "Interface: %s", &pd[offset+4] );
+				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, "Sent through Interface: %s", &pd[offset+4] );
 				offset+=ntohs(tlv->length);
 				break;
 			case 5: /* ??? IOS Version */
-				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, "IOS: %s", &pd[offset+4] );
+				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, 
+				                                       "Software Version: %s", &pd[offset+4] );
 				offset+=ntohs(tlv->length);
 				break;
-			case 0x01cc: /* ??? Mgmt Addr 
+			case 6: /* ??? platform */
+				
+				stringmem = malloc(ntohs(tlv->length));
+				bzero(stringmem, ntohs(tlv->length));
+				memcpy(stringmem, &pd[offset+4], ntohs(tlv->length) - 4 );
+				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, 
+                                                     "Platform: %s", stringmem );
+				free(stringmem);
 				offset+=ntohs(tlv->length);
 				break;
-*/
+			case 0x01cc: /* ??? Mgmt Addr */
+				memcpy(&mgmt_ip, &pd[offset+4], 4);
+				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length), 
+                                                     "Mgmt IP: %s", inet_ntoa((mgmt_ip)) );
+				offset+=ntohs(tlv->length) + 4;
+				break;
 			default:
+/*
+				if( ntohs(tlv->type) > 512){
+					dissect_data(pd, offset, fd, (GtkTree *) cdp_tree);
+					return;
+				}
+*/
 /*
 				add_item_to_tree(cdp_tree, offset, 2, "Type: %d",  ntohs(tlv->type));
 				add_item_to_tree(cdp_tree, offset + 2, 2, "Length: %d", ntohs(tlv->length));
-				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length), "Data");
+				add_item_to_tree(cdp_tree, offset + 4, ntohs(tlv->length) - 4, "Data");
 */
+
 				offset+=ntohs(tlv->length);
 		}
 
