0562b3cd20b6b98c25c564203880ed5478b2ea23
Guy Harris <guy@alum.mit.edu>
1999-05-10 20:17:36 +0000

    Get rid of the "P*" macros, as we're not using them (Ethereal only reads packets, it doesn't store into them), and make the "G*" macros use "pletohs()" and "pletohl()", making them platform-independent.
    svn path=/trunk/; revision=265

--- alignment.h
+++ alignment.h
@@ -18,19 +18,10 @@
 
 #ifndef __ALIGNMENT_H
 #define __ALIGNMENT_H
-#ifdef __i386__
 
 #define GBYTE(buf, pos)        (unsigned char)((char)buf[pos])
-#define PBYTE(buf, pos, val)   GBYTE(buf, pos) = (unsigned char)(val)
-#define GSHORT(buf, pos)       ((unsigned short *)((buf) + pos))[0]
-#define PSHORT(buf, pos, val)  GSHORT(buf, pos) = (unsigned short)(val)
-#define GSSHORT(buf, pos)      ((signed short *)((buf) + pos))[0]
+#define GSHORT(buf, pos)       pletohs(&buf[pos])
+#define GSSHORT(buf, pos)      (signed)pletohs(&buf[pos])
+#define GWORD(buf, pos)        pletohl(&buf[pos])
 
-#define GWORD(buf, pos)        ((unsigned int *)((buf) + pos))[0]
-#define PWORD(buf, pos, val)   GWORD(buf, pos) = (unsigned int)(val)
-
-#else
-
-
-#endif
 #endif
