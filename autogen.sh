16b8642f8d1190ef33946134f281d7865ee9ac90
Guy Harris <guy@alum.mit.edu>
1999-12-30 21:34:58 +0000

    Add a check for "libtool".
    Add the minimum version of {autoconf,automake,libtool} required to the
    message printed if it's not found (having it check to make sure you have
    at least that version is a bit more work).
    
    Update the paths for "ftp.gnu.org" to reflect the fact that they now give
    each package its own subdirectory.
    
    svn path=/trunk/; revision=1399

--- autogen.sh
+++ autogen.sh
@@ -2,24 +2,40 @@
 #
 # Run this to generate all the initial makefiles.
 #
-# $Id: autogen.sh,v 1.6 1999/08/31 05:51:59 gram Exp $
+# $Id: autogen.sh,v 1.7 1999/12/30 21:34:58 guy Exp $
 
 DIE=0
 PROJECT="Ethereal"
 
+#
+# XXX - we should really get the version numbers into variables, and
+# check to make sure they have a recent enough version, but I'm
+# not sure that the version strings you get are amenable to ordered
+# comparisons (e.g., I think some versions of Red Hat Linux may have
+# version numbers such as "1.4a"); that may be soluble, but it might
+# take some work.
+#
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to compile $PROJECT."
+	echo "You must have autoconf 2.13 or later installed to compile $PROJECT."
 	echo "Download the appropriate package for your distribution/OS,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/"
 	DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have automake installed to compile $PROJECT."
+	echo "You must have automake 1.4 or later installed to compile $PROJECT."
 	echo "Download the appropriate package for your distribution/OS,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake/"
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool 1.3.4 or later installed to compile $PROJECT."
+	echo "Download the appropriate package for your distribution/OS,"
+	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/"
 	DIE=1
 }
 
