f185b7716744d951554ba3794a4ed64f094d65af
Gilbert Ramirez <gram@alumni.rice.edu>
2000-06-07 02:37:06 +0000

    Move to version 0.8.9
    svn path=/trunk/; revision=2042

--- plugins.c
+++ plugins.c
@@ -1,7 +1,7 @@
 /* plugins.c
  * plugin routines
  *
- * $Id: plugins.c,v 1.17 2000/05/31 18:32:06 gram Exp $
+ * $Id: plugins.c,v 1.18 2000/06/07 02:36:59 gram Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -67,11 +67,11 @@ plugin *plugin_list;
 guint32 enabled_plugins_number;
 
 #ifdef WIN32
-static gchar std_plug_dir[] = "c:/program files/ethereal/plugins/0.8.8";
-static gchar local_plug_dir[] = "c:/ethereal/plugins/0.8.8";
+static gchar std_plug_dir[] = "c:/program files/ethereal/plugins/0.8.9";
+static gchar local_plug_dir[] = "c:/ethereal/plugins/0.8.9";
 #else
-static gchar std_plug_dir[] = "/usr/lib/ethereal/plugins/0.8.8";
-static gchar local_plug_dir[] = "/usr/local/lib/ethereal/plugins/0.8.8";
+static gchar std_plug_dir[] = "/usr/lib/ethereal/plugins/0.8.9";
+static gchar local_plug_dir[] = "/usr/local/lib/ethereal/plugins/0.8.9";
 #endif
 static gchar *user_plug_dir = NULL;
 static gchar *plugin_status_file = NULL;
