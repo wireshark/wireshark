78f877e3a5b55c516789e280b1385e456be89aaa
Uwe Girlich <Uwe.Girlich@philosys.de>
2000-07-14 12:53:00 +0000

    Heuristic dissector table added (just like tcp). Direct call to dissect_rpc() could finally disappear.
    svn path=/trunk/; revision=2138

--- packet-udp.c
+++ packet-udp.c
@@ -1,7 +1,7 @@
 /* packet-udp.c
  * Routines for UDP packet disassembly
  *
- * $Id: packet-udp.c,v 1.72 2000/05/31 05:07:52 guy Exp $
+ * $Id: packet-udp.c,v 1.73 2000/07/14 12:53:00 girlich Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -48,7 +48,7 @@
 #include "packet-udp.h"
 
 #include "packet-ip.h"
-#include "packet-rpc.h"
+#include "conversation.h"
 #include "packet-tftp.h"
 #include "packet-vines.h"
 
@@ -76,6 +76,7 @@ typedef struct _e_udphdr {
 #define UDP_PORT_VINES	573
 
 static dissector_table_t udp_dissector_table;
+static heur_dissector_list_t heur_subdissector_list;
 
 /* Determine if there is a sub-dissector and call it.  This has been */
 /* separated into a stand alone routine to other protocol dissectors */
@@ -95,12 +96,6 @@ decode_udp_ports( const u_char *pd, int offset, frame_data *fd,
 	(sub_dissector)(pd, offset, fd, tree);
 	return;
   }
-
-  /* ONC RPC.  We can't base this on anything in the UDP header; we have
-     to look at the payload.  If "dissect_rpc()" returns TRUE, it was
-     an RPC packet, otherwise it's some other type of packet. */
-  if (dissect_rpc(pd, offset, fd, tree))
-    return;
 
   /* try to apply the plugins */
 #ifdef HAVE_PLUGINS
@@ -123,6 +118,10 @@ decode_udp_ports( const u_char *pd, int offset, frame_data *fd,
   /* do lookup with the subdissector table */
   if (dissector_try_port(udp_dissector_table, uh_sport, pd, offset, fd, tree) ||
       dissector_try_port(udp_dissector_table, uh_dport, pd, offset, fd, tree))
+    return;
+
+  /* do lookup with the heuristic subdissector table */
+  if (dissector_try_heuristic(heur_subdissector_list, pd, offset, fd, tree))
     return;
 
   /* XXX - we should do these with the subdissector table as well. */
@@ -227,6 +226,7 @@ proto_register_udp(void)
 
 /* subdissector code */
 	udp_dissector_table = register_dissector_table("udp.port");
+	register_heur_dissector_list("udp", &heur_subdissector_list);
 }
 
 void
