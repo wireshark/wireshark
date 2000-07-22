9a1e130403dfd5f655270e9844725a63077839c5
Guy Harris <guy@alum.mit.edu>
2000-07-22 20:00:29 +0000

    Improved "autogen.sh" script from Bruce Korb.
    svn path=/trunk/; revision=2154

--- autogen.sh
+++ autogen.sh
@@ -2,48 +2,60 @@
 #
 # Run this to generate all the initial makefiles.
 #
-# $Id: autogen.sh,v 1.8 2000/02/11 03:17:43 gram Exp $
+# $Id: autogen.sh,v 1.9 2000/07/22 20:00:21 guy Exp $
 
-DIE=0
+DIE=true
 PROJECT="Ethereal"
 
-#
-# XXX - we should really get the version numbers into variables, and
-# check to make sure they have a recent enough version, but I'm
-# not sure that the version strings you get are amenable to ordered
-# comparisons (e.g., I think some versions of Red Hat Linux may have
-# version numbers such as "1.4a"); that may be soluble, but it might
-# take some work.
-#
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf 2.13 or later installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution/OS,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/"
-	DIE=1
-}
+VER=`autoconf --version | grep '^autoconf' | sed 's/.*)//'`
+case "$VER" in
+0* | 1\.* | 2\.[0-9] | 2\.[0-9][a-z]* | \
+2\.1[0-2] | 2\.1[0-2][a-z]* )
+  cat >&2 <<_EOF_
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake 1.4 or later installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution/OS,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake/"
-	DIE=1
-}
+	You must have autoconf 2.13 or later installed to compile $PROJECT.
+	Download the appropriate package for your distribution/OS,
+	or get the source tarball at ftp://ftp.gnu.org/pub/gnu/autoconf/
+_EOF_
+  DIE="exit 1"
+  ;;
+esac
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool 1.3.3 or later installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution/OS,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/"
-	DIE=1
-}
 
-if test "$DIE" -eq 1 ; then
-	exit 1
-fi
+VER=`automake --version | grep '^automake' | sed 's/.*)//'`
+case "$VER" in
+0* | 1\.[0-3] | 1\.[0-3][a-z]* )
 
-libtoolize --copy --force
+  cat >&2 <<_EOF_
+
+	You must have automake 1.4 or later installed to compile $PROJECT.
+	Download the appropriate package for your distribution/OS,
+	or get the source tarball at ftp://ftp.gnu.org/pub/gnu/automake/
+_EOF_
+  DIE="exit 1"
+  ;;
+esac
+
+
+VER=`libtool --version | grep ' libtool)' | \
+sed 's/.*) \([0-9][0-9.]*\) .*/\1/' `
+case "$VER" in
+0* | 1\.[0-2] | 1\.[0-2][a-z]* | \
+1\.3\.[0-2] | 1\.3\.[0-2][a-z]* )
+
+  cat >&2 <<_EOF_
+
+	You must have libtool 1.3.3 or later installed to compile $PROJECT.
+	Download the appropriate package for your distribution/OS,
+	or get the source tarball at ftp://ftp.gnu.org/pub/gnu/libtool/
+_EOF_
+  DIE="exit 1"
+  ;;
+esac
+
+$DIE
+
+libtoolize --copy --force || exit 1
 
 if test -z "$*"; then
 	echo "Running ./configure with no arguments. If you wish to pass any,"
@@ -51,14 +63,22 @@ if test -z "$*"; then
 fi
 
 for dir in . wiretap ;  do
-	echo processing $dir
-	(cd $dir; \
-		aclocalinclude="$ACLOCAL_FLAGS"; \
-		aclocal $aclocalinclude; \
-		autoheader; automake --add-missing --gnu $am_opt; autoconf)
+  echo processing $dir
+  (
+    cd $dir
+    aclocalinclude="$ACLOCAL_FLAGS"; \
+    echo aclocal $aclocalinclude
+    aclocal $aclocalinclude || exit 1
+    echo autoheader
+    autoheader || exit 1
+    echo automake --add-missing --gnu $am_opt
+    automake --add-missing --gnu $am_opt || exit 1
+    echo autoconf
+    autoconf || exit 1
+  ) || exit 1
 done
 
-./configure "$@"
+./configure "$@" || exit 1
 
 echo
 echo "Now type 'make' to compile $PROJECT."
