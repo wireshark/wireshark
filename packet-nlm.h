bfb39fbc44871150b8cb787fb5a9148d3c13afce
Uwe Girlich <Uwe.Girlich@philosys.de>
2000-08-02 11:32:31 +0000

    Procedure numbers as constants are much better.
    svn path=/trunk/; revision=2194

--- packet-nlm.h
+++ packet-nlm.h
@@ -1,10 +1,40 @@
 /* packet-nlm.h (c) 1999 Uwe Girlich */
-/* $Id: packet-nlm.h,v 1.1 1999/11/15 14:17:19 nneul Exp $ */
+/* $Id: packet-nlm.h,v 1.2 2000/08/02 11:32:31 girlich Exp $ */
 
 #ifndef __PACKET_NLM_H__
 #define __PACKET_NLM_H__
 
 #define NLM_PROGRAM 100021
+
+/* synchronous procedures */
+#define NLM_NULL		0
+#define NLM_TEST		1
+#define NLM_LOCK		2
+#define NLM_CANCEL		3
+#define NLM_UNLOCK		4
+#define NLM_GRANTED		5
+
+/* asynchronous requests */
+#define NLM_TEST_MSG		6
+#define NLM_LOCK_MSG		7
+#define NLM_CANCEL_MSG		8
+#define NLM_UNLOCK_MSG		9
+#define NLM_GRANTED_MSG		10
+
+/* asynchronous responses */
+#define NLM_TEST_RES		11
+#define NLM_LOCK_RES		12
+#define NLM_CANCEL_RES		13
+#define NLM_UNLOCK_RES		14
+#define NLM_GRANTED_RES		15
+
+/* 16-19 not assigned */
+
+/* DOS file sharing */
+#define NLM_SHARE		20
+#define NLM_UNSHARE		21
+#define NLM_NM_LOCK		22
+#define NLM_FREE_ALL		23
 
 #endif /* packet-nlm.h */
 
