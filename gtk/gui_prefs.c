af093a380e400c35f2ee5f466a70f99dfb7cc4a8
Guy Harris <guy@alum.mit.edu>
2000-08-21 22:30:04 +0000

    Don't set the "prefs.gui_XXX" values until the "fetch" routine for the GUI preferences is called.
    Pull all the code for creating option menus for enumerated
    "prefs.gui_XXX" items into a common routine.
    
    svn path=/trunk/; revision=2328

--- gtk/gui_prefs.c
+++ gtk/gui_prefs.c
@@ -1,7 +1,7 @@
 /* gui_prefs.c
  * Dialog box for GUI preferences
  *
- * $Id: gui_prefs.c,v 1.7 2000/08/21 08:09:11 guy Exp $
+ * $Id: gui_prefs.c,v 1.8 2000/08/21 22:30:04 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -33,29 +33,61 @@
 #include "gui_prefs.h"
 #include "gtkglobals.h"
 #include "prefs_dlg.h"
+#include "prefs.h"
+#include "prefs-int.h"
 #include "ui_util.h"
 #include "dlg_utils.h"
 #include "proto_draw.h"
 
-static void scrollbar_menu_item_cb(GtkWidget *w, gpointer data);
-static void plist_sel_browse_cb(GtkWidget *w, gpointer data);
-static void ptree_sel_browse_cb(GtkWidget *w, gpointer data);
-static void ptree_line_style_cb(GtkWidget *w, gpointer data);
-static void ptree_expander_style_cb(GtkWidget *w, gpointer data);
+static void create_option_menu(GtkWidget *main_vb, const gchar *key,
+    GtkWidget *main_tb, int table_position,
+    const gchar *label_text, const enum_val *enumvals, gint current_val);
 static void font_browse_cb(GtkWidget *w, gpointer data);
 static void font_browse_ok_cb(GtkWidget *w, GtkFontSelectionDialog *fs);
 static void font_browse_destroy(GtkWidget *win, gpointer data);
+static gint fetch_enum_value(gpointer control, const enum_val *enumvals);
 
-#define E_FONT_DIALOG_PTR_KEY	"font_dialog_ptr"
-#define E_FONT_CALLER_PTR_KEY	"font_caller_ptr"
+#define SCROLLBAR_PLACEMENT_KEY		"scrollbar_placement"
+#define PLIST_SEL_BROWSE_KEY		"plist_sel_browse"
+#define PTREE_SEL_BROWSE_KEY		"ptree_sel_browse"
+#define PTREE_LINE_STYLE_KEY		"ptree_line_style"
+#define PTREE_EXPANDER_STYLE_KEY	"ptree_expander_style"
+
+#define FONT_DIALOG_PTR_KEY	"font_dialog_ptr"
+#define FONT_CALLER_PTR_KEY	"font_caller_ptr"
+
+static const enum_val scrollbar_placement_vals[] = {
+	{ "Left",  FALSE },
+	{ "Right", TRUE },
+	{ NULL,    0 }
+};
+
+static const enum_val selection_mode_vals[] = {
+	{ "Selects", FALSE },
+	{ "Browses", TRUE },
+	{ NULL,      0 }
+};
+
+static const enum_val line_style_vals[] = {
+	{ "None",   0 },
+	{ "Solid",  1 },
+	{ "Dotted", 2 },
+	{ "Tabbed", 3 },
+	{ NULL,     0 }
+};
+
+static const enum_val expander_style_vals[] = {
+	{ "None",     0 },
+	{ "Square",   1 },
+	{ "Triangle", 2 },
+	{ "Circular", 3 },
+	{ NULL,       0 }
+};
 
 GtkWidget*
 gui_prefs_show(void)
 {
-	GtkWidget	*main_tb, *main_vb, *label;
-	GtkWidget	*menu_item_false, *menu_item_true,
-			*menu_item_0, *menu_item_1, *menu_item_2, *menu_item_3,
-			*scrollbar_menu, *scrollbar_option_menu, *font_bt;
+	GtkWidget	*main_tb, *main_vb, *font_bt;
 
 	/* Main vertical box */
 	main_vb = gtk_vbox_new(FALSE, 5);
@@ -67,157 +99,30 @@ gui_prefs_show(void)
 	gtk_table_set_row_spacings( GTK_TABLE(main_tb), 10 );
 	gtk_table_set_col_spacings( GTK_TABLE(main_tb), 15 );
 
-
-	/* Scrollbar placment */
-	label = gtk_label_new("Vertical Scrollbar Placement:");
-	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), label, 0, 1, 0, 1 );
-
-	/* Create a simple menu containing the LEFT/RIGHT choices for
-	 * the scrollbar placement option */
-	scrollbar_menu = gtk_menu_new();
-	menu_item_false  = gtk_menu_item_new_with_label("Left");
-	menu_item_true = gtk_menu_item_new_with_label("Right");
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_false );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_true );
-
-	gtk_signal_connect( GTK_OBJECT(menu_item_false), "activate",
-			scrollbar_menu_item_cb, GINT_TO_POINTER(FALSE) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_true), "activate",
-			scrollbar_menu_item_cb, GINT_TO_POINTER(TRUE) );
-
-	/* Create the option menu from the option */
-	scrollbar_option_menu = gtk_option_menu_new();
-	gtk_option_menu_set_menu( GTK_OPTION_MENU(scrollbar_option_menu),
-			scrollbar_menu );
-	gtk_option_menu_set_history( GTK_OPTION_MENU(scrollbar_option_menu),
-			prefs.gui_scrollbar_on_right);
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), scrollbar_option_menu,
-			1, 2, 0, 1 );
-
+	/* Scrollbar placement */
+	create_option_menu(main_vb, SCROLLBAR_PLACEMENT_KEY, main_tb, 0,
+	    "Vertical Scrollbar Placement:", scrollbar_placement_vals,
+	    prefs.gui_scrollbar_on_right);
 
 	/* Packet list selection browseable */
-	label = gtk_label_new("Packet-list selection bar movement:");
-	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), label, 0, 1, 1, 2 );
-
-	/* Create a simple menu containing the LEFT/RIGHT choices */
-	scrollbar_menu = gtk_menu_new();
-	menu_item_false  = gtk_menu_item_new_with_label("Selects");
-	menu_item_true = gtk_menu_item_new_with_label("Browses");
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_false );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_true );
-
-	gtk_signal_connect( GTK_OBJECT(menu_item_false), "activate",
-			plist_sel_browse_cb, GINT_TO_POINTER(FALSE) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_true), "activate",
-			plist_sel_browse_cb, GINT_TO_POINTER(TRUE) );
-
-	/* Create the option menu from the option */
-	scrollbar_option_menu = gtk_option_menu_new();
-	gtk_option_menu_set_menu( GTK_OPTION_MENU(scrollbar_option_menu),
-			scrollbar_menu );
-	gtk_option_menu_set_history( GTK_OPTION_MENU(scrollbar_option_menu),
-			prefs.gui_plist_sel_browse);
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), scrollbar_option_menu,
-			1, 2, 1, 2 );
-
+	create_option_menu(main_vb, PLIST_SEL_BROWSE_KEY, main_tb, 1,
+	    "Packet-list selection bar movement:", selection_mode_vals,
+	    prefs.gui_plist_sel_browse);
 
 	/* Proto tree selection browseable */
-	label = gtk_label_new("Protocol-tree selection bar movement:");
-	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), label, 0, 1, 2, 3 );
-
-	/* Create a simple menu containing the LEFT/RIGHT choices */
-	scrollbar_menu = gtk_menu_new();
-	menu_item_false  = gtk_menu_item_new_with_label("Selects");
-	menu_item_true = gtk_menu_item_new_with_label("Browses");
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_false );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_true );
-
-	gtk_signal_connect( GTK_OBJECT(menu_item_false), "activate",
-			ptree_sel_browse_cb, GINT_TO_POINTER(FALSE) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_true), "activate",
-			ptree_sel_browse_cb, GINT_TO_POINTER(TRUE) );
-
-	/* Create the option menu from the option */
-	scrollbar_option_menu = gtk_option_menu_new();
-	gtk_option_menu_set_menu( GTK_OPTION_MENU(scrollbar_option_menu),
-			scrollbar_menu );
-	gtk_option_menu_set_history( GTK_OPTION_MENU(scrollbar_option_menu),
-			prefs.gui_ptree_sel_browse);
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), scrollbar_option_menu,
-			1, 2, 2, 3 );
-
+	create_option_menu(main_vb, PTREE_SEL_BROWSE_KEY, main_tb, 2,
+	    "Protocol-tree selection bar movement:", selection_mode_vals,
+	    prefs.gui_ptree_sel_browse);
 
 	/* Proto tree line style */
-	label = gtk_label_new("Protocol-tree line style:");
-	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), label, 0, 1, 3, 4 );
-
-	/* Create a menu */
-	scrollbar_menu = gtk_menu_new();
-	menu_item_0 = gtk_menu_item_new_with_label("None");
-	menu_item_1 = gtk_menu_item_new_with_label("Solid");
-	menu_item_2 = gtk_menu_item_new_with_label("Dotted");
-	menu_item_3 = gtk_menu_item_new_with_label("Tabbed");
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_0 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_1 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_2 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_3 );
-
-	gtk_signal_connect( GTK_OBJECT(menu_item_0), "activate",
-			ptree_line_style_cb, GINT_TO_POINTER(0) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_1), "activate",
-			ptree_line_style_cb, GINT_TO_POINTER(1) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_2), "activate",
-			ptree_line_style_cb, GINT_TO_POINTER(2) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_3), "activate",
-			ptree_line_style_cb, GINT_TO_POINTER(3) );
-
-	/* Create the option menu from the option */
-	scrollbar_option_menu = gtk_option_menu_new();
-	gtk_option_menu_set_menu( GTK_OPTION_MENU(scrollbar_option_menu),
-			scrollbar_menu );
-	gtk_option_menu_set_history( GTK_OPTION_MENU(scrollbar_option_menu),
-			prefs.gui_ptree_line_style);
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), scrollbar_option_menu,
-			1, 2, 3, 4 );
-
+	create_option_menu(main_vb, PTREE_LINE_STYLE_KEY, main_tb, 3,
+	    "Protocol-tree line style:", line_style_vals,
+	    prefs.gui_ptree_line_style);
 
 	/* Proto tree expander style */
-	label = gtk_label_new("Protocol-tree expander style:");
-	gtk_misc_set_alignment( GTK_MISC(label), 1.0, 0.5 );
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), label, 0, 1, 4, 5 );
-
-	/* Create a menu */
-	scrollbar_menu = gtk_menu_new();
-	menu_item_0 = gtk_menu_item_new_with_label("None");
-	menu_item_1 = gtk_menu_item_new_with_label("Square");
-	menu_item_2 = gtk_menu_item_new_with_label("Triangle");
-	menu_item_3 = gtk_menu_item_new_with_label("Circular");
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_0 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_1 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_2 );
-	gtk_menu_append( GTK_MENU(scrollbar_menu), menu_item_3 );
-
-	gtk_signal_connect( GTK_OBJECT(menu_item_0), "activate",
-			ptree_expander_style_cb, GINT_TO_POINTER(0) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_1), "activate",
-			ptree_expander_style_cb, GINT_TO_POINTER(1) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_2), "activate",
-			ptree_expander_style_cb, GINT_TO_POINTER(2) );
-	gtk_signal_connect( GTK_OBJECT(menu_item_3), "activate",
-			ptree_expander_style_cb, GINT_TO_POINTER(3) );
-
-	/* Create the option menu from the option */
-	scrollbar_option_menu = gtk_option_menu_new();
-	gtk_option_menu_set_menu( GTK_OPTION_MENU(scrollbar_option_menu),
-			scrollbar_menu );
-	gtk_option_menu_set_history( GTK_OPTION_MENU(scrollbar_option_menu),
-			prefs.gui_ptree_expander_style);
-	gtk_table_attach_defaults( GTK_TABLE(main_tb), scrollbar_option_menu,
-			1, 2, 4, 5 );
+	create_option_menu(main_vb, PTREE_EXPANDER_STYLE_KEY, main_tb, 4,
+	    "Protocol-tree expander style:", expander_style_vals,
+	    prefs.gui_ptree_expander_style);
 
 	/* "Font..." button - click to open a font selection dialog box. */
 	font_bt = gtk_button_new_with_label("Font...");
@@ -231,50 +136,44 @@ gui_prefs_show(void)
 	return(main_vb);
 }
 
-
 static void
-scrollbar_menu_item_cb(GtkWidget *w, gpointer data)
+create_option_menu(GtkWidget *main_vb, const gchar *key,
+    GtkWidget *main_tb, int table_position,
+    const gchar *label_text, const enum_val *enumvals, gint current_val)
 {
-	gboolean	value = GPOINTER_TO_INT(data);
+	GtkWidget *label, *menu, *menu_item, *option_menu;
+	int menu_index, index;
+	const enum_val *enum_valp;
 
-	prefs.gui_scrollbar_on_right = value;
+	label = gtk_label_new(label_text);
+	gtk_misc_set_alignment(GTK_MISC(label), 1.0, 0.5);
+	gtk_table_attach_defaults(GTK_TABLE(main_tb), label, 0, 1,
+	    table_position, table_position + 1);
+
+	menu = gtk_menu_new();
+	menu_index = -1;
+	for (enum_valp = enumvals, index = 0;
+	    enum_valp->name != NULL; enum_valp++, index++) {
+		menu_item = gtk_menu_item_new_with_label(enum_valp->name);
+		gtk_menu_append(GTK_MENU(menu), menu_item);
+		if (enum_valp->value == current_val)
+			menu_index = index;
+	}
+
+	/* Create the option menu from the option */
+	option_menu = gtk_option_menu_new();
+	gtk_option_menu_set_menu(GTK_OPTION_MENU(option_menu), menu);
+
+	/* Set its current value to the variable's current value */
+	if (menu_index != -1)
+		gtk_option_menu_set_history(GTK_OPTION_MENU(option_menu),
+		    menu_index);
+
+	gtk_table_attach_defaults(GTK_TABLE(main_tb), option_menu,
+	    1, 2, table_position, table_position + 1);
+
+	gtk_object_set_data(GTK_OBJECT(main_vb), key, option_menu);
 }
-
-static void
-plist_sel_browse_cb(GtkWidget *w, gpointer data)
-{
-	gboolean	value = GPOINTER_TO_INT(data);
-
-	prefs.gui_plist_sel_browse = value;
-}
-
-static void
-ptree_sel_browse_cb(GtkWidget *w, gpointer data)
-{
-	gboolean	value = GPOINTER_TO_INT(data);
-
-	prefs.gui_ptree_sel_browse = value;
-}
-
-static void
-ptree_line_style_cb(GtkWidget *w, gpointer data)
-{
-	gint	value = GPOINTER_TO_INT(data);
-
-	prefs.gui_ptree_line_style = value;
-}
-
-static void
-ptree_expander_style_cb(GtkWidget *w, gpointer data)
-{
-	gint	value = GPOINTER_TO_INT(data);
-
-	prefs.gui_ptree_expander_style = value;
-}
-
-/* XXX - need a way to set this on the fly, so that a font change takes
-   effect immediately, rather than requiring the user to exit and restart
-   Ethereal. */
 
 /* Create a font dialog for browsing. */
 static void
@@ -287,7 +186,7 @@ font_browse_cb(GtkWidget *w, gpointer data)
 	/* Has a font dialog box already been opened for that top-level
 	   widget? */
 	font_browse_w = gtk_object_get_data(GTK_OBJECT(caller),
-	    E_FONT_DIALOG_PTR_KEY);
+	    FONT_DIALOG_PTR_KEY);
 
 	if (font_browse_w != NULL) {
 		/* Yes.  Just re-activate that dialog box. */
@@ -322,13 +221,13 @@ font_browse_cb(GtkWidget *w, gpointer data)
 	gtk_font_selection_dialog_set_font_name(
 	    GTK_FONT_SELECTION_DIALOG(font_browse_w), prefs.gui_font_name);
 
-	/* Set the E_FONT_CALLER_PTR_KEY for the new dialog to point to
+	/* Set the FONT_CALLER_PTR_KEY for the new dialog to point to
 	   our caller. */
-	gtk_object_set_data(GTK_OBJECT(font_browse_w), E_FONT_CALLER_PTR_KEY,
+	gtk_object_set_data(GTK_OBJECT(font_browse_w), FONT_CALLER_PTR_KEY,
 	    caller);
 
-	/* Set the E_FONT_DIALOG_PTR_KEY for the caller to point to us */
-	gtk_object_set_data(GTK_OBJECT(caller), E_FONT_DIALOG_PTR_KEY,
+	/* Set the FONT_DIALOG_PTR_KEY for the caller to point to us */
+	gtk_object_set_data(GTK_OBJECT(caller), FONT_DIALOG_PTR_KEY,
 	    font_browse_w);
   
 	/* Connect the ok_button to font_browse_ok_cb function and pass along a
@@ -373,11 +272,11 @@ font_browse_destroy(GtkWidget *win, gpointer data)
 	/* Get the widget that requested that we be popped up, if any.
 	   (It should arrange to destroy us if it's destroyed, so
 	   that we don't get a pointer to a non-existent window here.) */
-	caller = gtk_object_get_data(GTK_OBJECT(win), E_FONT_CALLER_PTR_KEY);
+	caller = gtk_object_get_data(GTK_OBJECT(win), FONT_CALLER_PTR_KEY);
 
 	if (caller != NULL) {
 		/* Tell it we no longer exist. */
-		gtk_object_set_data(GTK_OBJECT(caller), E_FONT_DIALOG_PTR_KEY,
+		gtk_object_set_data(GTK_OBJECT(caller), FONT_DIALOG_PTR_KEY,
 		    NULL);
 	}
 
@@ -386,9 +285,39 @@ font_browse_destroy(GtkWidget *win, gpointer data)
 	gtk_widget_destroy(GTK_WIDGET(win));
 }
 
+static gint
+fetch_enum_value(gpointer control, const enum_val *enumvals)
+{
+	GtkWidget *label;
+	char *label_string;
+
+	/* Get the label for the currently active entry in the option menu.
+	   Yes, this is how you do it.  See FAQ 6.8 in the GTK+ FAQ. */
+	label = GTK_BIN(control)->child;
+
+	/* Get the label string, and translate it to a value. */
+	gtk_label_get(GTK_LABEL(label), &label_string);
+	return find_val_for_string(label_string, enumvals, 1);
+}
+
 void
 gui_prefs_fetch(GtkWidget *w)
 {
+	prefs.gui_scrollbar_on_right = fetch_enum_value(
+	    gtk_object_get_data(GTK_OBJECT(w), SCROLLBAR_PLACEMENT_KEY),
+	    scrollbar_placement_vals);
+	prefs.gui_plist_sel_browse = fetch_enum_value(
+	    gtk_object_get_data(GTK_OBJECT(w), PLIST_SEL_BROWSE_KEY),
+	    selection_mode_vals);
+	prefs.gui_ptree_sel_browse = fetch_enum_value(
+	    gtk_object_get_data(GTK_OBJECT(w), PTREE_SEL_BROWSE_KEY),
+	    selection_mode_vals);
+	prefs.gui_ptree_line_style = fetch_enum_value(
+	    gtk_object_get_data(GTK_OBJECT(w), PTREE_LINE_STYLE_KEY),
+	    line_style_vals);
+	prefs.gui_ptree_expander_style = fetch_enum_value(
+	    gtk_object_get_data(GTK_OBJECT(w), PTREE_EXPANDER_STYLE_KEY),
+	    expander_style_vals);
 }
 
 void
@@ -427,7 +356,7 @@ gui_prefs_destroy(GtkWidget *w)
 
 	/* Is there a font selection dialog associated with this
 	   Preferences dialog? */
-	fs = gtk_object_get_data(GTK_OBJECT(caller), E_FONT_DIALOG_PTR_KEY);
+	fs = gtk_object_get_data(GTK_OBJECT(caller), FONT_DIALOG_PTR_KEY);
 
 	if (fs != NULL) {
 		/* Yes.  Destroy it. */
