4386002cc34d6a37e5f3f483c57efabe6359e771
Gilbert Ramirez <gram@alumni.rice.edu>
2000-10-27 02:22:08 +0000

    Move to version 0.8.13. Mention IGRP dissector in NEWS.
    svn path=/trunk/; revision=2539

--- epan/plugins.c
+++ epan/plugins.c
@@ -1,7 +1,7 @@
 /* plugins.c
  * plugin routines
  *
- * $Id: plugins.c,v 1.3 2000/10/16 23:17:39 guy Exp $
+ * $Id: plugins.c,v 1.4 2000/10/27 02:22:07 gram Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -74,11 +74,11 @@ plugin *plugin_list;
 guint32 enabled_plugins_number;
 
 #ifdef WIN32
-static gchar std_plug_dir[] = "c:/program files/ethereal/plugins/0.8.12";
-static gchar local_plug_dir[] = "c:/ethereal/plugins/0.8.12";
+static gchar std_plug_dir[] = "c:/program files/ethereal/plugins/0.8.13";
+static gchar local_plug_dir[] = "c:/ethereal/plugins/0.8.13";
 #else
-static gchar std_plug_dir[] = "/usr/lib/ethereal/plugins/0.8.12";
-static gchar local_plug_dir[] = "/usr/local/lib/ethereal/plugins/0.8.12";
+static gchar std_plug_dir[] = "/usr/lib/ethereal/plugins/0.8.13";
+static gchar local_plug_dir[] = "/usr/local/lib/ethereal/plugins/0.8.13";
 #endif
 static gchar *user_plug_dir = NULL;
 static gchar *plugin_status_file = NULL;
