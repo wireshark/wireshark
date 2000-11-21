f8d8ac9df63e9566582aca55fd658252475bf5b2
Guy Harris <guy@alum.mit.edu>
2000-11-21 23:50:56 +0000

    Tethereal includes no GUI stuff, so it doesn't need to include "ui_util.h".
    svn path=/trunk/; revision=2691

--- tethereal.c
+++ tethereal.c
@@ -1,6 +1,6 @@
 /* tethereal.c
  *
- * $Id: tethereal.c,v 1.58 2000/11/20 17:08:20 gram Exp $
+ * $Id: tethereal.c,v 1.59 2000/11/21 23:50:56 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
@@ -87,7 +87,6 @@
 #include "print.h"
 #include "resolv.h"
 #include "util.h"
-#include "ui_util.h"
 #include "conversation.h"
 #include "plugins.h"
 
