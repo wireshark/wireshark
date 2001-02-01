96e0398fc6e3d3dbe8a9d1bd0dbad3f2cf9d9a77
Gilbert Ramirez <gram@alumni.rice.edu>
2001-02-01 20:31:21 +0000

    Grumble, grumble. I forgot to add the license comment at the top of these files.
    svn path=/trunk/; revision=2968

--- epan/gdebug.h
+++ epan/gdebug.h
@@ -1,4 +1,28 @@
-/* $Id: gdebug.h,v 1.1 2001/02/01 20:21:16 gram Exp $ */
+/* gdebug.h
+ *
+ * Useful macro for use during development.
+ *
+ * $Id: gdebug.h,v 1.2 2001/02/01 20:31:17 gram Exp $
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@zing.org>
+ * Copyright 2001 Gerald Combs
+ *
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 #ifndef GDEBUG_H
 #define GDEBUG_H
