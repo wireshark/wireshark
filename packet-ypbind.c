0fa63e3baf23d875f85df2039a43945e14840ec8
Guy Harris <guy@alum.mit.edu>
2001-12-29 22:23:07 +0000

    Dissection of ypbind calls, from Ronnie Sahlberg.
    svn path=/trunk/; revision=4461

--- packet-ypbind.c
+++ packet-ypbind.c
@@ -1,13 +1,15 @@
 /* packet-ypbind.c
  * Routines for ypbind dissection
  *
- * $Id: packet-ypbind.c,v 1.7 2001/01/18 09:55:10 guy Exp $
+ * $Id: packet-ypbind.c,v 1.8 2001/12/29 22:23:07 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@zing.org>
  * Copyright 1998 Gerald Combs
  *
  * Copied from packet-smb.c
+ *
+ *    2001  Ronnie Sahlberg, added dissectors for the commands
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -38,8 +40,108 @@
 #include "packet-ypbind.h"
 
 static int proto_ypbind = -1;
+static int hf_ypbind_domain = -1;
+static int hf_ypbind_resp_type = -1;
+static int hf_ypbind_error = -1;
+static int hf_ypbind_addr = -1;
+static int hf_ypbind_port = -1;
+static int hf_ypbind_setdom_version = -1;
 
 static gint ett_ypbind = -1;
+
+
+static int
+dissect_ypbind_domain_v2_request(tvbuff_t *tvb, int offset, packet_info *pinfo, proto_tree *tree)
+{
+	/* domain */
+	offset = dissect_rpc_string(tvb, pinfo, tree, 
+			hf_ypbind_domain, offset, NULL);
+
+	return offset;
+}
+
+#define YPBIND_RESP_TYPE_SUCC_VAL	1
+#define YPBIND_RESP_TYPE_FAIL_VAL	2
+static const value_string resp_type_vals[] = {
+	{YPBIND_RESP_TYPE_SUCC_VAL,	"SUCC_VAL"},
+	{YPBIND_RESP_TYPE_FAIL_VAL,	"FAIL_VAL"},
+	{0, NULL}
+};
+
+#define YPBIND_ERROR_ERR	1
+#define YPBIND_ERROR_NOSERV	2
+#define YPBIND_ERROR_RESC	3
+static const value_string error_vals[] = {
+	{YPBIND_ERROR_ERR,	"Internal error"},
+	{YPBIND_ERROR_NOSERV,	"No bound server for passed domain"},
+	{YPBIND_ERROR_RESC,	"System resource allocation failure"},
+	{0, NULL}
+};
+
+static int
+dissect_ypbind_domain_v2_reply(tvbuff_t *tvb, int offset, packet_info *pinfo, proto_tree *tree)
+{
+	guint32 type;
+	guint32 tid;
+
+	/* response type */
+	type=tvb_get_ntohl(tvb, offset);
+	offset = dissect_rpc_uint32(tvb, pinfo, tree, hf_ypbind_resp_type, offset);
+
+	switch(type){
+	case YPBIND_RESP_TYPE_SUCC_VAL:
+		/* ip address */
+		tid=tvb_get_ntohl(tvb,offset);
+		tid=((tid&0x000000ff)<<24)|((tid&0x0000ff00)<<8)
+			|((tid&0x00ff0000)>>8)|((tid&0xff000000)>>24);
+		proto_tree_add_ipv4(tree, hf_ypbind_addr, 
+			tvb, offset, 4, tid);
+		offset += 4;
+
+		/* port */
+		offset = dissect_rpc_uint32(tvb, pinfo, tree, 
+				hf_ypbind_port, offset);
+		
+		break;
+	case YPBIND_RESP_TYPE_FAIL_VAL:
+		/* error */
+		offset = dissect_rpc_uint32(tvb, pinfo, tree, 
+				hf_ypbind_resp_type, offset);
+		break;
+	}
+
+	return offset;
+}
+
+static int
+dissect_ypbind_setdomain_v2_request(tvbuff_t *tvb, int offset, packet_info *pinfo, proto_tree *tree)
+{
+	guint32 tid;
+
+	/* domain */
+	offset = dissect_rpc_string(tvb, pinfo, tree, 
+			hf_ypbind_domain, offset, NULL);
+
+	/* ip address */
+	tid=tvb_get_ntohl(tvb,offset);
+	tid=((tid&0x000000ff)<<24)|((tid&0x0000ff00)<<8)
+		|((tid&0x00ff0000)>>8)|((tid&0xff000000)>>24);
+	proto_tree_add_ipv4(tree, hf_ypbind_addr, 
+		tvb, offset, 4, tid);
+	offset += 4;
+
+	/* port */
+	offset = dissect_rpc_uint32(tvb, pinfo, tree, 
+			hf_ypbind_port, offset);
+		
+	/* version */
+	offset = dissect_rpc_uint32(tvb, pinfo, tree, 
+			hf_ypbind_setdom_version, offset);
+		
+	return offset;
+}
+
+
 
 /* proc number, "proc name", dissect_request, dissect_reply */
 /* NULL as function pointer means: type of arguments is "void". */
@@ -53,8 +155,10 @@ static const vsff ypbind1_proc[] = {
 
 static const vsff ypbind2_proc[] = {
 	{ YPBINDPROC_NULL,	"NULL",		NULL,				NULL },
-	{ YPBINDPROC_DOMAIN,	"DOMAIN",		NULL,				NULL },
-	{ YPBINDPROC_SETDOM,	"SETDOMAIN",		NULL,				NULL },
+	{ YPBINDPROC_DOMAIN,	"DOMAIN",	
+		dissect_ypbind_domain_v2_request, dissect_ypbind_domain_v2_reply},
+	{ YPBINDPROC_SETDOM,	"SETDOMAN",
+		dissect_ypbind_setdomain_v2_request, NULL},
 	{ 0,    NULL,       NULL,               NULL }
 };
 /* end of YPBind version 2 */
@@ -63,12 +167,40 @@ static const vsff ypbind2_proc[] = {
 void
 proto_register_ypbind(void)
 {
+	static hf_register_info hf[] = {
+		{ &hf_ypbind_domain, {
+			"Domain", "ypbind.domain", FT_STRING, BASE_DEC,
+			NULL, 0, "Name of the NIS/YP Domain", HFILL }},
+
+		{ &hf_ypbind_resp_type, {
+			"Response Type", "ypbind.resp_type", FT_UINT32, BASE_DEC,
+			VALS(resp_type_vals), 0, "Response type", HFILL }},
+
+		{ &hf_ypbind_error, {
+			"Error", "ypbind.error", FT_UINT32, BASE_DEC,
+			VALS(error_vals), 0, "YPBIND Error code", HFILL }},
+
+		{ &hf_ypbind_addr, {
+			"IP Addr", "ypbind.addr", FT_IPv4, BASE_DEC,
+			NULL, 0, "IP Address of server", HFILL }},
+
+		{ &hf_ypbind_port, {
+			"Port", "ypbind.port", FT_UINT32, BASE_DEC,
+			NULL, 0, "Port to use", HFILL }},
+
+		{ &hf_ypbind_setdom_version, {
+			"Version", "ypbind.setdom.version", FT_UINT32, BASE_DEC,
+			NULL, 0, "Version of setdom", HFILL }},
+
+	};
+
 	static gint *ett[] = {
 		&ett_ypbind,
 	};
 
 	proto_ypbind = proto_register_protocol("Yellow Pages Bind",
 	    "YPBIND", "ypbind");
+	proto_register_field_array(proto_ypbind, hf, array_length(hf));
 	proto_register_subtree_array(ett, array_length(ett));
 }
 
