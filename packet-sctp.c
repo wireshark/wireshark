eb1d00480f14f053dc2dfdd19215969dde981e51
Guy Harris <guy@alum.mit.edu>
2002-01-15 22:58:06 +0000

    From Michael Tuexen:
    	- Support of the crc32c algorithm.
    	- Selection of the checksum algorithm (none, adler-32, crc32c,
    	  automatic) in the preferences/protocols menu.
    	- Display of the IP address in asconf chunks.
    
    svn path=/trunk/; revision=4548

--- packet-sctp.c
+++ packet-sctp.c
@@ -4,13 +4,13 @@
  * - RFC 2960, for basic SCTP support
  * - http://www.ietf.org/internet-drafts/draft-ietf-tsvwg-addip-sctp-03.txt for the add-IP extension
  * - http://www.sctp.org/draft-ietf-tsvwg-usctp-01.txt for the 'Limited Retransmission' extension
- * Copyright 2000, Michael Tuexen <Michael.Tuexen@icn.siemens.de>
+ * - http://www.ietf.org/internet-drafts/draft-ietf-tsvwg-sctpcsum-01.txt
+ * Copyright 2000, 2001, 2002, Michael Tuexen <Michael.Tuexen@icn.siemens.de>
  * Still to do (so stay tuned)
  * - support for reassembly
- * - support for new checksum
  * - code cleanup
  *
- * $Id: packet-sctp.c,v 1.25 2001/12/19 20:01:56 guy Exp $
+ * $Id: packet-sctp.c,v 1.26 2002/01/15 22:58:06 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -56,6 +56,7 @@
 # include "snprintf.h"
 #endif
 
+#include "prefs.h"
 #include "packet.h"
 #include "ipproto.h"
 
@@ -136,10 +137,14 @@ static int hf_sctp_asconf_correlation_id = -1;
 static int hf_sctp_asconf_reserved = -1;
 static int hf_sctp_asconf_addr_type = -1;
 static int hf_sctp_asconf_addr = -1;
+static int hf_sctp_asconf_ipv4_address = -1;
+static int hf_sctp_asconf_ipv6_address = -1;
 static int hf_sctp_adap_indication = -1;
 
 static dissector_table_t sctp_port_dissector_table;
 static dissector_table_t sctp_ppi_dissector_table;
+
+static module_t *sctp_module;
 
 /* Initialize the subtree pointers */
 static gint ett_sctp = -1;
@@ -456,6 +461,13 @@ static const true_false_string sctp_shutdown_complete_chunk_t_bit_value = {
   "TCB destroyed"
 };
 
+#define SCTP_CHECKSUM_NONE      0
+#define SCTP_CHECKSUM_ADLER32   1
+#define SCTP_CHECKSUM_CRC32C    2
+#define SCTP_CHECKSUM_AUTOMATIC 3
+
+static gint sctp_checksum = SCTP_CHECKSUM_ADLER32;
+
 /* adler32.c -- compute the Adler-32 checksum of a data stream
  * Copyright (C) 1995-1996 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
@@ -475,8 +487,7 @@ static const true_false_string sctp_shutdown_complete_chunk_t_bit_value = {
 #define DO16(buf)   DO8(buf,0); DO8(buf,8);
 
 static unsigned int
-sctp_adler_32(const unsigned char* buf,
-	      unsigned int len)
+sctp_adler32(const unsigned char* buf, unsigned int len)
 {
     unsigned int s1 = 1L;
     unsigned int s2 = 0L;
@@ -514,28 +525,98 @@ sctp_adler_32(const unsigned char* buf,
     return (s2 << 16) | s1;
 }
 
-static char 
-*sctp_checksum_state(tvbuff_t *tvb, guint orig_checksum)
-{
-  guint length;
+/* The CRC32C code is taken from the checksum draft. That code
+ * is copyrighted by D. Otis and has been modified.
+ */
   
-  length = tvb_length(tvb);
-  if (orig_checksum == sctp_adler_32(tvb_get_ptr(tvb, 0, length), length))
-    return "correct";
-  else
-    return "incorrect";
-}
-
-static gboolean 
-sctp_checksum_correct(tvbuff_t *tvb, guint orig_checksum)
+#define CRC32C(c,d) (c=(c>>8)^crc_c[(c^(d))&0xFF]) 
+static unsigned long crc_c[256] = 
+{ 
+0x00000000L, 0xF26B8303L, 0xE13B70F7L, 0x1350F3F4L,  
+0xC79A971FL, 0x35F1141CL, 0x26A1E7E8L, 0xD4CA64EBL,  
+0x8AD958CFL, 0x78B2DBCCL, 0x6BE22838L, 0x9989AB3BL,  
+0x4D43CFD0L, 0xBF284CD3L, 0xAC78BF27L, 0x5E133C24L,  
+0x105EC76FL, 0xE235446CL, 0xF165B798L, 0x030E349BL,  
+0xD7C45070L, 0x25AFD373L, 0x36FF2087L, 0xC494A384L,  
+0x9A879FA0L, 0x68EC1CA3L, 0x7BBCEF57L, 0x89D76C54L,  
+0x5D1D08BFL, 0xAF768BBCL, 0xBC267848L, 0x4E4DFB4BL,  
+0x20BD8EDEL, 0xD2D60DDDL, 0xC186FE29L, 0x33ED7D2AL,  
+0xE72719C1L, 0x154C9AC2L, 0x061C6936L, 0xF477EA35L,  
+0xAA64D611L, 0x580F5512L, 0x4B5FA6E6L, 0xB93425E5L,  
+0x6DFE410EL, 0x9F95C20DL, 0x8CC531F9L, 0x7EAEB2FAL,  
+0x30E349B1L, 0xC288CAB2L, 0xD1D83946L, 0x23B3BA45L,  
+0xF779DEAEL, 0x05125DADL, 0x1642AE59L, 0xE4292D5AL,  
+0xBA3A117EL, 0x4851927DL, 0x5B016189L, 0xA96AE28AL,  
+0x7DA08661L, 0x8FCB0562L, 0x9C9BF696L, 0x6EF07595L,  
+0x417B1DBCL, 0xB3109EBFL, 0xA0406D4BL, 0x522BEE48L,  
+0x86E18AA3L, 0x748A09A0L, 0x67DAFA54L, 0x95B17957L,  
+0xCBA24573L, 0x39C9C670L, 0x2A993584L, 0xD8F2B687L,  
+0x0C38D26CL, 0xFE53516FL, 0xED03A29BL, 0x1F682198L,  
+0x5125DAD3L, 0xA34E59D0L, 0xB01EAA24L, 0x42752927L,  
+0x96BF4DCCL, 0x64D4CECFL, 0x77843D3BL, 0x85EFBE38L,  
+0xDBFC821CL, 0x2997011FL, 0x3AC7F2EBL, 0xC8AC71E8L,  
+0x1C661503L, 0xEE0D9600L, 0xFD5D65F4L, 0x0F36E6F7L,  
+0x61C69362L, 0x93AD1061L, 0x80FDE395L, 0x72966096L,  
+0xA65C047DL, 0x5437877EL, 0x4767748AL, 0xB50CF789L,  
+0xEB1FCBADL, 0x197448AEL, 0x0A24BB5AL, 0xF84F3859L,  
+0x2C855CB2L, 0xDEEEDFB1L, 0xCDBE2C45L, 0x3FD5AF46L,  
+0x7198540DL, 0x83F3D70EL, 0x90A324FAL, 0x62C8A7F9L,  
+0xB602C312L, 0x44694011L, 0x5739B3E5L, 0xA55230E6L,  
+0xFB410CC2L, 0x092A8FC1L, 0x1A7A7C35L, 0xE811FF36L,  
+0x3CDB9BDDL, 0xCEB018DEL, 0xDDE0EB2AL, 0x2F8B6829L,  
+0x82F63B78L, 0x709DB87BL, 0x63CD4B8FL, 0x91A6C88CL,  
+0x456CAC67L, 0xB7072F64L, 0xA457DC90L, 0x563C5F93L,  
+0x082F63B7L, 0xFA44E0B4L, 0xE9141340L, 0x1B7F9043L,  
+0xCFB5F4A8L, 0x3DDE77ABL, 0x2E8E845FL, 0xDCE5075CL,  
+0x92A8FC17L, 0x60C37F14L, 0x73938CE0L, 0x81F80FE3L,  
+0x55326B08L, 0xA759E80BL, 0xB4091BFFL, 0x466298FCL,  
+0x1871A4D8L, 0xEA1A27DBL, 0xF94AD42FL, 0x0B21572CL,  
+0xDFEB33C7L, 0x2D80B0C4L, 0x3ED04330L, 0xCCBBC033L,  
+0xA24BB5A6L, 0x502036A5L, 0x4370C551L, 0xB11B4652L,  
+0x65D122B9L, 0x97BAA1BAL, 0x84EA524EL, 0x7681D14DL,  
+0x2892ED69L, 0xDAF96E6AL, 0xC9A99D9EL, 0x3BC21E9DL,  
+0xEF087A76L, 0x1D63F975L, 0x0E330A81L, 0xFC588982L,  
+0xB21572C9L, 0x407EF1CAL, 0x532E023EL, 0xA145813DL,  
+0x758FE5D6L, 0x87E466D5L, 0x94B49521L, 0x66DF1622L,  
+0x38CC2A06L, 0xCAA7A905L, 0xD9F75AF1L, 0x2B9CD9F2L,  
+0xFF56BD19L, 0x0D3D3E1AL, 0x1E6DCDEEL, 0xEC064EEDL,  
+0xC38D26C4L, 0x31E6A5C7L, 0x22B65633L, 0xD0DDD530L,  
+0x0417B1DBL, 0xF67C32D8L, 0xE52CC12CL, 0x1747422FL,  
+0x49547E0BL, 0xBB3FFD08L, 0xA86F0EFCL, 0x5A048DFFL,  
+0x8ECEE914L, 0x7CA56A17L, 0x6FF599E3L, 0x9D9E1AE0L,  
+0xD3D3E1ABL, 0x21B862A8L, 0x32E8915CL, 0xC083125FL,  
+0x144976B4L, 0xE622F5B7L, 0xF5720643L, 0x07198540L,  
+0x590AB964L, 0xAB613A67L, 0xB831C993L, 0x4A5A4A90L,  
+0x9E902E7BL, 0x6CFBAD78L, 0x7FAB5E8CL, 0x8DC0DD8FL,  
+0xE330A81AL, 0x115B2B19L, 0x020BD8EDL, 0xF0605BEEL,  
+0x24AA3F05L, 0xD6C1BC06L, 0xC5914FF2L, 0x37FACCF1L,  
+0x69E9F0D5L, 0x9B8273D6L, 0x88D28022L, 0x7AB90321L,  
+0xAE7367CAL, 0x5C18E4C9L, 0x4F48173DL, 0xBD23943EL,  
+0xF36E6F75L, 0x0105EC76L, 0x12551F82L, 0xE03E9C81L,  
+0x34F4F86AL, 0xC69F7B69L, 0xD5CF889DL, 0x27A40B9EL,  
+0x79B737BAL, 0x8BDCB4B9L, 0x988C474DL, 0x6AE7C44EL,  
+0xBE2DA0A5L, 0x4C4623A6L, 0x5F16D052L, 0xAD7D5351L,  
+}; 
+     
+static unsigned int
+sctp_crc32c(const unsigned char* buf, unsigned int len)
 {
-  guint length;
-  
-  length = tvb_length(tvb);
-  if (orig_checksum == sctp_adler_32(tvb_get_ptr(tvb, 0, length), length))
-    return TRUE;
-  else
-    return FALSE;
+  unsigned int i; 
+  unsigned long crc32 = ~0L; 
+            
+  for (i = 0; i < SOURCE_PORT_LENGTH + DESTINATION_PORT_LENGTH + VERIFICATION_TAG_LENGTH; i++) 
+  { 
+    CRC32C(crc32, buf[i]); 
+  }
+  CRC32C(crc32, 0);
+  CRC32C(crc32, 0);
+  CRC32C(crc32, 0);
+  CRC32C(crc32, 0);
+  for (i = COMMON_HEADER_LENGTH; i < len; i++) 
+  { 
+    CRC32C(crc32, buf[i]); 
+  }  
+  return crc32; 
 }
 
 static guint 
@@ -1720,10 +1801,18 @@ dissect_forward_tsn_chunk(tvbuff_t *chunk_tvb, packet_info *pinfo, proto_tree *t
 #define ASCONF_ADDR_LENGTH      16
 #define SERIAL_NUMBER_OFFSET    PARAMETER_VALUE_OFFSET
 
+#define IP_V4_ADDRESS_TYPE      5
+#define IP_V6_ADDRESS_TYPE      6
+
+static const value_string sctp_address_type_values[] = {
+  { IP_V4_ADDRESS_TYPE,         "IP V4 address" },
+  { IP_V6_ADDRESS_TYPE,         "IP V6 address" },
+  { 0,                           NULL } };
+
 static void
 dissect_asconf_chunk(tvbuff_t *chunk_tvb, packet_info *pinfo, proto_tree *tree, proto_tree *chunk_tree, proto_item *chunk_item, proto_item *flags_item)
 { 
-  guint32 serial_number, correlation_id;
+  guint32 serial_number, correlation_id, ipv4_address;
   guint offset, length, padding_length, total_length;
   guint8 addr_type;
   tvbuff_t *parameter_tvb;
@@ -1741,7 +1830,21 @@ dissect_asconf_chunk(tvbuff_t *chunk_tvb, packet_info *pinfo, proto_tree *tree, 
     addr_type = tvb_get_guint8(chunk_tvb, offset);
     proto_tree_add_uint(chunk_tree, hf_sctp_asconf_addr_type, chunk_tvb, offset, ASCONF_ADDR_TYPE_LENGTH, addr_type);
     offset          += ASCONF_ADDR_TYPE_LENGTH;
-    proto_tree_add_bytes(chunk_tree, hf_sctp_asconf_addr, chunk_tvb, offset, ASCONF_ADDR_LENGTH, tvb_get_ptr(chunk_tvb, offset, ASCONF_ADDR_LENGTH));
+    switch (addr_type) {
+      case IP_V4_ADDRESS_TYPE:
+        tvb_memcpy(chunk_tvb, (guint8 *)&ipv4_address, offset, IPV4_ADDRESS_LENGTH); 
+        proto_tree_add_ipv4(chunk_tree, hf_sctp_asconf_ipv4_address, chunk_tvb, offset, IPV4_ADDRESS_LENGTH, ipv4_address);
+        proto_tree_add_bytes(chunk_tree, hf_sctp_asconf_addr, chunk_tvb, offset + IPV4_ADDRESS_LENGTH, ASCONF_ADDR_LENGTH - IPV4_ADDRESS_LENGTH,
+                             tvb_get_ptr(chunk_tvb, offset + IPV4_ADDRESS_LENGTH, ASCONF_ADDR_LENGTH - IPV4_ADDRESS_LENGTH));
+        break;
+      case IP_V6_ADDRESS_TYPE:
+          proto_tree_add_ipv6(chunk_tree, hf_sctp_asconf_ipv6_address, chunk_tvb, offset, IPV6_ADDRESS_LENGTH,
+		                          tvb_get_ptr(chunk_tvb, offset, IPV6_ADDRESS_LENGTH));
+        break;
+      default:
+        proto_tree_add_bytes(chunk_tree, hf_sctp_asconf_addr, chunk_tvb, offset, ASCONF_ADDR_LENGTH, tvb_get_ptr(chunk_tvb, offset, ASCONF_ADDR_LENGTH));
+        break;
+    }
     offset          += ASCONF_ADDR_LENGTH;
     proto_item_set_text(chunk_item, "ASCONF chunk");
     
@@ -1970,9 +2073,9 @@ static void
 dissect_sctp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
   guint16 source_port, destination_port;
-  guint32 verification_tag, checksum, calculated_checksum;
+  guint32 verification_tag, checksum, calculated_crc32c, calculated_adler32;
   guint length;
-  gboolean checksum_correct;
+  gboolean crc32c_correct, adler32_correct;
   proto_item *sctp_item;
   proto_tree *sctp_tree;
 
@@ -2001,23 +2104,60 @@ dissect_sctp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     /* create the sctp protocol tree */
     sctp_item = proto_tree_add_item(tree, proto_sctp, tvb, 0, 0, FALSE);
     sctp_tree = proto_item_add_subtree(sctp_item, ett_sctp);
-
+    
     /* add the components of the common header to the protocol tree */
     proto_tree_add_uint(sctp_tree, hf_sctp_source_port, tvb, SOURCE_PORT_OFFSET, SOURCE_PORT_LENGTH, source_port);
     proto_tree_add_uint(sctp_tree, hf_sctp_destination_port, tvb, DESTINATION_PORT_OFFSET, DESTINATION_PORT_LENGTH, destination_port);
     proto_tree_add_uint(sctp_tree, hf_sctp_verification_tag, tvb, VERIFICATION_TAG_OFFSET, VERIFICATION_TAG_LENGTH, verification_tag);
   
     length = tvb_length(tvb);
-    calculated_checksum = sctp_adler_32(tvb_get_ptr(tvb, 0, length), length);
-    checksum_correct = (checksum == calculated_checksum);
-    if (checksum_correct)
-      proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, checksum, 
-                                 "Adler-32 checksum: 0x%08x (correct)", checksum);
-    else {
-      proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, checksum,
-                                 "Adler-32 checksum: 0x%08x (incorrect, should be 0x%08x)", checksum, calculated_checksum);    
-      proto_tree_add_boolean_hidden(sctp_tree, hf_sctp_checksum_correct, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, checksum_correct);
-    };
+    switch(sctp_checksum) {
+    case SCTP_CHECKSUM_NONE:
+      proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, checksum, "Checksum: 0x%08x (not verified)", checksum);
+      break;
+    case SCTP_CHECKSUM_ADLER32:
+      calculated_adler32 = sctp_adler32(tvb_get_ptr(tvb, 0, length), length);
+      adler32_correct    = (checksum == calculated_adler32);
+      if (adler32_correct)
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH,
+                                   checksum, "Checksum: 0x%08x (correct Adler32)", checksum);
+      else
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, 
+                                   checksum, "Checksum: 0x%08x (incorrect Adler32, should be 0x%08x)", checksum, calculated_adler32);    
+      proto_tree_add_boolean_hidden(sctp_tree, hf_sctp_checksum_correct, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, adler32_correct);
+      break;
+    case SCTP_CHECKSUM_CRC32C:
+      calculated_crc32c = sctp_crc32c(tvb_get_ptr(tvb, 0, length), length);
+      crc32c_correct    = (checksum == calculated_crc32c);
+      if (crc32c_correct)
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH,
+                                   checksum, "Checksum: 0x%08x (correct CRC32C)", checksum);
+      else
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, 
+                                   checksum, "Checksum: 0x%08x (incorrect CRC32C, should be 0x%08x)", checksum, calculated_crc32c);    
+      proto_tree_add_boolean_hidden(sctp_tree, hf_sctp_checksum_correct, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, crc32c_correct);
+      break;
+    case SCTP_CHECKSUM_AUTOMATIC:
+      calculated_adler32 = sctp_adler32(tvb_get_ptr(tvb, 0, length), length);
+      adler32_correct    = (checksum == calculated_adler32);
+      calculated_crc32c  = sctp_crc32c(tvb_get_ptr(tvb, 0, length), length);
+      crc32c_correct     = (checksum == calculated_crc32c);
+      if ((adler32_correct) && !(crc32c_correct))
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH,
+                                   checksum, "Checksum: 0x%08x (correct Adler32)", checksum);
+      else if (!(adler32_correct) && (crc32c_correct))
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH,
+                                   checksum, "Checksum: 0x%08x (correct CRC32C)", checksum);
+      else if ((adler32_correct) && (crc32c_correct))
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH,
+                                   checksum, "Checksum: 0x%08x (correct Adler32 and CRC32C)", checksum);
+      else
+        proto_tree_add_uint_format(sctp_tree, hf_sctp_checksum, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, 
+                                   checksum, "Checksum: 0x%08x (incorrect, should be 0x%08x (Adler32) or 0x%08x (CRC32C))",
+                                   checksum, calculated_adler32, calculated_crc32c);
+      proto_tree_add_boolean_hidden(sctp_tree, hf_sctp_checksum_correct, tvb, CHECKSUM_OFFSET, CHECKSUM_LENGTH, (crc32c_correct || adler32_correct));
+      break;
+    }
   } else {
     sctp_tree = NULL;
     sctp_item = NULL;
@@ -2260,7 +2400,7 @@ proto_register_sctp(void)
     }, 
     {&hf_sctp_asconf_addr_type,
      { "Address type", "sctp.asconf.address_type",
-       FT_UINT8, BASE_HEX, NULL, 0x0,          
+       FT_UINT8, BASE_HEX, VALS(sctp_address_type_values), 0x0,          
        "", HFILL }
     }, 
     {&hf_sctp_asconf_addr,
@@ -2268,6 +2408,16 @@ proto_register_sctp(void)
        FT_BYTES, BASE_NONE, NULL, 0x0,          
        "", HFILL }
     }, 
+    {&hf_sctp_asconf_ipv4_address,
+     { "IP Version 4 address", "sctp.asconf.ipv4_address",
+       FT_IPv4, BASE_NONE, NULL, 0x0,
+       "", HFILL }
+    },
+    {&hf_sctp_asconf_ipv6_address,
+     { "IP Version 6 address", "sctp.asconf.ipv6_address",
+       FT_IPv6, BASE_NONE, NULL, 0x0,
+       "", HFILL }
+    },
     {&hf_sctp_asconf_ack_serial,
      { "Serial Number", "sctp.asconf_ack.serial_number",
        FT_UINT32, BASE_HEX, NULL, 0x0,          
@@ -2333,19 +2483,27 @@ proto_register_sctp(void)
     &ett_sctp_unreliable_streams_interval
   };
   
+  static enum_val_t sctp_checksum_options[] = {
+    { "None",        SCTP_CHECKSUM_NONE },
+    { "Adler 32",    SCTP_CHECKSUM_ADLER32 },
+    { "CRC 32c",     SCTP_CHECKSUM_CRC32C },
+    { "Automatic",   SCTP_CHECKSUM_AUTOMATIC},
+    { NULL, 0 }
+  };
+ 
   /* Register the protocol name and description */
-  proto_sctp = proto_register_protocol("Stream Control Transmission Protocol",
-				       "SCTP", "sctp");
-  
+  proto_sctp = proto_register_protocol("Stream Control Transmission Protocol", "SCTP", "sctp");
+  sctp_module = prefs_register_protocol(proto_sctp, NULL);
+  prefs_register_enum_preference(sctp_module, "checksum", "SCTP Checksum", "SCTP Checksum",
+                                 &sctp_checksum, sctp_checksum_options, FALSE);
+
   /* Required function calls to register the header fields and subtrees used */
   proto_register_field_array(proto_sctp, hf, array_length(hf));
   proto_register_subtree_array(ett, array_length(ett));
 
   /* subdissector code */
-  sctp_port_dissector_table = register_dissector_table("sctp.port",
-	"SCTP port", FT_UINT16, BASE_DEC);
-  sctp_ppi_dissector_table  = register_dissector_table("sctp.ppi",
-        "SCTP payload protocol identifier", FT_UINT32, BASE_HEX);
+  sctp_port_dissector_table = register_dissector_table("sctp.port", "SCTP port", FT_UINT16, BASE_DEC);
+  sctp_ppi_dissector_table  = register_dissector_table("sctp.ppi",  "SCTP payload protocol identifier", FT_UINT32, BASE_HEX);
 
 };
 
