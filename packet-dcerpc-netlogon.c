e4832c8ca8c23f524b81ebb3093622204978af86
Guy Harris <guy@alum.mit.edu>
2002-03-14 05:46:59 +0000

    Fix a typo.
    svn path=/trunk/; revision=4941

--- packet-dcerpc-netlogon.c
+++ packet-dcerpc-netlogon.c
@@ -3,7 +3,7 @@
  * Copyright 2001, Tim Potter <tpot@samba.org>
  *  2002 structure and command dissectors by Ronnie Sahlberg
  *
- * $Id: packet-dcerpc-netlogon.c,v 1.7 2002/03/13 11:19:16 sahlberg Exp $
+ * $Id: packet-dcerpc-netlogon.c,v 1.8 2002/03/14 05:46:59 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -5269,7 +5269,7 @@ static hf_register_info hf[] = {
 		NULL, 0, "Attributes", HFILL }},
 
 	{ &hf_netlogon_unknown_string,
-		{ "Unknwon string", "netlogon.unknown_string", FT_STRING, BASE_NONE,
+		{ "Unknown string", "netlogon.unknown_string", FT_STRING, BASE_NONE,
 		NULL, 0, "Unknown string. If you know what this is, contact ethereal developers.", HFILL }},
 	{ &hf_netlogon_unknown_long,
 		{ "Unknown long", "netlogon.unknown.long", FT_UINT32, BASE_HEX, 
