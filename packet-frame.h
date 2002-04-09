61bcdf08281292c2525914b762ae15185f0b1be7
Guy Harris <guy@alum.mit.edu>
2002-04-09 08:43:43 +0000

    "dissect_frame()" is now static; don't declare it in "packet-frame.h".
    svn path=/trunk/; revision=5135

--- packet-frame.h
+++ packet-frame.h
@@ -2,12 +2,11 @@
  *
  * Top-most dissector. Decides dissector based on Wiretap Encapsulation Type.
  *
- * $Id: packet-frame.h,v 1.3 2002/02/18 23:51:55 guy Exp $
+ * $Id: packet-frame.h,v 1.4 2002/04/09 08:43:43 guy Exp $
  *
  * Ethereal - Network traffic analyzer
- * By Gerald Combs <gerald@zing.org>
+ * By Gerald Combs <gerald@ethereal.com>
  * Copyright 2000 Gerald Combs
- *
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -23,9 +22,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
-void
-dissect_frame(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
 
 /*
  * Routine used to add an indication of a ReportedBoundsError exception
