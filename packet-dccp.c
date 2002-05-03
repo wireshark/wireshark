185fb53e7263981eeed8bdd4b652bb5335f566c0
Nathan Neulinger <nneul@umr.edu>
2002-05-03 20:34:14 +0000

    fix field names, break down flood and trace control packets, other cosmetics
    svn path=/trunk/; revision=5375

--- packet-dccp.c
+++ packet-dccp.c
@@ -1,10 +1,10 @@
-/* packet-dcc.c
+/* packet-dccp.c
  * Routines for Distributed Checksum Clearinghouse packet dissection
  * DCC Home: http://www.rhyolite.com/anti-spam/dcc/
  *
  * Copyright 1999, Nathan Neulinger <nneul@umr.edu>
  *
- * $Id: packet-dccp.c,v 1.4 2002/05/03 19:47:09 nneul Exp $
+ * $Id: packet-dccp.c,v 1.5 2002/05/03 20:34:14 nneul Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -73,11 +73,21 @@ static int hf_dccp_target = -1;
 
 static int hf_dccp_adminop = -1;
 static int hf_dccp_adminval = -1;
+static int hf_dccp_floodop = -1;
+static int hf_dccp_trace = -1;
+static int hf_dccp_trace_admin = -1;
+static int hf_dccp_trace_anon = -1;
+static int hf_dccp_trace_client = -1;
+static int hf_dccp_trace_rlim = -1;
+static int hf_dccp_trace_query = -1;
+static int hf_dccp_trace_ridc = -1;
+static int hf_dccp_trace_flood = -1;
 
 static gint ett_dccp = -1;
 static gint ett_dccp_opnums = -1;
 static gint ett_dccp_op = -1;
 static gint ett_dccp_ck = -1;
+static gint ett_dccp_trace = -1;
 
 /* Utility macros */
 #define D_SIGNATURE() \
@@ -144,8 +154,8 @@ static const value_string dccp_op_vals[] = {
 	{DCC_OP_REPORT, "Report and Query"},
 	{DCC_OP_QUERY, "Query"},
 	{DCC_OP_QUERY_RESP, "Server Response"},
-	{DCC_OP_ADMN, "Admin Op"},
-	{DCC_OP_OK, "Admin Op Ok"},
+	{DCC_OP_ADMN, "Admin"},
+	{DCC_OP_OK, "Ok"},
 	{DCC_OP_ERROR, "Server Failing"},
 	{DCC_OP_DELETE, "Delete Checksum(s)"},
 	{0, NULL}
@@ -173,14 +183,14 @@ static const value_string dccp_adminop_vals[] = {
 	{DCC_AOP_OK, "Never sent"},
 	{DCC_AOP_STOP, "Stop Gracefully"},
 	{DCC_AOP_NEW_IDS, "Load keys and client IDs"},
-	{DCC_AOP_FLOD, "Start or Stop Flooding"},
-	{DCC_AOP_DB_UNLOCK, "Start Switch to New Database"},
-	{DCC_AOP_DB_NEW, "Finish Switch to New Database"},
+	{DCC_AOP_FLOD, "Flood control"},
+	{DCC_AOP_DB_UNLOCK, "Start Switch to new database"},
+	{DCC_AOP_DB_NEW, "Finish Switch to new database"},
 	{DCC_AOP_STATS, "Return counters"},
 	{DCC_AOP_STATS_CLEAR, "Return and zero counters"},
 	{DCC_AOP_TRACE_ON, "Enable tracing"},
 	{DCC_AOP_TRACE_OFF, "Disable tracing"},
-	{DCC_AOP_CUR_CLIENTS, "Some client IP addresses"},
+	{DCC_AOP_CUR_CLIENTS, "List clients"},
 	{0, NULL},
 };
 
@@ -193,10 +203,23 @@ static const value_string dccp_target_vals[] = {
 	{0, NULL},
 };
 
+static const value_string dccp_floodop_vals[] = {
+	{DCC_AOP_FLOD_CHECK, "Check"},   
+	{DCC_AOP_FLOD_SHUTDOWN, "Shutdown"},
+	{DCC_AOP_FLOD_HALT, "Halt"},
+	{DCC_AOP_FLOD_RESUME, "Resume"},
+	{DCC_AOP_FLOD_REWIND, "Rewind"},
+	{DCC_AOP_FLOD_LIST, "List"},
+	{DCC_AOP_FLOD_STATS, "Stats"},
+	{DCC_AOP_FLOD_STATS_CLEAR, "Clear Stats"},
+	{0,NULL},
+};
+
 static gboolean
 dissect_dccp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 	proto_tree      *dccp_tree, *dccp_optree, *dccp_opnumtree, *ti;
+	proto_tree *dccp_tracetree;
 	int offset = 0;
 	int client_is_le = 0;
 	int op = 0;
@@ -333,18 +356,50 @@ dissect_dccp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				}
 				else
 				{
+					int aop;
+
 					D_DATE();
-					proto_tree_add_item(dccp_optree, hf_dccp_adminval, tvb, offset,
-						4, FALSE);
-					offset += 4;
-					proto_tree_add_item(dccp_optree, hf_dccp_adminop, tvb, offset,
+
+					aop = tvb_get_guint8(tvb, offset+4);
+					proto_tree_add_item(dccp_optree, hf_dccp_adminop, tvb, offset+4,
 						1, FALSE);
 					if (check_col(pinfo->cinfo, COL_INFO)) {
 						col_append_fstr(pinfo->cinfo, COL_INFO, ", %s",
-							val_to_str(tvb_get_guint8(tvb,offset), 
+							val_to_str(tvb_get_guint8(tvb,offset+4), 
 							dccp_adminop_vals, "Unknown (%u)"));
 					}
-					offset += 1;
+	
+					if (aop == DCC_AOP_TRACE_ON || aop == DCC_AOP_TRACE_OFF )
+					{
+						ti = proto_tree_add_item(dccp_optree, hf_dccp_trace, tvb, offset,
+							4, FALSE);
+						dccp_tracetree = proto_item_add_subtree(ti, ett_dccp_trace);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_admin, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_anon, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_client, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_rlim, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_query, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_ridc, tvb, offset, 4, FALSE);
+						proto_tree_add_item(dccp_tracetree, hf_dccp_trace_flood, tvb, offset, 4, FALSE);
+					}
+					else if ( aop == DCC_AOP_FLOD )
+					{
+						proto_tree_add_item(dccp_optree, hf_dccp_floodop, 
+							tvb, offset, 4, FALSE);
+						if (check_col(pinfo->cinfo, COL_INFO)) {
+							col_append_fstr(pinfo->cinfo, COL_INFO, ", %s",
+								val_to_str(tvb_get_ntohl(tvb,offset), 
+								dccp_floodop_vals, "Unknown (%u)"));
+						}
+					}
+					else
+					{
+						proto_tree_add_item(dccp_optree, hf_dccp_adminval, 
+							tvb, offset, 4, FALSE);
+					}
+					offset += 4;
+
+					offset += 1; /* admin op we did in reverse order */
 					D_LABEL("Pad", 3);
 					D_SIGNATURE();
 				}
@@ -353,10 +408,6 @@ dissect_dccp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			case DCC_OP_OK:
 				proto_tree_add_item(dccp_optree, hf_dccp_max_pkt_vers, tvb,
 					offset, 1, FALSE);
-				if (check_col(pinfo->cinfo, COL_INFO)) {
-					col_append_fstr(pinfo->cinfo, COL_INFO, ", max=%d",
-						tvb_get_guint8(tvb, offset));
-				}
 				offset += 1;
 
 				D_LABEL("Unused", 1);
@@ -386,86 +437,124 @@ proto_register_dccp(void)
 {
 	static hf_register_info hf[] = {
 			{ &hf_dccp_len, {	
-				"Packet Length", "dcc.len", FT_UINT16, BASE_DEC,
+				"Packet Length", "dccp.len", FT_UINT16, BASE_DEC,
 				NULL, 0, "Packet Length", HFILL }},
 
 			{ &hf_dccp_pkt_vers, {	
-				"Packet Version", "dcc.pkt_vers", FT_UINT16, BASE_DEC,
+				"Packet Version", "dccp.pkt_vers", FT_UINT16, BASE_DEC,
 				NULL, 0, "Packet Version", HFILL }},
 
 			{ &hf_dccp_op, {	
-				"Operation Type", "dcc.op", FT_UINT8, BASE_DEC,
+				"Operation Type", "dccp.op", FT_UINT8, BASE_DEC,
 				VALS(dccp_op_vals), 0, "Operation Type", HFILL }},
 
 			{ &hf_dccp_clientid, {	
-				"Client ID", "dcc.clientid", FT_UINT32, BASE_DEC,
+				"Client ID", "dccp.clientid", FT_UINT32, BASE_DEC,
 				NULL, 0, "Client ID", HFILL }},
 
 			{ &hf_dccp_opnums_host, {	
-				"Host", "dcc.opnums.host", FT_IPv4, BASE_DEC,
+				"Host", "dccp.opnums.host", FT_IPv4, BASE_DEC,
 				NULL, 0, "Host", HFILL }},
 
 			{ &hf_dccp_opnums_pid, {	
-				"Process ID", "dcc.opnums.pid", FT_UINT32, BASE_DEC,
+				"Process ID", "dccp.opnums.pid", FT_UINT32, BASE_DEC,
 				NULL, 0, "Process ID", HFILL }},
 
 			{ &hf_dccp_opnums_report, {	
-				"Report", "dcc.opnums.report", FT_UINT32, BASE_DEC,
+				"Report", "dccp.opnums.report", FT_UINT32, BASE_DEC,
 				NULL, 0, "Report", HFILL }},
 
 			{ &hf_dccp_opnums_retrans, {	
-				"Retransmission", "dcc.opnums.retrans", FT_UINT32, BASE_DEC,
+				"Retransmission", "dccp.opnums.retrans", FT_UINT32, BASE_DEC,
 				NULL, 0, "Retransmission", HFILL }},
 
 			{ &hf_dccp_signature, {	
-				"Signature", "dcc.signature", FT_BYTES, BASE_HEX,
+				"Signature", "dccp.signature", FT_BYTES, BASE_HEX,
 				NULL, 0, "Signature", HFILL }},
 
 			{ &hf_dccp_max_pkt_vers, {	
-				"Maximum Packet Version", "dcc.max_pkt_vers", FT_UINT8, BASE_DEC,
+				"Maximum Packet Version", "dccp.max_pkt_vers", FT_UINT8, BASE_DEC,
 				NULL, 0, "Maximum Packet Version", HFILL }},
 
 			{ &hf_dccp_qdelay_ms, {	
-				"Client Delay", "dcc.qdelay_ms", FT_UINT16, BASE_DEC,
+				"Client Delay", "dccp.qdelay_ms", FT_UINT16, BASE_DEC,
 				NULL, 0, "Client Delay", HFILL }},
 
 			{ &hf_dccp_brand, {	
-				"Server Brand", "dcc.brand", FT_STRING, BASE_DEC,
+				"Server Brand", "dccp.brand", FT_STRING, BASE_DEC,
 				NULL, 0, "Server Brand", HFILL }},
 
 			{ &hf_dccp_ck_type, {	
-				"Type", "dcc.checksum.type", FT_UINT8, BASE_DEC,
+				"Type", "dccp.checksum.type", FT_UINT8, BASE_DEC,
 				VALS(dccp_cktype_vals), 0, "Checksum Type", HFILL }},
 
 			{ &hf_dccp_ck_len, {	
-				"Length", "dcc.checksum.length", FT_UINT8, BASE_DEC,
+				"Length", "dccp.checksum.length", FT_UINT8, BASE_DEC,
 				NULL, 0, "Checksum Length", HFILL }},
 
 			{ &hf_dccp_ck_sum, {	
-				"Sum", "dcc.checksum.sum", FT_BYTES, BASE_HEX,
+				"Sum", "dccp.checksum.sum", FT_BYTES, BASE_HEX,
 				NULL, 0, "Checksum", HFILL }},
 
 			{ &hf_dccp_target, {	
-				"Target", "dcc.target", FT_UINT32, BASE_HEX,
+				"Target", "dccp.target", FT_UINT32, BASE_HEX,
 				NULL, 0, "Target", HFILL }},
 
 			{ &hf_dccp_date, {	
-				"Date", "dcc.date", FT_ABSOLUTE_TIME, BASE_DEC,
+				"Date", "dccp.date", FT_ABSOLUTE_TIME, BASE_DEC,
 				NULL, 0, "Date", HFILL }},
 
 			{ &hf_dccp_adminop, {	
-				"Admin Op", "dcc.adminop", FT_UINT8, BASE_DEC,
+				"Admin Op", "dccp.adminop", FT_UINT8, BASE_DEC,
 				VALS(dccp_adminop_vals), 0, "Admin Op", HFILL }},
 
 			{ &hf_dccp_adminval, {	
-				"Admin Value", "dcc.adminval", FT_UINT32, BASE_DEC,
+				"Admin Value", "dccp.adminval", FT_UINT32, BASE_DEC,
 				NULL, 0, "Admin Value", HFILL }},
+
+			{ &hf_dccp_trace, {	
+				"Trace Bits", "dccp.trace", FT_UINT32, BASE_HEX,
+				NULL, 0, "Trace Bits", HFILL }},
+
+			{ &hf_dccp_trace_admin, {
+				"Admin Requests", "dccp.trace.admin", FT_UINT32, BASE_BIN,
+				NULL, 0x0001, "Admin Requests", HFILL }},
+
+			{ &hf_dccp_trace_anon, {
+				"Anonymous Requests", "dccp.trace.anon", FT_UINT32, BASE_BIN,
+				NULL, 0x0002, "Anonymous Requests", HFILL }},
+
+			{ &hf_dccp_trace_client, {
+				"Authenticated Client Requests", "dccp.trace.client", FT_UINT32, BASE_BIN,
+				NULL, 0x0004, "Authenticated Client Requests", HFILL }},
+
+			{ &hf_dccp_trace_rlim, {
+				"Rate-Limited Requests", "dccp.trace.rlim", FT_UINT32, BASE_BIN,
+				NULL, 0x0008, "Rate-Limited Requests", HFILL }},
+
+			{ &hf_dccp_trace_query, {
+				"Queries and Reports", "dccp.trace.query", FT_UINT32, BASE_BIN,
+				NULL, 0x0010, "Queries and Reports", HFILL }},
+
+			{ &hf_dccp_trace_ridc, {
+				"RID Cache Messages", "dccp.trace.ridc", FT_UINT32, BASE_BIN,
+				NULL, 0x0020, "RID Cache Messages", HFILL }},
+
+			{ &hf_dccp_trace_flood, {
+				"Input/Output Flooding", "dccp.trace.flood", FT_UINT32, BASE_BIN,
+				NULL, 0x0040, "Input/Output Flooding", HFILL }},
+
+			{ &hf_dccp_floodop, {
+				"Flood Control Operation", "dccp.floodop", FT_UINT32, BASE_DEC,
+				VALS(dccp_floodop_vals), 0, "Flood Control Operation", HFILL }},
+
         };
 	static gint *ett[] = {
 		&ett_dccp,
 		&ett_dccp_op,
 		&ett_dccp_ck,
 		&ett_dccp_opnums,
+		&ett_dccp_trace,
 	};
 
 	proto_dccp = proto_register_protocol("Distributed Checksum Clearinghouse Prototocl",
