4d4df14ace64971b56f76d8275d8ef9aed15865b
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2002-08-17 00:35:46 +0000

    Fixed incorrect NDR pointer type in Authenticate3
    svn path=/trunk/; revision=6003

--- packet-dcerpc-netlogon.c
+++ packet-dcerpc-netlogon.c
@@ -3,7 +3,7 @@
  * Copyright 2001, Tim Potter <tpot@samba.org>
  *  2002 structure and command dissectors by Ronnie Sahlberg
  *
- * $Id: packet-dcerpc-netlogon.c,v 1.48 2002/08/16 22:24:07 tpot Exp $
+ * $Id: packet-dcerpc-netlogon.c,v 1.49 2002/08/17 00:35:46 sahlberg Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -5040,7 +5040,7 @@ netlogon_dissect_netserverauthenticate3_rqst(tvbuff_t *tvb, int offset,
 		pinfo, tree, drep);
 
 	offset = dissect_ndr_pointer(tvb, offset, pinfo, tree, drep,
-		dissect_ndr_nt_UNICODE_STRING_str, NDR_POINTER_UNIQUE,
+		dissect_ndr_nt_UNICODE_STRING_str, NDR_POINTER_REF,
 		"Acct Name", hf_netlogon_acct_name, 0);
 
 	offset = netlogon_dissect_NETLOGON_SECURE_CHANNEL_TYPE(tvb, offset,
