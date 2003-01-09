c34844f7c6979f56b5244d1668afa270a299ccf3
Guy Harris <guy@alum.mit.edu>
2003-01-09 03:45:34 +0000

    Check the HEC in ATM cells.
    svn path=/trunk/; revision=6883

--- packet-atm.c
+++ packet-atm.c
@@ -1,7 +1,7 @@
 /* packet-atm.c
  * Routines for ATM packet disassembly
  *
- * $Id: packet-atm.c,v 1.54 2003/01/09 02:55:21 guy Exp $
+ * $Id: packet-atm.c,v 1.55 2003/01/09 03:45:34 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -1066,6 +1066,117 @@ dissect_reassembled_pdu(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
   }
 }
 
+/*
+ * Charles Michael Heard's HEC code, from
+ *
+ *	http://cell-relay.indiana.edu/cell-relay/publications/software/CRC/32bitCRC.tutorial.html
+ *
+ * with the syndrome and error position tables initialized with values
+ * computed by his "gen_syndrome_table()" and "gen_err_posn_table()" routines,
+ * rather than by calling those routines at run time, and with various data
+ * type cleanups and changes not to correct the header if a correctible
+ * error was detected.
+ */
+#define COSET_LEADER    0x055               /* x^6 + x^4 + x^2 + 1  */
+
+static const guint8 syndrome_table[256] = {
+	0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15,
+	0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
+	0x70, 0x77, 0x7e, 0x79, 0x6c, 0x6b, 0x62, 0x65,
+	0x48, 0x4f, 0x46, 0x41, 0x54, 0x53, 0x5a, 0x5d,
+	0xe0, 0xe7, 0xee, 0xe9, 0xfc, 0xfb, 0xf2, 0xf5,
+	0xd8, 0xdf, 0xd6, 0xd1, 0xc4, 0xc3, 0xca, 0xcd,
+	0x90, 0x97, 0x9e, 0x99, 0x8c, 0x8b, 0x82, 0x85,
+	0xa8, 0xaf, 0xa6, 0xa1, 0xb4, 0xb3, 0xba, 0xbd,
+	0xc7, 0xc0, 0xc9, 0xce, 0xdb, 0xdc, 0xd5, 0xd2,
+	0xff, 0xf8, 0xf1, 0xf6, 0xe3, 0xe4, 0xed, 0xea,
+	0xb7, 0xb0, 0xb9, 0xbe, 0xab, 0xac, 0xa5, 0xa2,
+	0x8f, 0x88, 0x81, 0x86, 0x93, 0x94, 0x9d, 0x9a,
+	0x27, 0x20, 0x29, 0x2e, 0x3b, 0x3c, 0x35, 0x32,
+	0x1f, 0x18, 0x11, 0x16, 0x03, 0x04, 0x0d, 0x0a,
+	0x57, 0x50, 0x59, 0x5e, 0x4b, 0x4c, 0x45, 0x42,
+	0x6f, 0x68, 0x61, 0x66, 0x73, 0x74, 0x7d, 0x7a,
+	0x89, 0x8e, 0x87, 0x80, 0x95, 0x92, 0x9b, 0x9c,
+	0xb1, 0xb6, 0xbf, 0xb8, 0xad, 0xaa, 0xa3, 0xa4,
+	0xf9, 0xfe, 0xf7, 0xf0, 0xe5, 0xe2, 0xeb, 0xec,
+	0xc1, 0xc6, 0xcf, 0xc8, 0xdd, 0xda, 0xd3, 0xd4,
+	0x69, 0x6e, 0x67, 0x60, 0x75, 0x72, 0x7b, 0x7c,
+	0x51, 0x56, 0x5f, 0x58, 0x4d, 0x4a, 0x43, 0x44,
+	0x19, 0x1e, 0x17, 0x10, 0x05, 0x02, 0x0b, 0x0c,
+	0x21, 0x26, 0x2f, 0x28, 0x3d, 0x3a, 0x33, 0x34,
+	0x4e, 0x49, 0x40, 0x47, 0x52, 0x55, 0x5c, 0x5b,
+	0x76, 0x71, 0x78, 0x7f, 0x6a, 0x6d, 0x64, 0x63,
+	0x3e, 0x39, 0x30, 0x37, 0x22, 0x25, 0x2c, 0x2b,
+	0x06, 0x01, 0x08, 0x0f, 0x1a, 0x1d, 0x14, 0x13,
+	0xae, 0xa9, 0xa0, 0xa7, 0xb2, 0xb5, 0xbc, 0xbb,
+	0x96, 0x91, 0x98, 0x9f, 0x8a, 0x8d, 0x84, 0x83,
+	0xde, 0xd9, 0xd0, 0xd7, 0xc2, 0xc5, 0xcc, 0xcb,
+	0xe6, 0xe1, 0xe8, 0xef, 0xfa, 0xfd, 0xf4, 0xf3,
+};
+
+#define NO_ERROR_DETECTED   -128
+#define UNCORRECTIBLE_ERROR  128
+
+static const int err_posn_table[256] = {
+	NO_ERROR_DETECTED,	37,
+	36,			UNCORRECTIBLE_ERROR,
+	35,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	29,
+	34,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	6,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	28,			UNCORRECTIBLE_ERROR,
+	33,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	21,
+	5,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	27,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	32,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	20,			UNCORRECTIBLE_ERROR,
+	4,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	26,			UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+	UNCORRECTIBLE_ERROR,	UNCORRECTIBLE_ERROR,
+};
+
+/*
+ * Return an indication of whether there was an error in the cell header
+ * and, if so, where the error was, if it was correctable.
+ */
+static int
+get_header_err(const guint8 *cell_header)
+{
+  register guint8 syndrome;
+  register int i, err_posn;
+
+  syndrome = 0;
+  for (i = 0;  i < 4;  i++)
+    syndrome = syndrome_table[syndrome ^ cell_header[i]];
+  syndrome ^= cell_header[4] ^ COSET_LEADER;
+
+  err_posn = err_posn_table [syndrome];
+
+  if (err_posn < 0)
+    return NO_ERROR_DETECTED;
+  else if (err_posn < 40)
+    return err_posn;
+  else
+    return UNCORRECTIBLE_ERROR;
+}
+
 static const value_string pt_vals[] = {
 	{ 0, "User data cell, congestion not experienced, SDU-type = 0" },
 	{ 1, "User data cell, congestion not experienced, SDU-type = 1" },
@@ -1124,6 +1235,7 @@ dissect_atm_cell(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
   proto_tree   *aal_tree;
   proto_item   *ti;
   guint8       octet;
+  int          err;
   guint8       vpi;
   guint16      vci;
   guint16      aal3_4_hdr, aal3_4_trlr;
@@ -1146,8 +1258,15 @@ dissect_atm_cell(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree,
         val_to_str((octet >> 1) & 0x7, pt_vals, "Unknown (%u)"));
   proto_tree_add_text(atm_tree, tvb, 3, 1, "Cell Loss Priority: %s",
 	(octet & 0x01) ? "Low priority" : "High priority");
-  proto_tree_add_text(atm_tree, tvb, 4, 1, "Header Error Check: 0x%02x",
+  ti = proto_tree_add_text(atm_tree, tvb, 4, 1, "Header Error Check: 0x%02x",
 	  tvb_get_guint8(tvb, 4));
+  err = get_header_err(tvb_get_ptr(tvb, 0, 5));
+  if (err == NO_ERROR_DETECTED)
+    proto_item_append_text(ti, " (correct)");
+  else if (err == UNCORRECTIBLE_ERROR)
+    proto_item_append_text(ti, " (uncorrectable error)");
+  else
+    proto_item_append_text(ti, " (error in bit %d)", err);
   offset = 5;
 
   switch (pinfo->pseudo_header->atm.aal) {
