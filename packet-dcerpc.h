c112d781b1bddc0e8bd2f7a4ca5ae9c327067b0a
Tim Potter <tpot@samba.org>
2003-02-10 02:06:28 +0000

    Allow dissect_ndr_cvstring to return a malloced copy of the string.
    svn path=/trunk/; revision=7109

--- packet-dcerpc.h
+++ packet-dcerpc.h
@@ -1,7 +1,7 @@
 /* packet-dcerpc.h
  * Copyright 2001, Todd Sabin <tas@webspan.net>
  *
- * $Id: packet-dcerpc.h,v 1.29 2003/02/07 22:44:54 guy Exp $
+ * $Id: packet-dcerpc.h,v 1.30 2003/02/10 02:06:28 tpot Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -194,8 +194,9 @@ int dissect_ndr_byte_array(tvbuff_t *tvb, int offset, packet_info *pinfo,
 char *fake_unicode(tvbuff_t *tvb, int offset, int len);
 
 int dissect_ndr_cvstring(tvbuff_t *tvb, int offset, packet_info *pinfo, 
-                               proto_tree *tree, char *drep, int size_is,
-                               int hfinfo, gboolean add_subtree);
+			 proto_tree *tree, char *drep, int size_is,
+			 int hfinfo, gboolean add_subtree,
+			 char **data);
 int dissect_ndr_char_cvstring(tvbuff_t *tvb, int offset, packet_info *pinfo, 
                            proto_tree *tree, char *drep);
 int dissect_ndr_wchar_cvstring(tvbuff_t *tvb, int offset, packet_info *pinfo, 
