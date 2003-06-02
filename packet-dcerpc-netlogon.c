4eaff6dc18e873f1cd51a2a3be43af40b00ba3ce
Tim Potter <tpot@samba.org>
2003-06-02 03:53:32 +0000

    Dissect a netlogon credential as eight FT_BYTES rather than two guint32 quantities.
    svn path=/trunk/; revision=7768

--- packet-dcerpc-netlogon.c
+++ packet-dcerpc-netlogon.c
@@ -3,7 +3,7 @@
  * Copyright 2001,2003 Tim Potter <tpot@samba.org>
  *  2002 structure and command dissectors by Ronnie Sahlberg
  *
- * $Id: packet-dcerpc-netlogon.c,v 1.81 2003/05/21 09:34:54 sahlberg Exp $
+ * $Id: packet-dcerpc-netlogon.c,v 1.82 2003/06/02 03:53:32 tpot Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -101,8 +101,7 @@ static int hf_netlogon_pwd_can_change_time = -1;
 static int hf_netlogon_pwd_must_change_time = -1;
 static int hf_netlogon_nt_chal_resp = -1;
 static int hf_netlogon_lm_chal_resp = -1;
-static int hf_netlogon_credential_high = -1;
-static int hf_netlogon_credential_low = -1;
+static int hf_netlogon_credential = -1;
 static int hf_netlogon_acct_name = -1;
 static int hf_netlogon_acct_desc = -1;
 static int hf_netlogon_group_desc = -1;
@@ -771,13 +770,9 @@ netlogon_dissect_CREDENTIAL(tvbuff_t *tvb, int offset,
 		return offset;
 	}
 
-	proto_tree_add_item(
-		tree, hf_netlogon_credential_low, tvb, offset, 4, TRUE);
-	offset += 4;
-
-	proto_tree_add_item(
-		tree, hf_netlogon_credential_high, tvb, offset, 4, TRUE);
-	offset += 4;
+	proto_tree_add_item(tree, hf_netlogon_credential, tvb, offset, 8,
+		FALSE);
+	offset += 8;
 
 	return offset;
 }
@@ -6288,13 +6283,9 @@ static hf_register_info hf[] = {
 		"Entries", "netlogon.entries", FT_UINT32, BASE_DEC,
 		NULL, 0x0, "", HFILL }},
 
-	{ &hf_netlogon_credential_low, {
-		"Credential low", "netlogon.credential.low", FT_UINT32, 
-		BASE_HEX, NULL, 0x0, "Netlogon credential (low)", HFILL }},
-
-	{ &hf_netlogon_credential_high, {
-		"Credential high", "netlogon.credential.high", FT_UINT32, 
-		BASE_HEX, NULL, 0x0, "Netlogon credential (high)", HFILL }},
+	{ &hf_netlogon_credential, {
+		"Credential", "netlogon.credential", FT_BYTES, BASE_HEX,
+		NULL, 0x0, "Netlogon Credential", HFILL }},
 
 	{ &hf_netlogon_challenge, {
 		"Challenge", "netlogon.challenge", FT_BYTES, BASE_HEX,
