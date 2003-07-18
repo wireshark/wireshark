44799be3972812585a0120669f7a82189dcca985
Guy Harris <guy@alum.mit.edu>
2003-07-18 05:48:39 +0000

    I've seen captures with Netlogon Secure Channel security with a protection level of DCE_C_AUTHN_LEVEL_PKT_INTEGRITY.
    svn path=/trunk/; revision=8041

--- packet-dcerpc-netlogon.c
+++ packet-dcerpc-netlogon.c
@@ -3,7 +3,7 @@
  * Copyright 2001,2003 Tim Potter <tpot@samba.org>
  *  2002 structure and command dissectors by Ronnie Sahlberg
  *
- * $Id: packet-dcerpc-netlogon.c,v 1.85 2003/07/18 05:19:42 guy Exp $
+ * $Id: packet-dcerpc-netlogon.c,v 1.86 2003/07/18 05:48:39 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -7094,6 +7094,9 @@ proto_reg_handoff_dcerpc_netlogon(void)
 	hf_info->strings = value_string_from_subdissectors(
 		dcerpc_netlogon_dissectors, array_length(dcerpc_netlogon_dissectors));
 
+	register_dcerpc_auth_subdissector(DCE_C_AUTHN_LEVEL_PKT_INTEGRITY,
+					  DCE_C_RPC_AUTHN_PROTOCOL_SEC_CHAN,
+					  &secchan_auth_fns);	
 	register_dcerpc_auth_subdissector(DCE_C_AUTHN_LEVEL_PKT_PRIVACY,
 					  DCE_C_RPC_AUTHN_PROTOCOL_SEC_CHAN,
 					  &secchan_auth_fns);	
