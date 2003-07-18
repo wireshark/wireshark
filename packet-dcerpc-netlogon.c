8e9693aa5058967c5966357d7f2ab88665205a80
Guy Harris <guy@alum.mit.edu>
2003-07-18 05:19:42 +0000

    Capitalize "C" in "Channel" in "Secure Channel", as is done elsewhere.
    svn path=/trunk/; revision=8040

--- packet-dcerpc-netlogon.c
+++ packet-dcerpc-netlogon.c
@@ -3,7 +3,7 @@
  * Copyright 2001,2003 Tim Potter <tpot@samba.org>
  *  2002 structure and command dissectors by Ronnie Sahlberg
  *
- * $Id: packet-dcerpc-netlogon.c,v 1.84 2003/07/16 04:20:33 tpot Exp $
+ * $Id: packet-dcerpc-netlogon.c,v 1.85 2003/07/18 05:19:42 guy Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -7011,7 +7011,7 @@ static hf_register_info hf[] = {
 	    BASE_HEX, NULL, 0x0, "", HFILL }},
 
         { &hf_netlogon_secchan_verf,
-          { "Secure channel Verifier", "netlogon.secchan.verifier", FT_NONE, BASE_NONE, 
+          { "Secure Channel Verifier", "netlogon.secchan.verifier", FT_NONE, BASE_NONE, 
 	    NULL, 0x0, "Verifier", HFILL }},
 
         { &hf_netlogon_secchan_verf_sig,
