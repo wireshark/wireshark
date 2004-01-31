124d4e8c90d371532bf1ae1cb2e6eefeafa3d3c6
Ulf Lamping <ulf.lamping@web.de>
2004-01-31 14:16:43 +0000

    minor change in dialog text
    svn path=/trunk/; revision=9924

--- gtk/file_dlg.c
+++ gtk/file_dlg.c
@@ -1,7 +1,7 @@
 /* file_dlg.c
  * Dialog boxes for handling files
  *
- * $Id: file_dlg.c,v 1.88 2004/01/31 12:13:22 ulfl Exp $
+ * $Id: file_dlg.c,v 1.89 2004/01/31 14:16:43 ulfl Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -490,7 +490,7 @@ file_close_cmd_cb(GtkWidget *widget _U_, gpointer data _U_) {
     /* user didn't saved his current file, ask him */
     dialog = simple_dialog(ESD_TYPE_WARN | ESD_TYPE_MODAL, 
                 ESD_BTN_YES | ESD_BTN_NO | ESD_BTN_CANCEL, 
-                PRIMARY_TEXT_START "Save capture file before closing?" PRIMARY_TEXT_END "\n\n"
+                PRIMARY_TEXT_START "Save capture file before closing it?" PRIMARY_TEXT_END "\n\n"
                 "If you close without saving, your capture data will be discarded.");
 
     simple_dialog_set_cb(dialog, file_close_confirmed_cb, NULL);
