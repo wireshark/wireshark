185ca863cb49d0b0e9cbfc9a17f2690d5f525268
Jörg Mayer <jmayer@loplof.de>
2004-05-21 21:08:41 +0000

    Don't print empty lines follwing a }
    svn path=/trunk/; revision=10948

--- make-authors-short.pl
+++ make-authors-short.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/perl -w
 
+# $Id: make-authors-short.pl,v 1.2 2004/05/21 21:08:41 jmayer Exp $
+
 # Remove tasks from individual author entries from AUTHORS file
 # for use in the about dialog.
 
 use strict;
 
 my $subinfo=0;
+my $nextline;
 
 while (<>) {
 	if (/(.*){/) {
@@ -13,6 +16,9 @@ while (<>) {
 		print "$1\n";
 	} elsif (/}/) {
 		$subinfo = 0;
+		if (($nextline = <>) !~ /^$/) {
+			print $nextline;
+		}
 	} elsif ($subinfo == 1) {
 		next;
 	} else {
