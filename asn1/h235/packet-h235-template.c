49112d11c4effe776a1aed466238c40ec90e6083
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2004-05-24 08:42:29 +0000

    From Tomas,
    sourcefiles to generate H235 dissector using Tomas's ASN1 to Ethereal compiler
    
    svn path=/trunk/; revision=10984

---
+++ asn1/h235/packet-h235-template.c
@@ -0,0 +1,86 @@
+/* packet-h235.c
+ * Routines for H.235 packet dissection
+ * 2004  Tomas Kukosa
+ *
+ * $Id: packet-h235-template.c,v 1.1 2004/05/24 08:42:29 sahlberg Exp $
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@ethereal.com>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib.h>
+#include <epan/packet.h>
+#include <epan/conversation.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "packet-per.h"
+#include "packet-h235.h"
+
+#define PNAME  "H235-SECURITY-MESSAGES"
+#define PSNAME "H235"
+#define PFNAME "h235"
+
+/* Initialize the protocol and registered fields */
+int proto_h235 = -1;
+#include "packet-h235-hf.c"
+
+/* Initialize the subtree pointers */
+#include "packet-h235-ett.c"
+
+static guint32
+dissect_xxx_ToBeSigned(tvbuff_t *tvb, guint32 offset, packet_info *pinfo, proto_tree *tree, int hf_index) {
+NOT_DECODED_YET("ToBeSigned");
+  return offset;
+}
+
+#include "packet-h235-fn.c"
+
+
+/*--- proto_register_h235 ----------------------------------------------*/
+void proto_register_h235(void) {
+
+  /* List of fields */
+  static hf_register_info hf[] = {
+#include "packet-h235-hfarr.c"
+  };
+
+  /* List of subtrees */
+  static gint *ett[] = {
+#include "packet-h235-ettarr.c"
+  };
+
+  /* Register protocol */
+  proto_h235 = proto_register_protocol(PNAME, PSNAME, PFNAME);
+
+  /* Register fields and subtrees */
+  proto_register_field_array(proto_h235, hf, array_length(hf));
+  proto_register_subtree_array(ett, array_length(ett));
+
+}
+
+
+/*--- proto_reg_handoff_h235 -------------------------------------------*/
+void proto_reg_handoff_h235(void) {
+}
+
