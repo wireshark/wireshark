ede1fb8ceeeb518a47f5ece6919bd4f293819ce6
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2004-06-10 13:33:50 +0000

    some more encryption types and checksum types
    svn path=/trunk/; revision=11135

--- packet-kerberos.c
+++ packet-kerberos.c
@@ -23,7 +23,7 @@
  *
  * Some structures from RFC2630
  *
- * $Id: packet-kerberos.c,v 1.69 2004/06/04 11:35:14 sahlberg Exp $
+ * $Id: packet-kerberos.c,v 1.70 2004/06/10 13:33:50 sahlberg Exp $
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
@@ -473,11 +473,49 @@ printf("woohoo decrypted keytype:%d in frame:%d\n", keytype, pinfo->fd->num);
 #define KRB5_ENCTYPE_DES3_CBC_SHA        5
 #define KRB5_ENCTYPE_DES3_CBC_RAW        6
 #define KRB5_ENCTYPE_DES_HMAC_SHA1       8
+#define KRB5_ENCTYPE_DSA_SHA1_CMS        9
+#define KRB5_ENCTYPE_RSA_MD5_CMS         10
+#define KRB5_ENCTYPE_RSA_SHA1_CMS        11
+#define KRB5_ENCTYPE_RC2_CBC_ENV         12
+#define KRB5_ENCTYPE_RSA_ENV             13
+#define KRB5_ENCTYPE_RSA_ES_OEAP_ENV     14
+#define KRB5_ENCTYPE_DES_EDE3_CBC_ENV    15
 #define KRB5_ENCTYPE_DES3_CBC_SHA1       16
+#define KRB5_ENCTYPE_DES_CBC_MD5         20
 #define KERB_ENCTYPE_RC4_HMAC            23 
 #define KERB_ENCTYPE_RC4_HMAC_EXP        24
 #define KRB5_ENCTYPE_UNKNOWN                0x1ff
 #define KRB5_ENCTYPE_LOCAL_DES3_HMAC_SHA1   0x7007
+#define KRB5_ENCTYPE_RC4_PLAIN_EXP	0xffffff73
+#define KRB5_ENCTYPE_RC4_PLAIN		0xffffff74
+#define KRB5_ENCTYPE_RC4_PLAIN_OLD_EXP	0xffffff78
+#define KRB5_ENCTYPE_RC4_HMAC_OLD_EXP	0xffffff79
+#define KRB5_ENCTYPE_RC4_PLAIN_OLD	0xffffff7a
+#define KRB5_ENCTYPE_RC4_HMAC_OLD	0xffffff7b
+#define KRB5_ENCTYPE_DES_PLAIN		0xffffff7c
+#define KRB5_ENCTYPE_RC4_SHA		0xffffff7d
+#define KRB5_ENCTYPE_RC4_LM		0xffffff7e
+#define KRB5_ENCTYPE_RC4_PLAIN		0xffffff7f
+#define KRB5_ENCTYPE_RC4_MD4		0xffffff80
+
+/* checksum types */
+#define KRB5_CHKSUM_NONE                0
+#define KRB5_CHKSUM_CRC32               1
+#define KRB5_CHKSUM_MD4                 2
+#define KRB5_CHKSUM_KRB_DES_MAC         4
+#define KRB5_CHKSUM_KRB_DES_MAC_K       5
+#define KRB5_CHKSUM_MD5                 7
+#define KRB5_CHKSUM_MD5_DES             8
+#define KRB5_CHKSUM_HMAC_MD5            0xffffff76
+#define KRB5_CHKSUM_MD5_HMAC            0xffffff77
+#define KRB5_CHKSUM_RC4_MD5             0xffffff78
+#define KRB5_CHKSUM_MD25                0xffffff79
+#define KRB5_CHKSUM_DES_MAC_MD5         0xffffff7a
+#define KRB5_CHKSUM_DES_MAC             0xffffff7b
+#define KRB5_CHKSUM_REAL_CRC32          0xffffff7c
+#define KRB5_CHKSUM_SHA1                0xffffff7d
+#define KRB5_CHKSUM_LM                  0xffffff7e
+
 
 /*
  * For KERB_ENCTYPE_RC4_HMAC and KERB_ENCTYPE_RC4_HMAC_EXP, see
@@ -758,14 +796,52 @@ static const value_string krb5_encryption_types[] = {
     { KRB5_ENCTYPE_DES3_CBC_SHA   , "des3-cbc-sha" },
     { KRB5_ENCTYPE_DES3_CBC_RAW   , "des3-cbc-raw" },
     { KRB5_ENCTYPE_DES_HMAC_SHA1  , "des-hmac-sha1" },
+    { KRB5_ENCTYPE_DSA_SHA1_CMS   , "dsa-sha1-cms" },
+    { KRB5_ENCTYPE_RSA_MD5_CMS    , "rsa-md5-cms" },
+    { KRB5_ENCTYPE_RSA_SHA1_CMS   , "rsa-sha1-cms" },
+    { KRB5_ENCTYPE_RC2_CBC_ENV    , "rc2-cbc-env" },
+    { KRB5_ENCTYPE_RSA_ENV        , "rsa-env" },
+    { KRB5_ENCTYPE_RSA_ES_OEAP_ENV, "rsa-es-oeap-env" },
+    { KRB5_ENCTYPE_DES_EDE3_CBC_ENV, "des-ede3-cbc-env" },
     { KRB5_ENCTYPE_DES3_CBC_SHA1  , "des3-cbc-sha1" },
+    { KRB5_ENCTYPE_DES_CBC_MD5    , "des-cbc-md5" },
     { KERB_ENCTYPE_RC4_HMAC       , "rc4-hmac" },
     { KERB_ENCTYPE_RC4_HMAC_EXP   , "rc4-hmac-exp" },
     { KRB5_ENCTYPE_UNKNOWN        , "unknown" },
     { KRB5_ENCTYPE_LOCAL_DES3_HMAC_SHA1    , "local-des3-hmac-sha1" },
+    { KRB5_ENCTYPE_RC4_PLAIN_EXP  , "rc4-plain-exp" },
+    { KRB5_ENCTYPE_RC4_PLAIN      , "rc4-plain" },
+    { KRB5_ENCTYPE_RC4_PLAIN_OLD_EXP, "rc4-plain-old-exp" },
+    { KRB5_ENCTYPE_RC4_HMAC_OLD_EXP, "rc4-hmac-old-exp" },
+    { KRB5_ENCTYPE_RC4_PLAIN_OLD  , "rc4-plain-old" },
+    { KRB5_ENCTYPE_RC4_HMAC_OLD   , "rc4-hmac-old" },
+    { KRB5_ENCTYPE_DES_PLAIN      , "des-plain" },
+    { KRB5_ENCTYPE_RC4_SHA        , "rc4-sha" },
+    { KRB5_ENCTYPE_RC4_LM         , "rc4-lm" },
+    { KRB5_ENCTYPE_RC4_PLAIN      , "rc4-plain" },
+    { KRB5_ENCTYPE_RC4_MD4        , "rc4-md4" },
     { 0                           , NULL },
 };
 
+static const value_string krb5_checksum_types[] = {
+    { KRB5_CHKSUM_NONE            , "none" },
+    { KRB5_CHKSUM_CRC32           , "crc32" },
+    { KRB5_CHKSUM_MD4             , "md4" },
+    { KRB5_CHKSUM_KRB_DES_MAC     , "krb-des-mac" },
+    { KRB5_CHKSUM_KRB_DES_MAC_K   , "krb-des-mac-k" },
+    { KRB5_CHKSUM_MD5             , "md5" },
+    { KRB5_CHKSUM_MD5_DES         , "md5-des" },
+    { KRB5_CHKSUM_HMAC_MD5        , "hmac-md5" },
+    { KRB5_CHKSUM_MD5_HMAC        , "md5-hmac" },
+    { KRB5_CHKSUM_RC4_MD5         , "rc5-md5" },
+    { KRB5_CHKSUM_MD25            , "md25" },
+    { KRB5_CHKSUM_DES_MAC_MD5     , "des-mac-md5" },
+    { KRB5_CHKSUM_DES_MAC         , "des-mac" },
+    { KRB5_CHKSUM_REAL_CRC32      , "real-crc32" },
+    { KRB5_CHKSUM_SHA1            , "sha1" },
+    { KRB5_CHKSUM_LM              , "lm" },
+    { 0                           , NULL },
+};
 
 #define KRB5_AD_IF_RELEVANT			1
 #define KRB5_AD_INTENDED_FOR_SERVER		2
@@ -3565,7 +3641,7 @@ proto_register_kerberos(void)
 	    NULL, 0, "Version Number for the encryption Key", HFILL }},
 	{ &hf_krb_checksum_type, {
 	    "Type", "kerberos.checksum.type", FT_UINT32, BASE_DEC,
-	    NULL, 0, "Type of checksum", HFILL }},
+	    VALS(krb5_checksum_types), 0, "Type of checksum", HFILL }},
 	{ &hf_krb_authenticator_vno, {
 	    "Authenticator vno", "kerberos.authenticator_vno", FT_UINT32, BASE_DEC,
 	    NULL, 0, "Version Number for the Authenticator", HFILL }},
