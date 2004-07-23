267b501cae8bfa5e614ed05179422dd5acae86d0
Guy Harris <guy@alum.mit.edu>
2004-07-23 06:37:58 +0000

    "iax_lookup_circuit_details()" can return null; check whether it's null before using it.
    svn path=/trunk/; revision=11477

--- epan/dissectors/packet-iax2.c
+++ epan/dissectors/packet-iax2.c
@@ -1307,10 +1307,12 @@ static iax_packet_data *iax2_get_packet_data_for_minipacket(packet_info * pinfo,
     p_add_proto_data(pinfo->fd,proto_iax2,p);
 
     /* set the codec for this frame to be whatever the last full frame used */
-    if( video ) 
-      p->codec = reversed ? iax_call -> dst_vformat : iax_call -> src_vformat;
-    else 
-      p->codec = reversed ? iax_call -> dst_codec : iax_call -> src_codec;
+    if( iax_call ) {
+     if( video ) 
+        p->codec = reversed ? iax_call -> dst_vformat : iax_call -> src_vformat;
+      else 
+        p->codec = reversed ? iax_call -> dst_codec : iax_call -> src_codec;
+    }
   }
   return p;
 }
