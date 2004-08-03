839034889e18ad6e9b92adcbb839ee67a32c570c
Gerald Combs <gerald@wireshark.org>
2004-08-03 14:51:12 +0000

    Add a missing file.
    svn path=/branches/win32-native/; revision=11596

---
+++ xulender/win32-c-sdk/win32-menu.h
@@ -0,0 +1,7 @@
+#ifndef __WIN32_MENU_H__
+#define __WIN32_MENU_H__
+
+void open_menu_recent_capture_file(HWND hw_mainwin, guint menu_id);
+void clear_menu_recent(HWND hw_mainwin);
+
+#endif /* win32-menu.h */
