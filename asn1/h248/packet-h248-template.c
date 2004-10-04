a1d0c69d8dd3d41bec9b6985ceb5aaa90489ab26
Anders Broman <anders.broman@ericsson.com>
2004-10-04 05:50:55 +0000

    Updated packetname value string.
    svn path=/trunk/; revision=12194

--- asn1/h248/packet-h248-template.c
+++ asn1/h248/packet-h248-template.c
@@ -59,7 +59,159 @@ static gint ett_packagename = -1;
 
 
 static const value_string package_name_vals[] = {
-	{0x001E,     "Bearer Characteristics G.1950 Annex A"},
+  {   0x0000, "addReq" },
+  {   0x0001, "g H.248.1 Annex E" },
+  {   0x0002, "root H.248.1 Annex E" },
+  {   0x0003, "tonegen H.248.1 Annex E" },
+  {   0x0004, "tonedet H.248.1 Annex E" },
+  {   0x0005, "dg H.248.1 Annex E" },
+  {   0x0006, "dd H.248.1 Annex E" },
+  {   0x0007, "cg H.248.1 Annex E" }, 
+  {   0x0008, "cd H.248.1 Annex E" },
+  {   0x0009, "al H.248.1 Annex E" },
+  {   0x000a, "ct H.248.1 Annex E" }, 
+  {   0x000b, "nt H.248.1 Annex E" },
+  {   0x000c, "rtp H.248.1 Annex E" },
+  {   0x000d, "tdmc H.248.1 Annex E" },
+  {   0x000e, "ftmd H.248.1 Annex E" },
+  {   0x000f, "txc H.248.2" },		/* H.248.2 */
+  {   0x0010, "txp H.248.2" },
+  {   0x0011, "ctyp H.248.2" },
+  {   0x0012, "fax H.248.2" }
+  {   0x0013, "ipfax H.248.2" },
+  {   0x0014, "dis H.248.3" },		*/ H.248.3 */
+  {   0x0015, "key H.248.3" },
+  {   0x0016, "kp H.248.3" },
+  {   0x0017, "labelkey H.248.3" },   
+  {   0x0018, "kf H.248.3" },
+  {   0x0019, "ind H.248.3" },
+  {   0x001a, "ks H.248.3" },
+  {   0x001b, "anci H.248.3" },
+  {   0x001c, "dtd H.248.6" },		/* H.248.6 */
+  {   0x001d, "an H.248.7" },		/* H.248.7 */
+  {   0x001e, "Bearer Characteristics Q.1950 Annex A" }, 	/* Q.1950 Annex A */ 
+  {   0x001f, "Bearer Network Connection Cut Q.1950 Annex A" },
+  {   0x0020, "Reuse Idle Q.1950 Annex A" },
+  {   0x0021, "Generic Bearer Connection Q.1950 Annex A" }, 
+  {   0x0022, "Bearer Control Tunnelling Q.1950 Annex A" },
+  {   0x0023, "Basic Call Progress Tones Q.1950 Annex A" },
+  {   0x0024, "Expanded Call Progress Tones Q.1950 Annex A" },
+  {   0x0025, "Basic Services Tones Q.1950 Annex A" },
+  {   0x0026, "Expanded Services Tones Q.1950 Annex A" }
+  {   0x0027, "Intrusion Tones Q.1950 Annex A" },
+  {   0x0028, "Business Tones Q.1950 Annex A" },
+  {   0x0029, "Media Gateway Resource Congestion Handling H.248.10" },	/* H.248.10 */
+  {   0x002a, "H245 package H248.12" },					/* H.248.12 */
+  {   0x002b, "H323 bearer control package H.248.12" },			/* H.248.12 */
+  {   0x002c, "H324 package H.248.12" },				/* H.248.12 */
+  {   0x002d, "H245 command package H.248.12" },			/* H.248.12 */
+  {   0x002e, "H245 indication package H.248.12" },			/* H.248.12 */
+  {   0x002f, "3G User Plane" },					/* 3GPP TS 29.232 v4.1.0 */
+  {   0x0030, "3G Circuit Switched Data" },
+  {   0x0031, "3G TFO Control" },
+  {   0x0032, "3G Expanded Call Progress Tones" },
+  {   0x0033, "Advanced Audio Server (AAS) Base)" },			/* H.248.9 */
+  {   0x0034, "AAS Digit Collection" }, 				/* H.248.9 */
+  {   0x0035, "AAS Recording" }, 					/* H.248.9 */
+  {   0x0036, "AAS Segment Management" },				/* H.248.9 */ 
+  {   0x0037, "Quality Alert Ceasing" },				/* H.248.13 */
+  {   0x0038, "Conferencing Tones Generation" },			/* H.248.27 */
+  {   0x0039, "Diagnostic Tones Generation" },				/* H.248.27 */
+  {   0x003a, "Carrier Tones Generation Package H.248.23" },		/* H.248.27 */
+  {   0x003b, "Enhanced Alerting Package H.248.23" },			/* H.248.23 */
+  {   0x003c, "Analog Display Signalling Package H.248.23" },		/* H.248.23 */
+  {   0x003d, "Multi-Frequency Tone Generation Package H.248.24" },	/* H.248.24 */												   
+  {   0x003e, "H.248.23Multi-Frequency Tone Detection Package H.248.24" }, /* H.248.24 */
+  {   0x003f, "" },Basic CAS Package H.248.25" },			/* H.248.25 */												   
+  {   0x0040, "rbs" },Robbed Bit Signalling Package H.248.25" },        /* H.248.25 */
+  {   0x0041, "Operator Services and Emgergency Services Package H.248.25" },												   
+  {   0x0042, "Operator Services Extension Package H.248.25" },
+  {   0x0043, "Extended Analog Line Supervision Package H.248.26" },
+  {   0x0044, "Automatic Metering Package H.248.26" },  
+  {   0x0045, "Inactivity Timer Package H.248.14" },      
+  {   0x0046, "3G Modification of Link Characteristics Bearer Capability" }, /* 3GPP TS 29.232 v4.4.0 */ 
+  {   0x0047, "Base Announcement Syntax H.248.9" },
+  {   0x0048, "Voice Variable Syntax H.248.9" },
+  {   0x0049, "Announcement Set Syntax H.248.9" },
+  {   0x004a, "Phrase Variable Syntax H.248.9" },
+  {   0x004b, "Basic NAS package" },
+  {   0x004c, "NAS incoming package" },
+  {   0x004d, "NAS outgoing package" },
+  {   0x004e, "NAS control package" },
+  {   0x004f, "NAS root package" },
+  {   0x0050, "Profile Handling Package H.248.18" }, 
+  {   0x0051, "Media Gateway Overload Control Package H.248.11" }, 
+  {   0x0052, "Extended DTMF Detection Package H.248.16" },
+  {   0x0053, "Quiet Termination Line Test" },
+  {   0x0054, "Loopback Line Test Response" }, 				/* H.248.17 */
+  {   0x0055. "ITU 404Hz Line Test" },					/* H.248.17 */
+  {   0x0056, "ITU 816Hz Line Test" },					/* H.248.17 */
+  {   0x0057, "ITU 1020Hz Line Test" },					/* H.248.17 */
+  {   0x0058, "ITU 2100Hz Disable Tone Line Test			/* H.248.17 */
+  {   0x0059, "ITU 2100Hz Disable Echo Canceller Tone Line Test		/* H.248.17 */
+  {   0x005a, "ITU 2804Hz Tone Line Test" },				/* H.248.17 */
+  {   0x005b, "ITU Noise Test Tone Line Test" },			/* H.248.17 */
+  {   0x005c, "ITU Digital Pseudo Random Test Line Test" },		/* H.248.17 */
+  {   0x005d, "ITU ATME No.2 Test Line Response" },			/* H.248.17 */
+  {   0x005e, "ANSI 1004Hz Test Tone Line Test" },			/* H.248.17 */
+  {   0x005f, "ANSI Test Responder Line Test" },			/* H.248.17 */
+  {   0x0060, "ANSI 2225Hz Test Progress Tone Line Test" },		/* H.248.17 */
+  {   0x0061, "ANSI Digital Test Signal Line Test" },			/* H.248.17 */
+  {   0x0062, "ANSI Inverting Loopback Line Test Repsonse" },		/* H.248.17 */
+  {   0x0063, "Extended H.324 Packages H.248.12 Annex A" },
+  {   0x0064, "Extended H.245 Command Package H.248.12 Annex A" },
+  {   0x0065, "Extended H.245 Indication Package H.248.12 Annex A" },
+  {   0x0066, "Enhanced DTMF Detection Package H.248.16" }, 
+  {   0x0067, "Connection Group Identity Package Q.1950 Annex E" }, 
+  {   0x0068, "CTM Text Transport 3GPP TS 29.232 v5.2.0" }, 
+  {   0x0069, "SPNE Control Package Q.115.0" },
+  {   0x006a, "Semi-permanent Connection Package H.248.21" },
+  {   0x006b, "Shared Risk Group Package H.248.22" },
+  {   0x006c, "isuptn Annex B of ITU-T Rec. J.171" },
+  {   0x006d, "Basic CAS Addressing Package H.248.25" },
+  {   0x006e, "Floor Control Package H.248.19" },
+  {   0x006f, "Indication of Being Viewed Package H.248.19" },
+  {   0x0070, "Volume Control Package H.248.19" },
+  {   0x0071, "UNASSIGNED" },
+  {   0x0072, "Volume Detection Package H.248.19" },
+  {   0x0073, "Volume Level Mixing Package H.248.19" },
+  {   0x0074, "Mixing Volume Level Control Package H.248.19" },
+  {   0x0075, "Voice Activated Video Switch Package H.248.19" },
+  {   0x0076, "Lecture Video Mode Package H.248.19" },
+  {   0x0077, "Contributing Video Source Package H.248.19" },
+  {   0x0078, "Video Window Package H.248.19" },
+  {   0x0079, "Tiled Window Package H.248.19" },
+  {   0x007a, "Adaptive Jitter Buffer Package H.248.31" },
+  {   0x007b, "International CAS Package H.248.28" },
+  {   0x007c, "CAS Blocking Package H.248.28" },
+  {   0x007d, "International CAS Compelled Package H.248.29" },
+  {   0x007e, "International CAS Compelled with Overlap Package H.248.29" },
+  {   0x007f, "International CAS Compelled with End-to-end Package H.248.29" },
+  {   0x0080, "RTCP XR Package H.248.30" },
+  {   0x0081, "RTCP XR Burst Metrics Package H.248.30" },
+  {   0x0082, "threegcsden 3G Circuit Switched Data" },				/* 3GPP TS 29.232 v5.6.0 */
+  {   0x0083, "threegiptra 3G Circuit Switched Data" },				/* 3GPP TS 29.232 v5.6.0 */
+  {   0x0084, "threegflex 3G Circuit Switched Data" },				/* 3GPP TS 29.232 v5.6.0 */												   
+  {   0x0085, "H.248 PCMSB" },
+  {   0x008a, "TIPHON Extended H.248/MEGACO Package" },				/* ETSI specification TS 101 3 */
+  {   0x008b, "Differentiated Services Package" },				/* Annex A of ETSI TS 102 333 */
+  {   0x008c, "Gate Management Package" },					/* Annex B of ETSI TS 102 333 */
+  {   0x008d, "Traffic Management Package" },					/* Annex C of ETSI TS 102 333 */
+  {   0x008e, "Gate Recovery Information Package" },				/* Annex D of ETSI TS 102 333 */
+  {   0x008f, "NAT Traversal Package" },					/* Annex E of ETSI TS 102 333 */
+  {   0x0090, "MPLS Package" },							/* Annex F of ETSI TS 102 333 */
+  {   0x0091, "VLAN Package" },							/* Annex G of ETSI TS 102 333 */
+  {   0x8000, "Ericsson IU" }, 
+  {   0x8001, "Ericsson UMTS and GSM Circuit" },
+  {   0x8002, "Ericsson Tone Generator Package" },
+  {   0x8003, "Ericsson Line Test Package" },
+  {   0x8004, "Nokia Advanced TFO Package" },
+  {   0x8005, "Nokia IWF Package" },
+  {   0x8006, "Nokia Root Package" },
+  {   0x8007, "Nokia Trace Package" },
+  {   0x8008, "Ericsson  V5.2 Layer" },
+  {   0x8009, "Ericsson Detailed Termination Information Package" },
+  {   0x800a, "Nokia Bearer Characteristics Package" },
 	{0,     NULL}
 };
 
