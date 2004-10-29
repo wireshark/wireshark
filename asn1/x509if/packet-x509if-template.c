0e7d35f8b43388af79cb34c9908c509c13e4a25a
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2004-10-29 11:10:31 +0000

    Update to x509if  use a workaround to emulate the type ANY and remove stuff from teh template
    svn path=/trunk/; revision=12431

--- asn1/x509if/packet-x509if-template.c
+++ asn1/x509if/packet-x509if-template.c
@@ -42,12 +42,14 @@
 
 /* Initialize the protocol and registered fields */
 int proto_x509if = -1;
-int hf_x509if_ATADV_attribute_id = -1;
+int hf_x509if_object_identifier_id = -1;
 #include "packet-x509if-hf.c"
 
 /* Initialize the subtree pointers */
 static gint ett_x509if_Attribute = -1;
 #include "packet-x509if-ett.c"
+
+static char object_identifier_id[64]; /*64 chars should be long enough? */
 
 
 static const ber_sequence Attribute_sequence[] = {
@@ -65,37 +67,6 @@ dissect_x509if_Attribute(gboolean implicit_tag, tvbuff_t *tvb, int offset, packe
 }
 
 
-
-static char ATADV_attribute_id[64]; /*64 chars should be long enough? */
-static int 
-dissect_hf_x509if_ATADV_attribute_id(packet_info *pinfo, proto_tree *tree, tvbuff_t *tvb, int offset) 
-{
-  offset = dissect_ber_object_identifier(FALSE, pinfo, tree, tvb, offset,
-                                         hf_x509if_ATADV_attribute_id, ATADV_attribute_id);
-  return offset;
-}
-static int 
-dissect_hf_x509if_ATADV_attribute_value(packet_info *pinfo, proto_tree *tree, tvbuff_t *tvb, int offset) 
-{
-  offset=call_ber_oid_callback(ATADV_attribute_id, tvb, offset, pinfo, tree);
-  return offset;
-}
-
-static const ber_sequence AttributeTypeAndDistinguishedValue_sequence[] = {
-  { BER_CLASS_UNI, BER_UNI_TAG_OID, BER_FLAGS_NOOWNTAG, dissect_hf_x509if_ATADV_attribute_id },
-  { BER_CLASS_ANY, 0, BER_FLAGS_NOOWNTAG, dissect_hf_x509if_ATADV_attribute_value },
-  /*XXX  missing stuff here */
-  { 0, 0, 0, NULL }
-};
-
-static int
-dissect_x509if_AttributeTypeAndDistinguishedValue(gboolean implicit_tag, tvbuff_t *tvb, int offset, packet_info *pinfo _U_, proto_tree *tree, int hf_index) {
-  offset = dissect_ber_sequence(implicit_tag, pinfo, tree, tvb, offset,
-                                AttributeTypeAndDistinguishedValue_sequence, hf_index, ett_x509if_AttributeTypeAndDistinguishedValue);
-
-  return offset;
-}
-
 #include "packet-x509if-fn.c"
 
 
@@ -104,10 +75,10 @@ void proto_register_x509if(void) {
 
   /* List of fields */
   static hf_register_info hf[] = {
-    { &hf_x509if_ATADV_attribute_id,
-      { "Attribute Id", "x509if.attribute.id",
-        FT_STRING, BASE_NONE, NULL, 0,
-        "Attribute Id", HFILL }},
+    { &hf_x509if_object_identifier_id, 
+      { "Id", "x509if.id", FT_STRING, BASE_NONE, NULL, 0,
+	"Object identifier Id", HFILL }},
+			 
 #include "packet-x509if-hfarr.c"
   };
 
