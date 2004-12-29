7dbaf3025987c7d90db933d264298a13369d8f54
Guy Harris <guy@alum.mit.edu>
2004-12-29 21:31:18 +0000

    From Albert Chin: "config.h" should always be included first first when using AC_SYS_LARGEFILE.
    svn path=/trunk/; revision=12860

--- gtk/cfilter_combo_utils.c
+++ gtk/cfilter_combo_utils.c
@@ -22,6 +22,10 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <string.h>
 #include <gtk/gtk.h>
 #include "compat_macros.h"
