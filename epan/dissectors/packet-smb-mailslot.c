cea394348c196c71af23df0baf3805dde77a1443
Gerald Combs <gerald@wireshark.org>
2005-04-13 22:04:50 +0000

    Fix a bug introduced by my previous checkin.
    svn path=/trunk/; revision=14067

--- epan/dissectors/packet-smb-mailslot.c
+++ epan/dissectors/packet-smb-mailslot.c
@@ -103,9 +103,11 @@ dissect_mailslot_smb(tvbuff_t *mshdr_tvb, tvbuff_t *setup_tvb,
 		col_set_str(pinfo->cinfo, COL_PROTOCOL, "SMB Mailslot");
 	}
 
-	if (check_col (pinfo->cinfo, COL_INFO) && ((tvb==NULL) || (tvb_reported_length(tvb)==0))) {
+	if ((tvb==NULL) || (tvb_reported_length(tvb)==0)) {
 		/* Interim reply */
-		col_set_str(pinfo->cinfo, COL_INFO, "Interim reply");
+		if (check_col (pinfo->cinfo, COL_INFO)) {
+			col_set_str(pinfo->cinfo, COL_INFO, "Interim reply");
+		}
 		return TRUE;
 	}
 
