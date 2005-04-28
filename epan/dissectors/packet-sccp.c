537fa72b2250b7cc44a2b036c88e72577b159568
Michael Tüxen <tuexen@fh-muenster.de>
2005-04-28 14:53:41 +0000

    Fix buffer overflow which is the cause of BUG 131.
    svn path=/trunk/; revision=14219

--- epan/dissectors/packet-sccp.c
+++ epan/dissectors/packet-sccp.c
@@ -716,7 +716,7 @@ dissect_sccp_gt_address_information(tvbuff_t *tvb, proto_tree *tree,
 {
   guint offset = 0;
   guint8 odd_signal, even_signal = 0x0f;
-  char gt_digits[GT_MAX_SIGNALS] = { 0 };
+  char gt_digits[GT_MAX_SIGNALS+1] = { 0 };
 
   while(offset < length)
   {
@@ -724,13 +724,13 @@ dissect_sccp_gt_address_information(tvbuff_t *tvb, proto_tree *tree,
     even_signal = tvb_get_guint8(tvb, offset) & GT_EVEN_SIGNAL_MASK;
     even_signal >>= GT_EVEN_SIGNAL_SHIFT;
 
-    strcat(gt_digits, val_to_str(odd_signal, sccp_address_signal_values,
-				 "Unknown"));
-
+    strncat(gt_digits, val_to_str(odd_signal, sccp_address_signal_values,
+				 "Unknown"), GT_MAX_SIGNALS - strlen(gt_digits));
+    
     /* If the last signal is NOT filler */
     if (offset != (length - 1) || even_length == TRUE)
-      strcat(gt_digits, val_to_str(even_signal, sccp_address_signal_values,
-				   "Unknown"));
+      strncat(gt_digits, val_to_str(even_signal, sccp_address_signal_values,
+				   "Unknown"), GT_MAX_SIGNALS - strlen(gt_digits));
 
     offset += GT_SIGNAL_LENGTH;
   }
