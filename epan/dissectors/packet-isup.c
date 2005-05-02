f8765a5a284e63e4e38d93a96561291493f7efd7
Gerald Combs <gerald@wireshark.org>
2005-05-02 14:41:07 +0000

    From Cvetan Ivanov:
    1) dissect_isup_calling_party_number_parameter reported malformed packet
    if the calling party number IE contains empty number (possibly with
    presentation set to 'number not available')
    
    I've basicly added if(length > 0) around the digits tree dissector,
    which may not be obvious from the patch becase of the indenting of the
    entire block.
    
    2) As the tap_calling_number is static, if there is call with missing
    calling_party_number IE, it gets assigned the calling party number of
    the previuos call containing such IE.
    
    fixed by adding tap_calling_number = NULL;
    in the beginning of dissect_isup_message, and
    
    tap_rec.calling_number=tap_calling_number?tap_calling_number:g_strdup("");
    
    in the end to avoid segfault
    
    Those things may not happen too often in the telecom world, but happen
    all the time with VoIP.
    
    
    svn path=/trunk/; revision=14268

--- epan/dissectors/packet-isup.c
+++ epan/dissectors/packet-isup.c
@@ -2848,50 +2848,51 @@ dissect_isup_calling_party_number_parameter(tvbuff_t *parameter_tvb, proto_tree 
   proto_tree_add_uint(parameter_tree, hf_isup_screening_indicator, parameter_tvb, 1, 1, indicators2);
   offset = 2;
 
-  address_digits_item = proto_tree_add_text(parameter_tree, parameter_tvb,
-					    offset, -1,
-					    "Calling Party Number");
-  address_digits_tree = proto_item_add_subtree(address_digits_item, ett_isup_address_digits);
-
   length = tvb_length_remaining(parameter_tvb, offset);
-  while(length > 0){
-    address_digit_pair = tvb_get_guint8(parameter_tvb, offset);
-    proto_tree_add_uint(address_digits_tree, hf_isup_calling_party_odd_address_signal_digit, parameter_tvb, offset, 1, address_digit_pair);
-    calling_number[i++] = number_to_char(address_digit_pair & ISUP_ODD_ADDRESS_SIGNAL_DIGIT_MASK);
-    if (i > MAXDIGITS)
-      THROW(ReportedBoundsError);
-    if ((length - 1) > 0 ){
-      proto_tree_add_uint(address_digits_tree, hf_isup_calling_party_even_address_signal_digit, parameter_tvb, offset, 1, address_digit_pair);
-      calling_number[i++] = number_to_char((address_digit_pair & ISUP_EVEN_ADDRESS_SIGNAL_DIGIT_MASK) / 0x10);
-      if (i > MAXDIGITS)
-	THROW(ReportedBoundsError);
-    }
-    offset++;
-    length = tvb_length_remaining(parameter_tvb, offset);
-  }
+  if (length > 0) {
+    address_digits_item = proto_tree_add_text(parameter_tree, parameter_tvb,
+  					      offset, -1,
+					      "Calling Party Number");
+    address_digits_tree = proto_item_add_subtree(address_digits_item, ett_isup_address_digits);
 
-  if  (((indicators1 & 0x80) == 0) && (tvb_length(parameter_tvb) > 0)){ /* Even Indicator set -> last even digit is valid & has be displayed */
+    while(length > 0){
+      address_digit_pair = tvb_get_guint8(parameter_tvb, offset);
+      proto_tree_add_uint(address_digits_tree, hf_isup_calling_party_odd_address_signal_digit, parameter_tvb, offset, 1, address_digit_pair);
+      calling_number[i++] = number_to_char(address_digit_pair & ISUP_ODD_ADDRESS_SIGNAL_DIGIT_MASK);
+      if (i > MAXDIGITS)
+        THROW(ReportedBoundsError);
+      if ((length - 1) > 0 ){
+        proto_tree_add_uint(address_digits_tree, hf_isup_calling_party_even_address_signal_digit, parameter_tvb, offset, 1, address_digit_pair);
+        calling_number[i++] = number_to_char((address_digit_pair & ISUP_EVEN_ADDRESS_SIGNAL_DIGIT_MASK) / 0x10);
+        if (i > MAXDIGITS)
+  	  THROW(ReportedBoundsError);
+      }
+      offset++;
+      length = tvb_length_remaining(parameter_tvb, offset);
+    }
+
+    if  (((indicators1 & 0x80) == 0) && (tvb_length(parameter_tvb) > 0)){ /* Even Indicator set -> last even digit is valid & has be displayed */
       proto_tree_add_uint(address_digits_tree, hf_isup_calling_party_even_address_signal_digit, parameter_tvb, offset - 1, 1, address_digit_pair);
       calling_number[i++] = number_to_char((address_digit_pair & ISUP_EVEN_ADDRESS_SIGNAL_DIGIT_MASK) / 0x10);
       if (i > MAXDIGITS)
 	THROW(ReportedBoundsError);
+    }
+    proto_item_set_text(address_digits_item, "Calling Party Number: %s", calling_number);
+    calling_number[i++] = '\0';
+    if ( number_plan == 1 ) {
+      e164_info.e164_number_type = CALLING_PARTY_NUMBER;
+      e164_info.nature_of_address = indicators1 & 0x7f;
+      e164_info.E164_number_str = calling_number;
+      e164_info.E164_number_length = i - 1;
+      dissect_e164_number(parameter_tvb, address_digits_tree, 2, (offset - 2), e164_info);
+      proto_tree_add_string_hidden(address_digits_tree, hf_isup_calling, parameter_tvb,
+  	    offset - length, length, calling_number);
+    } else {
+      proto_tree_add_string(address_digits_tree, hf_isup_calling, parameter_tvb,
+ 	  offset - length, length, calling_number);
+    }
   }
-  calling_number[i++] = '\0';
-
-  proto_item_set_text(address_digits_item, "Calling Party Number: %s", calling_number);
   proto_item_set_text(parameter_item, "Calling Party Number: %s", calling_number);
-  if ( number_plan == 1 ) {
-    e164_info.e164_number_type = CALLING_PARTY_NUMBER;
-    e164_info.nature_of_address = indicators1 & 0x7f;
-    e164_info.E164_number_str = calling_number;
-    e164_info.E164_number_length = i - 1;
-    dissect_e164_number(parameter_tvb, address_digits_tree, 2, (offset - 2), e164_info);
-    proto_tree_add_string_hidden(address_digits_tree, hf_isup_calling, parameter_tvb,
-	  offset - length, length, calling_number);
-  } else {
-    proto_tree_add_string(address_digits_tree, hf_isup_calling, parameter_tvb,
-	  offset - length, length, calling_number);
-  }
   tap_calling_number = g_strdup(calling_number);
 }
 /* ------------------------------------------------------------------
@@ -5213,6 +5214,7 @@ dissect_isup_message(tvbuff_t *message_tvb, packet_info *pinfo, proto_tree *isup
   guint8 message_type, opt_parameter_pointer;
   gint opt_part_possible = FALSE; /* default setting - for message types allowing optional
 				     params explicitely set to TRUE in case statement */
+  tap_calling_number = NULL;
   offset = 0;
 
   /* Extract message type field */
@@ -5440,7 +5442,7 @@ dissect_isup_message(tvbuff_t *message_tvb, packet_info *pinfo, proto_tree *isup
      proto_tree_add_text(isup_tree, message_tvb, 0, 0, "No optional parameters are possible with this message type");
    /* if there are calling/called number, we'll get them for the tap */
 
-   tap_rec.calling_number=tap_calling_number;
+   tap_rec.calling_number=tap_calling_number?tap_calling_number:g_strdup("");
    tap_rec.called_number=tap_called_number;
    tap_rec.cause_value=tap_cause_value;
    tap_queue_packet(isup_tap, pinfo, &tap_rec);
