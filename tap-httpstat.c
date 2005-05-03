455ae72ef83c7b87abb3eceea2b15765f1ff9cbd
Gerald Combs <gerald@wireshark.org>
2005-05-03 18:36:05 +0000

    Copy over r14286 from the trunk:
    Fix typos in error messages.
    
    svn path=/releases/ethereal-0.10.11/; revision=14294

--- tap-httpstat.c
+++ tap-httpstat.c
@@ -313,7 +313,7 @@ gtk_httpstat_init(char *optarg)
 		/* error, we failed to attach to the tap. clean up */
 		g_free(sp->filter);
 		g_free(sp);
-		fprintf (stderr, "tethereal: Coulnd't register http,stat tap: %s\n",
+		fprintf (stderr, "tethereal: Couldn't register http,stat tap: %s\n",
 				error_string->str);
 		g_string_free(error_string, TRUE);
 		exit(1);
