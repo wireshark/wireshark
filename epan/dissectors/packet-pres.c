c2b4cd7993b2e3f8b454f3cae9a381b65fe6fb62
Gerald Combs <gerald@wireshark.org>
2005-05-04 15:35:21 +0000

    Don't roll our own buffer-overflow-able string_to_hex() routine when there's a perfectly good tvb_bytes_to_str() lying around, just _begging_ to be used.
    Fix up white space.
    
    svn path=/trunk/; revision=14304

--- epan/dissectors/packet-pres.c
+++ epan/dissectors/packet-pres.c
@@ -247,33 +247,6 @@ call_acse_dissector(tvbuff_t *tvb, gint offset, gint param_len,
 		ENDTRY;
 	}
 }
-static char*
-string_to_hex(const unsigned char * in,char * out,int len)
-{
-	char ascii[MAXSTRING];
-	int  i;
-	memset(&ascii,0x00,sizeof(ascii));
-for(i=0;i<len;i++)
-			{
-	unsigned char o_out = *(in+i);
-    sprintf(out+(i<<1),"%.2x",*  (in+i));
-	if(  ( (o_out) >= 'a') & ( (o_out) <='z')  ||
-		 ( (o_out) >= 'A') & ( (o_out) <='Z')  ||
-		 ( (o_out) >= '0') & ( (o_out) <='9')
-	   )
-					{
-					ascii[i] = o_out;
-					}
-				else
-					{
-					ascii[i] = '.';
-					}
-
-			}
-		strcat(out," ");
-		strcat(out,ascii);
-    return out;
-}
 
 static int read_length(ASN1_SCK *a, proto_tree *tree, int hf_id, guint *len)
 {
@@ -498,17 +471,18 @@ display_string);
 }
 
 static void
-print_value(ASN1_SCK *asn,proto_tree *pres_tree,tvbuff_t *tvb,int 
-*offset,int item_len)
+print_value(ASN1_SCK *asn,proto_tree *pres_tree, tvbuff_t *tvb, int *offset, int item_len)
 {
-	  gint    start = *offset;
-	  char    tmp[MAXSTRING];
-		*offset = asn->offset;  /* align to data*/
-		string_to_hex(tvb_get_ptr(tvb,*offset,item_len),tmp,item_len);
-		proto_tree_add_text(pres_tree, tvb, *offset, item_len, tmp);
-		(*offset)=start+item_len;
-		asn->offset = (*offset);
+    gint    start = *offset;
+    gchar  *tmp;
+
+    *offset = asn->offset;  /* align to data*/
+    tmp = tvb_bytes_to_str(tvb, *offset, item_len);
+    proto_tree_add_text(pres_tree, tvb, *offset, item_len, tmp);
+    (*offset) = start+item_len;
+    asn->offset = (*offset);
 }
+
 static int
 get_integer_value(ASN1_SCK *asn,int length,int *offset)
 {
