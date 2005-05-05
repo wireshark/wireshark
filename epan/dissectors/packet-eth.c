f98e6c2533e3a74a5180b074d9db6fb34f3fb1cc
Guy Harris <guy@alum.mit.edu>
2005-05-05 11:02:31 +0000

    Squelch a compiler warning.
    svn path=/trunk/; revision=14313

--- epan/dissectors/packet-eth.c
+++ epan/dissectors/packet-eth.c
@@ -170,7 +170,7 @@ dissect_eth_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree,
   const char		*src_addr, *dst_addr;
   static eth_hdr 	ehdrs[4];
   static int		ehdr_num=0;
-  proto_tree		*tree;
+  proto_tree		*volatile tree;
 
   ehdr_num++;
   if(ehdr_num>=4){
