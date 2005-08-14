10f8a36eda97a3a62e3406294dc58ccd15c1344b
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2005-08-14 00:37:05 +0000

    get rid of another two GMemChunks
    svn path=/trunk/; revision=15347

--- epan/h225-persistentdata.c
+++ epan/h225-persistentdata.c
@@ -33,6 +33,7 @@
 #include <glib.h>
 #include <epan/packet.h>
 #include <epan/conversation.h>
+#include <epan/emem.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -40,9 +41,6 @@
 #include "h225-persistentdata.h"
 
 /* Global Memory Chunks for lists and Global hash tables*/
-
-static GMemChunk *h225ras_call_info_key_chunk = NULL;
-static GMemChunk *h225ras_call_info_value_chunk = NULL;
 
 static GHashTable *ras_calls[7] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
@@ -88,10 +86,10 @@ h225ras_call_t * new_h225ras_call(h225ras_call_info_key *h225ras_call_key, packe
 	   frame numbers are 1-origin, so we use 0
 	   to mean "we don't yet know in which frame
 	   the reply for this call appears". */
-	new_h225ras_call_key = (h225ras_call_info_key *)g_mem_chunk_alloc(h225ras_call_info_key_chunk);
+	new_h225ras_call_key = se_alloc(sizeof(h225ras_call_info_key));
 	new_h225ras_call_key->reqSeqNum = h225ras_call_key->reqSeqNum;
 	new_h225ras_call_key->conversation = h225ras_call_key->conversation;
-	h225ras_call = (h225ras_call_t *)g_mem_chunk_alloc(h225ras_call_info_value_chunk);
+	h225ras_call = se_alloc(sizeof(h225ras_call_t));
 	h225ras_call->req_num = pinfo->fd->num;
 	h225ras_call->rsp_num = 0;
 	h225ras_call->requestSeqNum = h225ras_call_key->reqSeqNum;
@@ -115,7 +113,7 @@ h225ras_call_t * append_h225ras_call(h225ras_call_t *prev_call, packet_info *pin
 	   frame numbers are 1-origin, so we use 0
 	   to mean "we don't yet know in which frame
 	   the reply for this call appears". */
-	h225ras_call = (h225ras_call_t *)g_mem_chunk_alloc(h225ras_call_info_value_chunk);
+	h225ras_call = se_alloc(sizeof(h225ras_call_t));
 	h225ras_call->req_num = pinfo->fd->num;
 	h225ras_call->rsp_num = 0;
 	h225ras_call->requestSeqNum = prev_call->requestSeqNum;
@@ -140,7 +138,7 @@ h225_init_routine(void)
 {
 	int i;
 
-	/* free hash-tables and mem_chunks for RAS SRT */
+	/* free hash-tables for RAS SRT */
 	for(i=0;i<7;i++) {
 		if (ras_calls[i] != NULL) {
 			g_hash_table_destroy(ras_calls[i]);
@@ -148,27 +146,10 @@ h225_init_routine(void)
 		}
 	}
 
-	if (h225ras_call_info_key_chunk != NULL) {
-		g_mem_chunk_destroy(h225ras_call_info_key_chunk);
-		h225ras_call_info_key_chunk = NULL;
-	}
-	if (h225ras_call_info_value_chunk != NULL) {
-		g_mem_chunk_destroy(h225ras_call_info_value_chunk);
-		h225ras_call_info_value_chunk = NULL;
-	}
-
-	/* create new hash-tables and mem_chunks for RAS SRT */
+	/* create new hash-tables for RAS SRT */
 
 	for(i=0;i<7;i++) {
 		ras_calls[i] = g_hash_table_new(h225ras_call_hash, h225ras_call_equal);
 	}
 
-	h225ras_call_info_key_chunk = g_mem_chunk_new("call_info_key_chunk",
-	    sizeof(h225ras_call_info_key),
-	    400 * sizeof(h225ras_call_info_key),
-	    G_ALLOC_ONLY);
-	h225ras_call_info_value_chunk = g_mem_chunk_new("call_info_value_chunk",
-	    sizeof(h225ras_call_t),
-	    400 * sizeof(h225ras_call_t),
-	    G_ALLOC_ONLY);
 }
