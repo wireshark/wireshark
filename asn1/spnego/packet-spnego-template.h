0060eaf3908ebdb5673b914a83ceca316d9591fb
Anders Broman <anders.broman@ericsson.com>
2005-09-15 05:38:37 +0000

    Replace the spnego dissector with an asn2eth generated one.
    svn path=/trunk/; revision=15810

---
+++ asn1/spnego/packet-spnego-template.h
@@ -0,0 +1,30 @@
+/* packet-spnego.h
+ * Routines for spnego packet dissection
+ *
+ * $Id$
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@ethereal.com>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef PACKET_SPNEGO_H
+#define PACKET_SPNEGO_H
+
+/*#include "packet-spnego-exp.h"*/
+
+#endif  /* PACKET_spnego_H */
