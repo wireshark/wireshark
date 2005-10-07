984850153d874b7ded006d6672cf3a3b61e9d2ed
Guy Harris <guy@alum.mit.edu>
2005-10-07 07:37:51 +0000

    Don't call "g_assert()" in dissectors - call "DISSECTOR_ASSERT()".
    svn path=/trunk/; revision=16151

--- plugins/h223/packet-h223.c
+++ plugins/h223/packet-h223.c
@@ -489,7 +489,7 @@ void h223_set_mc( packet_info* pinfo, guint8 mc, h223_mux_element* me )
 {
     circuit_t *circ = find_circuit( pinfo->ctype, pinfo->circuit_id, pinfo->fd->num );
     h223_vc_info* vc_info;
-    g_assert( circ );
+    DISSECTOR_ASSERT( circ );
     vc_info = circuit_get_proto_data(circ, proto_h223);
     add_h223_mux_element( &(vc_info->call_info->direction_data[pinfo->p2p_dir]), mc, me, pinfo->fd->num );
 }
@@ -498,7 +498,7 @@ void h223_add_lc( packet_info* pinfo, guint16 lc, h223_lc_params* params )
 {
     circuit_t *circ = find_circuit( pinfo->ctype, pinfo->circuit_id, pinfo->fd->num );
     h223_vc_info* vc_info;
-    g_assert( circ );
+    DISSECTOR_ASSERT( circ );
     vc_info = circuit_get_proto_data(circ, proto_h223);
     init_logical_channel( pinfo, vc_info->call_info, lc, pinfo->p2p_dir, params );
 }
