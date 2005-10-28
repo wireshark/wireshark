fa33d8ba22709813e31f92e0b706e378071234e4
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2005-10-28 22:13:13 +0000

    comment out the heuristics registration until we actually implement some heuristics
    svn path=/trunk/; revision=16353

--- epan/dissectors/packet-pvfs2.c
+++ epan/dissectors/packet-pvfs2.c
@@ -3579,6 +3579,6 @@ proto_reg_handoff_pvfs(void)
 	pvfs_handle = new_create_dissector_handle(dissect_pvfs, proto_pvfs);
 	dissector_add("tcp.port", TCP_PORT_PVFS2, pvfs_handle);
 
-	heur_dissector_add("tcp", dissect_pvfs_heur, proto_pvfs);
+/*	heur_dissector_add("tcp", dissect_pvfs_heur, proto_pvfs);*/
 }
 
