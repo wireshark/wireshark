e3c87533ae2e14ff8767955f400b0082f4f18b38
Jörg Mayer <jmayer@loplof.de>
2005-12-20 09:23:42 +0000

    packet-rnsap.[hc] were added to Makefile.common but missing from svn
    svn path=/trunk/; revision=16866

---
+++ epan/dissectors/packet-rnsap.h
@@ -0,0 +1,46 @@
+/* Do not modify this file.                                                   */
+/* It is created automatically by the ASN.1 to Ethereal dissector compiler    */
+/* ./packet-rnsap.h                                                           */
+/* ../../tools/asn2eth.py -X -e -p rnsap -c rnsap.cnf -s packet-rnsap-template rnsap.asn */
+
+/* Input file: packet-rnsap-template.h */
+
+#line 1 "packet-rnsap-template.h"
+/* packet-rnsap.h
+ * Routines for Universal Mobile Telecommunications System (UMTS);
+ * UTRAN Iur interface Radio Network Subsystem
+ * Application Part (RNSAP) signalling
+ * (3GPP TS 25.423 version 6.7.0 Release 6) packet dissection
+ * Copyright 2005, Anders Broman <anders.broman@ericsson.com>
+ *
+ * $Id$
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@ethereal.com>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef PACKET_RNSAP_H
+#define PACKET_RNSAP_H
+
+
+
+/*#include "packet-rnsap-exp.h" */
+
+#endif  /* PACKET_RNSAP_H */
+
+
