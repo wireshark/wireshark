da9e0c68313aff629b50f89d2b87276e8fa088ef
Luis Ontanon <luis.ontanon@gmail.com>
2006-01-24 00:04:53 +0000

    I forgot the Licence! set svn:keywords Id on source
    svn path=/trunk/; revision=17088

--- plugins/lua/lua_tvb.c
+++ plugins/lua/lua_tvb.c
@@ -1,3 +1,31 @@
+/*
+ * lua_tvb.c
+ *
+ * Ethereal's interface to the Lua Programming Language
+ *
+ * (c) 2006, Luis E. Garcia Ontanon <luis.ontanon@gmail.com>
+ *
+ * $Id$
+ *
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@ethereal.com>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #include "packet-lua.h"
 
 LUA_CLASS_DEFINE(Tvb,TVB,if (! *p) luaL_error(L,"null tvb"));
