246dc75d359ef5677e7ada7151466e53671be8f1
Luis Ontanon <luis.ontanon@gmail.com>
2006-02-04 20:34:16 +0000

    Propset eol-style and Id for all lua_gui.h has a licence
    svn path=/trunk/; revision=17152

--- plugins/lua/lua_gui.c
+++ plugins/lua/lua_gui.c
@@ -1,10 +1,27 @@
 /*
  *  lua_gui.c
  *  
+ * (c) 2006, Luis E. Garcia Ontanon <luis.ontanon@gmail.com>
+ * 
+ * $Id$
  *
- *  Created by L. E. G. O. on 2006/02/04.
- *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ * Ethereal - Network traffic analyzer
+ * By Gerald Combs <gerald@ethereal.com>
+ * Copyright 1998 Gerald Combs
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 #include "packet-lua.h"
@@ -101,7 +118,7 @@ static const luaL_reg TextWindow_methods[] = {
 
 static const luaL_reg TextWindow_meta[] = {
     {"__tostring", TextWindow_get_text},
-    {"__GC", TextWindow_gc},
+    {"__gc", TextWindow_gc},
     {0, 0}
 };
 
