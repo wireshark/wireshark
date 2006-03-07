14c4d6fad9bfe2c47edbfb20de4f8b8f6b95f804
Luis Ontanon <luis.ontanon@gmail.com>
2006-03-07 14:02:25 +0000

    From Jeff Morris,
    Avoid an off by one error in dissect_fields_dnsea() and dissect_fields_onsea()
    
    Fixes Bug 794
    
    
    svn path=/trunk/; revision=17495

--- epan/dissectors/packet-alcap.c
+++ epan/dissectors/packet-alcap.c
@@ -541,8 +541,7 @@ static gchar* dissect_fields_desea(packet_info* pinfo _U_, tvbuff_t *tvb, proto_
     
     e164->e164_number_type = CALLED_PARTY_NUMBER;
     e164->nature_of_address = tvb_get_guint8(tvb,offset) & 0x7f;
-    e164->E164_number_str = ep_tvb_memdup(tvb,offset+1,len);
-    e164->E164_number_str[len] = '\0';
+    e164->E164_number_str = tvb_get_ephemeral_string(tvb,offset+1,len);
     e164->E164_number_length = len-1;
     
     dissect_e164_number(tvb, tree, offset-1, len, *e164);
@@ -569,8 +568,7 @@ static gchar* dissect_fields_oesea(packet_info* pinfo _U_, tvbuff_t *tvb, proto_
     
     e164->e164_number_type = CALLING_PARTY_NUMBER;
     e164->nature_of_address = tvb_get_guint8(tvb,offset) & 0x7f;
-    e164->E164_number_str = ep_tvb_memdup(tvb,offset+1,len);
-    e164->E164_number_str[len] = '\0';
+    e164->E164_number_str = tvb_get_ephemeral_string(tvb,offset+1,len);
     e164->E164_number_length = len-1;
     
     dissect_e164_number(tvb, tree, offset-1, len, *e164);
