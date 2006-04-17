acb0e6d237ee125288dccca62ed29dffaacd4d05
Guy Harris <guy@alum.mit.edu>
2006-04-17 05:44:00 +0000

    Pull the plugin-specific code into "plugin.c", and generate a "register.c" with "make-reg-dotc"/"make-reg-dotc.py".
    Pull the lists of dissector .c and .h files into Makefile.common and
    have Makefile.am and Makefile.nmake use it.
    
    svn path=/trunk/; revision=17883

--- plugins/docsis/packet-docsis.c
+++ plugins/docsis/packet-docsis.c
@@ -49,21 +49,11 @@
 # include "config.h"
 #endif
 
-#include "moduleinfo.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include <gmodule.h>
-
 #include <epan/packet.h>
-
-
-#ifndef ENABLE_STATIC
-G_MODULE_EXPORT const gchar version[] = VERSION;
-#endif
-
 
 #include "packet-docsis.h"
 
@@ -769,85 +759,3 @@ proto_reg_handoff_docsis (void)
   docsis_mgmt_handle = find_dissector ("docsis_mgmt");
   eth_withoutfcs_handle = find_dissector ("eth_withoutfcs");
 }
-
-/* Start the functions we need for the plugin stuff */
-
-#ifndef ENABLE_STATIC
-
-G_MODULE_EXPORT void
-plugin_register (void)
-{
-  /* register the new protocol, protocol fields, and subtrees */
-  if (proto_docsis == -1)
-    {				/* execute protocol initialization only once */
-      proto_register_docsis ();
-      proto_register_docsis_bpkmattr ();
-      proto_register_docsis_dsarsp ();
-      proto_register_docsis_mgmt ();
-      proto_register_docsis_rngrsp ();
-      proto_register_docsis_bpkmreq ();
-      proto_register_docsis_dscack ();
-      proto_register_docsis_map ();
-      proto_register_docsis_tlv ();
-      proto_register_docsis_bpkmrsp ();
-      proto_register_docsis_dscreq ();
-      proto_register_docsis_regack ();
-      proto_register_docsis_uccreq ();
-      proto_register_docsis_dscrsp ();
-      proto_register_docsis_regreq ();
-      proto_register_docsis_uccrsp ();
-      proto_register_docsis_dsaack ();
-      proto_register_docsis_dsdreq ();
-      proto_register_docsis_regrsp ();
-      proto_register_docsis_ucd ();
-      proto_register_docsis_type29ucd ();
-      proto_register_docsis_dcd ();
-      proto_register_docsis_dccreq ();
-      proto_register_docsis_dccrsp ();
-      proto_register_docsis_dccack ();
-      proto_register_docsis_dsareq ();
-      proto_register_docsis_dsdrsp ();
-      proto_register_docsis_rngreq ();
-      proto_register_docsis_vsif ();
-      proto_register_docsis_intrngreq (); 
-
-    }
-}
-
-G_MODULE_EXPORT void
-plugin_reg_handoff (void)
-{
-  proto_reg_handoff_docsis ();
-  proto_reg_handoff_docsis_bpkmattr ();
-  proto_reg_handoff_docsis_dsarsp ();
-  proto_reg_handoff_docsis_mgmt ();
-  proto_reg_handoff_docsis_rngreq ();
-  proto_reg_handoff_docsis_rngrsp ();
-  proto_reg_handoff_docsis_bpkmreq ();
-  proto_reg_handoff_docsis_dscack ();
-  proto_reg_handoff_docsis_map ();
-  proto_reg_handoff_docsis_tlv ();
-  proto_reg_handoff_docsis_bpkmrsp ();
-  proto_reg_handoff_docsis_dscreq ();
-  proto_reg_handoff_docsis_regack ();
-  proto_reg_handoff_docsis_uccreq ();
-  proto_reg_handoff_docsis_dscrsp ();
-  proto_reg_handoff_docsis_regreq ();
-  proto_reg_handoff_docsis_uccrsp ();
-  proto_reg_handoff_docsis_dsaack ();
-  proto_reg_handoff_docsis_dsdreq ();
-  proto_reg_handoff_docsis_regrsp ();
-  proto_reg_handoff_docsis_ucd ();
-  proto_reg_handoff_docsis_type29ucd ();
-  proto_reg_handoff_docsis_dcd ();
-  proto_reg_handoff_docsis_dccreq ();
-  proto_reg_handoff_docsis_dccrsp ();
-  proto_reg_handoff_docsis_dccack ();
-  proto_reg_handoff_docsis_dsareq ();
-  proto_reg_handoff_docsis_dsdrsp ();
-  proto_reg_handoff_docsis_vsif ();
-  proto_reg_handoff_docsis_intrngreq ();
-
-}
-
-#endif
