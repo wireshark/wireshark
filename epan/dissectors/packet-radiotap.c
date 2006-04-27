4a84a894d90c52df8c79242bd3ac64386d058f89
Guy Harris <guy@alum.mit.edu>
2006-04-27 19:58:32 +0000

    This was completely broken, as the "length" variable actually reflected the amount of data left in the radiotap header after the fields we've processed so far, not the total length of the header, so it couldn't be used to skip past the radiotap header and get a tvbuff of the packet data.  Fix that.
    svn path=/trunk/; revision=18020

--- epan/dissectors/packet-radiotap.c
+++ epan/dissectors/packet-radiotap.c
@@ -382,7 +382,7 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     proto_item *ti = NULL;
     int offset;
     guint32 version, pad;
-    guint32 length;
+    guint length, length_remaining;
     guint32 rate, freq, flags;
     gint8 dbm;
     guint8 db, rflags;
@@ -416,11 +416,13 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	ti = proto_tree_add_uint(radiotap_tree, hf_radiotap_length,
 	    tvb, offset + 2, 2, length);
     }
+    length_remaining = length;
+
     /*
      * FIXME: This only works if there is exactly 1 it_present
      *        field in the header
      */
-    if (length < sizeof(struct ieee80211_radiotap_header)) {
+    if (length_remaining < sizeof(struct ieee80211_radiotap_header)) {
 	/*
 	 * Radiotap header is shorter than the fixed-length portion
 	 * plus one "present" bitset.
@@ -435,7 +437,7 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	present_tree = proto_item_add_subtree(pt, ett_radiotap_present);
     }
     offset += sizeof(struct ieee80211_radiotap_header);
-    length -= sizeof(struct ieee80211_radiotap_header);
+    length_remaining -= sizeof(struct ieee80211_radiotap_header);
 
     rflags = 0;
     for (; present; present = next_present) {
@@ -447,7 +449,7 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
 	switch (bit) {
 	case IEEE80211_RADIOTAP_FLAGS:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    if (tree) {
@@ -459,11 +461,11 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			tvb, 0, 0, (rflags&IEEE80211_RADIOTAP_F_DATAPAD) != 0);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    /* XXX CFP, WEP, FRAG */
 	    break;
 	case IEEE80211_RADIOTAP_RATE:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    rate = tvb_get_guint8(tvb, offset) & 0x7f;
@@ -477,10 +479,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			"Data Rate: %d.%d Mb/s", rate / 2, rate & 1 ? 5 : 0);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_DBM_ANTSIGNAL:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    dbm = (gint8) tvb_get_guint8(tvb, offset);
@@ -494,10 +496,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 					  "SSI Signal: %d dBm", dbm);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_DB_ANTSIGNAL:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    db = tvb_get_guint8(tvb, offset);
@@ -511,10 +513,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 					   "SSI Signal: %u dB", db);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_DBM_ANTNOISE:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    dbm = (gint8) tvb_get_guint8(tvb, offset);
@@ -525,10 +527,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 					  "SSI Noise: %d dBm", dbm);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_DB_ANTNOISE:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    db = tvb_get_guint8(tvb, offset);
@@ -539,10 +541,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 					   "SSI Noise: %u dB", db);
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_ANTENNA:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    if (tree) {
@@ -550,10 +552,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				   tvb, offset, 1, tvb_get_guint8(tvb, offset));
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_DBM_TX_POWER:
-	    if (length < 1)
+	    if (length_remaining < 1)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    if (tree) {
@@ -561,10 +563,10 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				   tvb, offset, 1, tvb_get_guint8(tvb, offset));
 	    }
 	    offset++;
-	    length--;
+	    length_remaining--;
 	    break;
 	case IEEE80211_RADIOTAP_CHANNEL:
-	    if (length < 4)
+	    if (length_remaining < 4)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    if (tree) {
@@ -577,23 +579,23 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			tvb, offset+2, 2, flags);
 	    }
 	    offset+=4;
-	    length-=4;
+	    length_remaining-=4;
 	    break;
 	case IEEE80211_RADIOTAP_FHSS:
 	case IEEE80211_RADIOTAP_LOCK_QUALITY:
 	case IEEE80211_RADIOTAP_TX_ATTENUATION:
 	case IEEE80211_RADIOTAP_DB_TX_ATTENUATION:
-	    if (length < 2)
+	    if (length_remaining < 2)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 #if 0
 	    tvb_get_letohs(tvb, offset);
 #endif
 	    offset+=2;
-	    length-=2;
+	    length_remaining-=2;
 	    break;
 	case IEEE80211_RADIOTAP_TSFT:
-	    if (length < 8)
+	    if (length_remaining < 8)
 		break;
 	    rflags = tvb_get_guint8(tvb, offset);
 	    if (tree) {
@@ -601,7 +603,7 @@ dissect_radiotap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				tvb, offset, 8, tvb_get_letoh64(tvb, offset));
 	    }
 	    offset+=8;
-	    length-=8;
+	    length_remaining-=8;
 	    break;
 	default:
 	    /*
