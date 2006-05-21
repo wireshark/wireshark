a79553b3af5327883f616a166db8fc2c4666bf70
Guy Harris <guy@alum.mit.edu>
2006-05-21 21:52:33 +0000

    Fix the only gerald@ethereal.com references left.
    svn path=/trunk/; revision=18203

--- capture_loop.c
+++ capture_loop.c
@@ -4,7 +4,7 @@
  * $Id$
  *
  * Ethereal - Network traffic analyzer
- * By Gerald Combs <gerald@ethereal.com>
+ * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
  * This program is free software; you can redistribute it and/or
