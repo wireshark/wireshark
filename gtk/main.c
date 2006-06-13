c922f5e48e8caa10955de348af582bd340ba1709
Guy Harris <guy@alum.mit.edu>
2006-06-13 05:36:27 +0000

    Fix the signatures of find_next_mark_cb() and find_next_mark_cb() to match their declarations, and mark the "action" argument as unused.
    svn path=/trunk/; revision=18446

--- gtk/main.c
+++ gtk/main.c
@@ -650,13 +650,13 @@ reftime_frame_cb(GtkWidget *w _U_, gpointer data _U_, REFTIME_ACTION_E action)
 }
 
 void
-find_next_mark_cb(GtkWidget *w _U_, gpointer data _U_, REFTIME_ACTION_E action)
+find_next_mark_cb(GtkWidget *w _U_, gpointer data _U_, int action _U_)
 {
     find_previous_next_frame_with_filter("frame.marked == TRUE", FALSE);
 }
 
 void
-find_prev_mark_cb(GtkWidget *w _U_, gpointer data _U_, REFTIME_ACTION_E action)
+find_prev_mark_cb(GtkWidget *w _U_, gpointer data _U_, int action _U_)
 {
     find_previous_next_frame_with_filter("frame.marked == TRUE", TRUE);
 }
