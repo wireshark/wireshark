07af276bf42e0b596f4a7955c91b77b6d767c837
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2006-07-22 22:15:15 +0000

    licence info
    svn path=/trunk/; revision=18787

--- epan/dissectors/packet-nfs.h
+++ epan/dissectors/packet-nfs.h
@@ -5,8 +5,6 @@
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
- * Copied from packet-smb.c
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
