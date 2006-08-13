3342dab89581074922d89f8ab997d6ef033051a9
Ulf Lamping <ulf.lamping@web.de>
2006-08-13 00:13:22 +0000

    "bug" #1013: from Stephen Fisher add the X --display command line parameter to the documentation
    svn path=/trunk/; revision=18889

--- gtk/main.c
+++ gtk/main.c
@@ -1234,6 +1234,9 @@ print_usage(gboolean print_ver) {
   fprintf(output, "  -h                       display this help and exit\n");
   fprintf(output, "  -v                       display version info and exit\n");
   fprintf(output, "  -o <name>:<value> ...    override preference or recent setting\n");
+#ifndef _WIN32
+  fprintf(output, "  --display=DISPLAY        X display to use\n");
+#endif
 
 #ifdef _WIN32
   destroy_console();
