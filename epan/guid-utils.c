5000c855135299e149eaac58cb8940b65a380110
Guy Harris <guy@alum.mit.edu>
2006-08-19 03:00:16 +0000

    Squelch a compiler warning.
    svn path=/trunk/; revision=18956

--- epan/guid-utils.c
+++ epan/guid-utils.c
@@ -190,6 +190,7 @@ const gchar *
 guids_resolve_guid_to_str(e_guid_t *guid)
 {
 	const gchar *name;
+	gchar *namebuf;
 
 	name=guids_get_guid_name(guid);
 	if(name){
@@ -197,14 +198,14 @@ guids_resolve_guid_to_str(e_guid_t *guid)
 	}
 
 
-	name=ep_alloc(64);
-	g_snprintf(name, 64, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+	namebuf=ep_alloc(64);
+	g_snprintf(namebuf, 64, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
                       guid->data1, guid->data2, guid->data3,
                       guid->data4[0], guid->data4[1],
                       guid->data4[2], guid->data4[3],
                       guid->data4[4], guid->data4[5],
                       guid->data4[6], guid->data4[7]);
-	return name;
+	return namebuf;
 }
 
 
