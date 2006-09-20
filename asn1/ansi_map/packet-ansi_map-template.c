bf6353c7670f2ff814dad20280e0db4f8bf053de
Anders Broman <anders.broman@ericsson.com>
2006-09-20 06:14:45 +0000

    Dont use itu_ssn
    svn path=/trunk/; revision=19263

--- asn1/ansi_map/packet-ansi_map-template.c
+++ asn1/ansi_map/packet-ansi_map-template.c
@@ -3566,7 +3566,7 @@ static void range_delete_callback(guint32 ssn)
  {
      if (ssn) {
  	delete_ansi_tcap_subdissector(ssn , ansi_map_handle);
- 	add_itu_tcap_subdissector(ssn , ansi_map_handle);
+ 	add_ansi_tcap_subdissector(ssn , ansi_map_handle);
      }
  }
 
