8439211e71f467c0f759b83ec18a2ec7d748940d
Ulf Lamping <ulf.lamping@web.de>
2006-09-24 20:37:30 +0000

    instead of simply removing G_BEGIN_DECLS (maybe someone wants to use it in a c++ project), use an #if GTK_MAJOR_VERSION >= 2 around all definitions. This will also make it clear that this widget won't work with gtk 1.x
    svn path=/trunk/; revision=19317

--- gtk/gtkvumeter.h
+++ gtk/gtkvumeter.h
@@ -18,6 +18,9 @@
 
 #include <gtk/gtk.h>
 
+#if GTK_MAJOR_VERSION >= 2
+
+G_BEGIN_DECLS
 
 #define GTK_TYPE_VUMETER                (gtk_vumeter_get_type ())
 #define GTK_VUMETER(obj)                (GTK_CHECK_CAST ((obj), GTK_TYPE_VUMETER, GtkVUMeter))
@@ -142,5 +145,8 @@ gint gtk_vumeter_get_yellow_level (GtkVUMeter *vumeter);
 void gtk_vumeter_set_brightness (GtkVUMeter *vumeter, gint foreground, gint background);
 void gtk_vumeter_get_brightness (GtkVUMeter *vumeter, gint *foreground, gint *background);
 
+G_END_DECLS
+
+#endif /* GTK_MAJOR_VERSION >= 2 */
 
 #endif /* __GTKVUMETER_H__ */
