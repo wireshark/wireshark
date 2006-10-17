c57d78db9ce708adf2cc36f78c00d5483ae8f461
Jaap Keuter <jaap.keuter@xs4all.nl>
2006-10-17 20:53:57 +0000

    From Stephen Fisher: While researching another bug in follow_dlg.c, I spotted a different one that causes the from and to hostnames/ip addresses to be the same in the follow tcp stream dialog only with IPv6.
    svn path=/trunk/; revision=19583

--- gtk/follow_dlg.c
+++ gtk/follow_dlg.c
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -332,7 +332,7 @@ follow_stream_cb(GtkWidget * w, gpointer data _U_)
 	  struct e_in6_addr ipaddr;
 	  memcpy(&ipaddr, stats.ip_address[0], 16);
 	  hostname0 = get_hostname6(&ipaddr);
-	  memcpy(&ipaddr, stats.ip_address[0], 16);
+	  memcpy(&ipaddr, stats.ip_address[1], 16);
 	  hostname1 = get_hostname6(&ipaddr);
 	} else {
 	  guint32 ipaddr;
