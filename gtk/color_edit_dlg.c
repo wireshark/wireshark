ed69c761c829e25f0fd79d00d6d3879619e30d01
Jaap Keuter <jaap.keuter@xs4all.nl>
2006-10-19 21:39:18 +0000

    From Stephen Fisher: Attached is a patch to fix bug #1165.  To summarize: "if you click on the button to change the background color and hit ok, it changes not only the name field but also the string field's color away from the red/green [syntax check]."
    svn path=/trunk/; revision=19619

--- gtk/color_edit_dlg.c
+++ gtk/color_edit_dlg.c
@@ -19,7 +19,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #ifdef HAVE_CONFIG_H
@@ -598,7 +598,6 @@ color_sel_ok_cb                        (GtkButton       *button _U_,
 	  style->text[GTK_STATE_NORMAL] = new_color;
 #endif
 	gtk_widget_set_style(filt_name_entry, style);
-	gtk_widget_set_style(filt_text_entry, style);
 	gtk_style_unref(style);
   }
 }
