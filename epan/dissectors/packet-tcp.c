8487e57880986b8270728ef57c4cbdcc70b127ed
Guy Harris <guy@alum.mit.edu>
2006-10-29 00:06:02 +0000

    The TCP options data is a byte array, not a character string.
    svn path=/trunk/; revision=19723

--- epan/dissectors/packet-tcp.c
+++ epan/dissectors/packet-tcp.c
@@ -2443,7 +2443,7 @@ dissect_tcp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     optlen = tcph->th_hlen - TCPH_MIN_LEN; /* length of options, in bytes */
     tvb_ensure_bytes_exist(tvb, offset +  20, optlen);
     if (tcp_tree != NULL) {
-      guint8 *p_options = tvb_get_ephemeral_string(tvb, offset + 20, optlen);
+      guint8 *p_options = ep_tvb_memdup(tvb, offset + 20, optlen);
       tf = proto_tree_add_bytes_format(tcp_tree, hf_tcp_options, tvb, offset +  20, 
         optlen, p_options, "Options: (%u bytes)", optlen);
       field_tree = proto_item_add_subtree(tf, ett_tcp_options);
