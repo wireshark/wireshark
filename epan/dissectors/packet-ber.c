fda137ce6ae65503569e91f96dc795197693a3f2
Anders Broman <anders.broman@ericsson.com>
2007-01-06 20:21:30 +0000

    Fix tag > 0x1f
    svn path=/trunk/; revision=20333

--- epan/dissectors/packet-ber.c
+++ epan/dissectors/packet-ber.c
@@ -493,8 +493,7 @@ int dissect_ber_identifier(packet_info *pinfo _U_, proto_tree *tree, tvbuff_t *t
 	if(show_internal_ber_fields){
 		proto_tree_add_uint(tree, hf_ber_id_class, tvb, old_offset, 1, tmp_class<<6);
 		proto_tree_add_boolean(tree, hf_ber_id_pc, tvb, old_offset, 1, (tmp_pc)?0x20:0x00);
-		if(tmp_tag==0x1F){
-			proto_tree_add_uint(tree, hf_ber_id_uni_tag, tvb, old_offset, 1, tmp_tag);
+		if(tmp_tag>0x1F){
 			if(tmp_class==BER_CLASS_UNI){
 				proto_tree_add_uint(tree, hf_ber_id_uni_tag_ext, tvb, old_offset + 1, offset - (old_offset + 1), tmp_tag);
 			} else {
