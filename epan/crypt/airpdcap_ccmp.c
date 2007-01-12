ba4a3277045ee81999188cd18a72fe25e361ea35
Gerald Combs <gerald@wireshark.org>
2007-01-12 00:54:13 +0000

    Add copyright attributions and licenses.  License each derived work according to its pedigree.
    svn path=/trunk/; revision=20401

--- epan/crypt/airpdcap_ccmp.c
+++ epan/crypt/airpdcap_ccmp.c
@@ -1,3 +1,43 @@
+/* airpdcap_ccmp.c
+ *
+ * $Id$
+ *
+ * Copyright (c) 2002-2005 Sam Leffler, Errno Consulting
+ * Copyright (c) 2006 CACE Technologies, Davis (California)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Note: This file was derived from the FreeBSD source code, RELENG 6,
+ *		sys/net80211/ieee80211_crypto_ccmp.c
+ */
+
 /****************************************************************************/
 /* File includes								*/
 
@@ -7,11 +47,6 @@
 #include "airpdcap_rijndael.h"
 
 #include "airpdcap_debug.h"
-
-/****************************************************************************/
-
-/* Note: this code were copied from FreeBSD source code, RELENG 6,		*/
-/*		sys/net80211/ieee80211_crypto_ccmp.c				*/
 
 /****************************************************************************/
 /*	Internal definitions							*/
