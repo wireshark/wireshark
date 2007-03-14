78b8245bcc7a2db26ba757629665f42bc36052bb
Guy Harris <guy@alum.mit.edu>
2007-03-14 18:23:19 +0000

    "dissect_tapa_static()" is a new-style dissector; register it as such.
    svn path=/trunk/; revision=21029

--- epan/dissectors/packet-tapa.c
+++ epan/dissectors/packet-tapa.c
@@ -603,7 +603,7 @@ proto_register_tapa(void)
         proto_register_field_array(proto_tapa, hf, array_length(hf));
 	proto_register_subtree_array(ett, array_length(ett));
 
-	register_dissector("tapa", dissect_tapa_static, proto_tapa);
+	new_register_dissector("tapa", dissect_tapa_static, proto_tapa);
 
 }
 
