6632dd3a3b65a71e19db94b2631035394319235f
Tomas Kukosa <tomas.kukosa@siemens.com>
2007-05-04 08:24:06 +0000

    missing asn1.c file
    svn path=/trunk/; revision=21678

---
+++ epan/asn1.c
@@ -0,0 +1,47 @@
+/* asn1.c
+ * Common routines for ASN.1
+ * 2007  Anders Broman
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib.h>
+#include <epan/packet.h>
+
+#include "asn1.h"
+
+void asn1_ctx_init(asn1_ctx_t *actx, asn1_enc_e encoding, gboolean aligned, packet_info *pinfo) {
+  actx->encoding = encoding;
+  actx->aligned = aligned;
+  actx->pinfo = pinfo;
+  actx->created_item = NULL;
+  actx->value_ptr = NULL;
+  actx->private_data = NULL;
+}
+
+void asn1_ctx_clean_external(asn1_ctx_t *actx) {
+  memset(&actx->external, '\0', sizeof(actx->external));
+  actx->external.hf_index = -1;
+}
