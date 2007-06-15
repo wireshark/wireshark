8e6ac5494a4fcc96be017c9705f963d1a6d708c5
Anders Broman <anders.broman@ericsson.com>
2007-06-15 17:58:23 +0000

    Change Alcatel Specific extension container PlmnContainer asn1 code to mach tracing info.
    svn path=/trunk/; revision=22108

--- epan/dissectors/packet-gsm_map.c
+++ epan/dissectors/packet-gsm_map.c
@@ -14267,7 +14267,7 @@ dissect_gsm_old_PlmnContainer_U(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, in
 static int
 dissect_gsm_old_PlmnContainer(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
   offset = dissect_ber_tagged_type(implicit_tag, actx, tree, tvb, offset,
-                                      hf_index, BER_CLASS_CON, 0, TRUE, dissect_gsm_old_PlmnContainer_U);
+                                      hf_index, BER_CLASS_PRI, 2, TRUE, dissect_gsm_old_PlmnContainer_U);
 
   return offset;
 }
