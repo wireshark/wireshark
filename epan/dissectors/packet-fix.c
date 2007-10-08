639b0468975221a588e087354270e27908b65623
Anders Broman <anders.broman@ericsson.com>
2007-10-08 15:58:22 +0000

    Register to TCP port 0 to be able to do "decode as".
    svn path=/trunk/; revision=23094

--- epan/dissectors/packet-fix.c
+++ epan/dissectors/packet-fix.c
@@ -6439,10 +6439,14 @@ proto_register_fix(void)
 void
 proto_reg_handoff_fix(void)
 {
+	static dissector_handle_t fix_handle;
     /*
      * The first time the function is called let the tcp dissector
      * know that we're interested in traffic
      */
     heur_dissector_add("tcp", dissect_fix, proto_fix);
+	/* Register fix to TCP port 0 to be able to do "dissect as"*/
+	fix_handle = new_create_dissector_handle(dissect_fix, proto_fix);
+	dissector_add("tcp.port", 0, fix_handle);
 }
 
