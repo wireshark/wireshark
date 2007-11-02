686ea4b77376b71de2db70bdf01b676aaa6d39b9
Tomas Kukosa <tomas.kukosa@siemens.com>
2007-11-02 12:18:01 +0000

    new H.282 (RDC) and H.283 (LCT) dissectors
    svn path=/trunk/; revision=23339

---
+++ asn1/h282/packet-h282-template.c
@@ -0,0 +1,106 @@
+/* packet-h282.c
+ * Routines for H.282 packet dissection
+ * 2007  Tomas Kukosa
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib.h>
+#include <epan/packet.h>
+#include <epan/prefs.h>
+#include <epan/conversation.h>
+#include <epan/oids.h>
+#include <epan/asn1.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "packet-per.h"
+
+#define PNAME  "H.282 Remote Device Control"
+#define PSNAME "RDC"
+#define PFNAME "rdc"
+
+/* Initialize the protocol and registered fields */
+int proto_h282 = -1;
+#include "packet-h282-hf.c"
+
+/* Initialize the subtree pointers */
+static int ett_h282 = -1;
+#include "packet-h282-ett.c"
+
+/* Dissectors */
+
+/* Subdissectors */
+
+#include "packet-h282-fn.c"
+
+static int
+dissect_h282(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  proto_item  *ti = NULL;
+  proto_tree  *h282_tree = NULL;
+
+  if (check_col(pinfo->cinfo, COL_PROTOCOL))
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, PSNAME);
+
+  ti = proto_tree_add_item(tree, proto_h282, tvb, 0, -1, FALSE);
+  h282_tree = proto_item_add_subtree(ti, ett_h282);
+
+  return dissect_RDCPDU_PDU(tvb, pinfo, h282_tree);
+}
+
+/*--- proto_register_h282 ----------------------------------------------*/
+void proto_register_h282(void) {
+
+  /* List of fields */
+  static hf_register_info hf[] = {
+#include "packet-h282-hfarr.c"
+  };
+
+  /* List of subtrees */
+  static gint *ett[] = {
+    &ett_h282,
+#include "packet-h282-ettarr.c"
+  };
+
+  /* Register protocol */
+  proto_h282 = proto_register_protocol(PNAME, PSNAME, PFNAME);
+
+  /* Register fields and subtrees */
+  proto_register_field_array(proto_h282, hf, array_length(hf));
+  proto_register_subtree_array(ett, array_length(ett));
+
+  register_dissector(PFNAME, dissect_h282, proto_h282);
+  register_dissector(PFNAME".device_list", dissect_NonCollapsingCapabilities_PDU, proto_h282);
+
+}
+
+/*--- proto_reg_handoff_h282 -------------------------------------------*/
+void proto_reg_handoff_h282(void) 
+{
+
+}
+
---
+++ asn1/h283/packet-h283-template.c
@@ -0,0 +1,119 @@
+/* packet-h283.c
+ * Routines for H.283 packet dissection
+ * 2007  Tomas Kukosa
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib.h>
+#include <epan/packet.h>
+#include <epan/prefs.h>
+#include <epan/conversation.h>
+#include <epan/oids.h>
+#include <epan/asn1.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "packet-per.h"
+
+#define PNAME  "H.283 Logical Channel Transport"
+#define PSNAME "LCT"
+#define PFNAME "lct"
+
+/* Initialize the protocol and registered fields */
+int proto_h283 = -1;
+#include "packet-h283-hf.c"
+
+/* Initialize the subtree pointers */
+static int ett_h283 = -1;
+#include "packet-h283-ett.c"
+
+/* Dissectors */
+static dissector_handle_t h283_udp_handle = NULL; 
+
+/* Subdissectors */
+static dissector_handle_t rdc_pdu_handle = NULL; 
+static dissector_handle_t rdc_device_list_handle = NULL; 
+static dissector_handle_t data_handle = NULL; 
+
+static gboolean info_is_set;
+
+#include "packet-h283-fn.c"
+
+static int
+dissect_h283_udp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  proto_item  *ti = NULL;
+  proto_tree  *h283_tree = NULL;
+
+  if (check_col(pinfo->cinfo, COL_PROTOCOL))
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, PSNAME);
+
+  info_is_set = FALSE;
+
+  ti = proto_tree_add_item(tree, proto_h283, tvb, 0, -1, FALSE);
+  h283_tree = proto_item_add_subtree(ti, ett_h283);
+
+  return dissect_LCTPDU_PDU(tvb, pinfo, h283_tree);
+}
+
+/*--- proto_register_h283 ----------------------------------------------*/
+void proto_register_h283(void) {
+
+  /* List of fields */
+  static hf_register_info hf[] = {
+#include "packet-h283-hfarr.c"
+  };
+
+  /* List of subtrees */
+  static gint *ett[] = {
+    &ett_h283,
+#include "packet-h283-ettarr.c"
+  };
+
+  /* Register protocol */
+  proto_h283 = proto_register_protocol(PNAME, PSNAME, PFNAME);
+
+  /* Register fields and subtrees */
+  proto_register_field_array(proto_h283, hf, array_length(hf));
+  proto_register_subtree_array(ett, array_length(ett));
+
+  register_dissector(PFNAME, dissect_h283_udp, proto_h283);
+  h283_udp_handle = find_dissector(PFNAME);
+
+}
+
+/*--- proto_reg_handoff_h283 -------------------------------------------*/
+void proto_reg_handoff_h283(void) 
+{
+
+  dissector_add_handle("udp.port", h283_udp_handle); 
+
+  rdc_pdu_handle = find_dissector("rdc");
+  rdc_device_list_handle = find_dissector("rdc.device_list");
+  data_handle = find_dissector("data");
+}
+
