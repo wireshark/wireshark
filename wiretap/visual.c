d283cb18abe7ed359d0166ad83338236fdddb165
Guy Harris <guy@alum.mit.edu>
2007-11-25 02:06:41 +0000

    Clean up a comment.
    svn path=/trunk/; revision=23579

--- wiretap/visual.c
+++ wiretap/visual.c
@@ -450,8 +450,8 @@ static gboolean visual_read(wtap *wth, int *err, gchar **err_info,
         }
         else
         {
-            /* Otherwise, we need to evaluate the first two
-               examine first two octets to verify encapsulation */
+            /* Otherwise, we need to examine the first two octets to
+               try to determine the encapsulation. */
             guint8 *buf = buffer_start_ptr(wth->frame_buffer);
             if ((0xff == buf[0]) && (0x03 == buf[1]))
             {
