d00e6fe59e3efb760453935657c547f804780dfc
Michael Tüxen <tuexen@fh-muenster.de>
2007-11-27 17:46:55 +0000

    Fix from Stephen Fisher for bug 2028.
    svn path=/trunk/; revision=23621

--- epan/dissectors/packet-sctp.c
+++ epan/dissectors/packet-sctp.c
@@ -953,11 +953,11 @@ sctp_ack_block(packet_info *pinfo, sctp_half_assoc_t *h, tvbuff_t *tvb,
 		h->peer->cumm_ack = rel_end + 1;
 
 	if (rel_start <= rel_end && rel_end - rel_start < 5000 ) {
-		guint32 rel_tsn;
-		for (rel_tsn = rel_start ; rel_tsn <= rel_end ; rel_tsn++) {
+		guint32 rel_tsn, i;
+		for (i=0; i <= rel_end-rel_start; i++) {
+			rel_tsn = (guint32) (i+rel_start);
 			sctp_ack(pinfo, tvb,  acks_tree, h, rel_tsn);
 		}
-
 	}
 }
 
