2a7d7da15770f9c51489af0e86c0c16e44c8726f
Anders Broman <anders.broman@ericsson.com>
2007-12-16 14:26:18 +0000

    Initial work on a E-UTRAN X2 Application Protocol (X2AP) packet dissector.
    svn path=/trunk/; revision=23886

---
+++ asn1/x2ap/packet-x2ap-template.c
@@ -0,0 +1,183 @@
+/* packet-x2ap.c
+ * Routines for dissecting Evolved Universal Terrestrial Radio Access Network (EUTRAN);
+ * X2 Application Protocol (X2AP);
+ * 3GPP TS 36.423 packet dissection
+ * Copyright 2007, Anders Broman <anders.broman@ericsson.com>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Ref: 
+ * 3GPP TS 36.423 V8.0.0 (2007-12)
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <glib.h>
+#include <epan/packet.h>
+#include <epan/conversation.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include <epan/asn1.h>
+
+#include "packet-per.h"
+
+#ifdef _MSC_VER
+/* disable: "warning C4146: unary minus operator applied to unsigned type, result still unsigned" */
+#pragma warning(disable:4146)
+#endif
+
+#define PNAME  "EUTRAN X2 Application Protocol (X2AP)"
+#define PSNAME "X2AP"
+#define PFNAME "x2ap"
+
+#define SCCP_SSN_X2AP 143
+
+#include "packet-x2ap-val.h"
+
+static dissector_handle_t x2ap_handle = NULL;
+
+/* Initialize the protocol and registered fields */
+static int proto_x2ap = -1;
+
+#include "packet-x2ap-hf.c"
+
+/* Initialize the subtree pointers */
+static int ett_x2ap = -1;
+
+#include "packet-x2ap-ett.c"
+
+/* Global variables */
+static guint32 ProcedureCode;
+static guint32 ProtocolIE_ID;
+static guint32 ddMode;
+static const gchar *ProcedureID;
+
+/* Dissector tables */
+static dissector_table_t x2ap_ies_dissector_table;
+static dissector_table_t x2ap_extension_dissector_table;
+static dissector_table_t x2ap_proc_imsg_dissector_table;
+static dissector_table_t x2ap_proc_sout_dissector_table;
+static dissector_table_t x2ap_proc_uout_dissector_table;
+
+static int dissect_ProtocolIEFieldValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static int dissect_ProtocolExtensionFieldExtensionValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static int dissect_InitiatingMessageValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static int dissect_SuccessfulOutcomeValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+static int dissect_UnsuccessfulOutcomeValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+
+#include "packet-x2ap-fn.c"
+
+static int dissect_ProtocolIEFieldValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  return (dissector_try_port(x2ap_ies_dissector_table, ProtocolIE_ID, tvb, pinfo, tree)) ? tvb_length(tvb) : 0;
+}
+
+static int dissect_ProtocolExtensionFieldExtensionValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  return (dissector_try_port(x2ap_extension_dissector_table, ProtocolIE_ID, tvb, pinfo, tree)) ? tvb_length(tvb) : 0;
+}
+
+static int dissect_InitiatingMessageValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  if (!ProcedureID) return 0;
+  return (dissector_try_string(x2ap_proc_imsg_dissector_table, ProcedureID, tvb, pinfo, tree)) ? tvb_length(tvb) : 0;
+}
+
+static int dissect_SuccessfulOutcomeValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  if (!ProcedureID) return 0;
+  return (dissector_try_string(x2ap_proc_sout_dissector_table, ProcedureID, tvb, pinfo, tree)) ? tvb_length(tvb) : 0;
+}
+
+static int dissect_UnsuccessfulOutcomeValue(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+  if (!ProcedureID) return 0;
+  return (dissector_try_string(x2ap_proc_uout_dissector_table, ProcedureID, tvb, pinfo, tree)) ? tvb_length(tvb) : 0;
+}
+
+static void
+dissect_x2ap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+	proto_item	*x2ap_item = NULL;
+	proto_tree	*x2ap_tree = NULL;
+
+	/* make entry in the Protocol column on summary display */
+	if (check_col(pinfo->cinfo, COL_PROTOCOL))
+		col_set_str(pinfo->cinfo, COL_PROTOCOL, "X2AP");
+
+	/* create the x2ap protocol tree */
+	x2ap_item = proto_tree_add_item(tree, proto_x2ap, tvb, 0, -1, FALSE);
+	x2ap_tree = proto_item_add_subtree(x2ap_item, ett_x2ap);
+	
+	dissect_X2AP_PDU_PDU(tvb, pinfo, x2ap_tree);
+}
+
+/*--- proto_register_x2ap -------------------------------------------*/
+void proto_register_x2ap(void) {
+
+  /* List of fields */
+
+  static hf_register_info hf[] = {
+#include "packet-x2ap-hfarr.c"
+  };
+
+  /* List of subtrees */
+  static gint *ett[] = {
+		  &ett_x2ap,
+#include "packet-x2ap-ettarr.c"
+  };
+
+
+  /* Register protocol */
+  proto_x2ap = proto_register_protocol(PNAME, PSNAME, PFNAME);
+  /* Register fields and subtrees */
+  proto_register_field_array(proto_x2ap, hf, array_length(hf));
+  proto_register_subtree_array(ett, array_length(ett));
+ 
+  /* Register dissector */
+  register_dissector("x2ap", dissect_x2ap, proto_x2ap);
+  x2ap_handle = find_dissector("x2ap");
+
+  /* Register dissector tables */
+  x2ap_ies_dissector_table = register_dissector_table("x2ap.ies", "X2AP-PROTOCOL-IES", FT_UINT32, BASE_DEC);
+  x2ap_extension_dissector_table = register_dissector_table("x2ap.extension", "X2AP-PROTOCOL-EXTENSION", FT_UINT32, BASE_DEC);
+  x2ap_proc_imsg_dissector_table = register_dissector_table("x2ap.proc.imsg", "X2AP-ELEMENTARY-PROCEDURE InitiatingMessage", FT_STRING, BASE_NONE);
+  x2ap_proc_sout_dissector_table = register_dissector_table("x2ap.proc.sout", "X2AP-ELEMENTARY-PROCEDURE SuccessfulOutcome", FT_STRING, BASE_NONE);
+  x2ap_proc_uout_dissector_table = register_dissector_table("x2ap.proc.uout", "X2AP-ELEMENTARY-PROCEDURE UnsuccessfulOutcome", FT_STRING, BASE_NONE);
+
+}
+
+
+/*--- proto_reg_handoff_x2ap ---------------------------------------*/
+void
+proto_reg_handoff_x2ap(void)
+{
+
+	dissector_add("sctp.port", 0, x2ap_handle);
+
+#include "packet-x2ap-dis-tab.c"
+}
+
+
