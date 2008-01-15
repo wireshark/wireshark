5b8742297688e79294f7cc2ab557890129962e15
Stig Bjørlykke <stig@bjorlykke.org>
2008-01-15 14:34:03 +0000

    Bump date
    svn path=/trunk/; revision=24096

--- version_info.c
+++ version_info.c
@@ -526,7 +526,7 @@ const char *
 get_copyright_info(void)
 {
 	return
-"Copyright 1998-2007 Gerald Combs <gerald@wireshark.org> and contributors.\n"
+"Copyright 1998-2008 Gerald Combs <gerald@wireshark.org> and contributors.\n"
 "This is free software; see the source for copying conditions. There is NO\n"
 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
 }
