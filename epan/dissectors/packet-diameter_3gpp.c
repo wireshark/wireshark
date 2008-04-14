e3463e8d53ff33fc636ccc21b07cb607176a30db
Jaap Keuter <jaap.keuter@xs4all.nl>
2008-04-14 06:43:42 +0000

    From Jim Young: Within the packet-diameter_3gpp.c module, the call to proto_register_protocol() comes AFTER the proto_register_field_array() call. While the delayed call to proto_register_protocol() does not appear to impair the use of the display filters, it DOES effect the generation of the wireshark-filter man page.
    svn path=/trunk/; revision=25014

--- epan/dissectors/packet-diameter_3gpp.c
+++ epan/dissectors/packet-diameter_3gpp.c
@@ -165,8 +165,7 @@ proto_register_diameter_3gpp(void)
 	};
 
 	/* Required function calls to register the header fields and subtrees used */
+	proto_diameter_3gpp = proto_register_protocol("Diameter 3GPP","Diameter3GPP", "diameter3gpp");
 	proto_register_field_array(proto_diameter_3gpp, hf, array_length(hf));
 	proto_register_subtree_array(ett, array_length(ett));
-
-	proto_diameter_3gpp = proto_register_protocol("Diameter 3GPP","Diameter3GPP", "diameter3gpp");
 }
