bc12adaf1b2dab8ce0dd2004748bb8d4b86ec607
Anders Broman <anders.broman@ericsson.com>
2008-05-05 19:06:31 +0000

    Add Id tag.
    svn path=/trunk/; revision=25236

--- plugins/v5ua/packet-v5ua.c
+++ plugins/v5ua/packet-v5ua.c
@@ -1,6 +1,8 @@
 /* packet-v5ua.c
  * Routines for V5.2-User Adaptation Layer dissection
- * 
+ *
+ * $Id$
+ *
  * Extension of ISDN Q.921-User Adaptation Layer dissection
  * Copyright 2002, Michael Tuexen <Michael.Tuexen[AT]siemens.com>
  *
