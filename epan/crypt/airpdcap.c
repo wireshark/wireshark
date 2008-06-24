dc7ae729831a64e078cd50b5d44c671faf70176c
Guy Harris <guy@alum.mit.edu>
2008-06-24 18:10:57 +0000

    Fix another variable named "index".
    svn path=/trunk/; revision=25581

--- epan/crypt/airpdcap.c
+++ epan/crypt/airpdcap.c
@@ -1082,16 +1082,16 @@ AirPDcapGetSa(
     PAIRPDCAP_CONTEXT ctx,
     AIRPDCAP_SEC_ASSOCIATION_ID *id)
 {
-    INT index;
+    INT sa_index;
 
     if (ctx->sa_index!=-1) {
         /* at least one association was stored														*/
         /* search for the association from sa_index to 0 (most recent added)	*/
-        for (index=ctx->sa_index; index>=0; index--) {
-            if (ctx->sa[index].used) {
-                if (memcmp(id, &(ctx->sa[index].saId), sizeof(AIRPDCAP_SEC_ASSOCIATION_ID))==0) {
-                    ctx->index=index;
-                    return index;
+        for (sa_index=ctx->sa_index; sa_index>=0; sa_index--) {
+            if (ctx->sa[sa_index].used) {
+                if (memcmp(id, &(ctx->sa[sa_index].saId), sizeof(AIRPDCAP_SEC_ASSOCIATION_ID))==0) {
+                    ctx->index=sa_index;
+                    return sa_index;
                 }
             }
         }
