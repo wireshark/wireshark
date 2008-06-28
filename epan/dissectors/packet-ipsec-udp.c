8e03447dd42fba34f78e164c5a9fb2d2b1521114
Guy Harris <guy@alum.mit.edu>
2008-06-28 01:51:12 +0000

    Set the Info column.
    svn path=/trunk/; revision=25622

--- epan/dissectors/packet-ipsec-udp.c
+++ epan/dissectors/packet-ipsec-udp.c
@@ -61,12 +61,16 @@ dissect_udpencap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
   /* First byte of 0 iindicates NAT-keepalive */
   if (tvb_get_guint8(tvb, 0) == 0xff) {
+    if (check_col(pinfo->cinfo, COL_INFO))
+      col_set_str(pinfo->cinfo, COL_INFO, "NAT-keepalive");
     if (tree)
       proto_tree_add_text(udpencap_tree, tvb, 0, 1, "NAT-keepalive packet");
   } else {
     /* SPI of zero indicates IKE traffic, otherwise it's ESP */
     tvb_memcpy(tvb, (guint8 *)&spi, 0, sizeof(spi));
     if (spi == 0) {
+      if (check_col(pinfo->cinfo, COL_INFO))
+        col_set_str(pinfo->cinfo, COL_INFO, "ISAKMP");
       if (tree) {
 	proto_tree_add_text(udpencap_tree, tvb, 0, sizeof(spi),
 		"Non-ESP Marker");
@@ -75,6 +79,8 @@ dissect_udpencap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
       next_tvb = tvb_new_subset(tvb, sizeof(spi), -1, -1);
       call_dissector(isakmp_handle, next_tvb, pinfo, tree);
     } else {
+      if (check_col(pinfo->cinfo, COL_INFO))
+        col_set_str(pinfo->cinfo, COL_INFO, "ESP");
       if (tree)
 	proto_item_set_len(ti, 0);
       call_dissector(esp_handle, tvb, pinfo, tree);
