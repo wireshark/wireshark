960cd2feee820ca007b00089a849dcc84404d540
Anders Broman <anders.broman@ericsson.com>
2008-12-27 16:24:16 +0000

    Use GTK+ 2.14.6
    svn path=/trunk/; revision=27121

--- tools/win32-setup.sh
+++ tools/win32-setup.sh
@@ -7,7 +7,7 @@
 # in order to provide backward compatibility with older trees (e.g. a
 # previous release or an older SVN checkout).
 # Save previous tag.
-DOWNLOAD_TAG="2008-12-23"
+DOWNLOAD_TAG="2008-12-27"
 DOWNLOAD_PREFIX="http://anonsvn.wireshark.org/wireshark-win32-libs/tags/$DOWNLOAD_TAG/packages/"
 TAG_FILE="current_tag.txt"
 
