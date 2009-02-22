c65d15f1e0ade0c578a0429e8de02e6f9ed7253e
Stig Bjørlykke <stig@bjorlykke.org>
2009-02-22 21:44:54 +0000

    Fix a typo in a dummy function.
    svn path=/trunk/; revision=27517

--- asn1/rrc/packet-rrc-template.c
+++ asn1/rrc/packet-rrc-template.c
@@ -82,7 +82,7 @@ TODO: Remove the dummy function when these functions are taken into use
  */
 
 static void
-dissect_rrc_dymmy_remove_unused_code_warnings(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_){
+dissect_rrc_dummy_remove_unused_code_warnings(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_){
 
 	/* Dummy code to keep GCC happy */
 	switch(hf_index){
