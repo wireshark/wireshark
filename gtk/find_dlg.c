6944dced3dc86adaff69e8b94ca13143544678be
Anders Broman <anders.broman@ericsson.com>
2009-03-03 05:52:46 +0000

    Try to make the solaris buildbot happy.
    svn path=/trunk/; revision=27583

--- gtk/find_dlg.c
+++ gtk/find_dlg.c
@@ -119,6 +119,10 @@ find_frame_cb(GtkWidget *w _U_, gpointer d _U_)
                 *bbox, *ok_bt, *cancel_bt, *help_bt;
   GtkTooltips   *tooltips;
 
+#if GTK_CHECK_VERSION(2,6,0)
+#else
+  GList *glist = NULL;
+#endif
   /* No Apply button, but "OK" not only sets our text widget, it
      activates it (i.e., it causes us to do the search). */
   static construct_args_t args = {
@@ -278,6 +282,7 @@ find_frame_cb(GtkWidget *w _U_, gpointer d _U_)
   /* Character Type Selection Dropdown Box
      These only apply to the string find option */
   /* Create Combo Box */
+ #if GTK_CHECK_VERSION(2,6,0)
   combo_cb = gtk_combo_box_new_text();
 
   gtk_combo_box_append_text(GTK_COMBO_BOX(combo_cb), "ASCII Unicode & Non-Unicode");
@@ -286,6 +291,18 @@ find_frame_cb(GtkWidget *w _U_, gpointer d _U_)
 
   gtk_combo_box_set_active(GTK_COMBO_BOX(combo_cb),0);
   gtk_container_add(GTK_CONTAINER(string_opt_vb), combo_cb);
+#else
+  combo_cb = gtk_combo_new();
+  glist = g_list_append(glist, "ASCII Unicode & Non-Unicode");
+  glist = g_list_append(glist, "ASCII Non-Unicode");
+  glist = g_list_append(glist, "ASCII Unicode");
+
+  gtk_combo_set_popdown_strings(GTK_COMBO(combo_cb), glist);
+  g_list_free(glist);
+  /* You only get to choose from the options we offer */
+  gtk_entry_set_editable(GTK_ENTRY(GTK_COMBO(combo_cb)->entry), FALSE);
+
+#endif
   gtk_widget_show(combo_cb);
 
 
@@ -534,8 +551,11 @@ find_frame_ok_cb(GtkWidget *ok_bt _U_, gpointer parent_w)
   summary_data_rb = (GtkWidget *)g_object_get_data(G_OBJECT(parent_w), E_SOURCE_SUMMARY_KEY);
 
   filter_text = gtk_entry_get_text(GTK_ENTRY(filter_te));
+#if GTK_CHECK_VERSION(2,6,0)
   string_type = gtk_combo_box_get_active_text(GTK_COMBO_BOX(combo_cb));
-
+#else
+  string_type = gtk_entry_get_text(GTK_ENTRY(GTK_COMBO(combo_cb)->entry));
+#endif
   case_type = !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(case_cb));
   decode_data = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(decode_data_rb));
   summary_data = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(summary_data_rb));
