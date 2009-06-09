97f33bab62ebf5fe96de045fa2d49ca99809d749
Stig Bjørlykke <stig@bjorlykke.org>
2009-06-09 23:23:07 +0000

    Resize the list in the autocompletion popup when narrowing down the elements to remove a possible horizontal scrollbar when not needed.
    svn path=/trunk/; revision=28681

--- gtk/filter_autocomplete.c
+++ gtk/filter_autocomplete.c
@@ -312,6 +312,7 @@ autocompletion_list_lookup(GtkWidget *filter_te, GtkWidget *popup_win, GtkWidget
 
     g_free (first);
 
+    gtk_tree_view_columns_autosize(GTK_TREE_VIEW(list));
     gtk_widget_size_request(list, &requisition);
 
     gtk_widget_set_size_request(popup_win, popup_win->allocation.width, (requisition.height<200? requisition.height+8:200));
