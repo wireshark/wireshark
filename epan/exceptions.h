fce2aea78529734aab208e6ed6ea24e57b5262eb
Stig Bjørlykke <stig@bjorlykke.org>
2009-06-11 10:07:57 +0000

    Add standard file heading.
    svn path=/trunk/; revision=28692

--- epan/exceptions.h
+++ epan/exceptions.h
@@ -1,7 +1,29 @@
+/* exceptions.h
+ * Wireshark's exceptions.
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 #ifndef __EXCEPTIONS_H__
 #define __EXCEPTIONS_H__
-
-/* $Id $ */
 
 #ifndef XCEPT_H
 #include "except.h"
@@ -9,8 +31,6 @@
 
 /* Wireshark has only one exception group, to make these macros simple */
 #define XCEPT_GROUP_WIRESHARK 1
-
-/* Wireshark's exceptions */
 
 /**
     Index is out of range.
