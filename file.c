5afe4057173c0ca3d3bd6c0f3536e3190fc4a700
Stig Bjørlykke <stig@bjorlykke.org>
2009-07-06 13:53:01 +0000

    Removed an incorrect optimalization for invalidating packet in row 0.
    svn path=/trunk/; revision=28950

--- file.c
+++ file.c
@@ -1863,7 +1863,7 @@ rescan_packets(capture_file *cf, const char *action, const char *action_item,
     /* Either the frame that was selected passed the filter, or we've
        found the nearest displayed frame to that frame.  Select it, make
        it the focus row, and make it visible. */
-    if (redissect && selected_row == 0) {
+    if (selected_row == 0) {
       /* Set to invalid to force update of packet list and packet details */
       cf->current_row = -1;
     }
