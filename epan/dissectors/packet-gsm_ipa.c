ee5d2e87883f3f1bf4fdc070d9a1c395385a6bb3
Gerald Combs <gerald@wireshark.org>
2009-07-31 19:19:25 +0000

    From Holger Freyther via bug 3776:
    Part 1:
    Rename packet-gsm_abis_ip.c to packet-gsm_ipa.c.
    
    Part 2:
    Rename to the content to match the filename.
    
    Part 3:
    Add GSM A/SCCP support to the packet-gsm_ipa.c dissector
    
    Adjusted patch to take laforge's comment into account. Use guint16 for the
    length and use the ntohs routine to convert the length.
    
    svn path=/trunk/; revision=29254

--- epan/dissectors/packet-gsm_ipa.c
+++ epan/dissectors/packet-gsm_ipa.c
@@ -1,6 +1,7 @@
-/* packet-gsm_abis_ip.c
- * Routines for packet dissection of ip.access A-bis over IP
+/* packet-gsm_ipa.c
+ * Routines for packet dissection of ip.access GSM over IP
  * Copyright 2009 by Harald Welte <laforge@gnumonks.org>
+ * Copyright 2009 by Holger Hans Peter Freyther <zecke@selfish.org>
  *
  * $Id$
  *
@@ -32,23 +33,24 @@
 #include <epan/packet.h>
 
 /* Initialize the protocol and registered fields */
-static int proto_abisip = -1;
+static int proto_ipa = -1;
 static int proto_ipaccess = -1;
 
-static int hf_abisip_data_len = -1;
-static int hf_abisip_protocol = -1;
+static int hf_ipa_data_len = -1;
+static int hf_ipa_protocol = -1;
 
 static int hf_ipaccess_msgtype = -1;
 static int hf_ipaccess_attr_tag = -1;
 static int hf_ipaccess_attr_string = -1;
 
 /* Initialize the subtree pointers */
-static gint ett_abisip = -1;
+static gint ett_ipa = -1;
 static gint ett_ipaccess = -1;
 
 enum {
 	SUB_OML,
 	SUB_RSL,
+	SUB_SCCP,
 /*	SUB_IPACCESS, */
 
 	SUB_MAX
@@ -59,13 +61,16 @@ static dissector_handle_t sub_handles[SUB_MAX];
 #define TCP_PORT_ABISIP_PRIM	 3002
 #define TCP_PORT_ABISIP_SEC	 3003
 #define TCP_PORT_ABISIP_INST	 3006
+#define TCP_PORT_AIP_PRIM	 5000
 
 #define ABISIP_RSL	0x00
+#define AIP_SCCP	0xfd
 #define ABISIP_IPACCESS	0xfe
 #define ABISIP_OML	0xff
 
-static const value_string abisip_protocol_vals[] = {
+static const value_string ipa_protocol_vals[] = {
 	{ 0x00,		"RSL" },
+	{ 0xfd,		"SCCP" },
 	{ 0xfe,		"IPA" },
 	{ 0xff,		"OML" },
 	{ 0, 		NULL }
@@ -164,37 +169,37 @@ dissect_ipaccess(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
 /* Code to actually dissect the packets */
 static void
-dissect_abisip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+dissect_ipa(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 
 	int offset = 0;
 
-	col_set_str(pinfo->cinfo, COL_PROTOCOL, "Abis/IP");
+	col_set_str(pinfo->cinfo, COL_PROTOCOL, "IPA");
 	col_clear(pinfo->cinfo, COL_INFO);
 
 	while (tvb_reported_length_remaining(tvb, offset) > 0) {
 		proto_item *ti;
-		proto_tree *abisip_tree;
-		guint8 len, msg_type;
+		proto_tree *ipa_tree;
+		guint16 len, msg_type;
 		tvbuff_t *next_tvb;
 
-		len = tvb_get_guint8(tvb, offset+1);
+		len = tvb_get_ntohs(tvb, offset);
 		msg_type = tvb_get_guint8(tvb, offset+2);
 
 		col_append_fstr(pinfo->cinfo, COL_INFO, "%s ",
-		                val_to_str(msg_type, abisip_protocol_vals,
+		                val_to_str(msg_type, ipa_protocol_vals,
 		                           "unknown 0x%02x"));
 
 		if (tree) {
-			ti = proto_tree_add_protocol_format(tree, proto_abisip,
+			ti = proto_tree_add_protocol_format(tree, proto_ipa,
 					tvb, offset, len+3,
-					"A-bis/IP protocol ip.access, type: %s",
-					val_to_str(msg_type, abisip_protocol_vals,
+					"IPA protocol ip.access, type: %s",
+					val_to_str(msg_type, ipa_protocol_vals,
 						   "unknown 0x%02x"));
-			abisip_tree = proto_item_add_subtree(ti, ett_abisip);
-			proto_tree_add_item(abisip_tree, hf_abisip_data_len,
+			ipa_tree = proto_item_add_subtree(ti, ett_ipa);
+			proto_tree_add_item(ipa_tree, hf_ipa_data_len,
 					    tvb, offset+1, 1, FALSE);
-			proto_tree_add_item(abisip_tree, hf_abisip_protocol,
+			proto_tree_add_item(ipa_tree, hf_ipa_protocol,
 					    tvb, offset+2, 1, FALSE);
 		}
 
@@ -214,23 +219,27 @@ dissect_abisip(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 		case ABISIP_IPACCESS:
 			dissect_ipaccess(next_tvb, pinfo, tree);
 			break;
+		case AIP_SCCP:
+			/* hand this off to the standard SCCP dissector */
+			call_dissector(sub_handles[SUB_SCCP], next_tvb, pinfo, tree);
+			break;
 		}
 		offset += len + 3;
 	}
 }
 
-void proto_register_abis_ip(void)
+void proto_register_ipa(void)
 {
 	static hf_register_info hf[] = {
-		{&hf_abisip_data_len,
-		 {"DataLen", "abisip.data_len",
+		{&hf_ipa_data_len,
+		 {"DataLen", "ipa.data_len",
 		  FT_UINT8, BASE_DEC, NULL, 0x0,
 		  "The length of the data (in bytes)", HFILL}
 		 },
-		{&hf_abisip_protocol,
-		 {"Protocol", "abisip.protocol",
-		  FT_UINT8, BASE_HEX, VALS(abisip_protocol_vals), 0x0,
-		  "The A-bis/IP Sub-Protocol", HFILL}
+		{&hf_ipa_protocol,
+		 {"Protocol", "ipa.protocol",
+		  FT_UINT8, BASE_HEX, VALS(ipa_protocol_vals), 0x0,
+		  "The IPA Sub-Protocol", HFILL}
 		 },
 	};
 	static hf_register_info hf_ipa[] = {
@@ -252,34 +261,36 @@ void proto_register_abis_ip(void)
 	};
 
 	static gint *ett[] = {
-		&ett_abisip,
+		&ett_ipa,
 		&ett_ipaccess,
 	};
 
-	proto_abisip =
-	    proto_register_protocol("GSM A-bis/IP protocol as used by ip.access",
-				    "GSM A-bis/IP", "gsm_abis_ip");
+	proto_ipa =
+	    proto_register_protocol("GSM over IP protocol as used by ip.access",
+				    "GSM over IP", "gsm_ipa");
 	proto_ipaccess =
-	    proto_register_protocol("GSM A-bis/IP ip.access CCM sub-protocol",
+	    proto_register_protocol("GSM over IP ip.access CCM sub-protocol",
 				    "IPA", "ipaccess");
 
-	proto_register_field_array(proto_abisip, hf, array_length(hf));
+	proto_register_field_array(proto_ipa, hf, array_length(hf));
 	proto_register_field_array(proto_ipaccess, hf_ipa, array_length(hf_ipa));
 	proto_register_subtree_array(ett, array_length(ett));
 
-	register_dissector("gsm_abis_ip", dissect_abisip, proto_abisip);
+	register_dissector("gsm_ipa", dissect_ipa, proto_ipa);
 }
 
-void proto_reg_handoff_gsm_abis_ip(void)
+void proto_reg_handoff_gsm_ipa(void)
 {
-	dissector_handle_t abisip_handle;
+	dissector_handle_t ipa_handle;
 
 	sub_handles[SUB_RSL] = find_dissector("gsm_abis_rsl");
 	sub_handles[SUB_OML] = find_dissector("gsm_abis_oml");
+	sub_handles[SUB_SCCP] = find_dissector("sccp");
 
-	abisip_handle = create_dissector_handle(dissect_abisip, proto_abisip);
-	dissector_add("tcp.port", TCP_PORT_ABISIP_PRIM, abisip_handle);
-	dissector_add("tcp.port", TCP_PORT_ABISIP_SEC, abisip_handle);
-	dissector_add("tcp.port", TCP_PORT_ABISIP_INST, abisip_handle);
-	dissector_add("udp.port", TCP_PORT_ABISIP_INST, abisip_handle);
+	ipa_handle = create_dissector_handle(dissect_ipa, proto_ipa);
+	dissector_add("tcp.port", TCP_PORT_ABISIP_PRIM, ipa_handle);
+	dissector_add("tcp.port", TCP_PORT_ABISIP_SEC, ipa_handle);
+	dissector_add("tcp.port", TCP_PORT_ABISIP_INST, ipa_handle);
+	dissector_add("tcp.port", TCP_PORT_AIP_PRIM, ipa_handle);
+	dissector_add("udp.port", TCP_PORT_ABISIP_INST, ipa_handle);
 }
