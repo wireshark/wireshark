df97e60d274d862431606a5b7be74f88ef4a00fd
Anders Broman <anders.broman@ericsson.com>
2009-08-13 21:46:00 +0000

    Start to implement display filtering.
    svn path=/trunk/; revision=29407

--- gtk/new_packet_list.c
+++ gtk/new_packet_list.c
@@ -74,6 +74,9 @@ static void show_cell_data_func(GtkTreeViewColumn *col,
 				GtkTreeModel *model,
 				GtkTreeIter *iter,
 				gpointer data);
+static void filter_function (GtkTreeView *treeview);
+static gboolean filter_visible_func (GtkTreeModel *model, GtkTreeIter *iter, gpointer data _U_);
+
 
 GtkWidget *
 new_packet_list_create(void)
@@ -215,6 +218,8 @@ new_packet_list_thaw(void)
 	/* Re-attach view to the model */
 	gtk_tree_view_set_model(GTK_TREE_VIEW(packetlist->view),
 				GTK_TREE_MODEL(packetlist));
+
+	filter_function(GTK_TREE_VIEW(packetlist->view));
 }
 
 void
@@ -637,5 +642,43 @@ void new_packet_list_mark_frame_cb(GtkWidget *w _U_, gpointer data _U_)
 	}
     mark_frames_ready();
 }
+
+static void filter_function (GtkTreeView *treeview) 
+{ 
+    GtkTreeModel *initial_model; 
+    GtkTreeModel *filter_model; 
+
+    initial_model= gtk_tree_view_get_model(GTK_TREE_VIEW( treeview )); 
+    filter_model = gtk_tree_model_filter_new( initial_model, NULL ); 
+
+    gtk_tree_model_filter_set_visible_func (GTK_TREE_MODEL_FILTER ( filter_model ),(GtkTreeModelFilterVisibleFunc) filter_visible_func, NULL , NULL); 
+
+    /* Apply model */ 
+    gtk_tree_view_set_model( GTK_TREE_VIEW( treeview ),filter_model); 
+
+    g_object_unref( filter_model ); 
+    return; 
+}/* filter_function */ 
+
+/* This function is called on every model row. We check whether the packet 
+ * should be visible or not. 
+ */
+
+static gboolean 
+filter_visible_func (GtkTreeModel *model, GtkTreeIter *iter, gpointer data _U_) 
+{ 
+	frame_data *fdata;
+	gint row;
+    
+    row = row_from_iter(iter); 
+    fdata = new_packet_list_get_row_data(row);
+	g_warning("filter_visible_func Row %u fdata %u",row,fdata);
+
+	if((!fdata)||(fdata->flags.passed_dfilter==1))
+		return TRUE;
+
+	return FALSE;
+} 
+
 
 #endif /* NEW_PACKET_LIST */
