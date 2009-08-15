154de6039339d4b432f7c987b321f6e79c164171
Kovarththanan Rajaratnam <kovarththanan.rajaratnam@gmail.com>
2009-08-15 09:13:16 +0000

    Switch over to using GLib's g_malloc/g_free in the name of consistency.
    svn path=/trunk/; revision=29430

--- epan/except.c
+++ epan/except.c
@@ -139,8 +139,8 @@ void except_deinit(void)
 static int init_counter;
 static void unhandled_catcher(except_t *);
 static void (*uh_catcher_ptr)(except_t *) = unhandled_catcher;
-static void *(*allocator)(size_t) = malloc;
-static void (*deallocator)(void *) = free;
+static void *(*allocator)(size_t) = g_malloc;
+static void (*deallocator)(void *) = g_free;
 static struct except_stacknode *stack_top;
 
 #define get_top() (stack_top)
