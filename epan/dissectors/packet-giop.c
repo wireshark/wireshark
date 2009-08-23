6444abbf7e2b95416a8174fd0331ea7e177ada6d
Kovarththanan Rajaratnam <kovarththanan.rajaratnam@gmail.com>
2009-08-23 05:20:30 +0000

    Avoid 'cast to pointer from integer of different size'
    svn path=/trunk/; revision=29511

--- epan/dissectors/packet-giop.c
+++ epan/dissectors/packet-giop.c
@@ -1734,9 +1734,9 @@ static gboolean try_heuristic_giop_dissector(tvbuff_t *tvb, packet_info *pinfo, 
     gboolean stream_is_big_endian = is_big_endian (header);
 
     if (stream_is_big_endian)
-      message_size = pntohl (header->message_size);
+      message_size = pntohl (&header->message_size);
     else
-      message_size = pletohl (header->message_size);
+      message_size = pletohl (&header->message_size);
 
     if (*offset > header->message_size)
       return FALSE;
