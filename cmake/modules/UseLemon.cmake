d26cdc30ea07e2ada9ee319a1ac4abdae0830ea3
Jörg Mayer <jmayer@loplof.de>
2009-09-02 16:33:01 +0000

    Beginnings of getting cmake to build stuff in epan. It's only beginnings, so epan is commented out in the subdirs statement.
    This is more a synch to avoid duplicate work and creating
    conflicting patches to the cmake stuff.
    
    svn path=/trunk/; revision=29666

---
+++ cmake/modules/UseLemon.cmake
@@ -0,0 +1,21 @@
+MACRO(ADD_LEMON_FILES _sources )
+    set(_lemonpardir ${CMAKE_SOURCE_DIR}/tools/lemon)
+    FOREACH (_current_FILE ${ARGN})
+      GET_FILENAME_COMPONENT(_in ${_current_FILE} ABSOLUTE)
+      GET_FILENAME_COMPONENT(_basename ${_current_FILE} NAME_WE)
+
+      SET(_out ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.c)
+
+      ADD_CUSTOM_COMMAND(
+         OUTPUT ${_out}
+         COMMAND ${CMAKE_BINARY_DIR}/lemon
+         ARGS
+         t=${_lemonpardir}/lempar.c
+         ${srcdir}/${_in}
+         DEPENDS ${_in}
+      )
+
+      SET(${_sources} ${${_sources}} ${_out} )
+   ENDFOREACH (_current_FILE)
+ENDMACRO(ADD_LEMON_FILES)
+
