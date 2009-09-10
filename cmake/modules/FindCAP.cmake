9250a69a9976617c735679aa5383e53802e5b43e
Jörg Mayer <jmayer@loplof.de>
2009-09-10 07:04:54 +0000

    Update to add the last missing detection stuff. Some of it is untested, kerberos is currently a hack. Apart from that, package detection should be complete.
    svn path=/trunk/; revision=29838

---
+++ cmake/modules/FindCAP.cmake
@@ -0,0 +1,32 @@
+# - Find capabilities
+# Find the native CAP includes and library
+#
+#  CAP_INCLUDE_DIRS - where to find cap.h, etc.
+#  CAP_LIBRARIES    - List of libraries when using cap.
+#  CAP_FOUND        - True if cap found.
+
+
+IF (CAP_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(CAP_FIND_QUIETLY TRUE)
+ENDIF (CAP_INCLUDE_DIRS)
+
+FIND_PATH(CAP_INCLUDE_DIR cap.h)
+
+SET(CAP_NAMES cap)
+FIND_LIBRARY(CAP_LIBRARY NAMES ${CAP_NAMES} )
+
+# handle the QUIETLY and REQUIRED arguments and set CAP_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CAP DEFAULT_MSG CAP_LIBRARY CAP_INCLUDE_DIR)
+
+IF(CAP_FOUND)
+  SET( CAP_LIBRARIES ${CAP_LIBRARY} )
+  SET( CAP_INCLUDE_DIRS ${CAP_INCLUDE_DIR} )
+ELSE(CAP_FOUND)
+  SET( CAP_LIBRARIES )
+  SET( CAP_INCLUDE_DIRS )
+ENDIF(CAP_FOUND)
+
+MARK_AS_ADVANCED( CAP_LIBRARIES CAP_INCLUDE_DIRS )
---
+++ cmake/modules/FindM.cmake
@@ -0,0 +1,32 @@
+# - Find math
+# Find the native M includes and library
+#
+#  M_INCLUDE_DIRS - where to find math.h, etc.
+#  M_LIBRARIES    - List of libraries when using math.
+#  M_FOUND        - True if math found.
+
+
+IF (M_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(M_FIND_QUIETLY TRUE)
+ENDIF (M_INCLUDE_DIRS)
+
+FIND_PATH(M_INCLUDE_DIR math.h)
+
+SET(M_NAMES m)
+FIND_LIBRARY(M_LIBRARY NAMES ${M_NAMES} )
+
+# handle the QUIETLY and REQUIRED arguments and set M_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(M DEFAULT_MSG M_LIBRARY M_INCLUDE_DIR)
+
+IF(M_FOUND)
+  SET( M_LIBRARIES ${M_LIBRARY} )
+  SET( M_INCLUDE_DIRS ${M_INCLUDE_DIR} )
+ELSE(M_FOUND)
+  SET( M_LIBRARIES )
+  SET( M_INCLUDE_DIRS )
+ENDIF(M_FOUND)
+
+MARK_AS_ADVANCED( M_LIBRARIES M_INCLUDE_DIRS )
