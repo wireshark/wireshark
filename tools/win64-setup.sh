fcaa314c6144d74c93a6eb4b4c2837f62dc43bc4
Gerald Combs <gerald@wireshark.org>
2009-09-17 01:18:51 +0000

    Try to fix the build.
    svn path=/trunk/; revision=29952

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win32-setup.sh.
 
-export DOWNLOAD_TAG="2009-06-15"
+export DOWNLOAD_TAG="2009-09-16"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN32_SETUP=`echo $0 | sed -e s/win64/win32/`
