cbc3d3f772627505d9c695e7a414b91e83915c84
Jörg Mayer <jmayer@loplof.de>
2009-09-25 18:09:42 +0000

    Try to add tpg, but it seems it doesn't build with autofoo either.
    svn path=/trunk/; revision=30151

---
+++ cmake/modules/FindYAPP.cmake
@@ -0,0 +1,25 @@
+#
+# $Id$
+#
+# - Find unix commands from cygwin
+# This module looks for some usual Unix commands.
+#
+
+INCLUDE(FindCygwin)
+
+FIND_PROGRAM(YAPP_EXECUTABLE
+  NAMES
+    yapp
+  PATHS
+    ${CYGWIN_INSTALL_PATH}/bin
+    /bin
+    /usr/bin
+    /usr/local/bin
+    /sbin
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(YAPP DEFAULT_MSG YAPP_EXECUTABLE)
+
+MARK_AS_ADVANCED(YAPP_EXECUTABLE)
+
