5e6b38d75a5e14ca609c96f9b6ab879a7d7e7c7d
Anders Broman <anders.broman@ericsson.com>
2009-10-01 13:48:36 +0000

    Update to 3GPP TS 36.413 V8.7.0 (2009-09)
    svn path=/trunk/; revision=30231

--- epan/dissectors/packet-s1ap.c
+++ epan/dissectors/packet-s1ap.c
@@ -1773,6 +1773,7 @@ static const value_string s1ap_CauseRadioNetwork_vals[] = {
   {  34, "s1-inter-system-handover-triggered" },
   {  35, "x2-handover-triggered" },
   {  36, "redirection-towards-1xRTT" },
+  {  37, "not-supported-QCI-value" },
   { 0, NULL }
 };
 
@@ -1780,7 +1781,7 @@ static const value_string s1ap_CauseRadioNetwork_vals[] = {
 static int
 dissect_s1ap_CauseRadioNetwork(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
   offset = dissect_per_enumerated(tvb, offset, actx, tree, hf_index,
-                                     36, NULL, TRUE, 1, NULL);
+                                     36, NULL, TRUE, 2, NULL);
 
   return offset;
 }
@@ -3201,7 +3202,7 @@ dissect_s1ap_RIMTransfer(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_
 static int
 dissect_s1ap_RepetitionPeriod(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
   offset = dissect_per_constrained_integer(tvb, offset, actx, tree, hf_index,
-                                                            0U, 4096U, NULL, FALSE);
+                                                            0U, 4095U, NULL, FALSE);
 
   return offset;
 }
