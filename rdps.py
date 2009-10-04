9de14754940d15e89af11f9f7c9f3637f1db5e62
Kovarththanan Rajaratnam <kovarththanan.rajaratnam@gmail.com>
2009-10-04 07:32:06 +0000

    Add GPL header
    svn path=/trunk/; revision=30280

--- rdps.py
+++ rdps.py
@@ -51,7 +51,6 @@ def ps_clean_string(raw_str):
 
 def start_code(fd, func):
     script_name = os.path.split(__file__)[-1]
-    fd.write("/* Created by %s. Do not edit! */\n" % script_name)
     fd.write("void print_ps_%s(FILE *fd) {\n" % func)
 
 def write_code(fd, raw_str):
@@ -83,7 +82,33 @@ def main():
     script_name = os.path.split(__file__)[-1]
 
     output.write('''\
-/* Created by %s. Do not edit! */
+/* DO NOT EDIT
+ *
+ * Created by %s.
+ *
+ * ps.c
+ * Definitions for generating PostScript(R) packet output.
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
 #include <stdio.h>
 
