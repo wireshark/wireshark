6300621b521f880ec08ccc6b974796e1cc687219
Anders Broman <anders.broman@ericsson.com>
2009-11-04 16:40:52 +0000

    Check if we have a handle for "v110".
    svn path=/trunk/; revision=30823

--- epan/dissectors/packet-iax2.c
+++ epan/dissectors/packet-iax2.c
@@ -2598,8 +2598,12 @@ proto_register_iax2 (void)
 void
 proto_reg_handoff_iax2 (void)
 {
+  dissector_handle_t v110_handle = NULL;
+  
   dissector_add("udp.port", IAX2_PORT, find_dissector("iax2"));
-  dissector_add("iax2.dataformat", AST_DATAFORMAT_V110, find_dissector("v110"));
+  v110_handle =  find_dissector("v110");
+  if(v110_handle)
+	  dissector_add("iax2.dataformat", AST_DATAFORMAT_V110, v110_handle);
   data_handle = find_dissector("data");
 }
 
