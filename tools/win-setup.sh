0023958937e58cea9b03b87a17c820b2823f7f09
Guy Harris <guy@alum.mit.edu>
2010-02-15 23:47:05 +0000

    More debugging - stripping out the newlines didn't suffice.
    svn path=/trunk/; revision=31891

--- tools/win-setup.sh
+++ tools/win-setup.sh
@@ -60,6 +60,8 @@ find_proxy() {
 	# Strip out newlines so that doesn't happen.
 	#
 	proxy_enabled=`regtool get /HKCU/Software/Microsoft/Windows/CurrentVersion/Internet\ Settings/ProxyEnable 2>/dev/null | tr -d '\012'`
+echo "proxy_enabled is:"
+echo -n "$proxy_enabled" | od -bc
 	if [ -n "$proxy_enabled" -a "$proxy_enabled" -ne 0 ] ; then
 		export http_proxy=`regtool get /HKCU/Software/Microsoft/Windows/CurrentVersion/Internet\ Settings/ProxyServer 2>/dev/null`
 		echo "Using Internet Explorer proxy settings."
