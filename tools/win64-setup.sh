c74f13983d92452941de8e883d4d6cba1c8e65ca
Anders Broman <anders.broman@ericsson.com>
2010-02-15 22:24:40 +0000

    Try to fix the win64 build.
    svn path=/trunk/; revision=31885

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2009-12-21"
+export DOWNLOAD_TAG="2010-01-26-1.2"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
