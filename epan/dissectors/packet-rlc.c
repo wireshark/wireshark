b9b7b1a57afd029b514a17e92501e76d477fdbe3
Bill Meier <wmeier@newsguy.com>
2010-02-16 21:22:55 +0000

    Keep gcc happy.
    svn path=/trunk/; revision=31901

--- epan/dissectors/packet-rlc.c
+++ epan/dissectors/packet-rlc.c
@@ -409,8 +409,9 @@ static gboolean free_table_entry(gpointer key _U_,
  *  from the sequence_table hash.
  * It frees the GList pointed to by the entry.
  */
-static void free_sequence_table_entry_data(struct rlc_seqlist *list)
+static void free_sequence_table_entry_data(gpointer data)
 {
+	struct rlc_seqlist *list = data;
 	if (list->list != NULL) {
 		g_list_free(list->list);
 		list->list = NULL;   /* for good measure */
