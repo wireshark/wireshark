af69a7236a03b6730f3880df2c31a18b3cac4bb2
Bill Meier <wmeier@newsguy.com>
2010-02-26 15:08:12 +0000

    Use consistent indentation.
    svn path=/trunk/; revision=32016

--- gtk/main_filter_toolbar.c
+++ gtk/main_filter_toolbar.c
@@ -83,8 +83,8 @@ GtkWidget *filter_toolbar_new()
     GtkWidget     *filter_tb;
     GList         *dfilter_list = NULL;
     GtkTooltips   *tooltips;
-	GtkToolItem	  *filter_bt, *filter_add_expr_bt, *filter_reset,
-		          *filter_apply, *item;
+    GtkToolItem   *filter_bt, *filter_add_expr_bt, *filter_reset;
+    GtkToolItem   *filter_apply, *item;
 
 
     /* Display filter construct dialog has an Apply button, and "OK" not
@@ -104,21 +104,21 @@ GtkWidget *filter_toolbar_new()
     gtk_toolbar_set_orientation(GTK_TOOLBAR(filter_tb),
                                 GTK_ORIENTATION_HORIZONTAL);
 
-	g_object_set_data(G_OBJECT(top_level), E_TB_FILTER_KEY, filter_tb);
+    g_object_set_data(G_OBJECT(top_level), E_TB_FILTER_KEY, filter_tb);
     gtk_widget_show(filter_tb);
 
     /* Create the "Filter:" button */
-	filter_bt = gtk_tool_button_new_from_stock (WIRESHARK_STOCK_DISPLAY_FILTER_ENTRY);
+    filter_bt = gtk_tool_button_new_from_stock (WIRESHARK_STOCK_DISPLAY_FILTER_ENTRY);
     g_signal_connect(filter_bt, "clicked", G_CALLBACK(display_filter_construct_cb), &args);
     gtk_widget_show(GTK_WIDGET (filter_bt));
     g_object_set_data(G_OBJECT(top_level), E_FILT_BT_PTR_KEY, filter_bt);
 
-	gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
-                           filter_bt,
-                           -1);
-	gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_bt),
-		"Open the \"Display Filter\" dialog, to edit/apply filters",
-		"Private");
+    gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
+                       filter_bt,
+                       -1);
+    gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_bt),
+                         "Open the \"Display Filter\" dialog, to edit/apply filters",
+                         "Private");
 
     /* Create the filter combobox */
     filter_cm = gtk_combo_new();
@@ -140,18 +140,19 @@ GtkWidget *filter_toolbar_new()
     gtk_widget_set_size_request(filter_cm, 400, -1);
     gtk_widget_show(filter_cm);
 
-	item = gtk_tool_item_new ();
-	gtk_container_add (GTK_CONTAINER (item), filter_cm);
-	gtk_widget_show (GTK_WIDGET (item));
+    item = gtk_tool_item_new ();
+    gtk_container_add (GTK_CONTAINER (item), filter_cm);
+    gtk_widget_show (GTK_WIDGET (item));
 
-	gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
-                           item,
-                           -1);
+    gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
+                       item,
+                       -1);
 
     /* setting a tooltip for a combobox will do nothing, so add it to the corresponding text entry */
     gtk_tooltips_set_tip(tooltips, filter_te,
         "Enter a display filter, or choose one of your recently used filters. "
-        "The background color of this field is changed by a continuous syntax check (green is valid, red is invalid, yellow may have unexpected results).",
+        "The background color of this field is changed by a continuous syntax check "
+        "(green is valid, red is invalid, yellow may have unexpected results).",
         NULL);
 
     /* Create the "Add Expression..." button, to pop up a dialog
@@ -161,26 +162,26 @@ GtkWidget *filter_toolbar_new()
     g_signal_connect(filter_add_expr_bt, "clicked", G_CALLBACK(filter_add_expr_bt_cb), filter_tb);
     gtk_widget_show(GTK_WIDGET(filter_add_expr_bt));
 
-	gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
-                           filter_add_expr_bt,
-                           -1);
+    gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
+                       filter_add_expr_bt,
+                       -1);
 
-	gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_add_expr_bt),
-		"Add an expression to this filter string", 
-		"Private");
+    gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_add_expr_bt),
+                         "Add an expression to this filter string", 
+                         "Private");
 
     /* Create the "Clear" button */
     filter_reset = gtk_tool_button_new_from_stock(WIRESHARK_STOCK_CLEAR_EXPRESSION);
     g_object_set_data(G_OBJECT(filter_reset), E_DFILTER_TE_KEY, filter_te);
     g_signal_connect(filter_reset, "clicked", G_CALLBACK(filter_reset_cb), NULL);
     gtk_widget_show(GTK_WIDGET(filter_reset));
-	gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
-                           filter_reset,
-                           -1);
+    gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
+                       filter_reset,
+                       -1);
 
-	gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_reset),
-		"Clear this filter string and update the display", 
-		"Private");
+    gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_reset),
+                         "Clear this filter string and update the display", 
+                         "Private");
 
     /* Create the "Apply" button */
     filter_apply = gtk_tool_button_new_from_stock(WIRESHARK_STOCK_APPLY_EXPRESSION);
@@ -188,13 +189,13 @@ GtkWidget *filter_toolbar_new()
     g_signal_connect(filter_apply, "clicked", G_CALLBACK(filter_activate_cb), filter_te);
     gtk_widget_show(GTK_WIDGET(filter_apply));
 
-	gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
-                           filter_apply,
-                           -1);
+    gtk_toolbar_insert(GTK_TOOLBAR(filter_tb),
+                       filter_apply,
+                       -1);
 
-	gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_apply),
-        "Apply this filter string to the display",
-		"Private");
+    gtk_tooltips_set_tip(GTK_TOOLTIPS(tooltips), GTK_WIDGET(filter_apply),
+                         "Apply this filter string to the display",
+                         "Private");
 
     /* Sets the text entry widget pointer as the E_DILTER_TE_KEY data
      * of any widget that ends up calling a callback which needs
@@ -275,11 +276,11 @@ dfilter_combo_add(GtkWidget *filter_cm, char *s) {
         g_list_length(dfilter_list) < prefs.gui_recent_df_entries_max &&
         g_list_find_custom(dfilter_list, s, dfilter_entry_match) == NULL) {
 
-      dfilter_list = g_list_append(dfilter_list, s);
-      s = NULL;
-      g_object_set_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY, dfilter_list);
-      gtk_combo_set_popdown_strings(GTK_COMBO(filter_cm), dfilter_list);
-      gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(filter_cm)->entry), g_list_first(dfilter_list)->data);
+        dfilter_list = g_list_append(dfilter_list, s);
+        s = NULL;
+        g_object_set_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY, dfilter_list);
+        gtk_combo_set_popdown_strings(GTK_COMBO(filter_cm), dfilter_list);
+        gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(filter_cm)->entry), g_list_first(dfilter_list)->data);
     }
 
     g_free(s);
@@ -292,84 +293,84 @@ dfilter_combo_add(GtkWidget *filter_cm, char *s) {
  * of the combo box GList to the user's recent file */
 void
 dfilter_recent_combo_write_all(FILE *rf) {
-  GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
-  GList     *dfilter_list = g_object_get_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY);
-  GList     *li;
-  guint      max_count = 0;
+    GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
+    GList     *dfilter_list = g_object_get_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY);
+    GList     *li;
+    guint      max_count = 0;
 
 
-  /* write all non empty display filter strings to the recent file (until max count) */
-  li = g_list_first(dfilter_list);
-  while ( li && (max_count++ < prefs.gui_recent_df_entries_max) ) {
-    if (strlen(li->data)) {
-      fprintf (rf, RECENT_KEY_DISPLAY_FILTER ": %s\n", (char *)li->data);
+    /* write all non empty display filter strings to the recent file (until max count) */
+    li = g_list_first(dfilter_list);
+    while ( li && (max_count++ < prefs.gui_recent_df_entries_max) ) {
+        if (strlen(li->data)) {
+            fprintf (rf, RECENT_KEY_DISPLAY_FILTER ": %s\n", (char *)li->data);
+        }
+        li = li->next;
     }
-    li = li->next;
-  }
 }
 
 /* empty the combobox entry field */
 void
 dfilter_combo_add_empty(void) {
-  GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
+    GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
 
-  gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(filter_cm)->entry), "");
+    gtk_entry_set_text(GTK_ENTRY(GTK_COMBO(filter_cm)->entry), "");
 }
 
 
 /* add a display filter coming from the user's recent file to the dfilter combo box */
 gboolean
 dfilter_combo_add_recent(gchar *s) {
-  GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
-  char      *dup;
+    GtkWidget *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
+    char      *dup;
 
-  dup = g_strdup(s);
+    dup = g_strdup(s);
 
-  return dfilter_combo_add(filter_cm, dup);
+    return dfilter_combo_add(filter_cm, dup);
 }
 
 /* call cf_filter_packets() and add this filter string to the recent filter list */
 gboolean
 main_filter_packets(capture_file *cf, const gchar *dftext, gboolean force)
 {
-  GtkCombo  *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
-  GList     *dfilter_list = g_object_get_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY);
-  gboolean   free_filter = TRUE;
-  char      *s;
-  cf_status_t cf_status;
+    GtkCombo  *filter_cm = g_object_get_data(G_OBJECT(top_level), E_DFILTER_CM_KEY);
+    GList     *dfilter_list = g_object_get_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY);
+    gboolean   free_filter = TRUE;
+    char      *s;
+    cf_status_t cf_status;
 
-  s = g_strdup(dftext);
+    s = g_strdup(dftext);
 
-  cf_status = cf_filter_packets(cf, s, force);
-  if (!s)
+    cf_status = cf_filter_packets(cf, s, force);
+    if (!s)
+        return (cf_status == CF_OK);
+
+    /* GtkCombos don't let us get at their list contents easily, so we maintain
+       our own filter list, and feed it to gtk_combo_set_popdown_strings when
+       a new filter is added. */
+    if (cf_status == CF_OK && strlen(s) > 0) {
+        GList *li;
+
+        while ((li = g_list_find_custom(dfilter_list, s, dfilter_entry_match)) != NULL)
+            /* Delete old/duplicate entry now. We'll re-add it later */
+            dfilter_list = g_list_delete_link(dfilter_list, li);
+
+        /* trim list size first */
+        while (g_list_length(dfilter_list) >= prefs.gui_recent_df_entries_max)
+            dfilter_list = g_list_delete_link(dfilter_list, g_list_last(dfilter_list));
+
+        free_filter = FALSE;
+        /* Push the filter to the front of the list */
+        dfilter_list = g_list_prepend(dfilter_list, s);
+        g_object_set_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY, dfilter_list);
+        gtk_combo_set_popdown_strings(filter_cm, dfilter_list);
+        gtk_entry_set_text(GTK_ENTRY(filter_cm->entry), g_list_first(dfilter_list)->data);
+    }
+
+    if (free_filter)
+        g_free(s);
+
     return (cf_status == CF_OK);
-
-  /* GtkCombos don't let us get at their list contents easily, so we maintain
-     our own filter list, and feed it to gtk_combo_set_popdown_strings when
-     a new filter is added. */
-  if (cf_status == CF_OK && strlen(s) > 0) {
-    GList *li;
-
-    while ((li = g_list_find_custom(dfilter_list, s, dfilter_entry_match)) != NULL)
-      /* Delete old/duplicate entry now. We'll re-add it later */
-      dfilter_list = g_list_delete_link(dfilter_list, li);
-
-    /* trim list size first */
-    while (g_list_length(dfilter_list) >= prefs.gui_recent_df_entries_max)
-      dfilter_list = g_list_delete_link(dfilter_list, g_list_last(dfilter_list));
-
-    free_filter = FALSE;
-    /* Push the filter to the front of the list */
-    dfilter_list = g_list_prepend(dfilter_list, s);
-    g_object_set_data(G_OBJECT(filter_cm), E_DFILTER_FL_KEY, dfilter_list);
-    gtk_combo_set_popdown_strings(filter_cm, dfilter_list);
-    gtk_entry_set_text(GTK_ENTRY(filter_cm->entry), g_list_first(dfilter_list)->data);
-  }
-
-  if (free_filter)
-    g_free(s);
-
-  return (cf_status == CF_OK);
 }
 
 
