96779de920fa5f308f29c35dce54d48e5d19b670
Jörg Mayer <jmayer@loplof.de>
2010-03-18 10:27:17 +0000

    cmake changes: - Add checking for linker flags - Install plugins with the name including the Wireshark version.   This will make it easier to find matching plugin versions if   files get just copied over.
    svn path=/trunk/; revision=32231

---
+++ cmake/modules/CheckCLinkerFlag.cmake
@@ -0,0 +1,22 @@
+# - Check whether the C linker supports a given flag.
+# CHECK_C_LINKER_FLAG(FLAG VARIABLE)
+#
+#  FLAG - the compiler flag
+#  VARIABLE - variable to store the result
+# 
+#  This actually calls the check_c_source_compiles macro.
+#  See help for CheckCSourceCompiles for a listing of variables
+#  that can modify the build.
+
+# Copyright (c) 2010, Joerg Mayer (see AUTHORS file)
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+
+INCLUDE(CheckCSourceCompiles)
+
+MACRO (CHECK_C_LINKER_FLAG _FLAG _RESULT)
+   SET(CMAKE_REQUIRED_FLAGS "${_FLAG}")
+   message(status "check linker flag - test linker flags: ${CMAKE_REQUIRED_FLAGS}")
+   CHECK_C_SOURCE_COMPILES("int main() { return 0;}" ${_RESULT})
+ENDMACRO (CHECK_C_LINKER_FLAG)
+
