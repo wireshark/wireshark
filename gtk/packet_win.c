01fbe6644cf1059a38a13a9786cf4aed3085fb22
Balint Reczey <balint.reczey@ericsson.com>
2010-04-02 20:40:20 +0000

    Fix crash when double clicking in packet list area before capturing any packet. The crash does not happen when using the new packet list.
    svn path=/trunk/; revision=32362

--- gtk/packet_win.c
+++ gtk/packet_win.c
@@ -131,6 +131,11 @@ void new_window_cb(GtkWidget *w _U_)
   struct PacketWinData *DataPtr;
   int i;
 
+  if (!cfile.current_frame) {
+    /* nothing has been captured so far */
+    return;
+  }
+
 #ifdef NEW_PACKET_LIST
   int err;
   gchar *err_info;
