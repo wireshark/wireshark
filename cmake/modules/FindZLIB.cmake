76cee2c2a1cceec6d55ddb44f83a7858ea8c81db
Jörg Mayer <jmayer@loplof.de>
2010-05-19 13:05:35 +0000

    Commit 32350 replaced the self written FindZ.cmake by cmakes FindZLIB.cmake. What it didn't do was do adapt the magic names that are derived from the name of the FindXXX.cmake script. Fix that. Also add $Id$ to the FindZLIB.cmake script.
    svn path=/trunk/; revision=32899

--- cmake/modules/FindZLIB.cmake
+++ cmake/modules/FindZLIB.cmake
@@ -1,6 +1,8 @@
 # - Find zlib
 # Find the native ZLIB includes and library
 #
+#  $Id$
+#
 #  ZLIB_INCLUDE_DIRS - where to find zlib.h, etc.
 #  ZLIB_LIBRARIES    - List of libraries when using zlib.
 #  ZLIB_FOUND        - True if zlib found.
