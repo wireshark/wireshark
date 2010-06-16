d4cc16d4a18b0b012d0284f49b381cc88a9e8de9
Guy Harris <guy@alum.mit.edu>
2010-06-16 01:30:29 +0000

    Handle WTAP_ERR_SHORT_WRITE in file_write_error_message().
    In eo_save_entry(), don't assume you can write the entire object in a
    single write call - if the object is big enough (which would require a
    64-bit address space), and you're not lucky enough to be running on
    UN*X (which means LP64, so size_t and ssize_t are big enough for the
    entire object), you can't.
    
    svn path=/trunk/; revision=33231

--- gtk/export_object.c
+++ gtk/export_object.c
@@ -126,6 +126,11 @@ static gboolean
 eo_save_entry(gchar *save_as_filename, export_object_entry_t *entry, gboolean show_err)
 {
 	int to_fd;
+	gint64 bytes_left;
+	int bytes_to_write;
+	ssize_t bytes_written;
+	guint8 *ptr;
+	int err;
 
 	to_fd = ws_open(save_as_filename, O_WRONLY | O_CREAT | O_EXCL |
 			 O_BINARY, 0644);
@@ -136,14 +141,41 @@ eo_save_entry(gchar *save_as_filename, export_object_entry_t *entry, gboolean sh
 		return FALSE;
 	}
 
-	if(ws_write(to_fd, entry->payload_data, entry->payload_len) < 0) {
-		if (show_err)
-			write_failure_alert_box(save_as_filename, errno);
-		ws_close(to_fd);
-		g_free(save_as_filename);
-		return FALSE;
+	/*
+	 * The third argument to _write() on Windows is an unsigned int,
+	 * so, on Windows, that's the size of the third argument to
+	 * ws_write().
+	 *
+	 * The third argument to write() on UN*X is a size_t, although
+	 * the return value is an ssize_t, so one probably shouldn't
+	 * write more than the max value of an ssize_t.
+	 *
+	 * In either case, there's no guarantee that a gint64 such as
+	 * payload_len can be passed to ws_write(), so we write in
+	 * chunks of, at most 2^31 bytes.
+	 */
+	ptr = entry->payload_data;
+	bytes_left = entry->payload_len;
+	while (bytes_left != 0) {
+		if (bytes_left > 0x40000000)
+			bytes_to_write = 0x40000000;
+		else
+			bytes_to_write = bytes_left;
+		bytes_written = ws_write(to_fd, ptr, bytes_to_write);
+		if(bytes_written <= 0) {
+			if (bytes_written < 0)
+				err = errno;
+			else
+				err = WTAP_ERR_SHORT_WRITE;
+			if (show_err)
+				write_failure_alert_box(save_as_filename, err);
+			ws_close(to_fd);
+			g_free(save_as_filename);
+			return FALSE;
+		}
+		bytes_left -= bytes_written;
+		ptr += bytes_written;
 	}
-
 	if (ws_close(to_fd) < 0) {
 		if (show_err)
 			write_failure_alert_box(save_as_filename, errno);
