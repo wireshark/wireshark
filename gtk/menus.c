ea7ec90221ea4cb3289585daf6994b0063305781
Guy Harris <guy@alum.mit.edu>
2010-06-29 22:38:00 +0000

    Back out what I suspect were private/not-ready-for-prime-time changes, not all of the parts of which were present, causing the build to fail.
    svn path=/trunk/; revision=33377

--- gtk/menus.c
+++ gtk/menus.c
@@ -96,7 +96,6 @@
 #include "gtk/gui_utils.h"
 #include "gtk/manual_addr_resolv.h"
 #include "gtk/proto_help.h"
-#include "gtk/dissector_tables_dlg.h"
 
 #ifdef NEW_PACKET_LIST
 #include "gtk/new_packet_list.h"
@@ -781,10 +780,7 @@ static GtkItemFactoryEntry menu_items[] =
     {"/Help/_Supported Protocols (slow!)", NULL, GTK_MENU_FUNC(supported_cb), 0, NULL, NULL,},
     {"/Help/<separator>", NULL, NULL, 0, "<Separator>", NULL,},
     {"/Help/_About Wireshark", NULL, GTK_MENU_FUNC(about_wireshark_cb),
-                       0, "<StockItem>", WIRESHARK_STOCK_ABOUT},
-    {"/Internal/Dissector tables", NULL, GTK_MENU_FUNC(dissector_tables_dlg_cb),
-                       0, NULL, NULL,}
-
+                       0, "<StockItem>", WIRESHARK_STOCK_ABOUT}
 };
 
 
