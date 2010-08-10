7678d060ae20c1f96bc4225777be064774729871
Stig Bjørlykke <stig@bjorlykke.org>
2010-08-10 21:16:41 +0000

    From Cal Turney via bug 5038: Fix for bug introduced in revision 33759.
    svn path=/trunk/; revision=33762

--- gtk/main.c
+++ gtk/main.c
@@ -1508,19 +1508,16 @@ main_cf_cb_file_read_finished(capture_file *cf)
 {
     gchar* cf_path_end;
     gchar* dir_path;
-    size_t dir_path_len;
 
     if (!cf->is_tempfile && cf->filename) {
         /* Add this filename to the list of recent files in the "Recent Files" submenu */
         add_menu_recent_capture_file(cf->filename);
 
-        /* Remember folder for next Open dialog and saved in recent */
+        /* Remember folder for next Open dialog and save it in recent */
         cf_path_end = find_last_pathname_separator(cf->filename);
-        dir_path_len = (cf_path_end - cf->filename);
-        dir_path = g_malloc (dir_path_len + 1);
-		g_snprintf (dir_path, dir_path_len, "%s", cf->filename);
-        dir_path[dir_path_len++] = '\0';
+        dir_path = g_strndup(cf->filename, cf_path_end - cf->filename);
         set_last_open_dir(dir_path);
+        g_free(dir_path);
     }
     set_display_filename(cf);
 
