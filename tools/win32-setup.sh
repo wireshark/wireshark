dd551b6585c83e029c45de5e7356f27935079d6f
Anders Broman <anders.broman@ericsson.com>
2010-10-05 15:46:13 +0000

    Use the new GTK bundles.
    svn path=/trunk/; revision=34382

--- tools/win32-setup.sh
+++ tools/win32-setup.sh
@@ -4,7 +4,7 @@
 
 # 32-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2010-08-25"
+export DOWNLOAD_TAG="2010-10-05"
 export WIRESHARK_TARGET_PLATFORM="win32"
 
 WIN_SETUP=`echo $0 | sed -e s/win32/win/`
