4ea7b6d2e82838f68f2824593ec467e821da3d49
Anders Broman <anders.broman@ericsson.com>
2010-10-28 16:36:14 +0000

    Apply 31718 Activate dedicated EPS bearer context accept may have no IE's. https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=4432
    svn path=/trunk-1.2/; revision=34676

--- epan/dissectors/packet-nas_eps.c
+++ epan/dissectors/packet-nas_eps.c
@@ -3189,6 +3189,9 @@ nas_esm_act_ded_eps_bearer_ctx_req(tvbuff_t *tvb, proto_tree *tree, guint32 offs
 	guint32	consumed;
 	guint	curr_len;
 
+	if(len==0)
+		return;
+
 	curr_offset = offset;
 	curr_len = len;
 
