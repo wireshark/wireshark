0fd60b4bd58c9f8121e54111c8350f76b902a8ee
Gerald Combs <gerald@wireshark.org>
2011-01-11 00:33:23 +0000

    Copy over r35298 by hand.
    ------------------------------------------------------------------------
    r35298 | gerald | 2010-12-29 12:09:27 -0800 (Wed, 29 Dec 2010) | 2 lines
    Changed paths:
       M /trunk/asn1/snmp/packet-snmp-template.c
       M /trunk/epan/dissectors/packet-snmp.c
    
    Fix a buffer overflow in the engineId preferences. Fixes bug 5530.
    ------------------------------------------------------------------------
    
    Update the release notes.
    
    svn path=/trunk-1.2/; revision=35470

--- epan/dissectors/packet-snmp.c
+++ epan/dissectors/packet-snmp.c
@@ -2895,8 +2895,8 @@ static void snmp_usm_password_to_key_md5(const guint8 *password,
 	/*****************************************************/
 	/* Now localize the key with the engineID and pass   */
 	/* through MD5 to produce final key                  */
-	/* May want to ensure that engineLength <= 32,       */
-	/* otherwise need to use a buffer larger than 64     */
+	/* We ignore invalid engineLengths here. More strict */
+	/* checking is done in snmp_users_update_cb.         */
 	/*****************************************************/
 
 	md5_init(&MD);
@@ -2921,7 +2921,7 @@ static void snmp_usm_password_to_key_sha1(const guint8 *password,
 								   guint   engineLength,
 								   guint8 *key ) {
 	sha1_context     SH;
-	guint8     *cp, password_buf[72];
+	guint8     *cp, password_buf[64];
 	guint32      password_index = 0;
 	guint32      count = 0, i;
 
@@ -2947,15 +2947,14 @@ static void snmp_usm_password_to_key_sha1(const guint8 *password,
 	/*****************************************************/
 	/* Now localize the key with the engineID and pass   */
 	/* through SHA to produce final key                  */
-	/* May want to ensure that engineLength <= 32,       */
-	/* otherwise need to use a buffer larger than 72     */
+	/* We ignore invalid engineLengths here. More strict */
+	/* checking is done in snmp_users_update_cb.         */
 	/*****************************************************/
-	memcpy(password_buf, key, 20);
-	memcpy(password_buf+20, engineID, engineLength);
-	memcpy(password_buf+20+engineLength, key, 20);
 
 	sha1_starts(&SH);
-	sha1_update(&SH, password_buf, 40+engineLength);
+	sha1_update(&SH, key, 20);
+	sha1_update(&SH, engineID, engineLength);
+	sha1_update(&SH, key, 20);
 	sha1_finish(&SH, key);
 	return;
  }
@@ -3024,6 +3023,11 @@ static void snmp_users_update_cb(void* p _U_, const char** err) {
 	for (i=0; i<num_ueas-1; i++) {
 		snmp_ue_assoc_t* u = &(ueas[i]);
 		
+		/* RFC 3411 section 5 */
+		if (u->engine.len < 5 || u->engine.len > 32) {
+			g_string_append_printf(es, "Invalid engineId length (%u). Must be between 5 and 32 (10 and 64 hex digits)\n", u->engine.len);
+		}
+
 		
 		if ( u->user.userName.len == ue->user.userName.len
 			&& u->engine.len == ue->engine.len ) {
@@ -3031,13 +3035,13 @@ static void snmp_users_update_cb(void* p _U_, const char** err) {
 			if (u->engine.len > 0 && memcmp( u->engine.data,   ue->engine.data,  u->engine.len ) == 0) {
 				if ( memcmp( u->user.userName.data, ue->user.userName.data, ue->user.userName.len ) == 0 ) {
 					/* XXX: make a string for the engineId */
-					g_string_append_printf(es,"duplicate key (userName='%s')\n",ue->user.userName.data);
+					g_string_append_printf(es,"Duplicate key (userName='%s')\n",ue->user.userName.data);
 				}
 			}
 			
 			if (u->engine.len == 0) {
 				if ( memcmp( u->user.userName.data, ue->user.userName.data, ue->user.userName.len ) == 0 ) {
-					g_string_append_printf(es,"duplicate key (userName='%s' engineId=NONE)\n",ue->user.userName.data);
+					g_string_append_printf(es,"Duplicate key (userName='%s' engineId=NONE)\n",ue->user.userName.data);
 				}
 			}
 		}
@@ -3396,7 +3400,7 @@ void proto_register_snmp(void) {
         "snmp.T_operation", HFILL }},
 
 /*--- End of included file: packet-snmp-hfarr.c ---*/
-#line 1943 "packet-snmp-template.c"
+#line 1947 "packet-snmp-template.c"
   };
 
   /* List of subtrees */
@@ -3436,7 +3440,7 @@ void proto_register_snmp(void) {
     &ett_snmp_RReqPDU_U,
 
 /*--- End of included file: packet-snmp-ettarr.c ---*/
-#line 1959 "packet-snmp-template.c"
+#line 1963 "packet-snmp-template.c"
   };
   module_t *snmp_module;
   static uat_field_t users_fields[] = {
