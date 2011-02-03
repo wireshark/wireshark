bec334b1c6a4268f7ca1cd3f16785a7544f968d6
Bill Meier <wmeier@newsguy.com>
2011-02-03 19:33:48 +0000

    Fix: Cygwin make fails after updating to bash v 4.1.9.2
    Extracts from the cygwin Bash 4.1.9.2 release announcement:
    
    4a. For a single affected script, add this line just after the she-bang:
     (set -o igncr) 2>/dev/null && set -o igncr; # comment is needed
    
    4e. bash-4.1.9-1 dropped support for 'shopt -s igncr';
    
    I'm assuming this is backwards compatible; If not, we'll know quite shortly :) 
    
    
    svn path=/trunk/; revision=35788

--- tools/win-setup.sh
+++ tools/win-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-(shopt -s igncr) 2>/dev/null && shopt -s igncr; # hack to force this file to be processed by cygwin bash with -o igncr
-                                                # needed when this file is exec'd from win32-setup.sh & win64-setup.sh
+(set -o igncr) 2>/dev/null && set -o igncr;  # hack to force this file to be processed by cygwin bash with -o igncr
+                                             # needed when this file is exec'd from win32-setup.sh & win64-setup.sh
 #
 # $Id$
 
