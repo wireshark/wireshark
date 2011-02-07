wireshark/wireshark

commit 9d3586b4f5de8f9a9e4fd5f4ec0e5a4fa97518ac

And even more -DGSEAL_ENABLE cleanup for GTK+ 3.0 preparation...
svn path=/trunk/; revision=35849

gtk/color_edit_dlg.c
@@ -475,23 +475,33 @@ color_sel_win_new(color_filter_t *colorf, gboolean is_bg)
 #endif
   }
 
-  color_sel_ok = GTK_COLOR_SELECTION_DIALOG (color_sel_win)->ok_button;
+  g_object_get(color_sel_win, "ok-button", color_sel_ok, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_ok", color_sel_ok);
+#if GTK_CHECK_VERSION(2,18,0)
+  gtk_widget_set_can_default(color_sel_ok, TRUE);
+#else
   GTK_WIDGET_SET_FLAGS (color_sel_ok, GTK_CAN_DEFAULT);
+#endif
 
-  color_sel_cancel = GTK_COLOR_SELECTION_DIALOG (color_sel_win)->cancel_button;
+  g_object_get(color_sel_win, "cancel-button", color_sel_cancel, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_cancel", color_sel_cancel);
+#if GTK_CHECK_VERSION(2,18,0)
+  gtk_widget_set_can_default(color_sel_cancel, TRUE);
+#else
   GTK_WIDGET_SET_FLAGS (color_sel_cancel, GTK_CAN_DEFAULT);
+#endif
   window_set_cancel_button(color_sel_win, color_sel_cancel, NULL); /* ensure esc does req'd local cxl action.    */
   /* esc as handled by the                      */
   /* gtk_color_selection_dialog widget          */
   /*  doesn't result in this happening.         */
 
-  color_sel_help = GTK_COLOR_SELECTION_DIALOG (color_sel_win)->help_button;
+  g_object_get(color_sel_win, "help-button", color_sel_help, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_help", color_sel_help);
-
-
+#if GTK_CHECK_VERSION(2,18,0)
+  gtk_widget_set_can_default(color_sel_help, TRUE);
+#else
   GTK_WIDGET_SET_FLAGS (color_sel_help, GTK_CAN_DEFAULT);
+#endif
 
   g_signal_connect(color_sel_ok, "clicked", G_CALLBACK(color_sel_ok_cb), color_sel_win);
   g_signal_connect(color_sel_cancel, "clicked", G_CALLBACK(color_sel_cancel_cb), color_sel_win);
@@ -540,8 +550,11 @@ color_sel_ok_cb                        (GtkButton       *button _U_,
 
   color_dialog = (GtkWidget *)user_data;
 
-  gtk_color_selection_get_current_color(GTK_COLOR_SELECTION(
-                                          GTK_COLOR_SELECTION_DIALOG(color_dialog)->colorsel), &new_color);
+#if GTK_CHECK_VERSION(2,14,0)
+  gtk_color_selection_get_current_color(GTK_COLOR_SELECTION(gtk_color_selection_dialog_get_color_selection(GTK_COLOR_SELECTION_DIALOG(color_dialog))), &new_color);
+#else
+  gtk_color_selection_get_current_color(GTK_COLOR_SELECTION(GTK_COLOR_SELECTION_DIALOG(color_dialog)->colorsel), &new_color);
+#endif
 
   if ( ! get_color(&new_color) ){
     simple_dialog(ESD_TYPE_ERROR, ESD_BTN_OK,

gtk/conversations_table.c
@@ -2583,8 +2583,11 @@ init_conversation_table(gboolean hide_ports, const char *table_name, const char 
     window_present(conversations->win);
 
     cf_retap_packets(&cfile);
+#if GTK_CHECK_VERSION(2,14,0)
+    gdk_window_raise(gtk_widget_get_window(conversations->win));
+#else
     gdk_window_raise(conversations->win->window);
-
+#endif
 }
 
 
@@ -2724,7 +2727,11 @@ ct_filter_toggle_dest(GtkWidget *widget, gpointer data)
 
     cf_retap_packets(&cfile);
     if (conversations) {
+#if GTK_CHECK_VERSION(2,14,0)
+        gdk_window_raise(gtk_widget_get_window(conversations->win));
+#else
         gdk_window_raise(conversations->win->window);
+#endif
     }
 }
 
@@ -2840,8 +2847,11 @@ init_conversation_notebook_cb(GtkWidget *w _U_, gpointer d _U_)
     window_present(win);
 
     cf_retap_packets(&cfile);
+#if GTK_CHECK_VERSION(2,14,0)
+    gdk_window_raise(gtk_widget_get_window(win));
+#else
     gdk_window_raise(win->window);
-
+#endif
 }
 
 typedef struct _key {

gtk/dlg_utils.c
@@ -48,7 +48,11 @@ dlg_button_new(GtkWidget *hbox, GtkWidget *button_hbox, const gchar *stock_id)
     GtkWidget *button;
 
     button = gtk_button_new_from_stock(stock_id);
+#if GTK_CHECK_VERSION(2,18,0)
+    gtk_widget_set_can_default(button, TRUE);
+#else
     GTK_WIDGET_SET_FLAGS(button, GTK_CAN_DEFAULT);
+#endif
     g_object_set_data(G_OBJECT(hbox), stock_id, button);
     gtk_box_pack_end(GTK_BOX(button_hbox), button, FALSE, FALSE, 0);
     gtk_widget_show(button);
@@ -230,7 +234,11 @@ dlg_button_row_new(const gchar *stock_id_first, ...)
     /* do we have a help button? -> special handling for it */
     if (help) {
         button = gtk_button_new_from_stock(help);
+#if GTK_CHECK_VERSION(2,18,0)
+	gtk_widget_set_can_default(button, TRUE);
+#else
         GTK_WIDGET_SET_FLAGS(button, GTK_CAN_DEFAULT);
+#endif
         g_object_set_data(G_OBJECT(hbox), help, button);
         gtk_box_pack_start(GTK_BOX(help_hbox), button, FALSE, FALSE, 0);
         gtk_widget_show(button);
@@ -240,7 +248,11 @@ dlg_button_row_new(const gchar *stock_id_first, ...)
     /* do we have a copy button? -> special handling for it */
     if (copy) {
         button = gtk_button_new_from_stock(copy);
+#if GTK_CHECK_VERSION(2,18,0)
+	gtk_widget_set_can_default(button, TRUE);
+#else
         GTK_WIDGET_SET_FLAGS(button, GTK_CAN_DEFAULT);
+#endif
         g_object_set_data(G_OBJECT(hbox), copy, button);
         gtk_box_pack_start(GTK_BOX(help_hbox), button, FALSE, FALSE, 0);
         gtk_widget_show(button);

gtk/drag_and_drop.c
@@ -313,6 +313,8 @@ dnd_data_received(GtkWidget *widget _U_, GdkDragContext *dc _U_, gint x _U_, gin
 {
     gpointer  dialog;
     gchar *cf_names_freeme;
+    const guchar *sel_data_data;
+    gint sel_data_len;
 
     if (info == DND_TARGET_URL) {
         /* Usually we block incoming events by disabling the corresponding menu/toolbar items.
@@ -342,11 +344,18 @@ dnd_data_received(GtkWidget *widget _U_, GdkDragContext *dc _U_, gint x _U_, gin
             return;
         }
 
-		/* the selection_data will soon be gone, make a copy first */
-		/* the data string is not zero terminated -> make a zero terminated "copy" of it */
-		cf_names_freeme = g_malloc(selection_data->length + 1);
-		memcpy(cf_names_freeme, selection_data->data, selection_data->length);
-		cf_names_freeme[selection_data->length] = '\0';
+	/* the selection_data will soon be gone, make a copy first */
+	/* the data string is not zero terminated -> make a zero terminated "copy" of it */
+#if GTK_CHECK_VERSION(2,14,0)
+	sel_data_len = gtk_selection_data_get_length(selection_data);
+	sel_data_data = gtk_selection_data_get_data(selection_data);
+#else
+	sel_data_len = selection_data->length;
+	sel_data_data = selection_data->data;
+#endif
+	cf_names_freeme = g_malloc(sel_data_len + 1);
+	memcpy(cf_names_freeme, sel_data_data, sel_data_len);
+	cf_names_freeme[sel_data_len] = '\0';
 
         /* ask the user to save it's current capture file first */
         if((cfile.state != FILE_CLOSED) && !cfile.user_saved && prefs.gui_ask_unsaved) {

gtk/expert_comp_table.c
@@ -278,7 +278,11 @@ error_select_filter_cb(GtkWidget *widget _U_, gpointer callback_data, guint call
     case ACTION_MATCH:
         gtk_entry_set_text(GTK_ENTRY(main_display_filter_widget), str);
         main_filter_packets(&cfile, str, FALSE);
+#if GTK_CHECK_VERSION(2,14,0)
+	gdk_window_raise(gtk_widget_get_window(top_level));
+#else
         gdk_window_raise(top_level->window);
+#endif
         break;
     case ACTION_PREPARE:
         gtk_entry_set_text(GTK_ENTRY(main_display_filter_widget), str);

gtk/filter_dlg.c
@@ -638,7 +638,11 @@ filter_dialog_new(GtkWidget *button, GtkWidget *parent_filter_te,
 
     if(construct_args->modal_and_transient) {
         parent = gtk_widget_get_parent_window(parent_filter_te);
+#if GTK_CHECK_VERSION(2,14,0)
+        gdk_window_set_transient_for(gtk_widget_get_window(main_w), parent);
+#else
         gdk_window_set_transient_for(main_w->window, parent);
+#endif
         gtk_window_set_modal(GTK_WINDOW(main_w), TRUE);
     }
 

gtk/filter_utils.c
@@ -83,7 +83,11 @@ apply_selected_filter (guint callback_action, char *filter)
 	case ACTION_MATCH:
 		gtk_entry_set_text(GTK_ENTRY(main_display_filter_widget), str);
 		main_filter_packets(&cfile, str, FALSE);
+#if GTK_CHECK_VERSION(2,14,0)
+		gdk_window_raise(gtk_widget_get_window(top_level));
+#else
 		gdk_window_raise(top_level->window);
+#endif
 		break;
 	case ACTION_PREPARE:
 		gtk_entry_set_text(GTK_ENTRY(main_display_filter_widget), str);

gtk/find_dlg.c
@@ -644,7 +644,7 @@ find_frame_ok_cb(GtkWidget *ok_bt _U_, gpointer parent_w)
    */
   g_free(cfile.sfilter);
   cfile.sfilter = g_strdup(filter_text);
-  cfile.dir = GTK_TOGGLE_BUTTON (up_rb)->active ? SD_BACKWARD : SD_FORWARD;
+  cfile.dir = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON (up_rb)) ? SD_BACKWARD : SD_FORWARD;
   cfile.hex = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON (hex_rb));
   cfile.string = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON (string_rb));
   cfile.scs_type = scs_type;

commit 75333216eec1c2c3e30e94d4a665615cb313ced7

Fix crash introduced in r35849
svn path=/trunk/; revision=35851

gtk/color_edit_dlg.c
@@ -475,7 +475,7 @@ color_sel_win_new(color_filter_t *colorf, gboolean is_bg)
 #endif
   }
 
-  g_object_get(color_sel_win, "ok-button", color_sel_ok, NULL);
+  g_object_get(color_sel_win, "ok-button", &color_sel_ok, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_ok", color_sel_ok);
 #if GTK_CHECK_VERSION(2,18,0)
   gtk_widget_set_can_default(color_sel_ok, TRUE);
@@ -483,7 +483,7 @@ color_sel_win_new(color_filter_t *colorf, gboolean is_bg)
   GTK_WIDGET_SET_FLAGS (color_sel_ok, GTK_CAN_DEFAULT);
 #endif
 
-  g_object_get(color_sel_win, "cancel-button", color_sel_cancel, NULL);
+  g_object_get(color_sel_win, "cancel-button", &color_sel_cancel, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_cancel", color_sel_cancel);
 #if GTK_CHECK_VERSION(2,18,0)
   gtk_widget_set_can_default(color_sel_cancel, TRUE);
@@ -495,7 +495,7 @@ color_sel_win_new(color_filter_t *colorf, gboolean is_bg)
   /* gtk_color_selection_dialog widget          */
   /*  doesn't result in this happening.         */
 
-  g_object_get(color_sel_win, "help-button", color_sel_help, NULL);
+  g_object_get(color_sel_win, "help-button", &color_sel_help, NULL);
   g_object_set_data(G_OBJECT(color_sel_win), "color_sel_help", color_sel_help);
 #if GTK_CHECK_VERSION(2,18,0)
   gtk_widget_set_can_default(color_sel_help, TRUE);