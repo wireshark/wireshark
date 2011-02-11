0d9152e7826b6a59ae9754df88c4865d2e0060b0
Guy Harris <guy@alum.mit.edu>
2011-02-11 05:54:34 +0000

    Register for the media type "application/ipp", so *any* HTTP request or response with that content type will be dissected as IPP.
    svn path=/trunk/; revision=35914

--- epan/dissectors/packet-ipp.c
+++ epan/dissectors/packet-ipp.c
@@ -710,5 +710,6 @@ proto_reg_handoff_ipp(void)
 	 */
 	ipp_handle = create_dissector_handle(dissect_ipp, proto_ipp);
 	http_dissector_add(631, ipp_handle);
+	dissector_add_string("media_type", "application/ipp", ipp_handle);
         data_handle = find_dissector("data");
 }
