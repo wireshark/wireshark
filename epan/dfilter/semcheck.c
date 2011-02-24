be52b33c28f3887c28180ae9a06a3aadbc075631
Jaap Keuter <jaap.keuter@xs4all.nl>
2011-02-24 07:50:30 +0000

    Copy over revisions from the trunk:
    ------------------------------------------------------------------------
    r35583 | sfisher | 2011-01-19 18:58:53 +0100 (Wed, 19 Jan 2011) | 5 lines
    Changed paths:
       M /trunk/gtk/main_proto_draw.c
    
    
    From Cal Turney via bug #5585: When searching in packet bytes, the field and
    bytes are not immediately shown.
    ------------------------------------------------------------------------
    r35594 | jake | 2011-01-20 09:58:03 +0100 (Thu, 20 Jan 2011) | 2 lines
    Changed paths:
       M /trunk/asn1/snmp/packet-snmp-template.c
       M /trunk/epan/dissectors/packet-snmp.c
    
    Allow for expert items to be actually attached somewhere.
    ------------------------------------------------------------------------
    r35599 | jake | 2011-01-20 21:04:39 +0100 (Thu, 20 Jan 2011) | 2 lines
    Changed paths:
       M /trunk/epan/oids.c
    
    Allow presentation of Gauge32 values larger than 0x7FFFFFFF.
    ------------------------------------------------------------------------
    r35621 | morriss | 2011-01-22 16:15:51 +0100 (Sat, 22 Jan 2011) | 14 lines
    Changed paths:
       M /trunk/epan/dfilter/semcheck.c
    
    From Johannes Lange  via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=5606 :
    
    In semcheck.c the display filter string of an expression is checked against the
    header_field_info.display value BASE_CUSTOM. But the value of BASE_CUSTOM is
    applied as bitmask while the actual type is an enum (BASE_CUSTOM = 6).
    
    With this BASE_DEC, BASE_DEC_HEX and BASE_HEX_DEC are also matching and are not
    accepted as filter expression.
    
    
    Actually: BASE_DEC works but not BASE_HEX.  And the problem only shows up when
    trying to match a field in one of these bases against a string (from a
    value_string).
    ------------------------------------------------------------------------
    r35626 | stig | 2011-01-22 19:07:28 +0100 (Sat, 22 Jan 2011) | 6 lines
    Changed paths:
       M /trunk/epan/dissectors/packet-p_mul.c
    
    Ensure we keep the pkg_data pointer for the init routine to cleanup correctly,
    because the ep memory is cleared before the init routine is called.
    
    Fixed the variable name for the no_yes true_false_string.
    Update reference to protocol description.
    ------------------------------------------------------------------------
    r35651 | jake | 2011-01-25 23:11:59 +0100 (Tue, 25 Jan 2011) | 4 lines
    Changed paths:
       M /trunk/epan/addr_resolv.c
    
    Fix fo bug 5615:
    Remove the filters on IPv6 Link Local and Multicast addresses, since 
    these _can_ be resolved through the hosts file and manual entry.
    ------------------------------------------------------------------------
    
    svn path=/trunk-1.4/; revision=36046

--- epan/dfilter/semcheck.c
+++ epan/dfilter/semcheck.c
@@ -230,7 +230,7 @@ mk_fvalue_from_val_string(header_field_info *hfinfo, char *s)
 				hfinfo->abbrev);
 		return NULL;
 	}
-	else if (hfinfo->display & BASE_CUSTOM) {
+	else if (hfinfo->display == BASE_CUSTOM) {
 		/*  If a user wants to match against a custom string, we would
 		 *  somehow have to have the integer value here to pass it in
 		 *  to the custom-display function.  But we don't have an
