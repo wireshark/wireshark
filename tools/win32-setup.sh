3741009a5ad53cc449d084700366aaf1e6728d54
Jaap Keuter <jaap.keuter@xs4all.nl>
2011-02-26 11:02:41 +0000

    Update the User's Guide.
    svn path=/trunk-1.4/; revision=36076

--- tools/win32-setup.sh
+++ tools/win32-setup.sh
@@ -4,7 +4,7 @@
 
 # 32-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2010-10-08-1.4"
+export DOWNLOAD_TAG="2011-02-25-1.4"
 export WIRESHARK_TARGET_PLATFORM="win32"
 
 WIN_SETUP=`echo $0 | sed -e s/win32/win/`
