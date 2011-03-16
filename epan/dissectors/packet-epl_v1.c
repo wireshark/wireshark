9486d7dcc2b4a7c902736a580deb0aafc15f3070
Anders Broman <anders.broman@ericsson.com>
2011-03-16 06:21:56 +0000

    From Roland Knall: Patch, which removes both EPL and EPLv1 .h files, as well as adding a heuristic dissector hook to EPL (v1 is nearly nowhere in use anymore, therefore not needed)
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=5751
    
    svn path=/trunk/; revision=36198

--- epan/dissectors/packet-epl_v1.c
+++ epan/dissectors/packet-epl_v1.c
@@ -41,7 +41,110 @@
 #include <epan/etypes.h>
 #include <epan/emem.h>
 
-#include "packet-epl_v1.h"
+
+/* Offsets of fields within an EPL_V1 packet. */
+#define EPL_V1_SERVICE_OFFSET                     0   /* same offset for all message types*/
+#define EPL_V1_DEST_OFFSET                        1   /* same offset for all message types*/
+#define EPL_V1_SRC_OFFSET                         2   /* same offset for all message types*/
+
+#define EPL_V1_SOC_C2_OFFSET                      3
+#define EPL_V1_SOC_PF_OFFSET                      3
+#define EPL_V1_SOC_NET_COMMAND_OFFSET             4
+#define EPL_V1_SOC_NET_TIME_OFFSET                6
+#define EPL_V1_SOC_POWERLINK_CYCLE_TIME_OFFSET   10
+#define EPL_V1_SOC_NET_COMMAND_PARAMETER_OFFSET  14
+
+#define EPL_V1_PREQ_C2_OFFSET                     3
+/* "Powerlink Multimanager Konzept V1.1" protocol extension*/
+#define PMM_KONZEPT_V1_1_PREQ_YA                    3
+#define PMM_KONZEPT_V1_1_PREQ_SC                    3
+/* end "Powerlink Multimanager Konzept V1.1" protocol extension*/
+#define EPL_V1_PREQ_RD_OFFSET                     3
+#define EPL_V1_PREQ_RD_OFFSET                     3
+#define EPL_V1_PREQ_POLL_SIZE_OUT_OFFSET          4
+#define EPL_V1_PREQ_OUT_DATA_OFFSET              10
+
+#define EPL_V1_PRES_C2_OFFSET                     3
+#define EPL_V1_PRES_EX_OFFSET                     3
+#define EPL_V1_PRES_RS_OFFSET                     3
+#define EPL_V1_PRES_WA_OFFSET                     3
+#define EPL_V1_PRES_ER_OFFSET                     3
+#define EPL_V1_PRES_RD_OFFSET                     3
+#define EPL_V1_PRES_POLL_SIZE_IN_OFFSET           4
+#define EPL_V1_PRES_IN_DATA_OFFSET               10
+
+#define EPL_V1_EOC_NET_COMMAND_OFFSET             4
+#define EPL_V1_EOC_NET_COMMAND_PARAMETER_OFFSET  14
+
+#define EPL_V1_AINV_CHANNEL_OFFSET                3
+
+#define EPL_V1_ASND_CHANNEL_OFFSET                3
+#define EPL_V1_ASND_SIZE_OFFSET                   4
+#define EPL_V1_ASND_DATA_OFFSET                   6
+#define EPL_V1_ASND_NODE_ID_OFFSET                6
+#define EPL_V1_ASND_HARDWARE_REVISION_OFFSET     10
+#define EPL_V1_ASND_FIRMWARE_VERSION_OFFSET      14
+#define EPL_V1_ASND_DEVICE_VARIANT_OFFSET        18
+#define EPL_V1_ASND_POLL_IN_SIZE_OFFSET          22
+#define EPL_V1_ASND_POLL_OUT_SIZE_OFFSET         26
+
+/* EPL_V1 message types */
+#define EPL_V1_SOC    0x01
+#define EPL_V1_EOC    0x02
+#define EPL_V1_PREQ   0x03
+#define EPL_V1_PRES   0x04
+#define EPL_V1_AINV   0x05
+#define EPL_V1_ASND   0x06
+
+static const value_string service_vals[] = {
+   {EPL_V1_SOC,  "Start of Cyclic (SoC)"   },
+   {EPL_V1_EOC,  "End of Cyclic (EoC)"     },
+   {EPL_V1_PREQ, "Poll Request (PReq)"     },
+   {EPL_V1_PRES, "Poll Response (PRes)"    },
+   {EPL_V1_AINV, "Acyclic Invite (AInv)"   },
+   {EPL_V1_ASND, "Acyclic Send (ASnd)"     },
+   {0,NULL}
+};
+
+/* Channel values for EPL_V1 message type "AInv" */
+#define EPL_V1_AINV_IDENT         1
+#define EPL_V1_AINV_GENERIC     255
+
+static const value_string ainv_channel_number_vals[] = {
+   {EPL_V1_AINV_IDENT,     "Ident"             },
+   {EPL_V1_AINV_GENERIC,   "Generic Channel"   },
+   {0,NULL}
+};
+
+/* Channel values for EPL_V1 message type "ASnd" */
+#define EPL_V1_ASND_IDENT         1
+#define EPL_V1_ASND_GENERIC     255
+
+static const value_string asnd_channel_number_vals[] = {
+   {EPL_V1_ASND_IDENT,     "Ident"             },
+   {EPL_V1_ASND_GENERIC,   "Generic Channel"   },
+   {0,NULL}
+};
+
+/* Net Command values for EPL_V1 message type "SoC" */
+#define EPL_V1_SOC_NET_COMMAND_IDLE   0
+#define EPL_V1_SOC_NET_COMMAND_ACTIVE 1
+
+static const value_string soc_net_command_vals[] = {
+   {EPL_V1_SOC_NET_COMMAND_IDLE,   "Net Command Idle"  },
+   {EPL_V1_SOC_NET_COMMAND_ACTIVE, "Net Command Active"},
+   {0,NULL}
+};
+
+/* Net Command values for EPL_V1 message type "EoC" */
+#define EPL_V1_EOC_NET_COMMAND_IDLE   0
+#define EPL_V1_EOC_NET_COMMAND_ACTIVE 1
+
+static const value_string eoc_net_command_vals[] = {
+   {EPL_V1_EOC_NET_COMMAND_IDLE,   "Net Command Idle"  },
+   {EPL_V1_EOC_NET_COMMAND_ACTIVE, "Net Command Active"},
+   {0,NULL}
+};
 
 
 /* Initialize the protocol and registered fields */
