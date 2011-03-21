6413294daeb95761158aadf8edfa28fda30e29cc
Bill Meier <wmeier@newsguy.com>
2011-03-21 17:34:58 +0000

    Add a comment re a "suspicious" sizeof() reported by Coverity Prevent. (The report seems correct but I'm not altogether sure).
    svn path=/trunk/; revision=36227

--- epan/crypt/crypt-md5.c
+++ epan/crypt/crypt-md5.c
@@ -177,6 +177,8 @@ void md5_finish(md5_state_t *ctx, unsigned char digest[16])
     MD5Transform(ctx->buf, ctx->in);
     byteReverse(ctx->buf, 4);
     memcpy(digest, ctx->buf, 16);
+    /* XXX: Reported as "suspicious" by Coverity Prevent ...         */
+    /*      Was sizeof(*ctx) [i.e., sizeof(md5_state_t)] intended ?  */
     memset(ctx, 0, sizeof(ctx));	/* In case it's sensitive */
 }
 
