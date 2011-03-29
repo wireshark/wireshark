9e5a9baddae0199db81b6a2ebdba5eb5cde26114
Stig Bjørlykke <stig@bjorlykke.org>
2011-03-29 11:09:59 +0000

    Guard against NULL return from find_protocol_by_id().
    svn path=/trunk/; revision=36393

--- epan/proto.c
+++ epan/proto.c
@@ -4478,6 +4478,9 @@ proto_get_protocol_name(const int proto_id)
 	protocol_t *protocol;
 
 	protocol = find_protocol_by_id(proto_id);
+
+	if (protocol == NULL)
+		return NULL;
 	return protocol->name;
 }
 
