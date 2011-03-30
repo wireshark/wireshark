13f6b77d92d5749637ac8d7e7a2d4f1a1ae1310f
Ronnie Sahlberg <ronnie_sahlberg@ozemail.com.au>
2011-03-30 04:36:09 +0000

    remove two pointless const in casts
    coverity 580 and 581
    
    svn path=/trunk/; revision=36407

--- epan/dissectors/packet-ber.c
+++ epan/dissectors/packet-ber.c
@@ -363,7 +363,7 @@ register_ber_oid_syntax(const char *oid, const char *name, const char *syntax)
 {
 
   if(syntax && *syntax)
-    g_hash_table_insert(syntax_table, (const gpointer)g_strdup(oid), (const gpointer)g_strdup(syntax));
+    g_hash_table_insert(syntax_table, (gpointer)g_strdup(oid), (gpointer)g_strdup(syntax));
 
   if(name && *name)
     register_ber_oid_name(oid, name);
