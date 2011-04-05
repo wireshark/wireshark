c13f84b3455db5c177dc88969cef93129adbce38
Stig Bjørlykke <stig@bjorlykke.org>
2011-04-05 15:49:56 +0000

    Compile fix: camel_obj_id -> obj_id.
    svn path=/trunk/; revision=36475

--- epan/dissectors/packet-inap.c
+++ epan/dissectors/packet-inap.c
@@ -1537,7 +1537,7 @@ static const ber_sequence_t ExtensionField_sequence[] = {
 static int
 dissect_inap_ExtensionField(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
 #line 216 "inap.cnf"
-	camel_obj_id = NULL;
+	obj_id = NULL;
 	
 
   offset = dissect_ber_sequence(implicit_tag, actx, tree, tvb, offset,
