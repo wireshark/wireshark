c039f9f8a8bb04d2a8dcd6e2ec1facaee3f1f855
Anders Broman <anders.broman@ericsson.com>
2011-04-06 06:24:19 +0000

    Add a comment
    svn path=/trunk/; revision=36490

--- epan/dissectors/packet-camel.c
+++ epan/dissectors/packet-camel.c
@@ -1979,6 +1979,21 @@ dissect_camel_BearerCapability(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int
 
 static int
 dissect_camel_CalledPartyBCDNumber(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
+/* See 3GPP TS 29.078
+ * and 3GPP TS 24.008, section 10.5.4.7
+ * Indicates the Called Party Number, including service selection information.
+ * Refer to 3GPP TS 24.008 [9] for encoding.
+ * This data type carries only the 'type of number', 'numbering plan
+ * identification' and 'number digit' fields defined in 3GPP TS 24.008 [9];
+ * it does not carry the 'called party BCD number IEI' or 'length of called
+ * party BCD number contents'.
+ * In the context of the DestinationSubscriberNumber field in ConnectSMSArg or 
+ * InitialDPSMSArg, a CalledPartyBCDNumber may also contain an alphanumeric 
+ * character string. In this case, type-of-number '101'B is used, in accordance 
+ * with 3GPP TS 23.040 [6]. The address is coded in accordance with the 
+ * GSM 7-bit default alphabet definition and the SMS packing rules 
+ * as specified in 3GPP TS 23.038 [15] in this case.
+ */
  tvbuff_t	*parameter_tvb; 
  proto_tree *subtree; 
 
