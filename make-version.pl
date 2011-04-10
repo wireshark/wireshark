8b8082442d72cffa8ab5dcf6edae539558ea1bc2
Gerald Combs <gerald@wireshark.org>
2011-04-10 16:42:10 +0000

    Build 1.5.1. Temporarily disable the Babel dissector.
    svn path=/trunk/; revision=36537

--- make-version.pl
+++ make-version.pl
@@ -72,12 +72,12 @@ my %version_pref = (
 	"format"     => "SVN %Y%m%d%H%M%S",
 
 	# Normal development builds
-	"pkg_enable" => 1,
-	"pkg_format" => "-SVN-%#",
+	#"pkg_enable" => 1,
+	#"pkg_format" => "-SVN-%#",
 
 	# Development releases
-	#"pkg_enable" => 0,
-	#"pkg_format" => "",
+	"pkg_enable" => 0,
+	"pkg_format" => "",
 	);
 my $srcdir = ".";
 
