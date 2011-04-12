b5125be726a07018f9063c868546b489ec789c66
Anders Broman <anders.broman@ericsson.com>
2011-04-12 15:44:05 +0000

    Build with zlib-1.2.5 for win32
    zlib for GTK hasd this comment:
    /* LFS conventions have no meaning on Windows. Looking for feature
     * macros like _LARGEFILE64_SOURCE or _FILE_OFFSET_BITS on Windows is
     * wrong. So make sure any such macros misguidedly defined by the
     * user have no effect. Windows has large file support, but the
     * official zlib DLL has not been built to provide the 64-bit offset
     * APIs, sigh.  So we have just patched out the 64-bit offset API
     * from this header file.
     */
    
    svn path=/trunk/; revision=36586

--- tools/win32-setup.sh
+++ tools/win32-setup.sh
@@ -4,7 +4,7 @@
 
 # 32-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2011-04-05"
+export DOWNLOAD_TAG="2011-04-12B"
 export WIRESHARK_TARGET_PLATFORM="win32"
 
 WIN_SETUP=`echo $0 | sed -e s/win32/win/`
