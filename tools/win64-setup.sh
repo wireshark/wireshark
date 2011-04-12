a0e2d8ec0a1171d3da53ebc81354f5c1b878b13d
Anders Broman <anders.broman@ericsson.com>
2011-04-12 21:09:34 +0000

    Use the new label(It will still break on zlib as zlib 1.2.5 isn't used yet).
    svn path=/trunk/; revision=36611

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2011-04-05"
+export DOWNLOAD_TAG="2011-04-12"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
