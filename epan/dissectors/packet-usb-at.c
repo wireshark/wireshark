79940000f2aea33e5e72df0cfa5f4bd78e1da0d9
Chris Maynard <Christopher.Maynard@GTECH.COM>
2011-05-10 17:06:20 +0000

    Add subversion Id and Wireshark copyright.
    svn path=/trunk/; revision=37046

--- epan/dissectors/packet-usb-at.c
+++ epan/dissectors/packet-usb-at.c
@@ -3,6 +3,12 @@
  *
  * Copyright 2011, Tyson Key <tyson.key@gmail.com>
  *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
