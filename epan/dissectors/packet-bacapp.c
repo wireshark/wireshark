b968cc5c0bb1c9d9f0b560299eca62f4471e764b
Chris Maynard <Christopher.Maynard@GTECH.COM>
2011-07-19 02:47:12 +0000

    From Michael Mann via bug 5769:  In fDevice_Instance(), add field in proper big-endian byte order.
    Reference: https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=5769
    
    svn path=/trunk/; revision=38105

--- epan/dissectors/packet-bacapp.c
+++ epan/dissectors/packet-bacapp.c
@@ -4555,7 +4555,7 @@ fDevice_Instance (tvbuff_t *tvb, proto_tree *tree, guint offset, int hf)
 	proto_tree *subtree;
 
 	tag_len = fTagHeader (tvb, offset, &tag_no, &tag_info, &lvt);
-	ti = proto_tree_add_item(tree, hf, tvb, offset+tag_len, lvt, TRUE);
+	ti = proto_tree_add_item(tree, hf, tvb, offset+tag_len, lvt, ENC_BIG_ENDIAN);
 	subtree = proto_item_add_subtree(ti, ett_bacapp_tag);
 	fTagHeaderTree (tvb, subtree, offset, &tag_no, &tag_info, &lvt);
 
