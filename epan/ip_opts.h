eefbff4f0a58b4a48ef4d8d487403a33b6d81ffc
Bill Meier <wmeier@newsguy.com>
2011-07-24 20:50:00 +0000

    (Trivial) Whitespace changes and reformatting ...
    svn path=/trunk/; revision=38191

--- epan/ip_opts.h
+++ epan/ip_opts.h
@@ -30,33 +30,38 @@
  */
 
 typedef enum {
-  NO_LENGTH,		/**< option has no data, hence no length */
-  FIXED_LENGTH,		/**< option always has the same length */
-  VARIABLE_LENGTH	/**< option is variable-length - optlen is minimum */
+  NO_LENGTH,                        /**< option has no data, hence no length */
+  FIXED_LENGTH,                     /**< option always has the same length */
+  VARIABLE_LENGTH                   /**< option is variable-length - optlen is minimum */
 } opt_len_type;
 
 /** Member of table of IP or TCP options. */
 typedef struct ip_tcp_opt {
-  int   optcode;			/**< code for option */
-  const char  *name;		/**< name of option */
-  int   *subtree_index;		/**< pointer to subtree index for option */
-  opt_len_type len_type;	/**< type of option length field */
-  int	optlen;				/**< value length should be (minimum if VARIABLE) */
-  void	(*dissect)(const struct ip_tcp_opt *, tvbuff_t *, int, guint,
-  		   packet_info *, proto_tree *);
-			/**< routine to dissect option */
+  int   optcode;                    /**< code for option */
+  const char  *name;                /**< name of option */
+  int   *subtree_index;             /**< pointer to subtree index for option */
+  opt_len_type len_type;            /**< type of option length field */
+  int   optlen;                     /**< value length should be (minimum if VARIABLE) */
+  void  (*dissect)(const struct ip_tcp_opt *,
+                   tvbuff_t *,
+                   int,
+                   guint,
+                   packet_info *,
+                   proto_tree *);   /**< routine to dissect option */
 } ip_tcp_opt;
 
 /** Routine to dissect options that work like IPv4 options, where the
    length field in the option, if present, includes the type and
    length bytes. */
 extern void dissect_ip_tcp_options(tvbuff_t *, int, guint,
-    const ip_tcp_opt *, int, int, packet_info *, proto_tree *, proto_item *);
+                                   const ip_tcp_opt *, int, int,
+                                   packet_info *, proto_tree *, proto_item *);
 
 /** Routine to dissect options that work like IPv6 options, where the
    length field in the option, if present, includes only the data, not
    the type and length bytes. */
 extern void dissect_ipv6_options(tvbuff_t *, int, guint,
-    const ip_tcp_opt *, int, int, packet_info *, proto_tree *);
+                                 const ip_tcp_opt *, int, int,
+                                 packet_info *, proto_tree *);
 
 #endif
