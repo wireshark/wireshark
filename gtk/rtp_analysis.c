1fe6e2b19b8b1dd6f78c4d1adf219fdfc2120230
Bill Meier <wmeier@newsguy.com>
2011-08-09 23:45:54 +0000

    Remove duplicate line-of-code.
    svn path=/trunk/; revision=38439

--- gtk/rtp_analysis.c
+++ gtk/rtp_analysis.c
@@ -3670,7 +3670,6 @@ static void rtp_analysis_cb(GtkWidget *w _U_, gpointer data _U_)
 	epan_dissect_init(&edt, TRUE, FALSE);
 	epan_dissect_prime_dfilter(&edt, sfcode);
 	epan_dissect_run(&edt, &cf->pseudo_header, cf->pd, fdata, NULL);
-	frame_matched = dfilter_apply_edt(sfcode, &edt);
 
 	/* if it is not an rtp frame, show the rtpstream dialog */
 	frame_matched = dfilter_apply_edt(sfcode, &edt);
