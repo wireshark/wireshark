d4951b8e7ec0dd0e945d0732bb3d62954d0a0904
Anders Broman <anders.broman@ericsson.com>
2011-08-19 04:36:40 +0000

    GTK 3.0 "Comment out" GTK 2 code for now. Functionality needs to be restored on GTK 3.
    svn path=/trunk/; revision=38614

--- gtk/rtp_analysis.c
+++ gtk/rtp_analysis.c
@@ -1605,12 +1605,15 @@ static void create_filter_box(dialog_graph_graph_t *dgg, GtkWidget *box, int num
 	gtk_widget_show(label);
 	gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
 
+#if GTK_CHECK_VERSION(3,0,0)
+	/* Fix me: Add the GTK 3.0 equivalents */
+#else	
 	gtk_widget_modify_fg(label, GTK_STATE_NORMAL, &dgg->color);
 	gtk_widget_modify_fg(label, GTK_STATE_ACTIVE, &dgg->color);
 	gtk_widget_modify_fg(label, GTK_STATE_PRELIGHT, &dgg->color);
 	gtk_widget_modify_fg(label, GTK_STATE_SELECTED, &dgg->color);
 	gtk_widget_modify_fg(label, GTK_STATE_INSENSITIVE, &dgg->color);
-
+#endif
 	return;
 }
 
