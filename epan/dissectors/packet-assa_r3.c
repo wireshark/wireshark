63e83b1303472f42860d690ffa29409f24a6edfe
Bill Meier <wmeier@newsguy.com>
2011-10-23 20:50:48 +0000

    Fix a proto_tree_add_item() encoding arg.
    svn path=/trunk/; revision=39527

--- epan/dissectors/packet-assa_r3.c
+++ epan/dissectors/packet-assa_r3.c
@@ -3839,7 +3839,7 @@ static void dissect_r3_upstreammfgfield_adcs (tvbuff_t *tvb, guint32 start_offse
 
   for (i = 0; i < tvb_reported_length (tvb); i++)
   {
-    proto_item *item = proto_tree_add_item (tree, hf_r3_adc [i], tvb, start_offset + i, 1, TRUE);
+    proto_item *item = proto_tree_add_item (tree, hf_r3_adc [i], tvb, start_offset + i, 1, ENC_LITTLE_ENDIAN);
     proto_item_append_text (item, " (%.2f Volts)", (float) tvb_get_guint8 (tvb, start_offset + i) * 0.04154);
   }
 }
