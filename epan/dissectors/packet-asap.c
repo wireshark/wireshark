cbb32e6b004bd49a7df9422fcdcd82da2b0c5aa8
Bill Meier <wmeier@newsguy.com>
2011-11-12 19:27:07 +0000

    Fix 2 typos
    svn path=/trunk/; revision=39806

--- epan/dissectors/packet-asap.c
+++ epan/dissectors/packet-asap.c
@@ -804,7 +804,7 @@ dissect_asap_message(tvbuff_t *message_tvb, packet_info *pinfo, proto_tree *asap
 
 
   type = tvb_get_guint8(message_tvb, MESSAGE_TYPE_OFFSET);
-  /* pinfo is NULL only if dissect_asap_message is called via dissect_error cause */
+  /* pinfo is NULL only if dissect_asap_message is called via dissect_error_cause */
   if (pinfo)
    col_add_fstr(pinfo->cinfo, COL_INFO, "%s ", val_to_str(type, message_type_values, "Unknown ASAP type"));
 
@@ -835,7 +835,7 @@ dissect_asap(tvbuff_t *message_tvb, packet_info *pinfo, proto_tree *tree)
   proto_item *asap_item;
   proto_tree *asap_tree;
 
-  /* pinfo is NULL only if dissect_asap is called from dissect_error cause */
+  /* pinfo is NULL only if dissect_asap is called from dissect_error_cause */
   if (pinfo)
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "ASAP");
 
