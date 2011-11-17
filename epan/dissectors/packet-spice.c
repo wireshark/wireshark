e550884d5e9a828066a741e17939b39ba6456a7f
Chris Maynard <Christopher.Maynard@GTECH.COM>
2011-11-17 16:10:02 +0000

    distribute -> redistribute.
    svn path=/trunk/; revision=39903

--- epan/dissectors/packet-spice.c
+++ epan/dissectors/packet-spice.c
@@ -8,7 +8,7 @@
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
- * This program is free software; you can distribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
