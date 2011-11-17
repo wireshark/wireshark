0ef4ff0ce7ed323da0739a3924fee15a492f396c
Chris Maynard <Christopher.Maynard@GTECH.COM>
2011-11-17 16:21:54 +0000

    Add missing copyright and license.
    svn path=/trunk/; revision=39904

--- epan/dissectors/packet-x11-keysym.h
+++ epan/dissectors/packet-x11-keysym.h
@@ -2,10 +2,27 @@
  *
  * $Id$
  *
- * Put there so as to make packet-x11.c lighter. See packet-x11.c
+ * Put here so as to make packet-x11.c lighter. See packet-x11.c
  *
  * This file is also used by packet-vnc.c
  *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef __PACKET_X11_KEYSYM_H__
