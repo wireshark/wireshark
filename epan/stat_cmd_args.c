f6ffb03ccbb2202d4a789aef955d4401692e546d
Gerald Combs <gerald@wireshark.org>
2012-01-12 19:16:15 +0000

    CMakeLists.txt: Make sure TSHARK_TAP_SRC is defined before register_tap_files so that the "-z" flag works for TShark.
    epan/stat_cmd_args.c: Fixup whitespace.
    
    svn path=/trunk/; revision=40443

--- epan/stat_cmd_args.c
+++ epan/stat_cmd_args.c
@@ -6,17 +6,17 @@
  * Wireshark - Network traffic analyzer
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
@@ -72,7 +72,7 @@ register_stat_cmd_arg(const char *cmd, void (*func)(const char*, void*),void* us
 	newsca=g_malloc(sizeof(stat_cmd_arg));
 	newsca->cmd=cmd;
 	newsca->func=func;
-    newsca->userdata=userdata;
+	newsca->userdata=userdata;
 	stat_cmd_arg_list=g_slist_insert_sorted(stat_cmd_arg_list, newsca,
 	    sort_by_name);
 }
