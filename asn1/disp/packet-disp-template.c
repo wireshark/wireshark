0dde64beaeaea3f185c091f85bdc18086f006fdd
Alexis La Goutte <alexis.lagoutte@gmail.com>
2012-01-13 16:30:39 +0000

    Fix some Dead Store (Dead assignement/Dead increment) Warning found by Clang
    svn path=/trunk/; revision=40467

--- asn1/disp/packet-disp-template.c
+++ asn1/disp/packet-disp-template.c
@@ -185,7 +185,6 @@ dissect_disp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree)
 	    offset=(*disp_dissector)(FALSE, tvb, offset, &asn1_ctx, tree, -1);
 	    if(offset == old_offset){
 	      proto_tree_add_text(tree, tvb, offset, -1,"Internal error, zero-byte DISP PDU");
-	      offset = tvb_length(tvb);
 	      break;
 	    }
 	  }
