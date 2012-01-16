6c8f38d16b6fe00cbb0e5a4b3630db1c75c7372e
Anders Broman <anders.broman@ericsson.com>
2012-01-16 21:41:54 +0000

    From Pascal Quantin: Upgrade LTE RRC dissector to v10.4.0
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6742
    
    Add missing file
    
    svn path=/trunk/; revision=40551

---
+++ asn1/lpp/packet-lpp-template.h
@@ -0,0 +1,34 @@
+/* packet-lpp.h
+ * Routines for 3GPP LTE Positioning Protocol (LLP) packet dissection
+ * Copyright 2011, Pascal Quantin <pascal.quantin@gmail.com>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Ref 3GPP TS 36.355 version 9.7.0 Release 9
+ * http://www.3gpp.org
+ */
+
+#ifndef PACKET_LPP_H
+#define PACKET_LPP_H
+
+#include "packet-lpp-exp.h"
+
+#endif  /* PACKET_LPP_H */
