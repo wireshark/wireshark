6d213e71bd4282dc4158bd588dbd9603845010f3
Chris Maynard <Christopher.Maynard@GTECH.COM>
2012-01-27 03:41:20 +0000

    Since we know the version of WinPcap we're compiling against, go ahead and display it instead of displaying "version unknown".
    svn path=/trunk/; revision=40740

--- capture-wpcap.c
+++ capture-wpcap.c
@@ -903,7 +903,7 @@ cant_get_if_list_error_message(const char *err_str)
 void
 get_compiled_pcap_version(GString *str)
 {
-	g_string_append(str, "with WinPcap (version unknown)");
+	g_string_append(str, "with WinPcap (" PCAP_VERSION ")");
 }
 
 /*
