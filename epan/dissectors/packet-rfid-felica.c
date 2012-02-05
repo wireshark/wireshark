b7922c825ec3796a001182a8091e37da8a523cc8
Anders Broman <anders.broman@ericsson.com>
2012-02-05 12:47:43 +0000

    Try to get rid of non-ASCII characters.
    svn path=/trunk/; revision=40859

--- epan/dissectors/packet-rfid-felica.c
+++ epan/dissectors/packet-rfid-felica.c
@@ -1,35 +1,35 @@
-﻿/* packet-rfid-felica.c
-* Dissector for the Sony FeliCa Protocol
-*
-* References:
-* http://www.sony.net/Products/felica/business/tech-support/data/fl_usmnl_1.2.pdf
-* http://www.sony.net/Products/felica/business/tech-support/data/format_sequence_guidelines_1.1.pdf
-* http://code.google.com/u/101410204121169118393/updates
-* https://github.com/codebutler/farebot/wiki/Suica
-* 
-* Copyright 2012, Tyson Key <tyson.key@gmail.com>
-*
-* $Id$
-*
-* Wireshark - Network traffic analyzer
-* By Gerald Combs <gerald@wireshark.org>
-* Copyright 1998 Gerald Combs
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* as published by the Free Software Foundation; either version 2
-* of the License, or (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*
-*/
+/*
+ * Dissector for the Sony FeliCa Protocol
+ *
+ * References:
+ * http://www.sony.net/Products/felica/business/tech-support/data/fl_usmnl_1.2.pdf
+ * http://www.sony.net/Products/felica/business/tech-support/data/format_sequence_guidelines_1.1.pdf
+ * http://code.google.com/u/101410204121169118393/updates
+ * https://github.com/codebutler/farebot/wiki/Suica
+ * 
+ * Copyright 2012, Tyson Key <tyson.key@gmail.com>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
