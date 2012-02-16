eb35d198e4e6aa7d4e800c1fd74515dabb1df8b9
Guy Harris <guy@alum.mit.edu>
2012-02-16 06:39:44 +0000

    Support Section Header Block options of arbitrary size - change backported from the trunk.
    Should fix the immediate problem in bug 6846; we also need to make sure
    (in all branches) that, as long as the file begins with an SHB with a
    valid byte order field, we recognize the file as a pcap-NG file (so that
    if there really *is* a problem with the SHB, the error reported to the
    user is an error in a pcap-NG file's SHB, *not* an "unknown file type"
    error).
    
    svn path=/trunk-1.6/; revision=41037

--- wiretap/pcapng.c
+++ wiretap/pcapng.c
@@ -399,10 +399,10 @@ pcapng_read_section_header_block(FILE_T fh, gboolean first_block,
 {
 	int	bytes_read;
 	int	block_read;
-	int to_read;
+	int to_read, opt_cont_buf_len;
 	pcapng_section_header_block_t shb;
 	pcapng_option_header_t oh;
-	char option_content[100]; /* XXX - size might need to be increased, if we see longer options */
+	char *option_content = NULL; /* Allocate as large as the options block */
 
 
 	/* read block content */
@@ -495,9 +495,14 @@ pcapng_read_section_header_block(FILE_T fh, gboolean first_block,
         - (int)sizeof(pcapng_block_header_t)
         - (int)sizeof (pcapng_section_header_block_t)
         - (int)sizeof(bh->block_total_length);
+	/* Allocate enough memory to hold all options */
+	opt_cont_buf_len = to_read;
+	option_content = g_malloc(opt_cont_buf_len);
+	pcapng_debug1("pcapng_read_section_header_block: Options %u bytes", to_read);
 	while(to_read > 0) {
 		/* read option */
-		bytes_read = pcapng_read_option(fh, pn, &oh, option_content, sizeof(option_content), err, err_info);
+		pcapng_debug1("pcapng_read_section_header_block: Options %u bytes remaining", to_read);
+		bytes_read = pcapng_read_option(fh, pn, &oh, option_content, opt_cont_buf_len, err, err_info);
 		if (bytes_read <= 0) {
 			pcapng_debug0("pcapng_read_section_header_block: failed to read option");
 			return bytes_read;
@@ -515,35 +520,35 @@ pcapng_read_section_header_block(FILE_T fh, gboolean first_block,
 			to_read = 0;
 			break;
 		    case(1): /* opt_comment */
-			if(oh.option_length > 0 && oh.option_length < sizeof(option_content)) {
-				wblock->data.section.opt_comment = g_strndup(option_content, sizeof(option_content));
+			if(oh.option_length > 0 && oh.option_length < opt_cont_buf_len) {
+				wblock->data.section.opt_comment = g_strndup(option_content, oh.option_length);
 				pcapng_debug1("pcapng_read_section_header_block: opt_comment %s", wblock->data.section.opt_comment);
 			} else {
 				pcapng_debug1("pcapng_read_section_header_block: opt_comment length %u seems strange", oh.option_length);
 			}
 			break;
 		    case(2): /* shb_hardware */
-			if(oh.option_length > 0 && oh.option_length < sizeof(option_content)) {
-				wblock->data.section.shb_hardware = g_strndup(option_content, sizeof(option_content));
+			if(oh.option_length > 0 && oh.option_length < opt_cont_buf_len) {
+				wblock->data.section.shb_hardware = g_strndup(option_content, oh.option_length);
 				pcapng_debug1("pcapng_read_section_header_block: shb_hardware %s", wblock->data.section.shb_hardware);
 			} else {
 				pcapng_debug1("pcapng_read_section_header_block: shb_hardware length %u seems strange", oh.option_length);
 			}
 			break;
 		    case(3): /* shb_os */
-			if(oh.option_length > 0 && oh.option_length < sizeof(option_content)) {
-				wblock->data.section.shb_os = g_strndup(option_content, sizeof(option_content));
+			if(oh.option_length > 0 && oh.option_length < opt_cont_buf_len) {
+				wblock->data.section.shb_os = g_strndup(option_content, oh.option_length);
 				pcapng_debug1("pcapng_read_section_header_block: shb_os %s", wblock->data.section.shb_os);
 			} else {
-				pcapng_debug1("pcapng_read_section_header_block: shb_os length %u seems strange", oh.option_length);
+				pcapng_debug2("pcapng_read_section_header_block: shb_os length %u seems strange, opt buffsize %u", oh.option_length,to_read);
 			}
 			break;
-		    case(4): /* shb_userappl */
-			if(oh.option_length > 0 && oh.option_length < sizeof(option_content)) {
-				wblock->data.section.shb_user_appl = g_strndup(option_content, sizeof(option_content));
-				pcapng_debug1("pcapng_read_section_header_block: shb_userappl %s", wblock->data.section.shb_user_appl);
+		    case(4): /* shb_user_appl */
+			if(oh.option_length > 0 && oh.option_length < opt_cont_buf_len) {
+				wblock->data.section.shb_user_appl = g_strndup(option_content, oh.option_length);
+				pcapng_debug1("pcapng_read_section_header_block: shb_user_appl %s", wblock->data.section.shb_user_appl);
 			} else {
-				pcapng_debug1("pcapng_read_section_header_block: shb_userappl length %u seems strange", oh.option_length);
+				pcapng_debug1("pcapng_read_section_header_block: shb_user_appl length %u seems strange", oh.option_length);
 			}
 			break;
 		    default:
@@ -551,6 +556,7 @@ pcapng_read_section_header_block(FILE_T fh, gboolean first_block,
 				      oh.option_code, oh.option_length);
 		}
 	}
+	g_free(option_content);
 
 	if (pn->interface_data != NULL) {
 		g_array_free(pn->interface_data, TRUE);
