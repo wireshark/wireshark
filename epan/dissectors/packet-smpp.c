wireshark/wireshark

commit 98d51466e54a778506addee39bf7f005e65f68ec

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6712 :
Since SMPP knows the time zone of its (absolute) times and since we don't
have access to a mktime() routine that doesn't take into account the local
time zone (and since I don't think repeatedly setting the TZ environment
variable is a healthy choice):

1) subtract the 'timezone' (or '_timezone' on Windows) back out after calling
   mktime()
2) then adjust the time to take into the protocol-specified time zone
3) and (finally) display the time in UTC (since we don't have the
   infrastructure to display it in the protocol-specified time zone).

(I *think* (1) is portable: POSIX says that variable should exist...)

svn path=/trunk/; revision=40742

epan/dissectors/packet-smpp.c
@@ -1167,6 +1167,14 @@ smpp_mktime(const char *datestr, time_t *secs, int *nsecs)
 
     if (relative == FALSE) {
         *secs = mktime(&r_time);
+	/* Subtract out the timezone information since we will adjust for
+	 * timezone below and then display in UTC.
+	 */
+#ifdef _WIN32
+	*secs -= _timezone;
+#else
+	*secs -= timezone;
+#endif
         *nsecs = (datestr[12] - '0') * 100000000;
         t_diff = (10 * (datestr[13] - '0') + (datestr[14] - '0')) * 900;
         if (datestr[15] == '+')
@@ -2865,7 +2873,7 @@ proto_register_smpp(void)
         },
         {   &hf_smpp_schedule_delivery_time,
             {   "Scheduled delivery time", "smpp.schedule_delivery_time",
-                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x00,
+                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC, NULL, 0x00,
                 "Scheduled time for delivery of short message.",
                 HFILL
             }
@@ -2879,7 +2887,7 @@ proto_register_smpp(void)
         },
         {   &hf_smpp_validity_period,
             {   "Validity period", "smpp.validity_period",
-                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x00,
+                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC, NULL, 0x00,
                 "Validity period of this message.",
                 HFILL
             }
@@ -2977,7 +2985,7 @@ proto_register_smpp(void)
         },
         {   &hf_smpp_final_date,
             {   "Final date", "smpp.final_date",
-                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x00,
+                FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC, NULL, 0x00,
                 "Date-time when the queried message reached a final state.",
                 HFILL
             }
@@ -3619,7 +3627,7 @@ proto_register_smpp(void)
         },
         {       &hf_smpp_broadcast_end_time,
                 {       "Broadcast Message - End Time", "smpp.broadcast_end_time",
-                        FT_ABSOLUTE_TIME, ABSOLUTE_TIME_LOCAL, NULL, 0x00,
+                        FT_ABSOLUTE_TIME, ABSOLUTE_TIME_UTC, NULL, 0x00,
                         "Cell Broadcast Message - Date and time at which MC set the state of the message to terminated", HFILL
                 }
         },

commit 1906e9b50d5b13a1e4319ef97ec52cfac71b20cd

(Re)fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6712 :
The change made in r40742 turned out to not be portable (FreeBSD doesn't have
the timezone global variable), so use another method to determine the current
timezone.

Also fix a bug introduced by r40742's change to display this timestamp in UTC:
if the reported (by the message) time zone has a negative offset to UTC, shift
it forward (not backward) to get UTC (and the opposite for positive offsets).

svn path=/trunk/; revision=41044

epan/dissectors/packet-smpp.c
@@ -1179,20 +1179,35 @@ smpp_mktime(const char *datestr, time_t *secs, int *nsecs)
     r_time.tm_isdst = -1;
 
     if (relative == FALSE) {
+	struct tm *gm, *local_time;
+	int gm_hour, gm_min;
+	time_t current_time;
+
         *secs = mktime(&r_time);
+
 	/* Subtract out the timezone information since we will adjust for
-	 * timezone below and then display in UTC.
+	 * the presented time's timezone below and then display in UTC.
+	 *
+	 * To do that, first determine the current timezone's offset to UTC.
 	 */
-#ifdef _WIN32
-	*secs -= _timezone;
-#else
-	*secs -= timezone;
-#endif
+	current_time = time(NULL);
+	gm = gmtime(&current_time);
+	gm_hour = gm->tm_hour;
+	gm_min = gm->tm_min;
+	local_time = localtime(&current_time);
+	/* Then subtract out that difference (whether the difference is
+	 * measured in hours, minutes, or both).
+	 */
+	*secs -= 3600*(gm_hour - local_time->tm_hour);
+	*secs -= 60*(gm_min - local_time->tm_min);
+
         *nsecs = (datestr[12] - '0') * 100000000;
         t_diff = (10 * (datestr[13] - '0') + (datestr[14] - '0')) * 900;
-        if (datestr[15] == '+')
+        if (datestr[15] == '-')
+	    /* Represented time is behind UTC, shift it forward to UTC */
             *secs += t_diff;
-        else if (datestr[15] == '-')
+        else if (datestr[15] == '+')
+	    /* Represented time is ahead of UTC, shift it backward to UTC */
             *secs -= t_diff;
     } else {
         *secs = r_time.tm_sec + 60 *