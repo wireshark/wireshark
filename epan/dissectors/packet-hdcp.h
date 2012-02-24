c45a9415971bf0be95a0bcd602bc4717c707687d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2012-02-24 08:18:30 +0000

    From Martin Kaiser via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6838 Support HDCP version 1 over I2c
    the attached patch adds support for HDCP version 1. This is the authentication that runs between your DVD/Bluray player and your TV when they're connected via an HDMI cable.
    
    svn path=/trunk/; revision=41173

---
+++ epan/dissectors/packet-hdcp.h
@@ -0,0 +1,47 @@
+/* packet-hdcp.h
+ * Routines for HDCP dissection
+ * Copyright 2011-2012, Martin Kaiser <martin@kaiser.cx>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef __PACKET_HDCP_H__
+#define __PACKET_HDCP_H__
+
+#include <glib.h>
+#include <epan/packet.h>
+
+gboolean sub_check_hdcp(packet_info *pinfo _U_);
+
+#endif
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
