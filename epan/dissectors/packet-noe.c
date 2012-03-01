94d95e3629c1d07be94271e9d1561e5325e0e020
Anders Broman <anders.broman@ericsson.com>
2012-03-01 17:19:57 +0000

    From Michael.
    svn path=/trunk/; revision=41276

--- epan/dissectors/packet-noe.c
+++ epan/dissectors/packet-noe.c
@@ -776,54 +776,54 @@ static guint64 decode_utf8(guint64 utf8)
 {
 	static guint64 unicode;
 
-	if(utf8 <= 0xFF)
+	if(utf8 <= G_GUINT64_CONSTANT(0xFF))
 	{
 		unicode =
-			utf8 & 0x7F;
+			utf8 & G_GUINT64_CONSTANT(0x7F);
 	}
-	else if(utf8 <= 0xFFFF)
+	else if(utf8 <= G_GUINT64_CONSTANT(0xFFFF))
 	{
 		unicode =
-			((utf8 & 0x1F00) >> 2) +
-			(utf8 & 0x3F);
+			((utf8 & G_GUINT64_CONSTANT(0x1F00) >> 2) +
+			(utf8 & G_GUINT64_CONSTANT(0x3F)));
 	}
-	else if(utf8 <= 0xFFFFFF)
+	else if(utf8 <= G_GUINT64_CONSTANT(0xFFFFFF))
 	{
 		unicode =
-			((utf8 & 0x0F0000) >> 4) +
-			((utf8 & 0x3F00) >> 2) +
-			(utf8 & 0x3F);
+			((utf8 & G_GUINT64_CONSTANT(0x0F0000)) >> 4) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F00)) >> 2) +
+			(utf8 & G_GUINT64_CONSTANT(0x3F));
 	}
-	else if(utf8 <= 0xFFFFFFFF)
+	else if(utf8 <= G_GUINT64_CONSTANT(0xFFFFFFFF))
 	{
 		unicode =
-			((utf8 & 0x07000000) >> 6) +
-			((utf8 & 0x3F0000) >> 4) +
-			((utf8 & 0x3F00) >> 2) +
-			(utf8 & 0x3F);
+			((utf8 & G_GUINT64_CONSTANT(0x07000000)) >> 6) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F0000)) >> 4) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F00)) >> 2) +
+			(utf8 & G_GUINT64_CONSTANT(0x3F));
 	}
-	else if(utf8 <= G_GINT64_CONSTANT(0xFFFFFFFFFF))
+	else if(utf8 <= G_GUINT64_CONSTANT(0xFFFFFFFFFF))
 	{
 		unicode =
-			((utf8 & G_GINT64_CONSTANT(0x0300000000)) >> 8) +
-			((utf8 & 0x3F000000) >> 6) +
-			((utf8 & 0x3F0000) >> 4) +
-			((utf8 & 0x3F00) >> 2) +
-			(utf8 & 0x3F);
+			((utf8 & G_GUINT64_CONSTANT(0x0300000000)) >> 8) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F000000)) >> 6) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F0000)) >> 4) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F00)) >> 2) +
+			(utf8 & G_GUINT64_CONSTANT(0x3F));
 	}
-	else if(utf8 <= G_GINT64_CONSTANT(0xFFFFFFFFFFFF))
+	else if(utf8 <= G_GUINT64_CONSTANT(0xFFFFFFFFFFFF))
 	{
 		unicode =
-			((utf8 & G_GINT64_CONSTANT(0x010000000000)) >> 10) +
-			((utf8 & G_GINT64_CONSTANT(0x3F00000000)) >> 8) +
-			((utf8 & 0x3F000000) >> 6) +
-			((utf8 & 0x3F0000) >> 4) +
-			((utf8 & 0x3F00) >> 2) +
-			(utf8 & 0x3F);
+			((utf8 & G_GUINT64_CONSTANT(0x010000000000)) >> 10) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F00000000)) >> 8) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F000000)) >> 6) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F0000)) >> 4) +
+			((utf8 & G_GUINT64_CONSTANT(0x3F00)) >> 2) +
+			(utf8 & G_GUINT64_CONSTANT(0x3F));
 	}
 	else
 	{
-		unicode = 0;
+		unicode = G_GUINT64_CONSTANT(0);
 	}
 	return unicode;
 } 
