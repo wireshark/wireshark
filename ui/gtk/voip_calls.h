b2515852f9c0ff6f8265ed3f56715ffa7574602a
Anders Broman <anders.broman@ericsson.com>
2012-03-12 12:18:02 +0000

    Add doxygen tags.
    svn path=/trunk/; revision=41498

--- ui/gtk/voip_calls.h
+++ ui/gtk/voip_calls.h
@@ -69,7 +69,7 @@ typedef enum _hash_indexes {
 
 extern const char *voip_protocol_name[];
 
-/* defines specific SIP data */
+/** defines specific SIP data */
 
 typedef enum _sip_call_state {
 	SIP_INVITE_SENT,
@@ -83,7 +83,7 @@ typedef struct _sip_calls_info {
 	sip_call_state sip_state;
 } sip_calls_info_t;
 
-/* defines specific ISUP data */
+/** defines specific ISUP data */
 typedef struct _isup_calls_info {
 	guint16			cic;
 	guint32			opc, dpc;
@@ -96,44 +96,44 @@ typedef struct _h245_address {
 	guint16 h245_port;
 } h245_address_t;
 
-/* defines specific H323 data */
+/** defines specific H323 data */
 typedef struct _h323_calls_info {
 	e_guid_t *guid;	/* Call ID to identify a H225 */
-	GList*  h245_list;   /* list of H245 Address and ports for tunneling off calls*/
-	address h225SetupAddr; /* we use the SETUP H225 IP to determine if packets are forward or reverse */					
+	GList*  h245_list;				/**< list of H245 Address and ports for tunneling off calls*/
+	address h225SetupAddr;			/**< we use the SETUP H225 IP to determine if packets are forward or reverse */					
 	gboolean is_h245;
-	gboolean is_faststart_Setup;	/* if faststart field is included in Setup*/
-	gboolean is_faststart_Proc;		/* if faststart field is included in Proce, Alerting, Progress or Connect*/
+	gboolean is_faststart_Setup;	/**< if faststart field is included in Setup*/
+	gboolean is_faststart_Proc;		/**< if faststart field is included in Proce, Alerting, Progress or Connect*/
 	gboolean is_h245Tunneling;
 	gint32 q931_crv;
 	gint32 q931_crv2;
 	guint requestSeqNum;
 } h323_calls_info_t;
 
-/* defines specific MGCP data */
+/**< defines specific MGCP data */
 typedef struct _mgcp_calls_info {
 	gchar *endpointId;
-	gboolean fromEndpoint; /* true if the call was originated from the Endpoint, false for calls from MGC */
+	gboolean fromEndpoint; /**< true if the call was originated from the Endpoint, false for calls from MGC */
 } mgcp_calls_info_t;
 
-/* defines specific ACTRACE ISDN data */
+/** defines specific ACTRACE ISDN data */
 typedef struct _actrace_isdn_calls_info {
 	gint32 crv;
 	int trunk;
 } actrace_isdn_calls_info_t;
 
-/* defines specific ACTRACE CAS data */
+/** defines specific ACTRACE CAS data */
 typedef struct _actrace_cas_calls_info {
 	gint32 bchannel;
 	int trunk;
 } actrace_cas_calls_info_t;
 
-/* defines specific SKINNY data */
+/** defines specific SKINNY data */
 typedef struct _skinny_calls_info {
 	guint32 callId;
 } skinny_calls_info_t;
 
-/* defines a voip call */
+/** defines a voip call */
 typedef struct _voip_calls_info {
 	voip_call_state call_state;
 	voip_call_active_state call_active_state;
@@ -148,23 +148,24 @@ typedef struct _voip_calls_info {
 	gchar *protocol_name;
 	gchar *call_comment;
 	guint16 call_num;
-	/* The frame_data struct holds the frame number and timing information needed. */
+	/**> The frame_data struct holds the frame number and timing information needed. */
 	frame_data *start_fd; 
 	frame_data *stop_fd;
 	gboolean selected;
 
 } voip_calls_info_t;
 
-/* structure that holds the information about all detected calls */
+/** 
+ * structure that holds the information about all detected calls */
 /* struct holding all information of the tap */
 
 typedef struct _voip_calls_tapinfo {
-	int     ncalls;       /* number of call */
-	GList*  callsinfo_list;   /* list with all calls */
-	GHashTable* callsinfo_hashtable[1]; /* array of hashes per voip protocol; currently only the one for SIP is used */
-	int     npackets;       /* total number of packets of all calls */
-	voip_calls_info_t* filter_calls_fwd;  /* used as filter in some tap modes */
-	guint32 launch_count;   /* number of times the tap has been run */
+	int     ncalls;							/**< number of call */
+	GList*  callsinfo_list;					/**< list with all calls */
+	GHashTable* callsinfo_hashtable[1];		/**< array of hashes per voip protocol; currently only the one for SIP is used */
+	int     npackets;						/**< total number of packets of all calls */
+	voip_calls_info_t* filter_calls_fwd;	/**< used as filter in some tap modes */
+	guint32 launch_count;					/**< number of times the tap has been run */
 	int start_packets;
 	int completed_calls;
 	int rejected_calls;
@@ -199,10 +200,10 @@ typedef struct _voip_calls_tapinfo {
 
 
 /* structure that holds the information about all RTP streams associated with the calls */
-/* struct holding all information of the RTP tap */
+/** struct holding all information of the RTP tap */
 typedef struct _voip_rtp_tapinfo {
-	int     nstreams;       /* number of rtp streams */
-	GList*  list;			/* list with the rtp streams */
+	int     nstreams;       /**< number of rtp streams */
+	GList*  list;			/**< list with the rtp streams */
 	int rtp_dummy;
 	int rtp_event_dummy;
 } voip_rtp_tapinfo_t;
@@ -210,13 +211,13 @@ typedef struct _voip_rtp_tapinfo {
 /****************************************************************************/
 /* INTERFACE */
 
-/*
-* Registers the voip_calls tap listeners (if not already done).
-* From that point on, the calls list will be updated with every redissection.
-* This function is also the entry point for the initialization routine of the tap system.
-* So whenever voip_calls.c is added to the list of WIRESHARK_TAP_SRCs, the tap will be registered on startup.
-* If not, it will be registered on demand by the voip_calls functions that need it.
-*/
+/**
+ * Registers the voip_calls tap listeners (if not already done).
+ * From that point on, the calls list will be updated with every redissection.
+ * This function is also the entry point for the initialization routine of the tap system.
+ * So whenever voip_calls.c is added to the list of WIRESHARK_TAP_SRCs, the tap will be registered on startup.
+ * If not, it will be registered on demand by the voip_calls functions that need it.
+ */
 void sip_calls_init_tap(void);
 void isup_calls_init_tap(void);
 void mtp3_calls_init_tap(void);
@@ -236,10 +237,10 @@ void skinny_calls_init_tap(void);
 void iax2_calls_init_tap(void);
 void VoIPcalls_init_tap(void);
 
-/*
-* Removes the voip_calls tap listener (if not already done)
-* From that point on, the voip calls list won't be updated any more.
-*/
+/**
+ * Removes the voip_calls tap listener (if not already done)
+ * From that point on, the voip calls list won't be updated any more.
+ */
 void remove_tap_listener_sip_calls(void);
 void remove_tap_listener_isup_calls(void);
 void remove_tap_listener_mtp3_calls(void);
@@ -259,15 +260,15 @@ void remove_tap_listener_skinny_calls(void);
 void remove_tap_listener_iax2_calls(void);
 void remove_tap_listener_voip_calls(void);
 
-/*
-* Retrieves a constant reference to the unique info structure of the voip_calls tap listener.
-* The user should not modify the data pointed to.
-*/
+/**
+ * Retrieves a constant reference to the unique info structure of the voip_calls tap listener.
+ * The user should not modify the data pointed to.
+ */
 voip_calls_tapinfo_t* voip_calls_get_info(void);
 
-/*
-* Cleans up memory of voip calls tap.
-*/
+/**
+ * Cleans up memory of voip calls tap.
+ */
 void isup_calls_reset(voip_calls_tapinfo_t *tapinfo);
 void mtp3_calls_reset(voip_calls_tapinfo_t *tapinfo);
 void q931_calls_reset(voip_calls_tapinfo_t *tapinfo);
