8fa5ef33f90abd7e3f21986b5532943de30ff9b7
Anders Broman <anders.broman@ericsson.com>
2012-03-12 06:42:47 +0000

    From Jose Pedro Oliveira: Add Modlines
    svn path=/trunk/; revision=41496

--- pcapio.c
+++ pcapio.c
@@ -758,3 +758,16 @@ libpcap_dump_close(FILE *pd, int *err)
 }
 
 #endif /* HAVE_LIBPCAP */
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
