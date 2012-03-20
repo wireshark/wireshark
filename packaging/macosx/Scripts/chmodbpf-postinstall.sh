b7cfa1359d4b5bd9d4bd962d97b9ca1123f96847
Guy Harris <guy@alum.mit.edu>
2012-03-20 04:06:36 +0000

    Now, isn't that swell!  Leopard's dseditgroup doesn't return a failure exit status if the group in question doesn't exist, so we can't use dseditgroup on Leopard to check whether the access_bpf group exists.
    Instead, as per Martin Williams' suggestion in bug 6526, use "dscl .
    -read /groups/{group}" instead; the suggestion came from the addgroup.sh
    script in
    
    	https://developer.apple.com/library/mac/documentation/OpenSource/Conceptual/ShellScripting/ShellScripting.pdf
    
    svn path=/trunk/; revision=41680

--- packaging/macosx/Scripts/chmodbpf-postinstall.sh
+++ packaging/macosx/Scripts/chmodbpf-postinstall.sh
@@ -5,7 +5,7 @@ CHMOD_BPF="$CHMOD_BPF_DIR/ChmodBPF"
 BPF_GROUP="access_bpf"
 BPF_GROUP_NAME="BPF device access ACL"
 
-dseditgroup -q -o read "$BPF_GROUP" > /dev/null 2>&1 || \
+dscl . -read /Groups/"$BPF_GROUP" > /dev/null 2>&1 || \
     dseditgroup -q -o create "$BPF_GROUP"
 dseditgroup -q -o edit -a "$USER" -t user "$BPF_GROUP"
 
