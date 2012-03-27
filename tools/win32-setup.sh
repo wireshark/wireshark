2a1e0baeaccc25ad511743cd75af3f39ed824cea
Gerald Combs <gerald@wireshark.org>
2012-03-27 22:57:42 +0000

    Use GnuTLS 2.12.18.
    svn path=/trunk/; revision=41809

--- tools/win32-setup.sh
+++ tools/win32-setup.sh
@@ -4,7 +4,7 @@
 
 # 32-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2012-02-29"
+export DOWNLOAD_TAG="2012-03-26"
 export WIRESHARK_TARGET_PLATFORM="win32"
 
 WIN_SETUP=`echo $0 | sed -e s/win32/win/`
--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2011-06-27"
+export DOWNLOAD_TAG="2012-03-26"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
