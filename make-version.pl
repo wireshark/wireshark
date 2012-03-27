cf4c0426d72bf3507f835242e7fc780847db3dbb
Gerald Combs <gerald@wireshark.org>
2012-03-27 19:56:22 +0000

    1.6.6 → 1.6.7.
    svn path=/trunk-1.6/; revision=41808

--- make-version.pl
+++ make-version.pl
@@ -75,7 +75,7 @@ my $set_release = 0;
 my %version_pref = (
 	"version_major" => 1,
 	"version_minor" => 6,
-	"version_micro" => 6,
+	"version_micro" => 7,
 	"version_build" => 0,
 
 	"enable"        => 1,
