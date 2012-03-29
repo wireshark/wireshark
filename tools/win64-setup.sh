3ae3fcee5c181b54761cfcbefc59838630e47243
Gerald Combs <gerald@wireshark.org>
2012-03-29 20:18:10 +0000

    Use the GnuTLS 2.12.18-1.2-1 bundle.
    svn path=/trunk/; revision=41829

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2012-03-26"
+export DOWNLOAD_TAG="2012-03-29"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
