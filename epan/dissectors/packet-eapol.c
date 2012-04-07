3580063a4072e5385b3e2e9c4ea1140d1c423960
Anders Broman <anders.broman@ericsson.com>
2012-04-07 13:07:53 +0000

    From Nick Lowe: Review use of proto_tree_add_*() in packet-eapol.c
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7037
    
    svn path=/trunk/; revision=41977

--- epan/dissectors/packet-eapol.c
+++ epan/dissectors/packet-eapol.c
@@ -39,16 +39,18 @@ static int hf_eapol_version = -1;
 static int hf_eapol_type = -1;
 static int hf_eapol_len = -1;
 static int hf_eapol_keydes_type = -1;
-static int hf_eapol_keydes_keylen = -1;
+static int hf_eapol_keydes_key_len = -1;
 static int hf_eapol_keydes_replay_counter = -1;
 static int hf_eapol_keydes_key_iv = -1;
-static int hf_eapol_keydes_key_index_keytype = -1;
-static int hf_eapol_keydes_key_index_indexnum = -1;
+static int hf_eapol_keydes_key_index = -1;
+static int hf_eapol_keydes_key_index_type = -1;
+static int hf_eapol_keydes_key_index_number = -1;
 static int hf_eapol_keydes_key_signature = -1;
 static int hf_eapol_keydes_key = -1;
+static int hf_eapol_keydes_key_generated_locally = -1;
 
 static int hf_eapol_wpa_keydes_keyinfo = -1;
-static int hf_eapol_wpa_keydes_keyinfo_keydes_ver = -1;
+static int hf_eapol_wpa_keydes_keyinfo_keydes_version = -1;
 static int hf_eapol_wpa_keydes_keyinfo_key_type = -1;
 static int hf_eapol_wpa_keydes_keyinfo_key_index = -1;
 static int hf_eapol_wpa_keydes_keyinfo_install = -1;
@@ -57,12 +59,12 @@ static int hf_eapol_wpa_keydes_keyinfo_key_mic = -1;
 static int hf_eapol_wpa_keydes_keyinfo_secure = -1;
 static int hf_eapol_wpa_keydes_keyinfo_error = -1;
 static int hf_eapol_wpa_keydes_keyinfo_request = -1;
-static int hf_eapol_wpa_keydes_keyinfo_encr_key_data = -1;
+static int hf_eapol_wpa_keydes_keyinfo_encrypted_key_data = -1;
 static int hf_eapol_wpa_keydes_nonce = -1;
 static int hf_eapol_wpa_keydes_rsc = -1;
 static int hf_eapol_wpa_keydes_id = -1;
 static int hf_eapol_wpa_keydes_mic = -1;
-static int hf_eapol_wpa_keydes_datalen = -1;
+static int hf_eapol_wpa_keydes_data_len = -1;
 static int hf_eapol_wpa_keydes_data = -1;
 
 static gint ett_eapol = -1;
@@ -107,33 +109,39 @@ static const value_string eapol_type_vals[] = {
 
 static const value_string eapol_keydes_type_vals[] = {
 	{ 1, "RC4 Descriptor" },
-	{ EAPOL_RSN_KEY, "EAPOL RSN key" },
-	{ EAPOL_WPA_KEY, "EAPOL WPA key" },
+	{ EAPOL_RSN_KEY, "EAPOL RSN Key" },
+	{ EAPOL_WPA_KEY, "EAPOL WPA Key" },
 	{ 0, NULL }
 };
 
-#define KEY_INFO_KEYDES_VER_MASK	0x0007
-#define KEY_INFO_KEY_TYPE_MASK		0x0008
-#define KEY_INFO_KEY_INDEX_MASK	0x0030
-#define KEY_INFO_INSTALL_MASK		0x0040
-#define KEY_INFO_KEY_ACK_MASK		0x0080
-#define KEY_INFO_KEY_MIC_MASK		0x0100
-#define KEY_INFO_SECURE_MASK		0x0200
-#define KEY_INFO_ERROR_MASK		0x0400
-#define KEY_INFO_REQUEST_MASK		0x0800
-#define KEY_INFO_ENCR_KEY_DATA_MASK	0x1000
+#define KEY_INFO_KEYDES_VERSION_MASK        0x0007
+#define KEY_INFO_KEY_TYPE_MASK              0x0008
+#define KEY_INFO_KEY_INDEX_MASK             0x0030
+#define KEY_INFO_INSTALL_MASK               0x0040
+#define KEY_INFO_KEY_ACK_MASK               0x0080
+#define KEY_INFO_KEY_MIC_MASK               0x0100
+#define KEY_INFO_SECURE_MASK                0x0200
+#define KEY_INFO_ERROR_MASK                 0x0400
+#define KEY_INFO_REQUEST_MASK               0x0800
+#define KEY_INFO_ENCRYPTED_KEY_DATA_MASK    0x1000
 
 static const true_false_string keytype_tfs =
 	{ "Unicast", "Broadcast" };
-static const true_false_string tfs_keyinfo_key_type =
-	{ "Pairwise key", "Group key" };
+
+static const true_false_string keyinfo_key_type_tfs =
+	{ "Pairwise Key", "Group Key" };
+
+#define KEYDES_KEY_INDEX_TYPE_MASK      0x80
+#define KEYDES_KEY_INDEX_NUMBER_MASK    0x7F
+
 #define KEYDES_VER_TYPE1	0x01
 #define KEYDES_VER_TYPE2	0x02
 #define KEYDES_VER_TYPE3	0x03
+
 static const value_string keydes_ver[] = {
-	{ KEYDES_VER_TYPE1,	"HMAC-MD5 for MIC and RC4 for encryption" },
-	{ KEYDES_VER_TYPE2,	"HMAC-SHA1 for MIC and AES key wrap for encryption" },
-	{ KEYDES_VER_TYPE3,	"AES-128-CMAC for MIC and AES key wrap for encryption" },
+	{ KEYDES_VER_TYPE1,	"RC4 Cipher, HMAC-MD5 MIC" },
+	{ KEYDES_VER_TYPE2,	"AES Cipher, HMAC-SHA1 MIC" },
+	{ KEYDES_VER_TYPE3,	"AES Cipher, AES-128-CMAC MIC" },
 	{ 0, 		NULL }
 };
 
@@ -146,8 +154,8 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
   guint16     eapol_len;
   guint       len;
   guint16     eapol_key_len, eapol_data_len;
-  guint8      key_index;
   guint16     keyinfo;
+  gboolean    generated_locally;
   proto_tree *ti = NULL;
   proto_tree *eapol_tree = NULL;
   proto_tree *keyinfo_item = NULL;
@@ -168,10 +176,10 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
   eapol_type = tvb_get_guint8(tvb, offset);
   if (tree)
-    proto_tree_add_uint(eapol_tree, hf_eapol_type, tvb, offset, 1, eapol_type);
+    proto_tree_add_item(eapol_tree, hf_eapol_type, tvb, offset, 1, ENC_BIG_ENDIAN);
   if (check_col(pinfo->cinfo, COL_INFO))
     col_add_str(pinfo->cinfo, COL_INFO,
-		val_to_str(eapol_type, eapol_type_vals, "Unknown type (0x%02X)"));
+		val_to_str(eapol_type, eapol_type_vals, "Unknown Type (0x%02X)"));
   offset++;
 
   eapol_len = tvb_get_ntohs(tvb, offset);
@@ -179,7 +187,7 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
   set_actual_length(tvb, len);
   if (tree) {
     proto_item_set_len(ti, len);
-    proto_tree_add_uint(eapol_tree, hf_eapol_len, tvb, offset, 2, eapol_len);
+    proto_tree_add_item(eapol_tree, hf_eapol_len, tvb, offset, 2, ENC_BIG_ENDIAN);
   }
   offset += 2;
 
@@ -209,72 +217,72 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
                KEY_INFO_KEY_MIC_MASK | KEY_INFO_SECURE_MASK);
             switch (masked) {
             case KEY_INFO_KEY_ACK_MASK:
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (msg 1/4)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Message 1 of 4)");
               break;
             case KEY_INFO_KEY_MIC_MASK:
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (msg 2/4)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Message 2 of 4)");
               break;
             case (KEY_INFO_INSTALL_MASK | KEY_INFO_KEY_ACK_MASK |
                   KEY_INFO_KEY_MIC_MASK | KEY_INFO_SECURE_MASK):
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (msg 3/4)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Message 3 of 4)");
               break;
             case (KEY_INFO_KEY_MIC_MASK | KEY_INFO_SECURE_MASK):
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (msg 4/4)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Message 4 of 4)");
               break;
             }
           } else {
             if (keyinfo & KEY_INFO_KEY_ACK_MASK)
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (Group msg 1/2)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Group Message 1 of 2)");
             else
-              col_set_str(pinfo->cinfo, COL_INFO, "Key (Group msg 2/2)");
+              col_set_str(pinfo->cinfo, COL_INFO, "Key (Group Message 2 of 2)");
           }
         }
 	keyinfo_item =
-	  proto_tree_add_uint(eapol_tree, hf_eapol_wpa_keydes_keyinfo, tvb,
-			      offset, 2, keyinfo);
+	  proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_keyinfo, tvb,
+			      offset, 2, ENC_BIG_ENDIAN);
 
 	keyinfo_tree = proto_item_add_subtree(keyinfo_item, ett_keyinfo);
-	proto_tree_add_uint(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_keydes_ver, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_type, tvb, offset, 2, keyinfo);
-	proto_tree_add_uint(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_index, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_install, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_ack, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_mic, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_secure, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_error, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_request, tvb, offset, 2, keyinfo);
-	proto_tree_add_boolean(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_encr_key_data, tvb, offset, 2, keyinfo);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_keydes_version, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_type, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_index, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_install, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_ack, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_key_mic, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_secure, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_error, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_request, tvb, offset, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(keyinfo_tree, hf_eapol_wpa_keydes_keyinfo_encrypted_key_data, tvb, offset, 2, ENC_BIG_ENDIAN);
 
         offset += 2;
-        proto_tree_add_uint(eapol_tree, hf_eapol_keydes_keylen, tvb, offset,
-        		    2, tvb_get_ntohs(tvb, offset));
+        proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_len, tvb, offset,
+        		    2, ENC_BIG_ENDIAN);
         offset += 2;
         proto_tree_add_item(eapol_tree, hf_eapol_keydes_replay_counter, tvb,
         		    offset, 8, ENC_BIG_ENDIAN);
         offset += 8;
         proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_nonce, tvb, offset,
-        		    32, ENC_NA);
+        		    32, ENC_BIG_ENDIAN);
         offset += 32;
         proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_iv, tvb,
-        		    offset, 16, ENC_NA);
+        		    offset, 16, ENC_BIG_ENDIAN);
         offset += 16;
         proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_rsc, tvb, offset,
-        		    8, ENC_NA);
+        		    8, ENC_BIG_ENDIAN);
         offset += 8;
         proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_id, tvb, offset, 8,
-        		    ENC_NA);
+        		    ENC_BIG_ENDIAN);
         offset += 8;
         proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_mic, tvb, offset,
-        		    16, ENC_NA);
+        		    16, ENC_BIG_ENDIAN);
         offset += 16;
         eapol_data_len = tvb_get_ntohs(tvb, offset);
-        proto_tree_add_uint(eapol_tree, hf_eapol_wpa_keydes_datalen, tvb,
-        		    offset, 2, eapol_data_len);
+        proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_data_len, tvb,
+        		    offset, 2, ENC_BIG_ENDIAN);
         offset += 2;
         if (eapol_data_len != 0) {
           ti = proto_tree_add_item(eapol_tree, hf_eapol_wpa_keydes_data,
-          	tvb, offset, eapol_data_len, ENC_NA);
-	  if ((keyinfo & KEY_INFO_ENCR_KEY_DATA_MASK) ||
+          	tvb, offset, eapol_data_len, ENC_BIG_ENDIAN);
+	  if ((keyinfo & KEY_INFO_ENCRYPTED_KEY_DATA_MASK) ||
 	      !(keyinfo & KEY_INFO_KEY_TYPE_MASK)) {
 	    /* RSN: EAPOL-Key Key Data is encrypted.
 	     * WPA: Group Keys use encrypted Key Data.
@@ -289,7 +297,7 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         }
       } else {
         eapol_key_len = tvb_get_ntohs(tvb, offset);
-        proto_tree_add_uint(eapol_tree, hf_eapol_keydes_keylen, tvb, offset, 2, eapol_key_len);
+        proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_len, tvb, offset, 2, ENC_BIG_ENDIAN);
         offset += 2;
         proto_tree_add_item(eapol_tree, hf_eapol_keydes_replay_counter, tvb,
   			  offset, 8, ENC_BIG_ENDIAN);
@@ -297,28 +305,26 @@ dissect_eapol(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_iv, tvb,
   			  offset, 16, ENC_NA);
         offset += 16;
-        key_index = tvb_get_guint8(tvb, offset);
-        ti = proto_tree_add_text(eapol_tree, tvb, offset, 1,
-  			       "Key Index: %s, index %u",
-  			       (key_index & 0x80) ? "unicast" : "broadcast",
-  			       key_index & 0x7F);
+        ti = proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_index, tvb, offset, 1, ENC_BIG_ENDIAN);
         key_index_tree = proto_item_add_subtree(ti, ett_eapol_key_index);
-        proto_tree_add_boolean(key_index_tree, hf_eapol_keydes_key_index_keytype,
-  			     tvb, offset, 1, key_index);
-        proto_tree_add_uint(key_index_tree, hf_eapol_keydes_key_index_indexnum,
-  			     tvb, offset, 1, key_index);
+        proto_tree_add_item(key_index_tree, hf_eapol_keydes_key_index_type,
+  			     tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(key_index_tree, hf_eapol_keydes_key_index_number,
+  			     tvb, offset, 1, ENC_BIG_ENDIAN);
         offset += 1;
         proto_tree_add_item(eapol_tree, hf_eapol_keydes_key_signature, tvb,
   			  offset, 16, ENC_NA);
         offset += 16;
         if (eapol_key_len != 0) {
-          if (eapol_len > 44) { /* Size of rc4 key with no key content */
-            proto_tree_add_item(eapol_tree, hf_eapol_keydes_key, tvb, offset,
-  			    eapol_key_len, ENC_NA);
-          } else {
-		/* IEEE 802.1X-2004 7.6.3.6: If no bytes remain, then  */
-		proto_tree_add_text(eapol_tree, tvb, offset, 0, "Key: Use key locally generated by peer");
+		  /* IEEE 802.1X-2004 7.6.3.6: If no bytes remain, then */
+          generated_locally = eapol_len <= 44; /* Size of rc4 key with no key content */
+          if (!generated_locally) { 
+              proto_tree_add_item(eapol_tree, hf_eapol_keydes_key, tvb, offset,
+                                  eapol_key_len, ENC_BIG_ENDIAN);
           }
+            
+          proto_tree_add_boolean(eapol_tree, hf_eapol_keydes_key_generated_locally, tvb, offset,
+                                 0, generated_locally);
         }
       }
     }
@@ -348,8 +354,8 @@ proto_register_eapol(void)
 	{ &hf_eapol_keydes_type, {
 		"Descriptor Type", "eapol.keydes.type", FT_UINT8, BASE_DEC,
 		VALS(eapol_keydes_type_vals), 0x0, "Key Descriptor Type", HFILL }},
-	{ &hf_eapol_keydes_keylen, {
-		"Key Length", "eapol.keydes.keylen", FT_UINT16, BASE_DEC,
+	{ &hf_eapol_keydes_key_len, {
+		"Key Length", "eapol.keydes.key_len", FT_UINT16, BASE_DEC,
 		NULL, 0x0, NULL, HFILL }},
 	{ &hf_eapol_keydes_replay_counter, {
 		"Replay Counter", "eapol.keydes.replay_counter", FT_UINT64, BASE_DEC,
@@ -357,101 +363,105 @@ proto_register_eapol(void)
 	{ &hf_eapol_keydes_key_iv, {
 		"Key IV", "eapol.keydes.key_iv", FT_BYTES, BASE_NONE,
 		NULL, 0x0, "Key Initialization Vector", HFILL }},
-	{ &hf_eapol_keydes_key_index_keytype, {
-		"Key Type", "eapol.keydes.index.keytype", FT_BOOLEAN, 8,
-		TFS(&keytype_tfs), 0x80, "Key Type (unicast/broadcast)", HFILL }},
-	{ &hf_eapol_keydes_key_index_indexnum, {
-		"Index Number", "eapol.keydes.index.indexnum", FT_UINT8, BASE_DEC,
-		NULL, 0x7F, "Key Index number", HFILL }},
+    { &hf_eapol_keydes_key_index, {
+          "Key Index", "eapol.keydes.key_index", FT_UINT8, BASE_HEX,
+          NULL, 0x0, "Type", HFILL }},
+	{ &hf_eapol_keydes_key_index_type, {
+		"Type", "eapol.keydes.key_index.type", FT_BOOLEAN, 8,
+		TFS(&keytype_tfs), KEYDES_KEY_INDEX_TYPE_MASK , "Type", HFILL }},
+	{ &hf_eapol_keydes_key_index_number, {
+		"Number", "eapol.keydes.key_index.number", FT_UINT8, BASE_DEC,
+		NULL, KEYDES_KEY_INDEX_NUMBER_MASK, "Number", HFILL }},
 	{ &hf_eapol_keydes_key_signature, {
 		"Key Signature", "eapol.keydes.key_signature", FT_BYTES, BASE_NONE,
 		NULL, 0x0, NULL, HFILL }},
 	{ &hf_eapol_keydes_key, {
 		"Key", "eapol.keydes.key", FT_BYTES, BASE_NONE,
 		NULL, 0x0, NULL, HFILL }},
-
+    { &hf_eapol_keydes_key_generated_locally, {
+          "Key Generated Locally", "eapol.keydes.key.generated_locally",
+          FT_BOOLEAN, BASE_NONE, NULL, 0x0, NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo, {
 		"Key Information", "eapol.keydes.key_info", FT_UINT16,
-		BASE_HEX, NULL, 0x0, "WPA key info", HFILL }},
-
-	{ &hf_eapol_wpa_keydes_keyinfo_keydes_ver, {
+		BASE_HEX, NULL, 0x0, NULL, HFILL }},
+	{ &hf_eapol_wpa_keydes_keyinfo_keydes_version, {
 		"Key Descriptor Version",
-		"eapol.keydes.key_info.keydes_ver",
+		"eapol.keydes.key_info.keydes_version",
 		FT_UINT16, BASE_DEC, VALS(keydes_ver),
-		KEY_INFO_KEYDES_VER_MASK,
-		"Key Descriptor Version Type", HFILL }},
+		KEY_INFO_KEYDES_VERSION_MASK,
+		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_key_type, {
 		"Key Type",
 		"eapol.keydes.key_info.key_type",
-		FT_BOOLEAN, 16, TFS(&tfs_keyinfo_key_type),
+		FT_BOOLEAN, 16, TFS(&keyinfo_key_type_tfs),
 		KEY_INFO_KEY_TYPE_MASK,
-		"Key Type (Pairwise or Group)", HFILL }},
+		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_key_index, {
 		"Key Index",
 		"eapol.keydes.key_info.key_index",
 		FT_UINT16, BASE_DEC, NULL,
 		KEY_INFO_KEY_INDEX_MASK,
-		"Key Index (0-3) (RSN: Reserved)", HFILL }},
+		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_install, {
-		"Install flag",
+		"Install",
 		"eapol.keydes.key_info.install",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_INSTALL_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_key_ack, {
-		"Key Ack flag",
+		"Key Ack",
 		"eapol.keydes.key_info.key_ack",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_KEY_ACK_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_key_mic, {
-		"Key MIC flag",
+		"Key MIC",
 		"eapol.keydes.key_info.key_mic",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_KEY_MIC_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_secure, {
-		"Secure flag",
+		"Secure",
 		"eapol.keydes.key_info.secure",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_SECURE_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_error, {
-		"Error flag",
+		"Error",
 		"eapol.keydes.key_info.error",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_ERROR_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_keyinfo_request, {
-		"Request flag",
+		"Request",
 		"eapol.keydes.key_info.request",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
 		KEY_INFO_REQUEST_MASK,
 		NULL, HFILL }},
-	{ &hf_eapol_wpa_keydes_keyinfo_encr_key_data, {
-		"Encrypted Key Data flag",
-		"eapol.keydes.key_info.encr_key_data",
+	{ &hf_eapol_wpa_keydes_keyinfo_encrypted_key_data, {
+		"Encrypted Key Data",
+		"eapol.keydes.key_info.encrypted_key_data",
 		FT_BOOLEAN, 16, TFS(&tfs_set_notset),
-		KEY_INFO_ENCR_KEY_DATA_MASK,
+		KEY_INFO_ENCRYPTED_KEY_DATA_MASK,
 		NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_nonce, {
 		"Nonce", "eapol.keydes.nonce", FT_BYTES, BASE_NONE,
 		NULL, 0x0, "WPA Key Nonce", HFILL }},
 	{ &hf_eapol_wpa_keydes_rsc, {
 		"WPA Key RSC", "eapol.keydes.rsc", FT_BYTES, BASE_NONE, NULL,
-		0x0, "WPA Key Receive Sequence Counter", HFILL }},
+		0x0, NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_id, {
 		"WPA Key ID", "eapol.keydes.id", FT_BYTES, BASE_NONE, NULL,
-		0x0, "WPA Key ID(RSN Reserved)", HFILL }},
+		0x0, NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_mic, {
 		"WPA Key MIC", "eapol.keydes.mic", FT_BYTES, BASE_NONE, NULL,
-		0x0, "WPA Key Message Integrity Check", HFILL }},
-	{ &hf_eapol_wpa_keydes_datalen, {
-		"WPA Key Length", "eapol.keydes.datalen", FT_UINT16, BASE_DEC,
-		NULL, 0x0, "WPA Key Data Length", HFILL }},
+		0x0, NULL, HFILL }},
+	{ &hf_eapol_wpa_keydes_data_len, {
+		"WPA Key Data Length", "eapol.keydes.data_len", FT_UINT16, BASE_DEC,
+		NULL, 0x0, NULL, HFILL }},
 	{ &hf_eapol_wpa_keydes_data, {
-		"WPA Key", "eapol.keydes.data", FT_BYTES, BASE_NONE,
-		NULL, 0x0, "WPA Key Data", HFILL }},
+		"WPA Key Data", "eapol.keydes.data", FT_BYTES, BASE_NONE,
+		NULL, 0x0, NULL, HFILL }},
   };
   static gint *ett[] = {
 	&ett_eapol,
