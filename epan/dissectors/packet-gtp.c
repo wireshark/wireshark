14d362a018cb0f2ee4ee55046d9918877d222548
pascal <pascal@localhost>
2012-04-08 16:03:21 +0000

    As suggested by Jakub, always update the digit value when the nibble is outside [0;9] range
    svn path=/trunk/; revision=41984

--- epan/dissectors/packet-gtp.c
+++ epan/dissectors/packet-gtp.c
@@ -1903,11 +1903,11 @@ id_to_str(tvbuff_t *tvb, gint offset)
         if (bits4to1 <= 9)
             str[j++] = BCD2CHAR(bits4to1);
         else
-            j++;
+            str[j++] = ' ';
         if (bits8to5 <= 9)
             str[j++] = BCD2CHAR(bits8to5);
         else
-            j++;
+            str[j++] = ' ';
     }
     str[j] = '\0';
     return str;
@@ -1928,7 +1928,7 @@ msisdn_to_str(tvbuff_t *tvb, gint offset, int len)
         if (bits4to1 <= 9)
             str[j++] = BCD2CHAR(bits4to1);
         else
-            j++;
+            str[j++] = '?';
         if (bits8to5 <= 9)
             str[j++] = BCD2CHAR(bits8to5);
         else if ((i == (MIN(len, 9) - 1)) && (bits8to5 == 0xF)) {
@@ -1936,7 +1936,7 @@ msisdn_to_str(tvbuff_t *tvb, gint offset, int len)
             break;
         }
         else
-            j++;
+            str[j++] = '?';
     }
     str[j] = '\0';
 
