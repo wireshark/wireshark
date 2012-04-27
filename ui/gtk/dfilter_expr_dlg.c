4253c73d8f3fe128199554de52327d7230defc8f
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2012-04-27 20:35:17 +0000

    Fix sigsegv in filter expression dialog when user clicked on some BASE_CUSTOM field.
    svn path=/trunk/; revision=42301

--- ui/gtk/dfilter_expr_dlg.c
+++ ui/gtk/dfilter_expr_dlg.c
@@ -193,7 +193,7 @@ field_select_row_cb(GtkTreeSelection *sel, gpointer tree)
          * fill up the list of values, otherwise clear the list of values.
          */
 	/* XXX: ToDo: Implement "range-string" filter ?   */
-        if ((hfinfo->strings != NULL) && !(hfinfo->display & BASE_RANGE_STRING)) {
+        if ((hfinfo->strings != NULL) && !(hfinfo->display & BASE_RANGE_STRING) && !(hfinfo->display & BASE_CUSTOM)) {
             const value_string *vals = hfinfo->strings;
             if (hfinfo->display & BASE_EXT_STRING)
                 vals = VALUE_STRING_EXT_VS_P((value_string_ext *) vals);
