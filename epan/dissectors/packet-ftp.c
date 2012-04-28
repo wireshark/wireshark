f0b6f7a6aee4b6b54ea2e1218a0caa69b3a6ab5f
Martin Mathieson <martin.r.mathieson@googlemail.com>
2012-04-28 16:48:11 +0000

    Avoid assignement and clang warning.
    svn path=/trunk/; revision=42317

--- epan/dissectors/packet-ftp.c
+++ epan/dissectors/packet-ftp.c
@@ -609,8 +609,7 @@ dissect_ftp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             /*
              * Find the end of the line.
              */
-            linelen = tvb_find_line_end(tvb, offset, -1,
-                &next_offset, FALSE);
+            tvb_find_line_end(tvb, offset, -1, &next_offset, FALSE);
 
             /*
              * Put this line.
