12ba85a2a4988c6c0b91c17572b4a8c7a6c54b8d
Bill Meier <wmeier@newsguy.com>
2012-05-19 16:07:50 +0000

    Rework proto_registrar_dump_protocols() to prevent a Clang scan-build "de-reference of a null poiunter" warning (In practice, bug would not actually occur).
    svn path=/trunk/; revision=42717

--- epan/proto.c
+++ epan/proto.c
@@ -6341,11 +6341,13 @@ proto_registrar_dump_protocols(void)
 	int	    i;
 	void	   *cookie = NULL;
 
-	for (i = proto_get_first_protocol(&cookie); i != -1;
-		i = proto_get_next_protocol(&cookie)) {
+
+	i = proto_get_first_protocol(&cookie);
+	while (i != -1) {
 		protocol = find_protocol_by_id(i);
 		printf("%s\t%s\t%s\n", protocol->name, protocol->short_name,
 			protocol->filter_name);
+		i = proto_get_next_protocol(&cookie);
 	}
 }
 
