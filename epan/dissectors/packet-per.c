wireshark/wireshark

commit 20d92719ea276109cb9b5283b769c703d8902562

Use length field from UDP header as the pseudo-header's UDP length field instead of using the reported_len.  Fixes https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6711
svn path=/trunk/; revision=40387

epan/dissectors/packet-udp.c
@@ -462,12 +462,12 @@ dissect(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ip_proto)
       switch (pinfo->src.type) {
 
       case AT_IPv4:
-        phdr[0] = g_htonl((ip_proto<<16) + reported_len);
+        phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
         cksum_vec[2].len = 4;
         break;
 
       case AT_IPv6:
-        phdr[0] = g_htonl(reported_len);
+        phdr[0] = g_htonl(udph->uh_ulen);
         phdr[1] = g_htonl(ip_proto);
         cksum_vec[2].len = 8;
         break;

commit c11264acc277c79cfd093b0438a690807942eb4e

Copy over revisions from the trunk:
  ------------------------------------------------------------------------
  r43184 | wmeier | 2012-06-10 08:10:11 -0700 (Sun, 10 Jun 2012) | 5 lines
  Changed paths:
     M /trunk/epan/proto.c

  Fix bug wherein values in a custom column aren't displayed as a value_string (when appropriate).
  Essentially: BASE_CUSTOM is "base_display_e value" (like BASE_DEC) *not* a bit-mask
   (like BASE_EXT_STRING).
  Bug introduced in SVN #39902
  ------------------------------------------------------------------------
  r43186 | wmeier | 2012-06-10 12:23:50 -0700 (Sun, 10 Jun 2012) | 3 lines
  Changed paths:
     M /trunk/epan/proto.c

  Fix "BASE_CUSTOM field is incorrectly formatted when applied as a column".
  Resolves Bug #6671 - https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6671
  ------------------------------------------------------------------------
  r43187 | cmaynard | 2012-06-10 12:43:40 -0700 (Sun, 10 Jun 2012) | 3 lines
  Changed paths:
     M /trunk/epan/dissectors/packet-udp.c

  Fix UDP-Lite checksum verification bug introduced with r40387 and reported by "answer" via http://ask.wireshark.org/questions/11798/is-udp-lite-checksum-correct-ipv6
  There is a difference between the UDP and UDP-Lite pseudo header that wasn't realized when r40387 was committed.  Details of the difference in http://tools.ietf.org/html/rfc3828#section-3.2
  ------------------------------------------------------------------------
  r43193 | cmaynard | 2012-06-10 19:43:16 -0700 (Sun, 10 Jun 2012) | 2 lines
  Changed paths:
     M /trunk/ui/gtk/print_dlg.c

  Set the default for printing or exporting packets to displayed packets only.
  ------------------------------------------------------------------------
  r43195 | cmaynard | 2012-06-10 20:51:47 -0700 (Sun, 10 Jun 2012) | 2 lines
  Changed paths:
     M /trunk/docbook/release-notes.xml

docbook/release-notes.xml
@@ -108,8 +108,9 @@ Wireshark Info
 
         <listitem>
           <para>
-            When saving packets, the default choice is now to save
-            only the displayed packets rather than all packets.
+            When saving, exporting or printing packets, the default choice
+            is now to save, export or print only the displayed packets
+            rather than all packets.
           </para>
         </listitem>
 

epan/dissectors/packet-per.c
@@ -518,17 +518,6 @@ DEBUG_ENTRY("dissect_per_sequence_of");
 }
 
 
-/* dissect a constrained IA5String that consists of the full ASCII set,
-   i.e. no FROM stuff limiting the alphabet
-*/
-guint32
-dissect_per_IA5String(tvbuff_t *tvb, guint32 offset, asn1_ctx_t *actx, proto_tree *tree, int hf_index, int min_len, int max_len, gboolean has_extension)
-{
-	offset=dissect_per_octet_string(tvb, offset, actx, tree, hf_index, min_len, max_len, has_extension, NULL);
-
-	return offset;
-}
-
 /* XXX we don't do >64k length strings   yet */
 static guint32
 dissect_per_restricted_character_string_sorted(tvbuff_t *tvb, guint32 offset, asn1_ctx_t *actx, proto_tree *tree, int hf_index, int min_len, int max_len, gboolean has_extension _U_,const char *alphabet, int alphabet_length, tvbuff_t **value_tvb)
@@ -656,7 +645,8 @@ DEBUG_ENTRY("dissect_per_restricted_character_string");
 		/* ALIGNED PER does not do any remapping of chars if
 		   bitsperchar is 8
 		*/
-		if(bits_per_char==8){
+		/* If alphabet is not provided, do not do any remapping either */
+		if((bits_per_char==8) || (alphabet==NULL)){
 			buf[char_pos]=val;
 		} else {
 			if (val < alphabet_length){
@@ -711,6 +701,18 @@ dissect_per_restricted_character_string(tvbuff_t *tvb, guint32 offset, asn1_ctx_
     alphabet_ptr = sort_alphabet(sorted_alphabet, alphabet, alphabet_length);
   }
   return dissect_per_restricted_character_string_sorted(tvb, offset, actx, tree, hf_index, min_len, max_len, has_extension, alphabet_ptr, alphabet_length, value_tvb);
+}
+
+/* dissect a constrained IA5String that consists of the full ASCII set,
+   i.e. no FROM stuff limiting the alphabet
+*/
+guint32
+dissect_per_IA5String(tvbuff_t *tvb, guint32 offset, asn1_ctx_t *actx, proto_tree *tree, int hf_index, int min_len, int max_len, gboolean has_extension)
+{
+	offset=dissect_per_restricted_character_string_sorted(tvb, offset, actx, tree, hf_index, min_len, max_len, has_extension,
+		NULL, 128, NULL);
+
+	return offset;
 }
 
 guint32

epan/dissectors/packet-rsvp.c
@@ -3656,8 +3656,10 @@ dissect_rsvp_label_request (proto_item *ti, proto_tree *rsvp_object_tree,
 	    dlci_len = 10;
 	    min_dlci &= 0x3ff;
 	    max_dlci &= 0x3ff;
+	    break;
 	case 2:
 	    dlci_len = 23;
+	    break;
 	default:
 	    dlci_len = 0;
 	    min_dlci = 0;

epan/dissectors/packet-udp.c
@@ -462,12 +462,18 @@ dissect(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ip_proto)
       switch (pinfo->src.type) {
 
       case AT_IPv4:
-        phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
+        if (ip_proto == IP_PROTO_UDP)
+          phdr[0] = g_htonl((ip_proto<<16) | udph->uh_ulen);
+	else
+          phdr[0] = g_htonl((ip_proto<<16) | reported_len);
         cksum_vec[2].len = 4;
         break;
 
       case AT_IPv6:
-        phdr[0] = g_htonl(udph->uh_ulen);
+        if (ip_proto == IP_PROTO_UDP)
+          phdr[0] = g_htonl(udph->uh_ulen);
+	else
+          phdr[0] = g_htonl(reported_len);
         phdr[1] = g_htonl(ip_proto);
         cksum_vec[2].len = 8;
         break;

epan/proto.c
@@ -3812,10 +3812,15 @@ proto_custom_set(proto_tree* tree, const int field_id, gint occurrence,
 			case FT_UINT32:
 			case FT_FRAMENUM:
 				u_integer = fvalue_get_uinteger(&finfo->value);
-				if (hfinfo->strings) {
-					if (hfinfo->display & BASE_CUSTOM) {
-						g_snprintf(result+offset_r, size-offset_r, "%u", u_integer);
-					} else if (hfinfo->display & BASE_RANGE_STRING) {
+				if ((hfinfo->display & BASE_DISPLAY_E_MASK) == BASE_CUSTOM) {
+					gchar tmp[ITEM_LABEL_LENGTH];
+					custom_fmt_func_t fmtfunc = (custom_fmt_func_t)hfinfo->strings;
+
+					DISSECTOR_ASSERT(fmtfunc);
+					fmtfunc(tmp, u_integer);
+					g_snprintf(result+offset_r, size-offset_r, "%s", tmp);
+				} else if (hfinfo->strings) {
+					if (hfinfo->display & BASE_RANGE_STRING) {
 						g_strlcpy(result+offset_r,
 							  rval_to_str(u_integer, hfinfo->strings, "%u"),
 							  size-offset_r);
@@ -3851,6 +3856,7 @@ proto_custom_set(proto_tree* tree, const int field_id, gint occurrence,
 				break;
 
 			case FT_INT64:
+				/* XXX: Should handle BASE_CUSTOM ? */
 				g_snprintf(result+offset_r, size-offset_r,
 					   "%" G_GINT64_MODIFIER "d",
 					   fvalue_get_integer64(&finfo->value));
@@ -3858,6 +3864,7 @@ proto_custom_set(proto_tree* tree, const int field_id, gint occurrence,
 				break;
 			case FT_UINT64:
 				g_snprintf(result+offset_r, size-offset_r,
+				/* XXX: Should handle BASE_CUSTOM ? */
 					   "%" G_GINT64_MODIFIER "u",
 					   fvalue_get_integer64(&finfo->value));
 				offset_r = (int)strlen(result);
@@ -3873,10 +3880,15 @@ proto_custom_set(proto_tree* tree, const int field_id, gint occurrence,
 			case FT_INT24:
 			case FT_INT32:
 				integer = fvalue_get_sinteger(&finfo->value);
-				if (hfinfo->strings) {
-					if (hfinfo->display & BASE_CUSTOM) {
-						g_snprintf(result+offset_r, size-offset_r, "%d", integer);
-					} else if (hfinfo->display & BASE_RANGE_STRING) {
+				if ((hfinfo->display & BASE_DISPLAY_E_MASK) == BASE_CUSTOM) {
+					gchar tmp[ITEM_LABEL_LENGTH];
+					custom_fmt_func_t fmtfunc = (custom_fmt_func_t)hfinfo->strings;
+
+					DISSECTOR_ASSERT(fmtfunc);
+					fmtfunc(tmp, integer);
+					g_snprintf(result+offset_r, size-offset_r, "%s", tmp);
+				} else if (hfinfo->strings) {
+					if (hfinfo->display & BASE_RANGE_STRING) {
 						g_strlcpy(result+offset_r,
 							  rval_to_str(integer, hfinfo->strings, "%d"),
 							  size-offset_r);
@@ -4474,7 +4486,7 @@ proto_register_protocol(const char *name, const char *short_name,
 	/* list will be sorted later by name, when all protocols completed registering */
 	protocols = g_list_prepend(protocols, protocol);
 
-	/* Here we do allocate a new header_field_info struct */
+	/* Here we allocate a new header_field_info struct */
 	hfinfo = g_slice_new(header_field_info);
 	hfinfo->name = name;
 	hfinfo->abbrev = filter_name;
@@ -4901,9 +4913,9 @@ tmp_fld_check_assert(header_field_info *hfinfo) {
 	/* Check for duplicate value_string values.
 	   There are lots that have the same value *and* string, so for now only
 	   report those that have same value but different string. */
-	if (hfinfo->strings != NULL &&
+	if ((hfinfo->strings != NULL) &&
 	    !(hfinfo->display & BASE_RANGE_STRING) &&
-	    !(hfinfo->display & BASE_CUSTOM) &&
+	    !((hfinfo->display & BASE_DISPLAY_E_MASK) == BASE_CUSTOM) &&
 	    (
 		    (hfinfo->type == FT_UINT8)  ||
 		    (hfinfo->type == FT_UINT16) ||
@@ -4918,10 +4930,10 @@ tmp_fld_check_assert(header_field_info *hfinfo) {
 		int n, m;
 		const value_string *start_values;
 		const value_string *current;
-		
+
 	    if (hfinfo->display & BASE_EXT_STRING)
 			start_values = VALUE_STRING_EXT_VS_P(((const value_string_ext*)hfinfo->strings));
-		else 
+		else
 			start_values = (const value_string*)hfinfo->strings;
 		current = start_values;
 

ui/gtk/print_dlg.c
@@ -148,6 +148,7 @@ file_print_cmd(gboolean print_selected)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   if(print_selected) {
       args->range.process = range_process_selected;
@@ -215,6 +216,7 @@ export_text_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_text_win = open_print_dialog("Wireshark: Export as \"Plain Text\" File", output_action_export_text, args);
   g_signal_connect(export_text_win, "destroy", G_CALLBACK(print_destroy_cb), &export_text_win);
@@ -268,6 +270,7 @@ export_ps_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_ps_win = open_print_dialog("Wireshark: Export as \"PostScript\" file", output_action_export_ps, args);
   g_signal_connect(export_ps_win, "destroy", G_CALLBACK(print_destroy_cb), &export_ps_win);
@@ -321,6 +324,7 @@ export_psml_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_psml_win = open_print_dialog("Wireshark: Export as \"PSML\" file", output_action_export_psml, args);
   g_signal_connect(export_psml_win, "destroy", G_CALLBACK(print_destroy_cb), &export_psml_win);
@@ -373,6 +377,7 @@ export_pdml_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_pdml_win = open_print_dialog("Wireshark: Export as \"PDML\" file", output_action_export_pdml, args);
   g_signal_connect(export_pdml_win, "destroy", G_CALLBACK(print_destroy_cb), &export_pdml_win);
@@ -424,6 +429,7 @@ export_csv_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_csv_win = open_print_dialog("Wireshark: Export as \"Comma Separated Values\" File", output_action_export_csv, args);
   g_signal_connect(export_csv_win, "destroy", G_CALLBACK(print_destroy_cb), &export_csv_win);
@@ -475,6 +481,7 @@ export_carrays_cmd_cb(GtkWidget *widget _U_, gpointer data _U_)
 
   /* init the printing range */
   packet_range_init(&args->range);
+  args->range.process_filtered = TRUE;
 
   export_carrays_win = open_print_dialog("Wireshark: Export as \"C Arrays\" File",
 					 output_action_export_carrays, args);