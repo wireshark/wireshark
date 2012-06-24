464464d8b6e33ee6c58d831c0073971ca78e1ba2
Anders Broman <anders.broman@ericsson.com>
2012-06-24 15:19:24 +0000

    From Pontus Fuchs: strncpy -> strlcpy
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6973
    
    svn path=/trunk/; revision=43457

--- ws80211_utils.c
+++ ws80211_utils.c
@@ -281,7 +281,7 @@ static int get_freq_wext(const char *ifname)
 	if (fd == -1)
 		return -1;
 
-	strncpy(wrq.name1, ifname, IFNAMSIZ);
+	g_strlcpy(wrq.name1, ifname, IFNAMSIZ);
 	/* SIOCGIWFREQ */
 	if (ioctl(fd, 0x8B05, &wrq) == 0) {
 		if (wrq.e == 6)
@@ -446,7 +446,7 @@ static int ws80211_iface_up(const char *ifname)
 	if (sock == -1)
 		return -1;
 
-	strncpy(ifreq.ifr_name, ifname, sizeof(ifreq.ifr_name));
+	g_strlcpy(ifreq.ifr_name, ifname, sizeof(ifreq.ifr_name));
 
 	if (ioctl(sock, SIOCGIFFLAGS, &ifreq))
 		goto out_err;
