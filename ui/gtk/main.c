5e077c6d6cea6cdc82071e1b78963cd7327713c0
Guy Harris <guy@alum.mit.edu>
2012-06-28 19:09:22 +0000

    <wsutil/file_util.h> is needed on Windows.
    svn path=/trunk/; revision=43529

--- ui/gtk/main.c
+++ ui/gtk/main.c
@@ -111,6 +111,8 @@
 #include "ui/simple_dialog.h"
 #include "ui/ui_util.h"
 
+#include <wsutil/file_util.h>
+
 #ifdef HAVE_LIBPCAP
 #include "../capture_ui_utils.h"
 #include "../capture-pcap-util.h"
