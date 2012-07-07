fa1f18d80b228952d969f0a767d9308e8bd755bd
Guy Harris <guy@alum.mit.edu>
2012-07-07 01:26:58 +0000

    Clean up mix of tabs and 4-space-tab indents.
    svn path=/trunk/; revision=43594

--- epan/prefs-int.h
+++ epan/prefs-int.h
@@ -33,18 +33,18 @@
  */
 
 struct pref_module {
-	const char *name;			/**< name of module */
-	const char *title;			/**< title of module (displayed in preferences list) */
-	const char *description;	/**< Description of module (displayed in preferences notebook) */
-	void (*apply_cb)(void);		/**< routine to call when preferences applied */
-	GList	*prefs;				/**< list of its preferences */
+    const char *name;           /**< name of module */
+    const char *title;          /**< title of module (displayed in preferences list) */
+    const char *description;    /**< Description of module (displayed in preferences notebook) */
+    void (*apply_cb)(void);     /**< routine to call when preferences applied */
+    GList *prefs;               /**< list of its preferences */
     struct pref_module *parent; /**< parent module */
-	emem_tree_t *submodules;	/**< list of its submodules */
-	int	numprefs;				/**< number of non-obsolete preferences */
-	gboolean prefs_changed;		/**< if TRUE, a preference has changed since we last checked */
-	gboolean obsolete;			/**< if TRUE, this is a module that used to
-								 * exist but no longer does
-								 */
+    emem_tree_t *submodules;    /**< list of its submodules */
+    int numprefs;               /**< number of non-obsolete preferences */
+    gboolean prefs_changed;     /**< if TRUE, a preference has changed since we last checked */
+    gboolean obsolete;          /**< if TRUE, this is a module that used to
+                                 * exist but no longer does
+                                 */
 };
 
 /**
@@ -58,58 +58,58 @@ WS_VAR_IMPORT module_t *protocols_module;
  * but no longer supports; we give different error messages for them.
  */
 typedef enum {
-	PREF_UINT,
-	PREF_BOOL,
-	PREF_ENUM,
-	PREF_STRING,
-	PREF_RANGE,
-	PREF_STATIC_TEXT,
-	PREF_UAT,
-	PREF_FILENAME,
-	PREF_OBSOLETE
+    PREF_UINT,
+    PREF_BOOL,
+    PREF_ENUM,
+    PREF_STRING,
+    PREF_RANGE,
+    PREF_STATIC_TEXT,
+    PREF_UAT,
+    PREF_FILENAME,
+    PREF_OBSOLETE
 } pref_type_t;
 
 /** Struct to hold preference data */
 struct preference {
-	const char *name;			/**< name of preference */
-	const char *title;			/**< title to use in GUI */
-	const char *description;	/**< human-readable description of preference */
-	int	ordinal;				/**< ordinal number of this preference */
-	pref_type_t type;			/**< type of that preference */
-	union {
-		guint *uint;
-		gboolean *boolp;
-		gint *enump;
-		const char **string;
-		range_t **range;
-		void* uat;
-	} varp;						/**< pointer to variable storing the value */
-	union {
-		guint uint;
-		gboolean boolval;
-		gint enumval;
-		char *string;
-		range_t *range;
-	} saved_val;				/**< original value, when editing from the GUI */
-	union {
-		guint uint;
-		gboolean boolval;
-		gint enumval;
-		char *string;
-		range_t *range;
-	} default_val;				/**< the default value of the preference */
-	union {
-	  guint base;				/**< input/output base, for PREF_UINT */
-	  guint32 max_value;		/**< maximum value of a range */
-	  struct {
-	    const enum_val_t *enumvals;	/**< list of name & values */
-	    gboolean radio_buttons;	/**< TRUE if it should be shown as
-								radio buttons rather than as an
-								option menu or combo box in
-								the preferences tab */
-	  } enum_info;				/**< for PREF_ENUM */
-	} info;						/**< display/text file information */
-	void	*control;			/**< handle for GUI control for this preference */
+    const char *name;                /**< name of preference */
+    const char *title;               /**< title to use in GUI */
+    const char *description;         /**< human-readable description of preference */
+    int ordinal;                     /**< ordinal number of this preference */
+    pref_type_t type;                /**< type of that preference */
+    union {
+        guint *uint;
+        gboolean *boolp;
+        gint *enump;
+        const char **string;
+        range_t **range;
+        void* uat;
+    } varp;                          /**< pointer to variable storing the value */
+    union {
+        guint uint;
+        gboolean boolval;
+        gint enumval;
+        char *string;
+        range_t *range;
+    } saved_val;                     /**< original value, when editing from the GUI */
+    union {
+        guint uint;
+        gboolean boolval;
+        gint enumval;
+        char *string;
+        range_t *range;
+    } default_val;                   /**< the default value of the preference */
+    union {
+      guint base;                    /**< input/output base, for PREF_UINT */
+      guint32 max_value;             /**< maximum value of a range */
+      struct {
+        const enum_val_t *enumvals;  /**< list of name & values */
+        gboolean radio_buttons;      /**< TRUE if it should be shown as
+                                          radio buttons rather than as an
+                                          option menu or combo box in
+                                          the preferences tab */
+      } enum_info;                   /**< for PREF_ENUM */
+    } info;                          /**< display/text file information */
+    void    *control;                /**< handle for GUI control for this preference */
 };
 
 /* read_prefs_file: read in a generic config file and do a callback to */
@@ -128,7 +128,5 @@ typedef prefs_set_pref_e (*pref_set_pair_cb) (gchar *key, gchar *value, void *pr
  */
 int
 read_prefs_file(const char *pf_path, FILE *pf, pref_set_pair_cb pref_set_pair_fct, void *private_data);
-
-
 
 #endif /* prefs-int.h */
