e6c5c00453fd3dd0f8c83c576b5053835048abbc
Anders Broman <anders.broman@ericsson.com>
2012-07-19 10:36:56 +0000

    gtk_container_add() -> gtk_box_pack_start().
    svn path=/trunk/; revision=43820

--- ui/gtk/about_dlg.c
+++ ui/gtk/about_dlg.c
@@ -85,13 +85,14 @@ about_wireshark(GtkWidget *parent _U_, GtkWidget *main_vb)
   /*icon = xpm_to_widget_from_parent(parent, wssplash_xpm);*/
   icon = xpm_to_widget(wssplash_xpm);
 
-  gtk_container_add(GTK_CONTAINER(main_vb), icon);
+  gtk_box_pack_start(GTK_BOX (main_vb), icon, FALSE, TRUE, 0);
+
 
   msg_label = gtk_label_new(title);
   message = g_strdup_printf("<span size=\"x-large\" weight=\"bold\">%s</span>", title);
   gtk_label_set_markup(GTK_LABEL(msg_label), message);
   g_free(message);
-  gtk_container_add(GTK_CONTAINER(main_vb), msg_label);
+  gtk_box_pack_start(GTK_BOX (main_vb), msg_label, FALSE, TRUE, 0);
 }
 
 static void
@@ -132,14 +133,15 @@ splash_new(const char *message)
     gtk_container_set_border_width(GTK_CONTAINER(main_box), 24);
     gtk_container_add(GTK_CONTAINER(win), main_box);
 
+
     about_wireshark(win, main_box);
 
     main_lb = gtk_label_new(message);
-    gtk_container_add(GTK_CONTAINER(main_box), main_lb);
+	gtk_box_pack_start(GTK_BOX (main_box), main_lb, FALSE, TRUE, 0);
     g_object_set_data(G_OBJECT(win), "splash_label", main_lb);
 
     main_lb = gtk_label_new("");
-    gtk_container_add(GTK_CONTAINER(main_box), main_lb);
+	gtk_box_pack_start(GTK_BOX (main_box), main_lb, FALSE, TRUE, 0);
     g_object_set_data(G_OBJECT(win), "protocol_label", main_lb);
 
     percentage_box = ws_gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 1, FALSE);
@@ -331,7 +333,8 @@ about_wireshark_page_new(void)
   g_free(message);
   gtk_label_set_justify(GTK_LABEL(msg_label), GTK_JUSTIFY_FILL);
   gtk_label_set_selectable(GTK_LABEL(msg_label), TRUE);
-  gtk_container_add(GTK_CONTAINER(main_box), msg_label);
+  gtk_box_pack_start(GTK_BOX (main_box), msg_label, FALSE, TRUE, 0);
+
 
   return main_box;
 }
