f4e322cd565ee1959dcce6999cf21ad1a372919e
Pascal Quantin <pascal.quantin@gmail.com>
2012-07-28 13:40:11 +0000

    Fix inverted dissection of Pre-emption Capability and Pre-emption Vulnerability
    svn path=/trunk/; revision=44091

--- epan/dissectors/packet-gtp.c
+++ epan/dissectors/packet-gtp.c
@@ -8584,7 +8584,7 @@ proto_register_gtp(void)
         },
         { &hf_gtp_qos_arp_pci,
           {"Pre-emption Capability (PCI)", "gtp.qos_arp_pci",
-          FT_BOOLEAN, 8, TFS(&tfs_enabled_disabled), 0x40,
+          FT_BOOLEAN, 8, TFS(&tfs_disabled_enabled), 0x40,
           NULL, HFILL}
         },
         { &hf_gtp_qos_arp_pl,
@@ -8594,7 +8594,7 @@ proto_register_gtp(void)
         },
         { &hf_gtp_qos_arp_pvi,
           {"Pre-emption Vulnerability (PVI)", "gtp.qos_arp_pvi",
-          FT_BOOLEAN, 8, TFS(&tfs_enabled_disabled), 0x01,
+          FT_BOOLEAN, 8, TFS(&tfs_disabled_enabled), 0x01,
           NULL, HFILL}
         },
         {&hf_gtp_qos_qci,
@@ -8934,7 +8934,7 @@ proto_register_gtp(void)
         },
         {&hf_gtp_earp_pvi,
          { "PVI Pre-emption Vulnerability", "gtp.EARP_pre_emption_par_vulnerability",
-           FT_UINT8, BASE_DEC, NULL, 0x01,
+           FT_BOOLEAN, 8, TFS(&tfs_disabled_enabled), 0x01,
            NULL, HFILL}
         },
         {&hf_gtp_earp_pl,
@@ -8944,7 +8944,7 @@ proto_register_gtp(void)
         },
         {&hf_gtp_earp_pci,
          { "PCI Pre-emption Capability", "gtp.EARP_pre_emption_Capability",
-           FT_UINT8, BASE_DEC, NULL, 0x40,
+           FT_BOOLEAN, 8, TFS(&tfs_disabled_enabled), 0x40,
            NULL, HFILL}
         },
         {&hf_gtp_cdr_app,
