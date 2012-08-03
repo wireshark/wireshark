e9ebe32dfd81ee73a9c272995d2054ed259e168b
Jeff Morriss <jeff.morriss.ws@gmail.com>
2012-08-03 14:47:32 +0000

    Fix the $ tag.
    svn path=/trunk/; revision=44246

--- epan/dissectors/packet-bpq.h
+++ epan/dissectors/packet-bpq.h
@@ -3,7 +3,7 @@
  * Routines for Amateur Packet Radio protocol dissection
  * Copyright 2005,2006,2007,2008,2009,2010,2012 R.W. Stearn <richard@rns-stearn.demon.co.uk>
  *
- * $Id: 
+ * $Id$
  *
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
