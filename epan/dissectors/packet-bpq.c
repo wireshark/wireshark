783405a2dae64d5506737ea532286a1b3f71b89d
Jeff Morriss <jeff.morriss.ws@gmail.com>
2012-08-03 14:46:12 +0000

    Replace blurbs which duplicate the field name with NULL. Don't use dissector_add(), it's long since deprecated.
    svn path=/trunk/; revision=44245

--- epan/dissectors/packet-bpq.c
+++ epan/dissectors/packet-bpq.c
@@ -13,12 +13,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
@@ -84,7 +84,7 @@ dissect_bpq( tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree )
 
 
 	col_set_str( pinfo->cinfo, COL_PROTOCOL, "BPQ" );
-    
+
 	col_clear( pinfo->cinfo, COL_INFO );
 
 	/* protocol offset for the BPQ header */
@@ -123,13 +123,13 @@ dissect_bpq( tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree )
 
 void
 proto_register_bpq(void)
-{                 
+{
 	/* Setup list of header fields */
 	static hf_register_info hf[] = {
 		{ &hf_bpq_len,
 			{ "BPQ len",			"bpq.len",
-			FT_UINT16, BASE_DEC, NULL, 0x0,          
-			"BPQ len", HFILL }
+			FT_UINT16, BASE_DEC, NULL, 0x0,
+			NULL, HFILL }
 		},
 	};
 
@@ -153,13 +153,13 @@ void
 proto_reg_handoff_bpq(void)
 {
         static gboolean inited = FALSE;
-        
-        if( !inited ) {
+
+        if(!inited) {
 
 		dissector_handle_t bpq_handle;
 
 		bpq_handle = create_dissector_handle( dissect_bpq, proto_bpq );
-		dissector_add("ethertype", ETHERTYPE_BPQ, bpq_handle);
+		dissector_add_uint("ethertype", ETHERTYPE_BPQ, bpq_handle);
 
 		/* BPQ is only implemented for AX.25 */
 		ax25_handle     = find_dissector( "ax25" );
