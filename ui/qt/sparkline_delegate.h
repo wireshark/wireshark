74a3a6e405271b8c6eae99cfa2ef746e3409cc94
Jörg Mayer <jmayer@loplof.de>
2012-08-06 10:59:00 +0000

    Don't call "Q_OBJECT" if there are no signals or slots defined.
    Fix cmake (and possibly autotools) build for:
    
    lib/libqtui.a(interface_tree.cpp.o): In function `SparkLineDelegate':
    /home/jmayer/work/wireshark/svn/trunk/ui/qt/sparkline_delegate.h:10: undefined reference to `vtable for SparkLineDelegate'
    
    This would not show up using qtcreator due to the magic of qmake.
    
    I must admit that I don't fully understand what I am doing here, but
    I read the following thread to the bottom and one of the last posts
    contained the solution:
    http://stackoverflow.com/questions/1095298/gcc-c-linker-errors-undefined-reference-to-vtable-for-xxx-undefined-refere
    
    
    svn path=/trunk/; revision=44280

--- ui/qt/sparkline_delegate.h
+++ ui/qt/sparkline_delegate.h
@@ -5,7 +5,6 @@
 
 class SparkLineDelegate : public QStyledItemDelegate
 {
-    Q_OBJECT
 public:
     SparkLineDelegate(QWidget *parent = 0) : QStyledItemDelegate(parent) {}
 
@@ -15,9 +14,9 @@ public:
                    const QModelIndex &index) const;
 
 signals:
-    
+
 public slots:
-    
+
 };
 
 Q_DECLARE_METATYPE(QList<int> *)
