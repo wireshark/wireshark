f1d777a40439189d4f93143ede82c76f201945b8
Gerald Combs <gerald@wireshark.org>
2012-08-15 20:27:17 +0000

    1.8.2 → 1.8.3
    svn path=/trunk-1.8/; revision=44532

--- make-version.pl
+++ make-version.pl
@@ -75,7 +75,7 @@ my $set_release = 0;
 my %version_pref = (
 	"version_major" => 1,
 	"version_minor" => 8,
-	"version_micro" => 2,
+	"version_micro" => 3,
 	"version_build" => 0,
 
 	"enable"        => 1,
