wireshark/wireshark

commit 28e925a381d428ca1ae475a28e021235dc5a8cf6

Allow future forward compatibility
svn path=/trunk/; revision=43312

epan/dissectors/packet-gtp.c
@@ -4218,7 +4218,7 @@ decode_qos_umts(tvbuff_t * tvb, int offset, proto_tree * tree, const gchar * qos
         break;
     }
 
-    if ((type == 3) && (rel_ind == 8)) {
+    if ((type == 3) && (rel_ind >= 8)) {
         /* Release 8 or higher P-GW QoS profile */
         offset++;
         arp = wrapped_tvb_get_guint8(tvb, offset, 2);

commit 1c2f498a8227a5fc09718e96e4cdb48bd2f9e269

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7697 : Fix dissection of release 98 and release 99 QoS profiles (bug introduced in r43312)
svn path=/trunk/; revision=44792

epan/dissectors/packet-gtp.c
@@ -4222,7 +4222,7 @@ decode_qos_umts(tvbuff_t * tvb, int offset, proto_tree * tree, const gchar * qos
         break;
     }
 
-    if ((type == 3) && (rel_ind >= 8)) {
+    if ((type == 3) && (rel_ind == 8)) {
         /* Release 8 or higher P-GW QoS profile */
         offset++;
         arp = wrapped_tvb_get_guint8(tvb, offset, 2);
@@ -8463,7 +8463,7 @@ proto_register_gtp(void)
         },
         {&hf_gtp_qos_version,
          { "Version", "gtp.qos_version",
-           FT_UINT8, BASE_DEC, NULL, 0,
+           FT_UINT8, BASE_HEX, NULL, 0,
            "Version of the QoS Profile", HFILL}
         },
         {&hf_gtp_qos_spare1,