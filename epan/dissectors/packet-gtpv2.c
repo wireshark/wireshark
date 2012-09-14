9ec55f59d77882b88d00b32005d21971d4d1ac93
Anders Broman <anders.broman@ericsson.com>
2012-09-14 08:24:35 +0000

    From Claude LeFrancois: Cosmetic changes to GTPv2(Add text to itenms) Minor code changes by me.
    svn path=/trunk/; revision=44891

--- epan/dissectors/packet-gtpv2.c
+++ epan/dissectors/packet-gtpv2.c
@@ -4413,7 +4413,7 @@ dissect_gtpv2_mbms_session_duration(tvbuff_t *tvb, packet_info *pinfo _U_, proto
  * 8.70 MBMS Service Area
  */
 void
-dissect_gtpv2_mbms_service_area(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
+dissect_gtpv2_mbms_service_area(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
 {
     int         offset = 0;
     proto_item *sai_item;
@@ -4437,6 +4437,7 @@ dissect_gtpv2_mbms_service_area(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tre
         if(sai == 0) {
             proto_item_append_text(sai_item, " Entire PLMN");
         }
+		proto_item_append_text(item, " %u", sai);
         offset += 2;
     }
 }
@@ -4460,11 +4461,12 @@ dissect_gtpv2_mbms_session_id(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree 
  * 8.72 MBMS Flow Identifier
  */
 static void
-dissect_gtpv2_mbms_flow_id(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
+dissect_gtpv2_mbms_flow_id(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
 {
     int offset = 0;
     /* Two octets OctetString. */
     proto_tree_add_item(tree, hf_gtpv2_mbms_flow_id, tvb, offset, 2, ENC_NA);
+    proto_item_append_text(item, " %s", tvb_bytes_to_str(tvb, offset, 2));
 
     offset += 2;
     if(length > 2)
@@ -4481,7 +4483,7 @@ static const value_string gtpv2_mbms_hc_indicator_vals[] = {
 };
 
 static void
-dissect_gtpv2_mbms_ip_mc_dist(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
+dissect_gtpv2_mbms_ip_mc_dist(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
 {
     int offset = 0;
 
@@ -4494,10 +4496,12 @@ dissect_gtpv2_mbms_ip_mc_dist(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree 
     if((tvb_get_guint8(tvb, offset)&0x3f) == 4) {
         offset += 1;
         proto_tree_add_item(tree, hf_gtpv2_mbms_ip_mc_dist_addrv4, tvb, offset, 4, ENC_BIG_ENDIAN);
+        proto_item_append_text(item, " IPv4 Dist %s", tvb_ip_to_str(tvb, offset));
         offset += 4;
     } else if((tvb_get_guint8(tvb, offset)&0x3f) == 16) {
         offset += 1;
         proto_tree_add_item(tree, hf_gtpv2_mbms_ip_mc_dist_addrv6, tvb, offset, 16, ENC_NA);
+        proto_item_append_text(item, " IPv6 Dist %s", tvb_ip6_to_str(tvb, offset));
         offset += 16;
     }
 
@@ -4507,10 +4511,12 @@ dissect_gtpv2_mbms_ip_mc_dist(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree 
     if((tvb_get_guint8(tvb, offset)&0x3f) == 4) {
         offset += 1;
         proto_tree_add_item(tree, hf_gtpv2_mbms_ip_mc_src_addrv4, tvb, offset, 4, ENC_BIG_ENDIAN);
+        proto_item_append_text(item, " IPv4 Src %s", tvb_ip_to_str(tvb, offset));
         offset += 4;
     } else if((tvb_get_guint8(tvb, offset)&0x3f) == 16) {
         offset += 1;
         proto_tree_add_item(tree, hf_gtpv2_mbms_ip_mc_src_addrv6, tvb, offset, 16, ENC_NA);
+        proto_item_append_text(item, " IPv6 Src %s", tvb_ip6_to_str(tvb, offset));
         offset += 16;
     }
 
@@ -4650,7 +4656,7 @@ dissect_gtpv2_node_features(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *t
  * MBMS Time to Data Transfer
  */
 void
-dissect_gtpv2_mbms_time_to_data_xfer(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
+dissect_gtpv2_mbms_time_to_data_xfer(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
 {
     int     offset = 0;
     guint8  binary_secs;
@@ -4660,7 +4666,7 @@ dissect_gtpv2_mbms_time_to_data_xfer(tvbuff_t *tvb, packet_info *pinfo _U_, prot
     real_secs = (guint16)binary_secs + 1;
 
     proto_tree_add_string_format(tree, hf_gtpv2_time_to_data_xfer, tvb, offset, 1, "", "MBMS Time to Data Transfer: %d second(s)", real_secs);
-
+    proto_item_append_text(item, " %u second(s)", real_secs);
     offset += 1;
     if(length > 1)
         proto_tree_add_text(tree, tvb, offset, length-1, "Spare: %s", tvb_bytes_to_str(tvb, offset, length-1));
