201c4c45bf0bbc34cff22de81756654333fbdd1e
Bill Meier <wmeier@newsguy.com>
2012-09-15 14:46:13 +0000

    Trivial whitespace formatting cleanup;
    svn path=/trunk/; revision=44919

--- ui/gtk/rpc_progs.c
+++ ui/gtk/rpc_progs.c
@@ -54,39 +54,40 @@
 
 #define NANOSECS_PER_SEC 1000000000
 
-static GtkWidget *win=NULL;
-static GtkWidget *table=NULL;
-static int num_progs=0;
+static GtkWidget *win   = NULL;
+static GtkWidget *table = NULL;
+static int num_progs    = 0;
 
 /* used to keep track of statistics for a specific program/version */
 typedef struct _rpc_program_t {
 	struct _rpc_program_t *next;
-	guint32 program;
+
+	guint32    program;
 	GtkWidget *wprogram;
-	gchar sprogram[24];
+	gchar      sprogram[24];
 
-	guint32 version;
+	guint32    version;
 	GtkWidget *wversion;
-	gchar sversion[16];
+	gchar      sversion[16];
 
-	int num;
+	int        num;
 	GtkWidget *wnum;
-	gchar snum[16];
+	gchar      snum[16];
 
-	nstime_t min;
+	nstime_t   min;
 	GtkWidget *wmin;
-	gchar smin[16];
+	gchar      smin[16];
 
-	nstime_t max;
+	nstime_t   max;
 	GtkWidget *wmax;
-	gchar smax[16];
+	gchar      smax[16];
 
-	nstime_t tot;
+	nstime_t   tot;
 	GtkWidget *wavg;
-	gchar savg[16];
+	gchar      savg[16];
 } rpc_program_t;
 
-static rpc_program_t *prog_list=NULL;
+static rpc_program_t *prog_list = NULL;
 
 
 static char *
@@ -107,26 +108,26 @@ rpcprogs_reset(void *dummy _U_)
 {
 	rpc_program_t *rp;
 
-	while(prog_list){
-		rp=prog_list;
-		prog_list=prog_list->next;
+	while(prog_list) {
+		rp = prog_list;
+		prog_list = prog_list->next;
 
 		gtk_widget_destroy(rp->wprogram);
-		rp->wprogram=NULL;
+		rp->wprogram = NULL;
 		gtk_widget_destroy(rp->wversion);
-		rp->wversion=NULL;
+		rp->wversion = NULL;
 		gtk_widget_destroy(rp->wnum);
-		rp->wnum=NULL;
+		rp->wnum     = NULL;
 		gtk_widget_destroy(rp->wmin);
-		rp->wmin=NULL;
+		rp->wmin     = NULL;
 		gtk_widget_destroy(rp->wmax);
-		rp->wmax=NULL;
+		rp->wmax     = NULL;
 		gtk_widget_destroy(rp->wavg);
-		rp->wavg=NULL;
+		rp->wavg     = NULL;
 		g_free(rp);
 	}
 	gtk_table_resize(GTK_TABLE(table), 1, 6);
-	num_progs=0;
+	num_progs = 0;
 }
 
 static void
@@ -134,32 +135,38 @@ add_new_program(rpc_program_t *rp)
 {
 	num_progs++;
 	gtk_table_resize(GTK_TABLE(table), num_progs+1, 6);
-	rp->wprogram=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wprogram, 0,1,num_progs,num_progs+1);
+
+	rp->wprogram = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wprogram, 0, 1, num_progs, num_progs+1);
 	gtk_widget_show(rp->wprogram);
-	rp->wversion=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wversion, 1,2,num_progs,num_progs+1);
+
+	rp->wversion = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wversion, 1, 2, num_progs, num_progs+1);
 	gtk_widget_show(rp->wversion);
-	rp->wnum=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wnum, 2,3,num_progs,num_progs+1);
+
+	rp->wnum = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wnum,     2, 3, num_progs, num_progs+1);
 	gtk_widget_show(rp->wnum);
-	rp->wmin=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wmin, 3,4,num_progs,num_progs+1);
+
+	rp->wmin = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wmin,     3, 4, num_progs, num_progs+1);
 	gtk_widget_show(rp->wmin);
-	rp->wmax=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wmax, 4,5,num_progs,num_progs+1);
+
+	rp->wmax = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wmax,     4, 5, num_progs, num_progs+1);
 	gtk_widget_show(rp->wmax);
-	rp->wavg=gtk_label_new("0");
-	gtk_table_attach_defaults(GTK_TABLE(table), rp->wavg, 5,6,num_progs,num_progs+1);
+
+	rp->wavg = gtk_label_new("0");
+	gtk_table_attach_defaults(GTK_TABLE(table), rp->wavg,     5, 6, num_progs, num_progs+1);
 	gtk_widget_show(rp->wavg);
 
-	rp->num=0;
-	rp->min.secs=0;
-	rp->min.nsecs=0;
-	rp->max.secs=0;
-	rp->max.nsecs=0;
-	rp->tot.secs=0;
-	rp->tot.nsecs=0;
+	rp->num       = 0;
+	rp->min.secs  = 0;
+	rp->min.nsecs = 0;
+	rp->max.secs  = 0;
+	rp->max.nsecs = 0;
+	rp->tot.secs  = 0;
+	rp->tot.nsecs = 0;
 }
 
 
@@ -168,50 +175,49 @@ static gboolean
 rpcprogs_packet(void *dummy _U_, packet_info *pinfo, epan_dissect_t *edt _U_, const void *arg)
 {
 	const rpc_call_info_value *ri = arg;
-	nstime_t delta;
+	nstime_t       delta;
 	rpc_program_t *rp;
 
-	if(!prog_list){
+	if (!prog_list) {
 		/* the list was empty */
-		rp=g_malloc(sizeof(rpc_program_t));
+		rp = g_malloc(sizeof(rpc_program_t));
 		add_new_program(rp);
-		rp->next=NULL;
-		rp->program=ri->prog;
-		rp->version=ri->vers;
-		prog_list=rp;
-	} else if((ri->prog==prog_list->program)
-		&&(ri->vers==prog_list->version)){
-		rp=prog_list;
-	} else if( (ri->prog<prog_list->program)
-		||((ri->prog==prog_list->program)&&(ri->vers<prog_list->version))){
+		rp->next = NULL;
+		rp->program = ri->prog;
+		rp->version = ri->vers;
+		prog_list = rp;
+	} else if ((ri->prog == prog_list->program) && (ri->vers == prog_list->version)) {
+		rp = prog_list;
+	} else if ( (ri->prog<prog_list->program)
+		||  ((ri->prog == prog_list->program) && (ri->vers<prog_list->version))) {
 		/* we should be first entry in list */
-		rp=g_malloc(sizeof(rpc_program_t));
+		rp = g_malloc(sizeof(rpc_program_t));
 		add_new_program(rp);
-		rp->next=prog_list;
-		rp->program=ri->prog;
-		rp->version=ri->vers;
-		prog_list=rp;
+		rp->next = prog_list;
+		rp->program = ri->prog;
+		rp->version = ri->vers;
+		prog_list = rp;
 	} else {
 		/* we go somewhere else in the list */
-		for(rp=prog_list;rp;rp=rp->next){
-			if((rp->next)
-			&& (rp->next->program==ri->prog)
-			&& (rp->next->version==ri->vers)){
-				rp=rp->next;
+		for(rp = prog_list; rp; rp = rp->next) {
+			if ((rp->next)
+			&&  (rp->next->program == ri->prog)
+			&&  (rp->next->version == ri->vers)) {
+				rp = rp->next;
 				break;
 			}
-			if((!rp->next)
+			if ((!rp->next)
 			|| (rp->next->program>ri->prog)
-			|| (  (rp->next->program==ri->prog)
-			    &&(rp->next->version>ri->vers))){
+			|| (  (rp->next->program == ri->prog)
+			    &&(rp->next->version>ri->vers))) {
 				rpc_program_t *trp;
-				trp=g_malloc(sizeof(rpc_program_t));
+				trp = g_malloc(sizeof(rpc_program_t));
 				add_new_program(trp);
-				trp->next=rp->next;
-				trp->program=ri->prog;
-				trp->version=ri->vers;
-				rp->next=trp;
-				rp=trp;
+				trp->next    = rp->next;
+				trp->program = ri->prog;
+				trp->version = ri->vers;
+				rp->next = trp;
+				rp = trp;
 				break;
 			}
 		}
@@ -219,43 +225,43 @@ rpcprogs_packet(void *dummy _U_, packet_info *pinfo, epan_dissect_t *edt _U_, co
 
 
 	/* we are only interested in reply packets */
-	if(ri->request || !rp){
+	if (ri->request || !rp) {
 		return FALSE;
 	}
 
 	/* calculate time delta between request and reply */
 	nstime_delta(&delta, &pinfo->fd->abs_ts, &ri->req_time);
 
-	if((rp->max.secs==0)
-	&& (rp->max.nsecs==0) ){
-		rp->max.secs=delta.secs;
-		rp->max.nsecs=delta.nsecs;
+	if ((rp->max.secs  == 0)
+	&&  (rp->max.nsecs == 0) ) {
+		rp->max.secs  = delta.secs;
+		rp->max.nsecs = delta.nsecs;
 	}
 
-	if((rp->min.secs==0)
-	&& (rp->min.nsecs==0) ){
-		rp->min.secs=delta.secs;
-		rp->min.nsecs=delta.nsecs;
+	if ((rp->min.secs  == 0)
+	&&  (rp->min.nsecs == 0) ) {
+		rp->min.secs  = delta.secs;
+		rp->min.nsecs = delta.nsecs;
 	}
 
-	if( (delta.secs<rp->min.secs)
-	||( (delta.secs==rp->min.secs)
-	  &&(delta.nsecs<rp->min.nsecs) ) ){
-		rp->min.secs=delta.secs;
-		rp->min.nsecs=delta.nsecs;
+	if ( (delta.secs<rp->min.secs)
+        ||   ( (delta.secs == rp->min.secs)
+            && (delta.nsecs<rp->min.nsecs) ) ) {
+		rp->min.secs  = delta.secs;
+		rp->min.nsecs = delta.nsecs;
 	}
 
-	if( (delta.secs>rp->max.secs)
-	||( (delta.secs==rp->max.secs)
-	  &&(delta.nsecs>rp->max.nsecs) ) ){
-		rp->max.secs=delta.secs;
-		rp->max.nsecs=delta.nsecs;
+	if ( (delta.secs>rp->max.secs)
+        ||   ( (delta.secs == rp->max.secs)
+	    && (delta.nsecs>rp->max.nsecs) ) ) {
+		rp->max.secs  = delta.secs;
+		rp->max.nsecs = delta.nsecs;
 	}
 
-	rp->tot.secs += delta.secs;
+	rp->tot.secs  += delta.secs;
 	rp->tot.nsecs += delta.nsecs;
-	if(rp->tot.nsecs>NANOSECS_PER_SEC){
-		rp->tot.nsecs-=NANOSECS_PER_SEC;
+	if (rp->tot.nsecs > NANOSECS_PER_SEC) {
+		rp->tot.nsecs -= NANOSECS_PER_SEC;
 		rp->tot.secs++;
 	}
 	rp->num++;
@@ -268,12 +274,12 @@ static void
 rpcprogs_draw(void *dummy _U_)
 {
 	rpc_program_t *rp;
-	int i;
-	guint64 td;
+	int            i;
+	guint64        td;
 
-	for(rp=prog_list,i=1;rp;rp=rp->next,i++){
+	for(rp=prog_list,i=1; rp; rp=rp->next,i++) {
 		/* Ignore procedures with no calls */
-		if(rp->num==0){
+		if (rp->num == 0) {
 			continue;
 		}
 		/* Scale the average SRT in units of 1us and round to the nearest us.
@@ -305,11 +311,11 @@ rpcprogs_draw(void *dummy _U_)
 	}
 }
 
-/* since the gtk2 implementation of tap is multithreaded we must protect
+/* Since the gtk2 implementation of tap is multithreaded we must protect
  * remove_tap_listener() from modifying the list while draw_tap_listener()
- * is running.  the other protected block is in main.c
+ * is running.  The other protected block is in main.c
  *
- * there should not be any other critical regions in gtk2
+ * There should not be any other critical regions in gtk2
  */
 static void
 win_destroy_cb(void *dummy _U_, gpointer data _U_)
@@ -320,13 +326,13 @@ win_destroy_cb(void *dummy _U_, gpointer data _U_)
 	remove_tap_listener(win);
 	unprotect_thread_critical_region();
 
-	win=NULL;
-	for(rp=prog_list;rp;){
+	win = NULL;
+	for(rp=prog_list;rp;) {
 		rp2=rp->next;
 		g_free(rp);
 		rp=rp2;
 	}
-	prog_list=NULL;
+	prog_list = NULL;
 }
 
 
@@ -335,15 +341,15 @@ win_destroy_cb(void *dummy _U_, gpointer data _U_)
 static void
 gtk_rpcprogs_init(const char *optarg _U_, void* userdata _U_)
 {
-	char *title_string;
+	char      *title_string;
 	GtkWidget *vbox;
 	GtkWidget *stat_label;
 	GtkWidget *tmp;
-	GString *error_string;
+	GString   *error_string;
 	GtkWidget *bt_close;
 	GtkWidget *bbox;
 
-	if(win){
+	if (win) {
 		gdk_window_raise(gtk_widget_get_window(win));
 		return;
 	}
@@ -352,44 +358,44 @@ gtk_rpcprogs_init(const char *optarg _U_, void* userdata _U_)
 	win = dlg_window_new(title_string);  /* transient_for top_level */
 	gtk_window_set_destroy_with_parent (GTK_WINDOW(win), TRUE);
 
-	vbox=ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 3, FALSE);
+	vbox = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 3, FALSE);
 	gtk_container_add(GTK_CONTAINER(win), vbox);
 	gtk_container_set_border_width(GTK_CONTAINER(vbox), 12);
 
-	stat_label=gtk_label_new(title_string);
+	stat_label = gtk_label_new(title_string);
 	g_free(title_string);
 	gtk_box_pack_start(GTK_BOX(vbox), stat_label, FALSE, FALSE, 0);
 
 
-	table=gtk_table_new(1, 5, TRUE);
+	table = gtk_table_new(1, 5, TRUE);
 	gtk_container_add(GTK_CONTAINER(vbox), table);
 
-	tmp=gtk_label_new("Program");
+	tmp = gtk_label_new("Program");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 0,1,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_LEFT);
 
-	tmp=gtk_label_new("Version");
+	tmp = gtk_label_new("Version");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 1,2,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_RIGHT);
 
-	tmp=gtk_label_new("Calls");
+	tmp = gtk_label_new("Calls");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 2,3,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_RIGHT);
 
-	tmp=gtk_label_new("Min SRT");
+	tmp = gtk_label_new("Min SRT");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 3,4,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_RIGHT);
 
-	tmp=gtk_label_new("Max SRT");
+	tmp = gtk_label_new("Max SRT");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 4,5,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_RIGHT);
 
-	tmp=gtk_label_new("Avg SRT");
+	tmp = gtk_label_new("Avg SRT");
 	gtk_table_attach_defaults(GTK_TABLE(table), tmp, 5,6,0,1);
 	gtk_label_set_justify(GTK_LABEL(tmp), GTK_JUSTIFY_RIGHT);
 
-	error_string=register_tap_listener("rpc", win, NULL, 0, rpcprogs_reset, rpcprogs_packet, rpcprogs_draw);
-	if(error_string){
+	error_string = register_tap_listener("rpc", win, NULL, 0, rpcprogs_reset, rpcprogs_packet, rpcprogs_draw);
+	if (error_string) {
 		fprintf(stderr, "wireshark: Couldn't register rpc,programs tap: %s\n",
 		    error_string->str);
 		g_string_free(error_string, TRUE);
