f7030c6940e6db43a111dd5a3513ccf7165de85a
Guy Harris <guy@alum.mit.edu>
2012-09-27 19:35:23 +0000

    Get rid of unused variable.
    svn path=/trunk/; revision=45179

--- epan/dissectors/packet-q931.c
+++ epan/dissectors/packet-q931.c
@@ -1439,11 +1439,8 @@ static const value_string q931_new_status_vals[] = {
 static void
 dissect_q931_change_status_ie(tvbuff_t *tvb, int offset, int len _U_, proto_tree *tree)
 {
-	guint8 octet;
-
 	if (len == 0)
 		return;
-	octet = tvb_get_guint8(tvb, offset);
 
 	proto_tree_add_item(tree, hf_q931_extension_ind, tvb, offset, 1, ENC_BIG_ENDIAN);
 	proto_tree_add_item(tree, hf_q931_extension_ind_preference, tvb, offset, 1, ENC_BIG_ENDIAN);
