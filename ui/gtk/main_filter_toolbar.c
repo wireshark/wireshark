wireshark/wireshark

commit 7afb363eab3c033f04dd34430117e014617afd36

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=6892 :
Sort the recent filters list by "most recently used" (that is, put the most
recently used filter at the head of the list).

svn path=/trunk/; revision=42439

ui/gtk/main_filter_toolbar.c
@@ -300,7 +300,7 @@ dfilter_entry_match(GtkWidget *filter_cm, char *s, int *index)
         *index = i;
         return FALSE;
     }
-    do{
+    do {
         i++;
         gtk_tree_model_get_value (model, &iter, 0, &value);
         filter_str = g_value_get_string (&value);
@@ -345,14 +345,14 @@ dfilter_recent_combo_write_all(FILE *rf) {
 
     if (!gtk_tree_model_get_iter_first (model, &iter))
         return;
-    do{
+    do {
         gtk_tree_model_get_value (model, &iter, 0, &value);
         filter_str = g_value_get_string (&value);
         if(filter_str)
             fprintf (rf, RECENT_KEY_DISPLAY_FILTER ": %s\n", filter_str);
         g_value_unset (&value);
 
-    }while (gtk_tree_model_iter_next (model, &iter)&& (max_count++ < prefs.gui_recent_df_entries_max));
+    } while (gtk_tree_model_iter_next (model, &iter)&& (max_count++ < prefs.gui_recent_df_entries_max));
 
 }
 
@@ -397,10 +397,20 @@ main_filter_packets(capture_file *cf, const gchar *dftext, gboolean force)
     if (cf_status == CF_OK && strlen(s) > 0) {
         int index;
 
-        if(!dfilter_entry_match(filter_cm,s, &index)) {
+        if(!dfilter_entry_match(filter_cm,s, &index) || index != 0) {
+
+            /* If the filter is already there but not the first entry, remove it */
+            if (index > 0) {
+                gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
+                index--;
+            }
+
+            /* Add the filter (at the head of the list) */
             gtk_combo_box_text_prepend_text(GTK_COMBO_BOX_TEXT(filter_cm), s);
             index++;
         }
+
+        /* If we have too many entries, remove some */
         while ((guint)index >= prefs.gui_recent_df_entries_max) {
             gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
             index--;

commit 8559469419bbff21dc8830d95ec6f7d84b35c596

Copy over: evision 44885 - Also disable the resize grip on the profile bar (as it is on the info and packets bars; this is for GTK < 3.0).
This fixes the problem I had that the profile bar normally took up way too
much (horizontal) room when I start Wireshark (causing the contents of the
much more interesting packets bar to be truncated).


Revision 45012 - Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7731 :

Allow the configured number of dfilters to be added to the drop-down list
(instead of limiting the list--on a freshly installed system with no recent
dfilters--to just 2).  Regression caused by r42439.
--------------------------------------------------------------------------------
Revision 45108 - allow setting a capture comment from the statusbar when a capture was started

for the decision to allow input of a capture comment, we should make no
difference between the statusbar icon and Statistics / Summary menu item

svn path=/trunk-1.8/; revision=45171

ui/gtk/main_filter_toolbar.c
@@ -19,7 +19,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 /*
@@ -314,7 +314,7 @@ dfilter_entry_match(GtkWidget *filter_cm, char *s, int *index)
         g_value_unset (&value);
     } while (gtk_tree_model_iter_next (model, &iter));
 
-    *index = i;
+    *index = -1;
     return FALSE;
 }
 
@@ -397,10 +397,10 @@ main_filter_packets(capture_file *cf, const gchar *dftext, gboolean force)
     if (cf_status == CF_OK && strlen(s) > 0) {
         int index;
 
-        if(!dfilter_entry_match(filter_cm,s, &index) || index != 0) {
+        if(!dfilter_entry_match(filter_cm,s, &index) || index > -1) {
 
             /* If the filter is already there but not the first entry, remove it */
-            if (index > 0) {
+            if (index > -1) {
                 gtk_combo_box_text_remove(GTK_COMBO_BOX_TEXT(filter_cm), index);
                 index--;
             }

ui/gtk/main_statusbar.c
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 
@@ -470,6 +470,9 @@ profile_bar_new(void)
     profile_ctx = gtk_statusbar_get_context_id(GTK_STATUSBAR(profile_bar), "profile");
     gtk_widget_set_tooltip_text(profile_bar_event, "Click to change configuration profile");
     profile_bar_update();
+#if !GTK_CHECK_VERSION(3,0,0)
+    gtk_statusbar_set_has_resize_grip(GTK_STATUSBAR(profile_bar), FALSE);
+#endif
 
     gtk_widget_show(profile_bar);
     gtk_widget_show(profile_bar_event);
@@ -811,6 +814,8 @@ statusbar_capture_update_started_cb(capture_options *capture_opts)
                             interface_names->str,
                             (capture_opts->save_file) ? capture_opts->save_file : "");
     g_string_free(interface_names, TRUE);
+
+    status_capture_comment_update();
 }
 
 static void