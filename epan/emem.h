ea3574c54ec135d6cde79ea3fbaa2467e404b780
Jaap Keuter <jaap.keuter@xs4all.nl>
2012-10-06 16:21:36 +0000

    ------------------------------------------------------------------------ r45340 | jake | 2012-10-06 16:35:59 +0200 (Sat, 06 Oct 2012) | 2 lines
    Vastly improve the debug presentation( of trees)+. 
    
    ------------------------------------------------------------------------
    
    svn path=/trunk-1.8/; revision=45348

--- epan/emem.h
+++ epan/emem.h
@@ -515,6 +515,13 @@ emem_strbuf_t *ep_strbuf_append_c(emem_strbuf_t *strbuf, const gchar c);
  */
 emem_strbuf_t *ep_strbuf_truncate(emem_strbuf_t *strbuf, gsize len);
 
+/**
+ * Dump the whole tree (of trees) to stdout.
+ *
+ * @param emem_tree The tree to dump to standard output.
+ *
+ * @return void
+ */
 void emem_print_tree(emem_tree_t* emem_tree);
 
 /* #define DEBUG_INTENSE_CANARY_CHECKS */
