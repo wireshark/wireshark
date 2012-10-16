691125d64392d12d5a70082b2f4626d0697e11c5
Anders Broman <anders.broman@ericsson.com>
2012-10-16 14:42:25 +0000

    Copy over: Revision 45585 - '*' wildcard in the 'Src IP' or 'Dest IP' field of the ESP SA dialog does not work
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7866
    
    svn path=/trunk-1.8/; revision=45586

--- epan/dissectors/packet-ipsec.c
+++ epan/dissectors/packet-ipsec.c
@@ -19,7 +19,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 
@@ -594,8 +594,10 @@ filter_address_match(gchar *addr, gchar *filter, gint typ)
   guint addr_len = (guint)strlen(addr);
   guint filter_len = (guint)strlen(filter);
 
-  if(addr_len != filter_len)
-     return FALSE;
+   if((filter_len == 1) && (filter[0] == IPSEC_SA_WILDCARDS_ANY))
+      return TRUE;
+   else if(addr_len != filter_len)
+	  return FALSE;
 
   /* No length specified */
    if( ((typ == IPSEC_SA_IPV6) && (filter_len > IPSEC_IPV6_ADDR_LEN))
