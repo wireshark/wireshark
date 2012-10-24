4438309f5eb607f746da7970b81f0406120df96d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2012-10-24 15:51:38 +0000

    Add Modelines info for new Qtshark source file(s)
    svn path=/trunk/; revision=45760

--- ui/qt/export_dissection_dialog.cpp
+++ ui/qt/export_dissection_dialog.cpp
@@ -246,3 +246,16 @@ void ExportDissectionDialog::on_buttonBox_helpRequested()
     wsApp->helpTopicAction(HELP_EXPORT_FILE_DIALOG);
 }
 #endif // Q_WS_WIN
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
--- ui/qt/export_dissection_dialog.h
+++ ui/qt/export_dissection_dialog.h
@@ -75,3 +75,16 @@ private:
 };
 
 #endif // EXPORT_DISSECTION_DIALOG_H
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
