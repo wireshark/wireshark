686e72c011e082bd6a3daeb27e685873f2d8b285
Alexis La Goutte <alexis.lagoutte@gmail.com>
2012-11-04 16:01:11 +0000

    Add Modelines infos to new Qtshark file (print_dialog.*)
    svn path=/trunk/; revision=45892

--- ui/qt/print_dialog.cpp
+++ ui/qt/print_dialog.cpp
@@ -319,3 +319,16 @@ void PrintDialog::on_buttonBox_clicked(QAbstractButton *button)
         break;
     }
 }
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
