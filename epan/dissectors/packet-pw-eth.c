35b912bcbf7674f34e934c583ecfcea40c74bf64
Bill Meier <wmeier@newsguy.com>
2012-12-06 15:15:05 +0000

    Fix "MPLS payload starting with valid-looking ethernet address dissected twice".
    Fixes Bug #8043
    https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8043
    
    
    svn path=/trunk/; revision=46420

--- epan/dissectors/packet-pw-eth.c
+++ epan/dissectors/packet-pw-eth.c
@@ -155,11 +155,9 @@ dissect_pw_eth_heuristic(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
     guint8 first_nibble = (tvb_get_guint8(tvb, 0) >> 4) & 0x0F;
 
-    if (looks_like_plain_eth(tvb)) {
+    if (looks_like_plain_eth(tvb))
         call_dissector(pw_eth_handle_nocw, tvb, pinfo, tree);
-    }
-
-    if (first_nibble == 0)
+    else if (first_nibble == 0)
         call_dissector(pw_eth_handle_cw, tvb, pinfo, tree);
     else
         call_dissector(pw_eth_handle_nocw, tvb, pinfo, tree);
