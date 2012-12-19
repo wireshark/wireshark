acab4fe903cc00dc62ff7d7781e96843d10b32e6
Anders Broman <anders.broman@ericsson.com>
2012-12-19 06:52:56 +0000

    Use the new bundle
    svn path=/trunk/; revision=46613

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2012-12-11"
+export DOWNLOAD_TAG="2012-12-19"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
