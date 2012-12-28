c6ede6cf558693879b636c880ee17b60556b2b6d
Guy Harris <guy@alum.mit.edu>
2012-12-28 09:10:59 +0000

    Use the "if (file_gets(...) == NULL) { fail }" style, as used elsewhere.
    svn path=/trunk/; revision=46822

--- wiretap/netscreen.c
+++ wiretap/netscreen.c
@@ -154,22 +154,21 @@ static gboolean netscreen_check_file_type(wtap *wth, int *err, gchar **err_info)
 	buf[NETSCREEN_LINE_LENGTH-1] = '\0';
 
 	for (line = 0; line < NETSCREEN_HEADER_LINES_TO_CHECK; line++) {
-		if (file_gets(buf, NETSCREEN_LINE_LENGTH, wth->fh) != NULL) {
-
-			reclen = (guint) strlen(buf);
-			if (reclen < strlen(NETSCREEN_HDR_MAGIC_STR1) ||
-				reclen < strlen(NETSCREEN_HDR_MAGIC_STR2)) {
-				continue;
-			}
-
-			if (strstr(buf, NETSCREEN_HDR_MAGIC_STR1) ||
-			    strstr(buf, NETSCREEN_HDR_MAGIC_STR2)) {
-				return TRUE;
-			}
-		} else {
+		if (file_gets(buf, NETSCREEN_LINE_LENGTH, wth->fh) == NULL) {
 			/* EOF or error. */
 			*err = file_error(wth->fh, err_info);
 			return FALSE;
+		}
+
+		reclen = (guint) strlen(buf);
+		if (reclen < strlen(NETSCREEN_HDR_MAGIC_STR1) ||
+			reclen < strlen(NETSCREEN_HDR_MAGIC_STR2)) {
+			continue;
+		}
+
+		if (strstr(buf, NETSCREEN_HDR_MAGIC_STR1) ||
+		    strstr(buf, NETSCREEN_HDR_MAGIC_STR2)) {
+			return TRUE;
 		}
 	}
 	*err = 0;
