61d423c41f87b031c5dec87d45fb1de3223ddd1b
Guy Harris <guy@alum.mit.edu>
2013-01-05 10:07:16 +0000

    Copy over r46945 from trunk:
      ------------------------------------------------------------------------
      r46945 | guy | 2013-01-05 02:06:05 -0800 (Sat, 05 Jan 2013) | 3 lines
    
      In the post-install script, get rid of plugins that got converted to
      built-in dissectors in 1.8.  Hopefully, this will smash bug 7401.
    
    svn path=/trunk-1.8/; revision=46946

--- packaging/macosx/Scripts/wireshark-postinstall.sh
+++ packaging/macosx/Scripts/wireshark-postinstall.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-# This space intentionally left blank
+#
+# After an upgrade, get rid of leftover plugins that were converted
+# to built-in dissectors, so that we won't have problems trying to load
+# them (wrong instruction set, collisions with the built-ins, etc.).
+#
+# XXX - apparently, there's no longer a "postinstall" vs. "postupgrade"
+# difference, but if you've never installed Wireshark before, this
+# will just silently and harmlessly fail to remove files that aren't
+# there in the first place.
+#
+PLUGINS="coseventcomm cosnaming interlink parlay sercosiii tango"
+
+PLUGINS_PATH="$2/Wireshark.app/Contents/Resources/lib/wireshark/plugins"
+
+for plugin in $PLUGINS
+do
+	rm -f "$PLUGINS_PATH"/$plugin.so "$PLUGINS_PATH"/$plugin.la
+done
