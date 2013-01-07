28808df71b6300ad92733f10cdc5afc7c4f636ed
Evan Huus <eapache@gmail.com>
2013-01-07 20:00:38 +0000

    Add mode-lines.
    svn path=/trunk/; revision=46988

--- epan/dissectors/packet-nwmtp.c
+++ epan/dissectors/packet-nwmtp.c
@@ -161,3 +161,16 @@ void proto_reg_handoff_nwmtp(void)
 	dissector_add_handle("udp.port", nwmtp_handle);
 	mtp_handle = find_dissector("mtp3");
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
