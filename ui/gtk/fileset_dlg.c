780ff66b6ec66417ad8afd5d593a3c7e338b5f23
Bill Meier <wmeier@newsguy.com>
2013-01-10 23:11:35 +0000

    Replace gtk_table...() with ws_gtk_grid...(). Do some minor changes so that the Gtk2 & Gtk3  window resizing behavior is the same.
    Also: Clean up whitespace: indentation & etc.
    
    svn path=/trunk/; revision=47024

--- ui/gtk/fileset_dlg.c
+++ ui/gtk/fileset_dlg.c
@@ -58,12 +58,12 @@ static GtkWidget *fs_w;
 
 
 /* various widget related global data */
-static int            row;
-static GtkWidget     *fs_tb;
-static GtkWidget     *fs_sw;
-static GtkWidget     *fs_dir_lb;
-static GtkWidget     *fs_first_rb;
-static GtkWidget     *fs_tb_vb;
+static int        row;
+static GtkWidget *fs_grid;
+static GtkWidget *fs_sw;
+static GtkWidget *fs_dir_lb;
+static GtkWidget *fs_first_rb;
+static GtkWidget *fs_grid_vb;
 
 
 
@@ -71,8 +71,8 @@ static GtkWidget     *fs_tb_vb;
 static void
 fs_open_entry(fileset_entry *entry)
 {
-    char            *fname;
-    int             err;
+    char *fname;
+    int   err;
 
 
     /* make a copy of the filename (cf_close will indirectly destroy it right now) */
@@ -92,10 +92,10 @@ fs_open_entry(fileset_entry *entry)
 static void
 fs_rb_cb(GtkWidget *open_bt, gpointer fs_data)
 {
-    fileset_entry   *entry = fs_data;
+    fileset_entry *entry = fs_data;
 
     /* button release should have no effect */
-    if(!gtk_toggle_button_get_active( GTK_TOGGLE_BUTTON(open_bt) )) {
+    if (!gtk_toggle_button_get_active( GTK_TOGGLE_BUTTON(open_bt) )) {
         return;
     }
 
@@ -116,15 +116,15 @@ fs_destroy_cb(GtkWidget *win _U_, gpointer user_data _U_)
 /* */
 static char *
 fileset_dlg_name2date_dup(const char * name) {
-    char        *pfx;
-    char        *filename;
-    size_t       pos;
+    char   *pfx;
+    char   *filename;
+    size_t  pos;
 
 
     /* just to be sure ... */
-	if(!fileset_filename_match_pattern(name)) {
-		return NULL;
-	}
+    if (!fileset_filename_match_pattern(name)) {
+        return NULL;
+    }
 
     /* find char position behind the last underscore */
     pfx = strrchr(name, '_');
@@ -148,13 +148,13 @@ fileset_dlg_name2date_dup(const char * name) {
 /* this file is a part of the current file set, add it to the dialog */
 void
 fileset_dlg_add_file(fileset_entry *entry, void *window _U_) {
-    char *created;
-    char *modified;
-    char *size;
+    char      *created;
+    char      *modified;
+    char      *size;
     struct tm *local;
-    GtkWidget     *fs_lb;
-    GtkWidget     *fs_rb;
-    gchar *title;
+    GtkWidget *fs_lb;
+    GtkWidget *fs_rb;
+    gchar     *title;
 
 
     if (fs_w == NULL) {
@@ -162,14 +162,14 @@ fileset_dlg_add_file(fileset_entry *entry, void *window _U_) {
     }
 
     created = fileset_dlg_name2date_dup(entry->name);
-	if(!created) {
-		/* if this file doesn't follow the file set pattern, */
-		/* use the creation time of that file */
-		local = localtime(&entry->ctime);
-		created = g_strdup_printf("%04u-%02u-%02u %02u:%02u:%02u",
-			local->tm_year+1900, local->tm_mon+1, local->tm_mday,
-			local->tm_hour, local->tm_min, local->tm_sec);
-	}
+    if (!created) {
+        /* if this file doesn't follow the file set pattern, */
+        /* use the creation time of that file */
+        local = localtime(&entry->ctime);
+        created = g_strdup_printf("%04u-%02u-%02u %02u:%02u:%02u",
+                                  local->tm_year+1900, local->tm_mon+1, local->tm_mday,
+                                  local->tm_hour, local->tm_min, local->tm_sec);
+    }
 
     local = localtime(&entry->mtime);
     modified = g_strdup_printf("%04u-%02u-%02u %02u:%02u:%02u",
@@ -179,29 +179,29 @@ fileset_dlg_add_file(fileset_entry *entry, void *window _U_) {
 
     fs_rb = gtk_radio_button_new_with_label_from_widget(
         fs_first_rb ? GTK_RADIO_BUTTON(fs_first_rb) : NULL, entry->name);
-    if(row == 1) {
+    if (row == 1) {
         fs_first_rb = fs_rb;
     }
-    if(entry->current) {
+    if (entry->current) {
         gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON (fs_rb), entry->current);
     }
-	gtk_widget_set_tooltip_text(fs_rb, "Open this capture file");
-    gtk_table_attach_defaults(GTK_TABLE(fs_tb), fs_rb, 0, 1, row, row+1);
+    gtk_widget_set_tooltip_text(fs_rb, "Open this capture file");
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_rb, 0, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
     g_signal_connect(fs_rb, "toggled", G_CALLBACK(fs_rb_cb), entry);
     gtk_widget_show(fs_rb);
 
     fs_lb = gtk_label_new(created);
-    gtk_table_attach_defaults(GTK_TABLE(fs_tb), fs_lb, 1, 2, row, row+1);
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 1, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
     gtk_widget_set_sensitive(fs_lb, entry->current);
     gtk_widget_show(fs_lb);
 
     fs_lb = gtk_label_new(modified);
-    gtk_table_attach_defaults(GTK_TABLE(fs_tb), fs_lb, 2, 3, row, row+1);
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 2, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
     gtk_widget_set_sensitive(fs_lb, entry->current);
     gtk_widget_show(fs_lb);
 
     fs_lb = gtk_label_new(size);
-    gtk_table_attach_defaults(GTK_TABLE(fs_tb), fs_lb, 3, 4, row, row+1);
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 3, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
     gtk_widget_set_sensitive(fs_lb, entry->current);
     gtk_widget_show(fs_lb);
 
@@ -213,24 +213,24 @@ fileset_dlg_add_file(fileset_entry *entry, void *window _U_) {
      gtk_label_set_text(GTK_LABEL(fs_dir_lb), title);
     g_free(title);
 
-    gtk_widget_show_all(fs_tb);
+    gtk_widget_show_all(fs_grid);
 
     /* resize the table until we use 18 rows (fits well into 800*600), if it's bigger use a scrollbar */
     /* XXX - I didn't found a way to automatically shrink the table size again */
-    if(row <= 18) {
+    if (row <= 18) {
       GtkRequisition requisition;
 
 #if GTK_CHECK_VERSION(3,0,0)
-	  gtk_widget_get_preferred_size(fs_tb, &requisition, NULL);
+      gtk_widget_get_preferred_size(fs_grid, &requisition, NULL);
 #else
-      gtk_widget_size_request(fs_tb, &requisition);
+      gtk_widget_size_request(fs_grid, &requisition);
 #endif
-	  /* XXX use gtk_window_set_default_size()? */
+      /* XXX use gtk_window_set_default_size()? */
       gtk_widget_set_size_request(fs_sw, -1, requisition.height);
       gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(fs_sw), GTK_POLICY_NEVER, GTK_POLICY_NEVER);
     }
 
-    if(row == 18) {
+    if (row == 18) {
       gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(fs_sw), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
     }
 
@@ -244,38 +244,37 @@ fileset_dlg_add_file(fileset_entry *entry, void *window _U_) {
 
 /* init the fileset table */
 static void
-fileset_init_table(GtkWidget *parent)
+fileset_init_table(GtkWidget *parent_vb)
 {
-  GtkWidget     *fs_lb;
+    GtkWidget *fs_lb;
 
+    fs_grid = ws_gtk_grid_new();
+    ws_gtk_grid_set_row_spacing(GTK_GRID(fs_grid), 1);
+    ws_gtk_grid_set_column_spacing(GTK_GRID(fs_grid), 12);
+    gtk_box_pack_start(GTK_BOX(parent_vb), fs_grid, FALSE, FALSE, 0);
 
-  fs_tb = gtk_table_new(6,1, FALSE);
-  gtk_table_set_row_spacings(GTK_TABLE(fs_tb), 1);
-  gtk_table_set_col_spacings(GTK_TABLE(fs_tb), 12);
-  gtk_container_add(GTK_CONTAINER(parent), fs_tb);
+    row = 0;
+    fs_first_rb = NULL;
 
-  row = 0;
-  fs_first_rb = NULL;
+    fs_lb = gtk_label_new("Filename");
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 0, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
 
-  fs_lb = gtk_label_new("Filename");
-  gtk_table_attach(GTK_TABLE(fs_tb), fs_lb, 0, 1, row, row+1, GTK_EXPAND|GTK_FILL, 0, 0,0);
+    fs_lb = gtk_label_new("Created");
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 1, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
 
-  fs_lb = gtk_label_new("Created");
-  gtk_table_attach(GTK_TABLE(fs_tb), fs_lb, 1, 2, row, row+1, GTK_EXPAND|GTK_FILL, 0, 0,0);
+    fs_lb = gtk_label_new("Last Modified");
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 2, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
 
-  fs_lb = gtk_label_new("Last Modified");
-  gtk_table_attach(GTK_TABLE(fs_tb), fs_lb, 2, 3, row, row+1, GTK_EXPAND|GTK_FILL, 0, 0,0);
+    fs_lb = gtk_label_new("Size");
+    ws_gtk_grid_attach_extended(GTK_GRID(fs_grid), fs_lb, 3, row, 1, 1, GTK_EXPAND|GTK_FILL, 0, 0, 0);
 
-  fs_lb = gtk_label_new("Size");
-  gtk_table_attach(GTK_TABLE(fs_tb), fs_lb, 3, 4, row, row+1, GTK_EXPAND|GTK_FILL, 0, 0,0);
+    gtk_widget_hide(fs_grid);
 
-  gtk_widget_hide(fs_tb);
+    gtk_window_set_title(GTK_WINDOW(fs_w), "Wireshark: 0 Files in Set");
 
-  gtk_window_set_title(GTK_WINDOW(fs_w), "Wireshark: 0 Files in Set");
+    gtk_label_set_text(GTK_LABEL(fs_dir_lb), "No capture file loaded!");
 
-   gtk_label_set_text(GTK_LABEL(fs_dir_lb), "No capture file loaded!");
-
-  row++;
+    row++;
 }
 
 
@@ -283,56 +282,56 @@ fileset_init_table(GtkWidget *parent)
 void
 fileset_cb(GtkWidget *w _U_, gpointer d _U_)
 {
-  GtkWidget     *main_vb, *bbox, *close_bt, *help_bt;
+    GtkWidget *main_vb, *bbox, *close_bt, *help_bt;
 
 
-  if (fs_w != NULL) {
-    /* There's already a "File Set" dialog box; reactivate it. */
-    reactivate_window(fs_w);
-    return;
-  }
+    if (fs_w != NULL) {
+        /* There's already a "File Set" dialog box; reactivate it. */
+        reactivate_window(fs_w);
+        return;
+    }
 
-  fs_w = dlg_window_new("");  /* transient_for top_level */
-  gtk_window_set_destroy_with_parent (GTK_WINDOW(fs_w), TRUE);
+    fs_w = dlg_window_new("");  /* transient_for top_level */
+    gtk_window_set_destroy_with_parent (GTK_WINDOW(fs_w), TRUE);
 
-  main_vb = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 5, FALSE);
-  gtk_container_set_border_width(GTK_CONTAINER(main_vb), 5);
-  gtk_container_add(GTK_CONTAINER(fs_w), main_vb);
+    main_vb = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 5, FALSE);
+    gtk_container_set_border_width(GTK_CONTAINER(main_vb), 5);
+    gtk_container_add(GTK_CONTAINER(fs_w), main_vb);
 
-  fs_sw = gtk_scrolled_window_new(NULL, NULL);
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(fs_sw), GTK_POLICY_NEVER, GTK_POLICY_NEVER);
-  gtk_box_pack_start(GTK_BOX(main_vb), fs_sw, TRUE, TRUE, 0);
+    fs_sw = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(fs_sw), GTK_POLICY_NEVER, GTK_POLICY_NEVER);
+    gtk_box_pack_start(GTK_BOX(main_vb), fs_sw, TRUE, TRUE, 0);
 
-  /* add a dummy container, so we can replace the table later */
-  fs_tb_vb = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 0, FALSE);
-  gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(fs_sw), fs_tb_vb);
+    /* add a dummy container, so we can replace the table later */
+    fs_grid_vb = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 0, FALSE);
+    gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(fs_sw), fs_grid_vb);
 
-  fs_dir_lb = gtk_label_new("");
-  gtk_box_pack_start(GTK_BOX(main_vb), fs_dir_lb, FALSE, FALSE, 0);
+    fs_dir_lb = gtk_label_new("");
+    gtk_box_pack_start(GTK_BOX(main_vb), fs_dir_lb, FALSE, FALSE, 0);
 
-  fileset_init_table(fs_tb_vb);
+    fileset_init_table(fs_grid_vb);
 
-  /* Button row: close and help button */
-  bbox = dlg_button_row_new(GTK_STOCK_CLOSE, GTK_STOCK_HELP, NULL);
-  gtk_box_pack_start(GTK_BOX(main_vb), bbox, FALSE, FALSE, 5);
+    /* Button row: close and help button */
+    bbox = dlg_button_row_new(GTK_STOCK_CLOSE, GTK_STOCK_HELP, NULL);
+    gtk_box_pack_start(GTK_BOX(main_vb), bbox, FALSE, FALSE, 5);
 
-  close_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_CLOSE);
-  window_set_cancel_button(fs_w, close_bt, window_cancel_button_cb);
-  gtk_widget_set_tooltip_text(close_bt, "Close this window.");
+    close_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_CLOSE);
+    window_set_cancel_button(fs_w, close_bt, window_cancel_button_cb);
+    gtk_widget_set_tooltip_text(close_bt, "Close this window.");
 
-  help_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_HELP);
-  g_signal_connect(help_bt, "clicked", G_CALLBACK(topic_cb), (gpointer)HELP_FILESET_DIALOG);
+    help_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_HELP);
+    g_signal_connect(help_bt, "clicked", G_CALLBACK(topic_cb), (gpointer)HELP_FILESET_DIALOG);
 
-  gtk_widget_grab_default(close_bt);
+    gtk_widget_grab_default(close_bt);
 
-  g_signal_connect(fs_w, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);
-  g_signal_connect(fs_w, "destroy", G_CALLBACK(fs_destroy_cb), NULL);
+    g_signal_connect(fs_w, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);
+    g_signal_connect(fs_w, "destroy", G_CALLBACK(fs_destroy_cb), NULL);
 
-  /* init the dialog content */
-  fileset_update_dlg(NULL);
+    /* init the dialog content */
+    fileset_update_dlg(NULL);
 
-  gtk_widget_show_all(fs_w);
-  window_present(fs_w);
+    gtk_widget_show_all(fs_w);
+    window_present(fs_w);
 }
 
 
@@ -340,11 +339,11 @@ fileset_cb(GtkWidget *w _U_, gpointer d _U_)
 void
 fileset_next_cb(GtkWidget *w _U_, gpointer d _U_)
 {
-    fileset_entry   *entry;
+    fileset_entry *entry;
 
     entry = fileset_get_next();
 
-    if(entry) {
+    if (entry) {
         fs_open_entry(entry);
     }
 }
@@ -354,11 +353,11 @@ fileset_next_cb(GtkWidget *w _U_, gpointer d _U_)
 void
 fileset_previous_cb(GtkWidget *w _U_, gpointer d _U_)
 {
-    fileset_entry   *entry;
+    fileset_entry *entry;
 
     entry = fileset_get_previous();
 
-    if(entry) {
+    if (entry) {
         fs_open_entry(entry);
     }
 }
@@ -367,14 +366,14 @@ fileset_previous_cb(GtkWidget *w _U_, gpointer d _U_)
 /* a new capture file was opened, browse the dir and look for files matching the given file set */
 void
 fileset_file_opened(const capture_file *cf) {
-  fileset_add_dir(cf->filename, NULL);
-  if(fs_w) {
-    window_present(fs_w);
-  }
+    fileset_add_dir(cf->filename, NULL);
+    if (fs_w) {
+        window_present(fs_w);
+    }
 
-  /* update the menu */
-  set_menus_for_file_set(TRUE /* file_set */,
-      fileset_get_previous() != NULL, fileset_get_next() != NULL );
+    /* update the menu */
+    set_menus_for_file_set(TRUE /* file_set */,
+                           fileset_get_previous() != NULL, fileset_get_next() != NULL );
 }
 
 
@@ -382,19 +381,19 @@ fileset_file_opened(const capture_file *cf) {
 void
 fileset_file_closed(void)
 {
-  if(fs_w) {
-    /* reinit the table, title and alike */
-    g_object_ref(G_OBJECT(fs_tb_vb));
-    gtk_widget_destroy(fs_tb);
-    fileset_delete();
-    fileset_init_table(fs_tb_vb);
-    window_present(fs_w);
-  } else {
-    fileset_delete();
-  }
+    if (fs_w) {
+        /* reinit the table, title and alike */
+        g_object_ref(G_OBJECT(fs_grid_vb));
+        gtk_widget_destroy(fs_grid);
+        fileset_delete();
+        fileset_init_table(fs_grid_vb);
+        window_present(fs_w);
+    } else {
+        fileset_delete();
+    }
 
-  /* update the menu */
-  set_menus_for_file_set(FALSE /* file_set */,
-                         fileset_get_previous() != NULL,
-                         fileset_get_next() != NULL );
+    /* update the menu */
+    set_menus_for_file_set(FALSE /* file_set */,
+                           fileset_get_previous() != NULL,
+                           fileset_get_next() != NULL );
 }
