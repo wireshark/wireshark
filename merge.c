2233b34cf13b0279510183a0b39c282d08febe54
Jeff Morriss <jeff.morriss.ws@gmail.com>
2013-01-15 02:17:16 +0000

    Add boilerplate GPLv2+ license as per email response from Scott Renfro:
    https://www.wireshark.org/lists/wireshark-dev/201301/msg00062.html
    
    svn path=/trunk/; revision=47088

--- merge.c
+++ merge.c
@@ -1,10 +1,29 @@
 /* Combine multiple dump files, either by appending or by merging by timestamp
  *
- * $Id$
- *
  * Written by Scott Renfro <scott@renfro.org> based on
  * editcap by Richard Sharpe and Guy Harris
  *
+ * Copyright 2013, Scott Renfro <scott[AT]renfro.org>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include "config.h"
@@ -188,7 +207,7 @@ merge_read_packet(int in_file_count, merge_in_file_t in_files[],
       } else
         in_files[i].state = PACKET_PRESENT;
     }
-    
+
     if (in_files[i].state == PACKET_PRESENT) {
       phdr = wtap_phdr(in_files[i].wth);
       if (is_earlier(&phdr->ts, &tv)) {
