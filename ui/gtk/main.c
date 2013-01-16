a694ccd01330d0f807ab08d15b78770a3f415adf
Guy Harris <guy@alum.mit.edu>
2013-01-16 00:12:27 +0000

    Set the crash info as early as possible in main(), so that it shows up in crashes that happen early in the process.
    Clean up white space.
    
    svn path=/trunk/; revision=47108

--- ui/gtk/main.c
+++ ui/gtk/main.c
@@ -61,6 +61,8 @@
 #include <portaudio.h>
 #endif /* HAVE_LIBPORTAUDIO */
 
+#include <wsutil/crash_info.h>
+
 #include <epan/epan.h>
 #include <epan/filesystem.h>
 #include <wsutil/privileges.h>
@@ -2215,6 +2217,22 @@ main(int argc, char *argv[])
 
   static const char optstring[] = OPTSTRING;
 
+  /* Assemble the compile-time version information string */
+  comp_info_str = g_string_new("Compiled ");
+
+  get_compiled_version_info(comp_info_str, get_gtk_compiled_info, get_gui_compiled_info);
+
+  /* Assemble the run-time version information string */
+  runtime_info_str = g_string_new("Running ");
+  get_runtime_version_info(runtime_info_str, get_gui_runtime_info);
+
+  ws_add_crash_info(PACKAGE " " VERSION "%s\n"
+         "\n"
+         "%s"
+         "\n"
+         "%s",
+      wireshark_svnversion, comp_info_str->str, runtime_info_str->str);
+
   /* Set the C-language locale to the native environment. */
   setlocale(LC_ALL, "");
 #ifdef _WIN32
@@ -2267,7 +2285,7 @@ main(int argc, char *argv[])
 
       /* select the first ad default (THIS SHOULD BE CHANGED) */
       airpcap_if_active = airpcap_get_default_if(airpcap_if_list);
-          }
+    }
     break;
 #if 0
     /*
@@ -2293,15 +2311,6 @@ main(int argc, char *argv[])
 #endif  /* _WIN32 */
 
   profile_store_persconffiles (TRUE);
-
-  /* Assemble the compile-time version information string */
-  comp_info_str = g_string_new("Compiled ");
-
-  get_compiled_version_info(comp_info_str, get_gtk_compiled_info, get_gui_compiled_info);
-
-  /* Assemble the run-time version information string */
-  runtime_info_str = g_string_new("Running ");
-  get_runtime_version_info(runtime_info_str, get_gui_runtime_info);
 
   /* Read the profile independent recent file.  We have to do this here so we can */
   /* set the profile before it can be set from the command line parameterts */
