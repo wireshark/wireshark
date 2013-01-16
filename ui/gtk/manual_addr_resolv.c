185f323231939cd2c463125b2f3e5cfa22241faf
Bill Meier <wmeier@newsguy.com>
2013-01-16 16:52:32 +0000

    Replace gtk_table...() with ws_gtk_grid...();
    Use gtk_box_pack_start() instead of
     gtk_container_add() in a few places so that
     window resize behavior is the same for
     Gtk2 and Gtk3.
    
    Also: do some whitespace cleanup.
    
    svn path=/trunk/; revision=47117

--- ui/gtk/manual_addr_resolv.c
+++ ui/gtk/manual_addr_resolv.c
@@ -43,29 +43,30 @@
 GtkWidget *man_addr_resolv_dlg = NULL;
 
 static void
-man_addr_ill_addr_cb (gpointer dialog _U_, gint btn _U_, gpointer data _U_)
+man_addr_ill_addr_cb(gpointer dialog _U_, gint btn _U_, gpointer data _U_)
 {
-  gtk_window_present (GTK_WINDOW(man_addr_resolv_dlg));
+  gtk_window_present(GTK_WINDOW(man_addr_resolv_dlg));
 }
 
 static void
-man_addr_resolv_ok (GtkWidget *w _U_, gpointer data _U_)
+man_addr_resolv_ok(GtkWidget *w _U_, gpointer data _U_)
 {
   GtkWidget   *addr_cb, *name_te, *resolv_cb;
   const gchar *name;
   gchar       *addr;
   gboolean     active, redissect = FALSE;
-  addr_cb = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "address");
-  name_te = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "name");
 
-  addr = gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(addr_cb));
-  name = gtk_entry_get_text (GTK_ENTRY (name_te));
+  addr_cb = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "address");
+  name_te = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "name");
 
-  if (strlen (addr) && strlen (name)) {
-    if (!add_ip_name_from_string (addr, name)) {
-      GtkWidget *dialog = simple_dialog (ESD_TYPE_ERROR, ESD_BTN_OK,
-					 "Illegal IP address: \"%s\".", addr);
-      simple_dialog_set_cb (dialog, man_addr_ill_addr_cb, NULL);
+  addr = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(addr_cb));
+  name = gtk_entry_get_text(GTK_ENTRY(name_te));
+
+  if (strlen(addr) && strlen(name)) {
+    if (!add_ip_name_from_string(addr, name)) {
+      GtkWidget *dialog = simple_dialog(ESD_TYPE_ERROR, ESD_BTN_OK,
+                                        "Illegal IP address: \"%s\".", addr);
+      simple_dialog_set_cb(dialog, man_addr_ill_addr_cb, NULL);
       g_free(addr);
       return;
     } else {
@@ -74,19 +75,19 @@ man_addr_resolv_ok (GtkWidget *w _U_, gpointer data _U_)
   }
   g_free(addr);
 
-  resolv_cb = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "resolv");
+  resolv_cb = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "resolv");
   active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(resolv_cb));
   if (!gbl_resolv_flags.network_name && active) {
     /* Name resolution for Network Layer activated */
     gbl_resolv_flags.network_name = TRUE;
-    menu_name_resolution_changed ();
+    menu_name_resolution_changed();
     redissect = TRUE;
   }
 
   if (redissect) {
-    redissect_packets ();
+    redissect_packets();
   }
-  window_destroy (man_addr_resolv_dlg);
+  window_destroy(man_addr_resolv_dlg);
   man_addr_resolv_dlg = NULL;
 }
 
@@ -96,97 +97,98 @@ changed_cb(GtkWidget *w _U_, GtkWidget *ok_bt)
   const gchar *name;
   gchar       *addr;
   GtkWidget   *addr_cb, *name_cb, *resolv_cb;
-  gboolean    active;
+  gboolean     active;
 
-  name_cb   = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "name");
-  addr_cb   = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "address");
-  resolv_cb = g_object_get_data (G_OBJECT(man_addr_resolv_dlg), "resolv");
+  name_cb   = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "name");
+  addr_cb   = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "address");
+  resolv_cb = g_object_get_data(G_OBJECT(man_addr_resolv_dlg), "resolv");
 
-  name = gtk_entry_get_text (GTK_ENTRY (name_cb));
-  addr = gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT(addr_cb));
+  name   = gtk_entry_get_text(GTK_ENTRY(name_cb));
+  addr   = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(addr_cb));
   active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(resolv_cb));
 
-  gtk_widget_set_sensitive (ok_bt, strlen(name) > 0 && strlen(addr) && active ? TRUE : FALSE);
+  gtk_widget_set_sensitive(ok_bt, strlen(name) > 0 && strlen(addr) && active ? TRUE : FALSE);
   g_free(addr);
 }
 
 void
-manual_addr_resolv_dlg (GtkWidget *w _U_, gpointer data)
+manual_addr_resolv_dlg(GtkWidget *w _U_, gpointer data)
 {
-  GtkWidget   *vbox, *bbox, *table, *sep;
-  GtkWidget   *ok_bt, *close_bt, *help_bt;
-  GtkWidget   *addr_lb, *addr_cb;
-  GtkWidget   *name_lb, *name_te, *resolv_cb;
-  GList       *addr_list = NULL;
+  GtkWidget *vbox, *bbox, *grid, *sep;
+  GtkWidget *ok_bt, *close_bt, *help_bt;
+  GtkWidget *addr_lb, *addr_cb;
+  GtkWidget *name_lb, *name_te, *resolv_cb;
+  GList     *addr_list = NULL;
 
-  man_addr_resolv_dlg = dlg_window_new ("Manual Address Resolve");
-  gtk_window_set_default_size (GTK_WINDOW(man_addr_resolv_dlg), 310, 80);
+  man_addr_resolv_dlg = dlg_window_new("Manual Address Resolve");
+  gtk_window_set_default_size(GTK_WINDOW(man_addr_resolv_dlg), 310, 80);
 
   vbox = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, 3, FALSE);
   gtk_container_add(GTK_CONTAINER(man_addr_resolv_dlg), vbox);
-  gtk_container_set_border_width (GTK_CONTAINER(vbox), 6);
+  gtk_container_set_border_width(GTK_CONTAINER(vbox), 6);
 
-  table = gtk_table_new (2, 2, FALSE);
-  gtk_container_add(GTK_CONTAINER(vbox), table);
+  grid = ws_gtk_grid_new();
+  gtk_box_pack_start(GTK_BOX(vbox), grid, TRUE, TRUE, 0);
+
   addr_lb = gtk_label_new("Address:");
-  gtk_table_attach_defaults (GTK_TABLE (table), addr_lb, 0, 1, 0, 1);
+  ws_gtk_grid_attach_defaults(GTK_GRID(grid), addr_lb, 0, 0, 1, 1);
 
   addr_cb = gtk_combo_box_text_new_with_entry();
   if (data) {
     GList *addr_entry;
     addr_list = get_ip_address_list_from_packet_list_row(data);
-    for (addr_entry = addr_list; addr_entry != NULL; addr_entry = g_list_next (addr_entry)) {
-      gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT(addr_cb), addr_entry->data);
+    for (addr_entry = addr_list; addr_entry != NULL; addr_entry = g_list_next(addr_entry)) {
+      gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(addr_cb), addr_entry->data);
     }
-    gtk_combo_box_set_active (GTK_COMBO_BOX(addr_cb), 0);
+    gtk_combo_box_set_active(GTK_COMBO_BOX(addr_cb), 0);
   }
-  gtk_table_attach_defaults (GTK_TABLE (table), addr_cb, 1, 2, 0, 1);
-  g_object_set_data (G_OBJECT(man_addr_resolv_dlg), "address", addr_cb);
+  ws_gtk_grid_attach_defaults(GTK_GRID(grid), addr_cb, 1, 0, 1, 1);
+  g_object_set_data(G_OBJECT(man_addr_resolv_dlg), "address", addr_cb);
 
   name_lb = gtk_label_new("Name:");
-  gtk_table_attach_defaults (GTK_TABLE (table), name_lb, 0, 1, 1, 2);
+  ws_gtk_grid_attach_defaults(GTK_GRID(grid), name_lb, 0, 1, 1, 1);
 
-  name_te = gtk_entry_new ();
-  gtk_table_attach_defaults (GTK_TABLE (table), name_te, 1, 2, 1, 2);
-  g_object_set_data (G_OBJECT(man_addr_resolv_dlg), "name", name_te);
+  name_te = gtk_entry_new();
+  ws_gtk_grid_attach_defaults(GTK_GRID(grid), name_te, 1, 1, 1, 1);
+  g_object_set_data(G_OBJECT(man_addr_resolv_dlg), "name", name_te);
 
-  sep = gtk_separator_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER(vbox), sep);
+  sep = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);
+  gtk_box_pack_start(GTK_BOX(vbox), sep, TRUE, TRUE, 0);
 
-  resolv_cb = gtk_check_button_new_with_mnemonic ("Enable network name resolution");
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(resolv_cb), gbl_resolv_flags.network_name);
-  gtk_widget_set_sensitive (resolv_cb, !gbl_resolv_flags.network_name);
+  resolv_cb = gtk_check_button_new_with_mnemonic("Enable network name resolution");
+  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(resolv_cb), gbl_resolv_flags.network_name);
+  gtk_widget_set_sensitive(resolv_cb, !gbl_resolv_flags.network_name);
 
   gtk_widget_set_tooltip_text(resolv_cb, "Perform network layer name resolution.");
-  g_object_set_data (G_OBJECT(man_addr_resolv_dlg), "resolv", resolv_cb);
-  gtk_container_add (GTK_CONTAINER(vbox), resolv_cb);
+  g_object_set_data(G_OBJECT(man_addr_resolv_dlg), "resolv", resolv_cb);
+  gtk_box_pack_start(GTK_BOX(vbox), resolv_cb, TRUE, TRUE, 0);
 
   /* Button row. */
-  bbox = dlg_button_row_new (GTK_STOCK_OK, GTK_STOCK_CLOSE, GTK_STOCK_HELP, NULL);
-  gtk_box_pack_end (GTK_BOX(vbox), bbox, FALSE, FALSE, 0);
+  bbox = dlg_button_row_new(GTK_STOCK_OK, GTK_STOCK_CLOSE, GTK_STOCK_HELP, NULL);
+  gtk_box_pack_end(GTK_BOX(vbox), bbox, FALSE, FALSE, 0);
 
-  ok_bt = g_object_get_data (G_OBJECT(bbox), GTK_STOCK_OK);
-  g_signal_connect (ok_bt, "clicked", G_CALLBACK(man_addr_resolv_ok), NULL);
-  gtk_widget_set_sensitive (ok_bt, FALSE);
+  ok_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_OK);
+  g_signal_connect(ok_bt, "clicked", G_CALLBACK(man_addr_resolv_ok), NULL);
+  gtk_widget_set_sensitive(ok_bt, FALSE);
 
   g_signal_connect(name_te, "changed", G_CALLBACK(changed_cb), ok_bt);
   g_signal_connect(addr_cb, "changed", G_CALLBACK(changed_cb), ok_bt);
   g_signal_connect(resolv_cb, "toggled", G_CALLBACK(changed_cb), ok_bt);
   dlg_set_activate(name_te, ok_bt);
 
-  close_bt = g_object_get_data (G_OBJECT(bbox), GTK_STOCK_CLOSE);
-  window_set_cancel_button (man_addr_resolv_dlg, close_bt, window_cancel_button_cb);
+  close_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_CLOSE);
+  window_set_cancel_button(man_addr_resolv_dlg, close_bt, window_cancel_button_cb);
 
-  help_bt = g_object_get_data (G_OBJECT(bbox), GTK_STOCK_HELP);
-  g_signal_connect (help_bt, "clicked", G_CALLBACK(topic_cb), (gpointer)HELP_MANUAL_ADDR_RESOLVE_DIALOG);
+  help_bt = g_object_get_data(G_OBJECT(bbox), GTK_STOCK_HELP);
+  g_signal_connect(help_bt, "clicked", G_CALLBACK(topic_cb), (gpointer)HELP_MANUAL_ADDR_RESOLVE_DIALOG);
 
-  gtk_widget_grab_default (ok_bt);
-  g_signal_connect (man_addr_resolv_dlg, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);
+  gtk_widget_grab_default(ok_bt);
+  g_signal_connect(man_addr_resolv_dlg, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);
 
   if (addr_list) {
     /* We have column data, activate name box */
-    gtk_widget_grab_focus (name_te);
+    gtk_widget_grab_focus(name_te);
   }
-  gtk_widget_show_all (man_addr_resolv_dlg);
-  window_present (man_addr_resolv_dlg);
+  gtk_widget_show_all(man_addr_resolv_dlg);
+  window_present(man_addr_resolv_dlg);
 }
