2afe999c21444c05a61ac1a2a214889a45f14684
Gerald Combs <gerald@wireshark.org>
2013-01-30 00:07:57 +0000

    Copy over r47277 from ui/gtk.
    svn path=/trunk/; revision=47364

--- ui/qt/byte_view_tab.cpp
+++ ui/qt/byte_view_tab.cpp
@@ -98,7 +98,13 @@ void ByteViewTab::protoTreeItemChanged(QTreeWidgetItem *current) {
                     /* In the hex view, only highlight the target bytes or string. The entire
                        field can then be displayed by clicking on any of the bytes in the field. */
                     if (cap_file_->hex) {
-                        f_len = (int)strlen(cap_file_->sfilter)/2;
+                        const char *p = cap_file_->sfilter;
+                        f_len = 0;
+                        while (*p) {
+                            if (g_ascii_isxdigit(*p++))
+                                f_len++;
+                        }
+                        f_len = (f_len + 1) / 2;
                     } else {
                         f_len = (int)strlen(cap_file_->sfilter);
                     }
