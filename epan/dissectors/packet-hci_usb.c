4056b11883d69beafde965d9e2ff55fcb8b487fa
Anders Broman <anders.broman@ericsson.com>
2013-02-11 15:18:26 +0000

    From: Michal Labedzki: Subject: [PATCH 3/6] Bluetooth: Add modeline to hci_usb https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8304
    svn path=/trunk/; revision=47633

--- epan/dissectors/packet-hci_usb.c
+++ epan/dissectors/packet-hci_usb.c
@@ -330,3 +330,16 @@ proto_reg_handoff_hci_usb(void)
 
     dissector_add_handle("usb.device", hci_usb_handle);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
