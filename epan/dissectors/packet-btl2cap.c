wireshark/wireshark

commit 4dc754f4542a31f89d011daee1890ac5e4659a06

From: Michal Labedzki: Bluetooth: Improve support of MAP, PBAP, BPP and BIP in OBEX
Add support for recognize profiles using OBEX (by "Target"), then add
all Application Parameters specific for MAP, PBAP, BPP, BIP.
Also fix one FIXME, so now dissecting by OBEX does not cause malformed
frames while jumping over dissected packets.

https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8304

svn path=/trunk/; revision=47632

epan/dissectors/packet-btl2cap.c
@@ -1290,12 +1290,16 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     proto_tree_add_item(btl2cap_tree, hf_btl2cap_cid, tvb, offset, 2, ENC_LITTLE_ENDIAN);
     offset += 2;
 
-    acl_data            = (bthci_acl_data_t *)pinfo->private_data;
-    l2cap_data          = ep_alloc(sizeof(btl2cap_data_t));
-    l2cap_data->chandle = (acl_data)? acl_data->chandle : 0;
-    l2cap_data->cid     = cid;
-    l2cap_data->psm     = 0;
+    acl_data = (bthci_acl_data_t *)pinfo->private_data;
+    l2cap_data = ep_alloc(sizeof(btl2cap_data_t));
+
+    l2cap_data->interface_id     = acl_data->interface_id;
+    l2cap_data->adapter_id       = acl_data->adapter_id;
+    l2cap_data->chandle          = (acl_data)? acl_data->chandle : 0;
+    l2cap_data->cid              = cid;
+    l2cap_data->psm              = 0;
     l2cap_data->first_scid_frame = 0;
+
     pd_save             = pinfo->private_data;
     pinfo->private_data = l2cap_data;
 

epan/dissectors/packet-bthci_acl.c
@@ -129,11 +129,14 @@ dissect_bthci_acl(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     offset += 2;
 
     hci_data = (hci_data_t *) pinfo->private_data;
+    acl_data = ep_alloc(sizeof(bthci_acl_data_t));
 
-    acl_data            = ep_alloc(sizeof(bthci_acl_data_t));
-    acl_data->chandle   = flags & 0x0fff;
-    pd_save             = pinfo->private_data;
-    pinfo->private_data = acl_data;
+    acl_data->interface_id = hci_data->interface_id;
+    acl_data->adapter_id   = hci_data->adapter_id;
+    acl_data->chandle      = flags & 0x0fff;
+
+    pd_save                = pinfo->private_data;
+    pinfo->private_data    = acl_data;
 
     k_interface_id      = hci_data->interface_id;
     k_adapter_id        = hci_data->adapter_id;

epan/dissectors/packet-bthci_acl.h
@@ -25,6 +25,8 @@
 #define __PACKET_BTHCI_ACL_H__
 
 typedef struct _bthci_acl_data_t {
+    guint32 interface_id;
+    guint32 adapter_id;
     guint16 chandle;  /* only low 12 bits used */
 } bthci_acl_data_t;
 

epan/dissectors/packet-btl2cap.h
@@ -51,6 +51,8 @@
  * chandle, cid and direction
  */
 typedef struct _btl2cap_data_t {
+    guint32   interface_id;
+    guint32   adapter_id;
     guint16   chandle;  /* only low 12 bits used */
     guint16   cid;
     guint16   psm;

epan/dissectors/packet-btrfcomm.c
@@ -715,9 +715,12 @@ dissect_btrfcomm(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
         l2cap_data = pinfo->private_data;
         pinfo->private_data = &rfcomm_data;
-        rfcomm_data.chandle = l2cap_data->chandle;
-        rfcomm_data.cid = l2cap_data->cid;
-        rfcomm_data.dlci = dlci;
+
+        rfcomm_data.interface_id = l2cap_data->interface_id;
+        rfcomm_data.adapter_id   = l2cap_data->adapter_id;
+        rfcomm_data.chandle      = l2cap_data->chandle;
+        rfcomm_data.cid          = l2cap_data->cid;
+        rfcomm_data.dlci         = dlci;
 
         decode_by_dissector = find_proto_by_channel(dlci >> 1);
         if (rfcomm_channels_enabled && decode_by_dissector) {

epan/dissectors/packet-btrfcomm.h
@@ -29,9 +29,11 @@
  * chandle, cid, dlci and direction
  */
 typedef struct _btrfcomm_data_t {
+    guint32 interface_id;
+    guint32 adapter_id;
     guint16 chandle;  /* only low 12 bits used */
     guint16 cid;
-    guint8 dlci;
+    guint8  dlci;
 } btrfcomm_data_t;
 
 #endif

commit 7410bf54a11dcdb194e6ba5d883df49d867e6eab

Fix Coverity CID 981007: Dereference before null check.  Bug introduced with r47632.
svn path=/trunk/; revision=47651

epan/dissectors/packet-btl2cap.c
@@ -947,9 +947,9 @@ dissect_connparamrequest(tvbuff_t *tvb, int offset, packet_info *pinfo _U_, prot
     proto_item_append_text(item, " LL Connection Events");
     slave_latency = tvb_get_letohs(tvb, offset);
 
-    if(slave_latency >= 500 || slave_latency > 10.0*tvb_get_letohs(tvb, offset+2)/(max_interval *1.25)) 
+    if(slave_latency >= 500 || slave_latency > 10.0*tvb_get_letohs(tvb, offset+2)/(max_interval *1.25))
         expert_add_info_format(pinfo, item, PI_PROTOCOL, PI_WARN, "Parameter mismatch");
-    
+
     offset += 2;
     item = proto_tree_add_item(tree, hf_btl2cap_timeout_multiplier, tvb, offset, 2, ENC_LITTLE_ENDIAN);
     proto_item_append_text(item, " (%g sec)",  tvb_get_letohs(tvb, offset)*0.01);
@@ -969,7 +969,7 @@ dissect_connparamresponse(tvbuff_t *tvb, int offset, packet_info *pinfo _U_, pro
 
     col_append_fstr(pinfo->cinfo, COL_INFO, " (%s)",
                     val_to_str_const(result, conn_param_result_vals, "Unknown result"));
-    
+
     return offset;
 }
 
@@ -1293,8 +1293,8 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     acl_data = (bthci_acl_data_t *)pinfo->private_data;
     l2cap_data = ep_alloc(sizeof(btl2cap_data_t));
 
-    l2cap_data->interface_id     = acl_data->interface_id;
-    l2cap_data->adapter_id       = acl_data->adapter_id;
+    l2cap_data->interface_id     = (acl_data)? acl_data->interface_id : 0;
+    l2cap_data->adapter_id       = (acl_data)? acl_data->adapter_id : 0;
     l2cap_data->chandle          = (acl_data)? acl_data->chandle : 0;
     l2cap_data->cid              = cid;
     l2cap_data->psm              = 0;
@@ -1410,7 +1410,7 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             case 0x13: /* Connection Parameter Response */
                 offset  = dissect_connparamresponse(tvb, offset, pinfo, btl2cap_cmd_tree);
                 break;
-                
+
             default:
                 proto_tree_add_item(btl2cap_cmd_tree, hf_btl2cap_cmd_data, tvb, offset, -1, ENC_NA);
                 offset += tvb_reported_length_remaining(tvb, offset);