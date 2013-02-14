wireshark/wireshark

commit 7410bf54a11dcdb194e6ba5d883df49d867e6eab

Fix Coverity CID 981007: Dereference before null check.  Bug introduced with r47632.
svn path=/trunk/; revision=47651

epan/dissectors/packet-btl2cap.c
@@ -947,9 +947,9 @@ dissect_connparamrequest(tvbuff_t *tvb, int offset, packet_info *pinfo _U_, prot
     proto_item_append_text(item, " LL Connection Events");
     slave_latency = tvb_get_letohs(tvb, offset);
 
-    if(slave_latency >= 500 || slave_latency > 10.0*tvb_get_letohs(tvb, offset+2)/(max_interval *1.25)) 
+    if(slave_latency >= 500 || slave_latency > 10.0*tvb_get_letohs(tvb, offset+2)/(max_interval *1.25))
         expert_add_info_format(pinfo, item, PI_PROTOCOL, PI_WARN, "Parameter mismatch");
-    
+
     offset += 2;
     item = proto_tree_add_item(tree, hf_btl2cap_timeout_multiplier, tvb, offset, 2, ENC_LITTLE_ENDIAN);
     proto_item_append_text(item, " (%g sec)",  tvb_get_letohs(tvb, offset)*0.01);
@@ -969,7 +969,7 @@ dissect_connparamresponse(tvbuff_t *tvb, int offset, packet_info *pinfo _U_, pro
 
     col_append_fstr(pinfo->cinfo, COL_INFO, " (%s)",
                     val_to_str_const(result, conn_param_result_vals, "Unknown result"));
-    
+
     return offset;
 }
 
@@ -1293,8 +1293,8 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     acl_data = (bthci_acl_data_t *)pinfo->private_data;
     l2cap_data = ep_alloc(sizeof(btl2cap_data_t));
 
-    l2cap_data->interface_id     = acl_data->interface_id;
-    l2cap_data->adapter_id       = acl_data->adapter_id;
+    l2cap_data->interface_id     = (acl_data)? acl_data->interface_id : 0;
+    l2cap_data->adapter_id       = (acl_data)? acl_data->adapter_id : 0;
     l2cap_data->chandle          = (acl_data)? acl_data->chandle : 0;
     l2cap_data->cid              = cid;
     l2cap_data->psm              = 0;
@@ -1410,7 +1410,7 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
             case 0x13: /* Connection Parameter Response */
                 offset  = dissect_connparamresponse(tvb, offset, pinfo, btl2cap_cmd_tree);
                 break;
-                
+
             default:
                 proto_tree_add_item(btl2cap_cmd_tree, hf_btl2cap_cmd_data, tvb, offset, -1, ENC_NA);
                 offset += tvb_reported_length_remaining(tvb, offset);

commit c3c6ebe6c0f1a9b4ebc33b19509895abc88178d1

From Michael Labedzki via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8304: Fix potential inconsistency with interfaces introduced with r47651.
svn path=/trunk/; revision=47655

epan/dissectors/packet-bluetooth-hci.h
@@ -60,9 +60,19 @@ extern const value_string bthci_cmd_page_scan_period_modes[];
 extern const value_string bthci_cmd_notification_types[];
 
 
+/* We support Bluetooth over various interfaces, interface_id and adapter_id
+   is used to decode further payload. Case: there is a host. Host has X
+   interfaces. Each interface has Y adapter. Each adapter has ACL handle or
+   L2CAP CID. ACL handle has L2CAP CID and/or L2CAP PSM. L2CAP CID or
+   L2CAP PSM has RFCOMM channel or other end-protocol like OBEX, AVRCP, HID,
+   AVDTP, BNEP etc. RFCOMM channel has end-protocol like OBEX, HFP, etc.
+   Important note: correct payload decoding should store needed data using
+   key contain interface_id, adapter_id, ..., last_channel_type (for example
+   RFCOMM channel, transaction_id, frame number etc. ) */
 #define HCI_INTERFACE_H1   0
 #define HCI_INTERFACE_H4   1
 #define HCI_INTERFACE_USB  2
+#define HCI_INTERFACE_AMP  3
 
 #define HCI_ADAPTER_DEFAULT 0
 

epan/dissectors/packet-btl2cap.c
@@ -33,6 +33,7 @@
 #include <epan/expert.h>
 #include <epan/tap.h>
 
+#include "packet-bluetooth-hci.h"
 #include "packet-bthci_acl.h"
 #include "packet-btsdp.h"
 #include "packet-btl2cap.h"
@@ -1293,8 +1294,8 @@ dissect_btl2cap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     acl_data = (bthci_acl_data_t *)pinfo->private_data;
     l2cap_data = ep_alloc(sizeof(btl2cap_data_t));
 
-    l2cap_data->interface_id     = (acl_data)? acl_data->interface_id : 0;
-    l2cap_data->adapter_id       = (acl_data)? acl_data->adapter_id : 0;
+    l2cap_data->interface_id     = (acl_data)? acl_data->interface_id : HCI_INTERFACE_AMP;
+    l2cap_data->adapter_id       = (acl_data)? acl_data->adapter_id : HCI_ADAPTER_DEFAULT;
     l2cap_data->chandle          = (acl_data)? acl_data->chandle : 0;
     l2cap_data->cid              = cid;
     l2cap_data->psm              = 0;