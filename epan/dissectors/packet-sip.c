c6f941e4ce990989c84b2abad238cbd6a093c542
Gerald Combs <gerald@wireshark.org>
2013-03-06 00:32:48 +0000

    Copy over revisions from the trunk:
      ------------------------------------------------------------------------
      r47966 | cmaynard | 2013-02-28 21:40:49 -0800 (Thu, 28 Feb 2013) | 3 lines
      Changed paths:
         M /trunk/epan/dissectors/packet-dcerpc-netlogon.c
    
      Fix Coverity CID 280365: Logically dead code.
      #BACKPORT (1.8, 1.6)
      ------------------------------------------------------------------------
    
    
    Copy over with manual intervention:
    
      ------------------------------------------------------------------------
      r47921 | pascal | 2013-02-27 13:19:37 -0800 (Wed, 27 Feb 2013) | 3 lines
      Changed paths:
         M /trunk/epan/dissectors/packet-netflow.c
    
      From J. Schoenwaelder via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8410 :
      Fix dissection of flowDurationMicroseconds in Netflow dissector
      ------------------------------------------------------------------------
      r47936 | pascal | 2013-02-28 00:13:40 -0800 (Thu, 28 Feb 2013) | 3 lines
      Changed paths:
         M /trunk/epan/dissectors/packet-sip.c
    
      From Hadriel Kaplan via https://www.wireshark.org/lists/wireshark-dev/201302/msg00216.html :
      Fix dissection of method name in case of RAck header
      ------------------------------------------------------------------------
    
    
    Update the release notes.
    
    svn path=/trunk-1.6/; revision=48121

--- epan/dissectors/packet-sip.c
+++ epan/dissectors/packet-sip.c
@@ -2415,7 +2415,7 @@ dissect_sip_common(tvbuff_t *tvb, int offset, packet_info *pinfo, proto_tree *tr
 						{
 							proto_tree_add_item(rack_tree, hf_sip_rack_cseq_method, tvb,
 							                    value_offset + sub_value_offset,
-							                    (int)linelen-sub_value_offset, FALSE);
+							                    (int)value_len-sub_value_offset, FALSE);
 						}
 
 						break;
