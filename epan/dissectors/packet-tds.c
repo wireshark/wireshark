b53f1ab2f03d487b313138f39c3bc750b18f928f
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-03-17 19:22:36 +0000

    Add Modelines info
    svn path=/trunk/; revision=48363

--- epan/dissectors/packet-tds.c
+++ epan/dissectors/packet-tds.c
@@ -3213,3 +3213,16 @@ proto_reg_handoff_tds(void)
     gssapi_handle = find_dissector("gssapi");
     data_handle = find_dissector("data");
 }
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
