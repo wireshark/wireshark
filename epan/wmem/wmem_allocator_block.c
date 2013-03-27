4252a18c90ca4e4e054d42db2d4af6d95cc3d59b
Evan Huus <eapache@gmail.com>
2013-03-27 00:27:45 +0000

    Greatly improve wmem tests, including random fuzz-style testing. Improve assertions in the block allocator, and fix one rare potential underflow caught by the improved tests.
    The tests now take ~200MB and 5-10 seconds to run. Hopefully this is small
    enough for the build-bots to handle, if not then we can reduce the max
    allocation size or max iterations to suit.
    
    svn path=/trunk/; revision=48574

--- epan/wmem/wmem_allocator_block.c
+++ epan/wmem/wmem_allocator_block.c
@@ -173,19 +173,19 @@ wmem_block_verify_chunk_chain(wmem_block_chunk_t *chunk)
 {
     guint32 total_len = 0;
 
-    g_assert(chunk->prev == 0);
+    g_assert_cmpuint(chunk->prev, ==, 0);
 
     do {
         total_len += chunk->len;
 
         if (WMEM_CHUNK_NEXT(chunk)) {
-            g_assert(chunk->len == WMEM_CHUNK_NEXT(chunk)->prev);
+            g_assert_cmpuint(chunk->len, ==, WMEM_CHUNK_NEXT(chunk)->prev);
         }
 
         chunk = WMEM_CHUNK_NEXT(chunk);
     } while (chunk);
 
-    g_assert(total_len == WMEM_BLOCK_SIZE);
+    g_assert_cmpuint(total_len, ==, WMEM_BLOCK_SIZE);
 }
 
 static void
@@ -312,7 +312,7 @@ wmem_block_add_to_free_list_after(wmem_block_allocator_t *allocator,
     wmem_block_free_t  *freeChunk;
 
     g_assert(!chunk->used);
-    g_assert(WMEM_CHUNK_DATA_LEN(chunk) >= sizeof(wmem_block_free_t));
+    g_assert_cmpuint(WMEM_CHUNK_DATA_LEN(chunk), >=, sizeof(wmem_block_free_t));
 
     freeChunk = WMEM_GET_FREE(chunk);
 
@@ -450,7 +450,7 @@ wmem_block_split_free_chunk(wmem_block_allocator_t *allocator,
     gboolean last;
 
     g_assert(!chunk->used);
-    g_assert(WMEM_CHUNK_DATA_LEN(chunk) >= size);
+    g_assert_cmpuint(WMEM_CHUNK_DATA_LEN(chunk), >=, size);
 
     aligned_size = WMEM_ALIGN_SIZE(size);
 
@@ -559,7 +559,7 @@ wmem_block_split_used_chunk(wmem_block_allocator_t *allocator,
     gboolean last;
 
     g_assert(chunk->used);
-    g_assert(WMEM_CHUNK_DATA_LEN(chunk) >= size);
+    g_assert_cmpuint(WMEM_CHUNK_DATA_LEN(chunk), >=, size);
 
     aligned_size = WMEM_ALIGN_SIZE(size);
 
@@ -657,7 +657,7 @@ wmem_block_alloc(void *private_data, const size_t size)
 
     /* We can't allocate more than will fit in a block (less our header),
      * which is an awful lot. */
-    g_assert(size < WMEM_BLOCK_SIZE - sizeof(wmem_block_chunk_t));
+    g_assert_cmpuint(size, <, WMEM_BLOCK_SIZE - sizeof(wmem_block_chunk_t));
 
     if (allocator->free_list_head == NULL) {
         /* No free chunks at all, grab a new block */
@@ -681,13 +681,13 @@ wmem_block_alloc(void *private_data, const size_t size)
     chunk = allocator->free_list_head;
 
     /* if we still don't have the space at this point, something is wrong */
-    g_assert(size <= WMEM_CHUNK_DATA_LEN(chunk));
+    g_assert_cmpuint(size, <=, WMEM_CHUNK_DATA_LEN(chunk));
 
     /* Split our chunk into two to preserve any trailing free space */
     wmem_block_split_free_chunk(allocator, chunk, size);
 
     /* if our split reduced our size too much, something went wrong */
-    g_assert(size <= WMEM_CHUNK_DATA_LEN(chunk));
+    g_assert_cmpuint(size, <=, WMEM_CHUNK_DATA_LEN(chunk));
 
     /* the resulting chunk should not be in the free list */
     g_assert(chunk != allocator->free_list_head);
@@ -739,12 +739,25 @@ wmem_block_realloc(void *private_data, void *ptr, const size_t size)
 
         if (tmp && (!tmp->used) &&
             (size < WMEM_CHUNK_DATA_LEN(chunk) + tmp->len)) {
-
             /* the next chunk is free and has enough extra, so just grab
              * from that */
-            wmem_block_split_free_chunk(allocator, tmp,
-                    (size - WMEM_CHUNK_DATA_LEN(chunk)
-                     - sizeof(wmem_block_chunk_t)));
+            size_t split_size;
+            
+            /* we ask for the next chunk to be split, but we don't end up
+             * using the split chunk header (it just gets merged into this one),
+             * so we want the split to be of (size - curdatalen - header_size).
+             * However, this can underflow by header_size, so we do a quick
+             * check here and floor the value to 0. */
+            split_size = size - WMEM_CHUNK_DATA_LEN(chunk);
+
+            if (split_size < sizeof(wmem_block_chunk_t)) {
+                split_size = 0;
+            }
+            else {
+                split_size -= sizeof(wmem_block_chunk_t);
+            }
+
+            wmem_block_split_free_chunk(allocator, tmp, split_size);
 
             /* Now update our 'next' count and our successor's 'prev' count */
             chunk->len += tmp->len;
--- epan/wmem/wmem_test.c
+++ epan/wmem/wmem_test.c
@@ -29,11 +29,14 @@
 #include "wmem_allocator_block.h"
 #include "config.h"
 
+#define MAX_SIMULTANEOUS_ALLOCS 1024
+#define MAX_ALLOC_SIZE (1024*64)
+
 static void
 wmem_test_block_allocator(void)
 {
     int i;
-    char *ptrs[1024];
+    char *ptrs[MAX_SIMULTANEOUS_ALLOCS];
     wmem_allocator_t *allocator;
 
     /* we set up our allocator directly to ensure our type doesn't get
@@ -42,22 +45,97 @@ wmem_test_block_allocator(void)
     allocator->type = WMEM_ALLOCATOR_BLOCK;
 
     wmem_block_verify(allocator);
+    
+    /* start with some fairly simple deterministic tests */
 
-    for (i=0; i<1024; i++) ptrs[i] = (char *)wmem_alloc(allocator, 8);
-    for (i=0; i<1024; i++) wmem_free(allocator, ptrs[i]);
+    /* we use wmem_alloc0 in part because it tests slightly more code, but
+     * primarily so that if the allocator doesn't give us enough memory or
+     * gives us memory that includes its own metadata, we write to it and
+     * things go wrong, causing the tests to fail */
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        ptrs[i] = (char *)wmem_alloc0(allocator, 8);
+    }
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        wmem_free(allocator, ptrs[i]);
+    }
 
     wmem_block_verify(allocator);
-
-    for (i=0; i<1024; i++) ptrs[i] = (char *)wmem_alloc(allocator, 64);
-    for (i=0; i<1024; i++) wmem_free(allocator, ptrs[i]);
-
+    wmem_free_all(allocator);
+    wmem_gc(allocator);
     wmem_block_verify(allocator);
 
-    for (i=0; i<1024; i++)  ptrs[i] = (char *)wmem_alloc(allocator, 512);
-    for (i=1023; i>=0; i--) ptrs[i] = (char *)wmem_realloc(allocator, ptrs[i], 16*1024);
-    for (i=0; i<1024; i++)  wmem_free(allocator, ptrs[i]);
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        ptrs[i] = (char *)wmem_alloc0(allocator, 64);
+    }
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        wmem_free(allocator, ptrs[i]);
+    }
 
     wmem_block_verify(allocator);
+    wmem_free_all(allocator);
+    wmem_gc(allocator);
+    wmem_block_verify(allocator);
+
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        ptrs[i] = (char *)wmem_alloc0(allocator, 512);
+    }
+    for (i=MAX_SIMULTANEOUS_ALLOCS-1; i>=0; i--) {
+        /* no wmem_realloc0 so just use memset manually */
+        ptrs[i] = (char *)wmem_realloc(allocator, ptrs[i], MAX_ALLOC_SIZE);
+        memset(ptrs[i], 0, MAX_ALLOC_SIZE);
+    }
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        wmem_free(allocator, ptrs[i]);
+    }
+
+    wmem_block_verify(allocator);
+    wmem_free_all(allocator);
+    wmem_gc(allocator);
+    wmem_block_verify(allocator);
+
+    /* now do some random fuzz-like tests */
+
+    /* reset our ptr array */
+    for (i=0; i<MAX_SIMULTANEOUS_ALLOCS; i++) {
+        ptrs[i] = NULL;
+    }
+
+    /* Run ~64,000 iterations */
+    for (i=0; i<1024*64; i++) {
+        gint index;
+        gint new_size;
+        
+        /* returns value 0 <= x < MAX_SIMULTANEOUS_ALLOCS which is a valid index into ptrs */
+        index = g_test_rand_int_range(0, MAX_SIMULTANEOUS_ALLOCS);
+
+        if (ptrs[index] == NULL) {
+            /* if that index is unused, allocate some random amount of memory
+             * between 0 and MAX_ALLOC_SIZE */
+            new_size = g_test_rand_int_range(0, MAX_ALLOC_SIZE);
+
+            ptrs[index] = (char *) wmem_alloc0(allocator, new_size);
+        }
+        else if (g_test_rand_bit()) {
+            /* the index is used, and our random bit has determined we will be
+             * reallocating instead of freeing. Do so to some random size
+             * between 0 and MAX_ALLOC_SIZE, then manually zero the
+             * new memory */
+            new_size = g_test_rand_int_range(0, MAX_ALLOC_SIZE);
+
+            ptrs[index] = (char *) wmem_realloc(allocator, ptrs[index],
+                    new_size);
+
+            memset(ptrs[index], 0, new_size);
+        }
+        else {
+            /* the index is used, and our random bit has determined we will be
+             * freeing instead of reallocating. Do so and NULL the pointer for
+             * the next iteration. */
+            wmem_free(allocator, ptrs[index]);
+            ptrs[index] = NULL;
+        }
+        wmem_block_verify(allocator);
+    }
 
     wmem_destroy_allocator(allocator);
 }
