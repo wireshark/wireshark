b2f2059c415d131af9c5e8155b503e34f1a4e2a1
Gerald Combs <gerald@wireshark.org>
2013-04-07 17:59:07 +0000

    Use a monospace font for the address resolution dialog so that tab alignment will work. Add modelines and reindent.
    svn path=/trunk/; revision=48777

--- ui/gtk/addr_resolution_dlg.c
+++ ui/gtk/addr_resolution_dlg.c
@@ -42,6 +42,7 @@
 
 #include "ui/gtk/dlg_utils.h"
 #include "ui/gtk/expert_comp_dlg.h"
+#include "ui/gtk/font_utils.h"
 #include "ui/gtk/gui_utils.h"
 #include "ui/gtk/help_dlg.h"
 #include "ui/gtk/main.h"
@@ -164,115 +165,129 @@ comment_summary_copy_to_clipboard_cb(GtkWidget *w _U_, GtkWidget *view)
 static void
 addres_resolution_to_texbuff(GtkTextBuffer *buffer)
 {
-	struct addrinfo *ai;
-	struct sockaddr_in *sa4;
-	struct sockaddr_in6 *sa6;
-	char   addr_str[ADDRSTRLEN];
-	int i, tab_count;
-	gchar string_buff[ADDRESS_STR_MAX];
+    struct addrinfo *ai;
+    struct sockaddr_in *sa4;
+    struct sockaddr_in6 *sa6;
+    char   addr_str[ADDRSTRLEN];
+    int i, tab_count;
+    gchar string_buff[ADDRESS_STR_MAX];
 
-	g_snprintf(string_buff, ADDRESS_STR_MAX, "# Hosts information in Wireshark \n#\n");
-	gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-	g_snprintf(string_buff, ADDRESS_STR_MAX, "# Host data gathered from %s\n\n", cfile.filename);
-	gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+    g_snprintf(string_buff, ADDRESS_STR_MAX, "# Hosts information in Wireshark \n#\n");
+    gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+    g_snprintf(string_buff, ADDRESS_STR_MAX, "# Host data gathered from %s\n\n", cfile.filename);
+    gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
 
-	/* Dump the v4 addresses first, then v6 */
-	for (ai = get_addrinfo_list(); ai; ai = ai->ai_next) {
-		if (ai->ai_family != AF_INET) {
-			continue;
-		}
+    /* Dump the v4 addresses first, then v6 */
+    for (ai = get_addrinfo_list(); ai; ai = ai->ai_next) {
+        if (ai->ai_family != AF_INET) {
+            continue;
+        }
 
-		sa4 = (struct sockaddr_in *)(void *)ai->ai_addr;
-		if (inet_ntop(AF_INET, &(sa4->sin_addr.s_addr), addr_str, ADDRSTRLEN)) {
-			tab_count = (HOSTNAME_POS - (int)strlen(addr_str)) / 8;
-			g_snprintf(string_buff, ADDRESS_STR_MAX, "%s", addr_str);
-			gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-			for (i = 0; i < tab_count; i++){
-				g_snprintf(string_buff, ADDRESS_STR_MAX, "\t");
-				gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-			}
-			g_snprintf(string_buff, ADDRESS_STR_MAX, "%s\n", ai->ai_canonname);
-			gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-		}
-	}
+        sa4 = (struct sockaddr_in *)(void *)ai->ai_addr;
+        if (inet_ntop(AF_INET, &(sa4->sin_addr.s_addr), addr_str, ADDRSTRLEN)) {
+            tab_count = (HOSTNAME_POS - (int)strlen(addr_str)) / 8;
+            g_snprintf(string_buff, ADDRESS_STR_MAX, "%s", addr_str);
+            gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+            for (i = 0; i < tab_count; i++){
+                g_snprintf(string_buff, ADDRESS_STR_MAX, "\t");
+                gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+            }
+            g_snprintf(string_buff, ADDRESS_STR_MAX, "%s\n", ai->ai_canonname);
+            gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+        }
+    }
 
 
-	for (ai = get_addrinfo_list(); ai; ai = ai->ai_next) {
-		if (ai->ai_family != AF_INET6) {
-			continue;
-		}
+    for (ai = get_addrinfo_list(); ai; ai = ai->ai_next) {
+        if (ai->ai_family != AF_INET6) {
+            continue;
+        }
 
-		sa6 = (struct sockaddr_in6 *)(void *)ai->ai_addr;
-		if (inet_ntop(AF_INET6, sa6->sin6_addr.s6_addr, addr_str, ADDRSTRLEN)) {
-			tab_count = (HOSTNAME_POS - (int)strlen(addr_str)) / 8;
-			g_snprintf(string_buff, ADDRESS_STR_MAX, "%s", addr_str);
-			gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-			for (i = 0; i < tab_count; i++){
-				g_snprintf(string_buff, ADDRESS_STR_MAX, "\t");
-				gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-			}
-			g_snprintf(string_buff, ADDRESS_STR_MAX, "%s\n", ai->ai_canonname);
-			gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
-		}
-	}
+        sa6 = (struct sockaddr_in6 *)(void *)ai->ai_addr;
+        if (inet_ntop(AF_INET6, sa6->sin6_addr.s6_addr, addr_str, ADDRSTRLEN)) {
+            tab_count = (HOSTNAME_POS - (int)strlen(addr_str)) / 8;
+            g_snprintf(string_buff, ADDRESS_STR_MAX, "%s", addr_str);
+            gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+            for (i = 0; i < tab_count; i++){
+                g_snprintf(string_buff, ADDRESS_STR_MAX, "\t");
+                gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+            }
+            g_snprintf(string_buff, ADDRESS_STR_MAX, "%s\n", ai->ai_canonname);
+            gtk_text_buffer_insert_at_cursor (buffer, string_buff, -1);
+        }
+    }
 
 }
+
 void
 addr_resolution_dlg (GtkAction *action _U_, gpointer data _U_)
 {
 
-  GtkWidget *vbox;
-  GtkWidget *view;
-  GtkWidget *scroll;
-  GtkWidget *bbox;
-  GtkWidget *ok_bt, *cancel_bt, *help_bt;
-  GtkTextBuffer *buffer;
+    GtkWidget *vbox;
+    GtkWidget *view;
+    GtkWidget *scroll;
+    GtkWidget *bbox;
+    GtkWidget *ok_bt, *cancel_bt, *help_bt;
+    GtkTextBuffer *buffer;
 
-  addr_resolution_dlg_w = dlg_window_new ("Address Resolution");
-  gtk_widget_set_size_request (addr_resolution_dlg_w, 500, 160);
-  gtk_window_set_resizable (GTK_WINDOW (addr_resolution_dlg_w), TRUE);
-  gtk_container_set_border_width (GTK_CONTAINER (addr_resolution_dlg_w), DLG_OUTER_MARGIN);
+    addr_resolution_dlg_w = dlg_window_new ("Address Resolution");
+    gtk_widget_set_size_request (addr_resolution_dlg_w, 750, 350);
+    gtk_window_set_resizable (GTK_WINDOW (addr_resolution_dlg_w), TRUE);
+    gtk_container_set_border_width (GTK_CONTAINER (addr_resolution_dlg_w), DLG_OUTER_MARGIN);
 
-  vbox = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, DLG_UNRELATED_SPACING, FALSE);
-  gtk_container_add (GTK_CONTAINER (addr_resolution_dlg_w), vbox);
-  gtk_widget_show (vbox);
+    vbox = ws_gtk_box_new(GTK_ORIENTATION_VERTICAL, DLG_UNRELATED_SPACING, FALSE);
+    gtk_container_add (GTK_CONTAINER (addr_resolution_dlg_w), vbox);
+    gtk_widget_show (vbox);
 
-  view = gtk_text_view_new ();
-  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(view), GTK_WRAP_WORD);
-  buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
-  gtk_widget_show (view);
+    view = gtk_text_view_new ();
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(view), GTK_WRAP_WORD);
+    buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
+    gtk_widget_modify_font(view, user_font_get_regular());
+    gtk_widget_show (view);
 
-  scroll = gtk_scrolled_window_new(NULL, NULL);
-  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll),
-                  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
-  gtk_container_add(GTK_CONTAINER(scroll), view);
-  gtk_widget_show(scroll);
-  gtk_box_pack_start(GTK_BOX (vbox), scroll, TRUE, TRUE, 0);
+    scroll = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll),
+            GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+    gtk_container_add(GTK_CONTAINER(scroll), view);
+    gtk_widget_show(scroll);
+    gtk_box_pack_start(GTK_BOX (vbox), scroll, TRUE, TRUE, 0);
 
-  /* Get the address list */
-  addres_resolution_to_texbuff(buffer);
+    /* Get the address list */
+    addres_resolution_to_texbuff(buffer);
 
-  /* Button row. */
-  bbox = dlg_button_row_new (GTK_STOCK_OK, GTK_STOCK_CANCEL, GTK_STOCK_HELP, NULL);
-  gtk_box_pack_end (GTK_BOX(vbox), bbox, FALSE, FALSE, 0);
+    /* Button row. */
+    bbox = dlg_button_row_new (GTK_STOCK_OK, GTK_STOCK_CANCEL, GTK_STOCK_HELP, NULL);
+    gtk_box_pack_end (GTK_BOX(vbox), bbox, FALSE, FALSE, 0);
 
-  ok_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_OK);
-  /*g_signal_connect (ok_bt, "clicked", G_CALLBACK(pkt_comment_text_buff_ok_cb), view);*/
-  gtk_widget_set_sensitive (ok_bt, TRUE);
+    ok_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_OK);
+    /*g_signal_connect (ok_bt, "clicked", G_CALLBACK(pkt_comment_text_buff_ok_cb), view);*/
+    gtk_widget_set_sensitive (ok_bt, TRUE);
 
-  cancel_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_CANCEL);
-  window_set_cancel_button (addr_resolution_dlg_w, cancel_bt, window_cancel_button_cb);
+    cancel_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_CANCEL);
+    window_set_cancel_button (addr_resolution_dlg_w, cancel_bt, window_cancel_button_cb);
 
-  help_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_HELP);
+    help_bt = (GtkWidget *)g_object_get_data (G_OBJECT(bbox), GTK_STOCK_HELP);
 #if 0
-  g_signal_connect (help_bt, "clicked",/* G_CALLBACK(topic_cb)*/NULL, /*(gpointer)HELP_MANUAL_ADDR_RESOLVE_DIALOG*/NULL);
+    g_signal_connect (help_bt, "clicked",/* G_CALLBACK(topic_cb)*/NULL, /*(gpointer)HELP_MANUAL_ADDR_RESOLVE_DIALOG*/NULL);
 #endif
-  gtk_widget_set_sensitive (help_bt, FALSE);
+    gtk_widget_set_sensitive (help_bt, FALSE);
 
-  gtk_widget_grab_default (ok_bt);
-  /*g_signal_connect (addr_resolution_dlg_w, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);*/
+    gtk_widget_grab_default (ok_bt);
+    /*g_signal_connect (addr_resolution_dlg_w, "delete_event", G_CALLBACK(window_delete_event_cb), NULL);*/
 
 
-  gtk_widget_show (addr_resolution_dlg_w);
+    gtk_widget_show (addr_resolution_dlg_w);
 }
 
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
