a0e54160af7cf1bf71f5a6326dec3621c64ed611
Guy Harris <guy@alum.mit.edu>
2013-04-21 03:22:30 +0000

    show_reported_bounds_error() should only be used to handle things such as overflows in offsets, where the error presumably was that some field size was so bogusly huge that it overflowed 32 bits; that's a malformed packet even if the packet wasn't reassembled.
    svn path=/trunk/; revision=48945

--- epan/show_exception.c
+++ epan/show_exception.c
@@ -148,27 +148,9 @@ show_reported_bounds_error(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 	proto_item *item;
 
-	if (pinfo->fragmented) {
-		/*
-		 * We were dissecting an unreassembled fragmented
-		 * packet when the exception was thrown, so the
-		 * problem isn't that the dissector expected
-		 * something but it wasn't in the packet, the
-		 * problem is that the dissector expected something
-		 * but it wasn't in the fragment we dissected.
-		 */
-		col_append_fstr(pinfo->cinfo, COL_INFO,
-		    "[Unreassembled Packet%s] ",
-		    pinfo->noreassembly_reason);
-		item = proto_tree_add_protocol_format(tree, proto_unreassembled,
-		    tvb, 0, 0, "[Unreassembled Packet%s: %s]",
-		    pinfo->noreassembly_reason, pinfo->current_proto);
-		expert_add_info_format(pinfo, item, PI_REASSEMBLE, PI_WARN, "Unreassembled Packet (Exception occurred)");
-	} else {
-		col_append_str(pinfo->cinfo, COL_INFO,
-		    "[Malformed Packet]");
-		item = proto_tree_add_protocol_format(tree, proto_malformed,
-		    tvb, 0, 0, "[Malformed Packet: %s]", pinfo->current_proto);
-		expert_add_info_format(pinfo, item, PI_MALFORMED, PI_ERROR, "Malformed Packet (Exception occurred)");
-	}
+	col_append_str(pinfo->cinfo, COL_INFO,
+	    "[Malformed Packet]");
+	item = proto_tree_add_protocol_format(tree, proto_malformed,
+	    tvb, 0, 0, "[Malformed Packet: %s]", pinfo->current_proto);
+	expert_add_info_format(pinfo, item, PI_MALFORMED, PI_ERROR, "Malformed Packet (Exception occurred)");
 }
