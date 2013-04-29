67840cc07d509d6856f95ff9c08d7a32e28e68e1
Gerald Combs <gerald@wireshark.org>
2013-04-29 20:26:44 +0000

    Copy over tools/fuzz-test.sh from the 1.8 branch, which includes the following changes:
      ------------------------------------------------------------------------
      r47305 | gerald | 2013-01-26 12:12:52 -0800 (Sat, 26 Jan 2013) | 6 lines
      
      Instead of setting resource limits on the fuzz-test.sh process itself,
      set limits on the TShark subprocess. This should hopefully take care
      of the strange fuzz failures we've seen lately.
      
      Reduce the maximum CPU time to 5 minutes while we're at it.
      
      ------------------------------------------------------------------------
      r47287 | gerald | 2013-01-25 15:06:52 -0800 (Fri, 25 Jan 2013) | 4 lines
      
      Temporarily run "ps" after fuzzing to show the cumulative CPU time.
      Hopefully this will shed some light on the recent "CPU time limit
      exceeded" errors.
      
      ------------------------------------------------------------------------
      r42295 | morriss | 2012-04-27 10:58:06 -0700 (Fri, 27 Apr 2012) | 1 line
      
      Fix r42293: Make the script actually fail when tshark fails: use a new variable to indicate when we've caught a signal rather than (re-)using MAX_PASSES.
      ------------------------------------------------------------------------
      r42293 | morriss | 2012-04-27 09:58:21 -0700 (Fri, 27 Apr 2012) | 1 line
      
      Get the script working again in the "unlimited passes" mode.
      ------------------------------------------------------------------------
      r42134 | gerald | 2012-04-18 09:10:08 -0700 (Wed, 18 Apr 2012) | 2 lines
      
      Terminate our inner loops as well.
      
      ------------------------------------------------------------------------
      r42133 | gerald | 2012-04-18 09:04:05 -0700 (Wed, 18 Apr 2012) | 3 lines
      
      Tell the main loop to terminate when we catch a signal instead of trying
      to clean up in two places.
      
      ------------------------------------------------------------------------
      r37513 | gerald | 2011-06-01 14:56:11 -0700 (Wed, 01 Jun 2011) | 2 lines
      
      Exit if we don't have enough plugins.
      
      ------------------------------------------------------------------------
      r37509 | guy | 2011-06-01 09:23:51 -0700 (Wed, 01 Jun 2011) | 2 lines
      
      Remove the temporary debugging stuff.
      
      ------------------------------------------------------------------------
      r37504 | guy | 2011-06-01 02:12:10 -0700 (Wed, 01 Jun 2011) | 4 lines
      
      Temporary debugging stuff to see in what environment tshark is being
      run - the plugins might not be getting loaded, and the goal is to figure
      out why.
      
      ------------------------------------------------------------------------
    
    Remove the minimum plugin check since Tshark 1.6 doesn't support it.
    
    svn path=/trunk-1.6/; revision=49086

--- tools/fuzz-test.sh
+++ tools/fuzz-test.sh
@@ -28,6 +28,9 @@ ERR_FILE=$BASE_NAME.err
 # Loop this many times (< 1 loops forever)
 MAX_PASSES=0
 
+# Did we catch a signal?
+DONE=0
+
 # Perform a two pass analysis on the capture file?
 TWO_PASS=
 
@@ -36,7 +39,7 @@ CONFIG_PROFILE=
 
 # These may be set to your liking
 # Stop the child process if it's running longer than x seconds
-MAX_CPU_TIME=900
+MAX_CPU_TIME=300
 # Stop the child process if it's using more than y * 1024 bytes
 MAX_VMEM=500000
 # Stop the child process if its stack is larger than than z * 1024 bytes
@@ -77,11 +80,6 @@ CAPINFOS="$BIN_DIR/capinfos"
 if [ "$BIN_DIR" = "." ]; then
     export WIRESHARK_RUN_FROM_BUILD_DIRECTORY=1
 fi
-
-# set some limits to the child processes, e.g. stop it if it's running longer then MAX_CPU_TIME seconds
-# (ulimit is not supported well on cygwin and probably other platforms, e.g. cygwin shows some warnings)
-ulimit -S -t $MAX_CPU_TIME -v $MAX_VMEM -s $MAX_STACK
-ulimit -c unlimited
 
 ### usually you won't have to change anything below this line ###
 
@@ -130,7 +128,7 @@ echo "Running $TSHARK with args: $TSHARK_ARGS ($HOWMANY)"
 echo ""
 
 # Clean up on <ctrl>C, etc
-trap "rm -f $TMP_DIR/$TMP_FILE $TMP_DIR/$ERR_FILE; echo ""; exit 0" HUP INT TERM
+trap "DONE=1; echo 'Caught signal'" HUP INT TERM
 
 
 ##############################################################################
@@ -172,12 +170,15 @@ export MallocBadFreeAbort=1
 
 # Iterate over our capture files.
 PASS=0
-while [ $PASS -lt $MAX_PASSES -o $MAX_PASSES -lt 1 ] ; do
+while [ \( $PASS -lt $MAX_PASSES -o $MAX_PASSES -lt 1 \) -a $DONE -ne 1 ] ; do
     PASS=`expr $PASS + 1`
     echo "Starting pass $PASS:"
     RUN=0
 
     for CF in "$@" ; do
+	if [ $DONE -eq 1 ]; then
+	    break # We caught a signal
+	fi
         RUN=$(( $RUN + 1 ))
         if [ $(( $RUN % 50 )) -eq 0 ] ; then
             echo "    [Pass $PASS]"
@@ -193,7 +194,7 @@ while [ $PASS -lt $MAX_PASSES -o $MAX_PASSES -lt 1 ] ; do
 	    echo "Not a valid capture file"
 	    rm -f $TMP_DIR/$ERR_FILE
 	    continue
-	elif [ $RETVAL -ne 0 ] ; then
+	elif [ $RETVAL -ne 0 -a $DONE -ne 1 ] ; then
 	    # Some other error
 	    echo ""
 	    echo " ERROR"
@@ -216,15 +217,23 @@ while [ $PASS -lt $MAX_PASSES -o $MAX_PASSES -lt 1 ] ; do
 	    fi
 	fi
 
-	"$TSHARK" $TSHARK_ARGS $TMP_DIR/$TMP_FILE \
-	    > /dev/null 2>> $TMP_DIR/$ERR_FILE
+        # Run TShark in a child process with limits, e.g. stop it if it's
+        # running longer then MAX_CPU_TIME seconds. ulimit is not supported
+        # well on cygwin and probably other platforms, e.g. cygwin shows
+        # some warnings)
+        (
+            ulimit -S -t $MAX_CPU_TIME -v $MAX_VMEM -s $MAX_STACK
+            ulimit -c unlimited
+            "$TSHARK" $TSHARK_ARGS $TMP_DIR/$TMP_FILE \
+                > /dev/null 2>> $TMP_DIR/$ERR_FILE
+        )
 	RETVAL=$?
 	# Uncomment the next two lines to enable dissector bug
 	# checking.
 	#grep -i "dissector bug" $TMP_DIR/$ERR_FILE \
 	#    > /dev/null 2>&1 && DISSECTOR_BUG=1
-	if [ $RETVAL -ne 0 -o $DISSECTOR_BUG -ne 0 ] ; then
-		echo ""
+	if [ \( $RETVAL -ne 0 -o $DISSECTOR_BUG -ne 0 \) -a $DONE -ne 1 ] ; then
+	    echo ""
 	    echo " ERROR"
 	    echo -e "Processing failed.  Capture info follows:\n"
 	    echo "  Output file: $TMP_DIR/$TMP_FILE"
@@ -232,8 +241,8 @@ while [ $PASS -lt $MAX_PASSES -o $MAX_PASSES -lt 1 ] ; do
 	    cat $TMP_DIR/$ERR_FILE
 	    exit 1
 	fi
+
 	echo " OK"
-	    rm -f $TMP_DIR/$TMP_FILE $TMP_DIR/$ERR_FILE
+	rm -f $TMP_DIR/$TMP_FILE $TMP_DIR/$ERR_FILE
     done
 done
-
