5ba458916a954261753794afba073fc0ed6243ce
Jörg Mayer <jmayer@loplof.de>
2013-05-07 03:25:49 +0000

    Move common code into UseAsn2Wrs.cmake. Include the new file. Use the new file. Add cmake support to rebuild the gsm_map dissector.
    svn path=/trunk/; revision=49194

---
+++ cmake/modules/UseAsn2Wrs.cmake
@@ -0,0 +1,44 @@
+#
+# $Id$
+#
+MACRO(ASN2WRS)
+	set( DISSECTOR ${CMAKE_SOURCE_DIR}/epan/dissectors/packet-${PROTOCOL_NAME}.c )
+
+	if ( NOT PROTO_OPT )
+		set( PROTO_OPT -p ${PROTOCOL_NAME} )
+	elseif ( PROTO_OPT STREQUAL "_EMPTY_" )
+		set( PROTO_OPT )
+	endif()
+
+	# Don't use packet-${PROTOCOL_NAME}.c instead of generate_dissector, it will
+	# cause EXCLUDE_FROM_ALL to be ignored.
+	ADD_CUSTOM_TARGET(generate_dissector-${PROTOCOL_NAME} ALL
+		COMMAND ${PYTHON_EXECUTABLE}
+		  ${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
+		  ${A2W_FLAGS}
+		  ${PROTO_OPT}
+		  -c ${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf
+		  -s ${CMAKE_CURRENT_SOURCE_DIR}/packet-${PROTOCOL_NAME}-template
+		  -D ${CMAKE_CURRENT_SOURCE_DIR}
+		  -O ${CMAKE_SOURCE_DIR}/epan/dissectors
+		  ${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
+		DEPENDS
+		  ${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
+		  ${SRC_FILES}
+		  ${EXTRA_CNF}
+	)
+
+	ADD_CUSTOM_TARGET(generate_export-${PROTOCOL_NAME} ALL
+		COMMAND ${PYTHON_EXECUTABLE}
+		  ${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
+		  -E
+		  ${A2W_FLAGS}
+		  ${PROTO_OPT}
+		  -c ${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf
+		  -D ${CMAKE_CURRENT_SOURCE_DIR}
+		  ${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
+		DEPENDS
+		  ${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
+		  ${SRC_FILES}
+	)
+ENDMACRO()
