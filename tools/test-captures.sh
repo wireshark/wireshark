7d63573632b45568ca4304c1b11d3ff1ab5b98bc
Evan Huus <eapache@gmail.com>
2013-05-19 12:51:35 +0000

    Update test-fuzzed-cap.sh: - support testing multiple files at once - use tools/test-common.sh to make sure we have the most up-to-date environment   variables set (the old version was missing the WMEM_ one for example) - rename to test-captures.sh since it works just fine on non-fuzzed files
    svn path=/trunk/; revision=49412

---
+++ tools/test-captures.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# A little script to run tshark on capture file[s] (potentially ones that
+# failed fuzz testing). Useful because it sets up ulimits and other environment
+# variables for you to ensure things like misused ephemeral memory are caught.
+# (I'm writing this after having my machine hang up for like 15 minutes because
+# I wasn't paying attention while tshark was running on a fuzzed capture and
+# it used all my RAM + swap--which was pretty painful.)
+#
+# Copyright 2012 Jeff Morriss <jeff.morriss.ws [AT] gmail.com>
+#
+# $Id$
+#
+# Wireshark - Network traffic analyzer
+# By Gerald Combs <gerald@wireshark.org>
+# Copyright 1998 Gerald Combs
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+if [ $# -lt 1 ]
+then
+	printf "Usage: $0 /path/to/file[s].pcap\n"
+	exit 1
+fi
+
+TEST_TYPE="manual"
+. `dirname $0`/test-common.sh
+
+# set some limits to the child processes, e.g. stop it if it's running longer then MAX_CPU_TIME seconds
+# (ulimit is not supported well on cygwin and probably other platforms, e.g. cygwin shows some warnings)
+ulimit -S -t $MAX_CPU_TIME -v $MAX_VMEM
+# Allow core files to be generated
+ulimit -c unlimited
+
+for file in "$@"
+do
+	echo "Testing file $file..."
+	echo -n " - with tree... "
+	if $BIN_DIR/tshark -nVxr $file > /dev/null
+	then
+		echo "OK"
+		echo -n " - without tree... "
+		if $BIN_DIR/tshark -nr $file > /dev/null
+		then
+			echo "OK"
+		else
+			echo "Failed"
+			exit 1
+		fi
+	else
+		echo "Failed"
+		exit 1
+	fi
+done
