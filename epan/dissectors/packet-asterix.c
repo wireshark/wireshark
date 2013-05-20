0a0b6bf6c23f177192e910d37e1fc9716e410f46
Evan Huus <eapache@gmail.com>
2013-05-20 20:30:23 +0000

    Third time lucky, hopefully fix the last of the compiler warnings...
    svn path=/trunk/; revision=49460

--- epan/dissectors/packet-asterix.c
+++ epan/dissectors/packet-asterix.c
@@ -4448,7 +4448,7 @@ static void twos_complement (gint64 *v, guint8 bit_len)
     i = 1 << (bit_len - 1);
     to_stuff = *v & i;
     i = 0L;
-    if (to_stuff) i = 0xffffffffffffffffL << bit_len;
+    if (to_stuff) i = G_GINT64_CONSTANT(0xffffffffffffffff) << bit_len;
     *v = *v | i;
 }
 
