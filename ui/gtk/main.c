107fa2d59947d6e7e6c483708ff6a53fd5b666bc
Guy Harris <guy@alum.mit.edu>
2013-05-22 10:21:52 +0000

    Sigh, it was a "building without libpcap" issue.
    svn path=/trunk/; revision=49507

--- ui/gtk/main.c
+++ ui/gtk/main.c
@@ -126,7 +126,6 @@
 #include "capture_ui_utils.h"
 #include "capture-pcap-util.h"
 #include "capture_ifinfo.h"
-#include "capture_session.h"
 #include "capture.h"
 #include "capture_sync.h"
 #endif
@@ -210,36 +209,10 @@
  */
 #define RC_FILE "gtkrc"
 
+#ifdef HAVE_LIBPCAP
 capture_options global_capture_opts;
-
-/* Current state of capture engine. XXX - differentiate states */
-typedef enum {
-    CAPTURE_STOPPED,        /**< stopped */
-    CAPTURE_PREPARING,      /**< preparing, but still no response from capture child */
-    CAPTURE_RUNNING         /**< capture child signalled ok, capture is running now */
-} capture_state;
-
-/*
- * State of a capture session.
- */
-struct oh_shut_up_already {
-    int fork_child;                 /**< If not -1, in parent, process ID of child */
-    int fork_child_status;          /**< Child exit status */
-#ifdef _WIN32
-    int signal_pipe_write_fd;       /**< the pipe to signal the child */
+capture_session global_capture_session;
 #endif
-    capture_state state;            /**< current state of the capture engine */
-#ifndef _WIN32
-    uid_t owner;                    /**< owner of the cfile */
-    gid_t group;                    /**< group of the cfile */
-#endif
-    gboolean session_started;
-    capture_options *capture_opts;  /**< options for this capture */
-    void *cf;                       /**< handle to cfile (note: untyped handle) */
-};
-
-typedef struct oh_shut_up_already capture_session;
-struct oh_shut_up_already global_capture_session;
 
 capture_file cfile;
 
