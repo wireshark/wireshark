f89f4cda6c8d1046dd349e1e4375e976d6756b19
Jeff Morriss <jeff.morriss.ws@gmail.com>
2013-05-29 03:19:31 +0000

    Fix the fuzz failure reported in https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8724 :
    Make proto_tree_children_foreach() NULL-tree safe.  Crash is new since r49602
    took out an if(tree).
    
    svn path=/trunk/; revision=49607

--- epan/proto.c
+++ epan/proto.c
@@ -517,6 +517,9 @@ proto_tree_children_foreach(proto_tree *tree, proto_tree_foreach_func func,
 	proto_node *node = tree;
 	proto_node *current;
 
+	if (!node)
+		return;
+
 	node = node->first_child;
 	while (node != NULL) {
 		current = node;
