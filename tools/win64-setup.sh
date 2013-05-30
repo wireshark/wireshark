bc78191e3180a5f1c60228a97d81fcc6b169f02c
Pascal Quantin <pascal.quantin@gmail.com>
2013-05-30 20:13:56 +0000

    Upgrade user manual
    svn path=/trunk/; revision=49634

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -4,7 +4,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2013-03-09"
+export DOWNLOAD_TAG="2013-04-22"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
