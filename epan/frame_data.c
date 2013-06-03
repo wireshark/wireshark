wireshark/wireshark

commit 85a8e304ddeb73c54ad7e86271d67543484645c9

Add the posibillity to use a key for per-packet-data.
svn path=/trunk/; revision=49259

epan/frame_data.c
@@ -38,6 +38,7 @@
    index and opaque pointer. */
 typedef struct _frame_proto_data {
   int   proto;
+  guint8 key;
   void *proto_data;
 } frame_proto_data;
 
@@ -60,11 +61,12 @@ p_compare(gconstpointer a, gconstpointer b)
 }
 
 void
-p_add_proto_data(frame_data *fd, int proto, void *proto_data)
+p_add_proto_data(frame_data *fd, int proto, guint8 key, void *proto_data)
 {
   frame_proto_data *p1 = (frame_proto_data *)wmem_alloc(wmem_file_scope(), sizeof(frame_proto_data));
 
   p1->proto = proto;
+  p1->proto = key;
   p1->proto_data = proto_data;
 
   /* Add it to the GSLIST */
@@ -75,12 +77,13 @@ p_add_proto_data(frame_data *fd, int proto, void *proto_data)
 }
 
 void *
-p_get_proto_data(frame_data *fd, int proto)
+p_get_proto_data(frame_data *fd, int proto, guint8 key)
 {
   frame_proto_data  temp, *p1;
   GSList           *item;
 
   temp.proto = proto;
+  temp.proto = key;
   temp.proto_data = NULL;
 
   item = g_slist_find_custom(fd->pfd, (gpointer *)&temp, p_compare);
@@ -95,12 +98,13 @@ p_get_proto_data(frame_data *fd, int proto)
 }
 
 void
-p_remove_proto_data(frame_data *fd, int proto)
+p_remove_proto_data(frame_data *fd, int proto, guint8 key)
 {
   frame_proto_data  temp;
   GSList           *item;
 
   temp.proto = proto;
+  temp.proto = key;
   temp.proto_data = NULL;
 
   item = g_slist_find_custom(fd->pfd, (gpointer *)&temp, p_compare);

commit 6c5507104a2e8c630c44c1b8f7dc233da30e315b

Copy over from trunk: ------------------------------------------------------------------------ r49310 | pascal | 2013-05-15 11:25:25 +0200 (mer., 15 mai 2013) | 2 lines
Fix copy/paste error introduced in r49259

------------------------------------------------------------------------
r49317 | gerald | 2013-05-15 19:49:45 +0200 (mer., 15 mai 2013) | 2 lines

Un-shadow a couple of variables.

------------------------------------------------------------------------

svn path=/trunk-1.10/; revision=49724

epan/frame_data.c
@@ -57,7 +57,7 @@ p_compare(gconstpointer a, gconstpointer b)
     if (ap -> key > bp -> key){
       return 1;
     }else if (ap -> key == bp -> key){
-    return 0;
+      return 0;
 	}
     return -1;
   }else{
@@ -71,7 +71,7 @@ p_add_proto_data(frame_data *fd, int proto, guint8 key, void *proto_data)
   frame_proto_data *p1 = (frame_proto_data *)wmem_alloc(wmem_file_scope(), sizeof(frame_proto_data));
 
   p1->proto = proto;
-  p1->proto = key;
+  p1->key = key;
   p1->proto_data = proto_data;
 
   /* Add it to the GSLIST */
@@ -88,7 +88,7 @@ p_get_proto_data(frame_data *fd, int proto, guint8 key)
   GSList           *item;
 
   temp.proto = proto;
-  temp.proto = key;
+  temp.key = key;
   temp.proto_data = NULL;
 
   item = g_slist_find_custom(fd->pfd, (gpointer *)&temp, p_compare);
@@ -109,7 +109,7 @@ p_remove_proto_data(frame_data *fd, int proto, guint8 key)
   GSList           *item;
 
   temp.proto = proto;
-  temp.proto = key;
+  temp.key = key;
   temp.proto_data = NULL;
 
   item = g_slist_find_custom(fd->pfd, (gpointer *)&temp, p_compare);
@@ -120,10 +120,10 @@ p_remove_proto_data(frame_data *fd, int proto, guint8 key)
 }
 
 gchar *
-p_get_proto_name_and_key(frame_data *fd, guint index){
+p_get_proto_name_and_key(frame_data *fd, guint pfd_index){
 	frame_proto_data  *temp;
 
-	temp = (frame_proto_data*)g_slist_nth_data(fd->pfd, index);
+	temp = (frame_proto_data*)g_slist_nth_data(fd->pfd, pfd_index);
 
 	return ep_strdup_printf("[%s, key %u]",proto_get_protocol_name(temp->proto), temp->key);
 