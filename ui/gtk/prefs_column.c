a0823d82c9d87b21550f518050646f31366ef5b8
Gerald Combs <gerald@wireshark.org>
2013-06-03 20:01:15 +0000

    Custom columns are specified using display filter field names, not display filters.
    svn path=/trunk/; revision=49726

--- ui/gtk/prefs_column.c
+++ ui/gtk/prefs_column.c
@@ -267,8 +267,7 @@ column_prefs_show(GtkWidget *prefs_window) {
     ws_gtk_grid_attach_extended(GTK_GRID(grid), field_lb, 0, 1, 1, 1, (GtkAttachOptions)(GTK_EXPAND|GTK_FILL), (GtkAttachOptions)0, 0, 0);
     gtk_widget_set_sensitive(field_lb, FALSE);
     gtk_widget_set_tooltip_text(field_lb,
-                          "Field name used when field type is \"Custom\". "
-                          "This string has the same syntax as a display filter string.");
+                          "Display filter field name to show when thefield type is \"Custom\".");
     gtk_widget_show(field_lb);
 
     field_te = gtk_entry_new();
