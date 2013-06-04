8df372bb6aaabaa3e781d183215be19a62e247f9
Chris Maynard <Christopher.Maynard@GTECH.COM>
2013-06-04 19:12:32 +0000

    Fix warning reported by MSVC: to_str.c(262) : warning C4114: same type qualifier used more than once
    svn path=/trunk/; revision=49770

--- epan/to_str.c
+++ epan/to_str.c
@@ -259,7 +259,7 @@ guint32_to_str_buf_len(const guint32 u) {
     return 1;
 }
 
-static const char const fast_strings[][4] = {
+static const char fast_strings[][4] = {
 "0", "1", "2", "3", "4", "5", "6", "7",
 "8", "9", "10", "11", "12", "13", "14", "15",
 "16", "17", "18", "19", "20", "21", "22", "23",
