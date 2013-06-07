e4893f0448eb85045fdc775958b1c4f6ae2ce7b7
Stig Bjørlykke <stig@bjorlykke.org>
2013-06-07 06:53:24 +0000

    Ensure we only put private profiles directly in the profiles menu and put global profiles in the "New from Global" menu.
    svn path=/trunk/; revision=49830

--- ui/gtk/profile_dlg.c
+++ ui/gtk/profile_dlg.c
@@ -679,7 +679,7 @@ profile_show_popup_cb(GtkWidget *w _U_, GdkEvent *event, gpointer user_data _U_)
   while (fl_entry && fl_entry->data) {
     profile = (profile_def *)fl_entry->data;
 
-    if (profile_exists(profile->name, FALSE)) {
+    if (!profile->is_global) {
       menu_item = gtk_check_menu_item_new_with_label(profile->name);
       if (strcmp(profile->name, profile_name)==0) {
         /* Check current profile */
@@ -689,7 +689,7 @@ profile_show_popup_cb(GtkWidget *w _U_, GdkEvent *event, gpointer user_data _U_)
       g_signal_connect(menu_item, "activate", G_CALLBACK(select_profile_cb), g_strdup(profile->name));
       gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
       gtk_widget_show(menu_item);
-    } else if (profile_exists(profile->name, TRUE)) {
+    } else {
       if (!sub_menu) {
         menu_item =  gtk_separator_menu_item_new();
         gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
