02a6c2b46219ad1b7b85e7ec46d584036323cf26
Bill Meier <wmeier@newsguy.com>
2013-07-01 20:54:15 +0000

    Do whitespace cleanup; Reformat hf[] entries.
    svn path=/trunk/; revision=50307

--- epan/dissectors/packet-nstrace.c
+++ epan/dissectors/packet-nstrace.c
@@ -12,12 +12,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -57,64 +57,64 @@ static int hf_ns_activity_pcb_zombie  = -1;
 static int hf_ns_activity_natpcb_zombie  = -1;
 static int hf_ns_activity_lbstats_sync  = -1;
 static int hf_ns_activity_stats_req  = -1;
- 
+
 
 static gint ett_ns = -1;
 static gint ett_ns_flags = -1;
 static gint ett_ns_activity_flags = -1;
 
 static const value_string ns_dir_vals[] = {
-	{ NSPR_PDPKTRACEFULLTX_V10, "TX" },
-	{ NSPR_PDPKTRACEFULLTX_V20, "TX" },
-	{ NSPR_PDPKTRACEFULLTXB_V10, "TXB" },
-	{ NSPR_PDPKTRACEFULLTXB_V20, "TXB" },
-	{ NSPR_PDPKTRACEFULLRX_V10, "RX" },
-	{ NSPR_PDPKTRACEFULLRX_V20, "RX" },
-	{ NSPR_PDPKTRACEPARTTX_V10, "TX"  },
-	{ NSPR_PDPKTRACEPARTTX_V20, "TX" },
-	{ NSPR_PDPKTRACEPARTTXB_V10, "TXB" },
-	{ NSPR_PDPKTRACEPARTTXB_V20, "TXB" },
-	{ NSPR_PDPKTRACEPARTRX_V10, "RX" },
-	{ NSPR_PDPKTRACEPARTRX_V20, "RX" },
-	{ NSPR_PDPKTRACEFULLTX_V21, "TX" },
-	{ NSPR_PDPKTRACEFULLTXB_V21, "TXB" },
-	{ NSPR_PDPKTRACEFULLRX_V21, "RX" },
-	{ NSPR_PDPKTRACEPARTTX_V21, "TX" },
-	{ NSPR_PDPKTRACEPARTTXB_V21, "TXB" },
-	{ NSPR_PDPKTRACEPARTRX_V21, "RX" },
-	{ NSPR_PDPKTRACEFULLTX_V22, "TX" },
-	{ NSPR_PDPKTRACEFULLTX_V23, "TX" },
-	{ NSPR_PDPKTRACEFULLTX_V24, "TX" },
-	{ NSPR_PDPKTRACEFULLTX_V25, "TX" },
-	{ NSPR_PDPKTRACEFULLTX_V26, "TX" },
-	{ NSPR_PDPKTRACEFULLTXB_V22, "TXB" },
-	{ NSPR_PDPKTRACEFULLTXB_V23, "TXB" },
-	{ NSPR_PDPKTRACEFULLTXB_V24, "TXB" },
-	{ NSPR_PDPKTRACEFULLTXB_V25, "TXB" },
-	{ NSPR_PDPKTRACEFULLTXB_V26, "TXB" },
-	{ NSPR_PDPKTRACEFULLRX_V22, "RX" },
-	{ NSPR_PDPKTRACEFULLRX_V23, "RX" },
-	{ NSPR_PDPKTRACEFULLRX_V24, "RX" },
-	{ NSPR_PDPKTRACEFULLRX_V25, "RX" },
-	{ NSPR_PDPKTRACEFULLRX_V26, "RX" },
+	{ NSPR_PDPKTRACEFULLTX_V10,    "TX" },
+	{ NSPR_PDPKTRACEFULLTX_V20,    "TX" },
+	{ NSPR_PDPKTRACEFULLTXB_V10,   "TXB" },
+	{ NSPR_PDPKTRACEFULLTXB_V20,   "TXB" },
+	{ NSPR_PDPKTRACEFULLRX_V10,    "RX" },
+	{ NSPR_PDPKTRACEFULLRX_V20,    "RX" },
+	{ NSPR_PDPKTRACEPARTTX_V10,    "TX"  },
+	{ NSPR_PDPKTRACEPARTTX_V20,    "TX" },
+	{ NSPR_PDPKTRACEPARTTXB_V10,   "TXB" },
+	{ NSPR_PDPKTRACEPARTTXB_V20,   "TXB" },
+	{ NSPR_PDPKTRACEPARTRX_V10,    "RX" },
+	{ NSPR_PDPKTRACEPARTRX_V20,    "RX" },
+	{ NSPR_PDPKTRACEFULLTX_V21,    "TX" },
+	{ NSPR_PDPKTRACEFULLTXB_V21,   "TXB" },
+	{ NSPR_PDPKTRACEFULLRX_V21,    "RX" },
+	{ NSPR_PDPKTRACEPARTTX_V21,    "TX" },
+	{ NSPR_PDPKTRACEPARTTXB_V21,   "TXB" },
+	{ NSPR_PDPKTRACEPARTRX_V21,    "RX" },
+	{ NSPR_PDPKTRACEFULLTX_V22,    "TX" },
+	{ NSPR_PDPKTRACEFULLTX_V23,    "TX" },
+	{ NSPR_PDPKTRACEFULLTX_V24,    "TX" },
+	{ NSPR_PDPKTRACEFULLTX_V25,    "TX" },
+	{ NSPR_PDPKTRACEFULLTX_V26,    "TX" },
+	{ NSPR_PDPKTRACEFULLTXB_V22,   "TXB" },
+	{ NSPR_PDPKTRACEFULLTXB_V23,   "TXB" },
+	{ NSPR_PDPKTRACEFULLTXB_V24,   "TXB" },
+	{ NSPR_PDPKTRACEFULLTXB_V25,   "TXB" },
+	{ NSPR_PDPKTRACEFULLTXB_V26,   "TXB" },
+	{ NSPR_PDPKTRACEFULLRX_V22,    "RX" },
+	{ NSPR_PDPKTRACEFULLRX_V23,    "RX" },
+	{ NSPR_PDPKTRACEFULLRX_V24,    "RX" },
+	{ NSPR_PDPKTRACEFULLRX_V25,    "RX" },
+	{ NSPR_PDPKTRACEFULLRX_V26,    "RX" },
 	{ NSPR_PDPKTRACEFULLNEWRX_V24, "NEW_RX" },
 	{ NSPR_PDPKTRACEFULLNEWRX_V25, "NEW_RX" },
 	{ NSPR_PDPKTRACEFULLNEWRX_V26, "NEW_RX" },
-	{ NSPR_PDPKTRACEPARTTX_V22, "TX" },
-	{ NSPR_PDPKTRACEPARTTX_V23, "TX" },
-	{ NSPR_PDPKTRACEPARTTX_V24, "TX" },
-	{ NSPR_PDPKTRACEPARTTX_V25, "TX" },
-	{ NSPR_PDPKTRACEPARTTX_V26, "TX" },
-	{ NSPR_PDPKTRACEPARTTXB_V22, "TXB" },
-	{ NSPR_PDPKTRACEPARTTXB_V23, "TXB" },
-	{ NSPR_PDPKTRACEPARTTXB_V24, "TXB" },
-	{ NSPR_PDPKTRACEPARTTXB_V25, "TXB" },
-	{ NSPR_PDPKTRACEPARTTXB_V26, "TXB" },
-	{ NSPR_PDPKTRACEPARTRX_V22, "RX" },
-	{ NSPR_PDPKTRACEPARTRX_V23, "RX" },
-	{ NSPR_PDPKTRACEPARTRX_V24, "RX" },
-	{ NSPR_PDPKTRACEPARTRX_V25, "RX" },
-	{ NSPR_PDPKTRACEPARTRX_V26, "RX" },
+	{ NSPR_PDPKTRACEPARTTX_V22,    "TX" },
+	{ NSPR_PDPKTRACEPARTTX_V23,    "TX" },
+	{ NSPR_PDPKTRACEPARTTX_V24,    "TX" },
+	{ NSPR_PDPKTRACEPARTTX_V25,    "TX" },
+	{ NSPR_PDPKTRACEPARTTX_V26,    "TX" },
+	{ NSPR_PDPKTRACEPARTTXB_V22,   "TXB" },
+	{ NSPR_PDPKTRACEPARTTXB_V23,   "TXB" },
+	{ NSPR_PDPKTRACEPARTTXB_V24,   "TXB" },
+	{ NSPR_PDPKTRACEPARTTXB_V25,   "TXB" },
+	{ NSPR_PDPKTRACEPARTTXB_V26,   "TXB" },
+	{ NSPR_PDPKTRACEPARTRX_V22,    "RX" },
+	{ NSPR_PDPKTRACEPARTRX_V23,    "RX" },
+	{ NSPR_PDPKTRACEPARTRX_V24,    "RX" },
+	{ NSPR_PDPKTRACEPARTRX_V25,    "RX" },
+	{ NSPR_PDPKTRACEPARTRX_V26,    "RX" },
 	{ NSPR_PDPKTRACEPARTNEWRX_V24, "NEW_RX" },
 	{ NSPR_PDPKTRACEPARTNEWRX_V25, "NEW_RX" },
 	{ NSPR_PDPKTRACEPARTNEWRX_V26, "NEW_RX" },
@@ -139,18 +139,18 @@ static dissector_handle_t eth_withoutfcs_handle;
 static void
 dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
-	proto_tree   *ns_tree = NULL, *flagtree = NULL;
-	proto_item   *ti = NULL, *flagitem = NULL;
+	proto_tree     *ns_tree = NULL, *flagtree = NULL;
+	proto_item     *ti = NULL, *flagitem = NULL;
 	struct nstr_phdr *pnstr = &(pinfo->pseudo_header->nstr);
-	tvbuff_t     *next_tvb_eth_client;
-	guint8      offset;
-	guint      	i, bpos;
-	emem_strbuf_t *flags_strbuf = ep_strbuf_new_label("None");
+	tvbuff_t       *next_tvb_eth_client;
+	guint8		offset;
+	guint		i, bpos;
+	emem_strbuf_t  *flags_strbuf = ep_strbuf_new_label("None");
 	static const gchar *flags[] = {"FP", "FR", "DFD", "SRSS", "RSSH"};
 	gboolean 	first_flag = TRUE;
 	guint8		flagoffset, flagval;
-	guint8      src_vmname_len = 0, dst_vmname_len = 0;
-	guint8      variable_ns_len = 0;
+	guint8		src_vmname_len = 0, dst_vmname_len = 0;
+	guint8		variable_ns_len = 0;
 	guint 		flagval32;
 
 	if (pnstr->rec_type == NSPR_HEADER_VERSION205)
@@ -163,7 +163,7 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
 	ti = proto_tree_add_protocol_format(tree, proto_nstrace, tvb, 0, pnstr->eth_offset, "NetScaler Packet Trace");
 	ns_tree = proto_item_add_subtree(ti, ett_ns);
-  
+
 	proto_tree_add_item(ns_tree, hf_ns_dir, tvb, pnstr->dir_offset, pnstr->dir_len, ENC_LITTLE_ENDIAN);
 	proto_tree_add_item(ns_tree, hf_ns_nicno, tvb, pnstr->nicno_offset, pnstr->nicno_len, ENC_LITTLE_ENDIAN);
 
@@ -171,7 +171,7 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	{
 	case NSPR_HEADER_VERSION206:
 		flagoffset = pnstr->ns_activity_offset;
-		flagval32 = tvb_get_letohl(tvb, flagoffset);	
+		flagval32 = tvb_get_letohl(tvb, flagoffset);
 		flagitem = proto_tree_add_uint_format(ns_tree, hf_ns_activity, tvb, flagoffset, 4, flagval32,
 						"Activity Flags: 0x%04x", flagval32);
 		flagtree = proto_item_add_subtree(flagitem, ett_ns_activity_flags);
@@ -186,7 +186,7 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 		if(src_vmname_len){
 			proto_tree_add_item(ns_tree,hf_ns_src_vm,tvb,pnstr->data_offset,src_vmname_len,ENC_LITTLE_ENDIAN);
 			}
-		
+
 		if(dst_vmname_len){
 			proto_tree_add_item(ns_tree,hf_ns_dst_vm,tvb,pnstr->data_offset+src_vmname_len,dst_vmname_len,ENC_LITTLE_ENDIAN);
 			}
@@ -195,7 +195,7 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	case NSPR_HEADER_VERSION204:
 
 		flagoffset = pnstr->clflags_offset;
-		flagval = tvb_get_guint8(tvb, flagoffset);	
+		flagval = tvb_get_guint8(tvb, flagoffset);
 
 		for (i = 0; i < 5; i++) {
 			bpos = 1 << i;
@@ -207,10 +207,10 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 				first_flag = FALSE;
 			}
 		}
-		
+
 		proto_tree_add_item(ns_tree, hf_ns_snode, tvb, pnstr->srcnodeid_offset, 2, ENC_LITTLE_ENDIAN);
 		proto_tree_add_item(ns_tree, hf_ns_dnode, tvb, pnstr->destnodeid_offset, 2, ENC_LITTLE_ENDIAN);
-		
+
 		flagitem = proto_tree_add_uint_format(ns_tree, hf_ns_clflags, tvb, flagoffset, 1, flagval,
 						"Cluster Flags: 0x%02x (%s)", flagval, flags_strbuf->str);
 		flagtree = proto_item_add_subtree(flagitem, ett_ns_flags);
@@ -245,11 +245,11 @@ dissect_nstrace(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	default:
 		break;
 	}
-  
+
 	/* Dissect as Ethernet */
 	offset = pnstr->eth_offset;
 	next_tvb_eth_client = tvb_new_subset_remaining(tvb, offset);
-	call_dissector(eth_withoutfcs_handle, next_tvb_eth_client, pinfo, tree);  
+	call_dissector(eth_withoutfcs_handle, next_tvb_eth_client, pinfo, tree);
 }
 
 void
@@ -258,100 +258,148 @@ proto_register_ns(void)
 	static hf_register_info hf[] = {
 
 		{ &hf_ns_nicno,
-		  { "Nic No",		"nstrace.nicno", FT_UINT8, BASE_DEC,NULL, 0x0,
-			NULL, HFILL }},
-			
+		  { "Nic No", "nstrace.nicno",
+		    FT_UINT8, BASE_DEC, NULL, 0x0,
+		    NULL, HFILL }
+		},
+
 		{ &hf_ns_src_vm,
-		  { "Src Vm Name",		"nstrace.src_vm", FT_STRINGZ, BASE_NONE,NULL, 0x0,
-			NULL, HFILL }},
+		  { "Src Vm Name", "nstrace.src_vm",
+		    FT_STRINGZ, BASE_NONE, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_dst_vm,
-		  { "Dst Vm Name",		"nstrace.dst_vm", FT_STRINGZ, BASE_NONE,NULL, 0x0,
-			NULL, HFILL }},
+		  { "Dst Vm Name", "nstrace.dst_vm",
+		    FT_STRINGZ, BASE_NONE, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_coreid,
-		  { "Core Id",		"nstrace.coreid", FT_UINT16, BASE_DEC,NULL, 0x0,
-			NULL, HFILL }},
+		  { "Core Id", "nstrace.coreid",
+		    FT_UINT16, BASE_DEC, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_dir,
-		  { "Operation",	"nstrace.dir", FT_UINT8, BASE_HEX,  VALS(ns_dir_vals), 0x0,
-			NULL, HFILL }},
+		  { "Operation", "nstrace.dir",
+		    FT_UINT8, BASE_HEX, VALS(ns_dir_vals), 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_pcbdevno,
-		  { "PcbDevNo",		"nstrace.pdevno", FT_UINT32, BASE_HEX, NULL, 0x0,
-			NULL, HFILL }},
+		  { "PcbDevNo", "nstrace.pdevno",
+		    FT_UINT32, BASE_HEX, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_l_pcbdevno,
-		  { "Linked PcbDevNo",	"nstrace.l_pdevno", FT_UINT32, BASE_HEX, NULL, 0x0,
-			NULL, HFILL }},
+		  { "Linked PcbDevNo", "nstrace.l_pdevno",
+		    FT_UINT32, BASE_HEX, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_devno,
-		  { "DevNo",		"nstrace.devno", FT_UINT32, BASE_HEX, NULL, 0x0,
-			NULL, HFILL }},
+		  { "DevNo", "nstrace.devno",
+		    FT_UINT32, BASE_HEX, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_vlantag,
-		  { "Vlan",		"nstrace.vlan", FT_UINT16, BASE_DEC, NULL, 0x0,
-			NULL, HFILL }},	
+		  { "Vlan", "nstrace.vlan",
+		    FT_UINT16, BASE_DEC, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_snode,
-		  { "Source Node",		"nstrace.snode", FT_INT16, BASE_DEC, NULL, 0x0,
-			NULL, HFILL }},	
-		
+		  { "Source Node", "nstrace.snode",
+		    FT_INT16, BASE_DEC, NULL, 0x0,
+		    NULL, HFILL }
+		},
+
 		{ &hf_ns_dnode,
-		  { "Destination Node",		"nstrace.dnode", FT_INT16, BASE_DEC, NULL, 0x0,
-			NULL, HFILL }},	
-		
+		  { "Destination Node", "nstrace.dnode",
+		    FT_INT16, BASE_DEC, NULL, 0x0,
+		    NULL, HFILL }
+		},
+
 		{ &hf_ns_clflags,
-		  { "Cluster Flags",	"nstrace.flags", FT_UINT8, BASE_HEX, NULL, 0x0,
-			  NULL, HFILL }},
+		  { "Cluster Flags", "nstrace.flags",
+		    FT_UINT8, BASE_HEX, NULL, 0x0,
+		    NULL, HFILL }
+		},
 
 		{ &hf_ns_clflags_res,
-		  { "Reserved",	"nstrace.flags.res", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RES,
-			  NULL, HFILL}},
-		
+		  { "Reserved", "nstrace.flags.res",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RES,
+		    NULL, HFILL}
+		},
+
 		{ &hf_ns_clflags_rssh,
-		  { "RSSHASH",	"nstrace.flags.rssh", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RSSH,
-			  NULL, HFILL}},
-		
+		  { "RSSHASH", "nstrace.flags.rssh",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RSSH,
+		    NULL, HFILL}
+		},
+
 		{ &hf_ns_clflags_rss,
-		  { "SRSS",	"nstrace.flags.srss", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RSS,
-			  NULL, HFILL}},
-		
+		  { "SRSS", "nstrace.flags.srss",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_RSS,
+		    NULL, HFILL}
+		},
+
 		{ &hf_ns_clflags_dfd,
-		  { "DFD",	"nstrace.flags.dfd", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_DFD,
-			  NULL, HFILL}},
-		
+		  { "DFD", "nstrace.flags.dfd",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_DFD,
+		    NULL, HFILL}
+		},
+
 		{ &hf_ns_clflags_fr,
-		  { "Flow receiver (FR)",	"nstrace.flags.fr", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_FR,
-			  NULL, HFILL}},
-		
+		  { "Flow receiver (FR)", "nstrace.flags.fr",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_FR,
+		    NULL, HFILL}
+		},
+
 		{ &hf_ns_clflags_fp,
-		  { "Flow processor (FP)",	"nstrace.flags.fp", FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_FP,
-			  NULL, HFILL}},
+		  { "Flow processor (FP)", "nstrace.flags.fp",
+		    FT_BOOLEAN, 8, TFS(&tfs_set_notset), CL_FP,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity,
-		  { "NetScaler Activity",	"nstrace.activity", FT_UINT32, BASE_HEX, NULL, 0x0,
-			  NULL, HFILL}},
+		  { "NetScaler Activity", "nstrace.activity",
+		    FT_UINT32, BASE_HEX, NULL, 0x0,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity_perf_collection,
-		  { "Perf Collection",	"nstrace.activity.perfcollection", FT_BOOLEAN, 32, NULL, NS_PE_STATE_PERF_COLLECTION_IN_PROG,
-			  NULL, HFILL}},
+		  { "Perf Collection", "nstrace.activity.perfcollection",
+		    FT_BOOLEAN, 32, NULL, NS_PE_STATE_PERF_COLLECTION_IN_PROG,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity_pcb_zombie,
-		  { "PCB Zombie",	"nstrace.activity.pcbzombie", FT_BOOLEAN, 32, NULL, NS_PE_STATE_PCB_ZOMBIE_IN_PROG,
-			  NULL, HFILL}},
+		  { "PCB Zombie", "nstrace.activity.pcbzombie",
+		    FT_BOOLEAN, 32, NULL, NS_PE_STATE_PCB_ZOMBIE_IN_PROG,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity_natpcb_zombie,
-		  { "NATPCB Zombie",	"nstrace.activity.natpcbzombie", FT_BOOLEAN, 32, NULL, NS_PE_STATE_NATPCB_ZOMBIE_IN_PROG,
-			  NULL, HFILL}},
+		  { "NATPCB Zombie", "nstrace.activity.natpcbzombie",
+		    FT_BOOLEAN, 32, NULL, NS_PE_STATE_NATPCB_ZOMBIE_IN_PROG,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity_lbstats_sync,
-		  { "LB Stats Sync",	"nstrace.activity.lbstatssync", FT_BOOLEAN, 32, NULL, NS_PE_STATE_LBSTATS_SYNC_IN_PROG,
-			  NULL, HFILL}},
+		  { "LB Stats Sync", "nstrace.activity.lbstatssync",
+		    FT_BOOLEAN, 32, NULL, NS_PE_STATE_LBSTATS_SYNC_IN_PROG,
+		    NULL, HFILL}
+		},
 
 		{ &hf_ns_activity_stats_req,
-		  { "Stats Req",	"nstrace.activity.statsreq", FT_BOOLEAN, 32, NULL, NS_PE_STATE_STATS_REQ_IN_PROG,
-			  NULL, HFILL}},
+		  { "Stats Req", "nstrace.activity.statsreq",
+		    FT_BOOLEAN, 32, NULL, NS_PE_STATE_STATS_REQ_IN_PROG,
+		    NULL, HFILL}
+		},
 
 	};
 
