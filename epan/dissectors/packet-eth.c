3db06ae582d0433c1c2b31b12d0feb894bbf8f7b
Pascal Quantin <pascal.quantin@gmail.com>
2013-07-08 18:49:42 +0000

    Fix "Could not compile color filter "Checksum Errors" from saved filters: eth.fcs_bad (type=Label) cannot participate in '==' comparison" warning
    svn path=/trunk/; revision=50448

--- epan/dissectors/packet-eth.c
+++ epan/dissectors/packet-eth.c
@@ -778,7 +778,7 @@ proto_register_eth(void)
     static ei_register_info ei[] = {
         { &ei_eth_invalid_lentype, { "eth.invalid_lentype", PI_PROTOCOL, PI_WARN, "Invalid length/type", EXPFILL }},
         { &ei_eth_src_not_group, { "eth.src_not_group", PI_PROTOCOL, PI_WARN, "Source MAC must not be a group address: IEEE 802.3-2002, Section 3.2.3(b)", EXPFILL }},
-        { &ei_eth_fcs_bad, { "eth.fcs_bad", PI_CHECKSUM, PI_ERROR, "Bad checksum", EXPFILL }},
+        { &ei_eth_fcs_bad, { "eth.fcs_bad.expert", PI_CHECKSUM, PI_ERROR, "Bad checksum", EXPFILL }},
     };
 
     module_t *eth_module;
