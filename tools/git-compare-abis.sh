234cd8dac4d97ca9babdd513597ea97d3749935a
Evan Huus <eapache@gmail.com>
2013-07-27 13:50:52 +0000

    Add the proper license header and attribution to Balint's git-compare-abis script.
    svn path=/trunk/; revision=50942

--- tools/git-compare-abis.sh
+++ tools/git-compare-abis.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-
+#
+# checks if Wireshark's ABI has been changes since last release (tag)
+#
+# Copyright 2011 Balint Reczey <balint@balintreczey.hu>
+#
 # $Id$
+#
+# Wireshark - Network traffic analyzer
+# By Gerald Combs <gerald@wireshark.org>
+# Copyright 1998 Gerald Combs
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-# check if Wireshark's ABI has been changes since last release (tag)
 set -e
 
 if test -z "$1"; then
