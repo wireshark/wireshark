93f56d8a770e9a29bfeb27f0698ae25ae89206a9
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2013-08-06 19:53:54 +0000

    Optimize wrs_count_bitshift()
    - Use __builtin_ctz for GCC/clang
    - Do multiply and lookup on other platforms
    
    svn path=/trunk/; revision=51173

--- epan/proto.c
+++ epan/proto.c
@@ -71,15 +71,20 @@ struct ptvcursor {
 	gint	     offset;
 };
 
-/* Candidates for assembler */
 static int
 wrs_count_bitshift(const guint32 bitmask)
 {
-	int bitshift = 0;
+#if defined(__GNUC__) && ((__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4))
+	return __builtin_ctz(bitmask);
+#else
+	/* From http://graphics.stanford.edu/~seander/bithacks.html#ZerosOnRightMultLookup */
+	static const int table[32] = {
+		0,   1, 28,  2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17,  4, 8,
+		31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18,  6, 11,  5, 10, 9
+	};
 
-	while ((bitmask & (1 << bitshift)) == 0)
-		bitshift++;
-	return bitshift;
+	return table[((guint32)((bitmask & -(gint32)bitmask) * 0x077CB531U)) >> 27];
+#endif
 }
 
 #define cVALS(x) (const value_string*)(x)
