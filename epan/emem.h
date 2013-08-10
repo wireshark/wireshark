7b4431903d09098ca53c202e7bf6d7e5721e4a45
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-08-10 21:27:52 +0000

    Fix (-W)documentation error found by Clang
    ../../epan/emem.h:560:5: error: '@return' command used in a comment that is attached to a function returning void
          [-Werror,-Wdocumentation]
     * @return void
       ~^~~~~~~~~~~
    
    svn path=/trunk/; revision=51253

--- epan/emem.h
+++ epan/emem.h
@@ -557,7 +557,7 @@ emem_strbuf_t *ep_strbuf_truncate(emem_strbuf_t *strbuf, gsize len);
  *
  * @param emem_tree The tree to dump to standard output.
  *
- * @return void
+ * 
  */
 void emem_print_tree(emem_tree_t* emem_tree);
 
