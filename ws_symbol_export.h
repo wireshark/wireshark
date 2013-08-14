0987692ad7242859362e3662e461a25e3aef974a
Guy Harris <guy@alum.mit.edu>
2013-08-14 07:39:31 +0000

    Copy over revisions from trunk:
      ------------------------------------------------------------------------
      r50382 | guy | 2013-07-04 11:11:59 -0700 (Thu, 04 Jul 2013) | 2 lines
    
      More details in a comment.
      ------------------------------------------------------------------------
      r50379 | guy | 2013-07-03 17:39:54 -0700 (Wed, 03 Jul 2013) | 2 lines
    
      Add more comments indicating what this is doing.
      ------------------------------------------------------------------------
      r50334 | guy | 2013-07-02 18:43:39 -0700 (Tue, 02 Jul 2013) | 12 lines
    
      WS_DLL_PUBLIC is now always WS_DLL_PUBLIC_NOEXTERN with "extern" added;
      just define WS_DLL_PUBLIC_NOEXTERN inside the ifdefs, and define
      WS_DLL_PUBLIC as WS_DLL_PUBLIC_NOEXTERN followed by "extern".
    
      Then rename WS_DLL_PUBLIC_NOEXTERN to WS_DLL_PUBLIC_DEF, to clarify that
      it's what should be used for definitions; at least on Windows, you
      *have* to use it when declaring arrays without a size, and, whilst you
      might be able to use WS_DLL_PUBLIC for definitions of functions and
      perhaps data definitions other than no-size arrays, it might be clearer
      to rename WS_DLL_PUBLIC to WS_DLL_PUBLIC_DECL and use it only for
      declarations.
      ------------------------------------------------------------------------
      r50332 | guy | 2013-07-02 17:38:41 -0700 (Tue, 02 Jul 2013) | 14 lines
    
      OK, try having WS_DLL_PUBLIC always say "extern".  If *that* doesn't
      work, because it can't be used with definitions, we'll probably have to
      have separate macros for declarations and definitions, as I don't think
     MSVC likes
    
    	int foo[];
    
      in a header file but should be fine with
    
    	extern int foo[];
    
      Add some more comments while we're at it; you are in a twisty little
      maze of #ifdefs, all different.
      ------------------------------------------------------------------------
      r50329 | guy | 2013-07-02 15:08:23 -0700 (Tue, 02 Jul 2013) | 7 lines
    
      Methinks the "not GCC 4 or later" #defines for WS_DLL_PUBLIC and
      WS_DLL_LOCAL were reversed; that might be what's causing a build error
      with ui/qt/main_status_bar.cpp.
    
      Add some comments for #else's while we're at it, so it's easier to
      figure out when particular sets of #defines are used.
    
    and then back out the "rename WS_DLL_PUBLIC_NOEXTERN to
    WS_DLL_PUBLIC_DEF" change, to minimize the number of changes.
    
    svn path=/trunk-1.10/; revision=51353

--- ws_symbol_export.h
+++ ws_symbol_export.h
@@ -49,33 +49,96 @@
 
 /* Originally copied from GCC Wiki at http://gcc.gnu.org/wiki/Visibility */
 #if defined _WIN32 || defined __CYGWIN__
+  /* Compiling for Windows, so we use the Windows DLL declarations. */
   #ifdef WS_BUILD_DLL
+    /*
+     * Building a DLL; for all definitions, we want dllexport, and
+     * (presumably so source from DLL and source from a program using the
+     * DLL can both include a header that declares APIs and exported data
+     * for the DLL), for declarations, either dllexport or dllimport will
+     * work (they mean the same thing for a declaration when building a DLL).
+     */
     #ifdef __GNUC__
-#define WS_DLL_PUBLIC __attribute__ ((dllexport))
+      /* GCC */
+#define WS_DLL_PUBLIC_NOEXTERN __attribute__ ((dllexport))
     #else /* ! __GNUC__ */
-#define WS_DLL_PUBLIC __declspec(dllexport) /* Note: actually gcc seems to also support this syntax. */
+      /*
+       * Presumably MSVC.
+       * Note: actually gcc seems to also support this syntax.
+       */
+#define WS_DLL_PUBLIC_NOEXTERN __declspec(dllexport)
     #endif /* __GNUC__ */
-  #else
+  #else /* WS_BUILD_DLL */
+    /*
+     * Building a program; we should only see declarations, not definitions,
+     * with WS_DLL_PUBLIC, and they all represent APIs or data imported
+     * from a DLL, so use dllimport.
+     *
+     * For functions, export shouldn't be necessary; for data, it might
+     * be necessary, e.g. if what's declared is an array whose size is
+     * not given in the declaration.
+     */
     #ifdef __GNUC__
-#define WS_DLL_PUBLIC __attribute__ ((dllimport))
+      /* GCC */
+#define WS_DLL_PUBLIC_NOEXTERN __attribute__ ((dllimport))
     #elif ! (defined ENABLE_STATIC) /* ! __GNUC__ */
-#define WS_DLL_PUBLIC __declspec(dllimport) /* Note: actually gcc seems to also support this syntax. */
+      /*
+       * Presumably MSVC, and we're not building all-static.
+       * Note: actually gcc seems to also support this syntax.
+       */
+#define WS_DLL_PUBLIC_NOEXTERN __declspec(dllimport)
     #else /* ! __GNUC__  && ENABLE_STATIC */
-#define WS_DLL_PUBLIC
+      /*
+       * Presumably MSVC, and we're building all-static, so we're
+       * not building any DLLs.
+       */
+#define WS_DLL_PUBLIC_NOEXTERN
     #endif /* __GNUC__ */
   #endif /* WS_BUILD_DLL */
-  #define WS_DLL_PUBLIC_NOEXTERN WS_DLL_PUBLIC
+
+  /*
+   * Symbols in a DLL are *not* exported unless they're specifically
+   * flagged as exported, so, for a non-static but non-exported
+   * symbol, we don't have to do anything.
+   */
   #define WS_DLL_LOCAL
-#else
+#else /* defined _WIN32 || defined __CYGWIN__ */
+  /*
+   * Compiling for UN*X, where the dllimport and dllexport stuff
+   * is neither necessary nor supported; just specify the
+   * visibility if we have a compiler that claims compatibility
+   * with GCC 4 or later.
+   */
   #if __GNUC__ >= 4
-#define WS_DLL_PUBLIC __attribute__ ((visibility ("default"))) extern
+    /*
+     * Symbols exported from libraries.
+     */
 #define WS_DLL_PUBLIC_NOEXTERN __attribute__ ((visibility ("default")))
+
+    /*
+     * Non-static symbols *not* exported from libraries.
+     */
 #define WS_DLL_LOCAL  __attribute__ ((visibility ("hidden")))
   #else /* ! __GNUC__ >= 4 */
-    #define WS_DLL_PUBLIC
+    /*
+     * We have no way to control visibility.
+     */
     #define WS_DLL_PUBLIC_NOEXTERN
-    #define WS_DLL_LOCAL extern
+    #define WS_DLL_LOCAL
   #endif /* __GNUC__ >= 4 */
 #endif
+
+/*
+ * You *must* use this for exported data *declarations*; if you use
+ * WS_DLL_PUBLIC_NOEXTERN, some compilers, such as MSVC++, will complain
+ * about array definitions with no size.
+ *
+ * You must *not* use this for exported data *definitions*, as that
+ * will, for some compilers, cause warnings about items being initialized
+ * and declared extern.
+ *
+ * Either can be used for exported *function* declarations and definitions.
+ */
+#define WS_DLL_PUBLIC	WS_DLL_PUBLIC_NOEXTERN extern
 
 #endif /* SYMBOL_EXPORT_H */
