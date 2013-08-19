02cae558f997dba1fd17011cc84e317f17d17a4d
Pascal Quantin <pascal.quantin@gmail.com>
2013-08-19 11:11:11 +0000

    Do not use ephemeral memory when adding a new data source
    svn path=/trunk/; revision=51424

--- epan/dissectors/packet-snmp.c
+++ epan/dissectors/packet-snmp.c
@@ -2980,7 +2980,7 @@ static void dissect_SMUX_PDUs_PDU(tvbuff_t *tvb _U_, packet_info *pinfo _U_, pro
 
 
 /*--- End of included file: packet-snmp-fn.c ---*/
-#line 1765 "../../asn1/snmp/packet-snmp-template.c"
+#line 1769 "../../asn1/snmp/packet-snmp-template.c"
 
 
 guint
@@ -3906,7 +3906,7 @@ void proto_register_snmp(void) {
         NULL, HFILL }},
 
 /*--- End of included file: packet-snmp-hfarr.c ---*/
-#line 2426 "../../asn1/snmp/packet-snmp-template.c"
+#line 2430 "../../asn1/snmp/packet-snmp-template.c"
   };
 
   /* List of subtrees */
@@ -3946,7 +3946,7 @@ void proto_register_snmp(void) {
     &ett_snmp_RReqPDU_U,
 
 /*--- End of included file: packet-snmp-ettarr.c ---*/
-#line 2442 "../../asn1/snmp/packet-snmp-template.c"
+#line 2446 "../../asn1/snmp/packet-snmp-template.c"
   };
   static ei_register_info ei[] = {
      { &ei_snmp_failed_decrypted_data_pdu, { "snmp.failed_decrypted_data_pdu", PI_MALFORMED, PI_WARN, "Failed to decrypt encryptedPDU", EXPFILL }},
