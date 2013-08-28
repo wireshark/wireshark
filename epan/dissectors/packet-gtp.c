wireshark/wireshark

commit 22c55855476f34447a72f35ac888368c39a6689d

Add a DRX subtree to GTP dissector now that the subtree (useless for NAS and EPS NAS dissectors) was removed from de_gmm_drx_param in r51537
svn path=/trunk/; revision=51541

epan/dissectors/packet-gtp.c
@@ -320,6 +320,7 @@ static gint ett_gtp_node_addr = -1;
 static gint ett_gtp_rel_pack = -1;
 static gint ett_gtp_can_pack = -1;
 static gint ett_gtp_data_resp = -1;
+static gint ett_gtp_drx = -1;
 static gint ett_gtp_net_cap = -1;
 static gint ett_gtp_tmgi = -1;
 static gint ett_gtp_cdr_ver = -1;
@@ -4070,6 +4071,8 @@ decode_gtp_mm_cntxt(tvbuff_t * tvb, int offset, packet_info * pinfo, proto_tree 
 /*
  * 3GPP TS 24.008 10.5.5.6 ( see packet-gsm_a.c )
  */
+    tf = proto_tree_add_text(ext_tree_mm, tvb, offset, 2, "DRX Parameter");
+    tf_tree = proto_item_add_subtree(tf, ett_gtp_drx);
     de_gmm_drx_param(tvb, ext_tree_mm, pinfo, offset, 2, NULL, 0);
     offset = offset + 2;
 
@@ -9270,7 +9273,7 @@ proto_register_gtp(void)
     };
 
     /* Setup protocol subtree array */
-#define GTP_NUM_INDIVIDUAL_ELEMS    27
+#define GTP_NUM_INDIVIDUAL_ELEMS    28
     static gint *ett_gtp_array[GTP_NUM_INDIVIDUAL_ELEMS + NUM_GTP_IES];
 
     ett_gtp_array[0] = &ett_gtp;
@@ -9289,17 +9292,18 @@ proto_register_gtp(void)
     ett_gtp_array[13] = &ett_gtp_tmgi;
     ett_gtp_array[14] = &ett_gtp_trip;
     ett_gtp_array[15] = &ett_gtp_quint;
-    ett_gtp_array[16] = &ett_gtp_net_cap;
-    ett_gtp_array[17] = &ett_gtp_can_pack;
-    ett_gtp_array[18] = &ett_gtp_proto;
-    ett_gtp_array[19] = &ett_gtp_gsn_addr;
-    ett_gtp_array[20] = &ett_gtp_tft;
-    ett_gtp_array[21] = &ett_gtp_tft_pf;
-    ett_gtp_array[22] = &ett_gtp_tft_flags;
-    ett_gtp_array[23] = &ett_gtp_rab_setup;
-    ett_gtp_array[24] = &ett_gtp_hdr_list;
-    ett_gtp_array[25] = &ett_gtp_rel_pack;
-    ett_gtp_array[26] = &ett_gtp_node_addr;
+    ett_gtp_array[16] = &ett_gtp_drx;
+    ett_gtp_array[17] = &ett_gtp_net_cap;
+    ett_gtp_array[18] = &ett_gtp_can_pack;
+    ett_gtp_array[19] = &ett_gtp_proto;
+    ett_gtp_array[20] = &ett_gtp_gsn_addr;
+    ett_gtp_array[21] = &ett_gtp_tft;
+    ett_gtp_array[22] = &ett_gtp_tft_pf;
+    ett_gtp_array[23] = &ett_gtp_tft_flags;
+    ett_gtp_array[24] = &ett_gtp_rab_setup;
+    ett_gtp_array[25] = &ett_gtp_hdr_list;
+    ett_gtp_array[26] = &ett_gtp_rel_pack;
+    ett_gtp_array[27] = &ett_gtp_node_addr;
 
     last_offset = GTP_NUM_INDIVIDUAL_ELEMS;
 

epan/dissectors/packet-gsm_a_gm.c
@@ -452,7 +452,6 @@ static gint ett_tc_err_code = -1;
 static gint ett_tc_prob_code = -1;
 static gint ett_tc_sequence = -1;
 
-static gint ett_gmm_drx = -1;
 static gint ett_gmm_detach_type = -1;
 static gint ett_gmm_attach_type = -1;
 static gint ett_gmm_context_stat = -1;
@@ -8266,7 +8265,7 @@ proto_register_gsm_a_gm(void)
 	};
 
 	/* Setup protocol subtree array */
-#define	NUM_INDIVIDUAL_ELEMS	19
+#define	NUM_INDIVIDUAL_ELEMS	18
 	gint *ett[NUM_INDIVIDUAL_ELEMS +
 		  NUM_GSM_DTAP_MSG_GMM + NUM_GSM_DTAP_MSG_SM +
 		  NUM_GSM_GM_ELEM];
@@ -8278,18 +8277,17 @@ proto_register_gsm_a_gm(void)
 	ett[4]  = &ett_tc_err_code;
 	ett[5]  = &ett_tc_prob_code;
 	ett[6]  = &ett_tc_sequence;
-	ett[7]  = &ett_gmm_drx;
-	ett[8]  = &ett_gmm_detach_type;
-	ett[9]  = &ett_gmm_attach_type;
-	ett[10] = &ett_gmm_context_stat;
-	ett[11] = &ett_gmm_update_type;
-	ett[12] = &ett_gmm_radio_cap;
-	ett[13] = &ett_gmm_rai;
-	ett[14] = &ett_sm_tft;
-	ett[15] = &ett_gmm_gprs_timer;
-	ett[16] = &ett_gmm_network_cap;
-	ett[17] = &ett_gsm_a_gm_msrac_multislot_capability;
-	ett[18] = &ett_sm_pco;
+	ett[7]  = &ett_gmm_detach_type;
+	ett[8]  = &ett_gmm_attach_type;
+	ett[9]  = &ett_gmm_context_stat;
+	ett[10] = &ett_gmm_update_type;
+	ett[11] = &ett_gmm_radio_cap;
+	ett[12] = &ett_gmm_rai;
+	ett[13] = &ett_sm_tft;
+	ett[14] = &ett_gmm_gprs_timer;
+	ett[15] = &ett_gmm_network_cap;
+	ett[16] = &ett_gsm_a_gm_msrac_multislot_capability;
+	ett[17] = &ett_sm_pco;
 
 	last_offset = NUM_INDIVIDUAL_ELEMS;
 

commit 519a7778aaab6759ae073778fa93877efab49752

Fix copy/paste error introduced in r51541 and spotted by Joerg
svn path=/trunk/; revision=51564

config.nmake
@@ -50,7 +50,7 @@ WTAP_VERSION_MICRO=0
 # This can be defined in the system environment.
 #
 !IFNDEF WIRESHARK_LIB_DIR
-WIRESHARK_LIB_DIR=C:\$(PROGRAM_NAME)-$(WIRESHARK_TARGET_PLATFORM)-libs
+WIRESHARK_LIB_DIR=D:\dev\wireshark-$(WIRESHARK_TARGET_PLATFORM)-libs
 !ENDIF
 
 #
@@ -119,7 +119,7 @@ PROCESSOR_ARCHITECTURE=amd64
 
 # "Microsoft Visual C++ 2010 Express Edition"
 # Visual C++ 10.0, _MSC_VER 1600, msvcr100.dll
-#MSVC_VARIANT=MSVC2010EE
+MSVC_VARIANT=MSVC2010EE
 
 # "Microsoft Visual C++ 2012 Express Edition"
 # Visual C++ 11.0, _MSC_VER 1700, msvcr110.dll
@@ -232,7 +232,7 @@ PCAP_NG_DEFAULT=^#define PCAP_NG_DEFAULT 1
 
 ##### To Use packet editor uncomment this line ####
 ### Warning Experimental - work in progress
-#WANT_PACKET_EDITOR=^#define WANT_PACKET_EDITOR 1
+WANT_PACKET_EDITOR=^#define WANT_PACKET_EDITOR 1
 
 !if "$(WIRESHARK_TARGET_PLATFORM)" == "win32"
 ##### Win32 Libraries #####
@@ -252,7 +252,7 @@ GLIB_VERSION=2.0
 # directory in which the "include" and "lib" directories reside.
 #
 # Set the name to gtk2 if you want to use gtk2
-GTK_NAME=gtk3
+GTK_NAME=gtk2
 GTK_DIR=$(WIRESHARK_LIB_DIR)\$(GTK_NAME)
 
 
@@ -695,7 +695,7 @@ GTK_SCHEMAS_DIR=\share\glib-2.0\schemas
 # The easiest way is to use the corresponding packages from cygwin.
 
 # Set up the path to the cygwin binaries
-CYGWIN_PATH=c:\cygwin\bin
+CYGWIN_PATH=d:\cygwin\bin
 
 # command for a shell (cygwin's bash package recommended)
 SH_PROG=bash

epan/dissectors/packet-gtp.c
@@ -4072,8 +4072,8 @@ decode_gtp_mm_cntxt(tvbuff_t * tvb, int offset, packet_info * pinfo, proto_tree 
  * 3GPP TS 24.008 10.5.5.6 ( see packet-gsm_a.c )
  */
     tf = proto_tree_add_text(ext_tree_mm, tvb, offset, 2, "DRX Parameter");
-    /* tf_tree = proto_item_add_subtree(tf, ett_gtp_drx); */
-    de_gmm_drx_param(tvb, ext_tree_mm, pinfo, offset, 2, NULL, 0);
+    tf_tree = proto_item_add_subtree(tf, ett_gtp_drx);
+    de_gmm_drx_param(tvb, tf_tree, pinfo, offset, 2, NULL, 0);
     offset = offset + 2;
 
     len = tvb_get_guint8(tvb, offset);