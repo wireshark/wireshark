c35cc805f413b0e59798d1da731f9ef404e4e892
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-09-02 23:13:58 +0000

    Fix some Dead Store (Dead assignement/Dead increment) Warning found by Clang
    svn path=/trunk/; revision=51672

--- epan/dissectors/packet-mq-pcf.c
+++ epan/dissectors/packet-mq-pcf.c
@@ -242,7 +242,7 @@ dissect_mqpcf(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			proto_tree_add_item(mq_tree, hf_mqpcf_cfh_reason, tvb, offset + 28, 4, bLittleEndian);
 			proto_tree_add_item(mq_tree, hf_mqpcf_cfh_paramcount, tvb, offset + 32, 4, bLittleEndian);
 		}
-		offset += iSizeMQCFH;
+		/*offset += iSizeMQCFH;*/
 	}
 }
 
