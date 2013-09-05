c248e3c5b2d64f25bbec6973b5eea42a55a87bc7
Jörg Mayer <jmayer@loplof.de>
2013-09-05 09:23:50 +0000

    Add modelines to avoid using tabs in the code
    svn path=/trunk/; revision=51779

--- tools/asn2wrs.py
+++ tools/asn2wrs.py
@@ -8004,3 +8004,10 @@ if __name__ == '__main__':
         main()
 
 #------------------------------------------------------------------------------
+#
+# Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+#
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+
