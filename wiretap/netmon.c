bb6274df20c4a986a47692f2a68081dc2881e318
Evan Huus <eapache@gmail.com>
2013-09-05 12:45:42 +0000

    From Peter Hatina via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9104
    Fix double-free on corrupt netmon file. Wiretap frees the struct for us, we
    don't need to free it as well.
    
    svn path=/trunk-1.8/; revision=51781

--- wiretap/netmon.c
+++ wiretap/netmon.c
@@ -337,14 +337,12 @@ int netmon_open(wtap *wth, int *err, gchar **err_info)
 		*err = WTAP_ERR_BAD_FILE;
 		*err_info = g_strdup_printf("netmon: frame table length is %u, which is not a multiple of the size of an entry",
 		    frame_table_length);
-		g_free(netmon);
 		return -1;
 	}
 	if (frame_table_size == 0) {
 		*err = WTAP_ERR_BAD_FILE;
 		*err_info = g_strdup_printf("netmon: frame table length is %u, which means it's less than one entry in size",
 		    frame_table_length);
-		g_free(netmon);
 		return -1;
 	}
 	/*
@@ -364,11 +362,9 @@ int netmon_open(wtap *wth, int *err, gchar **err_info)
 		*err = WTAP_ERR_BAD_FILE;
 		*err_info = g_strdup_printf("netmon: frame table length is %u, which is larger than we support",
 		    frame_table_length);
-		g_free(netmon);
 		return -1;
 	}
 	if (file_seek(wth->fh, frame_table_offset, SEEK_SET, err) == -1) {
-		g_free(netmon);
 		return -1;
 	}
 	frame_table = (guint32 *)g_malloc(frame_table_length);
