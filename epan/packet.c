8175865785f53c36ac1e78e2d03184ae255b9adf
Stig Bjørlykke <stig@bjorlykke.org>
2013-09-09 05:51:35 +0000

    Added casts in dissector_delete_all_check().
    svn path=/trunk/; revision=51861

--- epan/packet.c
+++ epan/packet.c
@@ -897,8 +897,8 @@ void dissector_delete_uint_range(const char *abbrev, range_t *range,
 static gboolean
 dissector_delete_all_check (gpointer key _U_, gpointer value, gpointer user_data)
 {
-	dtbl_entry_t *dtbl_entry = value;
-	dissector_handle_t handle = user_data;
+	dtbl_entry_t *dtbl_entry = (dtbl_entry_t *) value;
+	dissector_handle_t handle = (dissector_handle_t *) user_data;
 
 	return (proto_get_id (dtbl_entry->current->protocol) == proto_get_id (handle->protocol));
 }
