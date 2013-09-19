49648069eb409567fd2239c0a584a0b6c64b39a9
Evan Huus <eapache@gmail.com>
2013-09-19 12:50:25 +0000

    Remove unused enum that started conflicting with /usr/include/dirent.h on my system.
    svn path=/trunk/; revision=52142

--- epan/dissectors/packet-gluster.h
+++ epan/dissectors/packet-gluster.h
@@ -357,20 +357,6 @@ enum gf_fop_procnum {
 	GFS3_OP_MAXVALUE
 };
 
-/* dir-entry types from libglusterfs/src/compat.h */
-enum gluster_entry_types {
-	DT_UNKNOWN = 0,
-	DT_FIFO = 1,
-	DT_CHR = 2,
-	DT_DIR = 4,
-	DT_BLK = 6,
-	DT_REG = 8,
-	DT_LNK = 10,
-	DT_SOCK = 12,
-	DT_WHT = 14
-};
-
-
 /* LOCKING operators come from libglusterfs/src/glusterfs.h */
 
 /* based on original enum glusterfs_lk_cmds_t */
