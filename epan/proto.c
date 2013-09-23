f1ba7e10958715ef6f7fba1bc46bbe5fe4d6b2a9
Evan Huus <eapache@gmail.com>
2013-09-23 12:09:20 +0000

    The pointer into proto_tree_add_oid can be NULL if the length is 0. Exposed because wmem_alloc(pool, 0) returns NULL (like pretty much every other malloc-style function) where ep_ or se_ alloc did not.
    Fixes https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9175
    
    svn path=/trunk/; revision=52187

--- epan/proto.c
+++ epan/proto.c
@@ -2434,7 +2434,7 @@ proto_tree_set_oid(field_info *fi, const guint8* value_ptr, gint length)
 {
 	GByteArray *bytes;
 
-	DISSECTOR_ASSERT(value_ptr != NULL);
+	DISSECTOR_ASSERT(value_ptr != NULL || length == 0);
 
 	bytes = g_byte_array_new();
 	if (length > 0) {
