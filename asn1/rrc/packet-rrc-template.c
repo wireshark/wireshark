019e64f58d78f88846021cd15ae5af2bab853442
Pascal Quantin <pascal.quantin@gmail.com>
2013-09-24 17:56:17 +0000

    Upgrade RRC dissector to v11.7.0
    svn path=/trunk/; revision=52202

--- asn1/rrc/packet-rrc-template.c
+++ asn1/rrc/packet-rrc-template.c
@@ -24,7 +24,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Ref: 3GPP TS 25.331 V11.5.0 (2013-03)
+ * Ref: 3GPP TS 25.331 V11.7.0 (2013-09)
  */
 
 /**
