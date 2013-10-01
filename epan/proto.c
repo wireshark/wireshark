ccbb52b436730885dc6c6bb8f77d570567a5af8f
Evan Huus <eapache@gmail.com>
2013-10-01 22:50:29 +0000

    Add a dissector assert to _set_bytes like the one in _set_oid, noticed while debugging https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9214
    svn path=/trunk/; revision=52327

--- epan/proto.c
+++ epan/proto.c
@@ -2019,6 +2019,8 @@ proto_tree_set_bytes(field_info *fi, const guint8* start_ptr, gint length)
 {
 	GByteArray *bytes;
 
+	DISSECTOR_ASSERT(start_ptr != NULL || length == 0);
+
 	bytes = g_byte_array_new();
 	if (length > 0) {
 		g_byte_array_append(bytes, start_ptr, length);
