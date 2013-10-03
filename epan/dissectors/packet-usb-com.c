3dc5f049cfa88c597a6e7bfeb7f21b3b1c97c637
Pascal Quantin <pascal.quantin@gmail.com>
2013-10-03 22:09:28 +0000

    Add dissection of MBIM (extended) functional descriptor
    svn path=/trunk/; revision=52345

--- epan/dissectors/packet-usb-com.c
+++ epan/dissectors/packet-usb-com.c
@@ -30,12 +30,60 @@
 
 /* protocols and header fields */
 static int proto_usb_com = -1;
-static int hf_usb_com_subclass = -1;
-static int hf_usb_com_payload = -1;
+static int hf_usb_com_descriptor_subtype = -1;
+static int hf_usb_com_descriptor_cdc = -1;
+static int hf_usb_com_descriptor_payload = -1;
+static int hf_usb_com_control_subclass = -1;
+static int hf_usb_com_control_payload = -1;
 
 static gint ett_usb_com = -1;
 
-static dissector_handle_t mbim_handle = NULL;
+static dissector_handle_t mbim_control_handle;
+static dissector_handle_t mbim_descriptor_handle;
+
+#define CS_INTERFACE 0x24
+#define CS_ENDPOINT  0x25
+
+static const value_string usb_com_descriptor_type_vals[] = {
+    { CS_INTERFACE, "CS_INTERFACE"},
+    { CS_ENDPOINT, "CS_ENDPOINT"},
+    { 0, NULL}
+};
+static value_string_ext usb_com_descriptor_type_vals_ext = VALUE_STRING_EXT_INIT(usb_com_descriptor_type_vals);
+
+static const value_string usb_com_descriptor_subtype_vals[] = {
+    { 0x00, "Header Functional Descriptor"},
+    { 0x01, "Call Management Functional Descriptor"},
+    { 0x02, "Abstract Control Management Functional Descriptor"},
+    { 0x03, "Direct Line Management Functional Descriptor"},
+    { 0x04, "Telephone Ringer Functional Descriptor"},
+    { 0x05, "Telephone Call and Line State Reporting Capabilities Functional Descriptor"},
+    { 0x06, "Union Functional Descriptor"},
+    { 0x07, "Country Selection Functional Descriptor"},
+    { 0x08, "Telephone Operational Modes Functional Descriptor"},
+    { 0x09, "USB Terminal Functional Descriptor"},
+    { 0x0A, "Network Channel Terminal Descriptor"},
+    { 0x0B, "Protocol Unit Functional Descriptor"},
+    { 0x0C, "Extension Unit Functional Descriptor"},
+    { 0x0D, "Multi-Channel Management Functional Descriptor"},
+    { 0x0E, "CAPI Control Management Functional Descriptor"},
+    { 0x0F, "Ethernet Networking Functional Descriptor"},
+    { 0x10, "ATM Networking Functional Descriptor"},
+    { 0x11, "Wireless Handset Control Model Functional Descriptor"},
+    { 0x12, "Mobile Direct Line Model Functional Descriptor"},
+    { 0x13, "MDLM Detail Functional Descriptor"},
+    { 0x14, "Device Management Model Functional Descriptor"},
+    { 0x15, "OBEX Functional Descriptor"},
+    { 0x16, "Command Set Functional Descriptor"},
+    { 0x17, "Command Set Detail Functional Descriptor"},
+    { 0x18, "Telephone Control Model Functional Descriptor"},
+    { 0x19, "OBEX Service Identifier Functional Descriptor"},
+    { 0x1A, "NCM Functional Descriptor"},
+    { 0x1B, "MBIM Functional Descriptor"},
+    { 0x1C, "MBIM Extended Functional Descriptor"},
+    { 0, NULL}
+};
+static value_string_ext usb_com_descriptor_subtype_vals_ext = VALUE_STRING_EXT_INIT(usb_com_descriptor_subtype_vals);
 
 #define COM_SUBCLASS_RESERVED 0x00
 #define COM_SUBCLASS_DLCM     0x01
@@ -75,6 +123,47 @@ static const value_string usb_com_subclass_vals[] = {
 static int
 dissect_usb_com_descriptor(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
+    guint8 offset = 0, type, subtype;
+    proto_tree *subtree;
+    proto_item *ti;
+
+    ti = proto_tree_add_text(tree, tvb, offset, -1, "COMMUNICATIONS DESCRIPTOR");
+    subtree = proto_item_add_subtree(ti, ett_usb_com);
+
+    dissect_usb_descriptor_header(subtree, tvb, 0, &usb_com_descriptor_type_vals_ext);
+
+    type = tvb_get_guint8(tvb, 1);
+    switch (type) {
+        case CS_INTERFACE:
+            subtype = tvb_get_guint8(tvb, 2);
+            proto_tree_add_uint(subtree, hf_usb_com_descriptor_subtype, tvb, 2, 1, subtype);
+            switch (subtype) {
+                case 0x00:
+                    proto_tree_add_item(subtree, hf_usb_com_descriptor_cdc, tvb, 3, 2, ENC_LITTLE_ENDIAN);
+                    offset = 5;
+                    break;
+                case 0x1b:
+                case 0x1c:
+                    offset = call_dissector_only(mbim_descriptor_handle, tvb, pinfo, subtree, NULL);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case CS_ENDPOINT:
+        default:
+            break;
+    }
+
+    if (tvb_reported_length_remaining(tvb, offset) != 0) {
+        proto_tree_add_item(subtree, hf_usb_com_descriptor_payload, tvb, offset, -1, ENC_NA);
+    }
+    return tvb_length(tvb);
+}
+
+static int
+dissect_usb_com_control(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
+{
     usb_conv_info_t *usb_conv_info;
     proto_tree *subtree;
     proto_item *ti;
@@ -88,15 +177,14 @@ dissect_usb_com_descriptor(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
     subtree = proto_item_add_subtree(ti, ett_usb_com);
 
     if (usb_conv_info) {
-        ti = proto_tree_add_uint(subtree, hf_usb_com_subclass, tvb, 0, 0, usb_conv_info->interfaceSubclass);
+        ti = proto_tree_add_uint(subtree, hf_usb_com_control_subclass, tvb, 0, 0,
+                                 usb_conv_info->interfaceSubclass);
         PROTO_ITEM_SET_GENERATED(ti);
 
         switch (usb_conv_info->interfaceSubclass)
         {
             case COM_SUBCLASS_MBIM:
-                if (mbim_handle) {
-                    offset = call_dissector_only(mbim_handle, tvb, pinfo, tree, NULL);
-                }
+                offset = call_dissector_only(mbim_control_handle, tvb, pinfo, tree, NULL);
                 break;
             default:
                 break;
@@ -104,7 +192,7 @@ dissect_usb_com_descriptor(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
     }
 
     if (tvb_reported_length_remaining(tvb, offset) != 0) {
-        proto_tree_add_item(subtree, hf_usb_com_payload, tvb, offset, -1, ENC_NA);
+        proto_tree_add_item(subtree, hf_usb_com_control_payload, tvb, offset, -1, ENC_NA);
     }
     return tvb_length(tvb);
 }
@@ -113,11 +201,20 @@ void
 proto_register_usb_com(void)
 {
     static hf_register_info hf[] = {
-        { &hf_usb_com_subclass,
-            { "Subclass", "usbcom.subclass", FT_UINT8, BASE_HEX,
+        { &hf_usb_com_descriptor_subtype,
+            { "bDescriptorSubtype", "usbcom.descriptor.subtype", FT_UINT8, BASE_HEX|BASE_EXT_STRING,
+              &usb_com_descriptor_subtype_vals_ext, 0, NULL, HFILL }},
+        { &hf_usb_com_descriptor_cdc,
+            { "bcdCDC", "usbcom.descriptor.cdc", FT_UINT16, BASE_HEX,
+              NULL, 0, NULL, HFILL }},
+        { &hf_usb_com_descriptor_payload,
+            { "Payload", "usbcom.descriptor.payload", FT_BYTES, BASE_NONE,
+              NULL, 0, NULL, HFILL }},
+        { &hf_usb_com_control_subclass,
+            { "Subclass", "usbcom.control.subclass", FT_UINT8, BASE_HEX,
               VALS(usb_com_subclass_vals), 0, NULL, HFILL }},
-        { &hf_usb_com_payload,
-            { "Payload", "usbcom.payload", FT_BYTES, BASE_NONE,
+        { &hf_usb_com_control_payload,
+            { "Payload", "usbcom.control.payload", FT_BYTES, BASE_NONE,
               NULL, 0, NULL, HFILL }}
     };
 
@@ -133,11 +230,14 @@ proto_register_usb_com(void)
 void
 proto_reg_handoff_usb_com(void)
 {
-    dissector_handle_t usb_com_descriptor_handle;
+    dissector_handle_t usb_com_descriptor_handle, usb_com_control_handle;
 
     usb_com_descriptor_handle = new_create_dissector_handle(dissect_usb_com_descriptor, proto_usb_com);
-    dissector_add_uint("usb.control", IF_CLASS_COMMUNICATIONS, usb_com_descriptor_handle);
-    mbim_handle = find_dissector("mbim");
+    dissector_add_uint("usb.descriptor", IF_CLASS_COMMUNICATIONS, usb_com_descriptor_handle);
+    usb_com_control_handle = new_create_dissector_handle(dissect_usb_com_control, proto_usb_com);
+    dissector_add_uint("usb.control", IF_CLASS_COMMUNICATIONS, usb_com_control_handle);
+    mbim_control_handle = find_dissector("mbim.control");
+    mbim_descriptor_handle = find_dissector("mbim.descriptor");
 }
 
 /*
