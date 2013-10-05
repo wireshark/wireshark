7b3ac2ae6bdeac058c02d0d7f6d071f85dc04b91
Jörg Mayer <jmayer@loplof.de>
2013-10-05 10:17:21 +0000

    Attempt to get linking working with cmake - without success so far.
    svn path=/trunk/; revision=52377

---
+++ cmake/modules/UseWinLibs.cmake
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+
+# Right now this is a more or less inelegant hack to get Windows
+# builds going with MSVC
+
+if( WIN32 )
+
+	# We might need something like "if (CMAKE_COMPILER_ID MATCHES "MSVC")"
+	# here to support other compilers on Windows.
+
+	set( WIN_SETARGV_OBJECT   setargv.obj )
+
+	set( WS_LINK_FLAGS ${WS_LINK_FLAGS} "${WIN_SETARGV_OBJECT}" )
+
+endif()
