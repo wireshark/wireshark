5dfd0adb1ff1dd1e751b828598c4eb06b9f3d70d
Gilbert Ramirez <gram@alumni.rice.edu>
2013-10-06 19:05:31 +0000

    Add license text to dftestlib files, and point to the capture files that someone renamed from *.cap to *.pcap.
    svn path=/trunk/; revision=52414

--- tools/dftestlib/integer_1byte.py
+++ tools/dftestlib/integer_1byte.py
@@ -1,10 +1,27 @@
 # Copyright (c) 2013 by Gilbert Ramirez <gram@alumni.rice.edu>
+#
+# $Id: dfilter-test.py 52136 2013-09-18 05:07:46Z gram $
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 from dftestlib import dftest
 
 class testInteger1Byte(dftest.DFTest):
 
-    trace_file = "ipx_rip.cap"
+    trace_file = "ipx_rip.pcap"
 
     def test_ipx_1(self):
         dfilter = "ipx.src.net == 0x28"
--- tools/dftestlib/tvb.py
+++ tools/dftestlib/tvb.py
@@ -1,10 +1,27 @@
 # Copyright (c) 2013 by Gilbert Ramirez <gram@alumni.rice.edu>
+#
+# $Id: dfilter-test.py 52136 2013-09-18 05:07:46Z gram $
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 
 import unittest
 from dftestlib import dftest
 
 class testTVB(dftest.DFTest):
-    trace_file = "http.cap"
+    trace_file = "http.pcap"
 
     def test_eq_1(self):
         # We expect 0 because even though this byte
