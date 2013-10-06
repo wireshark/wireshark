30f8fb14df1478e18c74aa6aee6365a2b250c8ef
Jörg Mayer <jmayer@loplof.de>
2013-10-06 21:07:58 +0000

    Undo r52382 for this file: We use a different solution on Windows
    svn path=/trunk/; revision=52422

--- wsutil/strncasecmp.c
+++ wsutil/strncasecmp.c
@@ -19,6 +19,7 @@
    not, write to the Free Software Foundation, Inc., 51 Franklin Street,
    Fifth Floor, Boston, MA 02110-1301 USA. */
 
+#include <ansidecl.h>
 #include <string.h>
 #include <ctype.h>
 
