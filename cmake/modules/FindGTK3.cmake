59cca5016b07ac6a2557ec227ebddc8f45eee4da
Jörg Mayer <jmayer@loplof.de>
2013-10-06 18:11:05 +0000

    Finding GTK3 works now, but the list of libs is incomplete
    svn path=/trunk/; revision=52408

--- cmake/modules/FindGTK3.cmake
+++ cmake/modules/FindGTK3.cmake
@@ -21,6 +21,9 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
+INCLUDE(FindWSWinLibs)
+FindWSWinLibs("gtk3" "GTK3_HINTS")
+
 # use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
 FIND_PACKAGE(PkgConfig)
@@ -33,25 +36,39 @@ PKG_CHECK_MODULES(PC_GTK3 gtk+-3.0 QUIET)
 
 SET(GTK3_DEFINITIONS ${PC_GTK3_CFLAGS_OTHER})
 
-#FIND_PATH(GTK3_INCLUDE_DIR NAMES "gtk/gtk.h"
-#   HINTS
-#   ${PC_GTK3_INCLUDEDIR}
-#   ${PC_GTK3_INCLUDE_DIRS}
-#   PATH_SUFFIXES "gtk-3.0"
-#   )
-SET(GTK3_INCLUDE_DIRS ${PC_GTK3_INCLUDE_DIRS})
+FIND_PATH(GTK3_INCLUDE_DIR
+    NAMES
+        "gtk/gtk.h"
+        "gtk.h"
+    HINTS
+        ${GTK3_HINTS}/include
+        ${PC_GTK3_INCLUDEDIR}
+        ${PC_GTK3_INCLUDE_DIRS}
+   PATH_SUFFIXES
+        "gtk-3.0"
+   )
 
-#FIND_LIBRARY(GTK3_LIBRARIES NAMES gtk-3 gtk3
-#   HINTS
-#   ${PC_GTK3_LIBDIR}
-#   ${PC_GTK3_LIBRARY_DIRS}
-#   )
-set(GTK3_LIBRARIES ${PC_GTK3_LIBRARIES})
+FIND_LIBRARY(GTK3_LIBRARY
+    NAMES
+        gtk-3 gtk3
+    HINTS
+        ${GTK3_HINTS}/lib
+        ${PC_GTK3_LIBDIR}
+        ${PC_GTK3_LIBRARY_DIRS}
+   )
 
 # handle the QUIETLY and REQUIRED arguments and set GTK3_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_LIBRARIES GTK3_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GTK3 DEFAULT_MSG GTK3_LIBRARY GTK3_INCLUDE_DIR)
+
+if( GTK3_FOUND )
+    set( GTK3_LIBRARIES ${GTK3_LIBRARY} )
+    set( GTK3_INCLUDE_DIRS ${GTK3_INCLUDE_DIR} )
+else()
+    set( GTK3_LIBRARIES )
+    set( GTK3_INCLUDE_DIRS )
+endif()
 
 MARK_AS_ADVANCED(GTK3_INCLUDE_DIRS GTK3_LIBRARIES)
 
