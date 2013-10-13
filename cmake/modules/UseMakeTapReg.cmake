1299f24e7bafc26ea65c52159cd389aefbda9bdd
Graham Bloice <graham.bloice@trihedral.com>
2013-10-13 09:47:32 +0000

    Set CMake to use Python when building the tap register files.
    svn path=/trunk/; revision=52581

--- cmake/modules/UseMakeTapReg.cmake
+++ cmake/modules/UseMakeTapReg.cmake
@@ -1,19 +1,19 @@
 #
 # $Id$
 #
-MACRO(REGISTER_TAP_FILES _outputfile )
-	set( _sources ${ARGN} )
+MACRO(REGISTER_TAP_FILES _outputfile _registertype )
+    set( _sources ${ARGN})
     ADD_CUSTOM_COMMAND(
         OUTPUT
           ${_outputfile}
-        COMMAND ${SH_EXECUTABLE} ${SH_FLAGS1} ${SH_FLAGS2}
-          ${CMAKE_SOURCE_DIR}/tools/make-tapreg-dotc
-          ${_outputfile}
-          ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND ${PYTHON_EXECUTABLE}
+          ${CMAKE_SOURCE_DIR}/tools/make-tap-reg.py
+          "${CMAKE_CURRENT_SOURCE_DIR}"
+          ${_registertype}
           ${_sources}
         DEPENDS
-          ${CMAKE_SOURCE_DIR}/tools/make-tapreg-dotc
+          ${CMAKE_SOURCE_DIR}/tools/make-tap-reg.py
           ${_sources}
-    )
+)
 ENDMACRO(REGISTER_TAP_FILES)
 
