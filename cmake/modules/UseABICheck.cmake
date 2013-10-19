70dce86ab34c3170a17609f4aeb92fc1bf9b5e31
Balint Reczey <balint@balintreczey.hu>
2013-10-19 14:55:12 +0000

    Set and use TMPDIR for ABI dump generation when using CMake
    svn path=/trunk/; revision=52688

--- cmake/modules/UseABICheck.cmake
+++ cmake/modules/UseABICheck.cmake
@@ -6,6 +6,7 @@
 # Checked using version 1.21.12
 
 MACRO(ABICHECK _libname)
+	set(ABICHECK_TMPDIR ${CMAKE_CURRENT_BINARY_DIR}/abi-check-headers)
 	get_directory_property(INCLUDE_DIRS INCLUDE_DIRECTORIES)
 	list(REMOVE_DUPLICATES INCLUDE_DIRS)
 	string(REGEX REPLACE ";" "\n" INCLUDE_DIRS "${INCLUDE_DIRS}")
@@ -13,8 +14,8 @@ MACRO(ABICHECK _libname)
 	# discover and substitute list of include directories for ABI compatibility
 	# checks
 	file(GLOB HEADERS *.h)
-	file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/abi-check-headers)
-	file(COPY ${HEADERS} ../ws_symbol_export.h DESTINATION abi-check-headers)
+	file(MAKE_DIRECTORY ${ABICHECK_TMPDIR})
+	file(COPY ${HEADERS} ../ws_symbol_export.h DESTINATION ${ABICHECK_TMPDIR})
 	add_custom_target(dumpabi-${_libname} DEPENDS ${_libname}.abi.tar.gz)
 ENDMACRO()
 
