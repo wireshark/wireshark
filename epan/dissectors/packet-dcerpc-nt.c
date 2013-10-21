wireshark/wireshark

commit 598726be021328ed8d87e4affe7cc5793f85c3cf

Datablob size is NDR64/32 dependant.  Bug 9301 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9301).
From Matthieu Patou

svn path=/trunk/; revision=52734

epan/dissectors/packet-dcerpc-nt.c
@@ -82,7 +82,7 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 			int use_remaining_space)
 {
 	proto_item *item;
-	guint32 len;
+	guint3264 len;
 	proto_tree *subtree = tree;
 
 	item = proto_tree_add_text(tree, tvb, offset, 0, "%s",
@@ -93,7 +93,7 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 	if (use_remaining_space) {
 		len = tvb_length_remaining (tvb, offset);
 	} else {
-		offset = dissect_ndr_uint32(tvb, offset, pinfo, subtree, drep,
+		offset = dissect_ndr_uint3264(tvb, offset, pinfo, subtree, drep,
 				    hf_nt_data_blob_len, &len);
 	}
 	proto_tree_add_text(subtree, tvb, offset, len, "Blob data");

commit 651b85889292f711921955a8b06f8fcb898d1955

Fix compile errors introduced in r52734.
svn path=/trunk/; revision=52735

epan/dissectors/packet-dcerpc-nt.c
@@ -96,8 +96,8 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 		offset = dissect_ndr_uint3264(tvb, offset, pinfo, subtree, drep,
 				    hf_nt_data_blob_len, &len);
 	}
-	proto_tree_add_text(subtree, tvb, offset, len, "Blob data");
-	offset += len;
+	proto_tree_add_text(subtree, tvb, offset, (int)len, "Blob data");
+	offset += (int)len;
 	return offset;
 }
 