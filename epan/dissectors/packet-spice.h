53e771f2175120f44b28fba9dd3da42b27c64e52
Evan Huus <eapache@gmail.com>
2013-10-23 01:47:55 +0000

    From Jonathon Jongsma via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9143
    Add correct license header to packet-spice.h
    
    svn path=/trunk/; revision=52778

--- epan/dissectors/packet-spice.h
+++ epan/dissectors/packet-spice.h
@@ -1,3 +1,20 @@
+/*
+   Copyright (C) 2013 Red Hat, Inc.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
 /* NOTE:
  * packet-spice.h is auto-generated from a Spice protocol definition by a tool
  * included in the spice-common repository
