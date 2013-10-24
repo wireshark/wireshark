207cedd928803ada0b8bce5b67e364e3433a1fda
Evan Huus <eapache@gmail.com>
2013-10-24 12:41:27 +0000

    From Michal Labedzki via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8818
    Add correct license headers, $Id$ fields and modelines to the common dwarf
    files.
    
    svn path=/trunk/; revision=52813

--- epan/dwarf.c
+++ epan/dwarf.c
@@ -1,3 +1,27 @@
+/* dwarf.c
+ * Common DWARF parts
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include "config.h"
 #include <epan/packet.h>
 
@@ -43,3 +67,16 @@ dissect_leb128(tvbuff_t *tvb, gint offset, gint64 *value)
 
     return offset - start_offset;
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
--- epan/dwarf.h
+++ epan/dwarf.h
@@ -1,3 +1,27 @@
+/* dwarf.h
+ * Common DWARF parts
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #ifndef __DWARF_H__
 #define __DWARF_H__
 
@@ -7,3 +31,16 @@ gint dissect_uleb128(tvbuff_t *tvb, gint offset, guint64 *value);
 gint dissect_leb128(tvbuff_t *tvb, gint offset, gint64 *value);
 
 #endif /* __DWARF_H__ */
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
