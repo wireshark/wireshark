wireshark/wireshark

commit 598726be021328ed8d87e4affe7cc5793f85c3cf

Datablob size is NDR64/32 dependant.  Bug 9301 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9301).
From Matthieu Patou

svn path=/trunk/; revision=52734

epan/dissectors/packet-dcerpc-nt.c
@@ -82,7 +82,7 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 			int use_remaining_space)
 {
 	proto_item *item;
-	guint32 len;
+	guint3264 len;
 	proto_tree *subtree = tree;
 
 	item = proto_tree_add_text(tree, tvb, offset, 0, "%s",
@@ -93,7 +93,7 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 	if (use_remaining_space) {
 		len = tvb_length_remaining (tvb, offset);
 	} else {
-		offset = dissect_ndr_uint32(tvb, offset, pinfo, subtree, drep,
+		offset = dissect_ndr_uint3264(tvb, offset, pinfo, subtree, drep,
 				    hf_nt_data_blob_len, &len);
 	}
 	proto_tree_add_text(subtree, tvb, offset, len, "Blob data");

commit 6790fcc4104d98c5d285a431d297c676a14d5b70

Copy over revisions from the trunk:
  ------------------------------------------------------------------------
  r52662 | mmann | 2013-10-17 13:54:42 -0700 (Thu, 17 Oct 2013) | 3 lines
  Changed paths:
     M /trunk/epan/dissectors/packet-dns.c

  Bugfix an apparent typo in edns0 "Higher bits in extended RCODE".  Bug 9199 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9199).

  #BACKPORT(1.10)
  ------------------------------------------------------------------------
  r52679 | mmann | 2013-10-18 14:17:01 -0700 (Fri, 18 Oct 2013) | 5 lines
  Changed paths:
     M /trunk/epan/stats_tree.c
     M /trunk/plugins/stats_tree/pinfo_stats_tree.c

  Don't allow invalid ranges to be specified for the stats tree. Bug 9130 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9130)

  Not sure which memory allocation should be used here (using wmem caused crash), but this revision can at least be easily backported to 1.10 where the bug was reported.

  Also allow a single number to be used in the stats range since it's considered a valid "range" by the UAT.
  ------------------------------------------------------------------------
  r52357 | martink | 2013-10-04 05:52:36 -0700 (Fri, 04 Oct 2013) | 5 lines
  Changed paths:
     M /trunk/ui/gtk/uat_gui.c

  in a uat dialogue, allow only one popup for confirming a deletion of an
  element

  this should at least partially fix bug 9129
  ------------------------------------------------------------------------
  r52683 | mmann | 2013-10-18 19:45:01 -0700 (Fri, 18 Oct 2013) | 3 lines
  Changed paths:
     M /trunk/ui/gtk/uat_gui.c

  Only allow one popup dialog in UAT dialog.  Editing/copying/deleting can modify the uat list and other open dialogs will not be aware of it and lead to unexpected behavior (including crashes).  Fixes bug 9129 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9129)

  #BACKPORT(1.10)
  ------------------------------------------------------------------------
  r52733 | mmann | 2013-10-21 08:46:13 -0700 (Mon, 21 Oct 2013) | 3 lines
  Changed paths:
     M /trunk/epan/dissectors/packet-dcerpc-ndr.c

  Fix padding bytes overlapping due to NDR alignment.  Bug 9300 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9300)

  From Matthieu Patou
  ------------------------------------------------------------------------
  r52734 | mmann | 2013-10-21 08:50:23 -0700 (Mon, 21 Oct 2013) | 3 lines
  Changed paths:
     M /trunk/epan/dissectors/packet-dcerpc-nt.c

  Datablob size is NDR64/32 dependant.  Bug 9301 (https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9301).

  From Matthieu Patou
  ------------------------------------------------------------------------
  r52735 | mmann | 2013-10-21 08:58:52 -0700 (Mon, 21 Oct 2013) | 1 line
  Changed paths:
     M /trunk/epan/dissectors/packet-dcerpc-nt.c

  Fix compile errors introduced in r52734.

docbook/release-notes.asciidoc
@@ -93,11 +93,17 @@ The following bugs have been fixed:
 * The value of 'SEND_TO_UE' in the DIAMETER Gx dictionary for Packet-Filter-Usage AVP is 0
 instead of 1. (ws-buglink:9126[])
 
+* Crash then try to delete the same entry (length range) twice. (ws-buglink:9129[])
+
+* Crash if wrong "packet lengths range" entered. (ws-buglink:9130[])
+
 * Bssgp =>  SGSN-INVOKE-TRACE use the wrong function... (ws-buglink:9157[])
 
 * Minor correction to dissection of DLR frames in Ethernet/IP dissector. (ws-buglink:9186[])
 
 * WebSphere MQ V7 Bug Fix 8322 TSHM_EBCDIC. (ws-buglink:9198[])
+
+* EDNS0 "Higher bits in extended RCODE" incorrectly decoded in packet-dns.c. (ws-buglink:9199[])
 
 * Files with pcap-ng Simple Packet Blocks can't be read. (ws-buglink:9200[])
 
@@ -113,6 +119,14 @@ instead of 1. (ws-buglink:9126[])
 
 * PTP Dissector Wrongfully Reports Malformed Packet. (ws-buglink:9262[])
 
+* Padding in uint64 field in DCERPC protocol wrongly reported. (ws-buglink:9300[])
+
+* DCERPC data_blobs are not correctly dissected when NDR64 encoding is used. (ws-buglink:9301[])
+
+* Multiple PDUs in the same DCERPC packet are not correctly decrypted. (ws-buglink:9302[])
+
+* The tshark summary line doesn't display the frame number or displays it sporadically. (ws-buglink:9317[])
+
 === New and Updated Features
 
 There are no new features in this release.
@@ -127,8 +141,12 @@ There are no new protocols in this release.
 
 3GPP2 A11
 BSSGP
+DCERPC NDR
+DCERPC NT
 DIAMETER
+DNS
 DVB-S2
+Ethernet
 EtherNet/IP
 H.225
 IEEE 802.15.4

epan/dissectors/packet-dcerpc-ndr.c
@@ -383,9 +383,9 @@ dissect_ndr_uint64(tvbuff_t *tvb, gint offset, packet_info *pinfo,
 
     if (!di->no_align && (offset % 8)) {
         gint padding = 8 - (offset % 8);
-        offset += padding;
         /*add the item for padding bytes*/
         proto_tree_add_text(tree, tvb, offset, padding, "NDR-Padding: %d bytes", padding);
+        offset += padding;
     }
     return dissect_dcerpc_uint64(tvb, offset, pinfo,
                                  tree, drep, hfindex, pdata);

epan/dissectors/packet-dcerpc-nt.c
@@ -82,7 +82,7 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 			int use_remaining_space)
 {
 	proto_item *item;
-	guint32 len;
+	guint3264 len;
 	proto_tree *subtree = tree;
 
 	item = proto_tree_add_text(tree, tvb, offset, 0, "%s",
@@ -93,11 +93,11 @@ dissect_ndr_datablob(tvbuff_t *tvb, int offset, packet_info *pinfo,
 	if (use_remaining_space) {
 		len = tvb_length_remaining (tvb, offset);
 	} else {
-		offset = dissect_ndr_uint32(tvb, offset, pinfo, subtree, drep,
+		offset = dissect_ndr_uint3264(tvb, offset, pinfo, subtree, drep,
 				    hf_nt_data_blob_len, &len);
 	}
-	proto_tree_add_text(subtree, tvb, offset, len, "Blob data");
-	offset += len;
+	proto_tree_add_text(subtree, tvb, offset, (int)len, "Blob data");
+	offset += (int)len;
 	return offset;
 }
 

epan/dissectors/packet-dcerpc.c
@@ -4436,6 +4436,7 @@ dissect_dcerpc_cn(tvbuff_t *tvb, int offset, packet_info *pinfo,
     static const guint8 nulls[4]         = { 0 };
     int                    start_offset;
     int                    padding       = 0;
+    int                    subtvb_len    = 0;
     proto_item            *ti            = NULL;
     proto_item            *tf            = NULL;
     proto_tree            *dcerpc_tree   = NULL;
@@ -4613,9 +4614,10 @@ dissect_dcerpc_cn(tvbuff_t *tvb, int offset, packet_info *pinfo,
      * (and other functions might fail as well) computing the right start
      * offset otherwise.
      */
-    fragment_tvb = tvb_new_subset(tvb, 0,
-                                  MIN((hdr.frag_len + (guint) start_offset), tvb_length(tvb)) /* length */,
-                                  hdr.frag_len + start_offset /* reported_length */);
+    subtvb_len = MIN(hdr.frag_len, tvb_length(tvb));
+    fragment_tvb = tvb_new_subset(tvb, start_offset,
+                                  subtvb_len /* length */,
+                                  hdr.frag_len /* reported_length */);
 
     /*
      * Packet type specific stuff is next.
@@ -4623,36 +4625,36 @@ dissect_dcerpc_cn(tvbuff_t *tvb, int offset, packet_info *pinfo,
     switch (hdr.ptype) {
     case PDU_BIND:
     case PDU_ALTER:
-        dissect_dcerpc_cn_bind(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr);
+        dissect_dcerpc_cn_bind(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr);
         break;
 
     case PDU_BIND_ACK:
     case PDU_ALTER_ACK:
-        dissect_dcerpc_cn_bind_ack(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr);
+        dissect_dcerpc_cn_bind_ack(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr);
         break;
 
     case PDU_AUTH3:
         /*
          * Nothing after the common header other than credentials.
          */
-        dissect_dcerpc_cn_auth(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr, TRUE,
+        dissect_dcerpc_cn_auth(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr, TRUE,
                                &auth_info);
         break;
 
     case PDU_REQ:
-        dissect_dcerpc_cn_rqst(fragment_tvb, offset, pinfo, dcerpc_tree, tree, &hdr);
+        dissect_dcerpc_cn_rqst(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, tree, &hdr);
         break;
 
     case PDU_RESP:
-        dissect_dcerpc_cn_resp(fragment_tvb, offset, pinfo, dcerpc_tree, tree, &hdr);
+        dissect_dcerpc_cn_resp(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, tree, &hdr);
         break;
 
     case PDU_FAULT:
-        dissect_dcerpc_cn_fault(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr);
+        dissect_dcerpc_cn_fault(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr);
         break;
 
     case PDU_BIND_NAK:
-        dissect_dcerpc_cn_bind_nak(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr);
+        dissect_dcerpc_cn_bind_nak(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr);
         break;
 
     case PDU_CO_CANCEL:
@@ -4661,7 +4663,7 @@ dissect_dcerpc_cn(tvbuff_t *tvb, int offset, packet_info *pinfo,
          * Nothing after the common header other than an authentication
          * verifier.
          */
-        dissect_dcerpc_cn_auth(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr, FALSE,
+        dissect_dcerpc_cn_auth(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr, FALSE,
                                &auth_info);
         break;
 
@@ -4672,12 +4674,12 @@ dissect_dcerpc_cn(tvbuff_t *tvb, int offset, packet_info *pinfo,
          */
         break;
     case PDU_RTS:
-      dissect_dcerpc_cn_rts(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr);
+      dissect_dcerpc_cn_rts(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr);
       break;
 
     default:
         /* might as well dissect the auth info */
-        dissect_dcerpc_cn_auth(fragment_tvb, offset, pinfo, dcerpc_tree, &hdr, FALSE,
+        dissect_dcerpc_cn_auth(fragment_tvb, MIN(offset - start_offset, subtvb_len), pinfo, dcerpc_tree, &hdr, FALSE,
                                &auth_info);
         break;
     }

epan/dissectors/packet-dns.c
@@ -1226,7 +1226,7 @@ add_opt_rr_to_tree(proto_item *trr, int rr_type, tvbuff_t *tvb, int offset,
   }
   offset += 2;
   proto_tree_add_text(rr_tree, tvb, offset, 1, "Higher bits in extended RCODE: 0x%x",
-      (ttl >> 24) & 0xff0);
+      (ttl >> 24) & 0xff);
   offset++;
   proto_tree_add_text(rr_tree, tvb, offset, 1, "EDNS0 version: %u",
       (ttl >> 16) & 0xff);

epan/dissectors/packet-eth.c
@@ -513,7 +513,7 @@ add_ethernet_trailer(packet_info *pinfo, proto_tree *tree, proto_tree *fh_tree,
   proto_item *item;
   proto_tree *checksum_tree;
 
-  if (trailer_tvb && fh_tree) {
+  if (trailer_tvb) {
     guint trailer_length, trailer_reported_length;
     guint padding_length = 0;
     gboolean has_fcs = FALSE;

epan/dissectors/packet-ethertype.c
@@ -329,9 +329,6 @@ add_dix_trailer(packet_info *pinfo, proto_tree *tree, proto_tree *fh_tree, int t
 	guint		 length;
 	tvbuff_t	*trailer_tvb;
 
-	if (fh_tree == NULL)
-		return;	/* we're not building a protocol tree */
-
 	/* OK, how much is there in that tvbuff now? */
 	length = tvb_reported_length(next_tvb);
 

epan/stats_tree.c
@@ -621,29 +621,28 @@ get_range(char *rngstr)
           return NULL;
         }
 
-        /* means we have a non empty string
-         * which does not contain a delimiter */
-        if (split[1] == NULL) {
-          g_strfreev(split);
-          return NULL;
-        }
-
         rng = (range_pair_t *)g_malloc(sizeof(range_pair_t));
 
-        /* string == "X-?" */
-        if (*(split[0]) != '\0') {
+        if (split[1] == NULL) {
+          /* means we have a non empty string with no delimiter
+           * so it must be a single number */
             rng->floor = (gint)strtol(split[0],NULL,10);
-        } else
-          /* string == "-?" */
-          rng->floor = G_MININT;
+            rng->ceil = rng->floor;
+        } else {
+          /* string == "X-?" */
+          if (*(split[0]) != '\0') {
+              rng->floor = (gint)strtol(split[0],NULL,10);
+          } else
+            /* string == "-?" */
+            rng->floor = G_MININT;
 
-        /* string != "?-" */
-        if (*(split[1]) != '\0') {
-          rng->ceil  = (gint)strtol(split[1],NULL,10);
-        } else
-          /* string == "?-" */
-          rng->ceil = G_MAXINT;
-
+          /* string != "?-" */
+          if (*(split[1]) != '\0') {
+            rng->ceil  = (gint)strtol(split[1],NULL,10);
+          } else
+            /* string == "?-" */
+            rng->ceil = G_MAXINT;
+        }
         g_strfreev(split);
 
 	return rng;

plugins/stats_tree/pinfo_stats_tree.c
@@ -84,6 +84,18 @@ static void* uat_plen_record_copy_cb(void* n, const void* o, size_t siz _U_) {
 	return n;
 }
 
+static void
+uat_plen_record_update_cb(void *r, const char **err)
+{
+	uat_plen_record_t *rec = (uat_plen_record_t*)r;
+	if (rec->packet_range->nranges < 1) {
+		*err = ep_strdup_printf("Invalid range string");
+		return;
+	}
+
+	*err = NULL;
+}
+
 static void uat_plen_record_free_cb(void*r) {
     uat_plen_record_t* record = (uat_plen_record_t*)r;
 
@@ -222,7 +234,7 @@ void register_pinfo_stat_trees(void) {
 			0,                          /* not a dissector, so affects neither dissection nor fields */
 			NULL,                       /* help */
 			uat_plen_record_copy_cb,    /* copy callback */
-			NULL,                       /* update callback */
+			uat_plen_record_update_cb,  /* update callback */
 			uat_plen_record_free_cb,    /* free callback */
 			uat_plen_record_post_update_cb, /* post update callback */
 			plen_uat_flds);             /* UAT field definitions */

tshark.c
@@ -3383,21 +3383,6 @@ print_columns(capture_file *cf)
       continue;
     switch (cf->cinfo.col_fmt[i]) {
     case COL_NUMBER:
-#ifdef HAVE_LIBPCAP
-      /*
-       * Don't print this if we're doing a live capture from a network
-       * interface - if we're doing a live capture, you won't be
-       * able to look at the capture in the future (it's not being
-       * saved anywhere), so the frame numbers are unlikely to be
-       * useful.
-       *
-       * (XXX - it might be nice to be able to save and print at
-       * the same time, sort of like an "Update list of packets
-       * in real time" capture in Wireshark.)
-       */
-      if (global_capture_opts.ifaces->len > 0)
-        continue;
-#endif
       column_len = strlen(cf->cinfo.col_data[i]);
       if (column_len < 3)
         column_len = 3;

ui/gtk/uat_gui.c
@@ -113,7 +113,6 @@ struct _uat_dlg_data {
 	GPtrArray *tobe_freed;
 };
 
-
 static gboolean unsaved_dialog(GtkWindow *w, GdkEvent *e, gpointer u);
 static gboolean uat_window_delete_event_cb(GtkWindow *w, GdkEvent *e, gpointer u);
 
@@ -133,10 +132,17 @@ static void set_buttons(uat_t *uat, gint row) {
 		gtk_widget_set_sensitive (uat->rep->bt_down, FALSE);
 	}
 
+	gtk_widget_set_sensitive (uat->rep->bt_new, TRUE);
+	gtk_widget_set_sensitive (uat->rep->bt_clear, TRUE);
+
 	if (row < 0) {
 		gtk_widget_set_sensitive (uat->rep->bt_edit, FALSE);
 		gtk_widget_set_sensitive (uat->rep->bt_copy, FALSE);
 		gtk_widget_set_sensitive (uat->rep->bt_delete, FALSE);
+	} else {
+		gtk_widget_set_sensitive (uat->rep->bt_edit, TRUE);
+		gtk_widget_set_sensitive (uat->rep->bt_copy, TRUE);
+		gtk_widget_set_sensitive (uat->rep->bt_delete, TRUE);
 	}
 
 	if (uat->changed) {
@@ -148,6 +154,21 @@ static void set_buttons(uat_t *uat, gint row) {
 		g_signal_connect(GTK_WINDOW(uat->rep->window), "delete_event", G_CALLBACK(uat_window_delete_event_cb), uat);
 		g_signal_connect(GTK_WINDOW(uat->rep->window), "destroy", G_CALLBACK(uat_window_delete_event_cb), uat);
 	}
+}
+
+static void limit_buttons(uat_t *uat) {
+
+	if (!uat->rep) return;
+
+	gtk_widget_set_sensitive (uat->rep->bt_up, FALSE);
+	gtk_widget_set_sensitive (uat->rep->bt_down, FALSE);
+
+	gtk_widget_set_sensitive (uat->rep->bt_new, FALSE);
+	gtk_widget_set_sensitive (uat->rep->bt_edit, FALSE);
+	gtk_widget_set_sensitive (uat->rep->bt_copy, FALSE);
+	gtk_widget_set_sensitive (uat->rep->bt_delete, FALSE);
+
+	gtk_widget_set_sensitive (uat->rep->bt_clear, FALSE);
 }
 
 static char *fld_tostr(void *rec, uat_field_t *f) {
@@ -400,6 +421,9 @@ static gboolean uat_cancel_dlg_cb(GtkWidget *win _U_, gpointer user_data) {
 	if (dd->uat->rep)
 		window_present(GTK_WIDGET(dd->uat->rep->window));
 
+	/* Reset the buttons */
+	set_buttons(dd->uat, dd->uat->rep ? dd->uat->rep->selected : -1);
+
 	if (dd->is_new) g_free(dd->rec);
 	g_ptr_array_free(dd->entries, TRUE);
 	window_destroy(GTK_WIDGET(dd->win));
@@ -422,6 +446,9 @@ static void uat_edit_dialog(uat_t *uat, gint row, gboolean copy) {
 	struct _uat_dlg_data *dd = (struct _uat_dlg_data *)g_malloc(sizeof(struct _uat_dlg_data));
 	uat_field_t	     *f	 = uat->fields;
 	guint		      colnum;
+
+	/* Only allow a single operation at a time, prevents bug 9129 */
+	limit_buttons(uat);
 
 	dd->entries = g_ptr_array_new();
 	dd->win = dlg_conf_window_new(ep_strdup_printf("%s: %s", uat->name, (row == -1 ? "New" : "Edit")));
@@ -578,6 +605,9 @@ static void uat_cancel_del_cb(GtkButton *button _U_, gpointer u) {
 
 	window_destroy(GTK_WIDGET(ud->win));
 
+	/* Reset the buttons */
+	set_buttons(ud->uat, ud->uat->rep ? ud->uat->rep->selected : -1);
+
 	if (ud->uat->rep)
 		window_present(GTK_WIDGET(ud->uat->rep->window));
 	g_free(ud);
@@ -588,7 +618,11 @@ static void uat_del_dlg(uat_t *uat, int idx) {
 	uat_field_t	*f   = uat->fields;
 	guint		 colnum;
 	void		*rec = UAT_INDEX_PTR(uat, idx);
+
 	struct _uat_del *ud  = (struct _uat_del *)g_malloc(sizeof(struct _uat_del));
+
+	/* Only allow a single operation at a time, prevents bug 9129 */
+	limit_buttons(uat);
 
 	ud->uat = uat;
 	ud->idx = idx;
@@ -975,6 +1009,7 @@ static GtkWidget *uat_window(void *u) {
 
 	selection = gtk_tree_view_get_selection(rep->list);
 	gtk_tree_selection_set_mode(selection, GTK_SELECTION_SINGLE);
+	rep->selected = -1;
 
 	for ( colnum = 0; colnum < uat->ncols; colnum++ ) {
 		renderer = gtk_cell_renderer_text_new();