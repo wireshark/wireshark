9bdeb996063c29c50080dcb5b8b208dd621bc757
Jörg Mayer <jmayer@loplof.de>
2013-11-01 09:54:05 +0000

    Remove a comment about adding use of the sh version of the script.
    svn path=/trunk/; revision=53013

--- cmake/modules/UseMakeDissectorReg.cmake
+++ cmake/modules/UseMakeDissectorReg.cmake
@@ -2,19 +2,6 @@
 # $Id$
 #
 MACRO(REGISTER_DISSECTOR_FILES _outputfile _registertype )
-	# FIXME: Only the Python stuff has been implemented
-	#        Make this into a MACRO, to avoid duplication with plugins/.../
-	#register.c: $(plugin_src) $(ALL_DISSECTORS_SRC) $(top_srcdir)/tools/make-dissector-reg \
-	#    $(top_srcdir)/tools/make-dissector-reg.py
-	#        @if test -n "$(PYTHON)"; then \
-	#                echo Making register.c with python ; \
-	#                $(PYTHON) $(top_srcdir)/tools/make-dissector-reg.py $(srcdir) \
-	#                    dissectors $(ALL_DISSECTORS_SRC) ; \
-	#        else \
-	#                echo Making register.c with shell script ; \
-	#                $(top_srcdir)/tools/make-dissector-reg $(srcdir) \
-	#                   dissectors $(plugin_src) $(ALL_DISSECTORS_SRC) ; \
-	#        fi
 	set( _sources ${ARGN} )
 	ADD_CUSTOM_COMMAND(
 	    OUTPUT
