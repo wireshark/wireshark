0a0af313759c68187fcb2852c3829441d99dec60
Guy Harris <guy@alum.mit.edu>
2013-11-02 01:07:07 +0000

    Mark unused arguments.
    svn path=/trunk/; revision=53038

--- epan/dissectors/packet-ldss.c
+++ epan/dissectors/packet-ldss.c
@@ -802,7 +802,7 @@ is_broadcast(address* addr)
 }
 
 static int
-dissect_ldss (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
+dissect_ldss (tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
 	if (is_broadcast(&pinfo->dl_dst)) {
 
