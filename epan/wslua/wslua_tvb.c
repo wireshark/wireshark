8c11584ce12ab7aa63e3819cb9276ba1520e5251
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2013-11-15 23:33:02 +0000

    Make wslua byte_to_str[] array of char[3] instead of pointers to char.
    XXX, it should use byte_to_hex() from to_str.c
    
    svn path=/trunk/; revision=53350

--- epan/wslua/wslua_tvb.c
+++ epan/wslua/wslua_tvb.c
@@ -251,7 +251,7 @@ WSLUA_METHOD ByteArray_subset(lua_State* L) {
 
 static int ByteArray_tostring(lua_State* L) {
 	/* Obtain a string containing the bytes in a ByteArray so that it can be used in display filters (e.g. "01:23:45:67:89:AB") */
-    static const gchar* byte_to_str[] = {
+    static const char byte_to_str[][3] = {
         "00","01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F",
         "10","11","12","13","14","15","16","17","18","19","1A","1B","1C","1D","1E","1F",
         "20","21","22","23","24","25","26","27","28","29","2A","2B","2C","2D","2E","2F",
