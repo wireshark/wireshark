2276ff07d2260fda8c725797b6e476b2ef406892
Jörg Mayer <jmayer@loplof.de>
2013-11-18 07:42:23 +0000

    Beginnings of CheckAPI support. More diligence needed (need to seperate generated files into their own variables).
    svn path=/trunk/; revision=53407

---
+++ cmake/modules/UseCheckAPI.cmake
@@ -0,0 +1,18 @@
+#
+# $Id$
+#
+macro( CHECKAPI )
+	set( _sources ${ARGN} )
+
+	ADD_CUSTOM_TARGET(checkapi
+		COMMAND ${PERL_EXECUTABLE}
+		  ${CMAKE_SOURCE_DIR}/tools/checkAPIs.pl
+		  -build
+		  ${_sources}
+		WORKING_DIRECTORY
+		  ${CMAKE_SOURCE_DIR}
+		DEPENDS
+		  ${CMAKE_SOURCE_DIR}/tools/checkAPIs.pl
+		  ${_sources}
+	)
+ENDMACRO()
