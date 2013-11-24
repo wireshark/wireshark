eb223ff7967558f7c89ac0ab1da0623a523d3279
Guy Harris <guy@alum.mit.edu>
2013-11-24 03:21:50 +0000

    Regenerate - new API changes, get rid of trailing whitespace.
    svn path=/trunk/; revision=53539

--- epan/dissectors/packet-dcerpc-misc.c
+++ epan/dissectors/packet-dcerpc-misc.c
@@ -2,9 +2,9 @@
 	This filter was automatically generated
 	from misc.idl and misc.cnf.
 	
-	Pidl is a perl based IDL compiler for DCE/RPC idl files. 
+	Pidl is a perl based IDL compiler for DCE/RPC idl files.
 	It is maintained by the Samba team, not the Wireshark team.
-	Instructions on how to download and install Pidl can be 
+	Instructions on how to download and install Pidl can be
 	found at http://wiki.wireshark.org/Pidl
 
 	$Id$
@@ -121,13 +121,13 @@ misc_dissect_element_winreg_Data_string(tvbuff_t *tvb _U_, int offset _U_, packe
 	return offset;
 }
 static int
-misc_dissect_element_winreg_Data_value(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_, dcerpc_info* di _U_, guint8 *drep _U_)
+misc_dissect_element_winreg_Data_value(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_, dcerpc_info* di, guint8 *drep _U_)
 {
 	offset = PIDL_dissect_uint32(tvb, offset, pinfo, tree, di, drep, hf_misc_winreg_Data_value, 0);
 	return offset;
 }
 static int
-misc_dissect_struct_string_array(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_, guint8 *drep _U_, int hf_index _U_, guint32 param _U_)
+misc_dissect_struct_string_array(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_, dcerpc_info *di _U_, guint8 *drep _U_, int hf_index _U_, guint32 param _U_)
 {
     /* We don't do it yet */
     return offset;
@@ -488,7 +488,7 @@ misc_dissect_element_winreg_Data_binary(tvbuff_t *tvb _U_, int offset _U_, packe
 static int
 misc_dissect_element_winreg_Data_string_array(tvbuff_t *tvb _U_, int offset _U_, packet_info *pinfo _U_, proto_tree *tree _U_, dcerpc_info* di _U_, guint8 *drep _U_)
 {
-	offset = misc_dissect_struct_string_array(tvb,offset,pinfo,tree,drep,hf_misc_winreg_Data_string_array,0);
+	offset = misc_dissect_struct_string_array(tvb,offset,pinfo,tree,di,drep,hf_misc_winreg_Data_string_array,0);
 
 	return offset;
 }
@@ -560,41 +560,41 @@ static dcerpc_sub_dissector misc_dissectors[] = {
 void proto_register_dcerpc_misc(void)
 {
 	static hf_register_info hf[] = {
-	{ &hf_misc_GUID_time_mid, 
+	{ &hf_misc_GUID_time_mid,
 	  { "Time Mid", "misc.GUID.time_mid", FT_UINT16, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_winreg_Data_value, 
+	{ &hf_misc_winreg_Data_value,
 	  { "Value", "misc.winreg_Data.value", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_GUID_time_low, 
+	{ &hf_misc_GUID_time_low,
 	  { "Time Low", "misc.GUID.time_low", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_KRB5_EDATA_NTSTATUS_unknown2, 
+	{ &hf_misc_KRB5_EDATA_NTSTATUS_unknown2,
 	  { "Unknown2", "misc.KRB5_EDATA_NTSTATUS.unknown2", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_GUID_time_hi_and_version, 
+	{ &hf_misc_GUID_time_hi_and_version,
 	  { "Time Hi And Version", "misc.GUID.time_hi_and_version", FT_UINT16, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_KRB5_EDATA_NTSTATUS_ntstatus, 
+	{ &hf_misc_KRB5_EDATA_NTSTATUS_ntstatus,
 	  { "Ntstatus", "misc.KRB5_EDATA_NTSTATUS.ntstatus", FT_UINT32, BASE_DEC, VALS(NT_errors), 0, NULL, HFILL }},
-	{ &hf_misc_GUID_node, 
+	{ &hf_misc_GUID_node,
 	  { "Node", "misc.GUID.node", FT_UINT8, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_winreg_Data_string, 
+	{ &hf_misc_winreg_Data_string,
 	  { "String", "misc.winreg_Data.string", FT_STRING, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_winreg_Data_data, 
+	{ &hf_misc_winreg_Data_data,
 	  { "Data", "misc.winreg_Data.data", FT_NONE, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_winreg_Data_binary, 
+	{ &hf_misc_winreg_Data_binary,
 	  { "Binary", "misc.winreg_Data.binary", FT_NONE, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_policy_handle_uuid, 
+	{ &hf_misc_policy_handle_uuid,
 	  { "Uuid", "misc.policy_handle.uuid", FT_GUID, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_KRB5_EDATA_NTSTATUS_unknown1, 
+	{ &hf_misc_KRB5_EDATA_NTSTATUS_unknown1,
 	  { "Unknown1", "misc.KRB5_EDATA_NTSTATUS.unknown1", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_policy_handle_handle_type, 
+	{ &hf_misc_policy_handle_handle_type,
 	  { "Handle Type", "misc.policy_handle.handle_type", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_ndr_syntax_id_uuid, 
+	{ &hf_misc_ndr_syntax_id_uuid,
 	  { "Uuid", "misc.ndr_syntax_id.uuid", FT_GUID, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_opnum, 
+	{ &hf_misc_opnum,
 	  { "Operation", "misc.opnum", FT_UINT16, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_GUID_clock_seq, 
+	{ &hf_misc_GUID_clock_seq,
 	  { "Clock Seq", "misc.GUID.clock_seq", FT_UINT8, BASE_DEC, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_winreg_Data_string_array, 
+	{ &hf_misc_winreg_Data_string_array,
 	  { "String Array", "misc.winreg_Data.string_array", FT_STRING, BASE_NONE, NULL, 0, NULL, HFILL }},
-	{ &hf_misc_ndr_syntax_id_if_version, 
+	{ &hf_misc_ndr_syntax_id_if_version,
 	  { "If Version", "misc.ndr_syntax_id.if_version", FT_UINT32, BASE_DEC, NULL, 0, NULL, HFILL }},
 	};
 
