402fed459fb3f03c3f6fd2a0fd6649b935904c13
Pascal Quantin <pascal.quantin@gmail.com>
2013-11-25 11:23:11 +0000

    Fix a shadowed declaration error when compiling with gcc 4.3.2
    svn path=/trunk/; revision=53562

--- epan/dissectors/packet-lpp.c
+++ epan/dissectors/packet-lpp.c
@@ -8339,18 +8339,18 @@ static const value_string lpp_T_reportingInterval_vals[] = {
 static int
 dissect_lpp_T_reportingInterval(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
 #line 259 "../../asn1/lpp/lpp.cnf"
-  guint32 index;
+  guint32 idx;
   const gchar *interval[10] = {"", ": 1 s", ": 2 s", ": 4 s", ": 8 s", ": 10 s",
                                ": 16 s", ": 20 s", ": 32 s", ": 64 s"};
 
   offset = dissect_per_enumerated(tvb, offset, actx, tree, hf_index,
-                                     10, &index, FALSE, 0, NULL);
+                                     10, &idx, FALSE, 0, NULL);
 
 
 
 
 #line 266 "../../asn1/lpp/lpp.cnf"
-  proto_item_append_text(actx->created_item, "%s", interval[index]);
+  proto_item_append_text(actx->created_item, "%s", interval[idx]);
 
 
   return offset;
