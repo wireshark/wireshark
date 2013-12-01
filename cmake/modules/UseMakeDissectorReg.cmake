70f4223c2ae42f5695e6aa5ec5880cd38bfed30f
Jörg Mayer <jmayer@loplof.de>
2013-12-01 22:24:59 +0000

    The changes to work around the msbuild deficiencies broke dependeny tracking (replaced the list of dissectors by the file containing the dissectors). Fix that.
    svn path=/trunk/; revision=53703

--- cmake/modules/UseMakeDissectorReg.cmake
+++ cmake/modules/UseMakeDissectorReg.cmake
@@ -5,14 +5,16 @@ MACRO(REGISTER_DISSECTOR_FILES _outputfile _registertype )
 	if(${_registertype} STREQUAL "dissectors" )
 	    set( _makeregistertype "dissectorsinfile" )
 	    set( _ftmp "${CMAKE_CURRENT_BINARY_DIR}/_regc.tmp" )
+	    set( _depends ${ARGN} )
 	    file(REMOVE ${_ftmp})
-	    foreach(f ${ARGN})
+	    foreach(f ${_depends})
 	      file(APPEND ${_ftmp} "${f}\n")
 	    endforeach()
 	    set( _sources ${_ftmp} )
 	else()
 	    set( _makeregistertype ${_registertype} )
 	    set( _sources ${ARGN} )
+	    set( _depends ${_sources} )
 	endif()
 	ADD_CUSTOM_COMMAND(
 	    OUTPUT
@@ -23,7 +25,7 @@ MACRO(REGISTER_DISSECTOR_FILES _outputfile _registertype )
 	      ${_makeregistertype}
 	      ${_sources}
 	    DEPENDS
-	      ${_sources}
+	      ${_depends}
 	      ${CMAKE_SOURCE_DIR}/tools/make-dissector-reg
 	      ${CMAKE_SOURCE_DIR}/tools/make-dissector-reg.py
 	)
