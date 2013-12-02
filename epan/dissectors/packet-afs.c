baf569188ac49ad38912b82d23ff241321cd654f
Chris Maynard <Christopher.Maynard@GTECH.COM>
2013-12-02 02:31:33 +0000

    Reject the packet if data is NULL.
    svn path=/trunk/; revision=53709

--- epan/dissectors/packet-afs.c
+++ epan/dissectors/packet-afs.c
@@ -1655,6 +1655,9 @@ dissect_afs(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
 
 	void (*dissector)(tvbuff_t *tvb, struct rxinfo *rxinfo, proto_tree *tree, int offset, int opcode);
 
+	/* Reject the packet if data is NULL */
+	if (data == NULL)
+		return 0;
 
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "AFS (RX)");
 	col_clear(pinfo->cinfo, COL_INFO);
