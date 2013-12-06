7a5cf63f0444325632451a80e0c885305c0afef8
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-12-06 08:37:48 +0000

    Fix wireshark/epan/dissectors/packet-j1939.c:313: multiple definition of `proto_register_devicenet'
    svn path=/trunk/; revision=53806

--- epan/dissectors/packet-j1939.c
+++ epan/dissectors/packet-j1939.c
@@ -255,7 +255,7 @@ static int dissect_j1939(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, vo
     return tvb_length(tvb);
 }
 
-void proto_register_devicenet(void)
+void proto_register_j1939(void)
 {
     static hf_register_info hf[] = {
         { &hf_j1939_can_id,
