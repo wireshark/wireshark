e8203409962823a90cc0d26cb054362ff52a75d6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-12-06 16:40:55 +0000

    Add Modelines info
    svn path=/trunk/; revision=53813

--- epan/dissectors/packet-j1939.c
+++ epan/dissectors/packet-j1939.c
@@ -319,3 +319,16 @@ void proto_register_j1939(void)
 
     subdissector_pgn_table = register_dissector_table("j1939.pgn", "PGN Handle", FT_UINT32, BASE_DEC);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
