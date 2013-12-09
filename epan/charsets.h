3dbf837040ad3cbedd7e31602991afdef3334586
Martin Kaiser <wireshark@kaiser.cx>
2013-12-09 20:58:57 +0000

    add editor modelines
    svn path=/trunk/; revision=53890

--- epan/charsets.h
+++ epan/charsets.h
@@ -108,3 +108,16 @@ extern const gunichar2 charset_table_iso_8859_2[0x80];
 #endif /* __cplusplus */
 
 #endif /* __CHARSETS_H__ */
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
