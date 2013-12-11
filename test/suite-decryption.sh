1482ce49979feb576352755b6575bd80e9a24230
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2013-12-11 20:52:05 +0000

    Revert part of r53874
    svn path=/trunk/; revision=53944

--- test/suite-decryption.sh
+++ test/suite-decryption.sh
@@ -80,7 +80,8 @@ decryption_step_80211_wpa_psk() {
 decryption_step_dtls() {
 	env $TS_DC_ENV $TSHARK $TS_DC_ARGS \
 		-Tfields -e data.data \
-		-r "$CAPTURE_DIR/snakeoil-dtls.pcap" -Y http
+		-r "$CAPTURE_DIR/snakeoil-dtls.pcap" -Y http \
+		| grep "69:74:20:77:6f:72:6b:20:21:0a" > /dev/null 2>&1
 	RETURNVALUE=$?
 	if [ ! $RETURNVALUE -eq $EXIT_OK ]; then
 		test_step_failed "Failed to decrypt DTLS"
