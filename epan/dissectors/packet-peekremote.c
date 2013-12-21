2af870ce4c471a282ed653446e607a843948b941
Jörg Mayer <jmayer@loplof.de>
2013-12-21 01:50:54 +0000

    Convert to NEW_PROTO_TREE_API
    svn path=/trunk/; revision=54319

--- epan/dissectors/packet-peekremote.c
+++ epan/dissectors/packet-peekremote.c
@@ -49,6 +49,8 @@
  *  and so on"
  */
 
+#define NEW_PROTO_TREE_API
+
 #include "config.h"
 
 #include <glib.h>
@@ -59,22 +61,53 @@
 void proto_register_peekremote(void);
 void proto_reg_handoff_peekremote(void);
 
-static int proto_peekremote = -1;
-static gint hf_peekremote_signal = -1;
-static gint hf_peekremote_noise = -1;
-static gint hf_peekremote_packetlength = -1;
-static gint hf_peekremote_slicelength = -1;
-static gint hf_peekremote_unknown4 = -1;
-static gint hf_peekremote_speed = -1;
-static gint hf_peekremote_unknown6 = -1;
-static gint hf_peekremote_channel = -1;
-static gint hf_peekremote_timestamp = -1;
+/* hfi elements */
+#define THIS_HF_INIT HFI_INIT(proto_peekremote)
+static header_field_info *hfi_peekremote = NULL;
+
+static header_field_info hfi_peekremote_signal THIS_HF_INIT =
+      { "Signal [dBm]",      "peekremote.signal", FT_INT8, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_noise THIS_HF_INIT =
+      { "Noise [dBm]",      "peekremote.noise", FT_INT8, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_packetlength THIS_HF_INIT =
+      { "Packet length",      "peekremote.packetlength", FT_UINT16, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_slicelength THIS_HF_INIT =
+      { "Slice length",      "peekremote.slicelength", FT_UINT16, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_unknown4 THIS_HF_INIT =
+      { "Unknown4",      "peekremote.unknown4", FT_BYTES, BASE_NONE, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_speed THIS_HF_INIT =
+      { "Speed [500kHz]",      "peekremote.speed", FT_UINT8, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_unknown6 THIS_HF_INIT =
+      { "Unknown6",      "peekremote.unknown6", FT_BYTES, BASE_NONE, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_channel THIS_HF_INIT =
+      { "Channel",       "peekremote.channel", FT_UINT8, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
+static header_field_info hfi_peekremote_timestamp THIS_HF_INIT =
+      { "Timestamp?",       "peekremote.timestamp", FT_UINT32, BASE_DEC, NULL,
+        0x0, NULL, HFILL };
+
 static gint ett_peekremote = -1;
 
+static dissector_handle_t peekremote_handle;
 static dissector_handle_t ieee80211_handle;
 
-static void
-dissect_peekremote(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+static int
+dissect_peekremote(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *u _U_)
 {
   tvbuff_t *next_tvb;
   proto_tree *peekremote_tree = NULL;
@@ -84,82 +117,59 @@ dissect_peekremote(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
   col_clear(pinfo->cinfo, COL_INFO);
 
   if (tree) {
-    ti = proto_tree_add_item(tree, proto_peekremote, tvb, 0, -1, ENC_NA);
+    ti = proto_tree_add_item(tree, hfi_peekremote, tvb, 0, -1, ENC_NA);
     peekremote_tree = proto_item_add_subtree(ti, ett_peekremote);
 
-    proto_tree_add_item(peekremote_tree, hf_peekremote_signal, tvb, 0, 1,  ENC_NA);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_noise, tvb, 1, 1,  ENC_NA);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_packetlength, tvb, 2, 2,  ENC_BIG_ENDIAN);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_slicelength, tvb, 4, 2,  ENC_BIG_ENDIAN);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_unknown4, tvb, 6, 6, ENC_NA);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_timestamp, tvb, 12, 4, ENC_BIG_ENDIAN);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_speed, tvb, 16, 1, ENC_NA);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_channel, tvb, 17, 1, ENC_BIG_ENDIAN);
-    proto_tree_add_item(peekremote_tree, hf_peekremote_unknown6, tvb, 18, 2, ENC_NA);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_signal, tvb, 0, 1,  ENC_NA);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_noise, tvb, 1, 1,  ENC_NA);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_packetlength, tvb, 2, 2,  ENC_BIG_ENDIAN);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_slicelength, tvb, 4, 2,  ENC_BIG_ENDIAN);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_unknown4, tvb, 6, 6, ENC_NA);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_timestamp, tvb, 12, 4, ENC_BIG_ENDIAN);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_speed, tvb, 16, 1, ENC_NA);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_channel, tvb, 17, 1, ENC_BIG_ENDIAN);
+    proto_tree_add_item(peekremote_tree, &hfi_peekremote_unknown6, tvb, 18, 2, ENC_NA);
   }
   next_tvb = tvb_new_subset_remaining(tvb, 20);
   pinfo->pseudo_header->ieee_802_11.fcs_len = 4;
-  call_dissector(ieee80211_handle, next_tvb, pinfo, tree);
+  return 20 + call_dissector(ieee80211_handle, next_tvb, pinfo, tree);
 }
 
 void
 proto_register_peekremote(void)
 {
-  static hf_register_info hf[] = {
-    { &hf_peekremote_signal,
-      { "Signal [dBm]",      "peekremote.signal", FT_INT8, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_noise,
-      { "Noise [dBm]",      "peekremote.noise", FT_INT8, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_packetlength,
-      { "Packet length",      "peekremote.packetlength", FT_UINT16, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_slicelength,
-      { "Slice length",      "peekremote.slicelength", FT_UINT16, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_unknown4,
-      { "Unknown4",      "peekremote.unknown4", FT_BYTES, BASE_NONE, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_speed,
-      { "Speed [500kHz]",      "peekremote.speed", FT_UINT8, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_unknown6,
-      { "Unknown6",      "peekremote.unknown6", FT_BYTES, BASE_NONE, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_timestamp,
-      { "Timestamp?",       "peekremote.timestamp", FT_UINT32, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
-    { &hf_peekremote_channel,
-      { "Channel",       "peekremote.channel", FT_UINT8, BASE_DEC, NULL,
-        0x0, NULL, HFILL }},
-
+#ifndef HAVE_HFI_SECTION_INIT
+  static hfi_register_info *hfi[] = {
+    &hfi_peekremote_signal,
+    &hfi_peekremote_noise,
+    &hfi_peekremote_packetlength,
+    &hfi_peekremote_slicelength,
+    &hfi_peekremote_unknown4,
+    &hfi_peekremote_speed,
+    &hfi_peekremote_unknown6,
+    &hfi_peekremote_timestamp,
+    &hfi_peekremote_channel,
   };
+#endif
   static gint *ett[] = {
     &ett_peekremote,
   };
 
+  int proto_peekremote;
+
   proto_peekremote = proto_register_protocol(
     "AiroPeek/OmniPeek encapsulated IEEE 802.11", "PEEKREMOTE", "peekremote");
-  proto_register_field_array(proto_peekremote, hf, array_length(hf));
+  hfi_peekremote = proto_registrar_get_nth(proto_peekremote);
+  proto_register_fields(proto_peekremote, hfi, array_length(hfi));
   proto_register_subtree_array(ett, array_length(ett));
+
+  peekremote_handle = new_create_dissector_handle(dissect_peekremote, proto_peekremote);
 }
 
 void
 proto_reg_handoff_peekremote(void)
 {
-  dissector_handle_t peekremote_handle;
-
   ieee80211_handle = find_dissector("wlan_datapad");
 
-  peekremote_handle = create_dissector_handle(dissect_peekremote, proto_peekremote);
   dissector_add_uint("udp.port", 5000, peekremote_handle);
 }
