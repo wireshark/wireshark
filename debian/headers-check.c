00048cf418e4af9472c46e51d1cdc8294283aeea
Evan Huus <eapache@gmail.com>
2013-12-23 15:19:25 +0000

    Add GPLv2+ license header to headers-check.c as per private email from the author.
    svn path=/trunk/; revision=54384

--- debian/headers-check.c
+++ debian/headers-check.c
@@ -1,3 +1,27 @@
+/* headers-check.c
+ *
+ * Test program to ensure all required headers are in the debian package,
+ * by Laszio <ezerotven@gmail.com>.
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <wireshark/config.h>
 #include <epan/packet.h>
 #include <epan/prefs.h>
