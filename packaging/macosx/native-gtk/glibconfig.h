fa2c0bb556de44faaf14e537723643ce4b7f6b0e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2013-12-27 08:11:25 +0000

    From Peter Lemenkov via https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9576
    Replace g_memmove with memmove
    
    However there still one move g_memmove related code in "packaging/macosx/native-gtk/glibconfig.h".
    
    svn path=/trunk/; revision=54472

--- packaging/macosx/native-gtk/glibconfig.h
+++ packaging/macosx/native-gtk/glibconfig.h
@@ -78,7 +78,6 @@ typedef unsigned long gsize;
 # define g_ATEXIT(proc)	(atexit (proc))
 #endif
 
-#define g_memmove(dest,src,len) G_STMT_START { memmove ((dest), (src), (len)); } G_STMT_END
 
 #define GLIB_MAJOR_VERSION 2
 #define GLIB_MINOR_VERSION 12
