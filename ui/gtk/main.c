wireshark/wireshark

commit 33ef0c2600a986e8094caa266873d48c5899dece

isascii(x) && isprint(x) -> g_ascii_isprint(x)
svn path=/trunk/; revision=54328

ui/gtk/main.c
@@ -3686,10 +3686,10 @@ top_level_key_pressed_cb(GtkWidget *w _U_, GdkEventKey *event, gpointer user_dat
     /*
      * A comment in gdkkeysyms.h says that it's autogenerated from
      * freedesktop.org/x.org's keysymdef.h.  Although the GDK docs
-     * don't explicitly say so, isprint() should work as expected
+     * don't explicitly say so, g_ascii_isprint() should work as expected
      * for values < 127.
      */
-    } else if (isascii(event->keyval) && isprint(event->keyval)) {
+    } else if (g_ascii_isprint(event->keyval)) {
         /* Forward the keypress on to the display filter entry */
         if (main_display_filter_widget && !gtk_widget_is_focus(main_display_filter_widget)) {
             gtk_window_set_focus(GTK_WINDOW(top_level), main_display_filter_widget);

asn1/ldap/packet-ldap-template.c
@@ -681,7 +681,7 @@ dissect_ldap_AssertionValue(gboolean implicit_tag, tvbuff_t *tvb, int offset, as
 	str=tvb_get_ptr(tvb, offset, len);
 	is_ascii=TRUE;
 	for(i=0;i<len;i++){
-		if(!isascii(str[i]) || !isprint(str[i])){
+		if(!g_ascii_isprint(str[i])){
 			is_ascii=FALSE;
 			break;
 		}

epan/dissectors/packet-ldap.c
@@ -899,7 +899,7 @@ dissect_ldap_AssertionValue(gboolean implicit_tag, tvbuff_t *tvb, int offset, as
 	str=tvb_get_ptr(tvb, offset, len);
 	is_ascii=TRUE;
 	for(i=0;i<len;i++){
-		if(!isascii(str[i]) || !isprint(str[i])){
+		if(!g_ascii_isprint(str[i])){
 			is_ascii=FALSE;
 			break;
 		}

epan/dissectors/packet-ositp.c
@@ -346,7 +346,7 @@ static gboolean is_all_printable(const guchar *stringtocheck, int length)
 
   allprintable=TRUE;
   for (i=0;i<length;i++) {
-    if (!(isascii(stringtocheck[i]) && isprint(stringtocheck[i]))) {
+    if (!g_ascii_isprint(stringtocheck[i])) {
       allprintable=FALSE;
       break;
     }

epan/strutil.c
@@ -565,7 +565,7 @@ uri_str_to_bytes(const char *uri_str, GByteArray *bytes) {
     p = (const guchar *)uri_str;
 
     while (*p) {
-        if (! isascii(*p) || ! g_ascii_isprint(*p))
+        if (!g_ascii_isprint(*p))
             return FALSE;
         if (*p == '%') {
             p++;
@@ -636,7 +636,7 @@ format_uri(const GByteArray *bytes, const gchar *reserved_chars)
         }
         c = bytes->data[column];
 
-        if (!isascii(c) || !g_ascii_isprint(c) || c == '%') {
+        if (!g_ascii_isprint(c) || c == '%') {
             is_reserved = TRUE;
         }
 

wiretap/k12.c
@@ -116,7 +116,7 @@ void k12_hex_ascii_dump(guint level, gint64 offset, const char* label, const uns
         fprintf(dbg_out, "    ");
         for (j=0; j<16; j++) {
             if ((i+j)<len)
-                fprintf(dbg_out, "%c", isascii(b[i+j]) && isprint(b[i+j]) ? b[i+j] : '.');
+                fprintf(dbg_out, "%c", g_ascii_isprint(b[i+j]) ? b[i+j] : '.');
         }
         fprintf(dbg_out,"\n");
     }

commit ce68b8db87a39dfbc08972b71179376329541f7f

Make arrow down/ up work in gtk again.
Broken by r54328 g_ascii_isprint() can test only char type and event->keyval is unsigned int.

svn path=/trunk/; revision=54492

ui/gtk/main.c
@@ -3689,7 +3689,7 @@ top_level_key_pressed_cb(GtkWidget *w _U_, GdkEventKey *event, gpointer user_dat
      * don't explicitly say so, g_ascii_isprint() should work as expected
      * for values < 127.
      */
-    } else if (g_ascii_isprint(event->keyval)) {
+    } else if (event->keyval < 256 && g_ascii_isprint(event->keyval)) {
         /* Forward the keypress on to the display filter entry */
         if (main_display_filter_widget && !gtk_widget_is_focus(main_display_filter_widget)) {
             gtk_window_set_focus(GTK_WINDOW(top_level), main_display_filter_widget);