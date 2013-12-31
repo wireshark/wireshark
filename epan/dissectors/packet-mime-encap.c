8387a45fcc1f5cf586e1714e45d10fea20dc20d3
Guy Harris <guy@alum.mit.edu>
2013-12-31 23:47:24 +0000

    When reading MIME-encapsulated files, read the entire file at once, don't break it into chunks.
    This means we don't need to do reassembly in the MIME-encapsulated-data
    dissector.
    
    svn path=/trunk/; revision=54524

--- epan/dissectors/packet-mime-encap.c
+++ epan/dissectors/packet-mime-encap.c
@@ -26,9 +26,8 @@
 #include <glib.h>
 
 #include <epan/packet.h>
-#include <wiretap/wtap.h>
 
-#include "tvbuff-int.h"
+#include <wiretap/wtap.h>
 
 void proto_register_mime_encap(void);
 void proto_reg_handoff_mime_encap(void);
@@ -38,72 +37,19 @@ static int proto_mime_encap = -1;
 static heur_dissector_list_t heur_subdissector_list;
 static dissector_handle_t data_handle;
 
-static tvbuff_t *file_tvbs;
-static tvbuff_t *whole_tvb;
-
-static void
-mime_encap_init(void)
-{
-	if (file_tvbs) {
-		tvb_free_chain(file_tvbs);
-		file_tvbs = NULL;
-		whole_tvb = NULL;
-	}
-}
-
 static void
 dissect_mime_encap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 	proto_item* item;
-	guint len;
 
 	/* XXX, COL_INFO */
 
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "MIME_FILE");
 	item = proto_tree_add_item(tree, proto_mime_encap, tvb, 0, -1, ENC_NA);
 
-	/* frames with nsec >= 1000000000 means errors :) */
-	if (pinfo->fd->abs_ts.nsecs >= 1000000000) {
-		proto_item_append_text(item, " (Error)");
-		/* return; */ /* dissect what we have */
-	}
-
-	len = tvb_length(tvb);
-	if (!pinfo->fd->flags.visited) {
-		if (len) {
-			tvbuff_t *cloned_tvb = tvb_clone(tvb);
-
-			if (!file_tvbs) {
-				file_tvbs = cloned_tvb;
-				whole_tvb = tvb_new_composite();
-			} else
-				tvb_add_to_chain(file_tvbs, cloned_tvb);
-
-			tvb_composite_append(whole_tvb, cloned_tvb);
-		} else
-			tvb_composite_finalize(whole_tvb);
-	}
-
-	/* End of file? */
-	if (!len && whole_tvb) {
-		/*
-		 * Here we're doing some trick.
-		 *
-		 * We don't want to call dissectors with composite tvb, cause dissectors can create subsets or real data child
-		 * on it, which would append to whole_tvb chain and would be freed only in mime_encap_init.
-		 *
-		 * So we create some tvb which pass all calls to whole_tvb, but chain with tvb (which is freed in dissection cleanup)
-		 */
-		tvbuff_t *tmp_tvb = tvb_new_chain(tvb, whole_tvb);
-
-		proto_item_append_text(item, " (Final)");
-
-		add_new_data_source(pinfo, tmp_tvb, "Whole file");
-
-		if (!dissector_try_heuristic(heur_subdissector_list, tmp_tvb, pinfo, tree, NULL)) {
-			proto_item_append_text(item, " (Unhandled)");
-			call_dissector(data_handle, tmp_tvb, pinfo, tree);
-		}
+	if (!dissector_try_heuristic(heur_subdissector_list, tvb, pinfo, tree, NULL)) {
+		proto_item_append_text(item, " (Unhandled)");
+		call_dissector(data_handle, tvb, pinfo, tree);
 	}
 }
 
@@ -113,7 +59,6 @@ proto_register_mime_encap(void)
 	proto_mime_encap = proto_register_protocol("MIME file", "MIME_FILE", "mime_dlt");
 
 	register_dissector("mime_dlt", dissect_mime_encap, proto_mime_encap);
-	register_init_routine(mime_encap_init);
 	register_heur_dissector_list("wtap_file", &heur_subdissector_list);
 }
 
