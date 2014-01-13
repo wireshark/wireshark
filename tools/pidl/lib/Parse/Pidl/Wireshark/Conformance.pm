a2979508d011e897f9867997711e4496b1905ac7
Guy Harris <guy@alum.mit.edu>
2014-01-13 21:05:44 +0000

    Add HEADER START and HEADER END, similar to CODE START and CODE END, to allow stuff to be put into the .h file; that way, if you put helper functions into the .c file, for use by dissectors other than the one being generated, you can put declarations for them into the .h file.
    Do some checks for mis-nesting of CODE and HEADER blocks.
    
    Clean up the "this is generated code" header.
    
    svn path=/trunk/; revision=54720

--- tools/pidl/lib/Parse/Pidl/Wireshark/Conformance.pm
+++ tools/pidl/lib/Parse/Pidl/Wireshark/Conformance.pm
@@ -396,6 +396,7 @@ sub ReadConformanceFH($$$)
 	my ($fh,$data,$f) = @_;
 
 	my $incodeblock = 0;
+	my $inheaderblock = 0;
 
 	my $ln = 0;
 
@@ -407,9 +408,27 @@ sub ReadConformanceFH($$$)
 		s/[\r\n]//g;
 
 		if ($_ eq "CODE START") {
+			if ($incodeblock) {
+				warning({ FILE => $f, LINE => $ln }, 
+					"CODE START inside CODE section");
+			}
+			if ($inheaderblock) {
+				error({ FILE => $f, LINE => $ln }, 
+					"CODE START inside HEADER section");
+				return undef;
+			}
 			$incodeblock = 1;
 			next;
-		} elsif ($incodeblock and $_ eq "CODE END") {
+		} elsif ($_ eq "CODE END") {
+			if (!$incodeblock) {
+				warning({ FILE => $f, LINE => $ln }, 
+					"CODE END outside CODE section");
+			}
+			if ($inheaderblock) {
+				error({ FILE => $f, LINE => $ln }, 
+					"CODE END inside HEADER section");
+				return undef;
+			}
 			$incodeblock = 0;
 			next;
 		} elsif ($incodeblock) {
@@ -417,6 +436,37 @@ sub ReadConformanceFH($$$)
 				$data->{override}.="$_\n";
 			} else {
 				$data->{override} = "$_\n";
+			}
+			next;
+		} elsif ($_ eq "HEADER START") {
+			if ($inheaderblock) {
+				warning({ FILE => $f, LINE => $ln }, 
+					"HEADER START inside HEADER section");
+			}
+			if ($incodeblock) {
+				error({ FILE => $f, LINE => $ln }, 
+					"HEADER START inside CODE section");
+				return undef;
+			}
+			$inheaderblock = 1;
+			next;
+		} elsif ($_ eq "HEADER END") {
+			if (!$inheaderblock) {
+				warning({ FILE => $f, LINE => $ln }, 
+					"HEADER END outside HEADER section");
+			}
+			if ($incodeblock) {
+				error({ FILE => $f, LINE => $ln }, 
+					"CODE END inside HEADER section");
+				return undef;
+			}
+			$inheaderblock = 0;
+			next;
+		} elsif ($inheaderblock) {
+			if (exists $data->{header}) {
+				$data->{header}.="$_\n";
+			} else {
+				$data->{header} = "$_\n";
 			}
 			next;
 		}
