205162061e37d63417b003e43ecc10e94453bdb0
Guy Harris <guy@alum.mit.edu>
2014-01-14 01:29:16 +0000

    In a source file that defines external functions, the header file that declares the functions must be included, in order to make sure the declarations match the function signature.  Make it so.
    svn path=/trunk/; revision=54745

--- ui/qt/wireshark_application.cpp
+++ ui/qt/wireshark_application.cpp
@@ -45,6 +45,8 @@
 
 #include "ui/capture_globals.h"
 #include "ui/software_update.h"
+#include "ui/last_open_dir.h"
+#include "ui/recent_utils.h"
 
 #ifdef _WIN32
 #  include "ui/win32/console_win32.h"
