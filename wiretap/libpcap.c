wireshark/wireshark

commit 3f1f6305706b53618765897a32f61b2d8c151695

Don't limit the on-the-wire length of packets to 64KB, there are larger packets out there (especially over USB) and we should be able to load them as long as they are snapped to a sane length.
Also validate that packets do not specify a snapshot length larger than the one
in the file header, though only make it a warning, as this is not necessarily a
fatally corrupt packet.

https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=8808

svn path=/trunk/; revision=49999

wiretap/libpcap.c
@@ -765,21 +765,8 @@ static int libpcap_read_header(wtap *wth, FILE_T fh, int *err, gchar **err_info,
 		return -1;
 	}
 
-	if (hdr->hdr.orig_len > WTAP_MAX_PACKET_SIZE) {
-		/*
-		 * Probably a corrupt capture file; return an error,
-		 * so that our caller doesn't blow up trying to
-		 * cope with a huge "real" packet length, and so that
-		 * the code to try to guess what type of libpcap file
-		 * this is can tell when it's not the type we're guessing
-		 * it is.
-		 */
-		*err = WTAP_ERR_BAD_FILE;
-		if (err_info != NULL) {
-			*err_info = g_strdup_printf("pcap: File has %u-byte packet, bigger than maximum of %u",
-			    hdr->hdr.orig_len, WTAP_MAX_PACKET_SIZE);
-		}
-		return -1;
+	if (hdr->hdr.incl_len > wth->snapshot_length) {
+		g_warning("pcap: File has packet larger than file's snapshot length.");
 	}
 
 	return bytes_read;
@@ -929,7 +916,7 @@ static gboolean libpcap_dump(wtap_dumper *wdh,
 	rec_hdr.hdr.incl_len = phdr->caplen + phdrsize;
 	rec_hdr.hdr.orig_len = phdr->len + phdrsize;
 
-	if (rec_hdr.hdr.incl_len > WTAP_MAX_PACKET_SIZE || rec_hdr.hdr.orig_len > WTAP_MAX_PACKET_SIZE) {
+	if (rec_hdr.hdr.incl_len > WTAP_MAX_PACKET_SIZE) {
 		*err = WTAP_ERR_BAD_FILE;
 		return FALSE;
 	}

commit dae86605b61bbbabd54749e6bde7c0c10b031ca8

Reject pcap files that claim on-the-wire packet sizes > 64MB. This fixes many heuristic cases broken in r49999 when we permitted packets > 64KB, since that relaxed so severely the definition of a valid packet header.
64MB is an arbitrary and perhaps suboptimal number, but it seems to do the right
thing in all the examples I have handy.

Fixes https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9634

svn path=/trunk/; revision=54812

wiretap/libpcap.c
@@ -764,6 +764,24 @@ static int libpcap_read_header(wtap *wth, FILE_T fh, int *err, gchar **err_info,
 		return -1;
 	}
 
+        if (hdr->hdr.orig_len > 64*1024*1024) {
+		/*
+                 * In theory I guess the on-the-wire packet size can be
+                 * arbitrarily large, and it can certainly be larger than the
+                 * 64KB which bounds the snapshot size, but any file claiming
+                 * 64MB in a single packet is *probably* corrupt, and makes the
+                 * heuristics much more reliable. See, for example,
+                 * https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=9634
+                 * (64MB is an arbitrary size at this point)
+		 */
+		*err = WTAP_ERR_BAD_FILE;
+		if (err_info != NULL) {
+			*err_info = g_strdup_printf("pcap: File claims packet was %u bytes on the wire",
+			    hdr->hdr.orig_len);
+		}
+		return -1;
+        }
+
         /* Disabling because this is not a fatal error, and packets that have
          * one such packet probably have thousands. For discussion, see
          * https://www.wireshark.org/lists/wireshark-dev/201307/msg00076.html