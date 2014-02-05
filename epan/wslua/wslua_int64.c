32e4d901fcd3125c4646d8f3d3003e1f53a98b11
Evan Huus <eapache@gmail.com>
2014-02-05 15:14:04 -0500

    Two fixes for license headers
    - packet-bencode.c had the wrong FSF address
    - wslua_int64.c had the license as a footer instead of a header
    
    Change-Id: I71204b36a1034af72874d6fe87929c31c9ff03df
    Reviewed-on: https://code.wireshark.org/review/123
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    Reviewed-by: Hadriel Kaplan <hadrielk@yahoo.com>
    Reviewed-by: Evan Huus <eapache@gmail.com>
    Tested-by: Evan Huus <eapache@gmail.com>

--- epan/wslua/wslua_int64.c
+++ epan/wslua/wslua_int64.c
@@ -1,9 +1,41 @@
-
 /*
  * A Lua userdata object for 64-bit signed/unsigned integers.
  * $Id: wslua_int64.c,v 1.0 2013/03/31 02:08:32 hadrielk Exp $
- * See Copyright Notice at the end of this file
- */
+ *
+ * I, Hadriel Kaplan, the author of wslua_int6464.c, wish to put it in
+ * the Public Domain.  That is not universally accepted, however,
+ * so you may license it under the FreeBSD License instead, which is an open
+ * source license approved for GPL use as well as commercial etc.
+ * It's even less restrictive than the MIT license, because it requires
+ * no attribution anywhere - I don't *want* attribution.
+
+Copyright (C) 2013 Hadriel Kaplan <hadrielk@yahoo.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+*/
 
 #include "config.h"
 
@@ -873,39 +905,3 @@ LUALIB_API int UInt64_register(lua_State* L) {
     WSLUA_REGISTER_CLASS(UInt64);
     return 0;
 }
-
-
-/* I, Hadriel Kaplan, the author of wslua_int6464.c, wish to put it in
- * the Public Domain.  That is not universally accepted, however,
- * so you may license it under the FreeBSD License instead, which is an open
- * source license approved for GPL use as well as commercial etc.
- * It's even less restrictive than the MIT license, because it requires 
- * no attribution anywhere - I don't *want* attribution.
- *
-Copyright (C) 2013 Hadriel Kaplan <hadrielk@yahoo.com>
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met: 
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer. 
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution. 
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-The views and conclusions contained in the software and documentation are those
-of the authors and should not be interpreted as representing official policies, 
-either expressed or implied, of the FreeBSD Project.
-*/
