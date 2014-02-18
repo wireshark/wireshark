622d259afc97c611000c90a827c795b01e56e742
Martin Kaiser <wireshark@kaiser.cx>
2014-02-18 22:48:16 +0100

    the gif version string is 7bit ASCII
    Change-Id: I1e827ad4d2cf64411c5a87f4710235dc4d6efc35
    Reviewed-on: https://code.wireshark.org/review/250
    Reviewed-by: Martin Kaiser <wireshark@kaiser.cx>

--- epan/dissectors/file-gif.c
+++ epan/dissectors/file-gif.c
@@ -329,7 +329,7 @@ dissect_gif(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
     if (tvb_len < 20)
         return 0;
 
-    str = tvb_get_string(wmem_packet_scope(), tvb, 0, 6);
+    str = tvb_get_string_enc(wmem_packet_scope(), tvb, 0, 6, ENC_ASCII|ENC_NA);
     /* Check whether we're processing a GIF object */
     /* see http://www.w3.org/Graphics/GIF/spec-gif89a.txt section 17 */
     if (strcmp(str, "GIF87a") == 0) {
