9815a25971dba21b36145d5798164d3acbc8f9d0
Guy Harris <guy@alum.mit.edu>
2014-02-25 01:07:19 -0800

    Fix crash when exporting dissection summaries to a CSV file.
    The while loop in question ends with tmp_str being NULL; the next loop
    processes the same string, so you have to set tmp_str to csv_str before
    looping.
    
    Fix some comments while we're at it.
    
    Change-Id: I69dd7dc276e0cb11226eceee7921be87e3cc7534
    Reviewed-on: https://code.wireshark.org/review/352
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/print.c
+++ epan/print.c
@@ -684,17 +684,18 @@ static gchar *csv_massage_str(const gchar *source, const gchar *exceptions)
     gchar *csv_str;
     gchar *tmp_str;
 
-    /* In general, our output for any field can contain unicode characters,
+    /* In general, our output for any field can contain Unicode characters,
        so g_strescape (which escapes any non-ASCII) is the wrong thing to do.
        Unfortunately glib doesn't appear to provide g_unicode_strescape()... */
     csv_str = g_strescape(source, exceptions);
     tmp_str = csv_str;
-    /* Locate the UTF-8 righ arrow character and replace it by an ASCII equivalent */
+    /* Locate the UTF-8 right arrow character and replace it by an ASCII equivalent */
     while ( (tmp_str = strstr(tmp_str, "\xe2\x86\x92")) != NULL ) {
         tmp_str[0] = ' ';
         tmp_str[1] = '>';
         tmp_str[2] = ' ';
     }
+    tmp_str = csv_str;
     while ( (tmp_str = strstr(tmp_str, "\\\"")) != NULL )
         *tmp_str = '\"';
     return csv_str;
