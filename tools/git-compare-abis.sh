6d54ab69ffd8f6f57dbdc89036de8a4485b9dbcd
Gerald Combs <gerald@wireshark.org>
2014-02-28 17:21:29 -0800

    Copy git-compare-abis from ge635d3f / master-1.10.
        Have git-compare-abis build in a sub-sub-subdirectory.
    
        We have a lot of relative includes which apparently weren't updated
        correctly when we moved the gtk directory to ui/gtk. For example,
        ui/gtk/about_dlg.c has
    
        The Autotools Makefile adds "-I../.." to the include path, which means
        we end up including the current, top-level version_info.h instead of the
        one in the compare-abis build directory.
    
    Change-Id: Ie9d35057fe14bcb75736b54b7efb5f0a8deda39a
    Reviewed-on: https://code.wireshark.org/review/437
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- tools/git-compare-abis.sh
+++ tools/git-compare-abis.sh
@@ -26,16 +26,14 @@ bash -c "$BUILD_COMMAND"
 cd `git rev-parse --show-toplevel`
 # we are at top level
 
-# Stable branches with releases
+# e.g. "v1.10.6-rc1"
 LAST_TAG=`git describe --tags --abbrev=0`
-LAST_TAG_DIR=$LAST_TAG
+# Add extra directory levels to keep '-I../..' + '#include "../some_file.h"
+# from matching top-level files.
+LAST_TAG_DIR="compare-abis/build/$LAST_TAG"
 
-# Unstable branches, e.g. master don't have usable tags. Use a commit instead.
-#LAST_TAG=162f555720e480a405d9ba762124c984f74197e9
-#LAST_TAG_DIR=master-1.8
-
-rm -rf $LAST_TAG_DIR
-mkdir $LAST_TAG_DIR
+rm -rf compare-abis
+mkdir -p $LAST_TAG_DIR
 git archive $LAST_TAG | tar -x -C $LAST_TAG_DIR
 
 # build latest tag
