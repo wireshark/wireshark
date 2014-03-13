08bf1a4ae1c131486177e7e34b0c70b091e0613b
Hadriel Kaplan <hadrielk@yahoo.com>
2014-03-13 02:45:03 -0400

    Add support for Aruba ERM Radio-Format
    This adds support for a variant of the current Aruba ERM format,
    a new format that provides rdio information. This addresses
    enhancment bug 9880.
    
    Change-Id: Ia38ff09d9f814193bdc544466dbd005123771262
    Reviewed-on: https://code.wireshark.org/review/629
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-aruba-erm.c
+++ epan/dissectors/packet-aruba-erm.c
@@ -4,6 +4,8 @@
  *
  * Copyright 2010  Alexis La Goutte <alexis.lagoutte at gmail dot com>
  *
+ * ERM Radio-Format added by Hadriel Kaplan
+ *
  * Wireshark - Network traffic analyzer
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
@@ -37,6 +39,22 @@
  * Following with 802.11 header
  */
 
+/*
+ * Format:
+ *  The ERM Radio-Format has the above header, plus more, like this:
+ *
+ *  struct radio_pcap_hdr {
+ *   struct timeval ts;
+ *   __u32  capture_length;
+ *   __u32  frame_length;
+ *   __u16  rate_per_half_mhz;
+ *   __u8   channel;
+ *   __u8   signal_percent;
+ *  } __attribute__ ((packed));
+ *
+ * Following with 802.11 header
+ */
+
 #include "config.h"
 
 #include <glib.h>
@@ -45,33 +63,41 @@
 
 #define PROTO_SHORT_NAME "ARUBA_ERM"
 #define PROTO_LONG_NAME  "ARUBA encapsulated remote mirroring"
+#define PROTO_RADIO_SHORT_NAME "ARUBA_ERM_RADIO_FORMAT"
+#define PROTO_RADIO_LONG_NAME  "ARUBA encapsulated remote mirroring - radio format"
 
 void proto_register_aruba_erm(void);
 void proto_reg_handoff_aruba_erm(void);
+void proto_reg_handoff_aruba_erm_radio(void);
 
 static range_t *global_aruba_erm_port_range;
+static range_t *global_aruba_erm_radio_port_range;
 
 static int  proto_aruba_erm       = -1;
 
-static int  hf_aruba_erm_time     = -1;
-static int  hf_aruba_erm_incl_len = -1;
-static int  hf_aruba_erm_orig_len = -1;
+static int  hf_aruba_erm_time             = -1;
+static int  hf_aruba_erm_incl_len         = -1;
+static int  hf_aruba_erm_orig_len         = -1;
+static int  hf_aruba_erm_data_rate        = -1;
+static int  hf_aruba_erm_channel          = -1;
+static int  hf_aruba_erm_signal_strength  = -1;
 
-static gint ett_aruba_erm         = -1;
+static gint ett_aruba_erm = -1;
 
 static dissector_handle_t aruba_erm_handle;
+static dissector_handle_t aruba_erm_radio_handle;
 static dissector_handle_t ieee80211_handle;
 
 static void
-dissect_aruba_erm(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+dissect_aruba_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, gboolean is_radio)
 {
     proto_item *ti;
     proto_tree *aruba_erm_tree;
     tvbuff_t   *eth_tvb;
     nstime_t ts;
-
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, PROTO_SHORT_NAME);
-    col_set_str(pinfo->cinfo, COL_INFO, PROTO_SHORT_NAME ":");
+    int offset = 16;
+    guint16 data_rate;
+    guint8 signal_strength;
 
     if (tree) {
         ti = proto_tree_add_item(tree, proto_aruba_erm, tvb, 0, -1, ENC_NA);
@@ -82,10 +108,46 @@ dissect_aruba_erm(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         proto_tree_add_time(aruba_erm_tree, hf_aruba_erm_time, tvb, 0, 8,&ts);
         proto_tree_add_item(aruba_erm_tree, hf_aruba_erm_incl_len, tvb, 8, 4, ENC_BIG_ENDIAN);
         proto_tree_add_item(aruba_erm_tree, hf_aruba_erm_orig_len, tvb, 12, 4, ENC_BIG_ENDIAN);
+
+        if (is_radio) {
+            data_rate = tvb_get_ntohs(tvb, 16);
+            proto_tree_add_uint_format(aruba_erm_tree, hf_aruba_erm_data_rate, tvb, 16, 2,
+                                         (guint32)data_rate,
+                                         "Data Rate: %u.%u Mb/s",
+                                         data_rate / 2,
+                                         data_rate & 1 ? 5 : 0);
+
+            proto_tree_add_item(aruba_erm_tree, hf_aruba_erm_channel, tvb, 18, 1, ENC_NA);
+
+            signal_strength = tvb_get_guint8(tvb, 19);
+            proto_tree_add_uint_format(aruba_erm_tree, hf_aruba_erm_signal_strength, tvb, 19, 1,
+                                         (guint32)signal_strength,
+                                         "Signal Strength: %u%%",
+                                         signal_strength);
+            offset += 4;
+        }
     }
 
-    eth_tvb = tvb_new_subset_remaining(tvb, 16);
+    eth_tvb = tvb_new_subset_remaining(tvb, offset);
     call_dissector(ieee80211_handle, eth_tvb, pinfo, tree);
+}
+
+static void
+dissect_aruba_erm(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, PROTO_SHORT_NAME);
+    col_set_str(pinfo->cinfo, COL_INFO, PROTO_SHORT_NAME ":");
+
+    dissect_aruba_common(tvb, pinfo, tree, FALSE);
+}
+
+static void
+dissect_aruba_erm_radio(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+{
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, PROTO_RADIO_SHORT_NAME);
+    col_set_str(pinfo->cinfo, COL_INFO, PROTO_RADIO_SHORT_NAME ":");
+
+    dissect_aruba_common(tvb, pinfo, tree, TRUE);
 }
 
 void
@@ -103,8 +165,18 @@ proto_register_aruba_erm(void)
         { &hf_aruba_erm_orig_len,
           { "Packet Length", "aruba_erm.orig_len", FT_UINT32, BASE_DEC, NULL,
             0x00, NULL, HFILL }},
+        { &hf_aruba_erm_data_rate,
+          { "Data Rate", "aruba_erm.data_rate", FT_UINT16, BASE_DEC, NULL,
+            0x00, "Data rate (1/2 Mb/s)", HFILL }},
+        { &hf_aruba_erm_channel,
+          { "Channel", "aruba_erm.channel", FT_UINT8, BASE_DEC, NULL,
+            0x00, "802.11 channel number that this frame was sent/received on", HFILL }},
+        { &hf_aruba_erm_signal_strength,
+          { "Signal Strength", "aruba_erm.signal_strength", FT_UINT8, BASE_DEC, NULL,
+            0x00, "Signal strength (Percentage)", HFILL }},
     };
 
+    /* both formats share the same tree */
     static gint *ett[] = {
         &ett_aruba_erm,
     };
@@ -114,12 +186,21 @@ proto_register_aruba_erm(void)
     proto_aruba_erm = proto_register_protocol(PROTO_LONG_NAME, PROTO_SHORT_NAME, "aruba_erm");
 
     range_convert_str (&global_aruba_erm_port_range, "0", MAX_UDP_PORT);
+    range_convert_str (&global_aruba_erm_radio_port_range, "0", MAX_UDP_PORT);
+
     aruba_erm_module = prefs_register_protocol(proto_aruba_erm, proto_reg_handoff_aruba_erm);
+
     prefs_register_range_preference(aruba_erm_module, "udp.ports", "ARUBA_ERM UDP Port numbers",
-                                    "Set the UDP port numbers (by default is range 5555 to 5560) used for ARUBA"
+                                    "Set the UDP port numbers (typically the range 5555 to 5560) used for ARUBA"
                                     " encapsulated remote mirroring frames;\n"
                                     "0 (default) means that the ARUBA_ERM dissector is not active\n",
                                     &global_aruba_erm_port_range, MAX_UDP_PORT);
+
+    prefs_register_range_preference(aruba_erm_module, "radio.udp.ports", "ARUBA_ERM_RADIO_FORMAT UDP Port numbers",
+                                    "Set the UDP port numbers (typically the range 5555 to 5560) used for ARUBA"
+                                    " encapsulated remote mirroring frames with radio format;\n"
+                                    "0 (default) means that the ARUBA_ERM_RADIO_FORMAT dissector is not active\n",
+                                    &global_aruba_erm_radio_port_range, MAX_UDP_PORT);
 
     proto_register_field_array(proto_aruba_erm, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
@@ -129,20 +210,26 @@ void
 proto_reg_handoff_aruba_erm(void)
 {
     static range_t *aruba_erm_port_range;
+    static range_t *aruba_erm_radio_port_range;
     static gboolean initialized = FALSE;
 
     if (!initialized) {
         ieee80211_handle = find_dissector("wlan");
         aruba_erm_handle = create_dissector_handle(dissect_aruba_erm, proto_aruba_erm);
+        aruba_erm_radio_handle = create_dissector_handle(dissect_aruba_erm_radio, proto_aruba_erm);
         initialized = TRUE;
     } else {
         dissector_delete_uint_range("udp.port", aruba_erm_port_range, aruba_erm_handle);
+        dissector_delete_uint_range("udp.port", aruba_erm_radio_port_range, aruba_erm_radio_handle);
         g_free(aruba_erm_port_range);
+        g_free(aruba_erm_radio_port_range);
     }
 
     aruba_erm_port_range = range_copy(global_aruba_erm_port_range);
+    aruba_erm_radio_port_range = range_copy(global_aruba_erm_radio_port_range);
 
     dissector_add_uint_range("udp.port", aruba_erm_port_range, aruba_erm_handle);
+    dissector_add_uint_range("udp.port", aruba_erm_radio_port_range, aruba_erm_radio_handle);
 }
 
 /*
