6e21a4bfcd9e333f159c4b59ad0ddac0c132b2a1
AndersBroman <a.broman@bredband.net>
2014-05-01 11:10:22 +0200

    Try to fix warning C4244: '-=' : conversion from '__int64' to 'int', possible loss of data
    Change-Id: I8cf74090e507f21eebfb40d72a3630d9f9bb0390
    Reviewed-on: https://code.wireshark.org/review/1451
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/to_str.c
+++ epan/to_str.c
@@ -1196,7 +1196,7 @@ uint_to_str_back_len(char *ptr, guint32 value, int len)
 	new_ptr = uint_to_str_back(ptr, value);
 
 	/* substract from len number of generated characters */
-	len -= (ptr - new_ptr);
+	len -= (int)(ptr - new_ptr);
 
 	/* pad remaining with '0' */
 	while (len > 0)
