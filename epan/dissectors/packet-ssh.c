c27ad662f278d738435c9426c36f2af913ae8410
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-05-02 16:18:49 +0200

    Add modelines info
    Change-Id: I82f841e0ee9e00c69894d8c4c67f581278c87546
    Reviewed-on: https://code.wireshark.org/review/1469
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-ssh.c
+++ epan/dissectors/packet-ssh.c
@@ -1381,3 +1381,16 @@ proto_reg_handoff_ssh(void)
 	dissector_add_uint("sctp.port", SCTP_PORT_SSH, ssh_handle);
 	dissector_add_uint("sctp.ppi", SSH_PAYLOAD_PROTOCOL_ID, ssh_handle);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
