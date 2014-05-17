wireshark/wireshark

commit 3aee917058fb46b2e86d750766001c4db214fc78

wiretap: remove unused code, drop number_of_interfaces
While investigating an ASAN issue (fixed in
commit dcdd076ab0965c346efe90051678ba790eaf7a02), I got greatly confused
by three different types having the same "interface_data" field name:

 * pcapng_t *pn stores an array of interface_data_t objects.
 * wtap *wth stores an array of wtapng_if_descr_t objects.
 * pcapng_dump_t should store an array of interface_data_t objects.

pcapng_dump_t and friends are unused since
commit c7f1a431d23e17a15777652b1252e139f182b0e6, so drop it.

To fix the confusion, rename the interface_data_t type to
interface_info_t type and use the local variable "iface_info"
everywhere. Rename interface_data of pcapng_t to "interfaces" and
add a comment what this exactly means (interfaces listed in the capture
file).

Drop the number_of_interfaces field for interfaces as the array
length is already available from GArray. Now interface_data is always
initialized for wth (which also gets copied to idb).

s/int/guint/g and replace cast at some places.

There are no regressions for the in-tree test suite.

Change-Id: I2d5985c9f1e43f8230dbb4a73bd1e243c4858170
Reviewed-on: https://code.wireshark.org/review/1656
Reviewed-by: Evan Huus <eapache@gmail.com>
Tested-by: Evan Huus <eapache@gmail.com>
Reviewed-by: Michael Mann <mmann78@netscape.net>

wiretap/file_access.c
@@ -798,6 +798,11 @@ wtap* wtap_open_offline(const char *filename, unsigned int type, int *err, char 
 	wth->priv = NULL;
 	wth->wslua_data = NULL;
 
+	/* Initialize the array containing a list of interfaces. pcapng_open and
+	 * erf_open needs this (and libpcap_open for ERF encapsulation types).
+	 * Always initing it here saves checking for a NULL ptr later. */
+	wth->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
+
 	if (wth->random_fh) {
 		wth->fast_seek = g_ptr_array_new();
 
@@ -1021,8 +1026,6 @@ success:
 		descr.if_fcslen = -1;
 		descr.num_stat_entries = 0;          /* Number of ISB:s */
 		descr.interface_statistics = NULL;
-		wth->number_of_interfaces= 1;
-		wth->interface_data= g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 		g_array_append_val(wth->interface_data, descr);
 
 	}
@@ -1984,8 +1987,7 @@ wtap_dump_init_dumper(int file_type_subtype, int encap, int snaplen, gboolean co
 	/* Set Section Header Block data */
 	wdh->shb_hdr = shb_hdr;
 	/* Set Interface Description Block data */
-	if ((idb_inf != NULL) && (idb_inf->number_of_interfaces > 0)) {
-		wdh->number_of_interfaces = idb_inf->number_of_interfaces;
+	if ((idb_inf != NULL) && (idb_inf->interface_data->len > 0)) {
 		wdh->interface_data = idb_inf->interface_data;
 	} else {
 		wtapng_if_descr_t descr;
@@ -2006,8 +2008,6 @@ wtap_dump_init_dumper(int file_type_subtype, int encap, int snaplen, gboolean co
 		descr.if_fcslen = -1;
 		descr.num_stat_entries = 0;          /* Number of ISB:s */
 		descr.interface_statistics = NULL;
-		wdh->number_of_interfaces= 1;
-		wdh->interface_data= g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 		g_array_append_val(wdh->interface_data, descr);
 	}
 	return wdh;

cfile.c
@@ -38,7 +38,7 @@ cap_file_get_interface_name(void *data, guint32 interface_id)
 
   idb_info = wtap_file_get_idb_info(cf->wth);
 
-  if (interface_id < idb_info->number_of_interfaces)
+  if (interface_id < idb_info->interface_data->len)
     wtapng_if_descr = &g_array_index(idb_info->interface_data, wtapng_if_descr_t, interface_id);
 
   g_free(idb_info);

file.c
@@ -1382,9 +1382,9 @@ cf_merge_files(char **out_filenamep, int in_file_count,
 
     /* create fake IDB info */
     idb_inf = g_new(wtapng_iface_descriptions_t,1);
-    idb_inf->number_of_interfaces = in_file_count; /* TODO make this the number of DIFFERENT encapsulation types
-                                                    * check that snaplength is the same too?
-                                                    */
+    /* TODO make this the number of DIFFERENT encapsulation types
+     * check that snaplength is the same too?
+     */
     idb_inf->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 
     for (i = 0; i < in_file_count; i++) {

summary.c
@@ -172,7 +172,7 @@ summary_fill_in(capture_file *cf, summary_tally *st)
 
   st->ifaces  = g_array_new(FALSE, FALSE, sizeof(iface_options));
   idb_info = wtap_file_get_idb_info(cf->wth);
-  for (i = 0; i < idb_info->number_of_interfaces; i++) {
+  for (i = 0; i < idb_info->interface_data->len; i++) {
     wtapng_if_descr = g_array_index(idb_info->interface_data, wtapng_if_descr_t, i);
     iface.cfilter = g_strdup(wtapng_if_descr.if_filter_str);
     iface.name = g_strdup(wtapng_if_descr.if_name);

tshark.c
@@ -3080,7 +3080,7 @@ load_cap_file(capture_file *cf, char *save_file, int out_file_type,
   shb_hdr = wtap_file_get_shb_info(cf->wth);
   idb_inf = wtap_file_get_idb_info(cf->wth);
 #ifdef PCAP_NG_DEFAULT
-  if (idb_inf->number_of_interfaces > 1) {
+  if (idb_inf->interface_data->len > 1) {
     linktype = WTAP_ENCAP_PER_PACKET;
   } else {
     linktype = wtap_file_encap(cf->wth);

ui/gtk/file_import_dlg.c
@@ -493,7 +493,6 @@ file_import_open(text_import_info_t *info)
 
     /* Create fake IDB info */
     idb_inf = g_new(wtapng_iface_descriptions_t,1);
-    idb_inf->number_of_interfaces = 1;
     idb_inf->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 
     /* create the fake interface data */

ui/tap_export_pdu.c
@@ -120,7 +120,6 @@ exp_pdu_file_open(exp_pdu_t *exp_pdu_tap_data)
 
     /* Create fake IDB info */
     idb_inf = g_new(wtapng_iface_descriptions_t,1);
-    idb_inf->number_of_interfaces = 1;
     idb_inf->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 
     /* create the fake interface data */

wiretap/erf.c
@@ -726,10 +726,6 @@ int erf_populate_interfaces(wtap *wth)
   if (!wth)
     return -1;
 
-  if (!wth->interface_data) {
-    wth->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
-  }
-
   memset(&int_data, 0, sizeof(int_data)); /* Zero all fields */
 
   int_data.wtap_encap = WTAP_ENCAP_ERF;
@@ -762,7 +758,6 @@ int erf_populate_interfaces(wtap *wth)
     int_data.if_description = g_strdup_printf("ERF Interface Id %d (Port %c)", i, 'A'+i);
 
     g_array_append_val(wth->interface_data, int_data);
-    wth->number_of_interfaces++;
   }
 
   return 0;

commit cea149aa8916dd9c8b592bb53e5773b3ec141ceb

wiretap: fix regression, missing initialization
wtap_dump_open() allocates an empty wtap_dumper struct such that
interface_data is not initialized. Fix this by adding the member back.

Regression from 3aee917058fb46b2e86d750766001c4db214fc78 ("wiretap:
remove unused code, drop number_of_interfaces").

Bug: 10113
Change-Id: Ia6259bf50b25d5e7aa837b0fb7396b07d5d3c72c
Reviewed-on: https://code.wireshark.org/review/1672
Reviewed-by: Evan Huus <eapache@gmail.com>

wiretap/file_access.c
@@ -2008,6 +2008,7 @@ wtap_dump_init_dumper(int file_type_subtype, int encap, int snaplen, gboolean co
 		descr.if_fcslen = -1;
 		descr.num_stat_entries = 0;          /* Number of ISB:s */
 		descr.interface_statistics = NULL;
+		wdh->interface_data = g_array_new(FALSE, FALSE, sizeof(wtapng_if_descr_t));
 		g_array_append_val(wdh->interface_data, descr);
 	}
 	return wdh;