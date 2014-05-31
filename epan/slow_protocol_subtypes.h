5672da813275bc90aea1fbbb28248617d1eabb31
Guy Harris <guy@alum.mit.edu>
2014-05-30 23:08:16 -0700

    Add a copyright/idempotency wrapper.
    Change-Id: I13928c78fc9e2c15adcc9d028dd07058800f402b
    Reviewed-on: https://code.wireshark.org/review/1893
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/slow_protocol_subtypes.h
+++ epan/slow_protocol_subtypes.h
@@ -1,6 +1,37 @@
+/* slow_protocol_subtypes.h
+ * Defines subtypes for 802.3 "slow protocols"
+ *
+ * Copyright 2002 Steve Housley <steve_housley@3com.com>
+ * Copyright 2005 Dominique Bastien <dbastien@accedian.com>
+ * Copyright 2009 Artem Tamazov <artem.tamazov@telllabs.com>
+ * Copyright 2010 Roberto Morro <roberto.morro[AT]tilab.com>
+ * Copyright 2014 Philip Rosenberg-Watt <p.rosenberg-watt[at]cablelabs.com.>
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __SLOW_PROTOCOL_SUBTYPES_H__
+#define __SLOW_PROTOCOL_SUBTYPES_H__
+
 #define LACP_SUBTYPE                    0x1
 #define MARKER_SUBTYPE                  0x2
 #define OAM_SUBTYPE                     0x3
 #define OSSP_SUBTYPE                    0xa /* IEEE 802.3 Annex 57A*/
 
-
+#endif /* __SLOW_PROTOCOL_SUBTYPES_H__ */
