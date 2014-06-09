26ad82454810ec9421a3f37c72f1fd8c6bc135ec
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-06-09 12:43:40 +0200

    Add Modelines info
    Change-Id: If3e77cbdfdeab75766984b10b3356b8343100602
    Reviewed-on: https://code.wireshark.org/review/2065
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- epan/dissectors/packet-eigrp.c
+++ epan/dissectors/packet-eigrp.c
@@ -3366,3 +3366,16 @@ proto_reg_handoff_eigrp(void)
     dissector_add_uint("ddp.type", DDP_EIGRP, eigrp_handle);
     dissector_add_uint("ipx.socket", IPX_SOCKET_EIGRP, eigrp_handle);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
