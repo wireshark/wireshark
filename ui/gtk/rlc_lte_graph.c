f627ee867415478ab58edca8ae6fd31f40d8dc6f
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-06-09 12:43:34 +0200

    Add Modelines info
    Change-Id: I2364104145518d484c2e5fb0ae5d5d80cce0982d
    Reviewed-on: https://code.wireshark.org/review/2064
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- ui/gtk/rlc_lte_graph.c
+++ ui/gtk/rlc_lte_graph.c
@@ -3007,3 +3007,16 @@ static int rint(double x)
     return i;
 }
 #endif
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
