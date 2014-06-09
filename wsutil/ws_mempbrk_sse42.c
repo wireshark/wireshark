c9621e2befaba0543b5351f4fece3f7ef5c52c6b
AndersBroman <a.broman@bredband.net>
2014-06-09 21:28:32 +0200

    We must include config.h to have HAVE_SSE42
    Change-Id: I2b882b05b2864edbfef0ea03bc086efeaac84ad2
    Reviewed-on: https://code.wireshark.org/review/2086
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- wsutil/ws_mempbrk_sse42.c
+++ wsutil/ws_mempbrk_sse42.c
@@ -17,9 +17,10 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#ifdef HAVE_SSE42
 
 #include "config.h"
+
+#ifdef HAVE_SSE42
 
 #include <glib.h>
 
