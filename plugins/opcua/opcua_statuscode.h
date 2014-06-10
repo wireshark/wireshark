f399a637d6b25b2d231b9a8c7171301a55a8ef6f
Hannes Mezger <hannes.mezger@ascolab.com>
2014-06-10 13:36:06 +0200

    Display text representation of status codes
    Adds a table containing all known StatusCodes and displays them if found.
    The list of StatusCodes is in a separate file for easy generating of the list if necessary.
    
    Change-Id: Iab74b22b7fc4fb53d8f072c4e3a4cea4ae18196c
    Reviewed-on: https://code.wireshark.org/review/2126
    Reviewed-by: Evan Huus <eapache@gmail.com>
    (cherry picked from commit a9b18eff0fe904c87c38262148b39718f6e3cea5)
    Reviewed-on: https://code.wireshark.org/review/2293
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

---
+++ plugins/opcua/opcua_statuscode.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+** Copyright (C) 2014 ascolab GmbH. All Rights Reserved.
+** Web: http://www.ascolab.com
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** Project: OpcUa Wireshark Plugin
+**
+** Author: Hannes Mezger <hannes.mezger@ascolab.com>
+**
+******************************************************************************/
+
+extern const value_string g_statusCodes[];
