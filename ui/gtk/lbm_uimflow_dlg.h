46a808acf4e8c87b61169bfac00799857b38f1f0
dameiss <netshark@ameissnet.com>
2014-06-18 16:44:31 -0700

    Add GTK modules for existing LBM dissectors.
    bug: 10204
    Change-Id: Ie21cc07b0ac9a56648ec72062ce58a1ac800318e
    Reviewed-on: https://code.wireshark.org/review/2420
    Reviewed-by: Michael Mann <mmann78@netscape.net>

---
+++ ui/gtk/lbm_uimflow_dlg.h
@@ -0,0 +1,46 @@
+/* lbm_uimflow_dlg.h
+ * Routines for LBMC UIM flow graph dialog
+ *
+ * Copyright (c) 2005-2014 Informatica Corporation. All Rights Reserved.
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef LBM_UIMFLOW_DLG_H_INCLUDED
+#define LBM_UIMFLOW_DLG_H_INCLUDED
+
+#include "config.h"
+#include <glib.h>
+
+void lbmc_uim_flow_menu_cb(gpointer arg);
+
+#endif
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
