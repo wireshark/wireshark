b1c025ab1d4750c6e6d65525f9cbea0c0f795a07
Pascal Quantin <pascal.quantin@gmail.com>
2014-06-19 23:16:56 +0200

    Update User Guide for upcoming 1.12.0 release
    Change-Id: I01dace804dc011c096753e413c27615c0661edbe
    Reviewed-on: https://code.wireshark.org/review/2457
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- tools/win64-setup.sh
+++ tools/win64-setup.sh
@@ -22,7 +22,7 @@
 
 # 64-bit wrapper for win-setup.sh.
 
-export DOWNLOAD_TAG="2014-06-05"
+export DOWNLOAD_TAG="2014-06-19"
 export WIRESHARK_TARGET_PLATFORM="win64"
 
 WIN_SETUP=`echo $0 | sed -e s/win64/win/`
