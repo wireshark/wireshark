9079e3ad1d32c594309a52ccef5936d11a93a55d
AndersBroman <anders.broman@ericsson.com>
2014-06-23 17:28:36 +0200

    Cheat and try to fix the generated file manually.
    Change-Id: Iabf1821aa0ef676ac4d1d7f2983460b2e671a98a
    Reviewed-on: https://code.wireshark.org/review/2573
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-sip-hdrs.c
+++ epan/dissectors/packet-sip-hdrs.c
@@ -1,3 +1,23 @@
+/*
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 2014 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 /* ANSI-C code produced by gperf version 3.0.4 */
 /* Command-line: gperf -m 500 -D packet-sip-hdrs.gperf  */
 /* Computed positions: -k'1,3,7,$' */
@@ -431,16 +451,16 @@ _sip_header_find (register const char *str, register unsigned int len)
 
       if (key <= MAX_HASH_VALUE && key >= 0)
         {
-          register int index = lookup[key];
+          register int i = lookup[key];
 
-          if (index >= 0)
+          if (i >= 0)
             {
-              if (len == lengthtable[index])
+              if (len == lengthtable[i])
                 {
-                  register const char *s = wordlist[index].name;
+                  register const char *s = wordlist[i].name;
 
                   if (*str == *s && !memcmp (str + 1, s + 1, len - 1))
-                    return &wordlist[index];
+                    return &wordlist[i];
                 }
             }
         }
