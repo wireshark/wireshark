106c8b3e9e5575442369e3cf21d9d32395e73a07
Andrew Bitson <abitson@gmail.com>
2014-07-02 09:04:47 -0500

    adding abs() functions when creating the values to be graphed since the graph only handles positive values. Does not apply to FT_RELATIVE_TIME.
    This is being added to master-1.12 to avoid breaking future QT functionality in the graph.
    
    Bug 9985 - Add absolute values to i/o graph
    
    Change-Id: I32565c346bef48ec852f61c379df96f2c733e68f
    Reviewed-on: https://code.wireshark.org/review/2780
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- ui/io_graph_item.h
+++ ui/io_graph_item.h
@@ -25,6 +25,9 @@
 #ifndef __IO_GRAPH_ITEM_H__
 #define __IO_GRAPH_ITEM_H__
 
+#import <stdlib.h>
+#import <math.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
@@ -169,7 +172,7 @@ update_io_graph_item(io_graph_item_t *items, int idx, packet_info *pinfo, epan_d
             case FT_UINT16:
             case FT_UINT24:
             case FT_UINT32:
-                new_int = fvalue_get_uinteger(&((field_info *)gp->pdata[i])->value);
+                new_int = abs(fvalue_get_uinteger(&((field_info *)gp->pdata[i])->value));
 
                 if ((new_int > item->int_max) || (item->fields == 0)) {
                     item->int_max = new_int;
@@ -184,7 +187,7 @@ update_io_graph_item(io_graph_item_t *items, int idx, packet_info *pinfo, epan_d
             case FT_INT16:
             case FT_INT24:
             case FT_INT32:
-                new_int = fvalue_get_sinteger(&((field_info *)gp->pdata[i])->value);
+                new_int = abs(fvalue_get_sinteger(&((field_info *)gp->pdata[i])->value));
                 if ((new_int > item->int_max) || (item->fields == 0)) {
                     item->int_max = new_int;
                 }
@@ -196,7 +199,7 @@ update_io_graph_item(io_graph_item_t *items, int idx, packet_info *pinfo, epan_d
                 break;
             case FT_UINT64:
             case FT_INT64:
-                new_int64 = fvalue_get_integer64(&((field_info *)gp->pdata[i])->value);
+                new_int64 = labs(fvalue_get_integer64(&((field_info *)gp->pdata[i])->value));
                 if ((new_int64 > item->int_max) || (item->fields == 0)) {
                     item->int_max = new_int64;
                 }
@@ -207,7 +210,7 @@ update_io_graph_item(io_graph_item_t *items, int idx, packet_info *pinfo, epan_d
                 item->fields++;
                 break;
             case FT_FLOAT:
-                new_float = (gfloat)fvalue_get_floating(&((field_info *)gp->pdata[i])->value);
+                new_float = fabs((gfloat)fvalue_get_floating(&((field_info *)gp->pdata[i])->value));
                 if ((new_float > item->float_max) || (item->fields == 0)) {
                     item->float_max = new_float;
                 }
@@ -218,7 +221,7 @@ update_io_graph_item(io_graph_item_t *items, int idx, packet_info *pinfo, epan_d
                 item->fields++;
                 break;
             case FT_DOUBLE:
-                new_double = fvalue_get_floating(&((field_info *)gp->pdata[i])->value);
+                new_double = fabs(fvalue_get_floating(&((field_info *)gp->pdata[i])->value));
                 if ((new_double > item->double_max) || (item->fields == 0)) {
                     item->double_max = new_double;
                 }
