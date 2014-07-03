0e4e825309a1dea3dde17a44d29d2be666c71335
Guy Harris <guy@alum.mit.edu>
2014-07-02 18:28:35 -0700

    Add license information to the copyright information.
    See
    
    	http://www.gnu.org/prep/standards/html_node/_002d_002dversion.html
    
    Change-Id: I19c1cf560a1bb7ed7d7312bea727550977d66120
    Reviewed-on: https://code.wireshark.org/review/2787
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- wsutil/copyright_info.c
+++ wsutil/copyright_info.c
@@ -32,6 +32,7 @@ get_copyright_info(void)
 {
 	return
 "Copyright 1998-2014 Gerald Combs <gerald@wireshark.org> and contributors.\n"
+"License GPLv2+: GNU GPL version 2 or later <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\n"
 "This is free software; see the source for copying conditions. There is NO\n"
 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
 }
