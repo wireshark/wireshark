813aa935104dd287ed09e3f0bc9296347ec4d5d7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-07-09 00:16:48 +0200

    Cmake : (Find XLST Proc) fix indent (use 4spaces)
    Change-Id: Ic8abdc9aaae899dcc5d870bc023c52dc7a2bbfe6
    Reviewed-on: https://code.wireshark.org/review/2952
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- cmake/modules/FindXSLTPROC.cmake
+++ cmake/modules/FindXSLTPROC.cmake
@@ -42,9 +42,9 @@ MACRO(XML2HTML _validated _output _mode _xmlsources _gfxsources)
     SET(_OUTDIR ${CMAKE_CURRENT_BINARY_DIR}/${_OUTDIR})
 
     IF(${_mode} STREQUAL "chunked")
-	SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/html/chunk.xsl")
+        SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/html/chunk.xsl")
     ELSE() # single-page
-	SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl")
+        SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl")
     ENDIF()
 
     FOREACH(_tmpsource ${${_xmlsources}})
@@ -56,35 +56,35 @@ MACRO(XML2HTML _validated _output _mode _xmlsources _gfxsources)
         OUTPUT
             ${_output}
         COMMAND cmake
-	    -E make_directory ${_OUTDIR}
-	# Fixme: find out about subdirs (i.e. toolbar) automatically 
-	#   so this works for wsdg as well.
+            -E make_directory ${_OUTDIR}
+        # Fixme: find out about subdirs (i.e. toolbar) automatically
+        #   so this works for wsdg as well.
 #        COMMAND cmake
-#	    -E make_directory ${_OUTDIR}/${_GFXDIR}/toolbar
+#           -E make_directory ${_OUTDIR}/${_GFXDIR}/toolbar
 #        COMMAND cp
-#	    ${CMAKE_CURRENT_SOURCE_DIR}/${_GFXDIR}/*.* ${_OUTDIR}/${_GFXDIR}/
+#           ${CMAKE_CURRENT_SOURCE_DIR}/${_GFXDIR}/*.* ${_OUTDIR}/${_GFXDIR}/
 #        COMMAND cp
-#	    ${CMAKE_CURRENT_SOURCE_DIR}/${_GFXDIR}/toolbar/*.* ${_OUTDIR}/${_GFXDIR}/toolbar/
+#           ${CMAKE_CURRENT_SOURCE_DIR}/${_GFXDIR}/toolbar/*.* ${_OUTDIR}/${_GFXDIR}/toolbar/
         COMMAND cmake
-	    -E copy ${CMAKE_CURRENT_SOURCE_DIR}/ws.css ${_OUTDIR}
-	COMMAND ${XSLTPROC_EXECUTABLE}
-	    --path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src"
-	    --stringparam img.src.path ${CMAKE_CURRENT_SOURCE_DIR}
-	    --stringparam base.dir ${_OUTDIR}/
-	    --stringparam use.id.as.filename 1
-	    --stringparam admon.graphics 1
-	    --stringparam admon.graphics.path ${_GFXDIR}/
-	    --stringparam section.autolabel 1
-	    --stringparam section.label.includes.component.label 1
-	    --stringparam html.stylesheet ws.css
-	    --nonet
+            -E copy ${CMAKE_CURRENT_SOURCE_DIR}/ws.css ${_OUTDIR}
+        COMMAND ${XSLTPROC_EXECUTABLE}
+            --path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src"
+            --stringparam img.src.path ${CMAKE_CURRENT_SOURCE_DIR}
+            --stringparam base.dir ${_OUTDIR}/
+            --stringparam use.id.as.filename 1
+            --stringparam admon.graphics 1
+            --stringparam admon.graphics.path ${_GFXDIR}/
+            --stringparam section.autolabel 1
+            --stringparam section.label.includes.component.label 1
+            --stringparam html.stylesheet ws.css
+            --nonet
             --output ${_output}
-	    ${_STYLESHEET}
-	    ${_source}
-	COMMAND chmod
-	    -R og+rX ${_OUTDIR}
+            ${_STYLESHEET}
+            ${_source}
+        COMMAND chmod
+            -R og+rX ${_OUTDIR}
         DEPENDS
-	    ${_validated}
+            ${_validated}
             ${${_xmlsources}}
             ${${_gfxsources}}
     )
@@ -92,10 +92,10 @@ ENDMACRO(XML2HTML)
 
 
 #XML2PDF(
-#	user-guide-a4.fo or user-guide-us.fo
-#	WSUG_SOURCE
-#	custom_layer_pdf.xsl
-#	A4 or letter
+#       user-guide-a4.fo or user-guide-us.fo
+#       WSUG_SOURCE
+#       custom_layer_pdf.xsl
+#       A4 or letter
 #)
 MACRO(XML2PDF _output _sources _stylesheet _paper)
     FOREACH(_tmpsource ${${_sources}})
@@ -106,24 +106,24 @@ MACRO(XML2PDF _output _sources _stylesheet _paper)
     ADD_CUSTOM_COMMAND(
         OUTPUT
             ${_output}
-	COMMAND ${XSLTPROC_EXECUTABLE}
-	    --path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src"
-	    --stringparam paper.type ${_paper}
-	    --stringparam img.src.path ${CMAKE_CURRENT_SOURCE_DIR}
-	    --stringparam base.dir ${_OUTDIR}/
-	    --stringparam use.id.as.filename 1
-	    --stringparam admon.graphics 1
-	    --stringparam admon.graphics.path ${_GFXDIR}/
-	    --nonet
-	    --output ${_output}.fo
-	    ${_stylesheet}
-	    ${_source}
-	COMMAND ${FOP_EXECUTABLE}
-	    ${_output}.fo
-	    ${_output}
-	DEPENDS
-	    ${${_sources}}
-	    ${_stylesheet}
+        COMMAND ${XSLTPROC_EXECUTABLE}
+            --path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src"
+            --stringparam paper.type ${_paper}
+            --stringparam img.src.path ${CMAKE_CURRENT_SOURCE_DIR}
+            --stringparam base.dir ${_OUTDIR}/
+            --stringparam use.id.as.filename 1
+            --stringparam admon.graphics 1
+            --stringparam admon.graphics.path ${_GFXDIR}/
+            --nonet
+            --output ${_output}.fo
+            ${_stylesheet}
+            ${_source}
+        COMMAND ${FOP_EXECUTABLE}
+            ${_output}.fo
+            ${_output}
+        DEPENDS
+            ${${_sources}}
+            ${_stylesheet}
     )
 ENDMACRO(XML2PDF)
 
