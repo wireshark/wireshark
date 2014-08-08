e82dc025b2fc76be0da718d2e19c6706e518de90
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-08-08 19:50:24 +0200

    Qt: Fix when build with Qt < 4.8.0
    StyleName is not available in Qt < 4.8
    
    Change-Id: Icbfcef7c62b124ce67f1a02ed928a96233ae2d9d
    Reviewed-on: https://code.wireshark.org/review/3508
    Reviewed-by: Christopher Maynard <Christopher.Maynard@gtech.com>
    (cherry picked from commit 020d0f89637faab8ec0e25e07d64eb66d96c2a72)
    Reviewed-on: https://code.wireshark.org/review/3525
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- ui/qt/qt_ui_utils.cpp
+++ ui/qt/qt_ui_utils.cpp
@@ -55,7 +55,11 @@ QString gchar_free_to_qstring(gchar *glib_string) {
 
 void smooth_font_size(QFont &font) {
     QFontDatabase fdb;
+#if QT_VERSION < QT_VERSION_CHECK(4, 8, 0)
+    QList<int> size_list = fdb.smoothSizes(font.family(), "");
+#else
     QList<int> size_list = fdb.smoothSizes(font.family(), font.styleName());
+#endif
 
     if (size_list.size() < 2) return;
 
