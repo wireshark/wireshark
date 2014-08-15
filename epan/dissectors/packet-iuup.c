1111538200bfe198ff7b634fb266d3c66eaae551
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-08-15 18:54:14 +0200

    IUUP: Add modelines info
    Change-Id: I6cde1a17d917b9cf4c08ca202571fd07bb9302ba
    Reviewed-on: https://code.wireshark.org/review/3650
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-iuup.c
+++ epan/dissectors/packet-iuup.c
@@ -1024,3 +1024,15 @@ void proto_register_iuup(void) {
                                    &global_dynamic_payload_type);
 }
 
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
