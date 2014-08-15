ef77c1c33e532e3f8dc12dcae8089dec52af65b7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-08-15 18:58:30 +0200

    OptoMMP: Add modelines info
    Change-Id: Ia9ea6e553d92d1045f022254d714fea5440257a5
    Reviewed-on: https://code.wireshark.org/review/3652
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-optommp.c
+++ epan/dissectors/packet-optommp.c
@@ -21,8 +21,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-/* Tabsize: 4 */
-
 #include "config.h"
 
 #include <glib.h>
@@ -890,3 +888,16 @@ void proto_reg_handoff_optommp(void)
     dissector_add_uint("tcp.port", currentPort, optommp_tcp_handle);
     dissector_add_uint("udp.port", currentPort, optommp_udp_handle);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
