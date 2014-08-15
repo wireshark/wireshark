71901b6bbfcddebc06802f32996b70cfa453da75
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-08-15 18:43:34 +0200

    OUI: Fix indent (use 4spaces) and modelines info
    Use the same indent like epan/oui.h
    
    Change-Id: I02018788bc3f62d170a87a40bd4495630bded1c5
    Reviewed-on: https://code.wireshark.org/review/3647
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- epan/oui.c
+++ epan/oui.c
@@ -35,50 +35,63 @@
  * for the PIDs for VTP and DRiP that go with an OUI of OUI_CISCO.
  */
 const value_string oui_vals[] = {
-	{ OUI_ENCAP_ETHER,	"Encapsulated Ethernet" },
-	{ OUI_XEROX,		"Xerox" },
+    { OUI_ENCAP_ETHER,      "Encapsulated Ethernet" },
+    { OUI_XEROX,            "Xerox" },
 /*
 http://www.cisco.com/univercd/cc/td/doc/product/software/ios113ed/113ed_cr/ibm_r/brprt1/brsrb.htm
 */
-	{ OUI_CISCO,			"Cisco" },
-	{ OUI_NORTEL,			"Nortel Discovery Protocol" },
-	{ OUI_CISCO_90,			"Cisco IOS 9.0 Compatible" },
-	{ OUI_FORCE10,			"Force10 Networks" },
-	{ OUI_ERICSSON,			"Ericsson Group" },
-	{ OUI_CATENA,			"Catena Networks" },
-	{ OUI_SONY_ERICSSON,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_SONY_ERICSSON_2,	"Sony Ericsson Mobile Communications AB" },
-	/* PROFINET */
-	{ OUI_PROFINET,			"PROFIBUS Nutzerorganisation e.V." },
-	{ OUI_SONY_ERICSSON_3,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_CIMETRICS,		"Cimetrics" },
-	{ OUI_IEEE_802_3,		"IEEE 802.3" },
-	{ OUI_MEDIA_ENDPOINT,	"Media (TIA TR-41 Committee)" },
-	{ OUI_SONY_ERICSSON_4,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_ERICSSON_MOBILE,	"Ericsson Mobile Platforms" },
-	{ OUI_SONY_ERICSSON_5,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_SONY_ERICSSON_6,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_SONY_ERICSSON_7,	"Sony Ericsson Mobile Communications AB" },
-	{ OUI_BLUETOOTH,		"Bluetooth SIG, Inc." },
-	{ OUI_SONY_ERICSSON_8,	"Sony Ericsson Mobile Communications AB" },
-	/* Currently, the manuf file calls this "Procurve", as it's assigned to HP! */
-	{ OUI_IEEE_802_1QBG,	"IEEE 802.1Qbg" },
-	{ OUI_TURBOCELL,		"Karlnet (Turbocell)" },
-	{ OUI_CISCOWL,			"Cisco Wireless (Aironet) L2" },
-	{ OUI_MARVELL,			"Marvell Semiconductor" },
-	/* Used for RFC 2427 bridged Frame Relay and RFC 2684 bridged ATM */
-	{ OUI_IEEE_802_1,		"IEEE 802.1" },
-	{ OUI_ATM_FORUM,		"ATM Forum" },
-	{ OUI_EXTREME,			"Extreme Networks" },
-	/* RFC 2427, RFC 2684 */
-	{ OUI_CABLE_BPDU,		"DOCSIS Spanning Tree" }, /* DOCSIS spanning tree BPDU */
-	{ OUI_SIEMENS,			"Siemens AG" },
-	{ OUI_APPLE_ATALK,		"Apple (AppleTalk)" },
-	{ OUI_HP,				"Hewlett-Packard" },
-	{ OUI_HP_2,				"Hewlett-Packard" },
-	/* Registry Name: PPP Vendor Specific OUI Options */
-	{ OUI_3GPP2,			"3GPP2 Vendor specific packet ID" },
-	{ OUI_ERICSSON_2,		"Ericsson Group" },
-	{ OUI_DCBX,             "Data Center Bridging Capabilities Exchange" },
-	{ 0,	NULL }
+    { OUI_CISCO,            "Cisco" },
+    { OUI_NORTEL,           "Nortel Discovery Protocol" },
+    { OUI_CISCO_90,         "Cisco IOS 9.0 Compatible" },
+    { OUI_FORCE10,          "Force10 Networks" },
+    { OUI_ERICSSON,         "Ericsson Group" },
+    { OUI_CATENA,           "Catena Networks" },
+    { OUI_SONY_ERICSSON,    "Sony Ericsson Mobile Communications AB" },
+    { OUI_SONY_ERICSSON_2,  "Sony Ericsson Mobile Communications AB" },
+    /* PROFINET */
+    { OUI_PROFINET,         "PROFIBUS Nutzerorganisation e.V." },
+    { OUI_SONY_ERICSSON_3,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_CIMETRICS,        "Cimetrics" },
+    { OUI_IEEE_802_3,       "IEEE 802.3" },
+    { OUI_MEDIA_ENDPOINT,   "Media (TIA TR-41 Committee)" },
+    { OUI_SONY_ERICSSON_4,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_ERICSSON_MOBILE,  "Ericsson Mobile Platforms" },
+    { OUI_SONY_ERICSSON_5,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_SONY_ERICSSON_6,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_SONY_ERICSSON_7,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_BLUETOOTH,        "Bluetooth SIG, Inc." },
+    { OUI_SONY_ERICSSON_8,  "Sony Ericsson Mobile Communications AB" },
+    { OUI_IEEE_802_1QBG,    "IEEE 802.1Qbg" },
+    { OUI_TURBOCELL,        "Karlnet (Turbocell)" },
+    { OUI_CISCOWL,          "Cisco Wireless (Aironet) L2" },
+    { OUI_MARVELL,          "Marvell Semiconductor" },
+    /* Used for RFC 2427 bridged Frame Relay and RFC 2684 bridged ATM */
+    { OUI_IEEE_802_1,       "IEEE 802.1" },
+    { OUI_ATM_FORUM,        "ATM Forum" },
+    { OUI_EXTREME,          "Extreme Networks" },
+    /* RFC 2427, RFC 2684 */
+    { OUI_CABLE_BPDU,       "DOCSIS Spanning Tree" }, /* DOCSIS spanning tree BPDU */
+    { OUI_SIEMENS,          "Siemens AG" },
+    { OUI_APPLE_ATALK,      "Apple (AppleTalk)" },
+    /* Currently, the manuf file calls this "Procurve", as it's assigned to HP! */
+    { OUI_HP,               "Hewlett-Packard" },
+    { OUI_HP_2,             "Hewlett-Packard" },
+    /* Registry Name: PPP Vendor Specific OUI Options */
+    { OUI_3GPP2,            "3GPP2 Vendor specific packet ID" },
+    { OUI_ERICSSON_2,       "Ericsson Group" },
+    { OUI_DCBX,             "Data Center Bridging Capabilities Exchange" },
+    { 0,                    NULL }
 };
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
