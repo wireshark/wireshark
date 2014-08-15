721008c708c1b234aedc92b72d5f5a8e6effc1ef
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-08-15 19:49:52 +0200

    DCERPC PN IO: Add modelines info
    Change-Id: I124c5d38f8f3b4aee6d5dbee0367555742350e55
    Reviewed-on: https://code.wireshark.org/review/3657
    Reviewed-by: Evan Huus <eapache@gmail.com>

--- plugins/profinet/packet-dcerpc-pn-io.c
+++ plugins/profinet/packet-dcerpc-pn-io.c
@@ -12053,3 +12053,16 @@ proto_reg_handoff_pn_io (void)
 
     heur_dissector_add("pn_rt", dissect_PNIO_heur, proto_pn_io);
 }
+
+/*
+ * Editor modelines
+ *
+ * Local Variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * ex: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
