f415e1c34887ff5ca50f0af499a7f52cfc6c7d56
Guy Harris <guy@alum.mit.edu>
2014-08-30 16:21:10 -0700

    Remove some unnecessary definitions.
    text2pcap.c uses pcapio.c to write pcap files, so it doesn't itself need
    to know what those files look like.
    
    Change-Id: I013d0c094c27515af91f352b565bc9a8bad3e842
    Reviewed-on: https://code.wireshark.org/review/3927
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- text2pcap.c
+++ text2pcap.c
@@ -439,27 +439,7 @@ static char tempbuf[64];
 /*----------------------------------------------------------------------
  * Stuff for writing a PCap file
  */
-#define PCAP_MAGIC          0xa1b2c3d4
 #define PCAP_SNAPLEN        0xffff
-
-/* "libpcap" file header (minus magic number). */
-struct pcap_hdr {
-    guint32 magic;          /* magic */
-    guint16 version_major;  /* major version number */
-    guint16 version_minor;  /* minor version number */
-    guint32 thiszone;       /* GMT to local correction */
-    guint32 sigfigs;        /* accuracy of timestamps */
-    guint32 snaplen;        /* max length of captured packets, in octets */
-    guint32 network;        /* data link type */
-};
-
-/* "libpcap" record header. */
-struct pcaprec_hdr {
-    guint32 ts_sec;         /* timestamp seconds */
-    guint32 ts_usec;        /* timestamp microseconds */
-    guint32 incl_len;       /* number of octets of packet saved in file */
-    guint32 orig_len;       /* actual length of packet */
-};
 
 /* Link-layer type; see http://www.tcpdump.org/linktypes.html for details */
 static guint32 pcap_link_type = 1;   /* Default is LINKTYPE_ETHERNET */
