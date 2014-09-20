5f7321783bd4fc4c25229b7334696e975ec10c61
Guy Harris <guy@alum.mit.edu>
2014-09-20 13:18:53 -0700

    Report (and free) err_info for WTAP_ERR_DECOMPRESS.
    Change-Id: I61ecc58be4b0688c43df0b0f006fb899bdc1721c
    Reviewed-on: https://code.wireshark.org/review/4216
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit be7f0539848238627db0e18acdc2dae5715c3182)
    Reviewed-on: https://code.wireshark.org/review/4218

--- capinfos.c
+++ capinfos.c
@@ -1419,6 +1419,7 @@ main(int argc, char *argv[])
         case WTAP_ERR_UNSUPPORTED:
         case WTAP_ERR_UNSUPPORTED_ENCAP:
         case WTAP_ERR_BAD_FILE:
+        case WTAP_ERR_DECOMPRESS:
           fprintf(stderr, "(%s)\n", err_info);
           g_free(err_info);
           break;
