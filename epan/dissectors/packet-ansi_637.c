7c54eb23e5ee553dedd7ea61b9f583874d9a7d99
Pascal Quantin <pascal.quantin@gmail.com>
2014-09-25 17:48:26 +0200

    ANSI IS-637 A: get rid of tvb_length() deprecated API
    Change-Id: I40de03605c051024c604679a1e141afa841a77e9
    Reviewed-on: https://code.wireshark.org/review/4287
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- epan/dissectors/packet-ansi_637.c
+++ epan/dissectors/packet-ansi_637.c
@@ -2281,7 +2281,7 @@ dissect_ansi_637_tele_message(tvbuff_t *tvb, packet_info *pinfo, proto_tree *ans
     guint32     curr_offset;
 
     curr_offset = 0;
-    len = tvb_length(tvb);
+    len = tvb_reported_length(tvb);
 
     while ((len - curr_offset) > 0)
     {
@@ -2535,7 +2535,7 @@ dissect_ansi_637_trans(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
         curr_offset = 1;
 
-        len = tvb_length(tvb);
+        len = tvb_reported_length(tvb);
 
         while ((len - curr_offset) > 0)
         {
