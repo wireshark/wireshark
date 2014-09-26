ce36b20ca5d01490acd1fb6ca44a82dab85ba2a2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2014-09-26 11:56:31 +0200

    Fix Dead Store (Dead assignement/Dead increment) warning found by Clang
    Change-Id: Ia0f29a491af920415d6e56f30ef436544f7d5ddf
    Reviewed-on: https://code.wireshark.org/review/4303
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-pktap.c
+++ epan/dissectors/packet-pktap.c
@@ -202,7 +202,7 @@ dissect_pktap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	offset += 4;
 	proto_tree_add_item(pktap_tree, hf_pktap_ecmdname, tvb, offset, 20,
 	    ENC_UTF_8|ENC_NA);
-	offset += 20;
+	/*offset += 20;*/
 
 	if (rectype == PKT_REC_PACKET) {
 		next_tvb = tvb_new_subset_remaining(tvb, pkt_len);
