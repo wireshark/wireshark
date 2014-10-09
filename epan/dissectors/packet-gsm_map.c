080f11f5c611a319de436cdcf3209ddc2a079791
Pascal Quantin <pascal.quantin@gmail.com>
2014-10-09 22:48:59 +0200

    GSM MAP: fix detection of SMS direction when application context version is < 3
    Bug: 10547
    Change-Id: I4708fd9977e635c66ef1350ce5098520e4c2ce1e
    Reviewed-on: https://code.wireshark.org/review/4579
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    (cherry picked from commit 6463d2a7e66e259dabbeffbab67bcbeedd98133f)
    Reviewed-on: https://code.wireshark.org/review/4774
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- epan/dissectors/packet-gsm_map.c
+++ epan/dissectors/packet-gsm_map.c
@@ -2633,6 +2633,8 @@ static guint ProtocolId;
 static guint AccessNetworkProtocolId;
 static const char *obj_id = NULL;
 static int gsm_map_tap = -1;
+static guint8 gsmmap_pdu_type = 0;
+static guint8 gsm_map_pdu_size = 0;
 
 #define SMS_ENCODING_NOT_SET    0
 #define SMS_ENCODING_7BIT       1
@@ -17756,10 +17758,10 @@ dissect_gsm_old_ForwardSM_Arg(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int 
 	if (!actx->value_ptr)
 		return offset;
 	if (actx->pinfo->p2p_dir == P2P_DIR_UNKNOWN) {
-		if (opcode == 44) {
-			actx->pinfo->p2p_dir = P2P_DIR_RECV;
-		} else if (opcode == 46) {
+		if (gsmmap_pdu_type == 1) {
 			actx->pinfo->p2p_dir = P2P_DIR_SENT;
+		} else {
+			actx->pinfo->p2p_dir = P2P_DIR_RECV;
 		}
 	}
 	call_dissector_only(gsm_sms_handle, (tvbuff_t*)actx->value_ptr, actx->pinfo, top_tree, NULL);
@@ -20266,7 +20268,7 @@ dissect_NokiaMAP_Extensions_AllowedServiceData(gboolean implicit_tag _U_, tvbuff
 
 
 /*--- End of included file: packet-gsm_map-fn.c ---*/
-#line 847 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 849 "../../asn1/gsm_map/packet-gsm_map-template.c"
 
 /* Specific translation for MAP V3 */
 const value_string gsm_map_V1V2_opr_code_strings[] = {
@@ -20488,7 +20490,7 @@ const value_string gsm_map_opr_code_strings[] = {
 /* Unknown or empty loop list OPERATION */
 
 /*--- End of included file: packet-gsm_map-table.c ---*/
-#line 858 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 860 "../../asn1/gsm_map/packet-gsm_map-template.c"
   { 0, NULL }
 };
 
@@ -20705,7 +20707,7 @@ static const value_string gsm_map_err_code_string_vals[] = {
 /* Unknown or empty loop list OPERATION */
 
 /*--- End of included file: packet-gsm_map-table.c ---*/
-#line 864 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 866 "../../asn1/gsm_map/packet-gsm_map-template.c"
     { 0, NULL }
 };
 #endif
@@ -22008,9 +22010,6 @@ static void dissect_NokiaMAP_ext_DsdArgExt(tvbuff_t *tvb, packet_info *pinfo, pr
   }
   dissect_NokiaMAP_Extensions_DsdArgExt(FALSE, tvb, 0, &asn1_ctx, tree, -1);
 }
-
-static guint8 gsmmap_pdu_type = 0;
-static guint8 gsm_map_pdu_size = 0;
 
 static int
 dissect_gsm_map_GSMMAPPDU(gboolean implicit_tag _U_, tvbuff_t *tvb, int offset, asn1_ctx_t *actx, proto_tree *tree,
@@ -29568,7 +29567,7 @@ void proto_register_gsm_map(void) {
         NULL, HFILL }},
 
 /*--- End of included file: packet-gsm_map-hfarr.c ---*/
-#line 2957 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 2956 "../../asn1/gsm_map/packet-gsm_map-template.c"
   };
 
   /* List of subtrees */
@@ -30276,7 +30275,7 @@ void proto_register_gsm_map(void) {
     &ett_NokiaMAP_Extensions_AllowedServiceData,
 
 /*--- End of included file: packet-gsm_map-ettarr.c ---*/
-#line 2988 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 2987 "../../asn1/gsm_map/packet-gsm_map-template.c"
   };
 
   static ei_register_info ei[] = {
@@ -30378,7 +30377,7 @@ void proto_register_gsm_map(void) {
 
 
 /*--- End of included file: packet-gsm_map-dis-tab.c ---*/
-#line 3024 "../../asn1/gsm_map/packet-gsm_map-template.c"
+#line 3023 "../../asn1/gsm_map/packet-gsm_map-template.c"
   oid_add_from_string("ericsson-gsm-Map-Ext","1.2.826.0.1249.58.1.0" );
   oid_add_from_string("accessTypeNotAllowed-id","1.3.12.2.1107.3.66.1.2");
   /*oid_add_from_string("map-ac networkLocUp(1) version3(3)","0.4.0.0.1.0.1.3" );
