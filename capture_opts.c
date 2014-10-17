a6359868370cadb4e631f506aafb0e0819e890b7
Guy Harris <guy@alum.mit.edu>
2014-10-17 16:57:28 -0700

    Get rid of some no-longer-necessary casts.
    g_ascii_isXXX() can be handed a char value safely; they won't try to use
    it as an index into a table before checking it.
    
    (cherry picked from commit e3477bd13635a637d1337093457dbad499bd5538)
    
    Change-Id: I5a94f2d2ddb88a99e755c0ffbaed61547066ce66
    Reviewed-on: https://code.wireshark.org/review/4802
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- capture_opts.c
+++ capture_opts.c
@@ -294,7 +294,7 @@ get_ring_arguments(capture_options *capture_opts, const char *arg)
    * as we allow it in the preferences file, we might as well
    * allow it here).
    */
-  while (g_ascii_isspace((guchar)*p))
+  while (g_ascii_isspace(*p))
     p++;
   if (*p == '\0') {
     /*
@@ -340,7 +340,7 @@ get_sampling_arguments(capture_options *capture_opts, const char *arg)
     p = colonp;
     *p++ = '\0';
 
-    while (g_ascii_isspace((guchar)*p))
+    while (g_ascii_isspace(*p))
         p++;
     if (*p == '\0') {
         *colonp = ':';
@@ -398,7 +398,7 @@ get_auth_arguments(capture_options *capture_opts, const char *arg)
     p = colonp;
     *p++ = '\0';
 
-    while (g_ascii_isspace((guchar)*p))
+    while (g_ascii_isspace(*p))
         p++;
 
     if (capture_opts->ifaces->len > 0) {
