86a6642d3236a52d3c6ab8a657961be966fe7a50
Guy Harris <guy@alum.mit.edu>
2014-10-20 00:31:16 -0700

    Clean up a comment.
    Change-Id: I3990b9b92bb3dfd2ce3734ed164fa59a401f8c18
    Reviewed-on: https://code.wireshark.org/review/4855
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-icap.c
+++ epan/dissectors/packet-icap.c
@@ -139,7 +139,7 @@ dissect_icap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			c = *linep++;
 
 			/*
-			 * This must be a CHAR, and must not be a CTL to
+			 * This must be a CHAR, and must not be a CTL, to
 			 * be part of a token; that means it must be
 			 * printable ASCII.
 			 *
@@ -147,9 +147,8 @@ dissect_icap(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 			 * lines of a header?
 			 */
 			if (!g_ascii_isprint(c)) {
-				/* not ASCII, thus not a CHAR, or not printabe, thus a CTL */
 				is_icap = FALSE;
-				break;	/* not ASCII, thus not a CHAR or a CTL */
+				break;
 			}
 
 			switch (c) {
