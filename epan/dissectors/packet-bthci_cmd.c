ea7c867ba2165703702a8a7706be8736f098ceef
Michal Labedzki <michal.labedzki@tieto.com>
2014-10-24 15:48:56 +0200

    Bluetooth: Fix AVRCP UUID
    Change-Id: Iaddde4a1ce49b46e4473c1a470760d9cbbe8000e
    Reviewed-on: https://code.wireshark.org/review/5038
    Reviewed-by: Michal Labedzki <michal.labedzki@tieto.com>

--- epan/dissectors/packet-bthci_cmd.c
+++ epan/dissectors/packet-bthci_cmd.c
@@ -527,7 +527,7 @@ static const value_string bt_sig_uuid_vals[] = {
     { 0x110C,   "A/V Remote Control Target" },
     { 0x110D,   "Advanced Audio Distribution" },
     { 0x110E,   "A/V Remote Control" },
-    { 0x110F,   "Video Conferencing" },
+    { 0x110F,   "A/V Remote Control Controller" },
     { 0x1110,   "Intercom" },
     { 0x1111,   "Fax" },
     { 0x1112,   "Headset Audio Gateway" },
