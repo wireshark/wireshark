0011fc4b240ca7279b3f1b4ff4d5a1ca55b16cc7
Jeff Morriss <jeff.morriss.ws@gmail.com>
2014-10-28 11:19:23 -0400

    Add GPLv2+ license header to keep license-check happy.
    This change must be reviewed/approved by the script's author (Gerald).
    
    Change-Id: Id89c1d05bf43cdd2d6823b5ef46162a9f11fb263
    Reviewed-on: https://code.wireshark.org/review/4972
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- image/toolbar/svg-to-png.sh
+++ image/toolbar/svg-to-png.sh
@@ -2,6 +2,26 @@
 # svg-to-png
 # Convert SVG files to 1x and 2x PNGs. Dump a list of Qt resource
 # file entries upon successful completion.
+#
+# Copyright 2014 Gerald Combs <gerald [AT] wireshark.org>
+#
+# Wireshark - Network traffic analyzer
+# By Gerald Combs <gerald@wireshark.org>
+# Copyright 1998 Gerald Combs
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 COMMON_ARGS="--export-area-page"
 ONE_X_ARGS="--export-width=24 --export-height=24"
