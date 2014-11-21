1df0d9cbfa5af834495c28e46c6ed2d267e05e2f
Guy Harris <guy@alum.mit.edu>
2014-11-20 16:57:24 -0800

    Include "ws_symbol_export.h" to make sure WS_DLL_PUBLIC is defined.
    Wrap the declaration for C++ while we're at it.
    
    Change-Id: Ifcc1b47bab139f5fb8da8c3dd4f20b1ebb99739e
    Reviewed-on: https://code.wireshark.org/review/5418
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-a21.h
+++ epan/dissectors/packet-a21.h
@@ -22,7 +22,17 @@
 #ifndef __PACKET_A21_H__
 #define __PACKET_A21_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "ws_symbol_export.h"
+
 WS_DLL_PUBLIC
 void dissect_a21_ie_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *top_tree, proto_tree *tree, gint offset,  guint8 message_type);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif
