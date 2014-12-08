2341031471878d7676b8490f9dd5687ff01564f9
Hannes Mezger <hannes.mezger@ascolab.com>
2014-12-08 11:20:16 +0100

    Fix full name of NodeId namespace index field, was a copy&pase error
    Reviewed-on: https://code.wireshark.org/review/5668
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    
    Conflicts:
    	plugins/opcua/opcua_application_layer.c
    
    Change-Id: I7b1eea9965880e35c65f04406ff756d245b568d4
    Reviewed-on: https://code.wireshark.org/review/5669
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- plugins/opcua/opcua_application_layer.c
+++ plugins/opcua/opcua_application_layer.c
@@ -54,7 +54,7 @@ void registerApplicationLayerTypes(int proto)
         {  "NodeId EncodingMask",        "application.nodeid.encodingmask", FT_UINT8,   BASE_HEX,  VALS(g_nodeidmasks), 0x0,    NULL,    HFILL }
         },
         { &hf_opcua_app_nsid,
-        {  "NodeId EncodingMask",        "application.nodeid.nsid",         FT_UINT8,   BASE_DEC,  NULL, 0x0,    NULL,    HFILL }
+        {  "NodeId Namespace Index",     "application.nodeid.nsid",         FT_UINT8,   BASE_DEC,  NULL, 0x0,    NULL,    HFILL }
         },
         { &hf_opcua_app_numeric,
         {  "NodeId Identifier Numeric",  "application.nodeid.numeric",      FT_UINT32,  BASE_DEC,  VALS(g_requesttypes), 0x0,    NULL,    HFILL }
