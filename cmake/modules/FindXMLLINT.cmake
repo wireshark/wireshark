ffb6f10f4eb0ad126a8496981cfaac0fb324bbc8
Gerald Combs <gerald@wireshark.org>
2015-01-08 17:16:09 -0800

    CMake: Cygwin-ify the xmllint path as well.
    Change-Id: I5e62ed6baeed3fcb76cdffd30666ad01713acd2a
    Reviewed-on: https://code.wireshark.org/review/6439
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- cmake/modules/FindXMLLINT.cmake
+++ cmake/modules/FindXMLLINT.cmake
@@ -6,20 +6,45 @@
 INCLUDE(FindCygwin)
 
 FIND_PROGRAM(XMLLINT_EXECUTABLE
-  NAMES
-    xmllint
-  PATHS
-    ${CYGWIN_INSTALL_PATH}/bin
-    /bin
-    /usr/bin
-    /usr/local/bin
-    /sbin
+    NAMES
+        xmllint
+    PATHS
+        ${CYGWIN_INSTALL_PATH}/bin
+        /bin
+        /usr/bin
+        /usr/local/bin
+        /sbin
 )
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(XMLLINT DEFAULT_MSG XMLLINT_EXECUTABLE)
 
 MARK_AS_ADVANCED(XMLLINT_EXECUTABLE)
+
+if (WIN32 AND NOT "${CYGWIN_INSTALL_PATH}" STREQUAL "" AND ${XMLLINT_EXECUTABLE} MATCHES "${CYGWIN_INSTALL_PATH}")
+    FIND_PROGRAM(CYGPATH_EXECUTABLE
+        NAMES cygpath
+        PATHS ${CYGWIN_INSTALL_PATH}/bin
+    )
+    # XXX Duplicate of TO_A2X_COMPATIBLE_PATH
+    MACRO( TO_XMLLINT_COMPATIBLE_PATH _cmake_path _result )
+        execute_process(
+            COMMAND ${CYGPATH_EXECUTABLE} -u ${_cmake_path}
+            OUTPUT_VARIABLE _cygwin_path
+        )
+        # cygpath adds a linefeed.
+        string(STRIP "${_cygwin_path}" _cygwin_path)
+
+        set( ${_result} ${_cygwin_path} )
+    ENDMACRO()
+
+    TO_XMLLINT_COMPATIBLE_PATH( ${CMAKE_CURRENT_SOURCE_DIR} _xmllint_current_source_dir )
+    TO_XMLLINT_COMPATIBLE_PATH( ${CMAKE_CURRENT_BINARY_DIR} _xmllint_current_binary_dir )
+
+    set ( _xmllint_path "${_xmllint_current_source_dir}:${_xmllint_current_binary_dir}:${_xmllint_current_binary_dir}/wsluarm_src")
+else()
+    set ( _xmllint_path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src")
+endif()
 
 # Validate XML
 MACRO(VALIDATE_XML _validated _sources)
@@ -28,20 +53,20 @@ MACRO(VALIDATE_XML _validated _sources)
     # Oh, and I have no idea why I can't directly use _source instead of
     # having to introduce _tmpsource.
     FOREACH(_tmpsource ${${_sources}})
-	set(_source ${_tmpsource})
-	BREAK()
+        set(_source ${_tmpsource})
+        BREAK()
     ENDFOREACH()
     ADD_CUSTOM_COMMAND(
-	OUTPUT
-	    ${_validated}
-	COMMAND ${XMLLINT_EXECUTABLE}
-	  --path "${CMAKE_CURRENT_SOURCE_DIR}:${CMAKE_CURRENT_BINARY_DIR}:${CMAKE_CURRENT_BINARY_DIR}/wsluarm_src"
-	  --valid
-	  --noout
-	  ${_source}
-	COMMAND touch
-	  ${_validated}
-	DEPENDS
-	  ${${_sources}}
+        OUTPUT
+            ${_validated}
+        COMMAND ${XMLLINT_EXECUTABLE}
+            --path "${_xmllint_path}"
+            --valid
+            --noout
+            ${_source}
+        COMMAND touch
+            ${_validated}
+        DEPENDS
+            ${${_sources}}
     )
 ENDMACRO(VALIDATE_XML)
