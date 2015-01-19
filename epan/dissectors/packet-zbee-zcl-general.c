ab8a13dde85be5f6ed36c366c1e3d2aa492251e3
Guy Harris <guy@alum.mit.edu>
2015-01-19 12:26:06 -0800

    Don't mix declarations and code - it's not legal C90.
    Change-Id: I1a89c729255560d596e4d7701468cca3e9000357
    Reviewed-on: https://code.wireshark.org/review/6655
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-zbee-zcl-general.c
+++ epan/dissectors/packet-zbee-zcl-general.c
@@ -1770,8 +1770,9 @@ decode_zcl_ota_curr_time(gchar *s, guint32 value)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Now");
     }
     else {
+        gchar *tmp;
         value += ZBEE_ZCL_NSTIME_UTC_OFFSET;
-        gchar *tmp = abs_time_secs_to_str(NULL, value, ABSOLUTE_TIME_LOCAL, 1);
+        tmp = abs_time_secs_to_str(NULL, value, ABSOLUTE_TIME_LOCAL, 1);
         g_snprintf(s, ITEM_LABEL_LENGTH, "%s", tmp);
         wmem_free(NULL, tmp);
     }
