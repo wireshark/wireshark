f89edfd2dfbb59abc5a1bd488e9363e29dd978fd
Alexis La Goutte <alexis.lagoutte@gmail.com>
2015-02-19 22:40:57 +0100

    addr_resolv: Fix Dead Store (Dead assignement/Dead increment) warning found by Clang
    Change-Id: I559151afab48892c51b702d008c1583570bd8754
    Reviewed-on: https://code.wireshark.org/review/7262
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/addr_resolv.c
+++ epan/addr_resolv.c
@@ -3076,17 +3076,6 @@ const gchar *
 get_manuf_name(const guint8 *addr)
 {
     hashmanuf_t *manuf_value;
-    int manuf_key;
-    guint8 oct;
-
-    /* manuf needs only the 3 most significant octets of the ethernet address */
-    manuf_key = addr[0];
-    manuf_key = manuf_key<<8;
-    oct = addr[1];
-    manuf_key = manuf_key | oct;
-    manuf_key = manuf_key<<8;
-    oct = addr[2];
-    manuf_key = manuf_key | oct;
 
     manuf_value = manuf_name_lookup(addr);
     if (gbl_resolv_flags.mac_name && manuf_value->status != HASHETHER_STATUS_UNRESOLVED)
