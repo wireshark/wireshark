c466d401d9c41c837010f8c4b9da22727577192b
ranushekhar <ranushekhar@gmail.com>
2015-02-23 18:17:33 +0530

    NS Trace (NetScaler Trace) file format is not able to export specified packets
    Bug: 10998
    Change-Id: Ic2c5ad5b01be3e0c39b1e93badcc4f2246c420fe
    Reviewed-on: https://code.wireshark.org/review/7327
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- wiretap/netscaler.c
+++ wiretap/netscaler.c
@@ -1545,6 +1545,7 @@ static gboolean nstrace_seek_read_v30(wtap *wth, gint64 seek_off,
                              err, err_info))
             return FALSE;
     }
+    (phdr)->caplen = (phdr)->len = record_length;
 
 #define GENERATE_CASE_V30(phdr,type,acttype) \
     case NSPR_PDPKTRACEFULLTX_V##type:\
