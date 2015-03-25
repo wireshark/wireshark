6c09fba68a49d67210b5679195bdd841fea25bc6
Jeff Morriss <morriss@ulticom.com>
2015-03-25 10:56:06 -0400

    Fix warning-turned-error in string_repr_len().
    Broken in the back-port of Ie95700295ed707dc7f8c6461b026c67f8c0695c4
    
    Change-Id: Ib341c634c97465acff7a25c916821f881b22d263
    Reviewed-on: https://code.wireshark.org/review/7820
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/ftypes/ftype-string.c
+++ epan/ftypes/ftype-string.c
@@ -61,6 +61,7 @@ string_repr_len(fvalue_t *fv, ftrepr_t rtype)
 
 	switch (rtype) {
 		case FTREPR_DISPLAY:
+		case FTREPR_DISPLAY_HEX:
 			return (int)strlen(fv->value.string);
 		case FTREPR_DFILTER:
 			repr_len = 0;
