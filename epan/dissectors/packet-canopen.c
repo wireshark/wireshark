f4644f3e15866bb7fba54b2f147533c1c8b49d3d
Michael Mann <mmann78@netscape.net>
2015-04-04 09:10:13 -0400

    Some CANopen frame types can have zero size, so use tvb_reported_length instead of -1.
    Bug: 11083
    Change-Id: Id90c7adfa477a01f8e344b2e2ed7b782f3cd49b2
    Reviewed-on: https://code.wireshark.org/review/7907
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-canopen.c
+++ epan/dissectors/packet-canopen.c
@@ -256,7 +256,7 @@ dissect_canopen(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         proto_tree *canopen_cob_tree;
         proto_tree *canopen_type_tree;
 
-        ti = proto_tree_add_item(tree, proto_canopen, tvb, 0, -1, ENC_NA);
+        ti = proto_tree_add_item(tree, proto_canopen, tvb, 0, tvb_reported_length(tvb), ENC_NA);
 
         canopen_tree = proto_item_add_subtree(ti, ett_canopen);
 
@@ -272,9 +272,7 @@ dissect_canopen(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
         /* add CANopen frame type */
 
-        type_ti = proto_tree_add_text(canopen_tree, tvb,
-                                      (msg_type_id != MT_SYNC) ?  8 : 0,
-                                      (msg_type_id != MT_SYNC) ? -1 : 0,
+        type_ti = proto_tree_add_text(canopen_tree, tvb, tvb_reported_length(tvb),
                                       "Type: %s", function_code_str);
         canopen_type_tree = proto_item_add_subtree(type_ti, ett_canopen);
 
