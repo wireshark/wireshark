d64312433c8c821359b1ff26bc327c3c21cc8072
Robert P <tehownt@gmail.com>
2015-04-06 09:45:33 +0200

    PKCS1: fix a typo for OID 1.3.132.0.35
    Bug: 11106
    Change-Id: Ied414b8e0bae8f34445f4d36bc6dfcc96b4a7456
    Reviewed-on: https://code.wireshark.org/review/7941
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    (cherry picked from commit 686d3ef1a88a89278ea44988dcc5309e5f8e213d)
    Reviewed-on: https://code.wireshark.org/review/7946

--- epan/dissectors/packet-pkcs1.c
+++ epan/dissectors/packet-pkcs1.c
@@ -589,8 +589,8 @@ void proto_reg_handoff_pkcs1(void) {
 	oid_add_from_string("secp384r1","1.3.132.0.34");
 	oid_add_from_string("sect409k1","1.3.132.0.36");
 	oid_add_from_string("sect409r1","1.3.132.0.37");
-	oid_add_from_string("sect521r1","1.3.132.0.35")
-;	oid_add_from_string("sect571k1","1.3.132.0.38");
+	oid_add_from_string("secp521r1","1.3.132.0.35");
+	oid_add_from_string("sect571k1","1.3.132.0.38");
 	oid_add_from_string("sect571r1","1.3.132.0.39");
 
 	/* sha2 family, see RFC3447 and http://www.oid-info.com/get/2.16.840.1.101.3.4.2 */
