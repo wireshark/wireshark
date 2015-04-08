160915950c207ff36fae951dd92b9a0556b04f6a
Julien STAUB <atsju2@yahoo.fr>
2015-04-08 18:57:22 +0200

    zigbee: correct zcl meas sensing
    corrected the relative humidity measurement 0.00 to 100.00 values.
    Previous version had bug for example 0.01% -> display 0.1%
    
    Change-Id: I3955d36d1f2a1334ef7baab4fd854f13950d1951
    Reviewed-on: https://code.wireshark.org/review/7985
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-zbee-zcl-meas-sensing.c
+++ epan/dissectors/packet-zbee-zcl-meas-sensing.c
@@ -578,7 +578,7 @@ decode_temp_meas_value(gchar *s, gint16 value)
     if (value == (gint16)ZBEE_ZCL_ATTR_ID_TEMP_MEAS_INVALID_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Invalid value");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [\302\260C]", value/100, value%100);
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%.2f [\302\260C]", value/100.0);
 
     return;
 } /*decode_temp_meas_value*/
@@ -602,7 +602,7 @@ decode_temp_meas_min_value(gchar *s, gint16 value)
          (value > (gint16)ZBEE_ZCL_ATTR_ID_TEMP_MEAS_MIN_HI_VALUE) )
         g_snprintf(s, ITEM_LABEL_LENGTH, "Out of range");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [\302\260C]", value/100, value%100);
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%.2f [\302\260C]", value/100.0);
 
     return;
 } /*decode_temp_meas_min_value*/
@@ -625,7 +625,7 @@ decode_temp_meas_max_value(gchar *s, gint16 value)
     if (value < (gint16)ZBEE_ZCL_ATTR_ID_TEMP_MEAS_MAX_LO_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Out of range");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [\302\260C]", value/100, value%100);
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%.2f [\302\260C]", value/100.0);
 
     return;
 } /*decode_temp_meas_max_value*/
@@ -1311,7 +1311,7 @@ decode_relhum_meas_value(gchar *s, guint16 value)
     if (value == ZBEE_ZCL_ATTR_ID_RELHUM_MEAS_INVALID_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Invalid value");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [%c]", value/100, value%100, '%');
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%02d [%%]", value/100, value%100);
 
     return;
 } /*decode_relhum_meas_value*/
@@ -1334,7 +1334,7 @@ decode_relhum_meas_min_value(gchar *s, guint16 value)
     if (value > ZBEE_ZCL_ATTR_ID_RELHUM_MEAS_MIN_HI_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Out of range");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [%c]", value/100, value%100, '%');
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%02d [%%]", value/100, value%100);
 
     return;
 } /*decode_relhum_meas_min_value*/
@@ -1357,7 +1357,7 @@ decode_relhum_meas_max_value(gchar *s, guint16 value)
     if (value > ZBEE_ZCL_ATTR_ID_RELHUM_MEAS_MAX_HI_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Out of range");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [%c]", value/100, value%100, '%');
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%02d [%%]", value/100, value%100);
 
     return;
 } /*decode_relhum_meas_max_value*/
@@ -1380,7 +1380,7 @@ decode_relhum_meas_tolerance(gchar *s, guint16 value)
     if (value > ZBEE_ZCL_ATTR_ID_RELHUM_MEAS_TOL_HI_VALUE)
         g_snprintf(s, ITEM_LABEL_LENGTH, "Out of range");
     else
-        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%d [%c]", value/100, value%100, '%');
+        g_snprintf(s, ITEM_LABEL_LENGTH, "%d.%02d [%%]", value/100, value%100);
 
     return;
 } /*decode_relhum_meas_tolerance*/
