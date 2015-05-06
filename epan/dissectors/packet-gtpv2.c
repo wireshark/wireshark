67da8aeda13eff34c3e19bd76794e31bf7d50edb
Alexis La Goutte <alexis.lagoutte@gmail.com>
2015-05-06 14:47:59 +0200

    GTPv2 : fix unused parameter 'tree' [-Werror=unused-parameter]
    Change-Id: I01097889dd4a1c29135f50dcfb737ff50b0de2b7
    Reviewed-on: https://code.wireshark.org/review/8310
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>

--- epan/dissectors/packet-gtpv2.c
+++ epan/dissectors/packet-gtpv2.c
@@ -5586,7 +5586,8 @@ dissect_gtpv2_twan_identifier_timestamp(tvbuff_t *tvb, packet_info *pinfo _U_, p
  * 8.111        Overload Control Information
  */
 static void
-dissect_gtpv2_overload_control_inf(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
+
+dissect_gtpv2_overload_control_inf(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree _U_, proto_item *item _U_, guint16 length _U_, guint8 message_type _U_, guint8 instance _U_)
 {
 
     int         offset = 0;
