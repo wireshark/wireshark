53025444b7e2f51cf478d01307a9ef8ef1e03b6a
Guy Harris <guy@alum.mit.edu>
2015-05-08 18:26:49 -0700

    Keep everything unsigned there.
    This squelches a warning from Sun/Oracle C, and it's the right thing to
    do in any case, as the LHS of the & operator is unsigned.
    
    Change-Id: I824a82d945f8f2535282bbf466dcd65806c6f695
    Reviewed-on: https://code.wireshark.org/review/8362
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- wsutil/type_util.c
+++ wsutil/type_util.c
@@ -48,7 +48,7 @@
 gdouble
 type_util_guint64_to_gdouble(guint64 value)
 {
-  if (value & G_GINT64_CONSTANT (0x8000000000000000))
+  if (value & G_GUINT64_CONSTANT (0x8000000000000000))
     return (gdouble) ((gint64) value) + (gdouble) 18446744073709551616.;
   else
     return (gdouble) ((gint64) value);
