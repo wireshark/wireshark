wireshark/wireshark

commit 8ccf65bfb2444ce1cb3c4d0819d9657fface9fe9

Eliminate proto_tree_add_text from a few dissectors.
Change-Id: Ia6b62fae76ae76a2859ec47229e1c299bddb5a31
Reviewed-on: https://code.wireshark.org/review/8749
Reviewed-by: Michael Mann <mmann78@netscape.net>
Petri-Dish: Michael Mann <mmann78@netscape.net>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Anders Broman <a.broman58@gmail.com>

epan/dissectors/packet-ber.c
@@ -125,6 +125,7 @@ static gint hf_ber_arbitrary = -1;                /* BIT_STRING */
 
 /* Generated from convert_proto_tree_add_text.pl */
 static int hf_ber_seq_of_eoc = -1;
+static int hf_ber_64bit_uint_as_bytes = -1;
 static int hf_ber_choice_eoc = -1;
 static int hf_ber_seq_field_eoc = -1;
 static int hf_ber_seq_eoc = -1;
@@ -1884,21 +1885,14 @@ printf("INTEGERnew dissect_ber_integer(%s) entered implicit_tag:%d \n", name, im
     /* we can't handle integers > 64 bits */
     /* take into account the use case of a 64bits unsigned integer: you will have a 9th byte set to 0 */
     if ((len > 9) || ((len == 9) && (first != 0))) {
-        header_field_info *hfinfo;
-        proto_item        *pi = NULL;
-
         if (hf_id >= 0) {
-            hfinfo = proto_registrar_get_nth(hf_id);
-            pi = proto_tree_add_text(tree, tvb, offset, len, "%s : 0x", hfinfo->name);
+            header_field_info *hfinfo = proto_registrar_get_nth(hf_id);
+
+            proto_tree_add_bytes_format(tree, hf_ber_64bit_uint_as_bytes, tvb, offset, len,
+                "%s : 0x%s", hfinfo->name, tvb_bytes_to_str(wmem_packet_scope(), tvb, offset, len));
         }
-        if (pi) {
-            for (i=0; i<len; i++) {
-                proto_item_append_text(pi, "%02x", tvb_get_guint8(tvb, offset));
-                offset++;
-            }
-        } else {
-            offset += len;
-        }
+
+        offset += len;
         return offset;
     }
 
@@ -4409,6 +4403,7 @@ proto_register_ber(void)
       { &hf_ber_set_eoc, { "SET EOC", "ber.set_eoc", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
       { &hf_ber_choice_eoc, { "CHOICE EOC", "ber.choice_eoc", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
       { &hf_ber_seq_of_eoc, { "SEQ OF EOC", "ber.seq_of_eoc", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+      { &hf_ber_64bit_uint_as_bytes, { "64bits unsigned integer", "ber.64bit_uint_as_bytes", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
     };
 
 

epan/dissectors/packet-cfdp.c
@@ -23,6 +23,7 @@
 
 #include "config.h"
 #include <epan/packet.h>
+#include <epan/expert.h>
 
 
 /* The CFDP standard can be found here:
@@ -102,6 +103,14 @@ static int hf_cfdp_rep_resp_code = -1;
 static int hf_cfdp_suspension_ind = -1;
 static int hf_cfdp_tlv_len = - 1;
 
+/* Generated from convert_proto_tree_add_text.pl */
+static int hf_cfdp_filestore_message = -1;
+static int hf_cfdp_entity = -1;
+static int hf_cfdp_message_to_user = -1;
+static int hf_cfdp_flow_label = -1;
+static int hf_cfdp_segment_requests = -1;
+static int hf_cfdp_user_data = -1;
+
 /* Initialize the subtree pointers */
 static gint ett_cfdp = -1;
 static gint ett_cfdp_header = -1;
@@ -114,6 +123,9 @@ static gint ett_cfdp_filestore_resp = -1;
 static gint ett_cfdp_msg_to_user = -1;
 static gint ett_cfdp_fault_hdl_overr = -1;
 static gint ett_cfdp_flow_label = -1;
+
+static expert_field ei_cfdp_bad_length = EI_INIT;
+
 
 /* Generic data handle */
 static dissector_handle_t data_handle;
@@ -428,37 +440,37 @@ static const value_string cfdp_directive_codes[] = {
 
 /* Dissect the Source Entity ID field */
 static void
-dissect_cfdp_src_entity_id(tvbuff_t *tvb, proto_tree *tree, guint32 offset, guint8 len_ent_id)
+dissect_cfdp_src_entity_id(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 offset, guint8 len_ent_id)
 {
     if(len_ent_id > 0 && len_ent_id <= 8){
         proto_tree_add_item(tree, hf_cfdp_srcid, tvb, offset, len_ent_id, ENC_BIG_ENDIAN);
     }
     else{
-        proto_tree_add_text(tree, tvb, offset, 0, "Wrong length for the entity ID");
+        proto_tree_add_expert_format(tree, pinfo, &ei_cfdp_bad_length, tvb, offset, 0, "Wrong length for the entity ID");
     }
 }
 
 /* Dissect the Destination Entity ID field */
 static void
-dissect_cfdp_dst_entity_id(tvbuff_t *tvb, proto_tree *tree, guint32 offset, guint8 len_ent_id)
+dissect_cfdp_dst_entity_id(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 offset, guint8 len_ent_id)
 {
     if(len_ent_id > 0 && len_ent_id <= 8){
         proto_tree_add_item(tree, hf_cfdp_dstid, tvb, offset, len_ent_id, ENC_BIG_ENDIAN);
     }
     else{
-        proto_tree_add_text(tree, tvb, offset, 0, "Wrong length for the entity ID");
+        proto_tree_add_expert_format(tree, pinfo, &ei_cfdp_bad_length, tvb, offset, 0, "Wrong length for the entity ID");
     }
 }
 
 /* Dissect the Transaction Sequence Number field */
 static void
-dissect_cfdp_tseq_num(tvbuff_t *tvb, proto_tree *tree, guint32 offset, guint8 len_tseq_num)
+dissect_cfdp_tseq_num(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 offset, guint8 len_tseq_num)
 {
     if(len_tseq_num > 0 && len_tseq_num <= 8){
         proto_tree_add_item(tree, hf_cfdp_transeqnum, tvb, offset, len_tseq_num, ENC_BIG_ENDIAN);
     }
     else{
-        proto_tree_add_text(tree, tvb, offset, 0, "Wrong length for transaction sequence number");
+        proto_tree_add_expert_format(tree, pinfo, &ei_cfdp_bad_length, tvb, offset, 0, "Wrong length for transaction sequence number");
     }
 }
 
@@ -571,7 +583,7 @@ static guint32 dissect_cfdp_filestore_resp_tlv(tvbuff_t *tvb, proto_tree *tree, 
         aux_byte = tvb_get_guint8(tvb, offset);
         offset += 1;
         if(aux_byte > 0){
-            proto_tree_add_text(cfdp_filestore_resp_tree, tvb, offset, tlv_len, "Filestore Message");
+            proto_tree_add_item(cfdp_filestore_resp_tree, hf_cfdp_filestore_message, tvb, offset, tlv_len, ENC_NA);
             offset += aux_byte;
         }
     }
@@ -598,7 +610,7 @@ static guint32 dissect_cfdp_fault_location_tlv(tvbuff_t *tvb, proto_tree *tree, 
 
         proto_tree_add_uint(cfdp_fault_location_tree, hf_cfdp_tlv_len, tvb, offset-1, 1, tlv_len);
 
-        proto_tree_add_text(cfdp_fault_location_tree, tvb, offset, tlv_len, "Entity");
+        proto_tree_add_item(cfdp_fault_location_tree, hf_cfdp_entity, tvb, offset, tlv_len, ENC_NA);
         offset += tlv_len;
     }
 
@@ -606,7 +618,7 @@ static guint32 dissect_cfdp_fault_location_tlv(tvbuff_t *tvb, proto_tree *tree, 
 }
 
 /* Dissect the Message to User TLV */
-static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, guint32 ext_offset){
+static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ext_offset){
 
     guint8 aux_byte, tlv_type, tlv_len;
     proto_tree  *cfdp_msg_to_user_tree;
@@ -638,7 +650,7 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
             case PROXY_PUT_REQ:
                 tlv_len = tvb_get_guint8(tvb, offset);
                 offset += 1;
-                dissect_cfdp_dst_entity_id(tvb, cfdp_msg_to_user_tree, offset, tlv_len);
+                dissect_cfdp_dst_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, tlv_len);
                 offset += tlv_len;
 
                 tlv_len = tvb_get_guint8(tvb, offset);
@@ -656,7 +668,7 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
             case PROXY_MSG_TO_USER:
                 tlv_len = tvb_get_guint8(tvb, offset);
                 offset += 1;
-                proto_tree_add_text(cfdp_msg_to_user_tree, tvb, offset, tlv_len, "Message to User");
+                proto_tree_add_item(cfdp_msg_to_user_tree, hf_cfdp_message_to_user, tvb, offset, tlv_len, ENC_NA);
                 break;
 
             case PROXY_FILESTORE_REQ:
@@ -678,7 +690,7 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 break;
 
             case PROXY_FLOW_LABEL:
-                proto_tree_add_text(cfdp_msg_to_user_tree, tvb, offset, tlv_len, "Flow label");
+                proto_tree_add_item(cfdp_msg_to_user_tree, hf_cfdp_flow_label, tvb, offset, tlv_len, ENC_NA);
                 break;
 
             case PROXY_SEGMENT_CONTROL:
@@ -714,11 +726,11 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 offset += 1;
 
                 len_ent_id = ((aux_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-                dissect_cfdp_src_entity_id(tvb, cfdp_msg_to_user_tree, offset, len_ent_id);
+                dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_ent_id);
                 offset += len_ent_id;
 
                 len_tseq_num = (aux_byte & HDR_LEN_TSEQ_NUM) +1;
-                dissect_cfdp_tseq_num(tvb, cfdp_msg_to_user_tree, offset, len_tseq_num);
+                dissect_cfdp_tseq_num(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_tseq_num);
                 offset += len_tseq_num;
 
                 break;
@@ -762,11 +774,11 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 offset += 1;
 
                 len_ent_id = ((aux_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-                dissect_cfdp_src_entity_id(tvb, cfdp_msg_to_user_tree, offset, len_ent_id);
+                dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_ent_id);
                 offset += len_ent_id;
 
                 len_tseq_num = (aux_byte & HDR_LEN_TSEQ_NUM) +1;
-                dissect_cfdp_tseq_num(tvb, cfdp_msg_to_user_tree, offset, len_tseq_num);
+                dissect_cfdp_tseq_num(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_tseq_num);
                 offset += len_tseq_num;
 
                 /* Report File Name */
@@ -791,11 +803,11 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 offset += 1;
 
                 len_ent_id = ((aux_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-                dissect_cfdp_src_entity_id(tvb, cfdp_msg_to_user_tree, offset, len_ent_id);
+                dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_ent_id);
                 offset += len_ent_id;
 
                 len_tseq_num = (aux_byte & HDR_LEN_TSEQ_NUM) +1;
-                dissect_cfdp_tseq_num(tvb, cfdp_msg_to_user_tree, offset, len_tseq_num);
+                dissect_cfdp_tseq_num(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_tseq_num);
                 offset += len_tseq_num;
                 break;
 
@@ -809,11 +821,11 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 offset += 1;
 
                 len_ent_id = ((aux_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-                dissect_cfdp_src_entity_id(tvb, cfdp_msg_to_user_tree, offset, len_ent_id);
+                dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_ent_id);
                 offset += len_ent_id;
 
                 len_tseq_num = (aux_byte & HDR_LEN_TSEQ_NUM) +1;
-                dissect_cfdp_tseq_num(tvb, cfdp_msg_to_user_tree, offset, len_tseq_num);
+                dissect_cfdp_tseq_num(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_tseq_num);
                 offset += len_tseq_num;
                 break;
 
@@ -833,11 +845,11 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 offset += 1;
 
                 len_ent_id = ((aux_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-                dissect_cfdp_src_entity_id(tvb, cfdp_msg_to_user_tree, offset, len_ent_id);
+                dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_ent_id);
                 offset += len_ent_id;
 
                 len_tseq_num = (aux_byte & HDR_LEN_TSEQ_NUM) +1;
-                dissect_cfdp_tseq_num(tvb, cfdp_msg_to_user_tree, offset, len_tseq_num);
+                dissect_cfdp_tseq_num(tvb, pinfo, cfdp_msg_to_user_tree, offset, len_tseq_num);
                 offset += len_tseq_num;
                 break;
 
@@ -845,7 +857,7 @@ static guint32 dissect_cfdp_msg_to_user_tlv(tvbuff_t *tvb, proto_tree *tree, gui
                 break;
         }
     }else{
-        proto_tree_add_text(cfdp_msg_to_user_tree, tvb, offset, tlv_len, "Message to User");
+        proto_tree_add_item(cfdp_msg_to_user_tree, hf_cfdp_message_to_user, tvb, offset, tlv_len, ENC_NA);
         offset += tlv_len;
     }
 
@@ -895,7 +907,7 @@ static guint32 dissect_cfdp_flow_label_tlv(tvbuff_t *tvb, proto_tree *tree, guin
                                         ett_cfdp_flow_label, NULL, "Flow Label TLV");
 
     /* It is undefined, so no specific encoding */
-    proto_tree_add_text(cfdp_flow_label_tree, tvb, offset, tlv_len, "Flow label");
+    proto_tree_add_item(cfdp_flow_label_tree, hf_cfdp_flow_label, tvb, offset, tlv_len, ENC_NA);
 
     return offset;
 }
@@ -931,7 +943,7 @@ static guint32 dissect_cfdp_eof_pdu(tvbuff_t *tvb, packet_info *pinfo, proto_tre
             cfdp_fault_location_tree = proto_tree_add_subtree(tree, tvb, offset-2, tlv_len+2,
                         ett_cfdp_fault_location, NULL, "Fault location TLV");
 
-            proto_tree_add_text(cfdp_fault_location_tree, tvb, offset, tlv_len, "Entity");
+            proto_tree_add_item(cfdp_fault_location_tree, hf_cfdp_entity, tvb, offset, tlv_len, ENC_NA);
             offset += tlv_len;
         }
     }
@@ -1001,7 +1013,7 @@ static guint32 dissect_cfdp_ack_pdu(tvbuff_t *tvb, packet_info *pinfo, proto_tre
 }
 
 /* Dissect the Metadata PDU */
-static guint32 dissect_cfdp_metadata_pdu(tvbuff_t *tvb, proto_tree *tree, guint32 ext_offset, guint ext_packet_len){
+static guint32 dissect_cfdp_metadata_pdu(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, guint32 ext_offset, guint ext_packet_len){
 
     guint8 aux_byte, tlv_type;
     guint  cfdp_packet_data_length = ext_packet_len;
@@ -1032,7 +1044,7 @@ static guint32 dissect_cfdp_metadata_pdu(tvbuff_t *tvb, proto_tree *tree, guint3
                 break;
 
             case MSG_TO_USER:
-                offset = dissect_cfdp_msg_to_user_tlv(tvb, tree, offset);
+                offset = dissect_cfdp_msg_to_user_tlv(tvb, pinfo, tree, offset);
                 break;
 
             case FAULT_HDL_OVERR:
@@ -1061,7 +1073,7 @@ static guint32 dissect_cfdp_nak_pdu(tvbuff_t *tvb, proto_tree *tree, guint32 ext
     offset += 4;
     proto_tree_add_item(tree, hf_cfdp_nak_sp_scope, tvb, offset, 4, ENC_BIG_ENDIAN);
     offset += 4;
-    proto_tree_add_text(tree, tvb, offset, cfdp_packet_data_length-9, "Segment requests");
+    proto_tree_add_item(tree, hf_cfdp_segment_requests, tvb, offset, cfdp_packet_data_length-9, ENC_NA);
     offset += cfdp_packet_data_length-9;
 
     return offset;
@@ -1165,14 +1177,14 @@ dissect_cfdp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     offset += 1;
 
     len_ent_id = ((second_byte & HDR_LEN_ENT_ID) >> 4) + 1;
-    dissect_cfdp_src_entity_id(tvb, cfdp_header_tree, offset, len_ent_id);
+    dissect_cfdp_src_entity_id(tvb, pinfo, cfdp_header_tree, offset, len_ent_id);
     offset += len_ent_id;
 
     len_tseq_num = (second_byte & HDR_LEN_TSEQ_NUM) +1;
-    dissect_cfdp_tseq_num(tvb, cfdp_header_tree, offset, len_tseq_num);
+    dissect_cfdp_tseq_num(tvb, pinfo, cfdp_header_tree, offset, len_tseq_num);
     offset += len_tseq_num;
 
-    dissect_cfdp_dst_entity_id(tvb, cfdp_header_tree, offset, len_ent_id);
+    dissect_cfdp_dst_entity_id(tvb, pinfo, cfdp_header_tree, offset, len_ent_id);
     offset += len_ent_id;
 
     proto_item_set_end(cfdp_header, tvb, offset);
@@ -1208,7 +1220,7 @@ dissect_cfdp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
                 break;
 
             case METADATA_PDU:
-                offset = dissect_cfdp_metadata_pdu(tvb, cfdp_file_directive_header_tree, offset, cfdp_packet_data_length);
+                offset = dissect_cfdp_metadata_pdu(tvb, pinfo, cfdp_file_directive_header_tree, offset, cfdp_packet_data_length);
                 break;
 
             case NAK_PDU:
@@ -1241,7 +1253,7 @@ dissect_cfdp(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
         offset += 4;
 
-        proto_tree_add_text(cfdp_file_data_header_tree, tvb, offset, cfdp_packet_data_length-4, "User Data");
+        proto_tree_add_item(cfdp_file_data_header_tree, hf_cfdp_user_data, tvb, offset, cfdp_packet_data_length-4, ENC_NA);
         offset += cfdp_packet_data_length-4;
 
     }
@@ -1570,7 +1582,15 @@ proto_register_cfdp(void)
             {"Suspension indicator", "cfdp.suspension_ind",
             FT_UINT8, BASE_DEC, VALS(cfdp_suspension_ind), 0x80,
             NULL, HFILL}
-        }
+        },
+
+        /* Generated from convert_proto_tree_add_text.pl */
+        { &hf_cfdp_filestore_message, { "Filestore Message", "cfdp.filestore_message", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_cfdp_entity, { "Entity", "cfdp.entity", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_cfdp_message_to_user, { "Message to User", "cfdp.message_to_user", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_cfdp_flow_label, { "Flow label", "cfdp.flow_label", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_cfdp_segment_requests, { "Segment requests", "cfdp.segment_requests", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_cfdp_user_data, { "User Data", "cfdp.user_data", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
 
     };
 
@@ -1589,12 +1609,20 @@ proto_register_cfdp(void)
         &ett_cfdp_flow_label
     };
 
+    static ei_register_info ei[] = {
+        { &ei_cfdp_bad_length, { "cfdp.bad_length", PI_MALFORMED, PI_ERROR, "Bad length field", EXPFILL }},
+    };
+
+    expert_module_t* expert_cfdp;
+
     /* Register the protocol name and description */
     proto_cfdp = proto_register_protocol("CFDP", "CFDP", "cfdp");
 
     /* Required function calls to register the header fields and subtrees used */
     proto_register_field_array(proto_cfdp, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
+    expert_cfdp = expert_register_protocol(proto_cfdp);
+    expert_register_field_array(expert_cfdp, ei, array_length(ei));
 
     register_dissector ( "cfdp", dissect_cfdp, proto_cfdp );
 }

epan/dissectors/packet-cops.c
@@ -622,7 +622,9 @@ static gint hf_cops_epd_unknown = -1;
 static gint hf_cops_reserved8 = -1;
 static gint hf_cops_reserved16 = -1;
 static gint hf_cops_reserved24 = -1;
-
+static gint hf_cops_keyed_message_digest = -1;
+static gint hf_cops_integrity_contents = -1;
+static gint hf_cops_opaque_data = -1;
 
 /* For PacketCable D-QoS */
 static gint hf_cops_subtree = -1;
@@ -668,7 +670,8 @@ static gint hf_cops_pc_token_bucket_rate = -1;
 static gint hf_cops_pc_token_bucket_size = -1;
 static gint hf_cops_pc_transaction_id = -1;
 static gint hf_cops_pc_bcid_ts = -1;
-/* static gint hf_cops_pc_bcid = -1; */
+static gint hf_cops_pc_bcid_id = -1;
+static gint hf_cops_pc_bcid_tz = -1;
 static gint hf_cops_pc_bcid_ev = -1;
 static gint hf_cops_pc_dfcdc_ip = -1;
 static gint hf_cops_pc_dfccc_ip = -1;
@@ -1516,7 +1519,7 @@ static void dissect_cops_object_data(tvbuff_t *tvb, packet_info *pinfo, guint32 
 
         proto_tree_add_item(tree, hf_cops_key_id, tvb, offset, 4, ENC_BIG_ENDIAN);
         proto_tree_add_item(tree, hf_cops_seq_num, tvb, offset + 4, 4, ENC_BIG_ENDIAN);
-        proto_tree_add_text(tree, tvb, offset + 8 , len - 8, "Contents: Keyed Message Digest");
+        proto_tree_add_item(tree, hf_cops_keyed_message_digest, tvb, offset + 8 , len - 8, ENC_NA);
 
         break;
     default:
@@ -1779,7 +1782,7 @@ static int dissect_cops_pr_object_data(tvbuff_t *tvb, packet_info *pinfo, guint3
 
         break;
     default:
-        proto_tree_add_text(tree, tvb, offset, len, "Contents: %d bytes", len);
+        proto_tree_add_bytes_format_value(tree, hf_cops_integrity_contents, tvb, offset, len, NULL, "%d bytes", len);
         break;
     }
 
@@ -1987,6 +1990,21 @@ void proto_register_cops(void)
           { "Contents: Sequence Number",           "cops.integrity.seq_num",
             FT_UINT32, BASE_DEC, NULL, 0,
             "Sequence Number in Integrity object", HFILL }
+        },
+        { &hf_cops_keyed_message_digest,
+          { "Contents: Keyed Message Digest",           "cops.integrity.keyed_message_digest",
+            FT_BYTES, BASE_NONE, NULL, 0,
+            NULL, HFILL }
+        },
+        { &hf_cops_integrity_contents,
+          { "Contents",           "cops.integrity.contents",
+            FT_BYTES, BASE_NONE, NULL, 0,
+            NULL, HFILL }
+        },
+        { &hf_cops_opaque_data,
+          { "Opaque Data",           "cops.opaque_data",
+            FT_BYTES, BASE_NONE, NULL, 0,
+            NULL, HFILL }
         },
         { &hf_cops_gperror,
           { "Error",           "cops.gperror",
@@ -2265,13 +2283,16 @@ void proto_register_cops(void)
             FT_FLOAT, BASE_NONE, NULL, 0x00,
             NULL, HFILL }
         },
-#if 0
-        { &hf_cops_pc_bcid,
-          { "Billing Correlation ID", "cops.pc_bcid",
-            FT_UINT32, BASE_HEX, NULL, 0x00,
+        { &hf_cops_pc_bcid_id,
+          { "BCID - Element ID", "cops.pc_bcid.id",
+            FT_STRING, BASE_NONE, NULL, 0x00,
             NULL, HFILL }
         },
-#endif
+        { &hf_cops_pc_bcid_tz,
+          { "BCID - Time Zone", "cops.pc_bcid.tz",
+            FT_STRING, BASE_NONE, NULL, 0x00,
+            NULL, HFILL }
+        },
         { &hf_cops_pc_bcid_ts,
           { "BDID Timestamp", "cops.pc_bcid_ts",
             FT_UINT32, BASE_HEX, NULL, 0x00,
@@ -3209,7 +3230,6 @@ static void
 cops_surveillance_parameters(tvbuff_t *tvb, proto_tree *st, guint n, guint32 offset) {
 
      proto_tree *stt;
-     guint8 *bcid_str;
 
      /* Create a subtree */
      stt = info_to_cops_subtree(tvb,st,n,offset,"Electronic Surveillance Parameters");
@@ -3248,13 +3268,11 @@ cops_surveillance_parameters(tvbuff_t *tvb, proto_tree *st, guint n, guint32 off
      offset += 4;
 
      /* BCID Element ID */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Element ID",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_id, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Time Zone */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Time Zone",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_tz, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Event Counter */
@@ -3266,7 +3284,6 @@ static void
 cops_event_generation_info(tvbuff_t *tvb, proto_tree *st, guint n, guint32 offset) {
 
      proto_tree *stt;
-     guint8 *bcid_str;
 
      /* Create a subtree */
      stt = info_to_cops_subtree(tvb,st,n,offset,"Event Generation Info");
@@ -3309,13 +3326,11 @@ cops_event_generation_info(tvbuff_t *tvb, proto_tree *st, guint n, guint32 offse
      offset += 4;
 
      /* BCID Element ID */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Element ID",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_id, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Time Zone */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Time Zone",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_tz, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Event Counter */
@@ -3819,7 +3834,7 @@ cops_flow_spec(tvbuff_t *tvb, proto_tree *st, guint n, guint32 offset) {
 
 /* Cops - Section : DOCSIS Service Class Name */
 static int
-cops_docsis_service_class_name(tvbuff_t *tvb, proto_tree *st, guint object_len, guint32 offset) {
+cops_docsis_service_class_name(tvbuff_t *tvb, packet_info *pinfo, proto_tree *st, guint object_len, guint32 offset) {
 
     proto_tree *stt;
 
@@ -3838,7 +3853,8 @@ cops_docsis_service_class_name(tvbuff_t *tvb, proto_tree *st, guint object_len, 
         proto_tree_add_item(stt, hf_cops_pcmm_docsis_scn, tvb, offset, object_len - 8, ENC_ASCII|ENC_NA);
         offset += object_len - 8;
     } else {
-        proto_tree_add_text(stt, tvb, offset - 8, 2, "Invalid object length: %u", object_len);
+        proto_tree_add_expert_format(stt, pinfo, &ei_cops_bad_cops_object_length,
+                                    tvb, offset - 8, 2, "Invalid object length: %u", object_len);
     }
 
     return offset;
@@ -5634,7 +5650,6 @@ static void
 cops_mm_event_generation_info(tvbuff_t *tvb, proto_tree *st, guint n, guint32 offset) {
 
      proto_tree *stt;
-     guint8 *bcid_str;
 
      /* Create a subtree */
      stt = info_to_cops_subtree(tvb,st,n,offset,"Event Generation Info");
@@ -5669,13 +5684,11 @@ cops_mm_event_generation_info(tvbuff_t *tvb, proto_tree *st, guint n, guint32 of
      offset += 4;
 
      /* BCID Element ID */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Element ID",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_id, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Time Zone */
-     bcid_str = (guchar*)tvb_format_text(tvb, offset, 8);
-     proto_tree_add_text(stt, tvb, offset, 8,"%-28s : '%s'","BCID - Time Zone",bcid_str);
+     proto_tree_add_item(stt, hf_cops_pc_bcid_tz, tvb, offset, 8, ENC_ASCII|ENC_NA);
      offset += 8;
 
      /* BCID Event Counter */
@@ -5728,7 +5741,7 @@ cops_opaque_data(tvbuff_t *tvb, proto_tree *st, guint object_len, guint32 offset
      offset += 4;
 
      /* Opaque Data */
-     proto_tree_add_text(stt, tvb, offset, 8,"Opaque Data");
+     proto_tree_add_item(stt, hf_cops_opaque_data, tvb, offset, 8, ENC_NA);
 }
 
 /* Cops - Section : Gate Time Info */
@@ -5950,7 +5963,7 @@ cops_analyze_packetcable_dqos_obj(tvbuff_t *tvb, packet_info *pinfo, proto_tree 
         /* In case we have remaining data, then lets try to get this analyzed */
         object_len   = tvb_get_ntohs(tvb, offset);
         if (object_len < 4) {
-            proto_tree_add_text(tree, tvb, offset, 2,
+            proto_tree_add_expert_format(tree, pinfo, &ei_cops_bad_cops_object_length, tvb, offset, 2,
                                 "Incorrect PacketCable object length %u < 4", object_len);
             return;
         }
@@ -6082,7 +6095,7 @@ cops_analyze_packetcable_mm_obj(tvbuff_t *tvb, packet_info *pinfo, proto_tree *t
         /* In case we have remaining data, then lets try to get this analyzed */
         object_len   = tvb_get_ntohs(tvb, offset);
         if (object_len < 4) {
-            proto_tree_add_text(tree, tvb, offset, 2,
+            proto_tree_add_expert_format(tree, pinfo, &ei_cops_bad_cops_object_length, tvb, offset, 2,
                                 "Incorrect PacketCable object length %u < 4", object_len);
             return;
         }
@@ -6126,7 +6139,7 @@ cops_analyze_packetcable_mm_obj(tvbuff_t *tvb, packet_info *pinfo, proto_tree *t
             cops_flow_spec(tvb, tree, object_len, offset);
             break;
         case PCMM_DOCSIS_SERVICE_CLASS_NAME:
-            cops_docsis_service_class_name(tvb, tree, object_len, offset);
+            cops_docsis_service_class_name(tvb, pinfo, tree, object_len, offset);
             break;
         case PCMM_BEST_EFFORT_SERVICE:
             if (object_len == 44 || object_len == 80 || object_len == 116)

commit dcf69bb2717f50948a19655898ba1495486c0c0e

BER: fix call to proto_tree_add_bytes_format()
The start_ptr parameter was missing.

Bug: 11264
Fixes: 8ccf65bfb244 Eliminate proto_tree_add_text from a few dissectors.
Change-Id: I52a59fe572cd1d0da8c6eb64e703752f5e352ed6
Reviewed-on: https://code.wireshark.org/review/8878
Reviewed-by: Peter Wu <peter@lekensteyn.nl>
Tested-by: Peter Wu <peter@lekensteyn.nl>

epan/dissectors/packet-ber.c
@@ -1888,7 +1888,7 @@ printf("INTEGERnew dissect_ber_integer(%s) entered implicit_tag:%d \n", name, im
         if (hf_id >= 0) {
             header_field_info *hfinfo = proto_registrar_get_nth(hf_id);
 
-            proto_tree_add_bytes_format(tree, hf_ber_64bit_uint_as_bytes, tvb, offset, len,
+            proto_tree_add_bytes_format(tree, hf_ber_64bit_uint_as_bytes, tvb, offset, len, NULL,
                 "%s : 0x%s", hfinfo->name, tvb_bytes_to_str(wmem_packet_scope(), tvb, offset, len));
         }
 