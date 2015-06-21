a2e2c1f99783b5ad1048e9bba5b7a2834680b5fd
Evan Huus <eapache@gmail.com>
2015-06-21 14:02:43 +0000

    Fix memory leak printing eui64s
    The scope that is passed in should only be used for the return value - other
    temporary buffers we must alloc/free ourselves, since if the scope is NULL they
    will not be managed automatically.
    
    Bug: 11293
    Change-Id: I27be856f1c5cdf47f78e766192a29523664a543e
    Reviewed-on: https://code.wireshark.org/review/9007
    Reviewed-by: Evan Huus <eapache@gmail.com>
    Petri-Dish: Evan Huus <eapache@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- epan/addr_resolv.c
+++ epan/addr_resolv.c
@@ -3221,18 +3221,22 @@ char* get_hash_manuf_resolved_name(hashmanuf_t* manuf)
 const gchar *
 eui64_to_display(wmem_allocator_t *allocator, const guint64 addr_eui64)
 {
-    guint8 *addr = (guint8 *)wmem_alloc(allocator, 8);
+    guint8 *addr = (guint8 *)wmem_alloc(NULL, 8);
     hashmanuf_t *manuf_value;
+    const gchar *ret;
 
     /* Copy and convert the address to network byte order. */
     *(guint64 *)(void *)(addr) = pntoh64(&(addr_eui64));
 
     manuf_value = manuf_name_lookup(addr);
     if (!gbl_resolv_flags.mac_name || (manuf_value->status == HASHETHER_STATUS_UNRESOLVED)) {
-        return wmem_strdup_printf(allocator, "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7]);
+        ret = wmem_strdup_printf(allocator, "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7]);
+    } else {
+        ret = wmem_strdup_printf(allocator, "%s_%02x:%02x:%02x:%02x:%02x", manuf_value->resolved_name, addr[3], addr[4], addr[5], addr[6], addr[7]);
     }
-    return wmem_strdup_printf(allocator, "%s_%02x:%02x:%02x:%02x:%02x", manuf_value->resolved_name, addr[3], addr[4], addr[5], addr[6], addr[7]);
 
+    wmem_free(NULL, addr);
+    return ret;
 } /* eui64_to_display */
 
 #ifdef HAVE_C_ARES
--- epan/to_str.c
+++ epan/to_str.c
@@ -1118,7 +1118,7 @@ eui64_to_str(wmem_allocator_t *scope, const guint64 ad) {
 	gchar *buf, *tmp;
 	guint8 *p_eui64;
 
-	p_eui64=(guint8 *)wmem_alloc(scope, 8);
+	p_eui64=(guint8 *)wmem_alloc(NULL, 8);
 	buf=(gchar *)wmem_alloc(scope, EUI64_STR_LEN);
 
 	/* Copy and convert the address to network byte order. */
@@ -1126,6 +1126,7 @@ eui64_to_str(wmem_allocator_t *scope, const guint64 ad) {
 
 	tmp = bytes_to_hexstr_punct(buf, p_eui64, 8, ':');
 	*tmp = '\0'; /* NULL terminate */
+	wmem_free(NULL, p_eui64);
 	return buf;
 }
 
