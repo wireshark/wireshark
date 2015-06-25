a124424422754c16517b6f5c4dcde25783653f2c
Pascal Quantin <pascal.quantin@gmail.com>
2015-06-25 14:28:40 -0700

    CredSSP: update template after the API change done in gcdc7d25
    Change-Id: I548870ba423c7c3b714047f9f02c6cfb9a6a0661
    Reviewed-on: https://code.wireshark.org/review/9156
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- asn1/credssp/packet-credssp-template.c
+++ asn1/credssp/packet-credssp-template.c
@@ -112,7 +112,7 @@ dissect_credssp_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree,
               tags_bit_field = EXP_PDU_TAG_IP_SRC_BIT + EXP_PDU_TAG_IP_DST_BIT + EXP_PDU_TAG_SRC_PORT_BIT+
                   EXP_PDU_TAG_DST_PORT_BIT + EXP_PDU_TAG_ORIG_FNO_BIT;
 
-              exp_pdu_data = load_export_pdu_tags(pinfo, "credssp", -1, &tags_bit_field, 1);
+              exp_pdu_data = load_export_pdu_tags(pinfo, EXP_PDU_TAG_PROTO_NAME, "credssp", &tags_bit_field, 1);
 
               exp_pdu_data->tvb_captured_length = tvb_captured_length(tvb);
               exp_pdu_data->tvb_reported_length = tvb_reported_length(tvb);
