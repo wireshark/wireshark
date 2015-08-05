62e56ad92a50c7dd58e83447207b8be0cc0bbb76
Gerald Combs <gerald@wireshark.org>
2015-08-05 10:59:51 -0700

    Make sure we use "*.html" in our chunked output.
    Change-Id: Idb441084ec50ec336b7f514895aba799832e8f66
    Reviewed-on: https://code.wireshark.org/review/9879
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- cmake/modules/FindXSLTPROC.cmake
+++ cmake/modules/FindXSLTPROC.cmake
@@ -78,7 +78,7 @@ MACRO(XML2HTML _guide _mode _xmlsources _gfxsources)
     IF(${_mode} STREQUAL "chunked")
         SET(_basedir ${_guide}_html_chunked)
         SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/xhtml5/chunk.xsl")
-        SET(_modeparams --noout)
+        SET(_modeparams --stringparam html.ext .html --noout)
     ELSE() # single-page
         SET(_basedir ${_guide}_html)
         SET(_STYLESHEET "http://docbook.sourceforge.net/release/xsl/current/xhtml5/docbook.xsl")
