6087f2a2608f1c841e2a1af51d5fdef98906f965
Dario Lombardo <lomato@gmail.com>
2015-08-13 10:13:52 +0200

    fuzz-test.sh: added missing option in help.
    Change-Id: I7651e18ec6e8338cddc82be90257c5f183804c7c
    Reviewed-on: https://code.wireshark.org/review/10011
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- tools/fuzz-test.sh
+++ tools/fuzz-test.sh
@@ -117,7 +117,7 @@ if [ $FOUND -eq 0 ] ; then
     cat <<FIN
 Error: No valid capture files found.
 
-Usage: `basename $0` [-2] [-b bin_dir] [-C config_profile] [-d work_dir] [-e error probability] [-g] [-a] [-p passes] capture file 1 [capture file 2]...
+Usage: `basename $0` [-2] [-b bin_dir] [-C config_profile] [-d work_dir] [-e error probability] [-o changes offset] [-g] [-a] [-p passes] capture file 1 [capture file 2]...
 FIN
     exit 1
 fi
