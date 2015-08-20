b07ae6c2b2728c4cdc68ea56992e9fa68f38c350
Alexis La Goutte <alexis.lagoutte@gmail.com>
2015-08-20 22:02:50 +0200

    ANSI: Add modelines
    Change-Id: I641a9525ec5a6315cc4a2d97506a885605b3504b
    Reviewed-on: https://code.wireshark.org/review/10170
    Reviewed-by: Hadriel Kaplan <hadrielk@yahoo.com>

--- epan/dissectors/packet-ansi_map.c
+++ epan/dissectors/packet-ansi_map.c
@@ -19562,3 +19562,16 @@ void proto_register_ansi_map(void) {
     register_cleanup_routine(&ansi_map_cleanup);
     register_new_stat_tap_ui(&stat_table);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
