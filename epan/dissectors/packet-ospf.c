wireshark/wireshark

commit c4fe7129d802752aa4cc89a66d2d5d5f2698e020

Small cleanups and improvements   Show the LSA type and length in the summary line   Improve a message to make it clear when we are talking about a LLS TLV   Add an expert item for LLS bit set but data block missing   PI_MALFORMED is an error not a warning.
Change-Id: I516c2a2f0c27fcf101671527d4e4f018e17d3025
Reviewed-on: https://code.wireshark.org/review/10189
Reviewed-by: Jörg Mayer <jmayer@loplof.de>

epan/dissectors/packet-ospf.c
@@ -972,7 +972,7 @@ static void dissect_ospf_ls_req(tvbuff_t*, packet_info*, int, proto_tree*, guint
 static void dissect_ospf_ls_upd(tvbuff_t*, packet_info*, int, proto_tree*, guint8, guint16, guint8);
 static void dissect_ospf_ls_ack(tvbuff_t*, packet_info*, int, proto_tree*, guint8, guint16, guint8);
 static int dissect_ospf_authentication_trailer(tvbuff_t*, int, proto_tree*);
-static void dissect_ospf_lls_data_block(tvbuff_t*, int, proto_tree*, guint8);
+static void dissect_ospf_lls_data_block(tvbuff_t*, packet_info*, int, proto_tree*, guint8);
 
 /* dissect_ospf_v[23]lsa returns the offset of the next LSA
  * if disassemble_body is set to FALSE (e.g. in LSA ACK
@@ -1267,7 +1267,7 @@ dissect_ospf(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 
     /* take care of the LLS data block */
     if (ospf_has_lls_block(tvb, ospf_header_length, packet_type, version)) {
-        dissect_ospf_lls_data_block(tvb, ospflen + crypto_len, ospf_tree,
+        dissect_ospf_lls_data_block(tvb, pinfo, ospflen + crypto_len, ospf_tree,
                                     version);
     }
 
@@ -1289,7 +1289,7 @@ dissect_ospfv2_lls_tlv(tvbuff_t *tvb, int offset, proto_tree *tree)
     length = tvb_get_ntohs(tvb, offset + 2);
 
     ospf_lls_tlv_tree = proto_tree_add_subtree(tree, tvb, offset, length + 4, ett_ospf_lls_tlv,
-                             NULL, val_to_str_const(type, lls_tlv_type_vals, "Unknown TLV"));
+                             NULL, val_to_str_const(type, lls_tlv_type_vals, "Unknown LLS TLV"));
 
     proto_tree_add_item(ospf_lls_tlv_tree, hf_ospf_tlv_type, tvb, offset, 2, ENC_BIG_ENDIAN);
     proto_tree_add_item(ospf_lls_tlv_tree, hf_ospf_tlv_length, tvb, offset + 2, 2, ENC_BIG_ENDIAN);
@@ -1351,7 +1351,7 @@ dissect_ospfv3_lls_tlv(tvbuff_t *tvb, int offset, proto_tree *tree)
         break;
     default:
         ospf_lls_tlv_tree = proto_tree_add_subtree_format(tree, tvb, offset, length + 4, ett_ospf_lls_tlv, NULL,
-                                 "%s", val_to_str_const(type, lls_v3_tlv_type_vals, "Unknown TLV"));
+                                 "%s", val_to_str_const(type, lls_v3_tlv_type_vals, "Unknown LLS TLV"));
     }
 
     if (ti != NULL)
@@ -1429,12 +1429,20 @@ dissect_ospfv3_lls_tlv(tvbuff_t *tvb, int offset, proto_tree *tree)
 
 
 static void
-dissect_ospf_lls_data_block(tvbuff_t *tvb, int offset, proto_tree *tree,
+dissect_ospf_lls_data_block(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *tree,
                             guint8 version)
 {
     proto_tree *ospf_lls_data_block_tree;
     int ospf_lls_len;
     int orig_offset = offset;
+    guint length_remaining;
+
+    length_remaining = tvb_reported_length_remaining(tvb, offset);
+    if (length_remaining < 4) {
+        proto_tree_add_expert_format(tree, pinfo, &ei_ospf_lsa_bad_length,
+		tvb, offset, length_remaining, "LLS option bit set but data block missing");
+	return;
+    }
 
     ospf_lls_len = tvb_get_ntohs(tvb, offset + 2) * 4;
     ospf_lls_data_block_tree = proto_tree_add_subtree(tree, tvb, offset, -1, ett_ospf_lls_data_block, NULL, "OSPF LLS Data Block");
@@ -2441,14 +2449,10 @@ dissect_ospf_v2_lsa(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *t
     ls_length = tvb_get_ntohs(tvb, offset + 18);
     end_offset = offset + ls_length;
 
-    if (disassemble_body) {
-        ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset, ls_length,
-                                 ett_ospf_lsa, &lsa_ti, "%s",
-                                 val_to_str(ls_type, ls_type_vals, "Unknown (%d)"));
-    } else {
-        ospf_lsa_tree = proto_tree_add_subtree(tree, tvb, offset, OSPF_LSA_HEADER_LENGTH,
-                                 ett_ospf_lsa, &lsa_ti, "LSA Header");
-    }
+    ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset, ls_length,
+                                 ett_ospf_lsa, &lsa_ti, "LSA-type %d (%s), len %d",
+                                 ls_type, val_to_str(ls_type, ls_type_vals, "Unknown (%d)"),
+				 ls_length);
 
     proto_tree_add_item(ospf_lsa_tree, hf_ospf_ls_age, tvb,
                         offset, 2, ENC_BIG_ENDIAN);
@@ -3764,7 +3768,7 @@ proto_register_ospf(void)
 
     static ei_register_info ei[] = {
         { &ei_ospf_header_reserved, { "ospf.reserved.not_zero", PI_PROTOCOL, PI_WARN, "incorrect, should be 0", EXPFILL }},
-        { &ei_ospf_lsa_bad_length, { "ospf.lsa.invalid_length", PI_MALFORMED, PI_WARN, "Invalid length", EXPFILL }},
+        { &ei_ospf_lsa_bad_length, { "ospf.lsa.invalid_length", PI_MALFORMED, PI_ERROR, "Invalid length", EXPFILL }},
         { &ei_ospf_lsa_constraint_missing, { "ospf.lsa.tos_missing", PI_MALFORMED, PI_WARN, "Blocks missing", EXPFILL }},
         { &ei_ospf_lsa_bc_error, { "ospf.lsa.bc_error", PI_PROTOCOL, PI_WARN, "BC error", EXPFILL }},
         { &ei_ospf_lsa_unknown_type, { "ospf.lsa.unknown_type", PI_PROTOCOL, PI_WARN, "Unknown LSA Type", EXPFILL }},

commit 5a7f574cbd2af2f271df1a94e682ade7fa607ef9

Fix two bugs   1) Introduced by me in commit c4fe7129d802752aa4cc89a66d2d5d5f2698e020      where I didn't honor the "print lsa header only" flag properly for      data highlighting   2) Introduced in 2001 with commit dd1b7eafafb5748757a0396f4cc6ad6e65f37483      which moved offset increasing into an "if (tree)" statement with code      after the if statement continuing to work with offset. Also make display of v2 and v3 LSAs the same again (forgot one case in previous commit.
bug:11461
Change-Id: I1986c19842f1fa9c8a6d0a7c9a79c64939eeb9d0
Reviewed-on: https://code.wireshark.org/review/10192
Reviewed-by: Jörg Mayer <jmayer@loplof.de>

epan/dissectors/packet-ospf.c
@@ -1562,8 +1562,6 @@ dissect_ospf_db_desc(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *
             dissect_ospf_bitfield(ospf_db_desc_tree, tvb, offset + 3, &bfinfo_dbd);
 
             proto_tree_add_item(ospf_db_desc_tree, hf_ospf_db_dd_sequence, tvb, offset + 4, 4, ENC_BIG_ENDIAN);
-
-            offset += 8;
             break;
 
         case OSPF_VERSION_3:
@@ -1585,10 +1583,16 @@ dissect_ospf_db_desc(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *
             dissect_ospf_bitfield(ospf_db_desc_tree, tvb, offset + 7, &bfinfo_dbd);
 
             proto_tree_add_item(ospf_db_desc_tree, hf_ospf_db_dd_sequence, tvb, offset + 8, 4, ENC_BIG_ENDIAN);
-
-            offset += 12;
             break;
-        }
+	}
+    }
+    switch (version ) {
+    case OSPF_VERSION_2:
+        offset += 8;
+        break;
+    case OSPF_VERSION_3:
+        offset += 12;
+        break;
     }
 
     /* LS Headers will be processed here */
@@ -2449,11 +2453,11 @@ dissect_ospf_v2_lsa(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *t
     ls_length = tvb_get_ntohs(tvb, offset + 18);
     end_offset = offset + ls_length;
 
-    ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset, ls_length,
-                                 ett_ospf_lsa, &lsa_ti, "LSA-type %d (%s), len %d",
-                                 ls_type, val_to_str(ls_type, ls_type_vals, "Unknown (%d)"),
-				 ls_length);
-
+    ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset,
+			disassemble_body?ls_length:OSPF_LSA_HEADER_LENGTH,
+                        ett_ospf_lsa, &lsa_ti, "LSA-type %d (%s), len %d",
+                        ls_type, val_to_str_const(ls_type, ls_type_vals, "Unknown"),
+			ls_length);
     proto_tree_add_item(ospf_lsa_tree, hf_ospf_ls_age, tvb,
                         offset, 2, ENC_BIG_ENDIAN);
     proto_tree_add_item(ospf_lsa_tree, hf_ospf_ls_donotage, tvb,
@@ -2714,14 +2718,11 @@ dissect_ospf_v3_lsa(tvbuff_t *tvb, packet_info *pinfo, int offset, proto_tree *t
     ls_length = tvb_get_ntohs(tvb, offset + 18);
     end_offset = offset + ls_length;
 
-    if (disassemble_body) {
-        ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset, ls_length,
-                                 ett_ospf_lsa, &type_item, "%s (Type: 0x%04x)", val_to_str_const(ls_type, v3_ls_type_vals,"Unknown"), ls_type);
-    } else {
-        ospf_lsa_tree = proto_tree_add_subtree(tree, tvb, offset, OSPF_LSA_HEADER_LENGTH,
-                                 ett_ospf_lsa, &type_item, "LSA Header");
-    }
-
+    ospf_lsa_tree = proto_tree_add_subtree_format(tree, tvb, offset,
+			disassemble_body?ls_length:OSPF_LSA_HEADER_LENGTH,
+                        ett_ospf_lsa, &type_item, "LSA-type %d (%s), len %d",
+                        ls_type, val_to_str_const(ls_type, v3_ls_type_vals, "Unknown"),
+			ls_length);
     proto_tree_add_item(ospf_lsa_tree, hf_ospf_ls_age, tvb, offset, 2, ENC_BIG_ENDIAN);
     proto_tree_add_item(ospf_lsa_tree, hf_ospf_v3_lsa_do_not_age, tvb, offset, 2, ENC_BIG_ENDIAN);
 