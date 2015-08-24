cc15b11883238938eb354e81583433cd789a4243
Alexis La Goutte <alexis.lagoutte@gmail.com>
2015-08-24 10:29:18 +0200

    Multicast Stream: Fix Dereference of null pointer found by Clang analyzer
    Change-Id: I419f49132f8b0853c17b2fd1b4e9869ef3fafd13
    Reviewed-on: https://code.wireshark.org/review/10224
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- ui/mcast_stream.c
+++ ui/mcast_stream.c
@@ -119,10 +119,12 @@ static void
 mcaststream_reset_cb(void *ti_ptr)
 {
     mcaststream_tapinfo_t *tapinfo = (mcaststream_tapinfo_t *)ti_ptr;
-    if (tapinfo && tapinfo->tap_reset) {
-        tapinfo->tap_reset(ti_ptr);
+    if (tapinfo) {
+        if (tapinfo->tap_reset) {
+           tapinfo->tap_reset(ti_ptr);
+        }
+        mcaststream_reset(tapinfo);
     }
-    mcaststream_reset(tapinfo);
 }
 
 /****************************************************************************/
