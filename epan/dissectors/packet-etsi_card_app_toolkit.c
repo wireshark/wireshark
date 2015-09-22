4abed38c25c1d3ddd4dbd7f77e602ca9518f8ca6
Pascal Quantin <pascal.quantin@gmail.com>
2015-09-22 18:32:03 +0200

    ETSI CAT: always initialize gsm_sms_data_t structure
    Otherwise a 8-bit SMS-PP Data Download could be interpreted as requiring SMS packing
    
    Change-Id: I50b5e59194acc3d69d0e247fc909d3f96207094a
    Reviewed-on: https://code.wireshark.org/review/10610
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-etsi_card_app_toolkit.c
+++ epan/dissectors/packet-etsi_card_app_toolkit.c
@@ -965,7 +965,7 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 	tvbuff_t *new_tvb;
 	gboolean ims_event = FALSE, dns_server = FALSE;
 	guint length = tvb_reported_length(tvb);
-	gsm_sms_data_t sms_data;
+	gsm_sms_data_t sms_data = {0};
 
 	cat_ti = proto_tree_add_item(tree, proto_cat, tvb, 0, -1, ENC_NA);
 	cat_tree = proto_item_add_subtree(cat_ti, ett_cat);
