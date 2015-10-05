24b5300411cce612aef32aec289693f9c8f79869
Dario Lombardo <lomato@gmail.com>
2015-10-05 11:55:09 +0200

    lemon: add free() to avoid mem leak (CID 1309088)
    Change-Id: Id32864d324f72ffee9c1033f0f36a79eb2651334
    Reviewed-on: https://code.wireshark.org/review/10797
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- tools/lemon/lemon.c
+++ tools/lemon/lemon.c
@@ -2707,9 +2707,10 @@ void Parse(struct lemon *gp)
   filesize = ftell(fp);
   rewind(fp);
   filebuf = (char *)malloc( filesize+1 );
-  if( filesize>100000000 || filebuf==0 ){
+  if( filesize>100000000 || filebuf==NULL ){
     ErrorMsg(ps.filename,0,"Input file too large.");
     gp->errorcnt++;
+    free(filebuf);
     fclose(fp);
     return;
   }
