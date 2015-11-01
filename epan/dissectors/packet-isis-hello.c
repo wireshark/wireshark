56d01272474f9022a0ace0011f649dd59f573813
Jim Young <jyoung@gsu.edu>
2015-11-01 01:40:41 -0500

    ISIS Hello: Restore correct dissection of SPB BVID
    Commit 301a5b7ceac60d0569ce1717e839241d1072400 introduced a small
    regression with the dissection of the BVID.  BVID should be extracted
    from the 12 most significant bits not the 12 least significant bits.
    Make it so.
    
    Change-Id: Idcf0492eea1f6cb7282641ae243aa7092e5a1418
    Reviewed-on: https://code.wireshark.org/review/11472
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-isis-hello.c
+++ epan/dissectors/packet-isis-hello.c
@@ -1391,7 +1391,7 @@ proto_register_isis_hello(void)
       { &hf_isis_hello_digest_a, { "A", "isis.hello.digest.a", FT_UINT8, BASE_DEC, NULL, 0x0c, NULL, HFILL }},
       { &hf_isis_hello_digest_d, { "D", "isis.hello.digest.d", FT_UINT8, BASE_DEC, NULL, 0x03, NULL, HFILL }},
       { &hf_isis_hello_ect, { "ECT", "isis.hello.ect", FT_BYTES, SEP_DASH, NULL, 0x0, NULL, HFILL }},
-      { &hf_isis_hello_bvid, { "BVID", "isis.hello.bvid", FT_UINT16, BASE_HEX_DEC, NULL, 0x0FFF, NULL, HFILL }},
+      { &hf_isis_hello_bvid, { "BVID", "isis.hello.bvid", FT_UINT16, BASE_HEX_DEC, NULL, 0xFFF0, NULL, HFILL }},
       { &hf_isis_hello_bvid_u, { "U", "isis.hello.bvid.u", FT_UINT16, BASE_HEX_DEC, NULL, 0x0008, NULL, HFILL }},
       { &hf_isis_hello_bvid_m, { "M", "isis.hello.bvid.m", FT_UINT16, BASE_HEX_DEC, NULL, 0x0004, NULL, HFILL }},
       { &hf_isis_hello_area_address, { "Srea address", "isis.hello.area_address", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
