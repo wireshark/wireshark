1799834898dae1c22c8e65746983bc5a031030c3
Michael Mann <mmann78@netscape.net>
2015-11-06 10:07:49 -0500

    Fix crash in internals menu dissector tables dialog by supporting FT_GUID type.
    Change-Id: I1e4700b505746c49ea518dc401be590740720a57
    Reviewed-on: https://code.wireshark.org/review/11600
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- ui/gtk/dissector_tables_dlg.c
+++ ui/gtk/dissector_tables_dlg.c
@@ -152,7 +152,13 @@ decode_proto_add_to_list (const gchar *table_name _U_, ftenum_t selector_type,
             break;
 
         case FT_BYTES:
-            proto_add_to_list(tree_info, store, (gchar*)dissector_handle_get_dissector_name(handle), proto_name);
+        case FT_GUID:
+            str = (gchar*)dissector_handle_get_dissector_name(handle);
+            if (str == NULL)
+            {
+                str = (gchar*)"<Unknown>";
+            }
+            proto_add_to_list(tree_info, store, str, proto_name);
             break;
 
         default:
@@ -239,6 +245,7 @@ display_dissector_table_names(const char *table_name, const char *ui_name, void 
             table_name_add_to_list(tree_info, dis_tbl_trees->str_tree_wgt, table_name, ui_name);
             break;
         case FT_BYTES:
+        case FT_GUID:
             table_name_add_to_list(tree_info, dis_tbl_trees->custom_tree_wgt, table_name, ui_name);
             break;
         default:
