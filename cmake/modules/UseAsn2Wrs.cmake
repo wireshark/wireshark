a198f47c06723fabef174cb3983b26ead24f5bc9
Peter Wu <peter@lekensteyn.nl>
2015-11-24 23:18:44 +0100

    cmake: add asn1 target, avoid unnecessary rebuilds
    Add an "asn1" build target that rebuilds all ASN.1 dissectors.
    
    Do not always generate the ASN.1 dissector when invoking the
    "generate_dissector-PROTO" target. Use a special "packet-PROTO-stamp"
    file to detect whether the dissector is out of date.
    
    This indirection is needed because cmake removes output files from
    add_custom_command in the clean target, but we would like to keep the
    generated packet-PROTO.c files.
    
    Make all EXTRA_CNF and EXPORT_DEPENDS point to absolute paths for
    generated exported configs (PROTO-exp.cnf). This is done by automake too
    and is needed for proper dependency tracking. Example: when h225.cnf is
    updated, h225-exp.cnf needs to be generated. That is the purpose of the
    "generate-h225-exp.cnf" target. Now h235 (and other) dissectors need to
    be regenerated as well because the h225-exp.cnf is updated. This is why
    there is a separate add_custom_command and add_custom_target.
    
    Change-Id: I8cfc1f8f43cea595267b21eae30b4548f3407c0b
    Reviewed-on: https://code.wireshark.org/review/12080
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- cmake/modules/UseAsn2Wrs.cmake
+++ cmake/modules/UseAsn2Wrs.cmake
@@ -1,58 +1,88 @@
 # - Convert ASN.1 file into C source and header files that can be used to create a wireshark dissector
+#
+#  A2W_OUTPUT_DIR   - directory where the generated packet-${PROTOCOL_NAME}.c is
+#                     saved. The default location is meant for the Wireshark
+#                     source tree. For external dissectors, set it to the
+#                     absolute path (e.g. "${CMAKE_CURRENT_SOURCE_DIR}").
 
-MACRO(ASN2WRS)
+function(ASN2WRS)
 	include(LocatePythonModule)
-	locate_python_module(asn2wrs REQUIRED PATHS ${CMAKE_SOURCE_DIR}/tools)
+	locate_python_module(asn2wrs REQUIRED PATHS "${CMAKE_SOURCE_DIR}/tools")
 
-	set( DISSECTOR ${CMAKE_SOURCE_DIR}/epan/dissectors/packet-${PROTOCOL_NAME}.c )
-
-	if ( NOT PROTO_OPT )
-		set( PROTO_OPT -p ${PROTOCOL_NAME} )
-	elseif ( PROTO_OPT STREQUAL "_EMPTY_" )
-		set( PROTO_OPT )
+	if(NOT PROTO_OPT)
+		set(PROTO_OPT -p ${PROTOCOL_NAME})
+	elseif(PROTO_OPT STREQUAL "_EMPTY_")
+		set(PROTO_OPT)
 	endif()
 
-	# Backwards compability for build in dissectors,
-	# set to '_EMPTY_' for out of source dissector builds
-	if ( NOT A2W_OUTPUT_DIR )
-		set (A2W_OUTPUT_DIR -O ${CMAKE_SOURCE_DIR}/epan/dissectors)
-	elseif ( A2W_OUTPUT_DIR STREQUAL "_EMPTY_" )
-		set( A2W_OUTPUT_DIR )
+	if(NOT A2W_OUTPUT_DIR)
+		set(A2W_OUTPUT_DIR "${CMAKE_SOURCE_DIR}/epan/dissectors")
 	endif()
 
-	# Don't use packet-${PROTOCOL_NAME}.c instead of generate_dissector, it will
-	# cause EXCLUDE_FROM_ALL to be ignored.
-	ADD_CUSTOM_TARGET(generate_dissector-${PROTOCOL_NAME} ALL
-		COMMAND ${PYTHON_EXECUTABLE}
-		  ${PY_ASN2WRS}
-		  ${A2W_FLAGS}
-		  ${PROTO_OPT}
-		  -c ${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf
-		  -s ${CMAKE_CURRENT_SOURCE_DIR}/packet-${PROTOCOL_NAME}-template
-		  -D ${CMAKE_CURRENT_SOURCE_DIR}
-		  ${A2W_OUTPUT_DIR}
-		  ${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
+	set(DISSECTOR "${A2W_OUTPUT_DIR}/packet-${PROTOCOL_NAME}.c")
+
+	# Besides the file dependency (for timestamp comparison), add a target such
+	# that other directories can request it to be built (ordering dependency).
+	foreach(_v EXTRA_CNF EXPORT_DEPENDS)
+		set(${_v}_targets)
+		foreach(entry IN LISTS ${_v})
+			string(REGEX MATCH "[^/]+-exp\\.cnf$" _exp_cnf_filename "${entry}")
+			if(_exp_cnf_filename)
+				list(APPEND ${_v}_targets generate-${_exp_cnf_filename})
+			endif()
+		endforeach()
+	endforeach()
+
+	# Creates a dissector in the source directory and store the timestamp.
+	add_custom_command(
+		OUTPUT packet-${PROTOCOL_NAME}-stamp
+		COMMAND "${PYTHON_EXECUTABLE}"
+			${PY_ASN2WRS}
+			${A2W_FLAGS}
+			${PROTO_OPT}
+			-c "${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf"
+			-s "${CMAKE_CURRENT_SOURCE_DIR}/packet-${PROTOCOL_NAME}-template"
+			-D "${CMAKE_CURRENT_SOURCE_DIR}"
+			-O "${A2W_OUTPUT_DIR}"
+			${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
+		COMMAND
+			"${PYTHON_EXECUTABLE}" -c
+				"import shutil, sys; x,s,d=sys.argv; open(d, 'w'); shutil.copystat(s, d)"
+				"${A2W_OUTPUT_DIR}/packet-${PROTOCOL_NAME}.c"
+				packet-${PROTOCOL_NAME}-stamp
 		DEPENDS
-		  ${PY_ASN2WRS}
-		  ${SRC_FILES}
-		  ${EXTRA_CNF}
+			"${PY_ASN2WRS}"
+			${SRC_FILES}
+			${EXTRA_CNF_targets}
+			${EXTRA_CNF}
+		VERBATIM
 	)
 
-	foreach( _asn2wrs_export_file IN LISTS EXPORT_FILES )
-		ADD_CUSTOM_TARGET( ${_asn2wrs_export_file}
-			WORKING_DIRECTORY .
-			COMMAND ${PYTHON_EXECUTABLE}
-			  ${PY_ASN2WRS}
-			  -E
-			  ${A2W_FLAGS}
-			  ${PROTO_OPT}
-			  -c ${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf
-			  -D ${CMAKE_CURRENT_SOURCE_DIR}
-			  ${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
+	add_custom_target(generate_dissector-${PROTOCOL_NAME} ALL
+		DEPENDS packet-${PROTOCOL_NAME}-stamp
+	)
+
+	foreach(_asn2wrs_export_file IN LISTS EXPORT_FILES)
+		add_custom_command(
+			OUTPUT ${_asn2wrs_export_file}
+			COMMAND "${PYTHON_EXECUTABLE}"
+				"${PY_ASN2WRS}"
+				-E
+				${A2W_FLAGS}
+				${PROTO_OPT}
+				-c "${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf"
+				-D "${CMAKE_CURRENT_SOURCE_DIR}"
+				${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
 			DEPENDS
-			  ${PY_ASN2WRS}
-			  ${SRC_FILES}
-			  ${EXPORT_DEPENDS}
+				"${PY_ASN2WRS}"
+				${SRC_FILES}
+				${EXPORT_DEPENDS_targets}
+				${EXPORT_DEPENDS}
+		)
+		# This target enables other dissectors to trigger the -exp cnf build
+		add_custom_target(generate-${_asn2wrs_export_file}
+			DEPENDS ${_asn2wrs_export_file}
 		)
 	endforeach()
-ENDMACRO()
+
+endfunction()
