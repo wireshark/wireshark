bcb31916f5d356eb1578fdfa79dd3fd99d670030
Alexis La Goutte <alexis.lagoutte@gmail.com>
2015-11-25 11:51:51 +0100

    TACACS: fix indent (and add modelines)
    Change-Id: Ia8d52e9d8f8600c28988dd8d3cc11c7fe08943a3
    Reviewed-on: https://code.wireshark.org/review/12131
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-tacacs.h
+++ epan/dissectors/packet-tacacs.h
@@ -427,3 +427,16 @@ static const value_string tacplus_acct_flags[] = {
 #endif
 
 #endif   /* __PACKET_TACACS_H__ */
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
