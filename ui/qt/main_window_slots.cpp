c965fb06148d0144426ba1b3fb301281a30a39b8
Pascal Quantin <pascal.quantin@gmail.com>
2015-11-26 15:57:32 +0100

    Qt: make Files Set dialog modeless
    Otherwise you need to close the window each time you change the file
    so as to browse its content...
    
    Change-Id: Ibf8485695cec34bb950ad98cb671e77d4f663b63
    Reviewed-on: https://code.wireshark.org/review/12187
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- ui/qt/main_window_slots.cpp
+++ ui/qt/main_window_slots.cpp
@@ -1569,7 +1569,7 @@ void MainWindow::on_actionFileSaveAs_triggered()
 
 void MainWindow::on_actionFileSetListFiles_triggered()
 {
-    file_set_dialog_.exec();
+    file_set_dialog_.show();
 }
 
 void MainWindow::on_actionFileSetNextFile_triggered()
