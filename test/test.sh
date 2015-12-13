37d5b927361db41c93d72a8b39962b26ae8f211a
Guy Harris <guy@alum.mit.edu>
2015-12-13 10:05:06 -0800

    Explicitly specify the template for the mktemp command.
    Not all versions of mktemp support omitting the template; in particular,
    the one provided by some BSD-flavored OSes don't.
    
    Change-Id: I657e002559dce165c677a473aa10bb17cc506037
    Reviewed-on: https://code.wireshark.org/review/12592
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit e01f8fb3ad4e635a09f8beb88cb1fcc0baeb0232)
    Reviewed-on: https://code.wireshark.org/review/12594

--- test/test.sh
+++ test/test.sh
@@ -66,7 +66,7 @@ source test-backend.sh
 source config.sh
 
 # needed by some tests
-TEST_OUTDIR=$(mktemp -d)
+TEST_OUTDIR=$(mktemp -d wstest.XXXXXXXXXX)
 TEST_OUTDIR_CLEAN=${TEST_OUTDIR_CLEAN:-1}
 if [ -z "$TEST_OUTDIR" ] || ! cd "$TEST_OUTDIR"; then
 	# If for any reason the temporary tests output directory cannot be created...
