cefe1881b2ecf71a7f26f21e0a7a9f1f660a4c7f
Guy Harris <guy@alum.mit.edu>
2015-12-29 17:41:33 -0800

    Expand a comment.
    Also fix the capitalization of "Qt", and don't have a commented-out
    definition of GTK_VERSION to 3.5.2; if you want GTK+ 3.5.2, you'd do it
    by setting the environment variable GTK_VERSION to 3.5.2 when running
    the script.
    
    Change-Id: I71a5d8caf87c08a5f98730aea65f994aff611590
    Reviewed-on: https://code.wireshark.org/review/12939
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- macosx-setup.sh
+++ macosx-setup.sh
@@ -69,11 +69,12 @@ PKG_CONFIG_VERSION=0.28
 #
 # One or more of the following libraries are required to build Wireshark.
 #
-# To override the versions of QT and GTK call the script with some of the
+# To override the versions of Qt and GTK call the script with some of the
 # variables set to the new values. Setting a variable to empty will disable
-# the toolkit, e.g.
-# "GTK_VERSION=3.5.2 QT_VERSION= ./macos-setup.sh"
-# will build with GTK3 and without QT.
+# building the toolkit and will un-install any version previously installed
+# by the script, e.g. "GTK_VERSION=3.5.2 QT_VERSION= ./macos-setup.sh"
+# will build and install with GTK+ 3.5.2 and will not install Qt (and,
+# if the script installed Qt earlier, will un-install that version of Qt).
 #
 # Note that Qt 5, prior to 5.5.0, mishandles context menus in ways that,
 # for example, cause them not to work reliably in the packet detail or
@@ -82,7 +83,6 @@ PKG_CONFIG_VERSION=0.28
 #
 QT_VERSION=${QT_VERSION-5.5.0}
 GTK_VERSION=${GTK_VERSION-2.24.17}
-#GTK_VERSION=3.5.2
 if [ "$GTK_VERSION" ]; then
     #
     # We'll be building GTK+, so we need some additional libraries.
