d74f15068b6667d61eca6dbb73e03718f60c5a5b
Guy Harris <guy@alum.mit.edu>
2016-01-07 21:15:16 -0800

    Remove trailing whitespace.
    Change-Id: Ic115862d4bf9f2847c768cd539d83883e32eeed2
    Reviewed-on: https://code.wireshark.org/review/13119
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-lat.c
+++ epan/dissectors/packet-lat.c
@@ -4,22 +4,22 @@
  * Ethereal - Network traffic analyzer
  * By Gerald Combs <gerald@ethereal.com>
  * Copyright 1998 Gerald Combs
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
- 
+
 #include "config.h"
 
 #ifdef HAVE_SYS_TYPES_H
@@ -120,7 +120,7 @@ static int
 dissect_lat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
 	int offset = 0;
-	proto_item *ti; 
+	proto_item *ti;
 	proto_tree *lat_tree = NULL;
 	guint8 command;
 
