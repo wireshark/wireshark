62a7fb7b86144ba887501a2e035184e3f640cd53
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-02-05 14:21:22 +0100

    Multipath: use directly value of sizeof(guint32)
    it will be also make happy OS X x64 buildbot
    
    Change-Id: Ib718d717719739314170632f04b3ec68c2917ed6
    Reviewed-on: https://code.wireshark.org/review/13730
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-multipart.c
+++ epan/dissectors/packet-multipart.c
@@ -614,8 +614,8 @@ dissect_kerberos_encrypted_message(tvbuff_t *tvb, packet_info *pinfo, proto_tree
     gint offset = 0, len;
     guint8 *data;
 
-    proto_tree_add_item(tree, hf_multipart_sec_token_len, tvb, offset, sizeof(guint32), ENC_LITTLE_ENDIAN);
-    offset += sizeof(guint32);
+    proto_tree_add_item(tree, hf_multipart_sec_token_len, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+    offset += 4;
     len = tvb_reported_length_remaining(tvb, offset);
 
     DISSECTOR_ASSERT(tvb_bytes_exist(tvb, offset, len));
