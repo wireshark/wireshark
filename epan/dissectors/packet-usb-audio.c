4d460475784771515be00ae493b571c88b549a6e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-02-08 07:57:26 +0100

    USB Audio: Fix cannot optimize loop, the loop counter may overflow [-Wunsafe-loop-optimizations]
    Change-Id: Ie50ecb88ad1ea5f95b5772d71a6a4b02f06c4e2f
    Reviewed-on: https://code.wireshark.org/review/13817
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: João Valverde <j@v6e.pt>
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>

--- epan/dissectors/packet-usb-audio.c
+++ epan/dissectors/packet-usb-audio.c
@@ -490,9 +490,8 @@ dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tre
     usb_conv_info_t *usb_conv_info;
     proto_tree      *tree;
     proto_item      *ti;
-    gint             offset = 0;
-    guint            length = tvb_reported_length(tvb);
-
+    gint             offset, length;
+    gint             i;
 
     /* Reject the packet if data is NULL */
     if (data == NULL)
@@ -501,15 +500,18 @@ dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tre
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "USBAUDIO");
 
-    ti   = proto_tree_add_protocol_format(parent_tree, proto_usb_audio, tvb, offset, -1, "USB Audio");
+    ti   = proto_tree_add_protocol_format(parent_tree, proto_usb_audio, tvb, 0, -1, "USB Audio");
     tree = proto_item_add_subtree(ti, ett_usb_audio);
+
+    length = tvb_reported_length(tvb);
+    offset = 0;
 
     switch (usb_conv_info->interfaceSubclass)
     {
         case AUDIO_IF_SUBCLASS_MIDISTREAMING:
             col_set_str(pinfo->cinfo, COL_INFO, "USB-MIDI Event Packets");
 
-            while ((guint) offset < length)
+            for (i = 0; i < length / 4; i++)
             {
                 dissect_usb_midi_event(tvb, pinfo, tree, parent_tree, offset);
                 offset += 4;
@@ -519,7 +521,7 @@ dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tre
             proto_tree_add_expert(tree, pinfo, &ei_usb_audio_undecoded, tvb, offset, length);
     }
 
-    return tvb_reported_length(tvb);
+    return length;
 }
 
 static void
