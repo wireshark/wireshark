139297f79a819d47c13114e8f8bdd6d84d46b004
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-02-10 12:23:55 +0100

    atn ulcs/cpdlc: fix ia5alpha’ defined but not used [-Werror=unused-const-variable] found by gcc6
    Change-Id: I4483225d857636d3b76b6b03cab86868c366521d
    Reviewed-on: https://code.wireshark.org/review/13874
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-atn-cpdlc.c
+++ epan/dissectors/packet-atn-cpdlc.c
@@ -66,6 +66,19 @@ void proto_reg_handoff_atn_cpdlc(void);
 
 static const char *object_identifier_id;
 
+/* IA5 charset (7-bit) for PER IA5 decoding */
+static const gchar ia5alpha[] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, \
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, \
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, \
+    0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, \
+    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, \
+    0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, \
+    0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f, \
+    0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, \
+    0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f, '\0'
+};
+
 /* forward declarations */
 static int dissect_GroundPDUs_PDU(
     tvbuff_t *tvb _U_,
@@ -694,7 +707,7 @@ static int hf_atn_cpdlc_windSpeedEnglish = -1;    /* WindSpeedEnglish */
 static int hf_atn_cpdlc_windSpeedMetric = -1;     /* WindSpeedMetric */
 
 /*--- End of included file: packet-atn-cpdlc-hf.c ---*/
-#line 84 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
+#line 97 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
 
 
 /*--- Included file: packet-atn-cpdlc-ett.c ---*/
@@ -843,7 +856,7 @@ static gint ett_atn_cpdlc_Winds = -1;
 static gint ett_atn_cpdlc_WindSpeed = -1;
 
 /*--- End of included file: packet-atn-cpdlc-ett.c ---*/
-#line 86 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
+#line 99 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
 static gint ett_atn_cpdlc = -1;
 
 
@@ -5294,7 +5307,7 @@ static int dissect_ProtectedAircraftPDUs_PDU(tvbuff_t *tvb _U_, packet_info *pin
 
 
 /*--- End of included file: packet-atn-cpdlc-fn.c ---*/
-#line 89 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
+#line 102 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
 
 /* Wireshark ID of CPDLC protocol */
 static int proto_atn_cpdlc = -1;
@@ -7918,7 +7931,7 @@ void proto_register_atn_cpdlc (void)
         NULL, HFILL }},
 
 /*--- End of included file: packet-atn-cpdlc-hfarr.c ---*/
-#line 304 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
+#line 317 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
       };
 
     static gint *ett[] = {
@@ -8069,7 +8082,7 @@ void proto_register_atn_cpdlc (void)
     &ett_atn_cpdlc_WindSpeed,
 
 /*--- End of included file: packet-atn-cpdlc-ettarr.c ---*/
-#line 308 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
+#line 321 "../../asn1/atn-cpdlc/packet-atn-cpdlc-template.c"
         &ett_atn_cpdlc
     };
 
