072f0856cb546cd31df653af18acbad7c7edb037
Michael Mann <mmann78@netscape.net>
2016-02-21 12:30:47 -0500

    packet-amqp.c: Fix multiple fields with incompatible types
    Change-Id: I68b7fa0b5d7fae86289807d7ef01a2141dcb8ff6
    Reviewed-on: https://code.wireshark.org/review/14059
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>

--- epan/dissectors/packet-amqp.c
+++ epan/dissectors/packet-amqp.c
@@ -3667,7 +3667,7 @@ dissect_amqp_0_10_connection(tvbuff_t *tvb,
                                      hf_amqp_0_10_method_connection_open_capabilities,
                                      tvb,
                                      offset,
-                                     arg_length, ENC_NA);
+                                     arg_length, ENC_ASCII|ENC_NA);
             dissect_amqp_0_10_array (tvb,
                                      pinfo,
                                      offset,
@@ -11929,19 +11929,19 @@ proto_register_amqp(void)
             FT_BYTES, BASE_NONE, NULL, 0,
             "SASL outcome additional data", HFILL}},
         {&hf_amqp_1_0_outgoingLocales_sym, {
-            "Outgoing-Locales", "amqp.performative.arguments.outgoingLocales",
+            "Outgoing-Locales", "amqp.performative.arguments.outgoingLocales_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_1_0_incomingLocales_sym, {
-            "Incoming-Locales", "amqp.performative.arguments.incomingLocales",
+            "Incoming-Locales", "amqp.performative.arguments.incomingLocales_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_1_0_offeredCapabilities_sym, {
-            "Offered-Capabilities", "amqp.arguments.offeredCapabilities",
+            "Offered-Capabilities", "amqp.arguments.offeredCapabilities_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_1_0_desiredCapabilities_sym, {
-            "Desired-Capabilities", "amqp.performative.arguments.desiredCapabilities",
+            "Desired-Capabilities", "amqp.performative.arguments.desiredCapabilities_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_1_0_address_str, {
@@ -11957,11 +11957,11 @@ proto_register_amqp(void)
             FT_STRING, BASE_NONE, NULL, 0,
             "Target for messages", HFILL}},
         {&hf_amqp_1_0_outcomes_sym, {
-            "Outcomes", "amqp.properties.outcomes",
+            "Outcomes", "amqp.properties.outcomes_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             "Outcomes descriptors for the link", HFILL}},
         {&hf_amqp_1_0_capabilities_sym, {
-            "Capabilities", "amqp.properties.capabilities",
+            "Capabilities", "amqp.properties.capabilities_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             "Extension capabilities of the sender", HFILL}},
         {&hf_amqp_1_0_messageId_uint, {
@@ -12005,7 +12005,7 @@ proto_register_amqp(void)
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_1_0_mechanisms_sym, {
-            "Mechanisms", "amqp.sasl.mechanisms",
+            "Mechanisms", "amqp.sasl.mechanisms_sym",
             FT_STRING, BASE_NONE, NULL, 0,
             "Supported security mechanisms", HFILL}},
         {&hf_amqp_0_10_format, {
@@ -12846,7 +12846,7 @@ proto_register_amqp(void)
             NULL, HFILL}},
         {&hf_amqp_0_10_method_connection_start_mechanisms, {
             "Mechanisms", "amqp.method.arguments.mechanisms",
-            FT_NONE, BASE_NONE, NULL, 0,
+            FT_BYTES, BASE_NONE, NULL, 0,
             "Supported security mechanisms", HFILL}},
         {&hf_amqp_0_9_method_connection_start_locales, {
             "Locales", "amqp.method.arguments.locales",
@@ -12854,7 +12854,7 @@ proto_register_amqp(void)
             NULL, HFILL}},
         {&hf_amqp_0_10_method_connection_start_locales, {
             "Locales", "amqp.method.arguments.locales",
-            FT_NONE, BASE_NONE, NULL, 0,
+            FT_BYTES, BASE_NONE, NULL, 0,
             "Supported message locales", HFILL}},
         {&hf_amqp_method_connection_start_ok_client_properties, {
             "Client-Properties", "amqp.method.arguments.client_properties",
@@ -12930,7 +12930,7 @@ proto_register_amqp(void)
             NULL, HFILL}},
         {&hf_amqp_0_10_method_connection_open_capabilities, {
             "Capabilities", "amqp.method.arguments.capabilities",
-            FT_NONE, BASE_NONE, NULL, 0,
+            FT_STRING, BASE_NONE, NULL, 0,
             "Required capabilities", HFILL}},
         {&hf_amqp_0_9_method_connection_open_insist, {
             "Insist", "amqp.method.arguments.insist",
@@ -12945,8 +12945,8 @@ proto_register_amqp(void)
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_0_10_method_connection_open_ok_known_hosts, {
-            "Known-Hosts", "amqp.method.arguments.known_hosts",
-            FT_NONE, BASE_NONE, NULL, 0,
+            "Known-Hosts", "amqp.method.arguments.known_hosts_bytes",
+            FT_BYTES, BASE_NONE, NULL, 0,
             "Equivalent or alternate hosts for reconnection", HFILL}},
         {&hf_amqp_method_connection_redirect_host, {
             "Host", "amqp.method.arguments.host",
@@ -12957,8 +12957,8 @@ proto_register_amqp(void)
             FT_STRING, BASE_NONE, NULL, 0,
             NULL, HFILL}},
         {&hf_amqp_0_10_method_connection_redirect_known_hosts, {
-            "Known-Hosts", "amqp.method.arguments.known_hosts",
-            FT_NONE, BASE_NONE, NULL, 0,
+            "Known-Hosts", "amqp.method.arguments.known_hosts_bytes",
+            FT_BYTES, BASE_NONE, NULL, 0,
             "Equivalent or alternate hosts to redirect to", HFILL}},
         {&hf_amqp_0_9_method_connection_close_reply_code, {
             "Reply-Code", "amqp.method.arguments.reply_code",
