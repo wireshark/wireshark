wireshark/wireshark

commit f5340b2a07a0a5f6830a091283d3170195203b82

Split init routine into init/cleanup for more dissectors
This patch converts some dissectors using g_hash_table_foreach_remove.

 - 9p: drop no-op free func.
 - nfs: use g_hash_table_new_full such that a destructor function can
   be used. Drop NULL check since g_free can handle these just fine.
 - nlm: use g_hash_table_new_full such that a destructor function can
   be used. Simplify "matched" destruction by replacing the wrapper by
   a direct g_free call.

Change-Id: I455e7f0ad4e47e70dae05af7233fdcdebf583f9f
Reviewed-on: https://code.wireshark.org/review/9224
Reviewed-by: Michael Mann <mmann78@netscape.net>

epan/dissectors/packet-nfs.c
@@ -949,55 +949,37 @@ nfs_name_snoop_unmatched_hash(gconstpointer k)
 }
 
 
-static gboolean
-nfs_name_snoop_unmatched_free_all(gpointer key_arg _U_, gpointer value, gpointer user_data _U_)
+static void
+nfs_name_snoop_value_destroy(gpointer value)
 {
 	nfs_name_snoop_t *nns = (nfs_name_snoop_t *)value;
 
-	if (nns->name) {
-		g_free((gpointer)nns->name);
-		nns->name = NULL;
-		nns->name_len = 0;
-	}
-	if (nns->full_name) {
-		g_free((gpointer)nns->full_name);
-		nns->full_name = NULL;
-		nns->full_name_len = 0;
-	}
-	if (nns->parent) {
-		g_free((gpointer)nns->parent);
-		nns->parent = NULL;
-		nns->parent_len = 0;
-	}
-	if (nns->fh) {
-		g_free((gpointer)nns->fh);
-		nns->fh = NULL;
-		nns->fh_length = 0;
-	}
+	g_free((gpointer)nns->name);
+	g_free((gpointer)nns->full_name);
+	g_free((gpointer)nns->parent);
+	g_free((gpointer)nns->fh);
 	g_free(nns);
-	return TRUE;
 }
 
 
 static void
 nfs_name_snoop_init(void)
 {
-	if (nfs_name_snoop_unmatched != NULL) {
-		g_hash_table_foreach_remove(nfs_name_snoop_unmatched,
-				nfs_name_snoop_unmatched_free_all, NULL);
-	} else {
-		/* The fragment table does not exist. Create it */
-		nfs_name_snoop_unmatched = g_hash_table_new(nfs_name_snoop_unmatched_hash,
-			nfs_name_snoop_unmatched_equal);
-	}
-	if (nfs_name_snoop_matched != NULL) {
-		g_hash_table_foreach_remove(nfs_name_snoop_matched,
-				nfs_name_snoop_unmatched_free_all, NULL);
-	} else {
-		/* The fragment table does not exist. Create it */
-		nfs_name_snoop_matched = g_hash_table_new(nfs_name_snoop_matched_hash,
-			nfs_name_snoop_matched_equal);
-	}
+	nfs_name_snoop_unmatched =
+		g_hash_table_new_full(nfs_name_snoop_unmatched_hash,
+		nfs_name_snoop_unmatched_equal,
+		NULL, nfs_name_snoop_value_destroy);
+	nfs_name_snoop_matched =
+		g_hash_table_new_full(nfs_name_snoop_matched_hash,
+		nfs_name_snoop_matched_equal,
+		NULL, nfs_name_snoop_value_destroy);
+}
+
+static void
+nfs_name_snoop_cleanup(void)
+{
+	g_hash_table_destroy(nfs_name_snoop_unmatched);
+	g_hash_table_destroy(nfs_name_snoop_matched);
 }
 
 
@@ -12629,6 +12611,7 @@ proto_register_nfs(void)
 	nfs_file_handles        = wmem_tree_new_autoreset(wmem_epan_scope(), wmem_file_scope());
 	nfs_fhandle_frame_table = wmem_tree_new_autoreset(wmem_epan_scope(), wmem_file_scope());
 	register_init_routine(nfs_name_snoop_init);
+	register_cleanup_routine(nfs_name_snoop_cleanup);
 
 	register_decode_as(&nfs_da);
 }

epan/dissectors/packet-9p.c
@@ -1013,11 +1013,6 @@ static guint _9p_hash_hash(gconstpointer k)
 	return (key->conv_index ^ key->tag ^ key->fid);
 }
 
-static gboolean _9p_hash_free_all(gpointer key _U_, gpointer value _U_, gpointer user_data _U_)
-{
-	return TRUE;
-}
-
 static void _9p_hash_free_val(gpointer value)
 {
 	struct _9p_hashval *val = (struct _9p_hashval *)value;
@@ -1043,11 +1038,12 @@ static struct _9p_hashval *_9p_hash_new_val(gsize len)
 
 static void _9p_hash_init(void)
 {
-	if (_9p_hashtable != NULL) {
-		g_hash_table_foreach_remove(_9p_hashtable, _9p_hash_free_all, NULL);
-	} else {
-		_9p_hashtable = g_hash_table_new_full(_9p_hash_hash, _9p_hash_equal, g_free, _9p_hash_free_val);
-	}
+	_9p_hashtable = g_hash_table_new_full(_9p_hash_hash, _9p_hash_equal, g_free, _9p_hash_free_val);
+}
+
+static void _9p_hash_cleanup(void)
+{
+	g_hash_table_destroy(_9p_hashtable);
 }
 
 static void _9p_hash_set(packet_info *pinfo, guint16 tag, guint32 fid, struct _9p_hashval *val)
@@ -2757,6 +2753,7 @@ void proto_register_9P(void)
 	expert_register_field_array(expert_9P, ei, array_length(ei));
 
 	register_init_routine(_9p_hash_init);
+	register_cleanup_routine(_9p_hash_cleanup);
 }
 
 void proto_reg_handoff_9P(void)

epan/dissectors/packet-nlm.c
@@ -112,24 +112,13 @@ typedef struct _nlm_msg_res_matched_data {
 	nstime_t ns;
 } nlm_msg_res_matched_data;
 
-static gboolean
-nlm_msg_res_unmatched_free_all(gpointer key_arg _U_, gpointer value, gpointer user_data _U_)
+static void
+nlm_msg_res_unmatched_value_destroy(gpointer value)
 {
 	nlm_msg_res_unmatched_data *umd = (nlm_msg_res_unmatched_data *)value;
 
 	g_free((gpointer)umd->cookie);
 	g_free(umd);
-
-	return TRUE;
-}
-static gboolean
-nlm_msg_res_matched_free_all(gpointer key_arg _U_, gpointer value, gpointer user_data _U_)
-{
-	nlm_msg_res_matched_data *md = (nlm_msg_res_matched_data *)value;
-
-	g_free(md);
-
-	return TRUE;
 }
 
 static guint
@@ -177,21 +166,19 @@ nlm_msg_res_matched_equal(gconstpointer k1, gconstpointer k2)
 static void
 nlm_msg_res_match_init(void)
 {
-	if(nlm_msg_res_unmatched != NULL){
-		g_hash_table_foreach_remove(nlm_msg_res_unmatched,
-				nlm_msg_res_unmatched_free_all, NULL);
-	} else {
-		nlm_msg_res_unmatched=g_hash_table_new(nlm_msg_res_unmatched_hash,
-			nlm_msg_res_unmatched_equal);
-	}
+	nlm_msg_res_unmatched =
+		g_hash_table_new_full(nlm_msg_res_unmatched_hash,
+		nlm_msg_res_unmatched_equal,
+		NULL, nlm_msg_res_unmatched_value_destroy);
+	nlm_msg_res_matched = g_hash_table_new_full(nlm_msg_res_matched_hash,
+		nlm_msg_res_matched_equal, NULL, (GDestroyNotify)g_free);
+}
 
-	if(nlm_msg_res_matched != NULL){
-		g_hash_table_foreach_remove(nlm_msg_res_matched,
-				nlm_msg_res_matched_free_all, NULL);
-	} else {
-		nlm_msg_res_matched=g_hash_table_new(nlm_msg_res_matched_hash,
-			nlm_msg_res_matched_equal);
-	}
+static void
+nlm_msg_res_match_cleanup(void)
+{
+	g_hash_table_destroy(nlm_msg_res_unmatched);
+	g_hash_table_destroy(nlm_msg_res_matched);
 }
 
 static void
@@ -1221,6 +1208,7 @@ proto_register_nlm(void)
 		"Whether the dissector will track and match MSG and RES calls for asynchronous NLM",
 		&nlm_match_msgres);
 	register_init_routine(nlm_msg_res_match_init);
+	register_cleanup_routine(nlm_msg_res_match_cleanup);
 }
 
 void

commit f897899f6137cc471b3236719b96507471d58884

packet-nfs: fix double-free
fixes regression introduced by f5340b2

g_hash_table_remove will call free on object, thus there is no need for explicit g_free,
as is causes a double-free:

*** Error in `/usr/sbin/wireshark-gtk': double free or corruption (fasttop): 0x0000555556e6bf50 ***
======= Backtrace: =========
/lib64/libc.so.6(+0x77da5)[0x7fffef80ada5]
/lib64/libc.so.6(+0x804fa)[0x7fffef8134fa]
/lib64/libc.so.6(cfree+0x4c)[0x7fffef816cac]
/lib64/libglib-2.0.so.0(g_free+0xe)[0x7ffff09665ee]
/lib64/libglib-2.0.so.0(+0x388ba)[0x7ffff094f8ba]
/lib64/libwireshark.so.6(+0x1cfb46b)[0x7ffff49d646b]
/lib64/libwireshark.so.6(+0x1d03d99)[0x7ffff49ded99]
/lib64/libwireshark.so.6(+0x173b11f)[0x7ffff441611f]
/lib64/libwireshark.so.6(+0x173bba5)[0x7ffff4416ba5]
/lib64/libwireshark.so.6(call_dissector_with_data+0x26)[0x7ffff4419ad6]

.....

The g_hash_table_insert will remove and deallocate existing entry, so we
don't need to do it at all.

Change-Id: Ide47d1f9deb3e1b0d8adefd31fc6f3bf5cbaa010
Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
Reviewed-on: https://code.wireshark.org/review/14096
Petri-Dish: Peter Wu <peter@lekensteyn.nl>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Peter Wu <peter@lekensteyn.nl>

epan/dissectors/packet-nfs.c
@@ -1058,7 +1058,7 @@ void
 nfs_name_snoop_add_name(int xid, tvbuff_t *tvb, int name_offset, int name_len, int parent_offset,
 			int parent_len, const char *name)
 {
-	nfs_name_snoop_t *nns, *old_nns;
+	nfs_name_snoop_t *nns;
 	const char	 *ptr;
 
 	if (name_len <= 0) {
@@ -1111,24 +1111,7 @@ nfs_name_snoop_add_name(int xid, tvbuff_t *tvb, int name_offset, int name_len, i
 	nns->full_name_len = 0;
 	nns->full_name = NULL;
 
-	/* remove any old entry for this */
-	old_nns = (nfs_name_snoop_t *)g_hash_table_lookup(nfs_name_snoop_unmatched, GINT_TO_POINTER(xid));
-	if (old_nns) {
-		/* if we haven't seen the reply yet, then there are no
-		   matched entries for it, thus we can dealloc the arrays*/
-		if (!old_nns->fh) {
-			g_free(old_nns->name);
-			old_nns->name = NULL;
-			old_nns->name_len = 0;
-
-			g_free(old_nns->parent);
-			old_nns->parent = NULL;
-			old_nns->parent_len = 0;
-		}
-		g_free(old_nns);
-		g_hash_table_remove(nfs_name_snoop_unmatched, GINT_TO_POINTER(xid));
-	}
-
+	/* any old entry will be deallocated and removed */
 	g_hash_table_insert(nfs_name_snoop_unmatched, GINT_TO_POINTER(xid), nns);
 }
 