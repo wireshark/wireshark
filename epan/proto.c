7bd1158b95fa896f7bdb978dce21d4dabe173238
Jaap Keuter <jaap.keuter@xs4all.nl>
2016-03-07 23:20:44 +0100

    Remove double check for null (CID 1158871)
    Make full use of our proper implementation of this macro by
    giving it the pointer as is.
    
    Change-Id: I0bbe73d19cc3f578b94ea2d4d904d6fa87b20b48
    Reviewed-on: https://code.wireshark.org/review/14391
    Petri-Dish: Jaap Keuter <jaap.keuter@xs4all.nl>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/proto.c
+++ epan/proto.c
@@ -7974,10 +7974,7 @@ proto_check_for_protocol_or_field(const proto_tree* tree, const int id)
 {
 	GPtrArray *ptrs = proto_get_finfo_ptr_array(tree, id);
 
-	if (!ptrs) {
-		return FALSE;
-	}
-	else if (g_ptr_array_len(ptrs) > 0) {
+	if (g_ptr_array_len(ptrs) > 0) {
 		return TRUE;
 	}
 	else {
