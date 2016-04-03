8a89f66fb0b3b2ee7c8ddc38d3b5c3e7789a5aa1
Pascal Quantin <pascal.quantin@gmail.com>
2016-04-03 22:56:56 +0200

    RRC: upgrade dissector to v13.2.0
    Change-Id: Id2f7b90549057accf23b040cafcee738a2c0d9ee
    Reviewed-on: https://code.wireshark.org/review/14800
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/asn1/rrc/packet-rrc-template.c
+++ epan/dissectors/asn1/rrc/packet-rrc-template.c
@@ -22,7 +22,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Ref: 3GPP TS 25.331 V13.1.0 (2015-12)
+ * Ref: 3GPP TS 25.331 V13.2.0 (2016-03)
  */
 
 /**
