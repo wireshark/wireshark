8f8a2d06d3a4516b3e81fd115eb20f62db4ec947
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-05-16 16:26:34 +0200

    ethertype: Add old non-standard 802.1ad (QinQ)
    Issue reported by boaz
    Bug:12441
    
    Change-Id: I8652f165d782039f0a01f3c10bd67b34c33eacc5
    Reviewed-on: https://code.wireshark.org/review/15462
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-ethertype.c
+++ epan/dissectors/packet-ethertype.c
@@ -186,6 +186,7 @@ const value_string etype_vals[] = {
 	{ ETHERTYPE_CMD,                  "CiscoMetaData"},
 	{ ETHERTYPE_XIP,                  "eXpressive Internet Protocol"},
 	{ ETHERTYPE_NWP,                  "Neighborhood Watch Protocol"},
+	{ ETHERTYPE_QINQ_OLD,             "QinQ: old non-standard 802.1ad" },
 	{ 0, NULL }
 };
 
