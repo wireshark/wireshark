wireshark/wireshark

commit 6a992182ce47d721ce73eabf99983cea480dcf97

qt follow stream: don't crash when saving to an non-writable file
When the user clicks Save As in the Follow Stream window, check that the
selected target file can be opened for writing. Bring up a warning box
if the file is not writable.

Change-Id: I76e67b064377dd432d3b14592f5096b99d9968c0
Reviewed-on: https://code.wireshark.org/review/15536
Reviewed-by: Michael Mann <mmann78@netscape.net>

ui/qt/follow_stream_dialog.cpp
@@ -266,9 +266,13 @@ void FollowStreamDialog::saveAs()
 {
     QString file_name = QFileDialog::getSaveFileName(this, wsApp->windowTitleString(tr("Save Stream Content As" UTF8_HORIZONTAL_ELLIPSIS)));
     if (!file_name.isEmpty()) {
-        file_.setFileName(file_name);
-        file_.open(QIODevice::WriteOnly);
         QTextStream out(&file_);
+
+        if (!file_.open(QIODevice::WriteOnly)) {
+            open_failure_alert_box(file_name.toUtf8().constData(), errno, TRUE);
+            return;
+        }
+        file_.setFileName(file_name);
 
         save_as_ = true;
 

commit 6d9022d19f92ae0250300746c52a7f47478c93bb

qt follow stream: set the file name before opening
If we don't, Save As will always fail.

(I broke this in 6a992182ce47d721ce73eabf99983cea480dcf97)

Change-Id: I4abfe495af3aee470331ae5464acbd7d08028bf0
Reviewed-on: https://code.wireshark.org/review/15547
Reviewed-by: Martin Kaiser <wireshark@kaiser.cx>

ui/qt/follow_stream_dialog.cpp
@@ -268,11 +268,11 @@ void FollowStreamDialog::saveAs()
     if (!file_name.isEmpty()) {
         QTextStream out(&file_);
 
+        file_.setFileName(file_name);
         if (!file_.open(QIODevice::WriteOnly)) {
             open_failure_alert_box(file_name.toUtf8().constData(), errno, TRUE);
             return;
         }
-        file_.setFileName(file_name);
 
         save_as_ = true;
 