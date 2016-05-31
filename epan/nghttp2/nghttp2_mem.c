8f1efa9c095f8618287e4a610f73b426a360f6b2
Guy Harris <guy@alum.mit.edu>
2016-05-31 16:42:34 -0700

    Squelch a compiler warning.
    Change-Id: I7a0b48dd840a6b4795f87f983bbaf169afceece7
    Reviewed-on: https://code.wireshark.org/review/15664
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/nghttp2/nghttp2_mem.c
+++ epan/nghttp2/nghttp2_mem.c
@@ -52,8 +52,8 @@ void nghttp2_mem_free(nghttp2_mem *mem, void *ptr) {
   mem->free(ptr, mem->mem_user_data);
 }
 
-void nghttp2_mem_free2(nghttp2_free free, void *ptr, void *mem_user_data) {
-  free(ptr, mem_user_data);
+void nghttp2_mem_free2(nghttp2_free free_func, void *ptr, void *mem_user_data) {
+  free_func(ptr, mem_user_data);
 }
 
 void *nghttp2_mem_calloc(nghttp2_mem *mem, size_t nmemb, size_t size) {
