b48bff80265a408aec4f42d080d3db6f8329933b
Guy Harris <guy@alum.mit.edu>
2016-06-01 01:18:05 -0700

    Make the modelines more like other ones.
    Thou shalt not ever use modelines that put the tab-character tab spots
    anywhere other than every 8 characters; that's where Ken and Dennis put
    them, and that's where they belong.  Use whatever indentation you want,
    including 4-space indentation, but do *not* try to arrange that a tab
    character moves to the next 4-character boundary, because, in a lot of
    UN*X software, it doesn't.
    
    (Yes, this means that Xcode's default is wrong.  It *is* wrong,
    especially given that it's an IDE for a UNIX.)
    
    Change-Id: I308745cdeef35b7c91ea493da6487baadc357f58
    Reviewed-on: https://code.wireshark.org/review/15673
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-cql.c
+++ epan/dissectors/packet-cql.c
@@ -1392,10 +1392,10 @@ proto_register_cql(void)
  *
  * Local variables:
  * c-basic-offset: 4
- * tab-width: 4
- * indent-tabs-mode: t
+ * tab-width: 8
+ * indent-tabs-mode: nil
  * End:
  *
- * vi: set shiftwidth=4 tabstop=4 noexpandtab:
- * :indentSize=4:tabSize=4:noTabs=false:
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
  */
