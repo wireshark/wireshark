56e33a549f54038e9155c781bb2097fe30d4b2dc
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-06-04 18:55:47 +0200

    DNS: Update draft-ietf-dnsop-cookie to final rfc (7873)
    and fix Bad cookie text
    
    Change-Id: Ie9e2629a9f6cff4e2a35fcf4bf399858dd21c5ef
    Reviewed-on: https://code.wireshark.org/review/15721
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-dns.c
+++ epan/dissectors/packet-dns.c
@@ -593,7 +593,7 @@ typedef struct _dns_conv_info_t {
 #define O_CLIENT_SUBNET  8              /* Client subnet as assigned by IANA */
 #define O_EDNS_EXPIRE    9              /* EDNS Expire (RFC7314) */
 #define O_CLIENT_SUBNET_EXP 0x50fa      /* Client subnet (placeholder value, draft-vandergaast-edns-client-subnet) */
-#define O_COOKIE        10              /* draft-ietf-dnsop-cookie */
+#define O_COOKIE        10              /* Cookies (RFC7873) */
 #define O_EDNS_TCP_KA   11              /* edns-tcp-keepalive EDNS0 Option (RFC7828) */
 #define O_PADDING       12              /* EDNS(0) Padding Option (RFC7830) */
 #define O_CHAIN         13              /* draft-ietf-dnsop-edns-chain-query */
@@ -715,7 +715,7 @@ static const value_string rcode_vals[] = {
   { RCODE_BADNAME,    "Duplicate key name" },
   { RCODE_BADALG,     "Algorithm not supported" },
   { RCODE_BADTRUNC,   "Bad Truncation" },
-  { RCODE_BADCOOKIE,  "Bad/missing server cookie" },
+  { RCODE_BADCOOKIE,  "Bad/missing Server Cookie" },
   { 0,                NULL }
  };
 
