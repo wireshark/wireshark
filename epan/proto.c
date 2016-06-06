767a59e54e6df17f7d8e2c6806ce16062d17e6d7
Jens Kilian <jens.kilian@advantest.com>
2016-06-06 15:38:22 +0200

    Don't sign extend unsigned integer fields larger than 32 bits.
    Fix a bug in proto_tree_set_uint64() which caused fields to be sign extended.
    (This only showed up when a non-zero bitmask was present.)
    
    Change-Id: Ibcc986e3d42f97348966ea66c2ae9fca73b09992
    Reviewed-on: https://code.wireshark.org/review/15764
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Jaap Keuter <jaap.keuter@xs4all.nl>

--- epan/proto.c
+++ epan/proto.c
@@ -4060,7 +4060,6 @@ proto_tree_set_uint64(field_info *fi, guint64 value)
 {
 	header_field_info *hfinfo;
 	guint64		   integer;
-	gint		   no_of_bits;
 
 	hfinfo = fi->hfinfo;
 	integer = value;
@@ -4071,9 +4070,6 @@ proto_tree_set_uint64(field_info *fi, guint64 value)
 
 		/* Shift bits */
 		integer >>= hfinfo_bitshift(hfinfo);
-
-		no_of_bits = ws_count_ones(hfinfo->bitmask);
-		integer = ws_sign_ext64(integer, no_of_bits);
 	}
 
 	fvalue_set_uinteger64(&fi->value, integer);
