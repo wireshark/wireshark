7f102aa7229082a637a3fa68c1ece496d1851370
Pascal Quantin <pascal.quantin@gmail.com>
2016-07-09 12:02:32 +0200

    Fix detection of NSIS install path when running a win64 CMake
    On x86 system:
    System32\cmd.exe
    ProgramFiles=C:\Program Files
    
    On x64 system:
    System32\cmd.exe
    ProgramFiles=C:\Program Files
    ProgramFiles(x86)=C:\Program Files (x86)
    ProgramW6432=C:\Program Files
    Syswow64\cmd.exe
    ProgramFiles=C:\Program Files (x86)
    ProgramFiles(x86)=C:\Program Files (x86)
    ProgramW6432=C:\Program Files
    
    Change-Id: I8505a54dcefaef705d1d2be87660894163263b25
    Reviewed-on: https://code.wireshark.org/review/16354
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- cmake/modules/FindNSIS.cmake
+++ cmake/modules/FindNSIS.cmake
@@ -6,7 +6,8 @@
 #  CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS - System runtime DLLs
 
 # Find makensis
+set(_PF86 "PROGRAMFILES(x86)")
 find_program(MAKENSIS_EXECUTABLE makensis
-	PATH "$ENV{PROGRAMFILES}/NSIS" "$ENV{PROGRAMW6432}/NSIS"
+	PATH "$ENV{PROGRAMFILES}/NSIS" "$ENV{${_PF86}}/NSIS" "$ENV{PROGRAMW6432}/NSIS"
 	DOC "Path to the makensis utility."
 )
