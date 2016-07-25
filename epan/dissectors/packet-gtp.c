bcec07895aee4429c9b1e2d2dc142954b5f396ba
Marius Paliga <mareko.paliga@gmail.com>
2016-07-25 09:54:37 +0200

    Fix decoding of MS Info Change Reporting Action
    Bug: 12642
    Change-Id: I69b339ff4785a2fe3fb0e2111264616815339c1b
    Reviewed-on: https://code.wireshark.org/review/16644
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-gtp.c
+++ epan/dissectors/packet-gtp.c
@@ -6887,8 +6887,8 @@ decode_gtp_ps_ho_req_ctx(tvbuff_t * tvb, int offset, packet_info * pinfo _U_, pr
     offset++;
     proto_tree_add_item(ext_tree, hf_gtp_ext_length, tvb, offset, 2, ENC_BIG_ENDIAN);
     offset = offset + 2;
-
-    proto_tree_add_item(ext_tree, hf_gtp_rep_act_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+    /* TODO add decoding of data */
+    proto_tree_add_expert(ext_tree, pinfo, &ei_gtp_undecoded, tvb, offset, length);
 
     return 3 + length;
 
@@ -7146,8 +7146,8 @@ decode_gtp_ms_inf_chg_rep_act(tvbuff_t * tvb, int offset, packet_info * pinfo _U
     offset++;
     proto_tree_add_item(ext_tree, hf_gtp_ext_length, tvb, offset, 2, ENC_BIG_ENDIAN);
     offset = offset + 2;
-    /* TODO add decoding of data */
-    proto_tree_add_expert(ext_tree, pinfo, &ei_gtp_undecoded, tvb, offset, length);
+
+    proto_tree_add_item(ext_tree, hf_gtp_rep_act_type, tvb, offset, 1, ENC_BIG_ENDIAN);
 
     return 3 + length;
 
