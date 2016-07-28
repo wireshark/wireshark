12b859f4538fd84a1f1ae89bc994429ac51611ad
Thomas Klausner <tk@giga.or.at>
2016-07-28 16:31:44 +0200

    Fix build on NetBSD.
    sys/siginfo.h (which is included by this file) defines si_code.
    
    Change-Id: I59cfe1ea3ec01345965028db4bba3223752459e3
    Reviewed-on: https://code.wireshark.org/review/16750
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- ui/qt/mtp3_summary_dialog.cpp
+++ ui/qt/mtp3_summary_dialog.cpp
@@ -208,7 +208,7 @@ QString Mtp3SummaryDialog::summaryToHtml()
         << table_hheader15_tmpl.arg(tr("Bytes/s"))
         << table_row_end;
 
-    for (size_t si_code = 0; si_code < MTP3_NUM_SI_CODE; si_code++) {
+    for (size_t ws_si_code = 0; ws_si_code < MTP3_NUM_SI_CODE; ws_si_code++) {
         int si_msus = 0;
         int si_bytes = 0;
         QString msus_s_str = n_a;
@@ -216,8 +216,8 @@ QString Mtp3SummaryDialog::summaryToHtml()
         QString bytes_s_str = n_a;
 
         for (size_t stat_idx = 0; stat_idx < mtp3_num_used; stat_idx++) {
-            si_msus += mtp3_stat[stat_idx].mtp3_si_code[si_code].num_msus;
-            si_bytes += mtp3_stat[stat_idx].mtp3_si_code[si_code].size;
+            si_msus += mtp3_stat[stat_idx].mtp3_si_code[ws_si_code].num_msus;
+            si_bytes += mtp3_stat[stat_idx].mtp3_si_code[ws_si_code].size;
         }
         total_msus += si_msus;
         total_bytes += si_bytes;
@@ -232,7 +232,7 @@ QString Mtp3SummaryDialog::summaryToHtml()
         }
 
         out << table_row_begin
-            << table_data_tmpl.arg(mtp3_service_indicator_code_short_vals[si_code].strptr)
+            << table_data_tmpl.arg(mtp3_service_indicator_code_short_vals[ws_si_code].strptr)
             << table_data_tmpl.arg(si_msus)
             << table_data_tmpl.arg(msus_s_str)
             << table_data_tmpl.arg(si_bytes)
