fa74120b75dca2923598cd1964132751bbfc058f
Michael Tuexen <tuexen@wireshark.org>
2016-08-02 14:31:37 +0200

    Fix compilation on FreeBSD.
    Includes are needed for using macros like WIFEXITED.
    
    Change-Id: Ia09ca58eeab2d151a756d285b0aeb55141c4d24a
    Reviewed-on: https://code.wireshark.org/review/16841
    Reviewed-by: Michael Tüxen <tuexen@wireshark.org>
    (cherry picked from commit 18f775af55ddf60aa72b4d2b3d635701d1ac50b1)
    Reviewed-on: https://code.wireshark.org/review/17594
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- extcap.c
+++ extcap.c
@@ -37,6 +37,13 @@
 #include <unistd.h>
 #endif
 
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+#ifdef HAVE_SYS_WAIT_H
+#include <sys/wait.h>
+#endif
+
 #include <glib.h>
 #include <log.h>
 
