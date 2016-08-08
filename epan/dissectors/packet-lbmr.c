f2bee8dd57696c5da50f931fee8f8a03ba1ea756
Dario Lombardo <lomato@gmail.com>
2016-08-08 14:31:47 +0200

    lbmr: restore removed code.
    Code was removed in 975b02f879ca1407f40fdf5c3b446edcaa5171a2 but is still needed.
    This fixes CID 1250810.
    
    Change-Id: Ic0df5043c4486417e93d431898ff1493eb027b1c
    Reviewed-on: https://code.wireshark.org/review/16950
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- epan/dissectors/packet-lbmr.c
+++ epan/dissectors/packet-lbmr.c
@@ -4623,9 +4623,8 @@ int lbmr_dissect_umq_qmgmt(tvbuff_t * tvb, int offset, packet_info * pinfo, prot
 /*----------------------------------------------------------------------------*/
 static int dissect_lbmr_ctxinfo(tvbuff_t * tvb, int offset, packet_info * pinfo _U_, proto_tree * tree)
 {
+    guint16 flags16 = 0;
     guint8 reclen = 0;
-    int name_offset = -1;
-    int name_len = 0;
     static const int * flags[] =
     {
         &hf_lbmr_ctxinfo_flags_query,
@@ -4638,6 +4637,7 @@ static int dissect_lbmr_ctxinfo(tvbuff_t * tvb, int offset, packet_info * pinfo 
         NULL
     };
 
+    flags16 = tvb_get_ntohs(tvb, offset + O_LBMR_CTXINFO_T_FLAGS);
     reclen = tvb_get_guint8(tvb, offset + O_LBMR_CTXINFO_T_LEN);
     proto_tree_add_item(tree, hf_lbmr_ctxinfo_len, tvb, offset + O_LBMR_CTXINFO_T_LEN, L_LBMR_CTXINFO_T_LEN, ENC_BIG_ENDIAN);
     proto_tree_add_item(tree, hf_lbmr_ctxinfo_hop_count, tvb, offset + O_LBMR_CTXINFO_T_HOP_COUNT, L_LBMR_CTXINFO_T_HOP_COUNT, ENC_BIG_ENDIAN);
@@ -4645,9 +4645,9 @@ static int dissect_lbmr_ctxinfo(tvbuff_t * tvb, int offset, packet_info * pinfo 
     proto_tree_add_item(tree, hf_lbmr_ctxinfo_port, tvb, offset + O_LBMR_CTXINFO_T_PORT, L_LBMR_CTXINFO_T_FLAGS, ENC_BIG_ENDIAN);
     proto_tree_add_item(tree, hf_lbmr_ctxinfo_ip, tvb, offset + O_LBMR_CTXINFO_T_IP, L_LBMR_CTXINFO_T_IP, ENC_BIG_ENDIAN);
     proto_tree_add_item(tree, hf_lbmr_ctxinfo_instance, tvb, offset + O_LBMR_CTXINFO_T_INSTANCE, L_LBMR_CTXINFO_T_INSTANCE, ENC_NA);
-    if (name_offset != -1)
+    if ((flags16 & LBMR_CTXINFO_NAME_FLAG) != 0)
     {
-        proto_tree_add_item(tree, hf_lbmr_ctxinfo_name, tvb, name_offset, name_len, ENC_ASCII|ENC_NA);
+        proto_tree_add_item(tree, hf_lbmr_ctxinfo_name, tvb, offset + L_LBMR_CTXINFO_T, reclen - L_LBMR_CTXINFO_T, ENC_ASCII|ENC_NA);
     }
     return ((int)reclen);
 }
