6cfb20da0dd5617c620e7f48f95e931c19a0dac4
Pascal Quantin <pascal.quantin@gmail.com>
2016-08-19 18:00:36 +0200

    NLM: fix dissection of NLM_FREE_ALL message
    The second parameter is the state, not the status as seen in C702 doc, page 159
    
    Bug: 12764
    Change-Id: I0a91a0e586c7663ace7c4c6b1044cafc1c0975ac
    Reviewed-on: https://code.wireshark.org/review/17178
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>

--- epan/dissectors/packet-nlm.c
+++ epan/dissectors/packet-nlm.c
@@ -644,17 +644,10 @@ static int
 dissect_nlm_freeall(tvbuff_t *tvb, int offset, packet_info *pinfo _U_,
 		    proto_tree *tree,int version _U_)
 {
-	guint32 nlm_stat;
-
 	offset = dissect_rpc_string(tvb,tree,
 			hf_nlm_share_name, offset, NULL);
 
-	nlm_stat = tvb_get_ntohl(tvb, offset);
-	if (nlm_stat) {
-		col_append_fstr(pinfo->cinfo, COL_INFO, " %s",
-		    val_to_str(nlm_stat, names_nlm_stats, "Unknown Status (%u)"));
-	}
-	offset = dissect_rpc_uint32(tvb, tree, hf_nlm_stat, offset);
+	offset = dissect_rpc_uint32(tvb, tree, hf_nlm_state, offset);
 
 	return offset;
 }
