wireshark/wireshark

commit c711bc64573e20e343f343a53ad4d1d50add433d

Qt: Shorten capture file dialog name filters.
In the capture file dialog's "Files of type" combobox separate out the
wildcards we display from the ones that are applied. Set the
HideNameFilterDetails option and for "All Files" and "All Capture Files"
leave the option hidden.

For other options print the wildcard list twice so that it's both
displayed and applied. Go even further and filter out ".gz" wildcards in
the displayed list since they're effectively duplicates of their
uncompressed counterparts.

Based on Dario's work in change 17605.

Bug: 12837
Change-Id: I35de8f31492657e37b12ca4c8de5ed9e79d2e2f8
Reviewed-on: https://code.wireshark.org/review/17689
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Gerald Combs <gerald@wireshark.org>
Reviewed-by: Dario Lombardo <lomato@gmail.com>
Tested-by: Dario Lombardo <lomato@gmail.com>
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/capture_file_dialog.cpp
@@ -39,16 +39,17 @@
 
 #include "ui/all_files_wildcard.h"
 
-#include <QGridLayout>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QLabel>
-#include <QLineEdit>
 #include <QCheckBox>
 #include <QFileInfo>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
 #include <QMessageBox>
+#include <QSortFilterProxyModel>
 #include <QSpacerItem>
-#endif // Q_OS_WIN
+#include <QVBoxLayout>
+#endif // ! Q_OS_WIN
 
 #include <QPushButton>
 #include "epan/prefs.h"
@@ -93,6 +94,7 @@ CaptureFileDialog::CaptureFileDialog(QWidget *parent, capture_file *cf, QString 
     // Add extra widgets
     // https://wiki.qt.io/Qt_project_org_faq#How_can_I_add_widgets_to_my_QFileDialog_instance.3F
     setOption(QFileDialog::DontUseNativeDialog, true);
+    setOption(QFileDialog::HideNameFilterDetails, true);
     QGridLayout *fd_grid = qobject_cast<QGridLayout*>(layout());
     QHBoxLayout *h_box = new QHBoxLayout();
 
@@ -289,32 +291,38 @@ int CaptureFileDialog::mergeType() {
 // We use the Qt dialogs here
 QString CaptureFileDialog::fileExtensionType(int et, bool extension_globs)
 {
-    QString filter;
+    QString extension_type_name;
+    QStringList all_wildcards;
+    QStringList nogz_wildcards;
     GSList *extensions_list;
     GSList *extension;
 
-    filter = wtap_get_file_extension_type_name(et);
+    extension_type_name = wtap_get_file_extension_type_name(et);
 
     if (!extension_globs) {
-        return filter;
+        return extension_type_name;
     }
-
-    filter += " (";
 
     extensions_list = wtap_get_file_extension_type_extensions(et);
 
     /* Construct the list of patterns. */
     for (extension = extensions_list; extension != NULL;
          extension = g_slist_next(extension)) {
-        if (!filter.endsWith('('))
-            filter += ' ';
-        filter += "*.";
-        filter += (char *)extension->data;
+        QString bare_wc = QString("*.%1").arg((char *)extension->data);
+        all_wildcards << bare_wc;
+        if (!bare_wc.endsWith(".gz")) {
+            nogz_wildcards << bare_wc;
+        }
     }
     wtap_free_extensions_list(extensions_list);
-    filter += ')';
-    return filter;
-    /* XXX - does QStringList's destructor destroy the strings in the list? */
+
+    // We set HideNameFilterDetails so that "All Files" and "All Capture
+    // Files" don't show a wildcard list. We want to show the associated
+    // wildcards for individual file types so we add them twice.
+    return QString("%1 (%2) (%3)")
+            .arg(extension_type_name)
+            .arg(nogz_wildcards.join(" "))
+            .arg(all_wildcards.join(" "));
 }
 
 QString CaptureFileDialog::fileType(int ft, bool extension_globs)

commit ee47d8c8dbad10a7f55287aec1fee3257c59cc61

Qt: Fix bug in capture file dialog introduced by HideNameFilterDetails
Restore File->Save As and File->Export Specified Packets functionalities broken by commit c711bc64573e20e343f343a53ad4d1d50add433d.

Change-Id: I5d246d296979b5e509a90569094d672083b06b9b
Reviewed-on: https://code.wireshark.org/review/17783
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>
Reviewed-by: Gerald Combs <gerald@wireshark.org>

ui/qt/capture_file_dialog.cpp
@@ -660,6 +660,7 @@ QStringList CaptureFileDialog::buildFileSaveAsTypeList(bool must_support_all_com
 
     if (savable_file_types_subtypes != NULL) {
         QString file_type;
+        QString hash_file_type;
         int ft;
         /* OK, we have at least one file type we can save this file as.
            (If we didn't, we shouldn't have gotten here in the first
@@ -669,8 +670,9 @@ QStringList CaptureFileDialog::buildFileSaveAsTypeList(bool must_support_all_com
             if (default_ft_ < 1)
                 default_ft_ = ft; /* first file type is the default */
             file_type = fileType(ft);
+            hash_file_type = fileType(ft, false);
             filters << file_type;
-            type_hash_[file_type] = ft;
+            type_hash_[hash_file_type] = ft;
         }
         g_array_free(savable_file_types_subtypes, TRUE);
     }