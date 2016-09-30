85f96388877282d1c4f382cd8fd8f678ef5fcba7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-09-30 08:10:01 +0200

    spnego: use same name define name from krb5
    and for heimdal lib add the value
    
    Bug:12831
    Change-Id: Ibf04560867acfe2a430034248ce2a386ea89b668
    Reviewed-on: https://code.wireshark.org/review/17988
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit da8969ceefbcc1ad19f36309ccb5b208c6324b5c)
    Reviewed-on: https://code.wireshark.org/review/18076

--- epan/dissectors/asn1/spnego/packet-spnego-template.c
+++ epan/dissectors/asn1/spnego/packet-spnego-template.c
@@ -323,6 +323,9 @@ dissect_spnego_krb5(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
 #ifndef KEYTYPE_ARCFOUR_56
 # define KEYTYPE_ARCFOUR_56 24
 #endif
+#ifndef KEYTYPE_ARCFOUR_HMAC
+# define KEYTYPE_ARCFOUR_HMAC 23
+#endif
 /* XXX - We should probably do a configure-time check for this instead */
 #ifndef KRB5_KU_USAGE_SEAL
 # define KRB5_KU_USAGE_SEAL 22
@@ -847,7 +850,7 @@ dissect_spnego_krb5_wrap_base(tvbuff_t *tvb, int offset, packet_info *pinfo
 			decrypt_gssapi_krb_arcfour_wrap(tree,
 				pinfo,
 				tvb,
-				KERB_ENCTYPE_RC4_HMAC,
+				KEYTYPE_ARCFOUR_HMAC,
 				gssapi_encrypt);
 #endif /* HAVE_HEIMDAL_KERBEROS || HAVE_MIT_KERBEROS */
 		}
--- epan/dissectors/packet-spnego.c
+++ epan/dissectors/packet-spnego.c
@@ -775,6 +775,9 @@ dissect_spnego_krb5(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
 #ifndef KEYTYPE_ARCFOUR_56
 # define KEYTYPE_ARCFOUR_56 24
 #endif
+#ifndef KEYTYPE_ARCFOUR_HMAC
+# define KEYTYPE_ARCFOUR_HMAC 23
+#endif
 /* XXX - We should probably do a configure-time check for this instead */
 #ifndef KRB5_KU_USAGE_SEAL
 # define KRB5_KU_USAGE_SEAL 22
@@ -1299,7 +1302,7 @@ dissect_spnego_krb5_wrap_base(tvbuff_t *tvb, int offset, packet_info *pinfo
 			decrypt_gssapi_krb_arcfour_wrap(tree,
 				pinfo,
 				tvb,
-				KERB_ENCTYPE_RC4_HMAC,
+				KEYTYPE_ARCFOUR_HMAC,
 				gssapi_encrypt);
 #endif /* HAVE_HEIMDAL_KERBEROS || HAVE_MIT_KERBEROS */
 		}
@@ -1966,7 +1969,7 @@ void proto_register_spnego(void) {
         NULL, HFILL }},
 
 /*--- End of included file: packet-spnego-hfarr.c ---*/
-#line 1421 "./asn1/spnego/packet-spnego-template.c"
+#line 1424 "./asn1/spnego/packet-spnego-template.c"
 	};
 
 	/* List of subtrees */
@@ -1989,7 +1992,7 @@ void proto_register_spnego(void) {
     &ett_spnego_InitialContextToken_U,
 
 /*--- End of included file: packet-spnego-ettarr.c ---*/
-#line 1431 "./asn1/spnego/packet-spnego-template.c"
+#line 1434 "./asn1/spnego/packet-spnego-template.c"
 	};
 
 	static ei_register_info ei[] = {
