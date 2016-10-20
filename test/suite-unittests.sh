1b9d46a9502843c15c33891955f6242c883aa1a5
Peter Wu <peter@lekensteyn.nl>
2016-10-20 23:46:16 +0200

    tests: do not invoke missing unit tests
    If the "test-programs" target is not built, then it should not try to
    invoke the last program (wireshark-gtk in my case).
    
    Change-Id: Ida60a71d1dc47305ef8abe10e6c96c8a1cb8dcbb
    Reviewed-on: https://code.wireshark.org/review/18346
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- test/suite-unittests.sh
+++ test/suite-unittests.sh
@@ -64,47 +64,48 @@ check_dut() {
 		fi
 	else
 		test_step_failed "$1 not found. Have you built test-programs?"
+		return 1
 	fi
 }
 
 unittests_step_exntest() {
-	check_dut exntest
+	check_dut exntest || return
 	ARGS=
 	unittests_step_test
 }
 
 unittests_step_oids_test() {
-	check_dut oids_test
+	check_dut oids_test || return
 	ARGS=
 	unittests_step_test
 }
 
 unittests_step_reassemble_test() {
-	check_dut reassemble_test
+	check_dut reassemble_test || return
 	ARGS=
 	unittests_step_test
 }
 
 unittests_step_tvbtest() {
-	check_dut tvbtest
+	check_dut tvbtest || return
 	ARGS=
 	unittests_step_test
 }
 
 unittests_step_wmem_test() {
-	check_dut wmem_test
+	check_dut wmem_test || return
 	ARGS=--verbose
 	unittests_step_test
 }
 
 unittests_step_ftsanity() {
-	check_dut ftsanity.py
+	check_dut ftsanity.py || return
 	ARGS=$TSHARK_PATH
 	unittests_step_test
 }
 
 unittests_step_fieldcount() {
-	check_dut tshark
+	check_dut tshark || return
 	ARGS="-G fieldcount"
 	unittests_step_test
 }
