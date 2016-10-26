c52d9209cc524420f6900c8fdc990047cdddaca9
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-10-26 21:45:02 +0200

    Driver.pm (pidl): fix spelling typo found by lintian
    Change-Id: Idb288beb54c91e100c164e0026b39bfe27daec39
    Reviewed-on: https://code.wireshark.org/review/18485
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- tools/pidl/lib/Parse/Yapp/Driver.pm
+++ tools/pidl/lib/Parse/Yapp/Driver.pm
@@ -180,7 +180,7 @@ sub _CheckParams {
 	while(($prm,$value)=splice(@$inarray,0,2)) {
         $prm=uc($prm);
 			exists($$checklist{$prm})
-		or	croak("Unknow parameter '$prm'");
+		or	croak("Unknown parameter '$prm'");
 			ref($value) eq $$checklist{$prm}
 		or	croak("Invalid value for parameter '$prm'");
         $prm=unpack('@2A*',$prm);
