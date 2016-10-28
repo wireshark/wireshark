8c1a5505de729b5ea80bfbb4fd617b1441ef6e3b
Jaap Keuter <jaap.keuter@xs4all.nl>
2016-10-28 07:37:52 +0200

    MPLS: Mark only Bottom-of-Stack label as flow label
    When preference 'Assume bottom of stack label as flow label' is set
    all labels are shown as flow labels. This has to be limited to that
    MPLS label where Bottom-of-Stack is TRUE.
    
    Change-Id: Iaa8abd095edb7de7f745a78c742a9347d4dbf3d9
    Reviewed-on: https://code.wireshark.org/review/18516
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-mpls.c
+++ epan/dissectors/packet-mpls.c
@@ -401,7 +401,7 @@ dissect_mpls(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
             ti = proto_tree_add_item(tree, proto_mpls, tvb, offset, 4, ENC_NA);
             mpls_tree = proto_item_add_subtree(ti, ett_mpls);
 
-            if (mpls_bos_flowlabel) {
+            if (mpls_bos_flowlabel && bos) {
                 proto_item_append_text(ti, ", Label: %u (Flow Label)", label);
             } else {
                 proto_item_append_text(ti, ", Label: %u", label);
