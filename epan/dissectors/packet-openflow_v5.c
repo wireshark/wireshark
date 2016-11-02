43a60c03f63259dd0c759d0effe3a5dfc57d6546
sangodbole <sanket.godbole@spirent.com>
2016-11-02 16:56:18 -0400

    Added Dissector for Openflow Experimenter Value field for Openflow 1.3 and 1.4
    Change-Id: I79d7b9afbde34451b545ac7e67c22b012843d822
    Reviewed-on: https://code.wireshark.org/review/18654
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-openflow_v5.c
+++ epan/dissectors/packet-openflow_v5.c
@@ -69,6 +69,7 @@ static int hf_openflow_v5_match_pad = -1;
 static int hf_openflow_v5_action_type = -1;
 static int hf_openflow_v5_action_length = -1;
 static int hf_openflow_v5_action_experimenter_experimenter = -1;
+static int hf_openflow_v5_oxm_experimenter_value = -1;
 static int hf_openflow_v5_action_output_port = -1;
 static int hf_openflow_v5_action_output_port_reserved = -1;
 static int hf_openflow_v5_action_output_max_len = -1;
@@ -1111,6 +1112,7 @@ static int
 dissect_openflow_oxm_header_v5(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, int offset, guint16 length _U_)
 {
     guint16 oxm_class;
+    guint8  oxm_length;
 
     /* oxm_class */
     oxm_class = tvb_get_ntohs(tvb, offset);
@@ -1129,6 +1131,7 @@ dissect_openflow_oxm_header_v5(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree
     offset+=1;
 
     /* oxm_length */
+    oxm_length = tvb_get_guint8(tvb, offset);
     proto_tree_add_item(tree, hf_openflow_v5_oxm_length, tvb, offset, 1, ENC_BIG_ENDIAN);
     offset+=1;
 
@@ -1136,6 +1139,8 @@ dissect_openflow_oxm_header_v5(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree
         /* uint32_t experimenter; */
         proto_tree_add_item(tree, hf_openflow_v5_oxm_experimenter_experimenter, tvb, offset, 4, ENC_BIG_ENDIAN);
         offset+=4;
+        proto_tree_add_item(tree, hf_openflow_v5_oxm_experimenter_value, tvb, offset, oxm_length - 4, ENC_NA);
+        offset+=(oxm_length - 4);
     }
 
     return offset;
@@ -6064,6 +6069,11 @@ proto_register_openflow_v5(void)
         { &hf_openflow_v5_oxm_experimenter_experimenter,
             { "Experimenter", "openflow_v5.oxm_experimenter.experimenter",
                FT_UINT32, BASE_HEX, NULL, 0x0,
+               NULL, HFILL }
+        },
+        { &hf_openflow_v5_oxm_experimenter_value,
+            { "Experimenter Value", "openflow_v5.oxm_experimenter.value",
+               FT_BYTES, BASE_NONE, NULL, 0x0,
                NULL, HFILL }
         },
         { &hf_openflow_v5_oxm_value,
