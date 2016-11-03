4fd28724ca4ee6b37a59f993175f6fb530d1d67e
Dario Lombardo <lomato@gmail.com>
2016-11-03 22:39:34 +0100

    debian-setup: remove libssh-gcrypt-dev from trusty.
    Change-Id: I62080885a0f9a99ee02ecae8e67caefcefdea0e4
    Reviewed-on: https://code.wireshark.org/review/18660
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>
    Reviewed-by: Balint Reczey <balint@balintreczey.hu>
    Reviewed-by: Dario Lombardo <lomato@gmail.com>

--- tools/debian-setup.sh
+++ tools/debian-setup.sh
@@ -40,7 +40,7 @@ ADDITIONAL_LIST="libnl-3-dev qttools5-dev qttools5-dev-tools libgtk-3-dev \
 		libc-ares-dev libssh-dev libkrb5-dev libqt5svg5-dev lynx libsmi2-dev \
 		portaudio19-dev asciidoc libgcrypt-dev libsbc-dev libgeoip-dev \
 		libgnutls-dev qtmultimedia5-dev liblua5.2-dev libnl-cli-3-dev \
-		libparse-yapp-perl qt5-default cmake libcap-dev libssh-gcrypt-dev \
+		libparse-yapp-perl qt5-default cmake libcap-dev \
 		liblz4-dev libsnappy-dev"
 
 # Check for lsb_release command in $PATH
@@ -59,7 +59,7 @@ trusty)
 	;;
 *)
 	BASIC_LIST="$BASIC_LIST libtool-bin"
-	ADDITIONAL_LIST="$ADDITIONAL_LIST libnghttp2-dev"
+	ADDITIONAL_LIST="$ADDITIONAL_LIST libnghttp2-dev libssh-gcrypt-dev"
 	;;
 esac
 
