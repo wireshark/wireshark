d30be948bbbdc353582e14ce110495edc602573c
Peter Wu <peter@lekensteyn.nl>
2016-11-28 16:50:25 +0100

    Qt: fix heap-use-after-free when double-clicking a packet number
    On double-clicking a packet number, the current field item is
    invalidated by goToPacket. Skip the URL since a field can either have a
    URL or a frame number (but not both).
    
    Change-Id: I58e5445fa74071fa0fd203df77ebdecdd1478d31
    Reviewed-on: https://code.wireshark.org/review/18971
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>
    (cherry picked from commit 1239336af00215172c94d5d2af3926a72ea998c3)
    Reviewed-on: https://code.wireshark.org/review/18973

--- ui/qt/proto_tree.cpp
+++ ui/qt/proto_tree.cpp
@@ -568,7 +568,7 @@ void ProtoTree::itemDoubleClick(QTreeWidgetItem *item, int) {
     fi = item->data(0, Qt::UserRole).value<field_info *>();
     if (!fi || !fi->hfinfo) return;
 
-    if(fi->hfinfo->type == FT_FRAMENUM) {
+    if (fi->hfinfo->type == FT_FRAMENUM) {
 #if QT_VERSION >= QT_VERSION_CHECK(4, 8, 0)
         if (QApplication::queryKeyboardModifiers() & Qt::ShiftModifier) {
 #else
@@ -578,9 +578,7 @@ void ProtoTree::itemDoubleClick(QTreeWidgetItem *item, int) {
         } else {
             emit goToPacket(fi->value.value.uinteger);
         }
-    }
-
-    if(FI_GET_FLAG(fi, FI_URL) && IS_FT_STRING(fi->hfinfo->type)) {
+    } else if (FI_GET_FLAG(fi, FI_URL) && IS_FT_STRING(fi->hfinfo->type)) {
         gchar *url;
         url = fvalue_to_string_repr(NULL, &fi->value, FTREPR_DISPLAY, fi->hfinfo->display);
         if(url){
