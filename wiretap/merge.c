b49e20c10af2fd1d21f8b8399db3fe4ffd6366df
Guy Harris <guy@alum.mit.edu>
2016-12-03 18:54:15 -0800

    Remove unnecessary cast (gchar = char) that removes const.
    Change-Id: I3924c2b4a525c0ae5ab57b7f9867296586d78509
    Reviewed-on: https://code.wireshark.org/review/19061
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- wiretap/merge.c
+++ wiretap/merge.c
@@ -426,7 +426,7 @@ create_shb_header(const merge_in_file_t *in_files, const guint in_file_count,
          */
         wtap_block_remove_option(shb_hdr, OPT_SHB_OS);
     }
-    wtap_block_set_string_option_value(shb_hdr, OPT_SHB_USERAPPL, (char*)app_name, app_name ? strlen(app_name): 0 ); /* NULL if not available, UTF-8 string containing the name */
+    wtap_block_set_string_option_value(shb_hdr, OPT_SHB_USERAPPL, app_name, app_name ? strlen(app_name): 0 ); /* NULL if not available, UTF-8 string containing the name */
                                                                                       /*  of the application used to create this section.          */
 
     return shb_hdrs;
