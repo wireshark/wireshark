7b5e77406d5210489c82a7aa9312053ccb030766
Gerald Combs <gerald@wireshark.org>
2016-12-05 10:53:32 -0800

    CMake: Update FindWiX.
    The WiX Toolset installer sets the WIX environment variable. Search for
    our WiX executables there first.
    
    Change-Id: I5acc9cb369dc2bdbb071d35b5a39498c6db117a3
    Reviewed-on: https://code.wireshark.org/review/19096
    Petri-Dish: Gerald Combs <gerald@wireshark.org>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- cmake/modules/FindWiX.cmake
+++ cmake/modules/FindWiX.cmake
@@ -9,18 +9,30 @@ set(_PF86 "PROGRAMFILES(x86)")
 
 # Find candle
 find_program(WIX_CANDLE_EXECUTABLE candle
-	PATH "$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin" "$ENV{${_PF86}}/WiX Toolset v3.10/bin" "$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
+	PATH
+		"$ENV{WIX}"
+		"$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin"
+		"$ENV{${_PF86}}/WiX Toolset v3.10/bin"
+		"$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
 	DOC "Path to the WiX candle utility."
 )
 
 # Find light
 find_program(WIX_LIGHT_EXECUTABLE light
-	PATH "$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin" "$ENV{${_PF86}}/WiX Toolset v3.10/bin" "$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
+	PATH
+		"$ENV{WIX}"
+		"$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin"
+		"$ENV{${_PF86}}/WiX Toolset v3.10/bin"
+		"$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
 	DOC "Path to the WiX light utility."
 )
 
-# Find light
+# Find heat
 find_program(WIX_HEAT_EXECUTABLE heat
-	PATH "$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin" "$ENV{${_PF86}}/WiX Toolset v3.10/bin" "$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
+	PATH
+		"$ENV{WIX}"
+		"$ENV{PROGRAMFILES}/WiX Toolset v3.10/bin"
+		"$ENV{${_PF86}}/WiX Toolset v3.10/bin"
+		"$ENV{PROGRAMW6432}/WiX Toolset v3.10/bin"
 	DOC "Path to the WiX heat utility."
 )
