d7ac90bfab4f52fedecf6d00f84d6d196a71d40b
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-01-01 00:00:01 +0100

    Happy new Year 2017 !
    Change-Id: I8ae8a1fdc8d0df0779ef119c527f41dac9e0dbdb
    Reviewed-on: https://code.wireshark.org/review/19476
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- wsutil/copyright_info.c
+++ wsutil/copyright_info.c
@@ -31,7 +31,7 @@ const char *
 get_copyright_info(void)
 {
 	return
-		"Copyright 1998-2016 Gerald Combs <gerald@wireshark.org> and contributors.\n"
+		"Copyright 1998-2017 Gerald Combs <gerald@wireshark.org> and contributors.\n"
 		"License GPLv2+: GNU GPL version 2 or later <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\n"
 		"This is free software; see the source for copying conditions. There is NO\n"
 		"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
