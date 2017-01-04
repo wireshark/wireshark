6d03ab27de2bb9abc397c3db557756613394e031
Guy Harris <guy@alum.mit.edu>
2017-01-03 20:42:12 -0800

    Put symlinks to the executables in Contents/Resources/bin.
    PackageMaker appears not to put them into the installer package, so
    construct them in the Wireshark post-install script.
    
    Bug: 13270
    Change-Id: Idfa10d4d123d2c0e2f7b3ad65888e075fbfd27a7
    Reviewed-on: https://code.wireshark.org/review/19531
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 2cc2c2f883b4ce4ca8420877816332086e096940)
    Reviewed-on: https://code.wireshark.org/review/19543

--- packaging/macosx/Scripts/wireshark-postinstall.sh
+++ packaging/macosx/Scripts/wireshark-postinstall.sh
@@ -19,10 +19,22 @@ do
 	rm -f "$PLUGINS_PATH"/$plugin.so "$PLUGINS_PATH"/$plugin.la
 done
 
+BIN_PATH="$2/Wireshark.app/Contents/Resources/bin"
+#
+# For some reason, packagemaker isn't including Contents/Resources/bin
+# in the app bundle, so let's just construct it ourselves.
+#
+for i in capinfos dftest dumpcap editcap mergecap randpkt rawshark \
+    tshark text2pcap      
+do
+	ln -s "$BIN_PATH/$i" ../../MacOS/"$i"
+done
+ln -s "$BIN_PATH/wireshark" ../../MacOS/Wireshark
+
 # Setting PATH
 # if /etc/paths.d/Wireshark already exists we overwrite it.
 #
-WSPATH="$2/Wireshark.app/Contents/Resources/bin"
+WSPATH="$BIN_PATH"
 
 if [ ! -d /etc/paths.d ]
 then
