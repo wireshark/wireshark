387e7af2180327774beaa86cee3f9528146141cf
Pascal Quantin <pascal.quantin@gmail.com>
2017-01-20 08:08:52 +0100

    tap-tcp-stream.h: fix double declaration introduced in g73b5e3d
    Change-Id: I2656e323b9ea6d41566ab0cef1e4a94e3591b08c
    Reviewed-on: https://code.wireshark.org/review/19687
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- ui/tap-tcp-stream.h
+++ ui/tap-tcp-stream.h
@@ -131,22 +131,6 @@ tcp_seq_after(guint32 s1, guint32 s2) {
     return (s1 != s2) && !tcp_seq_before(s1, s2);
 }
 
-static inline int
-tcp_seq_before(guint32 s1, guint32 s2) {
-    return (gint32)(s1 - s2) < 0;
-}
-
-static inline int
-tcp_seq_eq_or_after(guint32 s1, guint32 s2) {
-    return !tcp_seq_before(s1, s2);
-}
-
-static inline int
-tcp_seq_after(guint32 s1, guint32 s2) {
-    return (s1 != s2) && !tcp_seq_before(s1, s2);
-}
-
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
