991f1b59bec9c0ec945ef3b443d354af55c6b332
Jakub Zawadzki <darkjames-ws@darkjames.pl>
2017-02-26 11:49:32 +0100

    Return correct filter for get_hostlist_filter()
    It was returning (ip.src == ... && <protocol>.port == ...).
    
    Now, I think correctly returns (ip.addr == ... && <protocol>.port == ...)
    
    Change-Id: I242e13c0c79c2222e2b27fc2f9ee348b89d21ec1
    Reviewed-on: https://code.wireshark.org/review/20281
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Jakub Zawadzki <darkjames-ws@darkjames.pl>
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    (cherry picked from commit 59b672e5907d272ea5bd5ef571b03e004d07705a)
    Reviewed-on: https://code.wireshark.org/review/20304

--- epan/dissectors/packet-sctp.c
+++ epan/dissectors/packet-sctp.c
@@ -876,13 +876,26 @@ static const char* sctp_host_get_filter_type(hostlist_talker_t* host, conv_filte
         return CONV_FILTER_INVALID;
     }
 
-    if (filter == CONV_FT_SRC_ADDRESS || filter == CONV_FT_DST_ADDRESS || filter == CONV_FT_ANY_ADDRESS) {
+    if (filter == CONV_FT_SRC_ADDRESS) {
+        if (host->myaddress.type == AT_IPv4)
+            return "ip.src";
+        if (host->myaddress.type == AT_IPv6)
+            return "ipv6.src";
+    }
+
+    if (filter == CONV_FT_DST_ADDRESS) {
+        if (host->myaddress.type == AT_IPv4)
+            return "ip.dst";
+        if (host->myaddress.type == AT_IPv6)
+            return "ipv6.dst";
+    }
+
+    if (filter == CONV_FT_ANY_ADDRESS) {
         if (host->myaddress.type == AT_IPv4)
             return "ip.addr";
         if (host->myaddress.type == AT_IPv6)
             return "ipv6.addr";
     }
-
 
     return CONV_FILTER_INVALID;
 }
