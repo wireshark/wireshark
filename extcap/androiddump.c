5c6f6333768e16fc682b732db561754434f41272
Michal Labedzki <michal.labedzki@tieto.com>
2017-02-27 07:44:39 +0100

    androiddump: Use only non-blocking connect() instead of the socket
    Architecture of androiddump based on "blocking" sockets,
    however for start-up it is better to use non-bloking connect()
    to avoid long waiting time then fail.
    
    Change-Id: I2bb8ea51e24db4dd1f5a6b97e1d2bc0156342d97
    Reviewed-on: https://code.wireshark.org/review/20293
    Petri-Dish: Michal Labedzki <michal.tomasz.labedzki@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- extcap/androiddump.c
+++ extcap/androiddump.c
@@ -305,8 +305,10 @@ static void useNormalConnectTimeout(socket_handle_t  sock) {
     int res_rcv;
 #ifdef _WIN32
     const DWORD socket_timeout = 0;
+    unsigned long non_blocking = 0;
 
     res_rcv = setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char *) &socket_timeout, sizeof(socket_timeout));
+    ioctlsocket(sock, FIONBIO, &non_blocking);
 #else
     const struct timeval socket_timeout = {
         .tv_sec = SOCKET_RW_TIMEOUT_MS / 1000,
