81161ddd02bb3204d2c8124bd9e228fee40a715e
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-03-05 16:35:02 +0100

    ieee80211: fix this statement may fall through [-Werror=implicit-fallthrough=] found by gcc7
    Change-Id: I294e0f426d760d0ccb59ebd8ea509245af1293a5
    Reviewed-on: https://code.wireshark.org/review/20405
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    (cherry picked from commit bd9b2b7eac963ca6320b82b26b223ebf8b9d450b)
    Reviewed-on: https://code.wireshark.org/review/28139
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- epan/dissectors/packet-ieee80211.c
+++ epan/dissectors/packet-ieee80211.c
@@ -17081,6 +17081,7 @@ dissect_ieee80211_common(tvbuff_t *tvb, packet_info *pinfo,
                            "Non-DMG STA shouldn't transmit BlockAckReq frame with Extended Compressed BlockAckReq");
                    break;
                 }
+              /* FALL THROUGH */
               case 2: /* Compressed BlockAckReq */
               {
                 proto_tree_add_item(bar_sub_tree,
