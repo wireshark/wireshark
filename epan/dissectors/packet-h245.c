784169bcb779e7c3c5092fd70435c1192eb8f18c
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-03-19 19:33:21 +0100

    h245 (asn1): fix typo identifer -> identifier
    found by lintian
    
    Change-Id: I15f734e1d2218756867cefbd311b620c8a84e9ac
    Reviewed-on: https://code.wireshark.org/review/20635
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-h245.c
+++ epan/dissectors/packet-h245.c
@@ -6648,7 +6648,7 @@ dissect_h245_SET_SIZE_1_256_OF_CapabilityDescriptor(tvbuff_t *tvb _U_, int offse
 static int
 dissect_h245_T_subMessageIdentifier(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
 #line 582 "./asn1/h245/h245.cnf"
-  guint32 subMessageIdentifer;
+  guint32 subMessageIdentifier;
   gef_ctx_t *gefx;
 
   gefx = gef_ctx_get(actx->private_data);
@@ -6662,16 +6662,16 @@ dissect_h245_T_subMessageIdentifier(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_
   }
 
   offset = dissect_per_constrained_integer(tvb, offset, actx, tree, hf_index,
-                                                            0U, 127U, &subMessageIdentifer, FALSE);
+                                                            0U, 127U, &subMessageIdentifier, FALSE);
 
   if (gefx) {
-    gefx->subid = wmem_strdup_printf(wmem_packet_scope(), "%u", subMessageIdentifer);
+    gefx->subid = wmem_strdup_printf(wmem_packet_scope(), "%u", subMessageIdentifier);
     gef_ctx_update_key(gef_ctx_get(actx->private_data));
   }
   if (hf_index == hf_h245_subMessageIdentifier_standard)
   {
-    col_append_fstr(actx->pinfo->cinfo, COL_INFO, "%s", val_to_str(subMessageIdentifer, h245_h239subMessageIdentifier_vals, "<unknown>") );
-    g_snprintf(h245_pi->frame_label, 50, "%s", val_to_str(subMessageIdentifer, h245_h239subMessageIdentifier_vals, "<unknown>"));
+    col_append_fstr(actx->pinfo->cinfo, COL_INFO, "%s", val_to_str(subMessageIdentifier, h245_h239subMessageIdentifier_vals, "<unknown>") );
+    g_snprintf(h245_pi->frame_label, 50, "%s", val_to_str(subMessageIdentifier, h245_h239subMessageIdentifier_vals, "<unknown>"));
   }
 
 
