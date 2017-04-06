438459183bebf650ba05bf1c3b4aa69ba0b011e6
Martin Kaiser <wireshark@kaiser.cx>
2017-04-06 20:41:03 +0200

    kpasswd: remove unused entry hf_kpasswd_newpassword
    Change-Id: I46a295ee55f97c9b384aca07f6de50bc85bd5151
    Reviewed-on: https://code.wireshark.org/review/20954
    Reviewed-by: Martin Kaiser <wireshark@kaiser.cx>
    Petri-Dish: Martin Kaiser <wireshark@kaiser.cx>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-kpasswd.c
+++ epan/dissectors/packet-kpasswd.c
@@ -42,7 +42,6 @@ static int hf_kpasswd_message_len = -1;
 static int hf_kpasswd_version = -1;
 static int hf_kpasswd_result = -1;
 static int hf_kpasswd_result_string = -1;
-static int hf_kpasswd_newpassword = -1;
 static int hf_kpasswd_ap_req_len = -1;
 static int hf_kpasswd_ap_req_data = -1;
 static int hf_kpasswd_krb_priv_message = -1;
@@ -282,9 +281,6 @@ proto_register_kpasswd(void)
         VALS(kpasswd_result_types), 0, NULL, HFILL }},
     { &hf_kpasswd_result_string,
         { "Result String", "kpasswd.result_string", FT_STRING, BASE_NONE,
-        NULL, 0, NULL, HFILL }},
-    { &hf_kpasswd_newpassword,
-        { "New Password", "kpasswd.new_password", FT_STRING, BASE_NONE,
         NULL, 0, NULL, HFILL }},
     { &hf_kpasswd_ap_req_data,
         { "AP_REQ", "kpasswd.ap_req", FT_NONE, BASE_NONE,
