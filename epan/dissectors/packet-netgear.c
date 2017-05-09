a78ff1093c9a776a601eb873b63ac1f52806b40b
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-05-09 18:56:33 +0200

    nge: add modelines info
    Change-Id: Ib75f637c36be575a85e75fee6282d1a16455bfd6
    Reviewed-on: https://code.wireshark.org/review/21572
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Charlie Lenahan <clenahan@sonicbison.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-netgear.c
+++ epan/dissectors/packet-netgear.c
@@ -215,3 +215,16 @@ proto_reg_handoff_nge(void)
     nge_handle = create_dissector_handle(dissect_nge, proto_nge);
     dissector_add_for_decode_as_with_preference("udp.port", nge_handle);
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
