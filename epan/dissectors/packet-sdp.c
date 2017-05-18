13d52a5e0b6cd22cd835087ab50ee212ce4bcd7f
Jaap Keuter <jaap.keuter@xs4all.nl>
2017-05-19 00:45:30 +0200

    SDP: Handle incomplete media attributes
    Media attributes have a format of name:value, where, if the value
    is missing the colon is missing too. Sometimes the colon is there,
    while the value is missing. Even though this is in error, continue
    dissecting the other attributes.
    
    Bug: 13698
    Change-Id: I369b6db657b33a368f4b12134412100774105615
    Reviewed-on: https://code.wireshark.org/review/21699
    Petri-Dish: Anders Broman <a.broman58@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit e71219c711e80a5534a03ab03e1c412cd53c1998)
    Reviewed-on: https://code.wireshark.org/review/21715
    Petri-Dish: Jaap Keuter <jaap.keuter@xs4all.nl>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>

--- epan/dissectors/packet-sdp.c
+++ epan/dissectors/packet-sdp.c
@@ -1307,6 +1307,7 @@ static void dissect_sdp_media_attribute(tvbuff_t *tvb, packet_info *pinfo, proto
     proto_tree   *sdp_media_attribute_tree, *parameter_item;
     proto_item   *fmtp_item, *media_format_item, *parameter_tree;
     proto_tree   *fmtp_tree;
+    proto_item   *pi;
     gint          offset, next_offset, tokenlen, n, colon_offset;
     /*??guint8   *field_name;*/
     const guint8 *payload_type;
@@ -1332,9 +1333,9 @@ static void dissect_sdp_media_attribute(tvbuff_t *tvb, packet_info *pinfo, proto
 
     /* Attribute field name is token before ':' */
     tokenlen = colon_offset - offset;
-    proto_tree_add_item(sdp_media_attribute_tree,
-                        hf_media_attribute_field,
-                        tvb, offset, tokenlen, ENC_UTF_8|ENC_NA);
+    pi = proto_tree_add_item(sdp_media_attribute_tree,
+                             hf_media_attribute_field,
+                             tvb, offset, tokenlen, ENC_UTF_8|ENC_NA);
     /*??field_name = tvb_get_string_enc(wmem_packet_scope(), tvb, offset, tokenlen, ENC_ASCII);*/
     sdp_media_attrbute_code = find_sdp_media_attribute_names(tvb, offset, tokenlen);
 
@@ -1344,9 +1345,13 @@ static void dissect_sdp_media_attribute(tvbuff_t *tvb, packet_info *pinfo, proto
     offset = tvb_skip_wsp(tvb, offset, tvb_captured_length_remaining(tvb, offset));
 
     /* Value is the remainder of the line */
-    attribute_value = tvb_get_string_enc(wmem_packet_scope(), tvb, offset, tvb_captured_length_remaining(tvb, offset), ENC_UTF_8|ENC_NA);
-
-
+    if (tvb_captured_length_remaining(tvb, offset) > 0)
+        attribute_value = tvb_get_string_enc(wmem_packet_scope(), tvb, offset, tvb_captured_length_remaining(tvb, offset), ENC_UTF_8|ENC_NA);
+    else
+    {
+        expert_add_info(pinfo, pi, &ei_sdp_invalid_line_fields);
+        return;
+    }
 
     /*********************************************/
     /* Special parsing for some field name types */
