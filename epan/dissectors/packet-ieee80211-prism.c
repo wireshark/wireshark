a092eaa09e96132ae58188807fdbc22026b9b49c
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-05-31 07:37:24 +0200

    80211 (prism): fix conflict for hf field
    Remove hf_ieee80211_prism_did_sig_rate.
    
    Change-Id: Id75deae2c074ecbb771e33c4a6f3855cb2ba9d9f
    Reviewed-on: https://code.wireshark.org/review/21848
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-ieee80211-prism.c
+++ epan/dissectors/packet-ieee80211-prism.c
@@ -72,8 +72,7 @@ static int hf_ieee80211_prism_did_unknown = -1;
 static int hf_ieee80211_prism_did_sig_a1 = -1;
 static int hf_ieee80211_prism_did_sig_a2 = -1;
 static int hf_ieee80211_prism_did_sig_b = -1;
-static int hf_ieee80211_prism_did_sig_rate = -1;
-static int hf_ieee80211_prism_did_sig_rate_field= -1;
+static int hf_ieee80211_prism_did_sig_rate_field = -1;
 
 
 static gint ett_prism = -1;
@@ -855,15 +854,12 @@ dissect_prism(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U
             case PRISM_TYPE1_RATE_SIG_B:
             case PRISM_TYPE2_RATE_SIG_B:
                 if (tree && rate_phy1 && rate_phy2) {
-                    proto_item *sig_item, *sig_sub_item;
-                    proto_tree *sig_tree;
+                    proto_item *sig_sub_item;
 
                     proto_tree_add_item(prism_did_tree, hf_ieee80211_prism_did_sig_b, tvb, offset, 4, byte_order);
                     proto_item_append_text(ti_did, " 0x%x", tvb_get_letohl(tvb, offset));
 
-                    sig_item = proto_tree_add_item(prism_tree, hf_ieee80211_prism_did_sig_rate, tvb, offset, 4, byte_order);
-                    sig_tree = proto_item_add_subtree(sig_item, ett_sig_ab);
-                    sig_sub_item = proto_tree_add_item(sig_tree, hf_ieee80211_prism_did_sig_rate_field, tvb, offset, 4, byte_order);
+                    sig_sub_item = proto_tree_add_item(prism_tree, hf_ieee80211_prism_did_sig_rate_field, tvb, offset, 4, byte_order);
                     proto_item_append_text(sig_sub_item, " %s", prism_rate_return_sig(rate_phy1, rate_phy2, &phdr));
                   }
                   break;
@@ -972,10 +968,6 @@ static hf_register_info hf_prism[] = {
     { &hf_ieee80211_prism_did_sig_b,
      {"SIG", "prism.did.sigb", FT_UINT32, BASE_HEX, NULL, 0x0,
      NULL, HFILL}},
-
-    { &hf_ieee80211_prism_did_sig_rate,
-     {"Rate (In Mb/s)", "prism.did.rate", FT_NONE, BASE_NONE, 0, 0x0,
-      NULL, HFILL }},
 
     { &hf_ieee80211_prism_did_sig_rate_field,
      {"SIG Field", "prism.did.sigab", FT_NONE, BASE_NONE, 0, 0x0,
