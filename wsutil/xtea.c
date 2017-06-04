345046c83d780c6c32a729ce5945b869f27c37f5
Ahmad Fatoum <ahmad@a3f.at>
2017-06-04 12:32:13 +0200

    wsutil: Add XTEA block cipher
    XTEA is a 64-bit block Feistel cipher with a 128-bit key and a suggested
    64 rounds. It's used by the MMORPG Tibia for encrypting game server traffic.
    
    Usual XTEA treats the blocks as big-endian. Tibia treats them as little
    endian, therefore both versions are provided.
    
    Change-Id: I9ad0c8e066f848b20772ce4e1d3df19deff307b8
    Reviewed-on: https://code.wireshark.org/review/21942
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

---
+++ wsutil/xtea.c
@@ -0,0 +1,70 @@
+/* xtea.c
+ * Implementation of XTEA cipher
+ * By Ahmad Fatoum <ahmad[AT]a3f.at>
+ * Copyright 2017 Ahmad Fatoum
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <glib.h>
+#include <string.h>
+
+#include "pint.h"
+#include "xtea.h"
+
+void decrypt_xtea_ecb(guint8 output[8], const guint8 v_in[8], const guint32 key[4], guint num_rounds)
+{
+    guint i;
+    guint32 v[2], delta = 0x9E3779B9, sum = delta * num_rounds;
+
+    v[0] = pntoh32(&v_in[0]);
+    v[1] = pntoh32(&v_in[4]);
+
+    for (i = 0; i < num_rounds; i++) {
+        v[1] -= (((v[0] << 4) ^ (v[0] >> 5)) + v[0]) ^ (sum + key[(sum >> 11) & 3]);
+        sum -= delta;
+        v[0] -= (((v[1] << 4) ^ (v[1] >> 5)) + v[1]) ^ (sum + key[sum & 3]);
+    }
+
+    v[0] = GUINT32_TO_BE(v[0]);
+    v[1] = GUINT32_TO_BE(v[1]);
+
+    memcpy(output, v, sizeof v);
+}
+
+void decrypt_xtea_le_ecb(guint8 output[8], const guint8 v_in[8], const guint32 key[4], guint num_rounds)
+{
+    guint i;
+    guint32 v[2], delta = 0x9E3779B9, sum = delta * num_rounds;
+
+    v[0] = pletoh32(&v_in[0]);
+    v[1] = pletoh32(&v_in[4]);
+
+    for (i = 0; i < num_rounds; i++) {
+        v[1] -= (((v[0] << 4) ^ (v[0] >> 5)) + v[0]) ^ (sum + key[(sum >> 11) & 3]);
+        sum -= delta;
+        v[0] -= (((v[1] << 4) ^ (v[1] >> 5)) + v[1]) ^ (sum + key[sum & 3]);
+    }
+
+    v[0] = GUINT32_TO_LE(v[0]);
+    v[1] = GUINT32_TO_LE(v[1]);
+
+    memcpy(output, v, sizeof v);
+}
---
+++ wsutil/xtea.h
@@ -0,0 +1,39 @@
+/* xtea.h
+ * Implementation of XTEA cipher
+ * By Ahmad Fatoum <ahmad[AT]a3f.at>
+ * Copyright 2017 Ahmad Fatoum
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef __XTEA_H__
+#define __XTEA_H__
+
+/* Actual XTEA is big-endian, nevertheless there exist protocols that treat every block
+ * as little endian, so we provide both
+ */
+#include "ws_symbol_export.h"
+#include <glib.h>
+
+WS_DLL_PUBLIC void decrypt_xtea_ecb(guint8 plaintext[8], const guint8 ciphertext[8], const guint32 key[4], guint num_rounds);
+
+WS_DLL_PUBLIC void decrypt_xtea_le_ecb(guint8 plaintext[8], const guint8 ciphertext[8], const guint32 key[4], guint num_rounds);
+
+#endif /* __XTEA_H__ */
