bf13a89707f8fb9b273d5cb18f4e2d343c816836
Alexis La Goutte <alexis.lagoutte@gmail.com>
2017-06-06 13:22:21 +0000

    epl: fix no previous prototype for function 'profile_load' [-Wmissing-prototypes]
    Change-Id: I5c3119436984cc7de3dcf4dffd1961481cacc553
    Reviewed-on: https://code.wireshark.org/review/21982
    Petri-Dish: Roland Knall <rknall@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>
    (cherry picked from commit a5a4a0bd55769ad63fa568897b0da504aab9a6c2)
    Reviewed-on: https://code.wireshark.org/review/21997
    Reviewed-by: Roland Knall <rknall@gmail.com>

--- epan/dissectors/packet-epl.c
+++ epan/dissectors/packet-epl.c
@@ -5113,7 +5113,7 @@ dissect_epl_sdo_command_read_by_index(struct epl_convo *convo, proto_tree *epl_t
 
 static gboolean epl_profile_uat_fld_fileopen_check_cb(void *, const char *path, guint len, const void *, const void *, char **err);
 
-struct profile *profile_load(wmem_allocator_t *allocator, const char *path)
+static struct profile *profile_load(wmem_allocator_t *allocator, const char *path)
 {
 	struct profile *profile = NULL;
 	char *err;
