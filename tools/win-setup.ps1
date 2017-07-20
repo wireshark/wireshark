4e6aaeeee766119c27b49a1751daf67944fc84e1
Gerald Combs <gerald@wireshark.org>
2017-07-19 18:04:11 -0700

    Update WinSparkle.
    Change-Id: I28f248647267d3a43bf1be051b8bca728ada6d97
    Reviewed-on: https://code.wireshark.org/review/22723
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- tools/win-setup.ps1
+++ tools/win-setup.ps1
@@ -99,8 +99,8 @@ Param(
 # trouble instead of trying to catch exceptions everywhere.
 $ErrorActionPreference = "Stop"
 
-$Win64CurrentTag = "2017-05-11"
-$Win32CurrentTag = "2017-05-11"
+$Win64CurrentTag = "2017-07-19-2.4"
+$Win32CurrentTag = "2017-07-19-2.4"
 
 # Archive file / subdir.
 $Win64Archives = @{
@@ -123,7 +123,7 @@ $Win64Archives = @{
     "snappy-1.1.3-1-win64ws.zip" = "";
     "spandsp-0.0.6-1-win64ws.zip" = "";
     "upx303w.zip" = "";
-    "WinSparkle-0.5.3.zip" = "";
+    "WinSparkle-0.5.4.zip" = "";
     "WpdPack_4_1_2.zip" = "";
     "zlib-1.2.8-ws.zip" = "";
 }
@@ -148,7 +148,7 @@ $Win32Archives = @{
     "snappy-1.1.3-1-win32ws.zip" = "";
     "spandsp-0.0.6-1-win32ws.zip" = "";
     "upx303w.zip" = "";
-    "WinSparkle-0.5.3.zip" = "";
+    "WinSparkle-0.5.4.zip" = "";
     "WpdPack_4_1_2.zip" = "";
     "zlib-1.2.8-ws.zip" = "";
 }
