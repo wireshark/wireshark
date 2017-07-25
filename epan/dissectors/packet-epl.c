97519ce5bbb94b31bc6e075a7a39c590af53bd36
Christoph Schlosser <christoph@schlosser.xyz>
2017-07-25 08:11:45 +0200

    EPL: Add padding to size calculation for last frame
     The padding was not part of the size calculation of the last subpayload in a
     multiple read/write by index.
    
    Change-Id: Ibbd3ded345352ea1ceaea7b871fc2d1a0e1a6832
    Reviewed-on: https://code.wireshark.org/review/22781
    Reviewed-by: Christoph Schlosser <christoph@schlosser.xyz>
    Reviewed-by: Roland Knall <rknall@gmail.com>
    (cherry picked from commit 7fadc161724cb5d3e1d3f8aa449c8078e5ffa9e1)
    Reviewed-on: https://code.wireshark.org/review/22785
    Petri-Dish: Roland Knall <rknall@gmail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-epl.c
+++ epan/dissectors/packet-epl.c
@@ -4294,7 +4294,7 @@ dissect_epl_sdo_command_write_multiple_by_index(struct epl_convo *convo, proto_t
 				if ( remlength < EPL_SOA_EPLV_OFFSET )
 					break;
 
-				size = remlength - EPL_SOA_EPLV_OFFSET;
+				size = remlength - EPL_SOA_EPLV_OFFSET - padding;
 				lastentry = TRUE;
 			}
 			else
@@ -4636,7 +4636,7 @@ dissect_epl_sdo_command_read_multiple_by_index(struct epl_convo *convo, proto_tr
 				if ( remlength < EPL_SOA_EPLV_OFFSET )
 					break;
 
-				size = remlength - EPL_SOA_EPLV_OFFSET;
+				size = remlength - EPL_SOA_EPLV_OFFSET - padding;
 				lastentry = TRUE;
 			}
 			else
