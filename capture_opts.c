wireshark/wireshark

commit aca55a29f7b982e7a0bd9911d1d176561c8d7a84

Add hardware timestamping support
pcap provides a pcap_set_tstamp_type function, which can be used to request
hardware timestamps from a supporting kernel.

This patch adds support for aforementioned function as well as two new
command line options to dumpcap, wireshark and tshark:

    --list-time-stamp-types
                             List time stamp types supported for the interface
    --time-stamp-type <type>
                             Change the interface's timestamp method

Name choice mimics those used by tcpdump(1), which already supports this
feature. However, unlike tcpdump, we provide both options unconditionally.
If Wireshark was configured without pcap_set_tstamp_type being available,
--list-time-stamp-types reports an empty list.

Change-Id: I418a4b2b84cb01949cd262aad0ad8427f5ac0652
Signed-off-by: Ahmad Fatoum <ahmad.fatoum@siemens.com>
Reviewed-on: https://code.wireshark.org/review/23113
Petri-Dish: Guy Harris <guy@alum.mit.edu>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Anders Broman <a.broman58@gmail.com>

capture_opts.c
@@ -90,6 +90,7 @@ capture_opts_init(capture_options *capture_opts)
     capture_opts->default_options.sampling_method = CAPTURE_SAMP_NONE;
     capture_opts->default_options.sampling_param  = 0;
 #endif
+    capture_opts->default_options.timestamp_type  = NULL;
     capture_opts->saving_to_file                  = FALSE;
     capture_opts->save_file                       = NULL;
     capture_opts->group_read_access               = FALSE;
@@ -193,6 +194,7 @@ capture_opts_log(const char *log_domain, GLogLevelFlags log_level, capture_optio
         g_log(log_domain, log_level, "Sampling meth.[%02d]  : %d", i, interface_opts.sampling_method);
         g_log(log_domain, log_level, "Sampling param.[%02d] : %d", i, interface_opts.sampling_param);
 #endif
+        g_log(log_domain, log_level, "Timestamp type [%02d] : %s", i, interface_opts.timestamp_type);
     }
     g_log(log_domain, log_level, "Interface name[df]  : %s", capture_opts->default_options.name ? capture_opts->default_options.name : "(unspecified)");
     g_log(log_domain, log_level, "Interface Descr[df] : %s", capture_opts->default_options.descr ? capture_opts->default_options.descr : "(unspecified)");
@@ -233,6 +235,7 @@ capture_opts_log(const char *log_domain, GLogLevelFlags log_level, capture_optio
     g_log(log_domain, log_level, "Sampling meth. [df] : %d", capture_opts->default_options.sampling_method);
     g_log(log_domain, log_level, "Sampling param.[df] : %d", capture_opts->default_options.sampling_param);
 #endif
+    g_log(log_domain, log_level, "Timestamp type [df] : %s", capture_opts->default_options.timestamp_type ? capture_opts->default_options.timestamp_type : "(unspecified)");
     g_log(log_domain, log_level, "SavingToFile        : %u", capture_opts->saving_to_file);
     g_log(log_domain, log_level, "SaveFile            : %s", (capture_opts->save_file) ? capture_opts->save_file : "");
     g_log(log_domain, log_level, "GroupReadAccess     : %u", capture_opts->group_read_access);
@@ -732,6 +735,7 @@ capture_opts_add_iface_opt(capture_options *capture_opts, const char *optarg_str
     interface_opts.sampling_method = capture_opts->default_options.sampling_method;
     interface_opts.sampling_param  = capture_opts->default_options.sampling_param;
 #endif
+    interface_opts.timestamp_type  = capture_opts->default_options.timestamp_type;
 
     g_array_append_val(capture_opts->ifaces, interface_opts);
 
@@ -799,6 +803,18 @@ capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg_
         break;
     case 'H':        /* Hide capture info dialog box */
         capture_opts->show_info = FALSE;
+        break;
+    case LONGOPT_SET_TSTAMP_TYPE:        /* Set capture time stamp type */
+        if (capture_opts->ifaces->len > 0) {
+            interface_options interface_opts;
+
+            interface_opts = g_array_index(capture_opts->ifaces, interface_options, capture_opts->ifaces->len - 1);
+            capture_opts->ifaces = g_array_remove_index(capture_opts->ifaces, capture_opts->ifaces->len - 1);
+            interface_opts.timestamp_type = g_strdup(optarg_str_p);
+            g_array_append_val(capture_opts->ifaces, interface_opts);
+        } else {
+            capture_opts->default_options.timestamp_type = g_strdup(optarg_str_p);
+        }
         break;
     case 'i':        /* Use interface x */
         status = capture_opts_add_iface_opt(capture_opts, optarg_str_p);
@@ -939,26 +955,40 @@ capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg_
 }
 
 void
-capture_opts_print_if_capabilities(if_capabilities_t *caps, char *name,
-                                   gboolean monitor_mode)
+capture_opts_print_if_capabilities(if_capabilities_t *caps, char *name, int queries)
 {
-    GList *lt_entry;
-    data_link_info_t *data_link_info;
+    GList *lt_entry, *ts_entry;
 
-    if (caps->can_set_rfmon)
-        printf("Data link types of interface %s when %sin monitor mode (use option -y to set):\n",
-               name, monitor_mode ? "" : "not ");
-    else
-        printf("Data link types of interface %s (use option -y to set):\n", name);
-    for (lt_entry = caps->data_link_types; lt_entry != NULL;
-         lt_entry = g_list_next(lt_entry)) {
-        data_link_info = (data_link_info_t *)lt_entry->data;
-        printf("  %s", data_link_info->name);
-        if (data_link_info->description != NULL)
-            printf(" (%s)", data_link_info->description);
+    if (queries & CAPS_QUERY_LINK_TYPES) {
+        if (caps->can_set_rfmon)
+            printf("Data link types of interface %s when %sin monitor mode (use option -y to set):\n",
+                   name, queries & CAPS_MONITOR_MODE ? "" : "not ");
         else
-            printf(" (not supported)");
-        printf("\n");
+            printf("Data link types of interface %s (use option -y to set):\n", name);
+        for (lt_entry = caps->data_link_types; lt_entry != NULL;
+             lt_entry = g_list_next(lt_entry)) {
+            data_link_info_t *data_link_info = (data_link_info_t *)lt_entry->data;
+            printf("  %s", data_link_info->name);
+            if (data_link_info->description != NULL)
+                printf(" (%s)", data_link_info->description);
+            else
+                printf(" (not supported)");
+            printf("\n");
+        }
+    }
+
+    if (queries & CAPS_QUERY_TIMESTAMP_TYPES) {
+        printf("Timestamp types of the interface (use option --time-stamp-type to set):\n");
+        for (ts_entry = caps->timestamp_types; ts_entry != NULL;
+             ts_entry = g_list_next(ts_entry)) {
+            timestamp_info_t *timestamp = (timestamp_info_t *)ts_entry->data;
+            printf("  %s", timestamp->name);
+            if (timestamp->description != NULL)
+                printf(" (%s)", timestamp->description);
+            else
+                printf(" (none)");
+            printf("\n");
+        }
     }
 }
 

capture_opts.h
@@ -60,7 +60,9 @@ extern "C" {
  * In short: we must not use 1 here, which is another reason to use
  * values outside the range of ASCII graphic characters.
  */
-#define LONGOPT_NUM_CAP_COMMENT 128
+#define LONGOPT_NUM_CAP_COMMENT   128
+#define LONGOPT_LIST_TSTAMP_TYPES 129
+#define LONGOPT_SET_TSTAMP_TYPE   130
 
 /*
  * Options for capturing common to all capturing programs.
@@ -89,17 +91,20 @@ extern "C" {
 #endif
 
 #define LONGOPT_CAPTURE_COMMON \
-    {"capture-comment",      required_argument, NULL, LONGOPT_NUM_CAP_COMMENT}, \
-    {"autostop",             required_argument, NULL, 'a'}, \
-    {"ring-buffer",          required_argument, NULL, 'b'}, \
+    {"capture-comment",       required_argument, NULL, LONGOPT_NUM_CAP_COMMENT}, \
+    {"autostop",              required_argument, NULL, 'a'}, \
+    {"ring-buffer",           required_argument, NULL, 'b'}, \
     LONGOPT_BUFFER_SIZE \
-    {"list-interfaces",      no_argument,       NULL, 'D'}, \
-    {"interface",            required_argument, NULL, 'i'}, \
+    {"list-interfaces",       no_argument,       NULL, 'D'}, \
+    {"interface",             required_argument, NULL, 'i'}, \
     LONGOPT_MONITOR_MODE \
-    {"list-data-link-types", no_argument,       NULL, 'L'}, \
-    {"no-promiscuous-mode",  no_argument,       NULL, 'p'}, \
-    {"snapshot-length",      required_argument, NULL, 's'}, \
-    {"linktype",             required_argument, NULL, 'y'}, \
+    {"list-data-link-types",  no_argument,       NULL, 'L'}, \
+    {"no-promiscuous-mode",   no_argument,       NULL, 'p'}, \
+    {"snapshot-length",       required_argument, NULL, 's'}, \
+    {"linktype",              required_argument, NULL, 'y'}, \
+    {"list-time-stamp-types", no_argument,       NULL, LONGOPT_LIST_TSTAMP_TYPES}, \
+    {"time-stamp-type",       required_argument, NULL, LONGOPT_SET_TSTAMP_TYPE},
+
 
 #define OPTSTRING_CAPTURE_COMMON \
     "a:" OPTSTRING_A "b:" OPTSTRING_B "c:Df:i:" OPTSTRING_I "Lps:y:"
@@ -248,6 +253,9 @@ typedef struct interface_options_tag {
     capture_sampling  sampling_method;
     int               sampling_param;
 #endif
+    gchar            *timestamp_type;       /* requested timestamp as string */
+    int               timestamp_type_id;    /* Timestamp type to pass to pcap_set_tstamp_type.
+                                               only valid if timestamp_type != NULL */
 } interface_options;
 
 /** Capture options coming from user interface */
@@ -342,10 +350,15 @@ capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg,
 extern void
 capture_opts_log(const char *log_domain, GLogLevelFlags log_level, capture_options *capture_opts);
 
+enum caps_query {
+    CAPS_MONITOR_MODE          = 0x1,
+    CAPS_QUERY_LINK_TYPES      = 0x2,
+    CAPS_QUERY_TIMESTAMP_TYPES = 0x4
+};
+
 /* print interface capabilities, including link layer types */
 extern void
-capture_opts_print_if_capabilities(if_capabilities_t *caps, char *name,
-                                   gboolean monitor_mode);
+capture_opts_print_if_capabilities(if_capabilities_t *caps, char *name, int queries);
 
 /* print list of interfaces */
 extern void

acinclude.m4
@@ -412,7 +412,7 @@ install a newer version of the header file.])
 	    AC_DEFINE(CAN_SET_CAPTURE_BUFFER_SIZE, 1,
 	     [Define to 1 if the capture buffer size can be set.])
 	  ])
-	  AC_CHECK_FUNCS(bpf_image pcap_set_tstamp_precision)
+	  AC_CHECK_FUNCS(bpf_image pcap_set_tstamp_precision pcap_set_tstamp_type)
 	fi
 
 	AC_WIRESHARK_POP_FLAGS

capchild/capture_ifinfo.c
@@ -228,11 +228,10 @@ capture_get_if_capabilities(const gchar *ifname, gboolean monitor_mode,
                             char **err_str, void (*update_cb)(void))
 {
     if_capabilities_t *caps;
-    GList              *linktype_list = NULL;
+    GList              *linktype_list = NULL, *timestamp_list = NULL;
     int                 err, i;
     gchar              *data, *primary_msg, *secondary_msg;
-    gchar             **raw_list, **lt_parts;
-    data_link_info_t   *data_link_info;
+    gchar             **raw_list;
 
     g_log(LOG_DOMAIN_CAPTURE, G_LOG_LEVEL_MESSAGE, "Capture Interface Capabilities ...");
 
@@ -309,11 +308,12 @@ capture_get_if_capabilities(const gchar *ifname, gboolean monitor_mode,
     }
 
     /*
-     * The rest are link-layer types.
+     * The following are link-layer types.
      */
-    for (i = 1; raw_list[i] != NULL; i++) {
+    for (i = 1; raw_list[i] != NULL && *raw_list[i] != '\0'; i++) {
+        data_link_info_t *data_link_info;
         /* ...and what if the interface name has a tab in it, Mr. Clever Programmer? */
-        lt_parts = g_strsplit(raw_list[i], "\t", 3);
+        char **lt_parts = g_strsplit(raw_list[i], "\t", 3);
         if (lt_parts[0] == NULL || lt_parts[1] == NULL || lt_parts[2] == NULL) {
             g_strfreev(lt_parts);
             continue;
@@ -330,6 +330,25 @@ capture_get_if_capabilities(const gchar *ifname, gboolean monitor_mode,
 
         linktype_list = g_list_append(linktype_list, data_link_info);
     }
+
+    if (raw_list[i]) { /* Oh, timestamp types! */
+        for (i++; raw_list[i] != NULL && *raw_list[i] != '\0'; i++) {
+            timestamp_info_t *timestamp_info;
+            char **tt_parts = g_strsplit(raw_list[i], "\t", 2);
+            if (tt_parts[0] == NULL || tt_parts[1] == NULL) {
+                g_strfreev(tt_parts);
+                continue;
+            }
+
+            timestamp_info = g_new(timestamp_info_t,1);
+            timestamp_info->name = g_strdup(tt_parts[0]);
+            timestamp_info->description = g_strdup(tt_parts[1]);
+            g_strfreev(tt_parts);
+
+            timestamp_list = g_list_append(timestamp_list, timestamp_info);
+        }
+    }
+
     g_strfreev(raw_list);
 
     /* Check to see if we built a list */
@@ -340,7 +359,11 @@ capture_get_if_capabilities(const gchar *ifname, gboolean monitor_mode,
         g_free(caps);
         return NULL;
     }
+
     caps->data_link_types = linktype_list;
+    /* Might be NULL. Not all systems report timestamp types */
+    caps->timestamp_types = timestamp_list;
+
     return caps;
 }
 

capchild/capture_sync.c
@@ -412,6 +412,10 @@ sync_pipe_start(capture_options *capture_opts, capture_session *cap_session, inf
             argv = sync_pipe_add_arg(argv, &argc, ssampling);
         }
 #endif
+        if (interface_opts.timestamp_type) {
+            argv = sync_pipe_add_arg(argv, &argc, "--time-stamp-type");
+            argv = sync_pipe_add_arg(argv, &argc, interface_opts.timestamp_type);
+        }
     }
 
     /* dumpcap should be running in capture child mode (hidden feature) */
@@ -1302,6 +1306,7 @@ sync_if_capabilities_open(const gchar *ifname, gboolean monitor_mode, const gcha
     argv = sync_pipe_add_arg(argv, &argc, "-i");
     argv = sync_pipe_add_arg(argv, &argc, ifname);
     argv = sync_pipe_add_arg(argv, &argc, "-L");
+    argv = sync_pipe_add_arg(argv, &argc, "--list-time-stamp-types");
     if (monitor_mode)
         argv = sync_pipe_add_arg(argv, &argc, "-I");
     if (auth) {

caputils/capture-pcap-util.c
@@ -668,11 +668,24 @@ free_linktype_cb(gpointer data, gpointer user_data _U_)
 	g_free(linktype_info);
 }
 
+static void
+free_timestamp_cb(gpointer data, gpointer user_data _U_)
+{
+	/* timestamp_info_t's contents are immutable and in static memory,
+	 * so we only need to free the struct itself
+	 */
+	g_free(data);
+}
+
 void
 free_if_capabilities(if_capabilities_t *caps)
 {
 	g_list_foreach(caps->data_link_types, free_linktype_cb, NULL);
 	g_list_free(caps->data_link_types);
+
+	g_list_foreach(caps->timestamp_types, free_timestamp_cb, NULL);
+	g_list_free(caps->timestamp_types);
+
 	g_free(caps);
 }
 
@@ -861,10 +874,7 @@ create_data_link_info(int dlt)
 	else
 		data_link_info->name = g_strdup_printf("DLT %d", dlt);
 	text = pcap_datalink_val_to_description(dlt);
-	if (text != NULL)
-		data_link_info->description = g_strdup(text);
-	else
-		data_link_info->description = NULL;
+	data_link_info->description = g_strdup(text);
 	return data_link_info;
 }
 
@@ -959,6 +969,34 @@ get_data_link_types(pcap_t *pch, interface_options *interface_opts,
 		*err_str = NULL;
 	return data_link_types;
 }
+
+/* Get supported timestamp types for a libpcap device.  */
+static GList*
+get_pcap_timestamp_types(pcap_t *pch _U_, char **err_str _U_)
+{
+	GList *list = NULL;
+#ifdef HAVE_PCAP_SET_TSTAMP_TYPE
+	int *types;
+	int ntypes = pcap_list_tstamp_types(pch, &types);
+
+	if (err_str)
+		*err_str = ntypes < 0 ? pcap_geterr(pch) : NULL;
+
+	if (ntypes <= 0)
+		return NULL;
+
+	while (ntypes--) {
+		timestamp_info_t *info = (timestamp_info_t *)g_malloc(sizeof *info);
+		info->name        = pcap_tstamp_type_val_to_name(types[ntypes]);
+		info->description = pcap_tstamp_type_val_to_description(types[ntypes]);
+		list = g_list_prepend(list, info);
+	}
+
+	pcap_free_tstamp_types(types);
+#endif
+	return list;
+}
+
 
 #ifdef HAVE_PCAP_CREATE
 #ifdef HAVE_BONDING
@@ -1079,6 +1117,8 @@ get_if_capabilities_pcap_create(interface_options *interface_opts,
 		return NULL;
 	}
 
+	caps->timestamp_types = get_pcap_timestamp_types(pch, NULL);
+
 	pcap_close(pch);
 
 	if (err_str != NULL)
@@ -1088,7 +1128,7 @@ get_if_capabilities_pcap_create(interface_options *interface_opts,
 
 pcap_t *
 open_capture_device_pcap_create(capture_options *capture_opts
-#ifdef HAVE_PCAP_SET_TSTAMP_PRECISION
+#if defined(HAVE_PCAP_SET_TSTAMP_PRECISION) || defined (HAVE_PCAP_SET_TSTAMP_TYPE)
     ,
 #else
     _U_,
@@ -1139,6 +1179,18 @@ open_capture_device_pcap_create(capture_options *capture_opts
 		 */
 		if (capture_opts->use_pcapng)
 			request_high_resolution_timestamp(pcap_h);
+#endif /* HAVE_PCAP_SET_TSTAMP_PRECISION */
+
+#ifdef HAVE_PCAP_SET_TSTAMP_TYPE
+		if (interface_opts->timestamp_type) {
+			err = pcap_set_tstamp_type(pcap_h, interface_opts->timestamp_type_id);
+			if (err == PCAP_ERROR) {
+				g_strlcpy(*open_err_str, pcap_geterr(pcap_h),
+				    sizeof *open_err_str);
+				pcap_close(pcap_h);
+				return NULL;
+			}
+		}
 #endif /* HAVE_PCAP_SET_TSTAMP_PRECISION */
 
 		g_log(LOG_DOMAIN_CAPTURE_CHILD, G_LOG_LEVEL_DEBUG,
@@ -1194,6 +1246,8 @@ get_if_capabilities_pcap_open_live(interface_options *interface_opts,
 		g_free(caps);
 		return NULL;
 	}
+
+	caps->timestamp_types = get_pcap_timestamp_types(pch, NULL);
 
 	pcap_close(pch);
 
@@ -1295,8 +1349,8 @@ get_if_capabilities(interface_options *interface_opts, char **err_str)
         caps->data_link_types = NULL;
         deflt = get_pcap_datalink(pch, interface_opts->name);
         data_link_info = create_data_link_info(deflt);
-        caps->data_link_types = g_list_append(caps->data_link_types,
-                                              data_link_info);
+        caps->data_link_types = g_list_append(caps->data_link_types, data_link_info);
+	caps->timestamp_types = get_pcap_timestamp_types(pch, NULL);
         pcap_close(pch);
 
         if (err_str != NULL)

caputils/capture_ifinfo.h
@@ -99,6 +99,7 @@ void free_interface_list(GList *if_list);
 typedef struct {
 	gboolean	can_set_rfmon;	/* TRUE if can be put into monitor mode */
 	GList		*data_link_types;	/* GList of data_link_info_t's */
+	GList		*timestamp_types;   /* GList of timestamp_info_t's */
 } if_capabilities_t;
 
 /*
@@ -109,6 +110,14 @@ typedef struct {
 	char	*name;          /* e.g. "EN10MB" or "DLT 1" */
 	char	*description;   /* descriptive name from wiretap e.g. "Ethernet", NULL if unknown */
 } data_link_info_t;
+
+/*
+ * Information about timestamp types.
+ */
+typedef struct {
+	const char	*name;          /* e.g. "adapter_unsynced" */
+	const char	*description;   /* description from libpcap e.g. "Adapter, not synced with system time" */
+} timestamp_info_t;
 
 /**
  * Fetch the linktype list for the specified interface from a child process.

cmake/modules/FindPCAP.cmake
@@ -85,6 +85,7 @@ if( PCAP_FOUND )
   check_function_exists( "bpf_image" HAVE_BPF_IMAGE )
   check_function_exists( "pcap_setsampling" HAVE_PCAP_SETSAMPLING )
   check_function_exists( "pcap_set_tstamp_precision" HAVE_PCAP_SET_TSTAMP_PRECISION )
+  check_function_exists( "pcap_set_tstamp_type" HAVE_PCAP_SET_TSTAMP_TYPE )
   # Remote pcap checks
   check_function_exists( "pcap_open" HAVE_PCAP_OPEN )
   if( HAVE_PCAP_OPEN )

commit be69761c78d0c285f88ea6b2da0399fcecf9d782

Fix requesting hardware timestamps without -k
The interface_options struct passed to dumpcap is populated
differently when running Wireshark with and without -k.

Previously, only with -k was there a valid pointer in
interface_opts.timestamp_type

Fixes: aca55a2 ("Add hardware timestamping support")
Signed-off-by: Ahmad Fatoum <ahmad.fatoum@siemens.com>
Change-Id: Ic7ecc5a1190c28197d6a7271f1b353f74d43ca61
Reviewed-on: https://code.wireshark.org/review/23160
Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Michael Mann <mmann78@netscape.net>

capture_opts.c
@@ -810,9 +810,11 @@ capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg_
 
             interface_opts = g_array_index(capture_opts->ifaces, interface_options, capture_opts->ifaces->len - 1);
             capture_opts->ifaces = g_array_remove_index(capture_opts->ifaces, capture_opts->ifaces->len - 1);
+            g_free(interface_opts.timestamp_type);
             interface_opts.timestamp_type = g_strdup(optarg_str_p);
             g_array_append_val(capture_opts->ifaces, interface_opts);
         } else {
+            g_free(capture_opts->default_options.timestamp_type);
             capture_opts->default_options.timestamp_type = g_strdup(optarg_str_p);
         }
         break;
@@ -1161,6 +1163,7 @@ capture_opts_del_iface(capture_options *capture_opts, guint if_index)
     if (interface_opts.console_display_name != NULL)
         g_free(interface_opts.console_display_name);
     g_free(interface_opts.cfilter);
+    g_free(interface_opts.timestamp_type);
 #ifdef HAVE_EXTCAP
     g_free(interface_opts.extcap);
     g_free(interface_opts.extcap_fifo);
@@ -1209,6 +1212,7 @@ collect_ifaces(capture_options *capture_opts)
             interface_opts.console_display_name = g_strdup(device.name);
             interface_opts.linktype = device.active_dlt;
             interface_opts.cfilter = g_strdup(device.cfilter);
+            interface_opts.timestamp_type = g_strdup(device.timestamp_type);
             interface_opts.snaplen = device.snaplen;
             interface_opts.has_snaplen = device.has_snaplen;
             interface_opts.promisc_mode = device.pmode;
@@ -1277,6 +1281,7 @@ capture_opts_free_interface_t(interface_t *device)
         g_free(device->friendly_name);
         g_free(device->addresses);
         g_free(device->cfilter);
+        g_free(device->timestamp_type);
         g_list_foreach(device->links,
                        capture_opts_free_interface_t_links, NULL);
         g_list_free(device->links);

capture_opts.h
@@ -201,6 +201,7 @@ typedef struct interface_tag {
     /* External capture cached data */
     GHashTable     *external_cap_args_settings;
 #endif
+    gchar          *timestamp_type;
 } interface_t;
 
 typedef struct link_row_tag {

ui/gtk/capture_dlg.c
@@ -1302,6 +1302,7 @@ insert_new_rows(GList *rlist)
       device.snaplen = global_capture_opts.default_options.snaplen;
     }
     device.cfilter = g_strdup(global_capture_opts.default_options.cfilter);
+    device.timestamp_type = g_strdup(global_capture_opts.default_options.timestamp_type);
     monitor_mode = prefs_capture_device_monitor_mode(if_string);
     if (roptions->remote_host_opts.auth_type == CAPTURE_AUTH_PWD) {
       auth_str = g_strdup_printf("%s:%s", roptions->remote_host_opts.auth_username,
@@ -2656,6 +2657,7 @@ void options_interface_cb(GtkTreeView *view, GtkTreePath *path, GtkTreeViewColum
   device.has_snaplen = FALSE;
   device.snaplen = WTAP_MAX_PACKET_SIZE_STANDARD;
   device.cfilter = NULL;
+  device.timestamp_type = NULL;
 #ifdef CAN_SET_CAPTURE_BUFFER_SIZE
   device.buffer = DEFAULT_CAPTURE_BUFFER_SIZE;
 #endif
@@ -3507,6 +3509,7 @@ add_pipe_cb(gpointer w _U_)
     device.has_snaplen  = global_capture_opts.default_options.has_snaplen;
     device.snaplen      = global_capture_opts.default_options.snaplen;
     device.cfilter      = g_strdup(global_capture_opts.default_options.cfilter);
+    device.timestamp_type = g_strdup(global_capture_opts.default_options.timestamp_type);
     device.addresses    = NULL;
     device.no_addresses = 0;
     device.last_packets = 0;

ui/iface_lists.c
@@ -80,6 +80,7 @@ fill_from_ifaces (interface_t *device)
         device->snaplen = interface_opts.snaplen;
         g_free(device->cfilter);
         device->cfilter = g_strdup(interface_opts.cfilter);
+        device->timestamp_type = g_strdup(interface_opts.timestamp_type);
         if (interface_opts.linktype != -1) {
             device->active_dlt = interface_opts.linktype;
         }
@@ -304,6 +305,7 @@ scan_local_interfaces(void (*update_cb)(void))
             device.snaplen = global_capture_opts.default_options.snaplen;
         }
         device.cfilter      = g_strdup(global_capture_opts.default_options.cfilter);
+        device.timestamp_type = g_strdup(global_capture_opts.default_options.timestamp_type);
 #ifdef CAN_SET_CAPTURE_BUFFER_SIZE
         if ((device.buffer = capture_dev_user_buffersize_find(if_info->name)) == -1) {
             device.buffer = global_capture_opts.default_options.buffer_size;
@@ -372,6 +374,7 @@ scan_local_interfaces(void (*update_cb)(void))
             device.has_snaplen = interface_opts.has_snaplen;
             device.snaplen = interface_opts.snaplen;
             device.cfilter = g_strdup(interface_opts.cfilter);
+            device.timestamp_type = g_strdup(interface_opts.timestamp_type);
             device.active_dlt = interface_opts.linktype;
             device.addresses    = NULL;
             device.no_addresses = 0;

ui/qt/manage_interfaces_dialog.cpp
@@ -234,6 +234,7 @@ void ManageInterfacesDialog::on_addPipe_clicked()
     device.has_snaplen  = global_capture_opts.default_options.has_snaplen;
     device.snaplen      = global_capture_opts.default_options.snaplen;
     device.cfilter      = g_strdup(global_capture_opts.default_options.cfilter);
+    device.timestamp_type = g_strdup(global_capture_opts.default_options.timestamp_type);
 #ifdef CAN_SET_CAPTURE_BUFFER_SIZE
     device.buffer       = DEFAULT_CAPTURE_BUFFER_SIZE;
 #endif
@@ -350,6 +351,7 @@ void ManageInterfacesDialog::updateRemoteInterfaceList(GList* rlist, remote_opti
             device.snaplen = global_capture_opts.default_options.snaplen;
         }
         device.cfilter = g_strdup(global_capture_opts.default_options.cfilter);
+        device.timestamp_type = g_strdup(global_capture_opts.default_options.timestamp_type);
         monitor_mode = prefs_capture_device_monitor_mode(if_string);
         if (roptions->remote_host_opts.auth_type == CAPTURE_AUTH_PWD) {
             auth_str = g_strdup_printf("%s:%s", roptions->remote_host_opts.auth_username,