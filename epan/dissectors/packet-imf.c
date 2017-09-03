8bd04c8be3304731dc319a35d1e6974193bcd3c1
Martin Mathieson <martin.r.mathieson@googlemail.com>
2017-09-03 20:50:40 +0100

    IMF: preserve carrying protocol (e.g. SMTP, POP, IMAP) in protocol column
    Change-Id: I8331040bbc9866878e74ad4c5f6b986dd94603fc
    Reviewed-on: https://code.wireshark.org/review/23381
    Petri-Dish: Martin Mathieson <martin.r.mathieson@googlemail.com>
    Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-imf.c
+++ epan/dissectors/packet-imf.c
@@ -752,7 +752,11 @@ dissect_imf(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_)
     eo_info->subject_data = "";
   }
 
+  /* Want to preserve existing protocol name and show that it is carrying IMF */
+  col_append_str(pinfo->cinfo, COL_PROTOCOL, "|");
+  col_set_fence(pinfo->cinfo, COL_PROTOCOL);
   col_set_str(pinfo->cinfo, COL_PROTOCOL, PSNAME);
+
   col_clear(pinfo->cinfo, COL_INFO);
 
   item = proto_tree_add_item(tree, proto_imf, tvb, 0, -1, ENC_NA);
