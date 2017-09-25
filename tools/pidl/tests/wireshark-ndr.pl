dc011cc53ebb424ed4fd1383282d929f8688e8e1
Guy Harris <guy@alum.mit.edu>
2017-09-25 00:20:04 -0700

    pidl: fix field2name wireshark dissector test.
    Pick up change from Samba:
    
        commit 82778a6d04580fc05f3f83d44f57f221ad4e8ac4
        Author: Günther Deschner <gd@samba.org>
        Date:   Tue Sep 6 22:37:12 2016 +0200
    
            pidl: fix field2name wireshark dissector test.
    
            Guenther
    
            Signed-off-by: Guenther Deschner <gd@samba.org>
            Reviewed-by: Stefan Metzmacher <metze@samba.org>
    
            Autobuild-User(master): Günther Deschner <gd@samba.org>
            Autobuild-Date(master): Wed Sep  7 16:05:26 CEST 2016 on sn-devel-144
    
    Change-Id: Ia02de7d36b2fcb0e7a5145361548acae844f73ed
    Reviewed-on: https://code.wireshark.org/review/23724
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- tools/pidl/tests/wireshark-ndr.pl
+++ tools/pidl/tests/wireshark-ndr.pl
@@ -14,7 +14,7 @@ use strict;
 use Parse::Pidl::Wireshark::NDR qw(field2name %res PrintIdl StripPrefixes RegisterInterfaceHandoff register_hf_field ProcessImport ProcessInclude find_type DumpEttList DumpEttDeclaration DumpHfList DumpHfDeclaration DumpFunctionTable register_type register_ett);
 
 is("Access Mask", field2name("access_mask"));
-is("Accessmask", field2name("AccessMask"));
+is("AccessMask", field2name("AccessMask"));
 
 my $x = new Parse::Pidl::Wireshark::NDR();
 $x->PrintIdl("foo\nbar\n");
