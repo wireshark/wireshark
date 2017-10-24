0527747e23f85bb9d3cf4ca69364e615bd22e1e9
João Valverde <joao.valverde@tecnico.ulisboa.pt>
2017-10-24 23:09:19 +0100

    Retire use of getprotobynumber()
    Dead weight. If this feature is wanted getprotobynumber()
    should be called once on startup.
    
    Change-Id: I0358bacdc60466f676fa1aab7f4b7c9e588d8d74
    Reviewed-on: https://code.wireshark.org/review/24045
    Petri-Dish: João Valverde <j@v6e.pt>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: João Valverde <j@v6e.pt>

--- epan/ipproto.c
+++ epan/ipproto.c
@@ -269,31 +269,7 @@ static const value_string ipproto_val[] = {
 value_string_ext ipproto_val_ext = VALUE_STRING_EXT_INIT(ipproto_val);
 
 const char *ipprotostr(const int proto) {
-    const char *s;
-
-    if ((s = try_val_to_str_ext(proto, &ipproto_val_ext)) != NULL)
-    return s;
-
-    s = "Unknown";
-
-#ifdef HAVE_GETPROTOBYNUMBER
-    /*
-     * XXX - have another flag for resolving network-layer
-     * protocol names?
-     */
-    if (gbl_resolv_flags.mac_name || gbl_resolv_flags.network_name ||
-        gbl_resolv_flags.transport_name) {
-        static char buf[128];
-        struct protoent *pe;
-
-    pe = getprotobynumber(proto);
-    if (pe) {
-        g_strlcpy(buf, pe->p_name, sizeof(buf));
-        s = buf;
-    }
-    }
-#endif
-    return s;
+    return val_to_str_ext_const(proto, &ipproto_val_ext, "Unknown");
 }
 
 /* https://www.iana.org/assignments/ipv6-parameters/ipv6-parameters.xhtml#extension-header */
