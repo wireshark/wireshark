5968fc1cae5028002d4f974e0464608f73204b60
João Valverde <joao.valverde@tecnico.ulisboa.pt>
2017-11-20 19:58:46 +0000

    Rename UseMakeDissectorReg.cmake
    Change-Id: I2723e7f0309dbe21f23b65818fbea3a7eadf13d4
    Reviewed-on: https://code.wireshark.org/review/24514
    Petri-Dish: João Valverde <j@v6e.pt>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: João Valverde <j@v6e.pt>

--- cmake/modules/UseMakePluginReg.cmake
+++ cmake/modules/UseMakePluginReg.cmake
@@ -1,21 +1,18 @@
 #
-MACRO(REGISTER_DISSECTOR_FILES _outputfile _registertype )
+MACRO(REGISTER_PLUGIN_FILES _outputfile _registertype )
 	include(LocatePythonModule)
 	locate_python_module(make-plugin-reg REQUIRED PATHS ${CMAKE_SOURCE_DIR}/tools)
 
-	set( _makeregistertype ${_registertype} )
-	set( _sources ${ARGN} )
-	set( _depends ${_sources} )
 	ADD_CUSTOM_COMMAND(
 	    OUTPUT
 	      ${_outputfile}
 	    COMMAND ${PYTHON_EXECUTABLE}
 	      ${PY_MAKE-PLUGIN-REG}
 	      ${CMAKE_CURRENT_SOURCE_DIR}
-	      ${_makeregistertype}
-	      ${_sources}
+	      ${_registertype}
+	      ${ARGN}
 	    DEPENDS
-	      ${_depends}
+	      ${ARGN}
 	      ${PY_MAKE-PLUGIN-REG}
 	)
-ENDMACRO(REGISTER_DISSECTOR_FILES)
+ENDMACRO(REGISTER_PLUGIN_FILES)
