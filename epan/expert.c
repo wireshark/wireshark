f945b85391976bc59da4b542fe84cb58033bd22e
Stig Bjørlykke <stig@bjorlykke.org>
2017-12-12 23:34:03 +0100

    epan: Use expert info summary text as field name
    Change the expert info registry to use summary text as field name instead
    of blurb to show the correct column header tooltip in custom columns.
    
    Preserve backward compability by not use empty summary text.
    
    Change-Id: Ibbaf142165be0d9f42d1e2476f39f8d251ea0593
    Reviewed-on: https://code.wireshark.org/review/24788
    Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>

--- epan/expert.c
+++ epan/expert.c
@@ -422,8 +422,10 @@ expert_register_field_array(expert_module_t *module, ei_register_info *exp, cons
 
 		/* Register with the header field info, so it's display filterable */
 		ptr->eiinfo.hf_info.p_id = &ptr->ids->hf;
+		if (ptr->eiinfo.summary && ptr->eiinfo.summary[0]) {
+			ptr->eiinfo.hf_info.hfinfo.name = ptr->eiinfo.summary;
+		}
 		ptr->eiinfo.hf_info.hfinfo.abbrev = ptr->eiinfo.name;
-		ptr->eiinfo.hf_info.hfinfo.blurb = ptr->eiinfo.summary;
 
 		proto_register_field_array(module->proto_id, &ptr->eiinfo.hf_info, 1);
 	}
