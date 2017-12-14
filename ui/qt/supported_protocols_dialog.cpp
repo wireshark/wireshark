68668b785c73a1676ef4573290be7b50abb1c5a8
Michael Mann <mmann78@netscape.net>
2017-12-14 13:44:05 -0500

    Convert Supported Protocols dialog to use model
    Also have search file search name, filter and description columns
    
    Change-Id: Id519c5b1deb4a80e3b95437cf2e6d3e9510e4b11
    Reviewed-on: https://code.wireshark.org/review/24857
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- ui/qt/supported_protocols_dialog.cpp
+++ ui/qt/supported_protocols_dialog.cpp
@@ -4,64 +4,44 @@
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * SPDX-License-Identifier: GPL-2.0+
  */
 
-// warning C4267: 'argument' : conversion from 'size_t' to 'int', possible loss of data
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning(disable : 4267)
-#endif
+#include "config.h"
 
 #include "supported_protocols_dialog.h"
 #include <ui_supported_protocols_dialog.h>
 
-#include "config.h"
-
-#include <algorithm>
-#include <glib.h>
-
-#include <epan/proto.h>
-
-#include <QTreeWidgetItem>
 #include <QElapsedTimer>
 
 #include "wireshark_application.h"
 
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-
-enum { name_col_, filter_col_, type_col_, descr_col_ };
-
 SupportedProtocolsDialog::SupportedProtocolsDialog(QWidget *parent) :
     GeometryStateDialog(parent),
     ui(new Ui::SupportedProtocolsDialog),
-    field_count_(0)
+    supported_protocols_model_(new SupportedProtocolsModel()),
+    proxyModel_(new SupportedProtocolsProxyModel(this))
 {
     ui->setupUi(this);
-    if (parent) loadGeometry(parent->width() * 3 / 4, parent->height());
+
+    proxyModel_->setSourceModel(supported_protocols_model_);
+    ui->supportedProtocolsTreeView->setModel(proxyModel_);
+
+    //always sort by protocol/field name
+    proxyModel_->sort(SupportedProtocolsModel::colName);
+
+    if (parent)
+        loadGeometry(parent->width() * 3 / 4, parent->height());
     setAttribute(Qt::WA_DeleteOnClose, true);
+
     setWindowTitle(wsApp->windowTitleString(tr("Supported Protocols")));
 
     // Some of our names are unreasonably long.
     int one_em = fontMetrics().height();
-    ui->protoTreeWidget->setColumnWidth(name_col_, one_em * 15);
-    ui->protoTreeWidget->setColumnWidth(filter_col_, one_em * 10);
-    ui->protoTreeWidget->setColumnWidth(type_col_, one_em * 12);
-    ui->protoTreeWidget->setColumnWidth(descr_col_, one_em * 30);
+    ui->supportedProtocolsTreeView->setColumnWidth(SupportedProtocolsModel::colName, one_em * 15);
+    ui->supportedProtocolsTreeView->setColumnWidth(SupportedProtocolsModel::colFilter, one_em * 10);
+    ui->supportedProtocolsTreeView->setColumnWidth(SupportedProtocolsModel::colType, one_em * 12);
+    ui->supportedProtocolsTreeView->setColumnWidth(SupportedProtocolsModel::colDescription, one_em * 30);
 
     QTimer::singleShot(0, this, SLOT(fillTree()));
 }
@@ -69,80 +49,26 @@ SupportedProtocolsDialog::SupportedProtocolsDialog(QWidget *parent) :
 SupportedProtocolsDialog::~SupportedProtocolsDialog()
 {
     delete ui;
+    delete supported_protocols_model_;
+    delete proxyModel_;
 }
 
 void SupportedProtocolsDialog::updateStatistics()
 {
     QLocale locale = QLocale::system();
     QString hint = tr("%1 protocols, %2 fields.")
-            .arg(locale.toString(ui->protoTreeWidget->topLevelItemCount()))
-            .arg(locale.toString(field_count_));
+            .arg(locale.toString(supported_protocols_model_->rowCount()))
+            .arg(locale.toString(supported_protocols_model_->fieldCount()));
     ui->hintLabel->setText(hint);
-    wsApp->processEvents(QEventLoop::ExcludeUserInputEvents | QEventLoop::ExcludeSocketNotifiers, 1);
 }
 
-// Nearly identical to DisplayFilterExpressionDialog::fillTree.
 void SupportedProtocolsDialog::fillTree()
 {
-    void *proto_cookie;
-    QList <QTreeWidgetItem *> proto_list;
-
-    for (int proto_id = proto_get_first_protocol(&proto_cookie); proto_id != -1;
-         proto_id = proto_get_next_protocol(&proto_cookie)) {
-        protocol_t *protocol = find_protocol_by_id(proto_id);
-        QTreeWidgetItem *proto_ti = new QTreeWidgetItem();
-        proto_ti->setText(name_col_, proto_get_protocol_short_name(protocol));
-        proto_ti->setText(filter_col_, proto_get_protocol_filter_name(proto_id));
-        // type_col_ empty
-        proto_ti->setText(descr_col_, proto_get_protocol_long_name(protocol));
-        proto_ti->setData(name_col_, Qt::UserRole, proto_id);
-        proto_list << proto_ti;
-    }
-
+    supported_protocols_model_->populate();
     updateStatistics();
-    ui->protoTreeWidget->invisibleRootItem()->addChildren(proto_list);
-    ui->protoTreeWidget->sortByColumn(name_col_, Qt::AscendingOrder);
-
-    foreach (QTreeWidgetItem *proto_ti, proto_list) {
-        void *field_cookie;
-        int proto_id = proto_ti->data(name_col_, Qt::UserRole).toInt();
-        QList <QTreeWidgetItem *> field_list;
-        for (header_field_info *hfinfo = proto_get_first_protocol_field(proto_id, &field_cookie); hfinfo != NULL;
-             hfinfo = proto_get_next_protocol_field(proto_id, &field_cookie)) {
-            if (hfinfo->same_name_prev_id != -1) continue;
-
-            QTreeWidgetItem *field_ti = new QTreeWidgetItem();
-            field_ti->setText(name_col_, hfinfo->name);
-            field_ti->setText(filter_col_, hfinfo->abbrev);
-            field_ti->setText(type_col_, ftype_pretty_name(hfinfo->type));
-            field_ti->setText(descr_col_, hfinfo->blurb);
-            field_list << field_ti;
-
-            field_count_++;
-            if (field_count_ % 10000 == 0) updateStatistics();
-        }
-        std::sort(field_list.begin(), field_list.end());
-        proto_ti->addChildren(field_list);
-    }
-
-    updateStatistics();
-    ui->protoTreeWidget->sortByColumn(name_col_, Qt::AscendingOrder);
 }
 
-// Copied from DisplayFilterExpressionDialog
 void SupportedProtocolsDialog::on_searchLineEdit_textChanged(const QString &search_re)
 {
-    QTreeWidgetItemIterator it(ui->protoTreeWidget);
-    QRegExp regex(search_re, Qt::CaseInsensitive);
-    while (*it) {
-        bool hidden = true;
-        if (search_re.isEmpty() || (*it)->text(0).contains(regex)) {
-            hidden = false;
-        }
-        (*it)->setHidden(hidden);
-        if (!hidden && (*it)->parent()) {
-            (*it)->parent()->setHidden(false);
-        }
-        ++it;
-    }
+    proxyModel_->setFilter(search_re);
 }
--- ui/qt/supported_protocols_dialog.h
+++ ui/qt/supported_protocols_dialog.h
@@ -4,25 +4,14 @@
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * SPDX-License-Identifier: GPL-2.0+
  */
 
 #ifndef SUPPORTED_PROTOCOLS_DIALOG_H
 #define SUPPORTED_PROTOCOLS_DIALOG_H
 
 #include "geometry_state_dialog.h"
+#include <ui/qt/models/supported_protocols_model.h>
 
 namespace Ui {
 class SupportedProtocolsDialog;
@@ -39,7 +28,9 @@ public:
 private:
     Ui::SupportedProtocolsDialog *ui;
 
-    int field_count_;
+    SupportedProtocolsModel* supported_protocols_model_;
+    SupportedProtocolsProxyModel* proxyModel_;
+
     void updateStatistics();
 
 private slots:
