adde937ed74926323b084713caf6fbba871d8791
João Valverde <joao.valverde@tecnico.ulisboa.pt>
2017-12-15 00:04:32 +0000

    plugins: Remove redundant plugin.rc.in files
    plugin.rc is Windows specific, also add condition to reflect that.
    
    Change-Id: Ibbb7dab77dd1f277e2302c8f931218ca433f8c72
    Reviewed-on: https://code.wireshark.org/review/24833
    Reviewed-by: João Valverde <j@v6e.pt>
    Petri-Dish: João Valverde <j@v6e.pt>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- cmake/modules/WiresharkPlugin.cmake
+++ cmake/modules/WiresharkPlugin.cmake
@@ -2,33 +2,41 @@
 
 # Set information
 macro(SET_MODULE_INFO _plugin _ver_major _ver_minor _ver_micro _ver_extra)
-	# Create the Windows .rc file for the plugin.
-	# The values come from several files in the source, I can't see how to reuse them
+	if(WIN32)
+		# Create the Windows .rc file for the plugin.
+		# The values come from several files in the source, I can't see how to reuse them
 
-	set(PACKAGE ${_plugin})
-	set(MODULE_VERSION_MAJOR ${_ver_major})
-	set(MODULE_VERSION_MINOR ${_ver_minor})
-	set(MODULE_VERSION_MICRO ${_ver_micro})
-	set(MODULE_VERSION_EXTRA ${_ver_extra})
-	set(MODULE_VERSION "${MODULE_VERSION_MAJOR}.${MODULE_VERSION_MINOR}.${MODULE_VERSION_MICRO}.${MODULE_VERSION_EXTRA}")
-	set(RC_MODULE_VERSION "${MODULE_VERSION_MAJOR},${MODULE_VERSION_MINOR},${MODULE_VERSION_MICRO},${MODULE_VERSION_EXTRA}")
+		set(PACKAGE ${_plugin})
+		set(MODULE_VERSION_MAJOR ${_ver_major})
+		set(MODULE_VERSION_MINOR ${_ver_minor})
+		set(MODULE_VERSION_MICRO ${_ver_micro})
+		set(MODULE_VERSION_EXTRA ${_ver_extra})
+		set(MODULE_VERSION "${MODULE_VERSION_MAJOR}.${MODULE_VERSION_MINOR}.${MODULE_VERSION_MICRO}.${MODULE_VERSION_EXTRA}")
+		set(RC_MODULE_VERSION "${MODULE_VERSION_MAJOR},${MODULE_VERSION_MINOR},${MODULE_VERSION_MICRO},${MODULE_VERSION_EXTRA}")
 
-	# This info is from Makefile.am
-	set(PLUGIN_NAME ${PACKAGE})
+		# This info is from Makefile.am
+		set(PLUGIN_NAME ${PACKAGE})
 
-	set(MSVC_VARIANT "${CMAKE_GENERATOR}")
+		set(MSVC_VARIANT "${CMAKE_GENERATOR}")
 
-	# The rc.in requires a plain VERSION variable
-	set(VERSION ${PROJECT_VERSION})
+		# The rc.in requires a plain VERSION variable
+		set(VERSION ${PROJECT_VERSION})
 
-	# Create the plugin.rc file from the template
-	configure_file(plugin.rc.in plugin.rc @ONLY)
+		# Create the plugin.rc file from the template
+		if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/plugin.rc.in)
+			set(_plugin_rc_in ${CMAKE_CURRENT_SOURCE_DIR}/plugin.rc.in)
+		else()
+			set(_plugin_rc_in ${CMAKE_SOURCE_DIR}/plugins/plugin.rc.in)
+		endif()
+		configure_file(${_plugin_rc_in} plugin.rc @ONLY)
+		set(HAVE_PLUGIN_RC TRUE)
+	endif()
 endmacro()
 
 macro(ADD_PLUGIN_LIBRARY _plugin _subfolder)
 	add_library(${_plugin} MODULE
 		${PLUGIN_FILES}
-		${CMAKE_CURRENT_BINARY_DIR}/plugin.rc
+		$<$<BOOL:${HAVE_PLUGIN_RC}>:${CMAKE_CURRENT_BINARY_DIR}/plugin.rc>
 	)
 
 	set_target_properties(${_plugin} PROPERTIES
