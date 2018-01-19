9e3e02f5e5662b4edb4cd273305f82000d84e039
Dario Lombardo <lomato@gmail.com>
2018-01-19 15:34:23 +0100

    wiretap: more SPDX license convertions.
    Change-Id: I12695d0713b1d7fe58f09b2037303fab523085e9
    Reviewed-on: https://code.wireshark.org/review/25394
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- wiretap/daintree-sna.c
+++ wiretap/daintree-sna.c
@@ -11,21 +11,7 @@
  * remains. Borrowed many snippets from dbs-etherwatch.c, the
  * daintree_sna_process_hex_data function having the largest chunk.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- */
+* SPDX-License-Identifier: GPL-2.0+ */
 
 /* This module reads capture files saved by Daintree's Sensor Network Analyzer.
  * Daintree captures are plain text files with a two line header,
