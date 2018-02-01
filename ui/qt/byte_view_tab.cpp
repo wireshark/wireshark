wireshark/wireshark

commit 47e1798762478ae19354297e5a95d46487cafd42

Qt: Byte view and proto tree fixes.
Select our byte field only when we enter marked / locked mode.

Emit fieldSelected from autoScrollTo so that we always set the marked
protocol.

Don't clobber the current index in saveSelectedField.

Change-Id: I967b20608f991a5f3e6a0979b1f702f874ce27b4
Reviewed-on: https://code.wireshark.org/review/25521
Reviewed-by: Gerald Combs <gerald@wireshark.org>
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/proto_tree.cpp
@@ -32,7 +32,6 @@
 #include <QWindow>
 #endif
 
-
 // To do:
 // - Fix "apply as filter" behavior.
 
@@ -283,6 +282,22 @@ void ProtoTree::autoScrollTo(const QModelIndex &index)
         return;
     }
 
+    // Find and highlight the protocol bytes. select above won't call
+    // selectionChanged if the current and selected indexes are the same
+    // so we do this here.
+    FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode(), this);
+    if (finfo.isValid()) {
+        QModelIndex parent = index;
+        while (parent.isValid() && parent.parent().isValid()) {
+            parent = parent.parent();
+        }
+        if (parent.isValid()) {
+            FieldInformation parent_finfo(proto_tree_model_->protoNodeFromIndex(parent).protoNode());
+            finfo.setParentField(parent_finfo.fieldInfo());
+        }
+        emit fieldSelected(&finfo);
+    }
+
     ScrollHint scroll_hint = PositionAtTop;
     if (prefs.gui_auto_scroll_percentage > 66) {
         scroll_hint = PositionAtBottom;
@@ -305,44 +320,7 @@ void ProtoTree::selectionChanged(const QItemSelection &selected, const QItemSele
     if (selected.isEmpty()) return;
 
     QModelIndex index = selected.indexes().first();
-
-    FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode(), this);
-    if (!finfo.isValid()) return;
-
-    // Find and highlight the protocol bytes
-    QModelIndex parent = index;
-    while (parent.isValid() && parent.parent().isValid()) {
-        parent = parent.parent();
-    }
-    if (parent.isValid()) {
-        FieldInformation parent_finfo(proto_tree_model_->protoNodeFromIndex(parent).protoNode());
-        finfo.setParentField(parent_finfo.fieldInfo());
-    }
-
-    if ( finfo.isValid() )
-    {
-        saveSelectedField(index);
-        emit fieldSelected(&finfo);
-    }
-    // else the GTK+ version pushes an empty string as described below.
-    /*
-     * Don't show anything if the field name is zero-length;
-     * the pseudo-field for text-only items is such
-     * a field, and we don't want "Text (text)" showing up
-     * on the status line if you've selected such a field.
-     *
-     * XXX - there are zero-length fields for which we *do*
-     * want to show the field name.
-     *
-     * XXX - perhaps the name and abbrev field should be null
-     * pointers rather than null strings for that pseudo-field,
-     * but we'd have to add checks for null pointers in some
-     * places if we did that.
-     *
-     * Or perhaps text-only items should have -1 as the field
-     * index, with no pseudo-field being used, but that might
-     * also require special checks for -1 to be added.
-     */
+    saveSelectedField(index);
 }
 
 void ProtoTree::syncExpanded(const QModelIndex &index) {
@@ -468,11 +446,12 @@ void ProtoTree::selectedFieldChanged(FieldInformation *finfo)
 void ProtoTree::saveSelectedField(QModelIndex &index)
 {
     selected_hfid_path_.clear();
-    while (index.isValid()) {
-        FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode());
+    QModelIndex save_index = index;
+    while (save_index.isValid()) {
+        FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(save_index).protoNode());
         if (!finfo.isValid()) break;
-        selected_hfid_path_.prepend(QPair<int,int>(index.row(), finfo.headerInfo().id));
-        index = index.parent();
+        selected_hfid_path_.prepend(QPair<int,int>(save_index.row(), finfo.headerInfo().id));
+        save_index = save_index.parent();
     }
 }
 

ui/qt/widgets/byte_view_text.cpp
@@ -265,14 +265,16 @@ void ByteViewText::mousePressEvent (QMouseEvent *event) {
     }
 
     if (marked_byte_offset_ < 0) {
+        // Marked mode.
         marked_byte_offset_ = byteOffsetAtPixel(event->pos());
         hovered_byte_offset_ = -1;
+        emit byteSelected(marked_byte_offset_);
+        viewport()->update();
     } else {
+        // Back to hover mode.
         marked_byte_offset_ = -1;
         mouseMoveEvent(event);
     }
-    emit byteSelected(marked_byte_offset_);
-    viewport()->update();
 }
 
 void ByteViewText::mouseMoveEvent(QMouseEvent *event)

commit c84b0aeead7cfaf595b546b99ff8bb73f73c8fc5

Qt: More byte view and proto tree fixes.
47e1798762 broke byte view highlighting when selecting a proto tree
item. Switch back to emitting fieldSelected from selectionChanged. Force
a new selection in selectedFieldChanged, which does what we were trying
to do in 47e1798762.

Clear our marked byte offset in the byte view when we mark a field. Emit
byteSelected whenever we click the mouse.

Don't highlight anything when a tree item is deselected. Deselect a tree
item if we click on something that's not a byte in the byte view.

Change-Id: Ibf419ccb005d69f733b2fe12ce674e1fe504bb96
Reviewed-on: https://code.wireshark.org/review/25541
Reviewed-by: Gerald Combs <gerald@wireshark.org>
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/byte_view_tab.cpp
@@ -255,7 +255,11 @@ void ByteViewTab::selectedFrameChanged(int frameNum)
 
 void ByteViewTab::selectedFieldChanged(FieldInformation *selected)
 {
-    ByteViewText * byte_view_text = 0;
+    // We need to handle both selection and deselection.
+    ByteViewText * byte_view_text = qobject_cast<ByteViewText *>(currentWidget());
+    int f_start = -1, f_length = -1;
+    int p_start = -1, p_length = -1;
+    int fa_start = -1, fa_length = -1;
 
     if (selected) {
         if (selected->parent() == this) {
@@ -268,29 +272,31 @@ void ByteViewTab::selectedFieldChanged(FieldInformation *selected)
         if ( fi )
             byte_view_text = findByteViewTextForTvb(fi->ds_tvb, &idx);
 
-        if (byte_view_text)
-        {
-            int f_start = -1, f_length = -1;
-
-            if (cap_file_->search_in_progress && (cap_file_->hex || (cap_file_->string && cap_file_->packet_data))) {
-                // In the hex view, only highlight the target bytes or string. The entire
-                // field can then be displayed by clicking on any of the bytes in the field.
-                f_start = cap_file_->search_pos - cap_file_->search_len + 1;
-                f_length = (int) cap_file_->search_len;
-            } else {
-                f_start = selected->position().start;
-                f_length = selected->position().length;
-            }
-
-            setCurrentIndex(idx);
-
-            byte_view_text->markField(f_start, f_length);
-            byte_view_text->markProtocol(selected->parentField()->position().start, selected->parentField()->position().length);
-            byte_view_text->markAppendix(selected->appendix().start, selected->appendix().length);
+        if (cap_file_->search_in_progress && (cap_file_->hex || (cap_file_->string && cap_file_->packet_data))) {
+            // In the hex view, only highlight the target bytes or string. The entire
+            // field can then be displayed by clicking on any of the bytes in the field.
+            f_start = cap_file_->search_pos - cap_file_->search_len + 1;
+            f_length = (int) cap_file_->search_len;
+        } else {
+            f_start = selected->position().start;
+            f_length = selected->position().length;
         }
+
+        setCurrentIndex(idx);
+
+        p_start = selected->parentField()->position().start;
+        p_length = selected->parentField()->position().length;
+        fa_start = selected->appendix().start;
+        fa_length = selected->appendix().length;
+    }
+
+    if (byte_view_text)
+    {
+        byte_view_text->markField(f_start, f_length);
+        byte_view_text->markProtocol(p_start, p_length);
+        byte_view_text->markAppendix(fa_start, fa_length);
     }
 }
-
 void ByteViewTab::highlightedFieldChanged(FieldInformation *highlighted)
 {
     ByteViewText * byte_view_text = qobject_cast<ByteViewText *>(currentWidget());

ui/qt/proto_tree.cpp
@@ -282,22 +282,6 @@ void ProtoTree::autoScrollTo(const QModelIndex &index)
         return;
     }
 
-    // Find and highlight the protocol bytes. select above won't call
-    // selectionChanged if the current and selected indexes are the same
-    // so we do this here.
-    FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode(), this);
-    if (finfo.isValid()) {
-        QModelIndex parent = index;
-        while (parent.isValid() && parent.parent().isValid()) {
-            parent = parent.parent();
-        }
-        if (parent.isValid()) {
-            FieldInformation parent_finfo(proto_tree_model_->protoNodeFromIndex(parent).protoNode());
-            finfo.setParentField(parent_finfo.fieldInfo());
-        }
-        emit fieldSelected(&finfo);
-    }
-
     ScrollHint scroll_hint = PositionAtTop;
     if (prefs.gui_auto_scroll_percentage > 66) {
         scroll_hint = PositionAtBottom;
@@ -317,10 +301,29 @@ void ProtoTree::goToHfid(int hfid)
 void ProtoTree::selectionChanged(const QItemSelection &selected, const QItemSelection &deselected)
 {
     QTreeView::selectionChanged(selected, deselected);
-    if (selected.isEmpty()) return;
+    if (selected.isEmpty()) {
+        emit fieldSelected(0);
+        return;
+    }
 
     QModelIndex index = selected.indexes().first();
     saveSelectedField(index);
+
+    // Find and highlight the protocol bytes. select above won't call
+    // selectionChanged if the current and selected indexes are the same
+    // so we do this here.
+    FieldInformation finfo(proto_tree_model_->protoNodeFromIndex(index).protoNode(), this);
+    if (finfo.isValid()) {
+        QModelIndex parent = index;
+        while (parent.isValid() && parent.parent().isValid()) {
+            parent = parent.parent();
+        }
+        if (parent.isValid()) {
+            FieldInformation parent_finfo(proto_tree_model_->protoNodeFromIndex(parent).protoNode());
+            finfo.setParentField(parent_finfo.fieldInfo());
+        }
+        emit fieldSelected(&finfo);
+    }
 }
 
 void ProtoTree::syncExpanded(const QModelIndex &index) {
@@ -431,12 +434,19 @@ void ProtoTree::itemDoubleClicked(const QModelIndex &index) {
 
 void ProtoTree::selectedFieldChanged(FieldInformation *finfo)
 {
-    QModelIndex index = proto_tree_model_->findFieldInformation(finfo);
-    if (!index.isValid() || finfo->parent() == this) {
-        // We only want valid, inbound signals.
+    if (finfo && finfo->parent() == this) {
+        // We only want inbound signals.
         return;
     }
+
+    QModelIndex index = proto_tree_model_->findFieldInformation(finfo);
+    setUpdatesEnabled(false);
+    // The new finfo might match the current index. Clear our selection
+    // so that we force a fresh item selection, so that fieldSelected
+    // will in turn be emitted.
+    selectionModel()->clearSelection();
     autoScrollTo(index);
+    setUpdatesEnabled(true);
 }
 
 // Remember the currently focussed field based on:

ui/qt/widgets/byte_view_text.cpp
@@ -147,6 +147,9 @@ void ByteViewText::markField(int start, int length, bool scroll_to)
 {
     field_start_ = start;
     field_len_ = length;
+    // This might be called as a result of (de)selecting a proto tree
+    // item, so take us out of marked mode.
+    marked_byte_offset_ = -1;
     if (scroll_to) {
         scrollToByte(start);
     }
@@ -264,17 +267,26 @@ void ByteViewText::mousePressEvent (QMouseEvent *event) {
         return;
     }
 
-    if (marked_byte_offset_ < 0) {
-        // Marked mode.
-        marked_byte_offset_ = byteOffsetAtPixel(event->pos());
+    // byteSelected does the following:
+    // - Triggers selectedFieldChanged in ProtoTree, which clears the
+    //   selection and selects the corresponding (or no) item.
+    // - The new tree selection triggers markField, which clobbers
+    //   marked_byte_offset_.
+
+    const bool hover_mode = marked_byte_offset_ < 0;
+    const int byte_offset = byteOffsetAtPixel(event->pos());
+    setUpdatesEnabled(false);
+    emit byteSelected(byte_offset);
+    if (hover_mode && byte_offset >= 0) {
+        // Switch to marked mode.
         hovered_byte_offset_ = -1;
-        emit byteSelected(marked_byte_offset_);
+        marked_byte_offset_ = byte_offset;
         viewport()->update();
     } else {
         // Back to hover mode.
-        marked_byte_offset_ = -1;
         mouseMoveEvent(event);
     }
+    setUpdatesEnabled(true);
 }
 
 void ByteViewText::mouseMoveEvent(QMouseEvent *event)
@@ -290,7 +302,8 @@ void ByteViewText::mouseMoveEvent(QMouseEvent *event)
 
 void ByteViewText::leaveEvent(QEvent *event)
 {
-    emit byteHovered(-1);
+    hovered_byte_offset_ = -1;
+    emit byteHovered(hovered_byte_offset_);
 
     viewport()->update();
     QAbstractScrollArea::leaveEvent(event);