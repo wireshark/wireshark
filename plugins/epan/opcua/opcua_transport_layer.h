d2d1f793f81904b26d4cfc4525abe52aba18341d
Dario Lombardo <lomato@gmail.com>
2018-02-09 09:52:20 +0100

    plugins: use SPDX identifiers.
    Change-Id: I8155573933daeb69c6e4c95c6702bdd6fc1fa89b
    Reviewed-on: https://code.wireshark.org/review/25707
    Petri-Dish: Dario Lombardo <lomato@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- plugins/epan/opcua/opcua_transport_layer.h
+++ plugins/epan/opcua/opcua_transport_layer.h
@@ -2,10 +2,7 @@
 ** Copyright (C) 2006-2009 ascolab GmbH. All Rights Reserved.
 ** Web: http://www.ascolab.com
 **
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the GNU General Public License
-** as published by the Free Software Foundation; either version 2
-** of the License, or (at your option) any later version.
+** SPDX-License-Identifier: GPL-2.0-or-later
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
