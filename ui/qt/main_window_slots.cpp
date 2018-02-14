52287e8e2fa09f412781ba14c5f05831aefa148e
Gerald Combs <gerald@wireshark.org>
2018-02-13 17:08:14 -0800

    Qt: Fix the filter expression toolbar layout.
    Take a hint from the QToolButton sources and update the filter
    expression toolbar using updateGeometry instead of adjustSize. This
    appears to do the layout updates and other changes required to draw the
    dropdown in the correct place.
    
    (cherry picked from commit 885f9e8170e5c842945e8890aec13b62c7e6fe82)
    
    Bug: 14121
    Change-Id: I45d5a1173703f9c16d89841a57f651935da43d92
    Reviewed-on: https://code.wireshark.org/review/25785
    Reviewed-by: Gerald Combs <gerald@wireshark.org>
    Petri-Dish: Gerald Combs <gerald@wireshark.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- ui/qt/main_window_slots.cpp
+++ ui/qt/main_window_slots.cpp
@@ -921,7 +921,9 @@ void MainWindow::filterExpressionsChanged()
     }
 
     if (actions_added) {
-        main_ui_->displayFilterToolBar->adjustSize();
+        // QToolButton calls updateGeometry+update all over the place.
+        // updateGeometry should be sufficient here.
+        main_ui_->displayFilterToolBar->updateGeometry();
     }
 }
 
