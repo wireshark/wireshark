8a5385b9c9ec4ccfd692263eb7c59b09f9bc1a15
Dario Lombardo <lomato@gmail.com>
2018-03-06 15:31:02 +0100

    More licenses converted to SPDX.
    Change-Id: Id4f987dcdacf06622d70263f4659a4400e30dc39
    Reviewed-on: https://code.wireshark.org/review/26332
    Petri-Dish: Dario Lombardo <lomato@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- tools/oss-fuzzshark/build.sh
+++ tools/oss-fuzzshark/build.sh
@@ -1,19 +1,7 @@
 #!/bin/bash -eux
 # Copyright 2017 Google Inc.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 # List of dissectors compiled below, which should be turned off.
 # This is done to avoid single fuzzer (like IP) to call UDP protocols, which can go back to IP, and so on..
