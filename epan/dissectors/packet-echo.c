6733df6e709c1b5bda0af658f34316c92f0db4a7
Martin Kaiser <wireshark@kaiser.cx>
2018-03-26 22:49:54 +0200

    clean up the echo dissector
    simplify the code to distinguish between request and response
    remove the if(tree) check
    remove the offset variable, which is always 0
    reformat the code
    remove the incorrect descriptions ("echo data") for request and response
    don't modify the display filter names
    
    Change-Id: Iffe074ceef5fe2dda9e0b134a8dc4db70881d3e4
    Reviewed-on: https://code.wireshark.org/review/26667
    Reviewed-by: Martin Kaiser <wireshark@kaiser.cx>
    Petri-Dish: Martin Kaiser <wireshark@kaiser.cx>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-echo.c
+++ epan/dissectors/packet-echo.c
@@ -32,57 +32,40 @@ static gint ett_echo = -1;
 
 static int dissect_echo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_)
 {
+  gboolean    request;
+  proto_tree *echo_tree;
+  proto_item *ti, *hidden_item;
 
-  int         offset    = 0;
-  gboolean    request   = FALSE;
-
-  if (pinfo->destport == pinfo->match_uint) {
-    request = TRUE;
-  }
+  request = (pinfo->destport == pinfo->match_uint);
 
   col_set_str(pinfo->cinfo, COL_PROTOCOL, "ECHO");
+  col_set_str(pinfo->cinfo, COL_INFO, request ? "Request" : "Response");
 
-  col_set_str(pinfo->cinfo, COL_INFO,
-                (request) ? "Request" : "Response");
+  ti = proto_tree_add_item(tree, proto_echo, tvb, 0, -1, ENC_NA);
+  echo_tree = proto_item_add_subtree(ti, ett_echo);
 
-  if (tree) {
-    proto_tree *echo_tree;
-    proto_item *ti, *hidden_item;
+  hidden_item = proto_tree_add_boolean(echo_tree,
+      request ?  hf_echo_request : hf_echo_response, tvb, 0, 0, 1);
+  PROTO_ITEM_SET_HIDDEN(hidden_item);
 
-    ti = proto_tree_add_item(tree, proto_echo, tvb, offset, -1, ENC_NA);
-    echo_tree = proto_item_add_subtree(ti, ett_echo);
-
-    if (request) {
-      hidden_item = proto_tree_add_boolean(echo_tree, hf_echo_request, tvb, 0, 0, 1);
-    } else {
-      hidden_item = proto_tree_add_boolean(echo_tree, hf_echo_response, tvb, 0, 0, 1);
-    }
-    PROTO_ITEM_SET_HIDDEN(hidden_item);
-
-    proto_tree_add_item(echo_tree, hf_echo_data, tvb, offset, -1, ENC_NA);
-
-  }
+  proto_tree_add_item(echo_tree, hf_echo_data, tvb, 0, -1, ENC_NA);
 
   return tvb_captured_length(tvb);
-
-} /* dissect_echo */
+}
 
 void proto_register_echo(void)
 {
 
   static hf_register_info hf[] = {
     { &hf_echo_data,
-      { "Echo data",    "echo.data",
-        FT_BYTES,       BASE_NONE,      NULL,   0x0,
-        NULL, HFILL }},
+      { "Echo data", "echo.data", FT_BYTES, BASE_NONE,
+        NULL, 0x0, NULL, HFILL }},
     { &hf_echo_request,
-      { "Echo request", "echo.request",
-        FT_BOOLEAN,     BASE_NONE,      NULL,   0x0,
-        "Echo data", HFILL }},
+      { "Echo request", "echo.request", FT_BOOLEAN, BASE_NONE,
+        NULL, 0x0, NULL, HFILL }},
     { &hf_echo_response,
-      { "Echo response","echo.response",
-        FT_BOOLEAN,     BASE_NONE,      NULL,   0x0,
-        "Echo data", HFILL }}
+      { "Echo response","echo.response", FT_BOOLEAN, BASE_NONE,
+        NULL, 0x0, NULL, HFILL }}
   };
 
   static gint *ett[] = {
@@ -92,19 +75,16 @@ void proto_register_echo(void)
   proto_echo = proto_register_protocol("Echo", "ECHO", "echo");
   proto_register_field_array(proto_echo, hf, array_length(hf));
   proto_register_subtree_array(ett, array_length(ett));
-
 }
 
 void proto_reg_handoff_echo(void)
 {
-
   dissector_handle_t echo_handle;
 
   echo_handle = create_dissector_handle(dissect_echo, proto_echo);
 
   dissector_add_uint_with_preference("udp.port", ECHO_PORT, echo_handle);
   dissector_add_uint_with_preference("tcp.port", ECHO_PORT, echo_handle);
-
 }
 
 /*
