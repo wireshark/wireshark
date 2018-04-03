baa2e30984f044d8621522283b5d16602e376bca
Darius Davis <darius@vmware.com>
2018-04-04 09:06:20 +1000

    Tweak ws_cpuid so that Valgrind is happy.
    Valgrind considers the "cpuid" instruction to always depend on inputs from eax
    and ecx, even though it's only a subset of values of eax for which ecx is
    relevant.  If ecx is undefined when cpuid is executed, the outputs of cpuid
    will be considered undefined.
    
    Instead of suppressing the resulting uninitialised-value warning (the
    suppression for which is now out-of-date anyway, now that
    register_all_protocols is moved to a worker thread), let's simply set ecx to
    zero in ws_cpuid.
    
    Testing done: Built Wireshark on Linux amd64.  Before this change, running
       "tools/valgrind-wireshark.sh ./test/captures/dhcp.pcap" with
       valgrind-3.12.0.SVN on Debian 9.4 amd64 would yield the following Valgrind
       error:
    
       ==2416== Thread 2:
       ==2416== Conditional jump or move depends on uninitialised value(s)
       ==2416==    at 0xACB8B22: ws_mempbrk_sse42_compile (ws_mempbrk_sse42.c:58)
       ==2416==    by 0x74F4960: register_all_protocols_worker (register.c:37)
       ==2416==    by 0xB1403D4: g_thread_proxy (gthread.c:784)
       ==2416==    by 0xD438493: start_thread (pthread_create.c:333)
       ==2416==    by 0xB4CAACE: clone (clone.S:97)
    
       With the change, the above message is gone.  Inspected the disassembly of
       function ws_cpuid, and it looks sane -- just an added "xor ecx, ecx" at the
       top.
    
    Change-Id: I2fb382309cac234c400286a6e9fac7d922912c63
    Reviewed-on: https://code.wireshark.org/review/26733
    Petri-Dish: Anders Broman <a.broman58@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit 5c2eade45d733433bfa5e513950b28819faa8751)
    Reviewed-on: https://code.wireshark.org/review/30106
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- wsutil/ws_cpuid.h
+++ wsutil/ws_cpuid.h
@@ -48,7 +48,8 @@ ws_cpuid(guint32 *CPUInfo, int selector)
 							"=b" (CPUInfo[1]),
 							"=c" (CPUInfo[2]),
 							"=d" (CPUInfo[3])
-						: "a"(selector));
+						: "a" (selector),
+							"c" (0));
 	return TRUE;
 }
 #elif defined(__i386__)
