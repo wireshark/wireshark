b492011ac1dc4b5cecc8ab738ddee8150df518e0
Martin Kaiser <wireshark@kaiser.cx>
2018-04-11 10:08:53 +0800

    zbee_zcl: (trivial) clean up decode_color_xy()
    remove unnecessary return statement
    remove a wrong comment
    
    Change-Id: I40afd8144178a2cccba67289d5a3120dd5719ad0
    Reviewed-on: https://code.wireshark.org/review/26868
    Reviewed-by: Martin Kaiser <wireshark@kaiser.cx>
    Petri-Dish: Martin Kaiser <wireshark@kaiser.cx>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-zbee-zcl-general.c
+++ epan/dissectors/packet-zbee-zcl-general.c
@@ -2303,8 +2303,7 @@ static void
 decode_color_xy(gchar *s, guint16 value)
 {
     g_snprintf(s, ITEM_LABEL_LENGTH, "%.4lf", value/65535.0);
-    return;
-} /*decode_power_conf_voltage*/
+}
 
 /*FUNCTION:------------------------------------------------------
  *  NAME
