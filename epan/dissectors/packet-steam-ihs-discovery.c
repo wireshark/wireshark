72836bbba2f3d31f849bcaa91a36769f854b3d69
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-04-20 07:42:37 +0000

    steam-ihs-discovery.c : fix typo Reponse => Response
    Change-Id: I20157ce72b33b8b957d47fe6abcffb754ce4e5a2
    Reviewed-on: https://code.wireshark.org/review/27043
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit 7124fe671583dd94367c83c4f74e3f2858cbcde0)
    Reviewed-on: https://code.wireshark.org/review/29297
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-steam-ihs-discovery.c
+++ epan/dissectors/packet-steam-ihs-discovery.c
@@ -1372,7 +1372,7 @@ proto_register_steam_ihs_discovery(void)
         },
         /* CMsgRemoteDeviceProofResponse */
         { &hf_steam_ihs_discovery_body_proofresponse_response,
-          { "Reponse", "steam_ihs_discovery.body_proofresponse_response",
+          { "Response", "steam_ihs_discovery.body_proofresponse_response",
             FT_BYTES, BASE_NONE, NULL, 0,
             NULL, HFILL }
         }
