dfb92c2a1d200f777322f532e377bfd8eb1865a8
chinarulezzz <alexandr.savca89@gmail.com>
2018-04-22 21:16:06 +0300

    tools/cppcheck/cppcheck.sh: improve script; add new option: colorize html output
    Change-Id: I34dad2fa9ea80529997103491219027edaf3ac41
    Reviewed-on: https://code.wireshark.org/review/27080
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- tools/cppcheck/cppcheck.sh
+++ tools/cppcheck/cppcheck.sh
@@ -5,6 +5,15 @@
 # Script to run CppCheck Static Analyzer.
 # http://cppcheck.sourceforge.net/
 #
+# Usage: tools/cppcheck/cppcheck.sh [options] [file]
+# Where options can be:
+#       -a      disable suppression list (see $CPPCHECK_DIR/suppressions)
+#       -c      colorize html output
+#       -h      html output (default is gcc)
+#       -t      threads (default: 4)
+#       -v      quiet mode
+# If argument file is omitted then checking all files in the current directory.
+#
 # Wireshark - Network traffic analyzer
 # By Gerald Combs <gerald@wireshark.org>
 # Copyright 2012 Gerald Combs
@@ -20,10 +29,27 @@ QUIET="--quiet"
 SUPPRESSIONS="--suppressions-list=$CPPCHECK_DIR/suppressions"
 INCLUDES="--includes-file=$CPPCHECK_DIR/includes"
 MODE="gcc"
+COLORIZE_HTML_MODE="no"
 
-while getopts "ahj:v" OPTCHAR ; do
+colorize_worker()
+{
+    # always uses stdin/stdout
+    [ "$COLORIZE_HTML_MODE" = "yes" ] && \
+        sed -e '/<td>warning<\/td>/s/^<tr>/<tr bgcolor="#ff3">/' \
+            -e '/<td>error<\/td>/s/^<tr>/<tr bgcolor="#faa">/' \
+        || sed ''
+}
+
+# switcher
+colorize()
+{
+    [ -z "$1" ] && colorize_worker || colorize_worker <<< "$1"
+}
+
+while getopts "achj:v" OPTCHAR ; do
     case $OPTCHAR in
         a) SUPPRESSIONS=" " ;;
+        c) COLORIZE_HTML_MODE="yes" ;;
         h) MODE="html" ;;
         j) THREADS="$OPTARG" ;;
         v) QUIET=" " ;;
@@ -40,6 +66,9 @@ elif [ "$MODE" = "html" ]; then
     TEMPLATE="<tr><td>{file}</td><td>{line}</td><td>{severity}</td><td>{message}</td><td>{id}</td></tr>"
 fi
 
+# Ensure that the COLORIZE_HTML_MODE option is used only with HTML-mode and not with GCC-mode.
+[ "$MODE" = "html" ] && [ "$COLORIZE_HTML_MODE" = "yes" ] || COLORIZE_HTML_MODE="no"
+
 if [ $# -eq 0 ]; then
     TARGET="."
 else
@@ -54,7 +83,7 @@ trap : INT
 $CPPCHECK --force --enable=style $QUIET  \
           $SUPPRESSIONS $INCLUDES        \
           --std=c89 --template=$TEMPLATE \
-          -j $THREADS $TARGET 2>&1
+          -j $THREADS $TARGET 2>&1 | colorize
 
 if [ "$MODE" = "html" ]; then
     echo "</table></body></html>"
