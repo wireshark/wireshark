645db15bba4f102a2e88caaba21894f15a2c145a
Guy Harris <guy@alum.mit.edu>
2018-04-22 20:31:26 -0700

    Add more bounds checks.
    If the calculated packet length in the header is bigger than the actual
    packet length value from the header, reject the packet.
    
    Change-Id: I86cb24c66ee0d6fd2ed6f9240d44c1adc5f0bf91
    Reviewed-on: https://code.wireshark.org/review/27087
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 83ecd467765408847afe6e5acdd7ab974c621f6e)
    Reviewed-on: https://code.wireshark.org/review/27088

--- epan/dissectors/packet-bvlc.c
+++ epan/dissectors/packet-bvlc.c
@@ -171,7 +171,7 @@ dissect_ipv4_bvlc(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *dat
 		bvlc_length = packet_length;
 	}
 
-	if (bvlc_length < 4) {
+	if (bvlc_length < 4 || bvlc_length > packet_length) {
 		return 0;	/* reject */
 	}
 
@@ -375,6 +375,10 @@ dissect_ipv6_bvlc(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *dat
 		break;
 	default:
 		break;
+	}
+
+	if (bvlc_length > packet_length) {
+		return 0;	/* reject */
 	}
 
 	ti = proto_tree_add_item(tree, proto_bvlc, tvb, 0,
