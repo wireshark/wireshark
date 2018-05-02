f48dfe90e3a0fbaf1d7665a3ab9f72ba36306531
Gerald Combs <gerald@wireshark.org>
2018-05-02 11:29:30 -0700

    Windows: Upgrade WinSparkle to 0.5.7.
    Bug: 14642
    Change-Id: Ifc4023220d6dd151b9eba2bb5663e3f31b8a190a
    Reviewed-on: https://code.wireshark.org/review/27278
    Petri-Dish: Gerald Combs <gerald@wireshark.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- tools/win-setup.ps1
+++ tools/win-setup.ps1
@@ -87,8 +87,8 @@ Param(
 # trouble instead of trying to catch exceptions everywhere.
 $ErrorActionPreference = "Stop"
 
-$Win64CurrentTag = "2018-03-16"
-$Win32CurrentTag = "2018-03-16"
+$Win64CurrentTag = "2018-05-02-2.6"
+$Win32CurrentTag = "2018-05-02-2.6"
 
 # Archive file / subdir.
 $Win64Archives = @{
