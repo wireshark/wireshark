990ea00b16cd363d2edd8ce96cf00b82d353e08a
Pascal Quantin <pascal.quantin@gmail.com>
2018-05-07 22:05:56 +0200

    Qt: fix IPv4 GeoIP lookup in endpoints dialog
    maxmind_db_lookup_ipv4() expects an address in network byte order
    
    Bug: 14656
    Change-Id: Ie47e3ae44d305d040e409d42f4398f55ae8c2395
    Reviewed-on: https://code.wireshark.org/review/27391
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Christopher Maynard
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
    (cherry picked from commit c2eb71c25c726213f8dbc03b8c34b84ffaa560d7)
    Reviewed-on: https://code.wireshark.org/review/27398

--- ui/qt/endpoint_dialog.cpp
+++ ui/qt/endpoint_dialog.cpp
@@ -209,7 +209,9 @@ public:
 
         const mmdb_lookup_t *mmdb_lookup = NULL;
         if (endp_item->myaddress.type == AT_IPv4) {
-            mmdb_lookup = maxmind_db_lookup_ipv4(pntoh32(endp_item->myaddress.data));
+            guint32 ip;
+            memcpy(&ip, endp_item->myaddress.data, 4);
+            mmdb_lookup = maxmind_db_lookup_ipv4(ip);
         } else if (endp_item->myaddress.type == AT_IPv6) {
             mmdb_lookup = maxmind_db_lookup_ipv6((ws_in6_addr *) endp_item->myaddress.data);
         }
