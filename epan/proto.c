f0e98f97e0a077079b6db534a5545a74d5c20f16
Guy Harris <guy@alum.mit.edu>
2018-05-22 20:42:28 -0700

    Creat protocol tvbuff before allocating and freeing memory.
    That way, if the attempt to create the protocol tvbuff throws an
    exception, we won't leak the protocol representation string, as we won't
    even try to allocate it.
    
    Bug: 14719
    Change-Id: Id2855bc97e71aa0682737d1a04486a2a01f5f1e6
    Reviewed-on: https://code.wireshark.org/review/27730
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 0f1f1d0ab827243d3e639ec1d00693e0d2edb883)
    Reviewed-on: https://code.wireshark.org/review/27731

--- epan/proto.c
+++ epan/proto.c
@@ -3508,6 +3508,7 @@ proto_tree_add_protocol_format(proto_tree *tree, int hfindex, tvbuff_t *tvb,
 			       gint start, gint length, const char *format, ...)
 {
 	proto_item	  *pi;
+	tvbuff_t	  *protocol_tvb;
 	va_list		   ap;
 	header_field_info *hfinfo;
 	gchar* protocol_rep;
@@ -3518,11 +3519,16 @@ proto_tree_add_protocol_format(proto_tree *tree, int hfindex, tvbuff_t *tvb,
 
 	DISSECTOR_ASSERT_FIELD_TYPE(hfinfo, FT_PROTOCOL);
 
+	/*
+	 * This can throw an exception, so do it before we allocate anything.
+	 */
+	protocol_tvb = (start == 0 ? tvb : tvb_new_subset_length(tvb, start, length));
+
 	pi = proto_tree_add_pi(tree, hfinfo, tvb, start, &length);
 
 	va_start(ap, format);
 	protocol_rep = g_strdup_vprintf(format, ap);
-	proto_tree_set_protocol_tvb(PNODE_FINFO(pi), (start == 0 ? tvb : tvb_new_subset_length(tvb, start, length)), protocol_rep);
+	proto_tree_set_protocol_tvb(PNODE_FINFO(pi), protocol_tvb, protocol_rep);
 	g_free(protocol_rep);
 	va_end(ap);
 
