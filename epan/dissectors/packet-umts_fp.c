78cb5c7776b0f5d568bc263ed81b390a329f433e
Darien Spencer <cusneud@mail.com>
2018-06-06 23:19:46 +0300

    FP: Show sir target units in tree
    Change-Id: Ica7fd6ab9372c55b41bdc0747f4be812567bd810
    Reviewed-on: https://code.wireshark.org/review/28073
    Petri-Dish: Anders Broman <a.broman58@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-umts_fp.c
+++ epan/dissectors/packet-umts_fp.c
@@ -2352,8 +2352,9 @@ static int
 dissect_dch_outer_loop_power_control(proto_tree *tree, packet_info *pinfo, tvbuff_t *tvb, int offset)
 {
     /* UL SIR target */
-    float target = (float)-8.2 + ((float)0.1 * (float)(int)(tvb_get_guint8(tvb, offset)));
-    proto_tree_add_float(tree, hf_fp_ul_sir_target, tvb, offset, 1, target);
+    guint8 encoded = tvb_get_guint8(tvb, offset);
+    float target = (float)-8.2 + ((float)0.1 * (float)(int)(encoded));
+    proto_tree_add_float_format_value(tree, hf_fp_ul_sir_target, tvb, offset, 1, target, "%.1f dB (%u)", target, encoded);
     offset++;
 
     col_append_fstr(pinfo->cinfo, COL_INFO, " UL SIR Target = %.1f", target);
