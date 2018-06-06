e8493fa7c4a741df245da8b8117e3c430cfe73b3
Jaap Keuter <jaap.keuter@xs4all.nl>
2018-06-06 10:51:23 +0200

    GTP: Don't test for the obvious
    When rel_id is larger then 6 don't test for this again.
    
    Change-Id: I20c6747b31758eadadfd746bdee2cc168c771799
    Signed-off-by: Jaap Keuter <jaap.keuter@xs4all.nl>
    Reviewed-on: https://code.wireshark.org/review/28051
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- epan/dissectors/packet-gtp.c
+++ epan/dissectors/packet-gtp.c
@@ -8299,7 +8299,7 @@ decode_gtp_data_req(tvbuff_t * tvb, int offset, packet_info * pinfo, proto_tree 
             if(format==1) {
                 if(rel_id <= 6){
                     dissect_gprscdr_GPRSCallEventRecord_PDU(next_tvb, pinfo, cdr_dr_tree, NULL);
-                }else if(rel_id >6){
+                }else{
                     dissect_gprscdr_GPRSRecord_PDU(next_tvb, pinfo, cdr_dr_tree, NULL);
                 }
             } else {
