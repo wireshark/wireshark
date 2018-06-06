79da59e31f569edd3bf88c10cbb58a338c4aeb0d
Darien Spencer <cusneud@mail.com>
2018-06-06 21:10:22 +0300

    FP: Only show 1 digit after the dot for UL SIR
    Using %f was printing 6 digits after the dot.
    The values for UL SIR TARGET are defined to the nearest 0.1
    
    Change-Id: I02eb1b8edeaeee2574c4a92a3479490e1428a282
    Reviewed-on: https://code.wireshark.org/review/28067
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- epan/dissectors/packet-umts_fp.c
+++ epan/dissectors/packet-umts_fp.c
@@ -2356,7 +2356,7 @@ dissect_dch_outer_loop_power_control(proto_tree *tree, packet_info *pinfo, tvbuf
     proto_tree_add_float(tree, hf_fp_ul_sir_target, tvb, offset, 1, target);
     offset++;
 
-    col_append_fstr(pinfo->cinfo, COL_INFO, " UL SIR Target = %f", target);
+    col_append_fstr(pinfo->cinfo, COL_INFO, " UL SIR Target = %.1f", target);
 
     return offset;
 }
