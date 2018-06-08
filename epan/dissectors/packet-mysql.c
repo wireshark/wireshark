194d0d2bf2fc295cbb41746633e9d1cfe612594b
Guy Harris <guy@alum.mit.edu>
2018-06-08 14:05:01 -0700

    The string part of a NULL string is zero bytes long, not 4 bytes long.
    There aren't 4 bytes of 'N', 'U', 'L', and 'L' in the packet, so the
    length is *not* 4 bytes.
    
    Change-Id: I81331ef3f307dc65458da37e7d46e299eb7e727a
    Reviewed-on: https://code.wireshark.org/review/28149
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 09a7e8e863ac8a3b80a7a016b9943f815fc92367)
    Reviewed-on: https://code.wireshark.org/review/28152

--- epan/dissectors/packet-mysql.c
+++ epan/dissectors/packet-mysql.c
@@ -1971,7 +1971,7 @@ mysql_field_add_lestring(tvbuff_t *tvb, int offset, proto_tree *tree, int field)
 
 	offset += tvb_get_fle(tvb, offset, &lelen, &is_null);
 	if(is_null)
-		proto_tree_add_string(tree, field, tvb, offset, 4, "NULL");
+		proto_tree_add_string(tree, field, tvb, offset, 0, "NULL");
 	else
 	{
 		proto_tree_add_item(tree, field, tvb, offset, (int)lelen, ENC_NA);
