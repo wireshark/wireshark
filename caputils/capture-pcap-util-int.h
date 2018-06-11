a3ce8d98a2b61a040b773c6b05a9a39d78be032d
Guy Harris <guy@alum.mit.edu>
2018-06-10 18:26:40 -0700

    Fix names in declarations to match definitions.
    Change-Id: I7b3dd147b523fd3251d93dd1917d2e2c47433207
    Reviewed-on: https://code.wireshark.org/review/28195
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit dd87f2018a45de643d05bc426e9353a1d67bb037)
    Reviewed-on: https://code.wireshark.org/review/28196

--- caputils/capture-pcap-util-int.h
+++ caputils/capture-pcap-util-int.h
@@ -35,19 +35,20 @@ extern void request_high_resolution_timestamp(pcap_t *pcap_h);
 extern if_capabilities_t *get_if_capabilities_local(interface_options *interface_opts,
     cap_device_open_err *err, char **err_str);
 extern pcap_t *open_capture_device_local(capture_options *capture_opts,
-    interface_options *interface_opts, int timeout, cap_device_open_err *err,
-    char (*open_err_str)[PCAP_ERRBUF_SIZE]);
+    interface_options *interface_opts, int timeout,
+    cap_device_open_err *open_err, char (*open_err_str)[PCAP_ERRBUF_SIZE]);
 #ifdef HAVE_PCAP_CREATE
 extern if_capabilities_t *get_if_capabilities_pcap_create(interface_options *interface_opts,
     cap_device_open_err *err, char **err_str);
 extern pcap_t *open_capture_device_pcap_create(capture_options *capture_opts,
-    interface_options *interface_opts, int timeout, cap_device_open_err *err,
+    interface_options *interface_opts, int timeout,
+    cap_device_open_err *open_err,
     char (*open_err_str)[PCAP_ERRBUF_SIZE]);
 #endif /* HAVE_PCAP_CREATE */
 extern if_capabilities_t *get_if_capabilities_pcap_open_live(interface_options *interface_opts,
     cap_device_open_err *err, char **err_str);
 extern pcap_t *open_capture_device_pcap_open_live(interface_options *interface_opts,
-    int timeout, cap_device_open_err *err,
+    int timeout, cap_device_open_err *open_err,
     char (*open_err_str)[PCAP_ERRBUF_SIZE]);
 
 /*
