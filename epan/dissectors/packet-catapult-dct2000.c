3b5b7b4da9659fc2b254c8d623fbd2cc7bb4a18e
Martin Mathieson <martin.r.mathieson@googlemail.com>
2018-06-11 13:30:15 +0100

    dct2000: call LTE NAS dissector for r13 release
    Change-Id: I35208b9bea8613468da53ae1541d0c5083cd27c0
    Reviewed-on: https://code.wireshark.org/review/28201
    Petri-Dish: Martin Mathieson <martin.r.mathieson@googlemail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Martin Mathieson <martin.r.mathieson@googlemail.com>

--- epan/dissectors/packet-catapult-dct2000.c
+++ epan/dissectors/packet-catapult-dct2000.c
@@ -2346,7 +2346,8 @@ dissect_catapult_dct2000(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, vo
 
     else if ((strcmp(protocol_name, "nas_rrc_r8_lte") == 0) ||
              (strcmp(protocol_name, "nas_rrc_r9_lte") == 0) ||
-             (strcmp(protocol_name, "nas_rrc_r10_lte") == 0)) {
+             (strcmp(protocol_name, "nas_rrc_r10_lte") == 0) ||
+             (strcmp(protocol_name, "nas_rrc_r13_lte") == 0)) {
         gboolean nas_body_found = TRUE;
         guint8 opcode = tvb_get_guint8(tvb, offset);
         proto_tree_add_item(tree, hf_catapult_dct2000_lte_nas_rrc_opcode,
