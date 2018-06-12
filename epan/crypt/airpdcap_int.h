8d694ba75a55393804407fbc75ff38acaf8e49aa
Guy Harris <guy@alum.mit.edu>
2018-06-12 14:30:50 -0700

    Add some length checks, remove a DISSECTOR_ASSERT().
    Do more checks to make sure we don't run past the end of the data we're
    handed, and don't do a DISSECTOR_ASSERT(), as there may well be packets
    that don't have enough data to pass the assertion - that was causing
    some errors to show up in the 2.6 buildbot when doing 802.11 decryption
    tests.  Those errors should instead be reported as "sorry, we can't do
    decryption" errors by the decryption code.
    
    (XXX - the 802.11 *dissector* should probably be extracting the relevant
    fields and doing the relevant checks, and hand the data to the
    decryption code, so that we don't duplicate 802.11 frame parsing with
    code that might not do as much necessary work as the 802.11 dissector.)
    
    Tweak some comments while we're at it.
    
    Change-Id: I1d230e07cec2fca8c23f265b5875a0bf83f79432
    Reviewed-on: https://code.wireshark.org/review/28240
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 871e96462750b616ad8cc1063c84b844c1a8bfc4)
    Reviewed-on: https://code.wireshark.org/review/28244

--- epan/crypt/airpdcap_int.h
+++ epan/crypt/airpdcap_int.h
@@ -67,7 +67,12 @@
 #define AIRPDCAP_SUBTYPE_ACTION			13
 #define AIRPDCAP_SUBTYPE_ACTION_NO_ACK		14
 
-/* Min length of encrypted data (TKIP=21bytes, CCMP=17bytes)			*/
+/*
+ * Min length of encrypted data (TKIP=21bytes, CCMP=17bytes)
+ * CCMP = 8 octets of CCMP header, 1 octet of data, 8 octets of MIC.
+ * TKIP = 4 octets of IV/Key ID, 4 octets of Extended IV, 1 octet of data,
+ *  8 octets of MIC, 4 octets of ICV
+ */
 #define	AIRPDCAP_CRYPTED_DATA_MINLEN	17
 
 #define AIRPDCAP_TA_OFFSET	10
