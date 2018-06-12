cce6ebdf323917720163b8c7d54e5455ab66a5f7
Guy Harris <guy@alum.mit.edu>
2018-06-12 11:23:32 -0700

    Don't let randpkt write packets libwiretap can't read.
    Wiretap imposes an arbitrary limit on the maximum packet size, to
    prevent it from trying to allocate a huge packet buffer and possibly
    running out of address space on ILP32 platforms or just eating too much
    backing store on LP64/LLP64 platforms.  Don't write packets with a
    length greater than that limit.
    
    Bug: 14107
    Change-Id: Iba4fe3b008b044215647ba3f838ae7b3ac66c585
    Reviewed-on: https://code.wireshark.org/review/28232
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 4e7f4881d2cf6fe69de0661c2441e82dd525e59e)
    Reviewed-on: https://code.wireshark.org/review/28234

--- randpkt_core/randpkt_core.c
+++ randpkt_core/randpkt_core.c
@@ -605,6 +605,13 @@ void randpkt_loop(randpkt_example* example, guint64 produce_count)
 		}
 
 		len_this_pkt = example->sample_length + len_random;
+		if (len_this_pkt > WTAP_MAX_PACKET_SIZE_STANDARD) {
+			/*
+			 * Wiretap will fail when trying to read packets
+			 * bigger than WTAP_MAX_PACKET_SIZE_STANDARD.
+			 */
+			len_this_pkt = WTAP_MAX_PACKET_SIZE_STANDARD;
+		}
 
 		pkthdr->caplen = len_this_pkt;
 		pkthdr->len = len_this_pkt;
