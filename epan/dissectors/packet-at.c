79a3829f4baded1c05419c69355a0941dd484493
Darien Spencer <cusneud@mail.com>
2018-06-16 11:25:12 +0300

    AT: Allow padding in heuristic check
    Bug: 14882
    Change-Id: I0aa26d2ae898d47e0e33794f95e9c464cf5ec6f1
    Reviewed-on: https://code.wireshark.org/review/28296
    Petri-Dish: Anders Broman <a.broman58@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-at.c
+++ epan/dissectors/packet-at.c
@@ -24,15 +24,30 @@ static int hf_at_command = -1;
 /* Subtree handles: set by register_subtree_array */
 static gint ett_at = -1;
 
-static gboolean allowed_chars(tvbuff_t *tvb)
+static gint allowed_chars_len(tvbuff_t *tvb, gint captured_len)
 {
-    gint offset, len;
+    gint offset;
     guint8 val;
 
-    len = tvb_captured_length(tvb);
-    for (offset = 0; offset < len; offset++) {
+    /* Get the amount of characters within the TVB which are ASCII,
+     * cartridge return or new line */
+    for (offset = 0; offset < captured_len; offset++) {
         val = tvb_get_guint8(tvb, offset);
         if (!(g_ascii_isprint(val) || (val == 0x0a) || (val == 0x0d)))
+            return offset;
+    }
+    return captured_len;
+}
+static gboolean is_padded(tvbuff_t *tvb, gint captured_len, gint first_pad_offset)
+{
+    gint offset;
+    guint8 val;
+
+    /* Check if the rest of the packet is 0x00 padding
+     * and no other values*/
+    for (offset = first_pad_offset; offset < captured_len; offset++) {
+        val = tvb_get_guint8(tvb, offset);
+        if (val != 0x00)
             return (FALSE);
     }
     return (TRUE);
@@ -61,19 +76,35 @@ static int dissect_at(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void*
 }
 
 
+#define MIN_PADDED_ALLOWED_CHARS 4
 /* Experimental approach based upon the one used for PPP */
 static gboolean heur_dissect_at(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 {
     const gchar at_magic1[2] = {0x0d, 0x0a};
     const gchar at_magic2[3] = {0x0d, 0x0d, 0x0a};
     const gchar at_magic3[2] = {'A', 'T'};
+    gint len, allwd_chars_len;
+    tvbuff_t *tvb_no_padding;
 
-    if (((tvb_memeql(tvb, 0, at_magic1, sizeof(at_magic1)) == 0) ||
-         (tvb_memeql(tvb, 0, at_magic2, sizeof(at_magic2)) == 0) ||
-         (tvb_memeql(tvb, 0, at_magic3, sizeof(at_magic3)) == 0)) &&
-         allowed_chars(tvb)) {
-        dissect_at(tvb, pinfo, tree, data);
-        return (TRUE);
+    if ((tvb_memeql(tvb, 0, at_magic1, sizeof(at_magic1)) == 0) ||
+        (tvb_memeql(tvb, 0, at_magic2, sizeof(at_magic2)) == 0) ||
+        (tvb_memeql(tvb, 0, at_magic3, sizeof(at_magic3)) == 0)){
+        len = tvb_captured_length(tvb);
+        allwd_chars_len = allowed_chars_len(tvb,len);
+        if(allwd_chars_len < len && allwd_chars_len > MIN_PADDED_ALLOWED_CHARS) {
+            /* Found some valid characters, check if rest is padding */
+            if(is_padded(tvb,len,allwd_chars_len)) {
+                /* This is a padded AT Command */
+                tvb_no_padding = tvb_new_subset_length_caplen(tvb, 0, allwd_chars_len, allwd_chars_len);
+                dissect_at(tvb_no_padding, pinfo, tree, data);
+                return (TRUE);
+            }
+        }
+        else if(allwd_chars_len == len) {
+            /* This is an (unpadded) AT Command */
+            dissect_at(tvb, pinfo, tree, data);
+            return (TRUE);
+        }
     }
     return (FALSE);
 }
