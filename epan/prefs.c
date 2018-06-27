wireshark/wireshark

commit 1a046d693b9dc8c805c2ece35277c989e00c97ac

From Edwin Groothuis via bug 6207: Added Filter Toolbar Save functionality.
From me:
Removed unused code.

svn path=/trunk/; revision=38937

epan/prefs.c
@@ -53,6 +53,8 @@
 #include <epan/prefs-int.h>
 #include <epan/uat-int.h>
 
+#include "epan/filter_expressions.h"
+
 /* Internal functions */
 static module_t *find_subtree(module_t *parent, const char *tilte);
 static module_t *prefs_register_module_or_subtree(module_t *parent,
@@ -1322,6 +1324,7 @@ init_prefs(void) {
   prefs.rtp_player_max_visible = RTP_PLAYER_DEFAULT_VISIBLE;
 
   prefs.display_hidden_proto_items = FALSE;
+  filter_expression_init(TRUE);
 
   prefs_initialized = TRUE;
 }
@@ -1847,6 +1850,9 @@ prefs_capture_device_monitor_mode(const char *name)
 #define PRS_GUI_LAYOUT_CONTENT_2         "gui.layout_content_2"
 #define PRS_GUI_LAYOUT_CONTENT_3         "gui.layout_content_3"
 #define PRS_CONSOLE_LOG_LEVEL		 "console.log.level"
+#define PRS_GUI_FILTER_LABEL             "gui.filter_expressions.label"
+#define PRS_GUI_FILTER_EXPR              "gui.filter_expressions.expr"
+#define PRS_GUI_FILTER_ENABLED           "gui.filter_expressions.enabled"
 
 /*
  * This applies to more than just captures, so it's not "capture.name_resolve";
@@ -2004,6 +2010,9 @@ set_pref(gchar *pref_name, gchar *value, void *private_data _U_,
   gint     enum_val;
   char     *p;
   gchar    *dotp, *last_dotp;
+  static gchar *filter_label = NULL;
+  static gboolean filter_enabled = FALSE;
+  gchar    *filter_expr = NULL;
   module_t *module;
   pref_t   *pref;
   gboolean had_a_dot;
@@ -2035,6 +2044,15 @@ set_pref(gchar *pref_name, gchar *value, void *private_data _U_,
     prefs.pr_cmd = g_strdup(value);
   } else if (strcmp(pref_name, PRS_COL_HIDDEN) == 0) {
     cols_hidden_list = g_strdup (value);
+  } else if (strcmp(pref_name, PRS_GUI_FILTER_LABEL) == 0) {
+    filter_label = g_strdup(value);
+  } else if (strcmp(pref_name, PRS_GUI_FILTER_ENABLED) == 0) {
+    filter_enabled = (strcmp(value, "TRUE") == 0) ? TRUE : FALSE;
+  } else if (strcmp(pref_name, PRS_GUI_FILTER_EXPR) == 0) {
+    filter_expr = g_strdup(value);
+    filter_expression_new(filter_label, filter_expr, filter_enabled);
+    g_free(filter_label);
+    g_free(filter_expr);
   } else if (strcmp(pref_name, PRS_COL_FMT) == 0) {
     col_l = prefs_get_string_list(value);
     if (col_l == NULL)
@@ -3380,6 +3398,22 @@ write_prefs(char **pf_path_return)
   fprintf(pf, "# An integer value greater than 0.\n");
   fprintf(pf, PRS_RTP_PLAYER_MAX_VISIBLE ": %d\n",
 	  prefs.rtp_player_max_visible);
+
+  fprintf(pf, "\n####### Filter Expressions ########\n");
+  {
+    struct filter_expression *fe;
+
+    fe = *(struct filter_expression **)prefs.filter_expressions;
+    while (fe != NULL) {
+      if (fe->deleted == FALSE) {
+	fprintf(pf, "%s: %s\n", PRS_GUI_FILTER_LABEL, fe->label);
+	fprintf(pf, "%s: %s\n", PRS_GUI_FILTER_ENABLED,
+	fe->enabled == TRUE ? "TRUE" : "FALSE");
+	fprintf(pf, "%s: %s\n", PRS_GUI_FILTER_EXPR, fe->expression);
+      }
+      fe = fe->next;
+    }
+  }
 
   fprintf(pf, "\n####### Protocols ########\n");
 

AUTHORS
@@ -3294,6 +3294,7 @@ Allison			<aobourn [AT] isilon.com> {
 
 Edwin Groothuis		<wireshark [AT] mavetju.org> {
 	Time Shift functionality
+	Filter Toolbar Save functionality
 }
 
 Andrew Kampjes		<andrew.kampjes [AT] endace.com> {

epan/CMakeLists.txt
@@ -1217,6 +1217,7 @@ set(LIBWIRESHARK_FILES
 	except.c
 	expert.c
 	filesystem.c
+	filter_expressions.c
 	follow.c
 	frame_data.c
 	frequency-utils.c

epan/Makefile.common
@@ -50,6 +50,7 @@ LIBWIRESHARK_SRC = 		\
 	except.c		\
 	expert.c		\
 	filesystem.c		\
+	filter_expressions.c	\
 	follow.c		\
 	frame_data.c		\
 	frequency-utils.c	\
@@ -163,6 +164,7 @@ LIBWIRESHARK_INCLUDES = 	\
 	except.h		\
 	exceptions.h		\
 	expert.h		\
+	filter_expressions.h	\
 	filesystem.h		\
 	follow.h		\
 	frame_data.h		\

epan/filter_expressions.c
@@ -1,0 +1,80 @@
+/* filter_expressions.c
+ * Submitted by Edwin Groothuis <wireshark@mavetju.org>
+ *
+ * $Id$
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <glib.h>
+
+#include <epan/prefs.h>
+
+#include "epan/filter_expressions.h"
+
+static struct filter_expression *_filter_expression_head = NULL;
+struct filter_expression **pfilter_expression_head = &_filter_expression_head;
+
+/*
+ * Create a new filter_expression and add it to the end of the list
+ * of filter_expressions.
+ */
+struct filter_expression *
+filter_expression_new(const gchar *label, const gchar *expr,
+    const gboolean enabled)
+{
+	struct filter_expression *expression;
+	struct filter_expression *prev;
+
+	expression = (struct filter_expression *)g_malloc(sizeof(struct filter_expression));
+	memset(expression, '\0', sizeof(struct filter_expression));
+	expression->button = NULL;
+	expression->label = g_strdup(label);
+	expression->expression = g_strdup(expr);
+	expression->enabled = enabled;
+	expression->deleted = FALSE;
+	expression->index = 0;
+
+	expression->next = NULL;
+
+	/* Add it at the end so the button order is always the same*/
+	if (*pfilter_expression_head == NULL) {
+		_filter_expression_head = expression;
+	} else {
+		prev = *pfilter_expression_head;
+		while (prev->next != NULL)
+			prev = prev->next;
+		prev->next = expression;
+		expression->index = prev->index + 1;
+	}
+
+	return(expression);
+}
+
+void
+filter_expression_init(gboolean enable_prefs)
+{
+	if (enable_prefs)
+		prefs.filter_expressions = pfilter_expression_head;
+}

epan/filter_expressions.h
@@ -1,6 +1,5 @@
-/* main_filter_toolbar.h
- * Definitions for filter toolbar routines
- * Copyright 2003, Ulf Lamping <ulf.lamping@web.de>
+/* filter_expressions.h
+ * Submitted by Edwin Groothuis <wireshark@mavetju.org>
  *
  * $Id$
  *
@@ -23,13 +22,28 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#ifndef __MAIN_FILTER_TOOLBAR_H__
-#define __MAIN_FILTER_TOOLBAR_H__
+#ifndef __FILTER_EXPRESSIONS_H__
+#define __FILTER_EXPRESSIONS_H__
 
-#define E_DFILTER_APPLY_KEY       "display_filter_apply"
-#define E_DFILTER_CLEAR_KEY       "display_filter_clear"
-#define E_DFILTER_SAVE_KEY        "display_filter_save"
+#include "globals.h"
 
-extern GtkWidget *filter_toolbar_new(void);
+struct filter_expression {
+	gpointer button;	/* Filter toolbar */
+	gchar	*label;
+	gchar	*expression;
 
-#endif /* __MAIN_FILTER_TOOLBAR_H__ */
+	gint	index;
+	gboolean enabled;	/* Can be set to FALSE by Preferences Dialog */
+	gboolean deleted;	/* Can be set to TRUE by Preferences Dialog */
+
+	struct filter_expression *next;
+};
+
+WS_VAR_IMPORT struct filter_expression **pfilter_expression_head;
+
+struct filter_expression *filter_expression_new(const gchar *label,
+    const gchar *expr, const gboolean enabled);
+
+void filter_expression_init(gboolean prefs);
+
+#endif /* __FILTER_EXPRESSIONS_H__ */

epan/libwireshark.def
@@ -443,6 +443,8 @@ file_open_error_message
 file_write_error_message
 files_identical
 filesystem_opt
+pfilter_expression_head		DATA
+filter_expression_new
 find_circuit
 find_codec
 find_conversation

epan/prefs.h
@@ -167,6 +167,7 @@ typedef struct _e_prefs {
   guint    rtp_player_max_visible;
   guint    tap_update_interval;
   gboolean display_hidden_proto_items;
+  gpointer filter_expressions;	/* Actually points to &head */
 } e_prefs;
 
 WS_VAR_IMPORT e_prefs prefs;

commit d3e3c00fbbe205f16fc279068f1cb989259c3b04

prefs: fix crash when importing old filter expression preference
When the filter label was missing, it would result in a crash
(use-after-free) while reading the next expression. For example:

    gui.filter_expressions.label: Not-Junk
    gui.filter_expressions.expr: tcp.flags.reset==1
    # note: missing label preference
    gui.filter_expressions.expr: dns

While at it, do not duplicate the filter expression,
"filter_expression_new" has always been copying it.

Change-Id: I980fd720c9a04b679a71dd2e7e8bf5e53c72ac43
Fixes: 1a046d693b ("Added Filter Toolbar Save functionality.")
Bug: 11648
Reviewed-on: https://code.wireshark.org/review/28471
Petri-Dish: Peter Wu <peter@lekensteyn.nl>
Tested-by: Petri Dish Buildbot
Reviewed-by: Anders Broman <a.broman58@gmail.com>

epan/prefs.c
@@ -5362,7 +5362,6 @@ set_pref(gchar *pref_name, const gchar *value, void *private_data _U_,
     gchar    *dotp, *last_dotp;
     static gchar *filter_label = NULL;
     static gboolean filter_enabled = FALSE;
-    gchar    *filter_expr = NULL;
     module_t *module, *containing_module;
     pref_t   *pref;
     int type;
@@ -5370,15 +5369,18 @@ set_pref(gchar *pref_name, const gchar *value, void *private_data _U_,
     //The PRS_GUI field names are here for backwards compatibility
     //display filters have been converted to a UAT.
     if (strcmp(pref_name, PRS_GUI_FILTER_LABEL) == 0) {
+        /* Assume that PRS_GUI_FILTER_EXPR follows this preference. In case of
+         * malicious preference files, free the previous value to limit the size
+         * of leaked memory.  */
+        g_free(filter_label);
         filter_label = g_strdup(value);
     } else if (strcmp(pref_name, PRS_GUI_FILTER_ENABLED) == 0) {
         filter_enabled = (strcmp(value, "TRUE") == 0) ? TRUE : FALSE;
     } else if (strcmp(pref_name, PRS_GUI_FILTER_EXPR) == 0) {
-        filter_expr = g_strdup(value);
         /* Comments not supported for "old" preference style */
-        filter_expression_new(filter_label, filter_expr, "", filter_enabled);
+        filter_expression_new(filter_label, value, "", filter_enabled);
         g_free(filter_label);
-        g_free(filter_expr);
+        filter_label = NULL;
     } else if (strcmp(pref_name, "gui.version_in_start_page") == 0) {
         /* Convert deprecated value to closest current equivalent */
         if (g_ascii_strcasecmp(value, "true") == 0) {