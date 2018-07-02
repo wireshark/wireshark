d8974921973ad1c3369c616ac545aa19f464d494
Julien Staub <atsju2@yahoo.fr>
2018-07-02 17:38:51 +0200

    WSUTIL/PINT: transform macros into static functions
    For better type safety
    
    Change-Id: Ida7b98af8c44a52ddac2c4ab0702db2519a0c4af
    Reviewed-on: https://code.wireshark.org/review/28570
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- wsutil/pint.h
+++ wsutil/pint.h
@@ -19,110 +19,148 @@
  * The pletohXX versions return the little-endian representation.
  */
 
-#define pntoh16(p)  ((guint16)                       \
-                     ((guint16)*((const guint8 *)(p)+0)<<8|  \
-                      (guint16)*((const guint8 *)(p)+1)<<0))
+static inline guint16 pntoh16(const void *p)
+{
+    return (guint16)*((const guint8 *)(p)+0)<<8|
+           (guint16)*((const guint8 *)(p)+1)<<0;
+}
 
-#define pntoh24(p)  ((guint32)*((const guint8 *)(p)+0)<<16|  \
-                     (guint32)*((const guint8 *)(p)+1)<<8|   \
-                     (guint32)*((const guint8 *)(p)+2)<<0)
+static inline guint32 pntoh24(const void *p)
+{
+    return (guint32)*((const guint8 *)(p)+0)<<16|
+           (guint32)*((const guint8 *)(p)+1)<<8|
+           (guint32)*((const guint8 *)(p)+2)<<0;
+}
 
-#define pntoh32(p)  ((guint32)*((const guint8 *)(p)+0)<<24|  \
-                     (guint32)*((const guint8 *)(p)+1)<<16|  \
-                     (guint32)*((const guint8 *)(p)+2)<<8|   \
-                     (guint32)*((const guint8 *)(p)+3)<<0)
+static inline guint32 pntoh32(const void *p)
+{
+    return (guint32)*((const guint8 *)(p)+0)<<24|
+           (guint32)*((const guint8 *)(p)+1)<<16|
+           (guint32)*((const guint8 *)(p)+2)<<8|
+           (guint32)*((const guint8 *)(p)+3)<<0;
+}
 
-#define pntoh40(p)  ((guint64)*((const guint8 *)(p)+0)<<32|  \
-                     (guint64)*((const guint8 *)(p)+1)<<24|  \
-                     (guint64)*((const guint8 *)(p)+2)<<16|  \
-                     (guint64)*((const guint8 *)(p)+3)<<8|   \
-                     (guint64)*((const guint8 *)(p)+4)<<0)
+static inline guint64 pntoh40(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+0)<<32|
+           (guint64)*((const guint8 *)(p)+1)<<24|
+           (guint64)*((const guint8 *)(p)+2)<<16|
+           (guint64)*((const guint8 *)(p)+3)<<8|
+           (guint64)*((const guint8 *)(p)+4)<<0;
+}
 
-#define pntoh48(p)  ((guint64)*((const guint8 *)(p)+0)<<40|  \
-                     (guint64)*((const guint8 *)(p)+1)<<32|  \
-                     (guint64)*((const guint8 *)(p)+2)<<24|  \
-                     (guint64)*((const guint8 *)(p)+3)<<16|  \
-                     (guint64)*((const guint8 *)(p)+4)<<8|   \
-                     (guint64)*((const guint8 *)(p)+5)<<0)
+static inline guint64 pntoh48(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+0)<<40|
+           (guint64)*((const guint8 *)(p)+1)<<32|
+           (guint64)*((const guint8 *)(p)+2)<<24|
+           (guint64)*((const guint8 *)(p)+3)<<16|
+           (guint64)*((const guint8 *)(p)+4)<<8|
+           (guint64)*((const guint8 *)(p)+5)<<0;
+}
 
-#define pntoh56(p)  ((guint64)*((const guint8 *)(p)+0)<<48|  \
-                     (guint64)*((const guint8 *)(p)+1)<<40|  \
-                     (guint64)*((const guint8 *)(p)+2)<<32|  \
-                     (guint64)*((const guint8 *)(p)+3)<<24|  \
-                     (guint64)*((const guint8 *)(p)+4)<<16|  \
-                     (guint64)*((const guint8 *)(p)+5)<<8|   \
-                     (guint64)*((const guint8 *)(p)+6)<<0)
+static inline guint64 pntoh56(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+0)<<48|
+           (guint64)*((const guint8 *)(p)+1)<<40|
+           (guint64)*((const guint8 *)(p)+2)<<32|
+           (guint64)*((const guint8 *)(p)+3)<<24|
+           (guint64)*((const guint8 *)(p)+4)<<16|
+           (guint64)*((const guint8 *)(p)+5)<<8|
+           (guint64)*((const guint8 *)(p)+6)<<0;
+}
 
-#define pntoh64(p)  ((guint64)*((const guint8 *)(p)+0)<<56|  \
-                     (guint64)*((const guint8 *)(p)+1)<<48|  \
-                     (guint64)*((const guint8 *)(p)+2)<<40|  \
-                     (guint64)*((const guint8 *)(p)+3)<<32|  \
-                     (guint64)*((const guint8 *)(p)+4)<<24|  \
-                     (guint64)*((const guint8 *)(p)+5)<<16|  \
-                     (guint64)*((const guint8 *)(p)+6)<<8|   \
-                     (guint64)*((const guint8 *)(p)+7)<<0)
+static inline guint64 pntoh64(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+0)<<56|
+           (guint64)*((const guint8 *)(p)+1)<<48|
+           (guint64)*((const guint8 *)(p)+2)<<40|
+           (guint64)*((const guint8 *)(p)+3)<<32|
+           (guint64)*((const guint8 *)(p)+4)<<24|
+           (guint64)*((const guint8 *)(p)+5)<<16|
+           (guint64)*((const guint8 *)(p)+6)<<8|
+           (guint64)*((const guint8 *)(p)+7)<<0;
+}
 
+static inline guint16 pletoh16(const void *p)
+{
+    return (guint16)*((const guint8 *)(p)+1)<<8|
+           (guint16)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh16(p) ((guint16)                       \
-                     ((guint16)*((const guint8 *)(p)+1)<<8|  \
-                      (guint16)*((const guint8 *)(p)+0)<<0))
+static inline guint32 pletoh24(const void *p)
+{
+    return (guint32)*((const guint8 *)(p)+2)<<16|
+           (guint32)*((const guint8 *)(p)+1)<<8|
+           (guint32)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh24(p) ((guint32)*((const guint8 *)(p)+2)<<16|  \
-                     (guint32)*((const guint8 *)(p)+1)<<8|   \
-                     (guint32)*((const guint8 *)(p)+0)<<0)
+static inline guint32 pletoh32(const void *p)
+{
+    return (guint32)*((const guint8 *)(p)+3)<<24|
+           (guint32)*((const guint8 *)(p)+2)<<16|
+           (guint32)*((const guint8 *)(p)+1)<<8|
+           (guint32)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh32(p) ((guint32)*((const guint8 *)(p)+3)<<24|  \
-                     (guint32)*((const guint8 *)(p)+2)<<16|  \
-                     (guint32)*((const guint8 *)(p)+1)<<8|   \
-                     (guint32)*((const guint8 *)(p)+0)<<0)
+static inline guint64 pletoh40(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+4)<<32|
+           (guint64)*((const guint8 *)(p)+3)<<24|
+           (guint64)*((const guint8 *)(p)+2)<<16|
+           (guint64)*((const guint8 *)(p)+1)<<8|
+           (guint64)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh40(p) ((guint64)*((const guint8 *)(p)+4)<<32|  \
-                     (guint64)*((const guint8 *)(p)+3)<<24|  \
-                     (guint64)*((const guint8 *)(p)+2)<<16|  \
-                     (guint64)*((const guint8 *)(p)+1)<<8|   \
-                     (guint64)*((const guint8 *)(p)+0)<<0)
+static inline guint64 pletoh48(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+5)<<40|
+           (guint64)*((const guint8 *)(p)+4)<<32|
+           (guint64)*((const guint8 *)(p)+3)<<24|
+           (guint64)*((const guint8 *)(p)+2)<<16|
+           (guint64)*((const guint8 *)(p)+1)<<8|
+           (guint64)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh48(p) ((guint64)*((const guint8 *)(p)+5)<<40|  \
-                     (guint64)*((const guint8 *)(p)+4)<<32|  \
-                     (guint64)*((const guint8 *)(p)+3)<<24|  \
-                     (guint64)*((const guint8 *)(p)+2)<<16|  \
-                     (guint64)*((const guint8 *)(p)+1)<<8|   \
-                     (guint64)*((const guint8 *)(p)+0)<<0)
+static inline guint64 pletoh56(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+6)<<48|
+           (guint64)*((const guint8 *)(p)+5)<<40|
+           (guint64)*((const guint8 *)(p)+4)<<32|
+           (guint64)*((const guint8 *)(p)+3)<<24|
+           (guint64)*((const guint8 *)(p)+2)<<16|
+           (guint64)*((const guint8 *)(p)+1)<<8|
+           (guint64)*((const guint8 *)(p)+0)<<0;
+}
 
-#define pletoh56(p) ((guint64)*((const guint8 *)(p)+6)<<48|  \
-                     (guint64)*((const guint8 *)(p)+5)<<40|  \
-                     (guint64)*((const guint8 *)(p)+4)<<32|  \
-                     (guint64)*((const guint8 *)(p)+3)<<24|  \
-                     (guint64)*((const guint8 *)(p)+2)<<16|  \
-                     (guint64)*((const guint8 *)(p)+1)<<8|   \
-                     (guint64)*((const guint8 *)(p)+0)<<0)
-
-#define pletoh64(p) ((guint64)*((const guint8 *)(p)+7)<<56|  \
-                     (guint64)*((const guint8 *)(p)+6)<<48|  \
-                     (guint64)*((const guint8 *)(p)+5)<<40|  \
-                     (guint64)*((const guint8 *)(p)+4)<<32|  \
-                     (guint64)*((const guint8 *)(p)+3)<<24|  \
-                     (guint64)*((const guint8 *)(p)+2)<<16|  \
-                     (guint64)*((const guint8 *)(p)+1)<<8|   \
-                     (guint64)*((const guint8 *)(p)+0)<<0)
-
+static inline guint64 pletoh64(const void *p)
+{
+    return (guint64)*((const guint8 *)(p)+7)<<56|
+           (guint64)*((const guint8 *)(p)+6)<<48|
+           (guint64)*((const guint8 *)(p)+5)<<40|
+           (guint64)*((const guint8 *)(p)+4)<<32|
+           (guint64)*((const guint8 *)(p)+3)<<24|
+           (guint64)*((const guint8 *)(p)+2)<<16|
+           (guint64)*((const guint8 *)(p)+1)<<8|
+           (guint64)*((const guint8 *)(p)+0)<<0;
+}
 /* Pointer routines to put items out in a particular byte order.
  * These will work regardless of the byte alignment of the pointer.
  */
 
-#define phton16(p, v) \
-                    {                                       \
-                    ((guint8*)(p))[0] = (guint8)((v) >> 8); \
-                    ((guint8*)(p))[1] = (guint8)((v) >> 0); \
-                    }
+static inline void phton16(guint8 *p, guint16 v)
+{
+    p[0] = (guint8)(v >> 8);
+    p[1] = (guint8)(v >> 0);
+}
 
-#define phton32(p, v) \
-                    {                                         \
-                    ((guint8*)(p))[0] = (guint8)((v) >> 24);  \
-                    ((guint8*)(p))[1] = (guint8)((v) >> 16);  \
-                    ((guint8*)(p))[2] = (guint8)((v) >> 8);   \
-                    ((guint8*)(p))[3] = (guint8)((v) >> 0);   \
-                    }
+static inline void phton32(guint8 *p, guint32 v)
+{
+    p[0] = (guint8)(v >> 24);
+    p[1] = (guint8)(v >> 16);
+    p[2] = (guint8)(v >> 8);
+    p[3] = (guint8)(v >> 0);
+}
 
 static inline void phton64(guint8 *p, guint64 v) {
     p[0] = (guint8)(v >> 56);
