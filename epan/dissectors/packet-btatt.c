954fe24e418723df7bac6be146528947ca11d060
Guy Harris <guy@alum.mit.edu>
2018-07-22 12:19:33 -0700

    If the dissector isn't registered, you shouldn't be calling it.
    Report a dissector bug, rather than calling the data dissector.
    
    Change-Id: I7bde1001a48d2443acf2dc7caa83434e0972aab7
    Reviewed-on: https://code.wireshark.org/review/28814
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- epan/dissectors/packet-btatt.c
+++ epan/dissectors/packet-btatt.c
@@ -4285,7 +4285,7 @@ btatt_call_dissector_by_dissector_name_with_data(const char *dissector_name,
 	if (handle != NULL)
 		return call_dissector_with_data(handle, tvb, pinfo, tree, data);
 	else
-		return call_data_dissector(tvb, pinfo, tree);
+		REPORT_DISSECTOR_BUG("Dissector %s not registered", dissector_name);
 }
 
 static gint
