2e7cae3fd46fd506006d94ee8b79ca4f883b50c7
Gerald Combs <gerald@wireshark.org>
2018-08-21 11:27:46 -0700

    WSUG: Fixup the "Internals" section.
    The Internals menu items were moved under the View menu a while back.
    Fix an internals dialog title.
    
    Change-Id: I78d61bf4f52bf9eb86cf7ff59fc036b9f7e846f5
    Reviewed-on: https://code.wireshark.org/review/29228
    Petri-Dish: Gerald Combs <gerald@wireshark.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- ui/qt/conversation_hash_tables_dialog.cpp
+++ ui/qt/conversation_hash_tables_dialog.cpp
@@ -27,7 +27,7 @@ ConversationHashTablesDialog::ConversationHashTablesDialog(QWidget *parent) :
     ui->setupUi(this);
     if (parent) loadGeometry(parent->width() * 3 / 4, parent->height() * 3 / 4);
     setAttribute(Qt::WA_DeleteOnClose, true);
-    setWindowTitle(wsApp->windowTitleString(tr("Dissector Tables")));
+    setWindowTitle(wsApp->windowTitleString(tr("Conversation Hash Tables")));
 
     QString html;
 
