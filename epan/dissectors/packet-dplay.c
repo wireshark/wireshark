wireshark/wireshark

commit 9c3be9d5aef912c8cad33cf414dff1b2c8ef9d8f

Try to optimize heuristics slightly.
svn path=/trunk/; revision=52624

epan/dissectors/packet-dplay.c
@@ -1153,17 +1153,18 @@ static void dissect_dplay_player_msg(tvbuff_t *tvb, packet_info *pinfo, proto_tr
 }
 static gboolean heur_dissect_dplay(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
-    guint8 signature[] = {'p','l','a','y'};
     guint32 dplay_id, token;
 
     if(tvb_length(tvb) < 25)
         return FALSE;
 
+    /* The string play = 0x706c6179 */
     dplay_id = tvb_get_letohl(tvb, 20);
-    if( memcmp(signature, (guint8 *)&dplay_id, 4) == 0) {
+    if( dplay_id == 0x706c6179) {
         dissect_dplay(tvb, pinfo, tree);
         return TRUE;
     }
+
 
     /* There is a player to player message that does not contain "play" */
     token = tvb_get_letohl(tvb, 0);

epan/dissectors/packet-clique-rm.c
@@ -276,11 +276,14 @@ dissect_clique_rm(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *dat
   guint8      version;
   guint8      type;
   int         offset = 0;
+  guint64     qword;
 
   if (tvb_length(tvb) < 12)
     return FALSE;
 
-  if (tvb_strneql(tvb, offset, "Clique", 6))
+  qword = tvb_get_ntoh48(tvb,0);
+  /* ASCII str for 'Clique' = 0x436c69717565 */
+  if(qword != G_GINT64_CONSTANT (0x436c69717565U))
     return FALSE;
   offset += 6;
 

commit 942bb3ea7c4342978ad3cbfe73fb9ca0c460d587

DPLAY: fix heuristic check
Regression introduced in 9c3be9d5

Bug: 15092
Change-Id: If52b1dfe05c8093d93ef6ed52cd38f3b2263e9e3
Reviewed-on: https://code.wireshark.org/review/29351
Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>
Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
Tested-by: Petri Dish Buildbot
Reviewed-by: Anders Broman <a.broman58@gmail.com>

epan/dissectors/packet-dplay.c
@@ -1115,7 +1115,7 @@ static gboolean heur_dissect_dplay(tvbuff_t *tvb, packet_info *pinfo, proto_tree
         return FALSE;
 
     /* The string play = 0x706c6179 */
-    dplay_id = tvb_get_letohl(tvb, 20);
+    dplay_id = tvb_get_ntohl(tvb, 20);
     if( dplay_id == 0x706c6179) {
         dissect_dplay(tvb, pinfo, tree);
         return TRUE;