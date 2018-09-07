4f49cb4010ab6c8133d932e70aa41d4b9a425621
Martin Boye Petersen <martinboyepetersen@gmail.com>
2018-09-07 09:29:39 +0200

    Qt: Add null check for finfo and hfinfo
    Adding the null check fixes a bug that made wireshark crash
    when right clicking a subtree which doesn't have any preferences
    and if none of the parents had one either.
    
    The problem was introduced in commit 589413d8772f1450be9bd4a2550cb99a95f71299
    
    Change-Id: Ia5bbae0a58298f3e9d912e44f33589da1cbfacc9
    Reviewed-on: https://code.wireshark.org/review/29455
    Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>
    (cherry picked from commit 3aa7e61b17a39be4ec0674f9933a07172df0e712)
    Reviewed-on: https://code.wireshark.org/review/29461

--- ui/qt/proto_tree.cpp
+++ ui/qt/proto_tree.cpp
@@ -182,8 +182,9 @@ void ProtoTree::contextMenuEvent(QContextMenuEvent *event)
     // The "text only" header field will not give preferences for the selected protocol.
     // Use parent in this case.
     proto_node *node = proto_tree_model_->protoNodeFromIndex(index).protoNode();
-    while (node && node->finfo->hfinfo->id == hf_text_only)
+    while (node && node->finfo && node->finfo->hfinfo && node->finfo->hfinfo->id == hf_text_only)
         node = node->parent;
+
     FieldInformation pref_finfo(node);
     proto_prefs_menu_.setModule(pref_finfo.moduleName());
 
