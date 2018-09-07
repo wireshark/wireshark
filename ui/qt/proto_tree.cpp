wireshark/wireshark

commit 589413d8772f1450be9bd4a2550cb99a95f71299

Qt: Improve fetching preferences from a tree item.
The special-case "text only" header field will not give preferences for the
selected protocol, so we have to traverse the parents to get a usable node.

Change-Id: I647f275839db337dedaeb67664402af1fe9f21c1
Reviewed-on: https://code.wireshark.org/review/29363
Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/proto_tree.cpp
@@ -181,7 +181,13 @@ void ProtoTree::contextMenuEvent(QContextMenuEvent *event)
     ctx_menu_.addAction(window()->findChild<QAction *>("actionGoGoToLinkedPacket"));
     ctx_menu_.addAction(window()->findChild<QAction *>("actionContextShowLinkedPacketInNewWindow"));
 
-    proto_prefs_menu_.setModule(finfo.moduleName());
+    // The "text only" header field will not give preferences for the selected protocol.
+    // Use parent in this case.
+    proto_node *node = proto_tree_model_->protoNodeFromIndex(index).protoNode();
+    while (node && node->finfo->hfinfo->id == hf_text_only)
+        node = node->parent;
+    FieldInformation pref_finfo(node);
+    proto_prefs_menu_.setModule(pref_finfo.moduleName());
 
     decode_as_->setData(QVariant::fromValue(true));
 

commit 7b37c5c180eff710c49ee324869c9ad4dc5c7262

Qt: Add null check for finfo and hfinfo
Adding the null check fixes a bug that made wireshark crash
when right clicking a subtree which doesn't have any preferences
and if none of the parents had one either.

The problem was introduced in commit 589413d8772f1450be9bd4a2550cb99a95f71299

Change-Id: Ia5bbae0a58298f3e9d912e44f33589da1cbfacc9
Reviewed-on: https://code.wireshark.org/review/29455
Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>

ui/qt/proto_tree.cpp
@@ -184,8 +184,9 @@ void ProtoTree::contextMenuEvent(QContextMenuEvent *event)
     // The "text only" header field will not give preferences for the selected protocol.
     // Use parent in this case.
     proto_node *node = proto_tree_model_->protoNodeFromIndex(index).protoNode();
-    while (node && node->finfo->hfinfo->id == hf_text_only)
+    while (node && node->finfo && node->finfo->hfinfo && node->finfo->hfinfo->id == hf_text_only)
         node = node->parent;
+
     FieldInformation pref_finfo(node);
     proto_prefs_menu_.setModule(pref_finfo.moduleName());
 

ui/qt/utils/field_information.cpp
@@ -15,7 +15,7 @@ FieldInformation::FieldInformation(field_info *fi, QObject * parent)
 :QObject(parent)
 {
     fi_ = fi;
-    parent_fi_ = 0;
+    parent_fi_ = NULL;
 }
 
 FieldInformation::FieldInformation(proto_node *node, QObject * parent)
@@ -34,7 +34,7 @@ bool FieldInformation::isValid() const
 
     if ( fi_ && fi_->hfinfo )
     {
-        if (fi_->hfinfo->blurb != 0 && fi_->hfinfo->blurb[0] != '\0') {
+        if (fi_->hfinfo->blurb != NULL && fi_->hfinfo->blurb[0] != '\0') {
             ret = true;
         } else {
             ret = QString((fi_->hfinfo->name)).length() > 0;