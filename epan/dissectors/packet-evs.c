ca64218e9314ea75b7182228a0a314775ee5e6b0
Alexis La Goutte <alexis.lagoutte@gmail.com>
2018-09-10 13:13:39 +0000

    evs: add modelines info
    Change-Id: I48f2c730b1eebb26fee53dcf22d20e3a9b9f536b
    Reviewed-on: https://code.wireshark.org/review/29557
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-evs.c
+++ epan/dissectors/packet-evs.c
@@ -661,3 +661,17 @@ proto_reg_handoff_evs(void)
         dissector_add_uint("rtp.pt", dynamic_payload_type, evs_handle);
     }
 }
+
+/*
+ * Editor modelines  -  http://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=8 expandtab:
+ * :indentSize=4:tabSize=8:noTabs=true:
+ */
+
