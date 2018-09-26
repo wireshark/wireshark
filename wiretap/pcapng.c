4321a94fc3aa9ab24b9147caecf78fa23b30f053
Stig Bjørlykke <stig@bjorlykke.org>
2018-09-26 20:04:35 +0200

    pcapng: Free option_content on error
    Change-Id: I5a406ecf4e495d0224473d01af4144479ceaf9c3
    Reviewed-on: https://code.wireshark.org/review/29863
    Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>
    (cherry picked from commit 65a422e235bd88859a27626f120a3d3ec478e29d)
    Reviewed-on: https://code.wireshark.org/review/29873
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- wiretap/pcapng.c
+++ wiretap/pcapng.c
@@ -1904,6 +1904,7 @@ pcapng_read_interface_statistics_block(FILE_T fh, pcapng_block_header_t *bh, pca
         bytes_read = pcapng_read_option(fh, pn, &oh, option_content, opt_cont_buf_len, to_read, err, err_info, "interface_statistics");
         if (bytes_read <= 0) {
             pcapng_debug("pcapng_read_interface_statistics_block: failed to read option");
+            g_free(option_content);
             return FALSE;
         }
         to_read -= bytes_read;
