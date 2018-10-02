d49e0576dc4bd1fcf8936182fee665cf98ef0214
Guy Harris <guy@alum.mit.edu>
2018-10-02 13:33:02 -0700

    We should only link executables with setargv.obj.
    Libraries shouldn't be linked with it.
    
    See if this fixes the weird problems I'm having with mergecap -
    including, apparently, the mergecap from the buildbots - when run with
    wildcard arguments, terminating before it gets to main() (making it hard
    to try to debug bug 15151).
    
    Change-Id: Ie793b0ea8157186a121106636ac8b782457c09f5
    Reviewed-on: https://code.wireshark.org/review/29985
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 97972ea9ff4ba1dc485070197486bb5fffedf774)
    Reviewed-on: https://code.wireshark.org/review/30057

--- cmake/modules/UseWinLibs.cmake
+++ cmake/modules/UseWinLibs.cmake
@@ -13,7 +13,4 @@ if( WIN32 )
 	set( WIN_COMCTL32_LIBRARY comctl32.lib )
 	set( WIN_VERSION_LIBRARY  version.lib )
 
-	# Linking with setargv.obj enables "wildcard expansion" of command-line arguments
-	set( WS_LINK_FLAGS "${WS_LINK_FLAGS} setargv.obj" )
-
 endif()
