2d8e3939616afbcf1a8adc300b2e3e334a0bd768
Martin Peylo <wireshark@izac.de>
2018-10-03 09:53:43 +0300

    New PKCS#10 CertificationRequest dissector
    Specified in RFC2986
    PEM header from RFC 7468
    MIME type and .p10 file extension from RFC 5967
    .csr file extension from common practice
    
    Change-Id: I7dfe0a19b70eaf3352af2463759a342277d53db6
    Reviewed-on: https://code.wireshark.org/review/29994
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

---
+++ epan/dissectors/packet-pkcs10.h
@@ -0,0 +1,46 @@
+/* Do not modify this file. Changes will be overwritten.                      */
+/* Generated automatically by the ASN.1 to Wireshark dissector compiler       */
+/* packet-pkcs10.h                                                            */
+/* asn2wrs.py -b -p pkcs10 -c ./pkcs10.cnf -s ./packet-pkcs10-template -D . -O ../.. PKCS10.asn */
+
+/* Input file: packet-pkcs10-template.h */
+
+#line 1 "./asn1/pkcs10/packet-pkcs10-template.h"
+/* packet-pkcs10.h
+ *
+ * Routines for PKCS10 dissection
+ *   Martin Peylo 2017
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef PACKET_PKCS10_H
+#define PACKET_PKCS10_H
+
+void proto_reg_handoff_pkcs10(void);
+
+
+/*--- Included file: packet-pkcs10-exp.h ---*/
+#line 1 "./asn1/pkcs10/packet-pkcs10-exp.h"
+int dissect_pkcs10_CertificationRequest(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_);
+
+/*--- End of included file: packet-pkcs10-exp.h ---*/
+#line 31 "./asn1/pkcs10/packet-pkcs10-template.h"
+
+#endif  /* PACKET_PKCS10_H */
