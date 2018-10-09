7571da435885021a07b7927f545a5eeb06dd3cfc
Peter Wu <peter@lekensteyn.nl>
2018-10-09 17:14:11 +0200

    ftype-protocol: do not return from TRY/CATCH
    TRY/CATCH are macros, before returning the ENDTRY block must be executed
    or the weirdest crashes can occur.
    
    Change-Id: Ic56871322f8567263e2b8a81cce5a3c7042301b7
    Fixes: v2.1.0rc0-2939-g5493fe0167 ("Convert ftype-tvbuff.c to ftype-protocol.c")
    Reviewed-on: https://code.wireshark.org/review/30095
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit 8c31cdc13c546107235b86cfa9a539c436448f2e)
    Reviewed-on: https://code.wireshark.org/review/30109
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- epan/ftypes/ftype-protocol.c
+++ epan/ftypes/ftype-protocol.c
@@ -258,7 +258,7 @@ cmp_gt(const fvalue_t *fv_a, const fvalue_t *fv_b)
 				gt = (memcmp(tvb_get_ptr(a->tvb, 0, a_len), tvb_get_ptr(b->tvb, 0, a_len), a_len) > 0);
 			}
 		} else {
-			return (strcmp(a->proto_string, b->proto_string) > 0);
+			gt = (strcmp(a->proto_string, b->proto_string) > 0);
 		}
 	}
 	CATCH_ALL {
@@ -287,7 +287,7 @@ cmp_ge(const fvalue_t *fv_a, const fvalue_t *fv_b)
 				ge = (memcmp(tvb_get_ptr(a->tvb, 0, a_len), tvb_get_ptr(b->tvb, 0, a_len), a_len) >= 0);
 			}
 		} else {
-			return (strcmp(a->proto_string, b->proto_string) >= 0);
+			ge = (strcmp(a->proto_string, b->proto_string) >= 0);
 		}
 	}
 	CATCH_ALL {
@@ -316,7 +316,7 @@ cmp_lt(const fvalue_t *fv_a, const fvalue_t *fv_b)
 				lt = (memcmp(tvb_get_ptr(a->tvb, 0, a_len), tvb_get_ptr(b->tvb, 0, a_len), a_len) < 0);
 			}
 		} else {
-			return (strcmp(a->proto_string, b->proto_string) < 0);
+			lt = (strcmp(a->proto_string, b->proto_string) < 0);
 		}
 	}
 	CATCH_ALL {
@@ -345,7 +345,7 @@ cmp_le(const fvalue_t *fv_a, const fvalue_t *fv_b)
 				le = (memcmp(tvb_get_ptr(a->tvb, 0, a_len), tvb_get_ptr(b->tvb, 0, a_len), a_len) <= 0);
 			}
 		} else {
-			return (strcmp(a->proto_string, b->proto_string) <= 0);
+			le = (strcmp(a->proto_string, b->proto_string) <= 0);
 		}
 	}
 	CATCH_ALL {
@@ -429,7 +429,7 @@ cmp_matches(const fvalue_t *fv_a, const fvalue_t *fv_b)
 		}
 	}
 	CATCH_ALL {
-		return FALSE;
+		rc = FALSE;
 	}
 	ENDTRY;
 	return rc;
