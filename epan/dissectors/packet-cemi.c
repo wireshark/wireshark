9040e7b0f53a1a2e968c436e6957e34a9880205d
Dario Lombardo <lomato@gmail.com>
2018-11-01 23:51:49 +0100

    cemi: fix dead assignment (found by clang).
    Change-Id: I30739813c84ae05c9c6edf31b204dfff8496a954
    Reviewed-on: https://code.wireshark.org/review/30477
    Petri-Dish: Dario Lombardo <lomato@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>

--- epan/dissectors/packet-cemi.c
+++ epan/dissectors/packet-cemi.c
@@ -1396,7 +1396,7 @@ static void dissect_range( tvbuff_t *tvb, packet_info *pinfo, proto_item *node, 
       }
 
       /* 12 bits Start Index */
-      node = proto_tree_add_item( range_list, hf_cemi_sx, tvb, offset, 2, ENC_BIG_ENDIAN );
+      proto_tree_add_item( range_list, hf_cemi_sx, tvb, offset, 2, ENC_BIG_ENDIAN );
     }
 
     offset += 2;
