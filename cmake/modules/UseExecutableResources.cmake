85f822b04442bdaaf5f39632412db3afbf842921
Pascal Quantin <pascal.quantin@gmail.com>
2018-11-06 22:22:29 +0100

    Windows: fix RC information generation
    Change-Id: I36e1da16b7eb8c224dab74750939a2c9a9a01cfa
    Reviewed-on: https://code.wireshark.org/review/30525
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- cmake/modules/UseExecutableResources.cmake
+++ cmake/modules/UseExecutableResources.cmake
@@ -2,11 +2,11 @@
 # UNIQUE_RC - Use if the program has its own .rc.in file. Otherwise cli_template.rc.in will be used.
 # EXE_NAME - The lowercase executable base name.
 # PROGRAM_NAME - The program's proper, capitalized name.
-# COPYRIGHT_HOLDERS - Year followed by copyright holder names if different from default.
+# COPYRIGHT_INFO - Year followed by copyright holder names if different from default.
 function(set_executable_resources EXE_NAME PROGRAM_NAME)
 	if (WIN32)
 		set(options UNIQUE_RC)
-		set(one_value_args COPYRIGHT_HOLDERS)
+		set(one_value_args COPYRIGHT_INFO)
 		cmake_parse_arguments(EXE_RC "${options}" "${one_value_args}" "" ${ARGN} )
 		if (EXE_RC_COPYRIGHT_INFO)
 			set(COPYRIGHT_INFO "${EXE_RC_COPYRIGHT_INFO}")
@@ -15,7 +15,7 @@ function(set_executable_resources EXE_NAME PROGRAM_NAME)
 			set(COPYRIGHT_INFO "2000 Gerald Combs <gerald@wireshark.org>, Gilbert Ramirez <gram@alumni.rice.edu> and many others")
 		endif()
 		set(${EXE_NAME}_FILES ${${EXE_NAME}_FILES} ${CMAKE_BINARY_DIR}/image/${EXE_NAME}.rc PARENT_SCOPE)
-		if (EXE_RC_UNIQUE)
+		if (EXE_RC_UNIQUE_RC)
 			set (_in_file ${EXE_NAME})
 		else()
 			set (_in_file "cli_template")
