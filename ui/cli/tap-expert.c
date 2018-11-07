8d109ce13b8fd2210b103b329ac7a9602e1e0a6d
Vasil Velichkov <vasko@vvelichkov2.dev.opencode.com>
2018-11-07 21:33:41 +0200

    tshark: Print the packets' comments in the expert info
    Previously 'tshark -z expert' was failing with abort when a packet
    contains a comment
    
    - Add a new comment parameter and update the tshark's manual page
    - Add a new comment_level severity and change the default lavel to it.
    - Add various 'tshark -z expert' tests
    
    Change-Id: I188317da5e00019b8f2b725f0fe84942f774520f
    Reviewed-on: https://code.wireshark.org/review/30610
    Petri-Dish: Guy Harris <guy@alum.mit.edu>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit ec95ae98ed29190f3df044e8676d6aed101405c0)
    Reviewed-on: https://code.wireshark.org/review/30611

--- ui/cli/tap-expert.c
+++ ui/cli/tap-expert.c
@@ -24,7 +24,8 @@ void register_tap_listener_expert_info(void);
 
 /* Tap data */
 typedef enum severity_level_t {
-    chat_level = 0,
+    comment_level = 0,
+    chat_level,
     note_level,
     warn_level,
     error_level,
@@ -33,7 +34,7 @@ typedef enum severity_level_t {
 
 /* This variable stores the lowest level that will be displayed.
    May be changed from the command line */
-static severity_level_t lowest_report_level = chat_level;
+static severity_level_t lowest_report_level = comment_level;
 
 typedef struct expert_entry
 {
@@ -81,6 +82,9 @@ expert_stat_packet(void *tapdata, packet_info *pinfo _U_, epan_dissect_t *edt _U
     guint                n;
 
     switch (ei->severity) {
+        case PI_COMMENT:
+            severity_level = comment_level;
+            break;
         case PI_CHAT:
             severity_level = chat_level;
             break;
@@ -176,6 +180,7 @@ expert_stat_draw(void *phs _U_)
     draw_items_for_severity(hs->ei_array[warn_level],  "Warns");
     draw_items_for_severity(hs->ei_array[note_level],  "Notes");
     draw_items_for_severity(hs->ei_array[chat_level],  "Chats");
+    draw_items_for_severity(hs->ei_array[comment_level],  "Comments");
 }
 
 /* Create a new expert stats struct */
@@ -212,6 +217,9 @@ static void expert_stat_init(const char *opt_arg, void *userdata _U_)
         } else if (g_ascii_strncasecmp(args, ",chat", 5) == 0) {
             lowest_report_level = chat_level;
             args += 5;
+        } else if (g_ascii_strncasecmp(args, ",comment", 8) == 0) {
+            lowest_report_level = comment_level;
+            args += 8;
         }
     }
 
