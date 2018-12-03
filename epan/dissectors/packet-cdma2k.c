1bd99df3535ac053f182a7e4e2246dae3e4c53ed
Jaap Keuter <jaap.keuter@xs4all.nl>
2018-12-03 17:15:27 +0100

    CDMA2000: Improper offset handling
    Somewhere in the code the handling of the offset goes wrong.
    Instead of incrementing the offset it's the pointer to the offset
    which is being incremented, leading to all sorts of problems.
    Add a dereference to these few statements which lack them.
    
    Bug: 15322
    Change-Id: If575711a5b120f25f0172e0efb26e01f07244e8b
    Signed-off-by: Jaap Keuter <jaap.keuter@xs4all.nl>
    Reviewed-on: https://code.wireshark.org/review/30899
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-cdma2k.c
+++ epan/dissectors/packet-cdma2k.c
@@ -3857,12 +3857,12 @@ static void cdma2k_message_ACTIVE_SET_RECORD_FIELDS(proto_item *item _U_, tvbuff
             *l_offset+=3;
             proto_tree_add_bits_item(subtree1, hf_cdma2k_Record_Len, tvb, *l_offset, 3, ENC_BIG_ENDIAN);
             recLen = tvb_get_bits8(tvb,*l_offset, 3);
-            l_offset+=3;
+            *l_offset+=3;
             item2 = proto_tree_add_item(subtree1, hf_cdma2k_Type_Specific_Fields, tvb, (*l_offset/8),recLen+1, ENC_NA);
             while(recLen > 0)
             {
                 proto_item_append_text(item2," 0x%02x",tvb_get_bits8(tvb,*l_offset, 8));
-                l_offset+=8;
+                *l_offset+=8;
                 recLen-=1;
             }
         }
