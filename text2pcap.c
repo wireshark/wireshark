df60e0bacc3fc8aee8bf8a76596e02cc5e9eb2c1
Jaap Keuter <jaap.keuter@xs4all.nl>
2018-12-05 16:08:06 +0100

    text2pcap: extend snap length
    The snaplength as saved in the capture file is set to the original
    PCAP file length of 65535. In reality the package size can grow to
    256kiB. Make use of the correct constant when writing the PCAP file.
    
    Bug: 15292
    Change-Id: Ib7710e4151cdc712f3344c1436252f9b3bfb556c
    Signed-off-by: Jaap Keuter <jaap.keuter@xs4all.nl>
    Reviewed-on: https://code.wireshark.org/review/30923
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit b63e2d7a950a30ac8791caa41b704b4b442ee451)
    Reviewed-on: https://code.wireshark.org/review/30928

--- text2pcap.c
+++ text2pcap.c
@@ -392,7 +392,6 @@ static char tempbuf[64];
 /*----------------------------------------------------------------------
  * Stuff for writing a PCap file
  */
-#define PCAP_SNAPLEN        0xffff
 
 /* Link-layer type; see http://www.tcpdump.org/linktypes.html for details */
 static guint32 pcap_link_type = 1;   /* Default is LINKTYPE_ETHERNET */
@@ -910,7 +909,7 @@ write_file_header (void)
                                                                "",
                                                                NULL,
                                                                pcap_link_type,
-                                                               PCAP_SNAPLEN,
+                                                               WTAP_MAX_PACKET_SIZE_STANDARD,
                                                                &bytes_written,
                                                                0,
                                                                9,
@@ -918,7 +917,7 @@ write_file_header (void)
         }
     } else {
         success = libpcap_write_file_header(output_file, pcap_link_type,
-                                            PCAP_SNAPLEN, FALSE,
+                                            WTAP_MAX_PACKET_SIZE_STANDARD, FALSE,
                                             &bytes_written, &err);
     }
     if (!success) {
