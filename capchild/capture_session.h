f727e8b387eb83b91f927c3e8944fb974e211804
Guy Harris <guy@alum.mit.edu>
2019-01-03 11:39:54 -0800

    Fix indentation.
    Change-Id: I60d9b7b5dce04dae560a68feb329041c0374c241
    Reviewed-on: https://code.wireshark.org/review/31342
    Reviewed-by: Guy Harris <guy@alum.mit.edu>

--- capchild/capture_session.h
+++ capchild/capture_session.h
@@ -52,7 +52,7 @@ typedef struct _capture_session {
     guint32   count;                      /**< Total number of frames captured */
     capture_options *capture_opts;        /**< options for this capture */
     capture_file *cf;                     /**< handle to cfile */
-    struct _info_data *cap_data_info;          /**< stats for this capture */
+    struct _info_data *cap_data_info;     /**< stats for this capture */
 } capture_session;
 
 extern void
