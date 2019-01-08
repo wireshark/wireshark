85c189fdb7c44bcafa7630ea0350fe3493d8538f
Maksim Salau <maksim.salau@gmail.com>
2019-01-08 20:35:00 +0300

    dissectors: j1939: Use raw id to filter packets
    Using raw CAN id allows us to drop error frames and frames with
    standard ids since J1939 work only with extended ids.
    Also if allows us to not to decode payload of RTR frames.
    
    Change-Id: I06cec52176be79028f9ac5bce2017907c06aacfb
    Reviewed-on: https://code.wireshark.org/review/31449
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-j1939.c
+++ epan/dissectors/packet-j1939.c
@@ -12,6 +12,7 @@
  */
 #include "config.h"
 
+#include <inttypes.h>
 #include <epan/packet.h>
 #include <epan/address_types.h>
 #include <epan/to_str.h>
@@ -20,9 +21,6 @@
 
 void proto_register_j1939(void);
 void proto_reg_handoff_j1939(void);
-
-#define J1939_CANID_MASK        0x1FFFFFFF
-#define J1939_11BIT_ID          0x000003FF
 
 static int proto_j1939 = -1;
 
@@ -172,9 +170,10 @@ static int dissect_j1939(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, vo
     DISSECTOR_ASSERT(data);
     can_id = *((struct can_identifier*)data);
 
-    if (can_id.id & (~J1939_CANID_MASK))
+    if ((can_id.raw_id & CAN_ERR_FLAG) ||
+        !(can_id.raw_id & CAN_EFF_FLAG))
     {
-        /* Not for us */
+        /* Error frames and frames with standards ids are not for us */
         return 0;
     }
 
@@ -185,52 +184,60 @@ static int dissect_j1939(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, vo
     j1939_tree = proto_item_add_subtree(ti, ett_j1939);
 
     can_tree = proto_tree_add_subtree_format(j1939_tree, tvb, 0, 0,
-                    ett_j1939_can, NULL, "CAN Identifier: 0x%08x", can_id.id);
-    can_id_item = proto_tree_add_uint(can_tree, hf_j1939_can_id, tvb, 0, 0, can_id.id);
+                    ett_j1939_can, NULL, "CAN Identifier: 0x%08x", can_id.raw_id);
+    can_id_item = proto_tree_add_uint(can_tree, hf_j1939_can_id, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(can_id_item);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_priority, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_priority, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_extended_data_page, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_extended_data_page, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_data_page, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_data_page, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_pdu_format, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_pdu_format, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_pdu_specific, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_pdu_specific, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
-    ti = proto_tree_add_uint(can_tree, hf_j1939_src_addr, tvb, 0, 0, can_id.id);
+    ti = proto_tree_add_uint(can_tree, hf_j1939_src_addr, tvb, 0, 0, can_id.raw_id);
     PROTO_ITEM_SET_GENERATED(ti);
 
     /* Set source address */
     src_addr = (guint8*)wmem_alloc(pinfo->pool, 1);
-    *src_addr = (guint8)(can_id.id & 0xFF);
+    *src_addr = (guint8)(can_id.raw_id & 0xFF);
     set_address(&pinfo->src, j1939_address_type, 1, (const void*)src_addr);
 
-    pgn = (can_id.id & 0x3FFFF00) >> 8;
+    pgn = (can_id.raw_id & 0x3FFFF00) >> 8;
 
     /* If PF < 240, PS is destination address, last byte of PGN is cleared */
-    if (((can_id.id & 0xFF0000) >> 16) < 240)
+    if (((can_id.raw_id & 0xFF0000) >> 16) < 240)
     {
         pgn &= 0x3FF00;
 
-        ti = proto_tree_add_uint(can_tree, hf_j1939_dst_addr, tvb, 0, 0, can_id.id);
+        ti = proto_tree_add_uint(can_tree, hf_j1939_dst_addr, tvb, 0, 0, can_id.raw_id);
         PROTO_ITEM_SET_GENERATED(ti);
     }
     else
     {
-        ti = proto_tree_add_uint(can_tree, hf_j1939_group_extension, tvb, 0, 0, can_id.id);
+        ti = proto_tree_add_uint(can_tree, hf_j1939_group_extension, tvb, 0, 0, can_id.raw_id);
         PROTO_ITEM_SET_GENERATED(ti);
     }
 
     /* Fill in "destination" address even if its "broadcast" */
     dest_addr = (guint8*)wmem_alloc(pinfo->pool, 1);
-    *dest_addr = (guint8)((can_id.id & 0xFF00) >> 8);
+    *dest_addr = (guint8)((can_id.raw_id & 0xFF00) >> 8);
     set_address(&pinfo->dst, j1939_address_type, 1, (const void*)dest_addr);
 
-    col_add_fstr(pinfo->cinfo, COL_INFO, "PGN: %d", pgn);
+    col_add_fstr(pinfo->cinfo, COL_INFO, "PGN: %-6"  PRIu32, pgn);
 
-    /* For now just include raw bytes */
-    col_append_fstr(pinfo->cinfo, COL_INFO, "   %s", tvb_bytes_to_str_punct(wmem_packet_scope(), tvb, 0, data_length, ' '));
+    if (can_id.raw_id & CAN_RTR_FLAG)
+    {
+        /* RTR frames don't have payload */
+        col_append_fstr(pinfo->cinfo, COL_INFO, "   %s", "(Remote Transmission Request)");
+    }
+    else
+    {
+        /* For now just include raw bytes */
+        col_append_fstr(pinfo->cinfo, COL_INFO, "   %s", tvb_bytes_to_str_punct(wmem_packet_scope(), tvb, 0, data_length, ' '));
+    }
 
     msg_tree = proto_tree_add_subtree(j1939_tree, tvb, 0, tvb_reported_length(tvb), ett_j1939_message, NULL, "Message");
 
@@ -276,7 +283,7 @@ void proto_register_j1939(void)
     static hf_register_info hf[] = {
         { &hf_j1939_can_id,
             {"CAN Identifier", "j1939.can_id",
-            FT_UINT32, BASE_HEX, NULL, J1939_CANID_MASK, NULL, HFILL }
+            FT_UINT32, BASE_HEX, NULL, CAN_EFF_MASK, NULL, HFILL }
         },
         { &hf_j1939_priority,
             {"Priority", "j1939.priority",
