75484b106da95c3fe73815fca10a6f8208e066cc
Pascal Quantin <pascal.quantin@gmail.com>
2019-01-25 15:22:46 +0100

    ETSI CAT: do not call tvb_bytes_to_str() with a 0 length
    Change-Id: Ibd8ebe801a7bb0196e9d0f8e1c19cc16050a4f60
    Reviewed-on: https://code.wireshark.org/review/31733
    Petri-Dish: Pascal Quantin <pascal.quantin@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

--- epan/dissectors/packet-etsi_card_app_toolkit.c
+++ epan/dissectors/packet-etsi_card_app_toolkit.c
@@ -1106,7 +1106,7 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 		ti = proto_tree_add_bytes_format(cat_tree, hf_cat_tlv, tvb, pos,
 					    len, ptr, "%s: %s",
 					    val_to_str_ext(tag, &comp_tlv_tag_vals_ext, "%02x"),
-					    (const guint8 *)tvb_bytes_to_str(wmem_packet_scope(), tvb, pos, len));
+					    (len > 0) ? tvb_bytes_to_str(wmem_packet_scope(), tvb, pos, len) : "");
 #else
 		ti = proto_tree_add_bytes_format(cat_tree, hf_cat_tlv, tvb, pos,
 					    len, ptr, "%s:   ",
