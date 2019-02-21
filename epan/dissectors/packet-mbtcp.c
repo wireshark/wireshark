e601a00d6eaca8e6d56939246931c32ed54fa005
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-02-21 09:46:17 +0100

    mbtcp: Fix Dead Store (Dead assignement/Dead increment) Warning found by Clang
    Change-Id: Ief90dd117bfc8f4b1f3717b89f405b025146e8ad
    Reviewed-on: https://code.wireshark.org/review/32132
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/dissectors/packet-mbtcp.c
+++ epan/dissectors/packet-mbtcp.c
@@ -1237,9 +1237,6 @@ dissect_modbus_response(tvbuff_t *tvb, packet_info *pinfo, proto_tree *modbus_tr
         case READ_INPUT_REGS:
             byte_cnt = (guint32)tvb_get_guint8(tvb, payload_start);
             proto_tree_add_uint(modbus_tree, hf_modbus_bytecnt, tvb, payload_start, 1, byte_cnt);
-            //if the request wasn't found set number of registers based on byte count
-            if (!pkt_info->request_found)
-                num_reg = byte_cnt/2;
             dissect_modbus_data(tvb, pinfo, modbus_tree, function_code, payload_start + 1, byte_cnt, pkt_info->register_format, pkt_info->reg_base, 0);
             break;
 
