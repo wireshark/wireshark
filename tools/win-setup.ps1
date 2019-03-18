aa3968d2c2ec434672d920f7c18b393236663d87
Gerald Combs <gerald@wireshark.org>
2019-03-18 14:59:34 -0700

    Windows: Switch to Npcap 0.991.
    Change-Id: I4f5d82bf0d205b9410db2098abe64dde1a33b21d
    Reviewed-on: https://code.wireshark.org/review/32474
    Petri-Dish: Gerald Combs <gerald@wireshark.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- tools/win-setup.ps1
+++ tools/win-setup.ps1
@@ -69,8 +69,8 @@ Param(
 # trouble instead of trying to catch exceptions everywhere.
 $ErrorActionPreference = "Stop"
 
-$Win64CurrentTag = "2019-01-23"
-$Win32CurrentTag = "2019-01-23"
+$Win64CurrentTag = "2019-03-18-3.0"
+$Win32CurrentTag = "2019-03-18-3.0"
 
 # Archive file / SHA256
 $Win64Archives = @{
@@ -127,12 +127,12 @@ $ArchivesSubDirectory = @{
 # Plain file downloads
 
 $Win32Files = @{
-    "npcap-0.99-r9.exe" = "fa35408528c4254cec6104f25f59b01fb7d35cd153507fc54a7291edc8824f7f";
+    "npcap-0.991.exe" = "01edecc5f9391051c512d8cd6bf1b0d106799c5b2a89454e3e98dd14b0819f59";
     "USBPcapSetup-1.2.0.4.exe" = "0a5ac30b0264e058f262e9c28e5865af7b836620ca5d68bb4bb42c9a808f7a43";
 }
 
 $Win64Files = @{
-    "npcap-0.99-r9.exe" = "fa35408528c4254cec6104f25f59b01fb7d35cd153507fc54a7291edc8824f7f";
+    "npcap-0.991.exe" = "01edecc5f9391051c512d8cd6bf1b0d106799c5b2a89454e3e98dd14b0819f59";
     "USBPcapSetup-1.2.0.4.exe" = "0a5ac30b0264e058f262e9c28e5865af7b836620ca5d68bb4bb42c9a808f7a43";
 }
 
