wireshark/wireshark

commit 018f6bff18785a1c3971a1ccfe3b1b5243d4d154

extcap: Interface Toolbar support on Windows
Add support for extcap control pipes on Windows.
Improved read loop in InterfaceToolbarReader.
Delay opening control pipes until extcap has opened the fifo pipe.
Make extcap_example.py work on Windows.

Bug: 13833
Change-Id: I4b47d25452637759b8a3be53be48eee5365bc0e4
Reviewed-on: https://code.wireshark.org/review/23211
Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
Tested-by: Petri Dish Buildbot <buildbot-no-reply@wireshark.org>
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/main_window_slots.cpp
@@ -657,7 +657,7 @@ void MainWindow::queuedFilterAction(QString action_filter, FilterAction::Action 
 
 // Capture callbacks
 
-void MainWindow::captureCapturePrepared(capture_session *session) {
+void MainWindow::captureCapturePrepared(capture_session *) {
 #ifdef HAVE_LIBPCAP
     setTitlebarForCaptureInProgress();
 
@@ -665,7 +665,7 @@ void MainWindow::captureCapturePrepared(capture_session *session) {
 
     /* Disable menu items that make no sense if you're currently running
        a capture. */
-    setForCaptureInProgress(true, session->capture_opts->ifaces);
+    setForCaptureInProgress(true);
 //    set_capture_if_dialog_for_capture_in_progress(TRUE);
 
 //    /* Don't set up main window for a capture file. */

AUTHORS.src
@@ -3955,6 +3955,7 @@ James Lynch		<lynch007[AT]gmail.com>
 Chidambaram Arunachalam	<carunach[AT]cisco.com>
 João Valverde		<joao.valverde[AT]tecnico.ulisboa.pt>
 Benoît Canet		<benoit[AT]scylladb.com>
+Håkon Øye Amundsen      <haakon.amundsen[AT]nordicsemi.no>
 
 Acknowledgements
 ------------

capture_opts.c
@@ -70,6 +70,8 @@ capture_opts_init(capture_options *capture_opts)
     capture_opts->default_options.extcap_pid      = INVALID_EXTCAP_PID;
 #ifdef _WIN32
     capture_opts->default_options.extcap_pipe_h   = INVALID_HANDLE_VALUE;
+    capture_opts->default_options.extcap_control_in_h  = INVALID_HANDLE_VALUE;
+    capture_opts->default_options.extcap_control_out_h = INVALID_HANDLE_VALUE;
 #endif
     capture_opts->default_options.extcap_control_in  = NULL;
     capture_opts->default_options.extcap_control_out = NULL;
@@ -718,6 +720,8 @@ capture_opts_add_iface_opt(capture_options *capture_opts, const char *optarg_str
     interface_opts.extcap_userdata = NULL;
 #ifdef _WIN32
     interface_opts.extcap_pipe_h = INVALID_HANDLE_VALUE;
+    interface_opts.extcap_control_in_h = INVALID_HANDLE_VALUE;
+    interface_opts.extcap_control_out_h = INVALID_HANDLE_VALUE;
 #endif
     interface_opts.extcap_control_in = g_strdup(capture_opts->default_options.extcap_control_in);
     interface_opts.extcap_control_out = g_strdup(capture_opts->default_options.extcap_control_out);
@@ -1234,6 +1238,8 @@ collect_ifaces(capture_options *capture_opts)
             interface_opts.extcap_userdata = NULL;
 #ifdef _WIN32
             interface_opts.extcap_pipe_h = INVALID_HANDLE_VALUE;
+            interface_opts.extcap_control_in_h = INVALID_HANDLE_VALUE;
+            interface_opts.extcap_control_out_h = INVALID_HANDLE_VALUE;
 #endif
             interface_opts.extcap_control_in = NULL;
             interface_opts.extcap_control_out = NULL;

capture_opts.h
@@ -238,6 +238,8 @@ typedef struct interface_options_tag {
     guint             extcap_child_watch;
 #ifdef _WIN32
     HANDLE            extcap_pipe_h;
+    HANDLE            extcap_control_in_h;
+    HANDLE            extcap_control_out_h;
 #endif
     gchar            *extcap_control_in;
     gchar            *extcap_control_out;

doc/README.extcap
@@ -356,7 +356,7 @@ Control packet:
   The common sync pipe indication. This protocol uses the value 'T'.
 
  Message Length:
-  Payload length + 2 bytes for argument number and command.
+  Payload length + 2 bytes for control number and command.
 
  Control Number:
    Unique number to identify the control.  This number also gives the order of

doc/extcap_example.py
@@ -340,28 +340,22 @@ def extcap_capture(interface, fifo, control_in, control_out, in_delay, in_verify
 	message = in_message
 	verify = in_verify
 	counter = 1
-
-	if not os.path.exists(fifo):
-		print ( "Fifo does not exist, exiting!", file=sys.stderr )
-		sys.exit(1)
-
 	fn_out = None
-	if control_out != None:
-		fn_out = open(control_out, 'wb', 0)
-		control_write(fn_out, CTRL_ARG_LOGGER, CTRL_CMD_SET, "Log started at " + time.strftime("%c") + "\n")
-
-
-	if control_in != None:
-		# Start reading thread
-		thread = Thread(target = control_read_thread, args = (control_in, fn_out))
-		thread.start()
-
-
-	if fn_out != None:
-		control_write_defaults(fn_out)
 
 	with open(fifo, 'wb', 0 ) as fh:
 		fh.write (pcap_fake_header())
+
+		if control_out != None:
+			fn_out = open(control_out, 'wb', 0)
+			control_write(fn_out, CTRL_ARG_LOGGER, CTRL_CMD_SET, "Log started at " + time.strftime("%c") + "\n")
+
+		if control_in != None:
+			# Start reading thread
+			thread = Thread(target = control_read_thread, args = (control_in, fn_out))
+			thread.start()
+
+		if fn_out != None:
+			control_write_defaults(fn_out)
 
 		while True:
 			if fn_out != None:
@@ -383,10 +377,6 @@ def extcap_capture(interface, fifo, control_in, control_out, in_delay, in_verify
 	        fn_out.close()
 
 def extcap_close_fifo(fifo):
-	if not os.path.exists(fifo):
-		print ( "Fifo does not exist!", file=sys.stderr )
-		return
-
 	# This is apparently needed to workaround an issue on Windows/macOS
 	# where the message cannot be read. (really?)
 	fh = open(fifo, 'wb', 0 )

extcap.c
@@ -1041,6 +1041,24 @@ void extcap_if_cleanup(capture_options *capture_opts, gchar **errormsg)
             CloseHandle(interface_opts.extcap_pipe_h);
             interface_opts.extcap_pipe_h = INVALID_HANDLE_VALUE;
         }
+        if (interface_opts.extcap_control_in_h != INVALID_HANDLE_VALUE)
+        {
+            g_log(LOG_DOMAIN_CAPTURE, G_LOG_LEVEL_DEBUG,
+                  "Extcap [%s] - Closing control_in pipe", interface_opts.name);
+            FlushFileBuffers(interface_opts.extcap_control_in_h);
+            DisconnectNamedPipe(interface_opts.extcap_control_in_h);
+            CloseHandle(interface_opts.extcap_control_in_h);
+            interface_opts.extcap_control_in_h = INVALID_HANDLE_VALUE;
+        }
+        if (interface_opts.extcap_control_out_h != INVALID_HANDLE_VALUE)
+        {
+            g_log(LOG_DOMAIN_CAPTURE, G_LOG_LEVEL_DEBUG,
+                  "Extcap [%s] - Closing control_out pipe", interface_opts.name);
+            FlushFileBuffers(interface_opts.extcap_control_out_h);
+            DisconnectNamedPipe(interface_opts.extcap_control_out_h);
+            CloseHandle(interface_opts.extcap_control_out_h);
+            interface_opts.extcap_control_out_h = INVALID_HANDLE_VALUE;
+        }
 #else
         if (interface_opts.extcap_fifo != NULL && file_exists(interface_opts.extcap_fifo))
         {
@@ -1355,14 +1373,20 @@ extcap_init_interfaces(capture_options *capture_opts)
         if (extcap_has_toolbar(interface_opts.name))
         {
             extcap_create_pipe(interface_opts.name, &interface_opts.extcap_control_in,
-                               EXTCAP_CONTROL_IN_PREFIX);
+                               EXTCAP_CONTROL_IN_PREFIX, FALSE);
+#ifdef _WIN32
+            interface_opts.extcap_control_in_h = pipe_h;
+#endif
             extcap_create_pipe(interface_opts.name, &interface_opts.extcap_control_out,
-                               EXTCAP_CONTROL_OUT_PREFIX);
+                               EXTCAP_CONTROL_OUT_PREFIX, FALSE);
+#ifdef _WIN32
+            interface_opts.extcap_control_out_h = pipe_h;
+#endif
         }
 
         /* create pipe for fifo */
         if (!extcap_create_pipe(interface_opts.name, &interface_opts.extcap_fifo,
-                                EXTCAP_PIPE_PREFIX))
+                                EXTCAP_PIPE_PREFIX, TRUE))
         {
             return FALSE;
         }
@@ -1403,7 +1427,18 @@ extcap_init_interfaces(capture_options *capture_opts)
          */
         if (pid != INVALID_EXTCAP_PID)
         {
-            extcap_wait_for_pipe(interface_opts.extcap_pipe_h, pid);
+            HANDLE pipe_handles[3];
+            int num_pipe_handles = 1;
+            pipe_handles[0] = interface_opts.extcap_pipe_h;
+
+            if (extcap_has_toolbar(interface_opts.name))
+            {
+                pipe_handles[1] = interface_opts.extcap_control_in_h;
+                pipe_handles[2] = interface_opts.extcap_control_out_h;
+                num_pipe_handles += 2;
+             }
+
+            extcap_wait_for_pipe(pipe_handles, num_pipe_handles, pid);
         }
 #endif
 
@@ -1416,15 +1451,14 @@ extcap_init_interfaces(capture_options *capture_opts)
     return TRUE;
 }
 
-gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe_prefix)
+gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe_prefix, gboolean byte_mode _U_)
 {
 #ifdef _WIN32
     gchar timestr[ 14 + 1 ];
     time_t current_time;
-
     gchar *pipename = NULL;
-
     SECURITY_ATTRIBUTES security;
+
     /* create pipename */
     current_time = time(NULL);
     /*
@@ -1440,18 +1474,19 @@ gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe
     security.bInheritHandle = TRUE;
     security.lpSecurityDescriptor = NULL;
 
-    /* create a namedPipe*/
+    /* create a namedPipe */
     pipe_h = CreateNamedPipe(
                  utf_8to16(pipename),
                  PIPE_ACCESS_DUPLEX | FILE_FLAG_OVERLAPPED,
-                 PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
-                 5, 65536, 65536,
+                 byte_mode ? PIPE_TYPE_BYTE : PIPE_TYPE_MESSAGE | byte_mode ? PIPE_READMODE_BYTE : PIPE_READMODE_MESSAGE | PIPE_WAIT,
+                 1, 65536, 65536,
                  300,
                  &security);
 
     if (pipe_h == INVALID_HANDLE_VALUE)
     {
         g_log(LOG_DOMAIN_CAPTURE, G_LOG_LEVEL_DEBUG, "\nError creating pipe => (%d)", GetLastError());
+        g_free (pipename);
         return FALSE;
     }
     else

extcap.h
@@ -40,8 +40,8 @@
 
 /* Prefix for the pipe interfaces */
 #define EXTCAP_PIPE_PREFIX "wireshark_extcap"
-#define EXTCAP_CONTROL_IN_PREFIX  "wireshark_control_in"
-#define EXTCAP_CONTROL_OUT_PREFIX "wireshark_control_out"
+#define EXTCAP_CONTROL_IN_PREFIX  "wireshark_control_ext_to_ws"
+#define EXTCAP_CONTROL_OUT_PREFIX "wireshark_control_ws_to_ext"
 
 #define EXTCAP_ARGUMENT_CONFIG                  "--extcap-config"
 #define EXTCAP_ARGUMENT_LIST_INTERFACES         "--extcap-interfaces"
@@ -143,11 +143,11 @@ gboolean
 extcap_init_interfaces(capture_options * capture_opts);
 
 gboolean
-extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe_prefix);
+extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe_prefix, gboolean byte_mode);
 
 /* Clean up all if related stuff */
 void
-extcap_if_cleanup(capture_options * capture_opts _U_, gchar ** errormsg);
+extcap_if_cleanup(capture_options * capture_opts, gchar ** errormsg);
 
 struct preference *
 extcap_pref_for_argument(const gchar *ifname, struct _extcap_arg * arg);

commit 82d24d907ae07b2cb9a558ec12a1f0cb3c47e0cb

Qt: Fix deadlock in extcap toolbar creation
Deadlock happened if extcap was writing to control pipe before it sent
the pcap data and the pipe buffer was not able to hold all the data.
The solution is to start reading data from extcap control pipe in
capture prepare.

This bug was introduced in 018f6bff18785a1c3971a1ccfe3b1b5243d4d154
which was reviewed on https://code.wireshark.org/review/23211
Unfortunately neither the commit message nor the review contains any
explaination why it delays the extcap control pipe opening.

Bug: 15261
Change-Id: I10d006128f6eb7d63f751d230edbd905eb46416b
Reviewed-on: https://code.wireshark.org/review/32898
Reviewed-by: Anders Broman <a.broman58@gmail.com>

ui/qt/main_window_slots.cpp
@@ -491,14 +491,14 @@ void MainWindow::queuedFilterAction(QString action_filter, FilterAction::Action 
 // Capture callbacks
 
 #ifdef HAVE_LIBPCAP
-void MainWindow::captureCapturePrepared(capture_session *) {
+void MainWindow::captureCapturePrepared(capture_session *session) {
     setTitlebarForCaptureInProgress();
 
     setWindowIcon(wsApp->captureIcon());
 
     /* Disable menu items that make no sense if you're currently running
        a capture. */
-    setForCaptureInProgress(true);
+    setForCaptureInProgress(true, session->capture_opts->ifaces);
 //    set_capture_if_dialog_for_capture_in_progress(TRUE);
 
 //    /* Don't set up main window for a capture file. */