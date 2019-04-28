af72ba7e84ca85bc5ea57c3c88b70398a83dbb0f
Peter Wu <peter@lekensteyn.nl>
2019-04-28 03:01:10 +0100

    FindPCAP: fix broken search logic
    Do not fully trust the output from neither pkg-config nor pcap-config.
    These tools might provide bogus output. Instead, use their outputs as
    hints to let CMake find the correct include and (static) library paths.
    
    Correct some variable names (PCAP_STATIC_LIBRARIES and PCAP_INCLUDE_DIRS
    *must not* be the result of find_path/find_library) and ensure that an
    empty include directory from pkg-config does not result in an empty
    PCAP_INCLUDE_DIRS variable that would break the build.
    
    Change-Id: If3de90fb497d8163d92e4fe190a227159f0b6acb
    Fixes: v3.1.0rc0-645-gc602119bcf ("Use pkg-config if possible; if not, use pcap-config if present.")
    Reviewed-on: https://code.wireshark.org/review/32999
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>

--- cmake/modules/FindPCAP.cmake
+++ cmake/modules/FindPCAP.cmake
@@ -6,241 +6,171 @@
 #  PCAP_LIBRARIES    - List of libraries when using pcap.
 #  PCAP_FOUND        - True if pcap found.
 
-include( FindWSWinLibs )
-FindWSWinLibs( "WpdPack" "PCAP_HINTS" )
+include(FindWSWinLibs)
+FindWSWinLibs("WpdPack" "PCAP_HINTS")
 
 # The 64-bit wpcap.lib is under /x64
-set ( _PLATFORM_SUBDIR "" )
-if( WIN32 AND "${WIRESHARK_TARGET_PLATFORM}" STREQUAL "win64" )
-  set ( _PLATFORM_SUBDIR "/x64" )
+set(_PLATFORM_SUBDIR "")
+if(WIN32 AND WIRESHARK_TARGET_PLATFORM STREQUAL "win64")
+  set(_PLATFORM_SUBDIR "/x64")
 endif()
 
 #
-# First, try pkg-config.
+# First, try pkg-config on platforms other than Windows.
 #
-find_package( PkgConfig )
-pkg_search_module( PCAP libpcap )
+if(NOT WIN32)
+  find_package(PkgConfig)
+  pkg_search_module(PC_PCAP libpcap)
+endif()
 
-if( PCAP_FOUND )
+if(NOT PC_PCAP_FOUND AND NOT WIN32)
   #
-  # That worked.
-  # Now, for each library, try to find it, so we get its full path.
-  # CMake *really* doesn't like the notion of specifying "here are
-  # the directories in which to look for libraries" except in
-  # find_library() calls; it *really* prefers using full paths to
-  # library files, rather than library names.
+  # That didn't work.  Try to retrieve hints from pcap-config.
+  # Do not use it on Windows as pcap-config is a shell script.
   #
-  set( _pcap_libraries "${PCAP_LIBRARIES}" )
-  set( PCAP_LIBRARIES "" )
-  foreach( _lib IN LISTS _pcap_libraries )
-      #
-      # Try to find that library.
-      #
-      find_library( _libfullpath ${_lib} HINTS ${PCAP_LIBRARY_DIRS} )
-      list( APPEND PCAP_LIBRARIES ${_libfullpath} )
-      #
-      # Remove that from the cache; we're using it as a local variable,
-      # but find_library insists on making it a cache variable.
-      #
-      unset( _libfullpath CACHE )
-    endforeach()
-
-    #
-    # Now find the static libraries.
-    # (XXX - what about AIX?)
-    #
-    set( _pcap_static_libraries "${PCAP_STATIC_LIBRARIES}" )
-    set( PCAP_STATIC_LIBRARIES "" )
-    set( SAVED_CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_FIND_LIBRARY_SUFFIXES}" )
-    set( CMAKE_FIND_LIBRARY_SUFFIXES ".a" )
-    foreach( _lib IN LISTS _pcap_static_libraries )
-      #
-      # Try to find that library, so we get its full path, as
-      # we do with dynamic libraries.
-      #
-      find_library( _libfullpath ${_lib} HINTS ${PCAP_LIBRARY_DIRS} )
-      list( APPEND PCAP_STATIC_LIBRARIES ${_libfullpath} )
-      #
-      # Remove that from the cache; we're using it as a local variable,
-      # but find_library insists on making it a cache variable.
-      #
-      unset( _libfullpath CACHE )
-    endforeach()
-    set( CMAKE_FIND_LIBRARY_SUFFIXES "${SAVED_CMAKE_FIND_LIBRARY_SUFFIXES}" )
-else( PCAP_FOUND )
-  #
-  # That didn't work.  Try pcap-config.
-  #
-  find_program( PCAP_CONFIG pcap-config )
-  if( PCAP_CONFIG )
+  find_program(PCAP_CONFIG pcap-config)
+  if(PCAP_CONFIG)
     #
     # We have pcap-config; use it.
-    # XXX - what if this is on Windows?  If you're using, for example,
-    # MinGW, that might be the right thing to do, *if* pcap-config
-    # were made to work properly on Windows, but what about MSVC?
     #
     # First, get the include directory.
     #
-    execute_process( COMMAND "${PCAP_CONFIG}" "--cflags"
+    execute_process(COMMAND "${PCAP_CONFIG}" "--cflags"
       RESULT_VARIABLE PCAP_CONFIG_RESULT
       OUTPUT_VARIABLE PCAP_CONFIG_OUTPUT
       OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    if( NOT PCAP_CONFIG_RESULT EQUAL 0 )
-      message( FATAL_ERROR "pcap-config --cflags failed" )
+    if(NOT PCAP_CONFIG_RESULT EQUAL 0)
+      message(FATAL_ERROR "pcap-config --cflags failed")
     endif()
     #
-    # XXX - this assumes that there's only one -I flag in the output
+    # Assumes there's exactly one -I flag in the output
     # of pcap-config --cflags.  That *should* be the case.
-    #
-    string(REGEX REPLACE "-I" "" _pcap_include_dir "${PCAP_CONFIG_OUTPUT}")
-
-    # Try to find the header
-    # We use what pcap-config provided as a hint, because the
-    # pcap-config that ships with macOS bogusly supplies
+    # Note that the hint might be bogus, on macOS it could be
     # -I/usr/local/include even though the header isn't
     # there (it may be under /usr/include or it may be
     # buried in the Xcode app bundle).
-    find_path(PCAP_INCLUDE_DIRS pcap.h HINTS ${_pcap_include_dir})
+    #
+    string(REGEX REPLACE "^-I" "" PCAP_CONFIG_INCLUDE_DIRS "${PCAP_CONFIG_OUTPUT}")
 
-    # Now, get the libraries.
-    execute_process( COMMAND "${PCAP_CONFIG}" "--libs"
+    # Now, get the library search path.
+    execute_process(COMMAND "${PCAP_CONFIG}" "--libs"
       RESULT_VARIABLE PCAP_CONFIG_RESULT
       OUTPUT_VARIABLE PCAP_CONFIG_OUTPUT
       OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    if( NOT PCAP_CONFIG_RESULT EQUAL 0 )
-      message( FATAL_ERROR "pcap-config --libs failed" )
+    if(NOT PCAP_CONFIG_RESULT EQUAL 0)
+      message(FATAL_ERROR "pcap-config --libs failed")
     endif()
-    separate_arguments( LIBS_LIST UNIX_COMMAND ${PCAP_CONFIG_OUTPUT} )
-    set( _pcap_library_dirs "" )
-    set( PCAP_LIBRARIES "" )
-    foreach( _arg IN LISTS LIBS_LIST )
-      if( _arg MATCHES "^-L" )
-        # Add this directory to _pcap_library_dirs
-        string( REGEX REPLACE "-L" "" _dir ${_arg} )
-        list( APPEND _pcap_library_dirs ${_dir} )
-      elseif( _arg MATCHES "^-l" )
-        string( REGEX REPLACE "-l" "" _lib ${_arg} )
-        #
-        # Try to find that library, so we get its full path.  See the
-        # comment above for why we do this.
-        #
-        # Furthermore, the pcap-config shipped with macOS reports
-        # -I/usr/local/include for --cflags and -L/usr/local/lib for
-        # --libs, rather than reporting the appropriate system (or
-        # Xcode application) directory.
-        #
-        find_library( _libfullpath ${_lib} HINTS ${__pcap_library_dirs} )
-        list( APPEND PCAP_LIBRARIES ${_libfullpath} )
-        #
-        # Remove that from the cache; we're using it as a local variable,
-        # but find_library insists on making it a cache variable.
-        #
-        unset( _libfullpath CACHE )
+    separate_arguments(LIBS_LIST UNIX_COMMAND ${PCAP_CONFIG_OUTPUT})
+    set(PCAP_CONFIG_LIBRARY_DIRS "")
+    foreach(_arg IN LISTS LIBS_LIST)
+      # At most one -L path is expected for -lpcap.
+      if(_arg MATCHES "^-L")
+        string(REGEX REPLACE "^-L" "" _dir ${_arg})
+        list(APPEND PCAP_CONFIG_LIBRARY_DIRS ${_dir})
       endif()
     endforeach()
 
-    # Now, get the library directories and libraries for static linking.
-    # (XXX - what about AIX?)
-    execute_process( COMMAND "${PCAP_CONFIG}" "--libs" "--static"
-      RESULT_VARIABLE PCAP_CONFIG_RESULT
-      OUTPUT_VARIABLE PCAP_CONFIG_OUTPUT
-    )
-    if( NOT PCAP_CONFIG_RESULT EQUAL 0 )
-      message( FATAL_ERROR "pcap-config --libs --static failed" )
-    endif()
-    separate_arguments( LIBS_LIST UNIX_COMMAND ${PCAP_CONFIG_OUTPUT} )
-    set( _pcap_static_library_dirs "" )
-    set( PCAP_STATIC_LIBRARIES "" )
-    set( SAVED_CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_FIND_LIBRARY_SUFFIXES}" )
-    set( CMAKE_FIND_LIBRARY_SUFFIXES ".a" )
-    foreach( _arg IN LISTS LIBS_LIST )
-      if( _arg MATCHES "^-L" )
-        # Add this directory to _pcap_static_library_dirs
-        string( REGEX REPLACE "-L" "" _dir ${_arg} )
-        list( APPEND _pcap_static_library_dirs ${_dir} )
-      elseif( _arg MATCHES "^-l" )
-        string( REGEX REPLACE "-l" "" _lib ${_arg} )
-        #
-        # Try to find that library, so we get its full path, as
-        # we do with dynamic libraries.
-        #
-        find_library( _libfullpath ${_lib} HINTS ${__pcap_static_library_dirs} )
-        list( APPEND PCAP_STATIC_LIBRARIES ${_libfullpath} )
-        #
-        # Remove that from the cache; we're using it as a local variable,
-        # but find_library insists on making it a cache variable.
-        #
-        unset( _libfullpath CACHE )
-      endif()
-    endforeach()
-
-    set( CMAKE_FIND_LIBRARY_SUFFIXES "${SAVED_CMAKE_FIND_LIBRARY_SUFFIXES}" )
-  else( PCAP_CONFIG )
-    #
-    # We don't have pcap-config.
-    # Try to find the header by just looking for it in whatever
-    # directories find_path() uses by default, plus ${PCAP_HINTS}.
-    #
-    find_path( PCAP_INCLUDE_DIRS
-      NAMES
-      pcap/pcap.h
-      pcap.h
-      HINTS
-      "${PCAP_HINTS}/include"
-    )
-
-    # Try to find the library
-    if( WIN32 )
-      # The 64-bit Packet.lib is located under /x64
-      if( "${WIRESHARK_TARGET_PLATFORM}" STREQUAL "win64" )
-        #
-        # For the WinPcap and Npcap SDKs, the Lib subdirectory of the top-level
-        # directory contains 32-bit libraries; the 64-bit libraries are in the
-        # Lib/x64 directory.
-        #
-        # The only way to *FORCE* CMake to look in the Lib/x64 directory
-        # without searching in the Lib directory first appears to be to set
-        # CMAKE_LIBRARY_ARCHITECTURE to "x64".
-        #
-        set(CMAKE_LIBRARY_ARCHITECTURE "x64")
-      endif()
-    endif()
-
-    find_library( PCAP_LIBRARIES
-      NAMES
-        pcap
-        wpcap
-      HINTS
-        "${PCAP_HINTS}/lib${_PLATFORM_SUBDIR}"
-    )
-
-    if( NOT WIN32 )
-      # Try to find the static library (XXX - what about AIX?)
-      set( SAVED_CMAKE_FIND_LIBRARY_SUFFIXES "${CMAKE_FIND_LIBRARY_SUFFIXES}")
-      set( CMAKE_FIND_LIBRARY_SUFFIXES ".a" )
-      find_library( PCAP_STATIC_LIBRARIES
-        NAMES
-          pcap
-          wpcap
-        HINTS
-          "${PCAP_HINTS}/lib${_PLATFORM_SUBDIR}"
+    if(UNIX AND CMAKE_FIND_LIBRARY_SUFFIXES STREQUAL ".a")
+      # Now, get the library directories and libraries for static linking.
+      # (XXX - what about AIX?)
+      execute_process(COMMAND "${PCAP_CONFIG}" "--libs" "--static"
+        RESULT_VARIABLE PCAP_CONFIG_RESULT
+        OUTPUT_VARIABLE PCAP_CONFIG_OUTPUT
       )
-      set( CMAKE_FIND_LIBRARY_SUFFIXES "${SAVED_CMAKE_FIND_LIBRARY_SUFFIXES}")
-    endif( NOT WIN32 )
-  endif( PCAP_CONFIG )
-endif( PCAP_FOUND )
+      if(NOT PCAP_CONFIG_RESULT EQUAL 0)
+        message(FATAL_ERROR "pcap-config --libs --static failed")
+      endif()
+      separate_arguments(LIBS_LIST UNIX_COMMAND ${PCAP_CONFIG_OUTPUT})
+      set(PCAP_CONFIG_STATIC_LIBRARY_DIRS "")
+      set(PCAP_CONFIG_STATIC_LIBRARIES "")
+      foreach(_arg IN LISTS LIBS_LIST)
+        if(_arg MATCHES "^-L")
+          # Add this directory to the library directory hints.
+          string(REGEX REPLACE "^-L" "" _dir ${_arg})
+          list(APPEND PCAP_CONFIG_STATIC_LIBRARY_DIRS ${_dir})
+        elseif(_arg MATCHES "^-l")
+          # Add this library to the requirements for static linking.
+          string(REGEX REPLACE "^-l" "" _lib ${_arg})
+          list(APPEND PCAP_CONFIG_STATIC_LIBRARIES ${_lib})
+        endif()
+      endforeach()
+    endif()
+  endif()
+endif()
 
-include( FindPackageHandleStandardArgs )
-find_package_handle_standard_args( PCAP DEFAULT_MSG PCAP_LIBRARIES PCAP_INCLUDE_DIRS )
+#
+# Locate the actual include directory. For pkg-config the
+# PC_PCAP_INCLUDE_DIRS variable could be empty if the default
+# header search path is sufficient to locate the header file.
+# For macOS, the directory returned by pcap-config is wrong, so
+# this will make sure to find a valid path.
+#
+find_path(PCAP_INCLUDE_DIR
+  NAMES
+    pcap/pcap.h
+    pcap.h
+  HINTS
+    ${PC_PCAP_INCLUDE_DIRS}
+    ${PCAP_CONFIG_INCLUDE_DIRS}
+    "${PCAP_HINTS}/include"
+)
 
-mark_as_advanced( PCAP_INCLUDE_DIR PCAP_LIBRARY PCAP_STATIC_LIBRARY )
+find_library(PCAP_LIBRARY
+  NAMES
+    pcap
+    wpcap
+  HINTS
+    ${PC_PCAP_LIBRARY_DIRS}
+    ${PCAP_CONFIG_LIBRARY_DIRS}
+    "${PCAP_HINTS}/lib${_PLATFORM_SUBDIR}"
+)
 
-if( PCAP_FOUND )
-  # Include transitive dependencies for static linking.
-  if( UNIX AND CMAKE_FIND_LIBRARY_SUFFIXES STREQUAL ".a" )
-    set( PCAP_LIBRARIES ${PCAP_STATIC_LIBRARIES} )
+if(UNIX AND CMAKE_FIND_LIBRARY_SUFFIXES STREQUAL ".a")
+  # Try to find the static library (XXX - what about AIX?)
+  if(PC_PCAP_FOUND)
+    set(_pcap_static_libraries ${PC_PCAP_STATIC_LIBRARIES})
+  elseif(PCAP_CONFIG)
+    set(_pcap_static_libraries ${PCAP_CONFIG_STATIC_LIBRARIES})
+  else()
+    #
+    # No pkg-config nor pcap-config found, hope that this single library is
+    # sufficient for static linking.
+    #
+    set(_pcap_static_libraries pcap)
+  endif()
+
+  set(PCAP_STATIC_LIBRARIES "")
+  foreach(_lib IN LISTS _pcap_static_libraries)
+    #
+    # Try to find that library, so we get its full path, as
+    # we do with dynamic libraries.
+    #
+    string(MAKE_C_IDENTIFIER "PCAP_STATIC_${_lib}_LIBRARY" _libvar)
+    find_library(${_libvar} ${_lib}
+      HINTS
+      ${PC_PCAP_STATIC_LIBRARY_DIRS}
+      ${PCAP_CONFIG_STATIC_LIBRARY_DIRS}
+    )
+    set(_libpath ${${_libvar}})
+    if(_libpath)
+      list(APPEND PCAP_STATIC_LIBRARIES ${_libpath})
+    endif()
+  endforeach()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PCAP DEFAULT_MSG PCAP_LIBRARY PCAP_INCLUDE_DIR)
+mark_as_advanced(PCAP_LIBRARY PCAP_INCLUDE_DIR)
+
+if(PCAP_FOUND)
+  set(PCAP_INCLUDE_DIRS ${PCAP_INCLUDE_DIR})
+  if(UNIX AND CMAKE_FIND_LIBRARY_SUFFIXES STREQUAL ".a")
+    # Link with static libpcap and its transitive dependencies.
+    set(PCAP_LIBRARIES ${PCAP_STATIC_LIBRARIES})
+  else()
+    set(PCAP_LIBRARIES ${PCAP_LIBRARY})
   endif()
 
   #Functions
