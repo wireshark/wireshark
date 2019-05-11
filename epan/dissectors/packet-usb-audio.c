4f599be06dfd03c3a30d24f9c1e94adb1bd420a0
Tomasz Moń <desowin@gmail.com>
2019-05-11 17:41:49 +0200

    USB Audio: Do not group MIDI Events under USB Audio
    Each MIDI Event creates its own protocol tree and thus the generic USB
    Audio tree is not needed.
    
    Ping-Bug: 15503
    Change-Id: I83ab01e340fce72e8ab824a2ee77ae37c033daae
    Reviewed-on: https://code.wireshark.org/review/33160
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Petri-Dish: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>

--- epan/dissectors/packet-usb-audio.c
+++ epan/dissectors/packet-usb-audio.c
@@ -696,7 +696,7 @@ is_last_sysex_packet_in_tvb(tvbuff_t *tvb, gint offset)
 
 static void
 dissect_usb_midi_event(tvbuff_t *tvb, packet_info *pinfo,
-                       proto_tree *usb_audio_tree, proto_tree *parent_tree,
+                       proto_tree *parent_tree,
                        gint offset)
 {
     guint8      code;
@@ -713,7 +713,7 @@ dissect_usb_midi_event(tvbuff_t *tvb, packet_info *pinfo,
         proto_item *ti;
         gint event_size, padding_size;
 
-        ti = proto_tree_add_protocol_format(usb_audio_tree, proto_usb_audio, tvb, offset, 4, "USB Midi Event Packet: %s",
+        ti = proto_tree_add_protocol_format(parent_tree, proto_usb_audio, tvb, offset, 4, "USB Midi Event Packet: %s",
                  try_val_to_str(code, code_index_vals));
         tree = proto_item_add_subtree(ti, ett_usb_audio);
         proto_tree_add_item(tree, hf_midi_cable_number, tvb, offset, 1, ENC_BIG_ENDIAN);
@@ -1793,8 +1793,6 @@ static int
 dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree, void *data)
 {
     usb_conv_info_t *usb_conv_info;
-    proto_tree      *tree;
-    proto_item      *ti;
     gint             offset, length;
     gint             i;
 
@@ -1804,9 +1802,6 @@ dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tre
     usb_conv_info = (usb_conv_info_t *)data;
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "USBAUDIO");
-
-    ti   = proto_tree_add_protocol_format(parent_tree, proto_usb_audio, tvb, 0, -1, "USB Audio");
-    tree = proto_item_add_subtree(ti, ett_usb_audio);
 
     length = tvb_reported_length(tvb);
     offset = 0;
@@ -1818,12 +1813,12 @@ dissect_usb_audio_bulk(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tre
 
             for (i = 0; i < length / 4; i++)
             {
-                dissect_usb_midi_event(tvb, pinfo, tree, parent_tree, offset);
+                dissect_usb_midi_event(tvb, pinfo, parent_tree, offset);
                 offset += 4;
             }
             break;
         default:
-            proto_tree_add_expert(tree, pinfo, &ei_usb_audio_undecoded, tvb, offset, length);
+            proto_tree_add_expert(parent_tree, pinfo, &ei_usb_audio_undecoded, tvb, offset, length);
     }
 
     return length;
