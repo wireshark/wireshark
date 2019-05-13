2ae17b88bcd915b07b6df01d6185b5e79045ac1b
Guy Harris <guy@alum.mit.edu>
2019-05-13 15:55:17 -0700

    Fix SMB2 decryption on big-endian machines.
    Convert the host-endian session ID passed to seskey_find_sid_key()
    before comparing it with the little-endian session IDs in the UAT.
    
    While we're at it, tag session ID fields in various structures with the
    byte order.
    
    Bug: 15772
    Change-Id: Ib1e7323bad1dfdb1ac24a08998205650f2744097
    Reviewed-on: https://code.wireshark.org/review/33188
    Petri-Dish: Guy Harris <guy@alum.mit.edu>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (cherry picked from commit 3234152bec1d1245cc0a8288c9be90242e2a11ed)
    Reviewed-on: https://code.wireshark.org/review/33190

--- epan/dissectors/packet-smb2.h
+++ epan/dissectors/packet-smb2.h
@@ -40,7 +40,7 @@ typedef struct _smb2_eo_file_info_t {
 typedef struct _smb2_fid_info_t {
 	guint64 fid_persistent;
 	guint64 fid_volatile;
-	guint64 sesid;
+	guint64 sesid;		/* *host* byte order - not necessarily little-endian! */
 	guint32 tid;
 	char *name;
 } smb2_fid_info_t;
@@ -74,7 +74,7 @@ typedef struct _smb2_tid_info_t {
 } smb2_tid_info_t;
 
 typedef struct _smb2_sesid_info_t {
-	guint64 sesid;
+	guint64 sesid;		/* *host* byte order - not necessarily little-endian! */
 	guint32 auth_frame;
 	char *acct_name;
 	char *domain_name;
@@ -169,7 +169,7 @@ typedef struct _smb2_info_t {
 	guint32 ioctl_function;
 	guint32 status;
 	guint32 tid;
-	guint64 sesid;
+	guint64 sesid;		/* *host* byte order - not necessarily little-endian! */
 	guint64  msg_id;
 	guint32 flags;
 	smb2_eo_file_info_t	*eo_file_info; /* eo_smb extra info */
@@ -187,7 +187,7 @@ typedef struct _smb2_transform_info_t {
 	guint8  nonce[16];
 	guint32 size;
 	guint16 alg;
-	guint64 sesid;
+	guint64 sesid;		/* *host* byte order - not necessarily little-endian! */
 	smb2_conv_info_t *conv;
 	smb2_sesid_info_t *session;
 } smb2_transform_info_t;
