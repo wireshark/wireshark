wireshark/wireshark

commit 0db39ae59aaefc13a38ec4e7728da44a647b1a10

smb2: add support for decompression
The latest iteration of Microsoft updates to SMB3 added compression to
the protocol. This commit implements decompressing and dissecting
compressed payloads.

The compression algorithms that can be used are "Plain LZ77",
"LZ77+Huffman" and "LZNT1" which you can read more about in the
[MS-XCA] documentation. This set of algorithm is sometimes referred to
as XPRESS.

This commit reuses the existing uncompression API scheme already in
place with zlib and brotli and adds 3 tvb_uncompress_*() function
implemented in:
* epan/tvbuff_lz77.c
* epan/tvbuff_lz77huff.c
* epan/tvbuff_lznt1.c

A new function wmem_array_try_index() was added to the wmem_array API
to make bound checked reads that fail gracefully. New tests for it
have been added as well.

Since both reads (tvb) and writes (wmem_array) are bound checked the
risk for buffer overruns is drastically reduced. LZ77+Huffman has
decoding tables and special care was taken to bound check these.

Simplified versions of the implementations were succesfully tested
against AFL (American Fuzzy Lop) for ~150 millions executions each.

The SMB2/3 dissector was changed to deal with the new transform header
for compressed packets (new protocol_id value) and READ request
flags (COMPRESSED). Badly compressed or encrypted packets are now
reported as such, and the decryption test suite was changed to reflect
that.

This commit also adds a test capture with 1 packet compressed with
each algorithm as returned by Windows Server 2019, along with 3
matching tests in test/suite_dissection.py

Change-Id: I2b84f56541f2f4ee7d886152794b993987dd10e7
Reviewed-on: https://code.wireshark.org/review/33855
Petri-Dish: Anders Broman <a.broman58@gmail.com>
Tested-by: Petri Dish Buildbot
Reviewed-by: Peter Wu <peter@lekensteyn.nl>

epan/tvbuff_lz77huff.c
@@ -1,0 +1,415 @@
+/*
+ * Decompression code for LZ77+Huffman. This encoding is used by
+ * Microsoft in various file formats and protocols including SMB3.
+ *
+ * Initial code from Samba re-licensed with Samuel's permission.
+ * Copyright (C) Samuel Cabrero 2017
+ *
+ * Glib-ification, extra error-checking and WS integration
+ * Copyright (C) Aurélien Aptel 2019
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <glib.h>
+#include <stdlib.h> /* qsort */
+#include <epan/exceptions.h>
+#include <epan/tvbuff.h>
+#include <epan/wmem/wmem.h>
+
+#define MAX_INPUT_SIZE (16*1024*1024) /* 16MB */
+
+#define TREE_SIZE 1024
+#define ENCODED_TREE_SIZE 256
+
+struct input {
+	tvbuff_t *tvb;
+	int offset;
+	gsize size;
+};
+
+/**
+ * Represents a node in a Huffman prefix code tree
+ */
+struct prefix_code_node {
+	/* Stores the symbol encoded by this node in the prefix code tree */
+	guint16 symbol;
+
+	/* Indicates whether this node is a leaf in the tree */
+	gboolean leaf;
+
+	/* Points to the node’s two children. The value NIL is used to
+	 * indicate that a particular child does not exist */
+	struct prefix_code_node *child[2];
+};
+
+/**
+ * Represent information about a Huffman-encoded symbol
+ */
+struct prefix_code_symbol {
+	/* Stores the symbol */
+	guint16 symbol;
+
+	/* Stores the symbol’s Huffman prefix code length */
+	guint16 length;
+};
+
+/**
+ * Represent a byte array as a bit string from which individual bits can
+ * be read
+ */
+struct bitstring {
+	/* The byte array */
+	const struct input *input;
+
+	/* The index in source from which the next set of bits will be pulled
+         * when the bits in mask have been consumed */
+	guint32 index;
+
+	/* Stores the next bits to be consumed in the bit string */
+	guint32 mask;
+
+	/* Stores the number of bits in mask that remain to be consumed */
+	gint32 bits;
+};
+
+struct hf_tree {
+	struct prefix_code_node nodes[TREE_SIZE];
+	struct prefix_code_node *root;
+};
+
+static gboolean is_node_valid(struct hf_tree *tree, struct prefix_code_node *node)
+{
+        return (node && node >= tree->nodes && node < tree->nodes + TREE_SIZE);
+}
+
+/**
+ * Links a symbol's prefix_code_node into its correct position in a Huffman
+ * prefix code tree
+ */
+static int prefix_code_tree_add_leaf(struct hf_tree *tree,
+				     guint32 leaf_index,
+				     guint32 mask,
+				     guint32 bits,
+				     guint32 *out_index)
+{
+	struct prefix_code_node *node = &tree->nodes[0];
+	guint32 i = leaf_index + 1;
+	guint32 child_index;
+
+	if (leaf_index >= TREE_SIZE)
+		return -1;
+
+	while (bits > 1) {
+		bits = bits - 1;
+		child_index = (mask >> bits) & 1;
+		if (node->child[child_index] == NULL) {
+			if (i >= TREE_SIZE)
+				return -1;
+			node->child[child_index] = &tree->nodes[i];
+			tree->nodes[i].leaf = FALSE;
+			i = i + 1;
+		}
+		node = node->child[child_index];
+		if (!is_node_valid(tree, node))
+			return -1;
+	}
+
+	node->child[mask & 1] = &tree->nodes[leaf_index];
+
+	*out_index = i;
+	return 0;
+}
+
+/**
+ * Determines the sort order of one prefix_code_symbol relative to another
+ */
+static int compare_symbols(const void *ve1, const void *ve2)
+{
+	const struct prefix_code_symbol *e1 = (const struct prefix_code_symbol *)ve1;
+	const struct prefix_code_symbol *e2 = (const struct prefix_code_symbol *)ve2;
+
+	if (e1->length < e2->length)
+		return -1;
+	else if (e1->length > e2->length)
+		return 1;
+	else if (e1->symbol < e2->symbol)
+		return -1;
+	else if (e1->symbol > e2->symbol)
+		return 1;
+	else
+		return 0;
+}
+
+/**
+ * Rebuilds the Huffman prefix code tree that will be used to decode symbols
+ * during decompression
+ */
+static int PrefixCodeTreeRebuild( struct hf_tree *tree,
+				 const struct input *input)
+{
+	struct prefix_code_symbol symbolInfo[512];
+	guint32 i, j, mask, bits;
+	int rc;
+
+	for (i = 0; i < TREE_SIZE; i++) {
+		tree->nodes[i].symbol = 0;
+		tree->nodes[i].leaf = FALSE;
+		tree->nodes[i].child[0] = NULL;
+		tree->nodes[i].child[1] = NULL;
+	}
+
+	if (input->size < ENCODED_TREE_SIZE)
+		return FALSE;
+
+	for (i = 0; i < ENCODED_TREE_SIZE; i++) {
+		symbolInfo[2*i].symbol = 2*i;
+		symbolInfo[2*i].length = tvb_get_guint8(input->tvb, input->offset+i) & 15;
+		symbolInfo[2*i+1].symbol = 2*i+1;
+		symbolInfo[2*i+1].length = tvb_get_guint8(input->tvb, input->offset+i) >> 4;
+	}
+
+	qsort(symbolInfo, 512, sizeof(symbolInfo[0]), compare_symbols);
+
+	i = 0;
+	while (i < 512 && symbolInfo[i].length == 0) {
+		i = i + 1;
+	}
+
+	mask = 0;
+	bits = 1;
+
+	tree->root = &tree->nodes[0];
+	tree->root->leaf = FALSE;
+
+	j = 1;
+	for (; i < 512; i++) {
+		tree->nodes[j].symbol = symbolInfo[i].symbol;
+		tree->nodes[j].leaf = TRUE;
+		mask <<= symbolInfo[i].length - bits;
+		bits = symbolInfo[i].length;
+		rc = prefix_code_tree_add_leaf(tree, j, mask, bits, &j);
+		if (rc)
+			return rc;
+		mask += 1;
+	}
+
+	return 0;
+}
+
+/**
+ * Initializes a bitstream data structure
+ */
+static void bitstring_init(struct bitstring *bstr,
+			   const struct input *input,
+			   guint32 index)
+{
+	bstr->mask = tvb_get_letohs(input->tvb, input->offset+index);
+	bstr->mask <<= sizeof(bstr->mask) * 8 - 16;
+	index += 2;
+
+	bstr->mask += tvb_get_letohs(input->tvb, input->offset+index);
+	index += 2;
+
+	bstr->bits = 32;
+	bstr->input = input;
+	bstr->index = index;
+}
+
+/**
+ * Returns the next n bits from the front of a bit string.
+ */
+static guint32 bitstring_lookup(struct bitstring *bstr, guint32 n)
+{
+	if (n == 0 || bstr->bits < 0 || n > (guint32)bstr->bits) {
+		return 0;
+	}
+	return bstr->mask >> (sizeof(bstr->mask) * 8 - n);
+}
+
+/**
+ * Advances the bit string's cursor by n bits.
+ */
+static void bitstring_skip(struct bitstring *bstr, guint32 n)
+{
+	bstr->mask = bstr->mask << n;
+	bstr->bits = bstr->bits - n;
+
+	if (bstr->bits < 16) {
+		bstr->mask += tvb_get_letohs(bstr->input->tvb,
+					     bstr->input->offset + bstr->index)
+			<< (16 - bstr->bits);
+		bstr->index = bstr->index + 2;
+		bstr->bits = bstr->bits + 16;
+	}
+}
+
+/**
+ * Returns the symbol encoded by the next prefix code in a bit string.
+ */
+static int prefix_code_tree_decode_symbol(struct hf_tree *tree,
+					  struct bitstring *bstr,
+					  guint32 *out_symbol)
+{
+	guint32 bit;
+	struct prefix_code_node *node = tree->root;
+
+	do {
+		bit = bitstring_lookup(bstr, 1);
+		bitstring_skip(bstr, 1);
+		node = node->child[bit];
+		if (!is_node_valid(tree, node))
+			return -1;
+	} while (node->leaf == FALSE);
+
+	*out_symbol = node->symbol;
+	return 0;
+}
+
+static gboolean do_uncompress(struct input *input,
+			      wmem_array_t *obuf)
+{
+	guint32 symbol;
+	guint32 length;
+	gint32 match_offset;
+	int rc;
+	struct hf_tree tree = {0};
+	struct bitstring bstr = {0};
+
+	if (!input->tvb)
+		return FALSE;
+
+	if (input->size > MAX_INPUT_SIZE)
+		return FALSE;
+
+	rc = PrefixCodeTreeRebuild(&tree, input);
+	if (rc)
+		return FALSE;
+
+	bitstring_init(&bstr, input, ENCODED_TREE_SIZE);
+
+	while (1) {
+		rc = prefix_code_tree_decode_symbol(&tree, &bstr, &symbol);
+		if (rc < 0)
+			return FALSE;
+
+		if (symbol < 256) {
+			guint8 v = symbol & 0xFF;
+			wmem_array_append_one(obuf, v);
+		} else {
+			if (symbol == 256) {
+				/* EOF symbol */
+				return bstr.index == bstr.input->size;
+			}
+			symbol = symbol - 256;
+			length = symbol & 0xF;
+			symbol = symbol >> 4;
+
+			match_offset = (1U << symbol) + bitstring_lookup(&bstr, symbol);
+			match_offset *= -1;
+
+			if (length == 15) {
+				if (bstr.index >= bstr.input->size)
+					return FALSE;
+				length = tvb_get_guint8(bstr.input->tvb,
+							bstr.input->offset+bstr.index) + 15;
+				bstr.index += 1;
+
+				if (length == 270) {
+					if (bstr.index+1 >= bstr.input->size)
+						return FALSE;
+					length = tvb_get_letohs(bstr.input->tvb, bstr.input->offset+bstr.index);
+					bstr.index += 2;
+				}
+			}
+
+			bitstring_skip(&bstr, symbol);
+
+			length += 3;
+			do {
+				guint8 byte;
+				int index = wmem_array_get_count(obuf)+match_offset;
+
+				if (index < 0)
+					return FALSE;
+				if (wmem_array_try_index(obuf, index, &byte))
+					return FALSE;
+				wmem_array_append_one(obuf, byte);
+				length--;
+			} while (length != 0);
+		}
+	}
+	return TRUE;
+}
+
+tvbuff_t *
+tvb_uncompress_lz77huff(tvbuff_t *tvb,
+			const int offset,
+			int input_size)
+{
+	volatile gboolean ok;
+	wmem_allocator_t *pool;
+	wmem_array_t *obuf;
+	tvbuff_t *out;
+	struct input input = {
+			      .tvb = tvb,
+			      .offset = offset,
+			      .size = input_size
+	};
+
+	pool = wmem_allocator_new(WMEM_ALLOCATOR_SIMPLE);
+	obuf = wmem_array_sized_new(pool, 1, input_size*2);
+
+	TRY {
+		ok = do_uncompress(&input, obuf);
+	} CATCH_ALL {
+		ok = FALSE;
+	}
+	ENDTRY;
+
+	if (ok) {
+		/*
+		 * Cannot pass a tvb free callback that frees the wmem
+		 * pool, so we make an make an extra copy that uses
+		 * bare pointers. This could be optimized if tvb API
+		 * had a free pool callback of some sort.
+		 */
+		guint size = wmem_array_get_count(obuf);
+		guint8 *p = (guint8 *)g_malloc(size);
+		memcpy(p, wmem_array_get_raw(obuf), size);
+		out = tvb_new_real_data(p, size, size);
+		tvb_set_free_cb(out, g_free);
+	} else {
+		out = NULL;
+	}
+
+	wmem_destroy_allocator(pool);
+
+	return out;
+}
+
+tvbuff_t *
+tvb_child_uncompress_lz77huff(tvbuff_t *parent, tvbuff_t *tvb, const int offset, int in_size)
+{
+	tvbuff_t *new_tvb = tvb_uncompress_lz77huff(tvb, offset, in_size);
+	if (new_tvb)
+		tvb_set_child_real_data_tvbuff(parent, new_tvb);
+	return new_tvb;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */

debian/libwireshark0.symbols
@@ -1688,6 +1688,9 @@ libwireshark.so.0 libwireshark0 #MINVER#
  tvb_captured_length_remaining@Base 1.12.0~rc1
  tvb_child_uncompress@Base 1.12.0~rc1
  tvb_child_uncompress_brotli@Base 3.1.0
+ tvb_child_uncompress_lz77@Base 3.1.0
+ tvb_child_uncompress_lz77huff@Base 3.1.0
+ tvb_child_uncompress_lznt1@Base 3.1.0
  tvb_clone@Base 1.12.0~rc1
  tvb_clone_offset_len@Base 1.12.0~rc1
  tvb_composite_append@Base 1.9.1
@@ -1810,6 +1813,9 @@ libwireshark.so.0 libwireshark0 #MINVER#
  tvb_strsize@Base 1.9.1
  tvb_uncompress@Base 1.9.1
  tvb_uncompress_brotli@Base 3.1.0
+ tvb_uncompress_lz77@Base 3.1.0
+ tvb_uncompress_lz77huff@Base 3.1.0
+ tvb_uncompress_lznt1@Base 3.1.0
  tvb_unicode_strsize@Base 1.9.1
  tvb_ws_mempbrk_pattern_guint8@Base 1.99.3
  tvbparse_casestring@Base 1.9.1
@@ -1959,6 +1965,7 @@ libwireshark.so.0 libwireshark0 #MINVER#
  wmem_array_set_null_terminator@Base 2.1.0
  wmem_array_sized_new@Base 1.12.0~rc1
  wmem_array_sort@Base 1.12.0~rc1
+ wmem_array_try_index@Base 3.1.0
  wmem_ascii_strdown@Base 1.12.0~rc1
  wmem_cleanup@Base 1.12.0~rc1
  wmem_destroy_allocator@Base 1.9.1

epan/CMakeLists.txt
@@ -270,6 +270,9 @@ set(LIBWIRESHARK_NONGENERATED_FILES
 	tvbuff_real.c
 	tvbuff_subset.c
 	tvbuff_zlib.c
+	tvbuff_lz77.c
+	tvbuff_lz77huff.c
+	tvbuff_lznt1.c
 	uat.c
 	value_string.c
 	unit_strings.c

commit 22e74ec018ffd42fd02c68bc3587a53362efe078

tvb_lz77huff: reduce stack memory usage
clang errs about the stack frame size being too big.

* use indexes instead of pointers
* use guint8 instead of gboolean (== int)
* remplace hard coded 512 with #define

Change-Id: Idae70677f20e88ac8bba93899573e0137b9a3d7e
Fixes: 0db39ae59a ("smb2: add support for decompression")
Reviewed-on: https://code.wireshark.org/review/33965
Petri-Dish: Peter Wu <peter@lekensteyn.nl>
Tested-by: Petri Dish Buildbot
Reviewed-by: Peter Wu <peter@lekensteyn.nl>

epan/tvbuff_lz77huff.c
@@ -25,6 +25,7 @@
 
 #define TREE_SIZE 1024
 #define ENCODED_TREE_SIZE 256
+#define SYMBOL_INFO_SIZE (2*ENCODED_TREE_SIZE)
 
 struct input {
 	tvbuff_t *tvb;
@@ -40,11 +41,14 @@ struct prefix_code_node {
 	guint16 symbol;
 
 	/* Indicates whether this node is a leaf in the tree */
-	gboolean leaf;
+	guint8 leaf;
 
-	/* Points to the node’s two children. The value NIL is used to
-	 * indicate that a particular child does not exist */
-	struct prefix_code_node *child[2];
+	/*
+	 * Points to the node's two children. Values are indexes in
+	 * the tree node array. The value -1 is used to indicate that
+	 * a particular child does not exist
+	 */
+	gint16 child[2];
 };
 
 /**
@@ -107,19 +111,19 @@ static int prefix_code_tree_add_leaf(struct hf_tree *tree,
 	while (bits > 1) {
 		bits = bits - 1;
 		child_index = (mask >> bits) & 1;
-		if (node->child[child_index] == NULL) {
+		if (node->child[child_index] < 0) {
 			if (i >= TREE_SIZE)
 				return -1;
-			node->child[child_index] = &tree->nodes[i];
+			node->child[child_index] = i;
 			tree->nodes[i].leaf = FALSE;
 			i = i + 1;
 		}
-		node = node->child[child_index];
+		node = tree->nodes + node->child[child_index];
 		if (!is_node_valid(tree, node))
 			return -1;
 	}
 
-	node->child[mask & 1] = &tree->nodes[leaf_index];
+	node->child[mask & 1] = leaf_index;
 
 	*out_index = i;
 	return 0;
@@ -152,15 +156,15 @@ static int compare_symbols(const void *ve1, const void *ve2)
 static int PrefixCodeTreeRebuild( struct hf_tree *tree,
 				 const struct input *input)
 {
-	struct prefix_code_symbol symbolInfo[512];
+	struct prefix_code_symbol symbolInfo[SYMBOL_INFO_SIZE];
 	guint32 i, j, mask, bits;
 	int rc;
 
 	for (i = 0; i < TREE_SIZE; i++) {
 		tree->nodes[i].symbol = 0;
 		tree->nodes[i].leaf = FALSE;
-		tree->nodes[i].child[0] = NULL;
-		tree->nodes[i].child[1] = NULL;
+		tree->nodes[i].child[0] = -1;
+		tree->nodes[i].child[1] = -1;
 	}
 
 	if (input->size < ENCODED_TREE_SIZE)
@@ -173,10 +177,10 @@ static int PrefixCodeTreeRebuild( struct hf_tree *tree,
 		symbolInfo[2*i+1].length = tvb_get_guint8(input->tvb, input->offset+i) >> 4;
 	}
 
-	qsort(symbolInfo, 512, sizeof(symbolInfo[0]), compare_symbols);
+	qsort(symbolInfo, SYMBOL_INFO_SIZE, sizeof(symbolInfo[0]), compare_symbols);
 
 	i = 0;
-	while (i < 512 && symbolInfo[i].length == 0) {
+	while (i < SYMBOL_INFO_SIZE && symbolInfo[i].length == 0) {
 		i = i + 1;
 	}
 
@@ -261,7 +265,7 @@ static int prefix_code_tree_decode_symbol(struct hf_tree *tree,
 	do {
 		bit = bitstring_lookup(bstr, 1);
 		bitstring_skip(bstr, 1);
-		node = node->child[bit];
+		node = tree->nodes + node->child[bit];
 		if (!is_node_valid(tree, node))
 			return -1;
 	} while (node->leaf == FALSE);