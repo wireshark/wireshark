b69f032d03909c800e80f4aa1fbba079e3128f0f
Dario Lombardo <lomato@gmail.com>
2019-08-05 23:27:09 +0200

    tools: add libnl to rpm-setup.
    Add libnl-devel on RHEL.
    
    Change-Id: I82a147ee6c67292d8e2571f91545ae5af5a5e8ff
    Reviewed-on: https://code.wireshark.org/review/34191
    Reviewed-by: Alexis La Goutte <alexis.lagoutte@gmail.com>
    Reviewed-by: Dario Lombardo <lomato@gmail.com>

--- tools/rpm-setup.sh
+++ tools/rpm-setup.sh
@@ -50,8 +50,7 @@ BASIC_LIST="cmake \
 	libpcap-devel \
 	zlib-devel"
 
-ADDITIONAL_LIST="libnl3-devel \
-	libcap-devel \
+ADDITIONAL_LIST="libcap-devel \
 	libgcrypt-devel \
 	libssh-devel \
 	krb5-devel \
@@ -218,6 +217,9 @@ echo "git-review is unavailabe" >&2
 
 add_package ADDITIONAL_LIST speexdsp-devel || add_package ADDITIONAL_LIST speex-devel ||
 echo "speex is unavailable" >&2
+
+add_package ADDITIONAL_LIST libnl3-devel || add_package ADDITIONAL_LIST libnl-devel ||
+echo "libnl3/libnl are unavailable" >&2
 
 ACTUAL_LIST=$BASIC_LIST
 
