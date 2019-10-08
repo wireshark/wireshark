3d7cd2ed6b6ee7cc5f663ccd551df44c28cf9fa6
Alexis La Goutte <alexis.lagoutte@gmail.com>
2019-10-08 19:39:57 +0000

    amp: remove duplicate copyright
    Only need SPDX-License-Identifier
    
    Change-Id: Ib7bae961acb5810d99a3e44d4f5a7fe437bb6133
    Reviewed-on: https://code.wireshark.org/review/34748
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- epan/dissectors/packet-amp.c
+++ epan/dissectors/packet-amp.c
@@ -7,20 +7,6 @@
  * By Gerald Combs <gerald@wireshark.org>
  * Copyright 1998 Gerald Combs
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You can obtain a version of the GNU General Public License by writing
- * to the Free Software Foundation, Inc., 51 Franklin Street,
- * Fifth Floor, Boston, MA 02110-1301 USA.
- *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 #include "config.h"
