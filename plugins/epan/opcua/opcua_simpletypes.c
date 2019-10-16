d3803fd816385a29bf0f2f0dcf0c73f3a0651b7f
Rainer Keller <Rainer.Keller@qt.io>
2019-10-16 14:42:02 +0200

    Opcua: Fix display of GUIDs
    GUIDs of the OPC UA protocol are always encoded in little endian format.
    Using the default RFC4122 decoder assumes big endian and results in displaying
    wrong values.
    
    Change-Id: I0c2f60ca2f1c0ae118e8388ec58291dc0d1222b9
    Reviewed-on: https://code.wireshark.org/review/34790
    Petri-Dish: Jaap Keuter <jaap.keuter@xs4all.nl>
    Reviewed-by: Graham Bloice <graham.bloice@trihedral.com>
    (cherry picked from commit df1c73d68f044c6ad19432253b5d62fa990e9be4)
    Reviewed-on: https://code.wireshark.org/review/34815
    Petri-Dish: Graham Bloice <graham.bloice@trihedral.com>
    Tested-by: Petri Dish Buildbot

--- plugins/epan/opcua/opcua_simpletypes.c
+++ plugins/epan/opcua/opcua_simpletypes.c
@@ -725,7 +725,7 @@ void parseLocalizedText(proto_tree *tree, tvbuff_t *tvb, packet_info *pinfo, gin
 
 proto_item* parseGuid(proto_tree *tree, tvbuff_t *tvb, packet_info *pinfo _U_, gint *pOffset, int hfIndex)
 {
-    proto_item *item = proto_tree_add_item(tree, hfIndex, tvb, *pOffset, GUID_LEN, ENC_NA);
+    proto_item *item = proto_tree_add_item(tree, hfIndex, tvb, *pOffset, GUID_LEN, ENC_LITTLE_ENDIAN);
     *pOffset+=GUID_LEN;
     return item;
 }
