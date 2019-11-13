wireshark/wireshark

commit 3c8c392b9d6001bd5fadca887caf69bdf7ec74ad

Qt: Cleanup push/pop Infos
Currently push pop is propagated by a massive load of signals
which partly are also propagated through parent objects.

This moves the status handling to WiresharkApplication, also
pathlining future moves to move status to different classes or
use additional methods of status information

Change-Id: Ibcb2c98688f1adf40dce1483f336596ef992bb06
Reviewed-on: https://code.wireshark.org/review/35071
Reviewed-by: Gerald Combs <gerald@wireshark.org>
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Roland Knall <rknall@gmail.com>

ui/qt/main_status_bar.cpp
@@ -41,16 +41,6 @@
 
 // XXX - The GTK+ code assigns priorities to these and pushes/pops accordingly.
 
-enum StatusContext {
-    STATUS_CTX_MAIN,
-    STATUS_CTX_FILE,
-    STATUS_CTX_FIELD,
-    STATUS_CTX_BYTE,
-    STATUS_CTX_FILTER,
-    STATUS_CTX_PROGRESS,
-    STATUS_CTX_TEMPORARY
-};
-
 Q_DECLARE_METATYPE(ProfileDialog::ProfileAction)
 
 // If we ever add support for multiple windows this will need to be replaced.
@@ -72,7 +62,7 @@ statusbar_push_temporary_msg(const gchar *msg_format, ...)
     push_msg.vsprintf(msg_format, ap);
     va_end(ap);
 
-    cur_main_status_bar_->pushTemporaryStatus(push_msg);
+    wsApp->pushStatus(WiresharkApplication::TemporaryStatus, push_msg);
 }
 
 /*
@@ -195,7 +185,7 @@ void MainStatusBar::showExpert() {
 void MainStatusBar::captureFileClosing() {
     expert_button_->hide();
     progress_frame_.captureFileClosing();
-    popFieldStatus();
+    popGenericStatus(STATUS_CTX_FIELD);
 }
 
 void MainStatusBar::expertUpdate() {
@@ -241,11 +231,11 @@ void MainStatusBar::expertUpdate() {
 void MainStatusBar::setFileName(CaptureFile &cf)
 {
     if (cf.isValid()) {
-        popFileStatus();
+        popGenericStatus(STATUS_CTX_FILE);
         QString msgtip = QString("%1 (%2)")
                 .arg(cf.capFile()->filename)
                 .arg(file_size_to_qstring(cf.capFile()->f_datalen));
-        pushFileStatus(cf.fileName(), msgtip);
+        pushGenericStatus(STATUS_CTX_FILE, cf.fileName(), msgtip);
     }
 }
 
@@ -271,7 +261,7 @@ void MainStatusBar::selectedFieldChanged(FieldInformation * finfo)
     QString item_info;
 
     if ( ! finfo ) {
-        pushFieldStatus(item_info);
+        pushGenericStatus(STATUS_CTX_FIELD, item_info);
         return;
     }
 
@@ -297,38 +287,7 @@ void MainStatusBar::selectedFieldChanged(FieldInformation * finfo)
         }
     }
 
-    pushFieldStatus(item_info);
-}
-
-void MainStatusBar::pushTemporaryStatus(const QString &message) {
-    info_status_.pushText(message, STATUS_CTX_TEMPORARY);
-}
-
-void MainStatusBar::popTemporaryStatus() {
-    info_status_.popText(STATUS_CTX_TEMPORARY);
-}
-
-void MainStatusBar::pushFileStatus(const QString &message, const QString &messagetip) {
-    info_status_.pushText(message, STATUS_CTX_FILE);
-    info_status_.setToolTip(messagetip);
-    expertUpdate();
-}
-
-void MainStatusBar::popFileStatus() {
-    info_status_.popText(STATUS_CTX_FILE);
-    info_status_.setToolTip(QString());
-}
-
-void MainStatusBar::pushFieldStatus(const QString &message) {
-    if (message.isEmpty()) {
-        popFieldStatus();
-    } else {
-        info_status_.pushText(message, STATUS_CTX_FIELD);
-    }
-}
-
-void MainStatusBar::popFieldStatus() {
-    info_status_.popText(STATUS_CTX_FIELD);
+    pushGenericStatus(STATUS_CTX_FIELD, item_info);
 }
 
 void MainStatusBar::highlightedFieldChanged(FieldInformation * finfo)
@@ -350,65 +309,48 @@ void MainStatusBar::highlightedFieldChanged(FieldInformation * finfo)
                 .arg(finfo->headerInfo().abbreviation);
     }
 
-    pushByteStatus(hint);
+    pushGenericStatus(STATUS_CTX_BYTE, hint);
 }
 
-void MainStatusBar::pushByteStatus(const QString &message)
+void MainStatusBar::pushGenericStatus(StatusContext status, const QString &message, const QString &messagetip)
 {
-    if (message.isEmpty()) {
-        popByteStatus();
-    } else {
-        info_status_.pushText(message, STATUS_CTX_BYTE);
-    }
+    LabelStack * stack = &info_status_;
+
+    if ( status == STATUS_CTX_MAIN )
+        stack = &packet_status_;
+
+    if ( message.isEmpty() && status != STATUS_CTX_FILE  && status != STATUS_CTX_TEMPORARY && status != STATUS_CTX_PROGRESS )
+        popGenericStatus(status);
+    else
+        stack->pushText(message, status);
+
+    stack->setToolTip(messagetip);
+
+    if ( status == STATUS_CTX_FILTER || status == STATUS_CTX_FILE )
+        expertUpdate();
+
+    if ( status == STATUS_CTX_PROGRESS )
+        progress_frame_.showBusy(true, false, NULL);
 }
 
-void MainStatusBar::popByteStatus()
+void MainStatusBar::popGenericStatus(StatusContext status)
 {
-    info_status_.popText(STATUS_CTX_BYTE);
-}
+    LabelStack * stack = &info_status_;
 
-void MainStatusBar::pushFilterStatus(const QString &message) {
-    if (message.isEmpty()) {
-        popFilterStatus();
-    } else {
-        info_status_.pushText(message, STATUS_CTX_FILTER);
-    }
-    expertUpdate();
-}
+    if ( status == STATUS_CTX_MAIN )
+        stack = &packet_status_;
 
-void MainStatusBar::popFilterStatus() {
-    info_status_.popText(STATUS_CTX_FILTER);
-}
+    stack->setToolTip(QString());
 
-void MainStatusBar::pushPacketStatus(const QString &message) {
-    if (message.isEmpty()) {
-        popPacketStatus();
-    } else {
-        packet_status_.pushText(message, STATUS_CTX_MAIN);
-    }
-}
+    stack->popText(status);
 
-void MainStatusBar::popPacketStatus() {
-    packet_status_.popText(STATUS_CTX_MAIN);
+    if ( status == STATUS_CTX_PROGRESS )
+        progress_frame_.hide();
 }
 
 void MainStatusBar::setProfileName()
 {
     profile_status_.setText(tr("Profile: %1").arg(get_profile_name()));
-}
-
-void MainStatusBar::pushBusyStatus(const QString &message, const QString &messagetip)
-{
-    info_status_.pushText(message, STATUS_CTX_PROGRESS);
-    info_status_.setToolTip(messagetip);
-    progress_frame_.showBusy(true, false, NULL);
-}
-
-void MainStatusBar::popBusyStatus()
-{
-    info_status_.popText(STATUS_CTX_PROGRESS);
-    info_status_.setToolTip(QString());
-    progress_frame_.hide();
 }
 
 void MainStatusBar::pushProgressStatus(const QString &message, bool animate, bool terminate_is_stop, gboolean *stop_flag)
@@ -420,12 +362,6 @@ void MainStatusBar::pushProgressStatus(const QString &message, bool animate, boo
 void MainStatusBar::updateProgressStatus(int value)
 {
     progress_frame_.setValue(value);
-}
-
-void MainStatusBar::popProgressStatus()
-{
-    info_status_.popText(STATUS_CTX_PROGRESS);
-    progress_frame_.hide();
 }
 
 void MainStatusBar::selectedFrameChanged(int)
@@ -498,8 +434,6 @@ void MainStatusBar::showCaptureStatistics()
     if (packets_str.isEmpty()) {
         packets_str = tr("No Packets");
     }
-    popPacketStatus();
-    pushPacketStatus(packets_str);
 }
 
 void MainStatusBar::updateCaptureStatistics(capture_session *cap_session)
@@ -713,7 +647,7 @@ void MainStatusBar::captureEventHandler(CaptureEvent ev)
         case CaptureEvent::Finished:
         case CaptureEvent::Failed:
         case CaptureEvent::Stopped:
-            popFileStatus();
+            popGenericStatus(STATUS_CTX_FILE);
             break;
         default:
             break;

ui/qt/address_editor_frame.cpp
@@ -25,6 +25,7 @@
 #include <QKeyEvent>
 
 #include <ui/qt/utils/qt_ui_utils.h>
+#include <ui/qt/wireshark_application.h>
 
 // To do:
 // - Fill in currently resolved address.
@@ -153,7 +154,7 @@ void AddressEditorFrame::on_buttonBox_accepted()
     QString name = ui->nameLineEdit->text();
     if (!cf_add_ip_name_from_string(cap_file_, addr.toUtf8().constData(), name.toUtf8().constData())) {
         QString error_msg = tr("Can't assign %1 to %2").arg(name).arg(addr);
-        emit editAddressStatus(error_msg);
+        wsApp->pushStatus(WiresharkApplication::TemporaryStatus, error_msg);
         ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
         return;
     }

ui/qt/address_editor_frame.h
@@ -33,7 +33,6 @@ public slots:
 
 signals:
     void showNameResolutionPreferences(const QString module_name);
-    void editAddressStatus(const QString &status);
     void redissectPackets();
 
 protected:

ui/qt/column_editor_frame.cpp
@@ -17,6 +17,7 @@
 #include <ui/recent.h>
 
 #include <ui/preference_utils.h>
+#include <ui/qt/wireshark_application.h>
 
 #include "column_editor_frame.h"
 #include <ui_column_editor_frame.h>
@@ -167,11 +168,11 @@ void ColumnEditorFrame::keyPressEvent(QKeyEvent *event)
             if (ui->buttonBox->button(QDialogButtonBox::Ok)->isEnabled()) {
                 on_buttonBox_accepted();
             } else if (ui->fieldsNameLineEdit->syntaxState() == SyntaxLineEdit::Empty) {
-                emit pushFilterSyntaxStatus(tr("Missing fields."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Missing fields."));
             } else if (ui->fieldsNameLineEdit->syntaxState() != SyntaxLineEdit::Valid) {
-                emit pushFilterSyntaxStatus(tr("Invalid fields."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Invalid fields."));
             } else if (ui->occurrenceLineEdit->syntaxState() == SyntaxLineEdit::Invalid) {
-                emit pushFilterSyntaxStatus(tr("Invalid occurrence value."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Invalid occurrence value."));
             }
         }
     }

ui/qt/column_editor_frame.h
@@ -27,7 +27,6 @@ public:
 
 signals:
     void columnEdited();
-    void pushFilterSyntaxStatus(const QString&);
 
 protected:
     virtual void showEvent(QShowEvent *event);

ui/qt/filter_expression_frame.cpp
@@ -15,6 +15,7 @@
 
 #include <ui/qt/models/uat_model.h>
 #include <ui/qt/models/pref_models.h>
+#include <ui/qt/wireshark_application.h>
 
 #include <QPushButton>
 #include <QKeyEvent>
@@ -169,11 +170,11 @@ void FilterExpressionFrame::keyPressEvent(QKeyEvent *event)
             if (ui->buttonBox->button(QDialogButtonBox::Ok)->isEnabled()) {
                 on_buttonBox_accepted();
             } else if (ui->labelLineEdit->text().length() == 0) {
-                emit pushFilterSyntaxStatus(tr("Missing label."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Missing label."));
             } else if (ui->displayFilterLineEdit->syntaxState() == SyntaxLineEdit::Empty) {
-                emit pushFilterSyntaxStatus(tr("Missing filter expression."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Missing filter expression."));
             } else if (ui->displayFilterLineEdit->syntaxState() != SyntaxLineEdit::Valid) {
-                emit pushFilterSyntaxStatus(tr("Invalid filter expression."));
+                wsApp->pushStatus(WiresharkApplication::FilterSyntax, tr("Invalid filter expression."));
             }
         }
     }

ui/qt/filter_expression_frame.h
@@ -30,7 +30,6 @@ public:
 signals:
     void showPreferencesDialog(QString pane_name);
     void filterExpressionsChanged();
-    void pushFilterSyntaxStatus(const QString&);
 
 protected:
     virtual void showEvent(QShowEvent *event);

ui/qt/main_status_bar.h
@@ -40,6 +40,17 @@ public:
     void setFileName(CaptureFile &cf);
 
 protected:
+
+    enum StatusContext {
+        STATUS_CTX_MAIN,
+        STATUS_CTX_FILE,
+        STATUS_CTX_FIELD,
+        STATUS_CTX_BYTE,
+        STATUS_CTX_FILTER,
+        STATUS_CTX_PROGRESS,
+        STATUS_CTX_TEMPORARY
+    };
+
     virtual void changeEvent(QEvent* event);
 
 private:
@@ -58,6 +69,9 @@ private:
 
     void showCaptureStatistics();
 
+    void pushGenericStatus(StatusContext status, const QString &message, const QString &messagetip = QString());
+    void popGenericStatus(StatusContext status);
+
 signals:
     void showExpertInfo();
     void editCaptureComment();
@@ -67,21 +81,8 @@ public slots:
     void setCaptureFile(capture_file *cf);
     void selectedFieldChanged(FieldInformation *);
     void highlightedFieldChanged(FieldInformation *);
-    void pushTemporaryStatus(const QString &message);
-    void popTemporaryStatus();
-    void pushFileStatus(const QString &message, const QString &messagetip = QString());
-    void popFileStatus();
-    void pushFieldStatus(const QString &message);
-    void popFieldStatus();
-    void pushByteStatus(const QString &message);
-    void popByteStatus();
-    void pushFilterStatus(const QString &message);
-    void popFilterStatus();
-    void pushBusyStatus(const QString &message, const QString &messagetip = QString());
-    void popBusyStatus();
     void pushProgressStatus(const QString &message, bool animate, bool terminate_is_stop = false, gboolean *stop_flag = NULL);
     void updateProgressStatus(int value);
-    void popProgressStatus();
     void selectedFrameChanged(int);
 
     void updateCaptureStatistics(capture_session * cap_session);
@@ -90,14 +91,13 @@ public slots:
     void captureEventHandler(CaptureEvent ev);
 
 private slots:
-    void pushPacketStatus(const QString &message);
-    void popPacketStatus();
-
     void toggleBackground(bool enabled);
     void setProfileName();
     void switchToProfile();
     void manageProfile();
     void showProfileMenu(const QPoint &global_pos, Qt::MouseButton button);
+
+    friend WiresharkApplication;
 };
 
 #endif // MAIN_STATUS_BAR_H

commit 14690c8315cf83dcf7f70fd9740ea4aa75712f96

Qt: Fix statusbar messages
Commit 3c8c392 (https://code.wireshark.org/review/c/35071/) introduced
a regression where the messages in the status bar are no longer visible.
This change corrects that.

Change-Id: I23059a5013a65efe73454fc798048630a9e66792
Reviewed-on: https://code.wireshark.org/review/35085
Petri-Dish: Roland Knall <rknall@gmail.com>
Tested-by: Petri Dish Buildbot
Reviewed-by: Roland Knall <rknall@gmail.com>

ui/qt/main_status_bar.cpp
@@ -434,6 +434,9 @@ void MainStatusBar::showCaptureStatistics()
     if (packets_str.isEmpty()) {
         packets_str = tr("No Packets");
     }
+
+    popGenericStatus(STATUS_CTX_MAIN);
+    pushGenericStatus(STATUS_CTX_MAIN, packets_str);
 }
 
 void MainStatusBar::updateCaptureStatistics(capture_session *cap_session)