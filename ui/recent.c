b3e5bac7b7aa4b1b12166026f832a7185ae54e51
Jim Young <jyoung@gsu.edu>
2019-12-03 22:53:40 -0500

    recent.c: cfield might be NULL
    Avoid crash in GUI when using Edit Column widget.
    
    Bug: 16245
    Change-Id: Ic98be3f98144dcfa9796fadd142180e3aaf84a9e
    Reviewed-on: https://code.wireshark.org/review/35301
    Petri-Dish: Jim Young <jim.young.ws@gmail.com>
    Reviewed-by: Jim Young <jim.young.ws@gmail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Craig Jackson <cejackson51@gmail.com>
    Reviewed-by: Gerald Combs <gerald@wireshark.org>
    (cherry picked from commit 1e3db49f054b2ff893d5a8139d3a2308c5ad3ba0)
    Reviewed-on: https://code.wireshark.org/review/35338
    Petri-Dish: Gerald Combs <gerald@wireshark.org>

--- ui/recent.c
+++ ui/recent.c
@@ -1428,7 +1428,9 @@ recent_get_column_width(gint col)
     while (col_l) {
         col_w = (col_width_data *) col_l->data;
         if (col_w->cfmt == cfmt) {
-            if (cfmt != COL_CUSTOM || strcmp (cfield, col_w->cfield) == 0) {
+            if (cfmt != COL_CUSTOM) {
+                return col_w->width;
+            } else if (cfield && strcmp (cfield, col_w->cfield) == 0) {
                 return col_w->width;
             }
         }
@@ -1492,7 +1494,9 @@ recent_get_column_xalign(gint col)
     while (col_l) {
         col_w = (col_width_data *) col_l->data;
         if (col_w->cfmt == cfmt) {
-            if (cfmt != COL_CUSTOM || strcmp (cfield, col_w->cfield) == 0) {
+            if (cfmt != COL_CUSTOM) {
+                return col_w->xalign;
+            } else if (cfield && strcmp (cfield, col_w->cfield) == 0) {
                 return col_w->xalign;
             }
         }
