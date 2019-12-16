e0bb1ddea7db6e1ea8018d048ee3a9a614a13cc1
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2019-12-16 14:50:55 +0200

    TPNCP: Update from downstream
    * Support little-endian systems
    * Improve error reporting
    * Implement some new features in the protocol
    
    Change-Id: I73e07a588c4a028fd0c22e1570adb7957ba2d52f
    Reviewed-on: https://code.wireshark.org/review/35467
    Petri-Dish: Michael Mann <mmann78@netscape.net>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Michael Mann <mmann78@netscape.net>

--- epan/prefs.c
+++ epan/prefs.c
@@ -5160,6 +5160,7 @@ deprecated_port_pref(gchar *pref_name, const gchar *value)
         {"vnc.alternate_port", "VNC", "tcp.port", 10},
         {"scop.port", "SCoP", "tcp.port", 10},
         {"scop.port_secure", "SCoP", "tcp.port", 10},
+        {"tcp.trunkpack_port", "TPNCP", "tcp.port", 10},
         /* UDP */
         {"h248.udp_port", "H248", "udp.port", 10},
         {"actrace.udp_port", "ACtrace", "udp.port", 10},
@@ -5191,6 +5192,7 @@ deprecated_port_pref(gchar *pref_name, const gchar *value)
         {"uaudp.port4", "UAUDP", "udp.port", 10},
         {"uhd.dissector_port", "UHD", "udp.port", 10},
         {"vrt.dissector_port", "vrt", "udp.port", 10},
+        {"udp.trunkpack_port", "TPNCP", "udp.port", 10},
     };
 
     struct port_pref_name port_range_prefs[] = {
