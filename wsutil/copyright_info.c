5ba082d51db4af2d91f10f235739dc137369c70c
Stig Bjørlykke <stig@bjorlykke.org>
2020-01-01 01:00:00 +0100

    Happy New Year 2020
    Change-Id: Ic569e499d40def9397203510a121ff17a495ba9a
    Reviewed-on: https://code.wireshark.org/review/35616
    Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>

--- wsutil/copyright_info.c
+++ wsutil/copyright_info.c
@@ -19,7 +19,7 @@ const char *
 get_copyright_info(void)
 {
 	return
-		"Copyright 1998-2019 Gerald Combs <gerald@wireshark.org> and contributors.\n"
+		"Copyright 1998-2020 Gerald Combs <gerald@wireshark.org> and contributors.\n"
 		"License GPLv2+: GNU GPL version 2 or later <https://www.gnu.org/licenses/gpl-2.0.html>\n"
 		"This is free software; see the source for copying conditions. There is NO\n"
 		"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n";
