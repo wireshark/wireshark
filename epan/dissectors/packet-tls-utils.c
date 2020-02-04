9c057e68093049d40788c324b3dfa2e24300faa0
Alexis La Goutte <alexis.lagoutte@gmail.com>
2020-02-04 21:46:55 +0100

    TLS (QUIC): Add min_ack_delay TP
    From https://tools.ietf.org/html/draft-iyengar-quic-delayed-ack-00
    
    And https://github.com/quicwg/base-drafts/wiki/Temporary-IANA-Registry
    
    Ping-Bug: 13881
    Change-Id: I60b262d44b03a0b454016392eb1a01d46ca03d52
    Reviewed-on: https://code.wireshark.org/review/36032
    Reviewed-by: Peter Wu <peter@lekensteyn.nl>
    Petri-Dish: Peter Wu <peter@lekensteyn.nl>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit 8a6000b6dc21393767c35729f3e83f214fde26b5)
    Reviewed-on: https://code.wireshark.org/review/36179

--- epan/dissectors/packet-tls-utils.c
+++ epan/dissectors/packet-tls-utils.c
@@ -1392,6 +1392,7 @@ const value_string quic_transport_parameter_id[] = {
     { SSL_HND_QUIC_TP_ACTIVE_CONNECTION_ID_LIMIT, "active_connection_id_limit" },
     { SSL_HND_QUIC_TP_MAX_DATAGRAM_FRAME_SIZE, "max_datagram_frame_size" },
     { SSL_HND_QUIC_TP_LOSS_BITS, "loss_bits" },
+    { SSL_HND_QUIC_TP_MIN_ACK_DELAY, "min_ack_delay" },
     { 0, NULL }
 };
 
@@ -6787,6 +6788,12 @@ ssl_dissect_hnd_hello_ext_quic_transport_parameters(ssl_common_dissect_t *hf, tv
                 proto_tree_add_item(parameter_tree, hf->hf.hs_ext_quictp_parameter_loss_bits,
                                     tvb, offset, 1, ENC_BIG_ENDIAN);
                 offset += 1;
+            break;
+            case SSL_HND_QUIC_TP_MIN_ACK_DELAY:
+                proto_tree_add_item_ret_varint(parameter_tree, hf->hf.hs_ext_quictp_parameter_min_ack_delay,
+                                               tvb, offset, -1, ENC_VARINT_QUIC, &value, &len);
+                proto_item_append_text(parameter_tree, " %" G_GINT64_MODIFIER "u", value);
+                offset += len;
             break;
             default:
                 offset += parameter_length;
