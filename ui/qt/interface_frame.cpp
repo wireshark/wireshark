wireshark/wireshark

commit 1e4c4f2209f64228bdf54011656bdbbfbcba7e63

Qt: Make it possible to hide welcome screen warnings.
The "The NPF driver isn't running. You may have trouble" warning dialog
is now redundant, so remove it along with the "privs.warn_if_no_npf"
recent setting.

Add a more general "sys.warn_if_no_capture" recent setting along with a
getter for SimpleDialog's "Don't show this message again." string. Use
them to add a "Don't show this..." link to the main welcome warning
label.

Change-Id: Idffb800761eebf04b75e4be3f6bf7727dd468949
Reviewed-on: https://code.wireshark.org/review/36042
Reviewed-by: Gerald Combs <gerald@wireshark.org>
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Tomasz Moń <desowin@gmail.com>

ui/qt/interface_frame.cpp
@@ -18,6 +18,7 @@
 #endif
 
 #include "ui/qt/interface_frame.h"
+#include <ui/qt/simple_dialog.h>
 #include <ui/qt/wireshark_application.h>
 
 #include <ui/qt/models/interface_tree_model.h>
@@ -28,13 +29,16 @@
 
 #include "extcap.h"
 
+#include <ui/recent.h>
 #include <wsutil/utf8_entities.h>
 
+#include <QDesktopServices>
 #include <QFrame>
-#include <QPushButton>
 #include <QHBoxLayout>
-#include <QLabel>
 #include <QItemSelection>
+#include <QLabel>
+#include <QPushButton>
+#include <QUrl>
 
 #include <epan/prefs.h>
 
@@ -42,6 +46,7 @@
 
 #ifdef HAVE_LIBPCAP
 const int stat_update_interval_ = 1000; // ms
+const char *no_capture_link = "#no_capture";
 #endif
 
 InterfaceFrame::InterfaceFrame(QWidget * parent)
@@ -320,8 +325,15 @@ void InterfaceFrame::resetInterfaceTreeDisplay()
         }
     }
 
-    if (!ui->warningLabel->text().isEmpty())
+    // XXX Should we have a separate recent pref for each message?
+    if (!ui->warningLabel->text().isEmpty() && recent.sys_warn_if_no_capture)
     {
+        QString warning_text = ui->warningLabel->text();
+        warning_text.append(QString("<p><a href=\"%1\">%2</a></p>")
+                            .arg(no_capture_link)
+                            .arg(SimpleDialog::dontShowThisAgain()));
+        ui->warningLabel->setText(warning_text);
+
         ui->warningLabel->show();
     }
 #endif // HAVE_LIBPCAP
@@ -472,6 +484,16 @@ void InterfaceFrame::showContextMenu(QPoint pos)
 
     ctx_menu.addAction(tr("Start capture"), this, SIGNAL(startCapture()));
     ctx_menu.exec(ui->interfaceTree->mapToGlobal(pos));
+}
+
+void InterfaceFrame::on_warningLabel_linkActivated(const QString &link)
+{
+    if (link.compare(no_capture_link) == 0) {
+        recent.sys_warn_if_no_capture = FALSE;
+        resetInterfaceTreeDisplay();
+    } else {
+        QDesktopServices::openUrl(QUrl(link));
+    }
 }
 
 /*

ui/qt/interface_frame.h
@@ -93,6 +93,7 @@ private slots:
     void updateStatistics(void);
     void actionButton_toggled(bool checked);
     void triggeredIfTypeButton();
+    void on_warningLabel_linkActivated(const QString &link);
 };
 
 #endif // INTERFACE_FRAME_H

ui/qt/interface_frame.ui
@@ -58,11 +58,8 @@
      <property name="wordWrap">
       <bool>true</bool>
      </property>
-     <property name="openExternalLinks">
-      <bool>true</bool>
-     </property>
      <property name="textInteractionFlags">
-      <set>Qt::LinksAccessibleByMouse|Qt::TextSelectableByMouse</set>
+      <set>Qt::TextBrowserInteraction</set>
      </property>
     </widget>
    </item>

ui/qt/main.cpp
@@ -315,11 +315,8 @@ g_log_message_handler(QtMsgType type, const QMessageLogContext &, const QString 
  *  we pop up will be above the main window.
  */
 static void
-check_and_warn_user_startup(const QString &cf_name)
+check_and_warn_user_startup()
 {
-#ifndef _WIN32
-    Q_UNUSED(cf_name)
-#endif
     gchar               *cur_user, *cur_group;
 
     /* Tell the user not to run as root. */
@@ -335,16 +332,6 @@ check_and_warn_user_startup(const QString &cf_name)
         g_free(cur_user);
         g_free(cur_group);
     }
-
-#ifdef _WIN32
-    /* Warn the user if npf.sys isn't loaded. */
-    if (!get_stdin_capture() && cf_name.isEmpty() && !npf_sys_is_running() && recent.privs_warn_if_no_npf) {
-        simple_message_box(ESD_TYPE_WARN, &recent.privs_warn_if_no_npf, "%s",
-        "The NPF driver isn't running. You may have trouble\n"
-        "capturing or listing interfaces.");
-    }
-#endif
-
 }
 #endif
 
@@ -899,7 +886,7 @@ int main(int argc, char *qt_argv[])
                 g_free(s);
             }
             /* "-k" was specified; start a capture. */
-            check_and_warn_user_startup(cf_name);
+            check_and_warn_user_startup();
 
             /* If no user interfaces were specified on the command line,
                copy the list of selected interfaces to the set of interfaces

ui/qt/simple_dialog.cpp
@@ -138,7 +138,7 @@ simple_message_box(ESD_TYPE_E type, gboolean *notagain,
     if (notagain) {
         cb = new QCheckBox();
         cb->setChecked(true);
-        cb->setText(QObject::tr("Don't show this message again."));
+        cb->setText(SimpleDialog::dontShowThisAgain());
         sd.setCheckBox(cb);
     }
 
@@ -341,6 +341,11 @@ void SimpleDialog::displayQueuedMessages(QWidget *parent)
     max_severity_ = ESD_TYPE_INFO;
 
     mb.exec();
+}
+
+QString SimpleDialog::dontShowThisAgain()
+{
+    return QObject::tr("Don't show this message again.");
 }
 
 int SimpleDialog::exec()

ui/qt/simple_dialog.h
@@ -35,6 +35,7 @@ public:
     ~SimpleDialog();
 
     static void displayQueuedMessages(QWidget *parent = 0);
+    static QString dontShowThisAgain();
     void setDetailedText(QString text) { detailed_text_ = text; }
     void setCheckBox(QCheckBox *cb) { check_box_ = cb; }
     int exec();

ui/recent.c
@@ -70,7 +70,7 @@
 #define RECENT_GUI_GEOMETRY                   "gui.geom."
 
 #define RECENT_KEY_PRIVS_WARN_IF_ELEVATED     "privs.warn_if_elevated"
-#define RECENT_KEY_PRIVS_WARN_IF_NO_NPF       "privs.warn_if_no_npf"
+#define RECENT_KEY_SYS_WARN_IF_NO_CAPTURE     "sys.warn_if_no_capture"
 
 #define RECENT_FILE_NAME "recent"
 #define RECENT_COMMON_FILE_NAME "recent_common"
@@ -744,9 +744,9 @@ write_recent(void)
             RECENT_KEY_PRIVS_WARN_IF_ELEVATED,
             recent.privs_warn_if_elevated);
 
-    write_recent_boolean(rf, "Warn if npf.sys isn't loaded on Windows >= 6.0",
-            RECENT_KEY_PRIVS_WARN_IF_NO_NPF,
-            recent.privs_warn_if_no_npf);
+    write_recent_boolean(rf, "Warn if Wireshark is unable to capture",
+            RECENT_KEY_SYS_WARN_IF_NO_CAPTURE,
+            recent.sys_warn_if_no_capture);
 
     write_recent_enum(rf, "Find packet search in", RECENT_GUI_SEARCH_IN, search_in_values,
                       recent.gui_search_in);
@@ -1011,8 +1011,8 @@ read_set_recent_common_pair_static(gchar *key, const gchar *value,
         }
     } else if (strcmp(key, RECENT_KEY_PRIVS_WARN_IF_ELEVATED) == 0) {
         parse_recent_boolean(value, &recent.privs_warn_if_elevated);
-    } else if (strcmp(key, RECENT_KEY_PRIVS_WARN_IF_NO_NPF) == 0) {
-        parse_recent_boolean(value, &recent.privs_warn_if_no_npf);
+    } else if (strcmp(key, RECENT_KEY_SYS_WARN_IF_NO_CAPTURE) == 0) {
+        parse_recent_boolean(value, &recent.sys_warn_if_no_capture);
     } else if (strcmp(key, RECENT_GUI_SEARCH_IN) == 0) {
         recent.gui_search_in = (search_in_type)str_to_val(value, search_in_values, SEARCH_IN_PACKET_LIST);
     } else if (strcmp(key, RECENT_GUI_SEARCH_CHAR_SET) == 0) {
@@ -1278,7 +1278,7 @@ recent_read_static(char **rf_path_return, int *rf_errno_return)
     recent.gui_geometry_wlan_stats_pane   = 200;
 
     recent.privs_warn_if_elevated = TRUE;
-    recent.privs_warn_if_no_npf = TRUE;
+    recent.sys_warn_if_no_capture = TRUE;
 
     recent.col_width_list = NULL;
     recent.gui_fileopen_remembered_dir = NULL;

ui/recent.h
@@ -120,7 +120,7 @@ typedef struct recent_settings_tag {
     gint        gui_geometry_status_pane_right;
     gint        gui_geometry_wlan_stats_pane;
     gboolean    privs_warn_if_elevated;
-    gboolean    privs_warn_if_no_npf;
+    gboolean    sys_warn_if_no_capture;
     GList      *col_width_list;                     /* column widths */
     GList      *conversation_tabs;                  /* enabled conversation dialog tabs */
     GList      *endpoint_tabs;                      /* enabled endpoint dialog tabs */

commit fca18e2e5cfff5fa9775e7866fd43fcb084899d4

Qt: fix compilation without pcap.
Fix a regression introduced in 1e4c4f2209f64228bdf54011656bdbbfbcba7e63
when compiling without pcap.

Change-Id: Ifda13a135285737a459155020134937069ac5d34
Reviewed-on: https://code.wireshark.org/review/36047
Petri-Dish: Gerald Combs <gerald@wireshark.org>
Tested-by: Petri Dish Buildbot
Reviewed-by: Gerald Combs <gerald@wireshark.org>

ui/qt/interface_frame.cpp
@@ -46,8 +46,8 @@
 
 #ifdef HAVE_LIBPCAP
 const int stat_update_interval_ = 1000; // ms
-const char *no_capture_link = "#no_capture";
 #endif
+const char *no_capture_link = "#no_capture";
 
 InterfaceFrame::InterfaceFrame(QWidget * parent)
 : QFrame(parent),