cc8f7726c0184868bf25cdd06e60b68192b03f2d
Guy Harris <guy@alum.mit.edu>
2020-02-26 21:17:11 -0800

    Clean up a comment in the preferences file.
    In the file, the channel name doesn't have UPDATE_CHANNEL_ in front of
    it; it's just DEVELOPMENT or STABLE.
    
    Change-Id: I11a8e10b3c0ee6c4eec2568bf94d42c4098b82a5
    Reviewed-on: https://code.wireshark.org/review/36207
    Reviewed-by: Guy Harris <guy@alum.mit.edu>
    (backported from commit e1e8e2dce97076941c8b48e8a5d06f46e3009b6f)
    Reviewed-on: https://code.wireshark.org/review/36209

--- epan/prefs.c
+++ epan/prefs.c
@@ -3278,7 +3278,7 @@ prefs_register_modules(void)
 
     prefs_register_enum_preference(gui_module, "update.channel",
                        "Update channel",
-                       "The type of update to fetch. You should probably leave this set to UPDATE_CHANNEL_STABLE.",
+                       "The type of update to fetch. You should probably leave this set to STABLE.",
                        (gint*)(void*)(&prefs.gui_update_channel), gui_update_channel, FALSE);
 
     prefs_register_uint_preference(gui_module, "update.interval",
