ed89fadd3b1d5d169084187ddd37b888845b0c4b
Martin Mathieson <martin.mathieson@keysight.com>
2020-04-06 21:15:12 +0100

    ISUP: Fix inconsistency within range_string definition.
    Educated guess on the part of Jeff Morriss as don't have
    ready access to appropriate spec.
    
    Change-Id: Ib6b7ed5911d3c219c61c43d41369af1e9e51d10c
    Reviewed-on: https://code.wireshark.org/review/36728
    Petri-Dish: Martin Mathieson <martin.r.mathieson@googlemail.com>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Martin Mathieson <martin.r.mathieson@googlemail.com>

--- epan/dissectors/packet-isup.c
+++ epan/dissectors/packet-isup.c
@@ -7530,8 +7530,9 @@ dissect_japan_isup_carrier_information(tvbuff_t *parameter_tvb, packet_info *pin
 
 static const range_string japan_isup_charge_delay_type_value[] = {
   {  0,    0,    "Spare" },
-  {  1,    0xfc, "Reserved for network specific use" },
-  {  0x81, 0xfa, "Spare" },                         /* TODO: overlaps with previous, need to check (non-free) spec */
+  {  1,    0x80, "Reserved for network specific use" },
+  {  0x81, 0xfa, "Spare" },
+  {  0xfb, 0xfc, "Reserved" },
   {  0xfd, 0xfd, "Charge rate transfer" },
   {  0xfe, 0xfe, "Terminating charge area information" },
   {  0xff, 0xff, "Spare" },
