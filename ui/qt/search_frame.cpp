12f50e78ce2ebb8d988e892d453fa253047fe450
Stig Bjørlykke <stig@bjorlykke.org>
2020-08-05 15:50:50 +0200

    Qt: Pop filter syntax in search frame
    Pop the filter syntax message in search frame when changing search type
    and when hiding the widget to avoid having outdated status messages.
    
    Change-Id: I87c63c070621cff0d5ecebc2fcd41f9d7c02adec
    Reviewed-on: https://code.wireshark.org/review/38051
    Petri-Dish: Stig Bjørlykke <stig@bjorlykke.org>
    Tested-by: Petri Dish Buildbot
    Reviewed-by: Martin Mathieson <martin.r.mathieson@googlemail.com>
    Reviewed-by: Anders Broman <a.broman58@gmail.com>
    (cherry picked from commit f54611d1104d85a425e52c7318c522ed249916b6)
    Reviewed-on: https://code.wireshark.org/review/38053
    Reviewed-by: Stig Bjørlykke <stig@bjorlykke.org>

--- ui/qt/search_frame.cpp
+++ ui/qt/search_frame.cpp
@@ -338,6 +338,7 @@ void SearchFrame::on_searchTypeComboBox_currentIndexChanged(int idx)
         break;
     }
 
+    wsApp->popStatus(WiresharkApplication::FilterSyntax);
     updateWidgets();
 }
 
@@ -502,6 +503,7 @@ void SearchFrame::on_findButton_clicked()
 
 void SearchFrame::on_cancelButton_clicked()
 {
+    wsApp->popStatus(WiresharkApplication::FilterSyntax);
     animatedHide();
 }
 
