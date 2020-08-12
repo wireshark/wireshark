8cda1eb09a58aed4edffb49096856eb21b35aba4
Gerald Combs <gerald@wireshark.org>
2020-08-12 13:02:57 -0700

    2.6.19 → 2.6.20.
    Change-Id: I63626b7f2acb80097c69724c08e067be55189144
    Reviewed-on: https://code.wireshark.org/review/38149
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

--- make-version.pl
+++ make-version.pl
@@ -73,7 +73,7 @@ my $set_release = 0;
 my %version_pref = (
 	"version_major" => 2,
 	"version_minor" => 6,
-	"version_micro" => 19,
+	"version_micro" => 20,
 	"version_build" => 0,
 
 	"enable"        => 1,
