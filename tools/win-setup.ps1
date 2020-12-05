f506d971b82cfcecc4a4f615c6645592682d9ddb
Gerald Combs <gerald@wireshark.org>
2020-12-05 10:36:15 -0800

    Windows: Upgrade c-ares and nghttp2.
    Upgrade c-ares to 1.17.1 and nghttp2 to 1.42.0.

--- tools/win-setup.ps1
+++ tools/win-setup.ps1
@@ -69,15 +69,15 @@ Param(
 # trouble instead of trying to catch exceptions everywhere.
 $ErrorActionPreference = "Stop"
 
-$Win64CurrentTag = "2020-11-30"
-$Win32CurrentTag = "2020-11-30"
+$Win64CurrentTag = "2020-12-05"
+$Win32CurrentTag = "2020-12-05"
 
 # Archive file / SHA256
 $Win64Archives = @{
     "AirPcap_Devpack_4_1_0_1622.zip" = "09d637f28a79b1d2ecb09f35436271a90c0f69bd0a1ee82b803abaaf63c18a69";
     "bcg729-1.0.4-win64ws.zip" = "9a095fda4c39860d96f0c568830faa6651cd17635f68e27aa6de46c689aa0ee2";
     "brotli-1.0.9-1-win64ws.zip" = "3f8d24aec8668201994327ff8d8542fe507d1d468a500a1aec50d0415f695aab";
-    "c-ares-1.15.0-win64ws.zip" = "3c4bfbbcbfe5e792660cb79769fc2a1996fa9938ec595247f611e7c59b1a62da";
+    "c-ares-1.17.1-1-win64ws.zip" = "7b344f78ac4a0de8e4da64fe7e50c503dcde44138eade1172d6888e02dc39851";
     "gnutls-3.6.3-1-win64ws.zip" = "994ac2578e7b4ca01e589ab2598927d53f7370bc3ff679f3006b0e6bb7a06df4";
     "krb5-1.17-1-win64ws.zip" = "1f4a7ab86ae331ea9e58c9776a60def81ae9fe622882b2e8da2ad6ce6f6fb1d8";
     "libgcrypt-1.8.3-win64ws.zip" = "53b1c636cb89de308ca4ea01b4990cf1deca7f6c2446189c7ff6e971137ffd76";
@@ -89,7 +89,7 @@ $Win64Archives = @{
     "lua-5.2.4-unicode-win64-vc14.zip" = "e8968d2c7871ce1ea82cbd29ac1b3a2c59d3dec25e483c5e12de85df66f5d928";
     "lz4-1.9.2-1-win64ws.zip" = "751c68b81454d0b4411b9306add61847471ad4b84c40fe852d23d3d071a51268";
     "minizip-1.2.11-4-win64ws.zip" = "dd6bf24e2d946465ad19aa4f8c38e0db91da6585887935de68011982cd6fb2cb";
-    "nghttp2-1.39.2-win64ws.zip" = "a53f4074bffd919539d90c8d0cde2ea6a10a383f14e38b706e7e70e55476e6bf";
+    "nghttp2-1.42.0-1-win64ws.zip" = "20164ae2189da68145a5d3ddc2eadcc554f15c6a4254b3d2f622cf3c6d3c32c2";
     "opus-1.3.1-3-win64ws.zip" = "1f7a55a6d2d7215dffa4a43bca8ca05024bd4ba1ac3d0d0c405fd38b09cc2205";
     "sbc-1.3-1-win64ws.zip" = "08cef6898c421277a6582ef3225d8820f74a037cbd5b6e673a4d8f4593ce80a1";
     "snappy-1.1.8-1-win64ws.zip" = "45d496ac98ffd365f2b86707a077498f4ab59cca33f65dcca1f89669a85ee92a";
@@ -103,7 +103,7 @@ $Win32Archives = @{
     "AirPcap_Devpack_4_1_0_1622.zip" = "09d637f28a79b1d2ecb09f35436271a90c0f69bd0a1ee82b803abaaf63c18a69";
     "bcg729-1.0.4-win32ws.zip" = "b785ec78dec6bca8252130eb884bfa28c1140001dd7369a535579176de9e4271";
     "brotli-1.0.9-1-win32ws.zip" = "37ce13b3d41f025b8f6ca962e7fbacca6421d9b3b58f2ebaa81b1262d0a972ba";
-    "c-ares-1.15.0-win32ws.zip" = "ddcdbdcc6c526496ed2083010fd913fb8a565ab726c86dfc57f30e65522f332d";
+    "c-ares-1.17.1-1-win32ws.zip" = "910f816efbded9b4c361f80ec4c9babb2436a063cfdbe8470c84ad97fdd118c7";
     "gnutls-3.6.3-1-win32ws.zip" = "42d8313ffb888f525d6c39330c39bcc2182e68ee8433a09dd85e1f1e1474f592";
     "krb5-1.17-1-win32ws.zip" = "f90cac08355ccfe624652d3e05f8e2e077b8830382315d4ea0a6fa52af08260b";
     "libgcrypt-1.8.3-win32ws.zip" = "409b72f2809019050cca91b9e670047c50a0752ff52999089178da54ef926393";
@@ -115,7 +115,7 @@ $Win32Archives = @{
     "lua-5.2.4-unicode-win32-vc14.zip" = "ca2368a83f623674178e9441f71fb791e3c0b46f208e3dac28c6ac735f034bff";
     "lz4-1.9.2-1-win32ws.zip" = "9bf6398e7f3d81d3db01b27356a9f4a0930573dbf9cc46011a59f89bef3bec69";
     "minizip-1.2.11-4-win32ws.zip" = "41e113930902c2519c4644e8307a0cc51c5855e001e1e69768c48deb376142d0";
-    "nghttp2-1.39.2-win32ws.zip" = "b511260befc210c1d6d0e920e5f5c47b9e6a21baee6e9874ef12a92093abd245";
+    "nghttp2-1.42.0-1-win32ws.zip" = "3b124dd883c6c1c7af2a12c262adec81b1e55ead71d1562f09b795d4e653400a";
     "opus-1.3.1-3-win32ws.zip" = "9700b14c8945fcfed2188b806a2ee7e8628922c22569a4c5183075f3dc133177";
     "sbc-1.3-1-win32ws.zip" = "ad37825e9ace4b849a5442c08f1ed7e30634e6b774bba4307fb86f35f82e71ba";
     "snappy-1.1.8-1-win32ws.zip" = "058c12605d747e805e0fcb310b8fe6efdde5b36d68664bbf54f7ee8fa5fd5adb";
