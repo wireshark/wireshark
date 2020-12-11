beb1877841c463cae9737373b68445ae1fbfb179
Jaap Keuter <jaap.keuter@xs4all.nl>
2020-12-11 11:05:29 +0100

    Qt: Add missing break in Win32 file export init dialog function

--- ui/win32/file_dlg_win32.c
+++ ui/win32/file_dlg_win32.c
@@ -680,6 +680,7 @@ format_handle_wm_initdialog(HWND dlg_hwnd, print_args_t *args) {
             break;
         case print_dissections_expanded:
             SendMessage(cur_ctrl, CB_SETCURSEL, 2, 0);
+            break;
         default:
             g_assert_not_reached();
     }
