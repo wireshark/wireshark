00b86cf64d638e332228ed6433fec82366b14f90
Moshe Kaplan <me@moshekaplan.com>
2020-12-20 08:56:15 -0500

    wslua: Fix crash on FieldInfo.range and FieldInfo.value for a nonexistent TVB
    Fields such as '_ws.expert' have no underlying tvb; they are added
    with offset 0 and length 0 and the field's underlying tvb is NULL. FieldInfo__call
    passes tvb to tvb_memdup() without checking if the tvb is null and
    assumes that a NULL tvb means that the tvb is expired and therefore raises an error:
    "epan/tvbuff.c:477: failed assertion "tvb && tvb->initialized"
    
    Fields such as '_ws.expert.group' have no underlying tvb; they are added
    with offset 0 and length 0 and the field's underlying tvb is NULL. FieldInfo_get_range
    calls push_TvbRange, which assumes that a NULL tvb means that the tvb is expired
    and therefore raises a lua error of "expired tvb".
    
    This commit explicitly adds a check to FieldInfo__call() to see if the tvb is null when
    attempting to access the underlying tvb.
    
    It also explicitly checks if the tvb is null when attempting to access the range
    and if it is, returns nil. This is consistent with how FieldInfo.source also
    returns nil for such fields.
    
    This commit should fix issue #13542.

--- epan/wslua/wslua_field.c
+++ epan/wslua/wslua_field.c
@@ -186,8 +186,11 @@ WSLUA_METAMETHOD FieldInfo__call(lua_State* L) {
             {
                 ByteArray ba = g_byte_array_new();
                 tvbuff_t* tvb = (tvbuff_t *) fvalue_get(&fi->ws_fi->value);
-                g_byte_array_append(ba, (const guint8 *)tvb_memdup(wmem_packet_scope(), tvb, 0,
+                if (tvb != NULL) {
+                    g_byte_array_append(ba, (const guint8 *)tvb_memdup(wmem_packet_scope(), tvb, 0,
                                             tvb_captured_length(tvb)), tvb_captured_length(tvb));
+                }
+
                 pushByteArray(L,ba);
                 return 1;
             }
@@ -297,9 +300,14 @@ static int FieldInfo_get_source(lua_State* L) {
     return 1;
 }
 
-/* WSLUA_ATTRIBUTE FieldInfo_range RO The `TvbRange` covering the bytes of this field in a Tvb. */
+/* WSLUA_ATTRIBUTE FieldInfo_range RO The `TvbRange` covering the bytes of this field in a Tvb or nil if there is none. */
 static int FieldInfo_get_range(lua_State* L) {
     FieldInfo fi = checkFieldInfo(L,1);
+
+    if (!fi->ws_fi->ds_tvb) {
+        lua_pushnil(L);
+        return 1;
+    }
 
     if (push_TvbRange (L, fi->ws_fi->ds_tvb, fi->ws_fi->start, fi->ws_fi->length)) {
         return 1;
