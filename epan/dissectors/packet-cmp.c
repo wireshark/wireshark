77151a28014c779a1b476b242e3acc6d8cb237e4
Pascal Quantin <pascal@wireshark.org>
2021-01-02 13:14:40 +0100

    CMP: regenerate dissector from ASN.1 source

--- epan/dissectors/packet-cmp.c
+++ epan/dissectors/packet-cmp.c
@@ -382,7 +382,7 @@ dissect_cmp_T_infoType(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset 
 
 static int
 dissect_cmp_T_infoValue(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
-#line 52 "./asn1/cmp/cmp.cnf"
+#line 56 "./asn1/cmp/cmp.cnf"
   offset=call_ber_oid_callback(actx->external.direct_reference, tvb, offset, actx->pinfo, tree, NULL);
 
 
@@ -481,7 +481,7 @@ static const value_string cmp_PKIStatus_vals[] = {
 
 static int
 dissect_cmp_PKIStatus(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
-#line 68 "./asn1/cmp/cmp.cnf"
+#line 72 "./asn1/cmp/cmp.cnf"
   guint32 value;
 
     offset = dissect_ber_integer(implicit_tag, actx, tree, tvb, offset, hf_index,
@@ -1069,7 +1069,7 @@ static const ber_choice_t PKIBody_choice[] = {
 
 static int
 dissect_cmp_PKIBody(gboolean implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
-#line 58 "./asn1/cmp/cmp.cnf"
+#line 62 "./asn1/cmp/cmp.cnf"
   gint branch_taken;
 
     offset = dissect_ber_choice(actx, tree, tvb, offset,
