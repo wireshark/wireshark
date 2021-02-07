cd4e55fa033505cce459d3be7b69802cd8e47f9c
Martin Kaiser <wireshark@kaiser.cx>
2021-02-07 18:53:32 +0100

    gsm_a_common: create and populate the stats table only once
    Use the new stat_tap_find_table function during init to check if our
    statistics table already exists.
    
    If so, we can safely assume that its rows have already beend initialized.
    All we have to do is clear the data that was collected by the tap.

--- epan/dissectors/packet-gsm_a_common.c
+++ epan/dissectors/packet-gsm_a_common.c
@@ -3692,6 +3692,14 @@ static void gsm_a_stat_init(stat_tap_table_ui* new_stat, const char *table_title
     items[COUNT_COLUMN].type = TABLE_ITEM_UINT;
     items[COUNT_COLUMN].value.uint_value = 0;
 
+    table = stat_tap_find_table(new_stat, table_title);
+    if (table) {
+        if (new_stat->stat_tap_reset_table_cb) {
+            new_stat->stat_tap_reset_table_cb(table);
+        }
+        return;
+    }
+
     table = stat_tap_init_table(table_title, num_fields, 0, NULL);
     stat_tap_add_table(new_stat, table);
 
