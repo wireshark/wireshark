wireshark/wireshark

commit b49b95af655737ef4bcc3b2640e882dfb051bc58

rpc: create the statistics table only once
Use the new stat_tap_find_table function during init to check if our
statistics table already exists.

epan/dissectors/packet-rpc.c
@@ -3926,12 +3926,15 @@ static stat_tap_table_item rpc_prog_stat_fields[] = {
 
 static void rpc_prog_stat_init(stat_tap_table_ui* new_stat)
 {
+	const char *table_name = "ONC-RPC Program Statistics";
 	int num_fields = sizeof(rpc_prog_stat_fields)/sizeof(stat_tap_table_item);
-	stat_tap_table* table;
+	stat_tap_table *table;
 
-	table = stat_tap_init_table("ONC-RPC Program Statistics", num_fields, 0, NULL);
-	stat_tap_add_table(new_stat, table);
-
+	table = stat_tap_find_table(new_stat, table_name);
+	if (!table) {
+		table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+		stat_tap_add_table(new_stat, table);
+	}
 }
 
 static tap_packet_status

commit dd1b76b4adbe2168c1467ed041e2a0a72219a465

rpc: populate stats table rows only once
If the init function is called and the statistics table already exists, it's
sufficient to clear the data that was collected when the tap parsed the
packets.

Fixes: b49b95af65 ("rpc: create the statistics table only once")

epan/dissectors/packet-rpc.c
@@ -3931,10 +3931,15 @@ static void rpc_prog_stat_init(stat_tap_table_ui* new_stat)
 	stat_tap_table *table;
 
 	table = stat_tap_find_table(new_stat, table_name);
-	if (!table) {
-		table = stat_tap_init_table(table_name, num_fields, 0, NULL);
-		stat_tap_add_table(new_stat, table);
+	if (table) {
+		if (new_stat->stat_tap_reset_table_cb) {
+			new_stat->stat_tap_reset_table_cb(table);
+		}
+		return;
 	}
+
+	table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+	stat_tap_add_table(new_stat, table);
 }
 
 static tap_packet_status