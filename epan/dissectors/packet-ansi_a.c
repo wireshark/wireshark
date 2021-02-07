wireshark/wireshark

commit 8963dff5189f8656cae35c6abb516177a907cb9c

ansi_a: dtap statistics: create the table only once
Use the new stat_tap_find_table function during init to check if our
statistics table already exists.

epan/dissectors/packet-ansi_a.c
@@ -10575,12 +10575,17 @@ static stat_tap_table_item dtap_stat_fields[] = {{TABLE_ITEM_UINT, TAP_ALIGN_RIG
 
 static void ansi_a_dtap_stat_init(stat_tap_table_ui* new_stat)
 {
+    const char *table_name = "ANSI A-I/F DTAP Statistics";
     int num_fields = sizeof(dtap_stat_fields)/sizeof(stat_tap_table_item);
-    stat_tap_table* table = stat_tap_init_table("ANSI A-I/F DTAP Statistics", num_fields, 0, NULL);
+    stat_tap_table *table;
     int i = 0;
     stat_tap_table_item_type items[sizeof(dtap_stat_fields)/sizeof(stat_tap_table_item)];
 
-    stat_tap_add_table(new_stat, table);
+    table = stat_tap_find_table(new_stat, table_name);
+    if (!table) {
+        table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+        stat_tap_add_table(new_stat, table);
+    }
 
     /* Add a fow for each value type */
     while (ansi_a_dtap_strings[i].strptr)

commit 395af33e745a94ea11debe14c02304d0f79852e1

ansi_a: dtap stats: populate table rows only once
If the init function is called and the statistics table already exists, it's
sufficient to clear the data that was collected when the tap parsed the
packets.

Fix a typo in a comment while at it.

Fixes: 8963dff518 ("ansi_a: dtap statistics: create the table only once")

epan/dissectors/packet-ansi_a.c
@@ -10582,12 +10582,17 @@ static void ansi_a_dtap_stat_init(stat_tap_table_ui* new_stat)
     stat_tap_table_item_type items[sizeof(dtap_stat_fields)/sizeof(stat_tap_table_item)];
 
     table = stat_tap_find_table(new_stat, table_name);
-    if (!table) {
-        table = stat_tap_init_table(table_name, num_fields, 0, NULL);
-        stat_tap_add_table(new_stat, table);
+    if (table) {
+        if (new_stat->stat_tap_reset_table_cb) {
+            new_stat->stat_tap_reset_table_cb(table);
+        }
+        return;
     }
 
-    /* Add a fow for each value type */
+    table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+    stat_tap_add_table(new_stat, table);
+
+    /* Add a row for each value type */
     while (ansi_a_dtap_strings[i].strptr)
     {
         items[IEI_COLUMN].type = TABLE_ITEM_UINT;