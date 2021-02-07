wireshark/wireshark

commit f21f1c292a804ec0bab56e419261c51d6abd3254

dhcp: create the statistics table only once
Use the new stat_tap_find_table function during init to check if our
statistics table already exists.

epan/dissectors/packet-dhcp.c
@@ -7256,12 +7256,17 @@ static stat_tap_table_item dhcp_stat_fields[] = {{TABLE_ITEM_STRING, TAP_ALIGN_L
 
 static void dhcp_stat_init(stat_tap_table_ui* new_stat)
 {
+	const char *table_name = "DHCP Statistics";
 	int num_fields = sizeof(dhcp_stat_fields)/sizeof(stat_tap_table_item);
-	stat_tap_table* table = stat_tap_init_table("DHCP Statistics", num_fields, 0, NULL);
+	stat_tap_table *table;
 	int i = 0;
 	stat_tap_table_item_type items[sizeof(dhcp_stat_fields)/sizeof(stat_tap_table_item)];
 
-	stat_tap_add_table(new_stat, table);
+	table = stat_tap_find_table(new_stat, table_name);
+	if (!table) {
+		table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+		stat_tap_add_table(new_stat, table);
+	}
 
 	/* Add a row for each value type */
 	while (opt53_text[i].strptr)

commit 9337e1cd530be80c033196788bb825cc2fd4af08

dhcp: populate stats table rows only once
If the init function is called and the statistics table already exists, it's
sufficient to clear the data that was collected when the tap parsed the
packets.

Fixes: f21f1c292a ("dhcp: create the statistics table only once")

epan/dissectors/packet-dhcp.c
@@ -7263,10 +7263,15 @@ static void dhcp_stat_init(stat_tap_table_ui* new_stat)
 	stat_tap_table_item_type items[sizeof(dhcp_stat_fields)/sizeof(stat_tap_table_item)];
 
 	table = stat_tap_find_table(new_stat, table_name);
-	if (!table) {
-		table = stat_tap_init_table(table_name, num_fields, 0, NULL);
-		stat_tap_add_table(new_stat, table);
+	if (table) {
+		if (new_stat->stat_tap_reset_table_cb) {
+			new_stat->stat_tap_reset_table_cb(table);
+		}
+		return;
 	}
+
+	table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+	stat_tap_add_table(new_stat, table);
 
 	/* Add a row for each value type */
 	while (opt53_text[i].strptr)