ce19a2512eaa76b066bc9885444354c4df2bf210
Gerald Combs <gerald@wireshark.org>
2021-02-08 09:40:44 -0800

    TCP: Add a SACK range check.
    Make sure we don't write past the ends of our sack_left_edge and
    sack_right_edge arrays. Fixes #17187.

--- epan/dissectors/packet-tcp.c
+++ epan/dissectors/packet-tcp.c
@@ -4377,7 +4377,7 @@ dissect_tcpopt_sack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
         num_sack_ranges++;
 
         /* Store blocks for BiF analysis */
-        if (tcp_analyze_seq && tcpd->fwd->tcp_analyze_seq_info && tcp_track_bytes_in_flight) {
+        if (tcp_analyze_seq && tcpd->fwd->tcp_analyze_seq_info && tcp_track_bytes_in_flight && num_sack_ranges < MAX_TCP_SACK_RANGES) {
             tcpd->fwd->tcp_analyze_seq_info->num_sack_ranges = num_sack_ranges;
             tcpd->fwd->tcp_analyze_seq_info->sack_left_edge[num_sack_ranges] = leftedge;
             tcpd->fwd->tcp_analyze_seq_info->sack_right_edge[num_sack_ranges] = rightedge;
