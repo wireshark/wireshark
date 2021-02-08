wireshark/wireshark

commit a5207b541ee21b9de3c0c07b33994fef6266d8c0

mtp3: create the statistics table only once
Use the new stat_tap_find_table function during init to check if our
statistics table already exists.

epan/dissectors/packet-mtp3.c
@@ -826,11 +826,15 @@ static stat_tap_table_item mtp3_stat_fields[] = {
 
 static void mtp3_stat_init(stat_tap_table_ui* new_stat)
 {
+  const char *table_name = "MTP3 Statistics";
   int num_fields = sizeof(mtp3_stat_fields)/sizeof(stat_tap_table_item);
-  stat_tap_table* table;
+  stat_tap_table *table;
 
-  table = stat_tap_init_table("MTP3 Statistics", num_fields, 0, NULL);
-  stat_tap_add_table(new_stat, table);
+  table = stat_tap_find_table(new_stat, table_name);
+  if (!table) {
+    table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+    stat_tap_add_table(new_stat, table);
+  }
 }
 
 static tap_packet_status

commit 0239c495dd52e242b91dc986f0d66cb58542d108

mtp3: populate stats table rows only once
If the init function is called and the statistics table already exists, it's
sufficient to clear the data that was collected when the tap parsed the
packets.

Fixes: a5207b541e ("mtp3: create the statistics table only once")

epan/dissectors/packet-mtp3.c
@@ -831,10 +831,15 @@ static void mtp3_stat_init(stat_tap_table_ui* new_stat)
   stat_tap_table *table;
 
   table = stat_tap_find_table(new_stat, table_name);
-  if (!table) {
-    table = stat_tap_init_table(table_name, num_fields, 0, NULL);
-    stat_tap_add_table(new_stat, table);
+  if (table) {
+    if (new_stat->stat_tap_reset_table_cb) {
+      new_stat->stat_tap_reset_table_cb(table);
+    }
+    return;
   }
+
+  table = stat_tap_init_table(table_name, num_fields, 0, NULL);
+  stat_tap_add_table(new_stat, table);
 }
 
 static tap_packet_status