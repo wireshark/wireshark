wireshark/wireshark

commit b287e7165e8aa89cde6ae37e7c257c5d87d16b9b

GQUIC: make sure our tag offset advances.
Make sure gquic tag offset advances so that we don't infinitely loop.

epan/dissectors/packet-gquic.c
@@ -198,6 +198,7 @@ static expert_field ei_gquic_tag_length = EI_INIT;
 static expert_field ei_gquic_tag_unknown = EI_INIT;
 static expert_field ei_gquic_version_invalid = EI_INIT;
 static expert_field ei_gquic_invalid_parameter = EI_INIT;
+static expert_field ei_gquic_length_invalid = EI_INIT;
 
 static const value_string gquic_short_long_header_vals[] = {
     { 0, "Short Header" },
@@ -1660,7 +1661,7 @@ dissect_gquic_tag(tvbuff_t *tvb, packet_info *pinfo, proto_tree *gquic_tree, gui
                                  "Dissector for (Google) QUIC Tag"
                                  " %s (%s) code not implemented, Contact"
                                  " Wireshark developers if you want this supported", tvb_get_string_enc(wmem_packet_scope(), tvb, offset-8, 4, ENC_ASCII|ENC_NA), val_to_str(tag, tag_vals, "Unknown"));
-                tag_offset += tag_len;
+                goto end;
             break;
         }
         if(tag_offset != offset_end){
@@ -1670,6 +1671,13 @@ dissect_gquic_tag(tvbuff_t *tvb, packet_info *pinfo, proto_tree *gquic_tree, gui
         }
 
         tag_number--;
+    }
+
+    end:
+    if (offset + total_tag_len <= offset) {
+        expert_add_info_format(pinfo, gquic_tree, &ei_gquic_length_invalid,
+                                "Invalid total tag length: %u", total_tag_len);
+        return offset + tvb_reported_length_remaining(tvb, offset);
     }
     return offset + total_tag_len;
 
@@ -3173,7 +3181,8 @@ proto_register_gquic(void)
         { &ei_gquic_tag_length, { "gquic.tag.length.truncated", PI_MALFORMED, PI_NOTE, "Truncated Tag Length...", EXPFILL }},
         { &ei_gquic_tag_unknown, { "gquic.tag.unknown.data", PI_UNDECODED, PI_NOTE, "Unknown Data", EXPFILL }},
         { &ei_gquic_version_invalid, { "gquic.version.invalid", PI_MALFORMED, PI_ERROR, "Invalid Version", EXPFILL }},
-        { &ei_gquic_invalid_parameter, { "gquic.invalid.parameter", PI_MALFORMED, PI_ERROR, "Invalid Parameter", EXPFILL }}
+        { &ei_gquic_invalid_parameter, { "gquic.invalid.parameter", PI_MALFORMED, PI_ERROR, "Invalid Parameter", EXPFILL }},
+        { &ei_gquic_length_invalid, { "gquic.length.invalid", PI_PROTOCOL, PI_WARN, "Invalid Length", EXPFILL }}
     };
 
     expert_module_t *expert_gquic;

commit 7fd71536962cc24614cdc932770f0a4a200f6008

GQUIC: add decoding of CGST tag
Regression introduced by b287e7165e.

To avoid an infinite loop with malformed packets, that commit stops
parsing the tags list after finding an unknown tag.
When this "unknown" tag is perfectly valid but not supported by
Wireshark, we don't decode any subsequent (valid) tags anymore.

GQUIC is going to die soon and it is quite unlikely it will change in
the next future. Therefore the best/quick solution is simply decoding
any valid tag.

Close #17250

epan/dissectors/packet-gquic.c
@@ -169,6 +169,7 @@ static int hf_gquic_tag_smhl = -1;
 static int hf_gquic_tag_tbkp = -1;
 static int hf_gquic_tag_mad0 = -1;
 static int hf_gquic_tag_qlve = -1;
+static int hf_gquic_tag_cgst = -1;
 
 /* Public Reset Tags */
 static int hf_gquic_tag_rnon = -1;
@@ -430,6 +431,7 @@ static const value_string message_tag_vals[] = {
 #define TAG_TBKP 0x54424B50
 #define TAG_MAD0 0x4d414400
 #define TAG_QLVE 0x514C5645
+#define TAG_CGST 0x43475354
 
 /* Public Reset Tag */
 #define TAG_RNON 0x524E4F4E
@@ -478,6 +480,7 @@ static const value_string tag_vals[] = {
     { TAG_TBKP, "Token Binding Key Params" },
     { TAG_MAD0, "Max Ack Delay (IETF QUIC)" },
     { TAG_QLVE, "Legacy Version Encapsulation" },
+    { TAG_CGST, "Congestion Control Feedback Type" },
 
     { TAG_RNON, "Public Reset Nonce Proof" },
     { TAG_RSEQ, "Rejected Packet Number" },
@@ -1670,6 +1673,10 @@ dissect_gquic_tag(tvbuff_t *tvb, packet_info *pinfo, proto_tree *gquic_tree, gui
 
                 tag_offset += tag_len;
             }
+            break;
+            case TAG_CGST:
+                proto_tree_add_item(tag_tree, hf_gquic_tag_cgst, tvb, tag_offset_start + tag_offset, tag_len, ENC_NA);
+                tag_offset += tag_len;
             break;
             default:
                 proto_tree_add_item(tag_tree, hf_gquic_tag_unknown, tvb, tag_offset_start + tag_offset, tag_len, ENC_NA);
@@ -3161,6 +3168,11 @@ proto_register_gquic(void)
         },
         { &hf_gquic_tag_qlve,
             { "Legacy Version Encapsulation", "gquic.tag.qlve",
+              FT_BYTES, BASE_NONE, NULL, 0x0,
+              NULL, HFILL }
+        },
+        { &hf_gquic_tag_cgst,
+            { "Congestion Control Feedback Type", "gquic.tag.cgst",
               FT_BYTES, BASE_NONE, NULL, 0x0,
               NULL, HFILL }
         },