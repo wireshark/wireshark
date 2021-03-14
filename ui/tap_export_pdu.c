wireshark/wireshark

commit 6ea2762f1241adb7b5bf4ad5082f577d87c41509

export_pdu: set pkt_encap to a WTAP_ENCAP value, not a pcap LINKTYPE_ value.
rec.rec_header.packet_header.pkt_encap expects WTAP encapsulation types,
not PCAP encapsulation types, so don't call
wtap_wtap_encap_to_pcap_encap().

Also, set the output file encapsulation, and the link-layer type of the
fake interface, to exp_pdu_tap_data->pkt_encap rather than wiring it to
WTAP_ENCAP_WIRESHARK_UPPER_PDU (we set exp_pdu_tap_data->pkt_encap to
WTAP_ENCAP_WIRESHARK_UPPER_PDU, but this is a bit cleaner).

(backported from commit ea60a57826624bcdf8f955af85af7130c8864018)

ui/tap_export_pdu.c
@@ -130,6 +130,7 @@ exp_pdu_open(exp_pdu_t *exp_pdu_tap_data, int fd, const char *comment)
     /* create the fake interface data */
     int_data = wtap_block_create(WTAP_BLOCK_IF_DESCR);
     int_data_mand = (wtapng_if_descr_mandatory_t*)wtap_block_get_mandatory_data(int_data);
+    int_data_mand->wtap_encap      = exp_pdu_tap_data->pkt_encap;
     int_data_mand->wtap_encap      = WTAP_ENCAP_WIRESHARK_UPPER_PDU;
     int_data_mand->time_units_per_second = 1000000000; /* default nanosecond resolution */
     int_data_mand->snap_len        = WTAP_MAX_PACKET_SIZE_STANDARD;
@@ -143,7 +144,7 @@ exp_pdu_open(exp_pdu_t *exp_pdu_tap_data, int fd, const char *comment)
     g_array_append_val(exp_pdu_tap_data->shb_hdrs, shb_hdr);
 
     const wtap_dump_params params = {
-        .encap = WTAP_ENCAP_WIRESHARK_UPPER_PDU,
+        .encap = exp_pdu_tap_data->pkt_encap,
         .snaplen = WTAP_MAX_PACKET_SIZE_STANDARD,
         .shb_hdrs = exp_pdu_tap_data->shb_hdrs,
         .idb_inf = exp_pdu_tap_data->idb_inf,
@@ -184,7 +185,7 @@ exp_pdu_pre_open(const char *tap_name, const char *filter, exp_pdu_t *exp_pdu_ta
     GString        *error_string;
 
     /* XXX: can we always assume WTAP_ENCAP_WIRESHARK_UPPER_PDU? */
-    exp_pdu_tap_data->pkt_encap = wtap_wtap_encap_to_pcap_encap(WTAP_ENCAP_WIRESHARK_UPPER_PDU);
+    exp_pdu_tap_data->pkt_encap = WTAP_ENCAP_WIRESHARK_UPPER_PDU;
 
     /* Register this tap listener now */
     error_string = register_tap_listener(tap_name,             /* The name of the tap we want to listen to */

commit 178f40cd98a4fcdc05c2ca4d6bf94ce9f239429a

export_pdu: Fixup backported commit
Commit 6ea2762f1241adb7b5bf4ad5082f577d87c41509 accidentally left in
a line when the backport to master-3.2 was done, remove it.

ui/tap_export_pdu.c
@@ -131,7 +131,6 @@ exp_pdu_open(exp_pdu_t *exp_pdu_tap_data, int fd, const char *comment)
     int_data = wtap_block_create(WTAP_BLOCK_IF_DESCR);
     int_data_mand = (wtapng_if_descr_mandatory_t*)wtap_block_get_mandatory_data(int_data);
     int_data_mand->wtap_encap      = exp_pdu_tap_data->pkt_encap;
-    int_data_mand->wtap_encap      = WTAP_ENCAP_WIRESHARK_UPPER_PDU;
     int_data_mand->time_units_per_second = 1000000000; /* default nanosecond resolution */
     int_data_mand->snap_len        = WTAP_MAX_PACKET_SIZE_STANDARD;
 