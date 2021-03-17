wireshark/wireshark

commit 2820156fbd4b0213ca015216d8cd97c31b503c8c

Move still *more* headers outside of extern "C".
If a header declares a function, or anything else requiring the extern
"C" decoration, have it wrap the declaration itself; don't rely on the
header itself being included inside extern "C".

caputils/capture-pcap-util.h
@@ -11,10 +11,6 @@
 #ifndef __CAPTURE_PCAP_UTIL_H__
 #define __CAPTURE_PCAP_UTIL_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #ifdef HAVE_LIBPCAP
 
 #include "wspcap.h"
@@ -29,6 +25,10 @@ extern "C" {
  * the packet"), so the minimum packet size is 1 byte.
  */
 #define MIN_PACKET_SIZE 1	/* minimum amount of packet data we can read */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 GList *get_interface_list(int *err, char **err_str);
 #ifdef HAVE_PCAP_REMOTE

capchild/capture_session.h
@@ -11,10 +11,6 @@
 #ifndef __CAPCHILD_CAPTURE_SESSION_H__
 #define __CAPCHILD_CAPTURE_SESSION_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #ifndef _WIN32
 #include <sys/types.h>
 #include <stdint.h>
@@ -24,6 +20,12 @@ extern "C" {
 
 #include <wsutil/processes.h>
 
+#include "cfile.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 #ifdef HAVE_LIBPCAP
 /* Current state of capture engine. XXX - differentiate states */
 typedef enum {
@@ -32,7 +34,6 @@ typedef enum {
     CAPTURE_RUNNING         /**< capture child signalled ok, capture is running now */
 } capture_state;
 
-#include "cfile.h"
 struct _info_data;
 
 /*

caputils/ws80211_utils.h
@@ -11,11 +11,11 @@
 #ifndef __WS80211_UTILS_H__
 #define __WS80211_UTILS_H__
 
+#include "ws_attributes.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-#include "ws_attributes.h"
 
 enum ws80211_channel_type {
 	WS80211_CHAN_NO_HT,

epan/dissectors/asn1/kerberos/packet-kerberos-template.h
@@ -122,10 +122,10 @@ extern gboolean krb_decrypt;
 
 #endif /* HAVE_KERBEROS */
 
-#include "packet-kerberos-exp.h"
-
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
+
+#include "packet-kerberos-exp.h"
 
 #endif  /* __PACKET_KERBEROS_H */

epan/dissectors/packet-a21.h
@@ -10,11 +10,11 @@
 #ifndef __PACKET_A21_H__
 #define __PACKET_A21_H__
 
+#include "ws_symbol_export.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-#include "ws_symbol_export.h"
 
 WS_DLL_PUBLIC
 void dissect_a21_ie_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *top_tree, proto_tree *tree, gint offset, guint8 message_type);

epan/dissectors/packet-bluetooth.h
@@ -10,14 +10,14 @@
 #ifndef __PACKET_BLUETOOTH_H__
 #define __PACKET_BLUETOOTH_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #include <epan/wmem/wmem.h>
 
 #include "packet-usb.h"
 #include "packet-ubertooth.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 #define PROTO_DATA_BLUETOOTH_SERVICE_UUID  0
 

epan/dissectors/packet-dcm.h
@@ -25,9 +25,6 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#include "config.h"
-
-
 #define DCM_VR_AE  1  /* Application Entity        */
 #define DCM_VR_AS  2  /* Age String                */
 #define DCM_VR_AT  3  /* Attribute Tag             */

epan/dissectors/packet-quic.h
@@ -10,13 +10,13 @@
 #ifndef __PACKET_QUIC_H__
 #define __PACKET_QUIC_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
-
 #include "ws_symbol_export.h"
 
 #include <wsutil/wsgcrypt.h>	/* needed to define HAVE_LIBGCRYPT_AEAD */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
 /**
  * Metadata for a STREAM frame.

commit 9ca121bcf87dc638d9f0bf1314bee7b906523b70

caputils: fix compilation without pcap.
The extern declaration must be put outside the ifdef to match the
closing statement as well as surrounding al the functions.

Fixes: 2820156fbd4b0213ca015216d8cd97c31b503c8c (Move still *more* headers outside of extern "C".)

caputils/capture-pcap-util.h
@@ -11,6 +11,10 @@
 #ifndef __CAPTURE_PCAP_UTIL_H__
 #define __CAPTURE_PCAP_UTIL_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 #ifdef HAVE_LIBPCAP
 
 #include "wspcap.h"
@@ -25,10 +29,6 @@
  * the packet"), so the minimum packet size is 1 byte.
  */
 #define MIN_PACKET_SIZE 1	/* minimum amount of packet data we can read */
-
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
 
 GList *get_interface_list(int *err, char **err_str);
 #ifdef HAVE_PCAP_REMOTE