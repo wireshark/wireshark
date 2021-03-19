4c513fb4ab880b2c63491d91b8ccf6ce17627f08
Darius Davis <darius@vmware.com>
2021-03-20 09:37:53 +1000

    Minor doc cleanups.
    Here's a grab bag of trivial cleanup to the documentation.  This change:
    
    - Cleans up some comments in the asciidoctor macros which are no longer
      accurate (and do not appear in the build products anyway).
    
    - Fixes a missing space in the text "Wireshark Q&A" in the release notes.
    
    - Allows the "docbook" backend to produce hyperlinks too... That seems to be
      necessary if we want to start using our custom link macros in WSDG, which
      seems like a reasonable thing to do.  And fixes up a wrong variable name in
      the handling of the case where we are not able to produce a hyperlink.

--- docbook/asciidoctor-macros/ws_utils.rb
+++ docbook/asciidoctor-macros/ws_utils.rb
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: MIT
 module WsUtils
   def create_doc_links(parent, target, text)
-    if parent.document.basebackend? 'html'
+    if (parent.document.basebackend? 'docbook') || (parent.document.basebackend? 'html')
       parent.document.register :links, target
       create_anchor(parent, text, type: :link, target: target).render.to_s
     elsif parent.document.backend == 'manpage'
       "\\fB#{text}"
     else
-      bugtext
+      text
     end
   end
 end
