5af458725595675bbb39af4a771e7f9d4a2e79d7
John Thacker <johnthacker@gmail.com>
2021-04-06 07:16:37 -0400

    QCustomPlot: Fix some warnings in upstream code
    QPainter::HighQualityAntialiasing is obsolete and ignored
    (https://doc.qt.io/qt-5/qpainter.html#RenderHint-enum) since
    at least 5.6, so use Antialiasing instead, as indicated by the docs.
    (https://doc.qt.io/archives/qt-5.6/qpainter.html#RenderHint-enum)
    
    (ported from commit d0088585b3df7be3efdc79037eead2a716b06ea6)

--- ui/qt/widgets/qcustomplot.cpp
+++ ui/qt/widgets/qcustomplot.cpp
@@ -673,7 +673,7 @@ QCPPainter *QCPPaintBufferPixmap::startPainting()
 {
   QCPPainter *result = new QCPPainter(&mBuffer);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  result->setRenderHint(QPainter::HighQualityAntialiasing);
+  result->setRenderHint(QPainter::Antialiasing);
 #endif
   return result;
 }
@@ -762,7 +762,7 @@ QCPPainter *QCPPaintBufferGlPbuffer::startPainting()
   }
 
   QCPPainter *result = new QCPPainter(mGlPBuffer);
-  result->setRenderHint(QPainter::HighQualityAntialiasing);
+  result->setRenderHint(QPainter::Antialiasing);
   return result;
 }
 
@@ -875,7 +875,7 @@ QCPPainter *QCPPaintBufferGlFbo::startPainting()
   mGlFrameBuffer->bind();
   QCPPainter *result = new QCPPainter(paintDevice.data());
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  result->setRenderHint(QPainter::HighQualityAntialiasing);
+  result->setRenderHint(QPainter::Antialiasing);
 #endif
   return result;
 }
@@ -15482,7 +15482,7 @@ void QCustomPlot::paintEvent(QPaintEvent *event)
   if (painter.isActive())
   {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  painter.setRenderHint(QPainter::HighQualityAntialiasing); // to make Antialiasing look good if using the OpenGL graphicssystem
+  painter.setRenderHint(QPainter::Antialiasing); // to make Antialiasing look good if using the OpenGL graphicssystem
 #endif
     if (mBackgroundBrush.style() != Qt::NoBrush)
       painter.fillRect(mViewport, mBackgroundBrush);
