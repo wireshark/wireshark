512adcb0463bcf4ca33ee974afb811e6079e5a50
Guy Harris <gharris@sonic.net>
2021-04-16 00:38:13 -0700

    ptvcursor: just do a realloc.
    ptvcursor_new_subtree_levels() just needs to do a wmem_realloc() on the
    subtree array; that will take care of ensuring the old data is in the
    new array.

--- epan/proto.c
+++ epan/proto.c
@@ -1113,15 +1113,12 @@ static void
 ptvcursor_new_subtree_levels(ptvcursor_t *ptvc)
 {
 	subtree_lvl *pushed_tree;
-	size_t pushed_tree_len = sizeof(subtree_lvl) * ptvc->pushed_tree_max;
 
 	DISSECTOR_ASSERT(ptvc->pushed_tree_max <= SUBTREE_MAX_LEVELS-SUBTREE_ONCE_ALLOCATION_NUMBER);
 	ptvc->pushed_tree_max += SUBTREE_ONCE_ALLOCATION_NUMBER;
 
-	pushed_tree = (subtree_lvl *)wmem_alloc(wmem_packet_scope(), sizeof(subtree_lvl) * ptvc->pushed_tree_max);
+	pushed_tree = (subtree_lvl *)wmem_realloc(wmem_packet_scope(), (void *)ptvc->pushed_tree, sizeof(subtree_lvl) * ptvc->pushed_tree_max);
 	DISSECTOR_ASSERT(pushed_tree != NULL);
-	if (ptvc->pushed_tree)
-		memcpy(pushed_tree, ptvc->pushed_tree, pushed_tree_len);
 	ptvc->pushed_tree = pushed_tree;
 }
 
