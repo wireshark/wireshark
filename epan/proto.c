3a0e1ba1e23252d9129aca9c7038c81f7af7bec4
Constantine Gavrilov <constg@il.ibm.com>
2021-04-18 13:09:06 +0300

    NVMe: Increase PROTO_PRE_ALLOC_HF_FIELDS_MEM.

--- epan/proto.c
+++ epan/proto.c
@@ -8788,7 +8788,7 @@ register_string_errors(void)
 	proto_set_cant_toggle(proto_string_errors);
 }
 
-#define PROTO_PRE_ALLOC_HF_FIELDS_MEM (231000+PRE_ALLOC_EXPERT_FIELDS_MEM)
+#define PROTO_PRE_ALLOC_HF_FIELDS_MEM (235000+PRE_ALLOC_EXPERT_FIELDS_MEM)
 static int
 proto_register_field_init(header_field_info *hfinfo, const int parent)
 {
