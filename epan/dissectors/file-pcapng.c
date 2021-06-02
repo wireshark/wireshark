65a554b9766d99fc7cf70bfbca51b847ccc24ad9
Guy Harris <gharris@sonic.net>
2021-06-01 18:59:29 -0700

    pcapng: set the length of the options item.
    It runs up to either the end of the option data or the terminating
    end-of-options option (readers MUST handle lists of options that
    contains an end-of-options option and lists of options that don't).
    
    
    (cherry picked from commit 2f5c0ffdb26d753943201029ff65e237c1a9776b)

--- epan/dissectors/file-pcapng.c
+++ epan/dissectors/file-pcapng.c
@@ -1339,6 +1339,7 @@ static gint dissect_options(proto_tree *tree, packet_info *pinfo,
             proto_item_append_text(option_item, " = %s", str);
 
     }
+    proto_item_set_end(options_item, tvb, offset);
 
     return offset;
 }
