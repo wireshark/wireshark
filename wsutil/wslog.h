ef0f022f8629009daf7c31e204b9d65b1805f6e4
João Valverde <joao.valverde@tecnico.ulisboa.pt>
2021-06-06 19:14:45 +0100

    wslog: Add a comment

--- wsutil/wslog.h
+++ wsutil/wslog.h
@@ -12,6 +12,14 @@
 #include <ws_symbol_export.h>
 #include <glib.h>
 
+
+/*
+ * XXX This API should be consolidated with ui/console.h. The way this works
+ * (or doesn't) with prefs->console_log_level is also weird, because not all
+ * log domains are using the same log handler. The function ws_log_full()
+ * currently ignores the console log level preference.
+ */
+
 WS_DLL_PUBLIC
 void ws_log_full(const char *log_domain, GLogLevelFlags log_level,
                     const char *file, int line, const char *func,
