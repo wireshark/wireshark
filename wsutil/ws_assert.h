7aae691f7d3c0c419e0b5b424dc2b62b2ded1ab3
João Valverde <j@v6e.pt>
2021-06-25 04:55:46 +0100

    wsutil: Rewrite ws_assert() to minimize dependencies
    This includes as little as possible in the assertion header, so
    that it can be included globally in every file without pulling
    any unwanted definitions. In particular pulling stdlib.h is
    avoided because that can have side effects if it wants to
    include non-portable extensions.
    
    It is possible to have side-effects from include glib.h too, for
    example because of G_LOG_DOMAIN.
    
    These side-effects are usually avoidable with careful ordering
    of pre-processor directives but with multiple levels of indirections
    it can be hard to track. Better to make it robust to these kinds
    of failures in the first place.
    
    Also integrate with our logger for a cohesive experience (but
    keep it a private dependency).

--- wsutil/ws_assert.h
+++ wsutil/ws_assert.h
@@ -10,43 +10,74 @@
 #ifndef __WS_ASSERT_H__
 #define __WS_ASSERT_H__
 
+#include <ws_symbol_export.h>
 #include <ws_attributes.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
+#ifdef WS_LOG_DOMAIN
+#define _ASSERT_DOMAIN WS_LOG_DOMAIN
+#else
+#define _ASSERT_DOMAIN ""
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+WS_DLL_PUBLIC
+WS_NORETURN
+void ws_assert_failed(const char *file, int line, const char *function,
+                        const char *domain, const char *assertion,
+                        bool unreachable);
+
+#define _ASSERT_FAIL(expr) \
+        ws_assert_failed(__FILE__, __LINE__, __func__, \
+                            _ASSERT_DOMAIN, #expr, false)
+
+/*
+ * ws_abort_if_fail() is not conditional on WS_DISABLE_ASSERT.
+ * Usually used to appease a static analyzer.
+ */
+#define ws_abort_if_fail(expr) \
+        do { if (!(expr)) _ASSERT_FAIL(expr); } while (0)
+
+#ifdef WS_DISABLE_ASSERT
 /*
  * ws_assert() cannot produce side effects, otherwise code will
  * behave differently because of WS_DISABLE_ASSERT, and probably introduce
- * some nasty bugs.
+ * some difficult to track bugs.
+ *
+ * We don't want to execute the expression with WS_DISABLE_ASSERT because
+ * it might be time and space costly and the goal here is to optimize for
+ * WS_DISABLE_ASSERT. However removing it completely is not good enough
+ * because it might generate many unused variable warnings. So we use
+ * if (false) and let the compiler optimize away the dead execution branch.
  */
-#ifndef WS_DISABLE_ASSERT
-#define ws_assert(expr) g_assert(expr)
+#define ws_assert(expr) do { if (false) ws_abort_if_fail(expr); } while (0)
 #else
-#define ws_assert(expr) G_STMT_START { if (0) g_assert(expr); } G_STMT_END
+#define ws_assert(expr) ws_abort_if_fail(expr)
 #endif
 
 /*
  * We don't want to disable ws_assert_not_reached() with WS_DISABLE_ASSERT.
  * That would blast compiler warnings everywhere for no benefit, not
- * even a miniscule performance gain.
+ * even a miniscule performance gain. Reaching this function is always
+ * a programming error and will unconditionally abort execution.
  *
- * Note: If the compiler supports unreachable built-ins (which recent
- * versions of GCC and MSVC do) there is no warning blast with
- * g_assert_not_reached() and G_DISABLE_ASSERT. However if that is not
- * the case then g_assert_not_reached() is simply (void)0 and that
+ * Note: With g_assert_not_reached() if the compiler supports unreachable
+ * built-ins (which recent versions of GCC and MSVC do) there is no warning
+ * blast with g_assert_not_reached() and G_DISABLE_ASSERT. However if that
+ * is not the case then g_assert_not_reached() is simply (void)0 and that
  * causes the spurious warnings, because the compiler can't tell anymore
- * that a certain code path is not used. We add the call to abort() so
- * that the function never returns, even with G_DISABLE_ASSERT.
+ * that a certain code path is not used. We avoid that with
+ * ws_assert_not_reached(). There is no reason to ever use a no-op here.
  */
-static inline
-WS_NORETURN void ws_assert_not_reached(void) {
-    g_assert_not_reached();
-    abort();
-}
+#define ws_assert_not_reached() \
+        ws_assert_failed(__FILE__, __LINE__, __func__, \
+                            _ASSERT_DOMAIN, NULL, true)
 
-/*
- * ws_abort_if_fail() is always enabled. Usually used to appease a static
- * analyzer.
- */
-#define ws_abort_if_fail(expr) g_assert_true(expr)
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* __WS_ASSERT_H__ */
