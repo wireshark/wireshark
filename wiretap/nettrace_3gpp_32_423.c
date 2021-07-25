cb98aa373026a9dbb0d15f5d9346ff44de8e8e7c
Guy Harris <gharris@sonic.net>
2021-07-25 13:34:11 -0700

    nettrace 3GPP 32.423: move the name related code together.
    Initialize name_str right before we fill it in if the name is present,
    and note what we're doing, to make it a bit clearer what the
    initializing code is doing.

--- wiretap/nettrace_3gpp_32_423.c
+++ wiretap/nettrace_3gpp_32_423.c
@@ -253,7 +253,6 @@ nettrace_msg_to_packet(nettrace_3gpp_32_423_file_info_t *file_info, wtap_rec *re
 		return FALSE;
 	}
 
-	name_str[0] = '\0';
 	prev_pos = curr_pos = input + CLEN(c_s_msg);
 
 	rec->rec_type = REC_TYPE_PACKET;
@@ -323,6 +322,7 @@ nettrace_msg_to_packet(nettrace_3gpp_32_423_file_info_t *file_info, wtap_rec *re
 	}
 
 	/* See if we have a "name" */
+	name_str[0] = '\0';	/* if we don't have a name */
 	curr_pos = STRNSTR(start_msg_tag_cont, c_proto_name);
 	if (curr_pos != NULL) {
 		/* extract the name */
