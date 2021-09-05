95b63d04d925fad738eaa1c1a90fb0b8b0393ded
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-09-05 12:40:28 +0000

    dcerpc_spoolss: fix display filter name
    found by conflict check
     'spoolss.driverversion' exists multiple times with incompatible types: FT_UINT64 and FT_UINT32

--- epan/dissectors/packet-dcerpc-spoolss.c
+++ epan/dissectors/packet-dcerpc-spoolss.c
@@ -7164,7 +7164,7 @@ proto_register_dcerpc_spoolss(void)
 		  { "Default data type", "spoolss.defaultdatatype", FT_STRING, BASE_NONE,
 		    NULL, 0, NULL, HFILL }},
 		{ &hf_driverinfo_cversion,
-		  { "Driver version", "spoolss.driverversion", FT_UINT32, BASE_DEC,
+		  { "Driver version", "spoolss.drivercversion", FT_UINT32, BASE_DEC,
 		    VALS(driverinfo_cversion_vals), 0, "Printer name", HFILL }},
 		{ &hf_dependentfiles,
 		  { "Dependent files", "spoolss.dependentfiles", FT_STRING, BASE_NONE,
