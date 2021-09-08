ddc883f6a785dd1f3cb6df778ac1a904e916c693
Mikael Kanstrup <mikael.kanstrup@gmail.com>
2021-09-08 13:36:24 +0200

    dot11decrypt: Fix AAD calculation for legacy ccmp implementation
    Re-implement below change but for the legacy ccmp decryption used on
    3.2 release track but also on later releases when Wireshark is built
    with older versions of libgcrypt:
    
    e5e37add9a 802.11 Decrypt: Fix AAD Calculation when HT-Control present in a QoS Data Frame
    
    Ping #17577.
    
    (backported from commit c58c106b304fbcc8333d470e93a7c0962c7d6b1d)

--- epan/crypt/dot11decrypt_ccmp.c
+++ epan/crypt/dot11decrypt_ccmp.c
@@ -41,6 +41,11 @@
 	(DOT11DECRYPT_FC0_TYPE_MASK | DOT11DECRYPT_FC0_SUBTYPE_QOS)) == \
 	(DOT11DECRYPT_FC0_TYPE_DATA | DOT11DECRYPT_FC0_SUBTYPE_QOS))
 
+#define DOT11DECRYPT_IS_QOS_DATA(wh) DOT11DECRYPT_QOS_HAS_SEQ(wh)
+
+#define FC1_AAD_MASK 0xc7
+#define FC1_AAD_QOS_MASK 0x47
+
 /****************************************************************************/
 /* Internal macros								*/
 
@@ -113,7 +118,7 @@ static void ccmp_init_blocks(
 	b0[15] = (UINT8)(dlen & 0xff);
 
 	/* AAD:
-	* FC with bits 4..6 and 11..13 masked to zero; 14 is always one
+	* FC with bits 4..6 and 11..13 masked to zero; 14 is always one; 15 zero when QoS Control field present
 	* A1 | A2 | A3
 	* SC with bits 4..15 (seq#) masked to zero
 	* A4 (if present)
@@ -125,7 +130,12 @@ static void ccmp_init_blocks(
 		aad[2] = (UINT8)(wh->fc[0] & 0x8f);    /* XXX magic #s */
 	else
 		aad[2] = wh->fc[0];
-	aad[3] = (UINT8)(wh->fc[1] & 0xc7);    /* XXX magic #s */
+	if (DOT11DECRYPT_IS_QOS_DATA(wh)) {
+		aad[3] = (UINT8)((wh->fc[1] & FC1_AAD_QOS_MASK) | 0x40);
+	} else {
+		aad[3] = (UINT8)((wh->fc[1] & FC1_AAD_MASK) | 0x40);
+	}
+
 	/* NB: we know 3 addresses are contiguous */
 	memcpy(aad + 4, &wh->addr1[0], 3 * DOT11DECRYPT_MAC_LEN);
 	aad[22] = (UINT8)(wh->seq[0] & DOT11DECRYPT_SEQ_FRAG_MASK);
