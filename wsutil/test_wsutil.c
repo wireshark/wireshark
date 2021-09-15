44538f6e4d9703d5611f15d7b4a304df9c8e8b49
João Valverde <j@v6e.pt>
2021-09-15 17:34:07 +0100

    wsutil: Add tests for to_str_back()

--- wsutil/test_wsutil.c
+++ wsutil/test_wsutil.c
@@ -103,6 +103,149 @@ void test_bytes_to_string_punct_trunc1(void)
     g_free(str);
 }
 
+static char to_str_back_buf[32];
+#define BACK_PTR (&to_str_back_buf[31]) /* pointer to NUL string terminator */
+
+void test_oct_to_str_back(void)
+{
+    char *str;
+
+    str = oct_to_str_back(BACK_PTR, 958769886);
+    g_assert_cmpstr(str,  ==, "07111325336");
+
+    str = oct_to_str_back(BACK_PTR, 781499127);
+    g_assert_cmpstr(str,  ==, "05645135367");
+
+    str = oct_to_str_back(BACK_PTR, 1177329882);
+    g_assert_cmpstr(str, ==, "010613120332");
+}
+
+void test_oct64_to_str_back(void)
+{
+    char *str;
+
+    str = oct64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(13873797580070999420));
+    g_assert_cmpstr(str, ==, "01402115026217563452574");
+
+    str = oct64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(7072159458371400691));
+    g_assert_cmpstr(str, ==, "0610452670726711271763");
+
+    str = oct64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(12453513102400590374));
+    g_assert_cmpstr(str, ==, "01263236102754220511046");
+}
+
+void test_hex_to_str_back_len(void)
+{
+    char *str;
+
+    str = hex_to_str_back_len(BACK_PTR, 2481, 8);
+    g_assert_cmpstr(str, ==, "0x000009b1");
+
+    str = hex_to_str_back_len(BACK_PTR, 2457, 8);
+    g_assert_cmpstr(str, ==, "0x00000999");
+
+    str = hex_to_str_back_len(BACK_PTR, 16230, 8);
+    g_assert_cmpstr(str, ==, "0x00003f66");
+}
+
+void test_hex64_to_str_back_len(void)
+{
+    char *str;
+
+    str = hex64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(1), 16);
+    g_assert_cmpstr(str, ==, "0x0000000000000001");
+
+    str = hex64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(4294967295), 16);
+    g_assert_cmpstr(str, ==, "0x00000000ffffffff");
+
+    str = hex64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(18446744073709551615), 16);
+    g_assert_cmpstr(str, ==, "0xffffffffffffffff");
+}
+
+void test_uint_to_str_back(void)
+{
+    char *str;
+
+    str = uint_to_str_back(BACK_PTR, 873735883);
+    g_assert_cmpstr(str, ==, "873735883");
+
+    str = uint_to_str_back(BACK_PTR, 1801148094);
+    g_assert_cmpstr(str, ==, "1801148094");
+
+    str = uint_to_str_back(BACK_PTR, 181787997);
+    g_assert_cmpstr(str, ==, "181787997");
+}
+
+void test_uint64_to_str_back(void)
+{
+    char *str;
+
+    str = uint64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(585143757104211265));
+    g_assert_cmpstr(str, ==, "585143757104211265");
+
+    str = uint64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(7191580247919484847));
+    g_assert_cmpstr(str, ==, "7191580247919484847");
+
+    str = uint64_to_str_back(BACK_PTR, G_GUINT64_CONSTANT(95778573911934485));
+    g_assert_cmpstr(str, ==, "95778573911934485");
+}
+
+void test_uint_to_str_back_len(void)
+{
+    char *str;
+
+    str = uint_to_str_back_len(BACK_PTR, 26630, 8);
+    g_assert_cmpstr(str, ==, "00026630");
+
+    str = uint_to_str_back_len(BACK_PTR, 25313, 8);
+    g_assert_cmpstr(str, ==, "00025313");
+
+    str = uint_to_str_back_len(BACK_PTR, 18750000, 8);
+    g_assert_cmpstr(str, ==, "18750000");
+}
+
+void test_uint64_to_str_back_len(void)
+{
+    char *str;
+
+    str = uint64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(1), 16);
+    g_assert_cmpstr(str, ==, "0000000000000001");
+
+    str = uint64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(4294967295), 16);
+    g_assert_cmpstr(str, ==, "0000004294967295");
+
+    str = uint64_to_str_back_len(BACK_PTR, G_GUINT64_CONSTANT(18446744073709551615), 16);
+    g_assert_cmpstr(str, ==, "18446744073709551615");
+}
+
+void test_int_to_str_back(void)
+{
+    char *str;
+
+    str = int_to_str_back(BACK_PTR, -763689611);
+    g_assert_cmpstr(str, ==, "-763689611");
+
+    str = int_to_str_back(BACK_PTR, -296015954);
+    g_assert_cmpstr(str, ==, "-296015954");
+
+    str = int_to_str_back(BACK_PTR, 898901469);
+    g_assert_cmpstr(str, ==, "898901469");
+}
+
+void test_int64_to_str_back(void)
+{
+    char *str;
+
+    str = int64_to_str_back(BACK_PTR, G_GINT64_CONSTANT(-9223372036854775807));
+    g_assert_cmpstr(str, ==, "-9223372036854775807");
+
+    str = int64_to_str_back(BACK_PTR, G_GINT64_CONSTANT(1));
+    g_assert_cmpstr(str, ==, "1");
+
+    str = int64_to_str_back(BACK_PTR, G_GINT64_CONSTANT(9223372036854775807));
+    g_assert_cmpstr(str, ==, "9223372036854775807");
+}
+
 int main(int argc, char **argv)
 {
     int ret;
@@ -115,6 +258,16 @@ int main(int argc, char **argv)
     g_test_add_func("/to_str/bytes_to_str_punct", test_bytes_to_str_punct);
     g_test_add_func("/to_str/bytes_to_str_trunc1", test_bytes_to_string_trunc1);
     g_test_add_func("/to_str/bytes_to_str_punct_trunc1", test_bytes_to_string_punct_trunc1);
+    g_test_add_func("/to_str/oct_to_str_back", test_oct_to_str_back);
+    g_test_add_func("/to_str/oct64_to_str_back", test_oct64_to_str_back);
+    g_test_add_func("/to_str/hex_to_str_back_len", test_hex_to_str_back_len);
+    g_test_add_func("/to_str/hex64_to_str_back_len", test_hex64_to_str_back_len);
+    g_test_add_func("/to_str/uint_to_str_back", test_uint_to_str_back);
+    g_test_add_func("/to_str/uint64_to_str_back", test_uint64_to_str_back);
+    g_test_add_func("/to_str/uint_to_str_back_len", test_uint_to_str_back_len);
+    g_test_add_func("/to_str/uint64_to_str_back_len", test_uint64_to_str_back_len);
+    g_test_add_func("/to_str/int_to_str_back", test_int_to_str_back);
+    g_test_add_func("/to_str/int64_to_str_back", test_int64_to_str_back);
 
     ret = g_test_run();
 
