16eb55b54eebc730e66cfecc16e1253291344ce1
João Valverde <j@v6e.pt>
2021-09-17 22:54:36 +0100

    IPv6: Rename "SA MAC" to "SLAAC MAC"
    The term "SLAAC" is widely used, "SA" is somewhat obscure.

--- epan/dissectors/packet-ipv6.c
+++ epan/dissectors/packet-ipv6.c
@@ -125,7 +125,7 @@ static int hf_ipv6_nxt                          = -1;
 static int hf_ipv6_hlim                         = -1;
 static int hf_ipv6_src                          = -1;
 static int hf_ipv6_src_host                     = -1;
-static int hf_ipv6_src_sa_mac                   = -1;
+static int hf_ipv6_src_slaac_mac                = -1;
 static int hf_ipv6_src_isatap_ipv4              = -1;
 static int hf_ipv6_src_6to4_gateway_ipv4        = -1;
 static int hf_ipv6_src_6to4_sla_id              = -1;
@@ -135,7 +135,7 @@ static int hf_ipv6_src_teredo_client_ipv4       = -1;
 static int hf_ipv6_src_embed_ipv4               = -1;
 static int hf_ipv6_dst                          = -1;
 static int hf_ipv6_dst_host                     = -1;
-static int hf_ipv6_dst_sa_mac                   = -1;
+static int hf_ipv6_dst_slaac_mac                = -1;
 static int hf_ipv6_dst_isatap_ipv4              = -1;
 static int hf_ipv6_dst_6to4_gateway_ipv4        = -1;
 static int hf_ipv6_dst_6to4_sla_id              = -1;
@@ -145,7 +145,7 @@ static int hf_ipv6_dst_teredo_client_ipv4       = -1;
 static int hf_ipv6_dst_embed_ipv4               = -1;
 static int hf_ipv6_addr                         = -1;
 static int hf_ipv6_host                         = -1;
-static int hf_ipv6_sa_mac                       = -1;
+static int hf_ipv6_slaac_mac                    = -1;
 static int hf_ipv6_isatap_ipv4                  = -1;
 static int hf_ipv6_6to4_gateway_ipv4            = -1;
 static int hf_ipv6_6to4_sla_id                  = -1;
@@ -2308,7 +2308,7 @@ add_ipv6_address_slaac(packet_info *pinfo, proto_tree *tree, tvbuff_t *tvb, int 
 
     struct { gint mac; } hf[2] = {
         { hf_slaac },
-        { hf_ipv6_sa_mac }
+        { hf_ipv6_slaac_mac }
     };
     proto_item *ti;
     for (int i = 0; i < 2; i++) {
@@ -2488,8 +2488,8 @@ dissect_ipv6(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
         add_ipv6_address_teredo(ipv6_tree, tvb, offset + IP6H_DST,
                 hf_ipv6_dst_teredo_server_ipv4, hf_ipv6_dst_teredo_port, hf_ipv6_dst_teredo_client_ipv4);
 
-        add_ipv6_address_slaac(pinfo, ipv6_tree, tvb, offset + IP6H_SRC, hf_ipv6_src_sa_mac);
-        add_ipv6_address_slaac(pinfo, ipv6_tree, tvb, offset + IP6H_DST, hf_ipv6_dst_sa_mac);
+        add_ipv6_address_slaac(pinfo, ipv6_tree, tvb, offset + IP6H_SRC, hf_ipv6_src_slaac_mac);
+        add_ipv6_address_slaac(pinfo, ipv6_tree, tvb, offset + IP6H_DST, hf_ipv6_dst_slaac_mac);
 
         add_ipv6_address_isatap(ipv6_tree, tvb, offset + IP6H_SRC, hf_ipv6_src_isatap_ipv4);
         add_ipv6_address_isatap(ipv6_tree, tvb, offset + IP6H_DST, hf_ipv6_dst_isatap_ipv4);
@@ -2674,10 +2674,10 @@ proto_register_ipv6(void)
                 FT_STRING, BASE_NONE, NULL, 0x0,
                 "Source IPv6 Host", HFILL }
         },
-        { &hf_ipv6_src_sa_mac,
-            { "Source SA MAC", "ipv6.src_sa_mac",
+        { &hf_ipv6_src_slaac_mac,
+            { "Source SLAAC MAC", "ipv6.src_slaac_mac",
                 FT_ETHER, BASE_NONE, NULL, 0x0,
-                "Source IPv6 Stateless Autoconfiguration MAC Address", HFILL }
+                "Source IPv6 Stateless Address Autoconfiguration (SLAAC) 48-bit MAC Identifier", HFILL }
         },
         { &hf_ipv6_src_isatap_ipv4,
             { "Source ISATAP IPv4", "ipv6.src_isatap_ipv4",
@@ -2724,10 +2724,10 @@ proto_register_ipv6(void)
                 FT_STRING, BASE_NONE, NULL, 0x0,
                 "Destination IPv6 Host", HFILL }
         },
-        { &hf_ipv6_dst_sa_mac,
-            { "Destination SA MAC", "ipv6.dst_sa_mac",
+        { &hf_ipv6_dst_slaac_mac,
+            { "Destination SLAAC MAC", "ipv6.dst_slaac_mac",
                 FT_ETHER, BASE_NONE, NULL, 0x0,
-                "Destination IPv6 Stateless Autoconfiguration MAC Address", HFILL }
+                "Destination IPv6 Stateless Address Autoconfiguration (SLAAC) 48-bit MAC Identifier", HFILL }
         },
         { &hf_ipv6_dst_isatap_ipv4,
             { "Destination ISATAP IPv4", "ipv6.dst_isatap_ipv4",
@@ -2774,10 +2774,10 @@ proto_register_ipv6(void)
                 FT_STRING, BASE_NONE, NULL, 0x0,
                 NULL, HFILL }
         },
-        { &hf_ipv6_sa_mac,
-            { "SA MAC", "ipv6.sa_mac",
+        { &hf_ipv6_slaac_mac,
+            { "SLAAC MAC", "ipv6.slaac_mac",
                 FT_ETHER, BASE_NONE, NULL, 0x0,
-                "IPv6 Stateless Autoconfiguration MAC Address", HFILL }
+                "IPv6 Stateless Address Autoconfiguration (SLAAC) 48-bit MAC Identifier", HFILL }
         },
         { &hf_ipv6_isatap_ipv4,
             { "ISATAP IPv4", "ipv6.isatap_ipv4",
