39036a0a307782537e48688672858a14adf5c6f3
João Valverde <j@v6e.pt>
2021-10-01 17:44:23 +0100

    dfilter: Add some more syntax tests

---
+++ test/suite_dfilter/group_syntax.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2013 by Gilbert Ramirez <gram@alumni.rice.edu>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import unittest
+import fixtures
+from suite_dfilter.dfiltertest import *
+
+
+@fixtures.uses_fixtures
+class case_syntax(unittest.TestCase):
+    trace_file = "http.pcap"
+
+    def test_exists_1(self, checkDFilterCount):
+        dfilter = "frame"
+        checkDFilterCount(dfilter, 1)
+
+    def test_commute_1(self, checkDFilterCount):
+        dfilter = "ip.proto == 6"
+        checkDFilterCount(dfilter, 1)
+
+    def test_commute_2(self, checkDFilterCount):
+        dfilter = "6 == ip.proto"
+        checkDFilterCount(dfilter, 1)
+
+    def test_func_1(self, checkDFilterCount):
+        dfilter = "len(frame) == 207"
+        checkDFilterCount(dfilter, 1)
