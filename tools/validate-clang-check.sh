73c9a3f3b33371309c0700553badba0b5be684f9
Jaap Keuter <jaap.keuter@xs4all.nl>
2021-10-04 18:00:27 +0200

    Update clang validation script
    Port the current script from master, including all new features, and
    adjust the list of files to those relevant for release-3.4

--- tools/validate-clang-check.sh
+++ tools/validate-clang-check.sh
@@ -11,21 +11,35 @@
 #
 
 COMMIT_FILES=$( git diff-index --cached --name-status HEAD^ | grep -v "^D" | cut -f2 | grep "\\.c$\|cpp$" )
+CLANG_CHECK_CMD=clang-check
+
+while getopts c: OPTCHAR
+do
+    case $OPTCHAR in
+    c)
+        CLANG_CHECK_CMD="clang-check-$OPTARG"
+        ;;
+    *)
+        echo "Usage: $( basename "$0" ) [ -c <clang version> ]"
+        exit 0
+    esac
+done
 
 for FILE in $COMMIT_FILES; do
     # Skip some special cases
-    FILE_BASENAME="$(basename $FILE)"
+    FILE_BASENAME="$( basename "$FILE" )"
     # iLBC: the file is not even compiled when ilbc is not installed
     if test "$FILE_BASENAME" = "iLBCdecode.c"
     then
         continue
     fi
-    # extcap/etl.c: that compiles, and is compiled, only on Windows
-    if test "$FILE_BASENAME" = "etl.c"
+    # capture-wpcap.c: this compiles, and is compiled,
+    # only on Windows
+    if test \( "$FILE_BASENAME" = "capture-wpcap.c" \)
     then
         continue
     fi
 
-    clang-check ../$FILE
-    clang-check -analyze ../$FILE
+    "$CLANG_CHECK_CMD" "../$FILE"
+    "$CLANG_CHECK_CMD" -analyze "../$FILE"
 done
