94b28aa79ad65f05b4e765e3211605bc51dcef62
Anders Broman <anders.broman@ericsson.com>
2021-11-02 21:53:34 +0100

    Ericsson ENB log reader: Check the whole line for the magic string.

--- wiretap/eri_enb_log.c
+++ wiretap/eri_enb_log.c
@@ -109,7 +109,7 @@ eri_enb_log_open(wtap *wth, int *err, gchar **err_info)
 		return WTAP_OPEN_NOT_MINE;
 	}
 
-	if (g_strstr_len(line1, strlen(eri_enb_log_magic), eri_enb_log_magic) == NULL)
+	if (g_strstr_len(line1, sizeof(line1), eri_enb_log_magic) == NULL)
 	{
 		return WTAP_OPEN_NOT_MINE;
 	}
