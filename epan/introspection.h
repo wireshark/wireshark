6fab4cbeff3d95d59547b70eecee7df03f9a0b7d
João Valverde <j@v6e.pt>
2021-11-24 16:35:45 +0000

    epan: Add introspection API to export some constants
    C is notoriously difficult to bind from other languages
    without additional metadata. The C ABI does not include
    enums and macros that are an essential component of the
    API.
    
    To make Wireshark instrospectable and more binding friendly
    include an introspection API to export enums and integer macros.
    
    To avoid the tedious need to manually keep the code up to date
    it uses the excellent pyclibrary python package to automatically
    parse C headers and extract this data.
    
    This is not a process that should be done automatically during
    the build.
    
    This could be used for example to replace most of the wslua
    make-init-lua.pl perl script, which tries to do the same thing
    using regular expressions.
    
    Besides the downside of using Perl using regular expressions
    is inferior to pyclibrary in 2 ways: 1) pyclibrary understands
    most of C99 grammar so it is much more powerful; 2) pyclibrary
    has a specific API to extract "values" (enums and constants)
    automagically. We just need to take care to use only integer
    values, for our purposes.

---
+++ epan/introspection.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2021, João Valverde <j@v6e.pt>
+ *
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef _INTROSPECTION_H_
+#define _INTROSPECTION_H_
+
+#include <ws_symbol_export.h>
+
+typedef struct {
+    const char *symbol;
+    int value;
+} ws_enum_t;
+
+/** Returns an array of ws_enum_t elements. The array is sorted and
+ * ends with {NULL, 0}.
+ *
+ * It can be used by language bindings to the Wireshark API to obtain
+ * the value of some magic constants. The array can be binary searched,
+ * imported to a hash table, serialized, etc.
+ */
+WS_DLL_PUBLIC
+const ws_enum_t *epan_inspect_enums(void);
+
+#endif
---
+++ tools/make-enums.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021, João Valverde <j@v6e.pt>
+#
+# Wireshark - Network traffic analyzer
+# By Gerald Combs <gerald@wireshark.org>
+# Copyright 1998 Gerald Combs
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+#
+# Uses pyclibrary to parse C headers for enums and integer macro
+# definitions. Exports that data to a C file for the introspection API.
+#
+# Requires: https://github.com/MatthieuDartiailh/pyclibrary
+#
+
+import os
+import sys
+import argparse
+from pyclibrary import CParser
+
+argp = argparse.ArgumentParser()
+argp.add_argument("-o", "--outfile")
+argp.add_argument("infiles", nargs="*")
+args = argp.parse_args()
+
+parser = CParser(args.infiles)
+
+source = """\
+/*
+ * Wireshark - Network traffic analyzer
+ * By Gerald Combs <gerald@wireshark.org>
+ * Copyright 1998 Gerald Combs
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Generated automatically from %s.
+ *
+ * It can be re-created using "make gen-enums".
+ *
+ * It is fine to edit this file by hand. Particularly if a symbol
+ * disappears from the API it can just be removed here. There is no
+ * requirement to re-run the generator script.
+ *
+ */
+#include "config.h"
+#include "introspection.h"
+""" % (os.path.basename(sys.argv[0]))
+
+for f in args.infiles:
+    source += '#include <{}>\n'.format(f)
+
+source += """
+#define ENUM(arg) { #arg, arg }
+
+static ws_enum_t all_enums[] = {
+"""
+
+definitions = parser.defs['values']
+symbols = list(definitions.keys())
+symbols.sort()
+
+for s in symbols:
+    if isinstance(definitions[s], int):
+        source += '    ENUM({}),\n'.format(s)
+
+source += """\
+    { NULL, 0 },
+};
+
+const ws_enum_t *epan_inspect_enums(void)
+{
+    return all_enums;
+}
+"""
+
+try:
+    if args.outfile:
+        fh = open(args.outfile, 'w')
+    else:
+        fh = sys.stdout
+except OSError:
+    sys.exit('Unable to write ' + args.outfile + '.\n')
+
+fh.write(source)
+fh.close()
+
+
+#
+# Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+#
+# Local variables:
+# c-basic-offset: 4
+# indent-tabs-mode: nil
+# End:
+#
+# vi: set shiftwidth=4 expandtab:
+# :indentSize=4:noTabs=true:
+#
