c6e6f21685f593d4be8dd5596cc802a5a5d6f495
AndersBroman <a.broman58@gmail.com>
2021-11-30 18:36:30 +0000

    proto.c: Increase PROTO_PRE_ALLOC_HF_FIELDS_MEM

--- epan/proto.c
+++ epan/proto.c
@@ -8833,7 +8833,7 @@ register_string_errors(void)
 	proto_set_cant_toggle(proto_string_errors);
 }
 
-#define PROTO_PRE_ALLOC_HF_FIELDS_MEM (240000+PRE_ALLOC_EXPERT_FIELDS_MEM)
+#define PROTO_PRE_ALLOC_HF_FIELDS_MEM (260000+PRE_ALLOC_EXPERT_FIELDS_MEM)
 static int
 proto_register_field_init(header_field_info *hfinfo, const int parent)
 {
