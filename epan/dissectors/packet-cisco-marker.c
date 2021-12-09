497d03aedd0a259e92e89a438be89f1d2df53002
João Valverde <j@v6e.pt>
2021-12-09 03:02:25 +0000

    ERSPAN: Convert to old proto tree API
    Related with #17774.

--- epan/dissectors/packet-cisco-marker.c
+++ epan/dissectors/packet-cisco-marker.c
@@ -14,9 +14,6 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
-
-#define NEW_PROTO_TREE_API
-
 #include "config.h"
 
 #include <epan/packet.h>
@@ -27,88 +24,21 @@ void proto_reg_handoff_erspan_marker(void);
 
 static dissector_handle_t marker_handle;
 
-static header_field_info *hfi_marker = NULL;
+static int proto_marker = -1;
 
-
-#define CISCO_ERSPAN_MARKER_HFI_INIT HFI_INIT(proto_marker)
-
-static header_field_info cisco_erspan_prop_header CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Proprietary CISCO Header", "cisco_erspan_marker.prop_header",
-  FT_BYTES, BASE_NONE, NULL, 0x0,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_info CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Header", "cisco_erspan_marker.header",
-  FT_BOOLEAN, 8, NULL, 0x0,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_version CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Version", "cisco_erspan_marker.version",
-  FT_UINT16, BASE_DEC, NULL, 0x0f00,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_type CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Type", "cisco_erspan_marker.type",
-  FT_UINT16, BASE_DEC, NULL, 0xf000,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_ssid CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "SSID", "cisco_erspan_marker.ssid",
-  FT_UINT16, BASE_DEC, NULL, 0x00ff,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_granularity CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Granularity", "cisco_erspan_marker.granularity",
-  FT_UINT16, BASE_DEC, NULL, 0xff00,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_utcoffset CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "UTC Offset", "cisco_erspan_marker.utc_offset",
-  FT_UINT16, BASE_DEC, NULL, 0x00ff,
-  NULL, HFILL };
-
-/* Timestamp is actually a 48-bit value, packed across 2 32-bit integers
- * Timestamp_hi : 0000 ffff (high 16-bits)
- * Timestamp_lo : ffff ffff (low 32-bits) */
-static header_field_info cisco_erspan_timestamp CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "ASIC 48-bit Timestamp", "cisco_erspan_marker.timestamp",
-  FT_UINT48, BASE_DEC, NULL, 0xffffffffffff,
-  NULL, HFILL };
-
-/* Comparison between the actual packet arrival time and this field
- * indicated that the Ethernet packet's arrival time was behind
- * the below field value by the value of the  UTC offset
- * (37 seconds as of Nov 2021) */
-static header_field_info cisco_erspan_utc_sec CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "UTC Seconds", "cisco_erspan_marker.utc_sec",
-  FT_UINT32, BASE_DEC, NULL, 0xffffffff,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_utc_usec CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "UTC Microseconds", "cisco_erspan_marker.utc_usec",
-  FT_UINT32, BASE_DEC, NULL, 0xffffffff,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_sequence_number CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Sequence Number", "cisco_erspan_marker.sequence_number",
-  FT_UINT32, BASE_DEC, NULL, 0xffffffff,
-  NULL, HFILL };
-
-static header_field_info cisco_erspan_reserved CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "Reserved", "cisco_erspan_marker.reserved",
-  FT_UINT32, BASE_DEC, NULL, 0xffffffff,
-  NULL, HFILL };
-
-/* The 32-bit signature is expected to be 0xA5A5A5A5,
- * and while the Cisco documentation does not mention packing details,
- * it does mention padding values to enforce alignment */
-static header_field_info cisco_erspan_tail CISCO_ERSPAN_MARKER_HFI_INIT =
-{ "TAIL", "cisco_erspan_marker.tail",
-  FT_UINT64, BASE_HEX, NULL, 0x00000000ffffffff,
-  NULL, HFILL };
-
-
-
+static int hf_cisco_erspan_granularity = -1;
+static int hf_cisco_erspan_info = -1;
+static int hf_cisco_erspan_prop_header = -1;
+static int hf_cisco_erspan_reserved = -1;
+static int hf_cisco_erspan_sequence_number = -1;
+static int hf_cisco_erspan_ssid = -1;
+static int hf_cisco_erspan_tail = -1;
+static int hf_cisco_erspan_timestamp = -1;
+static int hf_cisco_erspan_type = -1;
+static int hf_cisco_erspan_utc_sec = -1;
+static int hf_cisco_erspan_utc_usec = -1;
+static int hf_cisco_erspan_utcoffset = -1;
+static int hf_cisco_erspan_version = -1;
 
 
 static gint ett_marker = -1;
@@ -128,38 +58,38 @@ dissect_marker(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _
     /* Skip the proprietary CISCO header - no docs have been released for this */
     guint32 offset = 20;
 
-    ti = proto_tree_add_item(tree, hfi_marker, tvb, 0, -1, ENC_NA);
+    ti = proto_tree_add_item(tree, proto_marker, tvb, 0, -1, ENC_NA);
     marker_tree = proto_item_add_subtree(ti, ett_marker);
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_prop_header, tvb, 0, 20, ENC_LITTLE_ENDIAN);
-    proto_tree_add_item(marker_tree, &cisco_erspan_info, tvb, offset, 2, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_prop_header, tvb, 0, 20, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_info, tvb, offset, 2, ENC_LITTLE_ENDIAN);
     offset += 2;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_version, tvb, offset, 2, ENC_BIG_ENDIAN);
-    proto_tree_add_item(marker_tree, &cisco_erspan_type, tvb, offset, 2, ENC_BIG_ENDIAN);
-    proto_tree_add_item(marker_tree, &cisco_erspan_ssid, tvb, offset, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_version, tvb, offset, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_type, tvb, offset, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_ssid, tvb, offset, 2, ENC_BIG_ENDIAN);
     offset += 2;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_granularity, tvb, offset, 2, ENC_BIG_ENDIAN);
-    proto_tree_add_item(marker_tree, &cisco_erspan_utcoffset, tvb, offset, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_granularity, tvb, offset, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_utcoffset, tvb, offset, 2, ENC_BIG_ENDIAN);
     offset+= 2;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_timestamp, tvb, offset, 8, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_timestamp, tvb, offset, 8, ENC_BIG_ENDIAN);
     offset+=8;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_utc_sec, tvb, offset, 4, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_utc_sec, tvb, offset, 4, ENC_BIG_ENDIAN);
     offset+=4;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_utc_usec, tvb, offset, 4, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_utc_usec, tvb, offset, 4, ENC_BIG_ENDIAN);
     offset+=4;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_sequence_number, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_sequence_number, tvb, offset, 4, ENC_LITTLE_ENDIAN);
     offset+=4;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_reserved, tvb, offset, 4, ENC_LITTLE_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_reserved, tvb, offset, 4, ENC_LITTLE_ENDIAN);
     offset+=4;
 
-    proto_tree_add_item(marker_tree, &cisco_erspan_tail, tvb, offset, 8, ENC_BIG_ENDIAN);
+    proto_tree_add_item(marker_tree, hf_cisco_erspan_tail, tvb, offset, 8, ENC_BIG_ENDIAN);
   }
   return tvb_captured_length(tvb);
 }
@@ -168,39 +98,92 @@ dissect_marker(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _
 void
 proto_register_erspan_marker(void)
 {
-
-
-#ifndef HAVE_HFI_SECTION_INIT
-  static header_field_info *hfi[] = {
-    &cisco_erspan_prop_header,
-    &cisco_erspan_info,
-    &cisco_erspan_version,
-    &cisco_erspan_type,
-    &cisco_erspan_ssid,
-    &cisco_erspan_granularity,
-    &cisco_erspan_utcoffset,
-    &cisco_erspan_timestamp,
-    &cisco_erspan_utc_sec,
-    &cisco_erspan_utc_usec,
-    &cisco_erspan_sequence_number,
-    &cisco_erspan_reserved,
-    &cisco_erspan_tail
+  static hf_register_info hf[] = {
+    { &hf_cisco_erspan_prop_header,
+      { "Proprietary CISCO Header", "cisco_erspan_marker.prop_header",
+        FT_BYTES, BASE_NONE, NULL, 0x0,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_info,
+      { "Header", "cisco_erspan_marker.header",
+        FT_BOOLEAN, 8, NULL, 0x0,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_version,
+      { "Version", "cisco_erspan_marker.version",
+        FT_UINT16, BASE_DEC, NULL, 0x0f00,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_type,
+      { "Type", "cisco_erspan_marker.type",
+        FT_UINT16, BASE_DEC, NULL, 0xf000,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_ssid,
+      { "SSID", "cisco_erspan_marker.ssid",
+        FT_UINT16, BASE_DEC, NULL, 0x00ff,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_granularity,
+      { "Granularity", "cisco_erspan_marker.granularity",
+        FT_UINT16, BASE_DEC, NULL, 0xff00,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_utcoffset,
+      { "UTC Offset", "cisco_erspan_marker.utc_offset",
+        FT_UINT16, BASE_DEC, NULL, 0x00ff,
+        NULL, HFILL }
+    },
+    /* Timestamp is actually a 48-bit value, packed across 2 32-bit integers
+     * Timestamp_hi : 0000 ffff (high 16-bits)
+     * Timestamp_lo : ffff ffff (low 32-bits) */
+    { &hf_cisco_erspan_timestamp,
+      { "ASIC 48-bit Timestamp", "cisco_erspan_marker.timestamp",
+        FT_UINT48, BASE_DEC, NULL, 0xffffffffffff,
+        NULL, HFILL }
+    },
+    /* Comparison between the actual packet arrival time and this field
+     * indicated that the Ethernet packet's arrival time was behind
+     * the below field value by the value of the  UTC offset
+     * (37 seconds as of Nov 2021) */
+    { &hf_cisco_erspan_utc_sec,
+      { "UTC Seconds", "cisco_erspan_marker.utc_sec",
+        FT_UINT32, BASE_DEC, NULL, 0xffffffff,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_utc_usec,
+      { "UTC Microseconds", "cisco_erspan_marker.utc_usec",
+        FT_UINT32, BASE_DEC, NULL, 0xffffffff,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_sequence_number,
+      { "Sequence Number", "cisco_erspan_marker.sequence_number",
+        FT_UINT32, BASE_DEC, NULL, 0xffffffff,
+        NULL, HFILL }
+    },
+    { &hf_cisco_erspan_reserved,
+      { "Reserved", "cisco_erspan_marker.reserved",
+        FT_UINT32, BASE_DEC, NULL, 0xffffffff,
+        NULL, HFILL }
+    },
+    /* The 32-bit signature is expected to be 0xA5A5A5A5,
+     * and while the Cisco documentation does not mention packing details,
+     * it does mention padding values to enforce alignment */
+    { &hf_cisco_erspan_tail,
+      { "TAIL", "cisco_erspan_marker.tail",
+        FT_UINT64, BASE_HEX, NULL, 0x00000000ffffffff,
+        NULL, HFILL }
+    },
   };
-#endif
 
   static gint *ett[] = {
     &ett_marker,
   };
 
-  int proto_marker;
-
-
 
   proto_marker = proto_register_protocol("CISCO ERSPAN3 Marker Packet", "CISCO3 ERSPAN MARKER", "cisco-erspan3-marker");
 
-  hfi_marker = proto_registrar_get_nth(proto_marker);
-
-  proto_register_fields(proto_marker, hfi, array_length(hfi));
+  proto_register_field_array(proto_marker, hf, array_length(hf));
   proto_register_subtree_array(ett, array_length(ett));
 
   marker_handle = create_dissector_handle(dissect_marker, proto_marker);
