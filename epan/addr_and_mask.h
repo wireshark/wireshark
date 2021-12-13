6ecc6a2ab4a147d5d4f34e849a97e0185db43f9a
Moshe Kaplan <me@moshekaplan.com>
2021-12-13 00:44:14 -0500

    epan/addr_and_mask.h: Document functions
    Document functions in epan/addr_and_mask.h
    so doxygen documentation is generated for them.

--- epan/addr_and_mask.h
+++ epan/addr_and_mask.h
@@ -30,12 +30,38 @@ extern "C" {
 #define PREFIX_LEN_TOO_LONG	1
 #define PREFIX_LEN_ZERO		2
 
+/**
+* Fetches an IPv4 address from a tvbuff and
+* masks out bits other than those covered by a prefix length
+*
+* @param tvb tvbuff to read an IPv4 address from
+* @param offset offset in the tvbuff to read the IPv4 address from
+* @param addr memory location where the IPv4 address read should be stored
+* @param prefix_len the length of the prefix (in bits)
+* @return the length (in bytes) of the address on success, or -1 on failure
+*/
 extern int tvb_get_ipv4_addr_with_prefix_len(tvbuff_t *tvb, int offset,
     ws_in4_addr *addr, guint32 prefix_len);
 
+/**
+* Fetches an IPv6 address from a tvbuff and
+* masks out bits other than those covered by a prefix length
+*
+* @param tvb tvbuff to read an IPv6 address from
+* @param offset offset in the tvbuff to read the IPv6 address from
+* @param addr memory location where the IPv6 address read should be stored
+* @param prefix_len the length of the prefix (in bits)
+* @return the length (in bytes) of the address on success, or -1 on failure
+*/
 extern int tvb_get_ipv6_addr_with_prefix_len(tvbuff_t *tvb, int offset,
     ws_in6_addr *addr, guint32 prefix_len);
 
+/**
+* Returns the IPv4 subnet mask of the specified length
+*
+* @param mask_length the number of bits in the subnet mask (max of 32)
+* @return the subnet mask of the specified length
+*/
 guint32 ip_get_subnet_mask(const guint32 mask_length);
 
 #ifdef __cplusplus
