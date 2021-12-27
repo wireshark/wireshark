a0f1ffec7165687c8f7ca9e10db5d0e4be015069
João Valverde <j@v6e.pt>
2021-12-27 19:52:12 +0000

    epan: Remove obsolete header "garrayfix.h"

--- epan/dissectors/packet-xml.c
+++ epan/dissectors/packet-xml.c
@@ -25,7 +25,6 @@
 #include <wsutil/filesystem.h>
 #include <epan/prefs.h>
 #include <epan/expert.h>
-#include <epan/garrayfix.h>
 #include <wsutil/str_util.h>
 #include <wsutil/report_message.h>
 
@@ -1321,7 +1320,7 @@ static void register_dtd(dtd_build_data_t *dtd_data, GString *errors)
 
         root_element->hf_tag = proto_register_protocol(full_name, short_name, short_name);
         proto_register_field_array(root_element->hf_tag, (hf_register_info*)wmem_array_get_raw(hfs), wmem_array_get_count(hfs));
-        proto_register_subtree_array((gint **)g_array_data(etts), etts->len);
+        proto_register_subtree_array((gint **)etts->data, etts->len);
 
         if (dtd_data->media_type) {
             gchar* media_type = wmem_strdup(wmem_epan_scope(), dtd_data->media_type);
@@ -1511,7 +1510,7 @@ proto_register_xml(void)
     xml_ns.hf_tag = proto_register_protocol("eXtensible Markup Language", "XML", xml_ns.name);
 
     proto_register_field_array(xml_ns.hf_tag, (hf_register_info*)wmem_array_get_raw(hf_arr), wmem_array_get_count(hf_arr));
-    proto_register_subtree_array((gint **)g_array_data(ett_arr), ett_arr->len);
+    proto_register_subtree_array((gint **)ett_arr->data, ett_arr->len);
     expert_xml = expert_register_protocol(xml_ns.hf_tag);
     expert_register_field_array(expert_xml, ei, array_length(ei));
 
