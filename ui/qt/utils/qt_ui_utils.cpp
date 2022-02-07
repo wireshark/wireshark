81f220ae80a34b748ea8c3d7f129c444642a1051
Roland Knall <r.knall@moba.cc>
2022-02-07 16:54:07 +0100

    Qt: Fix open explorer
    Apparently on Windows, if a dialog contains spaces it is not properly
    opened via /select. But opening the file via QDesktopServices leads to
    the file not being selected in the open explorer windows (expectation by
    the user). Therefore the original change is restored, but with a
    differenc call which should handle the space issue in most cases better
    than before. See
    https://stackoverflow.com/questions/3490336/how-to-reveal-in-finder-or-show-in-explorer-with-qt
    for a short explanation (bottom of post)
    
    Fixes #17927

--- ui/qt/utils/qt_ui_utils.cpp
+++ ui/qt/utils/qt_ui_utils.cpp
@@ -196,7 +196,17 @@ void desktop_show_in_folder(const QString file_path)
 {
     bool success = false;
 
-#if defined(Q_OS_MAC)
+#if defined(Q_OS_WIN)
+    const QFileInfo fileInfo(file_path);
+
+
+    QString path = QDir::toNativeSeparators(file_path);
+    /* See comment at https ://stackoverflow.com/questions/3490336/how-to-reveal-in-finder-or-show-in-explorer-with-qt
+     * Apparently, Windows does not like quoted paths in Select statements, when given as arguments. So they are run as a command
+     */
+    QString command = "explorer.exe /select," + path + "";
+    success = QProcess::startDetached(command);
+#elif defined(Q_OS_MAC)
     QStringList script_args;
     QString escaped_path = file_path;
 
