1aef88df4b4820196a1db0c4828b2c612c8632d9
João Valverde <j@v6e.pt>
2022-02-23 18:05:30 +0000

    dfilter: Fix node debug representation

--- epan/dfilter/syntax-tree.c
+++ epan/dfilter/syntax-tree.c
@@ -328,7 +328,7 @@ sprint_node(stnode_t *node)
 	wmem_strbuf_append_printf(buf, "stnode{ ");
 	wmem_strbuf_append_printf(buf, "magic=0x%"PRIx32", ", node->magic);
 	wmem_strbuf_append_printf(buf, "type=%s, ", stnode_type_name(node));
-	wmem_strbuf_append_printf(buf, "data=<%s>, ", stnode_todisplay(node));
+	wmem_strbuf_append_printf(buf, "data=<%s>, ", stnode_todebug(node));
 	wmem_strbuf_append_printf(buf, "flags=0x%04"PRIx16" }", node->flags);
 	return wmem_strbuf_finalize(buf);
 }
