0a362d7daa304bf0df0312c4b28cb9fb2d2b5715
John Thacker <johnthacker@gmail.com>
2022-02-26 14:41:40 -0500

    LI5G: Add value strings
    Add value strings and units as appropriate from ETSI TS 103 221-2
    V1.1.1. (The dissector still needs to be updated to V1.4.1)
    Also change the XID field to a FT_GUID, as it is a version 4
    UUID per RFC 4122.

--- epan/dissectors/packet-li5g.c
+++ epan/dissectors/packet-li5g.c
@@ -10,6 +10,7 @@
  */
 #include "config.h"
 #include <epan/packet.h>
+#include <epan/ipproto.h>
 
 void proto_reg_handoff_li5g(void);
 void proto_register_li5g(void);
@@ -40,12 +41,62 @@ static int hf_li5g_attrContents[LI_5G_ATTR_TYPE_MAX];
 static dissector_handle_t li5g_handle;
 static dissector_handle_t subProtocol_handle[LI_5G_PAYLOAD_FORMAT_MAX]={NULL};
 
-static gchar* pdu_info_str[] = {
-    "",
-    "x2 xIRI",
-    "x3 CC",
-    "Keepalive",
-    "Keepalive Acknowledgement"
+static const value_string pdu_type_vals[] = {
+    {1, "X2 xIRI"},
+    {2, "X3 xCC"},
+    {3, "Keepalive"},
+    {4, "Keepalive Acknowledgement"},
+    {0, NULL}
+};
+
+static const value_string payload_format_vals[] = {
+    { 0, "Reserved for Keepalive"},
+    { 1, "ETSI TS 102 232-1 Defined Payload"},
+    { 2, "3GPP TS 33.128 Defined Payload"},
+    { 3, "3GPP TS 33.108 Defined Payload"},
+    { 4, "Proprietary Payload"},
+    { 5, "IPv4 Packet"},
+    { 6, "IPv6 Packet"},
+    { 7, "Ethernet Frame"},
+    { 8, "RTP Packet"},
+    { 9, "SIP Message"},
+    {10, "DHCP Message"},
+    {11, "RADIUS Packet"},
+    {12, "GTP-U Message"},
+    {13, "MSRP Message"},
+    { 0, NULL}
+};
+
+static const value_string payload_dir_vals[] = {
+    {0, "Reserved for Keepalive"},
+    {1, "The direction of the intercepted data or event is not known to the POI"},
+    {2, "The intercepted data or event was sent to (i.e. received by) the target"},
+    {3, "The intercepted data or event was sent from the target"},
+    {4, "The intercepted data or event is a result of intercepted data or events in more than one direction"},
+    {5, "The concept of direction is not applicable to this intercepted data or event"},
+    {0, NULL}
+};
+
+static const value_string attribute_type_vals[] = {
+    { 1, "ETSI TS 102 232-1 Defined Attribute"},
+    { 2, "3GPP TS 33.128 Defined Attribute"},
+    { 3, "3GPP TS 33.108 Defined Attribute"},
+    { 4, "Proprietary Attribute"},
+    { 5, "Domain ID (DID)"},
+    { 6, "Network Function ID (NFID)"},
+    { 7, "Interception Point ID (IPID)"},
+    { 8, "Sequence Number"},
+    { 9, "Timestamp"},
+    {10, "Source IPv4 Address"},
+    {11, "Destination IPv4 Address"},
+    {12, "Source IPv6 Address"},
+    {13, "Destination IPv6 Address"},
+    {14, "Source Port"},
+    {15, "Destination Port"},
+    {16, "IP Protocol"},
+    {17, "Matched Target Identifier"},
+    {18, "Other Target Identifier"},
+    {0, NULL}
 };
 
 static int
@@ -54,15 +105,15 @@ dissect_li5g(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
     proto_tree  *li5g_tree, *attr_tree, *parent=NULL;
     proto_item  *ti, *attr_ti;
     int offset = LI_5G_HEADER_LEN_MIN, hf_attr = -1;
-    guint32 headerLen, payloadLen;
-    guint16 pduType, payloadFormat, attrType, attrLen;
+    guint32 headerLen, payloadLen, pduType;
+    guint16 payloadFormat, attrType, attrLen;
+    const char* info;
 
     address src_addr;
     address dst_addr;
     guint32 src_port;
     guint32 dst_port;
 
-    pduType = tvb_get_ntohs(tvb, 2);
     headerLen = tvb_get_ntohl(tvb, 4);
     payloadLen = tvb_get_ntohl(tvb, 8);
     payloadFormat = tvb_get_ntohs(tvb, 12);
@@ -70,7 +121,7 @@ dissect_li5g(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
     ti = proto_tree_add_item(tree, proto_li5g, tvb, 0, headerLen+payloadLen, ENC_NA);
     li5g_tree = proto_item_add_subtree(ti, ett_li5g);
     proto_tree_add_item(li5g_tree, hf_li5g_version, tvb, 0, 2, ENC_BIG_ENDIAN);
-    proto_tree_add_item(li5g_tree, hf_li5g_pduType, tvb, 2, 2, ENC_BIG_ENDIAN);
+    proto_tree_add_item_ret_uint(li5g_tree, hf_li5g_pduType, tvb, 2, 2, ENC_BIG_ENDIAN, &pduType);
     proto_tree_add_item(li5g_tree, hf_li5g_headerLen, tvb, 4, 4, ENC_BIG_ENDIAN);
     proto_tree_add_item(li5g_tree, hf_li5g_payloadLen, tvb, 8, 4, ENC_BIG_ENDIAN);
     proto_tree_add_item(li5g_tree, hf_li5g_payloadFormat, tvb, 12, 2, ENC_BIG_ENDIAN);
@@ -124,8 +175,10 @@ dissect_li5g(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U_
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "5GLI");
     col_clear_fence(pinfo->cinfo, COL_INFO);
     col_clear(pinfo->cinfo, COL_INFO);
-    if (pduType <= 4)
-        col_set_str(pinfo->cinfo, COL_INFO, pdu_info_str[pduType]);
+    info = try_val_to_str(pduType, pdu_type_vals);
+    if (info != NULL) {
+        col_set_str(pinfo->cinfo, COL_INFO, info);
+    }
 
     /* copy back to the original value when return from innner protocol */
     copy_address_shallow(&pinfo->src, &src_addr);
@@ -163,16 +216,16 @@ proto_register_li5g(void)
 
     static hf_register_info hf[] = {
         { &hf_li5g_version, { "Version", "li5g.ver", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_pduType, { "PDU Type", "li5g.type", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_headerLen, { "Header Length", "li5g.hl", FT_UINT32, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_payloadLen, { "Payload Length", "li5g.pl", FT_UINT32, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_payloadFormat, { "Payload Format", "li5g.pf", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_payloadDirection, { "Payload Direction", "li5g.pd", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_xid, { "XID", "li5g.xid", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
+        { &hf_li5g_pduType, { "PDU Type", "li5g.type", FT_UINT16, BASE_DEC, VALS(pdu_type_vals), 0x0, NULL, HFILL }},
+        { &hf_li5g_headerLen, { "Header Length", "li5g.hl", FT_UINT32, BASE_DEC|BASE_UNIT_STRING, &units_octet_octets, 0x0, NULL, HFILL }},
+        { &hf_li5g_payloadLen, { "Payload Length", "li5g.pl", FT_UINT32, BASE_DEC|BASE_UNIT_STRING, &units_octet_octets, 0x0, NULL, HFILL }},
+        { &hf_li5g_payloadFormat, { "Payload Format", "li5g.pf", FT_UINT16, BASE_DEC, VALS(payload_format_vals), 0x0, NULL, HFILL }},
+        { &hf_li5g_payloadDirection, { "Payload Direction", "li5g.pd", FT_UINT16, BASE_DEC, VALS(payload_dir_vals), 0x0, NULL, HFILL }},
+        { &hf_li5g_xid, { "XID", "li5g.xid", FT_GUID, BASE_NONE, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_cid, { "Correlation ID", "li5g.cid", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
 
-        { &hf_li5g_attrType, { "Attribute Type", "li5g.attrType", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_attrLen, { "Attribute Length", "li5g.attrLen", FT_UINT16, BASE_DEC, NULL, 0x0, NULL, HFILL }},
+        { &hf_li5g_attrType, { "Attribute Type", "li5g.attrType", FT_UINT16, BASE_DEC, VALS(attribute_type_vals), 0x0, NULL, HFILL }},
+        { &hf_li5g_attrLen, { "Attribute Length", "li5g.attrLen", FT_UINT16, BASE_DEC|BASE_UNIT_STRING, &units_octet_octets, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[5], { "Domain ID", "li5g.did", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[6], { "Network Function ID", "li5g.nfid", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[7], { "Interception Point ID", "li5g.ipid", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
@@ -184,7 +237,7 @@ proto_register_li5g(void)
         { &hf_li5g_attrContents[13], { "Destination IPv6 address", "li5g.dstipv6", FT_IPv6, BASE_NONE, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[14], { "Source Port", "li5g.srcport", FT_UINT16, BASE_PT_TCP, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[15], { "Destination Port", "li5g.dstport", FT_UINT16, BASE_PT_TCP, NULL, 0x0, NULL, HFILL }},
-        { &hf_li5g_attrContents[16], { "IP Protocol", "li5g.ipproto", FT_UINT8, BASE_DEC, NULL, 0x0, NULL, HFILL }},
+        { &hf_li5g_attrContents[16], { "IP Protocol", "li5g.ipproto", FT_UINT8, BASE_DEC|BASE_EXT_STRING, &ipproto_val_ext, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[17], { "Matched Target Identifier", "li5g.mti", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
         { &hf_li5g_attrContents[18], { "Other Target Identifier", "li5g.oti", FT_BYTES, BASE_NONE, NULL, 0x0, NULL, HFILL }},
 
