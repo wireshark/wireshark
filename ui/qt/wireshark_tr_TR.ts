c04472160220fc8842bd6dea0d305a1b6673ad39
Gerald Combs <gerald@wireshark.org>
2022-03-27 16:40:01 +0000

    [Automatic update for 2022-03-27]
    Update manuf, services enterprise numbers, translations, and other items.

--- ui/qt/wireshark_tr_TR.ts
+++ ui/qt/wireshark_tr_TR.ts
@@ -970,7 +970,7 @@
     </message>
     <message>
         <source>Hash (SHA256)</source>
-        <translation>Karma SHA256)</translation>
+        <translation>Karma (SHA256)</translation>
     </message>
     <message>
         <source>Hash (RIPEMD160)</source>
@@ -3852,7 +3852,7 @@ For example, use 1 hour to have a new file created every hour on the hour.</sour
     </message>
     <message>
         <source>Time (s)</source>
-        <translation>Süre</translation>
+        <translation>Süre ()</translation>
     </message>
     <message>
         <source>I/O Graphs</source>
@@ -6779,7 +6779,7 @@ For example, use 1 hour to have a new file created every hour on the hour.</sour
     <message>
         <source>&amp;Mark/Unmark Packet(s)</source>
         <oldsource>&amp;Mark/Unmark Packet</oldsource>
-        <translation>&amp;Paket(ler)i İşaretle/İşaretini Kaldır</translation>
+        <translation>&amp;Paketi İşaretle/İşaretini Kaldır</translation>
     </message>
     <message>
         <source>Mark All Displayed</source>
@@ -6812,7 +6812,7 @@ For example, use 1 hour to have a new file created every hour on the hour.</sour
     <message>
         <source>&amp;Ignore/Unignore Packet(s)</source>
         <oldsource>&amp;Ignore/Unignore Packet</oldsource>
-        <translation>&amp;Paket(ler)i Yoksay/Yoksay</translation>
+        <translation>&amp;Paketi Yoksay/Yoksaymaktan vazgeç</translation>
     </message>
     <message>
         <source>Ignore All Displayed</source>
@@ -9676,11 +9676,11 @@ For example, use 1 hour to have a new file created every hour on the hour.</sour
     </message>
     <message>
         <source>Time (s)</source>
-        <translation>Süre</translation>
+        <translation>Süre ()</translation>
     </message>
     <message>
         <source>Window Size (B)</source>
-        <translation>Pencere Boyutu</translation>
+        <translation>Pencere Boyutu (B)</translation>
     </message>
     <message>
         <source>[no capture file]</source>
@@ -10756,7 +10756,7 @@ For example, use 1 hour to have a new file created every hour on the hour.</sour
     </message>
     <message>
         <source>Time Span (s)</source>
-        <translation>Zaman Aralığı</translation>
+        <translation>Zaman Aralığı (s)</translation>
     </message>
     <message>
         <source>Payloads</source>
