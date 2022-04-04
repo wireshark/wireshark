5971fcb9414353fe8bf9bed9ef192728799d4389
Gerald Combs <gerald@wireshark.org>
2022-04-04 12:21:29 -0700

    Qt: Remove some unused code.

--- ui/qt/main_window_layout.cpp
+++ ui/qt/main_window_layout.cpp
@@ -33,14 +33,6 @@
 
 #include <wsutil/ws_assert.h>
 
-/*
- * The generated Ui_MainWindow::setupUi() can grow larger than our configured limit,
- * so turn off -Wframe-larger-than= for ui_main_window.h.
- */
-//DIAG_OFF(frame-larger-than=)
-//#include <ui_main_window.h>
-//DIAG_ON(frame-larger-than=)
-
 void MainWindow::showWelcome()
 {
     main_stack_->setCurrentWidget(welcome_page_);
