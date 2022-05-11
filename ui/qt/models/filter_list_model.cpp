25515d21ed5321421fb20dc660873ade21d289ee
Roland Knall <rknall@gmail.com>
2022-05-11 12:22:29 +0000

    Qt: Fix display filter read/write
    Display filters on Windows where broken in such a way that the file was generated with empty lines and the wrong file delimiters.
    
    Likewise, loading the file lead to issues when the formatting was not 100% match by the plattform being used
    
    Fixes #18082
    
    
    (cherry picked from commit ad28888d2f5d98c4dfa5a689f6f0626aad44c60d)

--- ui/qt/models/filter_list_model.cpp
+++ ui/qt/models/filter_list_model.cpp
@@ -73,16 +73,24 @@ void FilterListModel::reload()
         return;
 
     QTextStream in(&file);
-    QRegularExpression rx("\\s*\\\"(.*?)\\\"\\s(.*)");
+    QRegularExpression rx("\\s*\\\"\\s*(.*?)\\s*\\\"\\s(.*)");
     while (!in.atEnd())
     {
-        QString line = in.readLine().trimmed();
-        if (line.startsWith("#") || line.indexOf("\"") <= -1)
+        QString data = in.readLine().trimmed();
+        /* Filter out lines that do not contain content:
+        *  - Starting with # is a comment
+        *  - Does not start with a quoted string
+        */ 
+        if (data.startsWith("#") || ! data.trimmed().startsWith("\""))
             continue;
 
-        QRegularExpressionMatch match = rx.match(line);
-        if (match.hasMatch()) {
-            addFilter(match.captured(1), match.captured(2));
+        QStringList content = data.split(QChar('\n'));
+        foreach (QString line, content)
+        {
+            QRegularExpressionMatch match = rx.match(line);
+            if (match.hasMatch()) {
+                addFilter(match.captured(1).trimmed(), match.captured(2).trimmed());
+            }
         }
     }
 }
@@ -233,15 +241,14 @@ void FilterListModel::saveList()
     QTextStream out(&file);
     for (int row = 0; row < rowCount(); row++)
     {
-        QString line = QString("\"%1\"").arg(index(row, ColumnName).data().toString());
+        QString line = QString("\"%1\"").arg(index(row, ColumnName).data().toString().trimmed());
         line.append(QString(" %1").arg(index(row, ColumnExpression).data().toString()));
 
-#ifdef _WIN32
-        line = line.append("\r\n");
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+        out << line << Qt::endl;
 #else
-        line = line.append("\n");
+        out << line << endl;
 #endif
-        out << line;
     }
 
     file.close();
