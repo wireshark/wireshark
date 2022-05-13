wireshark/wireshark

commit 1e0d117eb7ab1ce7f4ff8a4fd6dc2529634d7baa

Specify directory for temporary captures

wiretap/merge.c
@@ -1065,11 +1065,11 @@ merge_files_common(const gchar* out_filename, /* normal output mode */
         dsb_combined = g_array_new(FALSE, FALSE, sizeof(wtap_block_t));
         params.dsbs_growing = dsb_combined;
     }
-    if (out_filename) {
+    if (out_filename && !out_filenamep) {
         pdh = wtap_dump_open(out_filename, file_type, WTAP_UNCOMPRESSED,
                              &params, err, err_info);
-    } else if (out_filenamep) {
-        pdh = wtap_dump_open_tempfile(out_filenamep, pfx, file_type,
+    } else if (out_filename && out_filenamep) {
+        pdh = wtap_dump_open_tempfile(out_filename, out_filenamep, pfx, file_type,
                                       WTAP_UNCOMPRESSED, &params, err,
                                       err_info);
     } else {
@@ -1132,7 +1132,7 @@ merge_files(const gchar* out_filename, const int file_type,
  * on failure.
  */
 merge_result
-merge_files_to_tempfile(gchar **out_filenamep, const char *pfx,
+merge_files_to_tempfile(const char *tmpdir, gchar **out_filenamep, const char *pfx,
                         const int file_type, const char *const *in_filenames,
                         const guint in_file_count, const gboolean do_append,
                         const idb_merge_mode mode, guint snaplen,
@@ -1145,7 +1145,7 @@ merge_files_to_tempfile(gchar **out_filenamep, const char *pfx,
     /* no temporary file name yet */
     *out_filenamep = NULL;
 
-    return merge_files_common(NULL, out_filenamep, pfx,
+    return merge_files_common(tmpdir, out_filenamep, pfx,
                               file_type, in_filenames, in_file_count,
                               do_append, mode, snaplen, app_name, cb, err,
                               err_info, err_fileno, err_framenum);

capture/capture_sync.c
@@ -266,6 +266,11 @@ sync_pipe_start(capture_options *capture_opts, GPtrArray *capture_comments,
         }
     }
 
+    if (capture_opts->temp_dir) {
+            argv = sync_pipe_add_arg(argv, &argc, "--temp-dir");
+            argv = sync_pipe_add_arg(argv, &argc, capture_opts->temp_dir);
+    }
+
     if (capture_opts->multi_files_on) {
         if (capture_opts->has_autostop_filesize) {
             char sfilesize[ARGV_NUMBER_LEN];

capture_opts.c
@@ -13,6 +13,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #ifdef HAVE_LIBPCAP
 
 #include <string.h>
@@ -121,6 +125,7 @@ capture_opts_init(capture_options *capture_opts)
     capture_opts->capture_child                   = FALSE;
     capture_opts->print_file_names                = FALSE;
     capture_opts->print_name_to                   = NULL;
+    capture_opts->temp_dir                        = NULL;
     capture_opts->compress_type                   = NULL;
 }
 
@@ -147,6 +152,7 @@ capture_opts_cleanup(capture_options *capture_opts)
         capture_opts->all_ifaces = NULL;
     }
     g_free(capture_opts->save_file);
+    g_free(capture_opts->temp_dir);
 }
 
 /* log content of capture_opts */
@@ -261,6 +267,7 @@ capture_opts_log(const char *log_domain, enum ws_log_level log_level, capture_op
     ws_log(log_domain, log_level, "AutostopPackets (%u) : %u", capture_opts->has_autostop_packets, capture_opts->autostop_packets);
     ws_log(log_domain, log_level, "AutostopFilesize(%u) : %u (KB)", capture_opts->has_autostop_filesize, capture_opts->autostop_filesize);
     ws_log(log_domain, log_level, "AutostopDuration(%u) : %.3f", capture_opts->has_autostop_duration, capture_opts->autostop_duration);
+    ws_log(log_domain, log_level, "Temporary Directory  : %s", capture_opts->temp_dir && capture_opts->temp_dir[0] ? capture_opts->temp_dir : g_get_tmp_dir());
 }
 
 /*
@@ -801,6 +808,7 @@ int
 capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg_str_p)
 {
     int status, snaplen;
+    ws_statb64 fstat;
 
     switch(opt) {
     case 'a':        /* autostop criteria */
@@ -1001,6 +1009,30 @@ capture_opts_add_opt(capture_options *capture_opts, int opt, const char *optarg_
             return 1;
         }
         capture_opts->compress_type = g_strdup(optarg_str_p);
+        break;
+    case LONGOPT_CAPTURE_TMPDIR:  /* capture temporary directory */
+        if (capture_opts->temp_dir) {
+            cmdarg_err("--temp-dir can be set only once");
+            return 1;
+        }
+        if (ws_stat64(optarg_str_p, &fstat) < 0) {
+            cmdarg_err("Can't set temporary directory %s: %s",
+                    optarg_str_p, g_strerror(errno));
+            return 1;
+        }
+        if (!S_ISDIR(fstat.st_mode)) {
+            cmdarg_err("Can't set temporary directory %s: not a directory",
+                    optarg_str_p);
+            return 1;
+        }
+#ifdef S_IRWXU
+        if ((fstat.st_mode & S_IRWXU) != S_IRWXU) {
+            cmdarg_err("Can't set temporary directory %s: not a writable directory",
+                    optarg_str_p);
+            return 1;
+        }
+#endif /* S_IRWXU */
+        capture_opts->temp_dir = g_strdup(optarg_str_p);
         break;
     default:
         /* the caller is responsible to send us only the right opt's */

capture_opts.h
@@ -47,6 +47,7 @@ extern "C" {
 #define LONGOPT_LIST_TSTAMP_TYPES LONGOPT_BASE_CAPTURE+1
 #define LONGOPT_SET_TSTAMP_TYPE   LONGOPT_BASE_CAPTURE+2
 #define LONGOPT_COMPRESS_TYPE     LONGOPT_BASE_CAPTURE+3
+#define LONGOPT_CAPTURE_TMPDIR    LONGOPT_BASE_CAPTURE+4
 
 /*
  * Options for capturing common to all capturing programs.
@@ -87,7 +88,8 @@ extern "C" {
     {"linktype",              ws_required_argument, NULL, 'y'}, \
     {"list-time-stamp-types", ws_no_argument,       NULL, LONGOPT_LIST_TSTAMP_TYPES}, \
     {"time-stamp-type",       ws_required_argument, NULL, LONGOPT_SET_TSTAMP_TYPE}, \
-    {"compress-type",         ws_required_argument, NULL, LONGOPT_COMPRESS_TYPE},
+    {"compress-type",         ws_required_argument, NULL, LONGOPT_COMPRESS_TYPE}, \
+    {"temp-dir",              ws_required_argument, NULL, LONGOPT_CAPTURE_TMPDIR},
 
 
 #define OPTSTRING_CAPTURE_COMMON \
@@ -315,6 +317,7 @@ typedef struct capture_options_tag {
     gboolean           print_file_names;      /**< TRUE if printing names of completed
                                                    files as we close them */
     gchar             *print_name_to;         /**< output file name */
+    gchar             *temp_dir;              /**< temporary directory path */
 
     /* internally used (don't touch from outside) */
     gboolean           output_to_pipe;        /**< save_file is a pipe (named or stdout) */

dumpcap.c
@@ -38,6 +38,7 @@
 
 #include <wsutil/socket.h>
 #include <wsutil/wslog.h>
+#include <wsutil/file_util.h>
 
 #ifdef HAVE_LIBCAP
 # include <sys/prctl.h>
@@ -434,6 +435,8 @@ print_usage(FILE *output)
     fprintf(output, "  --capture-comment <comment>\n");
     fprintf(output, "                           add a capture comment to the output file\n");
     fprintf(output, "                           (only for pcapng)\n");
+    fprintf(output, "  --temp-dir <directory>   write temporary files to this directory\n");
+    fprintf(output, "                           (default: %s)\n", g_get_tmp_dir());
     fprintf(output, "\n");
 
     ws_log_print_usage(output);
@@ -3678,7 +3681,7 @@ capture_loop_open_output(capture_options *capture_opts, int *save_file_fd,
         } else {
             suffix = ".pcap";
         }
-        *save_file_fd = create_tempfile(&capfile_name, prefix, suffix, &err_tempfile);
+        *save_file_fd = create_tempfile(capture_opts->temp_dir, &capfile_name, prefix, suffix, &err_tempfile);
         g_free(prefix);
         is_tempfile = TRUE;
     }
@@ -5163,6 +5166,7 @@ main(int argc, char *argv[])
         case 'I':        /* Monitor mode */
 #endif
         case LONGOPT_COMPRESS_TYPE:        /* compress type */
+        case LONGOPT_CAPTURE_TMPDIR:       /* capture temp directory */
             status = capture_opts_add_opt(&global_capture_opts, opt, ws_optarg);
             if (status != 0) {
                 exit_main(status);

extcap.c
@@ -1547,13 +1547,13 @@ static gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, HANDLE *ha
     return TRUE;
 }
 #else
-static gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *pipe_prefix)
+static gboolean extcap_create_pipe(const gchar *ifname, gchar **fifo, const gchar *temp_dir, const gchar *pipe_prefix)
 {
     gchar *temp_name = NULL;
     int fd = 0;
 
     gchar *pfx = g_strconcat(pipe_prefix, "_", ifname, NULL);
-    if ((fd = create_tempfile(&temp_name, pfx, NULL, NULL)) < 0)
+    if ((fd = create_tempfile(temp_dir, &temp_name, pfx, NULL, NULL)) < 0)
     {
         g_free(pfx);
         return FALSE;
@@ -1611,11 +1611,15 @@ extcap_init_interfaces(capture_options *capture_opts)
             extcap_create_pipe(interface_opts->name, &interface_opts->extcap_control_in,
 #ifdef _WIN32
                                &interface_opts->extcap_control_in_h,
+#else
+                               capture_opts->temp_dir,
 #endif
                                EXTCAP_CONTROL_IN_PREFIX);
             extcap_create_pipe(interface_opts->name, &interface_opts->extcap_control_out,
 #ifdef _WIN32
                                &interface_opts->extcap_control_out_h,
+#else
+                               capture_opts->temp_dir,
 #endif
                                EXTCAP_CONTROL_OUT_PREFIX);
         }
@@ -1624,6 +1628,8 @@ extcap_init_interfaces(capture_options *capture_opts)
         if (!extcap_create_pipe(interface_opts->name, &interface_opts->extcap_fifo,
 #ifdef _WIN32
                                 &interface_opts->extcap_pipe_h,
+#else
+                               capture_opts->temp_dir,
 #endif
                                 EXTCAP_PIPE_PREFIX))
         {

file.c
@@ -1400,7 +1400,7 @@ merge_callback(merge_event event, int num _U_,
 
 
 cf_status_t
-cf_merge_files_to_tempfile(gpointer pd_window, char **out_filenamep,
+cf_merge_files_to_tempfile(gpointer pd_window, const char *temp_dir, char **out_filenamep,
                            int in_file_count, const char *const *in_filenames,
                            int file_type, gboolean do_append)
 {
@@ -1420,7 +1420,7 @@ cf_merge_files_to_tempfile(gpointer pd_window, char **out_filenamep,
   cf_callback_invoke(cf_cb_file_merge_started, NULL);
 
   /* merge the files */
-  status = merge_files_to_tempfile(out_filenamep, "wireshark", file_type,
+  status = merge_files_to_tempfile(temp_dir, out_filenamep, "wireshark", file_type,
                                    in_filenames,
                                    in_file_count, do_append,
                                    IDB_MERGE_MODE_ALL_SAME, 0 /* snaplen */,

file.h
@@ -678,7 +678,7 @@ void cf_unignore_frame(capture_file *cf, frame_data *frame);
  * @return one of cf_status_t
  */
 cf_status_t
-cf_merge_files_to_tempfile(gpointer pd_window, char **out_filenamep,
+cf_merge_files_to_tempfile(gpointer pd_window, const char *temp_dir, char **out_filenamep,
                            int in_file_count, const char *const *in_filenames,
                            int file_type, gboolean do_append);
 

commit 1f1ee198f2d013f1580958ded143b6e150093324

merge: Don't write to stdout if tempdir is not set
If merge_files_common() is called with a non NULL value for out_filenamep,
that always indicates tempfile mode, even if the tempdir is not set.
A NULL value for the tempdir is handled by wtap_dump_open_tempfile,
which writes to the OS default temp directory.

Only write to stdout if both out_filename and out_filenamep are NULL.

Fixes a crash introduced by commit 1e0d117eb7ab1ce7f4ff8a4f
when selecting Merge from the GUI and the new temp_dir option is not set.

wiretap/merge.c
@@ -985,7 +985,8 @@ merge_process_packets(wtap_dumper *pdh, const int file_type,
 }
 
 static merge_result
-merge_files_common(const gchar* out_filename, /* normal output mode */
+merge_files_common(const gchar* out_filename, /* filename in normal output mode,
+                   optional tempdir in tempfile mode (NULL for OS default) */
                    gchar **out_filenamep, const char *pfx, /* tempfile mode  */
                    const int file_type, const char *const *in_filenames,
                    const guint in_file_count, const gboolean do_append,
@@ -1065,13 +1066,13 @@ merge_files_common(const gchar* out_filename, /* normal output mode */
         dsb_combined = g_array_new(FALSE, FALSE, sizeof(wtap_block_t));
         params.dsbs_growing = dsb_combined;
     }
-    if (out_filename && !out_filenamep) {
-        pdh = wtap_dump_open(out_filename, file_type, WTAP_UNCOMPRESSED,
-                             &params, err, err_info);
-    } else if (out_filename && out_filenamep) {
+    if (out_filenamep) {
         pdh = wtap_dump_open_tempfile(out_filename, out_filenamep, pfx, file_type,
                                       WTAP_UNCOMPRESSED, &params, err,
                                       err_info);
+    } else if (out_filename) {
+        pdh = wtap_dump_open(out_filename, file_type, WTAP_UNCOMPRESSED,
+                             &params, err, err_info);
     } else {
         pdh = wtap_dump_open_stdout(file_type, WTAP_UNCOMPRESSED, &params, err,
                                     err_info);