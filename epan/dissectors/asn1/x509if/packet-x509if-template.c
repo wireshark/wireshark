d83af184bad14ae41ba08c704ce2f416047d5ffe
João Valverde <j@v6e.pt>
2022-06-24 14:00:15 +0100

    X509IF: Fix duplicate filter name
    Fixes #18155.
    
    (cherry picked from commit 229dad6a754c4c1840befafd7cc5917b36bd3f53)

--- epan/dissectors/asn1/x509if/packet-x509if-template.c
+++ epan/dissectors/asn1/x509if/packet-x509if-template.c
@@ -111,7 +111,7 @@ void proto_register_x509if(void) {
   /* List of fields */
   static hf_register_info hf[] = {
     { &hf_x509if_object_identifier_id,
-      { "Id", "x509if.id", FT_OID, BASE_NONE, NULL, 0,
+      { "Object Id", "x509if.oid", FT_OID, BASE_NONE, NULL, 0,
 	"Object identifier Id", HFILL }},
     { &hf_x509if_any_string,
       { "AnyString", "x509if.any.String", FT_BYTES, BASE_NONE,
