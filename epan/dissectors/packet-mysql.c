7160d9e0f036b55988a43a622b029ba0bdf5314e
Daniël van Eeden <git@myname.nl>
2022-08-10 14:58:26 +0200

    mysql: Fix capability check in login packet
    The flag for ZSTD compression is part of the extended capabilities, not
    the basic capabilities.

--- epan/dissectors/packet-mysql.c
+++ epan/dissectors/packet-mysql.c
@@ -1783,7 +1783,7 @@ mysql_dissect_login(tvbuff_t *tvb, packet_info *pinfo, int offset,
 		}
 	}
 
-	if (conn_data->clnt_caps & MYSQL_CAPS_ZS)
+	if (conn_data->clnt_caps_ext & MYSQL_CAPS_ZS)
 	{
 		proto_tree_add_item(login_tree, hf_mysql_zstd_compression_level, tvb, offset, 1, ENC_LITTLE_ENDIAN);
 		offset += 1;
