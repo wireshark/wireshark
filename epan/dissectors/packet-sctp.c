f7529c6cb7810383f728f421067b042fdb2fe076
Michael Tuexen <tuexen@wireshark.org>
2022-09-04 22:06:03 +0200

    SCTP: remove support for NONCE_SUPPORTED parameter
    The specification never hit RFC status, it wasn't implemented or
    deployed and the ECN nonce stuff was moved to HISTORIC recently..
    So remove support from the dissector, freeing parameter type
    0x8001, which was also never assigned by IANA.
    
    (cherry picked from commit 9315cffd6a57513a58e01989caa56d701936b460)

--- epan/dissectors/packet-sctp.c
+++ epan/dissectors/packet-sctp.c
@@ -1613,11 +1613,6 @@ dissect_ecn_parameter(tvbuff_t *parameter_tvb _U_)
 {
 }
 
-static void
-dissect_nonce_supported_parameter(tvbuff_t *parameter_tvb _U_)
-{
-}
-
 #define RANDOM_NUMBER_OFFSET PARAMETER_VALUE_OFFSET
 
 static void
@@ -1812,7 +1807,6 @@ dissect_unknown_parameter(tvbuff_t *parameter_tvb, proto_tree *parameter_tree, p
 #define ADD_OUTGOING_STREAMS_REQUEST_PARAMETER_ID 0x0011
 #define ADD_INCOMING_STREAMS_REQUEST_PARAMETER_ID 0x0012
 #define ECN_PARAMETER_ID                          0x8000
-#define NONCE_SUPPORTED_PARAMETER_ID              0x8001
 #define RANDOM_PARAMETER_ID                       0x8002
 #define CHUNKS_PARAMETER_ID                       0x8003
 #define HMAC_ALGO_PARAMETER_ID                    0x8004
@@ -1841,7 +1835,6 @@ static const value_string parameter_identifier_values[] = {
   { ADD_INCOMING_STREAMS_REQUEST_PARAMETER_ID, "Add incoming streams request" },
   { SUPPORTED_ADDRESS_TYPES_PARAMETER_ID,      "Supported address types"      },
   { ECN_PARAMETER_ID,                          "ECN"                          },
-  { NONCE_SUPPORTED_PARAMETER_ID,              "Nonce supported"              },
   { RANDOM_PARAMETER_ID,                       "Random"                       },
   { CHUNKS_PARAMETER_ID,                       "Authenticated Chunk list"     },
   { HMAC_ALGO_PARAMETER_ID,                    "Requested HMAC Algorithm"     },
@@ -1957,8 +1950,6 @@ dissect_parameter(tvbuff_t *parameter_tvb, packet_info *pinfo,
   case ECN_PARAMETER_ID:
     dissect_ecn_parameter(parameter_tvb);
     break;
-  case NONCE_SUPPORTED_PARAMETER_ID:
-    dissect_nonce_supported_parameter(parameter_tvb);
     break;
   case RANDOM_PARAMETER_ID:
     dissect_random_parameter(parameter_tvb, parameter_tree);
