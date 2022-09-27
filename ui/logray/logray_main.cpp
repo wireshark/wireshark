wireshark/wireshark

commit fcdb77dce9be78725d6bf3eab7560b3574423d0a

Qt: Manually connect our "Capture" menu actions.

ui/logray/logray_main_window.cpp
@@ -602,6 +602,7 @@ main_ui_->goToLineEdit->setValidator(goToLineQiv);
     connectEditMenuActions();
     connectViewMenuActions();
     connectGoMenuActions();
+    connectCaptureMenuActions();
 
     connect(packet_list_, SIGNAL(packetDissectionChanged()),
             this, SLOT(redissectPackets()));

ui/logray/logray_main_window.h
@@ -422,11 +422,9 @@ private slots:
 
     void resetPreviousFocus();
 
-    void on_actionCaptureOptions_triggered();
-#ifdef HAVE_LIBPCAP
-    void on_actionCaptureRefreshInterfaces_triggered();
-#endif
-    void on_actionCaptureCaptureFilters_triggered();
+    void connectCaptureMenuActions();
+    void showCaptureOptionsDialog();
+    void startCaptureTriggered();
 
     void on_actionAnalyzeDisplayFilters_triggered();
     void on_actionAnalyzeDisplayFilterMacros_triggered();
@@ -474,9 +472,6 @@ private slots:
     void on_goToCancel_clicked();
     void on_goToGo_clicked();
     void on_goToLineEdit_returnPressed();
-    void on_actionCaptureStart_triggered();
-    void on_actionCaptureStop_triggered();
-    void on_actionCaptureRestart_triggered();
 
     void on_actionStatisticsCaptureFileProperties_triggered();
     void on_actionStatisticsResolvedAddresses_triggered();

ui/qt/wireshark_main_window.cpp
@@ -630,6 +630,7 @@ main_ui_->goToLineEdit->setValidator(goToLineQiv);
     connectEditMenuActions();
     connectViewMenuActions();
     connectGoMenuActions();
+    connectCaptureMenuActions();
 
     connect(main_ui_->actionAnalyzeFollowTCPStream, &QAction::triggered, this,
             [this]() { this->openFollowStreamDialogForType(FOLLOW_TCP); },

ui/qt/wireshark_main_window.h
@@ -447,11 +447,9 @@ private slots:
 
     void resetPreviousFocus();
 
-    void on_actionCaptureOptions_triggered();
-#ifdef HAVE_LIBPCAP
-    void on_actionCaptureRefreshInterfaces_triggered();
-#endif
-    void on_actionCaptureCaptureFilters_triggered();
+    void connectCaptureMenuActions();
+    void showCaptureOptionsDialog();
+    void startCaptureTriggered();
 
     void on_actionAnalyzeDisplayFilters_triggered();
     void on_actionAnalyzeDisplayFilterMacros_triggered();
@@ -499,9 +497,6 @@ private slots:
     void on_goToCancel_clicked();
     void on_goToGo_clicked();
     void on_goToLineEdit_returnPressed();
-    void on_actionCaptureStart_triggered();
-    void on_actionCaptureStop_triggered();
-    void on_actionCaptureRestart_triggered();
 
     void on_actionStatisticsCaptureFileProperties_triggered();
     void on_actionStatisticsResolvedAddresses_triggered();

ui/logray/logray_main_window_slots.cpp
@@ -2703,6 +2703,137 @@ void LograyMainWindow::goToConversationFrame(bool go_next) {
     g_free(filter);
 }
 
+// Capture Menu
+
+void LograyMainWindow::connectCaptureMenuActions()
+{
+#ifdef HAVE_LIBPCAP
+    connect(main_ui_->actionCaptureOptions, &QAction::triggered, this,
+            [this]() { showCaptureOptionsDialog(); });
+#endif
+
+    connect(main_ui_->actionCaptureStart, &QAction::triggered, this,
+            [this]() { startCaptureTriggered(); });
+
+    connect(main_ui_->actionCaptureStop, &QAction::triggered, this,
+            [this]() { stopCapture(); });
+
+    connect(main_ui_->actionCaptureRestart, &QAction::triggered, this, [this]() {
+#ifdef HAVE_LIBPCAP
+        QString before_what(tr(" before restarting the capture"));
+        cap_session_.capture_opts->restart = TRUE;
+        if (!testCaptureFileClose(before_what, Restart)) {
+            return;
+        }
+        startCapture(QStringList());
+#endif // HAVE_LIBPCAP
+    });
+
+    connect(main_ui_->actionCaptureCaptureFilters, &QAction::triggered, this, [this]() {
+        if (!capture_filter_dlg_) {
+            capture_filter_dlg_ = new FilterDialog(this, FilterDialog::CaptureFilter);
+        }
+        capture_filter_dlg_->show();
+        capture_filter_dlg_->raise();
+        capture_filter_dlg_->activateWindow();
+    });
+
+#ifdef HAVE_LIBPCAP
+    connect(main_ui_->actionCaptureRefreshInterfaces, &QAction::triggered, this, [this]() {
+        main_ui_->actionCaptureRefreshInterfaces->setEnabled(false);
+        mainApp->refreshLocalInterfaces();
+        main_ui_->actionCaptureRefreshInterfaces->setEnabled(true);
+    });
+#endif
+}
+
+void LograyMainWindow::showCaptureOptionsDialog()
+{
+#ifdef HAVE_LIBPCAP
+    if (!capture_options_dialog_) {
+        capture_options_dialog_ = new CaptureOptionsDialog(this);
+
+        connect(capture_options_dialog_, &CaptureOptionsDialog::startCapture, this, [this]() { startCapture(); });
+        connect(capture_options_dialog_, &CaptureOptionsDialog::stopCapture, this, &LograyMainWindow::stopCapture);
+
+        connect(capture_options_dialog_, &CaptureOptionsDialog::interfacesChanged,
+                this->welcome_page_, &WelcomePage::interfaceSelected);
+        connect(capture_options_dialog_, &CaptureOptionsDialog::interfacesChanged,
+                this->welcome_page_->getInterfaceFrame(), &InterfaceFrame::updateSelectedInterfaces);
+        connect(capture_options_dialog_, &CaptureOptionsDialog::interfaceListChanged,
+                this->welcome_page_->getInterfaceFrame(), &InterfaceFrame::interfaceListChanged);
+        connect(capture_options_dialog_, &CaptureOptionsDialog::captureFilterTextEdited,
+                this->welcome_page_, &WelcomePage::setCaptureFilterText);
+        // Propagate selection changes from main UI to dialog.
+        connect(this->welcome_page_, &WelcomePage::interfacesChanged,
+                capture_options_dialog_, &CaptureOptionsDialog::interfaceSelected);
+
+        connect(capture_options_dialog_, &CaptureOptionsDialog::setFilterValid,
+                this, &LograyMainWindow::startInterfaceCapture);
+
+        connect(capture_options_dialog_, &CaptureOptionsDialog::showExtcapOptions,
+                this, &LograyMainWindow::showExtcapOptionsDialog);
+    }
+    capture_options_dialog_->updateInterfaces();
+
+    if (capture_options_dialog_->isMinimized()) {
+        capture_options_dialog_->showNormal();
+    } else {
+        capture_options_dialog_->show();
+    }
+
+    capture_options_dialog_->raise();
+    capture_options_dialog_->activateWindow();
+#endif
+}
+
+void LograyMainWindow::startCaptureTriggered()
+{
+//#ifdef HAVE_AIRPCAP
+//  airpcap_if_active = airpcap_if_selected;
+//  if (airpcap_if_active)
+//    airpcap_set_toolbar_start_capture(airpcap_if_active);
+//#endif
+
+//  if (cap_open_w) {
+//    /*
+//     * There's an options dialog; get the values from it and close it.
+//     */
+//    gboolean success;
+
+//    /* Determine if "capture start" while building of the "capture options" window */
+//    /*  is in progress. If so, ignore the "capture start.                          */
+//    /* XXX: Would it be better/cleaner for the "capture options" window code to    */
+//    /*      disable the capture start button temporarily ?                         */
+//    if (cap_open_complete == FALSE) {
+//      return;  /* Building options window: ignore "capture start" */
+//    }
+//    success = capture_dlg_prep(cap_open_w);
+//    window_destroy(GTK_WIDGET(cap_open_w));
+//    if (!success)
+//      return;   /* error in options dialog */
+//  }
+
+#ifdef HAVE_LIBPCAP
+    if (global_capture_opts.num_selected == 0) {
+        QString err_msg = tr("No Interface Selected.");
+        mainApp->pushStatus(WiresharkApplication::TemporaryStatus, err_msg);
+        main_ui_->actionCaptureStart->setChecked(false);
+        return;
+    }
+
+    /* XXX - will closing this remove a temporary file? */
+    QString before_what(tr(" before starting a new capture"));
+    if (testCaptureFileClose(before_what)) {
+        startCapture();
+    } else {
+        // simply clicking the button sets it to 'checked' even though we've
+        // decided to do nothing, so undo that
+        main_ui_->actionCaptureStart->setChecked(false);
+    }
+#endif // HAVE_LIBPCAP
+}
+
 // Analyze Menu
 
 void LograyMainWindow::filterMenuAboutToShow()
@@ -3086,80 +3217,6 @@ void LograyMainWindow::on_goToLineEdit_returnPressed()
     on_goToGo_clicked();
 }
 
-void LograyMainWindow::on_actionCaptureStart_triggered()
-{
-//#ifdef HAVE_AIRPCAP
-//  airpcap_if_active = airpcap_if_selected;
-//  if (airpcap_if_active)
-//    airpcap_set_toolbar_start_capture(airpcap_if_active);
-//#endif
-
-//  if (cap_open_w) {
-//    /*
-//     * There's an options dialog; get the values from it and close it.
-//     */
-//    gboolean success;
-
-//    /* Determine if "capture start" while building of the "capture options" window */
-//    /*  is in progress. If so, ignore the "capture start.                          */
-//    /* XXX: Would it be better/cleaner for the "capture options" window code to    */
-//    /*      disable the capture start button temporarily ?                         */
-//    if (cap_open_complete == FALSE) {
-//      return;  /* Building options window: ignore "capture start" */
-//    }
-//    success = capture_dlg_prep(cap_open_w);
-//    window_destroy(GTK_WIDGET(cap_open_w));
-//    if (!success)
-//      return;   /* error in options dialog */
-//  }
-
-#ifdef HAVE_LIBPCAP
-    if (global_capture_opts.num_selected == 0) {
-        QString err_msg = tr("No Interface Selected.");
-        mainApp->pushStatus(WiresharkApplication::TemporaryStatus, err_msg);
-        main_ui_->actionCaptureStart->setChecked(false);
-        return;
-    }
-
-    /* XXX - will closing this remove a temporary file? */
-    QString before_what(tr(" before starting a new capture"));
-    if (testCaptureFileClose(before_what)) {
-        startCapture();
-    } else {
-        // simply clicking the button sets it to 'checked' even though we've
-        // decided to do nothing, so undo that
-        main_ui_->actionCaptureStart->setChecked(false);
-    }
-#endif // HAVE_LIBPCAP
-}
-
-void LograyMainWindow::on_actionCaptureStop_triggered()
-{
-    stopCapture();
-}
-
-void LograyMainWindow::on_actionCaptureRestart_triggered()
-{
-#ifdef HAVE_LIBPCAP
-    QString before_what(tr(" before restarting the capture"));
-    cap_session_.capture_opts->restart = TRUE;
-    if (!testCaptureFileClose(before_what, Restart))
-        return;
-
-    startCapture();
-#endif // HAVE_LIBPCAP
-}
-
-void LograyMainWindow::on_actionCaptureCaptureFilters_triggered()
-{
-    if (!capture_filter_dlg_) {
-        capture_filter_dlg_ = new FilterDialog(this, FilterDialog::CaptureFilter);
-    }
-    capture_filter_dlg_->show();
-    capture_filter_dlg_->raise();
-    capture_filter_dlg_->activateWindow();
-}
-
 void LograyMainWindow::on_actionStatisticsCaptureFileProperties_triggered()
 {
     CaptureFilePropertiesDialog *capture_file_properties_dialog = new CaptureFilePropertiesDialog(*this, capture_file_);
@@ -3189,55 +3246,6 @@ void LograyMainWindow::on_actionStatisticsProtocolHierarchy_triggered()
             this, SIGNAL(filterAction(QString, FilterAction::Action, FilterAction::ActionType)));
     phd->show();
 }
-
-void LograyMainWindow::on_actionCaptureOptions_triggered()
-{
-#ifdef HAVE_LIBPCAP
-    if (!capture_options_dialog_) {
-        capture_options_dialog_ = new CaptureOptionsDialog(this);
-
-        connect(capture_options_dialog_, SIGNAL(startCapture()), this, SLOT(startCapture()));
-        connect(capture_options_dialog_, SIGNAL(stopCapture()), this, SLOT(stopCapture()));
-
-        connect(capture_options_dialog_, SIGNAL(interfacesChanged()),
-                this->welcome_page_, SLOT(interfaceSelected()));
-        connect(capture_options_dialog_, SIGNAL(interfacesChanged()),
-                this->welcome_page_->getInterfaceFrame(), SLOT(updateSelectedInterfaces()));
-        connect(capture_options_dialog_, SIGNAL(interfaceListChanged()),
-                this->welcome_page_->getInterfaceFrame(), SLOT(interfaceListChanged()));
-        connect(capture_options_dialog_, SIGNAL(captureFilterTextEdited(QString)),
-                this->welcome_page_, SLOT(setCaptureFilterText(QString)));
-        // Propagate selection changes from main UI to dialog.
-        connect(this->welcome_page_, SIGNAL(interfacesChanged()),
-                capture_options_dialog_, SLOT(interfaceSelected()));
-
-        connect(capture_options_dialog_, SIGNAL(setFilterValid(bool, const QString)),
-                this, SLOT(startInterfaceCapture(bool, const QString)));
-
-        connect(capture_options_dialog_, SIGNAL(showExtcapOptions(QString&, bool)),
-                this, SLOT(showExtcapOptionsDialog(QString&, bool)));
-    }
-    capture_options_dialog_->updateInterfaces();
-
-    if (capture_options_dialog_->isMinimized()) {
-        capture_options_dialog_->showNormal();
-    } else {
-        capture_options_dialog_->show();
-    }
-
-    capture_options_dialog_->raise();
-    capture_options_dialog_->activateWindow();
-#endif
-}
-
-#ifdef HAVE_LIBPCAP
-void LograyMainWindow::on_actionCaptureRefreshInterfaces_triggered()
-{
-    main_ui_->actionCaptureRefreshInterfaces->setEnabled(false);
-    mainApp->refreshLocalInterfaces();
-    main_ui_->actionCaptureRefreshInterfaces->setEnabled(true);
-}
-#endif
 
 void LograyMainWindow::externalMenuItem_triggered()
 {

ui/qt/capture_options_dialog.h
@@ -62,6 +62,9 @@ public:
 
     void updateInterfaces();
 
+public slots:
+    void interfaceSelected();
+
 protected:
     virtual void showEvent(QShowEvent *);
 
@@ -80,7 +83,6 @@ private slots:
     void on_buttonBox_accepted();
     void on_buttonBox_rejected();
     void on_buttonBox_helpRequested();
-    void interfaceSelected();
     void filterEdited();
     void updateWidgets();
     void updateStatistics(void);

commit f011e750258b7d36c7074661efd227de400090e3

Qt: Use new-style syntax for signal & slots connected by name.
Change anything that matches

    grep -Eir '(signal|slot) *\( *on_action' ui

to new-style connections so that we can catch any future direct
connection conversion issues at compile time.

Change a connection from on_actionCaptureOptions_triggered to
showCaptureOptionsDialog and make showCaptureOptionsDialog public. Fixes
an issue introduced in fcdb77dc.

ui/logray/logray_main.cpp
@@ -694,8 +694,8 @@ int main(int argc, char *qt_argv[])
     // to force the issue.
     main_w->connect(&ls_app, SIGNAL(openCaptureFile(QString,QString,unsigned int)),
             main_w, SLOT(openCaptureFile(QString,QString,unsigned int)));
-    main_w->connect(&ls_app, SIGNAL(openCaptureOptions()),
-            main_w, SLOT(on_actionCaptureOptions_triggered()));
+    main_w->connect(&ls_app, &LograyApplication::openCaptureOptions,
+            main_w, &LograyMainWindow::showCaptureOptionsDialog);
 
     /* Init the "Open file" dialog directory */
     /* (do this after the path settings are processed) */

ui/logray/logray_main_window.cpp
@@ -627,14 +627,14 @@ main_ui_->goToLineEdit->setValidator(goToLineQiv);
     connect(proto_tree_, SIGNAL(editProtocolPreference(preference*, pref_module*)),
             main_ui_->preferenceEditorFrame, SLOT(editPreference(preference*, pref_module*)));
 
-    connect(main_ui_->statusBar, SIGNAL(showExpertInfo()),
-            this, SLOT(on_actionAnalyzeExpertInfo_triggered()));
+    connect(main_ui_->statusBar, &MainStatusBar::showExpertInfo,
+            this, &LograyMainWindow::on_actionAnalyzeExpertInfo_triggered);
 
-    connect(main_ui_->statusBar, SIGNAL(stopLoading()),
-            &capture_file_, SLOT(stopLoading()));
+    connect(main_ui_->statusBar, &MainStatusBar::stopLoading,
+            &capture_file_, &CaptureFile::stopLoading);
 
-    connect(main_ui_->statusBar, SIGNAL(editCaptureComment()),
-            this, SLOT(on_actionStatisticsCaptureFileProperties_triggered()));
+    connect(main_ui_->statusBar, &MainStatusBar::editCaptureComment,
+            this, &LograyMainWindow::on_actionStatisticsCaptureFileProperties_triggered);
 
     connect(main_ui_->menuApplyAsFilter, &QMenu::aboutToShow,
             this, &LograyMainWindow::filterMenuAboutToShow);

ui/logray/logray_main_window.h
@@ -276,6 +276,8 @@ public slots:
     void setTitlebarForCaptureFile();
     void setWSWindowTitle(QString title = QString());
 
+    void showCaptureOptionsDialog();
+
 #ifdef HAVE_LIBPCAP
     void captureCapturePrepared(capture_session *);
     void captureCaptureUpdateStarted(capture_session *);
@@ -423,7 +425,6 @@ private slots:
     void resetPreviousFocus();
 
     void connectCaptureMenuActions();
-    void showCaptureOptionsDialog();
     void startCaptureTriggered();
 
     void on_actionAnalyzeDisplayFilters_triggered();

ui/qt/main.cpp
@@ -696,8 +696,8 @@ int main(int argc, char *qt_argv[])
     // to force the issue.
     main_w->connect(&ws_app, SIGNAL(openCaptureFile(QString,QString,unsigned int)),
             main_w, SLOT(openCaptureFile(QString,QString,unsigned int)));
-    main_w->connect(&ws_app, SIGNAL(openCaptureOptions()),
-            main_w, SLOT(on_actionCaptureOptions_triggered()));
+    main_w->connect(&ws_app, &WiresharkApplication::openCaptureOptions,
+            main_w, &WiresharkMainWindow::showCaptureOptionsDialog);
 
     /* Init the "Open file" dialog directory */
     /* (do this after the path settings are processed) */

ui/qt/protocol_hierarchy_dialog.cpp
@@ -232,10 +232,10 @@ ProtocolHierarchyDialog::ProtocolHierarchyDialog(QWidget &parent, CaptureFile &c
     QAction *ca;
     ca = copy_menu->addAction(tr("as CSV"));
     ca->setToolTip(ui->actionCopyAsCsv->toolTip());
-    connect(ca, SIGNAL(triggered()), this, SLOT(on_actionCopyAsCsv_triggered()));
+    connect(ca, &QAction::triggered, this, &ProtocolHierarchyDialog::on_actionCopyAsCsv_triggered);
     ca = copy_menu->addAction(tr("as YAML"));
     ca->setToolTip(ui->actionCopyAsYaml->toolTip());
-    connect(ca, SIGNAL(triggered()), this, SLOT(on_actionCopyAsYaml_triggered()));
+    connect(ca, &QAction::triggered, this, &ProtocolHierarchyDialog::on_actionCopyAsYaml_triggered);
     copy_button_->setMenu(copy_menu);
 
     QPushButton *close_bt = ui->buttonBox->button(QDialogButtonBox::Close);

ui/qt/rtp_stream_dialog.cpp
@@ -310,7 +310,7 @@ RtpStreamDialog::RtpStreamDialog(QWidget &parent, CaptureFile &cf) :
     find_reverse_button_->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
     find_reverse_button_->setPopupMode(QToolButton::MenuButtonPopup);
 
-    connect(ui->actionFindReverse, SIGNAL(triggered()), this, SLOT(on_actionFindReverseNormal_triggered()));
+    connect(ui->actionFindReverse, &QAction::triggered, this, &RtpStreamDialog::on_actionFindReverseNormal_triggered);
     find_reverse_button_->setDefaultAction(ui->actionFindReverse);
     // Overrides text striping of shortcut undercode in QAction
     find_reverse_button_->setText(ui->actionFindReverseNormal->text());
@@ -319,22 +319,22 @@ RtpStreamDialog::RtpStreamDialog(QWidget &parent, CaptureFile &cf) :
     analyze_button_ = RtpAnalysisDialog::addAnalyzeButton(ui->buttonBox, this);
     prepare_button_ = ui->buttonBox->addButton(ui->actionPrepareFilter->text(), QDialogButtonBox::ActionRole);
     prepare_button_->setToolTip(ui->actionPrepareFilter->toolTip());
-    connect(prepare_button_, SIGNAL(pressed()), this, SLOT(on_actionPrepareFilter_triggered()));
+    connect(prepare_button_, &QPushButton::pressed, this, &RtpStreamDialog::on_actionPrepareFilter_triggered);
     player_button_ = RtpPlayerDialog::addPlayerButton(ui->buttonBox, this);
     copy_button_ = ui->buttonBox->addButton(ui->actionCopyButton->text(), QDialogButtonBox::ActionRole);
     copy_button_->setToolTip(ui->actionCopyButton->toolTip());
     export_button_ = ui->buttonBox->addButton(ui->actionExportAsRtpDump->text(), QDialogButtonBox::ActionRole);
     export_button_->setToolTip(ui->actionExportAsRtpDump->toolTip());
-    connect(export_button_, SIGNAL(pressed()), this, SLOT(on_actionExportAsRtpDump_triggered()));
+    connect(export_button_, &QPushButton::pressed, this, &RtpStreamDialog::on_actionExportAsRtpDump_triggered);
 
     QMenu *copy_menu = new QMenu(copy_button_);
     QAction *ca;
     ca = copy_menu->addAction(tr("as CSV"));
     ca->setToolTip(ui->actionCopyAsCsv->toolTip());
-    connect(ca, SIGNAL(triggered()), this, SLOT(on_actionCopyAsCsv_triggered()));
+    connect(ca, &QAction::triggered, this, &RtpStreamDialog::on_actionCopyAsCsv_triggered);
     ca = copy_menu->addAction(tr("as YAML"));
     ca->setToolTip(ui->actionCopyAsYaml->toolTip());
-    connect(ca, SIGNAL(triggered()), this, SLOT(on_actionCopyAsYaml_triggered()));
+    connect(ca, &QAction::triggered, this, &RtpStreamDialog::on_actionCopyAsYaml_triggered);
     copy_button_->setMenu(copy_menu);
     connect(&cap_file_, SIGNAL(captureEvent(CaptureEvent)),
             this, SLOT(captureEvent(CaptureEvent)));

ui/qt/tap_parameter_dialog.cpp
@@ -79,10 +79,10 @@ TapParameterDialog::TapParameterDialog(QWidget &parent, CaptureFile &cf, int hel
 
     QPushButton *button;
     button = ui->buttonBox->addButton(tr("Copy"), QDialogButtonBox::ActionRole);
-    connect(button, SIGNAL(clicked()), this, SLOT(on_actionCopyToClipboard_triggered()));
+    connect(button, &QPushButton::clicked, this, &TapParameterDialog::on_actionCopyToClipboard_triggered);
 
     button = ui->buttonBox->addButton(tr("Save as…"), QDialogButtonBox::ActionRole);
-    connect(button, SIGNAL(clicked()), this, SLOT(on_actionSaveAs_triggered()));
+    connect(button, &QPushButton::clicked, this, &TapParameterDialog::on_actionSaveAs_triggered);
 
     connect(ui->displayFilterLineEdit, SIGNAL(textChanged(QString)),
             this, SLOT(updateWidgets()));

ui/qt/wireshark_main_window.cpp
@@ -680,14 +680,14 @@ main_ui_->goToLineEdit->setValidator(goToLineQiv);
     connect(proto_tree_, SIGNAL(editProtocolPreference(preference*, pref_module*)),
             main_ui_->preferenceEditorFrame, SLOT(editPreference(preference*, pref_module*)));
 
-    connect(main_ui_->statusBar, SIGNAL(showExpertInfo()),
-            this, SLOT(on_actionAnalyzeExpertInfo_triggered()));
+    connect(main_ui_->statusBar, &MainStatusBar::showExpertInfo,
+            this, &WiresharkMainWindow::on_actionAnalyzeExpertInfo_triggered);
 
-    connect(main_ui_->statusBar, SIGNAL(stopLoading()),
-            &capture_file_, SLOT(stopLoading()));
+    connect(main_ui_->statusBar, &MainStatusBar::stopLoading,
+            &capture_file_, &CaptureFile::stopLoading);
 
-    connect(main_ui_->statusBar, SIGNAL(editCaptureComment()),
-            this, SLOT(on_actionStatisticsCaptureFileProperties_triggered()));
+    connect(main_ui_->statusBar, &MainStatusBar::editCaptureComment,
+            this, &WiresharkMainWindow::on_actionStatisticsCaptureFileProperties_triggered);
 
     connect(main_ui_->menuApplyAsFilter, &QMenu::aboutToShow,
             this, &WiresharkMainWindow::filterMenuAboutToShow);

ui/qt/wireshark_main_window.h
@@ -287,6 +287,8 @@ public slots:
     void setTitlebarForCaptureFile();
     void setWSWindowTitle(QString title = QString());
 
+    void showCaptureOptionsDialog();
+
 #ifdef HAVE_LIBPCAP
     void captureCapturePrepared(capture_session *);
     void captureCaptureUpdateStarted(capture_session *);
@@ -448,7 +450,6 @@ private slots:
     void resetPreviousFocus();
 
     void connectCaptureMenuActions();
-    void showCaptureOptionsDialog();
     void startCaptureTriggered();
 
     void on_actionAnalyzeDisplayFilters_triggered();