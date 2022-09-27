bb4de1a046b4de96faead1a035333e87d1488af9
j.novak@netsystem.cz <j.novak@netsystem.cz>
2022-09-27 09:48:40 +0000

    RTP dialogs: Fix: Coloring of QTreeWidgetItem do not work

--- ui/qt/rtp_player_dialog.cpp
+++ ui/qt/rtp_player_dialog.cpp
@@ -763,6 +763,7 @@ void RtpPlayerDialog::addSingleRtpStream(rtpstream_id_t *id)
         for (int col = 0; col < ui->streamTreeWidget->columnCount(); col++) {
             QBrush fgBrush = ti->foreground(col);
             fgBrush.setColor(audio_stream->color());
+            fgBrush.setStyle(Qt::SolidPattern);
             ti->setForeground(col, fgBrush);
         }
 
