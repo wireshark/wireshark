687b15737abc37d272b13b649184e2a5eaf7a303
John Thacker <johnthacker@gmail.com>
2022-10-03 21:10:33 -0400

    gtp: Use ENC_APN_STR for decode_apn
    Using ENC_APN_STR is simpler and guarantees that we produce
    valid UTF-8. Fix #18402
    
    
    (cherry picked from commit 333fe08bb3ae72426522d86ede66b4e2abdf43b8)

--- epan/dissectors/packet-gtp.c
+++ epan/dissectors/packet-gtp.c
@@ -6021,13 +6021,10 @@ dissect_radius_qos_umts(proto_tree * tree, tvbuff_t * tvb, packet_info* pinfo)
     return tvb_get_string_enc(wmem_packet_scope(), tvb, 0, tvb_reported_length(tvb), ENC_UTF_8|ENC_NA);
 }
 
-#define MAX_APN_LENGTH          100
-
 static void
 decode_apn(tvbuff_t * tvb, int offset, guint16 length, proto_tree * tree, proto_item *item)
 {
-    guint8   str[MAX_APN_LENGTH+1];
-    guint    curr_len;
+    const guint8 *apn = NULL;
 
     /*
      * This is "a domain name represented as a sequence of labels, where
@@ -6037,22 +6034,10 @@ decode_apn(tvbuff_t * tvb, int offset, guint16 length, proto_tree * tree, proto_
      * XXX - does it involve compression?
      */
 
-    /* init buffer and copy it */
-    memset(str, 0, MAX_APN_LENGTH+1);
-    tvb_memcpy(tvb, str, offset, length<MAX_APN_LENGTH?length:MAX_APN_LENGTH);
-
-    curr_len = 0;
-    while ((curr_len < length) && (curr_len < MAX_APN_LENGTH))
-    {
-        guint step    = str[curr_len];
-        str[curr_len] = '.';
-        curr_len     += step+1;
-    }
-
     /* Highlight bytes including the first length byte */
-    proto_tree_add_string(tree, hf_gtp_apn, tvb, offset, length, str+1);
+    proto_tree_add_item_ret_string(tree, hf_gtp_apn, tvb, offset, length, ENC_APN_STR, wmem_packet_scope(), &apn);
     if(item){
-        proto_item_append_text(item, ": %s", str+1);
+        proto_item_append_text(item, ": %s", apn);
     }
 
 }
