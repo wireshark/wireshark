5af53da4342168b26470d2f5d38763ccb849e24a
AndersBroman <a.broman58@gmail.com>
2022-10-19 06:31:02 +0000

    json: Add media type application/3gppHal+json

--- epan/dissectors/packet-json.c
+++ epan/dissectors/packet-json.c
@@ -1222,6 +1222,7 @@ proto_reg_handoff_json(void)
 	dissector_add_string("media_type", "application/merge-patch+json", json_handle); /* RFC 7386 HTTP PATCH methods (RFC 5789) */
 	dissector_add_string("media_type", "application/json-patch+json", json_handle); /* RFC 6902 JavaScript Object Notation (JSON) Patch */
 	dissector_add_string("media_type", "application/x-ndjson", json_handle);
+	dissector_add_string("media_type", "application/3gppHal+json", json_handle);
 	dissector_add_string("grpc_message_type", "application/grpc+json", json_handle);
 	dissector_add_uint_range_with_preference("tcp.port", "", json_file_handle); /* JSON-RPC over TCP */
 	dissector_add_uint_range_with_preference("udp.port", "", json_file_handle); /* JSON-RPC over UDP */
