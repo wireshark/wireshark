f0002e0e3dd5765f48be91d2aed29c554efba2aa
John Thacker <johnthacker@gmail.com>
2022-12-09 21:29:40 -0500

    umts_fp: Handle absent checksums when there is no payload
    3GPP TS 25.427 and TS 25.435 both say that the Payload CRC IE
    may only be present if the frame contains payload for E-DCH
    frames, even where the setup of the transport bearer indicated
    that the CRC would be present otherwise. So if there's no payload
    and the CRC is missing, treat that as missing-but-expected rather
    than marking the packet as malformed.
    
    Take the opportunity to switch to proto_tree_add_checksum, which
    handles all the various cases. Ping #8859

--- epan/dissectors/packet-umts_fp.c
+++ epan/dissectors/packet-umts_fp.c
@@ -86,6 +86,7 @@ static int hf_fp_dch_control_frame_type = -1;
 static int hf_fp_dch_rx_timing_deviation = -1;
 static int hf_fp_quality_estimate = -1;
 static int hf_fp_payload_crc = -1;
+static int hf_fp_payload_crc_status = -1;
 static int hf_fp_edch_header_crc = -1;
 static int hf_fp_edch_fsn = -1;
 static int hf_fp_edch_subframe = -1;
@@ -1012,7 +1013,7 @@ dissect_spare_extension_and_crc(tvbuff_t *tvb, packet_info *pinfo,
                                 int offset, guint header_length)
 {
     int         crc_size = 0;
-    int         remain   = tvb_captured_length_remaining(tvb, offset);
+    int         remain   = tvb_reported_length_remaining(tvb, offset);
 
     /* Payload CRC (optional) */
     if ((dch_crc_present == 1) || ((dch_crc_present == 2) && (remain >= 2))) {
@@ -1029,25 +1030,28 @@ dissect_spare_extension_and_crc(tvbuff_t *tvb, packet_info *pinfo,
     }
 
     if (crc_size) {
-        proto_item * pi = proto_tree_add_item(tree, hf_fp_payload_crc, tvb, offset, crc_size,
-                            ENC_BIG_ENDIAN);
+        guint flags = PROTO_CHECKSUM_NO_FLAGS;
+        guint16 calc_crc = 0;
         if (preferences_payload_checksum) {
-            guint16 calc_crc, read_crc;
+            flags = PROTO_CHECKSUM_VERIFY;
             if ((guint)offset > header_length) {
                 guint8 * data = (guint8 *)tvb_memdup(wmem_packet_scope(), tvb, header_length, offset-header_length);
                 calc_crc = crc16_8005_noreflect_noxor(data, offset-header_length);
-            } else {
-                calc_crc = 0;
-            }
-            read_crc = tvb_get_bits16(tvb, offset*8, 16, ENC_BIG_ENDIAN);
-
-            if (calc_crc == read_crc) {
-                proto_item_append_text(pi, " [correct]");
-            } else {
-                proto_item_append_text(pi, " [incorrect, should be 0x%x]", calc_crc);
-                expert_add_info(pinfo, pi, &ei_fp_bad_payload_checksum);
             }
         }
+        if ((guint)offset == header_length && remain == 0) {
+            /* 3GPP TS 25.427 and TS 25.435: "The Payload CRC IE may
+             * only be present if the frame contains payload" (even
+             * if defined as present at the setup of the transport bearer.)
+             * If there's room for the CRC and no payload, assume zero,
+             * otherwise, assume it's absent.
+             */
+            flags = PROTO_CHECKSUM_NOT_PRESENT;
+        }
+        proto_tree_add_checksum(tree, tvb, offset,
+                hf_fp_payload_crc, hf_fp_payload_crc_status,
+                &ei_fp_bad_payload_checksum, pinfo, calc_crc,
+                ENC_BIG_ENDIAN, flags);
     }
 }
 
@@ -6225,6 +6229,12 @@ void proto_register_fp(void)
             { &hf_fp_payload_crc,
               { "Payload CRC",
                 "fp.payload-crc", FT_UINT16, BASE_HEX, 0, 0x0,
+                NULL, HFILL
+              }
+            },
+            { &hf_fp_payload_crc_status,
+              { "Payload CRC Status",
+                "fp.payload-crc.status", FT_UINT8, BASE_NONE, VALS(proto_checksum_vals), 0x0,
                 NULL, HFILL
               }
             },
