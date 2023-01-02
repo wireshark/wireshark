8ad7e2f53848e77aa5407a2bc7245031813cea2b
Kevin Albertson <kevin.eric.albertson@gmail.com>
2023-01-01 20:50:02 -0500

    zstd: add test for uncompressing 0 length

--- epan/tvbtest.c
+++ epan/tvbtest.c
@@ -757,6 +757,12 @@ zstd_tests (void) {
 			              "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"),
 			.expect = NULL
 		},
+		{
+			.desc = "Uncompressing no data",
+			.data = "\0",
+			.len = 0,
+			.expect = ""
+		},
 
 	};
 
