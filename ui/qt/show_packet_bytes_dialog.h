b4aa7e7ec13bdf325151c460f6816dcaf021c6cf
John Thacker <johnthacker@gmail.com>
2023-02-01 21:15:41 -0500

    Qt: Fix order of ShowAs enums
    The ShowAs enum should stay in the same order as the Items
    are added to the ShowAs combobox, because setCurrentIndex()
    is used with the enum values, e.g. when setting the value to
    "Show As Image" when the bytes are an image.
    Fixup c9e08b7be317e0e7e0cf95baa64f52d33b392964
    
    
    (cherry picked from commit 15dfa3aa20623c76c01c4e97dcab5b236c5c7beb)

--- ui/qt/show_packet_bytes_dialog.h
+++ ui/qt/show_packet_bytes_dialog.h
@@ -65,6 +65,9 @@ private slots:
     void saveAs();
 
 private:
+    /* Keep these enums in the same order (generally alphabetically)
+     * that they are added to the combo boxes.
+     */
     enum DecodeAsType {
         DecodeAsNone,
         DecodeAsBASE64,
@@ -77,13 +80,13 @@ private:
         ShowAsASCII,
         ShowAsASCIIandControl,
         ShowAsCArray,
-        ShowAsRustArray,
         ShowAsEBCDIC,
         ShowAsHexDump,
         ShowAsHTML,
         ShowAsImage,
         ShowAsJson,
         ShowAsRAW,
+        ShowAsRustArray,
         ShowAsCodec, // Ordered to match the UTF-8 combobox index
         ShowAsYAML,
     };
