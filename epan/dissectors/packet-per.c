ac56bdc70afda964de2a9605e8e4cddb3c6b2be1
John Thacker <johnthacker@gmail.com>
2023-05-22 07:38:36 -0400

    per: Fix leak in dissect_per_open_type_internal
    tvb_new_octet_aligned() can throw an exception, and creating
    a new composite TVB but not appending any data (and thus not
    attaching it to a chain) leaks data because the composite TVB
    is never freed. So try to create the aligned fragment TVB first
    before creating the composite TVB.

--- epan/dissectors/packet-per.c
+++ epan/dissectors/packet-per.c
@@ -166,7 +166,7 @@ static guint32
 dissect_per_open_type_internal(tvbuff_t *tvb, guint32 offset, asn1_ctx_t *actx, proto_tree *tree, int hf_index, void* type_cb, asn1_cb_variant variant)
 {
 	int type_length, start_offset, end_offset, fragmented_length = 0, pdu_length, pdu_offset;
-	tvbuff_t *val_tvb = NULL, *pdu_tvb = NULL;
+	tvbuff_t *val_tvb = NULL, *pdu_tvb = NULL, *fragment_tvb = NULL;
 	header_field_info *hfi;
 	proto_tree *subtree = tree;
 	gboolean is_fragmented;
@@ -179,10 +179,11 @@ dissect_per_open_type_internal(tvbuff_t *tvb, guint32 offset, asn1_ctx_t *actx, 
 		offset = dissect_per_length_determinant(tvb, offset, actx, tree, hf_per_open_type_length, &type_length, &is_fragmented);
 		if (actx->aligned) BYTE_ALIGN_OFFSET(offset);
 		if (is_fragmented) {
+			fragment_tvb = tvb_new_octet_aligned(tvb, offset, 8*type_length);
 			if (fragmented_length == 0) {
 				pdu_tvb = tvb_new_composite();
 			}
-			tvb_composite_append(pdu_tvb, tvb_new_octet_aligned(tvb, offset, 8*type_length));
+			tvb_composite_append(pdu_tvb, fragment_tvb);
 			offset += 8*type_length;
 			fragmented_length += type_length;
 		}
