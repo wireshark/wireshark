51accf1b905d1c9f93defc2d60d904eead299554
João Valverde <j@v6e.pt>
2023-05-25 20:45:31 +0100

    Allow using host Lemon executable
    Add support for using the host system's lemon binary instead of
    compiling our own. Many linux distributions include a lemon
    parser generator package. Using this binary when cross-compiling
    is much simpler than configuring the build to use the build host
    toolchain to compile lemon.

--- cmake/modules/UseLemon.cmake
+++ cmake/modules/UseLemon.cmake
@@ -1,12 +1,28 @@
 #
-macro(ADD_LEMON_FILES _source _generated)
-	set(_lemonpardir ${CMAKE_SOURCE_DIR}/tools/lemon)
-	foreach (_current_FILE ${ARGN})
-		get_filename_component(_in ${_current_FILE} ABSOLUTE)
-		get_filename_component(_basename ${_current_FILE} NAME_WE)
 
-		set(_out ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
+find_program(LEMON_EXECUTABLE lemon)
 
+if(LEMON_EXECUTABLE)
+	# Use system lemon
+	macro(generate_lemon_file _out _in)
+		add_custom_command(
+			OUTPUT
+				${_out}.c
+				# These files are generated as side-effect
+				${_out}.h
+				${_out}.out
+			COMMAND ${LEMON_EXECUTABLE}
+				-T/usr/share/lemon/lempar.c
+				-d.
+				${_in}
+			DEPENDS
+				${_in}
+		)
+	endmacro()
+	add_custom_target(lemon)
+else()
+	# Compile bundled lemon
+	macro(generate_lemon_file _out _in)
 		add_custom_command(
 			OUTPUT
 				${_out}.c
@@ -14,14 +30,26 @@ macro(ADD_LEMON_FILES _source _generated)
 				${_out}.h
 				${_out}.out
 			COMMAND $<TARGET_FILE:lemon>
-				-T${_lemonpardir}/lempar.c
+				-T${CMAKE_SOURCE_DIR}/tools/lemon/lempar.c
 				-d.
 				${_in}
 			DEPENDS
 				${_in}
 				lemon
-				${_lemonpardir}/lempar.c
+				${CMAKE_SOURCE_DIR}/tools/lemon/lempar.c
 		)
+	endmacro()
+endif()
+
+macro(ADD_LEMON_FILES _source _generated)
+
+	foreach (_current_FILE ${ARGN})
+		get_filename_component(_in ${_current_FILE} ABSOLUTE)
+		get_filename_component(_basename ${_current_FILE} NAME_WE)
+
+		set(_out ${CMAKE_CURRENT_BINARY_DIR}/${_basename})
+
+		generate_lemon_file(${_out} ${_in})
 
 		list(APPEND ${_source} ${_in})
 		list(APPEND ${_generated} ${_out}.c)
