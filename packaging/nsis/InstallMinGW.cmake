2048810a8f5194f460477815bbb945e7b6fc5965
João Valverde <j@v6e.pt>
2023-05-26 18:21:32 +0100

    Windows: Deploy MinGW DLLs with cross-compilation
    Use a static list to copy required system DLLs to the binary
    installer. We may want to make this list dynamic, by running
    peldd for example.
    
    Ping #19108

---
+++ packaging/nsis/InstallMinGW.cmake
@@ -0,0 +1,46 @@
+set(MINGW_BIN "/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
+
+if(MINGW AND NOT USE_MSYSTEM)
+	# mingw-w64 dlls
+	file(GLOB MINGW_DLLS
+		${MINGW_BIN}/iconv.dll
+		${MINGW_BIN}/icudata72.dll
+		${MINGW_BIN}/icui18n72.dll
+		${MINGW_BIN}/icuuc72.dll
+		${MINGW_BIN}/libbrotlicommon.dll
+		${MINGW_BIN}/libbrotlidec.dll
+		${MINGW_BIN}/libbrotlienc.dll
+		${MINGW_BIN}/libbz2-1.dll
+		${MINGW_BIN}/libcares-2.dll
+		${MINGW_BIN}/libcrypto-3-x64.dll
+		${MINGW_BIN}/libexpat-1.dll
+		${MINGW_BIN}/libffi-8.dll
+		${MINGW_BIN}/libfontconfig-1.dll
+		${MINGW_BIN}/libfreetype-6.dll
+		${MINGW_BIN}/libgcc_s_seh-1.dll
+		${MINGW_BIN}/libgcrypt-20.dll
+		${MINGW_BIN}/libglib-2.0-0.dll
+		${MINGW_BIN}/libgmodule-2.0-0.dll
+		${MINGW_BIN}/libgmp-10.dll
+		${MINGW_BIN}/libgnutls-30.dll
+		${MINGW_BIN}/libgpg-error-0.dll
+		${MINGW_BIN}/libharfbuzz-0.dll
+		${MINGW_BIN}/libhogweed-6.dll
+		${MINGW_BIN}/libintl-8.dll
+		${MINGW_BIN}/liblzma-5.dll
+		${MINGW_BIN}/libminizip-3.dll
+		${MINGW_BIN}/libnettle-8.dll
+		${MINGW_BIN}/libp11-kit-0.dll
+		${MINGW_BIN}/libpcre2-16-0.dll
+		${MINGW_BIN}/libpcre2-8-0.dll
+		${MINGW_BIN}/libpng16-16.dll
+		${MINGW_BIN}/libspeexdsp-1.dll
+		${MINGW_BIN}/libssp-0.dll
+		${MINGW_BIN}/libstdc++-6.dll
+		${MINGW_BIN}/libstdc++-6.dll
+		${MINGW_BIN}/libtasn1-6.dll
+		${MINGW_BIN}/libxml2-2.dll
+		${MINGW_BIN}/libzstd.dll
+		${MINGW_BIN}/zlib1.dll
+	)
+endif()
