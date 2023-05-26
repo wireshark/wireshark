66212f3b0df72b5a14d50ae16b651a449e0d8f5a
João Valverde <j@v6e.pt>
2023-05-26 14:16:06 +0100

    CMake: Remove module LocatePythonModule.cmake
    This module is unnecessary and frequently causes hard-to-debug
    issues during CMake's configure run. Nuke it.
    
    Additionally, it uses the Python imp module, which was removed
    in Python 3.12 (importlib has replacement functions since Python 3.4,
    but we don't need this, since we expect our modules to be inside
    our source tree.)
    
    (backported from 601bf39e6b2eaff9e77588ff1b1a8a987dad404d)

--- cmake/modules/UseAsn2Wrs.cmake
+++ cmake/modules/UseAsn2Wrs.cmake
@@ -6,11 +6,6 @@
 #                     absolute path (e.g. "${CMAKE_CURRENT_SOURCE_DIR}").
 
 function(ASN2WRS)
-	if(NOT PY_ASN2WRS)
-		include(LocatePythonModule)
-		locate_python_module(asn2wrs REQUIRED PATHS "${CMAKE_SOURCE_DIR}/tools")
-	endif()
-
 	if(NOT PROTO_OPT)
 		set(PROTO_OPT -p ${PROTOCOL_NAME})
 	elseif(PROTO_OPT STREQUAL "_EMPTY_")
@@ -39,7 +34,7 @@ function(ASN2WRS)
 	add_custom_command(
 		OUTPUT packet-${PROTOCOL_NAME}-stamp
 		COMMAND "${PYTHON_EXECUTABLE}"
-			${PY_ASN2WRS}
+			${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
 			${A2W_FLAGS}
 			${PROTO_OPT}
 			-c "${CMAKE_CURRENT_SOURCE_DIR}/${PROTOCOL_NAME}.cnf"
@@ -53,7 +48,7 @@ function(ASN2WRS)
 				"${A2W_OUTPUT_DIR}/packet-${PROTOCOL_NAME}.c"
 				packet-${PROTOCOL_NAME}-stamp
 		DEPENDS
-			"${PY_ASN2WRS}"
+			${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
 			${SRC_FILES}
 			${EXTRA_CNF_targets}
 			${EXTRA_CNF}
@@ -68,7 +63,7 @@ function(ASN2WRS)
 		add_custom_command(
 			OUTPUT ${_asn2wrs_export_file}
 			COMMAND "${PYTHON_EXECUTABLE}"
-				"${PY_ASN2WRS}"
+				${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
 				-E
 				${A2W_FLAGS}
 				${PROTO_OPT}
@@ -76,7 +71,7 @@ function(ASN2WRS)
 				-D "${CMAKE_CURRENT_SOURCE_DIR}"
 				${EXT_ASN_FILE_LIST} ${ASN_FILE_LIST} ${EXT_ASN_FILE_LIST_LATE}
 			DEPENDS
-				"${PY_ASN2WRS}"
+				${CMAKE_SOURCE_DIR}/tools/asn2wrs.py
 				${SRC_FILES}
 				${EXPORT_DEPENDS_targets}
 				${EXPORT_DEPENDS}
