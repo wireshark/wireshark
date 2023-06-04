4aee80e57925cdc339768e9c87b85d113b42a4b1
João Valverde <j@v6e.pt>
2023-06-04 20:00:00 +0100

    Tests: Remove unittest dependency (unittests)

--- test/suite_unittests.py
+++ test/suite_unittests.py
@@ -10,56 +10,52 @@
 #
 '''EPAN unit tests'''
 
-import difflib
-import os.path
-import re
-import subprocesstest
-import fixtures
+import subprocess
+import pytest
 
 
-@fixtures.uses_fixtures
-class case_unittests(subprocesstest.SubprocessTestCase):
+class TestUnitTests:
     def test_unit_exntest(self, program, base_env):
         '''exntest'''
-        self.assertRun(program('exntest'), env=base_env)
+        subprocess.check_call(program('exntest'), env=base_env)
 
     def test_unit_oids_test(self, program, base_env):
         '''oids_test'''
-        self.assertRun(program('oids_test'), env=base_env)
+        subprocess.check_call(program('oids_test'), env=base_env)
 
     def test_unit_reassemble_test(self, program, base_env):
         '''reassemble_test'''
-        self.assertRun(program('reassemble_test'), env=base_env)
+        subprocess.check_call(program('reassemble_test'), env=base_env)
 
     def test_unit_tvbtest(self, program, base_env):
         '''tvbtest'''
-        self.assertRun(program('tvbtest'), env=base_env)
+        subprocess.check_call(program('tvbtest'), env=base_env)
 
     def test_unit_wmem_test(self, program, base_env):
         '''wmem_test'''
-        self.assertRun((program('wmem_test'),
+        subprocess.check_call((program('wmem_test'),
             '--verbose'
         ), env=base_env)
 
     def test_unit_wscbor_test(self, program, base_env):
         '''wscbor_test'''
-        self.assertRun(program('wscbor_test'), env=base_env)
+        subprocess.check_call(program('wscbor_test'), env=base_env)
 
     def test_unit_epan(self, program, base_env):
         '''epan unit tests'''
-        self.assertRun((program('test_epan'),
+        subprocess.check_call((program('test_epan'),
             '--verbose'
         ), env=base_env)
 
     def test_unit_wsutil(self, program, base_env):
         '''wsutil unit tests'''
-        self.assertRun((program('test_wsutil'),
+        subprocess.check_call((program('test_wsutil'),
             '--verbose'
         ), env=base_env)
 
     def test_unit_fieldcount(self, cmd_tshark, test_env):
         '''fieldcount'''
-        self.assertRun((cmd_tshark, '-G', 'fieldcount'), env=test_env)
+        subprocess.check_call((cmd_tshark, '-G', 'fieldcount'), env=test_env)
 
 class Proto:
     """Data for a protocol."""
@@ -85,13 +81,13 @@ class Field:
         self.blurb = data[7]
 
 
-@fixtures.uses_fixtures
-class case_unit_ftsanity(subprocesstest.SubprocessTestCase):
+class TestUnitFtSanity:
     def test_unit_ftsanity(self, cmd_tshark, base_env):
         """Looks for problems in field type definitions."""
-        tshark_proc = self.assertRun((cmd_tshark, "-G", "fields"), env=base_env)
+        tshark_proc = subprocess.run((cmd_tshark, "-G", "fields"),
+                        check=True, capture_output=True, encoding='utf-8', env=base_env)
 
-        lines = tshark_proc.stdout_str.splitlines()
+        lines = tshark_proc.stdout.splitlines()
         # XXX We don't currently check protos.
         protos = [Proto(x) for x in lines if x[0] == "P"]
         fields = [Field(x) for x in lines if x[0] == "F"]
@@ -106,4 +102,4 @@ class case_unit_ftsanity(subprocesstest.SubprocessTestCase):
                     err_list.append("%s has a bitmask 0x%x but is type %s" % \
                             (field.abbrev, field.bitmask, field.ftype))
 
-        self.assertEqual(len(err_list), 0, 'Found field type errors: \n' + '\n'.join(err_list))
+        assert len(err_list) == 0, 'Found field type errors: \n' + '\n'.join(err_list)
