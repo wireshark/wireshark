d8a9e09166d69520b706f9f3716a99250bcaef81
João Valverde <j@v6e.pt>
2023-06-04 17:34:30 +0100

    Tests: Remove unittest dependency (outputformats)

--- test/suite_outputformats.py
+++ test/suite_outputformats.py
@@ -10,19 +10,18 @@
 
 import json
 import os.path
-import subprocesstest
-import fixtures
+import subprocess
 from matchers import *
+import pytest
 
-
-@fixtures.fixture
+@pytest.fixture
 def check_outputformat(cmd_tshark, request, dirs, capture_file):
     def check_outputformat_real(format_option, pcap_file='dhcp.pcap',
-                                extra_args=[], expected=None, multiline=False):
+                                extra_args=[], expected=None, multiline=False, env=None):
         ''' Check a capture file against a sample, in json format. '''
-        self = request.instance
-        tshark_proc = self.assertRun([cmd_tshark, '-r', capture_file(pcap_file),
-                                      '-T', format_option] + extra_args)
+        tshark_proc = subprocess.run([cmd_tshark, '-r', capture_file(pcap_file),
+                                      '-T', format_option] + extra_args,
+                                      check=True, capture_output=True, encoding='utf-8', env=env)
 
         # If a filename is given, load the expected values from those.
         if isinstance(expected, str):
@@ -31,41 +30,39 @@ def check_outputformat(cmd_tshark, request, dirs, capture_file):
                 expected = [json.loads(line) for line in testdata.splitlines()]
             else:
                 expected = json.loads(testdata)
-        actual = tshark_proc.stdout_str
+        actual = tshark_proc.stdout
         if multiline:
             actual = actual.splitlines()
-            self.assertEqual(len(expected), len(actual))
+            assert len(expected) == len(actual)
             for expectedObj, actualStr in zip(expected, actual):
-                self.assertEqual(expectedObj, json.loads(actualStr))
+                assert expectedObj == json.loads(actualStr)
         else:
             actual = json.loads(actual)
-            self.assertEqual(expected, actual)
+            assert expected == actual
 
     return check_outputformat_real
 
 
-@fixtures.mark_usefixtures('base_env')
-@fixtures.uses_fixtures
-class case_outputformats(subprocesstest.SubprocessTestCase):
+class TestOutputFormats:
     maxDiff = 1000000
 
-    def test_outputformat_json(self, check_outputformat):
+    def test_outputformat_json(self, check_outputformat, base_env):
         '''Decode some captures into json'''
-        check_outputformat("json", expected="dhcp.json")
+        check_outputformat("json", expected="dhcp.json", env=base_env)
 
-    def test_outputformat_jsonraw(self, check_outputformat):
+    def test_outputformat_jsonraw(self, check_outputformat, base_env):
         '''Decode some captures into jsonraw'''
-        check_outputformat("jsonraw", expected="dhcp.jsonraw")
+        check_outputformat("jsonraw", expected="dhcp.jsonraw", env=base_env)
 
-    def test_outputformat_ek(self, check_outputformat):
+    def test_outputformat_ek(self, check_outputformat, base_env):
         '''Decode some captures into ek'''
-        check_outputformat("ek", expected="dhcp.ek", multiline=True)
+        check_outputformat("ek", expected="dhcp.ek", multiline=True, env=base_env)
 
-    def test_outputformat_ek_raw(self, check_outputformat):
+    def test_outputformat_ek_raw(self, check_outputformat, base_env):
         '''Decode some captures into ek, with raw data'''
-        check_outputformat("ek", expected="dhcp-raw.ek", multiline=True, extra_args=['-x'])
+        check_outputformat("ek", expected="dhcp-raw.ek", multiline=True, extra_args=['-x'], env=base_env)
 
-    def test_outputformat_json_select_field(self, check_outputformat):
+    def test_outputformat_json_select_field(self, check_outputformat, base_env):
         '''Checks that the -e option works with -Tjson.'''
         check_outputformat("json", extra_args=['-eframe.number', '-c1'], expected=[
             {
@@ -80,16 +77,16 @@ class case_outputformats(subprocesstest.SubprocessTestCase):
                     }
                 }
             }
-        ])
+        ], env=base_env)
 
-    def test_outputformat_ek_select_field(self, check_outputformat):
+    def test_outputformat_ek_select_field(self, check_outputformat, base_env):
         '''Checks that the -e option works with -Tek.'''
         check_outputformat("ek", extra_args=['-eframe.number', '-c1'], expected=[
             {"index": {"_index": "packets-2004-12-05", "_type": "doc"}},
             {"timestamp": "1102274184317", "layers": {"frame_number": ["1"]}}
-        ], multiline=True)
+        ], multiline=True, env=base_env)
 
-    def test_outputformat_ek_filter_field(self, check_outputformat):
+    def test_outputformat_ek_filter_field(self, check_outputformat, base_env):
         ''' Check that the option -j works with -Tek.'''
         check_outputformat("ek", extra_args=['-j', 'dhcp'], expected="dhcp-filter.ek",
-            multiline=True)
+            multiline=True, env=base_env)
