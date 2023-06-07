6d3560d10033b7094a83e43c1d75bce762e4523a
John Thacker <johnthacker@gmail.com>
2023-06-07 06:44:06 -0400

    RFC7468: Use STRING_CASE_INSENSITIVE instead of TRUE
    Update for 05e404e8cbb8c3e45324fc924f0e9a30c72c9d32
    This got missed because it's a whole file dissector.

--- epan/dissectors/file-rfc7468.c
+++ epan/dissectors/file-rfc7468.c
@@ -459,7 +459,7 @@ proto_register_rfc7468(void)
 
     rfc7468_label_table = register_dissector_table("rfc7468.preeb_label", "FFF",
                                                    proto_rfc7468, FT_STRING,
-                                                   TRUE);
+                                                   STRING_CASE_INSENSITIVE);
 
     rfc7468_handle = register_dissector("rfc7468", dissect_rfc7468, proto_rfc7468);
 }
