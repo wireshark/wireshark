c3cbff5a23e43856ac5d62f8a709dfc7eef13723
Guy Harris <gharris@sonic.net>
2023-06-16 16:59:22 -0700

    Redo address_with_resolution_to_str_buf().
    Base "do we add the address string, wrapped in parentheses?" on "is the
    address string non-empty?"  That handles both AT_NONE and various string
    types when the string in question is empty.
    
    If the rsolved name string is empty, don't just skip adding parentheses
    around the address string, compute the space needed differently.

--- epan/address_types.c
+++ epan/address_types.c
@@ -951,7 +951,6 @@ static void address_with_resolution_to_str_buf(const address* addr, gchar *buf, 
     address_type_t *at;
     int addr_len;
     gsize pos;
-    gboolean empty;
 
     if (!buf || !buf_len)
         return;
@@ -975,28 +974,58 @@ static void address_with_resolution_to_str_buf(const address* addr, gchar *buf, 
     /* Copy the resolved name */
     g_strlcpy(buf, at->addr_name_res_str(addr), buf_len);
 
-    /* Don't wrap "emptyness" in parentheses */
-    if (addr->type == AT_NONE)
-        return;
-
-    /* Make sure there is enough room for the address string wrapped in parentheses */
+    /* Get the length of the copied resolved name */
     pos = strlen(buf);
-    if ((int)(pos + 4 + at->addr_str_len(addr)) >= buf_len)
+
+    /* Get an upper bound on the length of the address string. */
+    addr_len = at->addr_str_len(addr);
+    /*
+     * That includes the terminating '\0', so we subtract 1
+     * to get the length prior to the terminator.
+     */
+    addr_len--;
+
+    /*
+     * If the upper bound is 0, that means that the address string is
+     * empty, so don't add it after the resolved name.
+     */
+    if (addr_len == 0)
         return;
 
-    empty = (pos < 1) ? TRUE : FALSE;
+    /*
+     * If the resolved name is an empty string, don't wrap parentheses
+     * around the address string.
+     */
+    if (pos == 0) {
+        /*
+         * The resolved name is an empty string.
+         * Make sure there's room in the buffer for the address string;
+         * addr_len + 1 includes the terminating '\0', and buf_len
+         * includes room for the terminating '\0', so if the former
+         * is greater than the latter, there isn't room.
+         */
+        if (addr_len + 1 > buf_len)
+            return;
 
-    if (!empty)
-    {
+        /* There is; just put the address string into the buffer. */
+        at->addr_to_str(addr, buf, buf_len);
+    } else {
+        /*
+         * Make sure there is enough room for the maximum length of the
+         * address string wrapped in parentheses.  That's pos (the
+         * length of the resolved name plus 2 (for " (" plus addr_len
+         * (the length of the address string) plus 2 (for ")\0");
+         * it must not be greater than the buffer length.
+         */
+        if ((int)(pos + 4 + addr_len) > buf_len)
+            return;
+
         buf[pos++] = ' ';
         buf[pos++] = '(';
-    }
 
-    addr_len = at->addr_to_str(addr, &buf[pos], (int)(buf_len-pos));
-    pos += addr_len - 1; /* addr_len includes the trailing '\0' */
+        addr_len = at->addr_to_str(addr, &buf[pos], (int)(buf_len-pos));
+        pos += addr_len - 1; /* addr_len includes the trailing '\0' */
 
-    if (!empty)
-    {
         buf[pos++] = ')';
         buf[pos++] = '\0';
     }
