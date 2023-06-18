dafd3342701379c198c783239716eac245025e2c
Gerald Combs <gerald@wireshark.org>
2023-06-18 14:23:40 -0700

    Tools: Switch make-reg.py to standard C types

--- tools/make-regs.py
+++ tools/make-regs.py
@@ -57,8 +57,8 @@ def make_dissectors(outfile, infiles):
     output += """\
 #include "dissectors.h"
 
-const gulong dissector_reg_proto_count = {0};
-const gulong dissector_reg_handoff_count = {1};
+const unsigned long dissector_reg_proto_count = {0};
+const unsigned long dissector_reg_handoff_count = {1};
 
 """.format(len(protos), len(handoffs))
 
@@ -118,7 +118,7 @@ def make_taps(outfile, infiles):
     output += """\
 #include "ui/taps.h"
 
-const gulong tap_reg_listener_count = {0};
+const unsigned long tap_reg_listener_count = {0};
 
 """.format(len(taps))
 
