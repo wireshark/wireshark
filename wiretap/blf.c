wireshark/wireshark

commit 9bdf256a8d3c37118f9926cb7b4ae3694c576306

BLF: Put EthernetStatus Objects in packet lis using Exported PDU
Put AppText ans EthernetStatus objects in the packet list.
Closes #19269

wiretap/blf.c
@@ -24,6 +24,7 @@
 #include <errno.h>
 #include <epan/value_string.h>
 #include <wsutil/wslog.h>
+#include <wsutil/exported_pdu_tlvs.h>
 #include "file_wrappers.h"
 #include "wtap-int.h"
 
@@ -420,6 +421,21 @@ fix_endianness_blf_apptext_header(blf_apptext_t *header) {
     header->reservedAppText1 = GUINT32_FROM_LE(header->reservedAppText1);
     header->textLength = GUINT32_FROM_LE(header->textLength);
     header->reservedAppText2 = GUINT32_FROM_LE(header->reservedAppText2);
+}
+
+static void
+fix_endianness_blf_ethernet_status_header(blf_ethernet_status_t* header) {
+    header->channel = GUINT16_FROM_LE(header->channel);
+    header->flags = GUINT16_FROM_LE(header->channel);
+    /*uint8_t linkStatus;*/
+    /*uint8_t ethernetPhy;*/
+    /*uint8_t duplex;*/
+    /*uint8_t mdi;*/
+    /*uint8_t connector;*/
+    /*uint8_t clockMode;*/
+    /*uint8_t pairs;*/
+    /*uint8_t hardwareChannel;*/
+    header->bitrate = GUINT32_FROM_LE(header->bitrate);
 }
 
 static guint64
@@ -1998,7 +2014,7 @@ blf_read_linmessage(blf_params_t *params, int *err, gchar **err_info, gint64 blo
     return TRUE;
 }
 
-static gboolean
+static int
 blf_read_apptextmessage(blf_params_t *params, int *err, gchar **err_info, gint64 block_start, gint64 data_start, gint64 object_length, guint64 timestamp _U_) {
     blf_apptext_t            apptextheader;
 
@@ -2006,73 +2022,153 @@ blf_read_apptextmessage(blf_params_t *params, int *err, gchar **err_info, gint64
         *err = WTAP_ERR_BAD_FILE;
         *err_info = ws_strdup_printf("blf: APP_TEXT: not enough bytes for apptext header in object");
         ws_debug("not enough bytes for apptext header in object");
-        return FALSE;
+        return BLF_APPTEXT_FAILED;
     }
 
     if (!blf_read_bytes(params, data_start, &apptextheader, sizeof(apptextheader), err, err_info)) {
         ws_debug("not enough bytes for apptext header in file");
-        return FALSE;
+        return BLF_APPTEXT_FAILED;
     }
     fix_endianness_blf_apptext_header(&apptextheader);
 
-    if (apptextheader.source != BLF_APPTEXT_CHANNEL) {
-        return TRUE;
-    }
-
     /* Add an extra byte for a terminating '\0' */
-    gchar *text = g_try_malloc((gsize)apptextheader.textLength + 1);
+    gchar* text = g_try_malloc((gsize)apptextheader.textLength + 1);
 
     if (!blf_read_bytes(params, data_start + sizeof(apptextheader), text, apptextheader.textLength, err, err_info)) {
         ws_debug("not enough bytes for apptext text in file");
         g_free(text);
-        return FALSE;
+        return BLF_APPTEXT_FAILED;
     }
     text[apptextheader.textLength] = '\0'; /* Here's the '\0' */
 
-    /* returns a NULL terminated array of NULL terminates strings */
-    gchar **tokens = g_strsplit_set(text, ";", -1);
+    switch (apptextheader.source) {
+    case BLF_APPTEXT_CHANNEL:
+    {
 
-    if ( tokens == NULL || tokens[0] == NULL || tokens[1] == NULL) {
-        if (tokens != NULL) {
-            g_strfreev(tokens);
+        /* returns a NULL terminated array of NULL terminates strings */
+        gchar** tokens = g_strsplit_set(text, ";", -1);
+
+        if (tokens == NULL || tokens[0] == NULL || tokens[1] == NULL) {
+            if (tokens != NULL) {
+                g_strfreev(tokens);
+            }
+            g_free(text);
+            return BLF_APPTEXT_CHANNEL;
         }
+
+        guint32 channel = (apptextheader.reservedAppText1 >> 8) & 0xff;
+        int pkt_encap;
+
+        switch ((apptextheader.reservedAppText1 >> 16) & 0xff) {
+        case BLF_BUSTYPE_CAN:
+            pkt_encap = WTAP_ENCAP_SOCKETCAN;
+            break;
+
+        case BLF_BUSTYPE_FLEXRAY:
+            pkt_encap = WTAP_ENCAP_FLEXRAY;
+            break;
+
+        case BLF_BUSTYPE_LIN:
+            pkt_encap = WTAP_ENCAP_LIN;
+            break;
+
+        case BLF_BUSTYPE_ETHERNET:
+            pkt_encap = WTAP_ENCAP_ETHERNET;
+            break;
+
+        case BLF_BUSTYPE_WLAN:
+            pkt_encap = WTAP_ENCAP_IEEE_802_11;
+            break;
+
+        default:
+            pkt_encap = 0xffffffff;
+        }
+
+        /* we use lookup to create interface, if not existing yet */
+        blf_lookup_interface(params, pkt_encap, channel, tokens[1]);
+
+        g_strfreev(tokens);
         g_free(text);
-        return TRUE;
+        return BLF_APPTEXT_CHANNEL;
+        break;
     }
+    case BLF_APPTEXT_METADATA:
+    case BLF_APPTEXT_COMMENT:
+        if (apptextheader.textLength < 5) {
+            /* Arbitary lengt chosen */
+            g_free(text);
+            return BLF_APPTEXT_CHANNEL; /* Cheat - no block to write */
+        }
+        wtap_buffer_append_epdu_tag(params->buf, EXP_PDU_TAG_DISSECTOR_NAME, "data-text-lines", 16);
+        wtap_buffer_append_epdu_tag(params->buf, EXP_PDU_TAG_COL_PROT_TEXT, "BLF App text", 13);
+        if (apptextheader.source == BLF_APPTEXT_METADATA) {
+            wtap_buffer_append_epdu_tag(params->buf, EXP_PDU_TAG_COL_INFO_TEXT, "Metadata", 18);
+        } else {
+            wtap_buffer_append_epdu_tag(params->buf, EXP_PDU_TAG_COL_INFO_TEXT, "Comment", 18);
+        }
 
-    guint32 channel = (apptextheader.reservedAppText1 >> 8) & 0xff;
+        wtap_buffer_append_epdu_end(params->buf);
 
-    int pkt_encap;
-    switch ((apptextheader.reservedAppText1 >> 16) & 0xff) {
-    case BLF_BUSTYPE_CAN:
-        pkt_encap = WTAP_ENCAP_SOCKETCAN;
-        break;
+        ws_buffer_assure_space(params->buf, apptextheader.textLength + 1);
+        ws_buffer_append(params->buf, text, apptextheader.textLength + 1);
 
-    case BLF_BUSTYPE_FLEXRAY:
-        pkt_encap = WTAP_ENCAP_FLEXRAY;
-        break;
-
-    case BLF_BUSTYPE_LIN:
-        pkt_encap = WTAP_ENCAP_LIN;
-        break;
-
-    case BLF_BUSTYPE_ETHERNET:
-        pkt_encap = WTAP_ENCAP_ETHERNET;
-        break;
-
-    case BLF_BUSTYPE_WLAN:
-        pkt_encap = WTAP_ENCAP_IEEE_802_11;
-        break;
-
+        /* We'll write this as a WS UPPER PDU packet with a text blob */
+        blf_init_rec(params, timestamp, WTAP_ENCAP_WIRESHARK_UPPER_PDU, 0, (guint32)ws_buffer_length(params->buf), (guint32)ws_buffer_length(params->buf));
+        g_free(text);
+        return apptextheader.source;
     default:
-        pkt_encap = 0xffffffff;
+        return BLF_APPTEXT_CHANNEL; /* Cheat - no block to write */;
+        break;
+    }
+    return BLF_APPTEXT_CHANNEL; /* Cheat - no block to write */
+}
+
+static gboolean
+blf_read_ethernet_status(blf_params_t* params, int* err, gchar** err_info, gint64 block_start, gint64 data_start, gint64 object_length, guint64 timestamp) {
+
+    blf_ethernet_status_t            ethernet_status_header;
+    guint8 tmpbuf[16];
+
+    if (object_length < (data_start - block_start) + (int)sizeof(ethernet_status_header)) {
+        *err = WTAP_ERR_BAD_FILE;
+        *err_info = ws_strdup_printf("blf: ETHERNET_STATUS: not enough bytes for apptext header in object");
+        ws_debug("not enough bytes for apptext header in object");
+        return FALSE;
     }
 
-    /* we use lookup to create interface, if not existing yet */
-    blf_lookup_interface(params, pkt_encap, channel, tokens[1]);
+    if (!blf_read_bytes(params, data_start, &ethernet_status_header, sizeof(ethernet_status_header), err, err_info)) {
+        ws_debug("not enough bytes for ethernet_status_header header in file");
+        return FALSE;
+    }
 
-    g_strfreev(tokens);
-    g_free(text);
+    fix_endianness_blf_ethernet_status_header(&ethernet_status_header);
+
+    tmpbuf[0] = (ethernet_status_header.channel & 0xff00) >> 8;
+    tmpbuf[1] = (ethernet_status_header.channel & 0x00ff);
+    tmpbuf[2] = (ethernet_status_header.flags & 0xff00) >> 8;
+    tmpbuf[3] = (ethernet_status_header.flags & 0x00ff);
+    tmpbuf[4] = (ethernet_status_header.linkStatus);
+    tmpbuf[5] = (ethernet_status_header.ethernetPhy);
+    tmpbuf[6] = (ethernet_status_header.duplex);
+    tmpbuf[7] = (ethernet_status_header.mdi);
+    tmpbuf[8] = (ethernet_status_header.connector);
+    tmpbuf[9] = (ethernet_status_header.clockMode);
+    tmpbuf[10] = (ethernet_status_header.pairs);
+    tmpbuf[11] = (ethernet_status_header.hardwareChannel);
+    tmpbuf[12] = (ethernet_status_header.bitrate & 0xff000000) >> 24;
+    tmpbuf[13] = (ethernet_status_header.bitrate & 0x00ff0000) >> 16;
+    tmpbuf[14] = (ethernet_status_header.bitrate & 0x0000ff00) >> 8;
+    tmpbuf[15] = (ethernet_status_header.bitrate & 0x000000ff);
+
+    wtap_buffer_append_epdu_tag(params->buf, EXP_PDU_TAG_DISSECTOR_NAME, "blf-ethernetstatus-obj", 23);
+    wtap_buffer_append_epdu_end(params->buf);
+
+    ws_buffer_assure_space(params->buf, sizeof(ethernet_status_header));
+    ws_buffer_append(params->buf, tmpbuf, (gsize)16);
+
+    /* We'll write this as a WS UPPER PDU packet with a data blob */
+    blf_init_rec(params, timestamp, WTAP_ENCAP_WIRESHARK_UPPER_PDU, ethernet_status_header.channel, (guint32)ws_buffer_length(params->buf), (guint32)ws_buffer_length(params->buf));
+
     return TRUE;
 }
 
@@ -2216,15 +2312,26 @@ blf_read_block(blf_params_t *params, gint64 start_pos, int *err, gchar **err_inf
             break;
 
         case BLF_OBJTYPE_APP_TEXT:
-            if (!blf_read_apptextmessage(params, err, err_info, start_pos, start_pos + header.header_length, header.object_length, timestamp)) {
-                /* we only return errors */
-                return FALSE;
+        {
+            int result = blf_read_apptextmessage(params, err, err_info, start_pos, start_pos + header.header_length, header.object_length, timestamp);
+            switch (result) {
+                case BLF_APPTEXT_FAILED:
+                    return FALSE;
+                case BLF_APPTEXT_METADATA:
+                    return TRUE;
+                case BLF_APPTEXT_COMMENT:
+                    return TRUE;
+                case BLF_APPTEXT_CHANNEL:
+                default:
+                    /* we do not return since there is no packet to show here */
+                start_pos += MAX(MAX(16, header.object_length), header.header_length);
             }
-
-            /* we do not return since there is no packet to show here */
-            start_pos += MAX(MAX(16, header.object_length), header.header_length);
+        }
             break;
 
+        case BLF_OBJTYPE_ETHERNET_STATUS:
+            return blf_read_ethernet_status(params, err, err_info, start_pos, start_pos + header.header_length, header.object_length, timestamp);
+            break;
         default:
             ws_debug("unknown object type 0x%04x", header.object_type);
             start_pos += MAX(MAX(16, header.object_length), header.header_length);

epan/dissectors/file-blf.c
@@ -25,6 +25,7 @@
 #include <wiretap/blf.h>
 
 static int proto_blf = -1;
+static int proto_blf_ethernetstatus_obj = -1;
 
 static dissector_handle_t xml_handle;
 
@@ -75,7 +76,6 @@ static int hf_blf_trigg_cond_triggerblocknamelength = -1;
 static int hf_blf_trigg_cond_triggerconditionlength = -1;
 static int hf_blf_trigg_cond_triggerblockname = -1;
 static int hf_blf_trigg_cond_triggercondition = -1;
-static int hf_blf_not_parsed = -1;
 static int hf_blf_sys_var_type = -1;
 static int hf_blf_sys_var_rep = -1;
 static int hf_blf_sys_var_reservedsystemvariable1 = -1;
@@ -584,7 +584,7 @@ dissect_blf_lobj(tvbuff_t *tvb, packet_info *pinfo _U_, proto_tree *tree, gint o
             proto_tree_add_item(subtree, hf_blf_eth_status_channel, tvb, offset, 2, ENC_LITTLE_ENDIAN);
             offset += 2;
             /* uint16_t flags; */
-            proto_tree_add_bitmask_list(subtree, tvb, offset, 2, flags1, ENC_BIG_ENDIAN);
+            proto_tree_add_bitmask_list(subtree, tvb, offset, 2, flags1, ENC_LITTLE_ENDIAN);
             offset += 2;
             /* uint8_t linkStatus {}; */
             proto_tree_add_item(subtree, hf_blf_eth_status_linkstatus, tvb, offset, 1, ENC_LITTLE_ENDIAN);
@@ -780,6 +780,101 @@ dissect_blf_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data
     return dissect_blf(tvb, pinfo, tree, NULL) > 0;
 }
 
+static int
+dissect_blf_ethernetstatus_obj(tvbuff_t* tvb, packet_info* pinfo, proto_tree* tree, void* data _U_) {
+
+    proto_item *ti;
+    proto_tree* blf_tree;
+    int offset = 0;
+
+    static int* const flags1[] = {
+        &hf_blf_eth_status_flags1_b8,
+        &hf_blf_eth_status_flags1_b7,
+        &hf_blf_eth_status_flags1_b6,
+        &hf_blf_eth_status_flags1_b5,
+        &hf_blf_eth_status_flags1_b4,
+        &hf_blf_eth_status_flags1_b3,
+        &hf_blf_eth_status_flags1_b2,
+        &hf_blf_eth_status_flags1_b1,
+        &hf_blf_eth_status_flags1_b0,
+        NULL
+    };
+
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "BLF Ethernet Status");
+    col_clear(pinfo->cinfo, COL_INFO);
+
+    ti = proto_tree_add_item(tree, proto_blf, tvb, offset, -1, ENC_NA);
+    blf_tree = proto_item_add_subtree(ti, ett_blf);
+
+    /* uint16_t channel {}; */
+    proto_tree_add_item(blf_tree, hf_blf_eth_status_channel, tvb, offset, 2, ENC_BIG_ENDIAN);
+    offset += 2;
+    /* uint16_t flags; */
+    uint16_t flags = tvb_get_ntohs(tvb, offset);
+    proto_tree_add_bitmask_list(blf_tree, tvb, offset, 2, flags1, ENC_BIG_ENDIAN);
+    offset += 2;
+
+    /* uint8_t linkStatus {}; */
+    uint32_t linkstatus;
+    ti = proto_tree_add_item_ret_uint(blf_tree, hf_blf_eth_status_linkstatus, tvb, offset, 1, ENC_BIG_ENDIAN, &linkstatus);
+    if ((flags & 0x0001) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    } else {
+        col_add_fstr(pinfo->cinfo, COL_INFO, "%s", val_to_str_const(linkstatus, blf_eth_status_linkstatus_vals, "Unknown"));
+    }
+    offset += 1;
+    /* uint8_t ethernetPhy {};*/
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_ethernetphy, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0002) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t duplex {}; */
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_duplex, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0004) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t mdi {}; */
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_mdi, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0008) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t connector {};*/
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_connector, tvb, offset, 1, ENC_BIG_ENDIAN);
+    proto_tree_add_item(blf_tree, hf_blf_eth_status_mdi, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0010) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t clockMode {}; */
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_clockmode, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0020) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t pairs {}; */
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_pairs, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0040) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint8_t hardwareChannel {};*/
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_hardwarechannel, tvb, offset, 1, ENC_BIG_ENDIAN);
+    if ((flags & 0x0080) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    offset += 1;
+    /* uint32_t bitrate {}; */
+    ti = proto_tree_add_item(blf_tree, hf_blf_eth_status_bitrate, tvb, offset, 4, ENC_BIG_ENDIAN);
+    if ((flags & 0x0100) == 0) {
+        proto_item_append_text(ti, " - Invalid");
+    }
+    return tvb_reported_length(tvb);
+}
+
+
 void
 proto_register_file_blf(void) {
     static hf_register_info hf[] = {
@@ -873,8 +968,6 @@ proto_register_file_blf(void) {
             { "Trigger blockname", "blf.object.trigg_con.triggerblockname", FT_STRINGZPAD, BASE_NONE, NULL, 0x00, NULL, HFILL }},
         { &hf_blf_trigg_cond_triggercondition,
             { "Trigger condition", "blf.object.trigg_con.triggercondition", FT_STRINGZPAD, BASE_NONE, NULL, 0x00, NULL, HFILL }},
-        { &hf_blf_not_parsed,
-            { "Not parsed", "blf.object.not_parsed", FT_BYTES, BASE_NONE, NULL, 0x00, NULL, HFILL }},
         { &hf_blf_sys_var_type,
             { "Type", "blf.object.sys_var.type", FT_UINT32, BASE_DEC, VALS(blf_sys_var_type_vals), 0x00, NULL, HFILL}},
         { &hf_blf_sys_var_rep,
@@ -961,10 +1054,14 @@ proto_register_file_blf(void) {
     };
 
     proto_blf = proto_register_protocol("BLF File Format", "File-BLF", "file-blf");
+    proto_blf_ethernetstatus_obj = proto_register_protocol("BLF Ethernet Status", "BLF-Ethermet-Status", "blf-ethermet-status");
+
     proto_register_field_array(proto_blf, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
 
     register_dissector("file-blf", dissect_blf, proto_blf);
+
+    register_dissector("blf-ethernetstatus-obj", dissect_blf_ethernetstatus_obj, proto_blf_ethernetstatus_obj);
 }
 
 void

wiretap/blf.h
@@ -471,6 +471,8 @@ typedef struct blf_apptext {
 #define BLF_APPTEXT_COMMENT  0x00000000
 #define BLF_APPTEXT_CHANNEL  0x00000001
 #define BLF_APPTEXT_METADATA 0x00000002
+#define BLF_APPTEXT_FAILED   0x000000FF
+
 
 #define BLF_BUSTYPE_CAN 1
 #define BLF_BUSTYPE_LIN 5
@@ -480,6 +482,21 @@ typedef struct blf_apptext {
 #define BLF_BUSTYPE_ETHERNET 11
 #define BLF_BUSTYPE_WLAN 13
 #define BLF_BUSTYPE_AFDX 14
+
+/* see https://bitbucket.org/tobylorenz/vector_blf/src/master/src/Vector/BLF/EthernetStatus.h */
+typedef struct blf_ethernet_status {
+    uint16_t channel;
+    uint16_t flags;
+    uint8_t linkStatus;
+    uint8_t ethernetPhy;
+    uint8_t duplex;
+    uint8_t mdi;
+    uint8_t connector;
+    uint8_t clockMode;
+    uint8_t pairs;
+    uint8_t hardwareChannel;
+    uint32_t bitrate;
+} blf_ethernet_status_t;
 
 
 /* see https://bitbucket.org/tobylorenz/vector_blf/src/master/src/Vector/BLF/ObjectHeaderBase.h */

commit 233da618a6d6ef68d809d2ffedb8f6a61484b937

BLF: fix a typo introduced in 9bdf256a8d

wiretap/blf.c
@@ -426,7 +426,7 @@ fix_endianness_blf_apptext_header(blf_apptext_t *header) {
 static void
 fix_endianness_blf_ethernet_status_header(blf_ethernet_status_t* header) {
     header->channel = GUINT16_FROM_LE(header->channel);
-    header->flags = GUINT16_FROM_LE(header->channel);
+    header->flags = GUINT16_FROM_LE(header->flags);
     /*uint8_t linkStatus;*/
     /*uint8_t ethernetPhy;*/
     /*uint8_t duplex;*/