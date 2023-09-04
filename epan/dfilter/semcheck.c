a60c493194bfdd0ce56e8f1797621c6f8eb43be9
João Valverde <j@v6e.pt>
2023-09-04 15:46:35 +0100

    dfilter: Add more error locations to semcheck logs

--- epan/dfilter/semcheck.c
+++ epan/dfilter/semcheck.c
@@ -37,6 +37,12 @@
 		dfilter_fail_throw(dfw, DF_ERROR_GENERIC, stnode_location(node), __VA_ARGS__); \
 	} while (0)
 
+#define FAIL_HERE(dfw) \
+	do {								\
+		ws_noisy("Semantic check failed here.");		\
+		THROW(TypeError); \
+	} while (0)
+
 typedef gboolean (*FtypeCanFunc)(enum ftenum);
 
 static ftenum_t
@@ -185,7 +191,7 @@ dfilter_fvalue_from_literal(dfwork_t *dfw, ftenum_t ftype, stnode_t *st,
 	}
 	if (fv == NULL) {
 		dfw_set_error_location(dfw, stnode_location(st));
-		THROW(TypeError);
+		FAIL_HERE(dfw);
 	}
 
 	return fv;
@@ -216,7 +222,7 @@ dfilter_fvalue_from_string(dfwork_t *dfw, ftenum_t ftype, stnode_t *st,
 	}
 	if (fv == NULL) {
 		dfw_set_error_location(dfw, stnode_location(st));
-		THROW(TypeError);
+		FAIL_HERE(dfw);
 	}
 
 	return fv;
@@ -558,7 +564,7 @@ dfilter_fvalue_from_charconst(dfwork_t *dfw, ftenum_t ftype, stnode_t *st)
 
 	if (fvalue == NULL) {
 		dfw_set_error_location(dfw, stnode_location(st));
-		THROW(TypeError);
+		FAIL_HERE(dfw);
 	}
 
 	return fvalue;
@@ -1188,6 +1194,7 @@ check_relation_matches(dfwork_t *dfw, stnode_t *st_node,
 	if (errmsg) {
 		dfilter_fail(dfw, DF_ERROR_GENERIC, stnode_location(st_arg2), "Regex compilation error: %s.", errmsg);
 		g_free(errmsg);
+		ws_noisy("Semantic check failed here with a regex syntax error");
 		THROW(TypeError);
 	}
 
@@ -1386,7 +1393,7 @@ check_arithmetic_LHS(dfwork_t *dfw, stnode_op_t st_op,
 				dfilter_fail(dfw, DF_ERROR_GENERIC, stnode_location(st_arg1),
 							"%s: %s", stnode_todisplay(st_arg1), err_msg);
 				g_free(err_msg);
-				THROW(TypeError);
+				FAIL_HERE(dfw);
 			}
 			/* Replaces unary operator with result */
 			stnode_replace(st_node, STTYPE_FVALUE, new_fv);
