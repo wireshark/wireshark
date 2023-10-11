0711fa7b1e7ff5ba5e880fb54495908d6958f5c6
mistral13 <mistral@gmx.ch>
2023-10-11 22:17:50 +0200

    btmesh: Various fixes from master
    1. Register the company ID as big endian.
    2. The pinfo column of the own (lua-)dissector was overwritten
       by the btmesh dissector.
    3. Output of the own (lua-)dissector as root-tree element.
    
    Fixed some code indent while already visiting this file.
    
    Ping #19388
    
    (cherry picked from commit d3fab19401caf09e2e65047d2843db9d3caf2131)
    (cherry picked from commit 50f322ec1702f4ce0e2d94a19509a82b3fb3df24)
    (cherry picked from commit 38d05ad97a4a2158b834f973a5b6a034c78edc7b)
    (cherry picked from commit a4527fa0418605318c8986bc8947a606e72043a9)

--- epan/dissectors/packet-btmesh.c
+++ epan/dissectors/packet-btmesh.c
@@ -4779,6 +4779,7 @@ dissect_btmesh_model_layer(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
     proto_tree *manufacturer_property_ids_tree;
     proto_tree *generic_client_property_ids_tree;
     proto_tree *sensor_setting_property_ids_tree;
+    proto_tree *root_tree = proto_tree_get_parent_tree(tree);
 
     guint32 netkeyindexes, appkeyindexes;
     guint32 nums, numv, element;
@@ -4794,17 +4795,17 @@ dissect_btmesh_model_layer(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, 
         if (opcode & 0x40) {
             /* Vendor opcode */
             proto_tree_add_item(sub_tree, hf_btmesh_model_layer_vendor_opcode, tvb, offset, 1, ENC_NA);
-            vendor = tvb_get_guint16(tvb, offset + 1, ENC_BIG_ENDIAN);
+            vendor = tvb_get_guint16(tvb, offset + 1, ENC_LITTLE_ENDIAN);
             proto_tree_add_item(sub_tree, hf_btmesh_model_layer_vendor, tvb, offset + 1, 2, ENC_LITTLE_ENDIAN);
             payload_tvb = tvb_new_subset_remaining(tvb, offset);
-            dissector_try_uint_new(btmesh_model_vendor_dissector_table, vendor, payload_tvb, pinfo, tree, TRUE, GUINT_TO_POINTER(vendor));
             col_set_str(pinfo->cinfo, COL_INFO, "Access Message - Vendor Opcode");
+            dissector_try_uint_new(btmesh_model_vendor_dissector_table, vendor, payload_tvb, pinfo, root_tree, TRUE, GUINT_TO_POINTER(vendor));
             offset+=3;
         } else {
-        /* Two octet opcode */
-        proto_tree_add_item_ret_uint(sub_tree, hf_btmesh_model_layer_opcode, tvb, offset, 2, ENC_NA, &opcode);
-        col_set_str(pinfo->cinfo, COL_INFO, val_to_str_const(opcode, btmesh_models_opcode_vals, "Access Message Unknown"));
-        offset+=2;
+            /* Two octet opcode */
+            proto_tree_add_item_ret_uint(sub_tree, hf_btmesh_model_layer_opcode, tvb, offset, 2, ENC_NA, &opcode);
+            col_set_str(pinfo->cinfo, COL_INFO, val_to_str_const(opcode, btmesh_models_opcode_vals, "Access Message Unknown"));
+            offset+=2;
         }
     } else {
         /* One octet opcode */
