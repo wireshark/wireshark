6154e5ba7061c26904401480502935e8e2114b47
Dr. Lars Völker <lars.voelker@technica-engineering.de>
2023-10-30 23:48:16 +0100

    SOMEIP: Add macros to header and clean up
    (cherry picked from commit 8bbec5cc54246f29d243507179d695ec6c67001b)

--- epan/dissectors/packet-someip.h
+++ epan/dissectors/packet-someip.h
@@ -1,7 +1,7 @@
 /* packet-someip.h
  * Definitions for SOME/IP packet disassembly structures and routines
  * By Dr. Lars Voelker <lars.voelker@technica-engineering.de> / <lars.voelker@bmw.de>
- * Copyright 2012-2022 Dr. Lars Voelker
+ * Copyright 2012-2023 Dr. Lars Voelker
  *
  * Wireshark - Network traffic analyzer
  * By Gerald Combs <gerald@wireshark.org>
@@ -10,16 +10,18 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#ifndef __PACKET_SOMEIP_H__
+#define __PACKET_SOMEIP_H__
+
 /* used for SD to add ports dynamically */
 void register_someip_port_udp(guint32 portnumber);
 void register_someip_port_tcp(guint32 portnumber);
 
 /* look up names for SD */
-char* someip_lookup_service_name(guint16 serviceid);
-char* someip_lookup_eventgroup_name(guint16 serviceid, guint16 eventgroupid);
+char *someip_lookup_service_name(guint16 serviceid);
+char *someip_lookup_eventgroup_name(guint16 serviceid, guint16 eventgroupid);
 
-typedef struct _someip_info
-{
+typedef struct _someip_info {
     guint16 service_id;
     guint16 method_id;
     guint16 client_id;
@@ -35,6 +37,8 @@ typedef struct _someip_messages_tap {
     guint8  interface_version;
     guint8  message_type;
 } someip_messages_tap_t;
+
+#endif /* __PACKET_SOMEIP_H__ */
 
 /*
  * Editor modelines
