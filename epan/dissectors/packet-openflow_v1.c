4da59dbd0031e067f18ce1889357da5efedd07cb
Alexis La Goutte <alexis.lagoutte@gmail.com>
2023-11-04 10:12:44 +0100

    OpenFlow(v1): Add Actions to FLOW_MOD
    Close: #17072
    
    
    (cherry picked from commit 4b6438ee3696a80bf8acaf31db72c804bcad205c)

--- epan/dissectors/packet-openflow_v1.c
+++ epan/dissectors/packet-openflow_v1.c
@@ -728,7 +728,9 @@ dissect_openflow_flow_mod(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, i
 
     /* uint16_t flags; One of OFPFF_*. */
     proto_tree_add_item(tree, hf_openflow_flags, tvb, offset, 2, ENC_BIG_ENDIAN);
-    /*offset+=2;*/
+    offset+=2;
+
+    dissect_openflow_action_header(tvb, pinfo, tree, offset);
 }
 
 
