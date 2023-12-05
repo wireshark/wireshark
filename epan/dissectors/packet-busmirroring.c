d069a39c8911eccd234ccb2729f18f632b406a75
liu0hy <liu0hy@gmail.com>
2023-12-05 14:42:52 +0000

    Busmirroring: Add expert item for invalid value

--- epan/dissectors/packet-busmirroring.c
+++ epan/dissectors/packet-busmirroring.c
@@ -21,12 +21,13 @@
 
 #include "config.h"
 #include <epan/packet.h>
+#include <epan/expert.h>
 
 #define BUSMIRRORING_UDP_PORT 30511
 
 enum network_type
 {
-    NETWORK_TYPE_UNKNOWN = 0x00,
+    NETWORK_TYPE_INVALID = 0x00,
     NETWORK_TYPE_CAN = 0x01,
     NETWORK_TYPE_LIN = 0x02,
     NETWORK_TYPE_FLEXRAY = 0x03,
@@ -79,93 +80,127 @@ static int ett_header_timestamp;
 static int ett_data_item;
 static int ett_network_state;
 static int ett_frame_id;
+static expert_field ei_data_incomplete;
+static expert_field ei_data_item_incomplete;
+static expert_field ei_network_type_invalid;
+static expert_field ei_can_id_invalid;
+static expert_field ei_lin_pid_invalid;
+static expert_field ei_can_length_invalid;
+static expert_field ei_lin_length_invalid;
+
+static const uint8_t pid_table[] = {
+    0x80, 0xC1, 0x42, 0x03, 0xC4, 0x85, 0x06, 0x47,
+    0x08, 0x49, 0xCA, 0x8B, 0x4C, 0x0D, 0x8E, 0xCF,
+    0x50, 0x11, 0x92, 0xD3, 0x14, 0x55, 0xD6, 0x97,
+    0xD8, 0x99, 0x1A, 0x5B, 0x9C, 0xDD, 0x5E, 0x1F,
+    0x20, 0x61, 0xE2, 0xA3, 0x64, 0x25, 0xA6, 0xE7,
+    0xA8, 0xE9, 0x6A, 0x2B, 0xEC, 0xAD, 0x2E, 0x6F,
+    0xF0, 0xB1, 0x32, 0x73, 0xB4, 0xF5, 0x76, 0x37,
+    0x78, 0x39, 0xBA, 0xFB, 0x3C, 0x7D, 0xFE, 0xBF
+};
+
+static bool is_lin_pid_valid(uint8_t pid) {
+    return pid == pid_table[pid & 0x3F];
+}
 
 static int
 dissect_busmirroring(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_)
 {
-    static const int header_size = 14;
-    int buffer_length = tvb_captured_length(tvb);
+    static const uint32_t header_size = 14;
+    uint32_t buffer_length = tvb_captured_length(tvb);
     if (buffer_length < header_size)
     {
         return 0;
     }
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "BUSMIRRORING");
-    col_clear(pinfo->cinfo, COL_INFO);
-    col_add_fstr(pinfo->cinfo, COL_INFO, "Busmirroring Seq=%u Len=%u", tvb_get_guint8(tvb, 1), tvb_get_guint16(tvb, 12, ENC_BIG_ENDIAN));
 
     proto_item *ti = proto_tree_add_item(tree, proto_busmirroring, tvb, 0, -1, ENC_NA);
     proto_tree *busmirroring_tree = proto_item_add_subtree(ti, ett_busmirroring);
     proto_tree_add_item(busmirroring_tree, hf_protocol_version, tvb, 0, 1, ENC_BIG_ENDIAN);
     proto_tree_add_item(busmirroring_tree, hf_sequence_number, tvb, 1, 1, ENC_BIG_ENDIAN);
-    nstime_t header_timestamp;
+    nstime_t header_timestamp = {0, 0};
     header_timestamp.secs = tvb_get_guint48(tvb, 2, ENC_BIG_ENDIAN);
     header_timestamp.nsecs = tvb_get_guint32(tvb, 8, ENC_BIG_ENDIAN);
     proto_item *ht_item = proto_tree_add_time(busmirroring_tree, hf_header_timestamp, tvb, 2, 10, &header_timestamp);
     proto_tree *ht_tree = proto_item_add_subtree(ht_item, ett_header_timestamp);
     proto_tree_add_item(ht_tree, hf_seconds, tvb, 2, 6, ENC_BIG_ENDIAN);
     proto_tree_add_item(ht_tree, hf_nanoseconds, tvb, 8, 4, ENC_BIG_ENDIAN);
-    proto_tree_add_item(busmirroring_tree, hf_data_length, tvb, 12, 2, ENC_BIG_ENDIAN);
+    uint32_t data_length = 0;
+    proto_tree_add_item_ret_uint(busmirroring_tree, hf_data_length, tvb, 12, 2, ENC_BIG_ENDIAN, &data_length);
+    if (header_size + data_length > buffer_length) {
+        expert_add_info(pinfo, ti, &ei_data_incomplete);
+    }
 
     int data_item_index = 0;
-    int offset = header_size;
-    static const int min_data_header_size = 4;
-    while (offset + min_data_header_size <= buffer_length)
+    uint32_t offset = header_size;
+    while (offset < buffer_length)
     {
-        int data_item_length = min_data_header_size;
-        uint8_t flags = tvb_get_guint8(tvb, offset + 2);
+        int data_item_start = offset;
+        proto_item *data_item = proto_tree_add_item(busmirroring_tree, proto_busmirroring, tvb, offset, 0, ENC_NA);
+        proto_item_set_text(data_item, "Data Item #%d", data_item_index);
+        ++data_item_index;
+        col_clear(pinfo->cinfo, COL_INFO);
+        col_add_fstr(pinfo->cinfo, COL_INFO, "Busmirroring Seq=%u Len=%u DataItem=%u",
+            tvb_get_guint8(tvb, 1), tvb_get_guint16(tvb, 12, ENC_BIG_ENDIAN), data_item_index);
+        if (offset + 2 > buffer_length) {
+            expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+            return buffer_length;
+        }
+        proto_tree *data_tree = proto_item_add_subtree(data_item, ett_data_item);
+        proto_tree_add_item(data_tree, hf_timestamp, tvb, offset, 2, ENC_BIG_ENDIAN);
+        offset += 2;
+        proto_item_set_len(data_item, offset - data_item_start);
+
+        if (offset + 1 > buffer_length) {
+            expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+            return buffer_length;
+        }
+        uint8_t flags = tvb_get_guint8(tvb, offset);
+        proto_tree_add_item(data_tree, hf_network_state_available, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(data_tree, hf_frame_id_available, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(data_tree, hf_payload_available, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(data_tree, hf_network_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+        offset += 1;
+        proto_item_set_len(data_item, offset - data_item_start);
+
+        if (offset + 1 > buffer_length) {
+            expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+            return buffer_length;
+        }
+        proto_tree_add_item(data_tree, hf_network_id, tvb, offset, 1, ENC_BIG_ENDIAN);
+        offset += 1;
+        proto_item_set_len(data_item, offset - data_item_start);
+
+        bool is_can_fd = false;
         uint8_t type = flags & 0x1F;
+        switch (type)
+        {
+        case NETWORK_TYPE_CAN:
+        {
+            proto_item_append_text(data_item, ": CAN");
+        }
+        break;
+        case NETWORK_TYPE_LIN:
+        {
+            proto_item_append_text(data_item, ": LIN");
+        }
+        break;
+        case NETWORK_TYPE_FLEXRAY:
+        {
+            proto_item_append_text(data_item, ": FlexRay");
+        }
+        break;
+        default:
+            expert_add_info(pinfo, data_item, &ei_network_type_invalid);
+            break;
+        }
         uint8_t has_network_state = flags & 0x80;
         if (has_network_state)
         {
-            data_item_length += 1;
-        }
-        uint8_t has_frame_id = flags & 0x40;
-        if (has_frame_id)
-        {
-            uint8_t frame_id_length = 0;
-            switch (type)
-            {
-            case NETWORK_TYPE_CAN:
-                frame_id_length = 4;
-                break;
-            case NETWORK_TYPE_LIN:
-                frame_id_length = 1;
-                break;
-            case NETWORK_TYPE_FLEXRAY:
-                frame_id_length = 3;
-                break;
-            default:
-                break;
+            if (offset + 1 > buffer_length) {
+                expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                return buffer_length;
             }
-            data_item_length += frame_id_length;
-        }
-        uint8_t has_payload = flags & 0x20;
-        int length = 0;
-        if (has_payload)
-        {
-            length = tvb_get_guint8(tvb, offset + data_item_length);
-            data_item_length += 1; // "Payload length" field is 1 byte long
-            data_item_length += length;
-        }
-
-        if (offset + data_item_length > buffer_length) {
-            return buffer_length;
-        }
-
-        proto_item *data_item = proto_tree_add_item(busmirroring_tree, proto_busmirroring, tvb, offset, data_item_length, ENC_NA);
-        proto_item_set_text(data_item, "Data Item #%d", data_item_index);
-        proto_tree *data_tree = proto_item_add_subtree(data_item, ett_data_item);
-        proto_tree_add_item(data_tree, hf_timestamp, tvb, offset, 2, ENC_BIG_ENDIAN);
-        proto_tree_add_item(data_tree, hf_network_state_available, tvb, offset + 2, 1, ENC_BIG_ENDIAN);
-        proto_tree_add_item(data_tree, hf_frame_id_available, tvb, offset + 2, 1, ENC_BIG_ENDIAN);
-        proto_tree_add_item(data_tree, hf_payload_available, tvb, offset + 2, 1, ENC_BIG_ENDIAN);
-        proto_tree_add_item(data_tree, hf_network_type, tvb, offset + 2, 1, ENC_BIG_ENDIAN);
-        proto_tree_add_item(data_tree, hf_network_id, tvb, offset + 3, 1, ENC_BIG_ENDIAN);
-
-        offset += min_data_header_size;
-
-        if (has_network_state)
-        {
             proto_item *ns_item = proto_tree_add_item(data_item, hf_network_state, tvb, offset, 1, ENC_BIG_ENDIAN);
             proto_tree *ns_tree = proto_item_add_subtree(ns_item, ett_network_state);
             proto_tree_add_item(ns_tree, hf_frames_lost, tvb, offset, 1, ENC_BIG_ENDIAN);
@@ -199,32 +234,58 @@ dissect_busmirroring(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, vo
             default:
                 break;
             }
-            ++offset;
+            offset += 1;
+            proto_item_set_len(data_item, offset - data_item_start);
         }
+        uint8_t has_frame_id = flags & 0x40;
         if (has_frame_id)
         {
             switch (type)
             {
             case NETWORK_TYPE_CAN:
             {
+                if (offset + 4 > buffer_length) {
+                    expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                    return buffer_length;
+                }
                 proto_item *frame_id_item = proto_tree_add_item(data_item, hf_frame_id, tvb, offset, 4, ENC_BIG_ENDIAN);
                 proto_tree *frame_id_tree = proto_item_add_subtree(frame_id_item, ett_frame_id);
+                uint8_t can_id_type = tvb_get_guint8(tvb, offset) & 0x80;
+                is_can_fd = tvb_get_guint8(tvb, offset) & 0x40;
                 proto_tree_add_item(frame_id_tree, hf_can_id_type, tvb, offset, 4, ENC_BIG_ENDIAN);
                 proto_tree_add_item(frame_id_tree, hf_can_frame_type, tvb, offset, 4, ENC_BIG_ENDIAN);
-                proto_tree_add_item(frame_id_tree, hf_can_id, tvb, offset, 4, ENC_BIG_ENDIAN);
+                uint32_t can_id = 0;
+                proto_tree_add_item_ret_uint(frame_id_tree, hf_can_id, tvb, offset, 4, ENC_BIG_ENDIAN, &can_id);
+                if (can_id_type == 0 && can_id > 0x7FF) {
+                    expert_add_info(pinfo, frame_id_item, &ei_can_id_invalid);
+                }
                 offset += 4;
+                proto_item_set_len(data_item, offset - data_item_start);
             }
             break;
             case NETWORK_TYPE_LIN:
             {
+                if (offset + 1 > buffer_length) {
+                    expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                    return buffer_length;
+                }
                 proto_item *frame_id_item = proto_tree_add_item(data_item, hf_frame_id, tvb, offset, 1, ENC_BIG_ENDIAN);
                 proto_tree *frame_id_tree = proto_item_add_subtree(frame_id_item, ett_frame_id);
                 proto_tree_add_item(frame_id_tree, hf_lin_pid, tvb, offset, 1, ENC_BIG_ENDIAN);
+                uint8_t pid = tvb_get_guint8(tvb, offset);
+                if (!is_lin_pid_valid(pid)) {
+                    expert_add_info(pinfo, frame_id_item, &ei_lin_pid_invalid);
+                }
                 offset += 1;
+                proto_item_set_len(data_item, offset - data_item_start);
             }
             break;
             case NETWORK_TYPE_FLEXRAY:
             {
+                if (offset + 3 > buffer_length) {
+                    expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                    return buffer_length;
+                }
                 proto_item* frame_id_item = proto_tree_add_item(data_item, hf_frame_id, tvb, offset, 3, ENC_BIG_ENDIAN);
                 proto_tree *frame_id_tree = proto_item_add_subtree(frame_id_item, ett_frame_id);
                 proto_tree_add_item(frame_id_tree, hf_flexray_channel_b, tvb, offset, 2, ENC_BIG_ENDIAN);
@@ -234,20 +295,59 @@ dissect_busmirroring(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, vo
                 offset += 2;
                 proto_tree_add_item(frame_id_tree, hf_flexray_cycle, tvb, offset, 1, ENC_BIG_ENDIAN);
                 offset += 1;
+                proto_item_set_len(data_item, offset - data_item_start);
             }
             break;
             default:
                 break;
             }
         }
+        uint8_t has_payload = flags & 0x20;
         if (has_payload)
         {
-            proto_tree_add_item(data_item, hf_payload_length, tvb, offset, 1, ENC_BIG_ENDIAN);
-            proto_tree_add_item(data_item, hf_payload, tvb, offset + 1, length, ENC_NA);
-            offset += (length + 1);
+            if (offset + 1 > buffer_length) {
+                expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                return buffer_length;
+            }
+            uint32_t length = 0;
+            proto_item* pi = proto_tree_add_item_ret_uint(data_item, hf_payload_length, tvb, offset, 1, ENC_BIG_ENDIAN, &length);
+            switch (type)
+            {
+            case NETWORK_TYPE_CAN:
+            {
+                if (is_can_fd) {
+                    if (length > 8 && length!=12 && length!=16 && length !=20 &&
+                        length !=24 && length != 32 && length!=48 && length!=64 ) {
+                        expert_add_info(pinfo, pi, &ei_can_length_invalid);
+                    }
+                } else{
+                    if (length > 8) {
+                        expert_add_info(pinfo, pi, &ei_can_length_invalid);
+                    }
+                }
+            }
+            break;
+            case NETWORK_TYPE_LIN:
+            {
+                if (length > 8) {
+                    expert_add_info(pinfo, pi, &ei_lin_length_invalid);
+                }
+            }
+            break;
+            default:
+                break;
+            }
+            offset += 1;
+            proto_item_set_len(data_item, offset - data_item_start);
+            if (offset + length > buffer_length) {
+                expert_add_info(pinfo, data_item, &ei_data_item_incomplete);
+                return buffer_length;
+            }
+            proto_tree_add_item(data_item, hf_payload, tvb, offset, length, ENC_NA);
+            offset += length;
+            proto_item_set_len(data_item, offset - data_item_start);
         }
 
-        ++data_item_index;
     } // while
 
     return buffer_length;
@@ -481,6 +581,47 @@ void proto_register_busmirroring(void)
 
     proto_register_field_array(proto_busmirroring, hf, array_length(hf));
     proto_register_subtree_array(ett, array_length(ett));
+
+    static ei_register_info ei[] = {
+        {
+            &ei_data_incomplete,
+            { "busmirroring.data_incomplete", PI_UNDECODED, PI_WARN,
+              "Data is incomplete", EXPFILL }
+        },
+        {
+            &ei_data_item_incomplete,
+            { "busmirroring.data_item_incomplete", PI_UNDECODED, PI_WARN,
+              "Data item is incomplete", EXPFILL }
+        },
+        {
+            &ei_network_type_invalid,
+            { "busmirroring.network_type_invalid", PI_PROTOCOL, PI_WARN,
+              "Network type is invalid", EXPFILL }
+        },
+        {
+            &ei_can_id_invalid,
+            { "busmirroring.can_id_invalid", PI_PROTOCOL, PI_WARN,
+              "ID of CAN frame is invalid", EXPFILL }
+        },
+        {
+            &ei_lin_pid_invalid,
+            { "busmirroring.lin_pid_invalid", PI_PROTOCOL, PI_WARN,
+              "PID of LIN frame is invalid", EXPFILL }
+        },
+        {
+            &ei_can_length_invalid,
+            { "busmirroring.can_length_invalid", PI_PROTOCOL, PI_WARN,
+              "Length of CAN frame is invalid", EXPFILL }
+        },
+        {
+            &ei_lin_length_invalid,
+            { "busmirroring.lin_length_invalid", PI_PROTOCOL, PI_WARN,
+              "Length of LIN frame is invalid", EXPFILL }
+        }
+    };
+
+    expert_module_t* expert_busmirroring = expert_register_protocol(proto_busmirroring);
+    expert_register_field_array(expert_busmirroring, ei, array_length(ei));
 }
 
 void proto_reg_handoff_busmirroring(void)
