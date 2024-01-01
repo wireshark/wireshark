322ae0f0d86e62cedbea74dbe85fd9ca98d7f3a6
Stig Bjørlykke <stig@bjorlykke.org>
2024-01-01 12:25:09 +0100

    Happy New Year 2024

--- wsutil/version_info.c
+++ wsutil/version_info.c
@@ -573,7 +573,7 @@ const char *
 get_copyright_info(void)
 {
 	return
-		"Copyright 1998-2023 Gerald Combs <gerald@wireshark.org> and contributors.";
+		"Copyright 1998-2024 Gerald Combs <gerald@wireshark.org> and contributors.";
 }
 
 const char *
