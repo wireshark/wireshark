5822f3d10157b03780768ea9d961a6ec303eb536
Gerald Combs <gerald@wireshark.org>
2024-01-09 12:38:43 -0800

    Falco bridge: Add a null check

--- plugins/epan/falco_bridge/packet-falco-bridge.c
+++ plugins/epan/falco_bridge/packet-falco-bridge.c
@@ -551,7 +551,7 @@ dissect_falco_bridge(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *
 
     bridge_info* bi = get_bridge_info(source_id);
 
-    if (bi->source_id == 0) {
+    if (bi && bi->source_id == 0) {
         dissect_sinsp_enriched(tvb, pinfo, tree, bi);
     } else {
         proto_item *ti = proto_tree_add_item(tree, proto_falco_bridge, tvb, 0, 12, ENC_NA);
