4b940ca85229974415949702e9a559be954ebfe3
Guy Harris <gharris@sonic.net>
2024-02-11 22:33:22 -0800

    socketcan: fetch the protocol/VCID field in the right byte order.
    proto_tree_add_bitmask_list() is a bit of a pain if you want the values
    of the field, so you have to fetch it yourself.  It's little-endian, so
    tvb_get_letohl(), not tvb_get_ntohl(), is the write routine to call.
    
    
    (cherry picked from commit fdf4ecdb4aea61f6e557d75172d27ca0ffea79d7)

--- epan/dissectors/packet-socketcan.c
+++ epan/dissectors/packet-socketcan.c
@@ -669,7 +669,7 @@ dissect_socketcan_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, gu
         guint32 proto_vcid;
 
         proto_tree_add_bitmask_list(can_tree, tvb, 0, 4, canxl_prio_vcid_fields, xl_encoding);
-        proto_vcid = tvb_get_ntohl(tvb, 0);
+        proto_vcid = tvb_get_letohl(tvb, 0);
         col_add_fstr(pinfo->cinfo, COL_INFO, "Priority: %u (0x%03x), VCID: %u (0x%02X)", proto_vcid & 0x7FF, proto_vcid & 0x7FF, (proto_vcid >> 16) & 0xFF, (proto_vcid >> 16) & 0xFF);
         proto_item_append_text(can_tree, ", Priority: %u (0x%03x), VCID: %u (0x%02X)", proto_vcid & 0x7FF, proto_vcid & 0x7FF, (proto_vcid >> 16) & 0xFF, (proto_vcid >> 16) & 0xFF);
         proto_tree_add_bitmask_list(can_tree, tvb, 4, 1, canxl_flag_fields, xl_encoding);
