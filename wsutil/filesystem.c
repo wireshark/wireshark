wireshark/wireshark

commit 08ddd7e863c3ec9229accabbc43e31ac1f1d1578

wsutl: filesystem updates
Make the plugin directory logic similar to the extcap directory logic.
Make the profile directory name definition consistent with the other
directory names.

wsutil/filesystem.c
@@ -1168,50 +1168,24 @@ init_plugin_dir(void)
          * Let {WIRESHARK,LOGRAY}_PLUGIN_DIR take precedence.
          */
         plugin_dir = g_strdup(g_getenv(plugin_dir_envar));
-        return;
     }
 
 #if defined(HAVE_PLUGINS) || defined(HAVE_LUA)
 #if defined(HAVE_MSYSTEM)
-    if (running_in_build_directory_flag) {
+    else if (running_in_build_directory_flag) {
         plugin_dir = g_build_filename(install_prefix, "plugins", (char *)NULL);
     } else {
         plugin_dir = g_build_filename(install_prefix, PLUGIN_DIR, (char *)NULL);
     }
 #elif defined(_WIN32)
-    /*
-     * On Windows, the data file directory is the installation
-     * directory; the plugins are stored under it.
-     *
-     * Assume we're running the installed version of Wireshark;
-     * on Windows, the data file directory is the directory
-     * in which the Wireshark binary resides.
-     */
-    plugin_dir = g_build_filename(get_datafile_dir(), "plugins", (char *)NULL);
-
-    /*
-     * Make sure that pathname refers to a directory.
-     */
-    if (test_for_directory(plugin_dir) != EISDIR) {
+    else {
         /*
-         * Either it doesn't refer to a directory or it
-         * refers to something that doesn't exist.
-         *
-         * Assume that means we're running a version of
-         * Wireshark we've built in a build directory,
-         * in which case {datafile dir}\plugins is the
-         * top-level plugins source directory, and use
-         * that directory and set the "we're running in
-         * a build directory" flag, so the plugin
-         * scanner will check all subdirectories of that
-         * directory for plugins.
+         * On Windows, plugins are stored under the program file directory
+         * in both the build and the installation directories.
          */
-        g_free(plugin_dir);
-        plugin_dir = g_build_filename(get_datafile_dir(), "plugins", (char *)NULL);
-        running_in_build_directory_flag = true;
+        plugin_dir = g_build_filename(get_progfile_dir(), "plugins", (char *)NULL);
     }
-#else
-#ifdef ENABLE_APPLICATION_BUNDLE
+#elif defined(ENABLE_APPLICATION_BUNDLE)
     /*
      * If we're running from an app bundle and weren't started
      * with special privileges, use the Contents/PlugIns/wireshark
@@ -1225,7 +1199,7 @@ init_plugin_dir(void)
         plugin_dir = g_build_filename(appbundle_dir, "Contents/PlugIns",
                                         CONFIGURATION_NAMESPACE_LOWER, (char *)NULL);
     }
-#endif
+#else
     else if (running_in_build_directory_flag) {
         /*
          * We're (probably) being run from the build directory and

commit 32689c250f514bacf9e3a1bbb01d654229c129cb

wsutil: Fix init_plugin_dir
Fix some logic which was broken in 08ddd7e863. Make a couple of routines
static.

wsutil/filesystem.c
@@ -596,7 +596,7 @@ get_executable_path(const char *program_name)
  * g_mallocated string containing an error on failure.
  */
 #ifdef _WIN32
-char *
+static char *
 configuration_init_w32(const char* arg0 _U_)
 {
     TCHAR prog_pathname_w[_MAX_PATH+2];
@@ -681,7 +681,7 @@ configuration_init_w32(const char* arg0 _U_)
 
 #else /* !_WIN32 */
 
-char *
+static char *
 configuration_init_posix(const char* arg0)
 {
     const char *execname;
@@ -1185,7 +1185,8 @@ init_plugin_dir(void)
          */
         plugin_dir = g_build_filename(get_progfile_dir(), "plugins", (char *)NULL);
     }
-#elif defined(ENABLE_APPLICATION_BUNDLE)
+#else
+#ifdef ENABLE_APPLICATION_BUNDLE
     /*
      * If we're running from an app bundle and weren't started
      * with special privileges, use the Contents/PlugIns/wireshark
@@ -1199,7 +1200,7 @@ init_plugin_dir(void)
         plugin_dir = g_build_filename(appbundle_dir, "Contents/PlugIns",
                                         CONFIGURATION_NAMESPACE_LOWER, (char *)NULL);
     }
-#else
+#endif // ENABLE_APPLICATION_BUNDLE
     else if (running_in_build_directory_flag) {
         /*
          * We're (probably) being run from the build directory and
@@ -1211,7 +1212,7 @@ init_plugin_dir(void)
     } else {
         plugin_dir = g_build_filename(install_prefix, PLUGIN_DIR, (char *)NULL);
     }
-#endif
+#endif // HAVE_MSYSTEM / _WIN32
 #endif /* defined(HAVE_PLUGINS) || defined(HAVE_LUA) */
 }
 
@@ -1293,10 +1294,11 @@ init_extcap_dir(void)
          * in the program file directory in both the build and installation
          * directories.
          */
-        extcap_dir = g_build_filename(get_progfile_dir(),EXTCAP_DIR_NAME,
+        extcap_dir = g_build_filename(get_progfile_dir(), EXTCAP_DIR_NAME,
             CONFIGURATION_NAMESPACE_LOWER, (char *)NULL);
     }
-#elif defined (ENABLE_APPLICATION_BUNDLE)
+#else
+#ifdef ENABLE_APPLICATION_BUNDLE
     else if (appbundle_dir != NULL) {
         /*
          * If we're running from an app bundle and weren't started
@@ -1309,7 +1311,7 @@ init_extcap_dir(void)
          */
         extcap_dir = g_build_filename(appbundle_dir, "Contents/MacOS/extcap", (char *)NULL);
     }
-#else
+#endif // ENABLE_APPLICATION_BUNDLE
     else if (running_in_build_directory_flag) {
         /*
          * We're (probably) being run from the build directory and
@@ -1324,7 +1326,7 @@ init_extcap_dir(void)
         extcap_dir = g_build_filename(install_prefix,
             is_packet_configuration_namespace() ? EXTCAP_DIR : LOG_EXTCAP_DIR, (char *)NULL);
     }
-#endif
+#endif // HAVE_MSYSTEM / _WIN32
 }
 
 static void