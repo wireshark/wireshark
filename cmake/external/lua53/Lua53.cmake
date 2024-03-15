fa1a1b82e0a043ba95c4a271fffb32f665c76201
John Thacker <johnthacker@gmail.com>
2024-03-15 08:39:09 -0400

    Lua: Switch the downloaded Lua to 5.3
    This affects the MinGW builds

--- cmake/external/lua53/Lua53.cmake
+++ cmake/external/lua53/Lua53.cmake
@@ -24,9 +24,9 @@ set(LUA_LIBRARIES "${_staging_dir}/lib/liblua.a")
 set(LUA_FOUND TRUE CACHE INTERNAL "")
 set(Lua_FOUND TRUE CACHE INTERNAL "")
 
-set(_lua52_cflags "-fPIC")
+set(_lua53_cflags "-fPIC")
 if(APPLE)
-	set(_lua52_cflags "${lua52_cflags} -isysroot ${CMAKE_OSX_SYSROOT}")
+	set(_lua53_cflags "${lua53_cflags} -isysroot ${CMAKE_OSX_SYSROOT}")
 endif()
 
 #
@@ -34,13 +34,13 @@ endif()
 # external projects to a staging directory first, and the normal install target
 # does not work with MinGW.
 #
-ExternalProject_Add(lua52
-	URL               https://gitlab.com/wireshark/wireshark-development-libraries/-/raw/main/public/src/lua/lua-5.2.4.tar.gz
-	URL               https://www.lua.org/ftp/lua-5.2.4.tar.gz
-	URL_HASH          SHA256=b9e2e4aad6789b3b63a056d442f7b39f0ecfca3ae0f1fc0ae4e9614401b69f4b
+ExternalProject_Add(lua53
+	URL               https://gitlab.com/wireshark/wireshark-development-libraries/-/raw/main/public/src/lua/lua-5.3.6.tar.gz
+	URL               https://www.lua.org/ftp/lua-5.3.6.tar.gz
+	URL_HASH          SHA256=fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60
 	PATCH_COMMAND     patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/0001-Add-an-install-static-target.patch
 	CONFIGURE_COMMAND ""
-	BUILD_COMMAND     ${MAKE_EXE} MYCFLAGS=${_lua52_cflags} CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR}\ rcu RANLIB=${CMAKE_RANLIB} ${_target}
+	BUILD_COMMAND     ${MAKE_EXE} MYCFLAGS=${_lua53_cflags} CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR}\ rcu RANLIB=${CMAKE_RANLIB} ${_target}
 	BUILD_IN_SOURCE   True
 	BUILD_BYPRODUCTS  ${LUA_LIBRARIES}
 	INSTALL_COMMAND   ${MAKE_EXE} INSTALL_TOP=${_staging_dir} install-static
