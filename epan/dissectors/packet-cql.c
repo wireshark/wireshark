291822a25cfaf54a3556c19d5d47af7efb1bdf48
Yaniv Kaul <yaniv.kaul@scylladb.com>
2024-04-21 15:50:58 +0300

    CQL: take into account NULL and unset values
    Confusingly, their length is -1 or -2...
    This now allows to properly parse rows without metadata.
    
    Signed-off-by: Yaniv Kaul <mykaul@gmail.com>

--- epan/dissectors/packet-cql.c
+++ epan/dissectors/packet-cql.c
@@ -1576,13 +1576,17 @@ dissect_cql_tcp_pdu(tvbuff_t* raw_tvb, packet_info* pinfo, proto_tree* tree, voi
 								} else {
 									for (k = 0; k < result_rows_columns_count; ++k) {
 										proto_tree_add_item_ret_int(columns_subtree, hf_cql_bytes_length, tvb, offset, 4, ENC_BIG_ENDIAN, &bytes_length);
+										offset += 4;
+										single_column_subtree = proto_tree_add_subtree(columns_subtree, tvb, offset, bytes_length > 0 ? bytes_length : 0, ett_cql_results_no_metadata, &ti, "Column data");
 										if (bytes_length > 0) {
-											offset += 4;
-											single_column_subtree = proto_tree_add_subtree(columns_subtree, tvb, offset, bytes_length, ett_cql_results_no_metadata, &ti, "Column data");
 											proto_item_append_text(single_column_subtree, " for column # %" PRId64, k + 1);
 											proto_tree_add_item(single_column_subtree, hf_cql_bytes, tvb, offset, bytes_length, ENC_NA);
 											offset += bytes_length;
-										} else if (bytes_length <= 0) {
+										} else if (bytes_length == -1) {
+											proto_item_append_text(single_column_subtree, " is NULL for column # %" PRId64, k + 1);
+										} else if (bytes_length == -2) {
+											proto_item_append_text(single_column_subtree, " is not set for column # %" PRId64, k + 1);
+										} else {
 											expert_add_info(pinfo, ti, &ei_cql_unexpected_negative_value);
 											return tvb_reported_length(tvb);
 										}
