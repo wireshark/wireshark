6f8ca8498b6222a527da7e995da3ec317debb11d
Alexis La Goutte <alexis.lagoutte@gmail.com>
2024-04-23 07:13:35 +0200

    fix typo after gcf19f6f27

--- epan/dissectors/packet-dtls.c
+++ epan/dissectors/packet-dtls.c
@@ -6,7 +6,7 @@
  *
  * Wireshark - Network traffic analyzer
  * By Gerald Combs <gerald@wireshark.org>
- * Copyright 1998 Gerald @
+ * Copyright 1998 Gerald Combs
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
  *
