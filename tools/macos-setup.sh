490d821054d9a506dcd833693d38c512156f434b
Gerald Combs <gerald@wireshark.org>
2024-06-10 12:19:17 -0700

    macos-setup: Fix some shellcheck warnings
    [skip ci]

--- tools/macos-setup.sh
+++ tools/macos-setup.sh
@@ -249,35 +249,35 @@ AUTOMAKE_VERSION=1.16.5
 LIBTOOL_VERSION=2.4.6
 
 install_curl() {
-    if [ "$CURL_VERSION" -a ! -f curl-$CURL_VERSION-done ] ; then
+    if [ "$CURL_VERSION" ] && [ ! -f "curl-$CURL_VERSION-done" ] ; then
         echo "Downloading, building, and installing curl:"
-        [ -f curl-$CURL_VERSION.tar.bz2 ] || curl --fail --location --remote-name https://curl.haxx.se/download/curl-$CURL_VERSION.tar.bz2
+        [ -f "curl-$CURL_VERSION.tar.bz2" ] || curl --fail --location --remote-name "https://curl.haxx.se/download/curl-$CURL_VERSION.tar.bz2"
         $no_build && echo "Skipping installation" && return
-        bzcat curl-$CURL_VERSION.tar.bz2 | tar xf -
-        cd curl-$CURL_VERSION
+        bzcat "curl-$CURL_VERSION.tar.bz2" | tar xf -
+        cd "curl-$CURL_VERSION"
         ./configure "${CONFIGURE_OPTS[@]}"
         make "${MAKE_BUILD_OPTS[@]}"
         $DO_MAKE_INSTALL
         cd ..
-        touch curl-$CURL_VERSION-done
+        touch "curl-$CURL_VERSION-done"
     fi
 }
 
 uninstall_curl() {
     if [ -n "$installed_curl_version" ] ; then
         echo "Uninstalling curl:"
-        cd curl-$installed_curl_version
+        cd "curl-$installed_curl_version"
         $DO_MAKE_UNINSTALL
         make distclean
         cd ..
-        rm curl-$installed_curl_version-done
+        rm "curl-$installed_curl_version-done"
 
-        if [ "$#" -eq 1 -a "$1" = "-r" ] ; then
+        if [ "$#" -eq 1 ] && [ "$1" = "-r" ] ; then
             #
             # Get rid of the previously downloaded and unpacked version.
             #
-            rm -rf curl-$installed_curl_version
-            rm -rf curl-$installed_curl_version.tar.bz2
+            rm -rf "curl-$installed_curl_version"
+            rm -rf "curl-$installed_curl_version.tar.bz2"
         fi
 
         installed_curl_version=""
@@ -285,7 +285,7 @@ uninstall_curl() {
 }
 
 install_xz() {
-    if [ "$XZ_VERSION" -a ! -f xz-$XZ_VERSION-done ] ; then
+    if [ "$XZ_VERSION" ] && [ ! -f xz-$XZ_VERSION-done ] ; then
         echo "Downloading, building, and installing xz:"
         [ -f xz-$XZ_VERSION.tar.bz2 ] || curl "${CURL_REMOTE_NAME_OPTS[@]}" https://tukaani.org/xz/xz-$XZ_VERSION.tar.bz2
         $no_build && echo "Skipping installation" && return
@@ -308,18 +308,18 @@ install_xz() {
 uninstall_xz() {
     if [ -n "$installed_xz_version" ] ; then
         echo "Uninstalling xz:"
-        cd xz-$installed_xz_version
+        cd "xz-$installed_xz_version"
         $DO_MAKE_UNINSTALL
         make distclean
         cd ..
-        rm xz-$installed_xz_version-done
+        rm "xz-$installed_xz_version-done"
 
-        if [ "$#" -eq 1 -a "$1" = "-r" ] ; then
+        if [ "$#" -eq 1 ] && [ "$1" = "-r" ] ; then
             #
             # Get rid of the previously downloaded and unpacked version.
             #
-            rm -rf xz-$installed_xz_version
-            rm -rf xz-$installed_xz_version.tar.bz2
+            rm -rf "xz-$installed_xz_version"
+            rm -rf "xz-$installed_xz_version.tar.bz2"
         fi
 
         installed_xz_version=""
@@ -329,18 +329,18 @@ uninstall_xz() {
 uninstall_lzip() {
     if [ -n "$installed_lzip_version" ] ; then
         echo "Uninstalling lzip:"
-        cd lzip-$installed_lzip_version
+        cd "lzip-$installed_lzip_version"
         $DO_MAKE_UNINSTALL
         make distclean
         cd ..
-        rm lzip-$installed_lzip_version-done
+        rm "lzip-$installed_lzip_version-done"
 
-        if [ "$#" -eq 1 -a "$1" = "-r" ] ; then
+        if [ "$#" -eq 1 ] && [ "$1" = "-r" ] ; then
             #
             # Get rid of the previously downloaded and unpacked version.
             #
-            rm -rf lzip-$installed_lzip_version
-            rm -rf lzip-$installed_lzip_version.tar.gz
+            rm -rf "lzip-$installed_lzip_version"
+            rm -rf "lzip-$installed_lzip_version.tar.gz"
         fi
 
         installed_lzip_version=""
@@ -350,18 +350,18 @@ uninstall_lzip() {
 uninstall_pcre() {
     if [ -n "$installed_pcre_version" ] ; then
         echo "Uninstalling leftover pcre:"
-        cd pcre-$installed_pcre_version
+        cd "pcre-$installed_pcre_version"
         $DO_MAKE_UNINSTALL
         make distclean
         cd ..
-        rm pcre-$installed_pcre_version-done
+        rm "pcre-$installed_pcre_version-done"
 
-        if [ "$#" -eq 1 -a "$1" = "-r" ] ; then
+        if [ "$#" -eq 1 ] && [ "$1" = "-r" ] ; then
             #
             # Get rid of the previously downloaded and unpacked version.
             #
-            rm -rf pcre-$installed_pcre_version
-            rm -rf pcre-$installed_pcre_version.tar.bz2
+            rm -rf "pcre-$installed_pcre_version"
+            rm -rf "pcre-$installed_pcre_version.tar.bz2"
         fi
 
         installed_pcre_version=""
@@ -369,7 +369,7 @@ uninstall_pcre() {
 }
 
 install_pcre2() {
-    if [ "$PCRE2_VERSION" -a ! -f "pcre2-$PCRE2_VERSION-done" ] ; then
+    if [ "$PCRE2_VERSION" ] && [ ! -f "pcre2-$PCRE2_VERSION-done" ] ; then
         echo "Downloading, building, and installing pcre2:"
         [ -f "pcre2-$PCRE2_VERSION.tar.bz2" ] || curl "${CURL_REMOTE_NAME_OPTS[@]}" "https://github.com/PhilipHazel/pcre2/releases/download/pcre2-$PCRE2_VERSION/pcre2-10.39.tar.bz2"
         $no_build && echo "Skipping installation" && return
@@ -394,7 +394,7 @@ uninstall_pcre2() {
         while read -r ; do $DO_RM -v "$REPLY" ; done < <(cat "pcre2-$installed_pcre2_version/build_dir/install_manifest.txt"; echo)
         rm "pcre2-$installed_pcre2_version-done"
 
-        if [ "$#" -eq 1 -a "$1" = "-r" ] ; then
+        if [ "$#" -eq 1 ] && [ "$1" = "-r" ] ; then
             #
             # Get rid of the previously downloaded and unpacked version.
             #
@@ -851,7 +851,7 @@ install_glib() {
         #
         # Starting with GLib 2.28.8, xz-compressed tarballs are available.
         #
-        [ -f glib-$GLIB_VERSION.tar.xz ] || curl "${CURL_REMOTE_NAME_OPTS[@]}" https://download.gnome.org/sources/glib/$glib_dir/glib-$GLIB_VERSION.tar.xz
+        [ -f glib-$GLIB_VERSION.tar.xz ] || curl "${CURL_REMOTE_NAME_OPTS[@]}" "https://download.gnome.org/sources/glib/$glib_dir/glib-$GLIB_VERSION.tar.xz"
         $no_build && echo "Skipping installation" && return
         xzcat glib-$GLIB_VERSION.tar.xz | tar xf -
         cd glib-$GLIB_VERSION
@@ -998,7 +998,7 @@ EOF
                 #
                 #    https://bugzilla.gnome.org/show_bug.cgi?id=691608#c25
                 #
-                if grep -qs '#define.*MACOSX' $includedir/ffi/fficonfig.h
+                if grep -qs '#define.*MACOSX' "$includedir/ffi/fficonfig.h"
                 then
                     # It's defined, nothing to do
                     CFLAGS="$CFLAGS -Wno-format-nonliteral $VERSION_MIN_FLAGS $SDKFLAGS" CXXFLAGS="$CXXFLAGS -Wno-format-nonliteral $VERSION_MIN_FLAGS $SDKFLAGS" LDFLAGS="$LDFLAGS $VERSION_MIN_FLAGS $SDKFLAGS" \
@@ -3585,7 +3585,7 @@ uninstall_all() {
 # code will attempt to get you there, but is not perfect (particularly
 # if someone copies the script).
 
-topdir="$( pwd )/$( dirname $0 )/.."
+topdir="$( pwd )/$( dirname "$0" )/.."
 cd "$topdir"
 
 # Preference of the support libraries directory:
