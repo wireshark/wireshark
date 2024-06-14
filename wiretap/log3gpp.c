6ead3e26572ebc5be814206f4a5a833866eb4f32
Darius Davis <darius-wireshark@free-range.com.au>
2024-06-14 21:32:36 +1000

    log3gpp: Fix read of uninitialized data.
    Clang static analysis reports that subsecond_decimals_buf[1] through [3] might
    be used uninitialized.  This is because the array is being NUL-terminated, but
    the NUL-termination is not relevant (and is in fact erroneous) when the array
    is being directly evaluated one character at a time and assumed to be only
    digits.
    
    The NUL-termination can be removed (and the array size reduced by one) and
    instead we directly initialize to '0' any characters not read from the source
    line.

--- wiretap/log3gpp.c
+++ wiretap/log3gpp.c
@@ -502,7 +502,7 @@ bool parse_line(char* linebuff, int line_length, int *seconds, int *useconds,
     int  prot_option_chars = 0;
     char seconds_buff[MAX_SECONDS_CHARS+1];
     int  seconds_chars;
-    char subsecond_decimals_buff[MAX_SUBSECOND_DECIMALS+1];
+    char subsecond_decimals_buff[MAX_SUBSECOND_DECIMALS];
     int  subsecond_decimals_chars;
 
     /*********************************************************************/
@@ -573,7 +573,9 @@ bool parse_line(char* linebuff, int line_length, int *seconds, int *useconds,
     }
 
     /* Convert found value into microseconds */
-    subsecond_decimals_buff[subsecond_decimals_chars] = '\0';
+    while (subsecond_decimals_chars < MAX_SUBSECOND_DECIMALS) {
+        subsecond_decimals_buff[subsecond_decimals_chars++] = '0';
+    }
     /* Already know they are digits, so avoid expense of ws_strtoi32() */
     *useconds = ((subsecond_decimals_buff[0] - '0') * 100000) +
                 ((subsecond_decimals_buff[1] - '0') * 10000) +
