wireshark/wireshark

commit fe98af0e300c83d7b87391628e3c1a4176ab6c4a

prefs: Get rid of line continuations for gui.debounce.timer description
Due to the use of line continuations here, the preference description written
into the wireshark preferences file ends up with excessive whitespace.

Before:
```
% tshark -G defaultprefs | grep -B2 debounce
# How long to wait (in milliseconds) before processing                                   computationally intensive user input.                                   If you type quickly, consider lowering the value for a 'snappier'                                   experience.                                   If you type slowly, consider increasing the value to avoid performance issues.                                   This is currently used to delay searches in View -> Internals -> Supported Protocols                                   and Preferences -> Advanced menu.
# A decimal number
#gui.debounce.timer: 400
%
```

After:
```
% ./run/tshark -G defaultprefs | grep -B2 debounce
# How long to wait (in milliseconds) before processing computationally intensive user input. If you type quickly, consider lowering the value for a 'snappier' experience. If you type slowly, consider increasing the value to avoid performance issues.This is currently used to delay searches in View -> Internals -> Supported Protocols and Preferences -> Advanced menu.
# A decimal number
#gui.debounce.timer: 400
%
```

Fixes: 9e1905f88d ("Preferences: Support configuring debounce timers")

epan/prefs.c
@@ -3504,13 +3504,13 @@ prefs_register_modules(void)
 
     prefs_register_uint_preference(gui_module, "debounce.timer",
                                    "How long to wait before processing computationally intensive user input",
-                                   "How long to wait (in milliseconds) before processing\
-                                   computationally intensive user input.\
-                                   If you type quickly, consider lowering the value for a 'snappier'\
-                                   experience.\
-                                   If you type slowly, consider increasing the value to avoid performance issues.\
-                                   This is currently used to delay searches in View -> Internals -> Supported Protocols\
-                                   and Preferences -> Advanced menu.",
+                                   "How long to wait (in milliseconds) before processing "
+                                   "computationally intensive user input. "
+                                   "If you type quickly, consider lowering the value for a 'snappier' "
+                                   "experience. "
+                                   "If you type slowly, consider increasing the value to avoid performance issues."
+                                   "This is currently used to delay searches in View -> Internals -> Supported Protocols "
+                                   "and Preferences -> Advanced menu.",
                                    10,
                                    &prefs.gui_debounce_timer);
 

commit 2f19d0db5c22635171f07c4bd1858ae5ba9e9609

prefs: Add one space back to gui.debounce_timer description
The previous fix accidentally missed adding one trailing space in
a line.

Fixes: fe98af0e30 ("prefs: Get rid of line continuations for gui.debounce.timer description")

epan/prefs.c
@@ -3508,7 +3508,7 @@ prefs_register_modules(void)
                                    "computationally intensive user input. "
                                    "If you type quickly, consider lowering the value for a 'snappier' "
                                    "experience. "
-                                   "If you type slowly, consider increasing the value to avoid performance issues."
+                                   "If you type slowly, consider increasing the value to avoid performance issues. "
                                    "This is currently used to delay searches in View -> Internals -> Supported Protocols "
                                    "and Preferences -> Advanced menu.",
                                    10,