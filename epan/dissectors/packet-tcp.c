3a3efb5a3d336b513a4e39eb74b8823024b34a39
Eugène Adell <eugene.adell@gmail.com>
2024-07-15 07:17:06 +0000

    TCP: Fix SACK interpretation when deinterlacing and Relative SEQ

--- epan/dissectors/packet-tcp.c
+++ epan/dissectors/packet-tcp.c
@@ -5635,8 +5635,9 @@ dissect_tcpopt_wscale(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void*
     int offset = 0;
     struct tcp_analysis *tcpd;
 
+    /* find the conversation for this TCP session and its stored data */
     conversation_t *stratconv = find_conversation_strat(pinfo, CONVERSATION_TCP, 0);
-    tcpd=get_tcp_conversation_data(stratconv,pinfo);
+    tcpd=get_tcp_conversation_data_idempotent(stratconv);
 
     wscale_pi = proto_tree_add_item(tree, proto_tcp_option_wscale, tvb, offset, -1, ENC_NA);
     wscale_tree = proto_item_add_subtree(wscale_pi, ett_tcp_option_wscale);
@@ -5694,8 +5695,9 @@ dissect_tcpopt_sack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
      * and SACK handling for the in-flight update
      */
     if(tcp_analyze_seq) {
-        /* find(or create if needed) the conversation for this tcp session */
-        tcpd=get_tcp_conversation_data(NULL,pinfo);
+        /* find the conversation for this TCP session and its stored data */
+        conversation_t *stratconv = find_conversation_strat(pinfo, CONVERSATION_TCP, 0);
+        tcpd=get_tcp_conversation_data_idempotent(stratconv);
 
         if (tcpd) {
             if (tcp_relative_seq) {
@@ -6649,7 +6651,8 @@ dissect_tcpopt_scps(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
     uint8_t     connid;
     int         offset = 0, optlen = tvb_reported_length(tvb);
 
-    tcpd = get_tcp_conversation_data(NULL,pinfo);
+    conversation_t *stratconv = find_conversation_strat(pinfo, CONVERSATION_TCP, 0);
+    tcpd=get_tcp_conversation_data_idempotent(stratconv);
 
     /* check direction and get ua lists */
     direction=cmp_address(&pinfo->src, &pinfo->dst);
@@ -6878,7 +6881,8 @@ dissect_tcpopt_snack(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* 
     if (!tcp_option_len_check(length_item, pinfo, tvb_reported_length(tvb), TCPOLEN_SNACK))
         return tvb_captured_length(tvb);
 
-    tcpd = get_tcp_conversation_data(NULL,pinfo);
+    conversation_t *stratconv = find_conversation_strat(pinfo, CONVERSATION_TCP, 0);
+    tcpd=get_tcp_conversation_data_idempotent(stratconv);
 
     /* The SNACK option reports missing data with a granularity of segments. */
     proto_tree_add_item_ret_uint(field_tree, hf_tcp_option_snack_offset,
