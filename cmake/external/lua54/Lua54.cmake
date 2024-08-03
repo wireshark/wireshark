e247c146c8fe07062d062b212ad7347552094c7a
John Thacker <johnthacker@gmail.com>
2024-08-03 09:36:07 -0400

    Lua: Switch the downloaded Lua to 5.4
    This primarily affects cross-compilation (e.g. MinGW)

--- cmake/external/lua54/Lua54.cmake
+++ cmake/external/lua54/Lua54.cmake
@@ -26,9 +26,9 @@ set(Lua_FOUND TRUE CACHE INTERNAL "")
 # Fresh Lua 5.3+ install always has 64-bit lua_Integers (unlike Lua 5.2)
 set(LUA_INTEGER_SIZE 8 CACHE INTERNAL "Size of lua_Integer")
 
-set(_lua53_cflags "-fPIC")
+set(_lua54_cflags "-fPIC")
 if(APPLE)
-	set(_lua53_cflags "${lua53_cflags} -isysroot ${CMAKE_OSX_SYSROOT}")
+	set(_lua54_cflags "${lua54_cflags} -isysroot ${CMAKE_OSX_SYSROOT}")
 endif()
 
 #
@@ -36,13 +36,13 @@ endif()
 # external projects to a staging directory first, and the normal install target
 # does not work with MinGW.
 #
-ExternalProject_Add(lua53
-	URL               https://gitlab.com/wireshark/wireshark-development-libraries/-/raw/main/public/src/lua/lua-5.3.6.tar.gz
-	URL               https://www.lua.org/ftp/lua-5.3.6.tar.gz
-	URL_HASH          SHA256=fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60
+ExternalProject_Add(lua54
+	URL               https://gitlab.com/wireshark/wireshark-development-libraries/-/raw/main/public/src/lua/lua-5.4.6.tar.gz
+	URL               https://www.lua.org/ftp/lua-5.4.6.tar.gz
+	URL_HASH          SHA256=7d5ea1b9cb6aa0b59ca3dde1c6adcb57ef83a1ba8e5432c0ecd06bf439b3ad88
 	PATCH_COMMAND     patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/0001-Add-an-install-static-target.patch
 	CONFIGURE_COMMAND ""
-	BUILD_COMMAND     ${MAKE_EXE} MYCFLAGS=${_lua53_cflags} CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR}\ rcu RANLIB=${CMAKE_RANLIB} ${_target}
+	BUILD_COMMAND     ${MAKE_EXE} MYCFLAGS=${_lua54_cflags} CC=${CMAKE_C_COMPILER} AR=${CMAKE_AR}\ rcu RANLIB=${CMAKE_RANLIB} ${_target}
 	BUILD_IN_SOURCE   True
 	BUILD_BYPRODUCTS  ${LUA_LIBRARIES}
 	INSTALL_COMMAND   ${MAKE_EXE} INSTALL_TOP=${_staging_dir} install-static
