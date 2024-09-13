wireshark/wireshark

commit cdc8e2f5119a20e905b60d2afab66114a86b45c9

dfilter: Change default boolean representation to True/False

epan/ftypes/ftype-integer.c
@@ -1159,8 +1159,8 @@ static char *
 boolean_to_repr(wmem_allocator_t *scope, const fvalue_t *fv, ftrepr_t rtype _U_, int field_display _U_)
 {
 	if (fv->value.uinteger64)
-		return wmem_strdup(scope, "1");
-	return wmem_strdup(scope, "0");
+		return wmem_strdup(scope, "True");
+	return wmem_strdup(scope, "False");
 }
 
 /* False is less than True (arbitrary):

test/suite_decryption.py
@@ -825,10 +825,10 @@ class TestDecryptWireguard:
             '-e', 'wg.receiver_pubkey.known_privkey',
         ])
         assert len(lines) == 4
-        assert '1\t%s\t0' % self.key_Spub_r in lines
-        assert '2\t%s\t0' % self.key_Spub_i in lines
-        assert '13\t%s\t0' % self.key_Spub_r in lines
-        assert '14\t%s\t0' % self.key_Spub_i in lines
+        assert '1\t%s\tFalse' % self.key_Spub_r in lines
+        assert '2\t%s\tFalse' % self.key_Spub_i in lines
+        assert '13\t%s\tFalse' % self.key_Spub_r in lines
+        assert '14\t%s\tFalse' % self.key_Spub_i in lines
 
     def test_mac1_private(self, run_wireguard_test):
         """Check that MAC1 identification using private keys work."""
@@ -842,10 +842,10 @@ class TestDecryptWireguard:
             '-e', 'wg.receiver_pubkey.known_privkey',
         ])
         assert len(lines) == 4
-        assert '1\t%s\t1' % self.key_Spub_r in lines
-        assert '2\t%s\t1' % self.key_Spub_i in lines
-        assert '13\t%s\t1' % self.key_Spub_r in lines
-        assert '14\t%s\t1' % self.key_Spub_i in lines
+        assert '1\t%s\tTrue' % self.key_Spub_r in lines
+        assert '2\t%s\tTrue' % self.key_Spub_i in lines
+        assert '13\t%s\tTrue' % self.key_Spub_r in lines
+        assert '14\t%s\tTrue' % self.key_Spub_i in lines
 
     def test_decrypt_initiation_sprivr(self, run_wireguard_test):
         """Check for partial decryption using Spriv_r."""
@@ -860,8 +860,8 @@ class TestDecryptWireguard:
             '-e', 'wg.timestamp.nanoseconds',
         ])
         # static pubkey is unknown because Spub_i is not added to wg_keys.
-        assert '1\t%s\t0\t0\t%s' % (self.key_Spub_i, '356537872') in lines
-        assert '13\t%s\t0\t0\t%s' % (self.key_Spub_i, '490514356') in lines
+        assert '1\t%s\tFalse\tFalse\t%s' % (self.key_Spub_i, '356537872') in lines
+        assert '13\t%s\tFalse\tFalse\t%s' % (self.key_Spub_i, '490514356') in lines
 
     def test_decrypt_initiation_ephemeral_only(self, run_wireguard_test):
         """Check for partial decryption using Epriv_i."""
@@ -879,8 +879,8 @@ class TestDecryptWireguard:
         ])
         # The current implementation tries to write as much decrypted data as
         # possible, even if the full handshake cannot be derived.
-        assert '1\t1\t%s\t%s' % (self.key_Spub_i, '') in lines
-        assert '13\t1\t%s\t%s' % (self.key_Spub_i, '') in lines
+        assert '1\tTrue\t%s\t%s' % (self.key_Spub_i, '') in lines
+        assert '13\tTrue\t%s\t%s' % (self.key_Spub_i, '') in lines
 
     def test_decrypt_full_initiator(self, run_wireguard_test):
         """
@@ -902,12 +902,12 @@ class TestDecryptWireguard:
             '  LOCAL_EPHEMERAL_PRIVATE_KEY = %s' % self.key_Epriv_i0_alt,
             '  LOCAL_EPHEMERAL_PRIVATE_KEY = %s' % self.key_Epriv_i1,
         ])
-        assert '1\t1\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
-        assert '2\t0\t\t\t1\t\t' in lines
+        assert '1\tTrue\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
+        assert '2\tFalse\t\t\tTrue\t\t' in lines
         assert '3\t\t\t\t\t8\t' in lines
         assert '4\t\t\t\t\t0\t' in lines
-        assert '13\t1\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
-        assert '14\t0\t\t\t1\t\t' in lines
+        assert '13\tTrue\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
+        assert '14\tFalse\t\t\tTrue\t\t' in lines
         assert '17\t\t\t\t\t\t443' in lines
         assert '18\t\t\t\t\t\t49472' in lines
 
@@ -927,12 +927,12 @@ class TestDecryptWireguard:
             '-e', 'icmp.type',
             '-e', 'tcp.dstport',
         ], pcap_file='wireguard-ping-tcp-dsb.pcapng')
-        assert '1\t1\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
-        assert '2\t0\t\t\t1\t\t' in lines
+        assert '1\tTrue\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
+        assert '2\tFalse\t\t\tTrue\t\t' in lines
         assert '3\t\t\t\t\t8\t' in lines
         assert '4\t\t\t\t\t0\t' in lines
-        assert '13\t1\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
-        assert '14\t0\t\t\t1\t\t' in lines
+        assert '13\tTrue\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
+        assert '14\tFalse\t\t\tTrue\t\t' in lines
         assert '17\t\t\t\t\t\t443' in lines
         assert '18\t\t\t\t\t\t49472' in lines
 
@@ -953,12 +953,12 @@ class TestDecryptWireguard:
             'LOCAL_EPHEMERAL_PRIVATE_KEY=%s' % self.key_Epriv_r0,
             'LOCAL_EPHEMERAL_PRIVATE_KEY=%s' % self.key_Epriv_r1,
         ])
-        assert '1\t0\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
-        assert '2\t1\t\t\t1\t\t' in lines
+        assert '1\tFalse\t%s\t%s\t\t\t' % (self.key_Spub_i, '356537872') in lines
+        assert '2\tTrue\t\t\tTrue\t\t' in lines
         assert '3\t\t\t\t\t8\t' in lines
         assert '4\t\t\t\t\t0\t' in lines
-        assert '13\t0\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
-        assert '14\t1\t\t\t1\t\t' in lines
+        assert '13\tFalse\t%s\t%s\t\t\t' % (self.key_Spub_i, '490514356') in lines
+        assert '14\tTrue\t\t\tTrue\t\t' in lines
         assert '17\t\t\t\t\t\t443' in lines
         assert '18\t\t\t\t\t\t49472' in lines
 
@@ -976,8 +976,8 @@ class TestDecryptWireguard:
             'LOCAL_EPHEMERAL_PRIVATE_KEY=%s' % self.key_Epriv_r3,
             'PRESHARED_KEY=%s' % self.key_psk3,
         ], pcap_file='wireguard-psk.pcap')
-        assert '2\t1' in lines
-        assert '4\t1' in lines
+        assert '2\tTrue' in lines
+        assert '4\tTrue' in lines
 
     def test_decrypt_psk_responder(self, run_wireguard_test):
         """Check whether PSKs enable decryption for responder keys."""
@@ -1001,8 +1001,8 @@ class TestDecryptWireguard:
             'LOCAL_EPHEMERAL_PRIVATE_KEY=%s' % self.key_Epriv_i3,
             'PRESHARED_KEY=%s' % self.key_dummy,
         ], pcap_file='wireguard-psk.pcap')
-        assert '2\t1' in lines
-        assert '4\t1' in lines
+        assert '2\tTrue' in lines
+        assert '4\tTrue' in lines
 
     def test_decrypt_psk_wrong_orderl(self, run_wireguard_test):
         """Check that the wrong order of lines indeed fail decryption."""
@@ -1018,8 +1018,8 @@ class TestDecryptWireguard:
             'PRESHARED_KEY=%s' % self.key_psk2, # note: swapped with previous line
             'PRESHARED_KEY=%s' % self.key_psk3,
         ], pcap_file='wireguard-psk.pcap')
-        assert '2\t0' in lines
-        assert '4\t0' in lines
+        assert '2\tFalse' in lines
+        assert '4\tFalse' in lines
 
 
 class TestDecryptKnxip:

commit 24593b792ea888656361644892dbd3ff3b57e48c

proto: Fix unresolved boolean column
A boolean value used in a custom column can be represented as both
resolved and unresolved. The unresolved variant should show 0 or 1.

This issue was first introduced in commit cdc8e2f511.

epan/ftypes/ftype-integer.c
@@ -851,6 +851,7 @@ boolean_to_repr(wmem_allocator_t *scope, const fvalue_t *fv, ftrepr_t rtype, int
 			str = val ? "True" : "False";
 			break;
 		case FTREPR_JSON:
+		case FTREPR_RAW:
 			str = val ? "1" : "0";
 			break;
 	}

epan/ftypes/ftype-string.c
@@ -49,7 +49,7 @@ string_fvalue_set_strbuf(fvalue_t *fv, wmem_strbuf_t *value)
 static char *
 string_to_repr(wmem_allocator_t *scope, const fvalue_t *fv, ftrepr_t rtype _U_, int field_display _U_)
 {
-	if (rtype == FTREPR_DISPLAY || rtype == FTREPR_JSON) {
+	if (rtype == FTREPR_DISPLAY || rtype == FTREPR_JSON || rtype == FTREPR_RAW) {
 		/* XXX: This escapes NUL with "\0", but JSON (neither RFC 8259 nor
 		 * ECMA-404) does not allow that, it must be "\u0000".
 		 */

epan/ftypes/ftype-time.c
@@ -460,6 +460,7 @@ absolute_val_to_repr(wmem_allocator_t *scope, const fvalue_t *fv, ftrepr_t rtype
 	switch (rtype) {
 		case FTREPR_DISPLAY:
 		case FTREPR_JSON:
+		case FTREPR_RAW:
 			break;
 
 		case FTREPR_DFILTER:

epan/ftypes/ftypes.h
@@ -174,6 +174,7 @@ enum ftrepr {
 	FTREPR_DISPLAY,
 	FTREPR_DFILTER,
 	FTREPR_JSON,
+	FTREPR_RAW,
 };
 
 typedef enum ftrepr ftrepr_t;

epan/proto.c
@@ -7411,7 +7411,7 @@ proto_custom_set(proto_tree* tree, GSList *field_ids, int occurrence,
 						expr[--offset_e] = '\0'; /* Remove the added trailing ',' again */
 					}
 				} else {
-					str = fvalue_to_string_repr(NULL, finfo->value, FTREPR_DISPLAY, finfo->hfinfo->display);
+					str = fvalue_to_string_repr(NULL, finfo->value, FTREPR_RAW, finfo->hfinfo->display);
 					offset_e += protoo_strlcpy(expr+offset_e, str, size-offset_e);
 					wmem_free(NULL, str);
 				}