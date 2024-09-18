b0802f3665938ce478e8a6a32adf741d447851a5
Chuck Craft <bubbasnmp@gmail.com>
2024-09-18 21:19:42 +0000

    Qt: if copying null field let user know
    (cherry picked from commit f61931a1649a461b612f20a16e1c8cb998fff0f0)
    
    Co-authored-by: Chuck Craft <bubbasnmp@gmail.com>

--- ui/logray/logray_main_window_slots.cpp
+++ ui/logray/logray_main_window_slots.cpp
@@ -1968,7 +1968,7 @@ void LograyMainWindow::copySelectedItems(LograyMainWindow::CopySelected selectio
     case CopySelectedValue:
         if (finfo_selected && capture_file_.capFile()->edt != 0) {
             char* field_str = get_node_field_value(finfo_selected, capture_file_.capFile()->edt);
-            clip.append(field_str);
+            clip.append(field_str[0] ? field_str : "(null)");
             g_free(field_str);
         }
         break;
