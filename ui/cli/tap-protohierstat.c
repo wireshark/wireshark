c665decba3331b20d6e08f1a50829da255c31193
John Thacker <johnthacker@gmail.com>
2024-09-21 20:55:23 -0400

    phs cli tap: Parity with the GUI
    Skip the internal "protocol" used for the columns, in case this tap
    is attached to a dissection also showing the columns. (The GUI Protocol
    Hierarchy window does its own dissection, and never has the columns.)
    
    Also skip non-protocols, e.g. top-level desegmentation items, as the
    GUI does.
    
    Free the tap data after we're done.

--- ui/cli/tap-protohierstat.c
+++ ui/cli/tap-protohierstat.c
@@ -23,7 +23,8 @@
 #include <wsutil/cmdarg_err.h>
 #include "tap-protohierstat.h"
 
-int pc_proto_id = -1;
+int pc_proto_id;
+int col_proto_id;
 
 void register_tap_listener_protohierstat(void);
 
@@ -95,8 +96,22 @@ protohierstat_packet(void *prs, packet_info *pinfo, epan_dissect_t *edt, const v
 		 * This keeps us from having a top-level "pkt_comment"
 		 * entry that represents a nonexistent protocol,
 		 * and matches how the GUI treats comments.
+		 * Also skip the internal non-protocol for the columns
+		 * (the GUI does its own dissection with no columns instead
+		 * of adding a tap like other dialogs, so it never has the
+		 * column protocol.)
 		 */
-		if (G_UNLIKELY(fi->hfinfo->id == pc_proto_id)) {
+		if (G_UNLIKELY(fi->hfinfo->id == pc_proto_id || fi->hfinfo->id == col_proto_id)) {
+			continue;
+		}
+
+		/* Skip non protocols, e.g. top-level desegmentation items
+		 * "[Reassembled TCP Segments]", as the GUI does.
+		 *
+		 * Note the inverse issue (handling protocols that are not at
+		 * the top-level) is something done neither here nor in the GUI.
+		 */
+		if (!proto_registrar_is_protocol(fi->hfinfo->id)) {
 			continue;
 		}
 
@@ -200,10 +215,14 @@ protohierstat_init(const char *opt_arg, void *userdata _U_)
 	}
 
 	pc_proto_id = proto_registrar_get_id_byname("pkt_comment");
+	/* proto_get_id_by_filter_name searches a smaller table; we can't use
+	 * it for pkt_comment, which is a PINO, but can for _ws.col
+	 */
+	col_proto_id = proto_get_id_by_filter_name("_ws.col");
 
 	rs = new_phs_t(NULL, filter);
 
-	error_string = register_tap_listener("frame", rs, filter, TL_REQUIRES_PROTO_TREE, NULL, protohierstat_packet, protohierstat_draw, NULL);
+	error_string = register_tap_listener("frame", rs, filter, TL_REQUIRES_PROTO_TREE, NULL, protohierstat_packet, protohierstat_draw, (tap_finish_cb)free_phs);
 	if (error_string) {
 		/* error, we failed to attach to the tap. clean up */
 		free_phs(rs);
