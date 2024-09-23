a01f4ace45acb5267fbe21aaa0f10916578651d4
Gerald Combs <gerald@wireshark.org>
2024-09-23 16:31:59 -0700

    wsutil: Remove an unnecessary component from the Windows extcap path
    !14774 / 8541041922 ensured that we have separate namespaces everywhere
    for syscall+log and packet extcaps. It went a bit too far and added an
    unnecessary subdirectory to the global extcap path on Windows.
    
    Add missing "running in build directory" logic on Windows and use it
    to build the proper extcap paths for both the build and installation
    directories.
    
    Fixes #20069
    
    (cherry picked from commit a616eb00d1369cba8d42950ec3620a8490e7fe06)

--- packaging/nsis/logray.nsi
+++ packaging/nsis/logray.nsi
@@ -138,7 +138,7 @@ Page custom DisplayAdditionalTasksPage LeaveAdditionalTasksPage
 
   SetOutPath $INSTDIR
   File "${STAGING_DIR}\${EXTCAP_NAME}.html"
-  SetOutPath $INSTDIR\extcap\logray
+  SetOutPath $INSTDIR\extcap
   File "${STAGING_DIR}\extcap\logray\${EXTCAP_NAME}.exe"
 
 !macroend
@@ -817,7 +817,7 @@ File "${STAGING_DIR}\dtds\watcherinfo.dtd"
 SetOutPath $INSTDIR
 
 ; Create the extcap directory
-CreateDirectory $INSTDIR\extcap\logray
+CreateDirectory $INSTDIR\extcap
 
 ;
 ; install the protobuf .proto definitions in the protobuf subdirectory
@@ -1143,7 +1143,7 @@ Delete "$INSTDIR\COPYING*"
 Delete "$INSTDIR\audio\*.*"
 Delete "$INSTDIR\bearer\*.*"
 Delete "$INSTDIR\diameter\*.*"
-Delete "$INSTDIR\extcap\logray\falcodump.*"
+Delete "$INSTDIR\extcap\falcodump.*"
 Delete "$INSTDIR\gpl-2.0-standalone.html"
 Delete "$INSTDIR\Acknowledgements.md"
 Delete "$INSTDIR\generic\*.*"
@@ -1192,7 +1192,7 @@ RMDir "$INSTDIR\accessible"
 RMDir "$INSTDIR\audio"
 RMDir "$INSTDIR\bearer"
 RMDir "$INSTDIR\extcap"
-RMDir "$INSTDIR\extcap\logray"
+RMDir "$INSTDIR\extcap"
 RMDir "$INSTDIR\iconengines"
 RMDir "$INSTDIR\imageformats"
 RMDir "$INSTDIR\mediaservice"
--- wsutil/filesystem.c
+++ wsutil/filesystem.c
@@ -649,8 +649,14 @@ configuration_init_w32(const char* arg0 _U_)
          */
         progfile_dir = g_path_get_dirname(prog_pathname);
         if (progfile_dir != NULL) {
+            /* We succeeded. */
             trim_progfile_dir();
-            /* we succeeded */
+            /* Now try to figure out if we're running in a build directory. */
+            char *wsutil_lib = g_build_filename(progfile_dir, "wsutil.lib", (char *)NULL);
+            if (file_exists(wsutil_lib)) {
+                running_in_build_directory_flag = true;
+            }
+            g_free(wsutil_lib);
         } else {
             /*
              * OK, no. What do we do now?
@@ -1375,14 +1381,17 @@ init_extcap_dir(void)
         extcap_dir = g_build_filename(install_prefix, EXTCAP_DIR, (char *)NULL);
     }
 #elif defined(_WIN32)
-    else {
         /*
          * On Windows, extcap utilities are stored in "extcap/<program name>"
-         * in the program file directory in both the build and installation
-         * directories.
+         * in the build directory and in "extcap" in the installation
+         * directory.
          */
+    else if (running_in_build_directory_flag) {
         extcap_dir = g_build_filename(get_progfile_dir(), EXTCAP_DIR_NAME,
             CONFIGURATION_NAMESPACE_LOWER, (char *)NULL);
+    } else {
+        extcap_dir = g_build_filename(get_progfile_dir(), EXTCAP_DIR_NAME,
+            (char *)NULL);
     }
 #else
 #ifdef ENABLE_APPLICATION_BUNDLE
