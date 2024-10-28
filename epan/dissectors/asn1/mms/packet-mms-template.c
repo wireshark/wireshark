0a59f10bef195730ca651847f961457c86c4b187
Anders Broman <a.broman58@gmail.com>
2024-10-28 14:08:49 +0100

    MMS: Wrong bitmask used for CheckConditions bits.
    closes #20172

--- epan/dissectors/asn1/mms/packet-mms-template.c
+++ epan/dissectors/asn1/mms/packet-mms-template.c
@@ -69,8 +69,9 @@ static int hf_mms_iec61850_timequality40;
 static int hf_mms_iec61850_timequality20;
 static int hf_mms_iec61850_timequality1F;
 static int hf_mms_iec61850_check_bitstring;
-static int hf_mms_iec61850_check_b1;
-static int hf_mms_iec61850_check_b0;
+static int hf_mms_iec61850_check_b15;
+static int hf_mms_iec61850_check_b14;
+static int hf_mms_iec61850_check_b13_b0;
 static int hf_mms_iec61850_orcategory;
 static int hf_mms_iec61850_beh$stval;
 static int hf_mms_iec61850_mod$stval;
@@ -599,13 +600,17 @@ void proto_register_mms(void) {
           { "Check", "mms.iec61850.check_bitstring",
             FT_BYTES, BASE_NONE, NULL, 0,
             NULL, HFILL } },
-        { &hf_mms_iec61850_check_b1,
+        { &hf_mms_iec61850_check_b15,
         { "Synchrocheck", "mms.iec61850.synchrocheck",
-            FT_BOOLEAN, 2, NULL, 0x2,
+            FT_BOOLEAN, 8, NULL, 0x80,
             NULL, HFILL } },
-        { &hf_mms_iec61850_check_b0,
+        { &hf_mms_iec61850_check_b14,
         { "Interlock-check", "mms.iec61850.interlockcheck",
-            FT_BOOLEAN, 2, NULL, 0x1,
+            FT_BOOLEAN, 8, NULL, 0x40,
+            NULL, HFILL } },
+        { &hf_mms_iec61850_check_b13_b0,
+        { "Padding", "mms.iec61850.check.padding",
+            FT_UINT8, BASE_HEX, NULL, 0x3f,
             NULL, HFILL } },
         { &hf_mms_iec61850_orcategory,
         { "orCategory", "mms.iec61850.orcategory",
