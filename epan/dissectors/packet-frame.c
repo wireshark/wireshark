f0e59187880adc62400e86221d68da284134e8a2
Stig Bjørlykke <stig@bjorlykke.org>
2024-11-09 09:32:13 +0000

    packet: Make pinfo->rel_ts relative to first packet
    Always have packet_info rel_ts relative to the first packet.
    This will make taps and graphs do correct calculations when
    using time reference on packets.
    
    Fix #20203
    
    
    (cherry picked from commit 8d042ecc258c980bcc1896fe38b032636b418cc1)
    
    Co-authored-by: Stig Bjørlykke <stig@bjorlykke.org>

--- epan/dissectors/packet-frame.c
+++ epan/dissectors/packet-frame.c
@@ -562,7 +562,8 @@ static int
 dissect_frame(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree, void* data)
 {
 	proto_item  *volatile ti = NULL;
-	unsigned	     cap_len = 0, frame_len = 0;
+	unsigned     cap_len = 0, frame_len = 0;
+	nstime_t     rel_ts;
 	uint32_t     pack_flags;
 	uint32_t     interface_queue;
 	uint64_t     drop_count;
@@ -1035,8 +1036,10 @@ dissect_frame(tvbuff_t *tvb, packet_info *pinfo, proto_tree *parent_tree, void* 
 				proto_item_set_generated(item);
 			}
 
+			frame_delta_abs_time(pinfo->epan, pinfo->fd, pinfo->fd->frame_ref_num, &rel_ts);
+
 			item = proto_tree_add_time(fh_tree, hf_frame_time_relative, tvb,
-						   0, 0, &(pinfo->rel_ts));
+						   0, 0, &(rel_ts));
 			proto_item_set_generated(item);
 
 			if (pinfo->fd->ref_time) {
