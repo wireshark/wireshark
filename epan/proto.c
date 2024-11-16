f9be6d8b9a5ea9f02b99a9bffb6357f2c0c1c033
John Thacker <johnthacker@gmail.com>
2024-11-15 21:14:10 -0500

    epan: Fix get_display_repr crash on faked nodes
    If the node exists but does not have a field info, return an empty
    string for the same reason as if the node is faked and doesn't exist.
    
    Fixup 51e0dfa0057783b52d3b7e00cd4af1395b96b630
    (which allows faked nodes that exist but have null field info, in order
    to preserve the tree shape.)
    
    Fix #20224

--- epan/proto.c
+++ epan/proto.c
@@ -7906,8 +7906,10 @@ proto_item_get_display_repr(wmem_allocator_t *scope, proto_item *pi)
 	field_info *fi;
 
 	if (!pi)
-		return "";
+		return wmem_strdup(scope, "");
 	fi = PITEM_FINFO(pi);
+	if (!fi)
+		return wmem_strdup(scope, "");
 	DISSECTOR_ASSERT(fi->hfinfo != NULL);
 	return fvalue_to_string_repr(scope, fi->value, FTREPR_DISPLAY, fi->hfinfo->display);
 }
