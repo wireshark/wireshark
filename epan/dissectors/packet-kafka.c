6f6b8e9c4e76da5d5f8a3b18d848f36cd9f3e748
Mike Beaumont <mjboamail@gmail.com>
2024-11-19 23:21:04 +0100

    fix: only decode JoinGroup response skip_assignment with sufficient api_version

--- epan/dissectors/packet-kafka.c
+++ epan/dissectors/packet-kafka.c
@@ -5332,7 +5332,9 @@ dissect_kafka_join_group_response(tvbuff_t *tvb, packet_info *pinfo, proto_tree 
     offset = dissect_kafka_string(tree, hf_kafka_group_leader_id, tvb, pinfo, offset, api_version >= 6, NULL, NULL);
 
     /* skip_assignment */
-    offset = dissect_kafka_bool(tree, hf_kafka_skip_assignment, tvb, pinfo, offset);
+    if (api_version >= 9) {
+        offset = dissect_kafka_bool(tree, hf_kafka_skip_assignment, tvb, pinfo, offset);
+    }
 
     /* member_id */
     offset = dissect_kafka_string(tree, hf_kafka_member_id, tvb, pinfo, offset, api_version >= 6, &member_start, &member_len);
