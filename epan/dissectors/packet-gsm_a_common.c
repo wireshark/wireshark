8f375fe82e5c42f7e4fb4f8828e91aa18587e08d
Stig Bjørlykke <stig@bjorlykke.org>
2024-11-24 23:11:30 +0100

    gsm_a_common: Fix uncertainty speed abbrev
    Swap abbrev for Horizontal and Vertical uncertainty speed.

--- epan/dissectors/packet-gsm_a_common.c
+++ epan/dissectors/packet-gsm_a_common.c
@@ -4721,12 +4721,12 @@ proto_register_gsm_a_common(void)
         NULL, HFILL }
     },
     { &hf_gsm_a_h_uncertainty_speed,
-        { "Horizontal Uncertainty Speed", "gsm_a.gad.v_uncertainty_speed",
+        { "Horizontal Uncertainty Speed", "gsm_a.gad.h_uncertainty_speed",
         FT_UINT8, BASE_DEC, NULL, 0x0,
         NULL, HFILL }
     },
     { &hf_gsm_a_v_uncertainty_speed,
-        { "Vertical Uncertainty Speed", "gsm_a.gad.h_uncertainty_speed",
+        { "Vertical Uncertainty Speed", "gsm_a.gad.v_uncertainty_speed",
         FT_UINT8, BASE_DEC, NULL, 0x0,
         NULL, HFILL }
     },
