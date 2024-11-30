9dd1f894917aafa3b3eff2dc4500e7610203425d
Darius Davis <darius-wireshark@free-range.com.au>
2024-11-30 14:44:43 +1000

    Qt: Warn for non-executable "dumpcap" on Unix.
    This usually means that the user needs to be granted privilege to run
    "dumpcap", e.g. by being added to that binary's group ID.  For now, we
    just raise that condition in the user-interface as a generic "You
    don't have permission to capture on local interfaces" message.  It
    would be nice to give more detail and/or suggested steps to resolve it
    and/or a link to a wiki page.
    
    Ping #20257

--- ui/qt/interface_frame.cpp
+++ ui/qt/interface_frame.cpp
@@ -34,6 +34,10 @@
 #include "ui/capture_globals.h"
 #include <ui/iface_lists.h>
 #include <wsutil/utf8_entities.h>
+#ifdef Q_OS_UNIX
+#include <unistd.h> /* for access() and X_OK */
+#include <wsutil/filesystem.h>
+#endif
 
 #include <QDesktopServices>
 #include <QFrame>
@@ -395,6 +399,11 @@ bool InterfaceFrame::haveLocalCapturePermissions() const
 #ifdef Q_OS_MAC
     QFileInfo bpf0_fi = QFileInfo("/dev/bpf0");
     return bpf0_fi.isReadable() && bpf0_fi.isWritable();
+#elif defined(Q_OS_UNIX)
+    char *dumpcap_bin = get_executable_path("dumpcap");
+    bool executable = access(dumpcap_bin, X_OK) == 0;
+    g_free(dumpcap_bin);
+    return executable;
 #else
     // XXX Add checks for other platforms.
     return true;
