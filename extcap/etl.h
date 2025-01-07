febd09fad933d3d6043f34c70c4d435a9e42b8e1
Guy Harris <gharris@sonic.net>
2025-01-07 11:48:03 -0800

    etwdump: either declare functions static or declare them in headers.
    If a function is used only in the .c file that defines it, declare it
    static.
    
    If it's used in other .c files, don't declare it .c files that call it;
    declare it in a .h files also included in the .c file that defines it.

--- extcap/etl.h
+++ extcap/etl.h
@@ -29,7 +29,11 @@ typedef struct
     char padding[64];
 } SUPER_EVENT_TRACE_PROPERTIES;
 
-wtap_open_return_val etw_dump(const char* etl_filename, const char* pcapng_filename, const char* params, int* err, char** err_info);
+extern wtap_open_return_val etw_dump(const char* etl_filename, const char* pcapng_filename, const char* params, int* err, char** err_info);
+
+extern void wtap_etl_add_interface(int pkt_encap, char* interface_name, unsigned short interface_name_length, char* interface_desc, unsigned short interface_desc_length);
+
+extern void wtap_etl_rec_dump(char* etl_record, ULONG total_packet_length, ULONG original_packet_length, unsigned int interface_id, BOOLEAN is_inbound, ULARGE_INTEGER timestamp, int pkt_encap, char* comment, unsigned short comment_length);
 
 #endif
 
