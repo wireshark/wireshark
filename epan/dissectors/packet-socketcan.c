d48366c9f9e2f395aa9d3768184f9bcd8cef8b77
Giovanni Musto <giovanni.musto@partner.italdesign.it>
2025-01-09 17:25:54 +0100

    SocketCAN: Treat non-CAN-XL packets with length > 16 bytes as CAN-FD

--- epan/dissectors/packet-socketcan.c
+++ epan/dissectors/packet-socketcan.c
@@ -550,16 +550,29 @@ dissect_socketcan_common(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, un
 
         if (canxl_flags & CANXL_XLF) {
             /* CAN XL: check for min/max data length */
-            if ((tvb_reported_length(tvb) >= 13) && (tvb_reported_length(tvb) <= 2060))
+            if (tvb_reported_length(tvb) >= 13 && tvb_reported_length(tvb) <= 2060) {
                 can_packet_type = PACKET_TYPE_CAN_XL;
-        } else {
+            }
+        }
+        else if (tvb_reported_length(tvb) >= 8) {
             /* CAN CC/FD */
-            if ((tvb_reported_length(tvb) == 72) || (canfd_flags & CANFD_FDF)) {
-                /* CAN FD: check for min/max data length */
-                if ((tvb_reported_length(tvb) >= 8) && (tvb_reported_length(tvb) <= 72))
+            if ((canfd_flags & CANFD_FDF) || tvb_reported_length(tvb) > 16) {
+                /* CAN FD: check for max data length
+                 *
+                 * XXX - There's no "correct" way to detect a CAN-FD SocketCAN packet.
+                 * The FD Flag, contrary to the XL Flag, is not always set, so we have
+                 * to implement a heuristic. If the length is too big for a Classical
+                 * CAN packet, we arbitrarly decide it's CAN-FD. This will not detect
+                 * CAN-FD packets with DLC 0 through 8, but also misdetect any Classical
+                 * CAN packet with extra payload as CAN-FD. 
+                 */
+                if (tvb_reported_length(tvb) <= 72) {
                     can_packet_type = PACKET_TYPE_CAN_FD;
-            } else if ((tvb_reported_length(tvb) >= 8) && (tvb_reported_length(tvb) <= 16))
+                }
+            }
+            else {
                 can_packet_type = PACKET_TYPE_CAN;
+            }
         }
     }
 
