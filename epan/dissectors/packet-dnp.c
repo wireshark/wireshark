bc3cb6cf256685a177c133052ab0938ba3fa3cd8
John Thacker <johnthacker@gmail.com>
2025-01-14 08:47:45 -0500

    DNP3: Check for null TVB returned by process_reassembled_data
    If the reassembly was completed, we can always call
    process_reassembled_data. If it returns a non-null TVB,
    call the dissector. If it returns a null TVB, it was
    reassembled in another frame.
    
    Ping #20336

--- epan/dissectors/packet-dnp.c
+++ epan/dissectors/packet-dnp.c
@@ -4306,12 +4306,10 @@ dissect_dnp3_message(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* 
 
       if (frag_al)
       {
-        /* This is the fragment in which reassembly occurred iff FIN is set. */
-        if (tr_fin)
-        {
-          next_tvb = process_reassembled_data(al_tvb, 0, pinfo,
-            "Reassembled DNP 3.0 Application Layer message", frag_al, &dnp3_frag_items,
-            NULL, dnp3_tree);
+        next_tvb = process_reassembled_data(al_tvb, 0, pinfo,
+          "Reassembled DNP 3.0 Application Layer message", frag_al, &dnp3_frag_items,
+          NULL, dnp3_tree);
+        if (next_tvb) {
           /* As a complete AL message will have cleared the info column,
              make sure source and dest are always in the info column */
           //col_append_fstr(pinfo->cinfo, COL_INFO, "from %u to %u", dl_src, dl_dst);
@@ -4320,8 +4318,6 @@ dissect_dnp3_message(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* 
         }
         else
         {
-          proto_tree_add_uint(dnp3_tree, hf_dnp3_fragment_reassembled_in, tvb, 0, 0,
-            frag_al->reassembled_in);
           /* Lock any column info set by the DL and TL */
           col_set_fence(pinfo->cinfo, COL_INFO);
           col_append_fstr(pinfo->cinfo, COL_INFO,
