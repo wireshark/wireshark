5b830615d653fa9b80773a17a26d5ebc074a7bfe
Szilard Turi-Kovats <szilard.turi-kovats@commsignia.com>
2025-02-06 12:23:49 +0100

    ITS: GeoNetworking: Show CBR (Channel busy ratio) in 0.00[%] format
    The value is encoded as unsigned int (0..255)
    The actual represenation is percentage (0..100)

--- epan/dissectors/packet-geonw.c
+++ epan/dissectors/packet-geonw.c
@@ -2964,6 +2964,12 @@ display_heading( char *result, uint32_t hexver )
 }
 
 static void
+display_cbr( char *result, uint8_t hexver )
+{
+    snprintf( result, ITEM_LABEL_LENGTH, "%.2f %% (%u)", hexver * 100.0 / 255.0, hexver);
+}
+
+static void
 display_elevation( char *result, int32_t hexver )
 {
     //  0x0000 to 0xEFFF: positive numbers with a range from 0 to +6 143,9 meters. All numbers above +6 143,9 are
@@ -3231,12 +3237,12 @@ proto_register_geonw(void)
 
         { &hf_geonw_dccmco_cbr_l_0_hop,
           { "Local channel busy ratio", "geonw.cbr_l0hop",
-            FT_UINT8, BASE_DEC, NULL, 0x80,
+            FT_UINT8, BASE_CUSTOM, CF_FUNC(display_cbr), 0x00,
             NULL, HFILL }},
 
         { &hf_geonw_dccmco_cbr_l_1_hop,
           { "Max neighbouring CBR", "geonw.cbr_l1hop",
-            FT_UINT8, BASE_DEC, NULL, 0x80,
+            FT_UINT8, BASE_CUSTOM, CF_FUNC(display_cbr), 0x00,
             NULL, HFILL }},
 
         { &hf_geonw_dccmco_output_power,
