wireshark/wireshark

commit 05e404e8cbb8c3e45324fc924f0e9a30c72c9d32

epan: Add STRING_CASE_[IN]SENSITIVE for dissector tables
The last parameter of register_dissector_table() indicates the
base for integer tables, indicates case sensitivity for string
tables, and is ignored for other tables (FT_NONE, FT_GUID).

It can be a little difficult to remember what the code is doing
when reading it, and which of 0 and 1 is sensitive and which is
insensitive (0 is sensitive, the default).

Add STRING_CASE_SENSITIVE and STRING_CASE_INSENSITIVE.
Check in tools/fix-encoding-args.pl for STRING-like tables that use
BASE_NONE, TRUE, or FALSE, and convert them to the new values.

epan/packet.h
@@ -47,6 +47,12 @@ struct epan_range;
 	((guint)(offset) + (guint)(len) > (guint)(offset) && \
 	 (guint)(offset) + (guint)(len) <= (guint)(captured_len))
 
+/* 0 is case insenstive for backwards compatibility with tables that
+ * used FALSE or BASE_NONE for case sensitive, which was the default.
+ */
+#define STRING_CASE_SENSITIVE 0
+#define STRING_CASE_INSENSITIVE 1
+
 extern void packet_init(void);
 extern void packet_cache_proto_handles(void);
 extern void packet_cleanup(void);
@@ -166,9 +172,9 @@ WS_DLL_PUBLIC void dissector_all_tables_foreach_table (DATFunc_table func,
 
 /* a protocol uses the function to register a sub-dissector table
  *
- * 'param' is the display base for integer tables, and TRUE/FALSE for
- * string tables (true indicating case-insensitive, false indicating
- * case-sensitive)
+ * 'param' is the display base for integer tables, STRING_CASE_SENSITIVE
+ * or STRING_CASE_INSENSITIVE for string tables, and ignored for other
+ * table types.
  */
 WS_DLL_PUBLIC dissector_table_t register_dissector_table(const char *name,
     const char *ui_name, const int proto, const ftenum_t type, const int param);

commit 0838e5b2a224480311df4dd896710aaef45d5ed8

packet.h: Fix doc-comment of STRING_CASE_SENSITIVE
The comment was backwards from what was actually in the code, and
in the original commit's message, probably the result of a typo.

Fixes: 05e404e8cbb8 ("epan: Add STRING_CASE_[IN]SENSITIVE for dissector tables")
Signed-off-by: Bence Csókás <bence98@sch.bme.hu>

epan/packet.h
@@ -42,7 +42,7 @@ extern "C" {
 	((unsigned)(offset) + (unsigned)(len) > (unsigned)(offset) && \
 	 (unsigned)(offset) + (unsigned)(len) <= (unsigned)(captured_len))
 
-/* 0 is case insensitive for backwards compatibility with tables that
+/* 0 is case sensitive for backwards compatibility with tables that
  * used false or BASE_NONE for case sensitive, which was the default.
  */
 #define STRING_CASE_SENSITIVE 0