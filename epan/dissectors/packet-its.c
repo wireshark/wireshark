8e00860cbc775317d013de37995f01aa6548e942
AndersBroman <a.broman58@gmail.com>
2025-02-27 22:35:00 +0000

    Revert "ITS: CPM: Fix extensibility flag of WrappedCpmContainer"
    This reverts commit e8a8056a6bdb8d255ce5d5b984e981eae943fc4b

--- epan/dissectors/packet-its.c
+++ epan/dissectors/packet-its.c
@@ -22189,7 +22189,7 @@ static int
 dissect_cpm_WrappedCpmContainers(tvbuff_t *tvb _U_, int offset _U_, asn1_ctx_t *actx _U_, proto_tree *tree _U_, int hf_index _U_) {
   offset = dissect_per_constrained_sequence_of(tvb, offset, actx, tree, hf_index,
                                                   ett_cpm_WrappedCpmContainers, cpm_WrappedCpmContainers_sequence_of,
-                                                  1, 8, false);
+                                                  1, 8, true);
 
   return offset;
 }
