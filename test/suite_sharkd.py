6f9dd37b75a1e4da770e5ba7b727924b6e99a4ec
John Thacker <johnthacker@gmail.com>
2025-03-02 13:50:17 -0500

    sharkd: Update tests that use out-of-order capture
    The http-ooo.pcap requires setting the tcp.reassemble_out_of_order
    preference to true to dissect correctly. While there is sometimes
    value in testing that capture with the preference off, consistent
    results from release to release are not guaranteed (improvements to
    how out of order dissection is attempted with the preference off can
    change the results.) Out of order testing is done in the detection
    test suite. The sharkd suite should set the preference on so as to
    have consistent results that only test the sharkd behavior.

--- test/suite_sharkd.py
+++ test/suite_sharkd.py
@@ -1095,33 +1095,45 @@ class TestSharkd:
 
     def test_sharkd_req_tap_eo_http(self, check_sharkd_session, capture_file):
         check_sharkd_session((
-            {"jsonrpc":"2.0", "id":1, "method":"load",
+            {"jsonrpc":"2.0", "id":1, "method":"setconf",
+            "params":{"name": "tcp.reassemble_out_of_order", "value": "true"}
+            },
+            {"jsonrpc":"2.0", "id":2, "method":"load",
              "params":{"file": capture_file('http-ooo.pcap')}
              },
-            {"jsonrpc":"2.0", "id":2, "method":"tap", "params":{"tap0": "eo:http"}},
+            {"jsonrpc":"2.0", "id":3, "method":"tap", "params":{"tap0": "eo:http"}},
         ), (
             {"jsonrpc":"2.0","id":1,"result":{"status":"OK"}},
-            {"jsonrpc":"2.0","id":2,"result":{
+            {"jsonrpc":"2.0","id":2,"result":{"status":"OK"}},
+            {"jsonrpc":"2.0","id":3,"result":{
                 "taps":[{
                     "tap":"eo:http",
                     "type":"eo",
                     "proto":"HTTP",
                     "objects":[{
+                        "pkt":4,
+                        "filename":"1",
+                        "_download":"eo:http_0",
+                        "len":4,
+                        "sha1":"83f5a5c359f3dc8317519240e32f1f51f68bc051"
+                    },{
+                        "pkt":10,
+                        "filename":"3",
+                        "_download":"eo:http_1",
+                        "len":6,
+                        "sha1":"a214ad86e2def05fcb0f4c878dfebe5a6041fb7e"
+                    },{
                         "pkt":11,
                         "filename":"4",
-                        "_download":"eo:http_0",
+                        "_download":"eo:http_2",
                         "len":5,
                         "sha1":"4a4121ecd766ed16943a0c7b54c18f743e90c3f6"
                     },{
-                        "pkt":13,
-                        "_download":"eo:http_1",
-                        "len":5,
-                        "sha1":"29a51e7382d06ff40467272f02e413ca7b51636e"
-                    },{
-                        "pkt":14,
-                        "_download":"eo:http_2",
-                        "len":5,
-                        "sha1":"f6d0c643351580307b2eaa6a7560e76965496bc7"}]
+                        "pkt":15,
+                        "filename":"5",
+                        "_download":"eo:http_3",
+                        "len":4,
+                        "sha1":"580393f5a94fb469585f5dd2a6859a4aab899f37"}]
                 }]
             }}
         ))
@@ -1497,10 +1509,71 @@ class TestSharkd:
 
     def test_sharkd_req_download_eo_http_with_prior_tap_eo_http(self, check_sharkd_session, capture_file):
         check_sharkd_session((
-            {"jsonrpc":"2.0", "id":1, "method":"load",
+            {"jsonrpc":"2.0", "id":1, "method":"setconf",
+            "params":{"name": "tcp.reassemble_out_of_order", "value": "true"}
+            },
+            {"jsonrpc":"2.0", "id":2, "method":"load",
              "params":{"file": capture_file('http-ooo.pcap')}
              },
-            {"jsonrpc":"2.0", "id":2, "method":"tap", "params":{"tap0": "eo:http"}},
+            {"jsonrpc":"2.0", "id":3, "method":"tap", "params":{"tap0": "eo:http"}},
+            {"jsonrpc":"2.0", "id":4, "method":"download",
+             "params":{"token": "eo:http_0"}},
+            {"jsonrpc":"2.0", "id":5, "method":"download",
+             "params":{"token": "eo:http_1"}},
+            {"jsonrpc":"2.0", "id":6, "method":"download",
+             "params":{"token": "eo:http_2"}},
+            {"jsonrpc":"2.0", "id":7, "method":"download",
+             "params":{"token": "eo:http_999"}},
+        ), (
+            {"jsonrpc":"2.0","id":1,"result":{"status":"OK"}},
+            {"jsonrpc":"2.0","id":2,"result":{"status":"OK"}},
+            {"jsonrpc":"2.0","id":3,"result":{
+                "taps":[{
+                    "tap":"eo:http",
+                    "type":"eo",
+                    "proto":"HTTP",
+                    "objects":[{
+                        "pkt":4,
+                        "filename":"1",
+                        "_download":"eo:http_0",
+                        "len":4,
+                        "sha1":"83f5a5c359f3dc8317519240e32f1f51f68bc051"
+                    },{
+                        "pkt":10,
+                        "filename":"3",
+                        "_download":"eo:http_1",
+                        "len":6,
+                        "sha1":"a214ad86e2def05fcb0f4c878dfebe5a6041fb7e"
+                    },{
+                        "pkt":11,
+                        "filename":"4",
+                        "_download":"eo:http_2",
+                        "len":5,
+                        "sha1":"4a4121ecd766ed16943a0c7b54c18f743e90c3f6"
+                    },{
+                        "pkt":15,
+                        "filename":"5",
+                        "_download":"eo:http_3",
+                        "len":4,
+                        "sha1":"580393f5a94fb469585f5dd2a6859a4aab899f37"}]
+                }]
+            }},
+            {"jsonrpc":"2.0","id":4,"result":{
+                "file":"1","mime":"application/octet-stream","data":"MQoyCg=="}},
+            {"jsonrpc":"2.0","id":5,"result":{
+                "file":"3","mime":"application/octet-stream","data":"YWZ0ZXIK"}},
+            {"jsonrpc":"2.0","id":6,"result":{
+                "file":"4","mime":"application/octet-stream","data":"Zm91cgo="}},
+            {"jsonrpc":"2.0","id":7,"result":{}},
+        ))
+    def test_sharkd_req_download_eo_http_without_prior_tap_eo_http(self, check_sharkd_session, capture_file):
+        check_sharkd_session((
+            {"jsonrpc":"2.0", "id":1, "method":"setconf",
+            "params":{"name": "tcp.reassemble_out_of_order", "value": "true"}
+            },
+            {"jsonrpc":"2.0", "id":2, "method":"load",
+             "params":{"file": capture_file('http-ooo.pcap')}
+             },
             {"jsonrpc":"2.0", "id":3, "method":"download",
              "params":{"token": "eo:http_0"}},
             {"jsonrpc":"2.0", "id":4, "method":"download",
@@ -1511,59 +1584,14 @@ class TestSharkd:
              "params":{"token": "eo:http_999"}},
         ), (
             {"jsonrpc":"2.0","id":1,"result":{"status":"OK"}},
-            {"jsonrpc":"2.0","id":2,"result":{
-                "taps":[{
-                    "tap":"eo:http",
-                    "type":"eo",
-                    "proto":"HTTP",
-                    "objects":[{
-                        "pkt":11,
-                        "filename":"4",
-                        "_download":"eo:http_0",
-                        "len":5,
-                        "sha1":"4a4121ecd766ed16943a0c7b54c18f743e90c3f6"
-                    },{
-                        "pkt":13,
-                        "_download":"eo:http_1",
-                        "len":5,
-                        "sha1":"29a51e7382d06ff40467272f02e413ca7b51636e"
-                    },{
-                        "pkt":14,
-                        "_download":"eo:http_2",
-                        "len":5,
-                        "sha1":"f6d0c643351580307b2eaa6a7560e76965496bc7"}]
-                }]
-            }},
+            {"jsonrpc":"2.0","id":2,"result":{"status":"OK"}},
             {"jsonrpc":"2.0","id":3,"result":{
-                "file":"4","mime":"application/octet-stream","data":"Zm91cgo="}},
+                "file":"1","mime":"application/octet-stream","data":"MQoyCg=="}},
             {"jsonrpc":"2.0","id":4,"result":{
-                "file":"eo:http_1","mime":"application/octet-stream","data":"QVRBDQo="}},
+                "file":"3","mime":"application/octet-stream","data":"YWZ0ZXIK"}},
             {"jsonrpc":"2.0","id":5,"result":{
-                "file":"eo:http_2","mime":"application/octet-stream","data":"MA0KDQo="}},
-            {"jsonrpc":"2.0","id":6,"result":{}},
-        ))
-    def test_sharkd_req_download_eo_http_without_prior_tap_eo_http(self, check_sharkd_session, capture_file):
-        check_sharkd_session((
-            {"jsonrpc":"2.0", "id":1, "method":"load",
-             "params":{"file": capture_file('http-ooo.pcap')}
-             },
-            {"jsonrpc":"2.0", "id":2, "method":"download",
-             "params":{"token": "eo:http_0"}},
-            {"jsonrpc":"2.0", "id":3, "method":"download",
-             "params":{"token": "eo:http_1"}},
-            {"jsonrpc":"2.0", "id":4, "method":"download",
-             "params":{"token": "eo:http_2"}},
-            {"jsonrpc":"2.0", "id":5, "method":"download",
-             "params":{"token": "eo:http_999"}},
-        ), (
-            {"jsonrpc":"2.0","id":1,"result":{"status":"OK"}},
-            {"jsonrpc":"2.0","id":2,"result":{
                 "file":"4","mime":"application/octet-stream","data":"Zm91cgo="}},
-            {"jsonrpc":"2.0","id":3,"result":{
-                "file":"eo:http_1","mime":"application/octet-stream","data":"QVRBDQo="}},
-            {"jsonrpc":"2.0","id":4,"result":{
-                "file":"eo:http_2","mime":"application/octet-stream","data":"MA0KDQo="}},
-            {"jsonrpc":"2.0","id":5,"result":{}},
+            {"jsonrpc":"2.0","id":6,"result":{}},
         ))
     def test_sharkd_req_bye(self, check_sharkd_session):
         check_sharkd_session((
