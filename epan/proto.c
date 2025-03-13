fe1217ac75d57b44a3b0b37e4731834cbf4c4857
John Thacker <johnthacker@gmail.com>
2025-03-12 22:07:04 -0400

    epan: Fix elastic-mapping types
    "byte" means an 8-bit signed integer FT_INT8, not FT_BYTES.
    
    There's now an unsigned long type for FT_UINT64.
    
    FT_INT16 fits in a "short".
    
    Add the rarely-used FT_INT40 and FT_INT56.
    
    FT_DOUBLE needs a double, and FT_RELATIVE_DATE isn't a "date"
    (which is for absolute dates - any times since the epoch should
    be FT_ABSOLUTE_DATE) but also needs a double (or possibly a
    "scaled_float" with a 10^9 scaling factor to preserve precision.)
    
    For FT_ABSOLUTE_DATE, all supported versions of ElasticSearch/OpenSearch
    support "date_nanos" (it internally uses a 64-bit integer of nanoseconds,
    which creates a Y2262 problem, but that should be ok with our actual data.)
    
    Ping #19657

--- epan/proto.c
+++ epan/proto.c
@@ -11938,39 +11938,41 @@ elastic_add_base_mapping(json_dumper *dumper)
 }
 
 static char*
-ws_type_to_elastic(unsigned type _U_)
+ws_type_to_elastic(unsigned type)
 {
 	switch(type) {
-		case FT_UINT16:
+		case FT_INT8:
+			return "byte";
+		case FT_UINT8:
 		case FT_INT16:
+			return "short";
+		case FT_UINT16:
 		case FT_INT32:
 		case FT_UINT24:
 		case FT_INT24:
 			return "integer";
-		case FT_INT8:
-		case FT_UINT8:
-			return "short";
 		case FT_FRAMENUM:
 		case FT_UINT32:
 		case FT_UINT40:
 		case FT_UINT48:
 		case FT_UINT56:
-		case FT_UINT64: // Actually it's not handled by 'long' elastic type.
+		case FT_INT40:
 		case FT_INT48:
+		case FT_INT56:
 		case FT_INT64:
 			return "long";
+		case FT_UINT64:
+			return "unsigned long"; // ElasticSearch since 7.0, OpenSearch 2.8
 		case FT_FLOAT:
-		case FT_DOUBLE:
 			return "float";
+		case FT_DOUBLE:
+		case FT_RELATIVE_TIME: // "scaled_float" with "scaling_factor" 1e9 superior?
+			return "double";
 		case FT_IPv6:
 		case FT_IPv4:
 			return "ip";
 		case FT_ABSOLUTE_TIME:
-		case FT_RELATIVE_TIME:
-			return "date";
-		case FT_BYTES:
-		case FT_UINT_BYTES:
-			return "byte";
+			return "date_nanos"; // This is a 64 bit integer of nanoseconds, so it does have a Y2262 problem
 		case FT_BOOLEAN:
 			return "boolean";
 		default:
