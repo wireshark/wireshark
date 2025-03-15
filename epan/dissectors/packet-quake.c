wireshark/wireshark

commit 4f9707469dc6edb8792ec811fecebb0f377fc58b

replaced decode_boolean_bitfield calls with itemized filters
added tfs_no_yes to tfs.[ch]

svn path=/trunk/; revision=45115

epan/dissectors/packet-quake.c
@@ -36,6 +36,12 @@
 
 static int proto_quake = -1;
 static int hf_quake_header_flags = -1;
+static int hf_quake_header_flags_data = -1;
+static int hf_quake_header_flags_ack = -1;
+static int hf_quake_header_flags_no_ack = -1;
+static int hf_quake_header_flags_endmsg = -1;
+static int hf_quake_header_flags_unreliable = -1;
+static int hf_quake_header_flags_control = -1;
 static int hf_quake_header_length = -1;
 static int hf_quake_header_sequence = -1;
 static int hf_quake_control_command = -1;
@@ -74,18 +80,16 @@ static dissector_handle_t quake_handle;
 static dissector_handle_t data_handle;
 
 /* I took these names directly out of the Q1 source. */
-#define NETFLAG_LENGTH_MASK 0x0000ffff
 #define NET_HEADERSIZE 8
 #define DEFAULTnet_hostport 26000
 static guint gbl_quakeServerPort=DEFAULTnet_hostport;
 
-#define NETFLAG_LENGTH_MASK     0x0000ffff
-#define NETFLAG_DATA            0x00010000
-#define NETFLAG_ACK             0x00020000
-#define NETFLAG_NAK             0x00040000
-#define NETFLAG_EOM             0x00080000
-#define NETFLAG_UNRELIABLE      0x00100000
-#define NETFLAG_CTL             0x80000000
+#define NETFLAG_DATA            0x0001
+#define NETFLAG_ACK             0x0002
+#define NETFLAG_NAK             0x0004
+#define NETFLAG_EOM             0x0008
+#define NETFLAG_UNRELIABLE      0x0010
+#define NETFLAG_CTL             0x8000
 
 
 #define CCREQ_CONNECT           0x01
@@ -428,8 +432,7 @@ static void
 dissect_quake(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
 	proto_tree	*quake_tree = NULL;
-	guint32		length;
-	guint32		flags;
+	guint16		flags;
 	guint32		sequence = 0;
 	guint		rest_length;
 	tvbuff_t	*next_tvb;
@@ -437,9 +440,7 @@ dissect_quake(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "QUAKE");
 	col_clear(pinfo->cinfo, COL_INFO);
 
-	length = tvb_get_ntohl(tvb, 0);
-	flags = length & (~NETFLAG_LENGTH_MASK);
-	length &= NETFLAG_LENGTH_MASK;
+	flags = tvb_get_ntohs(tvb, 2);
 
 	if (tree) {
 		proto_item *quake_item;
@@ -452,30 +453,18 @@ dissect_quake(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 		proto_item* flags_item;
 		proto_tree* flags_tree;
 
-		flags_item = proto_tree_add_uint(quake_tree, hf_quake_header_flags,
-			tvb, 0, 2, flags);
+		flags_item = proto_tree_add_item(quake_tree, hf_quake_header_flags,
+			tvb, 2, 2, ENC_BIG_ENDIAN);
 		flags_tree = proto_item_add_subtree(flags_item, ett_quake_flags);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_data, tvb, 2, 2, ENC_BIG_ENDIAN);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_ack, tvb, 2, 2, ENC_BIG_ENDIAN);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_no_ack, tvb, 2, 2, ENC_BIG_ENDIAN);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_endmsg, tvb, 2, 2, ENC_BIG_ENDIAN);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_unreliable, tvb, 2, 2, ENC_BIG_ENDIAN);
+		proto_tree_add_item(flags_tree, hf_quake_header_flags_control, tvb, 2, 2, ENC_BIG_ENDIAN);
 
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_DATA, 32,
-							    "Data","-"));
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_ACK, 32,
-							    "Acknowledgment","-"));
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_NAK, 32,
-							    "No Acknowledgment","-"));
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_EOM, 32,
-							    "End Of Message","-"));
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_UNRELIABLE, 32,
-							    "Unreliable","-"));
-		proto_tree_add_text(flags_tree, tvb, 0, 2, "%s",
-				    decode_boolean_bitfield(flags, NETFLAG_CTL, 32,
-							    "Control","-"));
-		proto_tree_add_uint(quake_tree, hf_quake_header_length,
-			tvb, 2, 2, length);
+		proto_tree_add_item(quake_tree, hf_quake_header_length,
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	}
 
 	if (flags == NETFLAG_CTL) {
@@ -509,6 +498,30 @@ proto_register_quake(void)
     { &hf_quake_header_flags,
       { "Flags", "quake.header.flags",
 	FT_UINT16, BASE_HEX, NULL, 0x0,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_data,
+      { "Data", "quake.header.flags.data",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_DATA,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_ack,
+      { "Acknowledgment", "quake.header.flags.ack",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_ACK,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_no_ack,
+      { "No Acknowledgment", "quake.header.flags.no_ack",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_NAK,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_endmsg,
+      { "End Of Message", "quake.header.flags.endmsg",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_EOM,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_unreliable,
+      { "Unreliable", "quake.header.flags.unreliable",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_UNRELIABLE,
+	NULL, HFILL }},
+    { &hf_quake_header_flags_control,
+      { "Control", "quake.header.flags.control",
+	FT_BOOLEAN, 16, TFS(&tfs_set_notset), NETFLAG_CTL,
 	NULL, HFILL }},
     { &hf_quake_header_length,
       { "Length", "quake.header.length",

epan/dissectors/packet-cups.c
@@ -60,53 +60,6 @@ enum                                    /* Not a typedef'd enum so we can OR */
 };
 /* End insert from cups/cups.h */
 
-typedef struct {
-	guint32	bit;
-	const char	*on_string;
-	const char	*off_string;
-} cups_ptype_bit_info;
-
-static const cups_ptype_bit_info cups_ptype_bits[] = {
-	{ CUPS_PRINTER_DEFAULT,
-	  "Default printer on network", "Not default printer" },
-	{ CUPS_PRINTER_IMPLICIT,
-	  "Implicit class", "Explicit class" },
-	{ CUPS_PRINTER_VARIABLE,
-	  "Can print variable sizes", "Cannot print variable sizes" },
-	{ CUPS_PRINTER_LARGE,
-	  "Can print up to 36x48 inches", "Cannot print up to 36x48 inches" },
-	{ CUPS_PRINTER_MEDIUM,
-	  "Can print up to 18x24 inches", "Cannot print up to 18x24 inches" },
-	{ CUPS_PRINTER_SMALL,
-	  "Can print up to 9x14 inches", "Cannot print up to 9x14 inches" },
-	{ CUPS_PRINTER_SORT,
-	  "Can sort", "Cannot sort" },
-	{ CUPS_PRINTER_BIND,
-	  "Can bind", "Cannot bind" },
-	{ CUPS_PRINTER_COVER,
-	  "Can cover", "Cannot cover" },
-	{ CUPS_PRINTER_PUNCH,
-	  "Can punch holes", "Cannot punch holes" },
-	{ CUPS_PRINTER_COLLATE,
-	  "Can do fast collating", "Cannot do fast collating" },
-	{ CUPS_PRINTER_COPIES,
-	  "Can do fast copies", "Cannot do fast copies" },
-	{ CUPS_PRINTER_STAPLE,
-	  "Can staple", "Cannot staple" },
-	{ CUPS_PRINTER_DUPLEX,
-	  "Can duplex", "Cannot duplex" },
-	{ CUPS_PRINTER_COLOR,
-	  "Can print color", "Cannot print color" },
-	{ CUPS_PRINTER_BW,
-	  "Can print black", "Cannot print black" },
-	{ CUPS_PRINTER_REMOTE,
-	  "Remote", "Local (illegal)" },
-	{ CUPS_PRINTER_CLASS,
-	  "Printer class", "Single printer" }
-};
-
-#define N_CUPS_PTYPE_BITS	(sizeof cups_ptype_bits / sizeof cups_ptype_bits[0])
-
 typedef enum _cups_state {
 	CUPS_IDLE = 3,
 	CUPS_PROCESSING,
@@ -120,8 +73,29 @@ static const value_string cups_state_values[] = {
 	{ 0,			NULL }
 };
 
+static const true_false_string tfs_implicit_explicit = { "Implicit class", "Explicit class" };
+static const true_false_string tfs_printer_class = { "Printer class", "Single printer" };
+
 static int proto_cups = -1;
 static int hf_cups_ptype = -1;
+static int hf_cups_ptype_default = -1;
+static int hf_cups_ptype_implicit = -1;
+static int hf_cups_ptype_variable = -1;
+static int hf_cups_ptype_large = -1;
+static int hf_cups_ptype_medium = -1;
+static int hf_cups_ptype_small = -1;
+static int hf_cups_ptype_sort = -1;
+static int hf_cups_ptype_bind = -1;
+static int hf_cups_ptype_cover = -1;
+static int hf_cups_ptype_punch = -1;
+static int hf_cups_ptype_collate = -1;
+static int hf_cups_ptype_copies = -1;
+static int hf_cups_ptype_staple = -1;
+static int hf_cups_ptype_duplex = -1;
+static int hf_cups_ptype_color = -1;
+static int hf_cups_ptype_bw = -1;
+static int hf_cups_ptype_remote = -1;
+static int hf_cups_ptype_class = -1;
 static int hf_cups_state = -1;
 
 static gint ett_cups = -1;
@@ -152,7 +126,6 @@ dissect_cups(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	gint		offset = 0;
 	gint		next_offset;
 	guint		len;
-	unsigned int	u;
 	const guint8	*str;
 	cups_ptype_t	ptype;
 	unsigned int	state;
@@ -173,18 +146,26 @@ dissect_cups(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 	len = next_offset - offset;
 	if (len != 0) {
 		if (cups_tree) {
-			ti = proto_tree_add_uint(cups_tree, hf_cups_ptype,
-			    tvb, offset, len, ptype);
-			ptype_subtree = proto_item_add_subtree(ti,
-			    ett_cups_ptype);
-			for (u = 0; u < N_CUPS_PTYPE_BITS; u++) {
-				proto_tree_add_text(ptype_subtree, tvb,
-				    offset, len, "%s",
-				    decode_boolean_bitfield(ptype,
-				      cups_ptype_bits[u].bit, sizeof (ptype)*8,
-				      cups_ptype_bits[u].on_string,
-				      cups_ptype_bits[u].off_string));
-			}
+			ti = proto_tree_add_uint(cups_tree, hf_cups_ptype, tvb, offset, len, ptype);
+			ptype_subtree = proto_item_add_subtree(ti, ett_cups_ptype);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_default, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_implicit, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_variable, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_large, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_medium, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_small, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_sort, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_bind, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_cover, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_punch, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_collate, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_copies, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_staple, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_duplex, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_color, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_bw, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_remote, tvb, offset, len, ENC_BIG_ENDIAN);
+			proto_tree_add_item(ptype_subtree, hf_cups_ptype_class, tvb, offset, len, ENC_BIG_ENDIAN);
 		}
 	}
 	offset = next_offset;
@@ -353,11 +334,63 @@ void
 proto_register_cups(void)
 {
 	static hf_register_info hf[] = {
-		/* This one could be split in separate fields. */
 		{ &hf_cups_ptype,
 			{ "Type", 	"cups.ptype", FT_UINT32, BASE_HEX,
 			  NULL, 0x0, NULL, HFILL }},
-
+		{ &hf_cups_ptype_default,
+			{ "Default printer on network", "cups.ptype.default", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_DEFAULT, NULL, HFILL }},
+		{ &hf_cups_ptype_implicit,
+			{ "Class", "cups.ptype.implicit", FT_BOOLEAN, 32,
+			  TFS(&tfs_implicit_explicit), CUPS_PRINTER_IMPLICIT, NULL, HFILL }},
+		{ &hf_cups_ptype_variable,
+			{ "Can print variable sizes", "cups.ptype.variable", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_VARIABLE, NULL, HFILL }},
+		{ &hf_cups_ptype_large,
+			{ "Can print up to 36x48 inches", "cups.ptype.large", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_LARGE, NULL, HFILL }},
+		{ &hf_cups_ptype_medium,
+			{ "Can print up to 18x24 inches", "cups.ptype.medium", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_MEDIUM, NULL, HFILL }},
+		{ &hf_cups_ptype_small,
+			{ "Can print up to 9x14 inches", "cups.ptype.small", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_SMALL, NULL, HFILL }},
+		{ &hf_cups_ptype_sort,
+			{ "Can sort", "cups.ptype.sort", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_SORT, NULL, HFILL }},
+		{ &hf_cups_ptype_bind,
+			{ "Can bind", "cups.ptype.bind", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_BIND, NULL, HFILL }},
+		{ &hf_cups_ptype_cover,
+			{ "Can cover", "cups.ptype.cover", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_COVER, NULL, HFILL }},
+		{ &hf_cups_ptype_punch,
+			{ "Can punch holes", "cups.ptype.punch", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_PUNCH, NULL, HFILL }},
+		{ &hf_cups_ptype_collate,
+			{ "Can do fast collating", "cups.ptype.collate", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_COLLATE, NULL, HFILL }},
+		{ &hf_cups_ptype_copies,
+			{ "Can do fast copies", "cups.ptype.copies", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_COPIES, NULL, HFILL }},
+		{ &hf_cups_ptype_staple,
+			{ "Can staple", "cups.ptype.staple", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_STAPLE, NULL, HFILL }},
+		{ &hf_cups_ptype_duplex,
+			{ "Can duplex", "cups.ptype.duplex", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_DUPLEX, NULL, HFILL }},
+		{ &hf_cups_ptype_color,
+			{ "Can print color", "cups.ptype.color", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_COLOR, NULL, HFILL }},
+		{ &hf_cups_ptype_bw,
+			{ "Can print black", "cups.ptype.bw", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_BW, NULL, HFILL }},
+		{ &hf_cups_ptype_remote,
+			{ "Remote", "cups.ptype.remote", FT_BOOLEAN, 32,
+			  TFS(&tfs_yes_no), CUPS_PRINTER_REMOTE, NULL, HFILL }},
+		{ &hf_cups_ptype_class,
+			{ "Class", "cups.ptype.class", FT_BOOLEAN, 32,
+			  TFS(&tfs_printer_class), CUPS_PRINTER_CLASS, NULL, HFILL }},
 		{ &hf_cups_state,
 			{ "State",	"cups.state", FT_UINT8, BASE_HEX,
 			  VALS(cups_state_values), 0x0, NULL, HFILL }}

epan/dissectors/packet-dlsw.c
@@ -35,6 +35,7 @@ static int proto_dlsw = -1;
 static int hf_dlsw_flow_control_indication = -1;
 static int hf_dlsw_flow_control_ack = -1;
 static int hf_dlsw_flow_control_operator = -1;
+static int hf_dlsw_flags_explorer_msg = -1;
 
 static gint ett_dlsw = -1;
 static gint ett_dlsw_header = -1;
@@ -297,10 +298,7 @@ dissect_dlsw_pdu(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         flags = tvb_get_guint8(tvb,21);
         ti2 = proto_tree_add_text (dlsw_header_tree,tvb, 21,1,"SSP Flags      = 0x%02x",flags) ;
         dlsw_flags_tree = proto_item_add_subtree(ti2, ett_dlsw_sspflags);
-        proto_tree_add_text (dlsw_flags_tree, tvb, 21, 1, "%s",
-                             decode_boolean_bitfield(flags, 0x80, 8,
-                                                     "Explorer message: yes",
-                                                     "Explorer message: no"));
+        proto_tree_add_item (dlsw_flags_tree, hf_dlsw_flags_explorer_msg, tvb, 21, 1, ENC_BIG_ENDIAN);
         proto_tree_add_text (dlsw_header_tree,tvb, 22,1,"Circuit priority = %s",
                              val_to_str((tvb_get_guint8(tvb,22)&7),dlsw_pri_vals, "Unknown (%d)")) ;
         proto_tree_add_text (dlsw_header_tree,tvb, 23,1,"Old message type = %s (0x%02x)",
@@ -524,6 +522,9 @@ proto_register_dlsw(void)
 	  NULL, HFILL}},
 	{&hf_dlsw_flow_control_operator,
      {"Flow Control Operator", "dlsw.flow_control_operator", FT_UINT8, BASE_DEC, VALS(dlsw_fc_cmd_vals), 0x07,
+	  NULL, HFILL}},
+	{&hf_dlsw_flags_explorer_msg,
+     {"Explorer message", "dlsw.flags.explorer_msg", FT_BOOLEAN, 8, TFS(&tfs_yes_no), 0x80,
 	  NULL, HFILL}},
   };
 

epan/dissectors/packet-netbios.c
@@ -89,6 +89,8 @@ static int hf_netb_ack_expected = -1;
 static int hf_netb_recv_cont_req = -1;
 static int hf_netb_send_no_ack = -1;
 static int hf_netb_version = -1;
+static int hf_netbios_no_receive_flags = -1;
+static int hf_netbios_no_receive_flags_send_no_ack = -1;
 static int hf_netb_largest_frame = -1;
 static int hf_netb_nb_name = -1;
 static int hf_netb_nb_name_type = -1;
@@ -448,19 +450,11 @@ static void netbios_no_receive_flags( tvbuff_t *tvb, proto_tree *tree,
 {
 	proto_tree *field_tree;
 	proto_item *tf;
-	guint flags = tvb_get_guint8( tvb, offset);
 
 		/* decode the flag field for No Receive packet*/
-
-	tf = proto_tree_add_text(tree, tvb, offset, 1,
-			"Flags: 0x%02x", flags);
-
-	if (flags & 0x02) {
-		field_tree = proto_item_add_subtree(tf, ett_netb_flags);
-		proto_tree_add_text(field_tree, tvb, offset, 1, "%s",
-		    decode_boolean_bitfield(flags, 0x02, 8,
-			"SEND.NO.ACK data not received", NULL));
-	}
+	tf = proto_tree_add_item(tree, hf_netbios_no_receive_flags, tvb, offset, 1, ENC_LITTLE_ENDIAN);
+	field_tree = proto_item_add_subtree(tf, ett_netb_flags);
+	proto_tree_add_item(field_tree, hf_netbios_no_receive_flags_send_no_ack, tvb, offset, 1, ENC_LITTLE_ENDIAN);
 }
 
 
@@ -1325,6 +1319,14 @@ void proto_register_netbios(void)
 		{ &hf_netb_version,
 		{ "NetBIOS Version", "netbios.version", FT_BOOLEAN,  8,
 			TFS( &netb_version_str), 0x01, NULL, HFILL }},
+
+		{ &hf_netbios_no_receive_flags,
+		{ "Flags", "netbios.no_receive_flags", FT_UINT8, BASE_HEX, NULL, 0x0,
+			NULL, HFILL }},
+
+		{ &hf_netbios_no_receive_flags_send_no_ack,
+		{ "SEND.NO.ACK data received", "netbios.no_receive_flags.send_no_ack", FT_BOOLEAN,  8,
+			TFS( &tfs_no_yes), 0x02, NULL, HFILL }},
 
 		{ &hf_netb_largest_frame,
 		{ "Largest Frame", "netbios.largest_frame", FT_UINT8, BASE_DEC, VALS(max_frame_size_vals), 0x0E,

epan/dissectors/packet-nlsp.c
@@ -51,6 +51,9 @@ static int hf_nlsp_lsp_p                 = -1;
 static int hf_nlsp_lsp_attached_flag     = -1;
 static int hf_nlsp_lsp_lspdbol           = -1;
 static int hf_nlsp_lsp_router_type       = -1;
+static int hf_nlsp_lsp_link_info_clv_flags_cost_present = -1;
+static int hf_nlsp_lsp_link_info_clv_flags_cost_metric = -1;
+static int hf_nlsp_lsp_link_info_clv_flags_cost = -1;
 
 static gint ett_nlsp                     = -1;
 static gint ett_nlsp_hello_clv_area_addr = -1;
@@ -107,6 +110,8 @@ static const value_string nlsp_router_type_vals[] = {
 	{ 3, "Level 1 and Level 2 Router"},
 	{ 0, NULL}
 };
+
+static const true_false_string tfs_internal_external = { "Internal", "External" };
 
 /*
  * Our sub-packet dismantle structure for CLV's
@@ -671,20 +676,13 @@ dissect_lsp_link_info_clv(tvbuff_t *tvb, proto_tree *tree, int offset,
 	}
 	if (tree) {
 		flags_cost = tvb_get_guint8(tvb, offset);
-		proto_tree_add_text(tree, tvb, offset, 1, "%s",
-		    decode_boolean_bitfield(flags_cost, 0x80, 1*8,
-			"Cost not present", "Cost present"));
+		proto_tree_add_item(tree, hf_nlsp_lsp_link_info_clv_flags_cost_present, tvb, offset, 1, ENC_BIG_ENDIAN);
 		if (!(flags_cost & 0x80)) {
 			/*
 			 * 0x80 clear => cost present.
 			 */
-			proto_tree_add_text(tree, tvb, offset, 1, "%s",
-			    decode_boolean_bitfield(flags_cost, 0x40, 1*8,
-				"Cost is internal metric",
-				"Cost is external metric"));
-			proto_tree_add_text(tree, tvb, offset, 1, "%s",
-			    decode_numeric_bitfield(flags_cost, 0x3F, 1*8,
-				"Cost = %u"));
+			proto_tree_add_item(tree, hf_nlsp_lsp_link_info_clv_flags_cost_metric, tvb, offset, 1, ENC_BIG_ENDIAN);
+			proto_tree_add_item(tree, hf_nlsp_lsp_link_info_clv_flags_cost, tvb, offset, 1, ENC_BIG_ENDIAN);
 		}
 	}
 	offset += 1;
@@ -1622,6 +1620,18 @@ proto_register_nlsp(void)
 	      { "Router Type", "nlsp.lsp.router_type", FT_UINT8, BASE_DEC,
 	        VALS(nlsp_router_type_vals), NLSP_LSP_ROUTER_TYPE_MASK,
 	        NULL, HFILL }},
+
+	    { &hf_nlsp_lsp_link_info_clv_flags_cost_present,
+	      { "Cost present", "nlsp.lsp.link_info_clv.flags.cost_present", FT_BOOLEAN, 8,
+	        TFS(&tfs_no_yes), 0x80, NULL, HFILL }},
+
+	    { &hf_nlsp_lsp_link_info_clv_flags_cost_metric,
+	      { "Cost metric", "nlsp.lsp.link_info_clv.flags.cost_metric", FT_BOOLEAN, 8,
+	        TFS(&tfs_internal_external), 0x40, NULL, HFILL }},
+
+	    { &hf_nlsp_lsp_link_info_clv_flags_cost,
+	      { "Cost", "nlsp.lsp.link_info_clv.flags.cost", FT_UINT8, BASE_DEC,
+	        NULL, 0x3F, NULL, HFILL }},
 	};
 	static gint *ett[] = {
 		&ett_nlsp,

epan/dissectors/packet-pcep.c
@@ -287,6 +287,9 @@ static gint pcep_subobj_flags_lpa= -1;
 static gint pcep_subobj_flags_lpu= -1;
 static gint pcep_subobj_label_flags_gl= -1;
 static gint ett_pcep_obj_unknown = -1;
+static gint hf_pcep_no_path_tlvs_pce = -1;
+static gint hf_pcep_no_path_tlvs_unk_dest = -1;
+static gint hf_pcep_no_path_tlvs_unk_src = -1;
 
 /* PCEP message types.*/
 typedef enum {
@@ -699,27 +702,24 @@ dissect_pcep_tlvs(proto_tree *pcep_obj, tvbuff_t *tvb, int offset, gint length, 
 		proto_tree_add_text(tlv, tvb, offset + 2 + j, 2, "Length: %u", tlv_length);
 		switch (tlv_type)
 		{
-		    case 1:    /* NO-PATH TLV */
-			proto_tree_add_text(tlv, tvb, offset+4+j, tlv_length, "%s",
-					    decode_boolean_bitfield(tvb_get_ntohl(tvb, offset+4+j), 0x0001, 32, "PCE currently unavailable", ""));
-			proto_tree_add_text(tlv, tvb, offset+4+j, tlv_length, "%s",
-					    decode_boolean_bitfield(tvb_get_ntohl(tvb, offset+4+j), 0x0002, 32, "Unknown destination", ""));
-			proto_tree_add_text(tlv, tvb, offset+4+j, tlv_length, "%s",
-					    decode_boolean_bitfield(tvb_get_ntohl(tvb, offset+4+j), 0x0004, 32, "Unknown source", ""));
+			case 1:    /* NO-PATH TLV */
+			proto_tree_add_item(tlv, hf_pcep_no_path_tlvs_pce, tvb, offset+4+j, tlv_length, ENC_BIG_ENDIAN);
+			proto_tree_add_item(tlv, hf_pcep_no_path_tlvs_unk_dest, tvb, offset+4+j, tlv_length, ENC_BIG_ENDIAN);
+			proto_tree_add_item(tlv, hf_pcep_no_path_tlvs_unk_src, tvb, offset+4+j, tlv_length, ENC_BIG_ENDIAN);
 			break;
 
-		    case 3:   /* REQ-MISSING TLV */
+			case 3:   /* REQ-MISSING TLV */
 			proto_tree_add_text(tlv, tvb, offset+4+j, tlv_length, "Request-ID: %u", tvb_get_ntohl(tvb, offset+4+j));
 			break;
 
-		    case 4:   /* OF TLV */
+			case 4:   /* OF TLV */
 			for (i=0; i<tlv_length/2; i++)
 			    proto_tree_add_text(tlv, tvb, offset+4+j+i*2, 2, "OF-Code #%d: %s (%u)",
 						i+1, val_to_str_const(tvb_get_ntohs(tvb, offset+4+j+i*2), pcep_of_vals, "Unknown"),
 						tvb_get_ntohs(tvb, offset+4+j+i*2));
 			break;
 
-		    default:
+			default:
 			proto_tree_add_text(tlv, tvb, offset+4+j, tlv_length, "Data: %s",
 					tvb_bytes_to_str_punct(tvb, (offset) + 4 + j, tlv_length, ' '));
 		}
@@ -2980,6 +2980,15 @@ proto_register_pcep(void){
 		NULL, HFILL }},
 		{&pcep_subobj_label_flags_gl,
 		 { "Global Label", "pcep.subobj.label.flags.gl", FT_BOOLEAN, 8, TFS(&tfs_set_notset), PCEP_SUB_LABEL_GL,
+		NULL, HFILL }},
+		{&hf_pcep_no_path_tlvs_pce,
+		 { "PCE currently unavailable", "pcep.no_path_tlvs.pce", FT_BOOLEAN, 32, TFS(&tfs_true_false), 0x0001,
+		NULL, HFILL }},
+		{&hf_pcep_no_path_tlvs_unk_dest,
+		 { "Unknown destination", "pcep.no_path_tlvs.unk_dest", FT_BOOLEAN, 32, TFS(&tfs_true_false), 0x0002,
+		NULL, HFILL }},
+		{&hf_pcep_no_path_tlvs_unk_src,
+		 { "Unknown source", "pcep.no_path_tlvs.unk_src", FT_BOOLEAN, 32, TFS(&tfs_true_false), 0x0004,
 		NULL, HFILL }},
 	};
 

commit 6f4a70b4014ab74fc4640851c26c717d03e3d6e5

quake: fix header dissection
The flags come before the length, as can be seen in the original source here:

https://github.com/id-Software/Quake/blob/bf4ac424ce754894ac8f1dae6a3981954bc9852d/WinQuake/net_dgrm.c#L360-L362

This seems to be a regression introduced in 4f970746.

epan/dissectors/packet-quake.c
@@ -388,28 +388,28 @@ dissect_quake(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data _U
 	col_set_str(pinfo->cinfo, COL_PROTOCOL, "QUAKE");
 	col_clear(pinfo->cinfo, COL_INFO);
 
-	flags = tvb_get_ntohs(tvb, 2);
+	flags = tvb_get_ntohs(tvb, 0);
 
 	quake_item = proto_tree_add_item(tree, proto_quake, tvb, 0, -1, ENC_NA);
 	quake_tree = proto_item_add_subtree(quake_item, ett_quake);
 
 	flags_item = proto_tree_add_item(quake_tree, hf_quake_header_flags,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	flags_tree = proto_item_add_subtree(flags_item, ett_quake_flags);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_data,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_ack,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_no_ack,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_endmsg,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_unreliable,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 	proto_tree_add_item(flags_tree, hf_quake_header_flags_control,
-			tvb, 2, 2, ENC_BIG_ENDIAN);
+			tvb, 0, 2, ENC_BIG_ENDIAN);
 
-	proto_tree_add_item(quake_tree, hf_quake_header_length, tvb, 0, 2, ENC_BIG_ENDIAN);
+	proto_tree_add_item(quake_tree, hf_quake_header_length, tvb, 2, 2, ENC_BIG_ENDIAN);
 
 	if (flags == NETFLAG_CTL) {
 		next_tvb = tvb_new_subset_remaining(tvb, 4);