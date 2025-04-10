f9dfd57b3391fd3f3152dc2516cb019a7194c21b
Stefan Metzmacher <metze@samba.org>
2025-04-10 16:19:31 +0200

    spnego: handle the IAKerb realm discovery start without any krb5 payload
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- epan/dissectors/packet-spnego.c
+++ epan/dissectors/packet-spnego.c
@@ -898,6 +898,10 @@ dissect_spnego_krb5(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* d
 
     case KRB_TOKEN_IAKERB_PROXY:
       offset = dissect_spnego_IAKERB_HEADER(false, tvb, offset, &asn1_ctx, subtree, -1);
+      len = tvb_captured_length_remaining(tvb, offset);
+      if (len == 0) {
+        break;
+      }
       krb5_tvb = tvb_new_subset_remaining(tvb, offset);
       offset += dissect_kerberos_main(krb5_tvb, pinfo, subtree, false, NULL);
       break;
