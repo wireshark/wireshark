wireshark/wireshark

commit f10ca976ab1e5ad1e959c355c41b037e639efb0a

BGP: Add Source Address to Type 1 Session Transformed Route
See https://www.ietf.org/archive/id/draft-mpmz-bess-mup-safi-03.html#section-3.1.3.1

epan/dissectors/packet-bgp.c
@@ -616,7 +616,7 @@ static dissector_handle_t bgp_handle;
 #define SAFNUM_CT              76  /* draft-kaliraj-idr-bgp-classful-transport-planes-07 */
 #define SAFNUM_FLOWSPEC        77  /* draft-ietf-idr-flowspec-nvo3-13 */
 #define SAFNUM_MCAST_TREE      78  /* draft-ietf-bess-bgp-multicast-03 */
-#define SAFNUM_BGP_MUP         85  /* draft-mpmz-bess-mup-safi-00 */
+#define SAFNUM_BGP_MUP         85  /* draft-mpmz-bess-mup-safi-03 */
 #define SAFNUM_LAB_VPNUNICAST 128  /* RFC4364, RFC8277 */
 #define SAFNUM_LAB_VPNMULCAST 129  /* RFC6513, RFC6514 */
 #define SAFNUM_LAB_VPNUNIMULC 130  /* Obsolete and reserved, see RFC4760 */
@@ -2987,7 +2987,7 @@ static int hf_bgp_ls_igp_te_metric_bandwidth_utilized;
 static int hf_bgp_ls_igp_te_metric_bandwidth_utilized_value;
 static int hf_bgp_ls_igp_te_metric_reserved;
 
-/* draft-mpmz-bess-mup-safi-00 */
+/* draft-mpmz-bess-mup-safi-03 */
 static int hf_bgp_mup_nlri;
 static int hf_bgp_mup_nlri_at;
 static int hf_bgp_mup_nlri_rt;
@@ -3004,6 +3004,9 @@ static int hf_bgp_mup_nlri_3gpp_5g_qfi;
 static int hf_bgp_mup_nlri_3gpp_5g_ep_addr_len;
 static int hf_bgp_mup_nlri_3gpp_5g_ep_ip_addr;
 static int hf_bgp_mup_nlri_3gpp_5g_ep_ipv6_addr;
+static int hf_bgp_mup_nlri_3gpp_5g_source_addr_len;
+static int hf_bgp_mup_nlri_3gpp_5g_source_ip_addr;
+static int hf_bgp_mup_nlri_3gpp_5g_source_ipv6_addr;
 static int hf_bgp_mup_nlri_3gpp_5g_type2_st_route;
 static int hf_bgp_mup_nlri_ep_len;
 static int hf_bgp_mup_nlri_ep_ip_addr;
@@ -7080,6 +7083,7 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, g
 
     proto_item  *item;
     guint8      endpoint_address_length;
+    guint8      source_address_length;
     proto_item  *arch_spec_item;
     proto_tree  *arch_spec_tree;
     int         arch_spec_byte;
@@ -7102,9 +7106,13 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, g
             +-----------------------------------+
             |    Endpoint Address (variable)    |
             +-----------------------------------+
+            |  Source Address Length (1 octet)  |
+            +-----------------------------------+
+            |     Source Address (variable)     |
+            +-----------------------------------+
         */
         endpoint_address_length = tvb_get_guint8(tvb, reader_offset+5); // should be multiple of 8
-        arch_spec_byte = 6 + endpoint_address_length/8;
+        arch_spec_byte = 7 + endpoint_address_length/8;
 
         arch_spec_item = proto_tree_add_item(tree, hf_bgp_mup_nlri_3gpp_5g_type1_st_route, tvb, reader_offset, arch_spec_byte, ENC_NA);
         arch_spec_tree = proto_item_add_subtree(arch_spec_item, ett_bgp_mup_nlri_3gpp_5g_type1_st_route);
@@ -7127,6 +7135,22 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, g
         } else {
             expert_add_info_format(pinfo, arch_spec_tree, &ei_bgp_mup_nlri_addr_len_err,
                                    "Invalid length (%u) of Endpoint Address Length", endpoint_address_length);
+            return -1;
+        }
+        source_address_length = tvb_get_guint8(tvb, reader_offset); // should be zero or multiple of 8
+        if (source_address_length==0) {
+            reader_offset++;
+        } else if (source_address_length==32) {
+            proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ip_addr, tvb, reader_offset, 4, ENC_NA);
+            arch_spec_byte += 4;
+            reader_offset += 4;
+        } else if (source_address_length==128) {
+            proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ipv6_addr, tvb, reader_offset, 16, ENC_NA);
+            arch_spec_byte += 16;
+            reader_offset += 16;
+        } else {
+            expert_add_info_format(pinfo, arch_spec_tree, &ei_bgp_mup_nlri_addr_len_err,
+                                   "Invalid length (%u) of Source Address Length", source_address_length);
             return -1;
         }
         break;
@@ -13943,6 +13967,15 @@ proto_register_bgp(void)
           BASE_NONE, NULL, 0x0, NULL, HFILL}},
       { &hf_bgp_mup_nlri_3gpp_5g_ep_ipv6_addr,
         { "Endpoint Address", "bgp.mup.nlri.3gpp_5g.ep.ipv6_addr", FT_IPv6,
+          BASE_NONE, NULL, 0x0, NULL, HFILL}},
+      { &hf_bgp_mup_nlri_3gpp_5g_source_addr_len,
+        { "Source Address Length", "bgp.mup.nlri.3gpp_5g.source.len", FT_UINT8,
+          BASE_DEC, NULL, 0x0, NULL, HFILL}},
+      { &hf_bgp_mup_nlri_3gpp_5g_source_ip_addr,
+        { "Source Address", "bgp.mup.nlri.3gpp_5g.source.ip_addr", FT_IPv4,
+          BASE_NONE, NULL, 0x0, NULL, HFILL}},
+      { &hf_bgp_mup_nlri_3gpp_5g_source_ipv6_addr,
+        { "Source Address", "bgp.mup.nlri.3gpp_5g.source.ipv6_addr", FT_IPv6,
           BASE_NONE, NULL, 0x0, NULL, HFILL}},
       { &hf_bgp_mup_nlri_ep_len,
         { "Endpoint Length", "bgp.mup.nlri.ep.len", FT_UINT8,

commit 85ff46c76b2a6a4b94094e4f20dfa66b43086182

BGP: fix reader_offset handling for Source Address Length
Fixes: f10ca976ab1e ("BGP: Add Source Address to Type 1 Session Transformed Route")

Signed-off-by: Yuya Kusakabe <y-kusakabe@bbsakura.net>

epan/dissectors/packet-bgp.c
@@ -7579,9 +7579,8 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, i
             return -1;
         }
         source_address_length = tvb_get_uint8(tvb, reader_offset); // should be zero or multiple of 8
-        if (source_address_length==0) {
-            reader_offset++;
-        } else if (source_address_length==32) {
+        reader_offset++;
+        if (source_address_length==32) {
             proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ip_addr, tvb, reader_offset, 4, ENC_BIG_ENDIAN);
             reader_offset += 4;
         } else if (source_address_length==128) {
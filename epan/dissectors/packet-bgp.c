85ff46c76b2a6a4b94094e4f20dfa66b43086182
Yuya Kusakabe <y-kusakabe@bbsakura.net>
2025-04-17 09:00:10 +0900

    BGP: fix reader_offset handling for Source Address Length
    Fixes: f10ca976ab1e ("BGP: Add Source Address to Type 1 Session Transformed Route")
    
    Signed-off-by: Yuya Kusakabe <y-kusakabe@bbsakura.net>

--- epan/dissectors/packet-bgp.c
+++ epan/dissectors/packet-bgp.c
@@ -7579,9 +7579,8 @@ static int decode_bgp_mup_nlri_type1_st_route(proto_tree *tree, tvbuff_t *tvb, i
             return -1;
         }
         source_address_length = tvb_get_uint8(tvb, reader_offset); // should be zero or multiple of 8
-        if (source_address_length==0) {
-            reader_offset++;
-        } else if (source_address_length==32) {
+        reader_offset++;
+        if (source_address_length==32) {
             proto_tree_add_item(arch_spec_tree, hf_bgp_mup_nlri_3gpp_5g_source_ip_addr, tvb, reader_offset, 4, ENC_BIG_ENDIAN);
             reader_offset += 4;
         } else if (source_address_length==128) {
