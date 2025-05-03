b00a6851299371fa5760a1e6e9b53d8e09d6741f
Jaap Keuter <jaap.keuter@xs4all.nl>
2025-05-03 09:06:15 +0200

    LDAP: Don't leave out filter elements
    Commit b501e15fe98 introduced pretty dissection of LDAP search filters.
    In some case this drops non-essential parts of the expression. In order
    to adhere to the mantra: show it as it is, do not exclude these parts.
    
    Closes #20527

--- epan/dissectors/packet-ldap.c
+++ epan/dissectors/packet-ldap.c
@@ -1815,7 +1815,7 @@ dissect_ldap_T_and_item(bool implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_
   if(and_filter_string){
     and_filter_string=wmem_strdup_printf(actx->pinfo->pool, "(&%s%s)",and_filter_string,Filter_string);
   } else {
-    and_filter_string=Filter_string;
+    and_filter_string=wmem_strdup_printf(actx->pinfo->pool, "(&%s)",Filter_string);
   }
 
   return offset;
@@ -1860,7 +1860,7 @@ dissect_ldap_T_or_item(bool implicit_tag _U_, tvbuff_t *tvb _U_, int offset _U_,
   if(or_filter_string){
     or_filter_string=wmem_strdup_printf(actx->pinfo->pool, "(|%s%s)",or_filter_string,Filter_string);
   } else {
-    or_filter_string=Filter_string;
+    or_filter_string=wmem_strdup_printf(actx->pinfo->pool, "(|%s)",Filter_string);
   }
 
 
