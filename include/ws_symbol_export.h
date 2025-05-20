67bb36a181f9e643533bbfc96f52207edb47de0c
Michael Mann <mmann78@netscape.net>
2025-05-20 19:37:12 +0000

    Cmake: ENABLE_STATIC -> BUILD_SHARED_LIBS

--- include/ws_symbol_export.h
+++ include/ws_symbol_export.h
@@ -91,7 +91,7 @@
      * be necessary, e.g. if what's declared is an array whose size is
      * not given in the declaration.
      */
-    #ifdef ENABLE_STATIC
+    #ifdef BUILD_SHARED_LIBS
       /*
        * We're building all-static, so we're not building any DLLs.
        */
@@ -99,7 +99,7 @@
     #elif defined(__GNUC__)
       /* GCC */
       #define WS_DLL_PUBLIC_DEF __attribute__ ((dllimport))
-    #else /* ! ENABLE_STATIC && ! __GNUC__ */
+    #else /* ! BUILD_SHARED_LIBS && ! __GNUC__ */
       /*
        * Presumably MSVC, and we're not building all-static.
        * Note: actually gcc seems to also support this syntax.
