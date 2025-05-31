2101ffd5e0d39ed2a70954c800d280a7a7d67320
Martin Mathieson <martin.r.mathieson@googlemail.com>
2025-05-31 12:42:36 +0100

    USB-HID:  Fix incorrect encoding change in previous commit
    See https://gitlab.com/wireshark/wireshark/-/merge_requests/20021#note_2535306682

--- epan/dissectors/packet-usb-hid.c
+++ epan/dissectors/packet-usb-hid.c
@@ -5242,7 +5242,7 @@ dissect_hid_variable(tvbuff_t* tvb, packet_info _U_* pinfo, proto_tree* tree, hi
 
     /* vendor data (0xff00 - 0xffff) */
     if ((USAGE_PAGE(usage) & 0xff00) == 0xff00) {
-        proto_tree_add_bits_item(tree, hf_usbhid_vendor_data, tvb, bit_offset, field->report_size, ENC_NA);
+        proto_tree_add_bits_item(tree, hf_usbhid_vendor_data, tvb, bit_offset, field->report_size, ENC_LITTLE_ENDIAN);
         return;
     }
 
