wireshark/wireshark

commit 6738a873b3541e2a45bd735402aede2fbb58a9d2

Qt: Store Show Packet Bytes settings in recent
Store the Show Packt Bytes "Decode As" and "Show As" settings in
recent.

The options for "Show as" for Show Packet Bytes are a superset of the
"Show data as" for Follow Stream, and they're conceptually doing
the same thing. Use the same enum for both - Follow Stream doesn't
support all these options, but maybe it should in the future.

To do this, we break the strict correspondance between the index
in the respective comboboxes and the value of the enum. Use
findData when setting the current index so that this doesn't matter;
this also keeps us from having to be as strict about keeping new
entries in alphabetical order.

Fix #17796

ui/qt/show_packet_bytes_dialog.cpp
@@ -13,6 +13,7 @@
 #include "main_window.h"
 #include "main_application.h"
 #include "ui/qt/widgets/wireshark_file_dialog.h"
+#include "ui/recent.h"
 
 #include "epan/strutil.h"
 
@@ -34,12 +35,13 @@
 // - Use ByteViewText to ShowAsHexDump and supplementary view for custom protocol
 // - Handle large data blocks
 
+Q_DECLARE_METATYPE(bytes_show_type)
+Q_DECLARE_METATYPE(bytes_decode_type)
+
 ShowPacketBytesDialog::ShowPacketBytesDialog(QWidget &parent, CaptureFile &cf) :
     WiresharkDialog(parent, cf),
     ui(new Ui::ShowPacketBytesDialog),
     finfo_(cf.capFile()->finfo_selected),
-    decode_as_(DecodeAsNone),
-    show_as_(ShowAsASCII),
     use_regex_find_(false)
 {
     ui->setupUi(this);
@@ -65,23 +67,24 @@ ShowPacketBytesDialog::ShowPacketBytesDialog(QWidget &parent, CaptureFile &cf) :
     ui->cbDecodeAs->addItem(tr("Percent-Encoding"), DecodeAsPercentEncoding);
     ui->cbDecodeAs->addItem(tr("Quoted-Printable"), DecodeAsQuotedPrintable);
     ui->cbDecodeAs->addItem(tr("ROT13"), DecodeAsROT13);
+    ui->cbDecodeAs->setCurrentIndex(ui->cbDecodeAs->findData(recent.gui_show_bytes_decode));
     ui->cbDecodeAs->blockSignals(false);
 
     ui->cbShowAs->blockSignals(true);
-    ui->cbShowAs->addItem(tr("ASCII"), ShowAsASCII);
-    ui->cbShowAs->addItem(tr("ASCII & Control"), ShowAsASCIIandControl);
-    ui->cbShowAs->addItem(tr("C Array"), ShowAsCArray);
-    ui->cbShowAs->addItem(tr("EBCDIC"), ShowAsEBCDIC);
-    ui->cbShowAs->addItem(tr("Hex Dump"), ShowAsHexDump);
-    ui->cbShowAs->addItem(tr("HTML"), ShowAsHTML);
-    ui->cbShowAs->addItem(tr("Image"), ShowAsImage);
-    ui->cbShowAs->addItem(tr("JSON"), ShowAsJson);
-    ui->cbShowAs->addItem(tr("Raw"), ShowAsRAW);
-    ui->cbShowAs->addItem(tr("Rust Array"), ShowAsRustArray);
+    ui->cbShowAs->addItem(tr("ASCII"), SHOW_ASCII);
+    ui->cbShowAs->addItem(tr("ASCII & Control"), SHOW_ASCII_CONTROL);
+    ui->cbShowAs->addItem(tr("C Array"), SHOW_CARRAY);
+    ui->cbShowAs->addItem(tr("EBCDIC"), SHOW_EBCDIC);
+    ui->cbShowAs->addItem(tr("Hex Dump"), SHOW_HEXDUMP);
+    ui->cbShowAs->addItem(tr("HTML"), SHOW_HTML);
+    ui->cbShowAs->addItem(tr("Image"), SHOW_IMAGE);
+    ui->cbShowAs->addItem(tr("JSON"), SHOW_JSON);
+    ui->cbShowAs->addItem(tr("Raw"), SHOW_RAW);
+    ui->cbShowAs->addItem(tr("Rust Array"), SHOW_RUSTARRAY);
     // UTF-8 is guaranteed to exist as a QTextCodec
-    ui->cbShowAs->addItem(tr("UTF-8"), ShowAsCodec);
-    ui->cbShowAs->addItem(tr("YAML"), ShowAsYAML);
-    ui->cbShowAs->setCurrentIndex(show_as_);
+    ui->cbShowAs->addItem(tr("UTF-8"), SHOW_CODEC);
+    ui->cbShowAs->addItem(tr("YAML"), SHOW_YAML);
+    ui->cbShowAs->setCurrentIndex(ui->cbShowAs->findData(recent.gui_show_bytes_show));
     ui->cbShowAs->blockSignals(false);
 
     ui->sbStart->setMinimum(0);
@@ -116,7 +119,7 @@ void ShowPacketBytesDialog::addCodecs(const QMap<QString, QTextCodec *> &codecMa
     for (const auto &codec : qAsConst(codecMap)) {
         // This is already placed in the menu and handled separately
         if (codec->name() != "US-ASCII" && codec->name() != "UTF-8")
-            ui->cbShowAs->addItem(tr(codec->name()), ShowAsCodec);
+            ui->cbShowAs->addItem(tr(codec->name()), SHOW_CODEC);
     }
     ui->cbShowAs->blockSignals(false);
 }
@@ -127,7 +130,7 @@ void ShowPacketBytesDialog::showSelected(int start, int end)
         // end set to -1 means show all packet bytes
         setStartAndEnd(0, (finfo_->length - 1));
     } else {
-        if (show_as_ == ShowAsRAW) {
+        if (recent.gui_show_bytes_show == SHOW_RAW) {
             start /= 2;
             end = (end + 1) / 2;
         }
@@ -160,12 +163,12 @@ bool ShowPacketBytesDialog::enableShowSelected()
     // - DecodeAs must not alter the number of bytes in the buffer
     // - ShowAs must show all bytes in the buffer
 
-    return (((decode_as_ == DecodeAsNone) ||
-             (decode_as_ == DecodeAsROT13)) &&
-            ((show_as_ == ShowAsASCII) ||
-             (show_as_ == ShowAsASCIIandControl) ||
-             (show_as_ == ShowAsEBCDIC) ||
-             (show_as_ == ShowAsRAW)));
+    return (((recent.gui_show_bytes_decode == DecodeAsNone) ||
+             (recent.gui_show_bytes_decode == DecodeAsROT13)) &&
+            ((recent.gui_show_bytes_show == SHOW_ASCII) ||
+             (recent.gui_show_bytes_show == SHOW_ASCII_CONTROL) ||
+             (recent.gui_show_bytes_show == SHOW_EBCDIC) ||
+             (recent.gui_show_bytes_show == SHOW_RAW)));
 }
 
 void ShowPacketBytesDialog::updateWidgets()
@@ -207,7 +210,7 @@ void ShowPacketBytesDialog::on_sbEnd_valueChanged(int value)
 void ShowPacketBytesDialog::on_cbDecodeAs_currentIndexChanged(int idx)
 {
     if (idx < 0) return;
-    decode_as_ = static_cast<DecodeAsType>(ui->cbDecodeAs->itemData(idx).toInt());
+    recent.gui_show_bytes_decode = ui->cbDecodeAs->currentData().value<bytes_decode_type>();
 
     ui->tePacketBytes->setShowSelectedEnabled(enableShowSelected());
 
@@ -217,7 +220,7 @@ void ShowPacketBytesDialog::on_cbDecodeAs_currentIndexChanged(int idx)
 void ShowPacketBytesDialog::on_cbShowAs_currentIndexChanged(int idx)
 {
     if (idx < 0) return;
-    show_as_ = static_cast<ShowAsType>(ui->cbShowAs->itemData(idx).toInt());
+    recent.gui_show_bytes_show = ui->cbShowAs->currentData().value<bytes_show_type>();
 
     ui->tePacketBytes->setShowSelectedEnabled(enableShowSelected());
     ui->lFind->setEnabled(true);
@@ -275,9 +278,9 @@ void ShowPacketBytesDialog::printBytes()
 
 void ShowPacketBytesDialog::copyBytes()
 {
-    switch (show_as_) {
+    switch (recent.gui_show_bytes_show) {
 
-    case ShowAsASCII:
+    case SHOW_ASCII:
     {
         QByteArray ba(field_bytes_);
         sanitizeBuffer(ba, true);
@@ -285,26 +288,26 @@ void ShowPacketBytesDialog::copyBytes()
         break;
     }
 
-    case ShowAsASCIIandControl:
-    case ShowAsCArray:
-    case ShowAsRustArray:
-    case ShowAsEBCDIC:
-    case ShowAsHexDump:
-    case ShowAsJson:
-    case ShowAsRAW:
-    case ShowAsYAML:
+    case SHOW_ASCII_CONTROL:
+    case SHOW_CARRAY:
+    case SHOW_RUSTARRAY:
+    case SHOW_EBCDIC:
+    case SHOW_HEXDUMP:
+    case SHOW_JSON:
+    case SHOW_RAW:
+    case SHOW_YAML:
         mainApp->clipboard()->setText(ui->tePacketBytes->toPlainText());
         break;
 
-    case ShowAsHTML:
+    case SHOW_HTML:
         mainApp->clipboard()->setText(ui->tePacketBytes->toHtml());
         break;
 
-    case ShowAsImage:
+    case SHOW_IMAGE:
         mainApp->clipboard()->setImage(image_);
         break;
 
-    case ShowAsCodec:
+    case SHOW_CODEC:
         mainApp->clipboard()->setText(ui->tePacketBytes->toPlainText().toUtf8());
         break;
     }
@@ -318,17 +321,18 @@ void ShowPacketBytesDialog::saveAs()
         return;
 
     QFile::OpenMode open_mode = QFile::WriteOnly;
-    switch (show_as_) {
-    case ShowAsASCII:
-    case ShowAsASCIIandControl:
-    case ShowAsCArray:
-    case ShowAsRustArray:
+    switch (recent.gui_show_bytes_show) {
+    case SHOW_ASCII:
+    case SHOW_ASCII_CONTROL:
+    case SHOW_CARRAY:
+    case SHOW_RUSTARRAY:
+    case SHOW_EBCDIC:
     // We always save as UTF-8, so set text mode as we would for UTF-8
-    case ShowAsCodec:
-    case ShowAsHexDump:
-    case ShowAsJson:
-    case ShowAsYAML:
-    case ShowAsHTML:
+    case SHOW_CODEC:
+    case SHOW_HEXDUMP:
+    case SHOW_JSON:
+    case SHOW_YAML:
+    case SHOW_HTML:
         open_mode |= QFile::Text;
     default:
         break;
@@ -337,9 +341,9 @@ void ShowPacketBytesDialog::saveAs()
     QFile file(file_name);
     file.open(open_mode);
 
-    switch (show_as_) {
+    switch (recent.gui_show_bytes_show) {
 
-    case ShowAsASCII:
+    case SHOW_ASCII:
     {
         QByteArray ba(field_bytes_);
         sanitizeBuffer(ba, true);
@@ -347,35 +351,35 @@ void ShowPacketBytesDialog::saveAs()
         break;
     }
 
-    case ShowAsASCIIandControl:
-    case ShowAsCArray:
-    case ShowAsRustArray:
-    case ShowAsEBCDIC:
-    case ShowAsHexDump:
-    case ShowAsJson:
-    case ShowAsYAML:
+    case SHOW_ASCII_CONTROL:
+    case SHOW_CARRAY:
+    case SHOW_RUSTARRAY:
+    case SHOW_EBCDIC:
+    case SHOW_HEXDUMP:
+    case SHOW_JSON:
+    case SHOW_YAML:
     {
         QTextStream out(&file);
         out << ui->tePacketBytes->toPlainText();
         break;
     }
 
-    case ShowAsHTML:
+    case SHOW_HTML:
     {
         QTextStream out(&file);
         out << ui->tePacketBytes->toHtml();
         break;
     }
 
-    case ShowAsCodec:
+    case SHOW_CODEC:
     {
         QTextStream out(&file);
         out << ui->tePacketBytes->toPlainText().toUtf8();
         break;
     }
 
-    case ShowAsImage:
-    case ShowAsRAW:
+    case SHOW_IMAGE:
+    case SHOW_RAW:
         file.write(field_bytes_);
         break;
     }
@@ -554,7 +558,7 @@ void ShowPacketBytesDialog::updateFieldBytes(bool initialization)
     if (!finfo_->ds_tvb)
         return;
 
-    switch (decode_as_) {
+    switch (recent.gui_show_bytes_decode) {
 
     case DecodeAsNone:
         bytes = tvb_get_ptr(finfo_->ds_tvb, start, -1);
@@ -624,9 +628,9 @@ void ShowPacketBytesDialog::updateFieldBytes(bool initialization)
 
     // Try loading as image at startup
     if (initialization && image_.loadFromData(field_bytes_)) {
-        show_as_ = ShowAsImage;
+        recent.gui_show_bytes_show = SHOW_IMAGE;
         ui->cbShowAs->blockSignals(true);
-        ui->cbShowAs->setCurrentIndex(ShowAsImage);
+        ui->cbDecodeAs->setCurrentIndex(ui->cbDecodeAs->findData(SHOW_IMAGE));
         ui->cbShowAs->blockSignals(false);
     }
 
@@ -640,9 +644,9 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
     ui->tePacketBytes->clear();
     ui->tePacketBytes->setCurrentFont(mainApp->monospaceFont());
 
-    switch (show_as_) {
+    switch (recent.gui_show_bytes_show) {
 
-    case ShowAsASCII:
+    case SHOW_ASCII:
     {
         QByteArray ba(field_bytes_);
         sanitizeBuffer(ba, false);
@@ -651,7 +655,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsASCIIandControl:
+    case SHOW_ASCII_CONTROL:
     {
         QByteArray ba(field_bytes_);
         symbolizeBuffer(ba);
@@ -660,7 +664,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsCArray:
+    case SHOW_CARRAY:
     {
         int pos = 0, len = static_cast<int>(field_bytes_.length());
         QString text("char packet_bytes[] = {\n");
@@ -697,7 +701,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsRustArray:
+    case SHOW_RUSTARRAY:
     {
         int pos = 0, len = static_cast<int>(field_bytes_.length());
         QString text("let packet_bytes: [u8; _] = [\n");
@@ -734,7 +738,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsCodec:
+    case SHOW_CODEC:
     {
         // The QTextCodecs docs say that there's a flag to cause invalid
         // characters to be replaced with null. It's unclear what happens
@@ -748,7 +752,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsEBCDIC:
+    case SHOW_EBCDIC:
     {
         QByteArray ba(field_bytes_);
         EBCDIC_to_ASCII((guint8*)ba.data(), static_cast<int>(ba.length()));
@@ -758,7 +762,7 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsHexDump:
+    case SHOW_HEXDUMP:
     {
         int pos = 0, len = static_cast<int>(field_bytes_.length());
         // Use 16-bit offset if there are <= 65536 bytes, 32-bit offset if there are more
@@ -810,12 +814,12 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsHTML:
+    case SHOW_HTML:
         ui->tePacketBytes->setLineWrapMode(QTextEdit::WidgetWidth);
         ui->tePacketBytes->setHtml(field_bytes_);
         break;
 
-    case ShowAsImage:
+    case SHOW_IMAGE:
     {
         ui->lFind->setEnabled(false);
         ui->leFind->setEnabled(false);
@@ -832,12 +836,12 @@ void ShowPacketBytesDialog::updatePacketBytes(void)
         break;
     }
 
-    case ShowAsJson:
+    case SHOW_JSON:
         ui->tePacketBytes->setLineWrapMode(QTextEdit::NoWrap);
         ui->tePacketBytes->setPlainText(QJsonDocument::fromJson(field_bytes_).toJson());
         break;
 
-    case ShowAsYAML:
+    case SHOW_YAML:
     {
         const int base64_raw_len = 57; // Encodes to 76 bytes, common in RFCs
         int pos = 0, len = static_cast<int>(field_bytes_.length());
@@ -864,7 +868,7 @@ DIAG_ON(stringop-overread)
         break;
     }
 
-    case ShowAsRAW:
+    case SHOW_RAW:
         ui->tePacketBytes->setLineWrapMode(QTextEdit::WidgetWidth);
         ui->tePacketBytes->setPlainText(field_bytes_.toHex());
         break;

ui/qt/follow_stream_dialog.cpp
@@ -65,6 +65,8 @@ static QMutex loop_break_mutex;
 // Indicates that a Follow Stream is currently running
 static gboolean isReadRunning;
 
+Q_DECLARE_METATYPE(bytes_show_type)
+
 FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id) :
     WiresharkDialog(parent, cf),
     ui(new Ui::FollowStreamDialog),
@@ -111,7 +113,7 @@ FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, int pro
     // UTF-8 is guaranteed to exist as a QTextCodec
     cbcs->addItem(tr("UTF-8"), SHOW_CODEC);
     cbcs->addItem(tr("YAML"), SHOW_YAML);
-    cbcs->setCurrentIndex(static_cast<int>(recent.gui_follow_show));
+    cbcs->setCurrentIndex(cbcs->findData(recent.gui_follow_show));
     cbcs->blockSignals(false);
 
     b_filter_out_ = ui->buttonBox->addButton(tr("Filter Out This Stream"), QDialogButtonBox::ActionRole);
@@ -359,7 +361,7 @@ void FollowStreamDialog::on_cbDirections_currentIndexChanged(int idx)
 void FollowStreamDialog::on_cbCharset_currentIndexChanged(int idx)
 {
     if (idx < 0) return;
-    recent.gui_follow_show = static_cast<follow_show_type>(ui->cbCharset->itemData(idx).toInt());
+    recent.gui_follow_show = ui->cbCharset->currentData().value<bytes_show_type>();
     readStream();
 }
 
@@ -901,6 +903,12 @@ DIAG_ON(stringop-overread)
         addText(ba, is_from_server, packet_num);
         break;
     }
+
+    default:
+        /* The other Show types are supported in Show Packet Bytes but
+         * not here in Follow. (XXX: Maybe some could be added?)
+         */
+        ws_assert_not_reached();
     }
 
     if (last_packet_ == 0) {

ui/qt/show_packet_bytes_dialog.h
@@ -65,33 +65,6 @@ private slots:
     void saveAs();
 
 private:
-    /* Keep these enums in the same order (generally alphabetically)
-     * that they are added to the combo boxes.
-     */
-    enum DecodeAsType {
-        DecodeAsNone,
-        DecodeAsBASE64,
-        DecodeAsCompressed,
-        DecodeAsHexDigits,
-        DecodeAsPercentEncoding,
-        DecodeAsQuotedPrintable,
-        DecodeAsROT13
-    };
-    enum ShowAsType {
-        ShowAsASCII,
-        ShowAsASCIIandControl,
-        ShowAsCArray,
-        ShowAsEBCDIC,
-        ShowAsHexDump,
-        ShowAsHTML,
-        ShowAsImage,
-        ShowAsJson,
-        ShowAsRAW,
-        ShowAsRustArray,
-        ShowAsCodec, // Ordered to match the UTF-8 combobox index
-        ShowAsYAML,
-    };
-
     void setStartAndEnd(int start, int end);
     bool enableShowSelected();
     void updateWidgets(); // Needed for WiresharkDialog?
@@ -111,8 +84,6 @@ private:
     QPushButton *print_button_;
     QPushButton *copy_button_;
     QPushButton *save_as_button_;
-    DecodeAsType decode_as_;
-    ShowAsType  show_as_;
     bool        use_regex_find_;
     int         start_;
     int         end_;

ui/recent.c
@@ -76,6 +76,8 @@
 #define RECENT_GUI_SEARCH_CASE_SENSITIVE        "gui.search_case_sensitive"
 #define RECENT_GUI_SEARCH_TYPE                  "gui.search_type"
 #define RECENT_GUI_FOLLOW_SHOW                  "gui.follow_show"
+#define RECENT_GUI_SHOW_BYTES_DECODE            "gui.show_bytes_decode"
+#define RECENT_GUI_SHOW_BYTES_SHOW              "gui.show_bytes_show"
 
 #define RECENT_GUI_GEOMETRY                   "gui.geom."
 
@@ -165,14 +167,31 @@ static const value_string search_type_values[] = {
     { 0, NULL }
 };
 
-static const value_string follow_show_values[] = {
-    { SHOW_ASCII,   "ASCII" },
-    { SHOW_CARRAY,  "C_ARRAYS" },
-    { SHOW_EBCDIC,  "EBCDIC" },
-    { SHOW_HEXDUMP, "HEX_DUMP" },
-    { SHOW_RAW,     "RAW" },
-    { SHOW_CODEC,   "UTF-8" },
-    { SHOW_YAML,    "YAML"},
+static const value_string bytes_show_values[] = {
+    { SHOW_ASCII,         "ASCII" },
+    { SHOW_ASCII_CONTROL, "ASCII_CONTROL" },
+    { SHOW_CARRAY,        "C_ARRAYS" },
+    { SHOW_EBCDIC,        "EBCDIC" },
+    { SHOW_HEXDUMP,       "HEX_DUMP" },
+    { SHOW_HTML,          "HTML" },
+    { SHOW_IMAGE,         "IMAGE" },
+    { SHOW_JSON,          "JSON" },
+    { SHOW_RAW,           "RAW" },
+    { SHOW_RUSTARRAY,     "RUST_ARRAY" },
+    { SHOW_CODEC,         "UTF-8" },
+    // Other codecs are generated at runtime
+    { SHOW_YAML,          "YAML"},
+    { 0, NULL }
+};
+
+static const value_string show_bytes_decode_values[] = {
+    { DecodeAsNone,            "NONE" },
+    { DecodeAsBASE64,          "BASE64" },
+    { DecodeAsCompressed,      "COMPRESSED" },
+    { DecodeAsHexDigits,       "HEX_DIGITS" },
+    { DecodeAsPercentEncoding, "PERCENT_ENCODING" },
+    { DecodeAsQuotedPrintable, "QUOTED_PRINTABLE" },
+    { DecodeAsROT13,           "ROT13"},
     { 0, NULL }
 };
 
@@ -955,8 +974,16 @@ write_profile_recent(void)
             recent.gui_allow_hover_selection);
 
     write_recent_enum(rf, "Follow stream show as",
-            RECENT_GUI_FOLLOW_SHOW, follow_show_values,
+            RECENT_GUI_FOLLOW_SHOW, bytes_show_values,
             recent.gui_follow_show);
+
+    write_recent_enum(rf, "Show packet bytes decode as",
+            RECENT_GUI_SHOW_BYTES_DECODE, show_bytes_decode_values,
+            recent.gui_show_bytes_decode);
+
+    write_recent_enum(rf, "Show packet bytes show as",
+            RECENT_GUI_SHOW_BYTES_SHOW, bytes_show_values,
+            recent.gui_show_bytes_show);
 
     fprintf(rf, "\n# Main window upper (or leftmost) pane size.\n");
     fprintf(rf, "# Decimal number.\n");
@@ -1185,7 +1212,11 @@ read_set_recent_pair_static(gchar *key, const gchar *value,
     } else if (strcmp(key, RECENT_GUI_ALLOW_HOVER_SELECTION) == 0) {
         parse_recent_boolean(value, &recent.gui_allow_hover_selection);
     } else if (strcmp(key, RECENT_GUI_FOLLOW_SHOW) == 0) {
-        recent.gui_follow_show = (follow_show_type)str_to_val(value, follow_show_values, SHOW_ASCII);
+        recent.gui_follow_show = (bytes_show_type)str_to_val(value, bytes_show_values, SHOW_ASCII);
+    } else if (strcmp(key, RECENT_GUI_SHOW_BYTES_DECODE) == 0) {
+        recent.gui_show_bytes_decode = (bytes_decode_type)str_to_val(value, show_bytes_decode_values, DecodeAsNone);
+    } else if (strcmp(key, RECENT_GUI_SHOW_BYTES_SHOW) == 0) {
+        recent.gui_show_bytes_show = (bytes_show_type)str_to_val(value, bytes_show_values, SHOW_ASCII);
     } else if (strcmp(key, RECENT_GUI_GEOMETRY_MAIN_MAXIMIZED) == 0) {
         parse_recent_boolean(value, &recent.gui_geometry_main_maximized);
     } else if (strcmp(key, RECENT_GUI_GEOMETRY_MAIN_UPPER_PANE) == 0) {
@@ -1451,6 +1482,8 @@ recent_read_profile_static(char **rf_path_return, int *rf_errno_return)
     recent.gui_bytes_encoding        = BYTES_ENC_FROM_PACKET;
     recent.gui_allow_hover_selection = TRUE;
     recent.gui_follow_show           = SHOW_ASCII;
+    recent.gui_show_bytes_decode     = DecodeAsNone;
+    recent.gui_show_bytes_show       = SHOW_ASCII;
 
     /* pane size of zero will autodetect */
     recent.gui_geometry_main_upper_pane   = 0;

ui/recent.h
@@ -87,13 +87,29 @@ typedef enum {
 
 typedef enum {
     SHOW_ASCII,
+    SHOW_ASCII_CONTROL,
     SHOW_CARRAY,
     SHOW_EBCDIC,
     SHOW_HEXDUMP,
+    SHOW_HTML,
+    SHOW_IMAGE,
+    SHOW_JSON,
     SHOW_RAW,
-    SHOW_CODEC, // Ordered to match UTF-8 combobox index
+    SHOW_RUSTARRAY,
+    SHOW_CODEC, // Will map to UTF-8 in the combobox (other codecs
+                // are generated at runtime).
     SHOW_YAML
-} follow_show_type;
+} bytes_show_type;
+
+typedef enum {
+    DecodeAsNone,
+    DecodeAsBASE64,
+    DecodeAsCompressed,
+    DecodeAsHexDigits,
+    DecodeAsPercentEncoding,
+    DecodeAsQuotedPrintable,
+    DecodeAsROT13
+} bytes_decode_type;
 
 /** Recent settings. */
 typedef struct recent_settings_tag {
@@ -120,7 +136,9 @@ typedef struct recent_settings_tag {
     search_char_set_type gui_search_char_set;
     gboolean    gui_search_case_sensitive;
     search_type_type gui_search_type;
-    follow_show_type gui_follow_show;
+    bytes_show_type gui_follow_show;
+    bytes_decode_type gui_show_bytes_decode;
+    bytes_show_type gui_show_bytes_show;
 
     gint        gui_geometry_main_x;
     gint        gui_geometry_main_y;

commit c0e59c5d62352cefdc6cad1eba08f077084f8f61

Qt: Initialize show selected enabled
The Show Selected menu item only works when showing all bytes.
Initialize this setting after reading settings in recent.

This issue was introduced in commit 6738a873b3.

ui/qt/show_packet_bytes_dialog.cpp
@@ -90,6 +90,8 @@ ShowPacketBytesDialog::ShowPacketBytesDialog(QWidget &parent, CaptureFile &cf) :
     ui->sbStart->setMinimum(0);
     ui->sbEnd->setMaximum(finfo_->length - 1);
 
+    ui->tePacketBytes->setShowSelectedEnabled(enableShowSelected());
+
     print_button_ = ui->buttonBox->addButton(tr("Print"), QDialogButtonBox::ActionRole);
     connect(print_button_, SIGNAL(clicked()), this, SLOT(printBytes()));
 