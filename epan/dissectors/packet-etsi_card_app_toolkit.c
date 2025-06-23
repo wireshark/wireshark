wireshark/wireshark

commit e398c8ed6ba1a79c95428a974dfedd4480488fbe

ETSI CAT: upgrade dissector to Release 12
Change-Id: Id1988551ca94a862195eb1de1de399e428132b21
Reviewed-on: https://code.wireshark.org/review/2855
Reviewed-by: Pascal Quantin <pascal.quantin@gmail.com>

epan/dissectors/packet-etsi_card_app_toolkit.c
@@ -1,6 +1,6 @@
 /* packet-card_app_toolkit
  * Routines for packet dissection of
- *	ETSI TS 102 223 v10.0.0  (Release 10 / 2010-10)
+ *	ETSI TS 102 223 v12.0.0  (Release 12 / 2014-05)
  *	3GPP TS 11.14 v8.17.0 (Release 1999 / 2004-09)
  *	3GPP TS 31.111 v9.7.0 (Release 9 / 2012-03)
  * Copyright 2010-2011 by Harald Welte <laforge@gnumonks.org>
@@ -125,6 +125,9 @@ static int hf_ctlv_other_address_coding = -1;
 static int hf_ctlv_other_address_ipv4 = -1;
 static int hf_ctlv_other_address_ipv6 = -1;
 static int hf_ctlv_access_tech = -1;
+static int hf_ctlv_dns_server_address_coding = -1;
+static int hf_ctlv_dns_server_address_ipv4 = -1;
+static int hf_ctlv_dns_server_address_ipv6 = -1;
 static int hf_ctlv_utran_eutran_meas_qual = -1;
 static int hf_ctlv_upd_attach_type = -1;
 static int hf_ctlv_loci_lac = -1;
@@ -133,6 +136,8 @@ static int hf_ctlv_loci_ext_cell_id = -1;
 static int hf_ctlv_iari = -1;
 static int hf_ctlv_impu = -1;
 static int hf_ctlv_ims_status_code = -1;
+static int hf_ctlv_broadcast_nw_tech = -1;
+static int hf_ctlv_broadcast_nw_loc_info = -1;
 
 static int ett_cat = -1;
 static int ett_elem = -1;
@@ -187,9 +192,9 @@ static const value_string comp_tlv_tag_vals[] = {
 	{ 0x1e, "Icon identifier" },
 	{ 0x1f, "Item Icon identifier list" },
 	{ 0x20, "Card reader status" },
-	{ 0x21, "Card ATR" },
-	{ 0x22, "C-APDU" },
-	{ 0x23, "R-APDU" },
+	{ 0x21, "Card ATR / eCAT sequence number" },
+	{ 0x22, "C-APDU / Encrypted TLV list" },
+	{ 0x23, "R-APDU / SA template" },
 	{ 0x24, "Timer identifier" },
 	{ 0x25, "Timer value" },
 	{ 0x26, "Date-Time and Time zone" },
@@ -203,7 +208,7 @@ static const value_string comp_tlv_tag_vals[] = {
 	{ 0x2e, "GSM Timing Advance" },
 	{ 0x2f, "AID" },
 	{ 0x30, "Browser Identity" },
-	{ 0x31, "URL" },
+	{ 0x31, "URL / URI" },
 	{ 0x32, "Bearer" },
 	{ 0x33, "Provisioning Reference File" },
 	{ 0x34, "Browser Termination Cause" },
@@ -212,12 +217,12 @@ static const value_string comp_tlv_tag_vals[] = {
 	{ 0x37, "Channel data length" },
 	{ 0x38, "Channel status" },
 	{ 0x39, "Buffer size" },
-	{ 0x3a, "Card reader identifier" },
+	{ 0x3a, "Card reader identifier / REFRESH Enforcement Policy" },
 	{ 0x3b, "File Update Information" },
 	{ 0x3c, "UICC/terminal interface transport level" },
 	{ 0x3e, "Other address (data destination address)" },
 	{ 0x3f, "Access Technology" },
-	{ 0x40, "Display parameters" },
+	{ 0x40, "Display parameters / DNS server address" },
 	{ 0x41, "Service Record" },
 	{ 0x42, "Device Filter" },
 	{ 0x43, "Service Search" },
@@ -237,6 +242,8 @@ static const value_string comp_tlv_tag_vals[] = {
 	{ 0x55, "3GPP CSG cell selection status" },
 	{ 0x56, "3GPP CSG ID" },
 	{ 0x57, "3GPP HNB name" },
+	{ 0x60, "MAC" },
+	{ 0x61, "Emergency Call Object" },
 	{ 0x62, "IMEISV" },
 	{ 0x63, "Battery state" },
 	{ 0x64, "Browsing status" },
@@ -305,7 +312,7 @@ static const value_string cmd_qual_loci_vals[] = {
 	{ 0x0a, "Charge State of the Battery" },
 	{ 0x0b, "MEID of the terminal" },
 	{ 0x0c, "Current WSID" },
-	{ 0x0d, "Broadcast Network information according tocurrent Broadcast Network Technology used" },
+	{ 0x0d, "Broadcast Network information according to current Broadcast Network Technology used" },
 	{ 0x0e, "Multiple Access Technologies" },
 	{ 0x0f, "Location Information for multiple access technologies" },
 	{ 0x10, "Network Measurement results for multiple access technologies" },
@@ -348,6 +355,21 @@ static const value_string dev_id_vals[] = {
 	{ 0x25, "Channel ID 5" },
 	{ 0x26, "Channel ID 6" },
 	{ 0x27, "Channel ID 7" },
+	{ 0x31, "eCAT ID 1" },
+	{ 0x32, "eCAT ID 2" },
+	{ 0x33, "eCAT ID 3" },
+	{ 0x34, "eCAT ID 4" },
+	{ 0x35, "eCAT ID 5" },
+	{ 0x36, "eCAT ID 6" },
+	{ 0x37, "eCAT ID 7" },
+	{ 0x38, "eCAT ID 8" },
+	{ 0x39, "eCAT ID 9" },
+	{ 0x3a, "eCAT ID 10" },
+	{ 0x3b, "eCAT ID 11" },
+	{ 0x3c, "eCAT ID 12" },
+	{ 0x3d, "eCAT ID 13" },
+	{ 0x3e, "eCAT ID 14" },
+	{ 0x3f, "eCAT ID 15" },
 	{ 0x81, "SIM / USIM / UICC" },
 	{ 0x82, "Terminal (Card Reader)" },
 	{ 0x83, "Network" },
@@ -399,6 +421,8 @@ static const value_string cmd_type_vals[] = {
 	{ 0x62, "DISPLAY MULTIMEDIA MESSAGE" },
 	{ 0x70, "ACTIVATE" },
 	{ 0x71, "CONTACTLESS STATE CHANGED" },
+	{ 0x72, "COMMAND CONTAINER" },
+	{ 0x73, "ENCAPSULATED SESSION CONTROL" },
 	{ 0x81, "End of the proactive session" },
 	{ 0, NULL }
 };
@@ -476,6 +500,7 @@ static const value_string result_launch_browser_vals[] = {
 	{ 0x01, "Bearer unavailable" },
 	{ 0x02, "Browser unavailable" },
 	{ 0x03, "Terminal unable to read the provisioning data" },
+	{ 0x04, "Default URL unavailable" },
 	{ 0, NULL }
 };
 
@@ -606,6 +631,7 @@ static const value_string event_list_vals[] = {
 	{ 0x18, "Incoming IMS data" },
 	{ 0x19, "Profile Container" },
 	{ 0x1a, "Void" },
+	{ 0x1b, "Secured Profile Container" },
 	{ 0, NULL }
 };
 static value_string_ext event_list_vals_ext = VALUE_STRING_EXT_INIT(event_list_vals);
@@ -784,6 +810,7 @@ static const value_string access_tech_vals[] = {
 	{ 0x06, "cdma2000 1x (TIA/EIA/IS-2000)" },
 	{ 0x07, "cdma2000 HRPD (TIA/EIA/IS-856)" },
 	{ 0x08, "E-UTRAN" },
+	{ 0x09, "eHRPD" },
 	{ 0, NULL }
 };
 
@@ -797,6 +824,19 @@ static const value_string utran_eutran_meas_qual_vals[] = {
 	{ 0x06, "E-UTRAN Inter-frequency measurements" },
 	{ 0x07, "E-UTRAN Inter-RAT (GERAN) measurements" },
 	{ 0x08, "E-UTRAN Inter-RAT (UTRAN) measurements" },
+	{ 0, NULL }
+};
+
+/* ETSI 102.223 - Chapter 8.90 */
+static const value_string broadcast_nw_tech_vals[] = {
+	{ 0x00, "DVB-H" },
+	{ 0x01, "DVB-T" },
+	{ 0x02, "DVB-SH" },
+	{ 0x03, "T-DMB" },
+	{ 0x04, "FLO" },
+	{ 0x05, "WiMAX" },
+	{ 0x06, "DVB-NGH" },
+	{ 0x07, "DVB-T2" },
 	{ 0, NULL }
 };
 
@@ -927,7 +967,7 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 	proto_tree *cat_tree, *elem_tree;
 	unsigned int pos = 0;
 	tvbuff_t *new_tvb;
-	gboolean ims_event = FALSE;
+	gboolean ims_event = FALSE, dns_server = FALSE;
 	guint length = tvb_length(tvb);
 	gsm_sms_data_t sms_data;
 
@@ -979,6 +1019,10 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 			if (len < 3)
 				break;
 			proto_tree_add_item(elem_tree, hf_ctlv_cmd_nr, tvb, pos, 1, ENC_NA);
+			if (tvb_get_guint8(tvb, pos) == 0x40) {
+				ims_event = TRUE;
+				dns_server = TRUE;
+			}
 			proto_tree_add_item(elem_tree, hf_ctlv_cmd_type, tvb, pos+1, 1, ENC_NA);
 			/* append command type to INFO column */
 			g8 = tvb_get_guint8(tvb, pos+1);
@@ -997,10 +1041,6 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 				break;
 			case 0x27:
 				proto_tree_add_item(elem_tree, hf_ctlv_cmd_qual_timer_mgmt, tvb, pos+2, 1, ENC_NA);
-				break;
-			case 0x40:
-				proto_tree_add_item(elem_tree, hf_ctlv_cmd_qual, tvb, pos+2, 1, ENC_NA);
-				ims_event = TRUE;
 				break;
 			case 0x43:
 				proto_tree_add_item(elem_tree, hf_ctlv_cmd_qual_send_data, tvb, pos+2, 1, ENC_NA);
@@ -1281,6 +1321,22 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 			for (i = 0; i < len; i++)
 				proto_tree_add_item(elem_tree, hf_ctlv_access_tech, tvb, pos+i, 1, ENC_NA);
 			break;
+		case 0x40:	/* Display parameters / DNS server address */
+			if (dns_server) {
+				g8 = tvb_get_guint8(tvb, pos);
+				proto_tree_add_uint(elem_tree, hf_ctlv_dns_server_address_coding, tvb, pos, 1, g8);
+				switch (g8) {
+				case 0x21:
+					proto_tree_add_item(elem_tree, hf_ctlv_dns_server_address_ipv4, tvb, pos+1, 4, ENC_NA);
+					break;
+				case 0x57:
+					proto_tree_add_item(elem_tree, hf_ctlv_dns_server_address_ipv6, tvb, pos+1, 16, ENC_NA);
+					break;
+				default:
+					break;
+				}
+			}
+			break;
 		case 0x47:	/* network access name */
 			de_sm_apn(tvb, elem_tree, pinfo, pos, len, NULL, 0);
 			break;
@@ -1323,6 +1379,10 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 			for (i = 0; i < len; i+=3) {
 				dissect_e212_mcc_mnc(tvb, pinfo, elem_tree, pos+3*i, TRUE);
 			}
+			break;
+		case 0x7a:/* Broadcast Network Information */
+			proto_tree_add_item(elem_tree, hf_ctlv_broadcast_nw_tech, tvb, pos, 1, ENC_NA);
+			proto_tree_add_item(elem_tree, hf_ctlv_broadcast_nw_loc_info, tvb, pos+1, len-1, ENC_NA);
 			break;
 		case 0x7c:	/* EPS PDN connection activation parameters */
 			nas_esm_pdn_con_req(tvb, elem_tree, pinfo, pos, len);
@@ -1733,13 +1793,28 @@ proto_register_card_app_toolkit(void)
 			  NULL, HFILL },
 		},
 		{ &hf_ctlv_other_address_ipv6,
-			{ "IPv4 address", "etsi_cat.comp_tlv.other_address.ipv6",
+			{ "IPv6 address", "etsi_cat.comp_tlv.other_address.ipv6",
 			  FT_IPv6, BASE_NONE, NULL, 0,
 			  NULL, HFILL },
 		},
 		{ &hf_ctlv_access_tech,
 			{ "Access technology", "etsi_cat.comp_tlv.access_tech",
 			  FT_UINT8, BASE_HEX, VALS(access_tech_vals), 0,
+			  NULL, HFILL },
+		},
+		{ &hf_ctlv_dns_server_address_coding,
+			{ "Type of address", "etsi_cat.comp_tlv.dns_server_address.coding",
+			  FT_UINT8, BASE_HEX, VALS(other_address_coding_vals), 0,
+			  NULL, HFILL },
+		},
+		{ &hf_ctlv_dns_server_address_ipv4,
+			{ "IPv4 address", "etsi_cat.comp_tlv.dns_server_address.ipv4",
+			  FT_IPv4, BASE_NONE, NULL, 0,
+			  NULL, HFILL },
+		},
+		{ &hf_ctlv_dns_server_address_ipv6,
+			{ "IPv6 address", "etsi_cat.comp_tlv.dns_server_address.ipv6",
+			  FT_IPv6, BASE_NONE, NULL, 0,
 			  NULL, HFILL },
 		},
 		{ &hf_ctlv_utran_eutran_meas_qual,
@@ -1780,6 +1855,16 @@ proto_register_card_app_toolkit(void)
 		{ &hf_ctlv_ims_status_code,
 			{ "IMS Status-Code", "etsi_cat.comp_tlv.ims_status_code",
 			  FT_STRING, BASE_NONE, NULL, 0,
+			  NULL, HFILL },
+		},
+		{ &hf_ctlv_broadcast_nw_tech,
+			{ "Broadcast Network Technology", "etsi_cat.comp_tlv.broadcast_nw.tech",
+			  FT_UINT8, BASE_HEX, VALS(broadcast_nw_tech_vals), 0,
+			  NULL, HFILL },
+		},
+		{ &hf_ctlv_broadcast_nw_loc_info,
+			{ "Broadcast Network Location Information", "etsi_cat.comp_tlv.broadcast_nw.loc_info",
+			  FT_BYTES, BASE_NONE, NULL, 0,
 			  NULL, HFILL },
 		}
 	};

epan/dissectors/packet-gsm_sim.c
@@ -91,6 +91,8 @@ static int hf_tprof_b29 = -1;
 static int hf_tprof_b30 = -1;
 static int hf_tprof_b31 = -1;
 static int hf_tprof_b32 = -1;
+static int hf_tprof_b33 = -1;
+static int hf_tprof_unknown_byte = -1;
 /* First byte */
 static int hf_tp_prof_dld = -1;
 static int hf_tp_sms_data_dld = -1;
@@ -313,6 +315,12 @@ static int hf_tp_pa_prov_loci_henb_ip_addr = -1;
 static int hf_tp_pa_prov_loci_henb_surround_macro = -1;
 static int hf_tp_launch_params_support_open_chan_server_mode = -1;
 static int hf_tp_direct_com_support_open_chan_server_mode = -1;
+static int hf_tp_pa_sec_prof_env_cont = -1;
+static int hf_tp_cat_serv_list_ecat_client = -1;
+static int hf_tp_support_refresh_enforcement_policy = -1;
+/* 33th byte */
+static int hf_tp_support_dns_addr_req = -1;
+static int hf_tp_support_nw_access_name_reuse = -1;
 static int hf_tp_rfu11 = -1;
 
 static int hf_cat_ber_tag = -1;
@@ -354,6 +362,7 @@ static int ett_tprof_b29 = -1;
 static int ett_tprof_b30 = -1;
 static int ett_tprof_b31 = -1;
 static int ett_tprof_b32 = -1;
+static int ett_tprof_b33 = -1;
 
 static dissector_handle_t sub_handle_cap;
 
@@ -670,6 +679,15 @@ static const int *tprof_b32_fields[] = {
 	&hf_tp_pa_prov_loci_henb_surround_macro,
 	&hf_tp_launch_params_support_open_chan_server_mode,
 	&hf_tp_direct_com_support_open_chan_server_mode,
+	&hf_tp_pa_sec_prof_env_cont,
+	&hf_tp_cat_serv_list_ecat_client,
+	&hf_tp_support_refresh_enforcement_policy,
+	NULL
+};
+
+static const int *tprof_b33_fields[] = {
+	&hf_tp_support_dns_addr_req,
+	&hf_tp_support_nw_access_name_reuse,
 	&hf_tp_rfu11,
 	NULL
 };
@@ -1298,6 +1316,10 @@ dissect_gsm_apdu(guint8 ins, guint8 p1, guint8 p2, guint8 p3, tvbuff_t *tvb,
 		ADD_TP_BYTE(30);
 		ADD_TP_BYTE(31);
 		ADD_TP_BYTE(32);
+		ADD_TP_BYTE(33);
+		while ((offset - start_offset) < p3) {
+			proto_tree_add_item(tree, hf_tprof_unknown_byte, tvb, offset++, 1, ENC_BIG_ENDIAN);
+		}
 		break;
 	case 0x12: /* FETCH */
 		proto_tree_add_item(tree, hf_le, tvb, offset+P3_OFFS, 1, ENC_BIG_ENDIAN);
@@ -2724,9 +2746,47 @@ proto_register_gsm_sim(void)
 			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x10,
 			  NULL, HFILL }
 		},
+		{ &hf_tp_pa_sec_prof_env_cont,
+			{ "Proactive SIM: Security for Profile Container, Envelope Container, COMMAND CONTAINER and ENCAPSULATED SESSION CONTROL", "gsm_sim.tp.sec_prof_env_cont",
+			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x20,
+			  NULL, HFILL }
+		},
+		{ &hf_tp_cat_serv_list_ecat_client,
+			{ "CAT service list for eCAT client", "gsm_sim.tp.serv_list_ecat_client",
+			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x40,
+			  NULL, HFILL }
+		},
+		{ &hf_tp_support_refresh_enforcement_policy,
+			{ "Support of refresh enforcement policy", "gsm_sim.tp.refresh_enforcement_policy",
+			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x80,
+			  NULL, HFILL }
+		},
+
+		/* Terminal Profile Byte 33 */
+		{ &hf_tprof_b33,
+			{ "Terminal Profile Byte 33", "gsm_sim.tp.b33",
+			  FT_UINT8, BASE_HEX, NULL, 0,
+			  NULL, HFILL },
+		},
+		{ &hf_tp_support_dns_addr_req,
+			{ "Support of DNS server address request for OPEN CHANNEL related to packet data service bearer", "gsm_sim.tp.support_dns_addr_req",
+			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x01,
+			  NULL, HFILL }
+		},
+		{ &hf_tp_support_nw_access_name_reuse,
+			{ "Support of Network Access Name reuse indication for CLOSE CHANNEL related to packet data service bearer", "gsm_sim.tp.nw_access_name_reuse",
+			  FT_BOOLEAN, 8, TFS(&tfs_supported_not_supported), 0x02,
+			  NULL, HFILL }
+		},
 		{ &hf_tp_rfu11,
 			{ "RFU", "gsm_sim.tp.rfu",
-			  FT_UINT8, BASE_HEX, NULL, 0xe0,
+			  FT_UINT8, BASE_HEX, NULL, 0xfc,
+			  NULL, HFILL },
+		},
+
+		{ &hf_tprof_unknown_byte,
+			{ "Unknown Terminal Profile Byte", "gsm_sim.tp.unknown_byte",
+			  FT_UINT8, BASE_HEX, NULL, 0,
 			  NULL, HFILL },
 		},
 
@@ -2785,6 +2845,7 @@ proto_register_gsm_sim(void)
 		&ett_tprof_b30,
 		&ett_tprof_b31,
 		&ett_tprof_b32,
+		&ett_tprof_b33
 	};
 
 	proto_gsm_sim = proto_register_protocol("GSM SIM 11.11", "GSM SIM",

commit d9a3ebff61fb98014ef094e2612ca01bd325f7d8

etsi_card_app_toolkit: Dissect DNS server address
The DNS server address depends on command type OPEN CHANNEL,
not command number 0x40.

This issue was introduced in commit e398c8ed6b.

epan/dissectors/packet-etsi_card_app_toolkit.c
@@ -1093,11 +1093,11 @@ dissect_cat(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data)
 			if (len < 3)
 				break;
 			proto_tree_add_item_ret_uint(elem_tree, hf_ctlv_cmd_nr, tvb, pos, 1, ENC_BIG_ENDIAN, &cmd_nr);
-			if (cmd_nr == 0x40) {
+			proto_tree_add_item_ret_uint(elem_tree, hf_ctlv_cmd_type, tvb, pos+1, 1, ENC_BIG_ENDIAN, &g8);
+			if (g8 == 0x40) { /* OPEN CHANNEL */
 				ims_event = true;
 				dns_server = true;
 			}
-			proto_tree_add_item_ret_uint(elem_tree, hf_ctlv_cmd_type, tvb, pos+1, 1, ENC_BIG_ENDIAN, &g8);
 			/* append command type to INFO column */
 			col_append_fstr(pinfo->cinfo, COL_INFO, "%s ",
 					val_to_str_ext(g8, &cmd_type_vals_ext, "%02x "));