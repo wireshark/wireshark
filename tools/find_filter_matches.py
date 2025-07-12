2099f1193f7a86b6daf37f8b3083573c8b0e8892
Martin Mathieson <martin.r.mathieson@googlemail.com>
2025-07-12 18:55:56 +0000

    toos/check_filter_matches.py:  Add args to pass into tshark

--- tools/find_filter_matches.py
+++ tools/find_filter_matches.py
@@ -11,21 +11,59 @@ import subprocess
 import signal
 import argparse
 import pathlib
+import datetime
+import time
 
-# Search for capture files that match a given filter
+# Search for capture files that match a given filter (or filters)
 
-# Try to exit soon after Ctrl-C is pressed.
+def show_progress(filenum, num_files, filename, num_matching):
+    print('')
+    print('=== Progress: checking', filename, '-', filenum, 'of', num_files, 'files -', num_matching, 'matching so far ===')
+    print('=== Press Ctrl-C again to within 5 seconds to really exit ===')
+    time.sleep(2)
+
+def show_results(complete):
+    print('------------------------------')
+    print('Found', len(files_matching), 'files matching "' + args.filter[0] + '"', 'in folder', args.folder,
+          '' if complete else '(interrupted!)')
+    for find in files_matching:
+        print(find)
+    print('------------------------------')
+    # TODO: vary exit code depending upon how we got here, or if there were matches?
+    exit(0)
+
+
+
+# When Ctrl-C is pressed, show summary of progress - a 2nd press soon afterwards will cause exit
+previous_interrupt_time = datetime.datetime.now()
 should_exit = False
+should_show_progress = False
 
 def signal_handler(sig, frame):
     global should_exit
-    should_exit = True
-    print('You pressed Ctrl+C - exiting')
+    global should_show_progress
+    global previous_interrupt_time
+    this_interrupt_time = datetime.datetime.now()
+    if should_show_progress:
+        return
+
+    delta = (this_interrupt_time - previous_interrupt_time).seconds
+    if delta <= 2:
+        should_exit = True
+        print('You pressed Ctrl+C - exiting')
+
+        # Show any partial results anyway
+        show_results(False)
+    else:
+        should_show_progress = True
+
+    previous_interrupt_time = this_interrupt_time
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
 # TODO: others?
-capture_exts = set(['.pcap', '.pcapng', '.out'])
+capture_exts = set(['.pcap', '.pcapng', '.out', '.cap'])
 
 def isCaptureFile(filename):
     return pathlib.Path(filename).suffix in capture_exts
@@ -35,8 +73,10 @@ def findFilesInFolder(folder):
 
     for root, subfolders, files in os.walk(folder):
         for f in files:
-            if should_exit:
-                return
+            # Just get out and exit if user aborts now...
+            if should_exit or should_show_progress:
+                exit(1)
+
             f = os.path.join(root, f)
             if isCaptureFile(f):
                 files_to_check.append(f)
@@ -51,35 +91,54 @@ parser.add_argument('--filter', required=True, action='append',
                     help='display filter to search for')
 parser.add_argument('--folder', action='store', default='.',
                     help='specify folder to test')
+parser.add_argument('--profile', action='store',
+                    help='profile to invoke tshark with (-C)')
+parser.add_argument('--max-packets', action='store',
+                    help='maximum number of packets to read - tshark -c')
 parser.add_argument('--tshark', action='store', default='tshark',
                     help='version of tshark to run')
 args = parser.parse_args()
 
 
 # Find files worth checking.
+print('Compiling list of files to check.')
 files = findFilesInFolder(args.folder)
 print('Checking', len(files), 'files...')
-found = []
 
-for file in files:
+
+# Check files
+files_matching = []
+
+for filenum,file in enumerate(files):
     if should_exit:
         exit(1)
+
+    # May want to show progress here..
+    if should_show_progress:
+        show_progress(filenum, len(files), file, len(files_matching))
+        should_show_progress = False
 
     # N.B. if have multiple filters, must invoke tshark for each one, for cases where OR will not work
     lines_matching = {}
     for filter in args.filter:
-        # TODO: should show any non-default columns?
+        # TODO: Other args could usefully set?
         command = [ args.tshark, '-r', file, '-Y', filter ]
+        if args.profile:
+            command.extend(['-C', args.profile])
+        if args.max_packets:
+            command.extend(['-c', args.max_packets])
         try:
             output = subprocess.check_output(command)
         except Exception as e:
-            print('oops, exception', e)
+            #print('oops, exception', e)
             # Check for WS_EXIT_INVALID_FILTER (4)
+            # TODO: other errors possible, e.g., bad profile name..
             if str(e).find("exit status 4") != -1:
                 print('Please use a valid display filter!')
                 exit(4)
 
             break
+
 
         if len(output):
             sys.stdout.write('\n')
@@ -93,19 +152,15 @@ for file in files:
             # All filters must match so give up
             break
 
-    # Show each match in output.
-    if len(lines_matching) == len(args.filter):
-        found.append(file)
-        for filter in args.filter:
-            for line in lines_matching[filter]:
-                print(file, ':', filter, ':', line)
+        # Show each match in output, but only if every filter has at least one match in this file.
+        if len(lines_matching) == len(args.filter):
+            files_matching.append(file)
+            for filter in args.filter:
+                for line in lines_matching[filter]:
+                    print(file, ':', filter, ':', line.decode('utf-8'))
 
 
-# Summary
-print('------------------------------')
-print('Found', len(found), 'files matching "' + args.filter[0] + '"', 'in folder', args.folder)
-for find in found:
-    print(find)
-print('------------------------------')
 
-# TODO: return error code if no matches?
+# Show results if get to end
+show_results(True)
+
