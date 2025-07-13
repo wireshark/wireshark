43a9a643e1bfcd40f0886079c81d72772e654b3c
Michael Mann <mmann78@netscape.net>
2025-07-12 21:24:06 -0400

    Move x264_prt_id.h values to packet-x25.c
    It's the only dissector using it

--- epan/dissectors/packet-x25.c
+++ epan/dissectors/packet-x25.c
@@ -20,7 +20,6 @@
 #include <epan/prefs.h>
 #include <epan/expert.h>
 #include <epan/nlpid.h>
-#include <epan/x264_prt_id.h>
 #include <epan/lapd_sapi.h>
 #include <epan/tfs.h>
 #include <wiretap/wtap.h>
@@ -1181,6 +1180,13 @@ get_x25_pkt_len(tvbuff_t *tvb)
 
     return 0;
 }
+
+/* X.264 / ISO 11570 transport protocol ID values. */
+
+#define	PRT_ID_ISO_8073			0x01	/* X.224/ISO 8073 COTP */
+#define PRT_ID_ISO_8602			0x02	/* X.234/ISO 8602 CLTP */
+#define PRT_ID_ISO_10736_ISO_8073	0x03	/* X.274/ISO 10736 + X.224/ISO 8073 */
+#define PRT_ID_ISO_10736_ISO_8602	0x04	/* X.274/ISO 10736 + X.234/ISO 8602 */
 
 static const value_string prt_id_vals[] = {
     {PRT_ID_ISO_8073,           "ISO 8073 COTP"},
