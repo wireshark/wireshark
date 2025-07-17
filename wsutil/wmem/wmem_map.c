954e2ecaad740bc4e74e4685703e6ab6280f57c7
Anders Broman <a.broman58@gmail.com>
2025-07-17 16:46:22 +0200

    wmem_map: Use XXH3_64bits_withSeed() in wmem_strong_hash()

--- wsutil/wmem/wmem_map.c
+++ wsutil/wmem/wmem_map.c
@@ -537,7 +537,7 @@ uint32_t
 wmem_strong_hash(const uint8_t *buf, const size_t len)
 {
 #ifdef HAVE_XXHASH
-    return (uint32_t)XXH32(buf, len, postseed);
+    return (uint32_t)XXH3_64bits_withSeed(buf, len, postseed);
 #else
     const uint8_t * const end = (const uint8_t *)buf + len;
     uint32_t hash = preseed + (uint32_t)len;
