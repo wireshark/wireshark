4cc01a69b0bb3f265fbf7971ac08663aee8c9f02
John Thacker <johnthacker@gmail.com>
2025-07-16 20:34:38 -0400

    packaging: Install libxxhash.dll on MSYS2
    After adding it to the build process, make sure it gets added to the
    NSIS package.

--- packaging/nsis/InstallMSYS2.cmake
+++ packaging/nsis/InstallMSYS2.cmake
@@ -65,6 +65,7 @@ if(USE_MSYSTEM)
 		${MINGW_BIN}/libunistring-*.dll
 		${MINGW_BIN}/libwinpthread-*.dll
 		${MINGW_BIN}/libxml2-*.dll
+		${MINGW_BIN}/libxxhash.dll
 		${MINGW_BIN}/libzstd.dll
 		${MINGW_BIN}/lua54.dll
 		${MINGW_BIN}/zlib1.dll
