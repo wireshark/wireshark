ba370be47175cc12a437ff230ec0bbce76f6c78b
John Thacker <johnthacker@gmail.com>
2025-07-20 14:42:05 -0400

    GSM_MAP, Z3950: Don't register OIDs twice
    If an oid name lookup is added via register_ber_oid_dissector,
    don't call oid_add_from_string a second time. Some of these used
    a different name for the second registration. In such cases, I used
    the last registered name, which is the one it was renamed to, so
    that the dissection stayed the same.
    
    The other cases of duplicate registration involve two different
    dissectors, although it's still probably worth eliminating that.

--- epan/dissectors/asn1/z3950/packet-z3950-template.c
+++ epan/dissectors/asn1/z3950/packet-z3950-template.c
@@ -1184,7 +1184,7 @@ proto_reg_handoff_z3950(void)
 
 #include "packet-z3950-dis-tab.c"
 
-    register_ber_oid_dissector(Z3950_RECORDSYNTAX_MARC21_OID, dissect_marc_record, proto_z3950, "MARC21");
+    register_ber_oid_dissector(Z3950_RECORDSYNTAX_MARC21_OID, dissect_marc_record, proto_z3950, "MARC21 (formerly USMARC)");
 
     oid_add_from_string("Z39.50", "1.2.840.10003");
     oid_add_from_string("Z39.50-APDU", "1.2.840.10003.2");
@@ -1205,7 +1205,6 @@ proto_reg_handoff_z3950(void)
     oid_add_from_string("UNIMARC","1.2.840.10003.5.1");
     oid_add_from_string("INTERMARC","1.2.840.10003.5.2");
     oid_add_from_string("CCF","1.2.840.10003.5.3");
-    oid_add_from_string("MARC21 (formerly USMARC)",Z3950_RECORDSYNTAX_MARC21_OID);
     oid_add_from_string("UKMARC","1.2.840.10003.5.11");
     oid_add_from_string("NORMARC","1.2.840.10003.5.12");
     oid_add_from_string("Librismarc","1.2.840.10003.5.13");
@@ -1230,14 +1229,8 @@ proto_reg_handoff_z3950(void)
     oid_add_from_string("MARC21-fin","1.2.840.10003.5.32");
     oid_add_from_string("COMARC","1.2.840.10003.5.33");
     /* Non-MARC record syntaxes */
-    oid_add_from_string("Explain","1.2.840.10003.5.100");
     oid_add_from_string("Explain with ZSQL","1.2.840.10003.5.100.1");
-    oid_add_from_string("SUTRS","1.2.840.10003.5.101");
-    oid_add_from_string("OPAC","1.2.840.10003.5.102");
-    oid_add_from_string("Summary","1.2.840.10003.5.103");
     oid_add_from_string("GRS-0","1.2.840.10003.5.104");
-    oid_add_from_string("GRS-1","1.2.840.10003.5.105");
-    oid_add_from_string("ESTaskPackage","1.2.840.10003.5.106");
     oid_add_from_string("fragment","1.2.840.10003.5.108");
     /* Attribute sets */
     oid_add_from_string("bib-1",Z3950_ATSET_BIB1_OID);
