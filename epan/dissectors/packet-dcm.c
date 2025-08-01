3a60fb327e9e915c9232cf9c2703618e096039d3
John Thacker <johnthacker@gmail.com>
2025-07-31 21:49:43 -0400

    dcm: Don't recreate fixed hashmaps every dissection
    The hash maps in DCM are based on constant structs and are never
    changed during dissection, only used for lookups.
    Do not recreate them every dissection; create them in epan scope.
    
    In actuality, these should either use binary search (the two that
    use integers keys could do that easily) or tools/make-packet-dcm.py
    should use a perfect hash function generator like GNU gperf rather
    than inserting these into a map on startup.
    
    Fixup ba9aa3015f29f07bf81cdfebdfab54fdc379b94d

--- epan/dissectors/packet-dcm.c
+++ epan/dissectors/packet-dcm.c
@@ -756,25 +756,37 @@ dcm_init(void)
     unsigned   i;
 
     /* Create three hash tables for quick lookups */
+    /* XXX - These are constant hashmaps based on constant structs,
+     * produced by tools/make-packet-dcm.py
+     * The two with integer keys could use binary search (alter the
+     * Python script to sort). If a hash table is desired, GNU gperf
+     * or some perfect hash function generator should be used instead
+     * of inserting these on startup. */
     /* Add UID objects to hash table */
-    dcm_uid_table = wmem_map_new(wmem_file_scope(), wmem_str_hash, g_str_equal);
-    for (i = 0; i < array_length(dcm_uid_data); i++) {
-        wmem_map_insert(dcm_uid_table, (void *) dcm_uid_data[i].value,
-        (void *) &dcm_uid_data[i]);
+    if (dcm_uid_table == NULL) {
+        dcm_uid_table = wmem_map_new(wmem_epan_scope(), wmem_str_hash, g_str_equal);
+        for (i = 0; i < array_length(dcm_uid_data); i++) {
+            wmem_map_insert(dcm_uid_table, (void *) dcm_uid_data[i].value,
+            (void *) &dcm_uid_data[i]);
+        }
     }
 
     /* Add Tag objects to hash table */
-    dcm_tag_table = wmem_map_new(wmem_file_scope(), g_direct_hash, g_direct_equal);
-    for (i = 0; i < array_length(dcm_tag_data); i++) {
-        wmem_map_insert(dcm_tag_table, GUINT_TO_POINTER(dcm_tag_data[i].tag),
-        (void *) &dcm_tag_data[i]);
+    if (dcm_tag_table == NULL) {
+        dcm_tag_table = wmem_map_new(wmem_epan_scope(), g_direct_hash, g_direct_equal);
+        for (i = 0; i < array_length(dcm_tag_data); i++) {
+            wmem_map_insert(dcm_tag_table, GUINT_TO_POINTER(dcm_tag_data[i].tag),
+            (void *) &dcm_tag_data[i]);
+        }
     }
 
-   /* Add Status Values to hash table */
-    dcm_status_table = wmem_map_new(wmem_file_scope(), g_direct_hash, g_direct_equal);
-    for (i = 0; i < array_length(dcm_status_data); i++) {
-        wmem_map_insert(dcm_status_table, GUINT_TO_POINTER((uint32_t)dcm_status_data[i].value),
-        (void *)&dcm_status_data[i]);
+    /* Add Status Values to hash table */
+    if (dcm_status_table == NULL) {
+        dcm_status_table = wmem_map_new(wmem_epan_scope(), g_direct_hash, g_direct_equal);
+        for (i = 0; i < array_length(dcm_status_data); i++) {
+            wmem_map_insert(dcm_status_table, GUINT_TO_POINTER((uint32_t)dcm_status_data[i].value),
+            (void *)&dcm_status_data[i]);
+        }
     }
 }
 
