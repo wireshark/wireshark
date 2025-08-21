89b38b5c8229df2b0866a0e10ebc419e63ea8aa8
John Thacker <johnthacker@gmail.com>
2025-08-21 17:13:29 +0000

    Ensure that SSIZE_MAX is defined under Windows
    BaseTsd.h does not define SSIZE_MAX, and only defines MAXSSIZE_T
    in certain circumstances where cplusplus is defined. SIZE_MAX is
    defined so long as stdint.h is included.
    
    
    (cherry picked from commit 49a6624eeeffe9a12f407e3bb48b7ff8164849f7)
    
    Co-authored-by: John Thacker <johnthacker@gmail.com>

--- include/ws_posix_compat.h
+++ include/ws_posix_compat.h
@@ -14,14 +14,16 @@
 #include <stdint.h>
 #include <limits.h>
 
-#if !defined(SSIZE_MAX) && !defined(HAVE_SSIZE_T)
+#if !defined(HAVE_SSIZE_T)
 #if defined(_WIN32)
 #include <BaseTsd.h>
 
 typedef SSIZE_T ssize_t;
-#define SSIZE_MAX SSIZE_T_MAX
+#if !defined(SSIZE_MAX)
+#define SSIZE_MAX ((SSIZE_T)(SIZE_MAX >> 1))
+#endif /* !SSIZE_MAX */
 
 #endif /* _WIN32 */
-#endif /* !SSIZE_MAX && !HAVE_SSIZE_T */
+#endif /* !HAVE_SSIZE_T */
 
 #endif /* __POSIX_COMPAT_H__ */
