fa99f17ad2bab0147d34ce84380cfc9ef8e3cfb8
Moshe Kaplan <mosheekaplan@gmail.com>
2025-10-01 16:51:46 -0400

    wsutil: Doxygen crash_info.h

--- wsutil/crash_info.h
+++ wsutil/crash_info.h
@@ -17,8 +17,27 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Append formatted crash diagnostic information using a va_list.
+ *
+ * Adds a formatted message to the crash info log using a variable argument list.
+ * This function is typically used internally when forwarding variadic arguments.
+ *
+ * @param fmt Format string (printf-style).
+ * @param ap  va_list containing the arguments for the format string.
+ */
 WS_DLL_PUBLIC void ws_vadd_crash_info(const char *fmt, va_list ap);
 
+/**
+ * @brief Append formatted crash diagnostic information.
+ *
+ * Adds a formatted message to the crash info log. This is used to record
+ * contextual information during a crash or fatal error, aiding in post-mortem
+ * debugging and diagnostics.
+ *
+ * @param fmt Format string (printf-style).
+ * @param ... Arguments corresponding to the format string.
+ */
 WS_DLL_PUBLIC void ws_add_crash_info(const char *fmt, ...)
     G_GNUC_PRINTF(1,2);
 
