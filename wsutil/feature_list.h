6c125b4348001c42edbe1b39567fc309eae80fb1
Moshe Kaplan <mosheekaplan@gmail.com>
2025-10-02 22:33:29 -0400

    wsutil: Doxygen feature_list.h

--- wsutil/feature_list.h
+++ wsutil/feature_list.h
@@ -19,11 +19,16 @@
 extern "C" {
 #endif /* __cplusplus */
 
-/*
- * Handle to a list of features/dependencies.
- * Semi-opaque. Functions which gather the list of features
- * will be passed one of these to use with
- * `with_feature()`/`without_feature()` (below).
+
+ /**
+ * @brief Semi-opaque handle to a list of features or dependencies.
+ *
+ * Functions that collect or modify feature sets
+ * will receive a `feature_list` handle, which can be updated via
+ * `with_feature()` or `without_feature()` calls.
+ *
+ * @note Ownership and lifetime are managed by the caller; this typedef
+ * provides clarity and encapsulation for feature tracking APIs.
  */
 typedef GList **feature_list;
 
@@ -36,44 +41,68 @@ typedef GList **feature_list;
  */
 
 
-/*
- * Pointer to a function which gathers a list of features.
+/**
+ * @brief Pointer to a function which gathers a list of features.
  */
 typedef void(*gather_feature_func)(feature_list l);
 
-/*
- * Add an indicator to the given feature_list that the named
- * feature is present.
+/**
+ * @brief Mark a feature as present in the provided list.
+ *
+ * @param l   Mutable feature list to update.
+ * @param fmt Format string describing the feature.
+ * @param ... Arguments for the format string.
  */
 WS_DLL_PUBLIC
 void with_feature(feature_list l, const char *fmt, ...) G_GNUC_PRINTF(2,3);
 
-/*
- * Add an indicator to the given feature_list that the named
- * feature is absent.
+/**
+ * @brief Mark a feature as absent in the given list.
+ *
+ * @param l   Mutable feature list to update.
+ * @param fmt Format string describing the absent feature.
+ * @param ... Arguments for the format string.
  */
 WS_DLL_PUBLIC
 void without_feature(feature_list l, const char *fmt, ...) G_GNUC_PRINTF(2,3);
 
-/*
- * Sort the given feature list, alphabetically by feature name.
- * (The leading '+' or '-' is not factored into the sort.)
- * Currently unused.
+/**
+ * @brief Sort a feature list alphabetically by feature name.
+ *
+ * Reorders the entries in the given `feature_list` in ascending alphabetical
+ * order, ignoring any leading '+' or '-' indicators.
+ *
+ * @note The function is currently unused.
+ *
+ * @param l Mutable feature list to be sorted in-place.
  */
 WS_DLL_PUBLIC
 void sort_features(feature_list l);
 
-/*
- * Separate input list l into output lists with_list and without_list
- * (which should be empty when first called).
- * Note: output list members DO keep the leading '+'/'-' prefix.
+/**
+ * @brief Split a feature list into present and absent feature subsets.
+ *
+ * Iterates over the input `feature_list` and separates entries into two
+ * output lists: `with_list` for present features (prefixed with '+') and
+ * `without_list` for absent features (prefixed with '-'). The prefixes are
+ * preserved in the output lists.
+ *
+ * Both output lists must be empty when first passed to this function.
+ *
+ * @param l            Input feature list to be partitioned.
+ * @param with_list    Output list for present features (prefixed with '+').
+ * @param without_list Output list for absent features (prefixed with '-').
  */
 WS_DLL_PUBLIC
 void separate_features(feature_list l, feature_list with_list, feature_list without_list);
 
-/*
- * Free the memory used by the feature list,
- * and reset its pointer to NULL.
+/**
+ * @brief Free all memory associated with a feature list.
+ *
+ * Releases all entries in the given `feature_list` and resets the pointer
+ * to `NULL`.
+ *
+ * @param l Pointer to the feature list to be freed and nulled.
  */
 WS_DLL_PUBLIC
 void free_features(feature_list l);
