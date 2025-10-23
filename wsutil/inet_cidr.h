338394a63ed8c2fbd2548773226591adaaaa1d83
Moshe Kaplan <mosheekaplan@gmail.com>
2025-10-23 15:52:56 -0400

    wsutil: Doxygen inet_cidr.h

--- wsutil/inet_cidr.h
+++ wsutil/inet_cidr.h
@@ -34,27 +34,56 @@ typedef struct {
  */
 
 /**
-* Returns the IPv4 subnet mask of the specified length
-*
-* @param mask_length the number of bits in the subnet mask (max of 32)
-* @return the subnet mask of the specified length
-*/
+ * @brief Returns the IPv4 subnet mask of the specified length.
+ *
+ * Constructs a subnet mask with the given number of leading 1 bits.
+ * For example, a mask length of 24 yields 255.255.255.0.
+ *
+ * @param mask_length The number of bits in the subnet mask (0–32).
+ * @return The subnet mask as a 32-bit unsigned integer in host byte order.
+ */
 WS_DLL_PUBLIC
-uint32_t
-ws_ipv4_get_subnet_mask(const uint32_t mask_length);
+uint32_t ws_ipv4_get_subnet_mask(const uint32_t mask_length);
 
+/**
+ * @brief Initializes an IPv4 address-and-mask structure.
+ *
+ * Sets up an address-mask pair using the given address and prefix length.
+ *
+ * @param dst Pointer to the destination structure to initialize.
+ * @param src_addr The IPv4 address to use.
+ * @param src_bits The number of bits in the subnet mask.
+ */
 WS_DLL_PUBLIC
-void
-ws_ipv4_addr_and_mask_init(ipv4_addr_and_mask *dst, ws_in4_addr src_addr, int src_bits);
+void ws_ipv4_addr_and_mask_init(ipv4_addr_and_mask *dst, ws_in4_addr src_addr, int src_bits);
 
+/**
+ * @brief Checks whether an IPv4 address is contained within a subnet.
+ *
+ * Tests whether the given address falls within the subnet defined by the address-mask pair.
+ *
+ * @param ipv4 Pointer to the address-mask structure representing the subnet.
+ * @param addr Pointer to the IPv4 address to test.
+ * @return true if the address is within the subnet, false otherwise.
+ */
 WS_DLL_PUBLIC
-bool
-ws_ipv4_addr_and_mask_contains(const ipv4_addr_and_mask *ipv4, const ws_in4_addr *addr);
+bool ws_ipv4_addr_and_mask_contains(const ipv4_addr_and_mask *ipv4, const ws_in4_addr *addr);
 
 /*
  ********** IPv6 *********
  */
 
+/**
+ * @brief Checks whether an IPv6 address is contained within a subnet.
+ *
+ * Determines if the specified IPv6 address falls within the subnet defined by
+ * the given address and prefix length. This is useful for routing, filtering,
+ * and address classification tasks.
+ *
+ * @param ipv6 Pointer to the IPv6 address-and-prefix structure representing the subnet.
+ * @param addr Pointer to the IPv6 address to test.
+ * @return true if the address is within the subnet, false otherwise.
+ */
 WS_DLL_PUBLIC
 bool
 ws_ipv6_addr_and_prefix_contains(const ipv6_addr_and_prefix *ipv6, const ws_in6_addr *addr);
