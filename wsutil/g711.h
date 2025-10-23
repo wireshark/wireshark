afe4fa0368f47db637a11203621a0e0770219613
Moshe Kaplan <mosheekaplan@gmail.com>
2025-10-23 15:29:07 -0400

    wsutil: Doxygen g711.h

--- wsutil/g711.h
+++ wsutil/g711.h
@@ -18,10 +18,47 @@
 extern "C" {
 #endif /* __cplusplus */
 
-WS_DLL_PUBLIC unsigned char linear2alaw( int );
-WS_DLL_PUBLIC int alaw2linear( unsigned char );
-WS_DLL_PUBLIC unsigned char linear2ulaw( int );
-WS_DLL_PUBLIC int ulaw2linear( unsigned char );
+/**
+ * @brief Converts a 16-bit linear PCM value to 8-bit A-law encoded format.
+ *
+ * Encodes a signed 16-bit linear PCM value into an 8-bit A-law value,
+ * commonly used in telephony systems to compress audio data.
+ *
+ * @param pcm_val The 16-bit linear PCM value to encode.
+ * @return The corresponding 8-bit A-law encoded value.
+ */
+WS_DLL_PUBLIC unsigned char linear2alaw(int pcm_val);
+
+/**
+ * @brief Converts an 8-bit A-law encoded value to a 16-bit linear PCM value.
+ *
+ * Decodes an 8-bit A-law value back into a signed 16-bit linear PCM value.
+ *
+ * @param a_val The 8-bit A-law encoded value.
+ * @return The corresponding 16-bit linear PCM value.
+ */
+WS_DLL_PUBLIC int alaw2linear(unsigned char a_val);
+
+/**
+ * @brief Converts a 16-bit linear PCM value to u-law encoded format.
+ *
+ * Encodes a signed 16-bit linear PCM value into an 8-bit u-law value,
+ * used in North American and Japanese telephony systems for audio compression.
+ *
+ * @param pcm_val The 16-bit linear PCM value to encode.
+ * @return The corresponding 8-bit u-law encoded value.
+ */
+WS_DLL_PUBLIC unsigned char linear2ulaw(int pcm_val);
+
+/**
+ * @brief Converts a u-law encoded value to a 16-bit linear PCM value.
+ *
+ * Decodes an 8-bit u-law value back into a signed 16-bit linear PCM value.
+ *
+ * @param u_val The 8-bit u-law encoded value.
+ * @return The corresponding 16-bit linear PCM value.
+ */
+WS_DLL_PUBLIC int ulaw2linear(unsigned char u_val);
 
 #ifdef __cplusplus
 }
