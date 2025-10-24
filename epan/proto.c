4748d8c21ebcd20431a03973981ae16873a08ac1
Jaap Keuter <jaap.keuter@xs4all.nl>
2025-10-24 20:04:58 +0200

    EPAN: Have proto_tree_add_bits functions set correct FI bit offset
    Proto_tree_add_bits functions have their offset in bits, rather than
    bytes. During tree item creation a field_info is created with start at
    the first byte of the bit field. Therefore the bit offset to set in the
    field info should be the remaining bits in the byte, rather than the
    full bit_offset. Therefore pass bit_offset mod 8 to FI_BITS_OFFSET.
    
    Followup on b0d9cba7
    Fixes #20820

--- epan/proto.c
+++ epan/proto.c
@@ -13680,7 +13680,7 @@ proto_tree_add_bits_ret_val(proto_tree *tree, const int hfindex, tvbuff_t *tvb,
 	if ((item = _proto_tree_add_bits_ret_val(tree, hfindex, tvb,
 						 bit_offset, no_of_bits,
 						 return_value, encoding))) {
-		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_OFFSET(bit_offset));
+		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_OFFSET(bit_offset&0x7));
 		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_SIZE(no_of_bits));
 	}
 	return item;
@@ -13812,7 +13812,7 @@ proto_tree_add_bits_format_value(proto_tree *tree, const int hfindex,
 	if ((item = _proto_tree_add_bits_format_value(tree, hfindex,
 						      tvb, bit_offset, no_of_bits,
 						      value_ptr, encoding, value_str))) {
-		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_OFFSET(bit_offset));
+		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_OFFSET(bit_offset&0x7));
 		FI_SET_FLAG(PNODE_FINFO(item), FI_BITS_SIZE(no_of_bits));
 	}
 	return item;
