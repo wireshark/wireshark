wireshark/wireshark

commit bf4e30dd214cad9a3fcb847c43a63d3b3d8c2748

wsutil: Supply more version information for ws_init_version_info()
Remove the dependency of "application flavor" in the use of ws_init_version_info() by explicitly providing the parameters (defaulting to Wireshark if needed)

wsutil/version_info.c
@@ -42,7 +42,6 @@
 
 #include "vcs_version.h"
 
-#include <wsutil/application_flavor.h>
 #include <wsutil/cpu_info.h>
 #include <wsutil/os_version_info.h>
 #include <wsutil/crash_info.h>
@@ -60,6 +59,8 @@ static void get_mem_info(GString *str);
 
 void
 ws_init_version_info(const char *appname,
+		const char* appflavor,
+		get_version_func version_func,
 		gather_feature_func gather_compile,
 		gather_feature_func gather_runtime)
 {
@@ -80,16 +81,16 @@ ws_init_version_info(const char *appname,
 	 * version - including the VCS version, for a build from
 	 * a checkout.
 	 */
-	if (strstr(appname, application_flavor_name_proper()) != NULL) {
+	appname_with_version = ws_strdup_printf("%s %s",
+			appname, version_func());
+	if ((appflavor != NULL) && strstr(appname, appflavor) != NULL) {
 		appname_with_version = ws_strdup_printf("%s %s",
-			appname,
-			application_flavor_is_wireshark() ? get_ws_vcs_version_info() : get_ss_vcs_version_info());
+			appname, version_func());
 	}
 	/* Include our application flavor. The default is "Wireshark" */
 	else {
 		appname_with_version = ws_strdup_printf("%s (%s) %s",
-			appname, application_flavor_name_proper(),
-			application_flavor_is_wireshark() ? get_ws_vcs_version_info() : get_ss_vcs_version_info());
+			appname, (appflavor != NULL) ? appflavor : "Wireshark", version_func());
 	}
 
 	/* Get the compile-time version information string */

capinfos.c
@@ -1523,7 +1523,7 @@ main(int argc, char *argv[])
     }
 
     /* Initialize the version information. */
-    ws_init_version_info("Capinfos", NULL, NULL);
+    ws_init_version_info("Capinfos", NULL, get_ws_vcs_version_info, NULL, NULL);
 
     init_report_failure_message("capinfos");
 

captype.c
@@ -120,7 +120,7 @@ main(int argc, char *argv[])
     }
 
     /* Initialize the version information. */
-    ws_init_version_info("Captype", NULL, NULL);
+    ws_init_version_info("Captype", NULL, get_ws_vcs_version_info, NULL, NULL);
 
     init_report_failure_message("captype");
 

dftest.c
@@ -382,7 +382,7 @@ main(int argc, char **argv)
         g_free(configuration_init_error);
     }
 
-    ws_init_version_info("DFTest", NULL, NULL);
+    ws_init_version_info("DFTest", NULL, get_ws_vcs_version_info, NULL, NULL);
 
     for (;;) {
         opt = ws_getopt_long(argc, argv, optstring, long_options, NULL);

dumpcap.c
@@ -5337,7 +5337,7 @@ main(int argc, char *argv[])
 #endif
 
     /* Initialize the version information. */
-    ws_init_version_info("Dumpcap", gather_dumpcap_compiled_info,
+    ws_init_version_info("Dumpcap", NULL, get_ws_vcs_version_info, gather_dumpcap_compiled_info,
                          gather_dumpcap_runtime_info);
 
     /* Initialize the pcaps list and IDBs */

editcap.c
@@ -1493,7 +1493,7 @@ main(int argc, char *argv[])
     }
 
     /* Initialize the version information. */
-    ws_init_version_info("Editcap", NULL, NULL);
+    ws_init_version_info("Editcap", NULL, get_ws_vcs_version_info, NULL, NULL);
 
     init_report_failure_message("editcap");
 

fuzz/fuzzshark.c
@@ -253,7 +253,7 @@ fuzz_init(int argc, char **argv)
 	}
 
 	/* Initialize the version information. */
-	ws_init_version_info("OSS Fuzzshark",
+	ws_init_version_info("OSS Fuzzshark", NULL, get_ws_vcs_version_info,
 	    epan_gather_compile_info, epan_gather_runtime_info);
 
 	init_report_failure_message("fuzzshark");

mergecap.c
@@ -238,7 +238,7 @@ main(int argc, char *argv[])
     }
 
     /* Initialize the version information. */
-    ws_init_version_info("Mergecap", NULL, NULL);
+    ws_init_version_info("Mergecap", NULL, get_ws_vcs_version_info, NULL, NULL);
 
     init_report_failure_message("mergecap");
 

commit 5185418dfa503aa533b8a1cbf42259ba1e8299c3

Fix memory leak in ws_init_version_info
Introduced in bf4e30dd214cad9a3fcb847c43a63d3b3d8c2748

wsutil/version_info.c
@@ -81,8 +81,6 @@ ws_init_version_info(const char *appname,
 	 * version - including the VCS version, for a build from
 	 * a checkout.
 	 */
-	appname_with_version = ws_strdup_printf("%s %s",
-			appname, version_func());
 	if ((appflavor != NULL) && strstr(appname, appflavor) != NULL) {
 		appname_with_version = ws_strdup_printf("%s %s",
 			appname, version_func());