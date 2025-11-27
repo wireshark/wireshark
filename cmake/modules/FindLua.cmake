7ebd0c42cf0ec9c81dfed5e362a7cb8ac087f349
Guy Harris <gharris@sonic.net>
2025-11-27 14:29:36 -0800

    Work around a Lua 5.4.5 API botch.
    Lua 5.4.5 broke the Lua API by adding a second argument to
    lua_resetthread().
    
    Fedora, and possibly other Linux distributions, compounded that error by
    backporting a fix from 5.4.5 that iincluded that API-breaking change,
    but didn't change LUA_VERSION_RELEASE_NUM, so that checking for
    LUA_VERSION_RELEASE_NUM == 50404 isn't sufficient to detect the broken
    version of the API.
    
    See the thread starting at
    
    https://lists.wireshark.org/archives/wireshark-dev/202511/msg00031.html
    
    So we do it the hard way, by checking whether a program that calls the
    one-argument version compiles.
    
    (backported from commit 1f855f6061f61034bd1cb6da1b96a1c6f5ae52b2)
    
    Get rid of some unintended extra comment lines.
    
    (backported from commit 428ef7c65461a053ec5824a3c34bddfeea3b35c8)

--- cmake/modules/FindLua.cmake
+++ cmake/modules/FindLua.cmake
@@ -189,7 +189,7 @@ find_library(LUA_LIBRARY
 unset(_lua_library_names)
 
 if (LUA_LIBRARY)
-  # include the math library for Unix
+  # include the math library for Unixes oher than macOS
   if (UNIX AND NOT APPLE AND NOT BEOS)
     find_library(LUA_MATH_LIBRARY m)
     mark_as_advanced(LUA_MATH_LIBRARY)
@@ -244,6 +244,55 @@ IF(Lua_FOUND)
     # it here.
     set(HAVE_LUA_UNICODE True)
   endif()
+
+  #
+  # Lua 5.4.5 broke the Lua API by adding a second argument to
+  # lua_resetthread().
+  #
+  # Fedora, and possibly other Linux distributions, compounded
+  # that error by backporting a fix from 5.4.5 that iincluded
+  # that API-breaking change, but didn't change LUA_VERSION_RELEASE_NUM,
+  # so that checking for LUA_VERSION_RELEASE_NUM == 50404 isn't
+  # sufficient to detect the broken version of the API.
+  #
+  # See the thread starting at
+  #
+  #   https://lists.wireshark.org/archives/wireshark-dev/202511/msg00031.html
+  #
+  # So we do it the hard way, by checking whether a program that
+  # calls the one-argument version compiles.
+  #
+  # CMake doesn't appear to support "check whether this C source
+  # compiles, but don't bother testing whether it links", which
+  # is what we want here - all we care about is whether the
+  # two-argument call to lua_resetthread() compiles or fails to
+  # compile, not whether the routine is available in the library.
+  # This test will fail if the link fails; we are testing with
+  # a two-argument call, so that means that, if the link fails
+  # for any reason (wrong required libraries, wrong required flags,
+  # etc.), we'll treat Lua as having a one-argument lua_resetthread().
+  #
+  include(CheckCSourceCompiles)
+  cmake_push_check_state()
+  set(CMAKE_REQUIRED_INCLUDES ${LUA_INCLUDE_DIR})
+  set(CMAKE_EXTRA_INCLUDE_FILES "luaconf.h")
+  set(CMAKE_REQUIRED_LIBRARIES ${LUA_LIBRARIES})
+  check_c_source_compiles(
+  "
+//
+// Undo any definition of _FORTIFY_SOURCE, so that we don't get a
+// complaint that we're compiling without optimization.
+//
+#undef _FORTIFY_SOURCE
+#include <lua.h>
+
+int
+main(void)
+{
+    return lua_resetthread(NULL, NULL);
+}
+" HAVE_TWO_ARGUMENT_LUA_RESETTHREAD)
+  cmake_pop_check_state()
 ELSE(Lua_FOUND)
   SET( LUA_LIBRARIES )
   SET( LUA_INCLUDE_DIRS )
