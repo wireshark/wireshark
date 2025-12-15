8d02b6c78648a771442263e88d7690e18f50a1ee
Stefan Metzmacher <metze@samba.org>
2025-12-15 14:05:07 +0100

    Revert "infiniband/smbdirect: fix dissecting the first smbdirect negotiate request"
    This reverts commit c462f1f4d2ee67cce30d73ea411ae6172f534a58.
    
    This introduced a few regressions for other captures I have.
    
    A better fix will follow in the next commit.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- epan/dissectors/packet-infiniband.c
+++ epan/dissectors/packet-infiniband.c
@@ -2805,13 +2805,13 @@ static tvbuff_t *parse_PAYLOAD_reassemble_tvb(packet_info *pinfo,
     case RC_SEND_MIDDLE:
         more_frags = true;
         break;
+    case RC_SEND_ONLY:
+    case RC_SEND_ONLY_IMM:
+    case RC_SEND_ONLY_INVAL:
     case RC_SEND_LAST:
     case RC_SEND_LAST_IMM:
     case RC_SEND_LAST_INVAL:
         break;
-    case RC_SEND_ONLY:
-    case RC_SEND_ONLY_IMM:
-    case RC_SEND_ONLY_INVAL:
     default:
         /* not a fragmented RC Send */
         return tvb;
@@ -2840,6 +2840,7 @@ static tvbuff_t *parse_PAYLOAD_reassemble_tvb(packet_info *pinfo,
     if (fd_head == NULL) {
             fd_head_not_cached = true;
 
+            pinfo->fd->visited = 0;
             fd_head = fragment_add_seq_next(&infiniband_rc_send_reassembly_table,
                                             tvb, 0, pinfo,
                                             conversation->conv_index,
@@ -2847,7 +2848,7 @@ static tvbuff_t *parse_PAYLOAD_reassemble_tvb(packet_info *pinfo,
                                             more_frags);
     }
 
-    if (more_frags && fd_head == NULL) {
+    if (fd_head == NULL) {
             /*
              * We really want the fd_head and pass it to
              * process_reassembled_data()
@@ -3047,7 +3048,7 @@ reassemble:
             call_data_dissector(next_tvb, pinfo, top_tree);
         }
 
-        if (dissector_found && allow_reassembling && info->do_rc_send_reassembling) {
+        if (allow_reassembling && info->do_rc_send_reassembling) {
                 goto reassemble;
         }
 
