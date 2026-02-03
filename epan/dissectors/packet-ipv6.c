da6bf13ef0f00e13b0094f236acd32194a0c74f8
Mark Stout <mark.stout@markstout.com>
2026-02-03 13:44:51 -0600

    IPV6: adding NAT64 /48 to wildcard prefix
    adding this /48 to the wildcard will allow for the targeting of the 3rd quartet when using a /96 as your prefix length

--- epan/dissectors/packet-ipv6.c
+++ epan/dissectors/packet-ipv6.c
@@ -830,6 +830,7 @@ static const value_string nat64_prefix_wildcard_length_vals[] =
     {  8, "8" },
     { 16, "16" },
     { 32, "32" },
+    { 48, "48" },
     { 64, "64" },
     {  0, NULL }
 };
