d68f02aaf114abb0633524758e333b8692a9aa83
John Thacker <johnthacker@gmail.com>
2026-04-05 18:05:49 -0400

    wsutil: Doxygen rsa.h
    (cherry picked from commit 2036c3ca197ec89aabe25a499124a6dcc1e392df)
    
    Co-authored-by: Moshe Kaplan <mosheekaplan@gmail.com>

--- wsutil/rsa.h
+++ wsutil/rsa.h
@@ -18,10 +18,23 @@
 #ifdef HAVE_LIBGNUTLS
 #include <stdio.h>
 #include <gnutls/abstract.h>
+
+/**
+ * @brief Converts a GnuTLS RSA private key to a Libgcrypt S-expression.
+ *
+ * Translates a GnuTLS-managed RSA private key into a Libgcrypt `gcry_sexp_t` structure
+ * for use in cryptographic operations such as signing or decryption.
+ * On failure, sets an error message in `err`.
+ *
+ * @param priv_key The GnuTLS RSA private key to convert.
+ * @param err Pointer to a string for error reporting (set on failure).
+ * @return A Libgcrypt S-expression representing the RSA key, or NULL on error.
+ */
 WS_DLL_PUBLIC gcry_sexp_t rsa_privkey_to_sexp(gnutls_x509_privkey_t priv_key, char **err);
 
 /**
- * Load an RSA private key from specified file
+ * @brief Load an RSA private key from specified file
+ *
  * @param fp the file that contain the key data
  * @param [out] err   error message upon failure; NULL upon success
  * @return a pointer to the loaded key on success, or NULL upon failure
@@ -29,7 +42,8 @@ WS_DLL_PUBLIC gcry_sexp_t rsa_privkey_to_sexp(gnutls_x509_privkey_t priv_key, ch
 WS_DLL_PUBLIC gnutls_x509_privkey_t rsa_load_pem_key(FILE* fp, char **err);
 
 /**
- * Load a RSA private key from a PKCS#12 file (DER or PEM format)
+ * @brief Load a RSA private key from a PKCS#12 file (DER or PEM format)
+ *
  * @param fp          the file that contains the key data
  * @param cert_passwd password to decrypt the PKCS#12 file
  * @param [out] err   error message upon failure; NULL upon success
@@ -38,6 +52,13 @@ WS_DLL_PUBLIC gnutls_x509_privkey_t rsa_load_pem_key(FILE* fp, char **err);
 WS_DLL_PUBLIC gnutls_x509_privkey_t rsa_load_pkcs12(FILE* fp, const char *cert_passwd, char** err);
 #endif
 
+/**
+ * @brief Frees an RSA private key object.
+ *
+ * Releases memory and resources associated with a previously allocated RSA private key.
+ *
+ * @param key Pointer to the RSA private key object to free.
+ */
 WS_DLL_PUBLIC void rsa_private_key_free(void * key);
 
 
