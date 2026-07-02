wireshark/wireshark

commit 6f85fc6895911b8e369cdddbdc932efe4af1dee1

Qt: Make sure the main display filter history always updates
Run the filterPackets routine through the main display filter entry
just as as done with setDisplayFilter, so that the main display
filter entry is the single source for filtrs and all filters applied
to the entire capture file enter the history.

Move the routine that actually applies the filter to a protected
slot that is connected to the main display filter signal by the
main window itself, so that other routines use the appropriate
slots intead.

This means that (currently?) the "force" parameter for a new
filter is not respected (filters are always forced), but the check
in file.c for forcing is not necessarily correct anyway because
it looks at the filter text and not what it compiles to.

Fix #21031

AI-Assisted: no

ui/qt/wireshark_main_window_slots.cpp
@@ -306,32 +306,13 @@ finish:
     return ret;
 }
 
-void WiresharkMainWindow::filterPackets(QString new_filter, bool force)
+void WiresharkMainWindow::applyFilter(QString new_filter, bool force)
 {
     cf_status_t cf_status;
 
     cf_status = cf_filter_packets(CaptureFile::globalCapFile(), new_filter.toUtf8().data(), force);
 
     if (cf_status == CF_OK) {
-        // This slot is used both by the main DisplayFilterEntry as well as
-        // other objects, which is not great design. Update the main filter
-        // entry if the sender was something else to keep the text in sync.
-        if (sender() != df_combo_box_) {
-            // XXX - The display filter entry owns its own recent-filter history
-            // and records applied filters there itself, but doesn't know about
-            // filters applied via other methods (command line, other dialogs,
-            // etc.) Either it needs a method to update its history with a
-            // filter that's already been applied, or this method should be
-            // split into two slots, one for the DisplayFilterEntry and one
-            // for everything else.
-            if (new_filter.length() > 0) {
-                df_combo_box_->setText(new_filter);
-            } else {
-                df_combo_box_->clear();
-            }
-        }
-        // Only after the display filter has been updated,
-        // disable the arrow button
         emit displayFilterSuccess(true);
     } else {
         emit displayFilterSuccess(false);
@@ -525,8 +506,7 @@ void WiresharkMainWindow::queuedFilterAction(QString action_filter, FilterAction
         main_ui_->searchFrame->findFrameWithFilter(new_filter);
         break;
     case FilterAction::ActionPrepare:
-        df_combo_box_->setText(new_filter);
-        df_combo_box_->setFocus();
+        df_combo_box_->setDisplayFilter(new_filter);
         break;
     case FilterAction::ActionWebLookup:
     {

ui/stratoshark/stratoshark_main_window_slots.cpp
@@ -268,32 +268,13 @@ finish:
     return ret;
 }
 
-void StratosharkMainWindow::filterPackets(QString new_filter, bool force)
+void StratosharkMainWindow::applyFilter(QString new_filter, bool force)
 {
     cf_status_t cf_status;
 
     cf_status = cf_filter_packets(CaptureFile::globalCapFile(), new_filter.toUtf8().data(), force);
 
     if (cf_status == CF_OK) {
-        // This slot is used both by the main DisplayFilterEntry as well as
-        // other objects, which is not great design. Update the main filter
-        // entry if the sender was something else to keep the text in sync.
-        if (sender() != df_combo_box_) {
-            // XXX - The display filter entry owns its own recent-filter history
-            // and records applied filters there itself, but doesn't know about
-            // filters applied via other methods (command line, other dialogs,
-            // etc.) Either it needs a method to update its history with a
-            // filter that's already been applied, or this method should be
-            // split into two slots, one for the DisplayFilterEntry and one
-            // for everything else.
-            if (new_filter.length() > 0) {
-                df_combo_box_->setText(new_filter);
-            } else {
-                df_combo_box_->clear();
-            }
-        }
-        // Only after the display filter has been updated,
-        // disable the arrow button
         emit displayFilterSuccess(true);
     } else {
         emit displayFilterSuccess(false);

ui/qt/main_window.cpp
@@ -220,6 +220,14 @@ void MainWindow::setDisplayFilter(QString filter, FilterAction::Action action, F
     emit filterAction(filter, action, filterType);
 }
 
+void MainWindow::filterPackets(QString filter, bool)
+{
+    // Ignore the force parameter. We could add a new action type, and make
+    // the main DisplayFilterEntry check. (Note a filter can have the same text
+    // but compile differently if field references, macros, etc. have changed.)
+    emit filterAction(filter, FilterAction::ActionApply, FilterAction::ActionTypePlain);
+}
+
 /*
  * Used for registering custom packet menus
  *

ui/qt/main_window.h
@@ -167,11 +167,13 @@ public slots:
     void setDisplayFilter(QString filter, FilterAction::Action action, FilterAction::ActionType filterType);
 
     /**
-     * @brief Filters the packets.
-     * @param new_filter The filter string.
-     * @param force True to force the filter application.
+     * @brief Sets and applies a new display filter to the open capture file.
+     * @param new_filter New filter expression; empty string shows all packets.
+     * @param force      @c true to reapply even if the filter is unchanged.
+     *
+     * @note The force parameter is currently ignored.
      */
-    virtual void filterPackets(QString new_filter, bool force) = 0;
+    void filterPackets(QString new_filter = QString(), bool force = true);
 
     /**
      * @brief Shows the preferences dialog for a specific module.
@@ -403,6 +405,13 @@ protected slots:
      * @brief Updates the main title bar text.
      */
     void updateTitlebar();
+
+    /**
+     * @brief Applies a new display filter to the open capture file.
+     * @param new_filter New filter expression; empty string shows all packets.
+     * @param force      @c true to reapply even if the filter is unchanged.
+     */
+    virtual void applyFilter(QString new_filter, bool force) = 0;
 
 private:
     /**

ui/qt/wireshark_main_window.cpp
@@ -455,6 +455,8 @@ WiresharkMainWindow::WiresharkMainWindow(QWidget *parent) :
 
     df_combo_box_ = new DisplayFilterEntry(this);
 
+    connect(df_combo_box_, &DisplayFilterEntry::filterPackets, this, &WiresharkMainWindow::applyFilter);
+
     funnel_statistics_ = new FunnelStatistics(this, capture_file_);
     connect(df_combo_box_, &QLineEdit::textChanged, funnel_statistics_, &FunnelStatistics::displayFilterTextChanged);
     connect(funnel_statistics_, &FunnelStatistics::setDisplayFilter, this, &WiresharkMainWindow::setDisplayFilter);

ui/qt/wireshark_main_window.h
@@ -570,12 +570,6 @@ public slots:
      */
     bool openCaptureFile(QString cf_path = QString(), QString display_filter = QString()) { return openCaptureFile(cf_path, display_filter, WTAP_TYPE_AUTO); }
 
-    /**
-     * @brief Applies a new display filter to the open capture file.
-     * @param new_filter New filter expression; empty string shows all packets.
-     * @param force      @c true to reapply even if the filter is unchanged.
-     */
-    void filterPackets(QString new_filter = QString(), bool force = false) override;
 
     /** @brief Repositions and resizes toolbars according to current layout settings. */
     void layoutToolbars();
@@ -705,6 +699,14 @@ public slots:
      * @param stream_ids Vector of RTP stream identifiers to deselect.
      */
     void rtpStreamsDialogDeselectRtpStreams(QVector<rtpstream_id_t *> stream_ids);
+
+protected slots:
+    /**
+     * @brief Applies a new display filter to the open capture file.
+     * @param new_filter New filter expression; empty string shows all packets.
+     * @param force      @c true to reapply even if the filter is unchanged.
+     */
+    void applyFilter(QString new_filter = QString(), bool force = false) override;
 
 private slots:
     /**

ui/stratoshark/stratoshark_main_window.cpp
@@ -408,6 +408,8 @@ StratosharkMainWindow::StratosharkMainWindow(QWidget *parent) :
     updateRecentCaptures();
     df_combo_box_ = new DisplayFilterEntry(this);
 
+    connect(df_combo_box_, &DisplayFilterEntry::filterPackets, this, &StratosharkMainWindow::applyFilter);
+
     funnel_statistics_ = new FunnelStatistics(this, capture_file_);
     connect(df_combo_box_, &QLineEdit::textChanged, funnel_statistics_, &FunnelStatistics::displayFilterTextChanged);
     connect(funnel_statistics_, &FunnelStatistics::setDisplayFilter, this, &StratosharkMainWindow::setDisplayFilter);

commit f5e10372cdbf8772c659c58244875a39db492955

Qt: Rework Follow Stream Dialog
The Follow Stream dialog relied on updatePackets always completing
immediately (and taking up the entire thread) instead of being
event driven like other dialogs, so 6f85fc6895911b8e369cdddbdc932efe4af1dee1
broke it. Switch it to event driven like the other dialogs (though
it still removes its tap after completion, and only responds to its
own taps.)

Instead of always replacing the main display filter with the follow
filter, replace the "Filter Out This Stream" button with "Prepare as
Filter" and "Apply as Filter" buttons, as used elsewhere in the
application. Have the "Back" button close the window and restore the
main filter to the original filter when the window was opened, if
changed (instead of sometimes the previously filtered out filter.)

Also fixes #16290.

epan/prefs.c
@@ -3288,11 +3288,7 @@ prefs_register_modules(void)
     prefs_register_obsolete_preference(gui_module, "tree_view_altern_colors");
     prefs_register_obsolete_preference(gui_module, "expert_composite_eyecandy");
     prefs_register_obsolete_preference(gui_module, "filter_toolbar_show_in_statusbar");
-
-    prefs_register_bool_preference(gui_module, "restore_filter_after_following_stream",
-                                   "Restore current display filter after following a stream",
-                                   "Restore current display filter after following a stream?",
-                                   &prefs.restore_filter_after_following_stream);
+    prefs_register_obsolete_preference(gui_module, "restore_filter_after_following_stream");
 
     prefs_register_obsolete_preference(gui_module, "protocol_tree_line_style");
 
@@ -4299,7 +4295,6 @@ prefs_set_global_defaults(wmem_allocator_t* pref_scope, const char** col_fmt, in
     char        *col_name;
     fmt_data    *cfmt;
 
-    prefs.restore_filter_after_following_stream = false;
     prefs.gui_toolbar_main_style = TB_STYLE_ICONS;
     /* We try to find the best font in the Qt code */
     wmem_free(pref_scope, prefs.gui_font_name);

epan/prefs.h
@@ -214,8 +214,6 @@ typedef struct _e_prefs {
     GList        *col_list;                  /**< Ordered list of packet list column definitions */
     unsigned      num_cols;                  /**< Number of entries in @ref col_list */
 
-    bool          restore_filter_after_following_stream; /**< If true, restore the previous display filter after closing a stream follow dialog */
-
     /* GUI appearance */
     int           gui_toolbar_main_style;    /**< Style of the main toolbar (icon size/text) */
     char         *gui_font_name;             /**< Name and size of the font used in the packet list and details pane */

ui/qt/follow_stream_dialog.cpp
@@ -36,6 +36,7 @@
 
 #include <QElapsedTimer>
 #include <QKeyEvent>
+#include <QMenu>
 #include <QMessageBox>
 #include <QMutex>
 #include <QPrintDialog>
@@ -60,11 +61,13 @@ static bool isReadRunning;
 
 Q_DECLARE_METATYPE(bytes_show_type)
 
-FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id) :
+FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id, const QString& previous_filter) :
     WiresharkDialog(parent, cf),
     ui(new Ui::FollowStreamDialog),
     b_find_(NULL),
     follower_(NULL),
+    previous_filter_(previous_filter),
+    follow_filter_(),
     client_buffer_count_(0),
     server_buffer_count_(0),
     client_packet_count_(0),
@@ -114,8 +117,19 @@ FollowStreamDialog::FollowStreamDialog(QWidget &parent, CaptureFile &cf, int pro
 
     ui->deltaComboBox->setCurrentIndex(recent.gui_follow_delta);
 
-    b_filter_out_ = ui->buttonBox->addButton(tr("Filter Out This Stream"), QDialogButtonBox::ActionRole);
-    connect(b_filter_out_, &QPushButton::clicked, this, &FollowStreamDialog::filterOut);
+    QMenu *filter_menu = new QMenu(this);
+    connect(filter_menu, &QMenu::aboutToShow, this, [this, filter_menu]() {
+        this->filterMenuAboutToShow(filter_menu, true);
+    });
+    b_filter_prepare_ = ui->buttonBox->addButton(tr("Prepare as Filter"), QDialogButtonBox::ActionRole);
+    b_filter_prepare_->setMenu(filter_menu);
+
+    filter_menu = new QMenu(this);
+    connect(filter_menu, &QMenu::aboutToShow, this, [this, filter_menu]() {
+        this->filterMenuAboutToShow(filter_menu, false);
+    });
+    b_filter_apply_ = ui->buttonBox->addButton(tr("Apply as Filter"), QDialogButtonBox::ActionRole);
+    b_filter_apply_->setMenu(filter_menu);
 
     b_print_ = ui->buttonBox->addButton(tr("Print"), QDialogButtonBox::ActionRole);
     connect(b_print_, &QPushButton::clicked, this, &FollowStreamDialog::printStream);
@@ -239,6 +253,13 @@ void FollowStreamDialog::goToPacketForTextPos(int pkt)
     }
 }
 
+void FollowStreamDialog::filterMenuAboutToShow(QMenu *menu, bool prepare)
+{
+    menu->clear();
+    QActionGroup * group = FilterAction::createFilterGroup(follow_filter_, prepare, !follow_filter_.isEmpty(), menu);
+    menu->addActions(group->actions());
+}
+
 void FollowStreamDialog::updateWidgets(bool follow_in_progress)
 {
     // XXX: If follow_in_progress set cursor to Qt::BusyCursor or WaitCursor,
@@ -257,7 +278,8 @@ void FollowStreamDialog::updateWidgets(bool follow_in_progress)
     }
     ui->leFind->setEnabled(enable);
     ui->bFind->setEnabled(enable);
-    b_filter_out_->setEnabled(enable);
+    b_filter_prepare_->setEnabled(enable);
+    b_filter_apply_->setEnabled(enable);
     b_print_->setEnabled(enable);
     b_save_->setEnabled(enable);
 
@@ -351,23 +373,12 @@ void FollowStreamDialog::backButton()
     close();
 }
 
-void FollowStreamDialog::filterOut()
-{
-    if (terminating_)
-        return;
-
-    output_filter_ = filter_out_filter_;
-
-    close();
-}
-
 void FollowStreamDialog::close()
 {
     terminating_ = true;
 
     // Update filter - Use:
-    //     previous_filter if 'Close' (passed in follow() method)
-    //     filter_out_filter_ if 'Filter Out This Stream' (built by appending !current_stream to previous_filter)
+    //     previous_filter if 'Back' (passed in constructor)
     //     leave filter alone if window closed. (current stream)
     emit updateFilter(output_filter_, true);
 
@@ -479,7 +490,7 @@ void FollowStreamDialog::streamNumberSpinBoxValueChanged(int stream_num)
     }
 
     if (stream_num >= 0 && ok) {
-        follow(previous_filter_, true, stream_num, sub_stream_num);
+        follow(true, stream_num, sub_stream_num);
         previous_sub_stream_num_ = sub_stream_num;
     }
 }
@@ -518,7 +529,7 @@ void FollowStreamDialog::subStreamNumberSpinBoxValueChanged(int sub_stream_num)
     sub_stream_num = static_cast<int>(sub_stream_num_new);
 
     if (ok) {
-        follow(previous_filter_, true, stream_num, sub_stream_num);
+        follow(true, stream_num, sub_stream_num);
         previous_sub_stream_num_ = sub_stream_num;
     }
 }
@@ -1010,12 +1021,8 @@ QString FollowStreamDialog::bothDirectionsString() const
             FORMAT_SIZE_UNIT_BYTES, FORMAT_SIZE_PREFIX_SI)));
 }
 
-bool FollowStreamDialog::follow(QString previous_filter, bool use_stream_index, unsigned stream_num, unsigned sub_stream_num)
+bool FollowStreamDialog::follow(bool use_stream_index, unsigned stream_num, unsigned sub_stream_num)
 {
-    QString             follow_filter;
-    QString             server_to_client_string;
-    QString             client_to_server_string;
-    QString             both_directions_string;
     bool                is_follower = false;
     int                 stream_count;
     follow_stream_count_func stream_count_func = NULL;
@@ -1043,11 +1050,11 @@ bool FollowStreamDialog::follow(QString previous_filter, bool use_stream_index, 
     /* Create a new filter that matches all packets in the TCP stream,
         and set the display filter entry accordingly */
     if (use_stream_index) {
-        follow_filter = gchar_free_to_qstring(get_follow_index_func(follower_)(stream_num, sub_stream_num));
+        follow_filter_ = gchar_free_to_qstring(get_follow_index_func(follower_)(stream_num, sub_stream_num));
     } else {
-        follow_filter = gchar_free_to_qstring(get_follow_conv_func(follower_)(cap_file_.capFile()->edt, &cap_file_.capFile()->edt->pi, &stream_num, &sub_stream_num));
+        follow_filter_ = gchar_free_to_qstring(get_follow_conv_func(follower_)(cap_file_.capFile()->edt, &cap_file_.capFile()->edt->pi, &stream_num, &sub_stream_num));
     }
-    if (follow_filter.isEmpty()) {
+    if (follow_filter_.isEmpty()) {
         // XXX: This error probably has to do with tunneling (#18231), where
         // the addresses or ports changed after the TCP or UDP layer.
         // (The appropriate layer must be present, or else the GUI
@@ -1058,23 +1065,12 @@ bool FollowStreamDialog::follow(QString previous_filter, bool use_stream_index, 
         return false;
     }
 
-    previous_filter_ = previous_filter;
-    /* append the negation */
-    if (!previous_filter.isEmpty()) {
-        filter_out_filter_ = QStringLiteral("%1 and !(%2)")
-                .arg(previous_filter, follow_filter);
-    }
-    else
-    {
-        filter_out_filter_ = QStringLiteral("!(%1)").arg(follow_filter);
-    }
-
     follow_info_.stream_id = stream_num;
     follow_info_.substream_id = sub_stream_num;
 
     /* data will be passed via tap callback*/
     if (!registerTapListener(get_follow_tap_string(follower_), &follow_info_,
-                                follow_filter.toUtf8().constData(),
+                                follow_filter_.toUtf8().constData(),
                                 0, FollowStreamDialog::resetStream,
                                 get_follow_tap_handler(follower_), NULL)) {
         return false;
@@ -1122,58 +1118,34 @@ bool FollowStreamDialog::follow(QString previous_filter, bool use_stream_index, 
         ui->subStreamNumberLabel->setVisible(false);
     }
 
-    beginRetapPackets();
     updateWidgets(true);
 
-    /* Run the display filter so it goes in effect - even if it's the
-       same as the previous display filter. */
-    /* XXX: This forces a cf_filter_packets() - but if a rescan (or something else
-     * that sets cf->read_lock) is in progress, this will queue the filter
-     * and return immediately. It will also cause a rescan in progress to
-     * stop and restart with the new filter. That also applies to this rescan;
-     * changing the main display filter (from the main window, or from, e.g.
-     * another FollowStreamDialog) will cause this to restart and reset the
-     * tap.
-     *
-     * Other tapping dialogs call cf_retap_packets (which retaps but doesn't
-     * set the main display filter, freeze the packet list, etc.), which
-     * has somewhat different behavior when another dialog tries to retap,
-     * but also results in the taps being reset mid tap.
-     *
-     * Either way, we should be event driven and listening for CaptureEvents
-     * instead of drawing after this returns. (Or like other taps, draw
-     * periodically in a callback, provided that can be done without causing
-     * issues with changing the Decode As type.)
-     */
-    emit updateFilter(follow_filter, true);
-
-    removeTapListeners();
-
-    server_to_client_string = serverToClientString();
-    client_to_server_string = clientToServerString();
-    both_directions_string = bothDirectionsString();
-
     setWindowSubtitle(tr("Follow %1 Stream (%2)").arg(proto_get_protocol_short_name(find_protocol_by_id(get_follow_proto_id(follower_))),
-                                                follow_filter));
-
-    ui->cbDirections->blockSignals(true);
-    ui->cbDirections->clear();
-    ui->cbDirections->addItem(both_directions_string);
-    ui->cbDirections->addItem(client_to_server_string);
-    ui->cbDirections->addItem(server_to_client_string);
-    ui->cbDirections->blockSignals(false);
-
-    followStream();
-    fillHintLabel();
-
-    updateWidgets(false);
-    endRetapPackets();
-
-    if (prefs.restore_filter_after_following_stream) {
-        emit updateFilter(previous_filter_, true);
-    }
+                                                follow_filter_));
+    cap_file_.retapPackets();
 
     return true;
+}
+
+void FollowStreamDialog::endRetapPackets()
+{
+    if (listening()) {
+        removeTapListeners();
+
+        ui->cbDirections->blockSignals(true);
+        ui->cbDirections->clear();
+        ui->cbDirections->addItem(bothDirectionsString());
+        ui->cbDirections->addItem(clientToServerString());
+        ui->cbDirections->addItem(serverToClientString());
+        ui->cbDirections->blockSignals(false);
+
+        followStream();
+        fillHintLabel();
+
+        updateWidgets(false);
+    }
+
+    WiresharkDialog::endRetapPackets();
 }
 
 void FollowStreamDialog::captureFileClosed()

ui/qt/follow_stream_dialog.h
@@ -46,8 +46,9 @@ public:
      * @param parent The parent widget.
      * @param cf The capture file containing the stream data.
      * @param proto_id The protocol ID of the stream to follow.
+     * @param previous_filter The main display filter at the time of creation
      */
-    explicit FollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id);
+    explicit FollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id, const QString& previous_filter);
 
     /**
      * @brief Destroys the FollowStreamDialog.
@@ -62,13 +63,12 @@ public:
 
     /**
      * @brief Initiates the stream following process.
-     * @param previous_filter An optional previous display filter to restore later.
      * @param use_stream_index True to follow using a specific stream index rather than the selected packet.
      * @param stream_num The specific stream number to follow (if use_stream_index is true).
      * @param sub_stream_num The specific sub-stream number (e.g., HTTP2 streams within a TCP connection).
      * @return True if the stream was successfully followed, false otherwise.
      */
-    bool follow(QString previous_filter = QString(), bool use_stream_index = false, unsigned stream_num = 0, unsigned sub_stream_num = 0);
+    bool follow(bool use_stream_index = false, unsigned stream_num = 0, unsigned sub_stream_num = 0);
 
 protected:
     /**
@@ -77,18 +77,20 @@ protected:
      * @param event The event to filter.
      * @return True if the event was handled, false otherwise.
      */
-    bool eventFilter(QObject *obj, QEvent *event);
+    bool eventFilter(QObject *obj, QEvent *event) override;
 
     /**
      * @brief Handles key press events in the dialog.
      * @param event The key event.
      */
-    void keyPressEvent(QKeyEvent *event);
+    void keyPressEvent(QKeyEvent *event) override;
+
+    void endRetapPackets() override;
 
     /**
      * @brief Slot triggered when the underlying capture file is closed.
      */
-    void captureFileClosed();
+    void captureFileClosed() override;
 
     /**
      * @brief Generates a hint label based on the current packet.
@@ -185,11 +187,6 @@ private slots:
     void close();
 
     /**
-     * @brief Slot triggered to apply a filter excluding the current stream.
-     */
-    void filterOut();
-
-    /**
      * @brief Slot triggered to toggle regex search mode.
      * @param use_regex True to enable regex search, false for plain text.
      */
@@ -280,7 +277,7 @@ private:
     /**
      * @brief Updates the states of the dialog widgets.
      */
-    void updateWidgets() { updateWidgets(false); } // Needed for WiresharkDialog?
+    void updateWidgets() override { updateWidgets(false); } // Needed for WiresharkDialog?
 
     /**
      * @brief Appends a buffer of stream data to the text display.
@@ -318,11 +315,16 @@ private:
      */
     void addText(QString text, bool is_from_server, uint32_t packet_num, bool colorize = true);
 
+    void filterMenuAboutToShow(QMenu *, bool);
+
     /** Pointer to the generated UI elements. */
     Ui::FollowStreamDialog  *ui;
 
-    /** Pointer to the "Filter Out" button. */
-    QPushButton             *b_filter_out_;
+    /** Pointer to the "Prepare as Filter" button. */
+    QPushButton             *b_filter_prepare_;
+
+    /** Pointer to the "Apply as Filter" button. */
+    QPushButton             *b_filter_apply_;
 
     /** Pointer to the "Find" button. */
     QPushButton             *b_find_;
@@ -345,11 +347,11 @@ private:
     /** The display filter active before the dialog was opened. */
     QString                 previous_filter_;
 
-    /** The filter string used to exclude this stream. */
-    QString                 filter_out_filter_;
+    /** The display string applied, if any, when closing the dialog. */
+    QString                 output_filter_;
 
     /** The filter string representing this specific stream. */
-    QString                 output_filter_;
+    QString                 follow_filter_;
 
     /** Counter for buffers received from the client. */
     int                     client_buffer_count_;

ui/qt/wireshark_dialog.h
@@ -145,6 +145,8 @@ protected:
     /** @brief Flag indicating if the capture file has been closed. */
     bool file_closed_;
 
+    bool listening() const { return !tap_listeners_.isEmpty(); }
+
     /**
      * @brief Check to see if the user has closed (and not minimized) the dialog.
      * @return true if the dialog has been closed, false otherwise.

ui/qt/wireshark_main_window_slots.cpp
@@ -3345,7 +3345,7 @@ void WiresharkMainWindow::applyExportObject()
 }
 
 void WiresharkMainWindow::openFollowStreamDialog(int proto_id, unsigned stream_num, unsigned sub_stream_num, bool use_stream_index) {
-    FollowStreamDialog *fsd = new FollowStreamDialog(*this, capture_file_, proto_id);
+    FollowStreamDialog *fsd = new FollowStreamDialog(*this, capture_file_, proto_id, getFilter());
     connect(fsd, &FollowStreamDialog::updateFilter, this, &WiresharkMainWindow::filterPackets);
     connect(fsd, &FollowStreamDialog::goToPacket, this, [=](int packet_num) {packet_list_->goToPacket(packet_num);});
     fsd->addCodecs(text_codec_map_);
@@ -3353,9 +3353,9 @@ void WiresharkMainWindow::openFollowStreamDialog(int proto_id, unsigned stream_n
     if (use_stream_index) {
         // If a specific conversation was requested, then ignore any previous
         // display filters and display all related packets.
-        fsd->follow("", true, stream_num, sub_stream_num);
+        fsd->follow(true, stream_num, sub_stream_num);
     } else {
-        fsd->follow(getFilter());
+        fsd->follow();
     }
 }
 

ui/stratoshark/stratoshark_follow_stream_dialog.cpp
@@ -12,8 +12,8 @@
 #include <ui/qt/utils/theme_manager.h>
 
 
-StratosharkFollowStreamDialog::StratosharkFollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id) :
-    FollowStreamDialog(parent, cf, proto_id)
+StratosharkFollowStreamDialog::StratosharkFollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id, const QString& previous_filter) :
+    FollowStreamDialog(parent, cf, proto_id, previous_filter)
 {
 }
 

ui/stratoshark/stratoshark_follow_stream_dialog.h
@@ -31,8 +31,9 @@ public:
      * @param parent The parent widget.
      * @param cf The capture file.
      * @param proto_id The protocol identifier to follow.
+     * @param previous_filter The main display filter at time of creation
      */
-    explicit StratosharkFollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id);
+    explicit StratosharkFollowStreamDialog(QWidget &parent, CaptureFile &cf, int proto_id, const QString& previous_filter);
 
     /**
      * @brief Destroys the StratosharkFollowStreamDialog object.

ui/stratoshark/stratoshark_main_window_slots.cpp
@@ -2951,7 +2951,7 @@ void StratosharkMainWindow::applyConversationFilter()
 }
 
 void StratosharkMainWindow::openFollowStreamDialog(int proto_id, unsigned stream_num, unsigned sub_stream_num, bool use_stream_index) {
-    FollowStreamDialog *fsd = new StratosharkFollowStreamDialog(*this, capture_file_, proto_id);
+    FollowStreamDialog *fsd = new StratosharkFollowStreamDialog(*this, capture_file_, proto_id, getFilter());
     connect(fsd, &FollowStreamDialog::updateFilter, this, &StratosharkMainWindow::filterPackets);
     connect(fsd, &FollowStreamDialog::goToPacket, this, [=](int packet_num) {packet_list_->goToPacket(packet_num);});
     fsd->addCodecs(text_codec_map_);
@@ -2959,9 +2959,9 @@ void StratosharkMainWindow::openFollowStreamDialog(int proto_id, unsigned stream
     if (use_stream_index) {
         // If a specific conversation was requested, then ignore any previous
         // display filters and display all related packets.
-        fsd->follow("", true, stream_num, sub_stream_num);
+        fsd->follow(true, stream_num, sub_stream_num);
     } else {
-        fsd->follow(getFilter());
+        fsd->follow();
     }
 }
 