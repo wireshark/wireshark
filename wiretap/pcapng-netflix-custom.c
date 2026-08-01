8a3cbd7b91f26dfad9a8c5bda758418b2299a96f
Chuck Craft <bubbasnmp@gmail.com>
2026-08-01 12:39:29 +0000

    bblog: check for truncated option block
    Fixes: #21460
    
    
    (cherry picked from commit c72babdb036b2c1f9a3efb0a9ba5e188e224c73f)
    
    Co-authored-by: Chuck Craft <bubbasnmp@gmail.com>

--- wiretap/pcapng-netflix-custom.c
+++ wiretap/pcapng-netflix-custom.c
@@ -190,6 +190,9 @@ pcapng_process_nflx_custom_option(wtapng_block_t *wblock,
         break;
     case NFLX_OPT_TYPE_TCPINFO:
         ws_debug("BBLog tcpinfo of length: %u", length);
+        if (length < 2 * sizeof(uint64_t)) {
+            return false;
+        }
         if (wblock->type == BLOCK_TYPE_CB_COPY) {
             /*
              * This is in a BBlog custom block; we append the option's
