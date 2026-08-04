dc39ff806fb2561f989b069cd18208d5bb59a91e
Chuck Craft <bubbasnmp@gmail.com>
2026-08-04 15:10:40 -0500

    daintree: validate timestamp fraction
    Fixes #21473

--- wiretap/daintree-sna.c
+++ wiretap/daintree-sna.c
@@ -151,10 +151,10 @@ static bool
 daintree_sna_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, int *err, char **err_info)
 {
 	uint64_t seconds;
-	int useconds;
+	char useconds[10];
 	char readLine[DAINTREE_MAX_LINE_SIZE];
 	uint8_t readData[READDATA_BUF_SIZE];
-	uint8_t*str = (uint8_t*)readData;
+	uint8_t *str = (uint8_t *)readData;
 	unsigned bytes;
 	uint8_t *p;
 
@@ -171,11 +171,16 @@ daintree_sna_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, int *err, char **e
 	rec->block = wtap_block_create(WTAP_BLOCK_PACKET);
 	rec->presence_flags = WTAP_HAS_TS|WTAP_HAS_CAP_LEN;
 
-	if (sscanf(readLine, "%*s %18" SCNu64 ".%9d %9u %" READDATA_MAX_FIELD_SIZE "s",
-	    &seconds, &useconds, &rec->rec_header.packet_header.len, readData) != 4) {
+	memset(useconds, 0, sizeof(useconds));
+	if (sscanf(readLine, "%*s %18" SCNu64 ".%9[0-9] %9u %" READDATA_MAX_FIELD_SIZE "s",
+	    &seconds, useconds, &rec->rec_header.packet_header.len, readData) != 4) {
 		*err = WTAP_ERR_BAD_FILE;
 		*err_info = g_strdup("daintree_sna: invalid read record");
 		return false;
+	}
+
+	while (strlen(useconds) < 9) {
+		useconds[strlen(useconds)] = '0';
 	}
 
 	/* Daintree doesn't store the FCS, but pads end of packet with 0xffff, which we toss */
@@ -188,7 +193,7 @@ daintree_sna_read_packet(wtap *wth, FILE_T fh, wtap_rec *rec, int *err, char **e
 	rec->rec_header.packet_header.len -= FCS_LENGTH;
 
 	rec->ts.secs = (time_t) seconds;
-	rec->ts.nsecs = useconds * 1000; /* convert mS to nS */
+	sscanf(useconds, "%9u", &rec->ts.nsecs);
 
 	/*
 	 * READDATA_BUF_SIZE is < WTAP_MAX_PACKET_SIZE_STANDARD, and is the maximum
