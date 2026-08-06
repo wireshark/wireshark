f2429bb23a74f8f38cd1a60ae7107fc0a984ec1a
Pascal Quantin <pascal@wireshark.org>
2026-08-06 16:16:52 +0200

    NAS 5GS: fix dissection of type 1 service-level-AA parameter
    Fixes #21479
    
    (cherry picked from commit 077d999dff577ba4c60dff981e5e448cac8a7fc3)
    
    
    (cherry picked from commit 61469331d74488b63be76dd114b32eb408510250)
    
    Co-authored-by: Pascal Quantin <pascal@wireshark.org>

--- epan/dissectors/packet-nas_5gs.c
+++ epan/dissectors/packet-nas_5gs.c
@@ -6970,14 +6970,12 @@ de_nas_5gs_cmn_service_level_aa_cont(tvbuff_t *tvb, proto_tree *tree, packet_inf
         uint32_t type, param_len, param_offset = curr_offset;
 
         subtree = proto_tree_add_subtree_format(tree, tvb, curr_offset, -1, ett_nas_5gs_cmn_service_level_aa_cont_param, &item, "Service-level-AA parameter %d", i + 1);
-        type = tvb_get_uint8(tvb, curr_offset);
+        proto_tree_add_item_ret_uint(subtree, hf_nas_5gs_cmn_service_level_aa_param_type, tvb, curr_offset, 1, ENC_BIG_ENDIAN, &type);
+        curr_offset++;
         if ((type & NAS_5GS_TV_IE_MASK) == NAS_5GS_TV_IE_VAL) {
             param_len = 0;
             type &= 0xf0;
-        }
-        proto_tree_add_uint(subtree, hf_nas_5gs_cmn_service_level_aa_param_type, tvb, curr_offset, 1, type);
-        curr_offset++;
-        if ((type & NAS_5GS_TLV_E_IE_MASK) == NAS_5GS_TLV_E_IE_VAL) {
+        } else if ((type & NAS_5GS_TLV_E_IE_MASK) == NAS_5GS_TLV_E_IE_VAL) {
             proto_tree_add_item_ret_uint(subtree, hf_nas_5gs_cmn_service_level_aa_param_len, tvb, curr_offset, 2, ENC_BIG_ENDIAN, &param_len);
             curr_offset += 2;
         } else {
