481d70b23cfdd961285c28ccd2c078e1864d4207
John Thacker <johnthacker@gmail.com>
2026-08-08 09:53:51 -0400

    docs: Update path in extcap_example wrapper script
    If there's no path, the current working directory is used, which is
    probably not correct (it's probably the directory that has the Wireshark
    executable, not the extcap path.) Use the current path of the batch file
    instead of the current working directory by default, and add some
    comments.

--- doc/extcap_example.bat
+++ doc/extcap_example.bat
@@ -4,4 +4,7 @@ set PYTHONPATH=%~dp0
 set PYTHON=py
 WHERE %PYTHON% >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 set PYTHON=python
-%PYTHON% extcap_example.py %*
+REM Replace "%~dp0" with the path to the script if located elsewhere.
+REM Note that the current working directory is likely the Wireshark directory,
+REM not the extcap directory.
+%PYTHON% %~dp0extcap_example.py %*
