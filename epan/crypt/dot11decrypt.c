13e2d35799bf215f9e9d79b925fbeb217e5748c3
John Thacker <johnthacker@gmail.com>
2026-09-13 00:24:37 -0400

    IEEE 802.11: Save the FTE element for dot11decrypt after length check
    For certain elements, data is saved off as proto_data for the
    dot11decrypt functions. Most of such elements are those that *SHOULD*
    only appear once; if they appear more than once, rather than marking
    an expert info, the last element generally replaces the previous one
    in proto data. Certain elements can have "one or more" present; it
    does not appear that any of those are the ones saved as proto_data.
    For purposes of calculating the MIC, the entire element, including
    tag number and tag length, is concatenated, so the data is saved
    before the tag dissector is called. (It is also before defragmentation,
    though those elements, according to Table 9-92 Element IDs of IEEE
    802.11-2020, are not fragmentable.)
    
    *The* Fast BSS Transition Element (FTE) is one that MUST only appear
    once in a frame body if it appears. It also saves to proto_data some
    parsed data needed for the MIC calculation from later in the element.
    That is by necessity after a length check.
    
    This means that in the case of a short FTE, the full element is saved
    to FTE_TAG_KEY is saved but not the other proto data from the FTE. Worse,
    if a frame illegally contains multiple FTE elements, an earlier one which
    is of a normal size and a second one which is short, there is an
    inconsistency between the data from the FTE_TAG_KEY and the data from
    the FTE_MIC_LEN_KEY, as the former but not the latter will be copied
    from a short element.
    
    To prevent that, don't copy the FTE if it is too short.
    
    In addition, add some extra checks in Dot11DecryptFtMicCheck for the
    RSNE, MDE, or FTE being missing, as might somehow occur in a bogus
    Authentication, Reassociation Request, or Reassociation Response frame.
    
    Fix #21564
    
    Thanks to Anthropic and Ada Logics for the report.
    
    
    (cherry picked from commit 6971deb86e2ec687f1a86abef0988be0594d14ea)
    
    Co-authored-by: John Thacker <johnthacker@gmail.com>

--- epan/crypt/dot11decrypt.c
+++ epan/crypt/dot11decrypt.c
@@ -2111,6 +2111,12 @@ Dot11DecryptFtMicCheck(
     uint8_t zeros[16] = { 0 };
     gcry_mac_hd_t handle;
 
+    if (!assoc_parsed->rsne_tag || !assoc_parsed->mde_tag ||
+        !assoc_parsed->fte_tag) {
+        ws_debug("RSNE, MDE, or FTE not present");
+        return DOT11DECRYPT_RET_UNSUCCESS;
+    }
+
     fte_len = assoc_parsed->fte_tag[1] + 2;
     if (fte_len < 20) {
         ws_debug("FTE too short");
